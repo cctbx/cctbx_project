045a11b9c132c3e5b342a0e59f8ae64f1d8f97f0
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2001-04-13 02:34:03 +0000

    page_installation.dox done.

--- cctbx/examples/python/getting_started.py
+++ cctbx/examples/python/getting_started.py
@@ -3,9 +3,9 @@
 import sgtbx
 import uctbx
 
-UnitCell = uctbx.UnitCell((10, 10, 15, 90, 90, 120))
+UnitCell = uctbx.UnitCell((11, 12, 13, 90, 100, 90))
 print UnitCell
-Symbols = sgtbx.SpaceGroupSymbols("P 62 2 2")
+Symbols = sgtbx.SpaceGroupSymbols("C 2")
 SgOps = sgtbx.SgOps(Symbols.Hall())
 SgOps.CheckUnitCell(UnitCell)
 for i in xrange(SgOps.OrderZ()):
