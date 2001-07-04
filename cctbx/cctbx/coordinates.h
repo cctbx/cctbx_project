f40b3bcdf4d983a36d5b59dcf41aec5d2babcc5f
Ralf W. Grosse-Kunstleve <rwgk@cci.lbl.gov>
2001-07-04 21:24:40 +0000

    Documentation

--- cctbx/cctbx/coordinates.h
+++ cctbx/cctbx/coordinates.h
@@ -16,45 +16,69 @@
 
 namespace cctbx {
 
+  //! Class for cartesian (orthogonal, real) coordinates.
+  /*! The template parameter T should be a floating point type
+      (e.g. float or double).
+      <p>
+      See also: class fractional
+   */
   template <class T>
   class cartesian : public boost::array<T, 3> {
     public:
+      //! The elements of the coordinate vector are initialized with 0.
       cartesian() {
         for(std::size_t i=0;i<3;i++) elems[i] = 0;
       }
+      //! The elements of the coordinate vector are copied from v.
       template <class U>
       cartesian(const boost::array<U, 3> v) {
         for(std::size_t i=0;i<3;i++) elems[i] = v[i];
       }
+      //! The elements of the coordinate vector are copied from xyz.
       template <class U>
       cartesian(const U* xyz) {
         for(std::size_t i=0;i<3;i++) elems[i] = xyz[i];
       }
+      //! The elements of the coordinate vector are initialized with x,y,z.
       cartesian(const T& x, const T& y, const T& z) {
         elems[0] = x; elems[1] = y; elems[2] = z;
       }
+      //! Length squared (scalar product) of the coordinate vector.
       inline T Length2() const {
         return (*this) * (*this);
       }
   };
 
+  //! Class for fractional coordinates.
+  /*! The template parameter T should be a floating point type
+      (e.g. float or double).
+      <p>
+      See also: class cartesian
+   */
   template <class T>
   class fractional : public boost::array<T, 3> {
     public:
+      //! The elements of the coordinate vector are initialized with 0.
       fractional() {
         for(std::size_t i=0;i<3;i++) elems[i] = 0;
       }
+      //! The elements of the coordinate vector are copied from v.
       template <class U>
       fractional(const boost::array<U, 3> v) {
         for(std::size_t i=0;i<3;i++) elems[i] = v[i];
       }
+      //! The elements of the coordinate vector are copied from xyz.
       template <class U>
       fractional(const U* xyz) {
         for(std::size_t i=0;i<3;i++) elems[i] = xyz[i];
       }
+      //! The elements of the coordinate vector are initialized with x,y,z.
       fractional(const T& x, const T& y, const T& z) {
         elems[0] = x; elems[1] = y; elems[2] = z;
       }
+      /*! \brief Apply modulus operation such that 0.0 <= x < 1.0
+          for all elements of the coordinate vector.
+       */
       fractional modPositive() const {
         fractional result;
         for(std::size_t i=0;i<3;i++) {
@@ -64,6 +88,9 @@ namespace cctbx {
         }
         return result;
       }
+      /*! \brief Apply modulus operation such that -0.5 < x <= 0.5
+          for all elements of the coordinate vector.
+       */
       fractional modShort() const {
         fractional result;
         for(std::size_t i=0;i<3;i++) {
