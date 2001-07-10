d5efc141c295d51520bd9bf590458de717aee0f1
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2001-07-10 02:21:31 +0000

    Kevin's example convert_ccp4_symop_lib.cpp, convert_ccp4_symop_lib.py

--- cctbx/examples/cpp/Dependencies.py
+++ cctbx/examples/cpp/Dependencies.py
@@ -7,8 +7,11 @@ class write_makefiles(makefile_generator.write_makefiles):
   def dependencies(self):
     self.files = (
       "examples/cpp/getting_started.cpp",
+      "examples/cpp/convert_ccp4_symop_lib.cpp",
+      "examples/python/symop.lib",
     )
 
     self.examples = {
       "getting_started": ("getting_started",),
+      "convert_ccp4_symop_lib": ("convert_ccp4_symop_lib",),
     }
---
+++ cctbx/examples/python/convert_ccp4_symop_lib.py
@@ -0,0 +1,27 @@
+# Example by Kevin Cowtan, 2001. Public Domain.
+# Converted to Python by Ralf W. Grosse-Kunstleve.
+# This little program reads the CCP4 symmetry library file, and interprets
+# the symops. It then returns the Hall code for the spacegroup. Used to
+# create a new library file to allow compatible spacegroup naming between
+# CCP4 and cctbx.
+#
+# usage: convert_ccp4_symop_lib < symop.lib
+
+import sys
+import string
+import sgtbx
+
+while 1:
+  line = sys.stdin.readline()[:-1]
+  flds = string.split(line, None, 2)
+  if (len(flds) == 0): break
+  nspgrp = string.atoi(flds[0]) # read spacegroup number
+  nsym = string.atoi(flds[1]) # read nsym
+  print nspgrp, nsym, flds[2] # print it all
+  sgops = sgtbx.SgOps() # now interpret the symops
+  for i in xrange(nsym):
+    line = sys.stdin.readline()[:-1] # get the i'th symop
+    # print line
+    sgops.expandSMx( sgtbx.RTMx(line) ) # and interpret
+  print sgops.BuildHallSymbol() # now produce the sg symbol
+  print sgops.BuildLookupSymbol()
