dd5b805fddc01d6c31f0f9dec72d5749d0a77bbd
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2002-02-13 07:00:10 +0000

    Andrei Alexandrescu's alignment calculator included for use in small_plain

--- cctbx/cctbx/array_family/small_plain.h
+++ cctbx/cctbx/array_family/small_plain.h
@@ -14,6 +14,7 @@
 #include <algorithm>
 #include <cctbx/array_family/ref.h>
 #include <cctbx/array_family/small_helpers.h>
+#include <cctbx/array_family/auto_allocator.h>
 
 namespace cctbx { namespace af {
 
@@ -24,39 +25,43 @@ namespace cctbx { namespace af {
     public:
       CCTBX_ARRAY_FAMILY_TYPEDEFS
 
-      ElementType elems[N];
-
       CCTBX_ARRAY_FAMILY_SMALL_CONSTRUCTORS(small_plain)
       CCTBX_ARRAY_FAMILY_SMALL_COPY_AND_ASSIGNMENT(small_plain)
-      CCTBX_ARRAY_FAMILY_TAKE_REF(elems, N)
+      CCTBX_ARRAY_FAMILY_TAKE_REF(begin(), N)
 
       size_type size() const { return m_size; }
       bool empty() const { if (size() == 0) return true; return false; }
       static size_type max_size() { return N; }
       static size_type capacity() { return N; }
 
-      CCTBX_ARRAY_FAMILY_BEGIN_END_ETC(elems, m_size)
+      CCTBX_ARRAY_FAMILY_BEGIN_END_ETC(
+        ((ElementType*)(m_elems.buffer)), m_size) // fix this
 
       void swap(small_plain<ElementType, N>& other) {
         std::swap(*this, other);
       }
 
-      void resize(size_type new_size) {
+      void resize(const size_type& new_size,
+                  const ElementType& x = ElementType()) {
         if (new_size > N) throw_range_error();
+        size_type old_size = this->size();
+        if (new_size < old_size) {
+          detail::destroy_array_elements(this->begin()+new_size, this->end());
+        }
         m_size = new_size;
+        if (new_size > old_size) {
+          std::fill(this->begin()+old_size, this->end(), x);
+        }
       }
 
-      void auto_resize(size_type new_size) { resize(new_size); }
-
-      void resize(const size_type& new_size, const ElementType& x) {
-        ElementType* old_end = this->end();
-        this->resize(new_size);
-        if (this->end() > old_end) std::fill(old_end, this->end(), x);
+      void auto_resize(const size_type& new_size, const ElementType& x) {
+        resize(new_size, x);
       }
 
 #     include <cctbx/array_family/push_back_etc.h>
 
     protected:
+      detail::auto_allocator<ElementType, N> m_elems;
       size_type m_size;
   };
 
