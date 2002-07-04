eafe899b4c1d5e95c46ee486d3598ffa4a921ccd
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2002-07-04 02:01:56 +0000

    utils.h renamed to array_utils.h; min_max.h renamed to utils.h; new math::abs() in new utils.h

--- cctbx/include/cctbx/math/utils.h
+++ cctbx/include/cctbx/math/utils.h
@@ -5,49 +5,37 @@
    cctbx/LICENSE.txt for further details.
 
    Revision history:
-     Mar 2002: modified copy of cctbx/maps/utils.h (rwgk)
-     Jan 2002: Created (R.W. Grosse-Kunstleve)
+     2002 Jun: Created (R.W. Grosse-Kunstleve)
  */
 
 #ifndef CCTBX_MATH_UTILS_H
 #define CCTBX_MATH_UTILS_H
 
-#include <cctbx/fixes/cmath>
-#include <cctbx/array_family/reductions.h>
-#include <cctbx/array_family/shared.h>
-
 namespace cctbx { namespace math {
 
-  template <typename FloatType>
-  class array_statistics
+  template <typename NumType>
+  inline
+  NumType
+  abs(const NumType& x) {
+    if (x < NumType(0)) return -x;
+    return x;
+  }
+
+  template <typename NumType1, typename NumType2>
+  NumType1&
+  update_min(NumType1& m, NumType2 const& x)
   {
-    public:
-      array_statistics() {}
-      array_statistics(const af::const_ref<FloatType>& a)
-      {
-        m_min = af::min(a);
-        m_max = af::max(a);
-        m_mean = af::mean(a);
-        af::shared<FloatType> a2(a.begin(), a.end());
-        af::ref<FloatType> a2r = a2.ref();
-        for(std::size_t i=0;i<a2r.size();i++) a2r[i] *= a2r[i];
-        m_mean2 = af::mean(a2r);
-        m_sigma = m_mean2 - m_mean * m_mean;
-        if (m_sigma < FloatType(0)) m_sigma = 0;
-        m_sigma = std::sqrt(m_sigma);
-      }
-      const FloatType& min() const { return m_min; }
-      const FloatType& max() const { return m_max; }
-      const FloatType& mean() const { return m_mean; }
-      const FloatType& mean2() const { return m_mean2; }
-      const FloatType& sigma() const { return m_sigma; }
-    protected:
-      FloatType m_min;
-      FloatType m_max;
-      FloatType m_mean;
-      FloatType m_mean2;
-      FloatType m_sigma;
-  };
+    if (m > x) m = x;
+    return m;
+  }
+
+  template <typename NumType1, typename NumType2>
+  NumType1&
+  update_max(NumType1& m, NumType2 const& x)
+  {
+    if (m < x) m = x;
+    return m;
+  }
 
 }} // namespace cctbx::math
 
