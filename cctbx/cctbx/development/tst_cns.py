23051a980de1646ed3ad6a56e3f9780dd0be940a
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2002-07-13 07:16:20 +0000

    added: test complex phase shifts

--- cctbx/cctbx/development/tst_cns.py
+++ cctbx/cctbx/development/tst_cns.py
@@ -28,7 +28,8 @@ def are_similar_hl(coeff1, coeff2, tolerance=1.e-2):
     if (abs(coeff1[i] - coeff2[i]) > tolerance): return 0
   return 1
 
-def verify_hl(sg_miller_index_set, sg_hl, p1_miller_indices, p1_hl):
+def verify(sg_miller_index_set, sg_fcalc_set, sg_hl,
+           p1_miller_indices, p1_fcalc, p1_hl):
   sgops = sg_miller_index_set.SgOps
   asu = sgtbx.ReciprocalSpaceASU(sg_miller_index_set.SgInfo)
   lookup_dict = make_miller_lookup_dict(sg_miller_index_set.H)
@@ -38,8 +39,10 @@ def verify_hl(sg_miller_index_set, sg_hl, p1_miller_indices, p1_hl):
       h_eq = h_asu.HermitianLayout()
     else:
       h_eq = h_asu.AnomalousLayout()
+    fcalc_asu = h_eq.complex_eq(p1_fcalc[p1_i])
     hl_asu = h_eq.hl_eq(p1_hl[p1_i])
     sg_i = lookup_dict[h_eq.H()]
+    assert abs(sg_fcalc_set.F[sg_i] - fcalc_asu) < 1.e-2
     if (not are_similar_hl(sg_hl[sg_i], hl_asu)):
       print "Error:", sg_miller_index_set.SgInfo.BuildLookupSymbol()
       print sg_miller_index_set.H[sg_i]
@@ -48,7 +51,7 @@ def verify_hl(sg_miller_index_set, sg_hl, p1_miller_indices, p1_hl):
       return # XXX
       raise AssertionError
 
-def write_cns_input(miller_index_set, hl):
+def write_cns_input(miller_index_set, fcalc_set, hl):
   assert miller_index_set.H.size() == hl.size()
   cns_input = make_cns_input.unit_cell(miller_index_set.UnitCell)
   cns_input += make_cns_input.symmetry(miller_index_set.SgOps)
@@ -58,6 +61,7 @@ def write_cns_input(miller_index_set, hl):
     l("  anomalous=false")
   else:
     l("  anomalous=true")
+  l("  declare name=fcalc domain=reciprocal type=complex end")
   l("  declare name=pa domain=reciprocal type=real end")
   l("  declare name=pb domain=reciprocal type=real end")
   l("  declare name=pc domain=reciprocal type=real end")
@@ -71,7 +75,8 @@ def write_cns_input(miller_index_set, hl):
   l("  reflection")
   for i in xrange(miller_index_set.H.size()):
     l(  ("    index %d %d %d" % miller_index_set.H[i])
-      + (" pa=%.6g pb=%.6g pc=%.6g pd=%.6g") % hl[i])
+      + (" fcalc=%.6g %.6g\n" % xutils.f_as_ampl_phase(fcalc_set.F[i], 1))
+      + (" pa=%.6g pb=%.6g pc=%.6g pd=%.6g" % hl[i]))
   l("  end")
   l("  expand")
   l("  write reflections output=\"tmp.hkl\" end")
@@ -83,10 +88,16 @@ def write_cns_input(miller_index_set, hl):
   f.close()
 
 def exercise(SgInfo, d_min=2., friedel_flag=0, verbose=0):
-  xtal = debug_utils.get_compatible_unit_cell(SgInfo, 1000)
+  elements = ("N", "C", "C", "O")
+  xtal = debug_utils.random_structure(
+    SgInfo, elements,
+    volume_per_atom=50.,
+    min_distance=1.5,
+    general_positions_only=0)
   sg_miller_index_set = xutils.build_miller_indices(xtal, friedel_flag, d_min)
+  sg_fcalc_set = xutils.calculate_structure_factors(sg_miller_index_set, xtal)
   sg_hl = generate_random_hl(sg_miller_index_set)
-  write_cns_input(sg_miller_index_set, sg_hl)
+  write_cns_input(sg_miller_index_set, sg_fcalc_set, sg_hl)
   try: os.unlink("tmp.hkl")
   except: pass
   os.system("cns < tmp.cns > tmp.out")
@@ -98,7 +109,10 @@ def exercise(SgInfo, d_min=2., friedel_flag=0, verbose=0):
     print reflection_file
   assert reflection_file.anomalous == (not sg_miller_index_set.friedel_flag)
   p1_miller_indices, p1_hl = reflection_file.join_hl_group()
-  verify_hl(sg_miller_index_set, sg_hl, p1_miller_indices, p1_hl)
+  p1_fcalc_rso = reflection_file.reciprocal_space_objects["FCALC"]
+  assert not shared.join_sets(p1_miller_indices, p1_fcalc_rso.H).have_singles()
+  verify(sg_miller_index_set, sg_fcalc_set, sg_hl,
+    p1_miller_indices, p1_fcalc_rso.data, p1_hl)
 
 def run():
   Flags = debug_utils.command_line_options(sys.argv[1:], (
