1450eb6a203c3353691fb0cc41c953d365ba8c38
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2002-08-02 03:08:46 +0000

    g/setstate_manager (type independent code of shared g/setstate)

--- cctbx/arraytbx/shared_picklers.cpp
+++ cctbx/arraytbx/shared_picklers.cpp
@@ -16,31 +16,84 @@ namespace cctbx { namespace af {
 
   namespace {
 
+    struct getstate_manager
+    {
+      getstate_manager(std::size_t a_size, std::size_t size_per_element)
+      {
+        str_capacity = a_size * size_per_element;
+        str_obj = PyString_FromStringAndSize(
+          0, static_cast<int>(str_capacity + 100));
+        str_begin = PyString_AS_STRING(str_obj);
+        str_end = str_begin;
+        sprintf(str_end, "%lu", static_cast<unsigned long>(a_size));
+        while (*str_end) str_end++;
+        *str_end++ = ' ';
+      };
+
+      void advance()
+      {
+        while (*str_end) str_end++;
+        *str_end++ = ' ';
+        cctbx_assert(str_end - str_begin <= str_capacity);
+      }
+
+      PyObject* finalize()
+      {
+        str_capacity = str_end - str_begin;
+        cctbx_assert(
+          _PyString_Resize(&str_obj, static_cast<int>(str_capacity)) == 0);
+        return str_obj;
+      }
+
+      std::size_t str_capacity;
+      PyObject* str_obj;
+      char* str_begin;
+      char* str_end;
+    };
+
+    struct setstate_manager
+    {
+      setstate_manager(std::size_t a_size, PyObject* state)
+      {
+        cctbx_assert(a_size == 0);
+        str_ptr = PyString_AsString(state);
+        cctbx_assert(str_ptr != 0);
+        cctbx_assert(sscanf(str_ptr, "%lu", &a_capacity) == 1);
+        while (*str_ptr != ' ') str_ptr++;
+        str_ptr++;
+      };
+
+      void advance()
+      {
+        while (*str_ptr != ' ') str_ptr++;
+        str_ptr++;
+      }
+
+      void finalize()
+      {
+        cctbx_assert(*str_ptr == 0);
+      }
+
+      char* str_ptr;
+      std::size_t a_capacity;
+    };
+
     template <typename ElementType>
     struct num_picklers
     {
       static
       boost::python::ref
-      getstate(shared<ElementType> const& a, const char* fmt)
+      getstate(
+        shared<ElementType> const& a,
+        std::size_t size_per_element,
+        const char* fmt)
       {
-        std::size_t str_capacity = a.size() * 18;
-        PyObject* str_obj = PyString_FromStringAndSize(
-          0, static_cast<int>(str_capacity+100));
-        char* str_begin = PyString_AS_STRING(str_obj);
-        char* str_end = str_begin;
-        sprintf(str_end, "%lu", static_cast<unsigned long>(a.size()));
-        while (*str_end) str_end++;
-        *str_end++ = ' ';
+        getstate_manager mgr(a.size(), size_per_element);
         for(std::size_t i=0;i<a.size();i++) {
-          sprintf(str_end, fmt, a[i]);
-          while (*str_end) str_end++;
-          *str_end++ = ' ';
-          cctbx_assert(str_end - str_begin <= str_capacity);
+          sprintf(mgr.str_end, fmt, a[i]);
+          mgr.advance();
         }
-        str_capacity = str_end - str_begin;
-        cctbx_assert(
-          _PyString_Resize(&str_obj, static_cast<int>(str_capacity)) == 0);
-        return boost::python::ref(str_obj);
+        return boost::python::ref(mgr.finalize());
       }
 
       static
@@ -50,22 +103,15 @@ namespace cctbx { namespace af {
         boost::python::ref state,
         const char* fmt)
       {
-        cctbx_assert(a.size() == 0);
-        char* str_ptr = PyString_AsString(state.get());
-        cctbx_assert(str_ptr != 0);
-        std::size_t str_capacity;
-        cctbx_assert(sscanf(str_ptr, "%lu", &str_capacity) == 1);
-        while (*str_ptr != ' ') str_ptr++;
-        str_ptr++;
-        a.reserve(str_capacity);
-        for(std::size_t i=0;i<str_capacity;i++) {
+        setstate_manager mgr(a.size(), state.get());
+        a.reserve(mgr.a_capacity);
+        for(std::size_t i=0;i<mgr.a_capacity;i++) {
           ElementType val;
-          cctbx_assert(sscanf(str_ptr, fmt, &val) == 1);
-          while (*str_ptr != ' ') str_ptr++;
-          str_ptr++;
+          cctbx_assert(sscanf(mgr.str_ptr, fmt, &val) == 1);
+          mgr.advance();
           a.push_back(val);
         }
-        cctbx_assert(*str_ptr == 0);
+        mgr.finalize();
       }
     };
 
@@ -73,7 +119,7 @@ namespace cctbx { namespace af {
 
   boost::python::ref shared_int_getstate(shared<int> const& a)
   {
-    return num_picklers<int>::getstate(a, "%d");
+    return num_picklers<int>::getstate(a, 12, "%d");
   }
 
   void shared_int_setstate(shared<int>& a, boost::python::ref state)
@@ -83,7 +129,7 @@ namespace cctbx { namespace af {
 
   boost::python::ref shared_long_getstate(shared<long> const& a)
   {
-    return num_picklers<long>::getstate(a, "%ld");
+    return num_picklers<long>::getstate(a, 21, "%ld");
   }
 
   void shared_long_setstate(shared<long>& a, boost::python::ref state)
@@ -93,7 +139,7 @@ namespace cctbx { namespace af {
 
   boost::python::ref shared_float_getstate(shared<float> const& a)
   {
-    return num_picklers<float>::getstate(a, "%.6g");
+    return num_picklers<float>::getstate(a, 12, "%.6g");
   }
 
   void shared_float_setstate(shared<float>& a, boost::python::ref state)
@@ -103,7 +149,7 @@ namespace cctbx { namespace af {
 
   boost::python::ref shared_double_getstate(shared<double> const& a)
   {
-    return num_picklers<double>::getstate(a, "%.12g");
+    return num_picklers<double>::getstate(a, 18, "%.12g");
   }
 
   void shared_double_setstate(shared<double>& a, boost::python::ref state)
