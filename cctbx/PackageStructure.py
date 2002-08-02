c4d979cc8c9c600500b7f2639835957c59f11395
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2002-08-02 22:50:39 +0000

    eltbx must be compiled before arraytbx

--- cctbx/PackageStructure.py
+++ cctbx/PackageStructure.py
@@ -2,7 +2,7 @@ class PackageStructure:
   name       = "cctbx"
   supporting = () # other structured packages that provide supporting code
   externals  = ("external/boost_python",)
-  toolboxes  = ("misc", "uctbx", "sgtbx", "miller", "arraytbx",
-               "adptbx", "eltbx", "sftbx", "fftbx", "lbfgs", "dmtbx", "mintbx")
+  toolboxes  = ("misc", "uctbx", "sgtbx", "miller", "eltbx", "arraytbx",
+               "adptbx", "sftbx", "fftbx", "lbfgs", "dmtbx", "mintbx")
   examples   = ("examples/cpp",)
   tbx_subpkg = ("arraytbx","eltbx")
