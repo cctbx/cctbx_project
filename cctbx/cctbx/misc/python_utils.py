88a72a54a732ddcc6ca6041050fb2ebc7c8dfe23
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2002-08-06 19:21:54 +0000

    dict_with_default_value

--- cctbx/cctbx/misc/python_utils.py
+++ cctbx/cctbx/misc/python_utils.py
@@ -2,6 +2,18 @@ def adopt_init_args(obj, args):
   del args["self"]
   obj.__dict__.update(args)
 
+class dict_with_default_value(dict):
+
+  def __init__(self, default_value):
+    self.default_value = default_value
+
+  def __getitem__(self, key):
+    try: return dict.__getitem__(self, key)
+    except: pass
+    val = self.default_value
+    dict.__setitem__(self, key, val)
+    return val
+
 class dict_with_default_factory(dict):
 
   def __init__(self, default_factory):
