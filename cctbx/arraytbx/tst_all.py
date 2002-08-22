19b8333b72bd6d611e3bae74294a99bd991735e7
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2002-08-22 18:00:27 +0000

    arraytbx/tst_all.py

---
+++ cctbx/arraytbx/tst_all.py
@@ -0,0 +1,15 @@
+import os
+
+tests = (
+"tst_af_1",
+"tst_af_2",
+"tst_af_3",
+"tst_mat3",
+"tst_vec3",
+"tst_sym_mat3",
+"python tst_flex.py",
+"python tst_flex_utils.py",
+)
+
+for tst in tests:
+  os.system(tst)
