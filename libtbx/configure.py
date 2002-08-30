b0c6e279ccca71338510eec27115539c9e920863
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2002-08-30 20:16:01 +0000

    emit build.py

--- libtbx/configure.py
+++ libtbx/configure.py
@@ -4,6 +4,10 @@ join = os.path.join
 
 class empty: pass
 
+def open_info(path, mode="w", info="Creating:"):
+  print info, path
+  return open(path, mode)
+
 def insert_normed_path(path_list, addl_path):
   addl_path = norm(addl_path)
   if (not addl_path in path_list):
@@ -11,60 +15,60 @@ def insert_normed_path(path_list, addl_path):
     if (path_list[:1] == ["."]): i = 1
     path_list.insert(i, addl_path)
 
-def update_paths_raw(env, package_name, package_dist):
-  paths_raw_path = norm(join(env.libtbx_build, "paths_raw"))
+def update_libtbx_info(env, package_name, package_dist):
+  libtbx_info_path = norm(join(env.libtbx_build, "libtbx_info"))
   package_dist_varname = package_name.upper() + "_DIST"
-  paths_raw_dict = {
+  libtbx_info = {
     "LD_LIBRARY_PATH": [],
     "PYTHONPATH": [],
     "PATH": [],
     "LIBTBX_PYTHON_BIN": env.python_bin,
     "LIBTBX_DIST": env.libtbx_dist,
     "LIBTBX_BUILD": env.libtbx_build,
-    "LIBTBX_PACKAGES": [],
     package_dist_varname: package_dist,
+    "package_names": [],
   }
   try:
-    f = open(paths_raw_path, "r")
+    f = open(libtbx_info_path, "r")
   except:
-    pass
+    libtbx_info_write_action = "Creating:"
   else:
-    paths_raw_dict.update(eval(" ".join(f.readlines())))
+    libtbx_info_write_action = "Updating:"
+    libtbx_info.update(eval(" ".join(f.readlines())))
     f.close()
-  if (paths_raw_dict["LIBTBX_PYTHON_BIN"] != env.python_bin):
+  if (libtbx_info["LIBTBX_PYTHON_BIN"] != env.python_bin):
     print >> sys.stderr
     print >> sys.stderr, "#" * 78
     print >> sys.stderr, "FATAL ERROR: python executable mismatch:"
-    print >> sys.stderr, "  previously:", paths_raw_dict["LIBTBX_PYTHON_BIN"]
+    print >> sys.stderr, "  previously:", libtbx_info["LIBTBX_PYTHON_BIN"]
     print >> sys.stderr, "         now:", sys.executable
     print >> sys.stderr, "#" * 78
     print >> sys.stderr
     sys.exit(1)
-  assert paths_raw_dict["LIBTBX_DIST"] == env.libtbx_dist
-  assert paths_raw_dict["LIBTBX_BUILD"] == env.libtbx_build
-  assert paths_raw_dict[package_dist_varname] == package_dist
-  insert_normed_path(paths_raw_dict["LD_LIBRARY_PATH"],
+  assert libtbx_info["LIBTBX_DIST"] == env.libtbx_dist
+  assert libtbx_info["LIBTBX_BUILD"] == env.libtbx_build
+  assert libtbx_info[package_dist_varname] == package_dist
+  insert_normed_path(libtbx_info["LD_LIBRARY_PATH"],
     join(env.libtbx_build, "libtbx"))
-  insert_normed_path(paths_raw_dict["PYTHONPATH"],
+  insert_normed_path(libtbx_info["PYTHONPATH"],
     join(env.libtbx_build, "libtbx"))
-  insert_normed_path(paths_raw_dict["PYTHONPATH"],
+  insert_normed_path(libtbx_info["PYTHONPATH"],
     package_dist)
-  insert_normed_path(paths_raw_dict["PATH"],
+  insert_normed_path(libtbx_info["PATH"],
     join(package_dist, package_name, "command_line"))
-  package_names = paths_raw_dict["LIBTBX_PACKAGES"]
+  package_names = libtbx_info["package_names"]
   if (not package_name in package_names):
     package_names.append(package_name)
-  f = open(paths_raw_path, "w")
-  pprint.pprint(paths_raw_dict, f)
+  f = open_info(libtbx_info_path, "w", libtbx_info_write_action)
+  pprint.pprint(libtbx_info, f)
   f.close()
-  return paths_raw_dict
+  return libtbx_info
 
-def emit_env_run_sh(libtbx_build, paths_raw_dict):
+def emit_env_run_sh(libtbx_build, libtbx_info):
   env_run_sh_path = norm(join(libtbx_build, "env_run.sh"))
-  print "Updating:", env_run_sh_path
-  f = open(env_run_sh_path, "w")
-  for var_name, values in paths_raw_dict.items():
-    if (var_name == "LIBTBX_PACKAGES"): continue
+  f = open_info(env_run_sh_path)
+  for var_name, values in libtbx_info.items():
+    if (var_name.upper() != var_name): continue
     if (type(values) == type([])):
       val = os.pathsep.join(values)
       print >> f, 'if [ ! -n "$%s" ]; then' % (var_name,)
@@ -76,15 +80,14 @@ def emit_env_run_sh(libtbx_build, paths_raw_dict):
     print >> f, 'export %s' % (var_name,)
   print >> f, 'PATH="$LIBTBX_PYTHON_BIN%s$PATH"' % (os.pathsep,)
   print >> f, 'python "$LIBTBX_DIST/env_run.py" $*'
-  os.chmod(env_run_sh_path, 0755)
   f.close()
+  os.chmod(env_run_sh_path, 0755)
 
-def emit_setpaths_csh(libtbx_build, paths_raw_dict):
+def emit_setpaths_csh(libtbx_build, libtbx_info):
   setpaths_csh_path = norm(join(libtbx_build, "setpaths.csh"))
-  print "Updating:", setpaths_csh_path
-  f = open(setpaths_csh_path, "w")
-  for var_name, values in paths_raw_dict.items():
-    if (var_name == "LIBTBX_PACKAGES"): continue
+  f = open_info(setpaths_csh_path)
+  for var_name, values in libtbx_info.items():
+    if (var_name.upper() != var_name): continue
     if (type(values) == type([])):
       val = os.pathsep.join(values)
       print >> f, 'if (! $?%s) then' % (var_name,)
@@ -95,24 +98,23 @@ def emit_setpaths_csh(libtbx_build, paths_raw_dict):
       print >> f, 'setenv %s "%s"' % (var_name, values)
   f.close()
 
-def join_path_ld_library_path(paths_raw_dict):
-  joined_path = paths_raw_dict["PATH"]
-  for path in paths_raw_dict["LD_LIBRARY_PATH"]:
+def join_path_ld_library_path(libtbx_info):
+  joined_path = libtbx_info["PATH"]
+  for path in libtbx_info["LD_LIBRARY_PATH"]:
     if (not path in joined_path):
       joined_path.append(path)
   return joined_path
 
-def emit_setpaths_bat(libtbx_build, paths_raw_dict):
+def emit_setpaths_bat(libtbx_build, libtbx_info):
   setpaths_bat_path = norm(join(libtbx_build, "setpaths.bat"))
-  print "Updating:", setpaths_bat_path
-  f = open(setpaths_bat_path, "w")
+  f = open_info(setpaths_bat_path)
   print >> f, '@ECHO off'
-  for var_name, values in paths_raw_dict.items():
-    if (var_name == "LIBTBX_PACKAGES"): continue
+  for var_name, values in libtbx_info.items():
+    if (var_name.upper() != var_name): continue
     if (type(values) == type([])):
       if (var_name == "LD_LIBRARY_PATH"): continue
       if (var_name == "PATH"):
-        values = join_path_ld_library_path(paths_raw_dict)
+        values = join_path_ld_library_path(libtbx_info)
       val = os.pathsep.join(values)
       print >> f, 'if not defined %s set %s=' % (var_name, var_name)
       print >> f, 'set %s=%s%s%%%s%%' % (var_name, val, os.pathsep, var_name)
@@ -122,37 +124,63 @@ def emit_setpaths_bat(libtbx_build, paths_raw_dict):
   print >> f, 'set PATHEXT=.PY;%PATHEXT%'
   f.close()
 
-def emit_SConstruct(env, paths_raw_dict):
+def emit_SConstruct(env, libtbx_info):
   SConstruct_path = norm(join(env.libtbx_build, "SConstruct"))
-  print "Updating:", SConstruct_path
-  f = open(SConstruct_path, "w")
+  f = open_info(SConstruct_path)
   print >> f, 'Repository(r"%s")' % (env.dist_root,)
   print >> f, 'SConscript("libtbx/build/SConscript")'
   print >> f, 'SConscript("libtbx/SConscript")'
-  for package_name in paths_raw_dict["LIBTBX_PACKAGES"]:
+  for package_name in libtbx_info["package_names"]:
     print >> f, 'SConscript("libtbx/%s_boost/SConscript")' % (package_name,)
     print >> f, 'SConscript("%s/SConscript")' % (package_name,)
   f.close()
 
+def emit_build_py(env):
+  build_py_path = norm(join(env.libtbx_build, "build.py"))
+  f = open_info(build_py_path)
+  print >> f, """#!%s
+import os, sys
+args = sys.argv[1:]
+if (len(args) == 0):
+  os.chdir(r"%s")
+  args = ["."]
+cmd = r"%s " + " ".join(args)
+print cmd
+sys.stdout.flush()
+os.system(cmd)""" % (
+  sys.executable, env.libtbx_build, env.scons_cmd)
+  f.close()
+  os.chmod(build_py_path, 0755)
+
 def run():
   env = empty()
+  # XXX TODO encapsulate scons
+  if (hasattr(os, "symlink")):
+    env.scons_cmd = "/net/cci/rwgk/xp/scons-cvs/bin/scons"
+  else:
+    env.scons_cmd = r"python R:\xp\scons-cvs\bin\scons"
   env.python_bin = norm(os.path.split(sys.executable)[0])
   env.libtbx_build = norm(os.path.abspath(os.getcwd()))
   env.libtbx_dist = norm(os.path.dirname(norm(os.path.abspath(sys.argv[0]))))
   env.dist_root = norm(os.path.dirname(env.libtbx_dist))
+  libtbx_info = None
   for arg in sys.argv[1:]:
     package_dist = norm(join(env.dist_root, arg))
     if (not os.path.isdir(package_dist)):
       print "Error: Not a directory or platform identifier:", arg
       sys.exit(1)
     else:
-      paths_raw_dict = update_paths_raw(env, arg, package_dist)
+      libtbx_info = update_libtbx_info(env, arg, package_dist)
       if (hasattr(os, "symlink")):
-        emit_env_run_sh(env.libtbx_build, paths_raw_dict)
-        emit_setpaths_csh(env.libtbx_build, paths_raw_dict)
+        emit_env_run_sh(env.libtbx_build, libtbx_info)
+        emit_setpaths_csh(env.libtbx_build, libtbx_info)
       else:
-        emit_setpaths_bat(env.libtbx_build, paths_raw_dict)
-  emit_SConstruct(env, paths_raw_dict)
+        emit_setpaths_bat(env.libtbx_build, libtbx_info)
+      emit_build_py(env)
+  if (libtbx_info == None):
+    print "Specify at least one package name."
+  else:
+    emit_SConstruct(env, libtbx_info)
 
 if (__name__ == "__main__"):
   run()
