8a086d6f87fd2503abd54ab069efe291092e1f32
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2002-09-27 20:30:40 +0000

    minor adjustments according to g++ warnings

--- scitbx/include/scitbx/error.h
+++ scitbx/include/scitbx/error.h
@@ -18,7 +18,7 @@
 #include <exception>
 #include <string>
 
-#define CheckPoint std::cout << __FILE__ << "(" << __LINE__ << ")" << std::endl << std::flush
+#define SCITBX_CHECK_POINT std::cout << __FILE__ << "(" << __LINE__ << ")" << std::endl << std::flush
 
 //! Common scitbx namespace.
 namespace scitbx {
