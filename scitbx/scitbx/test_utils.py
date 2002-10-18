0d2ed8bc9de0276897f905cc8cfd5c441d226c3c
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2002-10-18 22:00:02 +0000

    approx_equal moved to central place

--- scitbx/scitbx/test_utils.py
+++ scitbx/scitbx/test_utils.py
@@ -19,3 +19,15 @@ def run_tests(build_dir, dist_dir, tst_list):
     print
     sys.stderr.flush()
     sys.stdout.flush()
+
+def approx_equal(a1, a2, eps=1.e-6):
+  if (hasattr(a1, "__len__")): # traverse list
+    assert len(a1) == len(a2)
+    for i in xrange(len(a1)):
+      if (not approx_equal(a1[i], a2[i], eps)): return 0
+  elif (hasattr(a1, "real")): # complex numbers
+    if (not approx_equal(a1.real, a2.real, eps)): return 0
+    if (not approx_equal(a1.imag, a2.imag, eps)): return 0
+  else:
+    if (abs(a1 - a2) > eps): return 0
+  return 1
