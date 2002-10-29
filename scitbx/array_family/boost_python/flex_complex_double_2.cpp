bae70742f85e6b5d722e09c204af6a2a4f324a83
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2002-10-29 13:27:03 +0000

    flex_complex_double_2.cpp to enable Visual C++ 7 compilation

--- scitbx/array_family/boost_python/flex_complex_double_2.cpp
+++ scitbx/array_family/boost_python/flex_complex_double_2.cpp
@@ -7,16 +7,15 @@
      2002 Aug: Created (R.W. Grosse-Kunstleve)
  */
 
-#include <scitbx/array_family/boost_python/flex_wrapper.h>
-#include <scitbx/array_family/boost_python/flex_pickle_single_buffered.h>
+#include <scitbx/array_family/boost_python/ref_c_grid_flex_conversions.h>
+#include <scitbx/array_family/accessors/c_grid.h>
 
 namespace scitbx { namespace af { namespace boost_python {
 
-  void wrap_flex_complex_double()
+  void wrap_flex_complex_double_2()
   {
-    flex_wrapper<std::complex<double> >::complex(
-      "complex_double", boost::python::scope())
-      .def_pickle(flex_pickle_single_buffered<std::complex<double>, 2*20>());
+    ref_c_grid_flex_conversions<std::complex<double>, c_grid<2> >();
+    ref_c_grid_flex_conversions<std::complex<double>, c_grid<3> >();
   }
 
 }}} // namespace scitbx::af::boost_python
