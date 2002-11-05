9ac951e91292c841d02013e6bb57e2d4fc244316
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2002-11-05 00:23:27 +0000

    dicts.easy bug fix

--- scitbx/scitbx/python_utils/dicts.py
+++ scitbx/scitbx/python_utils/dicts.py
@@ -1,7 +1,7 @@
 class easy(dict):
 
   def __getattr__(self, key):
-    dict.__getitem__(self, key)
+    return dict.__getitem__(self, key)
 
   def __setattr__(self, key, value):
     dict.__setitem__(self, key, value)
