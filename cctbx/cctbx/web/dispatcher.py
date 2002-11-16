87d6191deeee93cfff31016345bc1e673c5212c5
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2002-11-16 11:16:19 +0000

    cctbx/web/emma.py converted; new web/replay.py

--- cctbx/cctbx/web/dispatcher.py
+++ cctbx/cctbx/web/dispatcher.py
@@ -1,19 +1,33 @@
 import sys
 sys.stderr = sys.stdout
 
+import cgitb
+cgitb.enable()
+
 import os, cgi, urlparse
 
 if (0):
-  print "Content-type: text/plain"
+  cgi.test()
+
+if (0):
+  print "Content-type: text/html"
   print
-  import os
-  for k,v in os.environ.items():
-    print k, v
 
 cctbx_url = list(urlparse.urlsplit(os.environ["HTTP_REFERER"]))
 cctbx_url[2] = "/".join(cctbx_url[2].split("/")[:-1])
 
 form = cgi.FieldStorage()
 target_module = form["target_module"].value
+
 exec "import " + target_module + " as target"
-target.run(cctbx_url, form)
+inp = target.interpret_form_data(form)
+
+if (1):
+  # capture input to facilitate debugging
+  import time, pickle
+  time_stamp = "%d_%02d_%02d_%02d_%02d_%02d" % time.localtime(time.time())[:6]
+  f = open("/var/tmp/cctbx_web/"+target_module+"_"+time_stamp, "wb")
+  pickle.dump([cctbx_url, target_module, inp], f)
+  f.close()
+
+target.run(cctbx_url, inp)
---
+++ cctbx/cctbx/web/emma.py
@@ -0,0 +1,180 @@
+from cctbx import euclidean_model_matching as emma
+from cctbx import crystal
+from cctbx import sgtbx
+from cctbx import uctbx
+import sys
+import traceback
+
+class empty: pass
+
+def interpret_form_data(form):
+  inp = empty()
+  for key in (("ucparams_1", ""),
+              ("sgsymbol_1", ""),
+              ("convention_1", ""),
+              ("format_1", None),
+              ("coor_type_1", None),
+              ("skip_columns_1", 0),
+              ("ucparams_2", ""),
+              ("sgsymbol_2", ""),
+              ("convention_2", ""),
+              ("format_2", None),
+              ("coor_type_2", None),
+              ("skip_columns_2", 0),
+              ("tolerance", "1.0"),
+              ("diffraction_index_equivalent", None)):
+    if (form.has_key(key[0])):
+      inp.__dict__[key[0]] = form[key[0]].value.strip()
+    else:
+      inp.__dict__[key[0]] = key[1]
+  inp.coordinates = []
+  for i in xrange(2):
+    coordinates = []
+    for key_root in ("coordinates", "coor_file"):
+      key = key_root + "_" + str(i+1)
+      if (form.has_key(key)):
+        lines = form[key].value.replace("\015", "\012").split("\012")
+        for l in lines:
+          s = l.strip()
+          if (len(s) != 0): coordinates.append(s)
+    inp.coordinates.append(coordinates)
+  return inp
+
+def interpret_generic_coordinate_line(line, skip_columns):
+  flds = line.split()
+  try: site = [float(x) for x in flds[skip_columns: skip_columns+3]]
+  except: raise RuntimeError, "FormatError: " + line
+  return " ".join(flds[:skip_columns]), site
+
+def sdb_file_to_emma_model(crystal_symmetry, sdb_file):
+  positions = []
+  i = 0
+  for site in sdb_file.sites:
+    i += 1
+    positions.append(emma.position(
+      ":".join((str(i), site.segid, site.type)),
+      crystal_symmetry.unit_cell().fractionalize((site.x, site.y, site.z))))
+  m = emma.model(
+    crystal.special_position_settings(crystal_symmetry),
+    positions)
+  m.label = sdb_file.file_name
+  return m
+
+class web_to_models:
+
+  def __init__(self, ucparams, sgsymbol, convention,
+               format, coor_type, skip_columns, coordinates):
+    self.ucparams = ucparams
+    self.sgsymbol = sgsymbol
+    self.convention = convention
+    if (format == "generic"):
+      skip_columns = int(skip_columns)
+      self.positions = []
+      for line in coordinates:
+        label, site = interpret_generic_coordinate_line(line, skip_columns)
+        if (label == ""): label = "Site" + str(len(self.positions)+1)
+        if (coor_type != "Fractional"):
+          site = self.get_unit_cell().fractionalize(site)
+        self.positions.append(emma.position(label, site))
+    else:
+      from cctbx.macro_mol import cns_sdb_reader
+      self.sdb_files = cns_sdb_reader.multi_sdb_parser(coordinates)
+    self.i_next_model = 0
+
+  def get_unit_cell(self, other_unit_cell=None):
+    if (self.ucparams):
+      return uctbx.unit_cell(
+        [float(p) for p in self.ucparams.replace(",", " ").split()])
+    assert other_unit_cell != None, "Unit cell parameters unknown."
+    return other_unit_cell
+
+  def get_space_group_info(self, other_space_group_info=None):
+    if (self.sgsymbol):
+      return sgtbx.space_group_info(
+        symbol=self.sgsymbol,
+        table_id=self.convention)
+    assert other_space_group_info != None, "Space group symbol unknown."
+    return other_space_group_info
+
+  def get_next(self):
+    if (hasattr(self, "positions")):
+      if (self.i_next_model): return None
+      crystal_symmetry = crystal.symmetry(
+        unit_cell=self.get_unit_cell(),
+        space_group_info=self.get_space_group_info())
+      m = emma.model(
+        crystal.special_position_settings(crystal_symmetry),
+        self.positions)
+      m.label = "Model 2"
+      self.i_next_model += 1
+      return m
+    if (self.i_next_model >= len(self.sdb_files)): return None
+    sdb = self.sdb_files[self.i_next_model]
+    crystal_symmetry = crystal.symmetry(
+      unit_cell=self.get_unit_cell(sdb.unit_cell),
+      space_group_info=self.get_space_group_info(sdb.space_group_info))
+    m = sdb_file_to_emma_model(crystal_symmetry, sdb)
+    self.i_next_model += 1
+    return m
+
+def run(cctbx_url, inp):
+  print "Content-type: text/html"
+  print
+  print "<pre>"
+
+  if (inp.ucparams_2 == ""):
+      inp.ucparams_2 = inp.ucparams_1
+  if (inp.sgsymbol_2 == ""):
+      inp.sgsymbol_2 = inp.sgsymbol_1
+      inp.convention_2 = inp.convention_1
+
+  try:
+    tolerance = float(inp.tolerance)
+    print "Tolerance:", tolerance
+    if (tolerance <= 0.):
+      raise ValueError, "Tolerance must be greater than zero."
+    print
+
+    diffraction_index_equivalent = int(inp.diffraction_index_equivalent)
+    if (diffraction_index_equivalent):
+      print "Models are diffraction index equivalent."
+      print
+
+    models1 = web_to_models(
+      inp.ucparams_1,
+      inp.sgsymbol_1, inp.convention_1,
+      inp.format_1, inp.coor_type_1, inp.skip_columns_1,
+      inp.coordinates[0])
+    model1 = models1.get_next()
+    assert model1, "Problems reading reference model."
+    model1.show("Reference model")
+    assert not models1.get_next()
+
+    models2 = web_to_models(
+      inp.ucparams_2,
+      inp.sgsymbol_2, inp.convention_2,
+      inp.format_2, inp.coor_type_2, inp.skip_columns_2,
+      inp.coordinates[1])
+    while 1:
+      print "#" * 79
+      print
+      model2 = models2.get_next()
+      if (not model2): break
+      model2.show(model2.label)
+      refined_matches = emma.match_models(model1, model2)
+      if (len(refined_matches) == 0):
+        print "No matches."
+        print
+      else:
+        for match in refined_matches:
+          print "." * 79
+          print
+          match.show()
+
+  except RuntimeError, e:
+    print e
+  except AssertionError:
+    ei = sys.exc_info()
+    print traceback.format_exception_only(ei[0], ei[1])[0]
+
+  print "</pre>"
