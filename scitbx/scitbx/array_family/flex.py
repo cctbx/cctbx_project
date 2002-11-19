d82f52edf831e52329eaad935fee6e995f823ca5
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2002-11-19 19:31:42 +0000

    work around C++ exception handling bugs

--- scitbx/scitbx/array_family/flex.py
+++ scitbx/scitbx/array_family/flex.py
@@ -2,3 +2,11 @@ from scitbx.python_utils import misc
 ext = misc.import_ext("scitbx_boost.array_family.flex_scitbx_ext")
 misc.import_regular_symbols(globals(), ext.__dict__)
 del misc
+
+def to_list(array):
+  """Workaround for C++ exception handling bugs
+     (list(array) involves C++ exceptions)"""
+  result = []
+  for i in xrange(array.size()):
+    result.append(array[i])
+  return result
