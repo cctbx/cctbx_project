e2cd5f05e40ffc35b4a597a0ba5554f95d713999
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2002-11-20 01:48:37 +0000

    new run_examples.py

---
+++ cctbx/run_examples.py
@@ -0,0 +1,18 @@
+import os, os.path
+from scitbx import test_utils
+
+def run():
+  tst_list = (
+  "$B/examples/getting_started",
+  "$D/cctbx/examples/getting_started.py",
+  "$D/cctbx/examples/analyze_adp.py",
+  ["$D/cctbx/examples/all_axes.py", "P31"],
+  )
+
+  build_dir = os.path.join(os.environ["LIBTBX_BUILD"], "cctbx")
+  dist_dir = os.environ["CCTBX_DIST"]
+
+  test_utils.run_tests(build_dir, dist_dir, tst_list)
+
+if (__name__ == "__main__"):
+  run()
