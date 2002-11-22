73daefa71706c1068153533596d6f14fc0c7a7ec
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2002-11-22 02:09:15 +0000

    adopt_init_args(..., hide=False)

--- scitbx/scitbx/python_utils/misc.py
+++ scitbx/scitbx/python_utils/misc.py
@@ -1,12 +1,18 @@
 import sys
 
-def adopt_init_args(obj, args, exclude=()):
+def adopt_init_args(obj, args, exclude=(), hide=False):
   del args["self"]
   for param in exclude:
     del args[param]
-  for key in args.keys():
-    assert not hasattr(obj.__dict__, key)
-  obj.__dict__.update(args)
+  if (hide == False):
+    for key in args.keys():
+      assert not hasattr(obj.__dict__, key)
+    obj.__dict__.update(args)
+  else:
+    for key in args.keys():
+      _key = "_" + key
+      assert not hasattr(obj.__dict__, _key)
+      obj.__dict__[_key] = args[key]
 
 def import_regular_symbols(dict_target, dict_source):
   for key, value in dict_source.items():
