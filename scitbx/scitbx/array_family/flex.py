6889958a544b9378856ab3928007969f8d134a7a
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2002-11-27 00:00:19 +0000

    linear_regression.show_summary()

--- scitbx/scitbx/array_family/flex.py
+++ scitbx/scitbx/array_family/flex.py
@@ -3,6 +3,8 @@ ext = misc.import_ext("scitbx_boost.array_family.flex_scitbx_ext")
 misc.import_regular_symbols(globals(), ext.__dict__)
 del misc
 
+import sys
+
 def to_list(array):
   """Workaround for C++ exception handling bugs
      (list(array) involves C++ exceptions)"""
@@ -10,3 +12,13 @@ def to_list(array):
   for i in xrange(array.size()):
     result.append(array[i])
   return result
+
+class linear_regression(ext.linear_regression):
+
+  def __init__(self, x, y, epsilon=1.e-6):
+    ext.linear_regression.__init__(self, x, y, epsilon)
+
+  def show_summary(self, f=sys.stdout):
+    print >> f, "y_intercept:", self.y_intercept()
+    print >> f, "slope:", self.slope()
+    print >> f, "correlation:", self.correlation()
