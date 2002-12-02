9e73cc25d894356cabc0922df73a0d9a085c103a
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2002-12-02 21:08:09 +0000

    work around VC++7 internal structure overflow

--- scitbx/array_family/boost_python/flex_float.cpp
+++ scitbx/array_family/boost_python/flex_float.cpp
@@ -10,14 +10,15 @@
 #include <scitbx/array_family/boost_python/flex_fwd.h>
 
 #include <scitbx/array_family/boost_python/flex_wrapper.h>
-#include <scitbx/array_family/boost_python/flex_pickle_single_buffered.h>
 
 namespace scitbx { namespace af { namespace boost_python {
 
+  void wrap_flex_float_2(flex_wrapper<float>::class_f_t class_object);
+
   void wrap_flex_float()
   {
-    flex_wrapper<float>::numeric("float", boost::python::scope())
-      .def_pickle(flex_pickle_single_buffered<float>());
+    wrap_flex_float_2(
+      flex_wrapper<float>::numeric("float", boost::python::scope()));
   }
 
 }}} // namespace scitbx::af::boost_python
--- scitbx/array_family/boost_python/flex_float_2.cpp
+++ scitbx/array_family/boost_python/flex_float_2.cpp
@@ -10,15 +10,14 @@
 #include <scitbx/array_family/boost_python/flex_fwd.h>
 
 #include <scitbx/array_family/boost_python/flex_wrapper.h>
+#include <scitbx/array_family/boost_python/flex_pickle_single_buffered.h>
 
 namespace scitbx { namespace af { namespace boost_python {
 
-  void wrap_flex_double_2(flex_wrapper<double>::class_f_t class_object);
-
-  void wrap_flex_double()
+  void wrap_flex_float_2(flex_wrapper<float>::class_f_t class_object)
   {
-    wrap_flex_double_2(
-      flex_wrapper<double>::numeric("double", boost::python::scope()));
+    class_object
+      .def_pickle(flex_pickle_single_buffered<float>());
   }
 
 }}} // namespace scitbx::af::boost_python
