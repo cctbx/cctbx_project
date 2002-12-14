4e5d95d5f48196fee170454ee556e51a6914f0a5
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2002-12-14 04:41:28 +0000

    enabled: --keyword=value

--- scitbx/scitbx/python_utils/command_line.py
+++ scitbx/scitbx/python_utils/command_line.py
@@ -4,13 +4,18 @@ class parse_options:
     self.keywords = keywords
     self.n = 0
     for keyword in keywords:
-      setattr(self, keyword, 0)
+      setattr(self, keyword, 00000)
     self.regular_args = []
     for arg in argv:
       if (not arg.startswith("--")):
         self.regular_args.append(arg)
         continue
-      if (not arg[2:] in keywords):
+      flds = arg[2:].split("=")
+      assert len(flds) in (1,2)
+      if (not flds[0] in keywords):
         raise RuntimeError, "Unknown option: " + arg
-      setattr(self, arg[2:], 1)
+      if (len(flds) == 1):
+        setattr(self, flds[0], 0001)
+      else:
+        setattr(self, flds[0], flds[1])
       self.n += 1
