a83857010a226b865d5af98492e4d11ecbc4290f
Nicholas Sauter <nksauter@lbl.gov>
2002-12-16 18:42:13 +0000

    Initial revision

---
+++ iotbx/run_tests.py
@@ -0,0 +1,16 @@
+import os, os.path
+from scitbx import test_utils
+
+def run():
+  tst_list = (
+  "$D/iotbx/mtz/tst_mtz.py",
+  "$D/iotbx/detectors/tst_adsc.py",
+  )
+
+  build_dir = os.path.join(os.environ["LIBTBX_BUILD"], "iotbx")
+  dist_dir = os.environ["IOTBX_DIST"]
+
+  test_utils.run_tests(build_dir, dist_dir, tst_list)
+
+if (__name__ == "__main__"):
+  run()
