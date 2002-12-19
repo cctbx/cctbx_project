79dc121ab1f9a59454dc5414ddfc8b712a618c66
Nicholas Sauter <nksauter@lbl.gov>
2002-12-19 23:55:19 +0000

    Add mtz member functions to retrieve complex() structure factors from amplitude and phase, and HL() to get Hendrickson-Lattman coefficients from named columns.  See the tst_mtz.py file for an example.

--- iotbx/iotbx/mtz/__init__.py
+++ iotbx/iotbx/mtz/__init__.py
@@ -1,6 +1,6 @@
 from scitbx.python_utils.misc import import_regular_symbols
-from scitbx.array_family import flex
-from cctbx.array_family import flex #for exposing af_shared(miller_index)
+from scitbx.array_family import flex as sciflex
+from cctbx.array_family import flex as ccflex#for exposing af_shared(miller_index)
 from iotbx_boost import mtz
 import_regular_symbols(globals(), mtz.__dict__)
 del import_regular_symbols
@@ -14,3 +14,23 @@ class Mtz (baseMtz):
 
   def __getattr__(self,s):
     return self.getShared(s)
+    
+  def HL(self,hla_name,hlb_name,hlc_name,hld_name):
+    hla = self.getColumn(hla_name)
+    hlb = self.getColumn(hlb_name)
+    hlc = self.getColumn(hlc_name)
+    hld = self.getColumn(hld_name)
+    assert hla.type()=='A'
+    assert hlb.type()=='A'
+    assert hlc.type()=='A'
+    assert hld.type()=='A'
+    return baseMtz.HL(self,hla,hlb,hlc,hld)
+
+  def complex(self,amplitude_name,phi_name):
+    amplitude = self.getColumn(amplitude_name)
+    assert amplitude.type()=='F'
+    phase = self.getColumn(phi_name)
+    assert phase.type()=='P'
+    amplitude = self.getShared(amplitude_name)
+    phase = self.getShared(phi_name)
+    return ccflex.polar(amplitude,phase,1) #degrees=1
