14465f3467dc1997dab1d134b5745b87a0c612f3
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-02-13 11:12:31 +0000

    new class weighted_choice incl. regression test

--- scitbx/scitbx/python_utils/random_utils.py
+++ scitbx/scitbx/python_utils/random_utils.py
@@ -1,4 +1,5 @@
 import random
+from scitbx.array_family import flex
 
 def random_subset(set, n):
   assert n >= 0 and n <= len(set)
@@ -8,8 +9,32 @@ def random_subset(set, n):
     del set[random.randrange(len(set))]
   return set
 
+class weighted_choice:
+
+  def __init__(self, weights):
+    self.accumulated_weights = flex.double()
+    sum = 0
+    for w in weights:
+      sum += w
+      self.accumulated_weights.append(sum)
+    self.accumulated_weights /= sum
+
+  def next(self):
+    r = random.random()
+    for i,w in self.accumulated_weights.items():
+      if (w >= r): return i
+    return self.accumulated_weights.size()-1
+
 if (__name__ == "__main__"):
   print random_subset(range(5), 0)
   print random_subset(range(5), 5)
   print random_subset(range(5), 2)
   print random_subset(range(5), 3)
+  for weights in ([5,5], [4,3,2,1]):
+    r = weighted_choice(weights)
+    hist = [0 for i in xrange(len(weights))]
+    for i in xrange(10000):
+      hist[r.next()] += 1
+    hist = [int(round(s/1000.)) for s in hist]
+    assert hist == weights, (hist, weights)
+  print "OK"
