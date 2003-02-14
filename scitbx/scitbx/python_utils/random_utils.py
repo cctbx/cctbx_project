d96e4ea833e5c5b2cd7280790197671ee59fd717
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-02-14 13:08:41 +0000

    random seed with process id and time

--- scitbx/scitbx/python_utils/random_utils.py
+++ scitbx/scitbx/python_utils/random_utils.py
@@ -1,5 +1,13 @@
 import random
+import os
+import time
 from scitbx.array_family import flex
+
+def seed(a=None):
+  if (a == None):
+    try: a = long(os.getpid() * (2**16)) + long(time.time() * 2**8)
+    except: pass
+  random.seed(a)
 
 def random_subset(set, n):
   assert n >= 0 and n <= len(set)
@@ -26,6 +34,7 @@ class weighted_choice:
     return self.accumulated_weights.size()-1
 
 if (__name__ == "__main__"):
+  seed()
   print random_subset(range(5), 0)
   print random_subset(range(5), 5)
   print random_subset(range(5), 2)
