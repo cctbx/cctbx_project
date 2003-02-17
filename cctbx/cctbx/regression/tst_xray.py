f63927742904c66407528a9b881d25d4e07433f1
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-02-17 08:10:44 +0000

    xray.structure.change_hand()

--- cctbx/cctbx/regression/tst_xray.py
+++ cctbx/cctbx/regression/tst_xray.py
@@ -1,6 +1,7 @@
 from cctbx import xray
 from cctbx import crystal
 from cctbx.array_family import flex
+from scitbx.test_utils import approx_equal
 
 def exercise_structure():
   cs = crystal.symmetry((5.01, 5.01, 5.47, 90, 90, 120), "P 62 2 2")
@@ -13,6 +14,11 @@ def exercise_structure():
   assert tuple(xs.special_position_indices()) == (0, 1)
   xs.all_apply_symmetry()
   assert tuple(xs.special_position_indices()) == (0, 1)
+  sx = xs.change_hand()
+  assert sx.unit_cell().is_similar_to(xs.unit_cell())
+  assert str(sx.space_group_info()) == "P 64 2 2"
+  assert approx_equal(sx.scatterers()[0].site, (-1./2, -1./2, -1./3))
+  assert approx_equal(sx.scatterers()[1].site, (-0.19700, 0.19700, -0.833333))
 
 def run():
   exercise_structure()
