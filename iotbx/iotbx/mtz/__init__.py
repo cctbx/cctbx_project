1a587b19dde8a3b6f51d15dbb7f300a24c579037
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-03-20 02:18:34 +0000

    iotbx.mtz.dump

--- iotbx/iotbx/mtz/__init__.py
+++ iotbx/iotbx/mtz/__init__.py
@@ -9,6 +9,29 @@ from scitbx.array_family import flex as sciflex
 from cctbx import miller
 from cctbx.array_family import flex as ccflex
 
+column_type_legend_source = \
+  "http://www.ccp4.ac.uk/dist/html/mtzlib.html#fileformat"
+column_type_legend = {
+  "H": "index h,k,l",
+  "J": "intensity",
+  "F": "structure factor amplitude",
+  "D": "anomalous difference",
+  "Q": "standard deviation",
+  "G": "structure amplitude associated with one member of an"
+       +" hkl -h-k-l pair, F(+) or F(-)",
+  "L": "standard deviation",
+  "K": "intensity associated with one member of an"
+       +" hkl -h-k-l pair, I(+) or I(-)",
+  "M": "standard deviation",
+  "P": "phase angle in degrees",
+  "W": "weight (of some sort)",
+  "A": "phase probability coefficients (Hendrickson/Lattmann)",
+  "B": "BATCH number",
+  "Y": "M/ISYM, packed partial/reject flag and symmetry number",
+  "I": "integer",
+  "R": "real",
+}
+
 class Mtz (ext.Mtz):
   def __init__(self,s):
     ext.Mtz.__init__(self,s)
