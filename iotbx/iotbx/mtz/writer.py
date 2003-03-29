d3d709694d6a71f5b9ebdab7f4a4e8f298011a31
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-03-29 04:03:32 +0000

    efficient add_miller_array

--- iotbx/iotbx/mtz/writer.py
+++ iotbx/iotbx/mtz/writer.py
@@ -15,50 +15,28 @@ def _columnCombinations(writer,label,datatype,carry_miller,carry_data):
     _DoubleOrComplex(writer,label+"-",datatype,carry_miller[1],carry_data[1])
 
 def add_miller_array(self, miller_array, label_data, label_sigmas=None):
-
   assert (miller_array.sigmas() == None) == (label_sigmas == None)
-  anomalous_flag = miller_array.anomalous_flag()
-  miller_indices = miller_array.indices()
-  data = miller_array.data()
-  sigmas = miller_array.sigmas()
-  if (anomalous_flag):
+  if (miller_array.anomalous_flag()):
     carry_miller = []
     carry_data = []
     carry_sigma = []
     asu, matches = miller_array.match_bijvoet_mates()
     for i,s in ((0,"+"),(1,"-")):
-      sel = matches.hemisphere_selection(s)
+      sel = matches.pairs_hemisphere_selection(s) \
+          | matches.singles_hemisphere_selection(s)
       if (i == 0):
         carry_miller.append(asu.indices().select(sel))
       else:
-        carry_miller.append(-asu.indices().select(sel)) # XXX remove - for crash
+        carry_miller.append(-asu.indices().select(sel))
       carry_data.append(asu.data().select(sel))
-      if (sigmas != None):
+      if (asu.sigmas() != None):
         carry_sigma.append(asu.sigmas().select(sel))
-    reciprocal_space_asu = asu.space_group_info().reciprocal_space_asu()
-    for i in matches.singles():
-      h = asu.indices()[i]
-      f = asu.data()[i]
-      if sigmas:
-        g = asu.sigmas()[i]
-      s = reciprocal_space_asu.which(h)
-      assert s != 0
-      if (reciprocal_space_asu.which(h) > 0):
-        carry_miller[0].push_back(h)
-        carry_data[0].push_back(f)
-        if sigmas:
-          carry_sigma[0].push_back(g)
-      else:
-        carry_miller[1].push_back((-h[0],-h[1],-h[2]))
-        carry_data[1].push_back(f)
-        if sigmas:
-          carry_sigma[1].push_back(g)
     _columnCombinations(self,label_data,"G",carry_miller,carry_data)
-    if sigmas:
+    if (asu.sigmas()):
       _columnCombinations(self,label_sigmas,"L",carry_miller,carry_sigma)
-
   else:
-    D = miller_array.data()
-    _columnCombinations(self,label_data,"F",[miller_array.indices()],[miller_array.data()])
-    if sigmas:
-      _columnCombinations(self,label_sigmas,"Q",[miller_array.indices()],[miller_array.sigmas()])
+    _columnCombinations(self,label_data,"F",
+      [miller_array.indices()],[miller_array.data()])
+    if (miller_array.sigmas()):
+      _columnCombinations(self,label_sigmas,"Q",
+        [miller_array.indices()],[miller_array.sigmas()])
