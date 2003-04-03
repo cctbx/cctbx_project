d5e9db5408e294ffe144b5f208e5bbbc2236c550
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-04-03 20:25:41 +0000

    scalepack reader moved from cctbx to iotbx

--- cctbx/cctbx/command_line/run_scalepack_reader.py
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env python
-from cctbx.macro_mol import scalepack_reader
-scalepack_reader.run()
---
+++ iotbx/iotbx/command_line/iotbx.scalepack.reader.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python
+from iotbx.scalepack import reader
+import sys
+reader.run(sys.argv[1:])
