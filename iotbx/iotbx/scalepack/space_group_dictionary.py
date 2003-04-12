4a5606f3585253f0704d24c432f8a39b80833895
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-04-12 14:29:55 +0000

    sync from laptop: working on scalepack no_merge_original_index reader

---
+++ iotbx/iotbx/scalepack/space_group_dictionary.py
@@ -0,0 +1,89 @@
+scalepack manual, edition 5, page 141
+The space group may be entered as a name (e.g. P212121) or as a number (e.g. 19, for the same space group). Most of the numbers correspond to those of the International Tables. The numbers above 230 are non-standard definitions of space groups.
+
+1 P1
+3 P2
+4 P21
+5 C2
+16 P222
+17 P2221
+18 P21212
+19 P212121
+20 C2221
+21 C222
+22 F222
+23 I222
+24 I212121
+75 P4
+76 P41
+77 P42
+78 P43
+79 I4
+80 I41
+89 P422
+90 P4212
+91 P4122
+92 P41212
+93 P4222
+94 P42212
+95 P4322
+96 P43212
+97 I422
+98 I4122
+143 P3
+144 P31
+145 P32
+146 R3
+149 P312
+150 P321
+151 P3112
+152 P3121
+153 P3212
+154 P3221
+155 R32
+168 P6
+169 P61
+170 P65
+171 P62
+172 P64
+173 P63
+177 P622
+178 P6122
+179 P6522
+180 P6222
+181 P6422
+182 P6322
+195 P23
+196 F23
+197 I23
+198 P213
+199 I213
+207 P432
+208 P4232
+209 F432
+210 F4132
+211 I432
+212 P4332
+213 P4132
+214 I4132
+303 P2C
+305 B2
+318 P21221
+401 C1
+403 P21C
+446 H3
+455 H32
+501 I1
+503 I2
+505 C21
+Notes to particular space groups:
+146 R3 R3 in hexagonal setting
+446 H3 R3 in primitive setting
+155 R32 R32 in hexagonal setting
+455 H32 R32 in primitive setting
+401 C1 Non-standard, but useful to make angles close to 90.
+501 I1 Non-standard, but useful to make angles close to 90.
+303 P2C P2, C axis unique
+403 P21C P21, C axis unique
+305 B2 like C2, B face centered, c axis unique
+503 I2 Non-standard, but useful to make beta angle close to 90.
---
+++ iotbx/misc/boost_python/scalepack_ext.cpp
@@ -0,0 +1,169 @@
+#include <scitbx/array_family/boost_python/flex_fwd.h>
+#include <cctbx/miller.h>
+#include <cctbx/error.h>
+#include <boost/python/module.hpp>
+#include <boost/python/class.hpp>
+#include <fstream>
+#include <vector>
+
+namespace {
+
+  namespace miller = cctbx::miller;
+  namespace af = scitbx::af;
+
+  class read_formatted
+  {
+    public:
+      read_formatted() {}
+
+      void
+      new_line(std::string const& line)
+      {
+        len_ = line.size();
+        pos_ = 0;
+        buffer_.reserve(len_+1);
+        for(std::size_t i=0;i<len_;i++) buffer_[i] = line[i];
+        buffer_[len_] = '\0';
+      }
+
+      const char*
+      next_field(std::size_t w, char fill_char)
+      {
+        field_.reserve(w+1);
+        std::size_t i;
+        for(i=0; i < w && pos_ < len_; i++, pos_++) {
+          field_[i] = buffer_[pos_];
+        }
+        for(; i < w; i++) {
+          field_[i] = fill_char;
+        }
+        field_[w] = '\0';
+        return &*field_.begin();
+      }
+
+      int
+      next_int(std::size_t w)
+      {
+        return std::atoi(next_field(w, '0'));
+      }
+
+      double
+      next_double(std::size_t w)
+      {
+        return std::atof(next_field(w, '0'));
+      }
+
+      miller::index<>
+      next_miller_index(std::size_t w)
+      {
+        miller::index<> result;
+        for(std::size_t i=0;i<3;i++) {
+          result[i] = next_int(w);
+        }
+        return result;
+      }
+
+    protected:
+      std::size_t len_;
+      std::size_t pos_;
+      std::vector<char> buffer_;
+      std::vector<char> field_;
+  };
+
+  class no_merge_original_index_arrays
+  {
+    public:
+      no_merge_original_index_arrays(
+        std::string const& file_name,
+        std::size_t n_lines_skip)
+      {
+        std::ifstream cin(file_name.c_str());
+        std::string line;
+        for (std::size_t i=0;i<n_lines_skip;i++) {
+          std::getline(cin, line);
+          CCTBX_ASSERT(!cin.eof());
+        }
+        read_formatted rf;
+        while (true) {
+          std::getline(cin, line);
+          if (cin.eof()) break;
+          rf.new_line(line);
+          original_indices_.push_back(rf.next_miller_index(4));
+          unique_indices_.push_back(rf.next_miller_index(4));
+          batch_numbers_.push_back(rf.next_int(6));
+          centric_tags_.push_back(rf.next_int(2));
+          spindle_flags_.push_back(rf.next_int(2));
+          asymmetric_unit_indices_.push_back(rf.next_int(3));
+          i_obs_.push_back(rf.next_double(8));
+          sigmas_.push_back(rf.next_double(8));
+        }
+      }
+
+      af::shared<miller::index<> >
+      original_indices() const { return original_indices_; }
+
+      af::shared<miller::index<> >
+      unique_indices() const { return unique_indices_; }
+
+      af::shared<int>
+      batch_numbers() const { return batch_numbers_; }
+
+      af::shared<int>
+      centric_tags() const { return centric_tags_; }
+
+      af::shared<int>
+      asymmetric_unit_indices() const { return asymmetric_unit_indices_; }
+
+      af::shared<int>
+      spindle_flags() const { return spindle_flags_; }
+
+      af::shared<double>
+      i_obs() const { return i_obs_; }
+
+      af::shared<double>
+      sigmas() const { return sigmas_; }
+
+    private:
+      af::shared<miller::index<> > original_indices_;
+      af::shared<miller::index<> > unique_indices_;
+      af::shared<int> batch_numbers_;
+      af::shared<int> centric_tags_;
+      af::shared<int> asymmetric_unit_indices_;
+      af::shared<int> spindle_flags_;
+      af::shared<double> i_obs_;
+      af::shared<double> sigmas_;
+  };
+
+  struct no_merge_original_index_arrays_wrappers
+  {
+    typedef no_merge_original_index_arrays w_t;
+
+    static void
+    wrap()
+    {
+      using namespace boost::python;
+      class_<w_t>("no_merge_original_index_arrays", no_init)
+        .def(init<std::string const&, std::size_t>())
+        .def("original_indices", &w_t::original_indices)
+        .def("unique_indices", &w_t::unique_indices)
+        .def("batch_numbers", &w_t::batch_numbers)
+        .def("centric_tags", &w_t::centric_tags)
+        .def("asymmetric_unit_indices", &w_t::asymmetric_unit_indices)
+        .def("spindle_flags", &w_t::spindle_flags)
+        .def("i_obs", &w_t::i_obs)
+        .def("sigmas", &w_t::sigmas)
+      ;
+    }
+  };
+
+  void init_module()
+  {
+    no_merge_original_index_arrays_wrappers::wrap();
+  }
+
+} // namespace <anonymous>
+
+BOOST_PYTHON_MODULE(scalepack_ext)
+{
+  init_module();
+}
