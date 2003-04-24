88fd230f55b9b5d5f57f6e31cce6eedc8021f668
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-04-24 05:16:26 +0000

    utilities

---
+++ cctbx/cctbx/command_line/cctbx.euclidean_model_matching.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+
+from cctbx import euclidean_model_matching as emma
+from scitbx.python_utils import easy_pickle
+from scitbx.python_utils import dicts
+from scitbx.python_utils.misc import adopt_init_args
+import sys
+
+class match_record:
+
+  def __init__(self, n_matches, model_size):
+    adopt_init_args(self, locals())
+
+  def __repr__(self):
+    return "%d/%d" % (self.n_matches, self.model_size)
+
+def run():
+  assert len(sys.argv) == 3, \
+    "usage: cctbx.euclidean_model_matching reference_structure.pickle structures.pickle"
+  reference_structure = easy_pickle.load(sys.argv[1])
+  if (type(reference_structure) in (type([]), type(()))):
+    reference_structure = reference_structure[0]
+  structures = easy_pickle.load(sys.argv[2])
+  if (not type(structures) in (type([]), type(()))):
+    structures = [structures]
+
+  print "Reference model:"
+  reference_structure.show_summary()
+  print
+  reference_model = reference_structure.as_emma_model()
+
+  match_list = []
+  match_histogram = dicts.with_default_value(0)
+  for structure in structures:
+    structure.show_summary()
+    if (hasattr(structure, "info")):
+      print structure.info
+    print
+    sys.stdout.flush()
+    refined_matches = emma.model_matches(
+      reference_model,
+      structure.as_emma_model(),
+      tolerance=3,
+      models_are_diffraction_index_equivalent=00000,
+      break_if_match_with_no_singles=0001).refined_matches
+    if (len(refined_matches)):
+      refined_matches[0].show()
+      m = len(refined_matches[0].pairs)
+    else:
+      print "No matches"
+      m = 0
+    match_list.append(match_record(m, structure.scatterers().size()))
+    match_histogram[m] += 1
+    print
+    sys.stdout.flush()
+  print "match_list:", match_list
+  keys = match_histogram.keys()
+  keys.sort()
+  keys.reverse()
+  print "matches: frequency"
+  sum = 0
+  for key in keys:
+    v = match_histogram[key]
+    sum += v
+  s = 0
+  for key in keys:
+    v = match_histogram[key]
+    s += v
+    print "  %3d: %3d = %5.1f%%, %5.1f%%" % (key, v, 100*v/sum, 100*s/sum)
+  print
+  sys.stdout.flush()
+
+if (__name__ == "__main__"):
+  run()
---
+++ cctbx/cctbx/command_line/cctbx.xray_structure.show.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+from cctbx import xray
+from scitbx.python_utils import easy_pickle
+import sys
+
+def run(args):
+  for file_name in args:
+    structures = easy_pickle.load(file_name)
+    if (isinstance(structures, xray.structure)):
+      structures = [structures]
+    for structure in structures:
+      structure.show_summary().show_scatterers()
+      print
+
+if (__name__ == "__main__"):
+  run(sys.argv[1:])
