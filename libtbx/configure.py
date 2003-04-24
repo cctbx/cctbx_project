ae6340bbcef75c2b5c92323f8c201e90a7a04f83
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-04-24 22:45:22 +0000

    managed package dependencies

--- libtbx/configure.py
+++ libtbx/configure.py
@@ -1,12 +1,65 @@
 import sys, os, os.path, pprint
-norm = os.path.normpath
-join = os.path.join
+from os.path import normpath, join, abspath, dirname, isdir
+norm = normpath
+
+class UserError(Exception): pass
 
 class empty: pass
 
-def open_info(path, mode="w", info="Creating:"):
-  print info, path
-  return open(path, mode)
+class registry:
+
+  def __init__(self):
+    self.dict = {}
+    self.list = []
+
+  def append(self, key, value):
+    self.dict[key] = value
+    self.list.append(key)
+
+  def insert(self, position, key, value):
+    self.dict[key] = value
+    self.list.insert(position, key)
+
+  def merge(self, other):
+    i = 0
+    for name in other.list:
+      if (not name in self.dict):
+        self.insert(i, name, other.dict[name])
+        i += 1
+
+class package:
+
+  def __init__(self, dist_root, name):
+    self.dist_root = dist_root
+    self.name = name
+    self.dist_path = norm(join(dist_root, name))
+    if (not isdir(self.dist_path)):
+      raise UserError("Not a package directory: " + self.dist_path)
+    config_path = norm(join(self.dist_path, "libtbx_config"))
+    try:
+      f = open(config_path)
+    except:
+      self.config = None
+    else:
+      try:
+        self.config = eval(" ".join(f.readlines()))
+      except:
+        raise UserError("Corrupt file: " + config_path)
+    self._build_dependency_registry()
+
+  def _build_dependency_registry(self):
+    self.dependency_registry = registry()
+    self._resolve_dependencies(self.dependency_registry)
+
+  def _resolve_dependencies(self, registry):
+    if (self.name in registry.dict):
+      raise UserError("Dependency cycle detected: "
+        + str(registry.list) + " + " + self.name)
+    registry.append(self.name, self)
+    if (self.config != None):
+      for required_package_name in self.config["required_packages"]:
+        package(self.dist_root, required_package_name)._resolve_dependencies(
+          registry)
 
 def insert_normed_path(path_list, addl_path):
   addl_path = norm(addl_path)
@@ -15,56 +68,27 @@ def insert_normed_path(path_list, addl_path):
     if (path_list[:1] == ["."]): i = 1
     path_list.insert(i, addl_path)
 
-def update_libtbx_info(env, package_name, package_dist):
-  libtbx_info_path = norm(join(env.libtbx_build, "libtbx_info"))
-  package_dist_varname = package_name.upper() + "_DIST"
-  libtbx_info = {
-    "LD_LIBRARY_PATH": [],
-    "PYTHONPATH": [norm(join(env.libtbx_build, "libtbx")), env.libtbx_dist],
-    "PATH": [norm(join(env.libtbx_dist, "libtbx/command_line")),
-             norm(join(env.libtbx_build, "libtbx/bin"))],
-    "LIBTBX_PYTHON_EXE": env.libtbx_python_exe,
-    "LIBTBX_DIST": env.libtbx_dist,
-    "LIBTBX_BUILD": env.libtbx_build,
-    "LIBTBX_BOOST": env.libtbx_boost,
-    package_dist_varname: package_dist,
-    "package_names": [],
-  }
-  if (not env.libtbx_scons in (None, "default")):
-    libtbx_info["LIBTBX_SCONS"] = env.libtbx_scons
-  try:
-    f = open(libtbx_info_path, "r")
-  except:
-    libtbx_info_write_action = "Creating:"
-  else:
-    libtbx_info_write_action = "Updating:"
-    libtbx_info.update(eval(" ".join(f.readlines())))
-    f.close()
-  if (libtbx_info["LIBTBX_PYTHON_EXE"] != env.libtbx_python_exe):
-    print >> sys.stderr
-    print >> sys.stderr, "#" * 78
-    print >> sys.stderr, "FATAL ERROR: python executable mismatch:"
-    print >> sys.stderr, "  previously:", libtbx_info["LIBTBX_PYTHON_EXE"]
-    print >> sys.stderr, "         now:", env.libtbx_python_exe
-    print >> sys.stderr, "#" * 78
-    print >> sys.stderr
-    sys.exit(1)
-  assert libtbx_info["LIBTBX_DIST"] == env.libtbx_dist
-  assert libtbx_info["LIBTBX_BUILD"] == env.libtbx_build
-  assert libtbx_info[package_dist_varname] == package_dist
-  insert_normed_path(libtbx_info["LD_LIBRARY_PATH"],
-    join(env.libtbx_build, "libtbx"))
-  insert_normed_path(libtbx_info["PYTHONPATH"],
-    package_dist)
-  insert_normed_path(libtbx_info["PATH"],
-    join(package_dist, package_name, "command_line"))
-  package_names = libtbx_info["package_names"]
-  if (not package_name in package_names):
-    package_names.append(package_name)
-  f = open_info(libtbx_info_path, "w", libtbx_info_write_action)
-  pprint.pprint(libtbx_info, f)
-  f.close()
-  return libtbx_info
+class libtbx_info:
+
+  def __init__(self, env):
+    self.LD_LIBRARY_PATH = [norm(join(env.libtbx_build, "libtbx"))]
+    self.PYTHONPATH = [norm(join(env.libtbx_build, "libtbx")), env.libtbx_dist]
+    self.PATH = [norm(join(env.libtbx_dist, "libtbx/command_line")),
+                 norm(join(env.libtbx_build, "libtbx/bin"))]
+    self.LIBTBX_BUILD = env.libtbx_build
+    self.LIBTBX_DIST = env.libtbx_dist
+    self.LIBTBX_PYTHON_EXE = norm(abspath(sys.executable))
+
+  def update(self, package):
+    self.__dict__[package.name.upper() + "_DIST"] = package.dist_path
+    insert_normed_path(
+      self.PYTHONPATH, package.dist_path)
+    insert_normed_path(
+      self.PATH, join(package.dist_path, package.name, "command_line"))
+
+def open_info(path, mode="w", info="Creating:"):
+  print info, path
+  return open(path, mode)
 
 def emit_env_run_sh(libtbx_build, libtbx_info):
   env_run_sh_path = norm(join(libtbx_build, "env_run.sh"))
@@ -127,34 +151,9 @@ def emit_setpaths_bat(libtbx_build, libtbx_info):
   print >> f, 'set PATHEXT=.PY;%PATHEXT%'
   f.close()
 
-def find_scons_engine(env):
-  env.libtbx_scons = norm(join(env.dist_root, "scons/src/engine"))
-  if (not os.path.isdir(env.libtbx_scons)):
-    env.libtbx_scons = norm(join(env.dist_root, "scons/engine"))
-
-def find_scons(env):
-  from_env = 0
-  try: env.libtbx_scons = os.environ["LIBTBX_SCONS"]
-  except: find_scons_engine(env)
-  else: from_env = 1
-  sys.path.insert(0, env.libtbx_scons)
-  try: import SCons
-  except: del sys.path[0]
-  else: return
-  env.libtbx_scons = None
-  if (not from_env):
-    try: import SCons
-    except: pass
-    else: env.libtbx_scons = "default"
-
 def emit_SConstruct(env, libtbx_info):
-  if (env.libtbx_scons == None):
-    if (not os.path.isdir("libtbx")):
-      print >> sys.stderr, "Warning:",
-      print >> sys.stderr, "Cannot find SCons (Software Construction Tool)"
-      print >> sys.stderr, "         For more information please refer to:"
-      print >> sys.stderr, "         XXX"
-    return
+  package_list = env.packages.list[:]
+  package_list.reverse()
   SConstruct_path = norm(join(env.libtbx_build, "SConstruct"))
   f = open_info(SConstruct_path)
   print >> f, 'import os, os.path'
@@ -164,10 +163,10 @@ def emit_SConstruct(env, libtbx_info):
   print >> f, 'try:'
   print >> f, '  CScanSetFlags('
   print >> f, '    python=0,'
-  print >> f, '    boost=0,'
-  print >> f, '    libtbx=1,'
-  for package_name in libtbx_info["package_names"]:
-    print >> f, '    %s=1,' % package_name
+  for package_name in package_list:
+    flag = 1
+    if (package_name == "boost"): flag = 0
+    print >> f, '    %s=%d,' % (package_name, flag)
   print >> f, '  )'
   print >> f, 'except:'
   print >> f, '  pass'
@@ -180,35 +179,35 @@ def use_SConscript_if_present(package_name):
   if (os.path.isfile(dist + "/SConscript")):
     SConscript(package_name + "/SConscript")
 '''
-  for package_name in libtbx_info["package_names"]:
+  for package_name in package_list:
     print >> f, 'use_SConscript_if_present("%s")' % package_name
   f.close()
 
-def run():
+def run(args):
   env = empty()
-  env.libtbx_python_exe = norm(os.path.abspath(sys.executable))
-  env.libtbx_build = norm(os.path.abspath(os.getcwd()))
-  env.libtbx_dist = norm(os.path.dirname(norm(os.path.abspath(sys.argv[0]))))
-  env.dist_root = norm(os.path.dirname(env.libtbx_dist))
-  env.libtbx_boost = norm(join(env.dist_root, "boost"))
-  find_scons(env)
-  libtbx_info = None
-  for arg in sys.argv[1:]:
-    package_dist = norm(join(env.dist_root, arg))
-    if (not os.path.isdir(package_dist)):
-      print "Error: Not a directory or platform identifier:", arg
-      sys.exit(1)
-    else:
-      libtbx_info = update_libtbx_info(env, arg, package_dist)
-      if (hasattr(os, "symlink")):
-        emit_env_run_sh(env.libtbx_build, libtbx_info)
-        emit_setpaths_csh(env.libtbx_build, libtbx_info)
-      else:
-        emit_setpaths_bat(env.libtbx_build, libtbx_info)
-  if (libtbx_info == None):
-    print "Specify at least one package name."
+  env.libtbx_build = norm(abspath(os.getcwd()))
+  env.libtbx_dist = norm(dirname(norm(abspath(args[0]))))
+  env.dist_root = norm(dirname(env.libtbx_dist))
+  env.packages = registry()
+  for arg in args[1:]:
+    env.packages.merge(package(env.dist_root, arg).dependency_registry)
+  if (len(env.packages.list) == 0):
+    print "Error: At least one package must be specified."
+    return
+  info = libtbx_info(env)
+  print "Top-down list of all packages involved:"
+  for package_name in env.packages.list:
+    print " ", package_name
+    info.update(env.packages.dict[package_name])
+  if (hasattr(os, "symlink")):
+    emit_env_run_sh(env.libtbx_build, info.__dict__)
+    emit_setpaths_csh(env.libtbx_build, info.__dict__)
   else:
-    emit_SConstruct(env, libtbx_info)
+    emit_setpaths_bat(env.libtbx_build, info.__dict__)
+  emit_SConstruct(env, info)
 
 if (__name__ == "__main__"):
-  run()
+  try:
+    run(sys.argv)
+  except UserError, e:
+    print "Error:", e
