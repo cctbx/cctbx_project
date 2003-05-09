83a38203525c660293d1b44e878eb8b7d4928162
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-05-09 17:44:20 +0000

    check_python_api (when running configure and no SConstruct file is written)

--- libtbx/libtbx/config.py
+++ libtbx/libtbx/config.py
@@ -1,6 +1,34 @@
 import sys, os, pickle
+from libtbx.path import norm_join
 
 class UserError(Exception): pass
+
+def python_include_path(must_exist=True):
+  if (sys.platform == "win32"):
+    include_path = sys.prefix + r"\include"
+  else:
+    include_path = sys.prefix + "/include/python" + sys.version[0:3]
+  include_path = norm_join(include_path)
+  if (must_exist):
+    assert os.path.isdir(include_path)
+  return include_path
+
+def python_api_from_include(must_exist=True):
+  include_path = python_include_path(must_exist)
+  if (not os.path.isdir(include_path)): return None
+  modsupport_h = open(norm_join(include_path, "modsupport.h")).readlines()
+  python_api_version = None
+  for line in modsupport_h:
+    if (line.startswith("#define")):
+      flds = line.split()
+      if (len(flds) == 3 and flds[1] == "PYTHON_API_VERSION"):
+        python_api_version = flds[2]
+        break
+  assert python_api_version != None
+  return python_api_version
+
+def python_api_version_file_name(libtbx_build):
+  return norm_join(libtbx_build, "libtbx", "PYTHON_API_VERSION")
 
 class env:
 
@@ -21,6 +49,11 @@ class env:
       raise UserError("Python version incompatible with this build."
        + " Version used to configure: %d.%d." % self.python_version_major_minor
        + " Version used now: %d.%d." % sys.version_info[:2])
+
+  def write_api_file(self):
+    api_file_name = python_api_version_file_name(self.LIBTBX_BUILD)
+    api_from_include = python_api_from_include()
+    print >> open(api_file_name, "w"), api_from_include
 
   def dist_name(self, package_name):
     return package_name.upper() + "_DIST"
