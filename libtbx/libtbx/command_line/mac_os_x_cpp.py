c451e9ac0fc859827c4d63b64770696558c1650e
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-05-21 18:08:38 +0000

    multi-trial compilation under Mac OS 10

--- libtbx/libtbx/command_line/mac_os_x_cpp.py
+++ libtbx/libtbx/command_line/mac_os_x_cpp.py
@@ -1,16 +1,44 @@
 #! /usr/bin/env python
+
 import sys, os
-assert sys.argv[-4] == "-c"
-assert sys.argv[-3] == "-o"
-out = sys.argv[-2]
-assert out.endswith(".os")
-ppout = out[:-3] + "_pp.cpp"
-cmd = " ".join(["g++"] + sys.argv[1:-4] + ["-E", sys.argv[-1], ">", ppout])
-os.system(cmd)
-args = ["g++"]
-for arg in sys.argv[1:-4]:
-  if (not arg.startswith("-D") and not arg.startswith("-I")):
-    args.append(arg)
-args.extend(["-c", "-o", out, ppout])
-cmd = " ".join(args)
-os.system(cmd)
+
+def compile(argv):
+  assert argv[-4] == "-c"
+  assert argv[-3] == "-o"
+  out = argv[-2]
+  assert out.endswith(".os")
+  cmd = " ".join(["g++"] + argv[1:])
+  os.system(cmd)
+  sys.stderr.flush()
+  sys.stdout.flush()
+  if (os.path.isfile(out)):
+    print "One-phase success."
+    return True
+  ppout = out[:-3] + "_pp.cpp"
+  cmd = " ".join(["g++"] + argv[1:-4] + ["-E", argv[-1], ">", ppout])
+  os.system(cmd)
+  sys.stderr.flush()
+  sys.stdout.flush()
+  args = ["g++"]
+  for arg in argv[1:-4]:
+    if (not arg.startswith("-D") and not arg.startswith("-I")):
+      args.append(arg)
+  args.extend(["-c", "-o", out, ppout])
+  cmd = " ".join(args)
+  os.system(cmd)
+  sys.stderr.flush()
+  sys.stdout.flush()
+  if (os.path.isfile(out)):
+    print "Two-phase success."
+    return True
+  return False
+
+if (not compile(sys.argv)):
+  try: i = sys.argv.index("-O3")
+  except: sys.exit(1)
+  print "Compiling without optimization."
+  sys.stdout.flush()
+  argv = sys.argv[:i] + ["-O0"] + sys.argv[i+1:]
+  if (not compile(argv)):
+    print "All attempts failed."
+    sys.exit(1)
