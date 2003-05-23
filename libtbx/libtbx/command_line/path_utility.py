b8d7a2594cd24dad9d0124f737b9e1c950113e38
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-05-23 00:34:10 +0000

    unsetpath.csh

---
+++ libtbx/libtbx/command_line/path_utility.py
@@ -0,0 +1,35 @@
+import sys, os
+
+def norm(path):
+  return os.path.normpath(os.path.normcase(path))
+
+def run(args):
+  assert len(args) == 3
+  mode = args[0]
+  assert mode in ("prepend", "delete")
+  env_key = args[1]
+  arg_paths = args[2].split(os.pathsep)
+  try: env_val = os.environ[env_key]
+  except: env_val = ""
+  env_paths = env_val.split(os.pathsep)
+  remaining_env_paths = []
+  if (mode == "delete"):
+    arg_paths_norm = []
+    for path in arg_paths:
+      if (path == ""): continue
+      arg_paths_norm.append(norm(path))
+    for path in env_paths:
+      if (path == ""): continue
+      if (not norm(path) in arg_paths_norm):
+        remaining_env_paths.append(path)
+  else:
+    remaining_env_paths_norm = []
+    for path in arg_paths + env_paths:
+      if (path == ""): continue
+      if (not norm(path) in remaining_env_paths_norm):
+        remaining_env_paths.append(path)
+        remaining_env_paths_norm.append(norm(path))
+  return os.pathsep.join(remaining_env_paths)
+
+if (__name__ == "__main__"):
+  print run(sys.argv[1:])
