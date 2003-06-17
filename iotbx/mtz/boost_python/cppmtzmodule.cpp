06597ab9a779370c3078e80ca11133c3f330ae11
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-06-17 13:36:06 +0000

    reorganization to facilitate Mac OS 10.2 gcc 3.3 compilation

--- iotbx/mtz/boost_python/cppmtzmodule.cpp
+++ iotbx/mtz/boost_python/cppmtzmodule.cpp
@@ -1,72 +1,92 @@
-#include <boost/python.hpp>
 #include <cctbx/boost_python/flex_fwd.h>
-#include <scitbx/boost_python/utils.h>
-#include "iotbx/cppmtz.h"
-#include <iotbx/mtzwriter.h>
-using namespace boost::python;
+#include <iotbx/cppmtz.h>
+#include <boost/python/module.hpp>
+#include <boost/python/class.hpp>
+#include <boost/python/return_value_policy.hpp>
+#include <boost/python/copy_non_const_reference.hpp>
+
+namespace iotbx { namespace mtz { namespace boost_python {
+
+  void wrap_Mtz()
+  {
+    using namespace boost::python;
+    class_<Mtz>("Mtz", init<std::string>())
+      .def("title",      &Mtz::title)
+      .def("SpaceGroup", &Mtz::SpaceGroup)
+      .def("getSgtbxSpaceGroup", &Mtz::getSgtbxSpaceGroup)
+      .def("nsym",       &Mtz::nsym)
+      .def("size",       &Mtz::size,
+          return_value_policy<copy_non_const_reference>())
+      .def("ncrystals",  &Mtz::ncrystals,
+          return_value_policy<copy_non_const_reference>())
+      .def("columns",    &Mtz::columns)
+      .def("history",    &Mtz::history)
+      .def("getColumn",  &Mtz::getColumn)
+      .def("getCrystal", &Mtz::getCrystal)
+      .def("columnToCrystal", &Mtz::columnToCrystal)
+      .def("printHeader",&Mtz::printHeader)
+      .def("UnitCell",   &Mtz::UnitCell)
+      .def("ndatasets",  &Mtz::ndatasets)
+      .def("ncolumns",   &Mtz::ncolumns,
+           return_value_policy<copy_non_const_reference>())
+      .def("MIx",        &Mtz::MIx)
+      .def("printHeaderAdv",&Mtz::printHeaderAdv)
+      .def("valid_indices", &Mtz::valid_indices)
+      .def("valid_indices", &Mtz::valid_indices_anomalous)
+      .def("valid_values", &Mtz::valid_values)
+      .def("valid_values", &Mtz::valid_values_anomalous)
+      .def("valid_complex", &Mtz::valid_complex)
+      .def("valid_complex", &Mtz::valid_complex_anomalous)
+      .def("valid_hl", &Mtz::valid_hl)
+      .def("valid_integers", &Mtz::valid_integers)
+    ;
+  }
+
+  void wrap_Crystal()
+  {
+    using namespace boost::python;
+    class_<Crystal>("Crystal", no_init)
+      .def("crystal_name",&Crystal::crystal_name)
+      .def("project_name",&Crystal::project_name)
+      .def("ndatasets",   &Crystal::ndatasets)
+      .def("UnitCell",    &Crystal::UnitCell)
+      .def("getDataset",  &Crystal::getDataset)
+    ;
+  }
+
+  void wrap_Dataset()
+  {
+    using namespace boost::python;
+    class_<Dataset>("Dataset", no_init)
+      .def("dataset_name",&Dataset::dataset_name)
+      .def("wavelength",  &Dataset::wavelength)
+      .def("ncolumns",    &Dataset::ncolumns)
+      .def("getColumn",   &Dataset::getColumn)
+    ;
+  }
+
+  void wrap_Column()
+  {
+    using namespace boost::python;
+    class_<Column>("Column", no_init)
+      .def("label",       &Column::label)
+      .def("type",        &Column::type)
+      .def("__getitem__", &Column::lookup) // XXX potential crash!
+      .def("__call__",    &Column::lookup) // XXX potential crash!
+      .def("isnan",       &Column::isnan) // XXX potential crash!
+    ;
+  }
+
+  void wrap_MtzWriter();
+
+}}} // namespace iotbx::mtz::boost_python
 
 BOOST_PYTHON_MODULE(mtz)
 {
-    class_<iotbx::mtz::Mtz>("Mtz", init<std::string>())
-      .def("title",      &iotbx::mtz::Mtz::title)
-      .def("SpaceGroup", &iotbx::mtz::Mtz::SpaceGroup)
-      .def("getSgtbxSpaceGroup", &iotbx::mtz::Mtz::getSgtbxSpaceGroup)
-      .def("nsym",       &iotbx::mtz::Mtz::nsym)
-      .def("size",       &iotbx::mtz::Mtz::size,
-          return_value_policy<copy_non_const_reference>())
-      .def("ncrystals",  &iotbx::mtz::Mtz::ncrystals,
-          return_value_policy<copy_non_const_reference>())
-      .def("columns",    &iotbx::mtz::Mtz::columns)
-      .def("history",    &iotbx::mtz::Mtz::history)
-      .def("getColumn",  &iotbx::mtz::Mtz::getColumn)
-      .def("getCrystal", &iotbx::mtz::Mtz::getCrystal)
-      .def("columnToCrystal", &iotbx::mtz::Mtz::columnToCrystal)
-      .def("printHeader",&iotbx::mtz::Mtz::printHeader)
-      .def("UnitCell",   &iotbx::mtz::Mtz::UnitCell)
-      .def("ndatasets",  &iotbx::mtz::Mtz::ndatasets)
-      .def("ncolumns",   &iotbx::mtz::Mtz::ncolumns,
-           return_value_policy<copy_non_const_reference>())
-      .def("MIx",        &iotbx::mtz::Mtz::MIx)
-      .def("printHeaderAdv",&iotbx::mtz::Mtz::printHeaderAdv)
-      .def("valid_indices", &iotbx::mtz::Mtz::valid_indices)
-      .def("valid_indices", &iotbx::mtz::Mtz::valid_indices_anomalous)
-      .def("valid_values", &iotbx::mtz::Mtz::valid_values)
-      .def("valid_values", &iotbx::mtz::Mtz::valid_values_anomalous)
-      .def("valid_complex", &iotbx::mtz::Mtz::valid_complex)
-      .def("valid_complex", &iotbx::mtz::Mtz::valid_complex_anomalous)
-      .def("valid_hl", &iotbx::mtz::Mtz::valid_hl)
-      .def("valid_integers", &iotbx::mtz::Mtz::valid_integers)
-    ;
-
-    class_<iotbx::mtz::Crystal>("Crystal", no_init)
-      .def("crystal_name",&iotbx::mtz::Crystal::crystal_name)
-      .def("project_name",&iotbx::mtz::Crystal::project_name)
-      .def("ndatasets",   &iotbx::mtz::Crystal::ndatasets)
-      .def("UnitCell",    &iotbx::mtz::Crystal::UnitCell)
-      .def("getDataset",  &iotbx::mtz::Crystal::getDataset)
-    ;
-
-    class_<iotbx::mtz::Dataset>("Dataset", no_init)
-      .def("dataset_name",&iotbx::mtz::Dataset::dataset_name)
-      .def("wavelength",  &iotbx::mtz::Dataset::wavelength)
-      .def("ncolumns",    &iotbx::mtz::Dataset::ncolumns)
-      .def("getColumn",   &iotbx::mtz::Dataset::getColumn)
-    ;
-
-    class_<iotbx::mtz::Column>("Column", no_init)
-      .def("label",       &iotbx::mtz::Column::label)
-      .def("type",        &iotbx::mtz::Column::type)
-      .def("__getitem__", &iotbx::mtz::Column::lookup) // XXX potential crash!
-      .def("__call__",    &iotbx::mtz::Column::lookup) // XXX potential crash!
-      .def("isnan",       &iotbx::mtz::Column::isnan) // XXX potential crash!
-    ;
-
-    class_<iotbx::mtz::MtzWriter>("MtzWriter", init<>())
-      .def("setTitle",      &iotbx::mtz::MtzWriter::setTitle)
-      .def("raw_setSpaceGroup", &iotbx::mtz::MtzWriter::setSpaceGroup)
-      .def("oneCrystal",    &iotbx::mtz::MtzWriter::oneCrystal)
-      .def("oneDataset",    &iotbx::mtz::MtzWriter::oneDataset)
-      .def("addColumn",     &iotbx::mtz::MtzWriter::addColumn)
-      .def("write",         &iotbx::mtz::MtzWriter::write)
-    ;
+  using namespace iotbx::mtz::boost_python;
+  wrap_Mtz();
+  wrap_Crystal();
+  wrap_Dataset();
+  wrap_Column();
+  wrap_MtzWriter();
 }
