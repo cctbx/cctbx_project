3236fc3991e465a1598f2071cd9f99ef1e9bebe4
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-07-30 16:38:01 +0000

    xds reader integrated

--- iotbx/iotbx/xds/read_ascii.py
+++ iotbx/iotbx/xds/read_ascii.py
@@ -8,80 +8,98 @@ import sys
 def get_rhs(line):
   return line.split("=", 1)[1]
 
-class column_index_retriever:
+class reader:
 
-  def __init__(self, line):
-    self.number_of_items_in_each_data_record = int(get_rhs(line))
+  def __init__(self, file_handle, header_only=00000):
+    f = iter(file_handle)
+    flds = f.next().split()
+    assert flds[0] == "!FORMAT=XDS_ASCII"
+    assert flds[1] == "MERGE=TRUE"
+    if   (flds[2] == "FRIEDEL'S_LAW=FALSE"):
+      self.anomalous_flag = 0001
+    elif (flds[2] == "FRIEDEL'S_LAW=TRUE"):
+      self.anomalous_flag = 00000
+    else:
+      raise RuntimeError, "Expected FRIEDEL'S_LAW=FALSE|TRUE"
+    self.unit_cell = None
+    self.space_group_number = None
+    self.number_of_items_in_each_data_record = None
+    self.miller_index_columns = [None, None, None]
+    self.iobs_column = None
+    self.sigma_iobs_column = None
+    for line in f:
+      if (line.startswith("!SPACE_GROUP_NUMBER=")):
+        self.space_group_number = int(get_rhs(line))
+        assert 1 <= self.space_group_number <= 230
+      elif (line.startswith("!UNIT_CELL_CONSTANTS=")):
+        self.unit_cell = uctbx.unit_cell(get_rhs(line))
+      elif (line.startswith("!NUMBER_OF_ITEMS_IN_EACH_DATA_RECORD=")):
+        self.number_of_items_in_each_data_record = int(get_rhs(line))
+      elif (line.startswith("!ITEM_H=")):
+        self.miller_index_columns[0] = self.column_index(line)
+      elif (line.startswith("!ITEM_K=")):
+        self.miller_index_columns[1] = self.column_index(line)
+      elif (line.startswith("!ITEM_L=")):
+        self.miller_index_columns[2] = self.column_index(line)
+      elif (line.startswith("!ITEM_IOBS=")):
+        self.iobs_column = self.column_index(line)
+      elif (line.startswith("!ITEM_SIGMA(IOBS)=")):
+        self.sigma_iobs_column = self.column_index(line)
+      elif (line.startswith("!END_OF_HEADER")):
+        break
+    assert self.unit_cell is not None
+    assert self.space_group_number is not None
+    assert self.column_index is not None
+    assert None not in self.miller_index_columns
+    assert self.iobs_column is not None
+    assert self.sigma_iobs_column is not None
+    if (header_only):
+      self.miller_indices = None
+      self.iobs = None
+      self.sigma_iobs = None
+    else:
+      self.miller_indices = flex.miller_index()
+      self.iobs = flex.double()
+      self.sigma_iobs = flex.double()
+      for line in f:
+        if (line.startswith("!END_OF_DATA")):
+          break
+        data = line.split()
+        assert len(data) == self.number_of_items_in_each_data_record
+        h = [int(data[i]) for i in self.miller_index_columns]
+        self.miller_indices.append(h)
+        self.iobs.append(float(data[self.iobs_column]))
+        self.sigma_iobs.append(float(data[self.sigma_iobs_column]))
 
-  def get(self, line):
+  def column_index(self, line):
     i_column = int(get_rhs(line))-1
     assert 0 <= i_column < self.number_of_items_in_each_data_record
     return i_column
 
-def read(file_name):
-  f = iter(open(file_name))
-  flds = f.next().split()
-  assert flds[0] == "!FORMAT=XDS_ASCII"
-  assert flds[1] == "MERGE=TRUE"
-  if   (flds[2] == "FRIEDEL'S_LAW=FALSE"):
-    anomalous_flag = 0001
-  elif (flds[2] == "FRIEDEL'S_LAW=TRUE"):
-    anomalous_flag = 00000
-  else:
-    raise RuntimeError, "Expected FRIEDEL'S_LAW=FALSE|TRUE"
-  unit_cell = None
-  space_group_number = None
-  column_index = None
-  miller_index_columns = [None, None, None]
-  iobs_column = None
-  sigma_iobs_column = None
-  for line in f:
-    if (line.startswith("!SPACE_GROUP_NUMBER=")):
-      space_group_number = int(get_rhs(line))
-    elif (line.startswith("!UNIT_CELL_CONSTANTS=")):
-      unit_cell = uctbx.unit_cell(get_rhs(line))
-    elif (line.startswith("!NUMBER_OF_ITEMS_IN_EACH_DATA_RECORD=")):
-      column_index = column_index_retriever(line)
-    elif (line.startswith("!ITEM_H=")):
-      miller_index_columns[0] = column_index.get(line)
-    elif (line.startswith("!ITEM_K=")):
-      miller_index_columns[1] = column_index.get(line)
-    elif (line.startswith("!ITEM_L=")):
-      miller_index_columns[2] = column_index.get(line)
-    elif (line.startswith("!ITEM_IOBS=")):
-      iobs_column = column_index.get(line)
-    elif (line.startswith("!ITEM_SIGMA(IOBS)=")):
-      sigma_iobs_column = column_index.get(line)
-    elif (line.startswith("!END_OF_HEADER")):
-      break
-  assert unit_cell is not None
-  assert space_group_number is not None
-  assert column_index is not None
-  assert None not in miller_index_columns
-  assert iobs_column is not None
-  assert sigma_iobs_column is not None
-  miller_indices = flex.miller_index()
-  iobs = flex.double()
-  sigma_iobs = flex.double()
-  for line in f:
-    if (line.startswith("!END_OF_DATA")):
-      break
-    data = line.split()
-    assert len(data) == column_index.number_of_items_in_each_data_record
-    h = [int(data[i]) for i in miller_index_columns]
-    miller_indices.append(h)
-    iobs.append(float(data[iobs_column]))
-    sigma_iobs.append(float(data[sigma_iobs_column]))
-  miller_array = miller.array(
-    miller_set=miller.set(
-      crystal_symmetry=crystal.symmetry(
-        unit_cell=unit_cell,
-        space_group_symbol=space_group_number),
-      indices=miller_indices,
-      anomalous_flag=anomalous_flag),
-    data=iobs,
-    sigmas=sigma_iobs).set_observation_type_xray_intensity()
-  miller_array.show_comprehensive_summary()
+  def info(self):
+    return "iobs,sigma_iobs"
+
+  def crystal_symmetry(self):
+    return crystal.symmetry(
+      unit_cell=self.unit_cell,
+      space_group_symbol=self.space_group_number)
+
+  def as_miller_array(self, crystal_symmetry=None, force_symmetry=00000,
+                            info_prefix=""):
+    return (miller.array(
+      miller_set=miller.set(
+        crystal_symmetry=self.crystal_symmetry().join_symmetry(
+          other_symmetry=crystal_symmetry,
+          force=force_symmetry),
+        indices=self.miller_indices,
+        anomalous_flag=self.anomalous_flag),
+      data=self.iobs,
+      sigmas=self.sigma_iobs)
+      .set_info(info_prefix+self.info()).set_observation_type_xray_intensity())
+
+  def as_miller_arrays(self, crystal_symmetry=None, force_symmetry=00000,
+                             info_prefix=""):
+    return [self.as_miller_array(crystal_symmetry,force_symmetry,info_prefix)]
 
 if (__name__ == "__main__"):
-  read(sys.argv[1])
+  reader(open(sys.argv[1])).as_miller_array().show_comprehensive_summary()
