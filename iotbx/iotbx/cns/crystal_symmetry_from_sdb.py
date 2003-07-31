f78d116c7b7a34fca3e617a18af9c619db0dd027
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-07-31 20:05:11 +0000

    read symmetry from solve files

--- iotbx/iotbx/cns/crystal_symmetry_from_sdb.py
+++ iotbx/iotbx/cns/crystal_symmetry_from_sdb.py
@@ -4,7 +4,9 @@ def extract_from(file_name=None, file=None):
   assert [file_name, file].count(None) == 1
   if (file is None):
     file = open(file_name)
-  try: sdb_files = sdb_reader.multi_sdb_parser(file)
-  except: return None
+  sdb_files = sdb_reader.multi_sdb_parser(file)
   assert len(sdb_files) > 0
-  return sdb_files[0].crystal_symmetry()
+  crystal_symmetry = sdb_files[0].crystal_symmetry()
+  assert [crystal_symmetry.unit_cell(),
+          crystal_symmetry.space_group_info()].count(None) < 2
+  return crystal_symmetry
