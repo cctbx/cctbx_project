d50a92352f63229e0ecf9cb392b3ac387844d8db
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-08-08 01:19:27 +0000

    include sdb parser in regression test

--- iotbx/iotbx/cns/tst_cns.py
+++ iotbx/iotbx/cns/tst_cns.py
@@ -1,8 +1,13 @@
 import iotbx.cns.xray_structure
+from iotbx.cns import sdb_reader
+from cctbx import crystal
 from cctbx import sgtbx
 from cctbx.development import random_structure
+from cctbx.array_family import flex
+from cStringIO import StringIO
+import sys
 
-def exercise_xray_structure_as_pdb_file():
+def exercise_sdb(verbose=0):
   structure = random_structure.xray_structure(
     space_group_info=sgtbx.space_group_info("P 31"),
     elements=["N","C","C","O"]*2,
@@ -10,14 +15,33 @@ def exercise_xray_structure_as_pdb_file():
     min_distance=2.,
     general_positions_only=00000,
     random_u_iso=0001)
-  print structure.as_cns_sdb_file(
+  f_abs = abs(structure.structure_factors(
+    anomalous_flag=00000, d_min=2, direct=0001).f_calc())
+  sdb_out = structure.as_cns_sdb_file(
     file="foo.sdb",
     description="random_structure",
     comment=["any", "thing"],
-    group="best"),
+    group="best")
+  if (0 or verbose):
+    sys.stdout.write(sdb_out)
+  sdb_files = sdb_reader.multi_sdb_parser(StringIO(sdb_out))
+  assert len(sdb_files) == 1
+  structure_read = sdb_files[0].as_xray_structure(
+    crystal_symmetry=crystal.symmetry(
+      unit_cell=structure.unit_cell(),
+      space_group_info=None))
+  f_read = abs(f_abs.structure_factors_from_scatterers(
+    xray_structure=structure_read, direct=0001).f_calc())
+  regression = flex.linear_regression(f_abs.data(), f_read.data())
+  assert regression.is_well_defined()
+  if (0 or verbose):
+    regression.show_summary()
+  assert abs(regression.slope()-1) < 1.e-4
+  assert abs(regression.y_intercept()) < 1.e-3
 
 def run():
-  exercise_xray_structure_as_pdb_file()
+  verbose = "--Verbose" in sys.argv[1:]
+  exercise_sdb(verbose)
   print "OK"
 
 if (__name__ == "__main__"):
