cf35e2ae691892aab7dd32ecf29fb1780723e0fe
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-08-08 00:42:35 +0000

    low-level pdb parser from phenix tree copied to iotbx.pdb and significantly modified

--- iotbx/iotbx/pdb/tst_pdb.py
+++ iotbx/iotbx/pdb/tst_pdb.py
@@ -1,21 +1,43 @@
 import iotbx.pdb.xray_structure
 from cctbx import sgtbx
 from cctbx.development import random_structure
+from cctbx.array_family import flex
+from cStringIO import StringIO
+import sys
 
-def exercise_xray_structure_as_pdb_file():
+def exercise_xray_structure(anisotropic_flag, verbose=0):
   structure = random_structure.xray_structure(
     space_group_info=sgtbx.space_group_info("P 31"),
     elements=["N","C","C","O"]*2,
     volume_per_atom=500,
     min_distance=2.,
     general_positions_only=00000,
-    random_u_iso=0001)
-  print structure.as_pdb_file(remark="Title", remarks=["Any", "Thing"]),
-  print structure.as_pdb_file(remark="Title", remarks=["Any", "Thing"],
-                              fractional_coordinates=0001),
+    random_u_iso=0001,
+    anisotropic_flag=anisotropic_flag)
+  f_abs = abs(structure.structure_factors(
+    anomalous_flag=00000, d_min=2, direct=0001).f_calc())
+  for fractional_coordinates in (00000, 0001):
+    pdb_file = structure.as_pdb_file(
+      remark="Title", remarks=["Any", "Thing"],
+      fractional_coordinates=fractional_coordinates)
+    if (0 or verbose):
+      sys.stdout.write(pdb_file)
+    structure_read = iotbx.pdb.as_xray_structure(
+      file_iterator=StringIO(pdb_file),
+      fractional_coordinates=fractional_coordinates)
+    f_read = abs(f_abs.structure_factors_from_scatterers(
+      xray_structure=structure_read, direct=0001).f_calc())
+    regression = flex.linear_regression(f_abs.data(), f_read.data())
+    assert regression.is_well_defined()
+    if (0 or verbose):
+      regression.show_summary()
+    assert abs(regression.slope()-1) < 1.e-2
+    assert abs(regression.y_intercept()) < 0.1
 
 def run():
-  exercise_xray_structure_as_pdb_file()
+  verbose = "--Verbose" in sys.argv[1:]
+  for anisotropic_flag in (00000, 0001):
+    exercise_xray_structure(anisotropic_flag, verbose=verbose)
   print "OK"
 
 if (__name__ == "__main__"):
--- iotbx/iotbx/pdb/xray_structure.py
+++ iotbx/iotbx/pdb/xray_structure.py
@@ -1,9 +1,12 @@
+from iotbx.pdb import parser
+from iotbx.pdb import residue_info
 from cctbx import xray
 from cctbx import crystal
 from cctbx import uctbx
 from cctbx import sgtbx
 from cctbx import adptbx
-from phenix.foundation.Structure import PDBClass # XXX move to iotbx
+from cctbx.array_family import flex
+from scitbx.python_utils.math_utils import iround
 from cStringIO import StringIO
 
 def xray_structure_as_pdb_file(self, remark=None, remarks=[],
@@ -49,22 +52,27 @@ def xray_structure_as_pdb_file(self, remark=None, remarks=[],
   #                                      Angstroms.
   # 55 - 60  Real(6.2)     occupancy     Occupancy.
   # 61 - 66  Real(6.2)     tempFactor    Temperature factor.
-  i = 0
+  serial = 0
   for scatterer in self.scatterers():
-    i += 1
-    assert not scatterer.anisotropic_flag, "Not implemented." # XXX
+    serial += 1
+    if (scatterer.anisotropic_flag):
+      u_cart = adptbx.u_star_as_u_cart(self.unit_cell(), scatterer.u_star)
+      u = adptbx.u_cart_as_u_iso(u_cart)
+    else:
+      u_cart = None
+      u = scatterer.u_iso
     xyz = scatterer.site
     if (not fractional_coordinates):
       xyz = self.unit_cell().orthogonalize(xyz)
     label = scatterer.label.upper()
-    print >> s, "ATOM  %5d %-4s %-3s  %4d    %8.3f%8.3f%8.3f%6.2f%6.2f" % ((
-      i,
-      label[:4],
-      label[:3],
-      i,
-      ) + xyz + (
-      scatterer.occupancy,
-      adptbx.u_as_b(scatterer.u_iso)))
+    assert serial < 100000
+    atom_07_27 = ("%5d %-4s %-3s  %4d" % (
+      serial, label[:4], label[:3], serial%10000),)
+    print >> s, "ATOM  %s    %8.3f%8.3f%8.3f%6.2f%6.2f" % (
+      atom_07_27 + xyz + (scatterer.occupancy, adptbx.u_as_b(u)))
+    if (u_cart is not None):
+      print >> s, "ANISOU%s%7d%7d%7d%7d%7d%7d" % (
+        atom_07_27 + tuple([iround(x*10000) for x in u_cart]))
   if (connect is not None):
     assert len(connect) == self.scatterers().size()
     i = 0
@@ -79,38 +87,63 @@ def xray_structure_as_pdb_file(self, remark=None, remarks=[],
 
 xray.structure.as_pdb_file = xray_structure_as_pdb_file
 
-def from_pdb(file_name, crystal_symmetry=None, force_symmetry=00000):
-  pdb_file = PDBClass(file_name)
-  try:
-    cryst1 = pdb_file.GetUnitCellRecord()
-  except:
-    cryst1 = None
-  try:
-    unit_cell = uctbx.unit_cell(cryst1.ucparams)
-  except:
-    unit_cell = None
-  try:
-    space_group_info = sgtbx.space_group_info(symbol=cryst1.sGroup)
-  except:
-    space_group_info = None
-  else:
-    if (space_group_info.group() is None):
-      space_group_info = None
+def from_pdb(file_name=None, file_iterator=None,
+             crystal_symmetry=None, force_symmetry=00000,
+             fractional_coordinates=00000):
+  assert [file_name, file_iterator].count(None) == 1
+  if (file_iterator is None):
+    file_iterator = open(file_name)
+  pdb_records = parser.collect_records(raw_records=file_iterator)
+  unit_cell = None
+  space_group_info = None
+  for record in pdb_records:
+    if (record.record_name.startswith("CRYST1")):
+      try: unit_cell = uctbx.unit_cell(record.ucparams)
+      except: pass
+      try: space_group_info = sgtbx.space_group_info(symbol=record.sGroup)
+      except: pass
+      break
   crystal_symmetry=crystal.symmetry(
     unit_cell=unit_cell,
     space_group_info=space_group_info).join_symmetry(
     other_symmetry=crystal_symmetry,
     force=force_symmetry)
+  assert crystal_symmetry.unit_cell() is not None
+  assert crystal_symmetry.space_group_info() is not None
   structure = xray.structure(
     special_position_settings=crystal.special_position_settings(
       crystal_symmetry=crystal_symmetry))
-  for atom in pdb_file.GetAtomList():
-    scatterer = xray.scatterer(
-      label="%s%03d" % (atom.name, atom.serial),
-      site=structure.unit_cell().fractionalize(atom.coordinates),
-      b=atom.tempFactor,
-      occupancy=atom.occupancy)
-    assert structure.unit_cell() is not None
-    assert structure.space_group_info() is not None
-    structure.add_scatterer(scatterer)
+  scatterers = flex.xray_scatterer()
+  prev_record = None
+  for record in pdb_records:
+    if (record.record_name in ("ATOM", "HETATM")):
+      if (fractional_coordinates):
+        site=record.coordinates
+      else:
+        site=structure.unit_cell().fractionalize(record.coordinates)
+      try:
+        caasf = residue_info.get(
+          residue_name=record.resName,
+          atom_name=record.name).scattering_label
+      except KeyError:
+        caasf = ""
+      scatterer = xray.scatterer(
+        label="%s%03d" % (record.name, record.serial),
+        site=site,
+        b=record.tempFactor,
+        occupancy=record.occupancy,
+        caasf=caasf)
+      scatterers.append(scatterer)
+    elif (record.record_name == "ANISOU"):
+      if (not prev_record.record_name in ("ATOM", "HETATM")):
+        record.raise_FormatError(
+          "ANISOU record without preceeding ATOM or HETATM record.")
+      if (prev_record.raw[7:27] != record.raw[7:27]):
+        record.raise_FormatError(
+          "ANISOU record does not match preceeding %s record."
+            % prev_record.record_name)
+      scatterers[-1] = scatterer.copy(
+        u=adptbx.u_cart_as_u_star(structure.unit_cell(), record.Ucart))
+    prev_record = record
+  structure.add_scatterers(scatterers)
   return structure
