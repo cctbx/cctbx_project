c3c02339fc84e2637911e9257415d3bf11726079
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-08-31 11:54:34 +0000

    gruber_1973 reduction; minimal_reduction for both krivy_gruber_1976 and gruber_1973

--- cctbx/cctbx/uctbx/krivy_gruber_1976.py
+++ cctbx/cctbx/uctbx/krivy_gruber_1976.py
@@ -1,33 +1,17 @@
-from cctbx.uctbx import reduction_base
-from cctbx import uctbx
-from cctbx import matrix
+from cctbx.uctbx.reduction_base import iteration_limit_exceeded
+from cctbx.uctbx.reduction_base import reduction_base
+from cctbx.uctbx.reduction_base import minimal_reduction_mixin
 
-class iteration_limit_exceeded(RuntimeError): pass
-
-class reduction(reduction_base.gruber_parameterization):
+class reduction(reduction_base):
 
   def __init__(self, unit_cell, relative_epsilon=None, iteration_limit=None):
-    if (iteration_limit is None):
-      self._iteration_limit = 1000
-    else:
-      self._iteration_limit = iteration_limit
-    reduction_base.gruber_parameterization.__init__(
-      self, unit_cell, relative_epsilon)
-    self._r_inv = matrix.sqr((1,0,0,0,1,0,0,0,1))
-    self._n_iterations = 0
+    reduction_base.__init__(self, unit_cell, relative_epsilon, iteration_limit)
     while (self.step()): pass
 
-  def iteration_limit(self):
-    return self._iteration_limit
+  def _name(self):
+    return "Krivy-Gruber"
 
-  def r_inv(self):
-    return self._r_inv
-
-  def n_iterations(self):
-    return self._n_iterations
-
-  def step(self):
-    s = self
+  def step(s):
     eq = s.eps_eq
     lt = s.eps_lt
     gt = s.eps_gt
@@ -69,96 +53,75 @@ class reduction(reduction_base.gruber_parameterization):
       return 0001
     return 00000
 
-  def cb_update(self, m_elems):
-    if (self._n_iterations == self._iteration_limit):
-      raise iteration_limit_exceeded(
-        "Krivy-Gruber iteration limit exceeded (limit=%d)."
-        % self._iteration_limit)
-    self._r_inv *= matrix.sqr(m_elems)
-    self._n_iterations += 1
+  def a1_action(s):
+    s.n1_action()
 
-  def a1_action(self):
-    self.a, self.b = self.b, self.a
-    self.d, self.e = self.e, self.d
-    self.cb_update((0,-1,0, -1,0,0, 0,0,-1))
+  def a2_action(s):
+    s.n2_action()
 
-  def a2_action(self):
-    self.b, self.c = self.c, self.b
-    self.e, self.f = self.f, self.e
-    self.cb_update((-1,0,0, 0,0,-1, 0,-1,0))
+  def a3_action(s):
+    s.n3_true_action()
 
-  def a3_action(self):
-    lt = self.eps_lt
-    i,j,k = 1,1,1
-    if (lt(self.d, 0)): i = -1
-    if (lt(self.e, 0)): j = -1
-    if (lt(self.f, 0)): k = -1
-    self.d = abs(self.d)
-    self.e = abs(self.e)
-    self.f = abs(self.f)
-    self.cb_update((i,0,0, 0,j,0, 0,0,k))
+  def a4_action(s):
+    s.n3_false_action()
+
+  def a5_action(s):
+    if (s.d > 0):
+      s.c += s.b - s.d
+      s.d -= s.b + s.b
+      s.e -= s.f
+      s.cb_update((1,0,0,0,1,-1,0,0,1))
+    else:
+      s.c += s.b + s.d
+      s.d += s.b + s.b
+      s.e += s.f
+      s.cb_update((1,0,0,0,1,1,0,0,1))
+    assert s.c > 0
+
+  def a6_action(s):
+    if (s.e > 0):
+      s.c += s.a - s.e
+      s.d -= s.f
+      s.e -= s.a + s.a
+      s.cb_update((1,0,-1,0,1,0,0,0,1))
+    else:
+      s.c += s.a + s.e
+      s.d += s.f
+      s.e += s.a + s.a
+      s.cb_update((1,0,1,0,1,0,0,0,1))
+    assert s.c > 0
+
+  def a7_action(s):
+    if (s.f > 0):
+      s.b += s.a - s.f
+      s.d -= s.e
+      s.f -= s.a + s.a
+      s.cb_update((1,-1,0,0,1,0,0,0,1))
+    else:
+      s.b += s.a + s.f
+      s.d += s.e
+      s.f += s.a + s.a
+      s.cb_update((1,1,0,0,1,0,0,0,1))
+    assert s.b > 0
+
+  def a8_action(s):
+    s.c += s.a+s.b+s.d+s.e+s.f
+    s.d += s.b+s.b+s.f
+    s.e += s.a+s.a+s.f
+    s.cb_update((1,0,1,0,1,1,0,0,1))
+    assert s.c > 0
+
+class minimal_reduction(minimal_reduction_mixin, reduction):
+
+  def __init__(self, unit_cell, expected_cycle_limit=None,
+                                iteration_limit=None):
+    minimal_reduction_mixin.__init__(self,
+      unit_cell, expected_cycle_limit, iteration_limit)
 
   def a4_action(self):
-    lt = self.eps_lt
-    gt = self.eps_gt
-    f = [1,1,1]
-    z = -1
-    if (gt(self.d, 0)): f[0] = -1
-    elif (not lt(self.d, 0)): z = 0
-    if (gt(self.e, 0)): f[1] = -1
-    elif (not lt(self.e, 0)): z = 1
-    if (gt(self.f, 0)): f[2] = -1
-    elif (not lt(self.f, 0)): z = 2
-    if (f[0]*f[1]*f[2] < 0):
-      assert z != -1
-      f[z] = -1
-    self.d = -abs(self.d)
-    self.e = -abs(self.e)
-    self.f = -abs(self.f)
-    self.cb_update((f[0],0,0, 0,f[1],0, 0,0,f[2]))
-
-  def a5_action(self):
-    if (self.d > 0):
-      self.c += self.b - self.d
-      self.d -= self.b + self.b
-      self.e -= self.f
-      self.cb_update((1,0,0,0,1,-1,0,0,1))
-    else:
-      self.c += self.b + self.d
-      self.d += self.b + self.b
-      self.e += self.f
-      self.cb_update((1,0,0,0,1,1,0,0,1))
-    assert self.c > 0
-
-  def a6_action(self):
-    if (self.e > 0):
-      self.c += self.a - self.e
-      self.d -= self.f
-      self.e -= self.a + self.a
-      self.cb_update((1,0,-1,0,1,0,0,0,1))
-    else:
-      self.c += self.a + self.e
-      self.d += self.f
-      self.e += self.a + self.a
-      self.cb_update((1,0,1,0,1,0,0,0,1))
-    assert self.c > 0
-
-  def a7_action(self):
-    if (self.f > 0):
-      self.b += self.a - self.f
-      self.d -= self.e
-      self.f -= self.a + self.a
-      self.cb_update((1,-1,0,0,1,0,0,0,1))
-    else:
-      self.b += self.a + self.f
-      self.d += self.e
-      self.f += self.a + self.a
-      self.cb_update((1,1,0,0,1,0,0,0,1))
-    assert self.b > 0
+    self.current_cycle_id = 1
+    reduction.a4_action(self)
 
   def a8_action(self):
-    self.c += self.a+self.b+self.d+self.e+self.f
-    self.d += self.b+self.b+self.f
-    self.e += self.a+self.a+self.f
-    self.cb_update((1,0,1,0,1,1,0,0,1))
-    assert self.c > 0
+    self.current_cycle_id = 2
+    reduction.a8_action(self)
