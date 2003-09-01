f9e2fbcb645b2d489f56375ad53699f5d5836dca
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-09-01 02:51:35 +0000

    ifloor, iceil

--- scitbx/include/scitbx/math/utils.h
+++ scitbx/include/scitbx/math/utils.h
@@ -1,6 +1,8 @@
 #ifndef SCITBX_MATH_UTILS_H
 #define SCITBX_MATH_UTILS_H
 
+#include <cmath>
+
 namespace scitbx { namespace math {
 
   inline
@@ -10,6 +12,14 @@ namespace scitbx { namespace math {
     if (x < 0) return static_cast<int>(x-0.5);
     return static_cast<int>(x+.5);
   }
+
+  inline
+  int
+  iceil(double x) { return iround(std::ceil(x)); }
+
+  inline
+  int
+  ifloor(double x) { return iround(std::floor(x)); }
 
 }} // namespace scitbx::math
 
