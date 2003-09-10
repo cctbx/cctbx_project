3d35a260e52397411d9cd1460b3369fe92548277
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-09-10 18:31:18 +0000

    eliminate_u_extra() Python bindings

--- cctbx/xray/boost_python/sampled_model_density.cpp
+++ cctbx/xray/boost_python/sampled_model_density.cpp
@@ -68,6 +68,21 @@ namespace {
   BOOST_PYTHON_FUNCTION_OVERLOADS(
     calc_u_extra_overloads, calc_u_extra, 2, 4)
 
+  void
+  eliminate_u_extra_double(
+    uctbx::unit_cell const& unit_cell,
+    double const& u_extra,
+    af::const_ref<miller::index<> > const& miller_indices,
+    af::ref<std::complex<double> > const& structure_factors,
+    double const& norm=1)
+  {
+    eliminate_u_extra(unit_cell, u_extra, miller_indices, structure_factors,
+                      norm);
+  }
+
+  BOOST_PYTHON_FUNCTION_OVERLOADS(
+    eliminate_u_extra_double_overloads, eliminate_u_extra_double, 4, 5)
+
 } // namespace <anoymous>
 
   void wrap_sampled_model_density()
@@ -75,6 +90,8 @@ namespace {
     using namespace boost::python;
 
     def("calc_u_extra", calc_u_extra, calc_u_extra_overloads());
+    def("eliminate_u_extra", eliminate_u_extra_double,
+                             eliminate_u_extra_double_overloads());
 
     sampled_model_density_wrappers::wrap();
   }
