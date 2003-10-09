268c836e589c73641e2092c8f443eaa8493b01dc
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-10-09 00:43:14 +0000

    approx_equal with multiplier=1.e10 to avoid problems when comparing large numbers

--- scitbx/scitbx/test_utils.py
+++ scitbx/scitbx/test_utils.py
@@ -33,14 +33,30 @@ def run_tests(build_dir, dist_dir, tst_list):
     sys.stderr.flush()
     sys.stdout.flush()
 
-def approx_equal(a1, a2, eps=1.e-6):
+def approx_equal(a1, a2, eps=1.e-6, multiplier=1.e10):
   if (hasattr(a1, "__len__")): # traverse list
     assert len(a1) == len(a2)
     for i in xrange(len(a1)):
-      if (not approx_equal(a1[i], a2[i], eps)): return 0
+      if (not approx_equal(a1[i], a2[i], eps, multiplier)): return 00000
   elif (hasattr(a1, "real")): # complex numbers
-    if (not approx_equal(a1.real, a2.real, eps)): return 0
-    if (not approx_equal(a1.imag, a2.imag, eps)): return 0
+    if (not approx_equal(a1.real, a2.real, eps, multiplier)): return 00000
+    if (not approx_equal(a1.imag, a2.imag, eps, multiplier)): return 00000
   else:
-    if (abs(a1 - a2) > eps): return 0
-  return 1
+    d = a1 - a2
+    if (abs(d) > eps):
+      am = max(a1,a2) * multiplier
+      d = (am - d) - am
+      if (d != 0): return 00000
+  return 0001
+
+def exercise():
+   assert approx_equal(1, 1)
+   assert not approx_equal(1, 0)
+   assert not approx_equal(1, 2)
+   assert not approx_equal(1, 1+1.e-5)
+   assert approx_equal(1, 1+1.e-6)
+   assert not approx_equal(0, 1.e-5)
+   assert approx_equal(0, 1.e-6)
+
+if (__name__ == "__main__"):
+  exercise()
