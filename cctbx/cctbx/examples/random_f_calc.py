ef953857481c5ce13c34a2978e55d0c9ba31f7fd
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-10-09 18:28:51 +0000

    generate random structure factors, transform them to a map from within PyMOL, display map in PyMOL

---
+++ cctbx/cctbx/examples/random_f_calc.py
@@ -0,0 +1,40 @@
+"""
+usage: python random_f_calc.py space_group_symbol
+
+Example:
+  python random_f_calc.py P212121
+
+This will write a file map_coeff.pickle that can be used to
+view the electron density map with PyMOL (see view_fft_map.py).
+"""
+
+from cctbx.development import random_structure
+from cctbx import sgtbx
+from scitbx.python_utils import easy_pickle
+import sys
+
+def generate_random_f_calc(space_group_info, n_elements=10, d_min=1.5):
+  structure = random_structure.xray_structure(
+    space_group_info,
+    elements=["Si"]*n_elements,
+    volume_per_atom=1000,
+    min_distance=3.,
+    general_positions_only=00000)
+  structure.show_summary().show_scatterers()
+  print
+  f_calc = structure.structure_factors(
+    d_min=d_min, anomalous_flag=00000).f_calc()
+  f_calc.show_summary()
+  print
+  print "Writing file: map_coeff.pickle"
+  easy_pickle.dump("map_coeff.pickle", f_calc)
+  print
+
+def run():
+  if (len(sys.argv) != 2):
+    print __doc__
+    return
+  generate_random_f_calc(sgtbx.space_group_info(sys.argv[1]))
+
+if (__name__ == "__main__"):
+  run()
---
+++ cctbx/cctbx/examples/view_fft_map.py
@@ -0,0 +1,78 @@
+"""
+Loads the file map_coeff.pickle (see random_f_calc.py) and displays
+the FFT map based on these coefficients in PyMOL. Also computes
+and displays a list of peaks in the map.
+
+Usage:
+
+Setup the cctbx environment (e.g. source setpaths.csh) and
+launch PyMOL from the command line. Inside PyMOL enter:
+
+run view_fft_map.py
+show_fft()
+"""
+
+print "Loading module:", __name__
+
+# cctbx imports
+from cctbx import maptbx
+from scitbx.array_family import flex
+from scitbx.python_utils import easy_pickle
+
+# PyMOL imports
+from chempy.map import Map
+from pymol import cmd
+from pymol import cgo
+
+def show_map(unit_cell, map_covering_unit_cell, label):
+  map_grid = map_covering_unit_cell.focus()
+  print "map_grid:", map_grid
+  ucell_params = unit_cell.parameters()
+  first = [0,0,0]
+  last = [map_grid[i] + 1 for i in xrange(3)]
+  c_obj_map = maptbx.as_CObjectZYX(
+    map_unit_cell=map_covering_unit_cell,
+    first=first,
+    last=last,
+    apply_sigma_scaling=True)
+  map=Map()
+  map.from_c_object(c_obj_map,'CObjectZYXfloat',
+                    ucell_params[0:3], ucell_params[3:6],
+                    list(map_grid), first, last)
+  cmd.load_map(map, label+"_cell")
+  print "map loaded into PyMol"
+  cmd.isomesh(label+"_con", label+"_cell", 3.0) # create mesh
+  cmd.color('gray', label+"_cell") # color wire frame
+  cmd.set('auto_zoom', '0')    # disable zooming
+  cmd.set('ortho', '1')        # orthoscopic projects
+  cmd.enable(label+"_cell") # put box around map object
+  cmd.color('cyan', label+"_con")   # color mesh
+
+def show_peaks(unit_cell, clusters, radius=2.0):
+  go = []
+  go.extend([cgo.COLOR, 1, 0, 0,])
+  height0 = None
+  for site,height in zip(clusters.sites(), clusters.heights()):
+    print "%8.5f %8.5f %8.5f" % site, height
+    if (height0 == None): height0 = height
+    go.extend(  [cgo.SPHERE]
+              + list(unit_cell.orthogonalize(site))
+              + [radius*height/height0])
+  cmd.load_cgo(go, "peaks")
+
+def show_fft():
+  cmd.delete("all")
+  map_coeff = easy_pickle.load("map_coeff.pickle")
+  map_coeff.show_summary()
+  fft_map = map_coeff.fft_map(
+    symmetry_flags=maptbx.use_space_group_symmetry)
+  print "map gridding:", fft_map.n_real()
+  show_map(fft_map.unit_cell(), fft_map.real_map(), "fft_map")
+  clusters = fft_map.tags().peak_search(
+    parameters=maptbx.peak_search_parameters(
+      min_distance_sym_equiv=3.0,
+      max_clusters=10),
+    map=fft_map.real_map()).all()
+  show_peaks(fft_map.unit_cell(), clusters)
+  cmd.zoom('all', 15.0) # zoom with additional border of 15 Ang.
+  print
