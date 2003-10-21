d535cea10ffe62ceaee0bb39319b5ea16b9749bc
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-10-21 08:06:56 +0000

    use Python script to set assoc, ftype to suppress output from these commands

---
+++ libtbx/libtbx/assoc_ftype.py
@@ -0,0 +1,9 @@
+import sys, os
+
+def set():
+  os.popen(r'assoc .px=PythonLibtbx', "r").read()
+  py_exe = os.environ["LIBTBX_PYTHON_EXE"]
+  os.popen(r'ftype PythonLibtbx="%s" %%1 %%*' % py_exe, "r").read()
+
+if (__name__ == "__main__"):
+  set()
--- libtbx/libtbx/command_line/configure.py
+++ libtbx/libtbx/command_line/configure.py
@@ -243,8 +243,7 @@ def emit_setpaths_bat(env):
       print >> f, 'set %s=%s' % (var_name, values)
   print >> f, 'if not defined PATHEXT set PATHEXT='
   print >> f, 'set PATHEXT=.PX;.PY;%PATHEXT%'
-  print >> f, 'assoc .px=PythonLibtbx'
-  print >> f, 'ftype PythonLibtbx="%s" %%%%1 %%%%*' % env.LIBTBX_PYTHON_EXE
+  print >> f, 'call %LIBTBX_PYTHON_EXE% %LIBTBX_DIST%\\libtbx\\assoc_ftype.py'
   f.close()
 
 class build_options_t:
