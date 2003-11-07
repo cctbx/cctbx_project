fb72d0f04b84df3921faa7cb9f277d4ded914689
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-11-07 22:35:46 +0000

    support for cctbx::xray::minimization::apply_shifts

---
+++ scitbx/include/scitbx/array_family/block_iterator.h
@@ -0,0 +1,47 @@
+#ifndef SCITBX_ARRAY_FAMILY_BLOCK_ITERATOR_H
+#define SCITBX_ARRAY_FAMILY_BLOCK_ITERATOR_H
+
+#include <scitbx/array_family/ref.h>
+#include <scitbx/error.h>
+
+namespace scitbx { namespace af {
+
+  template <typename ElementType>
+  class const_block_iterator
+  {
+    public:
+      const_block_iterator(
+        af::const_ref<ElementType> const& array,
+        std::string const& error_message)
+      :
+        array_(array),
+        error_message_(error_message),
+        i_(0)
+      {}
+
+      const ElementType*
+      operator()(std::size_t block_size)
+      {
+        if (i_ + block_size > array_.size()) {
+          throw error(error_message_);
+        }
+        const ElementType* result = &array_[i_];
+        i_ += block_size;
+        return result;
+      }
+
+      ElementType
+      operator()() { return *(*this)(1); }
+
+      bool
+      is_at_end() const { return i_ == array_.size(); }
+
+    private:
+      af::const_ref<ElementType> array_;
+      std::string error_message_;
+      std::size_t i_;
+  };
+
+}} // namespace scitbx::af
+
+#endif // SCITBX_ARRAY_FAMILY_BLOCK_ITERATOR_H
