8a08a5d07b253baa601d0b2ea0f338ab06052842
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-11-13 02:46:40 +0000

    new dblite database emulation tailored for use with SCons; use unconditionally instead of anydbm

--- libtbx/libtbx/command_line/configure.py
+++ libtbx/libtbx/command_line/configure.py
@@ -266,6 +266,7 @@ def emit_SConstruct(env, build_options, packages_dict):
   SConstruct_path = norm(join(env.LIBTBX_BUILD, "SConstruct"))
   f = open_info(SConstruct_path)
   print >> f, 'import libtbx.config'
+  print >> f, 'import libtbx.dblite'
   print >> f, 'libtbx.config.build_options.set('
   print >> f, '  compiler="%s",' % build_options.compiler
   print >> f, '  optimization=%d,' % int(build_options.mode == "release")
@@ -274,8 +275,7 @@ def emit_SConstruct(env, build_options, packages_dict):
   print >> f, '  static_exe=%d,' % int(build_options.static_exe)
   print >> f, '  scan_boost=%d)' % int(build_options.scan_boost)
   print >> f, 'if ("SConsignFile" in dir()):'
-  print >> f, \
-    '  SConsignFile(name=libtbx.config.sconsign_dbm_file_name())'
+  print >> f, '  SConsignFile(name=".sconsign", dbm_module=libtbx.dblite)'
   print >> f
   print >> f, 'Repository(r"%s")' % (env.LIBTBX_DIST_ROOT,)
   print >> f, 'SConscript("libtbx/SConscript")'
