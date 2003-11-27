6a28ca2fb847332a3acf02beab94923485f3a8b6
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-11-27 17:13:12 +0000

    quotes around %1, following the example of the standard Python ftype definitions

--- libtbx/libtbx/assoc_ftype.py
+++ libtbx/libtbx/assoc_ftype.py
@@ -3,7 +3,7 @@ import sys, os
 def set():
   os.popen(r'assoc .px=PythonLibtbx', "r").read()
   py_exe = os.environ["LIBTBX_PYTHON_EXE"]
-  os.popen(r'ftype PythonLibtbx="%s" %%1 %%*' % py_exe, "r").read()
+  os.popen(r'ftype PythonLibtbx="%s" "%%1" %%*' % py_exe, "r").read()
 
 if (__name__ == "__main__"):
   set()
