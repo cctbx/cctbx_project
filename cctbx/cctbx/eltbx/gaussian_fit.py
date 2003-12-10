4864239c6d3f6aea2d425b7a0478811a80de05c0
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-12-10 02:23:48 +0000

    iterative addition of terms

--- cctbx/cctbx/eltbx/gaussian_fit.py
+++ cctbx/cctbx/eltbx/gaussian_fit.py
@@ -2,8 +2,7 @@ from cctbx.eltbx import xray_scattering
 from cctbx.array_family import flex
 from scitbx import lbfgs
 from scitbx.python_utils import easy_pickle
-from scitbx.python_utils.misc import adopt_init_args
-import random
+from scitbx.python_utils.misc import adopt_init_args, user_plus_sys_time
 import math
 import sys, os
 
@@ -20,6 +19,7 @@ class minimize:
   def __init__(self, diff_gaussian, d_star_sq,
                      weights=None,
                      b_min=-1,
+                     enforce_positive_b=0001,
                      lbfgs_termination_params=None,
                      lbfgs_core_params=lbfgs.core_parameters(m=7)):
     adopt_init_args(self, locals())
@@ -38,8 +38,22 @@ class minimize:
     self.final_diff_gaussian = self.diff_gaussian_shifted
 
   def apply_shifts(self):
+    if (not self.enforce_positive_b):
+      shifts = self.x
+    else:
+      n_ab = self.diff_gaussian.n_ab()
+      b = flex.double(self.diff_gaussian.b())
+      assert flex.min(b) >= 0
+      sqrt_b = flex.sqrt(b)
+      shifts_sqrt_b = self.x[n_ab:]
+      sqrt_b_shifted = sqrt_b + shifts_sqrt_b
+      b_shifted = flex.pow2(sqrt_b_shifted)
+      shifts_b = b_shifted - b
+      shifts = self.x[:n_ab]
+      shifts.append(shifts_b)
+      assert min(self.diff_gaussian.b()) >= 0
     self.diff_gaussian_shifted = self.diff_gaussian.apply_shifts(
-      self.x, self.b_min)
+      shifts, self.b_min)
 
   def compute_target(self, compute_gradients):
     dg = self.diff_gaussian_shifted
@@ -48,6 +62,16 @@ class minimize:
     if (compute_gradients):
       self.g = dg.sum_of_gradients_at_points(
         self.d_star_sq, self.weights, tt, 00000)
+      if (self.enforce_positive_b):
+        n_ab = self.diff_gaussian.n_ab()
+        b = flex.double(self.diff_gaussian.b())
+        assert flex.min(b) >= 0
+        sqrt_b = flex.sqrt(b)
+        shifts_sqrt_b = self.x[n_ab:]
+        d_b_d_shift = (sqrt_b + shifts_sqrt_b) * 2
+        g_shifts_sqrt_b = self.g[n_ab:] * d_b_d_shift
+        self.g = self.g[:n_ab]
+        self.g.append(g_shifts_sqrt_b)
     else:
       self.g = None
 
@@ -84,54 +108,51 @@ class d_interval:
   def d(self, f_range):
     return 1./self.d_star(f_range)
 
-def make_start_approximation(reference_gaussian, n_terms, start_interval,
-                             f_trial_point=None,
-                             min_partitioning_factor=0.01):
-  assert n_terms <= reference_gaussian.n_ab()
-  assert f_trial_point is None or n_terms == 1
-  assert 0 < min_partitioning_factor < 1
-  partitioning = flex.double()
+def make_start_gaussian(reference_gaussian,
+                        existing_gaussian,
+                        start_interval,
+                        i_start_fraction,
+                        f_range):
+  assert reference_gaussian.n_ab() >= existing_gaussian.n_ab()
+  stol_sq = start_interval.stol_sq(f_range)
+  f0_reference = reference_gaussian.at_stol_sq(0)
+  fs_reference = reference_gaussian.at_stol_sq(stol_sq)
+  f0_existing = existing_gaussian.at_stol_sq(0)
+  fs_existing = existing_gaussian.at_stol_sq(stol_sq)
+  n_terms = existing_gaussian.n_ab() + 1
   if (n_terms == 1):
-    partitioning.append(1)
+    a = flex.double([f0_reference])
+    b = flex.double()
+    fs_part = fs_reference
   else:
-    for i_term in xrange(n_terms):
-      partitioning.append(min_partitioning_factor + random.random())
-    min_partitioning = 1. / n_terms * min_partitioning_factor
-    while 1:
-      partitioning /= flex.sum(partitioning)
-      i = flex.min_index(partitioning)
-      if (partitioning[i] >= min_partitioning):
-        break
-      partitioning[i] = min_partitioning
-    assert abs(flex.sum(partitioning)-1) < 1.e-6
-  f0_reference = reference_gaussian.at_d_star_sq(0)
-  a = partitioning * f0_reference
-  b = flex.double()
-  f_range = f_trial_point
-  for i_term in xrange(n_terms):
-    if (f_trial_point is None):
-      f_range = random.random()
-    stol_sq = start_interval.stol_sq(f_range)
-    f0_reference = reference_gaussian.at_stol_sq(stol_sq)
-    f0_part = partitioning[i_term] * f0_reference
-    b.append(-math.log(f0_part / a[i_term]) / stol_sq)
-    assert abs(a[i_term] * math.exp(-b[i_term] * stol_sq) - f0_part) < 1.e-6
+    scale_old = (1-(1.+i_start_fraction)/n_terms)
+    a = flex.double(existing_gaussian.a()) * scale_old
+    a.append(f0_reference - flex.sum(a))
+    b = flex.double(existing_gaussian.b())
+    fs_part = fs_reference - fs_existing * scale_old
+  addl_b = 0
+  if (a[-1] != 0):
+    r = fs_part / a[-1]
+    if (0 < r <= 1):
+      addl_b = -math.log(r) / stol_sq
+  b.append(addl_b)
+  if (addl_b != 0):
+    assert abs(a[-1] * math.exp(-b[-1] * stol_sq) - fs_part) < 1.e-6
   result = xray_scattering.difference_gaussian(
     reference_gaussian,
     xray_scattering.gaussian(iter(a), iter(b)))
-  assert abs(result.at_d_star_sq(0)-reference_gaussian.at_d_star_sq(0)) < 1.e-4
-  if (f_trial_point is not None):
-    stol_sq = start_interval.stol_sq(f_trial_point)
-    assert abs(  result.at_stol_sq(stol_sq)
-               - reference_gaussian.at_stol_sq(stol_sq)) < 1.e-4
+  if (addl_b != 0):
+    assert abs(result.at_stol_sq(0) - f0_reference) < 1.e-4
+  if (n_terms == 1):
+    assert abs(result.at_stol_sq(stol_sq) - fs_reference) < 1.e-4
   return result
 
 class find_d_min:
 
-  def __init__(self, diff_gaussian, max_target_value,
+  def __init__(self, diff_gaussian,
                      n_points,
+                     max_target_value,
                      start_interval,
-                     dead_end_d_min,
                      min_interval):
     adopt_init_args(self, locals())
     interval = [start_interval.d_max, start_interval.d_min-min_interval]
@@ -148,10 +169,6 @@ class find_d_min:
         interval[0] = self.d_min
         good_min = self.min
       else:
-        if (dead_end_d_min is not None and self.d_min > dead_end_d_min):
-          self.d_min = None
-          self.min = None
-          break
         if (interval[0] - interval[1] < min_interval):
           if (good_min is not None):
             self.d_min = interval[0]
@@ -162,40 +179,45 @@ class find_d_min:
           break
         interval[1] = self.d_min
 
-class find_one_term_d_min:
+class find_d_min_multi:
 
   def __init__(self, reference_gaussian,
+                     existing_gaussian,
                      start_interval,
-                     n_trial_points,
+                     n_trial_points_factor,
+                     n_start_fractions,
                      n_points,
                      max_target_value,
                      min_interval):
+    n_terms = existing_gaussian.n_ab() + 1
+    n_trial_points = n_terms * n_trial_points_factor
     best_fit = None
     for i_trial_point in xrange(n_trial_points+1):
       f_range = i_trial_point / float(n_trial_points)
       d_min = start_interval.d(f_range)
       d_star_sq = d_star_sq_points(d_min=d_min, n_points=n_points)
-      diff_gaussian = make_start_approximation(
-        reference_gaussian=reference_gaussian,
-        n_terms=1,
-        start_interval=start_interval,
-        f_trial_point=f_range/2)
-      fit = find_d_min(
-        diff_gaussian=diff_gaussian,
-        max_target_value=max_target_value,
-        start_interval=start_interval,
-        n_points=n_points,
-          dead_end_d_min=None,
+      for i_start_fraction in xrange(min(n_start_fractions,n_terms)):
+        diff_gaussian = make_start_gaussian(
+          reference_gaussian=reference_gaussian,
+          existing_gaussian=existing_gaussian,
+          start_interval=start_interval,
+          i_start_fraction=i_start_fraction,
+          f_range=f_range/2)
+        fit = find_d_min(
+          diff_gaussian=diff_gaussian,
+          n_points=n_points,
+          max_target_value=max_target_value,
+          start_interval=start_interval,
           min_interval=min_interval)
-      if (fit.d_min is not None):
-        if (best_fit is None or best_fit.d_min > fit.d_min):
-          best_fit = fit
+        if (fit.d_min is not None):
+          if (best_fit is None or best_fit.d_min > fit.d_min):
+            best_fit = fit
     if (best_fit is None):
       self.d_min = None
       self.min = None
     else:
-      self.d_min = fit.d_min
-      self.min = fit.min
+      self.d_min = best_fit.d_min
+      self.min = best_fit.min
 
 def show_fit_summary(source, label, gaussian, d_min, q, q_other=None):
   n_terms = str(gaussian.n_ab())
@@ -248,14 +270,14 @@ def write_plots(plots_dir, label, reference_gaussian, gaussian,
 class fit_parameters:
 
   def __init__(self, n_points=50,
-                     max_relative_target_value=1.e-4,
-                     find_d_min_min_interval=0.01,
-                     n_similar_fits=10,
-                     max_d_min_spread_similar_fits=0.02,
-                     max_calls_find_d_min_per_term=1000):
+                     max_target_value_factor=1.e-4,
+                     n_trial_points_factor=3,
+                     n_start_fractions=2,
+                     find_d_min_min_interval=0.01):
     adopt_init_args(self, locals())
 
 def run(args=[], params=fit_parameters(), verbose=0):
+  timer = user_plus_sys_time()
   chunk_n = 1
   chunk_i = 0
   if (len(args) > 0 and len(args[0].split(",")) == 2):
@@ -269,6 +291,8 @@ def run(args=[], params=fit_parameters(), verbose=0):
     print "***************************************************************"
     print
     plots_dir = None
+  if (chunk_n > 1):
+    assert plots_dir is not None
   results = {}
   results["fit_parameters"] = params
   start_interval = d_interval(d_max=15, d_min=1/12.)
@@ -281,96 +305,56 @@ def run(args=[], params=fit_parameters(), verbose=0):
     if (len(args) > 0 and g5c.label() not in args): continue
     reference_gaussian = g5c.fetch()
     f0 = reference_gaussian.at_d_star_sq(0)
-    one_term_fit = find_one_term_d_min(
-      reference_gaussian=reference_gaussian,
-      start_interval=start_interval,
-      n_trial_points=3,
-      n_points=params.n_points,
-      max_target_value=params.max_relative_target_value*f0,
-      min_interval=params.find_d_min_min_interval)
-    assert one_term_fit.d_min is not None
-    show_fit_summary(
-      "Best fit", g5c.label(),
-      one_term_fit.min.final_diff_gaussian,
-      one_term_fit.d_min,
-      one_term_fit.min.final_target_value)
-    sys.stdout.flush()
+    max_target_value = params.max_target_value_factor * f0
     results[g5c.label()] = []
     previous_d_min = None
-    for n_terms in [2,3,4,5][:1]:
-      fits = []
-      fits_d_min = flex.double()
-      fits_min_d_min = 15
-      n_calls_find_d_min = 0
-      while 1:
-        diff_gaussian = make_start_approximation(
-          reference_gaussian=reference_gaussian,
-          n_terms=n_terms,
-          start_interval=start_interval)
-        n_calls_find_d_min += 1
-        fit = find_d_min(
-          diff_gaussian=diff_gaussian,
-          max_target_value=params.max_relative_target_value*f0,
-          start_interval=start_interval,
-          n_points=params.n_points,
-          dead_end_d_min=fits_min_d_min+params.max_d_min_spread_similar_fits,
-          min_interval=params.find_d_min_min_interval)
-        if (fit.d_min is not None):
-          fits.append(fit)
-          fits_d_min.append(fit.d_min)
-          fits_min_d_min = min(fits_min_d_min, fit.d_min)
-          if (fits_d_min.size() >= params.n_similar_fits):
-            perm = flex.sort_permutation(fits_d_min)
-            best_d_min = fits_d_min.select(perm[:params.n_similar_fits])
-            if (  best_d_min[-1] - best_d_min[0]
-                < params.max_d_min_spread_similar_fits):
-              fit = fits[perm[0]]
-              break
-        if (n_calls_find_d_min==params.max_calls_find_d_min_per_term*n_terms):
-          if (fits_d_min.size() == 0):
-            fit = None
-          else:
-            perm = flex.sort_permutation(fits_d_min)
-            best_d_min = fits_d_min.select(perm[:params.n_similar_fits])
-            print "best_d_min:", ", ".join(["%.2f" % d for d in best_d_min])
-            fit = fits[perm[0]]
-          break
-      print "n_calls_find_d_min:", n_calls_find_d_min
-      print "successful fits:", fits_d_min.size()
-      if (0 or verbose):
-        print "fits_d_min:", ", ".join(["%.2f" % d for d in fits_d_min])
-      if (fit is None):
-        print "No fit: %s n_terms=%d" % (g5c.label(), n_terms)
+    existing_gaussian = xray_scattering.gaussian([],[])
+    while (existing_gaussian.n_ab() < 5):
+      n_terms = existing_gaussian.n_ab() + 1
+      fit = find_d_min_multi(
+        reference_gaussian=reference_gaussian,
+        existing_gaussian=existing_gaussian,
+        start_interval=start_interval,
+        n_trial_points_factor=params.n_trial_points_factor,
+        n_start_fractions=params.n_start_fractions,
+        n_points=params.n_points,
+        max_target_value=max_target_value,
+        min_interval=params.find_d_min_min_interval)
+      if (fit.d_min is None):
+        print "Warning: No fit: %s n_terms=%d" % (g5c.label(), n_terms)
         print
-      else:
-        if (previous_d_min is not None and previous_d_min > fit.d_min):
-          print "Warning: previous d_min was larger."
-        previous_d_min = fit.d_min
-        show_fit_summary(
-          "Best fit", g5c.label(), fit.min.final_diff_gaussian, fit.d_min,
-          fit.min.final_target_value)
-        show_literature_fits(
+        break
+      if (previous_d_min is not None and previous_d_min < fit.d_min):
+        print "Warning: previous d_min was smaller."
+      previous_d_min = fit.d_min
+      show_fit_summary(
+        "Best fit", g5c.label(), fit.min.final_diff_gaussian, fit.d_min,
+        fit.min.final_target_value)
+      show_literature_fits(
+        label=g5c.label(),
+        reference_gaussian=reference_gaussian,
+        n_terms=n_terms,
+        d_min=fit.d_min,
+        d_star_sq=fit.min.d_star_sq,
+        q_other=fit.min.final_target_value)
+      fit.min.final_diff_gaussian.show()
+      existing_gaussian = fit.min.final_diff_gaussian
+      print
+      sys.stdout.flush()
+      if (plots_dir):
+        write_plots(
+          plots_dir=plots_dir,
           label=g5c.label(),
           reference_gaussian=reference_gaussian,
-          n_terms=n_terms,
+          gaussian=fit.min.final_diff_gaussian,
           d_min=fit.d_min,
-          d_star_sq=fit.min.d_star_sq,
-          q_other=fit.min.final_target_value)
-        fit.min.final_diff_gaussian.show()
-        print
-        if (plots_dir):
-          write_plots(
-            plots_dir=plots_dir,
-            label=g5c.label(),
-            reference_gaussian=reference_gaussian,
-            gaussian=fit.min.final_diff_gaussian,
-            d_min=fit.d_min,
-            n_points=100)
-        g = fit.min.final_diff_gaussian
-        results[g5c.label()].append(xray_scattering.fitted_gaussian(
-          d_min=fit.d_min, a=g.a(),b=g.b()))
-      sys.stdout.flush()
-      easy_pickle.dump("fits_%02d.pickle" % chunk_i, results)
+          n_points=100)
+      g = fit.min.final_diff_gaussian
+      results[g5c.label()].append(xray_scattering.fitted_gaussian(
+        d_min=fit.d_min, a=g.a(),b=g.b()))
+    sys.stdout.flush()
+    easy_pickle.dump("fits_%02d.pickle" % chunk_i, results)
+  print "CPU time: %.2f seconds" % timer.elapsed()
 
 if (__name__ == "__main__"):
   from cctbx.eltbx.gaussian_fit import run
