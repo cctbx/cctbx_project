17e9f1213886d01e6602492e6232e2d1fc504efd
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-12-11 04:39:10 +0000

    enable from stdlib import math to avoid import conflicts

---
+++ libtbx/stdlib.py
@@ -0,0 +1,24 @@
+import imp
+import sys
+
+def import_module(name):
+  try:
+    return sys.modules[name]
+  except KeyError:
+    pass
+  sys_path = sys.path[:]
+  sys_path.reverse()
+  for path in sys_path:
+    try:
+      fp, pathname, description = imp.find_module(name, [path])
+    except ImportError:
+      pass
+    else:
+      try:
+        return imp.load_module(name, fp, pathname, description)
+      finally:
+        if (fp is not None): fp.close()
+  raise RuntimeError("Cannot import %s module." % name)
+
+math = import_module("math")
+random = import_module("random")
