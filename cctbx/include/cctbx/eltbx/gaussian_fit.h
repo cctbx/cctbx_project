4cdda7e8c642ad76007b58cd201704b59db74565
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-12-14 17:36:23 +0000

    difference_gaussian removed; new gaussian_fit::target_function

--- cctbx/include/cctbx/eltbx/gaussian_fit.h
+++ cctbx/include/cctbx/eltbx/gaussian_fit.h
@@ -6,109 +6,6 @@
 
 namespace cctbx { namespace eltbx { namespace xray_scattering {
 
-  class difference_gaussian : public gaussian
-  {
-    public:
-      difference_gaussian() {}
-
-      difference_gaussian(
-        gaussian const& reference_gaussian,
-        gaussian const& target_gaussian)
-      :
-        gaussian(target_gaussian),
-        reference_gaussian_(reference_gaussian)
-      {
-        CCTBX_ASSERT(reference_gaussian.n_ab() >= target_gaussian.n_ab());
-      }
-
-      gaussian const&
-      reference_gaussian() const { return reference_gaussian_; }
-
-      difference_gaussian
-      apply_shifts(
-        af::const_ref<double> const& shifts,
-        double b_min)
-      {
-        CCTBX_ASSERT(   shifts.size() == n_ab() * 2
-                     || shifts.size() == n_ab() * 2 + 1);
-        difference_gaussian result;
-        std::size_t i=0;
-        for(;i<n_ab();i++) {
-          result.a_.push_back(a(i) + shifts[i]);
-        }
-        for(std::size_t j=0;j<n_ab();j++,i++) {
-          result.b_.push_back(std::max(b_min, b(j) + shifts[i]));
-        }
-        if (i == shifts.size()) result.c_ = 0;
-        else                    result.c_ = shifts[i];
-        result.reference_gaussian_ = reference_gaussian_;
-        return result;
-      }
-
-      double
-      target_term_at_d_star_sq(double d_star_sq) const
-      {
-        return at_d_star_sq(d_star_sq)
-             - reference_gaussian_.at_d_star_sq(d_star_sq);
-      }
-
-      double
-      target_at_d_star_sq(int power, double d_star_sq, double weight) const
-      {
-        double ptt = scitbx::fn::pow2(target_term_at_d_star_sq(d_star_sq));
-        if (power == 2) return weight * ptt;
-        if (power == 4) return weight * ptt * ptt;
-        throw error("power must be 2 or 4.");
-      }
-
-      af::shared<double>
-      target_terms_at_points(
-        af::const_ref<double> const& d_star_sq)
-      {
-        af::shared<double> result((af::reserve(d_star_sq.size())));
-        for(std::size_t i_point=0;i_point<d_star_sq.size();i_point++) {
-          result.push_back(target_term_at_d_star_sq(d_star_sq[i_point]));
-        }
-        return result;
-      }
-
-      af::shared<double>
-      sum_of_gradients_at_points(
-        int power,
-        af::const_ref<double> const& d_star_sq,
-        af::const_ref<double> const& weights,
-        af::const_ref<double> const& target_terms,
-        bool include_constant_term)
-      {
-        CCTBX_ASSERT(power == 2 || power == 4);
-        CCTBX_ASSERT(weights.size() == d_star_sq.size());
-        CCTBX_ASSERT(target_terms.size() == d_star_sq.size());
-        af::shared<double> result(n_ab() * 2 + 1, 0);
-        if (!include_constant_term) result.pop_back();
-        af::ref<double> g = result.ref();
-        for(std::size_t i_point=0;i_point<d_star_sq.size();i_point++) {
-          gaussian grg = gradients_at_d_star_sq(d_star_sq[i_point]);
-          double tt = target_terms[i_point];
-          double nwptt = 2 * weights[i_point] * tt;
-          if (power == 4) nwptt *= 2 * tt * tt;
-          std::size_t i=0;
-          for(;i<n_ab();i++) {
-            g[i] += nwptt * grg.a(i);
-          }
-          for(std::size_t j=0;j<n_ab();j++,i++) {
-            g[i] += nwptt * grg.b(j);
-          }
-          if (include_constant_term) {
-            g[i] += nwptt * grg.c();
-          }
-        }
-        return result;
-      }
-
-    protected:
-      gaussian reference_gaussian_;
-  };
-
   class gaussian_fit : public gaussian
   {
     public:
@@ -218,15 +115,36 @@ namespace cctbx { namespace eltbx { namespace xray_scattering {
           stols_, target_values_, sigmas_, gaussian(sh_a,sh_b,sh_c));
       }
 
+      double
+      target_function(
+        int power,
+        af::const_ref<double> const& differences)
+      {
+        CCTBX_ASSERT(differences.size() == stols_.size());
+        CCTBX_ASSERT(power == 2 || power == 4);
+        size_assert_intrinsic();
+        af::const_ref<double> sigmas = sigmas_.const_ref();
+        double result = 0;
+        for(std::size_t i=0;i<differences.size();i++) {
+          double diff_squared = differences[i] * differences[i];
+          double sigma_squared = sigmas[i] * sigmas[i];
+          CCTBX_ASSERT(sigma_squared > 0);
+          double term = diff_squared / sigma_squared;
+          if (power == 4) term *= diff_squared;
+          result += term;
+        }
+        return result;
+      }
+
       af::shared<double>
-      sum_of_gradients(
+      gradients(
         int power,
         af::const_ref<double> const& differences,
         bool include_constant_term) const
       {
         CCTBX_ASSERT(differences.size() == stols_.size());
-        size_assert_intrinsic();
         CCTBX_ASSERT(power == 2 || power == 4);
+        size_assert_intrinsic();
         af::shared<double> result(n_ab() * 2 + 1, 0);
         if (!include_constant_term) result.pop_back();
         af::const_ref<double> stols = stols_.const_ref();
@@ -234,11 +152,11 @@ namespace cctbx { namespace eltbx { namespace xray_scattering {
         af::ref<double> g = result.ref();
         for(std::size_t i_point=0;i_point<stols.size();i_point++) {
           gaussian grg = gradients_at_stol(stols[i_point]);
-          double d = differences[i_point];
+          double diff = differences[i_point];
           double sigma_squared = sigmas[i_point] * sigmas[i_point];
           CCTBX_ASSERT(sigma_squared > 0);
-          double nwpd = 2 * d / sigma_squared;
-          if (power == 4) nwpd *= 2 * d * d;
+          double nwpd = 2 * diff / sigma_squared;
+          if (power == 4) nwpd *= 2 * diff * diff;
           std::size_t i=0;
           for(;i<n_ab();i++) {
             g[i] += nwpd * grg.a(i);
