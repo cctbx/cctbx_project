db4e1fe6969a829765e9157b5e8b88f51db7aa02
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2003-12-16 00:54:50 +0000

    first step of moving basic tools for handling Gaussians from cctbx::eltbx to scitbx

---
+++ scitbx/include/scitbx/math/gaussian/namespace.h
@@ -0,0 +1,7 @@
+// Documentation only. Do not include.
+namespace scitbx { namespace math {
+
+//! Tools for the handling of Gaussians: a Exp[-b x^2].
+namespace gaussian {}
+
+}} // namespace scitbx::math
