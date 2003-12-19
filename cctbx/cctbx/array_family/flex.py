597834cff6a6c5b07b4a3da0794a7fd04bcc225a
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2003-12-19 20:40:57 +0000

    make sure ext is a reference to cctbx_array_family_flex_ext

--- cctbx/cctbx/array_family/flex.py
+++ cctbx/cctbx/array_family/flex.py
@@ -1,8 +1,9 @@
 import scitbx.array_family.flex
 
 import boost.python
-ext = boost.python.import_ext("cctbx_array_family_flex_ext")
+boost.python.import_ext("cctbx_array_family_flex_ext")
 from scitbx_array_family_flex_ext import *
 from cctbx_array_family_flex_ext import *
+import cctbx_array_family_flex_ext as ext
 
 scitbx.array_family.flex.export_to("cctbx.array_family.flex")
