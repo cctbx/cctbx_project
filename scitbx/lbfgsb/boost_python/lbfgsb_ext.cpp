41e848c8b9e69d876f2c96f459a4258bb5c3c1d1
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-01-07 10:39:20 +0000

    initial version of L-BFGS-B Python interface

--- scitbx/lbfgsb/boost_python/lbfgsb_ext.cpp
+++ scitbx/lbfgsb/boost_python/lbfgsb_ext.cpp
@@ -1,18 +1,47 @@
 #include <scitbx/array_family/boost_python/flex_fwd.h>
 
-#include <scitbx/lbfgsb/raw.h>
+#include <scitbx/lbfgsb.h>
 #include <boost/python/module.hpp>
+#include <boost/python/class.hpp>
 
 namespace scitbx { namespace lbfgsb { namespace {
+
+  template <typename FloatType>
+  struct minimizer_wrappers
+  {
+    typedef minimizer<FloatType> w_t;
+
+    static void
+    wrap()
+    {
+      using namespace boost::python;
+      class_<w_t>("minimizer", no_init)
+        .def(init<int const&,
+                  int const&,
+                  af::shared<FloatType>,
+                  af::shared<FloatType>,
+                  af::shared<int>,
+                  FloatType const&,
+                  FloatType const&,
+                  int const&>())
+        .def("task", &w_t::task)
+        .def("request_stop", &w_t::request_stop)
+        .def("request_stop_with_restore", &w_t::request_stop_with_restore)
+        .def("process", &w_t::process)
+      ;
+    }
+  };
 
   void init_module()
   {
     using namespace boost::python;
+
+    minimizer_wrappers<double>::wrap();
   }
 
 }}} // namespace scitbx::lbfgs::<anonymous>
 
-BOOST_PYTHON_MODULE(scitbx_lbfgs_ext)
+BOOST_PYTHON_MODULE(scitbx_lbfgsb_ext)
 {
   scitbx::lbfgsb::init_module();
 }
