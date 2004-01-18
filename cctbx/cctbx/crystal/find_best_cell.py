b26e636a489986d172fe566ee3588dfe0f6c075f
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-01-18 17:00:43 +0000

    brute-force search for best monoclinic cell (very slow)

--- cctbx/cctbx/crystal/find_best_cell.py
+++ cctbx/cctbx/crystal/find_best_cell.py
@@ -1,4 +1,6 @@
 from cctbx import sgtbx
+from cctbx.array_family import flex
+from cctbx import matrix
 
 def cmp_orthorhombic_cell_parameters(lhs, rhs):
   for i in xrange(3):
@@ -47,40 +49,46 @@ class find_best_cell:
     cb_op_std_ref = standard_info.type().cb_op()
     cb_op_std_inp = cb_op_inp_ref.inverse() * cb_op_std_ref
     assert standard_info.group().change_basis(cb_op_std_inp) == input_symmetry.space_group()
+    best_cell_parameters = input_symmetry.unit_cell().parameters()
+    best_cb_op = sgtbx.change_of_basis_op()
+    best_symmetry = input_symmetry
     if (space_group_number <= 15):
-      affine_hall_symbols = ("P 4 2 (y,z,x)", "P 6 2 (y,z,x)")
       two_fold_info = sgtbx.rot_mx_info(input_symmetry.space_group()(1).r())
       assert abs(two_fold_info.type()) == 2
       ev = list(two_fold_info.ev())
       assert ev.count(0) == 2
       unique_axis = ev.index(1) + 3
-    else:
-      assert not str(standard_info).endswith(":2")
-      affine_hall_symbols = ("P 4 3*",)
-      unique_axis = None
-    best_cell_parameters = input_symmetry.unit_cell().parameters()
-    best_cb_op = sgtbx.change_of_basis_op()
-    best_symmetry = input_symmetry
-    for affine_hall_symbol in affine_hall_symbols:
-      affine_group = sgtbx.space_group(affine_hall_symbol).change_basis(
-        cb_op_std_inp)
-      for affine_s in affine_group:
-        affine_cb_op = sgtbx.change_of_basis_op(affine_s) \
-          .new_denominators(best_cb_op)
-        alt_symmetry = input_symmetry.change_basis(affine_cb_op)
-        self._all_cells.append(alt_symmetry)
+      for elems in flex.nested_loop([-2]*9,[2]*9,00000):
+        m = matrix.rec(elems, (3,3))
+        if (m.determinant() != 1): continue
+        cb_op = sgtbx.change_of_basis_op(sgtbx.rt_mx(sgtbx.rot_mx(elems)))
+        alt_symmetry = input_symmetry.change_basis(cb_op)
         if (alt_symmetry.space_group() == input_symmetry.space_group()):
+          self._all_cells.append(alt_symmetry)
           alt_cell_parameters = alt_symmetry.unit_cell().parameters()
-          if (unique_axis is not None):
-            cmp_result = cmp_monoclinic_cell_parameters(
-              best_cell_parameters, alt_cell_parameters, unique_axis,
-              angular_tolerance)
-          else:
-            cmp_result = cmp_orthorhombic_cell_parameters(
-              best_cell_parameters, alt_cell_parameters)
+          cmp_result = cmp_monoclinic_cell_parameters(
+            best_cell_parameters, alt_cell_parameters, unique_axis,
+            angular_tolerance)
           if (cmp_result > 0):
             best_cell_parameters = alt_cell_parameters
-            best_cb_op = affine_cb_op
+            best_cb_op = cb_op
+            best_symmetry = alt_symmetry
+    else:
+      assert not str(standard_info).endswith(":2")
+      affine_group = sgtbx.space_group("P 4 3*").change_basis(
+        cb_op_std_inp)
+      for affine_s in affine_group:
+        cb_op = sgtbx.change_of_basis_op(affine_s) \
+          .new_denominators(best_cb_op)
+        alt_symmetry = input_symmetry.change_basis(cb_op)
+        if (alt_symmetry.space_group() == input_symmetry.space_group()):
+          self._all_cells.append(alt_symmetry)
+          alt_cell_parameters = alt_symmetry.unit_cell().parameters()
+          cmp_result = cmp_orthorhombic_cell_parameters(
+            best_cell_parameters, alt_cell_parameters)
+          if (cmp_result > 0):
+            best_cell_parameters = alt_cell_parameters
+            best_cb_op = cb_op
             best_symmetry = alt_symmetry
     self._cb_op = best_cb_op
     self._symmetry = best_symmetry
