d28432a509e66623f554fa450e638a876e5df6ab
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-02-01 07:32:39 +0000

    boost::python::arg(...) for all arguments

--- cctbx/sgtbx/boost_python/sym_equiv_sites.cpp
+++ cctbx/sgtbx/boost_python/sym_equiv_sites.cpp
@@ -1,16 +1,8 @@
-/* Copyright (c) 2001-2002 The Regents of the University of California
-   through E.O. Lawrence Berkeley National Laboratory, subject to
-   approval by the U.S. Department of Energy.
-   See files COPYRIGHT.txt and LICENSE.txt for further details.
-
-   Revision history:
-     2002 Sep: Created (rwgk)
- */
-
 #include <cctbx/boost_python/flex_fwd.h>
 
 #include <cctbx/sgtbx/sym_equiv_sites.h>
 #include <boost/python/class.hpp>
+#include <boost/python/args.hpp>
 #include <boost/python/return_value_policy.hpp>
 #include <boost/python/copy_const_reference.hpp>
 #include <boost/python/return_internal_reference.hpp>
@@ -27,29 +19,36 @@ namespace {
     wrap()
     {
       using namespace boost::python;
+      typedef boost::python::arg arg_; // gcc 2.96 workaround
       typedef return_value_policy<copy_const_reference> ccr;
       typedef return_internal_reference<> rir;
       class_<w_t>("sym_equiv_sites", no_init)
         .def(init<site_symmetry const&>())
-        .def(init<wyckoff::mapping const&>())
+        .def(init<wyckoff::mapping const&>(
+          (arg_("wyckoff_mapping"))))
         .def(init<sgtbx::space_group const&,
                   fractional<> const&,
-                  optional<uctbx::unit_cell const&> >())
+                  optional<uctbx::unit_cell const&> >(
+          (arg_("space_group"), arg_("original_site"), arg_("unit_cell"))))
         .def(init<uctbx::unit_cell const&,
                   sgtbx::space_group const&,
                   fractional<> const&,
-                  rt_mx const&>())
+                  rt_mx const&>(
+          (arg_("unit_cell"), arg_("space_group"), arg_("original_site"),
+           arg_("special_op"))))
         .def(init<uctbx::unit_cell const&,
                   sgtbx::space_group const&,
                   fractional<> const&,
-                  optional<double, double> >())
+                  optional<double, double> >(
+          (arg_("unit_cell"), arg_("space_group"), arg_("original_site"),
+           arg_("minimum_distance"), arg_("tolerance"))))
         .def("unit_cell", &w_t::unit_cell, rir())
         .def("space_group", &w_t::space_group, rir())
         .def("original_site", &w_t::original_site, ccr())
         .def("special_op", &w_t::special_op, ccr())
         .def("max_accepted_tolerance", &w_t::max_accepted_tolerance)
         .def("coordinates", &w_t::coordinates)
-        .def("sym_op", &w_t::sym_op)
+        .def("sym_op", &w_t::sym_op, (arg_("i_coor")))
         .def("sym_op_indices", &w_t::sym_op_indices)
       ;
     }
