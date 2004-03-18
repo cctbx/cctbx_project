6bd8ded0acf90af72329614010272f05ba25339d
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-03-18 23:02:49 +0000

    step

--- libtbx/libtbx/itertbx.py
+++ libtbx/libtbx/itertbx.py
@@ -13,3 +13,17 @@ except:
 
     def __iter__(self):
       return self
+
+class step:
+
+  def __init__(self, firstval=0, increment=1):
+    self.val = firstval
+    self.increment = increment
+
+  def next(self):
+    result = self.val
+    self.val += self.increment
+    return result
+
+  def __iter__(self):
+    return self
