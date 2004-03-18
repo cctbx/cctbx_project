fd72e6f74f13bebbd11e8637201b527b95747038
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-03-18 18:09:12 +0000

    use scitbx.math.euler_angles_as_matrix

--- cctbx/cctbx/adptbx.py
+++ cctbx/cctbx/adptbx.py
@@ -4,10 +4,10 @@ import boost.python
 ext = boost.python.import_ext("cctbx_adptbx_ext")
 from cctbx_adptbx_ext import *
 
-from cctbx.macro_mol import rotation_parameters
+from scitbx.math import euler_angles_as_matrix
 import random
 
 def random_rotate_ellipsoid(u_cart):
-  c = rotation_parameters.amore_alpha_beta_gamma_as_matrix(
+  c = euler_angles_as_matrix(
     [random.uniform(0,360) for i in xrange(3)]).elems
   return c_u_c_transpose(c, u_cart)
