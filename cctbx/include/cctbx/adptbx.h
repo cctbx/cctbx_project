9a379517720aa94aee1999c3eb0bc3a33c7fbf4a
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-03-19 19:13:30 +0000

    analytic computation of eigenvalues removed (wrong results for, e.g., (2.2918384340782829, 4.8055428477452207, 4.9026187181765, -0.28158497989059578, -3.1870283723379682, 1.8901963311753427)); adptbx::eigensystem is now a thin wrapper for scitbx::math::eigensystem::real_symmetric because the latter is much faster than the old adptbx::eigensystem implementation

--- cctbx/include/cctbx/adptbx.h
+++ cctbx/include/cctbx/adptbx.h
@@ -558,121 +558,60 @@ namespace cctbx {
     return result;
   }
 
-  namespace detail {
+  //! Group of associated eigenvectors and eigenvalues.
+  template <typename FloatType>
+  class eigensystem
+  {
+    public:
+      //! Default constructor. Some data members are not initialized!
+      eigensystem() {}
 
-    template <class T>
-    inline
-    std::complex<T>
-    root(std::complex<T> const& c, unsigned int m, unsigned int k=0)
-    {
-      T rho = std::abs(c);
-      if (rho == T(0)) return std::complex<T>(0, 0);
-      return std::polar(
-        std::pow(rho, T(1./m)),
-        (std::arg(c) + T(k * scitbx::constants::two_pi)) / T(m));
-    }
+      /*! \brief Determines the eigenvectors and eigenvalues of the
+          anisotropic displacement tensor.
+       */
+      /*! Since the anisotropic displacement tensor is a symmetric matrix,
+          all eigenvalues are real and the eigenvectors can be chosen
+          orthonormal.
 
-    // To ensure numerical stability we have to try all 3 roots
-    // of one of the complex numbers. The goal is to find the root
-    // which minimizes the sum of the imaginary parts of u and v.
-    template <typename FloatType>
-    struct third_roots
-    {
-      third_roots(
-        std::complex<FloatType> const& c1,
-        std::complex<FloatType> const& c2)
+          This class is implemented as a thin wrapper around:
+            scitbx::math::eigensystem::real_symmetric<>
+       */
+      eigensystem(sym_mat3<FloatType> const& adp)
       {
-        u = detail::root(c1, 3);
-        v = detail::root(c2, 3);
-        FloatType diff = scitbx::fn::absolute(u.imag() + v.imag());
-        for(unsigned int k=1;k<3;k++) {
-          std::complex<FloatType> trial_v = detail::root(c2, 3, k);
-          FloatType trial_diff=scitbx::fn::absolute(u.imag() + trial_v.imag());
-          if (diff > trial_diff) {
-            diff = trial_diff;
-            v = trial_v;
-          }
+        scitbx::math::eigensystem::real_symmetric<FloatType> es(adp);
+        for(std::size_t i=0;i<3;i++) {
+          vectors_[i] = vec3<FloatType>(&es.vectors()[i*3]);
         }
+        values_ = vec3<FloatType>(es.values().begin());
       }
 
-      std::complex<FloatType> u;
-      std::complex<FloatType> v;
-    };
+      //! The i'th eigenvector.
+      /*! An exception is thrown if i >= 3.
+       */
+      vec3<FloatType> const&
+      vectors(std::size_t i) const
+      {
+        if (i >= vectors_.size()) throw error_index();
+        return vectors_[i];
+      }
 
-  } // namespace detail
+      //! The eigenvalues.
+      vec3<FloatType> const&
+      values() const { return values_; }
+
+    private:
+      af::tiny<vec3<FloatType>, 3> vectors_;
+      vec3<FloatType> values_;
+  };
 
   //! Determines the eigenvalues of the anisotropic displacement tensor.
-  /*! Since the anisotropic displacement tensor is a symmetric matrix,
-      all eigenvalues are real. The eigenvalues lambda are determined
-      as the three real roots of the cubic equation
-      <p>
-          |(adp - lambda * I)| = 0
-      <p>
-      where I is the identity matrix. The solutions are
-      obtained analytically using Cardan's formula.
-      Detailed comments are embedded in the source code.
-      <p>
-      See also: eigensystem().
+  /*! Equivalent to eigensystem<>().values().
    */
   template <typename FloatType>
   vec3<FloatType>
   eigenvalues(sym_mat3<FloatType> const& adp)
   {
-    /* The eigenvalues lambda are found by:
-         1. Determining the elements of the matrix (adp - lambda * I),
-            where I is the identity matrix.
-         2. Computing the determinant of that matrix as a function
-            of lambda. This results in a cubic equation.
-         3. Finding the real roots of the cubic equation with
-            Cardan's formula, taken from Taschenbuch der Mathematik
-            by Bronstein & Semendjajew (p. 183 in the reprint of
-            the 20th edition from 1983).
-    */
-    /* Mathematica code used to generate the coefficients of the
-       "normal form" of the cubic equation:
-         U={{adp0,adp3,adp4},{adp3,adp1,adp5},{adp4,adp5,adp2}}
-         L={{lambda,0,0},{0,lambda,0},{0,0,lambda}}
-         Det[U-L]*(-1)
-         CForm[Det[U-L]*(-1)]
-     */
-    // normal form: x^3 + r x^2 + s x + t == 0
-    FloatType r = -adp[0] - adp[1] - adp[2];
-    FloatType s =   adp[0] * adp[1] + adp[0] * adp[2] + adp[1] * adp[2]
-                  - adp[3] * adp[3] - adp[4] * adp[4] - adp[5] * adp[5];
-    FloatType t =   adp[0] * adp[5] * adp[5] - adp[0] * adp[1] * adp[2]
-                  + adp[2] * adp[3] * adp[3] + adp[1] * adp[4] * adp[4]
-                  - FloatType(2) * adp[3] * adp[4] * adp[5];
-    /* "Reduced form" of the cubic equation according to
-       Taschenbuch der Mathematik.
-     */
-    // reduced form: y^3 + p y + q == 0
-    FloatType p = s - r * r / FloatType(3);
-    FloatType q =   FloatType(2) * r * r * r / FloatType(27)
-                  - r * s / FloatType(3) + t;
-    // to circumvent instabilities due to rounding errors the
-    // roots are determined as complex numbers.
-    FloatType d(p*p*p / FloatType(27) + q*q / FloatType(4));
-    std::complex<FloatType> sqrt_d;
-    if (d < FloatType(0)) {
-      sqrt_d = std::complex<FloatType>(0, std::sqrt(-d));
-    }
-    else {
-      sqrt_d = std::complex<FloatType>(std::sqrt(d), 0);
-    }
-    FloatType mq2 = -q / FloatType(2);
-    detail::third_roots<FloatType> roots(mq2 + sqrt_d, mq2 - sqrt_d);
-    std::complex<FloatType> epsilon1(-0.5, std::sqrt(3.) * 0.5);
-    std::complex<FloatType> epsilon2 = std::conj(epsilon1);
-    // since the anisotropic displacement tensor is a symmetric matrix,
-    // all the imaginary components of the roots must be zero.
-    vec3<FloatType> result;
-    result[0] = (roots.u + roots.v).real();
-    result[1] = (epsilon1 * roots.u + epsilon2 * roots.v).real();
-    result[2] = (epsilon2 * roots.u + epsilon1 * roots.v).real();
-    // convert the solutions y of the reduced form to the
-    // solutions x of the normal form.
-    for(std::size_t i = 0;i<3;i++) result[i] -= r / FloatType(3);
-    return result;
+    return eigensystem<FloatType>(adp).values();
   }
 
   /*! \brief Tests if the anisotropic displacement tensor is
@@ -731,125 +670,6 @@ namespace cctbx {
     FloatType const& tolerance)
   {
     return is_positive_definite(eigenvalues(adp), tolerance);
-  }
-
-  namespace detail {
-
-    template <typename FloatType>
-    struct recursively_multiply
-    {
-      vec3<FloatType> v;
-      FloatType abs_lambda;
-
-      recursively_multiply(mat3<FloatType> const& m,
-                           vec3<FloatType> const& v_start,
-                           FloatType tolerance=1.e-6)
-      :
-        v(v_start)
-      {
-        unsigned int run_away_counter = 0;
-        for (;;) {
-          vec3<FloatType> mv = m * v;
-          abs_lambda = mv.length();
-          if (abs_lambda == 0.) throw_not_positive_definite();
-          mv /= abs_lambda;
-          vec3<FloatType> abs_mv = af::fabs(mv.as_tiny());
-          std::size_t i_max = af::max_index(abs_mv);
-          FloatType scaled_tolerance = abs_mv[i_max] * tolerance;
-          bool converged = mv.const_ref().all_approx_equal(
-                            v.const_ref(), scaled_tolerance);
-          if (!converged && mv.const_ref().all_approx_equal(
-                            (-v).const_ref(), scaled_tolerance)) {
-            throw_not_positive_definite(); // lambda < 0
-          }
-          v = mv;
-          if (converged) return;
-          run_away_counter++;
-          CCTBX_ASSERT(run_away_counter < 10000000);
-        }
-      }
-    };
-
-  } // namespace detail
-
-  //! Group of associated eigenvectors and eigenvalues.
-  template <typename FloatType>
-  class eigensystem
-  {
-    public:
-      //! Default constructor. Some data members are not initialized!
-      eigensystem() {}
-
-      /*! \brief Determines the eigenvectors and eigenvalues of the
-          anisotropic displacement tensor.
-       */
-      /*! Since the anisotropic displacement tensor is a symmetric matrix,
-          all eigenvalues are real and the eigenvectors can be chosen
-          orthonormal.
-          <p>
-          The eigenvectors are determined with the method of
-          successive approximations as outlined in J.F. Nye,
-          Physical Properties of Crystals, Oxford Science
-          Publications, 1992, pp.165-168.
-          The given tolerance is used to determine convergence.
-          <p>
-          An exception is thrown if any of the eigenvalues is
-          less than or equal to zero. This indicates that the
-          anisotropic displacement tensor is not positive definite.
-          <p>
-          See also: eigenvalues().
-       */
-      eigensystem(sym_mat3<FloatType> const& adp,
-                  FloatType tolerance=1.e-6);
-
-      //! The i'th eigenvector.
-      /*! An exception is thrown if i >= 3.
-       */
-      vec3<FloatType> const&
-      vectors(std::size_t i) const
-      {
-        if (i >= vectors_.size()) throw error_index();
-        return vectors_[i];
-      }
-
-      //! The eigenvalues.
-      vec3<FloatType> const&
-      values() const { return values_; }
-
-    private:
-      af::tiny<vec3<FloatType>, 3> vectors_;
-      vec3<FloatType> values_;
-  };
-
-  template <typename FloatType>
-  eigensystem<FloatType>
-  ::eigensystem(sym_mat3<FloatType> const& adp,
-                FloatType tolerance)
-  {
-    mat3<FloatType> m[2];
-    m[0] = mat3<FloatType>(adp);
-    FloatType d = m[0].determinant();
-    if (d == 0.) throw_not_positive_definite();
-    m[1] = m[0].co_factor_matrix_transposed() / d;
-    std::size_t i_large[2];
-    for(std::size_t i_m=0;i_m<2;i_m++) {
-      vec3<FloatType> abs_diag = af::fabs(m[i_m].diagonal().as_tiny());
-      i_large[i_m] = af::max_index(abs_diag);
-      if (i_m != 0 && i_large[1] == i_large[0]) {
-        abs_diag[i_large[1]] = -1.;
-        i_large[1] = af::max_index(abs_diag);
-        CCTBX_ASSERT(i_large[1] != i_large[0]);
-      }
-      vec3<FloatType> v(0,0,0);
-      v[i_large[i_m]] = 1.;
-      detail::recursively_multiply<FloatType> v_lambda(m[i_m], v);
-      vectors_[i_m*2] = v_lambda.v;
-      values_[i_m*2] = v_lambda.abs_lambda;
-    }
-    vectors_[1] = vectors_[0].cross(vectors_[2]);
-    CCTBX_ASSERT(vectors_[1].length() != 0);
-    values_[2] = 1. / values_[2];
-    values_[1] = (adp[0] + adp[1] + adp[2]) - (values_[0] + values_[2]);
   }
 
   //! Modifies u_cart such that all eigenvalues are >= 0.
