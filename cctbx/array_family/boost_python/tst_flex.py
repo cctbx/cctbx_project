0e3ac80a3011e298c6936c357de06830b19cb2c2
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-03-21 14:45:38 +0000

    boost::python::make_constructor(from_double)

--- cctbx/array_family/boost_python/tst_flex.py
+++ cctbx/array_family/boost_python/tst_flex.py
@@ -11,7 +11,7 @@ def exercise_flex_sym_mat3_double():
   b = pickle.loads(p)
   assert tuple(a) == tuple(b)
   assert approx_equal(tuple(a.as_double()), (1,2,3,4,5,6,2,3,4,5,6,7))
-  b = flex.sym_mat3_double().from_double(a.as_double())
+  b = flex.sym_mat3_double(a.as_double())
   assert tuple(a) == tuple(b)
 
 def exercise_flex_hendrickson_lattman():
