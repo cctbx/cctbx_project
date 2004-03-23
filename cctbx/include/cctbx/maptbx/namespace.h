aecf8516d534ab572bad3c8faba8a6534e90d258
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-03-23 22:03:17 +0000

    doxygen hooks

---
+++ cctbx/include/cctbx/maptbx/namespace.h
@@ -0,0 +1,10 @@
+// Documentation only. Do not include.
+namespace cctbx {
+
+//! Map algorithms.
+namespace maptbx {
+
+//! Copy structure factors to and from complex maps.
+namespace structure_factors {
+
+}}} // namespace cctbx
