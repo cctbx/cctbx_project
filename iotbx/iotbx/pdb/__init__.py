1ca3b6bfc158e6631d90fd0c8ee2f0d49fb9ae34
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-03-25 05:16:57 +0000

    format_atom_record()

--- iotbx/iotbx/pdb/__init__.py
+++ iotbx/iotbx/pdb/__init__.py
@@ -1,1 +1,40 @@
 from iotbx.pdb.xray_structure import from_pdb as as_xray_structure
+
+def format_atom_record(serial=0,
+                       name=" C  ",
+                       altLoc=" ",
+                       resName="DUM",
+                       chainID=" ",
+                       resSeq=1,
+                       iCode=" ",
+                       site=(0,0,0),
+                       occupancy=1,
+                       tempFactor=0,
+                       segID="    ",
+                       element="  ",
+                       charge="  "):
+  # ATOM
+  #  7 - 11  Integer       serial        Atom serial number.
+  # 13 - 16  Atom          name          Atom name.
+  # 17       Character     altLoc        Alternate location indicator.
+  # 18 - 20  Residue name  resName       Residue name.
+  # 22       Character     chainID       Chain identifier.
+  # 23 - 26  Integer       resSeq        Residue sequence number.
+  # 27       AChar         iCode         Code for insertion of residues.
+  # 31 - 38  Real(8.3)     x             Orthogonal coordinates for X in
+  #                                      Angstroms.
+  # 39 - 46  Real(8.3)     y             Orthogonal coordinates for Y in
+  #                                      Angstroms.
+  # 47 - 54  Real(8.3)     z             Orthogonal coordinates for Z in
+  #                                      Angstroms.
+  # 55 - 60  Real(6.2)     occupancy     Occupancy.
+  # 61 - 66  Real(6.2)     tempFactor    Temperature factor.
+  # 73 - 76  LString(4)    segID         Segment identifier, left-justified.
+  # 77 - 78  LString(2)    element       Element symbol, right-justified.
+  # 79 - 80  LString(2)    charge        Charge on the atom.
+  return (
+      "%-6.6s%5d %-4.4s%1.1s%-3.3s %1.1s%4d%1.1s   %8.3f%8.3f%8.3f"
+    + "%6.2f%6.2f      %-4.4s%2.2s%2.2s") % (
+    "ATOM", serial, name, altLoc, resName, chainID, resSeq,
+            iCode, site[0], site[1], site[2], occupancy, tempFactor,
+            segID, element, charge)
