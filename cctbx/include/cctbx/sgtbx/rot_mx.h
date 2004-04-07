f05ac23e2571416e9eeef5f34f25b795a521d021
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-04-07 00:36:11 +0000

    reference documentation for tr_vec and rot_mx

--- cctbx/include/cctbx/sgtbx/rot_mx.h
+++ cctbx/include/cctbx/sgtbx/rot_mx.h
@@ -1,15 +1,3 @@
-/* Copyright (c) 2001-2002 The Regents of the University of California
-   through E.O. Lawrence Berkeley National Laboratory, subject to
-   approval by the U.S. Department of Energy.
-   See files COPYRIGHT.txt and LICENSE.txt for further details.
-
-   Revision history:
-     2002 Sep: Refactored parts of cctbx/sgtbx/matrix.h (rwgk)
-     2001 Jul: Merged from CVS branch sgtbx_special_pos (rwgk)
-     2001 May: merged from CVS branch sgtbx_type (R.W. Grosse-Kunstleve)
-     2001 Apr: SourceForge release (R.W. Grosse-Kunstleve)
- */
-
 #ifndef CCTBX_SGTBX_ROT_MX_H
 #define CCTBX_SGTBX_ROT_MX_H
 
@@ -21,19 +9,29 @@ namespace cctbx { namespace sgtbx {
 
   class rot_mx_info; // forward declaration
 
+  //! 3x3 rotation matrix.
+  /*! The elements of the matrix are stored as integers and a common
+      denominator. The actual value of an element is obtained by
+      dividing the integer number by the denominator.
+   */
   class rot_mx
   {
     public:
+      //! Initialization of a diagonal matrix with the given denominator.
+      /*! The diagonal elements are defined as diagonal * denominator.
+       */
       explicit
       rot_mx(int denominator = 1, int diagonal = 1)
       : num_(diagonal * denominator), den_(denominator)
       {}
 
+      //! Initialization with numerator m and the given denominator.
       explicit
       rot_mx(sg_mat3 const& m, int denominator = 1)
       : num_(m), den_(denominator)
       {}
 
+      //! Initialization with the given elements and denominator.
       rot_mx(int m00, int m01, int m02,
              int m10, int m11, int m12,
              int m20, int m21, int m22,
@@ -42,33 +40,48 @@ namespace cctbx { namespace sgtbx {
           den_(denominator)
       {}
 
+      //! Numerator of the rotation matrix.
       sg_mat3 const& num() const { return num_; }
+      //! Numerator of the rotation matrix.
       sg_mat3&       num()       { return num_; }
 
+      //! i'th element of the numerator of the rotation matrix.
       int const& operator[](std::size_t i) const { return num_[i]; }
+      //! i'th element of the numerator of the rotation matrix.
       int&       operator[](std::size_t i)       { return num_[i]; }
 
+      //! (r*3+c)'th element of the numerator of the rotation matrix.
       int const& operator()(int r, int c) const { return num_(r, c); }
+      //! (r*3+c)'th element of the numerator of the rotation matrix.
       int&       operator()(int r, int c)       { return num_(r, c); }
 
+      //! Denominator of the rotation matrix.
       int const& den() const { return den_; }
+      //! Denominator of the rotation matrix.
       int&       den()       { return den_; }
 
+      //! True only if both the numerators and the denominators are equal.
       bool operator==(rot_mx const& rhs) const
       {
         if (den_ != rhs.den_) return false;
         return num_.const_ref().all_eq(rhs.num_.const_ref());
       }
 
+      //! False only if both the numerators and the denominators are equal.
       bool operator!=(rot_mx const& rhs) const
       {
         return !((*this) == rhs);
       }
 
+      //! True only if den() != 0.
       bool is_valid() const { return den_ != 0; }
 
+      /*! \brief True only if this is a diagonal matrix and all diagonal
+          elements are equal to the denominator.
+       */
       bool is_unit_mx() const { return num_ == sg_mat3(den_); }
 
+      //! This minus the unit matrix.
       rot_mx minus_unit_mx() const
       {
         rot_mx result(*this);
@@ -76,30 +89,49 @@ namespace cctbx { namespace sgtbx {
         return result;
       }
 
+      //! New rotation matrix with denominator new_den.
+      /*! An exception is thrown if the old rotation matrix
+          cannot be represented using the new denominator.
+       */
       rot_mx new_denominator(int new_den) const;
 
+      //! New rotation matrix with num()*factor and den()*factor.
       rot_mx scale(int factor) const
       {
         if (factor == 1) return *this;
         return rot_mx(num_ * factor, den_ * factor);
       }
 
+      //! Inverse of this matrix.
+      /*! An exception is thrown if the result
+          cannot be represented using the denominator den().
+       */
       rot_mx inverse() const;
 
+      //! New rotation matrix with -num(), den().
       rot_mx operator-() const { return rot_mx(-num_, den_); }
 
+      //! Addition of numerators.
+      /*! An exception is thrown if the denominators are not equal.
+       */
       friend rot_mx operator+(rot_mx const& lhs, rot_mx const& rhs)
       {
         CCTBX_ASSERT(lhs.den_ == rhs.den_);
         return rot_mx(lhs.num_ + rhs.num_, lhs.den_);
       }
 
+      //! Subtraction of numerators.
+      /*! An exception is thrown if the denominators are not equal.
+       */
       friend rot_mx operator-(rot_mx const& lhs, rot_mx const& rhs)
       {
         CCTBX_ASSERT(lhs.den_ == rhs.den_);
         return rot_mx(lhs.num_ - rhs.num_, lhs.den_);
       }
 
+      //! In-place addition of numerators.
+      /*! An exception is thrown if the denominators are not equal.
+       */
       rot_mx& operator+=(rot_mx const& rhs)
       {
         CCTBX_ASSERT(den_ == rhs.den_);
@@ -107,68 +139,109 @@ namespace cctbx { namespace sgtbx {
         return *this;
       }
 
+      //! Matrix multiplication.
+      /*! The denominator of the result is the product lhs.den() * rhs.den().
+       */
       friend rot_mx operator*(rot_mx const& lhs, rot_mx const& rhs)
       {
         return rot_mx(lhs.num_ * rhs.num_, lhs.den_ * rhs.den_);
       }
 
+      //! Matrix*vector multiplication.
+      /*! The denominator of the result is the product lhs.den() * rhs.den().
+       */
       friend tr_vec operator*(rot_mx const& lhs, tr_vec const& rhs)
       {
         return tr_vec(lhs.num_ * rhs.num(), lhs.den_ * rhs.den());
       }
 
+      //! Vector*matrix multiplication.
+      /*! The denominator of the result is the product lhs.den() * rhs.den().
+       */
       friend tr_vec operator*(tr_vec const& lhs, rot_mx const& rhs)
       {
         return tr_vec(lhs.num() * rhs.num_, lhs.den() * rhs.den_);
       }
 
+      //! Matrix*vector multiplication, numerator only.
       friend sg_vec3 operator*(rot_mx const& lhs, sg_vec3 const& rhs)
       {
         return sg_vec3(lhs.num_ * rhs);
       }
 
+      //! New rotation matrix with num()*rhs, den().
       friend rot_mx operator*(rot_mx const& lhs, int rhs)
       {
         return rot_mx(lhs.num_ * rhs, lhs.den_);
       }
 
+      //! New rotation matrix with rhs.num()*lhs, rhs.den().
       friend rot_mx operator*(int lhs, rot_mx const& rhs)
       {
         return rhs * lhs;
       }
 
+      //! In-place multiplication of numerator with rhs.
       rot_mx& operator*=(int rhs)
       {
         num_ *= rhs;
         return *this;
       }
 
+      //! New rotation matrix with num() / rhs, den().
+      /*! An exception is thrown if the result cannot be represented
+          using den().
+       */
       friend rot_mx operator/(rot_mx const& lhs, int rhs);
 
+      //! Cancellation of factors.
+      /*! The denominator of the new rotation matrix is made
+          as small as possible.
+       */
       rot_mx cancel() const;
 
+      //! Matrix inversion with cancellation of factors.
       rot_mx inverse_cancel() const;
 
+      //! Matrix multiplication with cancellation of factors.
       rot_mx multiply(rot_mx const& rhs) const
       {
         return ((*this) * rhs).cancel();
       }
 
+      //! Matrix*vector multiplication with cancellation of factors.
       tr_vec multiply(tr_vec const& rhs) const
       {
         return ((*this) * rhs).cancel();
       }
 
+      //! num()/rhs,den() with cancellation of factors.
       rot_mx divide(int rhs) const;
 
+      //! Rotation-part type (1, 2, 3, 4, 6, -1, -2=m, -3, -4, -6)
+      /*! See also: cctbx::sgtbx::rot_mx_info
+       */
       int type() const;
 
+      //! Rotational order.
+      /*! Number of times the matrix must be multiplied with itself
+          in order to obtain the unit matrix.
+
+          See also: cctbx::sgtbx::rot_mx_info
+       */
       int order(int type=0) const;
 
+      //! Sum of repeated products of this matrix with itself.
+      /*! this + this*this + this*this*this + ... + this**order()
+
+          Restriction: the numerator must be one.
+       */
       rot_mx accumulate(int type=0) const;
 
+      //! Convenience method for constructing a rot_mx_info instance.
       rot_mx_info info() const;
 
+      //! Conversion to a floating-point array.
       template <typename FloatType>
       scitbx::mat3<FloatType>
       as_floating_point(scitbx::type_holder<FloatType>) const
@@ -176,6 +249,9 @@ namespace cctbx { namespace sgtbx {
         return scitbx::mat3<FloatType>(num_) / FloatType(den_);
       }
 
+      //! Conversion to an array with element type double.
+      /*! Python: float()
+       */
       scitbx::mat3<double>
       as_double() const
       {
--- cctbx/include/cctbx/sgtbx/tr_vec.h
+++ cctbx/include/cctbx/sgtbx/tr_vec.h
@@ -1,15 +1,3 @@
-/* Copyright (c) 2001-2002 The Regents of the University of California
-   through E.O. Lawrence Berkeley National Laboratory, subject to
-   approval by the U.S. Department of Energy.
-   See files COPYRIGHT.txt and LICENSE.txt for further details.
-
-   Revision history:
-     2002 Sep: Refactored parts of cctbx/sgtbx/matrix.h (rwgk)
-     2001 Jul: Merged from CVS branch sgtbx_special_pos (rwgk)
-     2001 May: merged from CVS branch sgtbx_type (R.W. Grosse-Kunstleve)
-     2001 Apr: SourceForge release (R.W. Grosse-Kunstleve)
- */
-
 #ifndef CCTBX_SGTBX_TR_VEC_H
 #define CCTBX_SGTBX_TR_VEC_H
 
@@ -23,55 +11,85 @@ namespace cctbx { namespace sgtbx {
 
   void throw_unsuitable_tr_vec(const char* file, long line);
 
+  //! Translation vector.
+  /*! The elements of the vector are stored as integers and a common
+      denominator. The actual value of an element is obtained by
+      dividing the integer number by the denominator.
+   */
   class tr_vec
   {
     public:
+      /*! \brief Initialization of a 0,0,0-translation with the denominator
+          tr_den.
+       */
       explicit
       tr_vec(int tr_den=sg_t_den)
       : num_(0,0,0), den_(tr_den)
       {}
 
+      /*! \brief Initialization a translation with the integer numerator v
+          and the denominator tr_den.
+       */
       explicit
       tr_vec(sg_vec3 const& v, int tr_den=sg_t_den)
       : num_(v), den_(tr_den)
       {}
 
+      /*! \brief Initialization of a v0,v1,v2-translation with the
+          denominator tr_den.
+       */
       tr_vec(int v0, int v1, int v2, int tr_den=sg_t_den)
       : num_(v0,v1,v2), den_(tr_den)
       {}
 
+      //! Numerator of the translation vector.
       sg_vec3 const& num() const { return num_; }
+      //! Numerator of the translation vector.
       sg_vec3&       num()       { return num_; }
 
+      //! i'th element of the numerator of the translation vector.
       int const& operator[](std::size_t i) const { return num_[i]; }
+      //! i'th element of the numerator of the translation vector.
       int&       operator[](std::size_t i)       { return num_[i]; }
 
+      //! Denominator of the translation vector.
       int const& den() const { return den_; }
+      //! Denominator of the translation vector.
       int&       den()       { return den_; }
 
+      //! True only if both the numerators and the denominators are equal.
       bool operator==(tr_vec const& rhs) const
       {
         if (den_ != rhs.den_) return false;
         return num_.const_ref().all_eq(rhs.num_.const_ref());
       }
 
+      //! False only if both the numerators and the denominators are equal.
       bool operator!=(tr_vec const& rhs) const
       {
         return !((*this) == rhs);
       }
 
+      //! True only if den() != 0.
       bool is_valid() const { return den_ != 0; }
 
+      //! True only if all elements of the numertor are equal to zero.
       bool is_zero() const { return num_.is_zero(); }
 
-      tr_vec new_denominator(int new_bf) const;
+      //! New translation vector with denominator new_den.
+      /*! An exception is thrown if the old translation vector
+          cannot be represented using the new denominator.
+       */
+      tr_vec new_denominator(int new_den) const;
 
+      //! New translation vector with num()*factor and den()*factor.
       tr_vec scale(int factor) const
       {
         if (factor == 1) return *this;
         return tr_vec(num_ * factor, den_ * factor);
       }
 
+      //! New translation vector with 0 <= num()[i] < den().
       tr_vec mod_positive() const
       {
         tr_vec result(num_, den_);
@@ -81,6 +99,7 @@ namespace cctbx { namespace sgtbx {
         return result;
       }
 
+      //! New translation vector with -den()/2 < num()[i] <= den()/2.
       tr_vec mod_short() const
       {
         tr_vec result(num_, den_);
@@ -90,20 +109,30 @@ namespace cctbx { namespace sgtbx {
         return result;
       }
 
+      //! New translation vector with -num(), den().
       tr_vec operator-() const { return tr_vec(-num_, den_); }
 
+      //! Addition of numerators.
+      /*! An exception is thrown if the denominators are not equal.
+       */
       friend tr_vec operator+(tr_vec const& lhs, tr_vec const& rhs)
       {
         CCTBX_ASSERT(lhs.den_ == rhs.den_);
         return tr_vec(lhs.num_ + rhs.num_, lhs.den_);
       }
 
+      //! Subtraction of numerators.
+      /*! An exception is thrown if the denominators are not equal.
+       */
       friend tr_vec operator-(tr_vec const& lhs, tr_vec const& rhs)
       {
         CCTBX_ASSERT(lhs.den_ == rhs.den_);
         return tr_vec(lhs.num_ - rhs.num_, lhs.den_);
       }
 
+      //! In-place addition of numerators.
+      /*! An exception is thrown if the denominators are not equal.
+       */
       tr_vec& operator+=(tr_vec const& rhs)
       {
         CCTBX_ASSERT(den_ == rhs.den_);
@@ -111,24 +140,37 @@ namespace cctbx { namespace sgtbx {
         return *this;
       }
 
+      //! New translation vector with num() * rhs, den().
       friend tr_vec operator*(tr_vec const& lhs, int rhs)
       {
         return tr_vec(lhs.num_ * rhs, lhs.den_);
       }
 
+      //! New translation vector with num() / rhs, den().
+      /*! An exception is thrown if the result cannot be represented
+          using den().
+       */
       friend tr_vec operator/(tr_vec const& lhs, int rhs);
 
+      //! New translation vector with rhs.num() * lhs, rhs.den().
       friend tr_vec operator*(int const& lhs, tr_vec const& rhs)
       {
         return rhs * lhs;
       }
 
+      //! Cancellation of factors.
+      /*! The denominator of the new translation vector is made
+          as small as possible.
+       */
       tr_vec cancel() const;
 
+      //! Addition with cancellation of factors.
       tr_vec plus(tr_vec const& rhs) const;
 
+      //! Subtraction with cancellation of factors.
       tr_vec minus(tr_vec const& rhs) const;
 
+      //! Conversion to a floating-point array.
       template <typename FloatType>
       scitbx::vec3<FloatType>
       as_floating_point(scitbx::type_holder<FloatType>) const
@@ -136,12 +178,18 @@ namespace cctbx { namespace sgtbx {
         return scitbx::vec3<FloatType>(num_) / FloatType(den_);
       }
 
+      //! Conversion to an array with element type double.
+      /*! Python: float()
+       */
       scitbx::vec3<double>
       as_double() const
       {
         return as_floating_point(scitbx::type_holder<double>());
       }
 
+      //! Conversion to a string with rational or decimal numbers.
+      /*! Python: str()
+       */
       std::string
       as_string(bool decimal=false, const char* separator=",") const;
 
