9a2447ac6d493d41a584c6e4bba0acd8037c0e72
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-04-08 22:09:39 +0000

    shared.std_set_size_t

--- scitbx/array_family/boost_python/shared_ext.cpp
+++ scitbx/array_family/boost_python/shared_ext.cpp
@@ -1,6 +1,7 @@
 #include <scitbx/array_family/boost_python/shared_wrapper.h>
 #include <boost/python/module.hpp>
 #include <vector>
+#include <set>
 
 namespace scitbx { namespace af { namespace boost_python {
 namespace {
@@ -11,6 +12,11 @@ namespace {
       "std_vector_size_t");
     scitbx::boost_python::container_conversions
     ::tuple_mapping_variable_capacity<std::vector<std::size_t> >();
+
+    shared_wrapper<std::set<std::size_t> >::wrap(
+      "std_set_size_t");
+    scitbx::boost_python::container_conversions
+    ::tuple_mapping_set<std::set<std::size_t> >();
   }
 
 }}}} // namespace scitbx::af::boost_python::<anonymous>
