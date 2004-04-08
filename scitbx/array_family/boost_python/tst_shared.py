588675b809b8365303bfbec9e4d2b162666dcad2
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-04-08 22:09:39 +0000

    shared.std_set_size_t

--- scitbx/array_family/boost_python/tst_shared.py
+++ scitbx/array_family/boost_python/tst_shared.py
@@ -39,8 +39,18 @@ def exercise_std_vector_size_t():
   a = shared.std_vector_size_t([[1,2],[2,3]])
   assert list(a) == [(1, 2), (2, 3)]
 
+def exercise_std_set_size_t():
+  a = shared.std_set_size_t()
+  assert a.size() == 0
+  a = shared.std_set_size_t([(2,1)])
+  assert a.size() == 1
+  assert a[0] == (1, 2)
+  a = shared.std_set_size_t([(2,1),(3,5,2)])
+  assert list(a) == [(1, 2), (2, 3, 5)]
+
 def exercise():
   exercise_std_vector_size_t()
+  exercise_std_set_size_t()
   print "OK"
 
 if (__name__ == "__main__"):
