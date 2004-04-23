15815b4e863ea2049b191f5536536ae5d472ee47
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-04-23 00:03:23 +0000

    general: __radd__ add_a_s, __rmul__ mul_a_s; complex_double: __mul__, __rmul__ mul_ac_ar (complex array times real array)

--- scitbx/array_family/boost_python/flex_complex_double.cpp
+++ scitbx/array_family/boost_python/flex_complex_double.cpp
@@ -1,23 +1,29 @@
-/* Copyright (c) 2001-2002 The Regents of the University of California
-   through E.O. Lawrence Berkeley National Laboratory, subject to
-   approval by the U.S. Department of Energy.
-   See files COPYRIGHT.txt and LICENSE.txt for further details.
-
-   Revision history:
-     2002 Aug: Created (R.W. Grosse-Kunstleve)
- */
-
 #include <scitbx/array_family/boost_python/flex_wrapper.h>
 #include <scitbx/array_family/boost_python/flex_pickle_single_buffered.h>
 #include <scitbx/array_family/boost_python/flex_wrapper_complex.h>
 
 namespace scitbx { namespace af { namespace boost_python {
 
+namespace {
+
+  versa<std::complex<double>, flex_grid<> >
+  mul_ac_ar(
+    versa<std::complex<double>, flex_grid<> > const& a1,
+    versa<             double , flex_grid<> > const& a2)
+  {
+    return a1 * a2;
+  }
+
+} // namespace <anonymous>
+
   void wrap_flex_complex_double()
   {
     flex_wrapper<std::complex<double> >::numeric_common(
       "complex_double", boost::python::scope())
-      .def_pickle(flex_pickle_single_buffered<std::complex<double> >());
+      .def_pickle(flex_pickle_single_buffered<std::complex<double> >())
+      .def("__mul__", mul_ac_ar)
+      .def("__rmul__", mul_ac_ar)
+    ;
     flex_wrapper_complex_functions<double>::wrap(boost::python::scope());
   }
 
