f6bdb416a7da91cd503ca0a81b8b71f40b05e406
Pavel Afonine <afonine@users.noreply.github.com>
2004-05-05 23:44:18 +0000

    i0(x) and i1(x) added

--- scitbx/include/scitbx/math/bessel.h
+++ scitbx/include/scitbx/math/bessel.h
@@ -48,6 +48,60 @@ namespace bessel {
     return result;
   }
 
+  //! Calculates I0(x).
+  template <typename FloatType>
+  FloatType
+  i0(FloatType const& x)
+  {
+    typedef FloatType f_t;
+    f_t abs_x = x;
+    if (abs_x < 0) abs_x = -abs_x;
+    f_t bessel_i0;
+    if (abs_x/3.75 < 1.0) {
+          f_t y = x/3.75;
+          y *= y;
+          bessel_i0=1.0+y*(3.5156229+y*(3.0899424+y*(1.2067492+
+         y*(0.2659732+y*(0.0360768+y*0.0045813)))));
+    }
+    else {
+          f_t y = 3.75/abs_x;
+          y=0.39894228+y*(0.01328592+y*(0.00225319+y*(-0.00157565+
+        y*(0.00916281+y*(-0.02057706+y*(0.02635537+
+        y*(-0.01647633+y*0.00392377)))))));
+      bessel_i0=y*std::exp(abs_x)/std::sqrt(abs_x);
+    }
+    return bessel_i0;
+  }
+
+  //! Calculates I1(x).
+  template <typename FloatType>
+  FloatType
+  i1(FloatType const& x)
+  {
+    typedef FloatType f_t;
+    f_t abs_x = x;
+    if (abs_x < 0) abs_x = -abs_x;
+    f_t ans;
+    if (abs_x/3.75 < 1.0) {
+      f_t y=x/3.75;
+      y*=y;
+      ans=abs_x*(0.5+y*(0.87890594+y*(0.51498869+y*(0.15084934
+          +y*(0.2658733e-1+y*(0.301532e-2+y*0.32411e-3))))));
+    }
+    else {
+      f_t y=3.75/abs_x;
+      ans=0.2282967e-1+y*(-0.2895312e-1+y*(0.1787654e-1
+          -y*0.420059e-2));
+      ans=0.39894228+y*(-0.3988024e-1+y*(-0.362018e-2
+          +y*(0.163801e-2+y*(-0.1031555e-1+y*ans))));
+      ans *= (std::exp(abs_x)/std::sqrt(abs_x));
+  }
+  //return x < 0.0 ? -ans : ans;
+  f_t result = ans;
+  if (x < 0.0 && result > 0.0) return -result;
+  return result;
+}
+
 }}} // namespace scitbx::math::bessel
 
 #endif // SCITBX_MATH_BESSEL_H
