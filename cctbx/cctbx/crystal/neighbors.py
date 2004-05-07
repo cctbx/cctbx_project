3bb933250688c6eda922c15c0a27a68c0e62280a
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-05-07 08:20:41 +0000

    highly experimental and inefficient code, proof of principle: enumeration of symmetrically equivalent homogeneous bonds

--- cctbx/cctbx/crystal/neighbors.py
+++ cctbx/cctbx/crystal/neighbors.py
@@ -4,17 +4,49 @@ from cctbx import crystal
 from cctbx.array_family import flex
 from scitbx.python_utils.misc import adopt_init_args
 from libtbx.itertbx import count
-from libtbx.test_utils import eps_eq
+from libtbx.test_utils import eps_eq, approx_equal
 import math
 import sys
 
+# XXX
+from mmtbx.stereochemistry.tst_interaction import pml_stick, write_pml
+from cctbx import sgtbx
+from scitbx import matrix
+
+def unit_cell_sticks(unit_cell, colors=[[1,1,1]]*2, width=0.05):
+  ortho = unit_cell.orthogonalize
+  points_frac = []
+  points_cart = []
+  for i in (0,1):
+    for j in (0,1):
+      for k in (0,1):
+        v = (i,j,k)
+        points_frac.append(v)
+        points_cart.append(ortho(v))
+  sticks = []
+  for i in xrange(0,7):
+    for j in xrange(i+1,8):
+      v1 = points_frac[i]
+      v2 = points_frac[j]
+      n = 0
+      for k in xrange(3):
+        if (v1[k] != v2[k]): n += 1
+      if (n == 1):
+        sticks.append(
+          pml_stick(
+            begin=points_cart[i],
+            end=points_cart[j],
+            colors=colors,
+            width=width))
+  return sticks
+
 class contact:
 
-  def __init__(self, i, label, site, rt_mx, dist, i_sym_equiv):
+  def __init__(self, i, j_seq, label, site, rt_mx, dist, i_sym_equiv):
     adopt_init_args(self, locals())
 
   def __str__(self):
-    result = "  %2d: %-6s (%7.4f %7.4f %7.4f) %-25s %8.3f" % (
+    result = " %3d: %-6s (%7.4f %7.4f %7.4f) %-25s %8.3f" % (
         (self.i, self.label)
       + self.site
       + (self.rt_mx, self.dist))
@@ -22,7 +54,16 @@ class contact:
       result += " eq %2d" % self.i_sym_equiv
     return result
 
-def show_distances(structure, distance_cutoff=5):
+class ata_group:
+
+  def __init__(self, mm, rt, special_op):
+    unit_mx = matrix.rt(([1,0,0,0,1,0,0,0,1], [0,0,0]))
+    self.a = float((unit_mx - rt.as_rational()) * special_op)
+    self.rtgr = (self.a.r.transpose() * mm * self.a.r)
+    self.rtgt = (self.a.r.transpose() * mm * self.a.t)
+    self.ttgt = (self.a.t.transpose() * mm * self.a.t)(0,0)+1
+
+def show_distances(structure, distance_cutoff=8):
   asu_mappings = structure.asu_mappings(
     buffer_thickness=distance_cutoff)
   pair_generator = crystal.neighbors_fast_pair_generator(
@@ -39,10 +80,15 @@ def show_distances(structure, distance_cutoff=5):
   for i,distances,pairs in zip(count(),distances_list,pairs_list):
     perm = flex.sort_permutation(distances)
     pairs_list[i] = flex.select(pairs, perm)
+  unit_mx = matrix.rt(([1,0,0,0,1,0,0,0,1], [0,0,0]))
+  orth_rt = matrix.rt(
+    (structure.unit_cell().orthogonalization_matrix(), [0,0,0]))
+  mm = matrix.sym(structure.unit_cell().metrical_matrix())
+  dist = structure.unit_cell().distance
   site_symmetries = []
-  for scatterer in structure.scatterers():
-    site_symmetries.append(structure.site_symmetry(scatterer.site))
   scatterers = structure.scatterers()
+  for scatterer in scatterers:
+    site_symmetries.append(structure.site_symmetry(scatterer.site))
   for pairs in pairs_list:
     if (len(pairs) > 0):
       i_seq = pairs[0].i_seq
@@ -50,6 +96,7 @@ def show_distances(structure, distance_cutoff=5):
       site_i = scatterer_i.site
       site_symmetry_i = site_symmetries[i_seq]
       rt_mx_i_inverse = asu_mappings.get_rt_mx(i_seq=i_seq, i_sym=0).inverse()
+      special_op_i = site_symmetries[i_seq].special_op().as_rational()
       print "%s: site symmetry: %s, multiplicity: %d" % (
         scatterer_i.label,
         site_symmetry_i.point_group_type(),
@@ -76,6 +123,7 @@ def show_distances(structure, distance_cutoff=5):
             contact_site = ms * scatterer_j.site
             contacts.append(contact(
               i=len(contacts),
+              j_seq=pair.j_seq,
               label=scatterer_j.label,
               site=contact_site,
               rt_mx=m.multiply(rt_mx_ji),
@@ -87,8 +135,134 @@ def show_distances(structure, distance_cutoff=5):
               structure.unit_cell().distance(site_i, contact_site)**2
               -pair.dist_sq)
             assert mismatch < asu_mappings.sym_equiv_tolerance()**2
+        if (pair.j_seq == i_seq):
+          ag_ji = ata_group(mm, rt_mx_ji, special_op_i)
+          site_ji = rt_mx_ji * site_i
+          for rt_mx_k in structure.space_group():
+            ag_k = ata_group(mm, rt_mx_k, special_op_i)
+            if (approx_equal(ag_ji.rtgr, ag_k.rtgr)):
+              print "CANDIDATE", i_seq, rt_mx_ji, pair.dist_sq**.5
+              site_k = rt_mx_k * site_i
+              print "  ji:", (matrix.col(site_ji) - matrix.col(site_i)).elems
+              print "  ki:", (matrix.col(site_k) - matrix.col(site_i)).elems
+              t_den = rt_mx_k.t().den()
+              for u in flex.nested_loop(begin=[-3]*3,
+                                        end=[3]*3, open_range=00000):
+                tu = [t+x*t_den for t,x in zip(rt_mx_k.t().num(), u)]
+                rt_mx_ku = sgtbx.rt_mx(rt_mx_k.r(), sgtbx.tr_vec(tu, t_den))
+                if (str(rt_mx_ku) != str(rt_mx_ji)):
+                  ag_ku = ata_group(mm, rt_mx_ku, special_op_i)
+                  assert approx_equal(ag_ji.rtgr, ag_ku.rtgr)
+                  if (    approx_equal(ag_ji.rtgt, ag_ku.rtgt)
+                      and approx_equal(ag_ji.ttgt, ag_ku.ttgt)):
+                    d = dist(site_i, rt_mx_ku*site_i)
+                    print "HIT", rt_mx_ku, d
+                    assert approx_equal(d, pair.dist_sq**.5)
+    orth = structure.unit_cell().orthogonalize
+    frac = structure.unit_cell().fractionalize
+    mm_aug = orth_rt.as_augmented_matrix()
+    mm_aug = mm_aug.transpose() * mm_aug
+    color_list = []
+    prev_c = None
     for c in contacts:
       print c
+      if (0): continue
+      colors=[[0,1,0]]*2
+      if (    prev_c != None
+          and c.j_seq == prev_c.j_seq
+          and abs(prev_c.dist - c.dist) < 1.e-5
+          and c.i_sym_equiv is None):
+        if (c.j_seq == i_seq):
+          atas = []
+          fatas = []
+          for rt in (prev_c.rt_mx, c.rt_mx):
+            fdm = float((unit_mx - rt.as_rational()) * special_op_i)
+            fa = fdm.as_augmented_matrix()
+            fata = fa.transpose() * mm_aug * fa
+            fatas.append(fata)
+            ag = ata_group(mm, rt, special_op_i)
+            if (0):
+              print "RtGR:", ag.rtgr.elems
+              print "xR:", fata.extract_block(stop=(3,3)).elems
+              print "RtGT:", ag.rtgt.elems
+              print "xT:", fata.extract_block(start=(0,3),stop=(3,4)).elems
+              print "TtGT:", ag.ttgt
+              print "x33:", fata(3,3)
+            assert approx_equal(ag.rtgr, fata.extract_block(stop=(3,3)))
+            assert approx_equal(ag.rtgt,
+              fata.extract_block(start=(0,3),stop=(3,4)).elems)
+            assert approx_equal(ag.ttgt, fata(3,3))
+            dm = orth_rt * fdm
+            a = dm.as_augmented_matrix()
+            ata = a.transpose() * a
+            atas.append(ata)
+          if (0):
+            for fata in fatas: print "fata:", list(fata)
+            for ata in atas: print "ata:", list(ata)
+          equiv_fata = approx_equal(fatas[0], fatas[1])
+          equiv_ata = approx_equal(atas[0], atas[1])
+          if (equiv_fata != equiv_ata):
+            print "ata mismatch", structure.space_group_info()
+        for shift in [0, distance_cutoff*0.1]:
+          site_j_mod = site_symmetries[c.j_seq].special_op() \
+            * frac([x+shift for x in orth(scatterers[c.j_seq].site)])
+          if (shift == 0 or c.j_seq != i_seq):
+            site_i_mod = site_i
+          else:
+            site_i_mod = site_j_mod
+          dv1 = matrix.col(c.rt_mx * site_j_mod) - matrix.col(site_i_mod)
+          dv2 = matrix.col(prev_c.rt_mx * site_j_mod) - matrix.col(site_i_mod)
+          d1 = dist(c.rt_mx * site_j_mod, site_i_mod)
+          d2 = dist(prev_c.rt_mx * site_j_mod, site_i_mod)
+          assert abs(d1-structure.unit_cell().length(dv1)) < 1.e-6
+          assert abs(d2-structure.unit_cell().length(dv2)) < 1.e-6
+          if (shift == 0):
+            assert abs(d1-d2) < 1.e-4
+        if (abs(d1-d2) < 1.e-3):
+          if (0):
+            if (c.j_seq != i_seq):
+              print "HARD",
+          if (1):
+            print "LOOK", scatterer_i.label,
+          if (0):
+            print site_symmetries[i_seq].point_group_type(),
+            print structure.space_group_info()
+            print "d1:", d1
+            print "d2:", d2
+          colors=[[1,0,0]]*2
+          color_list[-1] = colors
+          if (c.j_seq == i_seq):
+            if (not equiv_ata):
+              print "equiv_ata false mismatch"
+            elif (1):
+              print "equiv_ata true confirmation"
+        else:
+          if (0 or c.j_seq == i_seq):
+            print "FALSE ALARM"
+          if (c.j_seq == i_seq):
+            if (equiv_ata):
+              print "equiv_ata true mismatch"
+            elif (0):
+              print "equiv_ata false confirmation"
+      color_list.append(colors)
+      prev_c = c
+    sticks = []
+    for c,colors in zip(contacts, color_list):
+      sticks.append(
+        pml_stick(
+          begin=structure.unit_cell().orthogonalize(scatterer_i.site),
+          end=structure.unit_cell().orthogonalize(c.site),
+          colors=colors,
+          width=0.1))
+    write_pml(
+      f=open("contacts_%s.pml" % scatterer_i.label, "w"),
+      label=scatterer_i.label,
+      sticks=sticks)
+  sticks = unit_cell_sticks(structure.unit_cell())
+  write_pml(
+    f=open("cell.pml", "w"),
+    label="cell",
+    sticks=sticks)
 
 def test_hcp():
   a = 2
