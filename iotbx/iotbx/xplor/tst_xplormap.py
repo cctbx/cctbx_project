2025f22d0173c68baae60624309a495a131ab053
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-05-15 05:59:08 +0000

    first step of generalizing xplor map reading and writing

--- iotbx/iotbx/xplor/tst_xplormap.py
+++ iotbx/iotbx/xplor/tst_xplormap.py
@@ -1,43 +1,64 @@
-from iotbx.xplor import XplorMap
+from iotbx.xplor import map_gridding, XplorMap
+from cctbx import uctbx
 from libtbx.test_utils import approx_equal
 import urllib
 import filecmp
+
+def exercise_map_gridding():
+  try:
+    g = map_gridding(n=(0,20,30), first=(-3,-4,-5), last=(5,4,3))
+  except RuntimeError, e:
+    assert str(e) == "Illegal xplor map gridding for dimension X: " \
+                   + "gridding=0, first=-3, last=5"
+  g = map_gridding(n=(10,20,30), first=(-3,-4,-5), last=(5,4,3))
+  fg = g.as_flex_grid()
+  assert fg.origin() == g.first
+  assert fg.last(0) == g.last
+  assert fg.last(1) == (6,5,4)
+  assert fg.all() == (9,9,9)
+  assert not fg.is_padded()
 
 def get_test_files():
   urllib.urlretrieve(
     'http://cci.lbl.gov/cctbx_downloads/regression/iotbx/NSFN_C2221.xplor',
     'NSFN_C2221.xplor')
 
-def read_xplor(f):
-  a = XplorMap().read(f)
+def read_xplor(file_name):
+  a = XplorMap().read(file_name=file_name)
   assert a.title == [' REMARKS FILENAME=""',
                      ' REMARKS Phenix Xarray to CNS map format']
-  assert a.sections == [24, 0, 24, 120, 0, 120, 54, 0, 54]
-  assert ["%.3f"%v for v in a.unitcell.parameters()] \
-      == ['32.042', '175.362', '79.663', '90.000', '90.000', '90.000']
-  assert a.order == "ZYX"
+  assert a.gridding.n == (24,120,54)
+  assert a.gridding.first == (0,0,0)
+  assert a.gridding.last == (24,120,54)
+  assert a.unit_cell.is_similar_to(
+    uctbx.unit_cell((32.042, 175.362, 79.663, 90, 90, 90)))
   assert approx_equal(a.average, 0)
-  assert approx_equal(a.stddev, 1)
+  assert approx_equal(a.standard_deviation, 1)
   d = a.data
   assert approx_equal(d[0:5], (-0.284546,-0.60108,-1.11654,-1.17415,-0.827963))
-  assert a.data.focus() == (25, 121, 55)
+  assert a.data.origin() == (0,0,0)
+  assert a.data.last(0) == (24,120,54)
+  assert a.data.last() == (25,121,55)
+  assert a.data.focus() == (25,121,55)
   return a
 
-def write_xplor(map,f):
+def write_xplor(map, file_name):
   a = XplorMap()
-  a.title=map.title
-  a.sections=map.sections
-  a.unitcell=map.unitcell
-  a.order=map.order
-  a.data=map.data
-  a.write(f)
+  a.title = map.title
+  a.gridding = map.gridding
+  a.unit_cell = map.unit_cell
+  a.data = map.data
+  a.avarage = map.average
+  a.standard_deviation = map.standard_deviation
+  a.write(file_name=file_name)
 
 def run():
+  exercise_map_gridding()
   get_test_files()
-  map = read_xplor('NSFN_C2221.xplor')
-  write_xplor(map,'comparison.xplor')
-  assert filecmp.cmp('NSFN_C2221.xplor','comparison.xplor')
+  map = read_xplor("NSFN_C2221.xplor")
+  write_xplor(map, "comparison.xplor")
+  assert filecmp.cmp("NSFN_C2221.xplor", "comparison.xplor")
   print "OK"
 
-if __name__=="__main__":
+if (__name__=="__main__"):
   run()
--- iotbx/xplor/boost_python/xplor_ext.cpp
+++ iotbx/xplor/boost_python/xplor_ext.cpp
@@ -2,6 +2,7 @@
 #include <cctbx/uctbx.h>
 #include <scitbx/array_family/flex_types.h>
 #include <boost/python/class.hpp>
+#include <boost/python/args.hpp>
 #include <boost/python/module.hpp>
 #include <boost/smart_ptr.hpp>
 #include <fstream>
@@ -73,102 +74,108 @@ boost::shared_array<char> ScientificFormatter::operator() (double d) {
     return b;
   }
 
+  class XplorMap
+  {
+    private:
+      ScientificFormatter pretty;
+      ScientificFormatter pretty4;
 
-class XplorMap {
-private:
-  ScientificFormatter pretty;
-  ScientificFormatter pretty4;
-public:
-  XplorMap();
-  af::versa<double,af::c_grid<3> >
-  ReadXplorMap(std::string const&, int, af::int9 const& sec);
-  void WriteXplorMap(cctbx::uctbx::unit_cell const&,
-                     af::const_ref<double,af::c_grid<3> > const&,
-                     double, double, std::string const&);
-};
+    public:
+      XplorMap();
 
-XplorMap::XplorMap():pretty("%12.5E"),pretty4("%12.4E"){}
+      af::versa<double, af::flex_grid<> >
+      ReadXplorMap(
+        std::string const& file_name,
+        std::size_t n_header_lines,
+        af::flex_grid<> const& grid)
+      {
+        SCITBX_ASSERT(grid.nd() == 3);
+        SCITBX_ASSERT(grid.all().all_gt(0));
+        af::versa<double, af::flex_grid<> > map(grid, 0);
+        std::ifstream cin(file_name.c_str());
+        std::string line;
+        for (std::size_t i=0;i<n_header_lines;i++) {
+          std::getline(cin, line);
+        }
+        af::ref<double, af::c_grid<3> > map_ref(
+          map.begin(),
+          af::c_grid<3>(af::adapt(map.accessor().all())));
+        for(std::size_t iz=0;iz<map_ref.accessor()[2];iz++) {
+          std::getline(cin, line); // reads section number
+          std::size_t i_fld = 6;
+          for(std::size_t iy=0;iy<map_ref.accessor()[1];iy++) {
+            for(std::size_t ix=0;ix<map_ref.accessor()[0];ix++) {
+              if (i_fld == 6) {
+                std::getline(cin, line);
+                i_fld = 0;
+              }
+              map_ref(ix,iy,iz) = std::atof(line.substr(i_fld*12,12).c_str());
+              i_fld++;
+            }
+          }
+        }
+        cin.close();
+        return map;
+      }
 
-af::versa<double,af::c_grid<3> >
-XplorMap::ReadXplorMap(std::string const& filename,
-                       int headers,
-                       af::int9 const& sec)
-{
-  std::ifstream cin(filename.c_str());
-  std::string line;
-  for (std::size_t i = 0; i < headers; i++) {
-    std::getline(cin,line);
-  }
-  // dump everything to a 3-D array
-  int nX = sec[2] - sec[1] + 1;
-  int nY = sec[5] - sec[4] + 1;
-  int nZ = sec[8] - sec[7] + 1;
-  SCITBX_ASSERT(nX > 0);
-  SCITBX_ASSERT(nY > 0);
-  SCITBX_ASSERT(nZ > 0);
-  af::versa<double,af::c_grid<3> > m(af::c_grid<3>(nX,nY,nZ));
-  af::ref<double, af::c_grid<3> > mref=m.ref();
-  for (int section = 0; section < nZ; section++) {
-    std::getline(cin,line); //reads section number
-    int x=0;
-    int y=0;
-    for (int rect = 0, fast = 0; rect < (nX*nY); rect++,fast++) {
-      if (fast%6==0)
-        {    std::getline(cin,line); fast=0;      }
-      mref(x,y,section) = std::atof(line.substr(fast*12,12).c_str());
-      ++x;
-      if (x==nX) {x=0; ++y;}
-    }
-  }
+      void
+      WriteXplorMap(
+        std::string const& file_name,
+        cctbx::uctbx::unit_cell const& unit_cell,
+        af::const_ref<double, af::flex_grid<> > const& map,
+        double average,
+        double standard_deviation)
+      {
+        SCITBX_ASSERT(map.accessor().nd() == 3);
+        SCITBX_ASSERT(map.accessor().all().all_gt(0));
+        SCITBX_ASSERT(!map.accessor().is_padded());
+        FILE* fh = fopen(file_name.c_str(), "ab");
+        SCITBX_ASSERT(fh != 0);
+        for(std::size_t i=0;i<6;i++) {
+          fprintf(fh, "%s", pretty(unit_cell.parameters()[i]).get());
+        }
+        fprintf(fh, "\n");
+        fprintf(fh, "ZYX\n");
+        af::const_ref<double, af::c_grid<3> > map_ref(
+          map.begin(),
+          af::c_grid<3>(af::adapt(map.accessor().all())));
+        for(std::size_t iz=0;iz<map_ref.accessor()[2];iz++) {
+          fprintf(fh, "%8lu\n", static_cast<unsigned long>(iz));
+          std::size_t i_fld = 0;
+          for(std::size_t iy=0;iy<map_ref.accessor()[1];iy++) {
+            for(std::size_t ix=0;ix<map_ref.accessor()[0];ix++) {
+              fprintf(fh, "%s", pretty(map_ref(ix,iy,iz)).get());
+              i_fld++;
+              if (i_fld == 6) {
+                fprintf(fh, "\n");
+                i_fld = 0;
+              }
+            }
+          }
+          if (i_fld > 0) {
+            fprintf(fh, "\n");
+          }
+        }
+        fprintf(fh, "   -9999\n");
+        fprintf(fh, "%s%s\n",
+          pretty4(average).get(), pretty4(standard_deviation).get());
+        fclose(fh);
+      }
+  };
 
-  cin.close();
-  return m;
-}
-
-void XplorMap::WriteXplorMap(cctbx::uctbx::unit_cell const& uc,
-                             af::const_ref<double,af::c_grid<3> > const& data,
-                             double average,
-                             double stddev,
-                             std::string const& outputfile)
-{
-  FILE* fh = fopen(outputfile.c_str(),"ab");
-  SCITBX_ASSERT(fh != 0);
-  //Unit Cell
-  for(std::size_t i=0;i<6;i++) {
-    fprintf(fh, "%s", pretty(uc.parameters()[i]).get());
-  }
-  fprintf(fh, "\n");
-  //Data
-  fprintf(fh,"ZYX\n");
-  int xsize = data.accessor()[0];
-  int ysize = data.accessor()[1];
-  for (int z = 0; z< data.accessor()[2]; z++){
-    int eol=0;
-    int x=0;
-    int y=0;
-    fprintf(fh,"%8d\n",z);
-    for (int xy = 0; xy < xsize*ysize; ++xy){
-        fprintf(fh,"%s",pretty(data(x,y,z)).get());
-        ++x;
-        if (x==xsize) {x = 0;++y;}
-        ++eol;
-        if (eol % 6 == 0) {fprintf(fh,"\n");}
-    }
-    if (eol % 6 != 0) {fprintf(fh,"\n");}
-  }
-  //summary
-  fprintf(fh,"   -9999\n");
-  fprintf(fh, "%s%s\n", pretty4(0.0).get(), pretty4(1.0).get());
-  fclose(fh);
-}
+  XplorMap::XplorMap():pretty("%12.5E"),pretty4("%12.4E"){}
 
 } // namespace <anonymous>
 
 BOOST_PYTHON_MODULE(iotbx_xplor_ext)
 {
-    using namespace boost::python;
-    class_<XplorMap>("XplorMap", init<>())
-      .def("ReadXplorMap", &XplorMap::ReadXplorMap)
-      .def("WriteXplorMap", &XplorMap::WriteXplorMap)
-    ;
+  using namespace boost::python;
+  typedef boost::python::arg arg_; // gcc 2.96 workaround
+  class_<XplorMap>("XplorMap", init<>())
+    .def("ReadXplorMap", &XplorMap::ReadXplorMap,
+      (arg_("file_name"), arg_("n_header_lines"), arg_("grid")))
+    .def("WriteXplorMap", &XplorMap::WriteXplorMap,
+      (arg_("file_name"), arg_("unit_cell"), arg_("map"),
+       arg_("average"), arg_("standard_deviation")))
+  ;
 }
