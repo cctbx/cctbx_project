653c8b9d1684b959014009dafb57940a9280cb7a
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-05-15 09:04:23 +0000

    regression test: recycling of random maps

--- iotbx/iotbx/xplor/map.py
+++ iotbx/iotbx/xplor/map.py
@@ -63,12 +63,12 @@ class reader:
   def __init__(self, file_name):
     f = open(file_name, "r")
     f.readline()
-    self.title = []
+    self.title_lines = []
     ntitle = int(f.readline().strip().split("!")[0])
-    self.title=[]
+    self.title_lines=[]
     for x in xrange(ntitle):
       line = f.readline().rstrip()
-      self.title.append(line)
+      self.title_lines.append(line)
     line = f.readline()
     values = [int(line[i:i+8]) for i in xrange(0,72,8)]
     self.gridding = gridding(
@@ -83,20 +83,20 @@ class reader:
     f.close()
     ext_reader = ext.map_reader(
       file_name=file_name,
-      n_header_lines=len(self.title)+5,
+      n_header_lines=len(self.title_lines)+5,
       grid=self.gridding.as_flex_grid())
     self.data = ext_reader.data
     self.average = ext_reader.average
     self.standard_deviation = ext_reader.standard_deviation
 
-def writer(file_name, title, unit_cell, gridding, data,
+def writer(file_name, title_lines, unit_cell, gridding, data,
            average=0,
            standard_deviation=1):
   assert gridding.is_compatible_flex_grid(data.accessor())
   f = open(file_name, "wb")
   f.write("\n")
-  f.write("%8d !NTITLE\n" % len(title))
-  for line in title:
+  f.write("%8d !NTITLE\n" % len(title_lines))
+  for line in title_lines:
     f.write("%-264s\n" % line)
   f.write("%s\n" % gridding.format_9i8())
   f.close()
