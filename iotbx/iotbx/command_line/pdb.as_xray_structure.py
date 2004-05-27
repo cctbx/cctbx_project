e9c1f78d191d09f0ed473fb2c1b1dab3ff1ed179
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-05-27 14:21:10 +0000

    use iotbx_option_parser; --verbose option

--- iotbx/iotbx/command_line/pdb.as_xray_structure.py
+++ iotbx/iotbx/command_line/pdb.as_xray_structure.py
@@ -1,68 +1,59 @@
 #! /usr/bin/env python
 
 from iotbx import pdb
-from cctbx import crystal
-from cctbx import uctbx
-from cctbx import sgtbx
-from iotbx import crystal_symmetry_from_any
+from iotbx.option_parser import iotbx_option_parser
 from scitbx.python_utils import easy_pickle
-import sys
-
-def usage():
-  return (  "usage: iotbx.pdb.as_xray_structure"
-          + " [--unit_cell=1,1,1,90,90,90]"
-          + " [--space_group=P212121]"
-          + " [--extract_symmetry=any_file_format]"
-          + " [--force_symmetry]"
-          + " [--pickle=file_name]"
-          + " pdb_file ...")
+import sys, os
 
 def run(args):
-  unit_cell = None
-  space_group_info = None
-  force_symmetry = 00000
-  pickle_file_name = None
-  remaining_args = []
-  for arg in args:
-    if (arg.startswith("--unit_cell=")):
-      params = arg.split("=", 1)[1]
-      unit_cell = uctbx.unit_cell(params)
-    elif (arg.startswith("--space_group=")):
-      symbol = arg.split("=", 1)[1]
-      space_group_info = sgtbx.space_group_info(symbol=symbol)
-    elif (arg.startswith("--extract_symmetry=")):
-      file_name = arg.split("=", 1)[1]
-      crystal_symmetry = crystal_symmetry_from_any.extract_from(file_name)
-      unit_cell = crystal_symmetry.unit_cell()
-      space_group_info = crystal_symmetry.space_group_info()
-    elif (arg == "--force_symmetry"):
-      force_symmetry = 0001
-    elif (arg.startswith("--pickle=")):
-      pickle_file_name = arg.split("=", 1)[1]
-    elif (arg.startswith("--")):
-      print usage()
-      raise RuntimeError, "Unknown option: " + arg
-    else:
-      remaining_args.append(arg)
-  args = remaining_args
+  command_line = (iotbx_option_parser(
+    usage="iotbx.pdb.as_xray_structure [options] pdb_file ...",
+    description="Example: iotbx.pdb.as_xray_structure pdb1ab1.ent")
+    .enable_symmetry_comprehensive()
+    .option(None, "--force_symmetry",
+      action="store_true",
+      default=00000,
+      dest="force_symmetry",
+      help="symmetry on command line is stronger than symmetry found in files")
+    .option("-v", "--verbose",
+      action="store_true",
+      default=00000,
+      dest="verbose",
+      help="show scatterers")
+    .option(None, "--pickle",
+      action="store",
+      type="string",
+      dest="pickle",
+      help="write all data to FILE ('--pickle .' copies name of input file)",
+      metavar="FILE")
+  ).process()
+  if (len(command_line.args) == 0):
+    command_line.parser.show_help()
   all_structures = []
-  for file_name in args:
+  for file_name in command_line.args:
     print "file_name:", file_name
     sys.stdout.flush()
     structure = pdb.as_xray_structure(
-      file_name,
-      crystal_symmetry=crystal.symmetry(
-        unit_cell=unit_cell,
-        space_group_info=space_group_info),
-      force_symmetry=force_symmetry)
+      file_name=file_name,
+      crystal_symmetry=command_line.symmetry,
+      force_symmetry=command_line.options.force_symmetry)
     structure.show_summary()
+    if (command_line.options.verbose):
+      structure.show_scatterers()
     all_structures.append(structure)
     print
+  pickle_file_name = command_line.options.pickle
   if (pickle_file_name is not None and len(all_structures) > 0):
-    if (len(all_structures) == 1):
-      all_structures = all_structures[0]
+    if (pickle_file_name == "."):
+      if (len(command_line.args) > 1):
+        raise UserError(
+          "Ambiguous name for pickle file (more than one input file).")
+      pickle_file_name = os.path.basename(command_line.args[0])
     if (not pickle_file_name.lower().endswith(".pickle")):
       pickle_file_name += ".pickle"
+    if (len(all_structures) == 1):
+      all_structures = all_structures[0]
+    print
     print "Writing all xray structures to file:", pickle_file_name
     easy_pickle.dump(pickle_file_name, all_structures)
     print
