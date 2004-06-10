f8c46d4f7b62b03ce39c406be75318db726b455d
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-06-10 06:12:37 +0000

    unused code removed

--- libtbx/libtbx/assoc_ftype.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-
-def set():
-  os.popen(r'assoc .px=PythonLibtbx', "r").read()
-  py_exe = os.environ["LIBTBX_PYTHON_EXE"]
-  os.popen(r'ftype PythonLibtbx="%s" "%%1" %%*' % py_exe, "r").read()
-
-if (__name__ == "__main__"):
-  set()
