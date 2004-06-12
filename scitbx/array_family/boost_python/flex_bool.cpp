79eafde34d6c97b2ac75dead78fea2c259baaad8
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-06-12 08:18:16 +0000

    flex.bool.iselection() for conversion from bool selection to size_t selection; inplace +-*/% for array,array arguments

--- scitbx/array_family/boost_python/flex_bool.cpp
+++ scitbx/array_family/boost_python/flex_bool.cpp
@@ -1,21 +1,36 @@
-/* Copyright (c) 2001-2002 The Regents of the University of California
-   through E.O. Lawrence Berkeley National Laboratory, subject to
-   approval by the U.S. Department of Energy.
-   See files COPYRIGHT.txt and LICENSE.txt for further details.
-
-   Revision history:
-     2002 Aug: Created (R.W. Grosse-Kunstleve)
- */
-
 #include <scitbx/array_family/boost_python/flex_wrapper.h>
 #include <scitbx/array_family/boost_python/flex_pickle_single_buffered.h>
+#include <boost/python/args.hpp>
+#include <boost/python/overloads.hpp>
 
 namespace scitbx { namespace af { namespace boost_python {
 
+namespace {
+
+  af::shared<std::size_t>
+  iselection(
+    af::const_ref<bool, flex_grid<> > const& a,
+    bool test_value=true)
+  {
+    af::shared<std::size_t> result;
+    for(std::size_t i=0;i<a.size();i++) {
+      if (a[i] == test_value) result.push_back(i);
+    }
+    return result;
+  }
+
+  BOOST_PYTHON_FUNCTION_OVERLOADS(iselection_overloads, iselection, 1, 2)
+
+} // namespace <anonymous>
+
   void wrap_flex_bool()
   {
+    typedef boost::python::arg arg_; // gcc 2.96 workaround
     flex_wrapper<bool>::logical("bool", boost::python::scope())
-      .def_pickle(flex_pickle_single_buffered<bool>());
+      .def_pickle(flex_pickle_single_buffered<bool>())
+      .def("iselection", iselection,
+        iselection_overloads((arg_("self"), arg_("test_value")=true)))
+    ;
   }
 
 }}} // namespace scitbx::af::boost_python
