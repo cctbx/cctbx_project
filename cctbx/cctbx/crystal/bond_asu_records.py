6393b0bc713ff3ab98a7dd5b45e12dfce19ac408
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-06-28 04:48:56 +0000

    cleanup

--- cctbx/cctbx/crystal/bond_asu_records.py
+++ cctbx/cctbx/crystal/bond_asu_records.py
@@ -1,139 +1,47 @@
 from iotbx.kriber import strudat
 import iotbx.pdb
 from iotbx.option_parser import iotbx_option_parser
-from cctbx import restraints
 from cctbx import crystal
-from cctbx.array_family import flex
-from scitbx.python_utils.misc import adopt_init_args
 import libtbx.itertbx
 import math
 import sys, os
 
-class j_sym_dist:
-
-  def __init__(self, j_sym, dist):
-    adopt_init_args(self, locals())
-
-def get_bond_asu_proxies(asu_mappings, distance_cutoff):
+def start_bond_table(structure, distance_cutoff, verbose=0):
+  asu_mappings = structure.asu_mappings(buffer_thickness=distance_cutoff)
+  bond_table = [{} for i_seq in xrange(structure.scatterers().size())]
   pair_generator = crystal.neighbors_fast_pair_generator(
     asu_mappings=asu_mappings,
     distance_cutoff=distance_cutoff,
     minimal=0001)
-  bond_dicts = [{} for i in xrange(asu_mappings.mappings().size())]
   for pair in pair_generator:
-    bond_dicts[pair.i_seq].setdefault(pair.j_seq, []).append(
-      j_sym_dist(pair.j_sym, pair.dist_sq**.5))
-  return bond_dicts
-
-class bond_sym_proxy:
-
-  def __init__(self, i_seqs, rt_mx, distance_ideal=0, weight=0):
-    adopt_init_args(self, locals())
-
-def setup_bond_sym_proxies(structure, distance_cutoff, tolerance=1.e-6,
-                           short_cut=00000, verbose=0):
-  if (0 or verbose):
-    print "distance_cutoff:", distance_cutoff
-  distance_cutoff_plus = distance_cutoff * (1 + tolerance)
-  asu_mappings = structure.asu_mappings(buffer_thickness=distance_cutoff_plus)
-  bond_dicts = get_bond_asu_proxies(
-    asu_mappings=asu_mappings, distance_cutoff=distance_cutoff_plus)
-  bond_sym_proxies = []
-  for i_seq,bond_dict in enumerate(bond_dicts):
-    rt_mx_i_inv = asu_mappings.get_rt_mx(i_seq=i_seq, i_sym=0).inverse()
-    so_i = asu_mappings.special_op(i_seq)
-    sm_i = asu_mappings.site_symmetry_table().get(i_seq).matrices()
-    for j_seq,j_sym_dists in bond_dict.items():
-      if (0 or verbose):
-        print "i_seq:", i_seq
-      rt_mx_jis = [rt_mx_i_inv.multiply(
-        asu_mappings.get_rt_mx(i_seq=j_seq, i_sym=jd.j_sym))
-          for jd in j_sym_dists]
-      so_j = asu_mappings.special_op(j_seq)
-      is_processed = [-1] * len(j_sym_dists)
-      for i in xrange(len(j_sym_dists)):
-        jd_i = j_sym_dists[i]
-        if (is_processed[i] >= 0): continue
-        if (0 or verbose):
-          print "  indep", i
-        bond_sym_proxies.append(bond_sym_proxy(
-          i_seqs=(i_seq, j_seq),
-          rt_mx=rt_mx_jis[i]))
-        for j in xrange(i+1,len(j_sym_dists)):
-          jd_j = j_sym_dists[j]
-          if (short_cut and abs(jd_i.dist - jd_j.dist) > 1.e-6): continue
-          # the case j_seq == i_seq is not handled correctly here
-          # see process_bond for correct treatment
-          if (j_seq == i_seq and rt_mx_jis[i].inverse_cancel()==rt_mx_jis[j]):
-            is_processed[j] = i
-            if (0 or verbose):
-              print "    symeq a:", j
-            assert abs(jd_i.dist - jd_j.dist) <= 1.e-6
-          else:
-            ri = rt_mx_jis[i]
-            rj = rt_mx_jis[j]
-            for mi in sm_i:
-              if (mi.multiply(rj).multiply(so_j) == ri.multiply(so_j)):
-                is_processed[j] = i
-                if (0 or verbose):
-                  print "    symeq b:", j
-                assert abs(jd_i.dist - jd_j.dist) <= 1.e-6
-                break
-      if (0 or verbose):
-        print "  is_processed:", is_processed
-  return bond_sym_proxies
-
-def get_max_bond_length(structure, bond_sym_proxies):
-  max_distance = 0
-  sites_frac = structure.scatterers().extract_sites()
-  unit_cell = structure.unit_cell()
-  for proxy in bond_sym_proxies:
-    max_distance = max(max_distance, unit_cell.distance(
-      sites_frac[proxy.i_seqs[0]], proxy.rt_mx * sites_frac[proxy.i_seqs[1]]))
-  return max_distance
-
-def expand_bond_sym_proxies(structure, bond_sym_proxies, distance_cutoff=0,
-                            tolerance=1.e-6, verbose=0):
-  bond_asu_records = [{} for i_seq in xrange(structure.scatterers().size())]
-  if (len(bond_sym_proxies) == 0):
-    return None, bond_asu_records
-  max_bond_length = get_max_bond_length(
-    structure=structure, bond_sym_proxies=bond_sym_proxies)
-  if (0 or verbose):
-    print "max_bond_length:", max_bond_length
-  assert max_bond_length > 0
-  distance_cutoff = max(distance_cutoff, max_bond_length)
-  if (0 or verbose):
-    print "distance_cutoff:", distance_cutoff
-  distance_cutoff_plus = distance_cutoff * (1 + tolerance)
-  asu_mappings = structure.asu_mappings(buffer_thickness=distance_cutoff_plus)
-  for sym_proxy in bond_sym_proxies:
-    i_seq, j_seq = sym_proxy.i_seqs
+    rt_mx_i = asu_mappings.get_rt_mx(i_seq=pair.i_seq, i_sym=0)
+    rt_mx_j = asu_mappings.get_rt_mx(i_seq=pair.j_seq, i_sym=pair.j_sym)
+    rt_mx_ji = rt_mx_i.inverse().multiply(rt_mx_j)
     is_new = process_bond(
       asu_mappings=asu_mappings,
-      bond_asu_records=bond_asu_records,
-      i_seq=i_seq,
-      j_seq=j_seq,
-      rt_mx_ji=sym_proxy.rt_mx,
+      bond_table=bond_table,
+      i_seq=pair.i_seq,
+      j_seq=pair.j_seq,
+      rt_mx_ji=rt_mx_ji,
       verbose=verbose)
-    if (is_new and i_seq != j_seq):
+    if (is_new and pair.i_seq != pair.j_seq):
       is_new = process_bond(
         asu_mappings=asu_mappings,
-        bond_asu_records=bond_asu_records,
-        i_seq=j_seq,
-        j_seq=i_seq,
-        rt_mx_ji=sym_proxy.rt_mx.inverse_cancel(),
+        bond_table=bond_table,
+        i_seq=pair.j_seq,
+        j_seq=pair.i_seq,
+        rt_mx_ji=rt_mx_ji.inverse_cancel(),
         verbose=verbose)
       assert is_new
-  return asu_mappings, bond_asu_records
+  return asu_mappings, bond_table
 
-def process_bond(asu_mappings, bond_asu_records,
+def process_bond(asu_mappings, bond_table,
                  i_seq, j_seq, rt_mx_ji, verbose=0):
   rt_mx_i = asu_mappings.get_rt_mx(i_seq=i_seq, i_sym=0)
   rt_mx_j = rt_mx_i.multiply(rt_mx_ji)
   j_sym = asu_mappings.find_i_sym(i_seq=j_seq, rt_mx=rt_mx_j)
   assert j_sym >= 0
-  j_sym_groups = bond_asu_records[i_seq].setdefault(j_seq, [])
+  j_sym_groups = bond_table[i_seq].setdefault(j_seq, [])
   for j_sym_group in j_sym_groups:
     if (j_sym in j_sym_group):
       return 00000
@@ -186,10 +94,10 @@ def is_sym_equiv_interaction_simple(unit_cell,
         return 00000
   return abs(d1-d2) < 1.e-3
 
-def check_sym_equiv(structure, asu_mappings, bond_asu_records, weak=00000):
+def check_sym_equiv(structure, asu_mappings, bond_table, weak=00000):
   unit_cell = structure.unit_cell()
   sites_frac = structure.scatterers().extract_sites()
-  for i_seq,records in enumerate(bond_asu_records):
+  for i_seq,records in enumerate(bond_table):
     rt_mx_i_inv = asu_mappings.get_rt_mx(i_seq, 0).inverse()
     for j_seq,j_sym_groups in records.items():
       i_group_rt_mx_jis = []
@@ -213,9 +121,9 @@ def check_sym_equiv(structure, asu_mappings, bond_asu_records, weak=00000):
           else:
             assert gi != gj
 
-def check_connectivities(bond_asu_records, connectivities, verbose=0):
+def check_connectivities(bond_table, connectivities, verbose=0):
   n_mismatches = 0
-  for records,connectivity in zip(bond_asu_records, connectivities):
+  for records,connectivity in zip(bond_table, connectivities):
     n = 0
     for j_seq,j_sym_groups in records.items():
       for j_sym_group in j_sym_groups:
@@ -230,26 +138,19 @@ def exercise(
       connectivities=None,
       weak_check_sym_equiv=00000,
       verbose=0):
-  bond_sym_proxies = setup_bond_sym_proxies(
+  if (0 or verbose):
+    print "distance_cutoff:", distance_cutoff
+  asu_mappings, bond_table = start_bond_table(
     structure=structure,
     distance_cutoff=distance_cutoff,
     verbose=verbose)
-  if (0 or verbose):
-    print "bond_sym_proxies.size():", len(bond_sym_proxies)
-  for dist_cutoff in [distance_cutoff, 0]:
-    asu_mappings, bond_asu_records = expand_bond_sym_proxies(
-      structure=structure,
-      bond_sym_proxies=bond_sym_proxies,
-      distance_cutoff=dist_cutoff,
-      verbose=verbose)
-    if (connectivities is not None):
-      check_connectivities(bond_asu_records, connectivities, verbose)
-    if (asu_mappings is not None):
-      check_sym_equiv(
-        structure=structure,
-        asu_mappings=asu_mappings,
-        bond_asu_records=bond_asu_records,
-        weak=weak_check_sym_equiv)
+  if (connectivities is not None):
+    check_connectivities(bond_table, connectivities, verbose)
+  check_sym_equiv(
+    structure=structure,
+    asu_mappings=asu_mappings,
+    bond_table=bond_table,
+    weak=weak_check_sym_equiv)
 
 def run():
   command_line = (iotbx_option_parser(
