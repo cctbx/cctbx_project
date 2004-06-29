02d42cf483e3b8204a598da0689d1f7056382dce
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-06-29 22:02:51 +0000

    imports rearranged to work around strange Python 2.2.1 assertion failures (in Python C code)

--- cctbx/cctbx/development/tst_cns_epsilon.py
+++ cctbx/cctbx/development/tst_cns_epsilon.py
@@ -1,9 +1,9 @@
-import sys, os
-from cctbx import crystal
+from iotbx.cns import reflection_reader
 from cctbx import miller
+from cctbx import crystal
 from cctbx.development import debug_utils
 from cctbx.development import make_cns_input
-from iotbx.cns import reflection_reader
+import sys, os
 
 def verify(crystal_symmetry, anomalous_flag, reflection_file):
   assert reflection_file.anomalous == anomalous_flag
