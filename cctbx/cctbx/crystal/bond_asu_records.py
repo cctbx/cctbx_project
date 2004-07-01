12ac6c8ae4dc3c0e1101d6198ee5e988b3014742
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-07-01 02:37:31 +0000

    standalone functions combined into new pair_asu_table class

--- cctbx/cctbx/crystal/bond_asu_records.py
+++ cctbx/cctbx/crystal/bond_asu_records.py
@@ -7,104 +7,103 @@ import libtbx.itertbx
 import math
 import sys, os
 
-def start_bond_table(asu_mappings, distance_cutoff, verbose=0):
-  bond_table = [{} for i_seq in xrange(asu_mappings.mappings().size())]
-  pair_generator = crystal.neighbors_fast_pair_generator(
-    asu_mappings=asu_mappings,
-    distance_cutoff=distance_cutoff,
-    minimal=0001)
-  for pair in pair_generator:
-    rt_mx_i = asu_mappings.get_rt_mx(i_seq=pair.i_seq, i_sym=0)
-    rt_mx_j = asu_mappings.get_rt_mx(i_seq=pair.j_seq, i_sym=pair.j_sym)
-    rt_mx_ji = rt_mx_i.inverse().multiply(rt_mx_j)
-    is_new = process_bond(
-      asu_mappings=asu_mappings,
-      bond_table=bond_table,
-      i_seq=pair.i_seq,
-      j_seq=pair.j_seq,
-      rt_mx_ji=rt_mx_ji,
-      verbose=verbose)
-    if (is_new and pair.i_seq != pair.j_seq):
-      is_new = process_bond(
-        asu_mappings=asu_mappings,
-        bond_table=bond_table,
-        i_seq=pair.j_seq,
-        j_seq=pair.i_seq,
-        rt_mx_ji=rt_mx_ji.inverse_cancel(),
-        verbose=verbose)
-      assert is_new
-  return bond_table
+class pair_sym_proxy:
 
-def restore_bond_table(asu_mappings, bond_sym_proxies, verbose=0):
-  bond_table = [{} for i_seq in xrange(asu_mappings.mappings().size())]
-  for proxy in bond_sym_proxies:
-    is_new = process_bond(
-      asu_mappings=asu_mappings,
-      bond_table=bond_table,
+  def __init__(self, i_seqs, rt_mx, distance_ideal=0, weight=0):
+    adopt_init_args(self, locals())
+
+class pair_asu_table:
+
+  def __init__(self, asu_mappings):
+    self.asu_mappings = asu_mappings
+    self.table = [{} for i_seq in xrange(self.asu_mappings.mappings().size())]
+
+  def add_all_pairs(self, distance_cutoff, verbose=0):
+    pair_generator = crystal.neighbors_fast_pair_generator(
+      asu_mappings=self.asu_mappings,
+      distance_cutoff=distance_cutoff,
+      minimal=0001)
+    for pair in pair_generator:
+      rt_mx_i = self.asu_mappings.get_rt_mx(i_seq=pair.i_seq, i_sym=0)
+      rt_mx_j = self.asu_mappings.get_rt_mx(i_seq=pair.j_seq, i_sym=pair.j_sym)
+      rt_mx_ji = rt_mx_i.inverse().multiply(rt_mx_j)
+      is_new = self._process_bond(
+        i_seq=pair.i_seq,
+        j_seq=pair.j_seq,
+        rt_mx_ji=rt_mx_ji,
+        verbose=verbose)
+      if (is_new and pair.i_seq != pair.j_seq):
+        is_new = self._process_bond(
+          i_seq=pair.j_seq,
+          j_seq=pair.i_seq,
+          rt_mx_ji=rt_mx_ji.inverse_cancel(),
+          verbose=verbose)
+        assert is_new
+    return self
+
+  def add_pair_sym_proxies(self, proxies, verbose=0):
+    for proxy in proxies:
+      self.add_pair_sym_proxy(proxy=proxy, verbose=verbose)
+    return self
+
+  def add_pair_sym_proxy(self, proxy, verbose=0):
+    is_new = self._process_bond(
       i_seq=proxy.i_seqs[0],
       j_seq=proxy.i_seqs[1],
       rt_mx_ji=proxy.rt_mx,
       verbose=verbose)
     if (is_new and proxy.i_seqs[0] != proxy.i_seqs[1]):
-      is_new = process_bond(
-        asu_mappings=asu_mappings,
-        bond_table=bond_table,
+      is_new = self._process_bond(
         i_seq=proxy.i_seqs[1],
         j_seq=proxy.i_seqs[0],
         rt_mx_ji=proxy.rt_mx.inverse_cancel(),
         verbose=verbose)
       assert is_new
-  return bond_table
+    return self
 
-def process_bond(asu_mappings, bond_table,
-                 i_seq, j_seq, rt_mx_ji, verbose=0):
-  rt_mx_i = asu_mappings.get_rt_mx(i_seq=i_seq, i_sym=0)
-  rt_mx_j = rt_mx_i.multiply(rt_mx_ji)
-  j_sym = asu_mappings.find_i_sym(i_seq=j_seq, rt_mx=rt_mx_j)
-  assert j_sym >= 0
-  j_sym_groups = bond_table[i_seq].setdefault(j_seq, [])
-  for j_sym_group in j_sym_groups:
-    if (j_sym in j_sym_group):
-      return 00000
-  j_syms = [j_sym]
-  j_sym_groups.append(j_syms)
-  if (0 or verbose):
-    print "primary:     i_seq, j_seq, j_sym", i_seq, j_seq, j_sym
-  for mi in asu_mappings.site_symmetry_table().get(i_seq).matrices():
-    if (i_seq == j_seq):
-      rt_mx_j_eq = rt_mx_i.multiply(rt_mx_ji.multiply(mi).inverse_cancel())
-      j_sym_eq = asu_mappings.find_i_sym(i_seq=j_seq, rt_mx=rt_mx_j_eq)
+  def _process_bond(self, i_seq, j_seq, rt_mx_ji, verbose=0):
+    rt_mx_i = self.asu_mappings.get_rt_mx(i_seq=i_seq, i_sym=0)
+    rt_mx_j = rt_mx_i.multiply(rt_mx_ji)
+    j_sym = self.asu_mappings.find_i_sym(i_seq=j_seq, rt_mx=rt_mx_j)
+    assert j_sym >= 0
+    j_sym_groups = self.table[i_seq].setdefault(j_seq, [])
+    for j_sym_group in j_sym_groups:
+      if (j_sym in j_sym_group):
+        return 00000
+    j_syms = [j_sym]
+    j_sym_groups.append(j_syms)
+    if (0 or verbose):
+      print "primary:     i_seq, j_seq, j_sym", i_seq, j_seq, j_sym
+    for mi in self.asu_mappings.site_symmetry_table().get(i_seq).matrices():
+      if (i_seq == j_seq):
+        rt_mx_j_eq = rt_mx_i.multiply(rt_mx_ji.multiply(mi).inverse_cancel())
+        j_sym_eq = self.asu_mappings.find_i_sym(i_seq=j_seq, rt_mx=rt_mx_j_eq)
+        assert j_sym_eq >= 0
+        if (not j_sym_eq in j_syms):
+          j_syms.append(j_sym_eq)
+          if (0 or verbose):
+            print "    equiv a: i_seq, j_seq, j_sym", i_seq, j_seq, j_sym_eq
+      rt_mx_j_eq = rt_mx_i.multiply(mi.multiply(rt_mx_ji))
+      j_sym_eq = self.asu_mappings.find_i_sym(i_seq=j_seq, rt_mx=rt_mx_j_eq)
       assert j_sym_eq >= 0
       if (not j_sym_eq in j_syms):
         j_syms.append(j_sym_eq)
         if (0 or verbose):
-          print "    equiv a: i_seq, j_seq, j_sym", i_seq, j_seq, j_sym_eq
-    rt_mx_j_eq = rt_mx_i.multiply(mi.multiply(rt_mx_ji))
-    j_sym_eq = asu_mappings.find_i_sym(i_seq=j_seq, rt_mx=rt_mx_j_eq)
-    assert j_sym_eq >= 0
-    if (not j_sym_eq in j_syms):
-      j_syms.append(j_sym_eq)
-      if (0 or verbose):
-        print "    equiv b: i_seq, j_seq, j_sym", i_seq, j_seq, j_sym_eq
-  return 0001
+          print "    equiv b: i_seq, j_seq, j_sym", i_seq, j_seq, j_sym_eq
+    return 0001
 
-class bond_sym_proxy:
-
-  def __init__(self, i_seqs, rt_mx, distance_ideal=0, weight=0):
-    adopt_init_args(self, locals())
-
-def extract_bond_sym_proxies(asu_mappings, bond_table):
-  bond_sym_proxies = []
-  for i_seq,j_seq_dict in enumerate(bond_table):
-    rt_mx_i_inv = asu_mappings.get_rt_mx(i_seq=i_seq, i_sym=0).inverse()
-    for j_seq,j_sym_groups in j_seq_dict.items():
-      for j_sym_group in j_sym_groups:
-        j_sym = j_sym_group[0]
-        rt_mx_j = asu_mappings.get_rt_mx(i_seq=j_seq, i_sym=j_sym)
-        bond_sym_proxies.append(bond_sym_proxy(
-          i_seqs=(i_seq, j_seq),
-          rt_mx=rt_mx_i_inv.multiply(rt_mx_j)))
-  return bond_sym_proxies
+  def extract_pair_sym_proxies(self):
+    pair_sym_proxies = []
+    for i_seq,j_seq_dict in enumerate(self.table):
+      rt_mx_i_inv = self.asu_mappings.get_rt_mx(i_seq=i_seq, i_sym=0).inverse()
+      for j_seq,j_sym_groups in j_seq_dict.items():
+        for j_sym_group in j_sym_groups:
+          j_sym = j_sym_group[0]
+          rt_mx_j = self.asu_mappings.get_rt_mx(i_seq=j_seq, i_sym=j_sym)
+          pair_sym_proxies.append(pair_sym_proxy(
+            i_seqs=(i_seq, j_seq),
+            rt_mx=rt_mx_i_inv.multiply(rt_mx_j)))
+    return pair_sym_proxies
 
 def is_sym_equiv_interaction_simple(unit_cell,
                                     i_seq,
@@ -133,10 +132,11 @@ def is_sym_equiv_interaction_simple(unit_cell,
         return 00000
   return abs(d1-d2) < 1.e-3
 
-def check_sym_equiv(structure, asu_mappings, bond_table, weak=00000):
+def check_sym_equiv(structure, bond_asu_table, weak=00000):
   unit_cell = structure.unit_cell()
+  asu_mappings = bond_asu_table.asu_mappings
   sites_frac = structure.scatterers().extract_sites()
-  for i_seq,records in enumerate(bond_table):
+  for i_seq,records in enumerate(bond_asu_table.table):
     rt_mx_i_inv = asu_mappings.get_rt_mx(i_seq, 0).inverse()
     for j_seq,j_sym_groups in records.items():
       i_group_rt_mx_jis = []
@@ -160,9 +160,9 @@ def check_sym_equiv(structure, asu_mappings, bond_table, weak=00000):
           else:
             assert gi != gj
 
-def check_connectivities(bond_table, connectivities, verbose=0):
+def check_connectivities(bond_asu_table, connectivities, verbose=0):
   n_mismatches = 0
-  for records,connectivity in zip(bond_table, connectivities):
+  for records,connectivity in zip(bond_asu_table.table, connectivities):
     n = 0
     for j_seq,j_sym_groups in records.items():
       for j_sym_group in j_sym_groups:
@@ -182,22 +182,21 @@ def exercise(
   asu_mappings = structure.asu_mappings(buffer_thickness=distance_cutoff)
   for i_pass in xrange(2):
     if (i_pass == 0):
-      bond_table = start_bond_table(
-        asu_mappings=asu_mappings,
+      bond_asu_table = pair_asu_table(asu_mappings=asu_mappings)
+      bond_asu_table.add_all_pairs(
         distance_cutoff=distance_cutoff,
         verbose=verbose)
     else:
-      bond_sym_proxies = extract_bond_sym_proxies(asu_mappings, bond_table)
-      bond_table = restore_bond_table(
-        asu_mappings=asu_mappings,
-        bond_sym_proxies=bond_sym_proxies,
+      bond_sym_proxies = bond_asu_table.extract_pair_sym_proxies()
+      bond_asu_table = pair_asu_table(asu_mappings=asu_mappings)
+      bond_asu_table.add_pair_sym_proxies(
+        proxies=bond_sym_proxies,
         verbose=verbose)
     if (connectivities is not None):
-      check_connectivities(bond_table, connectivities, verbose)
+      check_connectivities(bond_asu_table, connectivities, verbose)
     check_sym_equiv(
       structure=structure,
-      asu_mappings=asu_mappings,
-      bond_table=bond_table,
+      bond_asu_table=bond_asu_table,
       weak=weak_check_sym_equiv)
 
 def run():
