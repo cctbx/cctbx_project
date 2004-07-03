79afdf230896c0104155a4e3bce3989c7fab059b
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-07-03 12:45:30 +0000

    workaround for MIPSpro type_id::name() problem

--- cctbx/include/cctbx/restraints/bond_tables.h
+++ cctbx/include/cctbx/restraints/bond_tables.h
@@ -7,7 +7,7 @@
 namespace cctbx { namespace restraints {
 
   typedef sgtbx::rt_mx bond_sym_op;
-  typedef std::vector<bond_sym_op> bond_sym_ops;
+  typedef std::vector<sgtbx::rt_mx> bond_sym_ops;
   typedef std::map<unsigned, bond_sym_ops> bond_sym_dict;
   typedef af::shared<bond_sym_dict> bond_sym_table;
 
