d74403f0a633699dd5e0a13c480fd8ad51a9e02c
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-07-03 12:04:12 +0000

    new bond_sym_table, bond_sym_dict, bond_sym_ops, bond_sym_op

---
+++ cctbx/include/cctbx/restraints/bond_tables.h
@@ -0,0 +1,16 @@
+#ifndef CCTBX_RESTRAINTS_BOND_TABLES_H
+#define CCTBX_RESTRAINTS_BOND_TABLES_H
+
+#include <cctbx/restraints/bond.h>
+#include <map>
+
+namespace cctbx { namespace restraints {
+
+  typedef sgtbx::rt_mx bond_sym_op;
+  typedef std::vector<bond_sym_op> bond_sym_ops;
+  typedef std::map<unsigned, bond_sym_ops> bond_sym_dict;
+  typedef af::shared<bond_sym_dict> bond_sym_table;
+
+}} // namespace cctbx::restraints
+
+#endif // CCTBX_RESTRAINTS_BOND_TABLES_H
--- cctbx/include/cctbx/restraints/restraints_ext.cpp
+++ cctbx/include/cctbx/restraints/restraints_ext.cpp
@@ -8,6 +8,7 @@ namespace cctbx { namespace restraints { namespace boost_python {
   void wrap_dihedral();
   void wrap_chirality();
   void wrap_planarity();
+  void wrap_bond_tables();
   void wrap_bonded_interactions();
 
 namespace {
@@ -20,6 +21,7 @@ namespace {
     wrap_dihedral();
     wrap_chirality();
     wrap_planarity();
+    wrap_bond_tables();
     wrap_bonded_interactions();
   }
 
