b7eedb322a7a2488dfa64e34d94abd19a83e0d6a
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-07-05 23:41:38 +0000

    aliases to support drop-in replacement of Python lists: append -> insert, extend -> insert

--- scitbx/include/scitbx/stl/tst_set.py
+++ scitbx/include/scitbx/stl/tst_set.py
@@ -10,7 +10,7 @@ def exercise_unsigned():
   assert s.size() == 1
   assert len(s) == 1
   assert 1 in s
-  s.insert(1)
+  s.append(1)
   assert s.size() == 1
   s.insert(3)
   assert s.size() == 2
@@ -25,7 +25,7 @@ def exercise_unsigned():
   assert list(s) == [3,4,5,7,8]
   s.insert(set.unsigned(s))
   assert s.size() == 5
-  s.insert([0,4,6,11])
+  s.extend([0,4,6,11])
   assert list(s) == [0,3,4,5,6,7,8,11]
   d = pickle.dumps(s)
   l = pickle.loads(d)
