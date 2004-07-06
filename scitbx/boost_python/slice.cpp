d87d6092b1f82d26b3d52ef696c515c61127afda
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-07-06 06:23:33 +0000

    code rearrangements to work around RedHat 7.3 gcc 2.96 optimizer bug; what made the difference in the end was to switch from extract<int> to extract<long>; other changes preserved because they present slight improvements

--- scitbx/boost_python/slice.cpp
+++ scitbx/boost_python/slice.cpp
@@ -1,67 +1,7 @@
-/* Copyright (c) 2001-2002 The Regents of the University of California
-   through E.O. Lawrence Berkeley National Laboratory, subject to
-   approval by the U.S. Department of Energy.
-   See files COPYRIGHT.txt and LICENSE.txt for further details.
-
-   Revision history:
-     2002 Dec: Created (R.W. Grosse-Kunstleve)
- */
-
 #include <scitbx/boost_python/slice.h>
-#include <scitbx/boost_python/utils.h>
 #include <scitbx/error.h>
 
 namespace scitbx { namespace boost_python {
-
-  slice_item
-  extract_slice_item(boost::python::object const& obj)
-  {
-    slice_item result;
-    if (obj.ptr() == Py_None) {
-      result.is_valid = false;
-      result.value = 0;
-    }
-    else {
-      result.is_valid = true;
-      boost::python::extract<int> proxy(obj);
-      result.value = proxy();
-    }
-    return result;
-  }
-
-  slice_from_python::slice_from_python()
-  {
-    boost::python::converter::registry::push_back(
-      &convertible,
-      &construct,
-      boost::python::type_id<slice>());
-  }
-
-  void*
-  slice_from_python::convertible(PyObject* obj_ptr)
-  {
-    if (!PySlice_Check(obj_ptr)) return 0;
-    return obj_ptr;
-  }
-
-  void
-  slice_from_python::construct(
-    PyObject* obj_ptr,
-    boost::python::converter::rvalue_from_python_stage1_data* data)
-  {
-    void* storage = (
-      (boost::python::converter::rvalue_from_python_storage<slice>*)
-        data)->storage.bytes;
-    new (storage) slice();
-    data->convertible = storage;
-    slice& result = *((slice*)storage);
-    boost::python::object slice_obj = boost::python::object(
-      boost::python::handle<>(
-        boost::python::borrowed(obj_ptr)));
-    result.start = extract_slice_item(slice_obj.attr("start"));
-    result.stop = extract_slice_item(slice_obj.attr("stop"));
-    result.step = extract_slice_item(slice_obj.attr("step"));
-  }
 
   adapted_slice::adapted_slice(slice const& sl, std::size_t sz)
   :
--- scitbx/include/scitbx/boost_python/slice.h
+++ scitbx/include/scitbx/boost_python/slice.h
@@ -1,43 +1,19 @@
-/* Copyright (c) 2001-2002 The Regents of the University of California
-   through E.O. Lawrence Berkeley National Laboratory, subject to
-   approval by the U.S. Department of Energy.
-   See files COPYRIGHT.txt and LICENSE.txt for further details.
-
-   Revision history:
-     2002 Dec: Created (R.W. Grosse-Kunstleve)
- */
-
 #ifndef SCITBX_BOOST_PYTHON_SLICE_H
 #define SCITBX_BOOST_PYTHON_SLICE_H
 
-#include <boost/python/object.hpp>
-#include <boost/python/extract.hpp>
+#include <cstddef>
 
 namespace scitbx { namespace boost_python {
 
   struct slice_item
   {
     bool is_valid;
-    int value;
+    long value;
   };
-
-  slice_item
-  extract_slice_item(boost::python::object const& obj);
 
   struct slice
   {
     slice_item start, stop, step;
-  };
-
-  struct slice_from_python
-  {
-    slice_from_python();
-
-    static void* convertible(PyObject* obj_ptr);
-
-    static void construct(
-      PyObject* obj_ptr,
-      boost::python::converter::rvalue_from_python_stage1_data* data);
   };
 
   struct adapted_slice
