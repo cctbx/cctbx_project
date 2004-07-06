45edeaec77e66c38991087f91d184e8ce611c812
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-07-06 06:09:57 +0000

    scitbx.stl.map.stl_string_stl_map_stl_string_double (to support cctbx.restraints.repulsion_difference_table)

--- cctbx/cctbx/restraints.py
+++ cctbx/cctbx/restraints.py
@@ -7,6 +7,7 @@ ext = boost.python.import_ext("cctbx_restraints_ext")
 from cctbx_restraints_ext import *
 
 import cctbx.sgtbx
+import scitbx.stl.map
 import scitbx.stl.set
 import scitbx.stl.vector
 
@@ -14,3 +15,8 @@ pair_sym_ops = cctbx.sgtbx.stl_vector_rt_mx
 
 pair_asu_j_sym_groups = scitbx.stl.vector.set_unsigned
 pair_asu_j_sym_group = scitbx.stl.set.unsigned
+
+repulsion_radius_table = scitbx.stl.map.stl_string_double
+
+repulsion_distance_table = scitbx.stl.map.stl_string_stl_map_stl_string_double
+repulsion_distance_dict = scitbx.stl.map.stl_string_double
