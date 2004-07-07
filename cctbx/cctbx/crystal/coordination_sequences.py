259fae4ba845f2fab3752a8a11d4b88987b597aa
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-07-07 09:09:57 +0000

    coordination sequence code from old distance_ls.py file reintegrated and cleaned up

---
+++ cctbx/cctbx/crystal/coordination_sequences.py
@@ -0,0 +1,88 @@
+from cctbx import sgtbx
+from cctbx.array_family import flex
+
+class node:
+
+  def __init__(self, asu_mappings, i_seq, rt_mx):
+    self.i_seq = i_seq
+    self.rt_mx = rt_mx
+    self.rt_mx_unique = str(rt_mx.multiply(asu_mappings.special_op(i_seq)))
+
+def find_node(test_node, node_list):
+  for list_node in node_list:
+    if (    list_node.i_seq == test_node.i_seq
+        and list_node.rt_mx_unique == test_node.rt_mx_unique):
+      return 0001
+  return 00000
+
+def simple_and_slow(pair_asu_table, n_shells=10):
+  asu_mappings = pair_asu_table.asu_mappings
+  term_table = []
+  for i_seq_pivot,pair_asu_dict_pivot in enumerate(pair_asu_table.table):
+    rt_mx_pivot = asu_mappings.get_rt_mx(i_seq=i_seq_pivot, i_sym=0)
+    if (pair_asu_dict_pivot.size() == 0):
+      term_table.append([])
+      continue
+    nodes_middle = []
+    nodes_next = [node(
+      asu_mappings=asu_mappings,
+      i_seq=i_seq_pivot,
+      rt_mx=sgtbx.rt_mx())]
+    terms = [1]
+    for i_shell_minus_1 in xrange(n_shells):
+      nodes_prev = nodes_middle
+      nodes_middle = nodes_next
+      nodes_next = []
+      for node_m in nodes_middle:
+        rt_mx_i = asu_mappings.get_rt_mx(i_seq=node_m.i_seq, i_sym=0)
+        rt_mx_ni = node_m.rt_mx.multiply(rt_mx_i.inverse())
+        for j_seq,j_sym_groups in pair_asu_table.table[node_m.i_seq].items():
+          for j_sym_group in j_sym_groups:
+            for j_sym in j_sym_group:
+              rt_mx_j = asu_mappings.get_rt_mx(i_seq=j_seq, i_sym=j_sym)
+              new_node = node(
+                asu_mappings=asu_mappings,
+                i_seq=j_seq,
+                rt_mx=rt_mx_ni.multiply(rt_mx_j))
+              if (    not find_node(test_node=new_node, node_list=nodes_prev)
+                  and not find_node(test_node=new_node, node_list=nodes_middle)
+                  and not find_node(test_node=new_node, node_list=nodes_next)):
+                nodes_next.append(new_node)
+      terms.append(len(nodes_next))
+    term_table.append(terms)
+  return term_table
+
+def get_kriber_coseq_file(file_name):
+  result = {}
+  for line in open(file_name):
+    flds = line.split()
+    tag = flds[0]
+    terms = [int(f) for f in flds[1:]]
+    result.setdefault(tag, []).append(terms)
+  return result
+
+def show_terms(structure, term_table, coseq_dict=None):
+  assert len(term_table) == structure.scatterers().size()
+  for scatterer,terms in zip(structure.scatterers(), term_table):
+    print scatterer.label, list(terms),
+    if (coseq_dict is not None):
+      terms_to_match = terms[1:]
+      have_match = 00000
+      tags = coseq_dict.keys()
+      tags.sort()
+      for tag in tags:
+        for coseq_terms in coseq_dict[tag]:
+          n = min(len(coseq_terms), len(terms_to_match))
+          if (coseq_terms[:n] == terms_to_match[:n]):
+            print tag,
+            have_match = 0001
+      if (not have_match):
+        print "Unknown",
+    print
+  sums_terms = flex.double()
+  multiplicities = flex.double()
+  for scatterer,terms in zip(structure.scatterers(), term_table):
+    sums_terms.append(flex.sum(flex.size_t(terms)))
+    multiplicities.append(scatterer.multiplicity())
+  print "TD%d: %.2f" % (
+    len(terms)-1, flex.mean_weighted(sums_terms, multiplicities))
--- iotbx/iotbx/command_line/show_distances.py
+++ iotbx/iotbx/command_line/show_distances.py
@@ -1,5 +1,6 @@
-import cctbx.crystal.pair_asu_table
+from cctbx.crystal import coordination_sequences
 from cctbx.crystal import distance_ls
+import cctbx.crystal.pair_asu_table
 from iotbx.kriber import strudat
 from iotbx.option_parser import iotbx_option_parser
 import sys
@@ -20,10 +21,29 @@ def run():
       dest="distance_cutoff",
       help="Maximum distance to be considered",
       metavar="FLOAT")
+    .option(None, "--cs",
+      action="store",
+      type="int",
+      dest="cs",
+      help="Compute N terms of the coordination sequences",
+      metavar="N")
+    .option(None, "--coseq",
+      action="store",
+      type="string",
+      dest="coseq",
+      help="name of file with known coordination sequences",
+      metavar="FILE")
   ).process()
   if (len(command_line.args) == 0):
     command_line.parser.show_help()
     return
+  n_shells = command_line.options.cs
+  if (command_line.options.coseq is not None):
+    coseq_dict = coordination_sequences.get_kriber_coseq_file(
+      file_name=command_line.options.coseq)
+    if (n_shells is None): n_shells = 10
+  else:
+    coseq_dict = None
   for file_name in command_line.args:
     strudat_entries = strudat.read_all_entries(open(file_name))
     for entry in strudat_entries.entries:
@@ -45,6 +65,17 @@ def run():
         structure=structure,
         pair_asu_table=pair_asu_table)
       print
+      print "Pair counts:", list(pairs.pair_counts)
+      print
+      if (n_shells is not None):
+        term_table = coordination_sequences.simple_and_slow(
+          pair_asu_table=pair_asu_table,
+          n_shells=n_shells)
+        coordination_sequences.show_terms(
+          structure=structure,
+          term_table=term_table,
+          coseq_dict=coseq_dict)
+        print
 
 if (__name__ == "__main__"):
   run()
