26ed16ab4620934833fe5de26e3f4dcbcb739834
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-07-09 23:23:51 +0000

    coordination_sequences_simple now uses pair_asu_table as parameter (instead of asu_mappings, pair_asu_table_table)

--- iotbx/iotbx/command_line/show_distances.py
+++ iotbx/iotbx/command_line/show_distances.py
@@ -1,6 +1,5 @@
 from cctbx.crystal import coordination_sequences
 from cctbx.crystal import distance_ls
-import cctbx.crystal.pair_asu_table
 from cctbx import crystal
 from iotbx.kriber import strudat
 from iotbx.option_parser import iotbx_option_parser
@@ -58,7 +57,7 @@ def run():
       print
       asu_mappings = structure.asu_mappings(
         buffer_thickness=command_line.options.distance_cutoff)
-      pair_asu_table = cctbx.crystal.pair_asu_table.pair_asu_table(
+      pair_asu_table = crystal.pair_asu_table(
         asu_mappings=asu_mappings)
       pair_asu_table.add_all_pairs(
         distance_cutoff=command_line.options.distance_cutoff)
@@ -70,8 +69,7 @@ def run():
       print
       if (n_shells is not None):
         term_table = crystal.coordination_sequences_simple(
-          asu_mappings=pair_asu_table.asu_mappings(),
-          pair_asu_table_table=pair_asu_table.table(),
+          pair_asu_table=pair_asu_table,
           n_shells=n_shells)
         coordination_sequences.show_terms(
           structure=structure,
