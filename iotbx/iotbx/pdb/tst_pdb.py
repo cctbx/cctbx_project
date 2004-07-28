b538aaecad77d2e2d27827db4cd7e1247f491e13
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-07-28 22:34:31 +0000

    parsing of LINK records

--- iotbx/iotbx/pdb/tst_pdb.py
+++ iotbx/iotbx/pdb/tst_pdb.py
@@ -3,6 +3,7 @@ from cctbx import crystal
 from cctbx import sgtbx
 from cctbx.development import random_structure
 from cctbx.array_family import flex
+import libtbx.itertbx
 from cStringIO import StringIO
 import sys
 
@@ -27,6 +28,21 @@ def exercise_format_records():
   assert iotbx.pdb.format_atom_record() \
       == "ATOM      0  C   DUM     1       0.000   0.000   0.000" \
         +"  1.00  0.00              "
+
+def exercise_parser():
+  for i,raw_record in enumerate("""\
+LINK         O1  DDA     1                 C3  DDL     2
+LINK        MN    MN   391                 OE2 GLU   217            2565
+""".splitlines()):
+    r = iotbx.pdb.parser.pdb_record(raw_record=raw_record)
+    _1 = [r.name1,r.altLoc1,r.resName1,r.chainID1,r.resSeq1,r.iCode1,r.sym1]
+    _2 = [r.name2,r.altLoc2,r.resName2,r.chainID2,r.resSeq2,r.iCode2,r.sym2]
+    if (i == 0):
+      assert _1 == [' O1 ', ' ', 'DDA', ' ', '   1', ' ', '      ']
+      assert _2 == [' C3 ', ' ', 'DDL', ' ', '   2', ' ', '      ']
+    else:
+      assert _1 == ['MN  ', ' ', ' MN', ' ', ' 391', ' ', '      ']
+      assert _2 == [' OE2', ' ', 'GLU', ' ', ' 217', ' ', '  2565']
 
 def exercise_xray_structure(anisotropic_flag, verbose=0):
   structure = random_structure.xray_structure(
@@ -62,6 +78,7 @@ def exercise_xray_structure(anisotropic_flag, verbose=0):
 def run():
   verbose = "--Verbose" in sys.argv[1:]
   exercise_format_records()
+  exercise_parser()
   for anisotropic_flag in (00000, 0001):
     exercise_xray_structure(anisotropic_flag, verbose=verbose)
   print "OK"
