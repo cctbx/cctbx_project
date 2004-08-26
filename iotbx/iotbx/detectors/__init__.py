8f18e62debd02595864485b7c9847690eaff5520
Nicholas Sauter <nksauter@lbl.gov>
2004-08-26 16:37:43 +0000

    better error reporting

--- iotbx/iotbx/detectors/__init__.py
+++ iotbx/iotbx/detectors/__init__.py
@@ -12,6 +12,7 @@ from iotbx.detectors.marIP import MARIPImage
 class ImageException(exceptions.Exception):
   def __init__(self,string):
     self.message = string
+  def __str__(self): return self.message
 
 all_image_types = [ADSCImage,MARImage,MARIPImage]
 
