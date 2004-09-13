75c20f87b16a676da4a933944725cad599107602
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-09-13 18:18:04 +0000

    new cctbx.geometry_restraints.bond_sorted_asu_proxies.show_sorted_by_residual

--- cctbx/cctbx/geometry_restraints/__init__.py
+++ cctbx/cctbx/geometry_restraints/__init__.py
@@ -10,6 +10,7 @@ from cctbx_geometry_restraints_ext import *
 
 import scitbx.stl.map
 from stdlib import math
+import sys
 
 nonbonded_radius_table = scitbx.stl.map.stl_string_double
 
@@ -176,3 +177,89 @@ class planarity_proxy_registry:
       else:
         self.counts[i_list] += 1
     return result
+
+class _bond_sorted_asu_proxies(boost.python.injector, bond_sorted_asu_proxies):
+
+  def show_sorted_by_residual(self,
+        sites_cart,
+        labels=None,
+        f=None,
+        prefix="",
+        max_lines=None):
+    assert labels is None or len(labels) == sites_cart.size()
+    if (self.n_total() == 0): return
+    if (f is None): f = sys.stdout
+    residuals = bond_residuals(
+      sites_cart=sites_cart,
+      sorted_asu_proxies=self)
+    i_proxies_sorted = flex.sort_permutation(data=residuals, reverse=True)
+    if (max_lines is not None and i_proxies_sorted.size() > max_lines+1):
+      i_proxies_sorted = i_proxies_sorted[:max_lines]
+    n_asu = 0
+    n_simple = self.simple.size()
+    if (labels is not None):
+      max_label_lengths = [0,0]
+      for i_proxy in i_proxies_sorted:
+        if (i_proxy < n_simple):
+          i_seqs = self.simple[i_proxy].i_seqs
+        else:
+          proxy = self.asu[i_proxy-n_simple]
+          i_seqs = (proxy.i_seq, proxy.j_seq)
+          n_asu += 1
+        max_label_lengths = [max(m, len(labels[i_seq]))
+          for m,i_seq in zip(max_label_lengths, i_seqs)]
+      if (max(max_label_lengths) == 0):
+        labels = None
+      else:
+        label_label_format = \
+          " - ".join(["%%-%ds" % m for m in max_label_lengths]) + " "
+        atom_i_atom_j_format = label_label_format.replace("-", "", 1)
+    if (self.asu.size() == 0):
+      asu_mappings = None
+    else:
+      asu_mappings = self.asu_mappings()
+    show_legend = True
+    atom_i_atom_j = ""
+    label_label = ""
+    for i_proxy in i_proxies_sorted:
+      if (i_proxy < n_simple):
+        proxy = self.simple[i_proxy]
+        i_seq,j_seq = proxy.i_seqs
+        rt_mx = None
+        restraint = bond(
+          sites_cart=sites_cart,
+          proxy=proxy)
+      else:
+        proxy = self.asu[i_proxy-n_simple]
+        i_seq,j_seq = proxy.i_seq,proxy.j_seq
+        rt_mx = asu_mappings.get_rt_mx_i(pair=proxy).inverse().multiply(
+                asu_mappings.get_rt_mx_j(pair=proxy))
+        restraint = bond(
+          sites_cart=sites_cart,
+          asu_mappings=asu_mappings,
+          proxy=proxy)
+      if (show_legend):
+        show_legend = False
+        if (labels is not None):
+          if (min(max_label_lengths) >= 6):
+            atom_i_atom_j = atom_i_atom_j_format % ("atom i", "atom j")
+          elif (min(max_label_lengths) >= 1):
+            atom_i_atom_j = atom_i_atom_j_format % ("i", "j")
+        print >> f, "%sBonded interactions sorted by residual:" % prefix
+        print >> f, "%s%sideal  model  delta   weight residual" % (
+          prefix, atom_i_atom_j),
+        if (n_asu > 0):
+          print >> f, "sym.op. j",
+        print >> f
+      if (labels is not None):
+        label_label = label_label_format % (labels[i_seq], labels[j_seq])
+      print >> f, "%s%s%5.3f %6.3f %6.3f %6.2e %6.2e" % (
+        prefix, label_label,
+        restraint.distance_ideal, restraint.distance_model, restraint.delta,
+        restraint.weight, restraint.residual()),
+      if (rt_mx is not None):
+        print >> f, rt_mx,
+      print >> f
+    n_not_shown = residuals.size() - i_proxies_sorted.size()
+    if (n_not_shown != 0):
+      print >> f, prefix + "... (remaining %d not shown)" % n_not_shown
