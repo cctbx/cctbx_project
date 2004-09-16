b50e69b3caa9b75f1b010ae83c9c6d567702a8cb
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-09-16 13:32:13 +0000

    new flex.extract_double_attributes()

--- scitbx/array_family/boost_python/flex_double.cpp
+++ scitbx/array_family/boost_python/flex_double.cpp
@@ -1,22 +1,65 @@
-/* Copyright (c) 2001-2002 The Regents of the University of California
-   through E.O. Lawrence Berkeley National Laboratory, subject to
-   approval by the U.S. Department of Energy.
-   See files COPYRIGHT.txt and LICENSE.txt for further details.
-
-   Revision history:
-     2002 Aug: Created (R.W. Grosse-Kunstleve)
- */
-
 #include <scitbx/array_family/boost_python/flex_wrapper.h>
 #include <scitbx/array_family/boost_python/flex_pickle_single_buffered.h>
+#include <boost/python/args.hpp>
 
-namespace scitbx { namespace af { namespace boost_python {
+namespace scitbx { namespace af {
+
+namespace {
+
+  af::shared<double>
+  extract_double_attributes(
+    boost::python::object array,
+    const char* attribute_name,
+    boost::python::object none_substitute)
+  {
+    PyObject* array_ptr = array.ptr();
+    char* attr_name = const_cast<char*>(attribute_name);
+    PyObject* none_substitute_ptr = none_substitute.ptr();
+    if (PyList_Check(array_ptr)) {
+      std::size_t len_array = PyList_GET_SIZE(array_ptr);
+      af::shared<double> result((reserve(len_array)));
+      for(std::size_t i=0;i<len_array;i++) {
+        PyObject* elem = PyList_GET_ITEM(array_ptr, i);
+        PyObject* elem_attr = PyObject_GetAttrString(elem, attr_name);
+        if (!elem_attr) boost::python::throw_error_already_set();
+        if (elem_attr == Py_None) elem_attr = none_substitute_ptr;
+        double value = PyFloat_AsDouble(elem_attr);
+        if (PyErr_Occurred()) boost::python::throw_error_already_set();
+        result.push_back(value);
+      }
+      return result;
+    }
+    if (PyTuple_Check(array_ptr)) {
+      std::size_t len_array = PyTuple_GET_SIZE(array_ptr);
+      af::shared<double> result((reserve(len_array)));
+      for(std::size_t i=0;i<len_array;i++) {
+        PyObject* elem = PyTuple_GET_ITEM(array_ptr, i);
+        PyObject* elem_attr = PyObject_GetAttrString(elem, attr_name);
+        if (!elem_attr) boost::python::throw_error_already_set();
+        if (elem_attr == Py_None) elem_attr = none_substitute_ptr;
+        double value = PyFloat_AsDouble(elem_attr);
+        if (PyErr_Occurred()) boost::python::throw_error_already_set();
+        result.push_back(value);
+      }
+      return result;
+    }
+    throw error("array must be a Python list or tuple.");
+  }
+
+} // namespace <anonymous>
+
+namespace boost_python {
 
   void wrap_flex_double()
   {
-    flex_wrapper<double>::numeric("double", boost::python::scope())
+    using namespace boost::python;
+
+    flex_wrapper<double>::numeric("double", scope())
       .def_pickle(flex_pickle_single_buffered<double>())
     ;
+
+    def("extract_double_attributes", extract_double_attributes,
+      (arg_("array"), arg_("attribute_name"), arg_("none_substitute")));
   }
 
 }}} // namespace scitbx::af::boost_python
