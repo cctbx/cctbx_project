661f59dc5b324bdf87e7805a515237c62c6b693e
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-09-17 14:51:07 +0000

    strict_conflict_handling option and informative exceptions for all restraint types

--- cctbx/cctbx/geometry_restraints/__init__.py
+++ cctbx/cctbx/geometry_restraints/__init__.py
@@ -24,31 +24,83 @@ def angle_delta_deg(angle_1, angle_2, periodicity=1):
   elif (d >  half_period): d -= 2*half_period
   return d
 
-class proxy_registry_add_result:
+class proxy_registry_process_result:
 
-  def __init__(self, tabulated_proxy=None, is_new=False, is_conflicting=False):
+  def __init__(self,
+        tabulated_proxy=None,
+        is_new=False,
+        is_conflicting=False,
+        conflict_source_labels=None):
     adopt_init_args(self, locals())
 
-class bond_simple_proxy_registry:
+class proxy_registry_base:
 
-  def __init__(self, n_seq):
-    self.n_seq = n_seq
-    self.proxies = shared_bond_simple_proxy()
+  def __init__(self, proxies, strict_conflict_handling):
+    self.proxies = proxies
+    self.strict_conflict_handling = strict_conflict_handling
+    self.n_resolved_conflicts = 0
     self.counts = flex.size_t()
-    self.table = None
+    self.discard_table()
 
   def initialize_table(self):
+    self.table = {}
+    self.source_labels = flex.std_string()
+    self.source_n_expected_atoms = flex.int()
+
+  def discard_table(self):
+    self.table = None
+    self.source_labels = None
+    self.source_n_expected_atoms = None
+
+  def _append_proxy(self, source_info, proxy, process_result):
+    self.proxies.append(proxy)
+    self.counts.append(1)
+    self.source_labels.append(source_info.labels())
+    self.source_n_expected_atoms.append(source_info.n_expected_atoms())
+    process_result.tabulated_proxy = proxy
+    process_result.is_new = True
+
+  def _handle_conflict(self,
+        source_info,
+        proxy,
+        i_list,
+        process_result):
+    source_n_expected_atoms = source_info.n_expected_atoms()
+    if (self.strict_conflict_handling
+        or self.source_n_expected_atoms[i_list]
+           == source_n_expected_atoms):
+      process_result.is_conflicting = True
+      process_result.conflict_source_labels = (self.source_labels[i_list],
+                                               source_info.labels())
+    else:
+      self.n_resolved_conflicts += 1
+      if (self.source_n_expected_atoms[i_list] < source_n_expected_atoms):
+        self.proxies[i_list] = proxy
+        self.source_labels[i_list].append(source_info.labels())
+        self.source_n_expected_atoms[i_list] = source_n_expected_atoms
+        process_result.tabulated_proxy = proxy
+
+class bond_simple_proxy_registry(proxy_registry_base):
+
+  def __init__(self, n_seq, strict_conflict_handling):
+    proxy_registry_base.__init__(self,
+      proxies=shared_bond_simple_proxy(),
+      strict_conflict_handling=strict_conflict_handling)
+    self.n_seq = n_seq
+
+  def initialize_table(self):
+    proxy_registry_base.initialize_table(self)
     self.table = [{} for i in xrange(self.n_seq)]
 
-  def process(self, proxy, tolerance=1.e-6):
-    result = proxy_registry_add_result()
+  def process(self, source_info, proxy, tolerance=1.e-6):
+    result = proxy_registry_process_result()
     proxy = proxy.sort_i_seqs()
     if (not self.table[proxy.i_seqs[0]].has_key(proxy.i_seqs[1])):
       self.table[proxy.i_seqs[0]][proxy.i_seqs[1]] = self.proxies.size()
-      self.proxies.append(proxy)
-      self.counts.append(1)
-      result.tabulated_proxy = proxy
-      result.is_new = True
+      self._append_proxy(
+        source_info=source_info,
+        proxy=proxy,
+        process_result=result)
     else:
       i_list = self.table[proxy.i_seqs[0]][proxy.i_seqs[1]]
       result.tabulated_proxy = self.proxies[i_list]
@@ -56,32 +108,33 @@ class bond_simple_proxy_registry:
                                 - proxy.distance_ideal) > tolerance
           or abs(result.tabulated_proxy.weight - proxy.weight)
                > tolerance):
-        result.is_conflicting = True
-      else:
+        self._handle_conflict(
+          source_info=source_info,
+          proxy=proxy,
+          i_list=i_list,
+          process_result=result)
+      if (not result.is_conflicting):
         self.counts[i_list] += 1
     return result
 
-class angle_proxy_registry:
+class angle_proxy_registry(proxy_registry_base):
 
-  def __init__(self):
-    self.proxies = shared_angle_proxy()
-    self.counts = flex.size_t()
-    self.table = None
+  def __init__(self, strict_conflict_handling):
+    proxy_registry_base.__init__(self,
+      proxies=shared_angle_proxy(),
+      strict_conflict_handling=strict_conflict_handling)
 
-  def initialize_table(self):
-    self.table = {}
-
-  def process(self, proxy, tolerance=1.e-6):
-    result = proxy_registry_add_result()
+  def process(self, source_info, proxy, tolerance=1.e-6):
+    result = proxy_registry_process_result()
     proxy = proxy.sort_i_seqs()
     tab_i_seq_1 = self.table.setdefault(proxy.i_seqs[1], {})
     i_seqs_0_2 = (proxy.i_seqs[0], proxy.i_seqs[2])
     if (not tab_i_seq_1.has_key(i_seqs_0_2)):
       tab_i_seq_1[i_seqs_0_2] = self.proxies.size()
-      self.proxies.append(proxy)
-      self.counts.append(1)
-      result.tabulated_proxy = proxy
-      result.is_new = True
+      self._append_proxy(
+        source_info=source_info,
+        proxy=proxy,
+        process_result=result)
     else:
       i_list = tab_i_seq_1[i_seqs_0_2]
       result.tabulated_proxy = self.proxies[i_list]
@@ -89,32 +142,33 @@ class angle_proxy_registry:
                                  proxy.angle_ideal)) > tolerance
           or abs(result.tabulated_proxy.weight - proxy.weight)
                > tolerance):
-        result.is_conflicting = True
-      else:
+        self._handle_conflict(
+          source_info=source_info,
+          proxy=proxy,
+          i_list=i_list,
+          process_result=result)
+      if (not result.is_conflicting):
         self.counts[i_list] += 1
     return result
 
-class dihedral_proxy_registry:
+class dihedral_proxy_registry(proxy_registry_base):
 
-  def __init__(self):
-    self.proxies = shared_dihedral_proxy()
-    self.counts = flex.size_t()
-    self.table = None
+  def __init__(self, strict_conflict_handling):
+    proxy_registry_base.__init__(self,
+      proxies=shared_dihedral_proxy(),
+      strict_conflict_handling=strict_conflict_handling)
 
-  def initialize_table(self):
-    self.table = {}
-
-  def process(self, proxy, tolerance=1.e-6):
-    result = proxy_registry_add_result()
+  def process(self, source_info, proxy, tolerance=1.e-6):
+    result = proxy_registry_process_result()
     proxy = proxy.sort_i_seqs()
     tab_i_seq_0 = self.table.setdefault(proxy.i_seqs[0], {})
     i_seqs_1_2_3 = (proxy.i_seqs[1], proxy.i_seqs[2], proxy.i_seqs[3])
     if (not tab_i_seq_0.has_key(i_seqs_1_2_3)):
       tab_i_seq_0[i_seqs_1_2_3] = self.proxies.size()
-      self.proxies.append(proxy)
-      self.counts.append(1)
-      result.tabulated_proxy = proxy
-      result.is_new = True
+      self._append_proxy(
+        source_info=source_info,
+        proxy=proxy,
+        process_result=result)
     else:
       i_list = tab_i_seq_0[i_seqs_1_2_3]
       result.tabulated_proxy = self.proxies[i_list]
@@ -124,32 +178,33 @@ class dihedral_proxy_registry:
           or abs(result.tabulated_proxy.weight - proxy.weight)
                > tolerance
           or result.tabulated_proxy.periodicity != proxy.periodicity):
-        result.is_conflicting = True
-      else:
+        self._handle_conflict(
+          source_info=source_info,
+          proxy=proxy,
+          i_list=i_list,
+          process_result=result)
+      if (not result.is_conflicting):
         self.counts[i_list] += 1
     return result
 
-class chirality_proxy_registry:
+class chirality_proxy_registry(proxy_registry_base):
 
-  def __init__(self):
-    self.proxies = shared_chirality_proxy()
-    self.counts = flex.size_t()
-    self.table = None
+  def __init__(self, strict_conflict_handling):
+    proxy_registry_base.__init__(self,
+      proxies=shared_chirality_proxy(),
+      strict_conflict_handling=strict_conflict_handling)
 
-  def initialize_table(self):
-    self.table = {}
-
-  def process(self, proxy, tolerance=1.e-6):
-    result = proxy_registry_add_result()
+  def process(self, source_info, proxy, tolerance=1.e-6):
+    result = proxy_registry_process_result()
     proxy = proxy.sort_i_seqs()
     tab_i_seq_0 = self.table.setdefault(proxy.i_seqs[0], {})
     i_seqs_1_2_3 = (proxy.i_seqs[1], proxy.i_seqs[2], proxy.i_seqs[3])
     if (not tab_i_seq_0.has_key(i_seqs_1_2_3)):
       tab_i_seq_0[i_seqs_1_2_3] = self.proxies.size()
-      self.proxies.append(proxy)
-      self.counts.append(1)
-      result.tabulated_proxy = proxy
-      result.is_new = True
+      self._append_proxy(
+        source_info=source_info,
+        proxy=proxy,
+        process_result=result)
     else:
       i_list = tab_i_seq_0[i_seqs_1_2_3]
       result.tabulated_proxy = self.proxies[i_list]
@@ -157,40 +212,45 @@ class chirality_proxy_registry:
                > tolerance
           or abs(result.tabulated_proxy.weight - proxy.weight)
                > tolerance):
-        result.is_conflicting = True
-      else:
+        self._handle_conflict(
+          source_info=source_info,
+          proxy=proxy,
+          i_list=i_list,
+          process_result=result)
+      if (not result.is_conflicting):
         self.counts[i_list] += 1
     return result
 
-class planarity_proxy_registry:
+class planarity_proxy_registry(proxy_registry_base):
 
-  def __init__(self):
-    self.proxies = shared_planarity_proxy()
-    self.counts = flex.size_t()
-    self.table = None
+  def __init__(self, strict_conflict_handling):
+    proxy_registry_base.__init__(self,
+      proxies=shared_planarity_proxy(),
+      strict_conflict_handling=strict_conflict_handling)
 
-  def initialize_table(self):
-    self.table = {}
-
-  def process(self, proxy, tolerance=1.e-6):
+  def process(self, source_info, proxy, tolerance=1.e-6):
     assert proxy.i_seqs.size() > 0
-    result = proxy_registry_add_result()
+    result = proxy_registry_process_result()
     proxy = proxy.sort_i_seqs()
     tab_i_seq_0 = self.table.setdefault(proxy.i_seqs[0], {})
     i_seqs_1_up = tuple(proxy.i_seqs[1:])
     if (not tab_i_seq_0.has_key(i_seqs_1_up)):
       tab_i_seq_0[i_seqs_1_up] = self.proxies.size()
-      self.proxies.append(proxy)
-      self.counts.append(1)
-      result.tabulated_proxy = proxy
-      result.is_new = True
+      self._append_proxy(
+        source_info=source_info,
+        proxy=proxy,
+        process_result=result)
     else:
       i_list = tab_i_seq_0[i_seqs_1_up]
       result.tabulated_proxy = self.proxies[i_list]
       if (not approx_equal(result.tabulated_proxy.weights, proxy.weights,
                            eps=tolerance)):
-        result.is_conflicting = True
-      else:
+        self._handle_conflict(
+          source_info=source_info,
+          proxy=proxy,
+          i_list=i_list,
+          process_result=result)
+      if (not result.is_conflicting):
         self.counts[i_list] += 1
     return result
 
