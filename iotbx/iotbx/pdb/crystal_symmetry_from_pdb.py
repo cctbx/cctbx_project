a9f52d836f9fded7b9c96e72edb680113b8cf0db
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-09-22 00:17:34 +0000

    also look at CNS REMARK sg= records

--- iotbx/iotbx/pdb/crystal_symmetry_from_pdb.py
+++ iotbx/iotbx/pdb/crystal_symmetry_from_pdb.py
@@ -1,5 +1,6 @@
 from iotbx.pdb import cryst1_interpretation
 from iotbx.misc import detect_binary_file
+from iotbx.cns import pdb_remarks as cns_pdb_remarks
 
 def extract_from(file_name=None, file=None, monitor_initial=None):
   assert [file_name, file].count(None) == 1
@@ -14,8 +15,12 @@ def extract_from(file_name=None, file=None, monitor_initial=None):
       if (is_binary is not None):
         if (is_binary): break
         detect_binary = None
-    if (not line.startswith("CRYST1")): continue
-    return cryst1_interpretation.crystal_symmetry(
-      cryst1_record=line,
-      line_number=line_number)
+    if (line.startswith("CRYST1")):
+      return cryst1_interpretation.crystal_symmetry(
+        cryst1_record=line,
+        line_number=line_number)
+    crystal_symmetry = cns_pdb_remarks.extract_symmetry(
+      pdb_record=line)
+    if (crystal_symmetry is not None):
+      return crystal_symmetry
   raise RuntimeError, "No CRYST1 record."
