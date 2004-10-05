063b2659e25ac124b00dad494b426c621c506895
Pavel Afonine <afonine@users.noreply.github.com>
2004-10-05 16:51:47 +0000

    rigid-bond test, final...

--- cctbx/include/cctbx/adp_restraints/rigid_bond.cpp
+++ cctbx/include/cctbx/adp_restraints/rigid_bond.cpp
@@ -13,14 +13,17 @@ rigid_bond_pair::rigid_bond_pair(vec3<double> const& site1,
                                  sym_mat3<double> const& ustar1,
                                  sym_mat3<double> const& ustar2,
                                  cctbx::uctbx::unit_cell const& uc)
+/* Hirshfeld's Rigid Bond Test (1: Acta Cryst. (1976). A32, 239,
+                                2: SHELX manual)
+*/
 {
     sym_mat3<double> g = uc.metrical_matrix();
     vec3<double> l_12 = site1 - site2;
     vec3<double> l_21 = site2 - site1;
     double bond_length_sq = l_12 * g * l_12;
-    z_12_ = (g * l_12) * ustar1 * (g * l_12);
-    z_21_ = (g * l_21) * ustar2 * (g * l_21);
-    delta_z_ = (z_12_ - z_21_) / bond_length_sq;
+    z_12_ = (g * l_12) * ustar1 * (g * l_12) / bond_length_sq;
+    z_21_ = (g * l_21) * ustar2 * (g * l_21) / bond_length_sq;
+    delta_z_ = std::abs(z_12_ - z_21_);
 }
 
 }} // namespace cctbx::apd_restraints
