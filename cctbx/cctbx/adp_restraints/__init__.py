913a836d5d64cd4c9a891f41bec57e718e85dc59
Pavel Afonine <pafonine@users.noreply.github.com>
2004-10-05 16:55:09 +0000

    rbt

---
+++ cctbx/cctbx/adp_restraints/__init__.py
@@ -0,0 +1,5 @@
+import cctbx.array_family.flex
+import boost.python
+ext = boost.python.import_ext("cctbx_adp_restraints_ext")
+from cctbx_adp_restraints_ext import *
+import cctbx_adp_restraints_ext as ext
