24b011aa2542de1a2d2b1cc7d68d1df01db8bde4
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-10-10 13:29:48 +0000

    contains_one_of() moved from iotbx

--- scitbx/scitbx/python_utils/str_utils.py
+++ scitbx/scitbx/python_utils/str_utils.py
@@ -1,2 +1,8 @@
 def overwrite_at(s, offset, replacement):
   return s[:offset] + replacement + s[offset+len(replacement):]
+
+def contains_one_of(label, patterns):
+  for pattern in patterns:
+    if (label.find(pattern) >= 0):
+      return True
+  return False
