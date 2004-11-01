3f306f192b33be9a88e5d38726a680a37fbcca3f
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-11-01 19:58:01 +0000

    conversions from stl.vector.unsigned -> flex.size_t, vector.double -> flex.double

--- scitbx/array_family/boost_python/flex_size_t.cpp
+++ scitbx/array_family/boost_python/flex_size_t.cpp
@@ -1,8 +1,19 @@
 #include <scitbx/array_family/boost_python/flex_wrapper.h>
 #include <scitbx/array_family/boost_python/flex_pickle_single_buffered.h>
+#include <boost/python/make_constructor.hpp>
 
 namespace scitbx { namespace af { namespace boost_python {
 namespace {
+
+  flex<std::size_t>::type*
+  from_stl_vector_unsigned(std::vector<unsigned> const& v)
+  {
+    af::shared<std::size_t> result(af::reserve(v.size()));
+    for(std::size_t i=0;i<v.size();i++) {
+      result.push_back(v[i]);
+    }
+    return new flex<std::size_t>::type(result, result.size());
+  }
 
   bool
   next_permutation(af::ref<std::size_t> const& a)
@@ -26,8 +37,11 @@ namespace {
 
   void wrap_flex_size_t()
   {
+    using namespace boost::python;
     flex_wrapper<std::size_t>::integer("size_t", boost::python::scope())
       .def_pickle(flex_pickle_single_buffered<std::size_t>())
+      .def("__init__", make_constructor(
+        from_stl_vector_unsigned, default_call_policies()))
       .def("next_permutation", next_permutation)
       .def("inverse_permutation", inverse_permutation)
     ;
