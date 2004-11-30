3ad1bc3e8a48c802eddfc805051654555069e1a7
Nicholas Sauter <nksauter@lbl.gov>
2004-11-30 23:26:34 +0000

    Raxis format is now supported; the ImageFactory class automatically checks to see if the file can be opened in this format.

--- iotbx/iotbx/detectors/__init__.py
+++ iotbx/iotbx/detectors/__init__.py
@@ -8,13 +8,14 @@ import exceptions
 from iotbx.detectors.adsc import ADSCImage
 from iotbx.detectors.mar import MARImage
 from iotbx.detectors.marIP import MARIPImage
+from iotbx.detectors.raxis import RAXISImage
 
 class ImageException(exceptions.Exception):
   def __init__(self,string):
     self.message = string
   def __str__(self): return self.message
 
-all_image_types = [ADSCImage,MARImage,MARIPImage]
+all_image_types = [ADSCImage,MARImage,MARIPImage,RAXISImage]
 
 def ImageFactory(filename):
   for itype in all_image_types:
