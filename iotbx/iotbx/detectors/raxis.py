e338ebb810605157e911fcb2f57d5faa6630415e
Nicholas Sauter <nksauter@lbl.gov>
2004-11-30 23:26:34 +0000

    Raxis format is now supported; the ImageFactory class automatically checks to see if the file can be opened in this format.

---
+++ iotbx/iotbx/detectors/raxis.py
@@ -0,0 +1,60 @@
+from iotbx.detectors.detectorbase import DetectorImageBase
+from iotbx.detectors.raxisbase import Raxis
+from iotbx.detectors import ReadRAXIS
+
+class RAXISImage(DetectorImageBase,Raxis):
+  def __init__(self,filename):
+    DetectorImageBase.__init__(self,filename)
+    Raxis.__init__(self,filename)
+    self.vendortype = "RAXIS"
+
+  def readHeader(self,):
+    if not self.parameters:
+      Raxis.readHeader(self)
+
+      self.parameters={}
+      self.parameters['SIZE1'] = self.head['nSlow']
+      self.parameters['SIZE2'] = self.head['nFast']
+      self.parameters['CCD_IMAGE_SATURATION'] = 32767 * self.head['Ratio']
+      self.parameters['PIXEL_SIZE'] = self.head['sizeFast']
+      self.parameters['OSC_START'] = self.head['phistart']
+      self.parameters['OSC_RANGE'] = self.head['phiend']-self.head['phistart']
+      self.parameters['DISTANCE'] = self.head['distance']
+      self.parameters['WAVELENGTH'] = self.head['wavelength']
+      self.parameters['BEAM_CENTER_X'] = self.head['beampixels_x']*self.head[
+                                                              'sizeFast']
+      self.parameters['BEAM_CENTER_Y'] = self.head['beampixels_y']*self.head[
+                                                              'sizeSlow']
+      self.parameters['TWOTHETA'] = self.head['twotheta']
+      self.parameters['DETECTOR_SN'] = self.head['operatorname']
+
+  def dataoffset(self):
+    return self.head['record_length']
+
+  def integerdepth(self):
+    return self.head['record_length']/self.head['nFast']
+
+  def getEndian(self):
+    return 0
+
+  def read(self):
+    self.fileLength()
+    self.data()
+    self.linearintdata = ReadRAXIS(self.CharTemp,self.integerdepth(),
+         self.size1*self.bin,self.size2*self.bin,self.getEndian())
+    if self.bin==2:
+      from iotbx.detectors import Bin2_by_2
+      self.linearintdata = Bin2_by_2(self.linearintdata)
+
+if __name__=='__main__':
+  import sys
+  i = sys.argv[1]
+  a = RAXISImage(i)
+  a.read()
+  print a.linearintdata
+  print a.linearintdata.size()
+  print a.linearintdata.accessor()
+  #
+  #from labelit.dptbx.graphics_support import GenericImageWorker
+  #W = GenericImageWorker(i)
+  #W.output(sys.argv[2])
