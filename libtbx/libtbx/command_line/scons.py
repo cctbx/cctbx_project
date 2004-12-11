132d3a0b0e963eb78bebdd89a401476ccc36b9f7
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-12-11 01:12:50 +0000

    show_times

--- libtbx/libtbx/command_line/scons.py
+++ libtbx/libtbx/command_line/scons.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-
 import sys, os
 
 dist_root = os.environ["LIBTBX_DIST_ROOT"]
@@ -9,6 +7,21 @@ if (not os.path.isdir(engine)):
 sys.path.insert(0, engine)
 try: import SCons
 except: del sys.path[0]
+
+def show_times():
+  t = os.times()
+  usr_plus_sys = t[0] + t[1]
+  try: ticks = sys.gettickeraccumulation()
+  except: ticks = None
+  s = "usr+sys time: %.2f" % usr_plus_sys
+  if (ticks is not None):
+    s += ", ticks: %d" % ticks
+    if (ticks != 0):
+      s += ", micro-seconds/tick: %.3f" % (usr_plus_sys*1.e6/ticks)
+  print s
+
+import atexit
+atexit.register(show_times)
 
 from SCons import Script
 Script.main()
