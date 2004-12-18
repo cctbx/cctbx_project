3d1b93590099c9819ba3496d73d04f1931d5f1cb
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2004-12-18 10:36:48 +0000

    show correlations in sorted order; show binned correlations only for high overall cc; support quick searching; show lattice symmetry along with possible twin laws; new regression test

--- iotbx/iotbx/command_line/reflection_statistics.py
+++ iotbx/iotbx/command_line/reflection_statistics.py
@@ -2,6 +2,7 @@ from iotbx import reflection_file_reader
 from iotbx.option_parser import iotbx_option_parser
 from cctbx import crystal
 from cctbx import sgtbx
+from cctbx.array_family import flex
 from libtbx.itertbx import count
 import sys
 
@@ -16,7 +17,7 @@ class array_cache:
         .expand_to_p1() \
         .map_to_asu()
     if (self.input.anomalous_flag()):
-      self.anom_diffs = self.input.anomalous_differences().change_basis(
+      self.anom_diffs = abs(self.input.anomalous_differences()).change_basis(
         cb_op=self.change_of_basis_op_to_minimum_cell) \
           .expand_to_p1() \
           .map_to_asu()
@@ -28,6 +29,7 @@ class array_cache:
     self.patterson_group = self.minimum_cell_symmetry.space_group() \
       .build_derived_patterson_group()
     self.patterson_group.make_tidy()
+    self.resolution_range = self.input.resolution_range()
 
   def similarity_transformations(self,
         other,
@@ -50,21 +52,49 @@ class array_cache:
       else:
         for patterson_group in patterson_groups:
           expanded_group = sgtbx.space_group(patterson_group)
-          expanded_group.expand_smx(c_inv)
-          expanded_group.make_tidy()
-          def is_in_expanded_groups():
-            for g in expanded_groups:
-              if (g == expanded_group): return True
-            return False
-          if (not is_in_expanded_groups()):
-            expanded_groups.append(expanded_group)
+          try:
+            expanded_group.expand_smx(c_inv)
+          except:
             result.append(sgtbx.change_of_basis_op(c_inv).inverse())
+          else:
+            expanded_group.make_tidy()
+            def is_in_expanded_groups():
+              for g in expanded_groups:
+                if (g == expanded_group): return True
+              return False
+            if (not is_in_expanded_groups()):
+              expanded_groups.append(expanded_group)
+              result.append(sgtbx.change_of_basis_op(c_inv).inverse())
     return result
 
   def combined_cb_op(self, other, cb_op):
     s = self.change_of_basis_op_to_minimum_cell
     o = other.change_of_basis_op_to_minimum_cell
     return s.inverse() * cb_op.new_denominators(s) * o
+
+  def setup_common_binner(self,
+        other,
+        auto_binning=False,
+        reflections_per_bin=0,
+        n_bins=0):
+    d_max = min(self.resolution_range[0], other.resolution_range[0])
+    d_min = max(self.resolution_range[1], other.resolution_range[1])
+    if (d_max == d_min):
+      d_max += d_max*0.5
+      d_min -= d_min*0.5
+    self.observations.setup_binner(
+      d_max=d_max,
+      d_min=d_min,
+      auto_binning=auto_binning,
+      reflections_per_bin=reflections_per_bin,
+      n_bins=n_bins)
+    if (self.anom_diffs is not None and other.anom_diffs is not None):
+      self.anom_diffs.setup_binner(
+        d_max=d_max,
+        d_min=d_min,
+        auto_binning=auto_binning,
+        reflections_per_bin=reflections_per_bin,
+        n_bins=n_bins)
 
 def binned_correlation_fmt(correlation):
   return "%6.3f" % correlation.coefficient()
@@ -98,65 +128,88 @@ def run(args):
   for file_name in command_line.args:
     reflection_file = reflection_file_reader.any_reflection_file(
       file_name=file_name)
+    miller_arrays = None
     if (reflection_file.file_type() is not None):
       try:
         miller_arrays = reflection_file.as_miller_arrays(
           crystal_symmetry=command_line.symmetry)
       except:
-        print >> sys.stderr, "Warning: unknown file format:", file_name
-        print >> sys.stderr
-        sys.stderr.flush()
-        miller_array = None
-      if (miller_arrays is not None):
-        for miller_array in miller_arrays:
-          info = miller_array.info()
-          if (miller_array.is_xray_intensity_array()):
-            miller_array = miller_array.f_sq_as_f()
-          elif (miller_array.is_complex_array()):
-            miller_array = abs(miller_array)
-          if (miller_array.is_real_array()):
-            if (miller_array.unit_cell() is None):
-              print
-              print "*" * 79
-              print "Unknown unit cell parameters:", miller_array.info()
-              print "Use --symmetry or --unit_cell to define unit cell:"
-              print "*" * 79
-              print
-              command_line.parser.show_help()
-              return
-            if (miller_array.space_group_info() is None):
-              print
-              print "*" * 79
-              print "Unknown space group:", miller_array.info()
-              print "Use --symmetry or --space_group to define space group:"
-              print "*" * 79
-              print
-              command_line.parser.show_help()
-              return
-            if (   command_line.options.resolution is not None
-                or command_line.options.low_resolution is not None):
-              miller_array = miller_array.resolution_filter(
-                d_max=command_line.options.low_resolution,
-                d_min=command_line.options.resolution)
-            miller_array = miller_array.map_to_asu()
-            miller_array.set_info(info=info)
-            array_caches.append(array_cache(input=miller_array))
+        pass
+    if (miller_arrays is None):
+      print >> sys.stderr, "Warning: unknown file format:", file_name
+      print >> sys.stderr
+      sys.stderr.flush()
+    else:
+      for miller_array in miller_arrays:
+        info = miller_array.info()
+        miller_array = miller_array.select(
+          miller_array.indices() != (0,0,0))
+        if (miller_array.indices().size() == 0): continue
+        if (miller_array.is_xray_intensity_array()):
+          miller_array = miller_array.f_sq_as_f()
+        elif (miller_array.is_complex_array()):
+          miller_array = abs(miller_array)
+        if (miller_array.is_real_array()):
+          if (miller_array.unit_cell() is None):
+            print
+            print "*" * 79
+            print "Unknown unit cell parameters:", miller_array.info()
+            print "Use --symmetry or --unit_cell to define unit cell:"
+            print "*" * 79
+            print
+            command_line.parser.show_help()
+            return
+          if (miller_array.space_group_info() is None):
+            print
+            print "*" * 79
+            print "Unknown space group:", miller_array.info()
+            print "Use --symmetry or --space_group to define space group:"
+            print "*" * 79
+            print
+            command_line.parser.show_help()
+            return
+          if (   command_line.options.resolution is not None
+              or command_line.options.low_resolution is not None):
+            miller_array = miller_array.resolution_filter(
+              d_max=command_line.options.low_resolution,
+              d_min=command_line.options.resolution)
+          miller_array = miller_array.map_to_asu()
+          miller_array.set_info(info=info)
+          array_caches.append(array_cache(input=miller_array))
+  if (len(array_caches) > 2):
+    print "Array indices (for quick searching):"
+    for i_0,cache_0 in enumerate(array_caches):
+      print "  %2d:" % (i_0+1), cache_0.input.info()
+    print
+    print "Useful search patterns are:"
+    print "    Summary i"
+    print "    CC Obs i j"
+    print "    CC Ano i j"
+    print "  i and j are the indices shown above."
+    print
   n_bins = command_line.options.n_bins
   for i_0,cache_0 in enumerate(array_caches):
+    print "Summary", i_0+1
     cache_0.input.show_comprehensive_summary()
     print
-    reindexing_matrices = cache_0.input.reindexing_matrices()
-    print "Possible twin laws:",
-    if (len(reindexing_matrices) == 0):
-      print "None"
-    else:
+    reindexing_info = cache_0.input.reindexing_info(max_delta=0.1)
+    if (len(reindexing_info.matrices) == 0):
+      print "Possible twin laws: None"
       print
-      for c in reindexing_matrices:
+    else:
+      print "Space group of the metric:", \
+        reindexing_info.lattice_symmetry() \
+          .as_reference_setting() \
+          .space_group_info()
+      s = str(reindexing_info.idealized_unit_cell())
+      if (s != str(cache_0.input.unit_cell())):
+        print "Idealized unit cell:", s
+      print "Possible twin laws:"
+      for c in reindexing_info.matrices:
         print " ", c.r().as_hkl()
       print
     print "Completeness of %s:" % str(cache_0.input.info())
-    if (cache_0.input.binner() is None):
-      cache_0.input.setup_binner(n_bins=n_bins)
+    cache_0.input.setup_binner(n_bins=n_bins)
     cache_0.input.show_completeness_in_bins()
     print
     if (cache_0.input.anomalous_flag()):
@@ -166,65 +219,81 @@ def run(args):
       anom_signal.show(data_fmt="%.4f")
       print
     if (not command_line.options.quick):
-      for info_1 in array_caches[i_0+1:]:
+      for j_1,cache_1 in enumerate(array_caches[i_0+1:]):
+        i_1 = j_1+i_0+1
         similarity_transformations = cache_0.similarity_transformations(
-          other=info_1,
+          other=cache_1,
           relative_length_tolerance=0.05,
           absolute_angle_tolerance=5)
         if (len(similarity_transformations) == 0):
-          print "Incompatible symmetries:"
+          print "Incompatible unit cells:"
           print " ", cache_0.input.info()
-          print " ", info_1.input.info()
+          print " ", cache_1.input.info()
           print "No comparison."
           print
         else:
+          ccs = flex.double()
           for cb_op in similarity_transformations:
-            print "Correlation of:"
-            print " ", cache_0.input.info()
-            print " ", info_1.input.info()
-            similar_array_1 = info_1.observations \
+            similar_array_1 = cache_1.observations \
               .change_basis(cb_op) \
               .map_to_asu()
-            correlation = cache_0.observations.correlation(
+            ccs.append(cache_0.observations.correlation(
               other=similar_array_1,
-              assert_is_similar_symmetry=False)
-            combined_cb_op = cache_0.combined_cb_op(other=info_1, cb_op=cb_op)
+              assert_is_similar_symmetry=False).coefficient())
+          permutation = flex.sort_permutation(ccs, reverse=True)
+          ccs = ccs.select(permutation)
+          similarity_transformations = flex.select(
+            similarity_transformations, permutation=permutation)
+          for i_cb_op,cb_op,cc in zip(count(),
+                                      similarity_transformations,
+                                      ccs):
+            combined_cb_op = cache_0.combined_cb_op(other=cache_1, cb_op=cb_op)
             if (not combined_cb_op.c().is_unit_mx()):
               reindexing_note = " with reindexing"
               hkl_str = " "+combined_cb_op.as_hkl()
             else:
               reindexing_note = ""
               hkl_str = ""
+            print "CC Obs", i_0+1, i_1+1, "%6.3f"%cc, combined_cb_op.as_hkl()
+            print "Correlation of:"
+            print " ", cache_0.input.info()
+            print " ", cache_1.input.info()
             print "Overall correlation%s: %6.3f%s" % (
-              reindexing_note, correlation.coefficient(), hkl_str)
-            if (cache_0.observations.binner() is None):
-              cache_0.observations.setup_binner(n_bins=n_bins)
-            correlation = cache_0.observations.correlation(
-              other=similar_array_1,
-              use_binning=True,
-              assert_is_similar_symmetry=False)
-            correlation.show(data_fmt=binned_correlation_fmt)
+              reindexing_note, cc, hkl_str)
+            show_in_bins = False
+            if (i_cb_op == 0 or (cc >= 0.3 and cc >= ccs[0]-0.2)):
+              show_in_bins = True
+              similar_array_1 = cache_1.observations \
+                .change_basis(cb_op) \
+                .map_to_asu()
+              cache_0.setup_common_binner(cache_1, n_bins=n_bins)
+              correlation = cache_0.observations.correlation(
+                other=similar_array_1,
+                use_binning=True,
+                assert_is_similar_symmetry=False)
+              correlation.show(data_fmt=binned_correlation_fmt)
             print
             if (    cache_0.anom_diffs is not None
-                and info_1.anom_diffs is not None):
-              print "Anomalous difference correlation of:"
-              print " ", cache_0.input.info()
-              print " ", info_1.input.info()
-              similar_anom_diffs_1 = info_1.anom_diffs \
+                and cache_1.anom_diffs is not None):
+              similar_anom_diffs_1 = cache_1.anom_diffs \
                 .change_basis(cb_op) \
                 .map_to_asu()
               correlation = cache_0.anom_diffs.correlation(
                 other=similar_anom_diffs_1,
                 assert_is_similar_symmetry=False)
+              print "CC Ano", i_0+1, i_1+1, \
+                "%6.3f"%correlation.coefficient(), combined_cb_op.as_hkl()
+              print "Anomalous difference correlation of:"
+              print " ", cache_0.input.info()
+              print " ", cache_1.input.info()
               print "Overall correlation%s: %6.3f%s" % (
                 reindexing_note, correlation.coefficient(), hkl_str)
-              if (cache_0.anom_diffs.binner() is None):
-                cache_0.anom_diffs.setup_binner(n_bins=n_bins)
-              correlation = cache_0.anom_diffs.correlation(
-                other=similar_anom_diffs_1,
-                use_binning=True,
-                assert_is_similar_symmetry=False)
-              correlation.show(data_fmt=binned_correlation_fmt)
+              if (show_in_bins):
+                correlation = cache_0.anom_diffs.correlation(
+                  other=similar_anom_diffs_1,
+                  use_binning=True,
+                  assert_is_similar_symmetry=False)
+                correlation.show(data_fmt=binned_correlation_fmt)
               print
     print "=" * 79
     print
---
+++ iotbx/iotbx/regression/tst_reflection_statistics.py
@@ -0,0 +1,80 @@
+from iotbx import mtz
+from iotbx.command_line import reflection_statistics
+from cctbx import miller
+from cctbx import crystal
+from cctbx import sgtbx
+from cctbx.sgtbx import subgroups
+from cctbx.development import debug_utils
+from cctbx.array_family import flex
+from cStringIO import StringIO
+import sys
+
+def generate_mtz_files(space_group_info, anomalous_flag):
+  crystal_symmetry = crystal.symmetry(
+    unit_cell=space_group_info.any_compatible_unit_cell(volume=1000),
+    space_group_info=space_group_info)
+  miller_set = miller.build_set(
+    crystal_symmetry=crystal_symmetry,
+    anomalous_flag=anomalous_flag,
+    d_min=1)
+  miller_array = miller.array(
+    miller_set=miller_set,
+    data=flex.random_double(size=miller_set.indices().size()))
+  miller_array_p1 = miller_array.expand_to_p1()
+  file_names = []
+  subgrs = subgroups.subgroups(space_group_info).groups_parent_setting()
+  for i_subgroup, subgroup in enumerate(subgrs):
+    subgroup_miller_array = miller_array_p1.customized_copy(
+      space_group_info=sgtbx.space_group_info(group=subgroup)) \
+        .merge_equivalents() \
+        .array() \
+        .as_reference_setting() \
+        .set_observation_type_xray_intensity()
+    file_name = "tmp%d.mtz" % i_subgroup
+    mtz_object = subgroup_miller_array.as_mtz_dataset(
+      column_root_label="FOBS").mtz_object().write(file_name=file_name)
+    file_names.append(file_name)
+  return file_names
+
+def exercise_reflection_statistics(anomalous_flag, file_names, verbose):
+  out = StringIO()
+  try:
+    sys.stdout = out
+    reflection_statistics.run(args=file_names)
+  finally:
+    sys.stdout = sys.__stdout__
+    if (0 or verbose):
+      sys.stdout.write(out.getvalue())
+  done = {}
+  for line in out.getvalue().splitlines():
+    if (line.startswith("CC ")):
+      type,i,j,cc = line.split()[1:5]
+      key = " ".join([type,i,j])
+      cc = float(cc)
+      if (key not in done):
+        if (cc != 1.000):
+          raise AssertionError(line.strip())
+        done[key] = None
+  n = len(file_names)
+  expected_number_of_cc_lines = n*(n-1)/2
+  if (anomalous_flag):
+    expected_number_of_cc_lines *= 2
+  assert len(done) == expected_number_of_cc_lines
+
+def run_call_back(flags, space_group_info):
+  for anomalous_flag in [False, True]:
+    if (anomalous_flag and space_group_info.group().is_centric()): continue
+    file_names = generate_mtz_files(
+      space_group_info=space_group_info,
+      anomalous_flag=anomalous_flag)
+    exercise_reflection_statistics(
+      anomalous_flag=anomalous_flag,
+      file_names=file_names,
+      verbose=flags.Verbose)
+
+def run():
+  debug_utils.parse_options_loop_space_groups(sys.argv[1:], run_call_back)
+  print "OK"
+
+if (__name__ == "__main__"):
+  run()
