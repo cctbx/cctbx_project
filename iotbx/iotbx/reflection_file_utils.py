0a34ef23813b6ac32798f91160417dd41b2e4696
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-01-05 00:55:51 +0000

    new reflection_file_utils.reflection_file_server

--- iotbx/iotbx/reflection_file_utils.py
+++ iotbx/iotbx/reflection_file_utils.py
@@ -1,3 +1,7 @@
+from iotbx import reflection_file_reader
+from cctbx import miller
+from cctbx.array_family import flex
+import libtbx.path
 from libtbx.itertbx import count
 from libtbx.utils import UserError
 import sys, os
@@ -11,8 +15,10 @@ def find_labels(search_labels, candidate_labels):
 
 class label_table:
 
-  def __init__(self, miller_arrays):
+  def __init__(self, miller_arrays, err=None):
     self.miller_arrays = miller_arrays
+    if (err is None): self.err = sys.stderr
+    else: self.err = err
     self.labels = []
     for p_array,miller_array in zip(count(1), miller_arrays):
       if (miller_array.info() is not None):
@@ -20,74 +26,360 @@ class label_table:
       else:
         self.labels.append(str(p_array))
 
-  def show_possible_choices(self, f=None, scores=None, high_score=None):
-    if (f is None): f = sys.stdout
-    print >> f, "Possible choices:"
-    if (scores is None):
-      for p_array,lbl in zip(count(1), self.labels):
-        print >> f, "  %d:" % p_array, lbl
-    else:
-      for p_array,lbl,score in zip(count(1), self.labels, scores):
-        if (score == high_score):
-          print >> f, "  %d:" % p_array, lbl
-    print >> f
-    print >> f, "Please specify a number or an unambiguous substring of the",
-    print >> f, "target data label."
-    print >> f
-
-  def match_data_label(self,
-        label=None,
-        labels=None,
-        f=None,
-        command_line_switch=None,
-        parameter_name=None):
-    assert label is None or labels is None
-    assert label is not None or labels is not None
-    if (f is None): f = sys.stdout
-    assert command_line_switch is None or parameter_name is None
+  def scores(self, label=None, labels=None):
+    assert [label, labels].count(None) == 1
     if (labels is None):
       labels = [label]
     else:
       assert len(labels) > 0
       label = labels[0]
-    if (command_line_switch is None):
-      if (parameter_name is None):
-        label_identifier = "--label"
-      else:
-        label_identifier = parameter_name
-    else:
-      label_identifier = parameter_name
     if (len(labels) == 1):
       try: i = int(label)-1
-      except: pass
+      except (TypeError, ValueError): pass
       else:
         if (0 <= i < len(self.miller_arrays)):
-          return self.miller_arrays[i]
-    scores = []
+          result = [0]*len(self.miller_arrays)
+          result[i] = 2
+          return result
+    result = []
     labels_lower = [lbl.lower() for lbl in labels]
     for lbl in self.labels:
       if (not find_labels(
                search_labels=labels_lower, candidate_labels=[lbl.lower()])):
-        scores.append(0)
+        result.append(0)
       elif (not find_labels(search_labels=labels, candidate_labels=[lbl])):
-        scores.append(1)
+        result.append(1)
       else:
-        scores.append(2)
+        result.append(2)
+    return result
+
+  def show_possible_choices(self,
+        f=None,
+        scores=None,
+        minimum_score=None,
+        parameter_name=None):
+    if (f is None): f = self.err
+    print >> f, "Possible choices:"
+    if (scores is None):
+      for lbl in self.labels:
+        print >> f, " ", lbl
+    else:
+      for lbl,score in zip(self.labels, scores):
+        if (score >= minimum_score):
+          print >> f, " ", lbl
+    print >> f
+    if (parameter_name is None): hint = ""
+    else: hint = "use %s\nto " % parameter_name
+    print >> f, \
+      "Please %sspecify an unambiguous substring of the target label." % hint
+    print >> f
+
+  def match_data_label(self, label, command_line_switch, f=None):
+    if (f is None): f = self.err
+    scores = self.scores(label=label)
     selected_array = None
     for high_score in [2,1]:
       if (scores.count(high_score) > 0):
         if (scores.count(high_score) > 1):
           print >> f
-          print >> f, "Ambiguous %s=%s" % (label_identifier, " ".join(labels))
+          print >> f, "Ambiguous %s=%s" % (command_line_switch, label)
           print >> f
-          self.show_possible_choices(f=f, scores=scores, high_score=high_score)
+          self.show_possible_choices(
+            f=f, scores=scores, minimum_score=high_score)
           return None
         return self.miller_arrays[scores.index(high_score)]
     print >> f
-    print >> f, "Unknown %s=%s" % (label_identifier, " ".join(labels))
+    print >> f, "Unknown %s=%s" % (command_line_switch, label)
     print >> f
     self.show_possible_choices(f=f)
     return None
+
+def get_xray_data_scores(miller_arrays):
+  result = []
+  for miller_array in miller_arrays:
+    if (not miller_array.is_real_array()):
+      result.append(0)
+    else:
+      if (miller_array.is_xray_intensity_array()):
+        result.append(4)
+      elif (miller_array.is_xray_amplitude_array()):
+        if (miller_array.is_xray_reconstructed_amplitude_array()):
+          result.append(2)
+        else:
+          result.append(3)
+      elif (    isinstance(miller_array.data(), flex.double)
+            and isinstance(miller_array.sigmas(), flex.double)):
+        result.append(1)
+      else:
+        result.append(0)
+  return result
+
+def looks_like_r_free_flags_info(array_info):
+  if (not isinstance(array_info, miller.array_info)): return False
+  if (len(array_info.labels) != 1): return False
+  label = array_info.labels[0].lower()
+  for word in ["free", "test", "cross"]:
+    if (label.find(word) >= 0): return True
+  return False
+
+class get_r_free_flags_score:
+
+  def __init__(self, test_flag_value, n, n_free, miller_array_info):
+    if (test_flag_value is not None or n_free < n*0.50):
+      self.reversed = False
+    else:
+      self.reversed = True
+      n_free = n - n_free
+    self.flag_score = 0
+    if (n*0.01 < n_free < n*0.35):
+      if (   looks_like_r_free_flags_info(miller_array_info)
+          or n*0.05 < n_free < n*0.20):
+        self.flag_score = 2
+      else:
+        self.flag_score = 1
+
+class get_r_free_flags_scores:
+
+  def __init__(self, miller_arrays, test_flag_value):
+    self.scores = []
+    self.test_flag_values = []
+    for i_array,miller_array in enumerate(miller_arrays):
+      flag_score = 0
+      effective_test_flag_value = None
+      data = miller_array.data()
+      if (miller_array.is_bool_array()):
+        trial_test_flag_value = (
+          test_flag_value is None or bool(test_flag_value))
+        n_free = data.count(trial_test_flag_value)
+        scoring = get_r_free_flags_score(
+          test_flag_value=test_flag_value,
+          n=data.size(),
+          n_free=n_free,
+          miller_array_info=miller_array.info())
+        if (scoring.flag_score != 0):
+          flag_score = scoring.flag_score
+          if (scoring.reversed):
+            trial_test_flag_value = not trial_test_flag_value
+          effective_test_flag_value = trial_test_flag_value
+      elif (miller_array.is_integer_array()):
+        try: counts = data.counts(max_keys=200)
+        except RuntimeError: pass
+        else:
+          c_keys = counts.keys()
+          c_values = counts.values()
+          if (   test_flag_value is None
+              or test_flag_value in c_keys):
+            if (counts.size() == 2):
+              if (test_flag_value is None):
+                if (c_values[1] < c_values[0]):
+                  i_free = 1
+                else:
+                  i_free = 0
+              elif (test_flag_value == c_keys[0]):
+                i_free = 0
+              else:
+                i_free = 1
+              scoring = get_r_free_flags_score(
+                test_flag_value=test_flag_value,
+                n=data.size(),
+                n_free=c_values[i_free],
+                miller_array_info=miller_array.info())
+              if (scoring.flag_score != 0):
+                flag_score = scoring.flag_score
+                if (scoring.reversed): i_free = 1-i_free
+                effective_test_flag_value = c_keys[i_free]
+            elif (counts.size() > 3):
+              if (c_keys == range(min(c_keys), max(c_keys)+1)):
+                c_values = flex.size_t(c_values)
+                count_not_almost_full = (
+                  c_values < int(flex.max(c_values)*0.8)).count(True)
+                if (count_not_almost_full == 0):
+                  if (looks_like_r_free_flags_info(miller_array.info())):
+                    flag_score = 2
+                  else:
+                    flag_score = 1
+                  if (test_flag_value is None):
+                    if (miller_array.info().source_type.startswith("cns_")
+                        and 1 in counts):
+                      effective_test_flag_value = 1
+                    else:
+                      effective_test_flag_value = min(c_keys)
+                  else:
+                    effective_test_flag_value = test_flag_value
+      self.scores.append(flag_score)
+      self.test_flag_values.append(effective_test_flag_value)
+    assert len(self.scores) == len(miller_arrays)
+    assert len(self.test_flag_values) == len(miller_arrays)
+
+def get_experimental_phases_scores(miller_arrays):
+  result = []
+  for miller_array in miller_arrays:
+    if (miller_array.is_hendrickson_lattman_array()):
+      result.append(1)
+    else:
+      result.append(0)
+  return result
+
+def select_array(
+      parameter_name,
+      labels,
+      miller_arrays,
+      data_scores,
+      err,
+      error_message_no_array,
+      error_message_not_a_suitable_array,
+      error_message_multiple_equally_suitable):
+  if (labels is not None): assert parameter_name is not None
+  if (len(miller_arrays) == 0):
+    raise UserError("No reflection arrays available.")
+  if (data_scores is not None):
+    assert max(data_scores) >= 0
+  else:
+    data_scores = [1]*len(miller_arrays)
+  lbl_tab = label_table(miller_arrays=miller_arrays, err=err)
+  if (labels is None):
+    label_scores = None
+  else:
+    label_scores = lbl_tab.scores(labels=labels)
+  if (label_scores is not None and max(label_scores) == 0):
+    error = "No matching array: %s=%s" % (parameter_name, " ".join(labels))
+    print >> err, "\n" + error + "\n"
+    if (max(data_scores) > 0):
+      lbl_tab.show_possible_choices(
+        scores=data_scores,
+        minimum_score=1,
+        parameter_name=parameter_name)
+    raise UserError(error)
+  if (max(data_scores) == 0):
+    if (label_scores is None):
+      error = error_message_no_array
+    else:
+      error = "%s%s=%s" % (
+        error_message_not_a_suitable_array, parameter_name, " ".join(labels))
+    print >> err, "\n" + error + "\n"
+    raise UserError(error)
+  if (label_scores is None):
+    combined_scores = data_scores
+  else:
+    n = max(data_scores) + 1
+    combined_scores = []
+    for label_score,data_score in zip(label_scores, data_scores):
+      combined_scores.append(label_score*n+data_score)
+  i = combined_scores.index(max(combined_scores))
+  if (combined_scores.count(combined_scores[i]) > 1):
+    error = error_message_multiple_equally_suitable
+    print >> err, "\n" + error + "\n"
+    lbl_tab.show_possible_choices(
+      scores=combined_scores,
+      minimum_score=max(combined_scores),
+      parameter_name=parameter_name)
+    raise UserError(error)
+  return i
+
+class reflection_file_server:
+
+  def __init__(self,
+        crystal_symmetry=None,
+        force_symmetry=None,
+        reflection_files=None,
+        err=None):
+    self.crystal_symmetry = crystal_symmetry
+    self.force_symmetry = force_symmetry
+    if (err is None): self.err = sys.stderr
+    else: self.err = err
+    self.miller_arrays = []
+    for reflection_file in reflection_files:
+      self.miller_arrays.extend(reflection_file.as_miller_arrays(
+        crystal_symmetry=self.crystal_symmetry,
+        force_symmetry=self.force_symmetry))
+    self.file_name_miller_arrays = {}
+    for miller_array in self.miller_arrays:
+      self.file_name_miller_arrays.setdefault(
+        libtbx.path.canonical_path(
+          miller_array.info().source), []).append(miller_array)
+
+  def get_miller_arrays(self, file_name):
+    if (file_name is None): return self.miller_arrays
+    canonical_file_name = libtbx.path.canonical_path(file_name)
+    result = self.file_name_miller_arrays.get(canonical_file_name, None)
+    if (result is None and hasattr(os.path, "samefile")):
+      for tabulated_file_name in self.file_name_miller_arrays.keys():
+        if (os.path.samefile(canonical_file_name, tabulated_file_name)):
+          result = self.file_name_miller_arrays[canonical_file_name] \
+                 = self.file_name_miller_arrays[tabulated_file_name]
+          break
+    if (result is None):
+      reflection_file = reflection_file_reader.any_reflection_file(
+        file_name=file_name)
+      if (reflection_file.file_type() is None):
+        self.file_name_miller_arrays[canonical_file_name] = None
+      else:
+        result = self.file_name_miller_arrays[canonical_file_name] \
+               = reflection_file.as_miller_arrays(
+                   crystal_symmetry=self.crystal_symmetry,
+                   force_symmetry=self.force_symmetry)
+    if (result is None):
+      raise UserError("No reflection data in file: %s" % file_name)
+    return result
+
+  def get_xray_data(self, file_name, labels, parameter_scope):
+    miller_arrays = self.get_miller_arrays(file_name=file_name)
+    data_scores = get_xray_data_scores(miller_arrays=miller_arrays)
+    i = select_array(
+      parameter_name=parameter_scope+".labels",
+      labels=labels,
+      miller_arrays=miller_arrays,
+      data_scores=data_scores,
+      err=self.err,
+      error_message_no_array
+        ="No array of observed xray data found.",
+      error_message_not_a_suitable_array
+        ="Not a suitable array of observed xray data: ",
+      error_message_multiple_equally_suitable
+        ="Multiple equally suitable arrays of observed xray data found.")
+    return miller_arrays[i]
+
+  def get_r_free_flags(self,
+        file_name,
+        label,
+        test_flag_value,
+        parameter_scope):
+    miller_arrays = self.get_miller_arrays(file_name=file_name)
+    flag_scores = get_r_free_flags_scores(
+      miller_arrays=miller_arrays,
+      test_flag_value=test_flag_value)
+    if (label is None): labels = None
+    else: labels=[label]
+    i = select_array(
+      parameter_name=parameter_scope+".label",
+      labels=labels,
+      miller_arrays=miller_arrays,
+      data_scores=flag_scores.scores,
+      err=self.err,
+      error_message_no_array
+        ="No array of R-free flags found.",
+      error_message_not_a_suitable_array
+        ="Not a suitable array of R-free flags: ",
+      error_message_multiple_equally_suitable
+        ="Multiple equally suitable arrays of R-free flags found.")
+    return miller_arrays[i], flag_scores.test_flag_values[i]
+
+  def get_experimental_phases(self, file_name, labels, parameter_scope):
+    miller_arrays = self.get_miller_arrays(file_name=file_name)
+    data_scores = get_experimental_phases_scores(miller_arrays=miller_arrays)
+    i = select_array(
+      parameter_name=parameter_scope+".labels",
+      labels=labels,
+      miller_arrays=miller_arrays,
+      data_scores=data_scores,
+      err=self.err,
+      error_message_no_array
+        ="No array of experimental phases found.",
+      error_message_not_a_suitable_array
+        ="Not a suitable array of experimental phases: ",
+      error_message_multiple_equally_suitable
+        ="Multiple equally suitable arrays of experimental phases found.")
+    return miller_arrays[i]
 
 def construct_output_file_name(input_file_names,
                                user_file_name,
---
+++ iotbx/iotbx/tst_reflection_file_utils.py
@@ -0,0 +1,307 @@
+from iotbx.reflection_file_utils import reflection_file_server
+from iotbx import reflection_file_reader
+from iotbx import mtz
+from cctbx import miller
+from cctbx import crystal
+from cctbx.array_family import flex
+from libtbx.utils import UserError
+from cStringIO import StringIO
+import os
+
+def exercise_get_xtal_data():
+  crystal_symmetry = crystal.symmetry(
+    unit_cell=(10,11,12,85,95,100),
+    space_group_symbol="P 1")
+  miller_set = miller.build_set(
+    crystal_symmetry=crystal_symmetry,
+    anomalous_flag=False,
+    d_min=3)
+  input_arrays = [miller_set.array(
+    data=flex.random_double(size=miller_set.indices().size()),
+    sigmas=flex.random_double(size=miller_set.indices().size())/10)
+      .set_observation_type_xray_intensity()
+        for i in [0,1]]
+  mtz_dataset = input_arrays[0].as_mtz_dataset(column_root_label="F0")
+  mtz_dataset.mtz_object().write("tmp1.mtz")
+  reflection_files = [reflection_file_reader.any_reflection_file(
+    file_name="tmp1.mtz")]
+  reflection_file_srv = reflection_file_server(
+    crystal_symmetry=crystal_symmetry,
+    force_symmetry=True,
+    reflection_files=reflection_files)
+  f_obs = reflection_file_srv.get_xray_data(
+    file_name=None,
+    labels=None,
+    parameter_scope="xray_data")
+  assert str(f_obs.info()) == "tmp1.mtz:F0,SIGF0"
+  mtz_dataset.add_miller_array(
+    miller_array=input_arrays[1], column_root_label="F1")
+  mtz_dataset.mtz_object().write("tmp2.mtz")
+  reflection_files = [reflection_file_reader.any_reflection_file(
+    file_name="tmp2.mtz")]
+  err = StringIO()
+  reflection_file_srv = reflection_file_server(
+    crystal_symmetry=crystal_symmetry,
+    force_symmetry=True,
+    reflection_files=reflection_files,
+    err=err)
+  try:
+    f_obs = reflection_file_srv.get_xray_data(
+      file_name=None,
+      labels=None,
+      parameter_scope="xray_data")
+  except UserError:
+    assert err.getvalue() == """\
+
+Multiple equally suitable arrays of observed xray data found.
+
+Possible choices:
+  tmp2.mtz:F0,SIGF0
+  tmp2.mtz:F1,SIGF1
+
+Please use xray_data.labels
+to specify an unambiguous substring of the target label.
+
+"""
+    err = reflection_file_srv.err = StringIO()
+  f_obs = reflection_file_srv.get_xray_data(
+    file_name=None,
+    labels=["F1", "SIGF1"],
+    parameter_scope="xray_data")
+  assert str(f_obs.info()) == "tmp2.mtz:F1,SIGF1"
+  try:
+    f_obs = reflection_file_srv.get_xray_data(
+      file_name=None,
+      labels=["F1", "SIGF0"],
+      parameter_scope="xray_data")
+  except UserError:
+    assert err.getvalue() == """\
+
+No matching array: xray_data.labels=F1 SIGF0
+
+Possible choices:
+  tmp2.mtz:F0,SIGF0
+  tmp2.mtz:F1,SIGF1
+
+Please use xray_data.labels
+to specify an unambiguous substring of the target label.
+
+"""
+    err = reflection_file_srv.err = StringIO()
+  f_obs = reflection_file_srv.get_xray_data(
+    file_name=None,
+    labels=["1"],
+    parameter_scope="xray_data")
+  assert str(f_obs.info()) == "tmp2.mtz:F0,SIGF0"
+  f_obs = reflection_file_srv.get_xray_data(
+    file_name=None,
+    labels=["2"],
+    parameter_scope="xray_data")
+  assert str(f_obs.info()) == "tmp2.mtz:F1,SIGF1"
+  f_obs = reflection_file_srv.get_xray_data(
+    file_name="tmp2.mtz",
+    labels=["2"],
+    parameter_scope="xray_data")
+  assert str(f_obs.info()) == "tmp2.mtz:F1,SIGF1"
+  assert len(reflection_file_srv.file_name_miller_arrays) == 1
+  f_obs = reflection_file_srv.get_xray_data(
+    file_name="tmp1.mtz",
+    labels=None,
+    parameter_scope="xray_data")
+  assert len(reflection_file_srv.file_name_miller_arrays) == 2
+  assert str(f_obs.info()) == "tmp1.mtz:F0,SIGF0"
+  f_obs = reflection_file_srv.get_xray_data(
+    file_name=os.path.abspath("tmp1.mtz"),
+    labels=["sigf0"],
+    parameter_scope="xray_data")
+  assert len(reflection_file_srv.file_name_miller_arrays) == 2
+  assert str(f_obs.info()) == "tmp1.mtz:F0,SIGF0"
+  try:
+    f_obs = reflection_file_srv.get_xray_data(
+      file_name=None,
+      labels=None,
+      parameter_scope="xray_data")
+  except UserError:
+    assert err.getvalue() == """\
+
+Multiple equally suitable arrays of observed xray data found.
+
+Possible choices:
+  tmp2.mtz:F0,SIGF0
+  tmp2.mtz:F1,SIGF1
+
+Please use xray_data.labels
+to specify an unambiguous substring of the target label.
+
+"""
+    err = reflection_file_srv.err = StringIO()
+
+def exercise_get_r_free_flags():
+  crystal_symmetry = crystal.symmetry(
+    unit_cell=(30,31,32,85,95,100),
+    space_group_symbol="P 1")
+  miller_set = miller.build_set(
+    crystal_symmetry=crystal_symmetry,
+    anomalous_flag=False,
+    d_min=3)
+  n = miller_set.indices().size()
+  sorted_flags = flex.int(range(10))
+  while (sorted_flags.size() <= n/2):
+    sorted_flags.extend(sorted_flags)
+  if (sorted_flags.size() < n):
+    sorted_flags.extend(sorted_flags[:n-sorted_flags.size()])
+  assert sorted_flags.size() == n
+  perm = flex.sort_permutation(
+    flex.random_double(size=miller_set.indices().size()))
+  exercise_flag_arrays = []
+  exercise_flag_arrays.append(sorted_flags.select(perm))
+  exercise_flag_arrays.append(flex.int(xrange(n)))
+  exercise_flag_arrays.append(flex.int(n, 0))
+  for style in ["ccp4", "cns", "shelx", "bool"]:
+    for i_exercise,exercise_flag_array in enumerate(exercise_flag_arrays):
+      for reversed in [False, True]:
+        if (style == "ccp4"):
+          if (reversed): break
+          data = exercise_flag_array
+          test_flag_value = 3
+        else:
+          if (not reversed):
+            data = (exercise_flag_array == 0)
+            test_flag_value = True
+          else:
+            data = (exercise_flag_array != 0)
+            test_flag_value = False
+          if (style == "cns"):
+            data = data.as_int()
+            test_flag_value = int(test_flag_value)
+          elif (style == "shelx"):
+            data = -data.as_int()
+            data.set_selected((data == 0), 1)
+            if (not reversed): test_flag_value = -1
+            else:              test_flag_value = 1
+        input_array = miller_set.array(data=data)
+        mtz_dataset = input_array.as_mtz_dataset(
+          column_root_label="FreeRflags")
+        mtz_dataset.mtz_object().write("tmp.mtz")
+        reflection_files = [reflection_file_reader.any_reflection_file(
+          file_name="tmp.mtz")]
+        err = StringIO()
+        reflection_file_srv = reflection_file_server(
+          crystal_symmetry=crystal_symmetry,
+          force_symmetry=True,
+          reflection_files=reflection_files,
+          err=err)
+        for trial_test_flag_value in [None, test_flag_value]:
+          for trial_label in [None, "free", "foo"]:
+            try:
+              r_free_flags, actual_test_flag_value = \
+                reflection_file_srv.get_r_free_flags(
+                  file_name=None,
+                  label=trial_label,
+                  parameter_scope="r_free_flags",
+                  test_flag_value=trial_test_flag_value)
+            except UserError, e:
+              if (trial_label != "foo"):
+                assert i_exercise > 0
+                if (trial_label is None):
+                  assert str(e) == "No array of R-free flags found."
+                else:
+                  assert str(e) == "Not a suitable array of R-free flags:" \
+                                 + " r_free_flags.label=free"
+              else:
+                assert str(e) == "No matching array: r_free_flags.label=foo"
+                if (i_exercise == 0):
+                  assert err.getvalue() == """\
+
+No matching array: r_free_flags.label=foo
+
+Possible choices:
+  tmp.mtz:FreeRflags
+
+Please use r_free_flags.label
+to specify an unambiguous substring of the target label.
+
+"""
+                else:
+                  assert err.getvalue() == """\
+
+No matching array: r_free_flags.label=foo
+
+"""
+              err = reflection_file_srv.err = StringIO()
+            else:
+              assert i_exercise == 0
+  for second_label in ["test", "foo"]:
+    input_array = miller_set.array(data=exercise_flag_arrays[0])
+    mtz_dataset = input_array.as_mtz_dataset(
+      column_root_label="FreeRflags")
+    mtz_dataset.add_miller_array(
+      miller_array=input_array,
+      column_root_label=second_label)
+    mtz_dataset.mtz_object().write("tmp.mtz")
+    reflection_files = [reflection_file_reader.any_reflection_file(
+      file_name="tmp.mtz")]
+    err = StringIO()
+    reflection_file_srv = reflection_file_server(
+      crystal_symmetry=crystal_symmetry,
+      force_symmetry=True,
+      reflection_files=reflection_files,
+      err=err)
+    try:
+      r_free_flags, actual_test_flag_value = \
+        reflection_file_srv.get_r_free_flags(
+          file_name=None,
+          label=None,
+          parameter_scope="r_free_flags",
+          test_flag_value=None)
+    except UserError, e:
+      assert str(e)=="Multiple equally suitable arrays of R-free flags found."
+      assert err.getvalue() == """\
+
+Multiple equally suitable arrays of R-free flags found.
+
+Possible choices:
+  tmp.mtz:FreeRflags
+  tmp.mtz:test
+
+Please use r_free_flags.label
+to specify an unambiguous substring of the target label.
+
+"""
+      err = reflection_file_srv.err = StringIO()
+    else:
+      assert str(r_free_flags.info()) == "tmp.mtz:FreeRflags"
+
+def exercise_get_experimental_phases():
+  crystal_symmetry = crystal.symmetry(
+    unit_cell=(30,31,32,85,95,100),
+    space_group_symbol="P 1")
+  miller_set = miller.build_set(
+    crystal_symmetry=crystal_symmetry,
+    anomalous_flag=False,
+    d_min=3)
+  input_array = miller_set.array(
+    data=flex.hendrickson_lattman(miller_set.indices().size()))
+  mtz_dataset = input_array.as_mtz_dataset(column_root_label="P")
+  mtz_dataset.mtz_object().write("tmp.mtz")
+  reflection_files = [reflection_file_reader.any_reflection_file(
+    file_name="tmp.mtz")]
+  reflection_file_srv = reflection_file_server(
+    crystal_symmetry=crystal_symmetry,
+    force_symmetry=True,
+    reflection_files=reflection_files,
+    err=None)
+  experimental_phases = reflection_file_srv.get_experimental_phases(
+    file_name=None,
+    labels=None,
+    parameter_scope="experimental_phases")
+  assert str(experimental_phases.info()) == "tmp.mtz:PA,PB,PC,PD"
+
+def exercise():
+  exercise_get_xtal_data()
+  exercise_get_r_free_flags()
+  exercise_get_experimental_phases()
+  print "OK"
+
+if (__name__ == "__main__"):
+  exercise()
