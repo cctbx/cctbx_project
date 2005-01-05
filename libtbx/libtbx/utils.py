65a81de24b7f18026a39317fe7f77b6ca0acef90
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-01-05 00:51:05 +0000

    UserError.reset_tracebacklimit()

--- libtbx/libtbx/utils.py
+++ libtbx/libtbx/utils.py
@@ -5,5 +5,20 @@ class Keep: pass
 class UserError(Exception):
 
   def __init__(self, *args, **keyword_args):
+    self.previous_tracebacklimit = getattr(sys, "tracebacklimit", None)
     sys.tracebacklimit = 0
     Exception.__init__(self, *args, **keyword_args)
+
+  def __str__(self):
+    self.reset_tracebacklimit()
+    return Exception.__str__(self)
+
+  def __del__(self):
+    self.reset_tracebacklimit()
+
+  def reset_tracebacklimit(self):
+    if (hasattr(sys, "tracebacklimit")):
+      if (self.previous_tracebacklimit is None):
+        del sys.tracebacklimit
+      else:
+        sys.tracebacklimit = self.previous_tracebacklimit
