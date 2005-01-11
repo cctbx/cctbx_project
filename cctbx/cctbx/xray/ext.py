8c28225f8d8fc3798dbd0531344def530ebc2f57
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-01-11 07:41:13 +0000

    scattering_dictionary.show() enhancements

--- cctbx/cctbx/xray/ext.py
+++ cctbx/cctbx/xray/ext.py
@@ -2,9 +2,16 @@ import boost.python
 ext = boost.python.import_ext("cctbx_xray_ext")
 from cctbx_xray_ext import *
 
+from cctbx.array_family import flex
 import sys
 
 class _scattering_dictionary(boost.python.injector, scattering_dictionary):
+
+  def sorted_items(self, heaviest_first=True):
+    items = self.dict().items()
+    weights = flex.double([val.gaussian.at_stol(0) for key,val in items])
+    perm = flex.sort_permutation(weights, reverse=heaviest_first)
+    return flex.select(items, permutation=perm)
 
   def show_summary(self, f=None, prefix=""):
     if (f is None): f = sys.stdout
@@ -17,19 +24,31 @@ class _scattering_dictionary(boost.python.injector, scattering_dictionary):
     print >> f
 
   def show(self,
-        types_only=False,
-        header="Scattering types:",
+        header="Number of scattering types:",
+        show_weights=True,
+        show_gaussians=True,
         f=None,
         prefix=""):
     if (f is None): f = sys.stdout
+    items = self.sorted_items()
     if (header is not None):
-      print >> f, prefix + header
-    gn = ""
-    for key,val in self.dict().items():
-      print >> f, "%s  %s: %d" % (prefix, key, val.member_indices.size()),
-      if (not types_only):
-        gn = str(val.gaussian.n_terms())
-        if (val.gaussian.c() != 0): gn += "+c"
-        print >> f, gn,
-      print >> f
-    print >> f
+      print >> f, prefix + header, len(items)
+    nk = max(3,max([len(key) for key,val in items]))
+    nk_fmt = "%%-%ds " % nk
+    nn = max(5,len(str(max([val.member_indices.size() for key,val in items]))))
+    nn_fmt = "%%%dd" % nn
+    if (len(items) > 0):
+      line = prefix + "  Type%s %sNumber" % (" "*(nk-3), " "*(nn-5))
+      if (show_weights): line += "   Weight"
+      if (show_gaussians): line += "   Gaussians"
+      print >> f, line
+      for key,val in items:
+        line = prefix + "   " \
+             + nk_fmt%key \
+             + nn_fmt%val.member_indices.size() + " "
+        if (show_weights):
+          line += " %8.2f" % val.gaussian.at_stol(0)
+        if (show_gaussians):
+          line += " %7s" % str(val.gaussian.n_terms())
+          if (val.gaussian.c() != 0): line += "+c"
+        print >> f, line.rstrip()
