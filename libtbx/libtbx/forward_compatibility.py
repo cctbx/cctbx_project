628856d13e66a6a228a82a0106134b05950c1c6d
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-01-19 12:05:31 +0000

    Python 2.2 compatibility

--- libtbx/libtbx/forward_compatibility.py
+++ libtbx/libtbx/forward_compatibility.py
@@ -1,23 +1,25 @@
 from __future__ import generators
 
-if (__builtins__.get("bool", None) != None):
-  __builtins__.setdefault("False", bool(0))
-  __builtins__.setdefault("True", bool(1))
-else:
-  __builtins__.setdefault("False", 0)
-  __builtins__.setdefault("True", 1)
+__builtins__.setdefault("False", 0)
+__builtins__.setdefault("True", 1)
 
-def _enumerate(iterable):
-  """enumerate(iterable) -> iterator for index, value of iterable
+if (__builtins__.get("bool", None) is None):
+  def bool(value):
+    if (value): return True
+    return False
+  __builtins__["bool"] = bool
+
+if (__builtins__.get("_enumerate", None) is None):
+  def enumerate(iterable):
+    """enumerate(iterable) -> iterator for index, value of iterable
 
   Return an enumerate object.  iterable must be an other object that supports
   iteration.  The enumerate object yields pairs containing a count (from
   zero) and a value yielded by the iterable argument.  enumerate is useful
   for obtaining an indexed list: (0, seq[0]), (1, seq[1]), (2, seq[2]), ..."""
-  i = 0
-  it = iter(iterable)
-  while 1:
-    yield (i, it.next())
-    i += 1
-
-__builtins__.setdefault("enumerate", _enumerate)
+    i = 0
+    it = iter(iterable)
+    while 1:
+      yield (i, it.next())
+      i += 1
+  __builtins__["enumerate"] = enumerate
