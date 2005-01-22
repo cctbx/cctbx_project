a8758402364ec2ad1ed15d2d859934562a4790af
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-01-22 14:36:09 +0000

    assertions instead of print statements; print Skipping if input file not available

--- iotbx/iotbx/detectors/tst_adsc.py
+++ iotbx/iotbx/detectors/tst_adsc.py
@@ -1,19 +1,28 @@
 from iotbx.detectors import adsc
+from libtbx.test_utils import approx_equal
 import urllib
+import os
 
 def get_test_files():
-  urllib.urlretrieve(
-    'http://cci.lbl.gov/cctbx_downloads/regression/iotbx/adsc.img',
-    'adsc.img')
+  try:
+    urllib.urlretrieve(
+      'http://cci.lbl.gov/cctbx_downloads/regression/iotbx/adsc.img',
+      'adsc.img')
+  except IOError:
+    pass
 
 def exercise_adscread():
+  if (not os.path.isfile("adsc.img")):
+    print "Skipping exercise_adscread(): input file not available"
+    return
   a = adsc.ADSCImage('adsc.img')
   a.read()
-  print "Completed test read of ADSC image:"
-  print "image size",a.size1,"x",a.size2,'=',a.npixels,"pixels"
-  print "pixel size",a.pixel_size
-  print "saturated value",a.saturation
-  print "starting phi",a.osc_start
+  assert a.size1 == 2304
+  assert a.size2 == 2304
+  assert a.npixels == 5308416
+  assert approx_equal(a.pixel_size, 0.0816)
+  assert a.saturation == 65535
+  assert approx_equal(a.osc_start, 0)
 
 def run():
   get_test_files()
