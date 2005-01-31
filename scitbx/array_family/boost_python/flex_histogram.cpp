414579d84b72ecc624ca0fa05307e71e621abf59
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-01-31 21:17:49 +0000

    flex.histogram with slots from previously computed histogram

--- scitbx/array_family/boost_python/flex_histogram.cpp
+++ scitbx/array_family/boost_python/flex_histogram.cpp
@@ -23,13 +23,20 @@ namespace scitbx { namespace af { namespace boost_python { namespace {
                   optional<std::size_t> >((
           arg_("data"),
           arg_("n_slots")=1000)))
+        .def(init<w_t const&,
+                  af::const_ref<double> const&,
+                  optional<double const&> >((
+          arg_("other"),
+          arg_("data"),
+          arg_("relative_tolerance")=1.e-4)))
         .def("data_min", &w_t::data_min)
         .def("data_max", &w_t::data_max)
         .def("slot_width", &w_t::slot_width)
         .def("slots", &w_t::slots)
+        .def("n_out_of_slot_range", &w_t::n_out_of_slot_range)
         .def("get_cutoff", &w_t::get_cutoff, get_cutoff_overloads((
           arg_("max_points"),
-          arg_("tolerance")=1.e-4)))
+          arg_("relative_tolerance")=1.e-4)))
       ;
     }
   };
