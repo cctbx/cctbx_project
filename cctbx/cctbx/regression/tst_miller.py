4b534f88b20e46a1d5b26f06fcea653c399db7f5
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-02-16 01:16:41 +0000

    bug fix in exercise_array_2()

--- cctbx/cctbx/regression/tst_miller.py
+++ cctbx/cctbx/regression/tst_miller.py
@@ -587,13 +587,18 @@ def exercise_array_2(space_group_info):
         assert approx_equal(sr, sigmas)
       #
       orig = m.array()
-      ave = orig.average_bijvoet_mates()
       if (not orig.anomalous_flag()):
+        ave = orig.average_bijvoet_mates()
         assert ave.correlation(orig).coefficient() > 1-1.e-6
         if (sigmas is not None):
           assert flex.linear_correlation(
             ave.sigmas(), orig.sigmas()).coefficient() > 1-1.e-6
       else:
+        if (sigmas is not None):
+          # merge_equivalents uses the sigmas as weights
+          orig = orig.customized_copy(
+            sigmas=flex.double(orig.sigmas().size(),1))
+        ave = orig.average_bijvoet_mates()
         asu, matches = orig.match_bijvoet_mates()
         vfy_indices = asu.indices().select(matches.pairs().column(0))
         vfy_data = (  asu.data().select(matches.pairs().column(0))
@@ -608,7 +613,7 @@ def exercise_array_2(space_group_info):
             indices=vfy_indices,
             anomalous_flag=False),
           data=vfy_data).adopt_set(ave)
-        assert vfy.correlation(ave).coefficient() > 1-1.e-5
+        assert vfy.correlation(ave).coefficient() > 1-1.e-6
 
 def exercise_fft_map():
   xs = crystal.symmetry((3,4,5), "P 2 2 2")
