2148c8a5cc70732bc5226ab337621438543f8044
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-02-26 02:22:26 +0000

    new libtbx.show_commands

---
+++ libtbx/libtbx/command_line/show_commands.py
@@ -0,0 +1,8 @@
+import libtbx.env
+import os
+print "# Listing of commands in: %s" % libtbx.env.bin_path
+file_names = os.listdir(libtbx.env.bin_path)
+file_names.sort()
+print "# Number of commands: %d" % len(file_names)
+for file_name in file_names:
+  print file_name
