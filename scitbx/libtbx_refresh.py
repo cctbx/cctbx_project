b70190b6256035f34a3af79265f5ecfc82731300
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-03-04 19:35:18 +0000

    generate headers only if libtbx.env.is_ready_for_build()

--- scitbx/libtbx_refresh.py
+++ scitbx/libtbx_refresh.py
@@ -2,7 +2,8 @@ from scitbx.source_generators.array_family import generate_all
 import libtbx.load_env
 import os
 
-target_dir = libtbx.env.under_build("include/scitbx/array_family/detail")
-if (not os.path.isdir(target_dir)):
-  os.makedirs(target_dir)
-generate_all.refresh(array_family=os.path.dirname(target_dir))
+if (libtbx.env.is_ready_for_build()):
+  target_dir = libtbx.env.under_build("include/scitbx/array_family/detail")
+  if (not os.path.isdir(target_dir)):
+    os.makedirs(target_dir)
+  generate_all.refresh(array_family=os.path.dirname(target_dir))
