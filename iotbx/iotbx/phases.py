e408699c4ae1d8348cd287fcee8bfb15a1606cc5
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-03-15 19:04:38 +0000

    scale amplitudes to fit into %8.2f format; scale_amplitudes, phases, phases_deg, figures_of_merit parameters

--- iotbx/iotbx/phases.py
+++ iotbx/iotbx/phases.py
@@ -1,6 +1,40 @@
-def miller_array_as_phases_phs(self, out):
-  assert self.is_complex_array()
-  ampl = self.amplitudes().data()
-  pdeg = self.phases(deg=True).data()
-  for h,a,p in zip(self.indices(),ampl,pdeg):
-    print >> out, "%4d%4d%4d" % h + "%8.2f" % a + "%8.2f" % 1 + "%8.2f" % p
+from cctbx.array_family import flex
+import math
+
+def miller_array_as_phases_phs(self,
+      out,
+      scale_amplitudes=True,
+      phases=None,
+      phases_deg=None,
+      figures_of_merit=None):
+  if (phases is not None): assert phases_deg is False or phases_deg is True
+  if (self.is_complex_array()):
+    amplitudes = self.amplitudes().data()
+  else:
+    amplitudes = self.data()
+  if (scale_amplitudes):
+    amplitudes_max = flex.max(amplitudes)
+    if (amplitudes_max > 0):
+      amplitudes = (99999.99/amplitudes_max) * amplitudes
+  assert len("%8.2f" % flex.min(amplitudes)) == 8
+  assert len("%8.2f" % flex.max(amplitudes)) == 8
+  if (phases is None):
+    phases = self.phases(deg=True).data()
+  else:
+    if (hasattr(phases, "data")):
+      phases = phases.data()
+    if (not phases_deg):
+      phases = phases * (180/math.pi)
+  assert len("%8.2f" % flex.min(phases)) == 8
+  assert len("%8.2f" % flex.max(phases)) == 8
+  if (figures_of_merit is None):
+    for h,a,p in zip(self.indices(),amplitudes,phases):
+      print >> out, "%4d%4d%4d" % h + "%8.2f" % a + "%8.2f" % 1 + "%8.2f" % p
+  else:
+    if (hasattr(figures_of_merit, "data")):
+      assert figures_of_merit.indices().all_eq(self.indices())
+      figures_of_merit = figures_of_merit.data()
+    assert len("%8.2f" % flex.min(figures_of_merit)) == 8
+    assert len("%8.2f" % flex.max(figures_of_merit)) == 8
+    for h,a,p,f in zip(self.indices(),amplitudes,phases,figures_of_merit):
+      print >> out, "%4d%4d%4d" % h + "%8.2f" % a + "%8.2f" % f + "%8.2f" % p
--- iotbx/iotbx/tst_phases.py
+++ iotbx/iotbx/tst_phases.py
@@ -1,37 +1,101 @@
 import iotbx.pdb.xray_structure
 from cctbx.development import random_structure
+from cctbx import miller
+from cctbx import crystal
 from cctbx import sgtbx
+from cctbx.array_family import flex
 from scitbx.python_utils import easy_pickle
+from libtbx.test_utils import show_diff
+from cStringIO import StringIO
+import math
 import sys
 
-def generate_random_f_calc(space_group_info, n_elements=10, d_min=1.5):
+def exercise_basic():
+  a = flex.double((10,20))
+  p = flex.double((-25,355))
+  c = flex.polar(a, p, True)
+  f = flex.double((0.3,0.9))
+  s = miller.set(
+    crystal_symmetry=crystal.symmetry(
+      unit_cell=(10,10,10,90,90,90),
+      space_group_symbol="P1"),
+    indices=flex.miller_index([(1,2,3),(-3,4,-6)]),
+    anomalous_flag=False)
+  out = StringIO()
+  s.array(data=c).as_phases_phs(out=out)
+  assert not show_diff(out.getvalue(), """\
+   1   2   349999.99    1.00  -25.00
+  -3   4  -699999.99    1.00   -5.00
+""")
+  out = StringIO()
+  s.array(data=c).as_phases_phs(out=out, scale_amplitudes=False)
+  assert not show_diff(out.getvalue(), """\
+   1   2   3   10.00    1.00  -25.00
+  -3   4  -6   20.00    1.00   -5.00
+""")
+  for phases in [s.array(data=p), p]:
+    out = StringIO()
+    s.array(data=c).amplitudes().as_phases_phs(
+      out=out, phases=phases, phases_deg=True)
+    assert not show_diff(out.getvalue(), """\
+   1   2   349999.99    1.00  -25.00
+  -3   4  -699999.99    1.00  355.00
+""")
+  for phases in [s.array(data=p*(math.pi/180)), p*(math.pi/180)]:
+    out = StringIO()
+    s.array(data=c).amplitudes().as_phases_phs(
+      out=out, phases=phases, phases_deg=False)
+    assert not show_diff(out.getvalue(), """\
+   1   2   349999.99    1.00  -25.00
+  -3   4  -699999.99    1.00  355.00
+""")
+  for figures_of_merit in [s.array(data=f), f]:
+    out = StringIO()
+    s.array(data=c).as_phases_phs(out=out, figures_of_merit=figures_of_merit)
+    assert not show_diff(out.getvalue(), """\
+   1   2   349999.99    0.30  -25.00
+  -3   4  -699999.99    0.90   -5.00
+""")
+
+def generate_random_f_calc(
+      space_group_info,
+      n_elements=10,
+      d_min=1.5,
+      verbose=0):
   structure = random_structure.xray_structure(
     space_group_info,
     elements=["Si"]*n_elements,
     volume_per_atom=1000,
     min_distance=3.,
     general_positions_only=False)
-  structure.show_summary().show_scatterers()
-  print
+  if (0 or verbose):
+    structure.show_summary()
+    structure.show_scatterers()
+    print
   print "Writing tmp.pdb"
   s = structure.as_pdb_file(
     remark="random structure",
     res_name="RND")
   open("tmp.pdb", "w").write(s)
-  print
+  if (0 or verbose):
+    print
   f_calc = structure.structure_factors(
     d_min=d_min, anomalous_flag=False).f_calc()
-  f_calc.show_summary()
-  print
+  if (0 or verbose):
+    f_calc.show_summary()
+    print
   print "Writing: tmp.phs"
   f_calc.as_phases_phs(out=open("tmp.phs", "w"))
-  print
+  if (0 or verbose):
+    print
 
 def run():
-  if (len(sys.argv) != 2):
-    print __doc__
-    return
-  generate_random_f_calc(sgtbx.space_group_info(sys.argv[1]))
+  exercise_basic()
+  if (len(sys.argv) > 1):
+    generate_random_f_calc(
+      sgtbx.space_group_info(sys.argv[1]),
+      verbose="--verbose" in sys.argv[2:])
+  print "OK"
 
 if (__name__ == "__main__"):
   run()
