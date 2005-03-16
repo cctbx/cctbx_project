71f326aa6a40beff2ba1e48ffa41245852866acf
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-03-16 05:51:34 +0000

    don't show file names

--- scitbx/scitbx/source_generators/utils.py
+++ scitbx/scitbx/source_generators/utils.py
@@ -3,9 +3,9 @@ import os
 def norm_join(path1, path2):
   return os.path.normpath(os.path.join(path1, path2))
 
-def join_open(path1, path2, mode):
+def join_open(path1, path2, mode, verbose=0):
   path = norm_join(path1, path2)
-  if (mode == "w"):
+  if (mode == "w" and verbose):
     print "     ", path2
   return open(path, mode)
 
