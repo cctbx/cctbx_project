cf4b0317318c4eb19eea2e1ebf70ebf4852d6f0a
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-04-02 04:01:18 +0000

    refine from random positions (fails)

--- cctbx/cctbx/maptbx/tst_real_space_refinement.py
+++ cctbx/cctbx/maptbx/tst_real_space_refinement.py
@@ -5,8 +5,8 @@ from cctbx import crystal
 from cctbx import maptbx
 from cctbx.maptbx.real_space_refinement import lbfgs
 from cctbx import uctbx
+from scitbx.python_utils.math_utils import ifloor, iceil
 from libtbx.test_utils import approx_equal
-import math
 
 def exercise_real_space_refinement():
   crystal_symmetry = crystal.symmetry(
@@ -25,19 +25,27 @@ def exercise_real_space_refinement():
   fft_map = f_calc.fft_map()
   fft_map.apply_sigma_scaling()
   real_map = fft_map.real_map_unpadded()
-
+  #
   minfrac = crystal_symmetry.unit_cell().fractionalize((-5,-5,-5))
   maxfrac = crystal_symmetry.unit_cell().fractionalize((5,5,5))
-  gridding_first = [int(math.floor(n*b)) for n,b in zip(fft_map.n_real(), minfrac)]
-  gridding_last = [int(math.ceil(n*b)) for n,b in zip(fft_map.n_real(), maxfrac)]
+  gridding_first = [ifloor(n*b) for n,b in zip(fft_map.n_real(), minfrac)]
+  gridding_last = [iceil(n*b) for n,b in zip(fft_map.n_real(), maxfrac)]
   data=maptbx.copy(real_map, gridding_first, gridding_last)
-
+  #
   grid_cell=uctbx.unit_cell((10.0/30,10.0/30,10.0/30,90,90,90))
   grid_mat = grid_cell.fractionalization_matrix()
-  ref = lbfgs(data, grid_mat, flex.vec3_double([(-1,-1,-1)]))
-  for site,sitec in zip(ref.sites_cart,xray_structure.sites_cart()):
+  for i_trial in xrange(100):
+    sites_cart = flex.vec3_double((flex.random_double(size=3)-0.5)*1)
+    ref = lbfgs(
+      data=data,
+      grid_mat=grid_mat,
+      sites_cart=sites_cart.deep_copy())
+    for site,sitec in zip(ref.sites_cart,xray_structure.sites_cart()):
+      print sitec
+      print sites_cart[0]
+      print site
       assert approx_equal(site,sitec)
-
+      print
 
 if (__name__ == "__main__"):
   exercise_real_space_refinement()
