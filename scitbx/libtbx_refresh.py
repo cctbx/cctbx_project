efc3529edf3743aa47ac006e484c78d1a27fcca7
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-04-09 07:44:46 +0000

    use "self" defined by the calling libtbx.config.module instance

--- scitbx/libtbx_refresh.py
+++ scitbx/libtbx_refresh.py
@@ -1,9 +1,8 @@
 from scitbx.source_generators.array_family import generate_all
-import libtbx.load_env
 import os
 
-if (libtbx.env.is_ready_for_build()):
-  target_dir = libtbx.env.under_build("include/scitbx/array_family/detail")
+if (self.env.is_ready_for_build()):
+  target_dir = self.env.under_build("include/scitbx/array_family/detail")
   if (not os.path.isdir(target_dir)):
     os.makedirs(target_dir)
   generate_all.refresh(array_family=os.path.dirname(target_dir))
