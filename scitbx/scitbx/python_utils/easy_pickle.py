0950d740c03f35e62188469b60a6e0a4f0d69c6a
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-05-06 02:17:41 +0000

    scitbx.python_utils.easy_pickle -> libtbx.easy_pickle

--- scitbx/scitbx/python_utils/easy_pickle.py
+++ scitbx/scitbx/python_utils/easy_pickle.py
@@ -1,7 +1,2 @@
-import cPickle
-
-def dump(file_name, obj):
-  return cPickle.dump(obj, open(file_name, "wb"), 1)
-
-def load(file_name):
-  return cPickle.load(open(file_name, "rb"))
+# backward compatibility
+from libtbx.easy_pickle import dump, load
