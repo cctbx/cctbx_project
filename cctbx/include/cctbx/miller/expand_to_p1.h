272f5e333cc3e82b18c780223561e1d83e3396d5
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-05-09 18:28:05 +0000

    obsolete class expand_to_p1 removed

--- cctbx/include/cctbx/miller/expand_to_p1.h
+++ cctbx/include/cctbx/miller/expand_to_p1.h
@@ -5,53 +5,6 @@
 
 namespace cctbx { namespace miller {
 
-  //! XXX OBSOLETE
-  /*! Expands an array of Miller indices to P1 symmetry.
-      <p>
-      The symmetry operations are applied to each element
-      of the input array.
-      <p>
-      If anomalous_flag == false, centric indices are treated in a
-      special way: Friedel mates are suppressed. If N is the
-      number of unique symmetrically equivalent indices for
-      a given centric index, only N/2 indices will be generated.
-      <p>
-      See also: sym_equiv_indices
-   */
-  template <typename FloatType = double>
-  class expand_to_p1
-  {
-    public:
-      //! Default constructor. All output arrays have zero length.
-      /*! Not available in Python.
-       */
-      expand_to_p1() {}
-
-      //! Expands a list of Miller indices.
-      expand_to_p1(
-        sgtbx::space_group const& space_group,
-        bool anomalous_flag,
-        af::const_ref<index<> > const& indices)
-      {
-        for(std::size_t i_indices=0;i_indices<indices.size();i_indices++) {
-          sym_equiv_indices eq(space_group, indices[i_indices]);
-          af::shared<sym_equiv_index> p1_list = eq.p1_listing(anomalous_flag);
-          for(sym_equiv_index const* p1=p1_list.begin();
-                                     p1!=p1_list.end();
-                                     p1++) {
-            indices_.push_back(p1->h());
-          }
-        }
-      }
-
-      //! Indices in P1.
-      af::shared<index<> > const&
-      indices() const { return indices_; }
-
-    protected:
-      af::shared<index<> > indices_;
-  };
-
   namespace detail {
 
     struct expand_to_p1_generator
