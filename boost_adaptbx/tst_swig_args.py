1457981455d88ad0f21b625a477abd141616d32d
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-07-03 14:02:10 +0000

    prototype for automatic conversion of SWIG-wrapped objects

---
+++ boost_adaptbx/tst_swig_args.py
@@ -0,0 +1,21 @@
+import example
+import boost_python_swig_args_ext
+import sys
+
+def exercise():
+  c = example.Circle(10)
+  c.x = 20
+  c.y = 30
+
+  s = example.Square(10)
+  s.x = -10
+  s.y = 5
+
+  forever = "--forever" in sys.argv[1:]
+  while True:
+    boost_python_swig_args_ext.show(c.this)
+    boost_python_swig_args_ext.show(s.this)
+    if (not forever): break
+
+if (__name__ == "__main__"):
+  exercise()
