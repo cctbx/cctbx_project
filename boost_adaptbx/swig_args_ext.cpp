1a72ba101ae8c206ce5c42f28f6edde42df8a45b
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-07-05 08:47:35 +0000

    separate header file swig_arg.hpp

--- boost_adaptbx/swig_args_ext.cpp
+++ boost_adaptbx/swig_args_ext.cpp
@@ -1,10 +1,8 @@
 #include <boost/python/module.hpp>
 #include <boost/python/def.hpp>
-#include <boost/python/lvalue_from_pytype.hpp>
+#include "swig_arg.hpp"
 
 #include <iostream>
-#include <exception>
-#include <cstring>
 
 //############################################################################
 // copy of SWIG-1.3.24/Examples/python/class/example.h
@@ -43,50 +41,6 @@ public:
   virtual double area(void);
   virtual double perimeter(void);
 };
-
-
-//############################################################################
-// this should go somewhere into the boost/boost/python include directory
-//############################################################################
-extern "C" {
-
-// definition lifted from SWIG-1.3.24/Lib/python/pyrun.swg
-typedef struct {
-  PyObject_HEAD
-  void *ptr;
-  const char *desc;
-} PySwigObject;
-
-}
-
-namespace boost { namespace python {
-
-template <typename T>
-struct swig_arg
-{
-    static std::string desc;
-
-    swig_arg(const char* name, const char* prefix="_p_")
-    {
-        desc = std::string(prefix) + name;
-        converter::registry::insert(&extract, type_id<T>());
-    }
- private:
-    static void* extract(PyObject* op)
-    {
-        if (std::strcmp(op->ob_type->tp_name, "PySwigObject") != 0) return 0;
-
-        PySwigObject* swig_obj_ptr = reinterpret_cast<PySwigObject*>(op);
-        if (std::strcmp(swig_obj_ptr->desc, desc.c_str()) != 0) return 0;
-        return swig_obj_ptr->ptr;
-    }
-};
-
-template <typename T> std::string swig_arg<T>::desc;
-
-}} // namespace boost::python
-
-#define BOOST_PYTHON_SWIG_ARG(T) boost::python::swig_arg<T>(#T);
 
 //############################################################################
 // user code
