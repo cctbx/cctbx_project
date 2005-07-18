780d0928976b439c8c314f0cfaa00a4db4ca3041
Erik McKee <emckee@users.noreply.github.com>
2005-07-18 23:37:25 +0000

    adds functionality to extract space group information from a string

---
+++ iotbx/iotbx/tst_crystal_symmetry_from_any.py
@@ -0,0 +1,54 @@
+import crystal_symmetry_from_any
+
+def exercise():
+  space_group = "P1"
+  unit_cell = "100,100,100,90,90,90"
+  symmetry = unit_cell+","+space_group
+  sp_space_group = " P 1 "
+  sp_symmetry = unit_cell+","+sp_space_group
+  killer="100,P1"
+  killer2=space_group+","+unit_cell
+  killer3="100 100 100 90 90 90"
+  killer4=killer3+" "+sp_space_group
+
+  cs = crystal_symmetry_from_any.from_string(space_group)
+  assert(cs is not None)
+  assert(cs.unit_cell() is None)
+  assert(str(cs.space_group_info())=='P 1')
+
+  cs = crystal_symmetry_from_any.from_string(unit_cell)
+  assert(cs is not None)
+  assert(cs.unit_cell().parameters()==(100.0,100.0,100.0,90.0,90.0,90.0))
+  assert(cs.space_group_info() is None)
+
+  cs = crystal_symmetry_from_any.from_string(symmetry)
+  assert(cs is not None)
+  assert(cs.unit_cell().parameters()==(100.0,100.0,100.0,90.0,90.0,90.0))
+  assert(str(cs.space_group_info())=='P 1')
+
+  cs = crystal_symmetry_from_any.from_string(sp_space_group)
+  assert(cs is not None)
+  assert(cs.unit_cell() is None)
+  assert(str(cs.space_group_info())=='P 1')
+
+  cs = crystal_symmetry_from_any.from_string(sp_symmetry)
+  assert(cs is not None)
+  assert(cs.unit_cell().parameters()==(100.0,100.0,100.0,90.0,90.0,90.0))
+  assert(str(cs.space_group_info())=='P 1')
+
+  cs = crystal_symmetry_from_any.from_string(killer)
+  assert(cs is None)
+
+  cs = crystal_symmetry_from_any.from_string(killer2)
+  assert(cs is None)
+
+  cs = crystal_symmetry_from_any.from_string(killer3)
+  assert(cs is None)
+
+  cs = crystal_symmetry_from_any.from_string(killer4)
+  assert(cs is None)
+
+  print "OK"
+
+if __name__=="__main__":
+  exercise()
--- iotbx/run_tests.py
+++ iotbx/run_tests.py
@@ -6,6 +6,7 @@ def run():
   "$D/iotbx/tst_wildcard.py",
   "$D/iotbx/tst_simple_parser.py",
   "$D/iotbx/tst_phil.py",
+  "$D/iotbx/tst_crystal_symmetry_from_any.py",
   "$D/iotbx/kriber/tst_strudat.py",
   "$D/iotbx/pdb/tst_pdb.py",
   "$D/iotbx/cns/space_group_symbols.py",
