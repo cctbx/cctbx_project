a6db31ce16107bb803439500d7963291f18f47dc
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-07-20 19:03:12 +0000

    signatures() built-in

--- libtbx/libtbx/forward_compatibility.py
+++ libtbx/libtbx/forward_compatibility.py
@@ -1,4 +1,5 @@
 from __future__ import generators
+import inspect
 
 __builtins__.setdefault("False", 0)
 __builtins__.setdefault("True", 1)
@@ -23,3 +24,25 @@ if (__builtins__.get("_enumerate", None) is None):
       yield (i, it.next())
       i += 1
   __builtins__["enumerate"] = enumerate
+
+# libtbx enhancement
+if (__builtins__.get("signatures", None) is None):
+  def signatures(function_object):
+    signatures = getattr(function_object, "__signatures__", None)
+    if (signatures is None):
+      im_func = getattr(function_object, "im_func", None)
+      if (im_func is not None):
+        signatures = getattr(im_func, "__signatures__", None)
+    if (signatures is not None):
+      return signatures
+    name = []
+    try: module = inspect.getmodule(function_object)
+    except KeyboardInterrupt: raise
+    except: pass
+    else:
+      if (module is not None):
+        name.append(module.__name__)
+    name.append(function_object.__name__)
+    return [".".join(name) + inspect.formatargspec(
+      *inspect.getargspec(function_object))]
+  __builtins__["signatures"] = signatures
