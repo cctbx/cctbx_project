e1e3342d7721f9c3850f96146599bfb2ddac7ef8
Jacob thechao Smith <jaroslov@users.noreply.github.com>
2005-09-12 18:45:27 +0000

    Updates to make basic_map support work with MIPS, MacOSX, etc.

--- cctbx/maptbx/boost_python/mapper.cpp
+++ cctbx/maptbx/boost_python/mapper.cpp
@@ -14,27 +14,30 @@ namespace {
 
 struct mapper_wrappers {
 
-  typedef double                      FloatType;
-  typedef signed long                    IntType;
+  typedef double                                           FloatType;
+  typedef signed long                                      IntType;
 
-  typedef fractional<FloatType>              fractional;
-  typedef grid_point<IntType>                unit_shift;
-  typedef IntType                      symmetry_operation;
-  typedef scitbx::mat3<FloatType>              matrix3;
-  typedef sgtbx::space_group                space_group;
-  typedef crystal::direct_space_asu::float_asu<FloatType>  float_asu;
+  typedef fractional<FloatType>                            fractional;
+  typedef grid_point<IntType>                              unit_shift;
+  typedef IntType                                          symmetry_operation;
+  typedef scitbx::mat3<FloatType>                          matrix3;
+  typedef sgtbx::space_group                               space_group;
+//  I have expressed my dislike for MIPS elsewhere
+//  later on you will see various "double"s sprinkled through,
+//  feel free to consider these "FloatType" instead. Oy.
+//  typedef crystal::direct_space_asu::float_asu<FloatType>  float_asu;
 
-  typedef basic_mapper<void,FloatType,IntType>      void_basic_mapper;
-  typedef basic_mapper<non_symmetric,FloatType,IntType>  ns_basic_mapper;
-  typedef basic_mapper<unit_cell,FloatType,IntType>    uc_basic_mapper;
-  typedef basic_mapper<asu,FloatType,IntType>        as_basic_mapper;
+  typedef basic_mapper<void,FloatType,IntType>             void_basic_mapper;
+  typedef basic_mapper<non_symmetric,FloatType,IntType>    ns_basic_mapper;
+  typedef basic_mapper<unit_cell,FloatType,IntType>        uc_basic_mapper;
+  typedef basic_mapper<asu,FloatType,IntType>              as_basic_mapper;
 
-  typedef mapper_factory<void,FloatType,IntType>      vv_factory;
+  typedef mapper_factory<void,FloatType,IntType>           vv_factory;
   typedef mapper_factory<non_symmetric,FloatType,IntType>  ns_factory;
-  typedef mapper_factory<unit_cell,FloatType,IntType>    uc_factory;
-  typedef mapper_factory<asu,FloatType,IntType>      as_factory;
+  typedef mapper_factory<unit_cell,FloatType,IntType>      uc_factory;
+  typedef mapper_factory<asu,FloatType,IntType>            as_factory;
 
-  typedef chiltbx::handle::handle<vv_factory>        factory_handle;
+  typedef chiltbx::handle::handle<vv_factory>              factory_handle;
 
     static void_basic_mapper
     get_non_symmetric_mapper ( fractional const& f ) {
@@ -46,7 +49,7 @@ struct mapper_wrappers {
     }
     static void_basic_mapper
     get_asu_mapper ( fractional const& f,
-        space_group const& sg, float_asu const& fa ) {
+        space_group const& sg, crystal::direct_space_asu::float_asu<double> const& fa ) {
         return as_basic_mapper(f,sg,fa);
     }
 
@@ -63,34 +66,36 @@ struct mapper_wrappers {
       .add_property("symmetry_operation",
         make_getter(&void_basic_mapper::symmetry_operation, rbv()));
 
-        def("get_non_symmetric_mapper",
-            &mapper_wrappers::get_non_symmetric_mapper,
-            arg_("coordinate"));
+    def("get_non_symmetric_mapper",
+      &mapper_wrappers::get_non_symmetric_mapper,
+      arg_("coordinate"));
 
-        def("get_unit_cell_mapper",
-            &mapper_wrappers::get_unit_cell_mapper,
-            arg_("coordinate"));
+    def("get_unit_cell_mapper",
+      &mapper_wrappers::get_unit_cell_mapper,
+      arg_("coordinate"));
 
-        def("get_asu_mapper",
-            &mapper_wrappers::get_asu_mapper,
-            (arg_("coordinate")
-            ,arg_("space_group")
-            ,arg_("float_asu")));
+    def("get_asu_mapper",
+      &mapper_wrappers::get_asu_mapper,
+      (arg_("coordinate")
+      ,arg_("space_group")
+      ,arg_("float_asu")));
 
     class_<factory_handle>("factory_handle",no_init);
 
-    class_<ns_factory>("non_symmetric_factory",init<>())
+    class_<ns_factory>("non_symmetric_factory",no_init)
+      .def(init<>())
       .def("map",&ns_factory::map,arg_("coordinate"))
-            .def("as_handle",&ns_factory::as_handle);
+      .def("as_handle",&ns_factory::as_handle);
 
-    class_<uc_factory>("unit_cell_factory",init<>())
+    class_<uc_factory>("unit_cell_factory",no_init)
+      .def(init<>())
       .def("map",&uc_factory::map,arg_("coordinate"))
-            .def("as_handle",&uc_factory::as_handle);
+      .def("as_handle",&uc_factory::as_handle);
 
     class_<as_factory>("asu_factory",no_init)
-      .def(init<space_group,float_asu>())
+      .def(init<space_group,crystal::direct_space_asu::float_asu<double> >())//float_asu>())
       .def("map",&as_factory::map,arg_("coordinate"))
-            .def("as_handle",&as_factory::as_handle);
+      .def("as_handle",&as_factory::as_handle);
 
   }
 };
