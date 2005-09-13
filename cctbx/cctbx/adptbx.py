9ca78d59cbcf1fd569575c375bbe62b155b5d999
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-09-13 23:45:20 +0000

    bug fix in random_rotate_ellipsoid(): deg=True

--- cctbx/cctbx/adptbx.py
+++ cctbx/cctbx/adptbx.py
@@ -13,7 +13,7 @@ mtpss = mtps**2
 
 def random_rotate_ellipsoid(u_cart):
   c = scitbx.math.euler_angles_as_matrix(
-    [random.uniform(0,360) for i in xrange(3)]).elems
+    [random.uniform(0,360) for i in xrange(3)], deg=True).elems
   return c_u_c_transpose(c, u_cart)
 
 def random_u_cart(u_scale=1, u_min=0):
