8705efc2fa42b4f9a3e179c6464ea711b8817446
Pavel Afonine <afonine@users.noreply.github.com>
2005-09-22 20:16:14 +0000

    big changes: model.py

--- cctbx/cctbx/xray/structure.py
+++ cctbx/cctbx/xray/structure.py
@@ -299,10 +299,12 @@ class structure(crystal.special_position_settings):
       site_symmetry_table=self._site_symmetry_table.select(selection),
       scattering_dict=self._scattering_dict)
 
+
   def select_inplace(self, selection):
     assert self.scatterers() is not None
     self._scatterers          = self._scatterers.select(selection)
     self._site_symmetry_table = self._site_symmetry_table.select(selection)
+    self._scattering_dict_is_out_of_date = True
 
   def add_scatterer(self, scatterer, site_symmetry_ops=None):
     self._scatterers.append(scatterer)
