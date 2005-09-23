0099b99f1d4c74712412b85db469517518d79480
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-09-23 01:15:35 +0000

    Much better error message if SCons is not available.

--- libtbx/libtbx/command_line/scons.py
+++ libtbx/libtbx/command_line/scons.py
@@ -1,16 +1,10 @@
+from libtbx.utils import Sorry
+from libtbx.str_utils import show_string
 import libtbx.load_env
 import time
 import sys, os
 
 time_start = time.time()
-
-engine = os.path.join(libtbx.env.scons_dist_path, "engine")
-if (not os.path.isdir(engine)):
-  engine = os.path.join(libtbx.env.scons_dist_path, "src", "engine")
-if (os.path.isdir(engine)):
-  sys.path.insert(0, engine)
-  try: import SCons
-  except: del sys.path[0]
 
 def show_times():
   t = os.times()
@@ -33,12 +27,43 @@ def show_times():
     print "%d minutes %.2f seconds (%.2f seconds total)" % (
       m, s, wall_clock_time)
 
+def find_scons_engine_path():
+  join = os.path.join
+  isdir = os.path.isdir
+  if (libtbx.env.scons_dist_path is not None):
+    result = join(libtbx.env.scons_dist_path, "engine")
+    if (isdir(result)): return result
+    result = join(libtbx.env.scons_dist_path, "src", "engine")
+    if (isdir(result)): return result
+  for path in libtbx.env.repository_paths:
+    result = join(path, "scons", "engine")
+    if (isdir(result)): return result
+    result = join(path, "scons", "src", "engine")
+    if (isdir(result)): return result
+  return None
+
 def run():
+  engine_path = find_scons_engine_path()
+  if (engine_path is not None):
+    sys.path.insert(0, engine_path)
+    try: import SCons
+    except ImportError: del sys.path[0]
+  try: import SCons.Script
+  except ImportError:
+    msg = ["SCons is not available.",
+      "  A possible solution is to unpack a SCons distribution in",
+      "  one of these directories:"]
+    for path in libtbx.env.repository_paths:
+      msg.append("    " + show_string(path))
+    msg.extend([
+      "  SCons distributions are available at this location:",
+      "    http://www.scons.org/",
+      "  It may be necessary to rename the unpacked distribution, e.g.:",
+      "    mv scons-0.96.1 scons"])
+    raise Sorry("\n".join(msg))
   import atexit
   atexit.register(show_times)
-
-  from SCons import Script
-  Script.main()
+  SCons.Script.main()
 
 if (__name__ == "__main__"):
   run()
