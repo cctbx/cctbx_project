994964329d20964644dbcaf7c82d1ea07b0911c4
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-09-28 00:35:40 +0000

    "--optional" option -> print None instead of raising an exception of the module was not found

--- libtbx/libtbx/command_line/find_in_repositories.py
+++ libtbx/libtbx/command_line/find_in_repositories.py
@@ -4,12 +4,16 @@ from libtbx.utils import Sorry
 import sys, os
 
 def run(args, this_command="libtbx.find_in_repositories"):
+  optional = False
+  if ("--optional" in args):
+    optional = True
+    args.remove("--optional")
   if (len(args) != 1):
-    raise Sorry("usage: %s name" % this_command)
+    raise Sorry("usage: %s [--optional] name" % this_command)
   name = args[0]
   result = libtbx.env.find_in_repositories(
     relative_path=name, test=os.path.exists)
-  if (result is None):
+  if (result is None and not optional):
     raise Sorry("%s: cannot locate %s" % (this_command, show_string(name)))
   print result
 
