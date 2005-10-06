07aadd69ab22ab7e2679009d639f63aae489e6a4
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-10-06 02:28:35 +0000

    more helpful error message if open fails

--- libtbx/libtbx/easy_pickle.py
+++ libtbx/libtbx/easy_pickle.py
@@ -1,7 +1,14 @@
+from libtbx.str_utils import show_string
 import cPickle
 
+def _open(file_name, mode):
+  try: f = open(file_name, mode)
+  except IOError, e:
+    raise IOError("Cannot open pickle file %s (%s)" % (
+      show_string(file_name), str(e)))
+
 def dump(file_name, obj):
-  return cPickle.dump(obj, open(file_name, "wb"), 1)
+  return cPickle.dump(obj, _open(file_name, "wb"), 1)
 
 def load(file_name):
-  return cPickle.load(open(file_name, "rb"))
+  return cPickle.load(_open(file_name, "rb"))
