b5f42df43350bd2feff217ed0cb1062b94631c23
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-11-03 20:25:22 +0000

    generalization of exp_i_alpha_derivatives.py -> g_exp_i_alpha_derivatives.py

---
+++ cctbx/cctbx/examples/tst_g_exp_i_alpha_derivatives.py
@@ -0,0 +1,91 @@
+from cctbx.examples.exp_i_alpha_derivatives import least_squares
+from cctbx.examples.g_exp_i_alpha_derivatives \
+  import parameters, gradients, pack_gradients, g_exp_i_alpha_sum
+from libtbx.test_utils import approx_equal
+import random
+import math
+import copy
+from cStringIO import StringIO
+import sys
+
+random.seed(0)
+
+def d_target_d_params_finite(obs, params, eps=1.e-8):
+  result = []
+  params_eps = copy.deepcopy(params)
+  for i_param in xrange(len(params)):
+    dx = []
+    for ix in [0,1]:
+      ts = []
+      for signed_eps in [eps, -eps]:
+        pi_eps = params[i_param].as_list()
+        pi_eps[ix] += signed_eps
+        params_eps[i_param] = parameters(*pi_eps)
+        exp_sum = g_exp_i_alpha_sum(params=params_eps)
+        target = least_squares(obs=obs, calc=exp_sum.f())
+        ts.append(target.f())
+      dx.append((ts[0]-ts[1])/(2*eps))
+    result.append(gradients(*dx))
+    params_eps[i_param] = params[i_param]
+  return result
+
+def d2_target_d_params_finite(obs, params, eps=1.e-8):
+  result = []
+  params_eps = copy.deepcopy(params)
+  for i_param in xrange(len(params)):
+    for ix in [0,1]:
+      gs = []
+      for signed_eps in [eps, -eps]:
+        pi_eps = params[i_param].as_list()
+        pi_eps[ix] += signed_eps
+        params_eps[i_param] = parameters(*pi_eps)
+        exp_sum = g_exp_i_alpha_sum(params=params_eps)
+        target = least_squares(obs=obs, calc=exp_sum.f())
+        dp = exp_sum.d_target_d_params(target=target)
+        gs.append(pack_gradients(dp))
+      result.append([(gp-gm)/(2*eps) for gp,gm in zip(gs[0],gs[1])])
+    params_eps[i_param] = params[i_param]
+  return result
+
+def compare_analytical_and_finite(obs, params, out):
+  grads_fin = d_target_d_params_finite(obs=obs, params=params)
+  print >> out, "grads_fin:", pack_gradients(grads_fin)
+  exp_sum = g_exp_i_alpha_sum(params=params)
+  target = least_squares(obs=obs, calc=exp_sum.f())
+  grads_ana = exp_sum.d_target_d_params(target=target)
+  print >> out, "grads_ana:", pack_gradients(grads_ana)
+  assert approx_equal(pack_gradients(grads_ana), pack_gradients(grads_fin))
+  curvs_fin = d2_target_d_params_finite(obs=obs, params=params)
+  print >> out, "curvs_fin:", curvs_fin
+  curvs_ana = exp_sum.d2_target_d_params(target=target)
+  print >> out, "curvs_ana:", curvs_ana
+  assert approx_equal(curvs_ana, curvs_fin)
+  print >> out
+
+def exercise(args):
+  verbose =  "--verbose" in args
+  if (not verbose):
+    out = StringIO()
+  else:
+    out = sys.stdout
+  for n_params in xrange(2,5):
+    for i_trial in xrange(5):
+      params = []
+      for i in xrange(n_params):
+        params.append(parameters(
+          g=random.random()*10,
+          alpha=2*math.pi*random.random()))
+      exp_sum = g_exp_i_alpha_sum(params=params)
+      obs = abs(exp_sum.f())
+      compare_analytical_and_finite(
+        obs=obs,
+        params=params,
+        out=out)
+      compare_analytical_and_finite(
+        obs=obs*(random.random()+0.5),
+        params=params,
+        out=out)
+  print "OK"
+
+if (__name__ == "__main__"):
+  exercise(sys.argv[1:])
--- cctbx/run_examples.py
+++ cctbx/run_examples.py
@@ -8,6 +8,7 @@ def run():
   "$D/cctbx/examples/analyze_adp.py",
   "$D/cctbx/examples/g_exp_i_partial_derivatives.py",
   "$D/cctbx/examples/tst_exp_i_alpha_derivatives.py",
+  "$D/cctbx/examples/tst_g_exp_i_alpha_derivatives.py",
   ["$D/cctbx/examples/all_axes.py", "P31"],
   ["$D/cctbx/examples/tst_phase_o_phrenia.py", "P2"],
   "$D/cctbx/examples/map_skewness.py",
