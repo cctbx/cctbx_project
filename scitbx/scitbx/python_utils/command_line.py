e0405abd54b589465605b2a015683955cf358e37
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-11-10 08:53:59 +0000

    new parse_options case_sensitive argument

--- scitbx/scitbx/python_utils/command_line.py
+++ scitbx/scitbx/python_utils/command_line.py
@@ -1,10 +1,13 @@
 class parse_options(object):
 
-  def __init__(self, argv, keywords):
+  def __init__(self, argv, keywords, case_sensitive=True):
     self.keywords = keywords
     self.n = 0
+    keywords_lower = []
     for keyword in keywords:
       setattr(self, keyword, False)
+      if (not case_sensitive):
+        keywords_lower.append(keyword.lower())
     self.regular_args = []
     for arg in argv:
       if (not arg.startswith("--")):
@@ -12,16 +15,24 @@ class parse_options(object):
         continue
       flds = arg[2:].split("=")
       assert len(flds) in (1,2)
-      if (not flds[0] in keywords):
-        raise RuntimeError, "Unknown option: " + arg
+      try:
+        if (case_sensitive):
+          i = keywords.index(flds[0])
+        else:
+          i = keywords_lower.index(flds[0].lower())
+      except ValueError:
+        raise RuntimeError("Unknown option: " + arg)
       if (len(flds) == 1):
-        setattr(self, flds[0], True)
+        setattr(self, keywords[i], True)
       else:
-        setattr(self, flds[0], flds[1])
+        setattr(self, keywords[i], flds[1])
       self.n += 1
 
-def parse_options_with_chunk(argv, keywords=[]):
-  flags = parse_options(argv, ["ChunkSize", "ChunkMember"] + keywords)
+def parse_options_with_chunk(argv, keywords=[], case_sensitive=True):
+  flags = parse_options(
+    argv=argv,
+    keywords=["ChunkSize", "ChunkMember"] + keywords,
+    case_sensitive=case_sensitive)
   if (flags.ChunkSize == False):
     flags.ChunkSize = 1
   else:
