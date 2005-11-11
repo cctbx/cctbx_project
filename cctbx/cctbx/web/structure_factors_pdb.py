fe3d6acece2a40e8a66e659cae31dae6b769642a
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-11-11 06:19:20 +0000

    make algorithm handling similar to that in latest structure_factors.py; cosmetic changes

--- cctbx/cctbx/web/structure_factors_pdb.py
+++ cctbx/cctbx/web/structure_factors_pdb.py
@@ -11,32 +11,44 @@ def interpret_form_data(form):
   return inp
 
 def run(server_info, inp, status):
+  print "<h2>Structure factor calculation</h2>"
   print "<pre>"
-  print "Structure factors calculation\n"
-  print "Algorithm: %s\n" % inp.algorithm
-  print "Formfactors: %s\n" % inp.form_factors
-  print "Minimum d-spacing (highest resolution): %s\n" % inp.d_min
+  print "Atomic form factors: %s" % inp.form_factors
+  print
+  print "Minimum d-spacing (highest resolution): %s" % inp.d_min
   if (float(inp.d_min) <= 0.):
-    raise ValueError, "d-spacing must be greater than zero."
+    raise ValueError("d-spacing must be greater than zero.")
   print
 
   structure = io_utils.structure_from_inp_pdb(inp, status)
   structure.scattering_dict(table = inp.form_factors,
                             d_min = float(inp.d_min))
-  print "Input model summary:\n"
-  structure.show_summary()
-  structure.scattering_dict().show_summary()
-  f_calc = structure.structure_factors(anomalous_flag = False,
-                                       algorithm      = inp.algorithm,
-                                       d_min          = float(inp.d_min)).f_calc()
-  print "\nNumber of Miller indices:", f_calc.indices().size()
+  print "Input model summary:"
   print
-  status.in_table = True
+  structure.show_summary()
+  print
+  structure.scattering_dict().show()
+  print
+  algorithm = inp.algorithm
+  if (algorithm == "automatic"):
+    if (structure.scatterers().size() <= 100):
+      algorithm = "direct"
+    else:
+      algorithm = None
+  elif (algorithm not in ["direct", "fft"]):
+    algorithm = None
+  f_calc_manager = structure.structure_factors(
+    anomalous_flag = False,
+    algorithm      = algorithm,
+    d_min          = float(inp.d_min))
+  f_calc = f_calc_manager.f_calc()
+  print "Number of Miller indices:", f_calc.indices().size()
+  print
+  print "Structure factor algorithm:", f_calc_manager.algorithm(verbose=True)
+  print
   print "    h     k     l           Fcalc      Pcalc"
-  for i,h in enumerate(f_calc.indices()):
-    print "%5d %5d %5d %15.6f %10.4f" % (
-      h + complex_math.abs_arg(f_calc.data()[i], deg=True))
-  status.in_table = False
+  for h,f in zip(f_calc.indices(), f_calc.data()):
+    print "%5d %5d %5d %15.6f %8.2f" % (h + complex_math.abs_arg(f, deg=True))
   print
 
   print "</pre>"
