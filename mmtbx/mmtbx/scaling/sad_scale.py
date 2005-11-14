6c2dfc9b21bca7504f6b87d4bd10932f62d485d5
Peter Zwart <phzwart@users.noreply.github.com>
2005-11-14 22:45:58 +0000

    Scaling for SAD data, almost the same as hyss, but includes local scaling (optional) as well as a multitude of outlier rejection options

---
+++ mmtbx/mmtbx/scaling/sad_scale.py
@@ -0,0 +1,332 @@
+from cctbx import maptbx
+from cctbx import miller
+from cctbx import crystal
+from cctbx import sgtbx
+from cctbx import adptbx
+import cctbx.sgtbx.lattice_symmetry
+import cctbx.sgtbx.cosets
+from cctbx.array_family import flex
+from libtbx.utils import Sorry, date_and_time, multi_out
+import iotbx.phil
+from iotbx import reflection_file_reader
+from iotbx import reflection_file_utils
+from iotbx import crystal_symmetry_from_any
+import mmtbx.scaling
+from mmtbx.scaling import absolute_scaling, relative_scaling
+from mmtbx.scaling import matthews, twin_analyses
+from mmtbx.scaling import basic_analyses, data_statistics,pair_analyses
+import libtbx.phil.command_line
+from cStringIO import StringIO
+from scitbx.python_utils import easy_pickle
+from mmtbx.scaling import pre_scale
+import sys, os
+
+
+from mmtbx.scaling import fa_estimation
+
+
+master_params = iotbx.phil.parse("""\
+scaling.input {
+   basic{
+     n_residues=None
+     .type=float
+     n_bases=None
+     .type=float
+     n_copies_per_asu=None
+     .type=float
+   }
+
+   xray_data{
+
+     unit_cell=None
+     .type=unit_cell
+
+     space_group=None
+     .type=space_group
+
+     reference{
+       file_name=None
+       .type=path
+       labels=None
+       .type=strings
+     }
+
+   }
+
+   scaling_strategy{
+
+       pre_scaler_protocol{
+         high_resolution=None
+         .type=float
+         low_resolution=None
+         .type=float
+         aniso_correction=True
+         .type=bool
+         outlier_level_wilson=1e-6
+         .type=float
+         outlier_level_extreme=1e-2
+         .type=float
+       }
+
+       ano_protocol{
+         target = loc *None
+         .type=choice
+         iterations = *auto specified_by_max_iterations
+         .type=choice
+         max_iterations = 3
+         .type=int
+
+         least_squares_options{
+           use_experimental_sigmas=True
+           .type=bool
+           scale_data=*intensities amplitudes
+           .type=choice
+           scale_target=basic *fancy
+           .type=choice
+         }
+
+         local_scaling_options{
+           use_experimental_sigmas=True
+           .type=bool
+           scale_data=*intensities amplitudes
+           .type=choice
+           scale_target=*local_moment local_lsq
+           .type=choice
+           max_depth=10
+           .type=int
+           target_neighbours=100
+           .type=int
+           neighbourhood_sphere=1
+           .type=int
+         }
+
+         outlier_rejection_options{
+           cut_level_sigma=3
+           .type=float
+           cut_level_rms_primary=4
+           .type=float
+           cut_level_rms_secondary=5
+           .type=float
+           protocol=solve rms *rms_and_sigma
+           .type=choice
+         }
+     }
+
+   }
+
+   output{
+     log = 'logfile.log'
+     .type = path
+
+     hklout = 'test.mtz'
+     .type = path
+     outlabel = '_SAD'
+     .type = str
+   }
+
+}
+""")
+
+
+def run(args):
+
+  if len(args)==0:
+    print "no help available"
+  elif ( "--help" in args ):
+    print "no help available"
+  elif ( "--h" in args ):
+    print "no help available"
+  else:
+    log = multi_out()
+    if (not "--quiet" in args):
+      log.register(label="stdout", file_object=sys.stdout)
+    string_buffer = StringIO()
+    string_buffer_plots = StringIO()
+    log.register(label="log_buffer", file_object=string_buffer)
+
+    log_plots = StringIO()
+    print >> log,"#phil __OFF__"
+    print >> log
+    print >> log, date_and_time()
+    print >> log
+    print >> log
+
+    phil_objects = []
+    argument_interpreter = libtbx.phil.command_line.argument_interpreter(
+      master_params=master_params,
+      home_scope="scaling")
+
+    reflection_file = None
+
+    for arg in args:
+      command_line_params = None
+      arg_is_processed = False
+      if arg == '--quiet':
+        arg_is_processed = True
+        ## The associated action with this keyword is implemented above
+      if (os.path.isfile(arg)): ## is this a file name?
+        ## Check if this is a phil file
+        try:
+          command_line_params = iotbx.phil.parse(file_name=arg)
+        except KeyboardInterrupt: raise
+        except : pass
+        if command_line_params is not None:
+            phil_objects.append(command_line_params)
+            arg_is_processed = True
+        ## Check if this file is a reflection file
+        if command_line_params is None:
+          reflection_file = reflection_file_reader.any_reflection_file(
+            file_name=arg, ensure_read_access=False)
+        if (reflection_file is not None):
+          reflection_file = arg
+          arg_is_processed = True
+      ## If it is not a file, it must be a phil command
+      else:
+        try:
+          command_line_params = argument_interpreter.process(arg=arg)
+          if command_line_params is not None:
+            phil_objects.append(command_line_params)
+            arg_is_processed = True
+        except KeyboardInterrupt: raise
+        except : pass
+
+      if not arg_is_processed:
+        print >> log, "##----------------------------------------------##"
+        print >> log, "## Unknown phil-file or phil-command:", arg
+        print >> log, "##----------------------------------------------##"
+        print >> log
+        raise Sorry("Unknown file format or phil command: %s" % arg)
+
+
+    effective_params = master_params.fetch(sources=phil_objects)
+    params = effective_params.extract()
+
+    effective_params = master_params.fetch(sources=phil_objects)
+    new_params = master_params.format(python_object=params)
+    print >> log, "Effective parameters"
+    print >> log, "#phil __ON__"
+    new_params.show(out=log)
+    print >> log, "#phil __END__"
+    print >> log
+
+    ## Now please read in the reflections files
+
+    ## get symmetry and cell data first please
+    ## By default, the native cell and symmetry are used
+    ## as reference
+    crystal_symmetry_nat = None
+    crystal_symmetry_nat = crystal_symmetry_from_any.extract_from(
+      file_name=params.scaling.input.xray_data.reference.file_name)
+
+    if params.scaling.input.xray_data.space_group is None:
+      params.scaling.input.xray_data.space_group =\
+        crystal_symmetry_nat.space_group_info()
+      print >> log, "Using symmetry of native data"
+
+    if params.scaling.input.xray_data.unit_cell is None:
+      params.scaling.input.xray_data.unit_cell =\
+        crystal_symmetry_nat.unit_cell()
+      print >> log, "Using cell of native data"
+
+    ## Check if a unit cell is defined
+    if params.scaling.input.xray_data.space_group is None:
+      raise Sorry("No space group defined")
+    if params.scaling.input.xray_data.unit_cell is None:
+      raise Sorry("No unit cell defined")
+
+
+    crystal_symmetry = crystal_symmetry = crystal.symmetry(
+      unit_cell =  params.scaling.input.xray_data.unit_cell,
+      space_group_symbol = str(
+        params.scaling.input.xray_data.space_group) )
+
+
+    effective_params = master_params.fetch(sources=phil_objects)
+    new_params = master_params.format(python_object=params)
+    print >> log, "Effective parameters"
+    print >> log, "#phil __ON__"
+    new_params.show(out=log)
+    print >> log, "#phil __END__"
+    print >> log
+
+    ## define a xray data server
+    xray_data_server =  reflection_file_utils.reflection_file_server(
+      crystal_symmetry = crystal_symmetry,
+      force_symmetry = True,
+      reflection_files=[])
+
+    ## Read in native data and make appropriatre selections
+    miller_array_native = None
+    miller_array_native = xray_data_server.get_xray_data(
+      file_name = params.scaling.input.xray_data.reference.file_name,
+      labels = params.scaling.input.xray_data.reference.labels,
+      ignore_all_zeros = True,
+      parameter_scope = 'scaling.input.SIR_scale.xray_data.native'
+    )
+    info_native = miller_array_native.info()
+    miller_array_native=miller_array_native.map_to_asu().select(
+      miller_array_native.indices()!=(0,0,0) )
+    miller_array_native = miller_array_native.select(
+      miller_array_native.data() > 0 )
+    ## Convert to amplitudes
+    if (miller_array_native.is_xray_intensity_array()):
+      miller_array_native = miller_array_native.f_sq_as_f()
+    elif (miller_array_native.is_complex_array()):
+      miller_array_native = abs(miller_array_native)
+    if not miller_array_native.is_real_array():
+      raise Sorry("miller_array_native is not a real array")
+    miller_array_native.set_info(info = info_native)
+
+
+    ## Print info
+    print >> log
+    print >> log, "Reference data"
+    print >> log, "=============="
+    miller_array_native.show_comprehensive_summary(f=log)
+    print >> log
+    native_pre_scale = pre_scale.pre_scaler(
+      miller_array_native,
+      params.scaling.input.scaling_strategy.pre_scaler_protocol,
+      params.scaling.input.basic)
+    miller_array_native =  native_pre_scale.x1.deep_copy()
+    del native_pre_scale
+
+    scaler = fa_estimation.ano_scaling(
+      miller_array_native,
+      params.scaling.input.scaling_strategy.ano_protocol)
+
+    positive_miller = scaler.x1p.deep_copy()
+    negative_miller = scaler.x1n.deep_copy()
+
+    print >> log
+    print >> log, "Making delta f's"
+    print >> log, "----------------"
+    print >> log
+
+    delta_gen = pair_analyses.delta_generator( positive_miller,
+                                               negative_miller )
+    print >> log
+    print >> log, "writing mtz file"
+    print >> log, "----------------"
+    print >> log
+
+    ## some assertions to make sure nothing went weerd
+    assert positive_miller.observation_type() is not None
+    assert negative_miller.observation_type() is not None
+    assert delta_gen.abs_delta_f.observation_type() is not None
+
+    ## Please write out the abs_delta_f array
+
+    mtz_dataset = delta_gen.abs_delta_f.as_mtz_dataset(
+      column_root_label='F'+params.scaling.input.output.outlabel)
+    mtz_dataset.mtz_object().write(
+      file_name=params.scaling.input.output.hklout)
+
+
+
+
+
+
+
+if (__name__ == "__main__"):
+  run(sys.argv[1:])
