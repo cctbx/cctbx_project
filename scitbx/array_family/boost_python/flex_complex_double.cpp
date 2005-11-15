23e5f79abacfe811394f925892df75f32d6eef28
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-11-15 03:34:33 +0000

    new flex.complex_double.matrix_transpose(), matrix_paste_block_in_place methods()

--- scitbx/array_family/boost_python/flex_complex_double.cpp
+++ scitbx/array_family/boost_python/flex_complex_double.cpp
@@ -1,6 +1,8 @@
 #include <scitbx/array_family/boost_python/flex_wrapper.h>
 #include <scitbx/array_family/boost_python/flex_pickle_single_buffered.h>
 #include <scitbx/array_family/boost_python/flex_wrapper_complex.h>
+#include <scitbx/array_family/versa_matrix.h>
+#include <scitbx/matrix/move.h>
 
 namespace scitbx { namespace af { namespace boost_python {
 
@@ -18,11 +20,25 @@ namespace {
 
   void wrap_flex_complex_double()
   {
+    using namespace boost::python;
     flex_wrapper<std::complex<double> >::numeric_common(
       "complex_double", boost::python::scope())
       .def_pickle(flex_pickle_single_buffered<std::complex<double> >())
       .def("__mul__", mul_ac_ar)
       .def("__rmul__", mul_ac_ar)
+      .def("matrix_transpose",
+        (versa<std::complex<double>, c_grid<2> >(*)(
+           const_ref<std::complex<double>, c_grid<2> > const&))
+             matrix_transpose)
+      .def("matrix_paste_block_in_place",
+        (void(*)(
+          ref<std::complex<double>, c_grid<2> > const&,
+          const_ref<std::complex<double>, c_grid<2> > const&,
+          unsigned, unsigned))
+            matrix::paste_block_in_place, (
+              arg_("block"),
+              arg_("i_row"),
+              arg_("i_column")))
     ;
     flex_wrapper_complex_functions<double>::wrap(boost::python::scope());
   }
