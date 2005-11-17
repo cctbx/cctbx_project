2b8070111d52bcb44bd5f5bb2861b485bc09313d
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-11-17 08:30:17 +0000

    new flex.complex_double.matrix_copy_block() method

--- scitbx/array_family/boost_python/flex_complex_double.cpp
+++ scitbx/array_family/boost_python/flex_complex_double.cpp
@@ -30,6 +30,15 @@ namespace {
         (versa<std::complex<double>, c_grid<2> >(*)(
            const_ref<std::complex<double>, c_grid<2> > const&))
              matrix_transpose)
+      .def("matrix_copy_block",
+        (versa<std::complex<double>, c_grid<2> >(*)(
+          const_ref<std::complex<double>, c_grid<2> > const&,
+          unsigned, unsigned, unsigned, unsigned))
+            matrix::copy_block, (
+              arg_("i_row"),
+              arg_("i_column"),
+              arg_("n_rows"),
+              arg_("n_columns")))
       .def("matrix_paste_block_in_place",
         (void(*)(
           ref<std::complex<double>, c_grid<2> > const&,
