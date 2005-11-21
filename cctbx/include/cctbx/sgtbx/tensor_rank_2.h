5656708b3955a527fb249cd9dd5a7cba9a48276c
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-11-21 21:41:26 +0000

    reduce size of constraints instances + other optimizations

--- cctbx/include/cctbx/sgtbx/tensor_rank_2.h
+++ cctbx/include/cctbx/sgtbx/tensor_rank_2.h
@@ -2,9 +2,8 @@
 #define CCTBX_SGTBX_TENSOR_RANK_2
 
 #include <cctbx/sgtbx/site_symmetry.h>
-#include <scitbx/matrix/row_echelon.h>
-#include <scitbx/array_family/versa.h>
-#include <scitbx/array_family/accessors/c_grid.h>
+#include <boost/shared_array.hpp>
+#include <boost/scoped_array.hpp>
 
 namespace cctbx { namespace sgtbx {
 
@@ -29,17 +28,19 @@ namespace tensor_rank_2 {
     int* c);
 
   //! Handling of symmetry constraints for tensors of rank 2.
+  /*! Example:
+        http://phenix-online.org/cctbx_sources/cctbx/cctbx/examples/adp_symmetry_constraints.py
+   */
   template <typename FloatType=double>
   class constraints
   {
     protected:
-      af::versa<int, af::c_grid<2> > row_echelon_form_memory;
-      mutable af::versa<FloatType, af::c_grid<2> > gradient_sum_matrix_memory;
-      mutable bool have_gradient_sum_matrix;
-
+      boost::shared_array<int> row_echelon_form_memory;
+      mutable boost::shared_array<FloatType> gradient_sum_matrix_memory;
     public:
       af::small<unsigned, 6> independent_indices;
 
+      //! Default constructor. Some data members are not initialized!
       constraints() {}
 
       constraints(
@@ -76,19 +77,11 @@ namespace tensor_rank_2 {
         initialize(symmetry_matrices, i_first_matrix_to_use, reciprocal_space);
       }
 
-      void
-      initialize(
-        af::const_ref<rt_mx> const& symmetry_matrices,
-        std::size_t i_first_matrix_to_use,
-        bool reciprocal_space);
-
       scitbx::mat_const_ref<int>
       row_echelon_form() const
       {
         return scitbx::mat_const_ref<int>(
-          row_echelon_form_memory.begin(),
-          6-independent_indices.size(),
-          6);
+          row_echelon_form_memory.get(), 6-independent_indices.size(), 6);
       }
 
       std::size_t
@@ -121,38 +114,20 @@ namespace tensor_rank_2 {
         return result;
       }
 
-    protected:
-      void
-      initialize_gradient_sum_matrix() const
-      {
-        gradient_sum_matrix_memory.resize(
-          af::c_grid<2>(independent_indices.size(), 6), 0);
-        FloatType* row = gradient_sum_matrix_memory.begin();
-        scitbx::mat_const_ref<int> rem = row_echelon_form();
-        for(std::size_t i=0;i<independent_indices.size();i++,row+=6) {
-          row[independent_indices[i]] = 1;
-          scitbx::matrix::row_echelon::back_substitution_float(
-            rem, static_cast<const FloatType*>(0), row);
-        }
-        have_gradient_sum_matrix = true;
-      }
-
-    public:
       scitbx::mat_const_ref<FloatType>
       gradient_sum_matrix() const
       {
-        if (!have_gradient_sum_matrix) initialize_gradient_sum_matrix();
+        const FloatType* gsm = gradient_sum_matrix_memory.get();
+        if (gsm == 0) gsm = initialize_gradient_sum_matrix();
         return scitbx::mat_const_ref<FloatType>(
-          gradient_sum_matrix_memory.begin(),
-          independent_indices.size(),
-          6);
+          gsm, independent_indices.size(), 6);
       }
 
       af::small<FloatType, 6>
       independent_gradients(
         scitbx::sym_mat3<FloatType> const& all_gradients) const;
 
-      /*! \brief Efficient implementation of the matrix product:
+      /*! \brief Matrix product:
               gradient_sum_matrix
             * all_curvatures
             * gradient_sum_matrix.transpose()
@@ -172,36 +147,46 @@ namespace tensor_rank_2 {
       af::shared<FloatType>
       independent_curvatures(
         af::const_ref<FloatType> const& all_curvatures) const;
+
+    protected:
+      void
+      initialize(
+        af::const_ref<rt_mx> const& symmetry_matrices,
+        std::size_t i_first_matrix_to_use,
+        bool reciprocal_space);
+
+      const FloatType*
+      initialize_gradient_sum_matrix() const;
   };
 
   template <typename FloatType>
   void
-  constraints<FloatType>::initialize(
+  constraints<FloatType>::
+  initialize(
     af::const_ref<rt_mx> const& symmetry_matrices,
     std::size_t i_first_matrix_to_use,
     bool reciprocal_space)
   {
     CCTBX_ASSERT(i_first_matrix_to_use <= symmetry_matrices.size());
-    row_echelon_form_memory = af::versa<int, af::c_grid<2> >(
-      af::c_grid<2>((symmetry_matrices.size()-i_first_matrix_to_use)*6, 6),
-      af::init_functor_null<int>());
-    scitbx::mat_ref<int> row_echelon_form_ref(
-      row_echelon_form_memory.begin(),
-      row_echelon_form_memory.accessor()[0],
-      row_echelon_form_memory.accessor()[1]);
+    unsigned n_rows = (symmetry_matrices.size() - i_first_matrix_to_use) * 6;
+    boost::shared_array<int> row_echelon_setup_memory(new int[n_rows*6]);
+    scitbx::mat_ref<int> row_echelon_setup(
+      row_echelon_setup_memory.get(), n_rows, 6);
     CCTBX_ASSERT(constraints_raw(
       symmetry_matrices,
       i_first_matrix_to_use,
       reciprocal_space,
-      row_echelon_form_ref.begin()) == row_echelon_form_ref.end());
-    CCTBX_ASSERT(
-      scitbx::matrix::row_echelon::form(row_echelon_form_ref) <= 6);
-    row_echelon_form_memory.resize(af::c_grid<2>(
-      row_echelon_form_ref.n_rows(),
-      row_echelon_form_ref.n_columns()));
+      row_echelon_setup.begin()) == row_echelon_setup.end());
+    n_rows = scitbx::matrix::row_echelon::form(row_echelon_setup);
+    CCTBX_ASSERT(n_rows <= 6);
+    row_echelon_form_memory = boost::shared_array<int>(new int[n_rows*6]);
+    std::copy(
+      row_echelon_setup.begin(),
+      row_echelon_setup.end(),
+      row_echelon_form_memory.get());
     af::tiny<bool, 6> independent_flags;
     scitbx::matrix::row_echelon::back_substitution_int(
-      row_echelon_form_ref,
+      row_echelon_setup,
       static_cast<const int*>(0),
       static_cast<int*>(0),
       independent_flags.begin());
@@ -210,17 +195,36 @@ namespace tensor_rank_2 {
         independent_indices.push_back(i);
       }
     }
-    have_gradient_sum_matrix = false;
+  }
+
+  template <typename FloatType>
+  const FloatType*
+  constraints<FloatType>::
+  initialize_gradient_sum_matrix() const
+  {
+    gradient_sum_matrix_memory = boost::shared_array<FloatType>(
+      new FloatType[independent_indices.size()*6]);
+    FloatType* row = gradient_sum_matrix_memory.get();
+    std::fill_n(
+      row, independent_indices.size()*6, static_cast<FloatType>(0));
+    scitbx::mat_const_ref<int> rem = row_echelon_form();
+    for(std::size_t i=0;i<independent_indices.size();i++,row+=6) {
+      row[independent_indices[i]] = 1;
+      scitbx::matrix::row_echelon::back_substitution_float(
+        rem, static_cast<const FloatType*>(0), row);
+    }
+    return gradient_sum_matrix_memory.get();
   }
 
   template <typename FloatType>
   af::small<FloatType, 6>
-  constraints<FloatType>::independent_gradients(
+  constraints<FloatType>::
+  independent_gradients(
     scitbx::sym_mat3<FloatType> const& all_gradients) const
   {
     af::small<FloatType, 6> result;
-    if (!have_gradient_sum_matrix) initialize_gradient_sum_matrix();
-    const FloatType *row = gradient_sum_matrix_memory.begin();
+    const FloatType* row = gradient_sum_matrix_memory.get();
+    if (row == 0) row = initialize_gradient_sum_matrix();
     for(std::size_t i=0;i<independent_indices.size();i++,row+=6) {
       FloatType indep_grad = 0;
       for(std::size_t j=0;j<6;j++) {
@@ -233,48 +237,20 @@ namespace tensor_rank_2 {
 
   template <typename FloatType>
   af::shared<FloatType>
-  constraints<FloatType>::independent_curvatures(
+  constraints<FloatType>::
+  independent_curvatures(
     af::const_ref<FloatType> const& all_curvatures) const
   {
     CCTBX_ASSERT(all_curvatures.size() == 6*(6+1)/2);
-    static const unsigned sym_mat6_indices[] = {
-      0, 1, 2, 3, 4, 5,
-      1, 6, 7, 8, 9,10,
-      2, 7,11,12,13,14,
-      3, 8,12,15,16,17,
-      4, 9,13,16,18,19,
-      5,10,14,17,19,20
-    };
-    if (!have_gradient_sum_matrix) initialize_gradient_sum_matrix();
-    const FloatType* gsm = gradient_sum_matrix_memory.begin();
+    const FloatType* gsm = gradient_sum_matrix_memory.get();
+    if (gsm == 0) gsm = initialize_gradient_sum_matrix();
     unsigned n_indep = n_independent_params();
-    af::shared<FloatType> result(n_indep*(n_indep+1)/2, 0);
-    std::vector<FloatType> c_times_a(n_indep*6, 0);
-    typename std::vector<FloatType>::iterator ca = c_times_a.begin();
-    unsigned i6 = 0;
-    for (unsigned i=0;i<n_indep;i++,i6+=6) {
-      for (unsigned k=0;k<6;k++) {
-        unsigned i6j = i6;
-        unsigned j6k = k;
-        for (unsigned j=0;j<6;j++,i6j++,j6k+=6) {
-          *ca += gsm[i6j] * all_curvatures[sym_mat6_indices[j6k]];
-        }
-        ca++;
-      }
-    }
-    FloatType* cact = result.begin();
-    i6 = 0;
-    for (unsigned i=0;i<n_indep;i++,i6+=6) {
-      unsigned k6 = i6;
-      for (unsigned k=i;k<n_indep;k++,k6+=6) {
-        unsigned i6j = i6;
-        unsigned k6j = k6;
-        for (unsigned j=0;j<6;j++,i6j++,k6j++) {
-          *cact += c_times_a[i6j] * gsm[k6j];
-        }
-        cact++;
-      }
-    }
+    af::shared<FloatType> result(
+      n_indep*(n_indep+1)/2, af::init_functor_null<FloatType>());
+    boost::scoped_array<FloatType> c_times_a(new FloatType[n_indep*6]);
+    scitbx::matrix::multiply_packed_u_multiply_lhs_transpose(
+      gsm, all_curvatures.begin(), n_indep, 6,
+      &*c_times_a.get(), result.begin());
     return result;
   }
 
