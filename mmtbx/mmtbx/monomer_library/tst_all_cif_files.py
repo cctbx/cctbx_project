5612f3ff0f51d691c43c2a895d66b142fbd4e1ef
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2005-12-07 21:30:27 +0000

    loop over all cif files in monomer library (basic syntax check)

---
+++ mmtbx/mmtbx/monomer_library/tst_all_cif_files.py
@@ -0,0 +1,27 @@
+from mmtbx.monomer_library import server
+import string
+import os
+
+def exercise():
+  list_cif = server.mon_lib_list_cif()
+  srv = server.server(list_cif=list_cif)
+  print "srv.root_path:", srv.root_path
+  n_get_comp_comp_id_successes = 0
+  for first_char in string.lowercase+string.digits:
+    sub_dir = os.path.join(srv.root_path, first_char)
+    if (not os.path.isdir(sub_dir)): continue
+    for node in os.listdir(sub_dir):
+      if (not node.lower().endswith(".cif")): continue
+      comp_id = node[:-4]
+      if (comp_id.upper() != comp_id):
+        print "Mixed case:", os.path.join(first_char, node)
+      comp_comp_id = srv.get_comp_comp_id(comp_id=comp_id)
+      if (comp_comp_id is None):
+        print "Error instantiating comp_comp_id %s (%s)" % (
+          comp_id, os.path.join(sub_dir, node))
+      else:
+        n_get_comp_comp_id_successes += 1
+  print "number of cif files read successfully:", n_get_comp_comp_id_successes
+
+if (__name__ == "__main__"):
+  exercise()
