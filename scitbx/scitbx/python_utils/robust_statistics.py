1a9cb8f6e42d1206ccedf00c1daa9193ec3ce960
Peter Zwart <phzwart@users.noreply.github.com>
2005-12-09 23:52:48 +0000

    Some robust estimators

---
+++ scitbx/scitbx/python_utils/robust_statistics.py
@@ -0,0 +1,41 @@
+from scitbx.array_family import flex
+
+def percentile(x, percent):
+  ## see http://cnx.rice.edu/content/m10805/latest/
+  assert percent >=0.0
+  assert percent <=1.0
+  n = x.size()
+  order = flex.sort_permutation(x)
+  np = float(n+1)*percent
+  ir = int(np)
+  fr = np - int(np)
+  tmp1 = x[ order[ir-1] ]
+  tmp2 = x[ order[ir] ]
+  result = tmp1 + fr*(tmp2-tmp1)
+  return result
+
+def median(x):
+  result=percentile(x, 0.5)
+  return( result )
+
+def hinges(x):
+  h1 = percentile( x, 0.25 )
+  h2 = percentile( x, 0.75 )
+  return h1, h2
+
+def h_spread(x):
+  low, high = hinges( x)
+  return ( high - low )
+
+def trimean(x):
+  h1, h2 = hinges(x)
+  m = median(x)
+  result = (h1+h2+2.0*m)/4.0
+  return result
+
+def trimmed_mean(x,percent):
+  low = percentile(x,percent/2.0)
+  high = percentile(x,1.0-percent/2.0)
+  tmp = x.select ( ( x > low ).iselection() )
+  tmp = tmp.select( ( tmp < high ).iselection() )
+  return flex.mean(tmp)
---
+++ scitbx/scitbx/python_utils/tst_robust.py
@@ -0,0 +1,47 @@
+import math
+from scitbx.array_family import flex
+from scitbx.python_utils import robust_statistics as rs
+
+def tst_median():
+  x = flex.double(range(10))
+  assert rs.median(x)==4.5
+  x = flex.double(range(11))
+  assert rs.median(x)==5.0
+
+def tst_hinges():
+  n = 12
+  x = flex.double(range(n))
+  hinges = rs.hinges(x)
+  assert hinges[0] == 2.25
+  assert hinges[1] == 8.75
+
+def tst_h_spread():
+  n = 12
+  x = flex.double(range(n))
+  assert rs.h_spread(x) == 6.5
+
+def tst_trimean():
+  n = 13
+  x = flex.double(range(n))
+  assert rs.trimean(x) == rs.median(x)
+
+def tst_percentile():
+  n = 10
+  x = flex.double(range(n))
+  assert rs.percentile(x,0.50) == rs.median(x)
+
+def tst_trimmed_mean():
+  n = 10
+  x = flex.double(range(n))
+  assert rs.trimmed_mean(x,0.50)==4.5
+
+def run():
+  tst_median()
+  tst_hinges()
+  tst_trimean()
+  tst_h_spread()
+  tst_percentile()
+  tst_trimmed_mean()
+  print "OK"
+
+run()
