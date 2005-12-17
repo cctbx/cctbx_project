5098cdb7c5350bc6358b5295d57d16cfeec3edf0
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2005-12-17 07:57:02 +0000

    list of windows_device_names

--- libtbx/libtbx/utils.py
+++ libtbx/libtbx/utils.py
@@ -3,6 +3,10 @@ import time
 import traceback
 import sys, os
 
+windows_device_names = """\
+CON PRN AUX NUL COM1 COM2 COM3 COM4 COM5 COM6 COM7 COM8 COM9
+LPT1 LPT2 LPT3 LPT4 LPT5 LPT6 LPT7 LPT8 LPT9""".split()
+
 def flat_list(nested_list):
   result = []
   if (hasattr(nested_list, "__len__")):
