132f5e61096362e3a6eadfdd1eb456f06243614e
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2006-01-02 20:55:13 +0000

    platform_info: sizeof for several types; more macros

--- boost_adaptbx/meta_ext.cpp
+++ boost_adaptbx/meta_ext.cpp
@@ -24,61 +24,101 @@ namespace {
   platform_info()
   {
     std::string result;
+    std::string nl = "\n";
+    result += "__FILE__ = " __FILE__ "\n";
+#if defined(__DATE__)
+    result += "__DATE__ = " __DATE__ "\n";
+#endif
+#if defined(__TIME__)
+    result += "__TIME__ = " __TIME__ "\n";
+#endif
+#if defined(__TIMESTAMP__)
+    result += "__TIMESTAMP__ = " __TIMESTAMP__ "\n";
+#endif
 #if defined(__alpha__)
-    result += " __alpha__";
+    result += "__alpha__\n";
 #endif
 #if defined(__host_mips)
-    result += " __host_mips";
+    result += "__host_mips\n";
 #endif
 #if defined(__i386__)
-    result += " __i386__";
+    result += "__i386__\n";
 #endif
 #if defined(__x86_64__)
-    result += " __x86_64__";
+    result += "__x86_64__\n";
 #endif
 #if defined(__linux)
-    result += " __linux";
+    result += "__linux\n";
 #endif
 #if defined(__osf__)
-    result += " __osf__";
+    result += "__osf__\n";
 #endif
 #if defined(__sgi)
-    result += " __sgi";
+    result += "__sgi\n";
 #endif
 #if defined(_WIN32)
-    result += " _WIN32";
+    result += "_WIN32\n";
 #endif
 #if defined(_WIN64)
-    result += " _WIN64";
+    result += "_WIN64\n";
 #endif
 #if defined(__APPLE_CC__)
-    result += " __APPLE_CC__=" + to_str(__APPLE_CC__);
+    result += "__APPLE_CC__ = " + to_str(__APPLE_CC__) + nl;
 #endif
 #if defined(_COMPILER_VERSION)
-    result += " _COMPILER_VERSION=" + to_str(_COMPILER_VERSION);
+    result += "_COMPILER_VERSION = " + to_str(_COMPILER_VERSION) + nl;
 #endif
 #if defined(__DECCXX_VER)
-    result += " __DECCXX_VER=" + to_str(__DECCXX_VER);
+    result += "__DECCXX_VER = " + to_str(__DECCXX_VER) + nl;
 #endif
 #if defined(__EDG_VERSION__)
-    result += " __EDG_VERSION__=" + to_str(__EDG_VERSION__);
+    result += "__EDG_VERSION__ = " + to_str(__EDG_VERSION__) + nl;
 #endif
 #if defined(__GNUC__)
-    result += " __GNUC__=" + to_str(__GNUC__);
+    result += "__GNUC__ = " + to_str(__GNUC__) + nl;
 #endif
 #if defined(__GNUC_MINOR__)
-    result += " __GNUC_MINOR__=" + to_str(__GNUC_MINOR__);
+    result += "__GNUC_MINOR__ = " + to_str(__GNUC_MINOR__) + nl;
 #endif
 #if defined(__GNUC_PATCHLEVEL__)
-    result += " __GNUC_PATCHLEVEL__=" + to_str(__GNUC_PATCHLEVEL__);
+    result += "__GNUC_PATCHLEVEL__ = " + to_str(__GNUC_PATCHLEVEL__) + nl;
+#endif
+#if defined(__GXX_WEAK__)
+    result += "__GXX_WEAK__ = " + to_str(__GXX_WEAK__) + nl;
+#endif
+#if defined(__IEEE_FLOAT)
+    result += "__IEEE_FLOAT = " + to_str(__IEEE_FLOAT) + nl;
 #endif
 #if defined(__INTEL_COMPILER)
-    result += " __INTEL_COMPILER=" + to_str(__INTEL_COMPILER);
+    result += "__INTEL_COMPILER = " + to_str(__INTEL_COMPILER) + nl;
+#endif
+#if defined(__LP64__)
+    result += "__LP64__ = " + to_str(__LP64__) + nl;
+#endif
+#if defined(_M_IX86)
+    result += "_M_IX86 = " + to_str(_M_IX86) + nl;
+#endif
+#if defined(_MSC_EXTENSIONS)
+    result += "_MSC_EXTENSIONS = " + to_str(_MSC_EXTENSIONS) + nl;
 #endif
 #if defined(_MSC_VER)
-    result += " _MSC_VER=" + to_str(_MSC_VER);
+    result += "_MSC_VER = " + to_str(_MSC_VER) + nl;
 #endif
-    if (result.size() > 0) result = result.substr(1);
+#if defined(__VERSION__)
+    result += "__VERSION__ = " __VERSION__ "\n";
+#endif
+#undef P
+#define P(T) result += "sizeof(" #T ") = " + to_str(sizeof(T)) + nl;
+    P(short)
+    P(int)
+    P(long)
+    P(std::size_t)
+    P(void*)
+    P(long long)
+    P(float)
+    P(double)
+    P(long double)
+#undef P
     return result;
   }
 
