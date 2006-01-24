33d9e42780783e79bf1b067379cd4d14d4b7c1e6
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2006-01-24 14:18:10 +0000

    use TryCompile() to disable compilation if OpenGL headers are not available; new gltbx.try_imports

---
+++ gltbx/command_line/try_imports.py
@@ -0,0 +1,21 @@
+def run():
+  available = []
+  missing = []
+  for module in ["gl", "glu", "fonts", "util"]:
+    try: exec "import gltbx."+module
+    except ImportError: missing.append(module)
+    else: available.append(module)
+  if (len(available) == 0):
+    available = "None"
+  else:
+    available = " ".join(available)
+  if (len(missing) == 0):
+    missing = "None"
+  else:
+    missing = " ".join(missing)
+  print "gltbx modules:"
+  print "  available:", available
+  print "    missing:", missing
+
+if (__name__ == "__main__"):
+  run()
