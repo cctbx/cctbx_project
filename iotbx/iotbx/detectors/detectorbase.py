e908a6ca2275a7139c67044028e013d2a7c2a18b
Nicholas Sauter <nksauter@lbl.gov>
2006-02-07 20:11:44 +0000

    Add the ability to write a test ADSC-format image with arbitrary data.

--- iotbx/iotbx/detectors/detectorbase.py
+++ iotbx/iotbx/detectors/detectorbase.py
@@ -38,6 +38,35 @@ class DetectorImageBase(object):
       from iotbx.detectors import Bin2_by_2
       self.linearintdata = Bin2_by_2(self.linearintdata)
 
+  def debug_write(self,fileout,mod_data=None):
+    info = """{
+HEADER_BYTES= 1024;
+DIM=2;
+BYTE_ORDER=little_endian;
+TYPE=unsigned_short;
+SIZE1=%(SIZE1)4d;
+SIZE2=%(SIZE1)4d;
+PIXEL_SIZE=%(PIXEL_SIZE)8.6f;
+TIME=0.000000;
+DISTANCE=%(DISTANCE).2f;
+TWOTHETA=%(TWOTHETA).2f;
+PHI=%(OSC_START).3f;
+OSC_START=%(OSC_START).3f;
+OSC_RANGE=%(OSC_RANGE).3f;
+WAVELENGTH=%(WAVELENGTH).6f;
+BEAM_CENTER_X=%(BEAM_CENTER_X).2f;
+BEAM_CENTER_Y=%(BEAM_CENTER_Y).2f;
+CCD_IMAGE_SATURATION=65535;
+}\f"""%self.parameters
+    F = open(fileout,"wb")
+    F.write(info)
+    len_null=1024-len(info)
+    F.write('\0'*len_null)
+    F.close()
+    from iotbx.detectors import WriteADSC
+    if mod_data==None: mod_data=self.linearintdata
+    WriteADSC(fileout,mod_data,self.size1,self.size2,self.getEndian())
+
   def __getattr__(self, attr):
     if   attr=='size1' : return self.parameters['SIZE1']
     elif attr=='size2' : return self.parameters['SIZE2']
--- iotbx/run_tests.py
+++ iotbx/run_tests.py
@@ -17,6 +17,7 @@ def run():
   ["$D/iotbx/mtz/tst.py", "P31"],
   "$D/iotbx/tst_reflection_file_utils.py",
   "$D/iotbx/detectors/tst_adsc.py",
+  "$D/iotbx/detectors/tst_debug_write.py",
   "$D/iotbx/xplor/tst_xplormap.py",
   ["$D/iotbx/tst_phases.py", "P31"],
   ["$D/iotbx/regression/tst_reflection_statistics.py", "Fdd2 P31m"]
