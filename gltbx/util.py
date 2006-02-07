1a1972d1d8ba06cd9ba5b21150edd5f01c4a7b61
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2006-02-07 07:06:49 +0000

    gltbx.util.show_versions()

--- gltbx/util.py
+++ gltbx/util.py
@@ -3,3 +3,19 @@ import scitbx.array_family.flex
 import boost.python
 ext = boost.python.import_ext("gltbx_util_ext")
 from gltbx_util_ext import *
+
+def show_versions():
+  from gltbx import gl
+  from gltbx import glu
+  if (hasattr(gl, "GL_VENDOR")):
+    print "GL_VENDOR:", gl.glGetString(gl.GL_VENDOR)
+  if (hasattr(gl, "GL_RENDERER")):
+    print "GL_RENDERER:", gl.glGetString(gl.GL_RENDERER)
+  if (hasattr(gl, "GL_VERSION")):
+    print "GL_VERSION:", gl.glGetString(gl.GL_VERSION)
+  if (hasattr(gl, "GL_EXTENSIONS")):
+    print "GL_EXTENSIONS:", gl.glGetString(gl.GL_EXTENSIONS)
+  if (hasattr(glu, "GLU_VERSION")):
+    print "GLU_VERSION:", glu.gluGetString(glu.GLU_VERSION)
+  if (hasattr(glu, "GLU_EXTENSIONS")):
+    print "GLU_EXTENSIONS:", glu.gluGetString(glu.GLU_EXTENSIONS)
