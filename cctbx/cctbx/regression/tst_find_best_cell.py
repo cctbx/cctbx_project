61f9bdcc45d0513bbb6618f551da9b17abf623c0
Peter Zwart <phzwart@users.noreply.github.com>
2006-02-20 00:49:08 +0000

    Updated tests

--- cctbx/cctbx/regression/tst_find_best_cell.py
+++ cctbx/cctbx/regression/tst_find_best_cell.py
@@ -12,15 +12,27 @@ def tst_find_best_cell():
              uctbx.unit_cell( '60, 40, 50, 90, 90, 90' ),
              uctbx.unit_cell( '60, 50, 40, 90, 90, 90' ) ]
 
-  sg_info =  sgtbx.space_group_info( 'I 21 21 21' )
+  uc_correct = [ uctbx.unit_cell( '40, 50, 60, 90, 90, 90' ),
+                 uctbx.unit_cell( '40, 60, 50, 90, 90, 90' ),
+                 uctbx.unit_cell( '40, 50, 60, 90, 90, 90' ),
+                 uctbx.unit_cell( '50, 60, 40, 90, 90, 90' ),
+                 uctbx.unit_cell( '40, 60, 50, 90, 90, 90' ),
+                 uctbx.unit_cell( '50, 60, 40, 90, 90, 90' ) ]
+
+  sg_info =  sgtbx.space_group_info( 'P 21 21 2' )
+  sg_info_2 = sgtbx.space_group_info( 'I 21 21 21' )
   sg = sg_info.group()
+  sg_2 = sg_info_2.group()
 
-  for uc in uc_array:
+  for uc, correct in zip(uc_array,uc_correct):
     best_cell_finder = fbc( uc, sg )
+    assert approx_equal( correct.parameters(),
+                         best_cell_finder.return_best_cell().parameters() )
 
+    best_cell_finder = fbc( uc, sg_2 )
     assert approx_equal( uc_array[0].parameters(),
                          best_cell_finder.return_best_cell().parameters() )
-    best_cell_finder.return_best_xs().show_summary()
+
 
 def run():
   tst_find_best_cell()
