2c8f5470fa6944cd21ebbb44de04d011213e5cbe
Peter Zwart <phzwart@users.noreply.github.com>
2006-02-24 22:52:19 +0000

    new options: --graph: makes a graph of the point group grap if graphviz is available; --but_not: removes a certain point group and all its supergroups from the constructred graph

--- iotbx/iotbx/command_line/ehms.py
+++ iotbx/iotbx/command_line/ehms.py
@@ -1,15 +1,12 @@
 from cctbx.sgtbx import pointgroup_tools as pgt
 from cctbx import crystal
 from iotbx.option_parser import iotbx_option_parser
+from libtbx.utils import Sorry, date_and_time, multi_out
+from cStringIO import StringIO
+
 import sys, os
 
 def ehms( args ):
-  print "Arguments: ",
-  for arg in args:
-    print arg,
-  print
-  print
-
   command_line = (
     iotbx_option_parser(
     usage="iotbx.ehms [options]",
@@ -27,55 +24,96 @@ def ehms( args ):
             dest="niggli",
             default=False,
             help="Reduce to niggli cell and assume P1")
+    .option(None, "--graph",
+            action="store",
+            dest="graph",
+            default=None,
+            help="A graphical representation of the graph will be written out")
+    .option(None, "--but_not",
+            action="store",
+            dest="but_not",
+            default=None,
+            help="Remove this particular point group from the graph and find out the consequences")
 
     ).process(args=args)
 
+  log = multi_out()
+  log.register(label="stdout", file_object=sys.stdout)
+
+
+  if len(args)==0:
+    command_line.parser.show_help()
+
   if ( command_line.symmetry.unit_cell() == None ):
-    print "Sorry: Unit cell not specified."
+    print >> log, "Sorry: Unit cell not specified."
     command_line.parser.show_help()
     return
 
   if ( command_line.symmetry.space_group() == None ):
-    print "Sorry: space group not specified."
+    print>> log,  "Sorry: space group not specified."
     command_line.parser.show_help()
     return
 
   if not ( command_line.symmetry.space_group().is_chiral() ):
-    print "Sorry, Non chiral space groups not yet supported."
+    print >> log, "Sorry, Non chiral space groups not yet supported."
     return
 
 
   if command_line.options.niggli:
-    print "*Unit cell will be niggli reduced and P1 will be assumed*"
+    print >> log, "*Unit cell will be niggli reduced and P1 will be assumed*"
     uc = command_line.symmetry.change_basis(
       command_line.symmetry.change_of_basis_op_to_niggli_cell() ).unit_cell()
     xs = crystal.symmetry( uc, "P 1" )
     command_line.symmetry = xs
 
-  print
-  print
+  print >> log
+  print >> log
   sg_explorer = pgt.space_group_graph_from_cell_and_sg(
       command_line.symmetry.unit_cell(),
       command_line.symmetry.space_group(),
       max_delta=command_line.options.max_delta)
 
 
-  print "A summary of the constructed point group graph object is given below"
-  print "===================================================================="
-  print
-  print "----------------------"
-  print "Input crystal symmetry"
-  print "----------------------"
-  print "Unit cell: ", command_line.symmetry.unit_cell().parameters()
-  print "Unit cell volume: ", command_line.symmetry.unit_cell().volume()
-  print "Space group: ", command_line.symmetry.space_group_info()
-  print
-  print
+  if not (command_line.options.but_not==None):
+    # remnove this point group
+    sg_explorer.pg_graph.remove_point_group_and_its_super_groups_from_graph(
+      command_line.options.but_not )
 
+  print >> log, "A summary of the constructed point group graph object is given below"
+  print >> log, "===================================================================="
+  print >> log
+  print >> log, "----------------------"
+  print >> log, "Input crystal symmetry"
+  print >> log, "----------------------"
+  print >> log, "Unit cell: ", command_line.symmetry.unit_cell().parameters()
+  print >> log, "Unit cell volume: ", command_line.symmetry.unit_cell().volume()
+  print >> log, "Space group: ", command_line.symmetry.space_group_info()
+  print >> log
+  print >> log
 
+  sg_explorer.show(out=log)
 
-  sg_explorer.show()
+  if command_line.options.graph is not None:
+    # check if the program 'dot' is in the path
+    graphviz = os.popen3("""
+dot<< EOF
+EOF""", "r"
+                )
+    if len(graphviz[2].readlines())>0:
+      raise Sorry("The program dot has not been installed or is not in the path; get it from http://www.graphviz.org")
 
+    # it seems graphviz is there, please proceed
+    buffer = StringIO()
+
+    sg_explorer.graphviz_pg_graph(out=buffer)
+    exec_command = """dot -Tpng >""" + str(command_line.options.graph) + """ << EOF""" + """
+""" + buffer.getvalue()
+    exec_command += """
+EOF
+    """
+    graphviz = os.popen( exec_command, "r" )
+    print >> log, "A file named" ,  command_line.options.graph, " contains a graphical representation "
+    print >> log, "of the point group relations."
 
 if (__name__ == "__main__"):
   ehms(sys.argv[1:])
