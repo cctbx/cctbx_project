1904a2389218fcd95b8f9496e3e8f4ec3f99325f
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2006-03-07 00:44:00 +0000

    bug fix: must infer lattice centring type from space group symbol

--- iotbx/iotbx/scalepack/no_merge_original_index.py
+++ iotbx/iotbx/scalepack/no_merge_original_index.py
@@ -2,6 +2,7 @@ from cctbx import miller
 from cctbx import crystal
 from cctbx import sgtbx
 from cctbx.array_family import flex
+from libtbx.str_utils import show_string
 import sys, os
 
 import boost.python
@@ -32,14 +33,13 @@ class reader(object):
     n_sym_ops_from_file = int(line[:5].strip())
     assert line[5] == " "
     self.space_group_symbol = line[6:].strip()
-    self.space_group = sgtbx.space_group()
+    self.space_group_from_ops = sgtbx.space_group()
     for i in xrange(n_sym_ops_from_file):
       line = f.readline()
       r = sgtbx.rot_mx([int(line[j*3:(j+1)*3]) for j in xrange(9)], 1)
       line = f.readline()
       t = sgtbx.tr_vec([int(line[j*3:(j+1)*3]) for j in xrange(3)], 12)
-      self.space_group.expand_smx(sgtbx.rt_mx(r, t))
-    assert self.space_group.order_p() == n_sym_ops_from_file
+      self.space_group_from_ops.expand_smx(sgtbx.rt_mx(r, t))
     f.close()
     if (header_only): return
     all_arrays = scalepack_ext.no_merge_original_index_arrays(
@@ -54,13 +54,21 @@ class reader(object):
     self.sigmas = all_arrays.sigmas()
 
   def space_group_info(self):
-    return sgtbx.space_group_info(group=self.space_group)
+    return combine_symops_and_symbol(
+      space_group_from_ops=self.space_group_from_ops,
+      space_group_symbol=self.space_group_symbol)
 
   def unmerged_miller_set(self, crystal_symmetry=None, force_symmetry=False):
+    if (not force_symmetry
+        or crystal_symmetry is None
+        or crystal_symmetry.space_group_info() is None):
+      space_group_info = self.space_group_info()
+    else:
+      space_group_info = None
     return miller.set(
       crystal_symmetry=crystal.symmetry(
         unit_cell=None,
-        space_group_info=self.space_group_info()).join_symmetry(
+        space_group_info=space_group_info).join_symmetry(
           other_symmetry=crystal_symmetry,
           force=force_symmetry),
       indices=self.original_indices,
@@ -97,6 +105,75 @@ class reader(object):
       merge_equivalents=merge_equivalents,
       base_array_info=base_array_info)]
 
+def combine_symops_and_symbol(space_group_from_ops, space_group_symbol):
+  space_group_symbol = space_group_symbol.replace(" ","").upper()
+  z = space_group_symbol[:1]
+  if ("PABCIFRH".find(z) < 0):
+    raise RuntimeError(
+      "Cannot determine lattice centring type given space group symbol"
+      " %s" % show_string(space_group_symbol))
+  if (z == "P"):
+    return sgtbx.space_group_info(group=space_group_from_ops)
+  if (z == "H"):
+    space_group_symbol = "R" + space_group_symbol[1:] + ":H"
+    z = "R"
+  elif (z == "R" and not space_group_symbol.endswith(":H")):
+    if (space_group_symbol.endswith(":R")):
+      z = None
+    else:
+      for s in space_group_from_ops:
+        r_info = s.r().info()
+        if (abs(r_info.type()) == 3):
+          if (r_info.ev() == (0,0,1)):
+            space_group_symbol = "R" + space_group_symbol[1:] + ":H"
+            break
+          elif (r_info.ev() == (1,1,1)):
+            space_group_symbol += ":R"
+            z = None
+            break
+  space_group_exp = sgtbx.space_group(space_group_from_ops)
+  if (z is not None):
+    try:
+      space_group_exp.expand_conventional_centring_type(z)
+    except RuntimeError:
+      space_group_exp = None
+  if (space_group_exp is not None):
+    try:
+      space_group_from_symbol = sgtbx.space_group_info(
+        symbol=space_group_symbol).group()
+    except RuntimeError:
+      space_group_from_symbol = None
+  if (   space_group_exp is None
+      or space_group_from_symbol is None
+      or space_group_exp != space_group_from_symbol):
+    raise RuntimeError(
+      "Symmetry operations in unmerged SCALEPACK file incompatible with"
+      " space group symbol %s"
+        % show_string(space_group_symbol))
+  return sgtbx.space_group_info(group=space_group_exp)
+
+def exercise_combine_symops_and_symbol():
+  for symbols in sgtbx.space_group_symbol_iterator():
+    space_group = sgtbx.space_group_info(
+      symbol="Hall: %s" % symbols.hall()).group()
+    space_group_from_ops = sgtbx.space_group()
+    for s in list(space_group)[:space_group.order_p()]:
+      space_group_from_ops.expand_smx(s)
+    combined = combine_symops_and_symbol(
+      space_group_from_ops=space_group_from_ops,
+      space_group_symbol=symbols.extended_hermann_mauguin())
+    assert combined.group() == space_group
+    if (symbols.extension() in ["R", "H"]):
+      combined = combine_symops_and_symbol(
+        space_group_from_ops=space_group_from_ops,
+        space_group_symbol=symbols.hermann_mauguin())
+      assert combined.group() == space_group
+      if (symbols.extension() == "H"):
+        combined = combine_symops_and_symbol(
+          space_group_from_ops=space_group_from_ops,
+          space_group_symbol="H "+symbols.hermann_mauguin()[2:])
+        assert combined.group() == space_group
+
 def quick_test(file_name):
   from scitbx.python_utils.misc import user_plus_sys_time
   t = user_plus_sys_time()
@@ -126,6 +203,7 @@ def quick_test(file_name):
   print
 
 def run(args):
+  exercise_combine_symops_and_symbol()
   file_names = """
 p9/data/infl.sca
 p9/data/peak.sca
@@ -149,6 +227,7 @@ bnl_2003/karen/shelxd/pk1234-unmerged.sca""".split()
       if (os.path.isfile(fn)):
         print "File name:", fn
         quick_test(fn)
+  print "OK"
 
 if (__name__ == "__main__"):
   run(sys.argv[1:])
