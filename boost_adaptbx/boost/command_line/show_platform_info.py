4f9750bcd7848c59c6120a3f6700a749a5727fa5
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2006-03-18 03:53:05 +0000

    EDG defines, HP-UX defines, boost::int32_t, boost::uint32_t, import thread: OK/NO, sys.byteorder

--- boost_adaptbx/boost/command_line/show_platform_info.py
+++ boost_adaptbx/boost/command_line/show_platform_info.py
@@ -3,6 +3,10 @@ import sys
 
 def run():
   sys.stdout.write(boost.python.platform_info)
+  print "sys.byteorder:", sys.byteorder
+  try: import thread
+  except ImportError: print "import thread: NO"
+  else: print "import thread: OK"
 
 if (__name__ == "__main__"):
   run()
