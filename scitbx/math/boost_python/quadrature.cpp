62f6c6093d9c7c7e372c0e16f6243be3f334d613
Peter Zwart <phzwart@users.noreply.github.com>
2006-05-16 06:13:16 +0000

    cubature for fast 2d integration of exp(-x*x-y*y)f(x,y) added

--- scitbx/math/boost_python/quadrature.cpp
+++ scitbx/math/boost_python/quadrature.cpp
@@ -27,6 +27,79 @@ namespace {
     }
   };
 
+  struct seven_twelve_0120_wrappers
+  {
+    typedef scitbx::math::quadrature::seven_twelve_0120<double> w_t;
+
+    static void
+    wrap()
+    {
+      using namespace boost::python;
+      class_<w_t>("seven_twelve_0120",no_init)
+        .def(init<> () )
+        .def("coord", &w_t::coord)
+        .def("weight", &w_t::weight)
+        ;
+    }
+  };
+
+
+  struct five_nine_1001_wrappers
+  {
+    typedef scitbx::math::quadrature::five_nine_1001<double> w_t;
+
+    static void
+    wrap()
+    {
+      using namespace boost::python;
+      class_<w_t>("five_nine_1001",no_init)
+        .def(init<> () )
+        .def("coord", &w_t::coord)
+        .def("weight", &w_t::weight)
+        ;
+    }
+  };
+
+
+  struct five_nine_1110_wrappers
+  {
+    typedef scitbx::math::quadrature::five_nine_1110<double> w_t;
+
+    static void
+    wrap()
+    {
+      using namespace boost::python;
+      class_<w_t>("five_nine_1110",no_init)
+        .def(init<> () )
+        .def("coord", &w_t::coord)
+        .def("weight", &w_t::weight)
+        ;
+    }
+  };
+
+
+  struct nine_twentyone_1012_wrappers
+  {
+    typedef scitbx::math::quadrature::nine_twentyone_1012<double> w_t;
+
+    static void
+    wrap()
+    {
+      using namespace boost::python;
+      class_<w_t>("nine_twentyone_1012",no_init)
+        .def(init<> () )
+        .def("coord", &w_t::coord)
+        .def("weight", &w_t::weight)
+        ;
+    }
+  };
+
+
+
+
+
+
+
 } // namespace <anonymous>
 
 namespace boost_python {
@@ -34,6 +107,10 @@ namespace boost_python {
   void wrap_quadrature()
   {
     gauss_hermite_engine_wrappers::wrap();
+    seven_twelve_0120_wrappers::wrap();
+    five_nine_1001_wrappers::wrap();
+    five_nine_1110_wrappers::wrap();
+    nine_twentyone_1012_wrappers::wrap();
   }
 
 }}} // namespace scitbx::math::boost_python
