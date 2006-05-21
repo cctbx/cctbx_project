0acb76ef56ac7311d1cc9d7a3abfdeab86c7d77b
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2006-05-21 20:24:00 +0000

    new show_sorted_by_counts

--- libtbx/libtbx/str_utils.py
+++ libtbx/libtbx/str_utils.py
@@ -1,4 +1,5 @@
 from __future__ import generators
+import sys
 
 def size_as_string_with_commas(sz):
   if (sz is None): return "unknown"
@@ -23,6 +24,28 @@ def show_string(s):
   if (s.find('"') < 0): return '"'+s+'"'
   if (s.find("'") < 0): return "'"+s+"'"
   return '"'+s.replace('"','\\"')+'"'
+
+def show_sorted_by_counts(
+      label_count_pairs,
+      reverse=True,
+      out=None,
+      prefix=""):
+  if (out is None): out = sys.stdout
+  if (len(label_count_pairs) == 0): return False
+  def sort_function(a, b):
+    if (reverse):
+      if (a[1] > b[1]): return -1
+      if (a[1] < b[1]): return  1
+    else:
+      if (a[1] > b[1]): return -1
+      if (a[1] < b[1]): return  1
+    return cmp(a[0], b[0])
+  label_count_pairs = list(label_count_pairs)
+  label_count_pairs.sort(sort_function)
+  fmt = "%%s %%%dd" % max([len(str(label_count_pairs[i][1])) for i in [0,-1]])
+  for l,c in label_count_pairs:
+    print >> out, prefix+fmt % (show_string(l), c)
+  return True
 
 def overwrite_at(s, offset, replacement):
   return s[:offset] + replacement + s[offset+len(replacement):]
@@ -50,7 +73,8 @@ def line_breaker(string, width):
       yield string[i_block_start:]
 
 def exercise():
-  import libtbx.forward_compatibility
+  from libtbx.test_utils import show_diff
+  from cStringIO import StringIO
   assert size_as_string_with_commas(0) == "0"
   assert size_as_string_with_commas(1) == "1"
   assert size_as_string_with_commas(-1) == "-1"
@@ -64,6 +88,24 @@ def exercise():
   assert show_string("a'c") == '"a\'c"'
   assert show_string('a"c') == "'a\"c'"
   assert show_string('\'"c') == '"\'\\"c"'
+  out = StringIO()
+  assert show_sorted_by_counts(
+    label_count_pairs=[("b", 3), ("a", 3), ("c", -2)],
+    out=out, prefix="%")
+  assert not show_diff(out.getvalue(), """\
+%"a"  3
+%"b"  3
+%"c" -2
+""")
+  out = StringIO()
+  assert show_sorted_by_counts(
+    label_count_pairs=[("b", -3), ("a", -3), ("c", 2)], reverse=False,
+     out=out, prefix="%")
+  assert not show_diff(out.getvalue(), """\
+%"c"  2
+%"a" -3
+%"b" -3
+""")
   for string, expected_result in [
     ("", [""]),
     ("this is", ["this is"]),
