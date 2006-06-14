d2a4f1ae665308b5f3ba6b2b5b98f88b7cdffffc
Peter Zwart <phzwart@users.noreply.github.com>
2006-06-14 07:11:33 +0000

    minor and major changes. buit cel comparison is now possible via --other_unit_cell and--other_space_group or --other_centring_type keywords. A semi exhausitve search is performed.

--- iotbx/iotbx/command_line/ehms.py
+++ iotbx/iotbx/command_line/ehms.py
@@ -1,5 +1,6 @@
 from cctbx import sgtbx
 from cctbx.sgtbx import pointgroup_tools as pgt
+from cctbx.sgtbx import sub_lattice_tools as slt
 from cctbx import crystal
 from iotbx.option_parser import iotbx_option_parser
 from libtbx.utils import Sorry, date_and_time, multi_out
@@ -7,12 +8,100 @@ from cStringIO import StringIO
 
 import sys, os
 
+def do_pointgroup_tricks(input_uc,
+                         input_ls,
+                         max_delta,
+                         out=None):
+  if out is None:
+    out = sys.stdout
+
+  sg_explorer = pgt.space_group_graph_from_cell_and_sg(
+      input_uc,
+      input_ls,
+      max_delta)
+
+
+  print >> out, "A summary of the constructed point group graph object is given below"
+  print >> out, "===================================================================="
+  print >> out
+  print >> out, "----------------------"
+  print >> out, "Input crystal symmetry"
+  print >> out, "----------------------"
+  print >> out, "Unit cell: ", input_uc.parameters()
+  print >> out, "Unit cell volume: ", input_uc.volume()
+  print >> out, "Space group: ", sgtbx.space_group_info( group=input_ls )
+  print >> out
+  print >> out
+  print >> out, "--------------------------"
+  print >> out, "Lattice symmetry deduction"
+  print >> out, "--------------------------"
+  print >> out, "Niggli cell: ", sg_explorer.xs_prim_set.unit_cell().parameters()
+  print >> out, "Niggli cell volume: ", sg_explorer.xs_prim_set.unit_cell().volume()
+  print >> out, "Niggli transformed input symmetry: ", sg_explorer.xs_prim_set.space_group_info()
+  print >> out, "Symmetry of Niggli cell: ", sgtbx.space_group_info( group = sg_explorer.pg_high )
+  print >> out
+  print >> out
+  print >> out, "All pointgroups that are both a subgroup of the lattice symmetry and"
+  print >> out, "a supergroup of the Niggli transformed input symmetry wil now be listed,"
+  print >> out, "as well as their minimal supergroups/maximal subgroups and symmetry"
+  print >> out, "operators that generate them."
+  print >> out, "For each pointgroup, a list of compatible spacegroups will be listed."
+  print >> out, "Care is taken that there are no sysmetatic absence violation with the "
+  print >> out, "provided input spacegroup."
+  print >> out
+  out.flush()
+    
+  sg_explorer.show(out=out)
+  
+  # return the object 
+  return sg_explorer
+
+
+
+
+def make_graph_of_graph(pg_object,
+                        file_name,
+                        out=None):
+  if out is None:
+    out = sys.stdout
+ 
+  graphviz = os.popen3("""
+dot<< EOF
+EOF""", "r"
+                )
+  if len(graphviz[2].readlines())>0:
+    raise Sorry("The program dot has not been installed or is not in the path; get it from http://www.graphviz.org")
+
+  # it seems graphviz is there, please proceed
+  buffer = StringIO()
+
+  pg_object.graphviz_pg_graph(out=buffer)
+  exec_command = """dot -Tpng >""" + str(file_name) + """ << EOF""" + """
+""" + buffer.getvalue()
+  exec_command += """
+EOF
+  """
+  graphviz = os.popen( exec_command, "r" )
+  print >> out, "A file named" ,  file_name, " contains a graphical representation "
+  print >> out, "of the point group relations."
+
+ 
+
+
+
+
+
+
+
+
 def ehms( args ):
   command_line = (
     iotbx_option_parser(
     usage="iotbx.ehms [options]",
     description="Explore Higher Metric Symmetry. A list of possible unit cells and spacegroups is given for the given specified unit cell and spacegroup combination.")
+
     .enable_symmetry_comprehensive()
+
     .option(None, "--max_delta",
             action = "store",
             type="float",
@@ -20,26 +109,66 @@ def ehms( args ):
             dest = "max_delta",
             help = "Maximum delta/obliquity used in determining the lattice symmetry, using a modified Le-Page algorithm. Default is 5.0 degrees",
             metavar="FLOAT")
+
     .option(None, "--start_from_p1",
             action="store_true",
             dest="niggli",
             default=False,
             help="Reduce to Niggli cell and forget the input spacegroup before higher metric symmetry is sought.")
+
     .option(None, "--graph",
             action="store",
             dest="graph",
             default=None,
             help="A graphical representation of the graph will be written out. Requiers Graphviz to be installed and in path.")
-    .option(None, "--but_not",
-            action="store",
-            dest="but_not",
-            default=None,
-            help="Remove this particular point group from the graph and find out the consequences.")
+
     .option(None, "--centring_type",
             action="store",
-            type=str,
+            type="str",
             dest="centring_type",
             help="Centring type, choose from P,A,B,C,I,R,F")
+    
+    .option(None, "--other_unit_cell",
+            action="store",
+            type="str",
+            dest="other_unit_cell",
+            help="Other unit cell, for unit cell comparison",
+            metavar="10,20,30,90,103.7,90")    
+
+    .option(None, "--other_space_group",
+            action="store",
+            type="str",
+            dest="other_space_group",
+            help="space group for other_unit_cell, for unit cell comparison")
+
+    .option(None, "--other_centring_type",
+            action="store",
+            type="str",
+            dest="other_centring_type",
+            help="Centring type, choose from P,A,B,C,I,R,F")
+  
+    .option(None, "--no_point_group_graph",
+            action="store_true",
+            dest="pg_graph",
+            default=False,
+            help="Do not carry out the construction of a point group graph." )
+
+    .option(None, "--relative_length_tolerance",
+            action="store",
+            type="float",
+            help="Tolerance for unit cell lengths to be considered equal-ish.",
+            default=0.05,
+            metavar="FLOAT",
+            dest="rel_length_tol")
+
+    .option(None, "--absolute_angle_tolerance",
+            action="store",
+            dest="abs_angle_tol",
+            type="float",
+            default=10.0,
+            metavar="FLOAT",
+            help="Angular tolerance in unit cell comparison")
+  
     ).process(args=args)
 
   log = multi_out()
@@ -57,6 +186,9 @@ def ehms( args ):
       print >> log, "Sorry, the centring type %s is not known."%(command_line.options.centring_type)
       print >> log, "Choose from P,A,B,C,I,R,F "
       return
+
+  xs = None
+  other_xs = None
 
   if len(args)==0:
     command_line.parser.show_help()
@@ -92,74 +224,80 @@ def ehms( args ):
     print >> log, "*Unit cell will be niggli reduced and P1 will be assumed*"
     uc = command_line.symmetry.change_basis(
       command_line.symmetry.change_of_basis_op_to_niggli_cell() ).unit_cell()
-    xs = crystal.symmetry( uc, "P 1" )
-    command_line.symmetry = xs
+    command_line.symmetry = crystal.symmetry( uc, "P 1" )
 
-  sg_explorer = pgt.space_group_graph_from_cell_and_sg(
-      command_line.symmetry.unit_cell(),
-      command_line.symmetry.space_group(),
-      max_delta=command_line.options.max_delta)
+  xs = command_line.symmetry
 
-
-  if not (command_line.options.but_not==None):
-    # remnove this point group
-    sg_explorer.pg_graph.remove_point_group_and_its_super_groups_from_graph(
-      command_line.options.but_not )
-
-  print >> log, "A summary of the constructed point group graph object is given below"
-  print >> log, "===================================================================="
-  print >> log
-  print >> log, "----------------------"
-  print >> log, "Input crystal symmetry"
-  print >> log, "----------------------"
-  print >> log, "Unit cell: ", command_line.symmetry.unit_cell().parameters()
-  print >> log, "Unit cell volume: ", command_line.symmetry.unit_cell().volume()
-  print >> log, "Space group: ", command_line.symmetry.space_group_info()
-  print >> log
-  print >> log
-  print >> log, "--------------------------"
-  print >> log, "Lattice symmetry deduction"
-  print >> log, "--------------------------"
-  print >> log, "Niggli cell: ", sg_explorer.xs_prim_set.unit_cell().parameters()
-  print >> log, "Niggli cell volume: ", sg_explorer.xs_prim_set.unit_cell().volume()
-  print >> log, "Niggli transformed input symmetry: ", sg_explorer.xs_prim_set.space_group_info()
-  print >> log, "Symmetry of Niggli cell: ", sgtbx.space_group_info( group = sg_explorer.pg_high )
-  print >> log
-  print >> log
-  print >> log, "All pointgroups that are both a subgroup of the lattice symmetry and"
-  print >> log, "a supergroup of the Niggli transformed input symmetry wil now be listed,"
-  print >> log, "as well as their minimal supergroups/maximal subgroups and symmetry"
-  print >> log, "operators that generate them."
-  print >> log, "For each pointgroup, a list of compatible spacegroups will be listed."
-  print >> log, "Care is takebn that there are no sysmetatic absence violation with the "
-  print >> log, "provided input spacegroup."
-  print >> log
+  ############################################################################
+  # ABOVE IS JUST INPUT PARSING, NOW THE ACTUAL STUFF HAPPENS
+  ############################################################################
 
 
+  if not command_line.options.pg_graph:
+    ##############################
+    #   get a point group graph  #
+    ##############################
 
-  sg_explorer.show(out=log)
+    pg_object = do_pointgroup_tricks( xs.unit_cell(),
+                                      xs.space_group(),
+                                      command_line.options.max_delta,
+                                      log )
 
-  if command_line.options.graph is not None:
-    # check if the program 'dot' is in the path
-    graphviz = os.popen3("""
-dot<< EOF
-EOF""", "r"
-                )
-    if len(graphviz[2].readlines())>0:
-      raise Sorry("The program dot has not been installed or is not in the path; get it from http://www.graphviz.org")
+    ################################################
+    #  make a graphical representation if desired  #
+    ################################################
 
-    # it seems graphviz is there, please proceed
-    buffer = StringIO()
+    if command_line.options.graph is not None:
+      make_graph_of_graph(pg_object,
+                          command_line.options.graph,
+                          log)
 
-    sg_explorer.graphviz_pg_graph(out=buffer)
-    exec_command = """dot -Tpng >""" + str(command_line.options.graph) + """ << EOF""" + """
-""" + buffer.getvalue()
-    exec_command += """
-EOF
-    """
-    graphviz = os.popen( exec_command, "r" )
-    print >> log, "A file named" ,  command_line.options.graph, " contains a graphical representation "
-    print >> log, "of the point group relations."
+
+  #########################################
+  #  Check if other cell has been defined #
+  #########################################
+
+  if command_line.options.other_unit_cell is not None:    
+    print >> log, "A second unit cell has been specified. "
+    other_xs = None
+
+    if command_line.options.other_space_group is None:
+      if command_line.options.other_centring_type is None:
+        raise Sorry("No space group or centring type for other cell specified.")
+      else:
+        other_xs = crystal.symmetry( command_line.options.other_unit_cell,
+                                     space_group_symbol="Hall: %s 1" %( command_line.options.other_centring_type )
+                                   )
+    else:
+      other_xs = crystal.symmetry( command_line.options.other_unit_cell,
+                                   space_group_symbol=command_line.options.other_space_group
+                                 )
+
+    # get the graph is desired
+    if not command_line.options.pg_graph:
+      other_pg_object = do_pointgroup_tricks( other_xs.unit_cell(),
+                                              other_xs.space_group(),
+                                              command_line.options.max_delta,
+                                              log )
+    # do the unit cell comparison
+    print >> log
+    print >> log
+    print >> log, "Unit cell comparison"
+    print >> log, "--------------------"
+    print >> log
+    print >> log, "The unit cells will be compared. The smallest niggli cell,"
+    print >> log, "will be used as a (semi-flexible) lego-block to see if it"
+    print >> log, "can construct the larger Niggli cell."
+    print >> log
+    sl_object =  slt.compare_lattice(xs_a=xs,
+                                     xs_b=other_xs,
+                                     max_delta=command_line.options.max_delta,
+                                     out=log,
+                                     relative_length_tolerance=command_line.options.rel_length_tol,
+                                     absolute_angle_tolerance=command_line.options.abs_angle_tol)
+    
+
+
 
 if (__name__ == "__main__"):
   ehms(sys.argv[1:])
