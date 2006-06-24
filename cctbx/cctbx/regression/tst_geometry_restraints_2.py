ed2271e16a5d6ca3e0c5ebc4f1abaf7522e530da
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2006-06-24 01:12:36 +0000

    enk.pdb inlined to make the test self-contained

--- cctbx/cctbx/regression/tst_geometry_restraints_2.py
+++ cctbx/cctbx/regression/tst_geometry_restraints_2.py
@@ -291,24 +291,72 @@ d%SI2 - O4   3.386 1.000
 @r... (remaining 52 not shown)
 """)
 
+enk_pdb = """\
+CRYST1   10.851   13.095   21.192  90.00  90.00  90.00 P 21 21 21
+ATOM      1  CA  TYR A   1       8.787   2.175   5.487  1.00  0.91           C
+ATOM      2  CB  TYR A   1       8.968   2.012   6.998  1.00  1.05           C
+ATOM      3  CG  TYR A   1       9.527   0.669   7.410  1.00  1.04           C
+ATOM      4  CD2 TYR A   1       8.768  -0.222   8.157  1.00  1.37           C
+ATOM      5  CE2 TYR A   1       9.275  -1.449   8.537  1.00  1.50           C
+ATOM      6  CZ  TYR A   1      10.559  -1.798   8.175  1.00  1.15           C
+ATOM      7  CE1 TYR A   1      11.334  -0.929   7.437  1.00  1.37           C
+ATOM      8  CD1 TYR A   1      10.818   0.296   7.062  1.00  1.34           C
+ATOM      9  C   TYR A   1       7.767   1.172   4.959  1.00  0.85           C
+ATOM     10  OH  TYR A   1      11.069  -3.019   8.553  1.00  1.40           O
+ATOM     11  O   TYR A   1       6.576   1.472   4.870  1.00  1.01           O
+ATOM     12  N   TYR A   1       8.388   3.541   5.166  1.00  1.11           N
+ATOM     13  CA  GLY A   2       7.387  -1.050   4.044  1.00  1.14           C
+ATOM     14  C   GLY A   2       6.345  -1.569   5.016  1.00  0.97           C
+ATOM     15  O   GLY A   2       5.234  -1.918   4.619  1.00  1.15           O
+ATOM     16  N   GLY A   2       8.241  -0.020   4.608  1.00  0.93           N
+ATOM     17  CA  GLY A   3       5.804  -2.100   7.324  1.00  1.36           C
+ATOM     18  C   GLY A   3       4.651  -1.149   7.578  1.00  1.01           C
+ATOM     19  O   GLY A   3       3.598  -1.553   8.071  1.00  1.38           O
+ATOM     20  N   GLY A   3       6.706  -1.622   6.294  1.00  1.11           N
+ATOM     21  CA  PHE A   4       3.819   1.134   7.419  1.00  0.89           C
+ATOM     22  CB  PHE A   4       4.397   2.380   8.094  1.00  1.13           C
+ATOM     23  CG  PHE A   4       4.930   2.130   9.475  1.00  1.00           C
+ATOM     24  CD1 PHE A   4       6.267   1.825   9.673  1.00  1.51           C
+ATOM     25  CE1 PHE A   4       6.760   1.595  10.943  1.00  1.80           C
+ATOM     26  CZ  PHE A   4       5.916   1.667  12.033  1.00  1.59           C
+ATOM     27  CE2 PHE A   4       4.582   1.970  11.850  1.00  1.49           C
+ATOM     28  CD2 PHE A   4       4.095   2.199  10.577  1.00  1.24           C
+ATOM     29  C   PHE A   4       3.185   1.509   6.084  1.00  0.94           C
+ATOM     30  N   PHE A   4       4.852   0.121   7.242  1.00  0.88           N
+ATOM     31  O   PHE A   4       2.361   2.421   6.010  1.00  1.47           O
+ATOM     32  CA  LEU A   5       3.055   1.059   3.693  1.00  0.87           C
+ATOM     33  CB  LEU A   5       3.965   0.435   2.634  1.00  1.13           C
+ATOM     34  CG  LEU A   5       3.531   0.603   1.177  1.00  1.16           C
+ATOM     35  CD1 LEU A   5       3.411   2.076   0.818  1.00  1.88           C
+ATOM     36  CD2 LEU A   5       4.502  -0.103   0.245  1.00  1.67           C
+ATOM     37  C   LEU A   5       1.634   0.527   3.541  1.00  0.87           C
+ATOM     38  N   LEU A   5       3.576   0.800   5.030  1.00  0.92           N
+ATOM     39  OXT LEU A   5       1.246  -0.440   4.196  1.00  1.23           O
+ATOM     41  O   HOH B   1      14.655  -4.248   8.995  1.00  1.51           O
+ATOM     81  H1  HOH B   1      14.954  -3.400   8.664  1.00  1.13           H
+ATOM     82  H2  HOH B   1      13.712  -4.246   8.805  1.00  0.76           H
+ATOM     42  O   HOH B   2      12.055  -3.540   8.243  1.00  1.81           O
+ATOM     83  H1  HOH B   2      11.456  -4.167   7.841  1.00  0.96           H
+ATOM     84  H2  HOH B   2      11.476  -3.010   8.803  1.00  1.17           H
+ATOM     43  O   HOH B   3       9.622  -2.103   5.551  1.00  2.24           O
+ATOM     85  H1  HOH B   3      10.394  -1.650   5.193  1.00  1.02           H
+ATOM     86  H2  HOH B   3       9.588  -2.843   4.937  1.00  1.29           H
+END
+"""
+
 def exercise_with_pdb(verbose):
   try: from mmtbx.monomer_library import pdb_interpretation
   except ImportError:
     print "Skipping exercise_with_pdb():", \
       "mmtbx.monomer_library.pdb_interpretation not available"
     return
-  pdb_file = libtbx.env.find_in_repositories(
-    relative_path="regression/pdb/enk.pdb",
-    test=os.path.isfile)
-  if (pdb_file is None):
-    print "Skipping exercise_with_pdb(): input file not available"
-    return
   if (verbose):
     out = sys.stdout
   else:
     out = StringIO()
+  open("tmp.pdb", "w").write(enk_pdb)
   processed_pdb_file = pdb_interpretation.run(
-    args=[pdb_file],
+    args=["tmp.pdb"],
     strict_conflict_handling=False,
     log=out)
   geo = processed_pdb_file.geometry_restraints_manager()
