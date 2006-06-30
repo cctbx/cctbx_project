56dc67f4685337045a76e4edc22a30f7e1d3f675
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2006-06-30 16:52:33 +0000

    upgrade to optik 1.5.1; use customized version unconditionally

--- libtbx/libtbx/optparse_wrapper.py
+++ libtbx/libtbx/optparse_wrapper.py
@@ -1,14 +1,9 @@
-# optik141 is the unmodified distribution, except for:
-# mv lib optik
 import sys, os
-if (sys.version_info[0] < 3 and sys.version_info[1] < 3):
-  sys.path.insert(0, os.path.join(
-    os.path.dirname(os.path.dirname(__file__)),
-    "optik141"))
-  from optik import *
-else:
-  from optparse import *
-  from optparse import make_option
+
+sys.path.insert(0, os.path.join(
+  os.path.dirname(os.path.dirname(__file__)), "optik151"))
+from optik import *
+make_option = Option
 
 class option_parser(OptionParser):
 
