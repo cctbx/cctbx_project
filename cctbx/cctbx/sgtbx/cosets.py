1b59766302b6a29917b860ca0f8da0dcd51ac0d1
Peter Zwart <phzwart@users.noreply.github.com>
2006-07-12 21:09:35 +0000

    New double coset decomposition algorithm

--- cctbx/cctbx/sgtbx/cosets.py
+++ cctbx/cctbx/sgtbx/cosets.py
@@ -1,3 +1,5 @@
+import sys
+
 class left_decomposition(object):
 
   def __init__(self, g, h):
@@ -29,16 +31,111 @@ class left_decomposition(object):
       raise RuntimeError("h is not a subgroup of g")
 
 def double_unique(g, h1, h2):
+  """g is the supergroup
+     h1 and h2 are subgroups
+  """
+  # Make lists of symops for all groups
   g = [s for s in g]
   h1 = [s for s in h1]
   h2 = [s for s in h2]
+  # this is our final result
   result = []
+  # This dictionairy keeps track of equivalent symops
   done = {}
+  #
   for a in g:
-    if (str(a) in done): continue
+    if (str( a ) in done): continue
     result.append(a)
     for hi in h1:
       for hj in h2:
         b = hi.multiply(a).multiply(hj)
-        done[str(b)] = None
+        done[str( b )] = None
   return result
+
+
+class double_unique_new(object):
+  def __init__(self,g, h1, h2):
+    """g is the supergroup
+       h1 and h2 are subgroups
+    """
+    # Make lists of symops for all groups
+    g = [s for s in g]
+    h1 = [s for s in h1]
+    h2 = [s for s in h2]
+
+    # a list of lists with our double cosets
+    self.double_cosets = []
+
+    #
+    for a in g:
+      # first we have to check whether or not
+      # this symmetry operator is allready in a coset we
+      # might have cnostructured earlier
+      if not self.is_in_list_of_cosets( a ):
+        # not present, make de double coset please
+        tmp_double_coset = []
+        tmp_double_coset.append( a )
+        # The other members will now be made
+        for hi in h1:
+          for hj in h2:
+            b = hi.multiply(a).multiply(hj)
+            #check if this element is allready in this coset please
+            if not self.is_in_coset( b, tmp_double_coset ):
+              tmp_double_coset.append( b )
+        self.double_cosets.append( tmp_double_coset )
+
+  def is_in_coset(self, a, coset_list):
+    found_it=False
+    for hi in coset_list:
+      if ( str(hi.mod_positive() ) == str(a.mod_positive() ) ):
+        found_it = True
+        break
+    return found_it
+
+  def is_in_list_of_cosets( self, a ):
+    found_it = False
+    for cs in self.double_cosets:
+      if self.is_in_coset( a, cs ):
+        found_it = True
+    return found_it
+
+  def assert_no_duplicates(self):
+    n_cosets = len(self.double_cosets)
+    for ics in xrange(n_cosets):
+      tmp_cs = self.double_cosets[ics]
+      for jcs in xrange(n_cosets):
+        if ics != jcs :
+          tmp_cs_2 = self.double_cosets[jcs]
+          # now check each element of tmp_cs
+          for hi in tmp_cs:
+            assert( not self.is_in_coset(hi, tmp_cs_2) )
+
+  def show(self,out=None):
+    if out == None:
+      out = sys.stdout
+    print >> out, "The double cosets are listed below"
+    for cs in self.double_cosets:
+      for a in cs:
+        print >> out, "("+str(a)+")    ",
+      print >> out
+
+
+def test_double_coset_decomposition():
+  from  cctbx.sgtbx import subgroups
+  from cctbx import sgtbx
+  for space_group_number in xrange(17,44):
+    parent_group_info = sgtbx.space_group_info(space_group_number)
+    subgrs = subgroups.subgroups(parent_group_info).groups_parent_setting()
+    g = parent_group_info.group()
+    for h1 in subgrs:
+      for h2 in subgrs:
+        tmp_new = double_unique_new(g, h1, h2)
+        tmp_new.assert_no_duplicates()
+
+def run():
+  test_double_coset_decomposition()
+  print "OK"
+
+
+if (__name__ == "__main__"):
+  run()
