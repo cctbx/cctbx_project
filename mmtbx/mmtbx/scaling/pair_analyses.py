2d7ea9f960818e2c82ad979467c0891e07a712db
Peter Zwart <phzwart@users.noreply.github.com>
2006-07-14 23:34:07 +0000

    updated the reindexing routines

--- mmtbx/mmtbx/scaling/pair_analyses.py
+++ mmtbx/mmtbx/scaling/pair_analyses.py
@@ -6,6 +6,7 @@ from cctbx import adptbx
 from libtbx import table_utils
 import cctbx.sgtbx.lattice_symmetry
 import cctbx.sgtbx.cosets
+from cctbx.crystal import reindex
 from cctbx.array_family import flex
 from libtbx.utils import Sorry, date_and_time, multi_out
 import iotbx.phil
@@ -32,7 +33,8 @@ class reindexing(object):
                out=None,
                relative_length_tolerance=0.05,
                absolute_angle_tolerance=3.0,
-               lattice_symmetry_max_delta=3.0):
+               lattice_symmetry_max_delta=3.0,
+               file_name=None):
 
     self.relative_length_tolerance=relative_length_tolerance
     self.absolute_angle_tolerance=absolute_angle_tolerance
@@ -40,151 +42,67 @@ class reindexing(object):
     self.out = out
     if self.out is None:
       self.out = sys.stdout
+    self.file_name=file_name
 
-
-    ## ameka deep copy as we,
+    ## make deep copy for safety
     self.set_a = set_a.deep_copy().set_observation_type( set_a )
     self.set_b = set_b.deep_copy().set_observation_type( set_b )
 
+    self.set_a_xs = crystal.symmetry( unit_cell=self.set_a.unit_cell(),
+                                      space_group=self.set_a.space_group() )
+    self.set_b_xs = crystal.symmetry( unit_cell=self.set_b.unit_cell(),
+                                      space_group=self.set_b.space_group() )
 
-    # We need to go to the minimum cell
-    self.change_of_basis_op_to_minimum_cell_a=\
-      self.set_a.change_of_basis_op_to_minimum_cell()
-    self.change_of_basis_op_to_minimum_cell_b=\
-      self.set_b.change_of_basis_op_to_minimum_cell()
+    self.cosets = reindex.reindexing_operators(
+      xs1=self.set_a_xs,
+      xs2=self.set_b_xs,
+      relative_length_tolerance=self.relative_length_tolerance,
+      absolute_angle_tolerance=self.absolute_angle_tolerance,
+      max_delta=self.lattice_symmetry_max_delta,
+      anomalous_flag=self.set_a.anomalous_flag(),
+      out=self.out
+      )
 
-    ## make the change
-    self.set_a  = self.set_a.change_basis(
-      self.change_of_basis_op_to_minimum_cell_a ).map_to_asu().\
-       set_observation_type( set_a )
+    self.set_a_to_niggli = self.cosets.cb_op_to_n_1
+    self.set_b_to_niggli = self.cosets.cb_op_to_n_2
 
-    self.set_b  = self.set_b.change_basis(
-      self.change_of_basis_op_to_minimum_cell_b ).map_to_asu().\
-       set_observation_type( set_b )
+    self.set_a_nig = self.set_a.change_basis( self.set_a_to_niggli  ).map_to_asu()
+    self.set_b_nig = self.set_b.change_basis( self.set_b_to_niggli  ).map_to_asu()
 
-    ## Get the lattice group
-    self.lattice_group_a = sgtbx.lattice_symmetry.group(
-      self.set_a.unit_cell(),
-      max_delta=self.lattice_symmetry_max_delta)
-    self.lattice_group_b = sgtbx.lattice_symmetry.group(
-      self.set_b.unit_cell(),
-      max_delta=self.lattice_symmetry_max_delta)
+    self.ops_in_niggli_setting = self.cosets.cb_ops_in_niggli_setting()
+    self.nice_cb_ops = self.cosets.combined_cb_ops()
 
-    ## Get the lattice symmetry
-    self.lattice_symmetry_a = crystal.symmetry(
-      unit_cell=self.set_a.unit_cell(),
-      space_group_info=sgtbx.space_group_info(group=self.lattice_group_a),
-      assert_is_compatible_unit_cell=False)
-
-    self.lattice_symmetry_b = crystal.symmetry(
-      unit_cell=self.set_b.unit_cell(),
-      space_group_info=sgtbx.space_group_info(group=self.lattice_group_b),
-      assert_is_compatible_unit_cell=False)
-
-    ## Get the intensity symmetry please
-    tmp_a = crystal.symmetry.change_basis(
-      self.set_a,
-      self.set_a.change_of_basis_op_to_minimum_cell())
-    tmp_b = crystal.symmetry.change_basis(
-      self.set_b,
-      self.set_b.change_of_basis_op_to_minimum_cell())
-
-    self.intensity_symmetry_a = tmp_a.reflection_intensity_symmetry(
-      anomalous_flag=self.set_a.anomalous_flag() )
-    self.intensity_symmetry_b = tmp_b.reflection_intensity_symmetry(
-      anomalous_flag=self.set_b.anomalous_flag() )
-
-
-    self.intensity_symmetry_a = self.set_a.reflection_intensity_symmetry()
-    self.intensity_symmetry_b = self.set_b.reflection_intensity_symmetry()
-
-
-    c_inv_rs = self.set_a.unit_cell().\
-      similarity_transformations(
-        other=self.set_b.unit_cell(),
-        relative_length_tolerance=self.relative_length_tolerance,
-        absolute_angle_tolerance=self.absolute_angle_tolerance)
-
-    min_bases_msd = None
-    similarity_cb_op = None
-
-    result = None
-
-    for c_inv_r in c_inv_rs:
-      c_inv = sgtbx.rt_mx(sgtbx.rot_mx(c_inv_r))
-      cb_op = sgtbx.change_of_basis_op(c_inv).inverse()
-      bases_msd = self.set_a.unit_cell() \
-        .bases_mean_square_difference(
-          other=cb_op.apply(self.set_b.unit_cell()))
-      if ( (min_bases_msd is None) or (min_bases_msd > bases_msd) ):
-        min_bases_msd = bases_msd
-        similarity_cb_op = cb_op
-    if (similarity_cb_op is None):
-      result
-
-    common_lattice_group = sgtbx.space_group(self.lattice_group_a)
-    for s in self.lattice_group_b.build_derived_acentric_group() \
-               .change_basis(similarity_cb_op):
-      try: common_lattice_group.expand_smx(s)
-      except RuntimeError:
-        result = []
-
-    common_lattice_group.make_tidy()
-    result = []
-
-    for s in sgtbx.cosets.double_unique(
-               g=common_lattice_group,
-               h1=self.intensity_symmetry_a.space_group()
-                   .build_derived_acentric_group()
-                   .make_tidy(),
-               h2=self.intensity_symmetry_b.space_group()
-                   .build_derived_acentric_group()
-                   .change_basis(similarity_cb_op)
-                   .make_tidy()):
-      if (s.r().determinant() > 0):
-        result.append(sgtbx.change_of_basis_op(s) * similarity_cb_op)
-    self.matrices = result
-    self.matrices_for_table = []
-    self.cc_values= []
+    self.cc_values = []
     self.matches = []
-    self.table=None
-    self.analyse()
 
-  def combined_cb_op(self, cb_op):
-    s = self.change_of_basis_op_to_minimum_cell_a
-    o = self.change_of_basis_op_to_minimum_cell_b
-    return s.inverse() * cb_op.new_denominators(s) * o
+    self.analyse()
+    self.select_and_transform()
 
   def analyse(self):
     table_data=[]
-    for cb_op in self.matrices:
-      cb_op_comb = self.combined_cb_op(cb_op)
-      self.matrices_for_table.append( cb_op_comb )
-
-      tmp_set_b = self.set_b.change_basis(cb_op).map_to_asu()
-
-      tmp_set_a, tmp_set_b = self.set_a.map_to_asu().common_sets(
-        tmp_set_b,
-        assert_is_similar_symmetry=False)
-      tmp_cc = tmp_set_a.correlation(
-        tmp_set_b,
-        assert_is_similar_symmetry=False)
-      ## STore the cc values
+    for (cb_op, comb_cb_op) in zip(self.ops_in_niggli_setting,
+                                   self.nice_cb_ops):
+      tmp_set_b_nig = self.set_b_nig.change_basis( cb_op ).map_to_asu()
+      tmp_set_a_nig, tmp_set_b_nig = self.set_a_nig.common_sets(
+        tmp_set_b_nig, assert_is_similar_symmetry=False )
+      tmp_cc = tmp_set_a_nig.correlation(tmp_set_b_nig,
+                                         assert_is_similar_symmetry=False )
       self.cc_values.append(  tmp_cc.coefficient()  )
-      self.matches.append(
-        float(tmp_set_a.indices().size())/float(self.set_a.indices().size())
-      )
+      self.matches.append( float(   tmp_set_a_nig.indices().size()  )/
+                           float(  self.set_a_nig.indices().size()  )   )
+
+
 
   def select_and_transform(self,
                            matches_cut_off=0.75
-                          ):
+                           ):
     ## hopsa
     max_cc=-1.0
     location = 0
     table_data=[]
-    for ii in range(len(self.matrices)):
+    for ii in range(len(self.nice_cb_ops)):
       table_data.append(
-        [self.matrices_for_table[ii].as_hkl(),
+        [self.nice_cb_ops[ii].as_hkl(),
          "%4.3f"%(self.cc_values[ii]),
          "%4.3f"%(self.matches[ii]),
          '   ']
@@ -203,18 +121,26 @@ class reindexing(object):
                                        separate_rows=False,
                                        prefix='| ',
                                        postfix=' |')
-
+    print >> self.out
+    print >> self.out, "Reference analyses"
+    print >> self.out, "   The following reindexing operators have been found:"
+    print >> self.out
     print >> self.out, self.table
+    print >> self.out
+    print >> self.out, "If the data is reindexed with operator (%s), the correlation of"%( self.nice_cb_ops[location].as_hkl() )
+    print >> self.out, "the intensities to the reference data is maximized. "
+    if self.file_name is not None:
+      print >> self.out, "This can be done for instance with:"
+      print >> self.out, "  iotbx.reflection_file_converter %s --change_of_basis=\"%s\" <output_options> "%(self.file_name, self.nice_cb_ops[location].as_hkl() )
+    print >> self.out, "-------------------------------------------------------------------------------"
     ##  change things in primitive setting
-    print location
-    print self.matrices[ location ].as_hkl()
-    transform_b = self.set_b.change_basis( self.matrices[ location ] ).map_to_asu()
-    ##  go back to standard setting
-    transform_b = transform_b.change_basis(
-      self.change_of_basis_op_to_minimum_cell_b.inverse() ).map_to_asu()\
-      .set_observation_type( self.set_b )
 
+    transform_b = self.set_b.change_basis( self.set_b_to_niggli ).change_basis(
+      self.ops_in_niggli_setting[location]  ).change_basis(
+      self.set_b_to_niggli.inverse() ).map_to_asu().set_observation_type(
+      self.set_b)
     return ( transform_b )
+
 
 
 class delta_generator(object):
