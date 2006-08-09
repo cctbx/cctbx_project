0c2a8581b5bf7dbf2b45449e695ac40022a22024
Peter Zwart <phzwart@users.noreply.github.com>
2006-08-09 00:01:20 +0000

    Detection of possible outliers on the basis of a likelihood ratio test. The null hypothesis is that the intensity is equal to the mode of the distribution given by Fcalc, alpha, beta. The alternative hypothesis is that fobs is equal to the observed value. the log-likelihood gain given the two different hypothesis is used as a criterion for outlier detection.

---
+++ mmtbx/scaling/outlier_bpl.cpp
@@ -0,0 +1,68 @@
+#include <scitbx/array_family/boost_python/flex_fwd.h>
+
+#include <boost/python/class.hpp>
+#include <boost/python/args.hpp>
+#include <boost/python/return_value_policy.hpp>
+#include <boost/python/return_by_value.hpp>
+#include <boost/python/copy_const_reference.hpp>
+#include <boost/python/module.hpp>
+#include <boost/python/def.hpp>
+#include <boost/python/overloads.hpp>
+
+
+#include <mmtbx/scaling/outlier.h>
+
+namespace mmtbx { namespace scaling { namespace outlier {
+
+namespace{
+
+
+  struct likelihood_ratio_outlier_test_wrapper
+  {
+    typedef likelihood_ratio_outlier_test<double> w_t;
+
+    static void
+    wrap()
+    {
+      using namespace boost::python;
+
+      class_<w_t>("likelihood_ratio_outlier_test", no_init)
+        .def(init<
+             scitbx::af::const_ref< double >  const&,
+             scitbx::af::const_ref< double >  const&,
+             scitbx::af::const_ref< double >  const&,
+             scitbx::af::const_ref< double >  const&,
+             scitbx::af::const_ref< bool >  const&,
+             scitbx::af::const_ref< double >  const&,
+             scitbx::af::const_ref< double >  const&>
+             ((arg_("f_obs"),
+               arg_("sigma_obs"),
+               arg_("f_calc"),
+               arg_("epsilon"),
+               arg_("centric"),
+               arg_("alpha"),
+               arg_("beta")
+               )))
+        .def("log_likelihood", &w_t::log_likelihood)
+        .def("posterior_mode_log_likelihood", &w_t::posterior_mode_log_likelihood)
+        .def("posterior_mode", &w_t::posterior_mode)
+        .def("flag_potential_outliers", &w_t::flag_potential_outliers)
+        ;
+    }
+
+  };
+
+}} // namespace <anonymous>
+
+namespace boost_python{
+
+  void wrap_outlier()
+  {
+    mmtbx::scaling::outlier::likelihood_ratio_outlier_test_wrapper::wrap();
+  }
+
+
+
+}
+
+}}
