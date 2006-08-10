e290b69ee8eadf526077d107dc771abece5474a2
Nicholas Sauter <nksauter@lbl.gov>
2006-08-10 18:16:40 +0000

    Trial commit of new code to enable reading of Bruker images

--- iotbx/iotbx/detectors/__init__.py
+++ iotbx/iotbx/detectors/__init__.py
@@ -3,6 +3,7 @@ from scitbx.array_family import flex
 import boost.python
 ext = boost.python.import_ext("iotbx_detectors_ext")
 from iotbx_detectors_ext import *
+from iotbx_detectors_bruker_ext import Bruker_base
 
 import exceptions
 from iotbx.detectors.adsc import ADSCImage
@@ -12,6 +13,7 @@ from iotbx.detectors.raxis import RAXISImage
 from iotbx.detectors.raxis_nonsquare import NonSquareRAXISImage
 from iotbx.detectors.macscience import DIPImage
 from iotbx.detectors.saturn import SaturnImage
+from iotbx.detectors.bruker import BrukerImage
 
 class ImageException(exceptions.Exception):
   def __init__(self,string):
@@ -20,17 +22,21 @@ class ImageException(exceptions.Exception):
 
 all_image_types = [SaturnImage,DIPImage,ADSCImage,
                   MARImage,MARIPImage,RAXISImage,
-                  NonSquareRAXISImage]
+                  NonSquareRAXISImage,BrukerImage]
 
 def ImageFactory(filename):
   for itype in all_image_types:
     try:
       I = itype(filename)
       I.readHeader()
+
       if itype==RAXISImage:
         assert I.head['sizeFast']==I.head['sizeSlow']
+        assert 0.4 < I.head['wavelength'] < 10.0 #needed to disambiguate from Bruker
       if itype==ADSCImage:
         assert I.parameters.has_key('DETECTOR_SN')
+      if itype==BrukerImage:
+        assert I.distance > 0.0 #needed to disambiguate from RAXIS
       return I
     except:
       pass
