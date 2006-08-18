003a46299df0e267b85e933663cd776e7bfd7e2a
Erik McKee <emckee@users.noreply.github.com>
2006-08-18 22:29:21 +0000

    fixed distance formula

--- iotbx/iotbx/detectors/bruker.py
+++ iotbx/iotbx/detectors/bruker.py
@@ -19,7 +19,7 @@ class BrukerImage (DetectorImageBase):
       'SIZE2':1024,
       'PIXEL_SIZE':self.bruker.pixel_size,
       'OSC_START':self.bruker.osc_start,
-      'DISTANCE':10.*(self.bruker.distance_cm+10.*self.bruker.distance_delta),
+      'DISTANCE':10.0*(self.bruker.distance_cm+self.bruker.distance_delta),
       'WAVELENGTH':self.bruker.wavelength,
 
       #first attempt formula to support two theta offset.
