946a3e60dc49a6b56472159b2aba8c2bd0bc0edc
Nigel W Moriarty <nwmoriarty@users.noreply.github.com>
2006-09-13 22:11:25 +0000

    bug fix: need extra () to avoid misunderstanding: function pointer vs. constructor

--- scitbx/include/scitbx/boost_python/array_as_list.h
+++ scitbx/include/scitbx/boost_python/array_as_list.h
@@ -11,8 +11,8 @@ namespace scitbx { namespace boost_python {
     const ElementType* array,
     std::size_t array_size)
   {
-    boost::python::object result(boost::python::handle<>(
-      PyList_New(array_size)));
+    boost::python::object result((boost::python::handle<>(
+      PyList_New(array_size))));
     PyObject* r = result.ptr();
     for(std::size_t i=0;i<array_size;i++) {
       boost::python::object ri(array[i]);
