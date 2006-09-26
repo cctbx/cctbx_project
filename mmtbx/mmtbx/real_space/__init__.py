12207d2ac3f3661395e8f328eb188b9cf2fc6490
Pavel Afonine <afonine@users.noreply.github.com>
2006-09-26 23:57:17 +0000

    first set of tools for NEW real space refinement

---
+++ mmtbx/mmtbx/real_space/__init__.py
@@ -0,0 +1,77 @@
+import cctbx.array_family.flex
+from scitbx.array_family import flex
+from cctbx.xray import ext
+from libtbx.test_utils import approx_equal
+
+class sampled_model_density(object):
+
+  def __init__(self, xray_structure,
+                     n_real    = None,
+                     grid_step = None):
+    assert [n_real, grid_step].count(None) == 1
+    self.xray_structure = xray_structure
+    if(n_real is None):
+       a_cell, b_cell, c_cell = xray_structure.unit_cell().parameters()[:3]
+       nx,ny,nz = \
+              int(a_cell/grid_step),int(b_cell/grid_step),int(c_cell/grid_step)
+    else:
+       nx,ny,nz = n_real[0],n_real[1],n_real[2]
+    u_base = flex.min_default(xray_structure.extract_u_iso_or_u_equiv(),0)
+    self.sampled_model_density = ext.sampled_model_density(
+       unit_cell                        = xray_structure.unit_cell(),
+       scatterers                       = xray_structure.scatterers(),
+       scattering_type_registry    = xray_structure.scattering_type_registry(),
+       fft_n_real                       = (nx,ny,nz),
+       fft_m_real                       = (nx,ny,nz),
+       u_base                           = u_base,
+       wing_cutoff                      = 1.e-9,
+       exp_table_one_over_step_size     = -100,
+       force_complex                    = False,
+       sampled_density_must_be_positive = False,
+       tolerance_positive_definite      = 1.e-5)
+    assert approx_equal(self.sampled_model_density.u_extra(), 0.0)
+
+  def data(self):
+    return self.sampled_model_density.real_map_unpadded()
+
+  def write_as_xplor_map(self, file_name):
+    import iotbx.xplor.map
+    model_map = self.data()
+    gridding = iotbx.xplor.map.gridding(n     = model_map.focus(),
+                                        first = (0,0,0),
+                                        last  = model_map.focus())
+    iotbx.xplor.map.writer(file_name   = file_name,
+                           is_p1_cell  = True,
+                           title_lines = [' REMARKS FILENAME=""',
+                                          ' REMARKS '],
+                           unit_cell   = self.xray_structure.unit_cell(),
+                           gridding    = gridding,
+                           data        = model_map,
+                           average     = -1,
+                           standard_deviation=-1)
+
+class around_atom(object):
+
+  def __init__(self, unit_cell,
+                     map_data,
+                     radius,
+                     shell,
+                     site_frac):
+    from cctbx import maptbx
+    obj = maptbx.grid_points_in_sphere_around_atom_and_distances(
+                                                         unit_cell = unit_cell,
+                                                         data      = map_data,
+                                                         radius    = radius,
+                                                         shell     = shell,
+                                                         site_frac = site_frac)
+    data = obj.data_at_grid_points()
+    dist = obj.distances()
+    p = flex.sort_permutation(dist)
+    self.data_ = data.select(p)
+    self.dist_ = dist.select(p)
+
+  def data(self):
+    return self.data_
+
+  def distances(self):
+    return self.dist_
---
+++ mmtbx/mmtbx/real_space/tst.py
@@ -0,0 +1,246 @@
+from cctbx.array_family import flex
+from cctbx import uctbx
+from cctbx import crystal
+from cctbx import xray
+from cctbx import maptbx
+from cctbx.development import random_structure
+from cctbx import eltbx
+import cctbx.eltbx.xray_scattering
+from cctbx import miller
+import mmtbx.real_space
+from libtbx.test_utils import approx_equal
+from cctbx import sgtbx
+from libtbx.utils import format_cpu_times
+
+
+def xray_structure_of_one_atom(site_cart,
+                               buffer_layer,
+                               a,
+                               b,
+                               scatterer_chemical_type = "C"):
+  custom_dict = \
+             {scatterer_chemical_type: eltbx.xray_scattering.gaussian([a],[b])}
+
+  box = uctbx.non_crystallographic_unit_cell_with_the_sites_in_its_center(
+                                                   sites_cart   = site_cart,
+                                                   buffer_layer = buffer_layer)
+  crystal_symmetry = crystal.symmetry(unit_cell          = box.unit_cell,
+                                      space_group_symbol = "p 1")
+  site_frac =crystal_symmetry.unit_cell().fractionalization_matrix()*site_cart
+  scatterer = flex.xray_scatterer((
+    [xray.scatterer(scatterer_chemical_type, site = site_frac[0], u = 0.0)]))
+  xray_structure = xray.structure(special_position_settings = None,
+                                  scatterers                = scatterer,
+                                  crystal_symmetry          = crystal_symmetry)
+  xray_structure.scattering_type_registry(custom_dict = custom_dict)
+  return xray_structure
+
+def exercise_1(grid_step,
+               radius,
+               shell,
+               a,
+               b,
+               buffer_layer,
+               site_cart):
+  xray_structure = xray_structure_of_one_atom(site_cart    = site_cart,
+                                              buffer_layer = buffer_layer,
+                                              a            = a,
+                                              b            = b)
+  sampled_density = mmtbx.real_space.sampled_model_density(
+                                            xray_structure = xray_structure,
+                                            grid_step      = grid_step)
+  site_frac = xray_structure.unit_cell().fractionalization_matrix()*site_cart
+  assert approx_equal(flex.max(sampled_density.data()),
+              sampled_density.data().value_at_closest_grid_point(site_frac[0]))
+
+  around_atom_obj = mmtbx.real_space.around_atom(
+                           unit_cell = xray_structure.unit_cell(),
+                           map_data  = sampled_density.data(),
+                           radius    = radius,
+                           shell     = shell,
+                           site_frac = list(xray_structure.sites_frac())[0])
+  data_exact = around_atom_obj.data()
+  dist_exact = around_atom_obj.distances()
+  approx_obj = maptbx.one_gaussian_peak_approximation(
+                                           data_at_grid_points = data_exact,
+                                           distances           = dist_exact)
+  assert approx_equal(approx_obj.a_reciprocal_space_opt(), 6.0, 1.e-3)
+  assert approx_equal(approx_obj.b_reciprocal_space_opt(), 3.0, 1.e-3)
+  assert approx_obj.gof_opt() < 0.3
+  assert approx_obj.radius_opt() < radius
+
+def exercise_2(grid_step,
+               radius,
+               shell,
+               a,
+               b,
+               volume_per_atom,
+               scatterer_chemical_type = "C"):
+  xray_structure = random_structure.xray_structure(
+                        space_group_info       = sgtbx.space_group_info("P 1"),
+                        elements               = ((scatterer_chemical_type)*1),
+                        volume_per_atom        = volume_per_atom,
+                        min_distance           = 1.5,
+                        general_positions_only = True)
+  custom_dict = \
+             {scatterer_chemical_type: eltbx.xray_scattering.gaussian([a],[b])}
+  xray_structure.scattering_type_registry(custom_dict = custom_dict)
+  sampled_density = mmtbx.real_space.sampled_model_density(
+                                            xray_structure = xray_structure,
+                                            grid_step      = grid_step)
+  site_frac = xray_structure.sites_frac()
+  r = abs(abs(flex.max(sampled_density.data()))-\
+      abs(sampled_density.data().value_at_closest_grid_point(site_frac[0])))/\
+      abs(flex.max(sampled_density.data())) * 100.0
+  assert r < 10.
+
+  around_atom_obj = mmtbx.real_space.around_atom(
+                           unit_cell = xray_structure.unit_cell(),
+                           map_data  = sampled_density.data(),
+                           radius    = radius,
+                           shell     = shell,
+                           site_frac = list(xray_structure.sites_frac())[0])
+  data_exact = around_atom_obj.data()
+  dist_exact = around_atom_obj.distances()
+  approx_obj = maptbx.one_gaussian_peak_approximation(
+                                           data_at_grid_points = data_exact,
+                                           distances           = dist_exact)
+  assert approx_equal(approx_obj.a_reciprocal_space_opt(), 6.0, 1.e-2)
+  assert approx_equal(approx_obj.b_reciprocal_space_opt(), 3.0, 1.e-2)
+  assert approx_obj.gof_opt() < 0.3
+  assert approx_obj.radius_opt() < radius
+
+def exercise_3(grid_step,
+               radius,
+               shell,
+               a,
+               b,
+               d_min,
+               site_cart,
+               buffer_layer):
+  xray_structure = xray_structure_of_one_atom(site_cart    = site_cart,
+                                              buffer_layer = buffer_layer,
+                                              a            = a,
+                                              b            = b)
+  miller_set = miller.build_set(
+                          crystal_symmetry = xray_structure.crystal_symmetry(),
+                          anomalous_flag   = False,
+                          d_min            = d_min,
+                          d_max            = None)
+  f_calc = miller_set.structure_factors_from_scatterers(
+                                 xray_structure               = xray_structure,
+                                 algorithm                    = "direct",
+                                 cos_sin_table                = False,
+                                 exp_table_one_over_step_size = False).f_calc()
+  fft_map = f_calc.fft_map(grid_step = grid_step, symmetry_flags = None)
+  fft_map = fft_map.apply_volume_scaling()
+  site_frac = xray_structure.sites_frac()
+  r = abs(abs(flex.max(fft_map.real_map_unpadded()))-\
+    abs(fft_map.real_map_unpadded().value_at_closest_grid_point(site_frac[0])))/\
+    abs(flex.max(fft_map.real_map_unpadded())) * 100.0
+  assert approx_equal(r, 0.0)
+  around_atom_obj_ = mmtbx.real_space.around_atom(
+                            unit_cell = xray_structure.unit_cell(),
+                            map_data  = fft_map.real_map_unpadded(),
+                            radius    = radius,
+                            shell     = shell,
+                            site_frac = list(xray_structure.sites_frac())[0])
+  data = around_atom_obj_.data()
+  dist = around_atom_obj_.distances()
+  approx_obj_ = maptbx.one_gaussian_peak_approximation(
+                                                  data_at_grid_points = data,
+                                                  distances           = dist)
+  assert approx_equal(approx_obj_.a_reciprocal_space_opt(), 6.0, 0.1)
+  assert approx_equal(approx_obj_.b_reciprocal_space_opt(), 3.0, 0.01)
+  assert approx_obj_.gof_opt() < 0.6
+  assert approx_obj_.radius_opt() < radius
+
+def exercise_4(grid_step,
+               radius,
+               shell,
+               a,
+               b,
+               d_min,
+               volume_per_atom,
+               scatterer_chemical_type = "C"):
+  xray_structure = random_structure.xray_structure(
+                        space_group_info       = sgtbx.space_group_info("P 1"),
+                        elements               = ((scatterer_chemical_type)*1),
+                        volume_per_atom        = volume_per_atom,
+                        min_distance           = 1.5,
+                        general_positions_only = True)
+  custom_dict = \
+             {scatterer_chemical_type: eltbx.xray_scattering.gaussian([a],[b])}
+  xray_structure.scattering_type_registry(custom_dict = custom_dict)
+  miller_set = miller.build_set(
+                          crystal_symmetry = xray_structure.crystal_symmetry(),
+                          anomalous_flag   = False,
+                          d_min            = d_min,
+                          d_max            = None)
+  f_calc = miller_set.structure_factors_from_scatterers(
+                                 xray_structure               = xray_structure,
+                                 algorithm                    = "direct",
+                                 cos_sin_table                = False,
+                                 exp_table_one_over_step_size = False).f_calc()
+  fft_map = f_calc.fft_map(grid_step = grid_step, symmetry_flags = None)
+  fft_map = fft_map.apply_volume_scaling()
+  site_frac = xray_structure.sites_frac()
+  r = abs(abs(flex.max(fft_map.real_map_unpadded()))-\
+    abs(fft_map.real_map_unpadded().value_at_closest_grid_point(site_frac[0])))/\
+    abs(flex.max(fft_map.real_map_unpadded())) * 100.0
+  assert r < 10.
+  around_atom_obj_ = mmtbx.real_space.around_atom(
+                            unit_cell = xray_structure.unit_cell(),
+                            map_data  = fft_map.real_map_unpadded(),
+                            radius    = radius,
+                            shell     = shell,
+                            site_frac = list(xray_structure.sites_frac())[0])
+  data = around_atom_obj_.data()
+  dist = around_atom_obj_.distances()
+  approx_obj_ = maptbx.one_gaussian_peak_approximation(
+                                                  data_at_grid_points = data,
+                                                  distances           = dist)
+  assert approx_equal(approx_obj_.a_reciprocal_space_opt(), 6.0, 0.1)
+  assert approx_equal(approx_obj_.b_reciprocal_space_opt(), 3.0, 0.1)
+  assert approx_obj_.gof_opt() < 1.0
+  assert approx_obj_.radius_opt() < radius
+
+def run():
+  for site in ([0,0,0], [0.1,0.1,0.1], [6,6,6], [3,3,3], [-0.1,0.1,-0.1],
+                                                           [6,-6,6], [3,-3,3]):
+      exercise_1(grid_step    = 0.1,
+                 radius       = 1.0,
+                 shell        = 0.0,
+                 a            = 6.0,
+                 b            = 3.0,
+                 site_cart    = flex.vec3_double([site]),
+                 buffer_layer = 3.)
+  for volume_per_atom in range(100,550,100)*10:
+      exercise_2(grid_step    = 0.08,
+                 radius       = 1.0,
+                 shell        = 0.0,
+                 a            = 6.0,
+                 b            = 3.0,
+                 volume_per_atom    = volume_per_atom)
+  for site in ([0,0,0], [0.1,0.1,0.1], [6,6,6], [3,3,3], [-0.1,0.1,-0.1],
+                                                           [6,-6,6], [3,-3,3]):
+      exercise_3(grid_step    = 0.1,
+                 radius       = 0.9,
+                 shell        = 0.0,
+                 a            = 6.0,
+                 b            = 3.0,
+                 d_min        = 0.08,
+                 site_cart    = flex.vec3_double([site]),
+                 buffer_layer = 3.)
+  for volume_per_atom in range(100,550,100)*5:
+      exercise_4(grid_step    = 0.1,
+                 radius       = 0.9,
+                 shell        = 0.0,
+                 a            = 6.0,
+                 b            = 3.0,
+                 d_min        = 0.08,
+                 volume_per_atom = volume_per_atom)
+
+if (__name__ == "__main__"):
+    run()
+    print "OK: ",format_cpu_times()
