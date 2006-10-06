fb69e18f6ee36c954389a038eec3acc5b8a2c350
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2006-10-06 02:01:02 +0000

    use scitbx::math::unsigned_product_leads_to_overflow() test before trying to allocate cubicles

--- cctbx/include/cctbx/crystal/cubicles.h
+++ cctbx/include/cctbx/crystal/cubicles.h
@@ -9,6 +9,32 @@
 
 namespace cctbx { namespace crystal { namespace neighbors {
 
+  namespace detail {
+
+    template <typename FloatType>
+    void
+    throw_show_cubicle_dimensions(
+      const char* message,
+      scitbx::vec3<FloatType> const& space_span,
+      FloatType const& cubicle_edge,
+      af::c_grid<3, unsigned> const& n_cubicles)
+    {
+      char buf[512];
+      std::sprintf(buf,
+        "%s\n"
+        "  This may be due to unreasonable parameters:\n"
+        "    cubicle_edge=%.6g\n"
+        "    space_span=(%.6g,%.6g,%.6g)\n"
+        "    n_cubicles=(%u,%u,%u)",
+        message,
+        cubicle_edge,
+        space_span[0], space_span[1], space_span[2],
+        n_cubicles[0], n_cubicles[1], n_cubicles[2]);
+      throw std::runtime_error(buf);
+    }
+
+  } // namespace detail
+
   template <typename CubicleContentType, typename FloatType=double>
   struct cubicles
   {
@@ -34,21 +60,23 @@ namespace cctbx { namespace crystal { namespace neighbors {
         n_cubicles[i] = static_cast<unsigned>(
           std::max(1, fic::iceil(space_span[i] / cubicle_edge)));
       }
+      if (scitbx::math::unsigned_product_leads_to_overflow(
+            n_cubicles.begin(), 3)) {
+        detail::throw_show_cubicle_dimensions(
+          "Excessive number of cubicles:",
+          space_span,
+          cubicle_edge_,
+          n_cubicles);
+      }
       try {
         memory.resize(n_cubicles);
       }
       catch(const std::bad_alloc&) {
-        char buf[512];
-        std::sprintf(buf,
-          "Not enough memory for cubicles.\n"
-          "  This may be due to unreasonable parameters:\n"
-          "    cubicle_edge=%.6g\n"
-          "    space_span=(%.6g,%.6g,%.6g)\n"
-          "    n_cubicles=(%u,%u,%u)",
-          cubicle_edge,
-          space_span[0], space_span[1], space_span[2],
-          n_cubicles[0], n_cubicles[1], n_cubicles[2]);
-        throw error(buf);
+        detail::throw_show_cubicle_dimensions(
+          "Not enough memory for cubicles:",
+          space_span,
+          cubicle_edge_,
+          n_cubicles);
       }
       ref = memory.ref();
     }
