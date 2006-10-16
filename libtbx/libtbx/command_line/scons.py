67997afda69eed735ad8652d3d01c771e587973e
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2006-10-16 23:09:56 +0000

    show_times moved from command_line/scons.py to utils.py

--- libtbx/libtbx/command_line/scons.py
+++ libtbx/libtbx/command_line/scons.py
@@ -1,31 +1,10 @@
-from libtbx.utils import Sorry
+from libtbx.utils import Sorry, show_times_at_exit
 from libtbx.str_utils import show_string
 import libtbx.load_env
 import time
 import sys, os
 
 time_start = time.time()
-
-def show_times():
-  t = os.times()
-  usr_plus_sys = t[0] + t[1]
-  try: ticks = sys.gettickeraccumulation()
-  except: ticks = None
-  s = "usr+sys time: %.2f seconds" % usr_plus_sys
-  if (ticks is not None):
-    s += ", ticks: %d" % ticks
-    if (ticks != 0):
-      s += ", micro-seconds/tick: %.3f" % (usr_plus_sys*1.e6/ticks)
-  print s
-  wall_clock_time = time.time() - time_start
-  print "wall clock time:",
-  if (wall_clock_time < 120):
-    print "%.2f seconds" % wall_clock_time
-  else:
-    m = int(wall_clock_time / 60 + 1.e-6)
-    s = wall_clock_time - m * 60
-    print "%d minutes %.2f seconds (%.2f seconds total)" % (
-      m, s, wall_clock_time)
 
 def find_scons_engine_path():
   join = os.path.join
@@ -61,8 +40,7 @@ def run():
       "  It may be necessary to rename the unpacked distribution, e.g.:",
       "    mv scons-0.96.1 scons"])
     raise Sorry("\n".join(msg))
-  import atexit
-  atexit.register(show_times)
+  show_times_at_exit(time_start=time_start)
   SCons.Script.main()
 
 if (__name__ == "__main__"):
