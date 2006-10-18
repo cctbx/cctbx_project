a3f80020ef57a1959ffc68ed782561481e019107
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2006-10-18 17:13:16 +0000

    support path_prefix

--- libtbx/libtbx/dlite.py
+++ libtbx/libtbx/dlite.py
@@ -12,28 +12,31 @@ class node_info(object):
     self.mtime = None
     self.md5 = None
 
-  def path_exists(self):
-    return os.path.exists(self.path)
+  def full_path(self, path_prefix=None):
+    if (path_prefix is None): return self.path
+    return os.path.join(path_prefix, self.path)
 
-  def current_mtime(self):
-    if (not self.path_exists()): return None
-    return os.path.getmtime(self.path)
+  def current_mtime(self, path_prefix=None):
+    full_path = self.full_path(path_prefix=path_prefix)
+    if (not os.path.exists(full_path)): return None
+    return os.path.getmtime(full_path)
 
-  def current_md5(self):
-    if (not self.path_exists()): return None
+  def current_md5(self, path_prefix=None):
+    full_path = self.full_path(path_prefix=path_prefix)
+    if (not os.path.exists(full_path)): return None
     m = md5.new()
-    m.update(open(self.path, "rb").read())
+    m.update(open(full_path, "rb").read())
     return m.hexdigest()
 
-  def has_changed(self, mtime_resolution=2):
+  def has_changed(self, path_prefix=None, mtime_resolution=2):
     old_mtime = self.mtime
     if (old_mtime is None): return True
-    self.mtime = self.current_mtime()
+    self.mtime = self.current_mtime(path_prefix=path_prefix)
     if (self.mtime == old_mtime
         and time.time() > old_mtime + mtime_resolution): return False
     if (self.md5 is None): return True
     old_md5 = self.md5
-    self.md5 = self.current_md5()
+    self.md5 = self.current_md5(path_prefix=path_prefix)
     return self.md5 != old_md5
 
 class pair_info(object):
@@ -43,25 +46,25 @@ class pair_info(object):
     self.target = node_info(path=target_path)
     self.needs_update = needs_update
 
-  def eval_needs_update(self, source_path=None):
+  def eval_needs_update(self, source_path=None, path_prefix=None):
     if (source_path != self.source.path):
       self.source = node_info(path=source_path)
       self.needs_update = True
     elif (not self.needs_update):
-      if (   self.source.has_changed()
-          or self.target.has_changed()):
+      if (   self.source.has_changed(path_prefix=path_prefix)
+          or self.target.has_changed(path_prefix=path_prefix)):
         self.needs_update = True
     return self.needs_update
 
-  def start_building_target(self):
+  def start_building_target(self, path_prefix=None):
     if (self.source.mtime is None):
-      self.source.mtime = self.source.current_mtime()
+      self.source.mtime = self.source.current_mtime(path_prefix=path_prefix)
     if (self.source.md5 is None):
-      self.source.md5 = self.source.current_md5()
+      self.source.md5 = self.source.current_md5(path_prefix=path_prefix)
 
-  def done_building_target(self):
-    self.target.mtime = self.target.current_mtime()
-    self.target.md5 = self.target.current_md5()
+  def done_building_target(self, path_prefix=None):
+    self.target.mtime = self.target.current_mtime(path_prefix=path_prefix)
+    self.target.md5 = self.target.current_md5(path_prefix=path_prefix)
     self.needs_update = False
 
 class target_db(object):
@@ -85,13 +88,14 @@ class target_db(object):
       os.remove(self.file_name)
     os.rename(self.file_name_during_write, self.file_name)
 
-  def pair_info(self, source_path, target_path):
+  def pair_info(self, source_path, target_path, path_prefix=None):
     result = self.pair_infos.get(target_path)
     if (result is None):
       result = pair_info(source_path=source_path, target_path=target_path)
       self.pair_infos[target_path] = result
     else:
-      result.eval_needs_update(source_path=source_path)
+      result.eval_needs_update(
+        source_path=source_path, path_prefix=path_prefix)
     return result
 
   def show(self, out=None):
