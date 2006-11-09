8ba7c13f7a762c9b5018c2b646c2c0904702e0a9
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2006-11-09 00:43:32 +0000

    make libtbx.valgrind work without having to define LIBTBX_VALGRIND first

--- libtbx/libtbx/command_line/valgrind.sh
+++ libtbx/libtbx/command_line/valgrind.sh
@@ -1,11 +1,16 @@
 #! /bin/sh
-if [ ! -n "$LIBTBX_VALGRIND" ]; then
-  echo 'Error: $LIBTBX_VALGRIND is not defined.'
-  exit 1
-fi
 valgrind --version
-if [ $? -eq 0 ]; then
-  LIBTBX__VALGRIND_FLAG__=1
-  export LIBTBX__VALGRIND_FLAG__
-  exec "$@"
+if [ $? -ne 0 ]; then exit 1; fi
+if [ ! -n "$LIBTBX_VALGRIND" ]; then
+  echo "### LIBTBX_VALGRIND not set: using default."
+  echo "### To override, define LIBTBX_VALGRIND"
+  echo "### before calling $LIBTBX_DISPATCHER_NAME."
+  LIBTBX_VALGRIND="valgrind --tool=memcheck --suppressions=`libtbx.show_dist_paths libtbx`/valgrind-python24.supp"
+  if [ $? -ne 0 ]; then exit 1; fi
+  export LIBTBX_VALGRIND
 fi
+echo "LIBTBX_VALGRIND=$LIBTBX_VALGRIND"
+if [ $? -ne 0 ]; then exit 1; fi
+LIBTBX__VALGRIND_FLAG__=1
+export LIBTBX__VALGRIND_FLAG__
+exec "$@"
