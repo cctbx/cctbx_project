c6423b042831bddbe1bb36c2a30e9fa3c53f9c58
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2006-11-15 00:47:10 +0000

    libtbx.utils.host_and_user: include SGE info if available

--- libtbx/libtbx/sge_utils.py
+++ libtbx/libtbx/sge_utils.py
@@ -6,13 +6,13 @@ def int_or_none(v):
   if (v is None or v == "undefined"): return None
   return int(v)
 
-def arch():
-  return os.environ.get("SGE_ARCH")
-
 def job_id():
   return int_or_none(os.environ.get("JOB_ID"))
 
-class task_info:
+def arch():
+  return os.environ.get("SGE_ARCH")
+
+class task_info(object):
 
   def __init__(self):
     self.first = int_or_none(os.environ.get("SGE_TASK_FIRST"))
@@ -20,14 +20,36 @@ class task_info:
     self.id = int_or_none(os.environ.get("SGE_TASK_ID"))
     assert [self.first, self.last, self.id].count(None) in [0, 3]
 
-  def show(self, out=None):
+  def show(self, out=None, prefix="", even_if_none=False):
     if (out is None): out = sys.stdout
-    print >> out, "SGE_TASK_FIRST:", self.first
-    print >> out, "SGE_TASK_LAST: ", self.last
-    print >> out, "SGE_TASK_ID:   ", self.id
+    if (self.first is not None or even_if_none):
+      print >> out, prefix+"SGE_TASK_FIRST =", self.first
+    if (self.last is not None or even_if_none):
+      print >> out, prefix+"SGE_TASK_LAST =", self.last
+    if (self.id is not None or even_if_none):
+      print >> out, prefix+"SGE_TASK_ID =", self.id
     return self
 
   def as_n_i_pair(self):
     if (self.first is None): return 1, 0
     assert self.first == 1
     return self.last, self.id-1
+
+class info(task_info):
+
+  def __init__(self):
+    task_info.__init__(self)
+    self.job_id = job_id()
+    self.arch = arch()
+
+  def show(self, out=None, prefix="", even_if_none=False):
+    if (out is None): out = sys.stdout
+    if (self.job_id is not None or even_if_none):
+      print >> out, prefix+"JOB_ID =", self.job_id
+    if (self.arch is not None or even_if_none):
+      print >> out, prefix+"SGE_ARCH =", self.arch
+    task_info.show(self, out=out, prefix=prefix, even_if_none=even_if_none)
+
+if (__name__ == "__main__"):
+  info().show(prefix="*** ", even_if_none=True)
+  print "OK"
