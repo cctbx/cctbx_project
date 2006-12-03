37156943c08ddda964b73f41af7e545e76f8b448
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2006-12-03 20:23:59 +0000

    source code duplication removed; no default element, wavelength, show usage if one or both missing; simple tests in regression/all_tests.csh

--- cctbx/cctbx/command_line/form_factor_query.py
+++ cctbx/cctbx/command_line/form_factor_query.py
@@ -1,14 +1,14 @@
+from cctbx.eltbx import sasaki, henke
 import libtbx.phil
 import libtbx.phil.command_line
-from libtbx.utils import Sorry
-from cctbx.eltbx import sasaki,henke
+from libtbx.utils import Sorry, Usage
 import sys
 
 master_params = libtbx.phil.parse("""\
 form_factor_query {
-  element = Se
+  element = None
     .type = str
-  wavelength = 1.5481
+  wavelength = None
     .type = float
   table = *sasaki henke
     .type = choice(multi = True)
@@ -17,58 +17,48 @@ form_factor_query {
 }
 """)
 
-def run(args):
+def run(args, command_name="cctbx.form_factor_query"):
   phil_objects=[]
   argument_interpreter = libtbx.phil.command_line.argument_interpreter(
     master_params=master_params, home_scope="form_factor_query")
-  symbol = None
-  wavelength = None
+  plain_element = None
+  plain_wavelength = None
   for arg in args:
     if (arg.find('=') != -1):
       try:
         command_line_params = argument_interpreter.process(arg=arg)
+      except KeyboardInterrupt: raise
       except:
         raise Sorry("Unknown keyword: %s" % arg)
       else:
         phil_objects.append(command_line_params)
     else:
       try:
-        wavelength = float(arg)
+        plain_wavelength = float(arg)
+      except KeyboardInterrupt: raise
       except:
-        symbol = arg
+        plain_element = arg
 
   working_params = master_params.fetch(sources=phil_objects)
   params = working_params.extract()
-
+  element = params.form_factor_query.element
+  if (element is None):
+    element = plain_element
+  wavelength = params.form_factor_query.wavelength
   if (wavelength is None):
-    wavelength = params.form_factor_query.wavelength
-  if (symbol is None):
-    symbol = params.form_factor_query.element
+    wavelength = plain_wavelength
   unit = params.form_factor_query.unit
 
-  if ('sasaki' in params.form_factor_query.table):
-    t = sasaki.table(symbol)
-    print "Information from %s table about %s (Z = %s) at %s %s" % (
-      "Sasaki", t.label(), t.atomic_number(), wavelength,
-      params.form_factor_query.unit)
-    if (unit == "angstroms"):
-      f = t.at_angstrom(wavelength)
-    elif (unit == "ev"):
-      f = t.at_ev(wavelength)
-    elif (unit == "kev"):
-      f = t.at_kev(wavelength)
-    else:
-      raise Sorry("Invalid unit chosen for query")
-    if (f.is_valid_fp()):
-      print "fp:", f.fp()
-      if (f.is_valid_fdp()):
-        print "fdp:", f.fdp()
+  if (element is None or wavelength is None):
+    raise Usage(
+      "%s element=symbol wavelength=float [table=sasaki|henke]"
+      " [unit=angstroms|ev|kev]" % command_name)
 
-  if ('henke' in params.form_factor_query.table):
-    t = henke.table(symbol)
+  for table in ["sasaki", "henke"]:
+    if (table not in params.form_factor_query.table): continue
+    t = eval(table).table(element)
     print "Information from %s table about %s (Z = %s) at %s %s" % (
-      "Henke", t.label(), t.atomic_number(), wavelength,
-      params.form_factor_query.unit)
+      table.capitalize(), t.label(), t.atomic_number(), wavelength, unit)
     if (unit == "angstroms"):
       f = t.at_angstrom(wavelength)
     elif (unit == "ev"):
@@ -76,11 +66,15 @@ def run(args):
     elif (unit == "kev"):
       f = t.at_kev(wavelength)
     else:
-      raise Sorry("Invalid unit chosen for query")
+      raise Sorry("Invalid unit chosen for query: %s" % unit)
     if (f.is_valid_fp()):
-      print "fp:", f.fp()
-      if (f.is_valid_fdp()):
-        print "fdp:", f.fdp()
+      print "fp:  %.6g" % f.fp()
+    else:
+      print "fp:  unknown"
+    if (f.is_valid_fdp()):
+      print "fdp: %.6g" % f.fdp()
+    else:
+      print "fdp: unknown"
 
 if (__name__ == "__main__"):
   run(sys.argv[1:])
