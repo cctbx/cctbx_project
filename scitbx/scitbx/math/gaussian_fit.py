f20e7a723f6830f76a317974799f22890dc800af
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2007-01-22 19:44:39 +0000

    minor cleanup to remove cctbx dependence

--- scitbx/scitbx/math/gaussian_fit.py
+++ scitbx/scitbx/math/gaussian_fit.py
@@ -3,7 +3,7 @@ from scitbx.math import golay_24_12_generator
 from scitbx import lbfgs
 from scitbx import lbfgsb
 from scitbx.examples import immoptibox_ports
-from cctbx.array_family import flex
+from scitbx.array_family import flex
 from scitbx.python_utils import dicts
 from libtbx.math_utils import ifloor
 from libtbx import adopt_init_args
