f88758a04ccde54186b421df6a74c40c132e8d63
Ian Davis <ian_davis@users.noreply.github.com>
2007-03-09 19:28:55 +0000

    - added tools for scoring backbone conformations according to the Richardson lab Ramachandran plots, following the example of the rotamer_eval.py file. - updated rebuild_rotarama_cache.py to also build Ramachandran data files. - new Rama data for ext_ref_files has been checked into rotarama_data SVN

--- mmtbx/mmtbx/command_line/rebuild_rotarama_cache.py
+++ mmtbx/mmtbx/command_line/rebuild_rotarama_cache.py
@@ -5,8 +5,12 @@ from libtbx.utils import format_cpu_times
 from libtbx.str_utils import show_string
 from mmtbx.rotamer.n_dim_table import NDimTable
 from mmtbx.rotamer import rotamer_eval
+from mmtbx.rotamer import ramachandran_eval
 import sys
 
+# NB:  this can be run from the command line as "mmtbx.rebuild_rotarama_cache"
+# if PHENIX paths have been initialized, but it only works properly
+# when run from inside the $PHENIX/ext_ref_files/rotarama_data/ directory.
 def run():
     rotamer_data_dir = rotamer_eval.find_rotamer_data_dir()
     print "Processing data files in %s:" % show_string(rotamer_data_dir)
@@ -19,6 +23,30 @@ def run():
           source_path=data_file,
           target_path=pickle_file,
           path_prefix=rotamer_data_dir)
+        print "  %s -> %s:" % (data_file, pickle_file),
+        if not pair_info.needs_update:
+            print "already up to date."
+        else:
+            print "converting ...",
+            sys.stdout.flush()
+            pair_info.start_building_target()
+            ndt = NDimTable.createFromText(data_file)
+            easy_pickle.dump(file_name=pickle_file, obj=ndt)
+            pair_info.done_building_target()
+            print "done."
+        sys.stdout.flush()
+    target_db.write()
+    ramachandran_data_dir = ramachandran_eval.find_ramachandran_data_dir()
+    print "Processing data files in %s:" % show_string(ramachandran_data_dir)
+    target_db = ramachandran_eval.open_rotarama_dlite(
+      rama_data_dir=ramachandran_data_dir)
+    for aa, aafile in ramachandran_eval.aminoAcids.items():
+        data_file = "rama500-"+aafile+".data"
+        pickle_file = "rama500-"+aafile+".pickle"
+        pair_info = target_db.pair_info(
+          source_path=data_file,
+          target_path=pickle_file,
+          path_prefix=ramachandran_data_dir)
         print "  %s -> %s:" % (data_file, pickle_file),
         if not pair_info.needs_update:
             print "already up to date."
