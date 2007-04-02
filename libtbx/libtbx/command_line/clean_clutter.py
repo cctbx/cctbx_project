a41a24ead3d93d7bd3f37e893ff46a8e39f4070c
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-04-02 22:59:14 +0000

    - the scripts can now take as command line arguments 	+ either the list of files to clean 	+ or --committing or -c: it then cleans those files marked as Modified, Added or Replaced by svn

--- libtbx/libtbx/command_line/clean_clutter.py
+++ libtbx/libtbx/command_line/clean_clutter.py
@@ -1,10 +1,11 @@
 import sys
 from fileinput import input, isfirstline, filename, isstdin
 from string import expandtabs, rstrip
+from libtbx import subversion
 
-def run():
+def clean_clutter_in(files):
   n_empty = 0
-  for line in input(inplace=1):
+  for line in input(files, inplace=1):
     if (isfirstline()):
       if (not isstdin()):
         print >> sys.__stdout__, filename() + ':'
@@ -17,6 +18,32 @@ def run():
       n_empty = 0
       sys.stdout.write(clean_line)
       sys.stdout.write("\n")
+
+def run():
+  files = []
+  for arg in sys.argv[1:]:
+    if arg == '--committing' or arg == '-c':
+      if files:
+        print_usage()
+        exit(1)
+      else:
+        files = list(subversion.marked_for_commit())
+    else:
+      files.append(arg)
+  if not files:
+    print_usage()
+    exit(1)
+  clean_clutter_in(files)
+
+def print_usage():
+  print """
+Usage:
+  clean_clutter file1 file2 ...
+  clean_clutter --committing|-c
+
+The first form cleans the specified files whereas the second form cleans those
+files which would be committed by running "svn commit"
+"""
 
 if (__name__ == "__main__"):
   run()
