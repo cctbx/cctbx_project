3c21766fb72781734c51b803b166e7bdca07baad
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2007-04-08 21:06:57 +0000

    new MMTBX_ERROR macro

--- mmtbx/error.h
+++ mmtbx/error.h
@@ -5,45 +5,10 @@
 #ifndef MMTBX_ERROR_H
 #define MMTBX_ERROR_H
 
-#include <stdio.h>
-#include <exception>
-#include <string>
-
-#define MMTBX_CHECK_POINT std::cout << __FILE__ << "(" << __LINE__ << ")" << std::endl << std::flush
+#include <scitbx/error.h>
 
 //! Common mmtbx namespace.
 namespace mmtbx {
-
-  namespace detail {
-
-    inline
-    std::string
-    compose_error_message(
-      std::string const& prefix,
-      std::string const& msg)
-    {
-      return prefix + " Error: " + msg;
-    }
-
-    inline
-    std::string
-    compose_error_message(
-      std::string const& prefix,
-      const char* file,
-      long line,
-      std::string const& msg,
-      bool internal)
-    {
-      const char *s = "";
-      if (internal) s = " Internal";
-      char buf[64];
-      sprintf(buf, "%ld", line);
-      std::string result = prefix + s + " Error: " + file + "(" + buf + ")";
-      if (msg.size()) result += std::string(": ") + msg;
-      return result;
-    }
-
-  }
 
   //! All mmtbx exceptions are derived from this class.
   class error : public std::exception
@@ -52,7 +17,7 @@ namespace mmtbx {
       //! General mmtbx error message.
       explicit
       error(std::string const& msg) throw()
-      : msg_(detail::compose_error_message("mmtbx", msg))
+      : msg_(scitbx::detail::compose_error_message("mmtbx", msg))
       {}
 
       //! Error message with file name and line number.
@@ -60,7 +25,7 @@ namespace mmtbx {
        */
       error(const char* file, long line, std::string const& msg = "",
             bool internal = true) throw()
-      : msg_(detail::compose_error_message(
+      : msg_(scitbx::detail::compose_error_message(
           "mmtbx", file, line, msg, internal))
       {}
 
@@ -83,24 +48,10 @@ namespace mmtbx {
       std::string msg_;
   };
 
-  //! Special class for "Index out of range." exceptions.
-  /*! These exceptions are propagated to Python as IndexError.
-   */
-  class error_index : public error
-  {
-    public:
-      //! Default constructor. The message may be customized.
-      explicit
-      error_index(std::string const& msg = "Index out of range.") throw()
-      : error(msg)
-      {}
-
-      //! Virtual destructor.
-      virtual ~error_index() throw() {}
-  };
-
 } // namespace mmtbx
 
+//! For throwing an error exception with file name, line number, and message.
+#define MMTBX_ERROR(msg) ::mmtbx::error(__FILE__, __LINE__, msg, false)
 //! For throwing an "Internal Error" exception.
 #define MMTBX_INTERNAL_ERROR() ::mmtbx::error(__FILE__, __LINE__)
 //! For throwing a "Not implemented" exception.
