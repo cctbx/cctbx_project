2099ca9a2df1fcf30c8271fef4c5bc845b5f41d4
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-05-14 19:10:56 +0000

    - add the alternative test of setting the state of random.random as it was before the generation of the structure crashing tst_xray_minimization and then running tst_xray_minimization.exercise

--- cctbx/cctbx/regression/tst_xray_minimization_failure.py
+++ cctbx/cctbx/regression/tst_xray_minimization_failure.py
@@ -1,7 +1,11 @@
 from cctbx import xray
+from cctbx.development import random_structure
 from cctbx.array_family import flex
+from cctbx.regression import tst_xray_minimization
+from cctbx import sgtbx
 
-def run():
+
+def test1():
   import libtbx.easy_pickle
   import os.path
   structure_ideal = libtbx.easy_pickle.load(
@@ -18,10 +22,12 @@ def run():
     cos_sin_table=True).f_calc()
   y_obs = abs(rnd_f_calc)
   structure_shake = structure_start.deep_copy_scatterers()
+  structure_shake_bis = structure_ideal.deep_copy_scatterers()
+  tst_xray_minimization.shift_u_aniso(structure_shake_bis, 0.001)
   minimizer = xray.minimization.lbfgs(
     target_functor=xray.target_functors.intensity_correlation(y_obs),
     xray_structure=structure_shake,
-    #use_special_position_constraints=False,
+    use_special_position_constraints=False,
     occupancy_penalty=None,
     structure_factor_algorithm="direct")
   assert minimizer.final_target_value < minimizer.first_target_value
@@ -36,6 +42,28 @@ def run():
   assert c.is_well_defined()
   c_coefficient = c.coefficient()
   assert c_coefficient > 0.999
+
+def test2():
+  import random
+  import libtbx.easy_pickle
+  random.setstate(libtbx.easy_pickle.load('random_generator_state.pickle'))
+  tst_xray_minimization.exercise(
+    target_functor=xray.target_functors.intensity_correlation,
+    data_type='F',
+    space_group_info= sgtbx.space_group_info('R -3 c :H'),
+    anomalous_flag=False,
+    gradient_flags=xray.structure_factors.gradient_flags(
+                                        site      = 0,
+                                        u_iso     = 0,
+                                        u_aniso   = 1,
+                                        occupancy = 0),
+    occupancy_penalty=None,
+    d_min=2.5 )
+
+def run():
+  test2()
+  test1()
+  print 'OK'
 
 
 if __name__ == '__main__':
