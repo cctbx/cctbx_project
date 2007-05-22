2a3f0c421409dfddfe2d871f4294c8c5424d3779
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-05-22 19:23:32 +0000

    - add a bund of tests: only tst_minimization_at_random.py, a shameful copy of cctbx.regression.tst_xray_minimization, does anything - note: the latter has the following tweaks compared to the original: 	+ if the assert for the correlation fails, the same test is executed twice with 2 new random structures and they pass, the assertion error is considered harmless; the idea is to avoid flukes 	+ the script takes more command lines argument to choose whether to do F or F^2 or special position constraints or not; instead of doing more inner loops to do all cases

---
+++ smtbx/refinement/tests/tst_minimization.py
@@ -0,0 +1,37 @@
+from smtbx import refinement
+from iotbx.shelx.from_ins import from_ins
+from iotbx.reflection_file_reader import any_reflection_file
+from cctbx import xray
+
+def exercise_disorder(name):
+  xs0 = from_ins('%s.res' % name)
+  data_file = any_reflection_file('hklf4=%s.hkl' % name)
+  f_obs_sqr = data_file.as_miller_arrays(xs0.crystal_symmetry())[0]
+  xs = refinement.tests.shaked_structure(xs0,
+                                         thermal_shift=0.01, # in %
+                                         site_shift=0.02)
+  for a in xs.scatterers():
+    a.flags.set_grad_site(True)
+    if a.flags.use_u_iso(): a.flags.set_grad_u_iso(True)
+    if a.flags.use_u_aniso(): a.flags.set_grad_u_aniso(True)
+    assert not( a.flags.use_u_iso() and a.flags.grad_u_aniso() )
+    assert not( a.flags.use_u_aniso() and a.flags.grad_u_iso() )
+  ls = xray.unified_least_squares_residual(f_obs_sqr)
+  minimisation = refinement.minimization.lbfgs(
+    target_functor=ls,
+    xray_structure=xs,
+    cos_sin_table=True,
+  )
+  
+  
+
+def run():
+  exercise_disorder(name='6-ring-twist-disorder')
+  print 'OK'
+
+if __name__ == '__main__':
+  run()
+
+
+
+
