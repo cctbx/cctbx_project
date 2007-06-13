2b4f01175f8962091f651b4c11de7a9331d1ced7
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-06-13 15:51:55 +0000

    - "pro" command line option parsing - add the option -t | --tabsize to specify the number of spaces to replace a tab with

--- libtbx/libtbx/command_line/clean_clutter.py
+++ libtbx/libtbx/command_line/clean_clutter.py
@@ -1,16 +1,16 @@
 import sys
 from fileinput import input, isfirstline, filename, isstdin
-from string import expandtabs, rstrip
 from libtbx import subversion
+from libtbx.option_parser import option_parser
 
-def clean_clutter_in(files):
+def clean_clutter_in(files, tabsize=8):
   n_empty = 0
   for line in input(files, inplace=1):
     if (isfirstline()):
       if (not isstdin()):
         print >> sys.__stdout__, filename() + ':'
       n_empty = 0
-    clean_line = line.expandtabs().rstrip()
+    clean_line = line.expandtabs(tabsize).rstrip()
     if (len(clean_line) == 0):
       n_empty += 1
     else:
@@ -20,30 +20,40 @@ def clean_clutter_in(files):
       sys.stdout.write("\n")
 
 def run():
-  files = []
-  for arg in sys.argv[1:]:
-    if arg == '--committing' or arg == '-c':
-      if files:
-        print_usage()
-        exit(1)
-      else:
-        files = list(subversion.marked_for_commit())
-    else:
-      files.append(arg)
-  if not files:
-    print_usage()
+  opt_parser = (option_parser(
+    usage="""
+clean_clutter [-t n | --tabsize==n] file1 file2 ...
+clean_clutter [-t n | --tabsize==n] --committing|-c""",
+    description="""The first form cleans the specified files whereas the second
+form cleans those files which would be committed by running svn commit.""")
+    .option("-t", "--tabsize",
+      action="store",
+      type="int",
+      default=8,
+      help="the number of spaces a tab is to be replaced by",
+      metavar="INT")
+     .option("-c", "--committing",
+      action="store_true",
+      default=False,
+      help="whether to clean the files which are to be committed")
+  )
+  command_line = opt_parser.process(args=sys.argv[1:])
+  co = command_line.options
+  files = command_line.args
+  if co.committing and files:
+      opt_parser.show_help()
+      exit(1)
+  if co.committing:
+    try:
+      files = list(subversion.marked_for_commit())
+    except RuntimeError, err:
+      print err
+      exit(1)
+  elif not files:
+    opt_parser.show_help()
     exit(1)
-  clean_clutter_in(files)
-
-def print_usage():
-  print """
-Usage:
-  clean_clutter file1 file2 ...
-  clean_clutter --committing|-c
-
-The first form cleans the specified files whereas the second form cleans those
-files which would be committed by running "svn commit"
-"""
+  clean_clutter_in(files, tabsize=co.tabsize)
 
 if (__name__ == "__main__"):
+  import sys
   run()
