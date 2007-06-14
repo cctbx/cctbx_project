3b54290856d1763579cfe36728a16024384fabaf
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-06-14 22:18:54 +0000

    - refactor the exception business for the scitbx, cctbx and mmtbx: + scitbx defines the template smart_error and the macros which takes care of all the machinery + the error headers in the scitbx, cctbx and mmtbx are just pure boiler plate
    - the machinery in smart_error relies entirely on std::ostringstream and a stupid bug is corrected in what()

--- mmtbx/error.h
+++ mmtbx/error.h
@@ -5,19 +5,20 @@
 #ifndef MMTBX_ERROR_H
 #define MMTBX_ERROR_H
 
-#include <scitbx/error.h>
+#include <scitbx/smart_error.h>
 
 //! Common mmtbx namespace.
 namespace mmtbx {
 
   //! All mmtbx exceptions are derived from this class.
-  class error : public std::exception
+  class error : public scitbx::smart_error<error>
   {
     public:
+
       //! General mmtbx error message.
       explicit
       error(std::string const& msg) throw()
-      : msg_(scitbx::detail::compose_error_message("mmtbx", msg))
+        : scitbx::smart_error<error>("mmtbx", msg)
       {}
 
       //! Error message with file name and line number.
@@ -25,41 +26,34 @@ namespace mmtbx {
        */
       error(const char* file, long line, std::string const& msg = "",
             bool internal = true) throw()
-      : msg_(scitbx::detail::compose_error_message(
-          "mmtbx", file, line, msg, internal))
+        : scitbx::smart_error<error>("mmtbx", file, line, msg, internal)
       {}
+  };
 
-      //! Virtual destructor.
-      virtual ~error() throw() {}
-
-      //! Access to the error messages.
-      virtual const char* what() const throw()
-      {
-        return msg_.c_str();
-      }
-
-    protected:
-      struct complete_msg_tag {};
-
-      error(std::string const& msg, complete_msg_tag)
-      : msg_(msg)
+  //! Special class for "Index out of range." exceptions.
+  /*! These exceptions are propagated to Python as IndexError.
+   */
+  class error_index : public error
+  {
+    public:
+      //! Default constructor. The message may be customized.
+      explicit
+      error_index(std::string const& msg = "Index out of range.") throw()
+        : error(msg)
       {}
-
-      std::string msg_;
   };
 
 } // namespace mmtbx
 
 //! For throwing an error exception with file name, line number, and message.
-#define MMTBX_ERROR(msg) ::mmtbx::error(__FILE__, __LINE__, msg, false)
+#define MMTBX_ERROR(msg) REPORT_ERROR(mmtbx::error, msg)
 //! For throwing an "Internal Error" exception.
-#define MMTBX_INTERNAL_ERROR() ::mmtbx::error(__FILE__, __LINE__)
+#define MMTBX_INTERNAL_ERROR() REPORT_INTERNAL_ERROR(mmtbx::error)
 //! For throwing a "Not implemented" exception.
-#define MMTBX_NOT_IMPLEMENTED() ::mmtbx::error(__FILE__, __LINE__, \
-             "Not implemented.")
+#define MMTBX_NOT_IMPLEMENTED() REPORT_NOT_IMPLEMENTED(mmtbx::error)
+
 //! Custom mmtbx assertion.
-#define MMTBX_ASSERT(bool) \
-  if (!(bool)) throw ::mmtbx::error(__FILE__, __LINE__,\
-    "MMTBX_ASSERT(" # bool ") failure.")
+#define MMTBX_ASSERT(assertion)\
+  SMART_ASSERT(mmtbx::error, MMTBX_ASSERT, assertion)
 
 #endif // MMTBX_ERROR_H
--- scitbx/include/scitbx/error.h
+++ scitbx/include/scitbx/error.h
@@ -10,64 +10,25 @@
 #include <string>
 #include <sstream>
 
-#ifdef _MSC_VER
-# pragma warning(disable:4355)
-#endif
+#include <scitbx/smart_error.h>
 
-#define SCITBX_CHECK_POINT std::cout << __FILE__ << "(" << __LINE__ << ")" << std::endl << std::flush
-#define SCITBX_EXAMINE(A) std::cout << "variable " <<#A<< ": " << A << " " << std::endl << std::flush
+#define SCITBX_CHECK_POINT\
+  std::cout << __FILE__ << "(" << __LINE__ << ")" << std::endl << std::flush
+#define SCITBX_EXAMINE(A)\
+  std::cout << "variable " <<#A<< ": " << A << " " << std::endl << std::flush
 
 //! Common scitbx namespace.
 namespace scitbx {
 
-  namespace detail {
-
-    inline
-    std::string
-    compose_error_message(
-      std::string const& prefix,
-      std::string const& msg)
-    {
-      return prefix + " Error: " + msg;
-    }
-
-    inline
-    std::string
-    compose_error_message(
-      std::string const& prefix,
-      const char* file,
-      long line,
-      std::string const& msg,
-      bool internal)
-    {
-      std::ostringstream o;
-      o << prefix;
-      if (internal) o << " Internal";
-      o << "Error: " << file << "(" << line << ")";
-      if (msg.size()) o << ": " << msg;
-      return o.str();
-//      const char *s = "";
-//      if (internal) s = " Internal";
-//      char buf[64];
-//      sprintf(buf, "%ld", line);
-//      std::string result = prefix + s + " Error: " + file + "(" + buf + ")";
-//      if (msg.size()) result += std::string(": ") + msg;
-//      return result;
-    }
-
-  }
-
   //! All scitbx exceptions are derived from this class.
-  class error : public std::exception
+  class error : public smart_error<error>
   {
     public:
-      error &SCITBX_ASSERT_HACK_A, &SCITBX_ASSERT_HACK_B;
 
       //! General scitbx error message.
       explicit
       error(std::string const& msg) throw()
-      : msg_(detail::compose_error_message("scitbx", msg)),
-        SCITBX_ASSERT_HACK_A(*this), SCITBX_ASSERT_HACK_B(*this)
+        : smart_error<error>("scitbx", msg)
       {}
 
       //! Error message with file name and line number.
@@ -75,40 +36,8 @@ namespace scitbx {
        */
       error(const char* file, long line, std::string const& msg = "",
             bool internal = true) throw()
-      : msg_(detail::compose_error_message(
-          "scitbx", file, line, msg, internal)),
-        SCITBX_ASSERT_HACK_A(*this), SCITBX_ASSERT_HACK_B(*this)
+        : smart_error<error>("scitbx", file, line, msg, internal)
       {}
-
-      //! Virtual destructor.
-      virtual ~error() throw() {}
-
-      //! Access to the error messages.
-      virtual const char* what() const throw()
-      {
-        std::string result = msg_;
-        if (values_.length() > 0) result += "\n" + values_;
-        return result.c_str();
-      }
-
-      template<typename T>
-      error& with_current_value(const T& value, const char* label) {
-        std::ostringstream o;
-        o << "\t" << label << " = " << value << "\n";
-        values_ += o.str();
-        return *this;
-      }
-
-    protected:
-      struct complete_msg_tag {};
-
-      error(std::string const& msg, complete_msg_tag)
-      : msg_(msg),
-        SCITBX_ASSERT_HACK_A(*this), SCITBX_ASSERT_HACK_B(*this)
-      {}
-
-      std::string msg_;
-      std::string values_;
   };
 
   //! Special class for "Index out of range." exceptions.
@@ -120,48 +49,21 @@ namespace scitbx {
       //! Default constructor. The message may be customized.
       explicit
       error_index(std::string const& msg = "Index out of range.") throw()
-      : error(msg)
+        : error(msg)
       {}
-
-      //! Virtual destructor.
-      virtual ~error_index() throw() {}
   };
 
 } // namespace scitbx
 
 //! For throwing an error exception with file name, line number, and message.
-#define SCITBX_ERROR(msg) ::scitbx::error(__FILE__, __LINE__, msg, false)
+#define SCITBX_ERROR(msg) REPORT_ERROR(scitbx::error, msg)
 //! For throwing an "Internal Error" exception.
-#define SCITBX_INTERNAL_ERROR() ::scitbx::error(__FILE__, __LINE__)
+#define SCITBX_INTERNAL_ERROR() REPORT_INTERNAL_ERROR(scitbx::error)
 //! For throwing a "Not implemented" exception.
-#define SCITBX_NOT_IMPLEMENTED() ::scitbx::error(__FILE__, __LINE__, \
-             "Not implemented.")
+#define SCITBX_NOT_IMPLEMENTED() REPORT_NOT_IMPLEMENTED(scitbx::error)
 
 //! Custom scitbx assertion.
-/*!
-Here is an example of use:
- \code
- SCITBX_ASSERT(n > 0 && m < n)(m)(n);
- \endcode
- The first parenthesis contains the expression to assert whereas the subsequent
- parentheses contain the variables whose values will be reported upon failure of
- the assertion. The C++ stream system must know how to handle the type of m and
- n through the operator <<.
- If the condition is violated, an instance of scitbx::error is thrown.
-
- The implementation uses the hacks described in [1].
-
- [1] A. Alexandrescu and J. Torjo, "Enhancing Assertions",
-     C/C++ Users Journal, August 2003
- */
-
-#define SCITBX_ASSERT_HACK_A(x) SCITBX_ASSERT_HACK_OP(x, B)
-#define SCITBX_ASSERT_HACK_B(x) SCITBX_ASSERT_HACK_OP(x, A)
-#define SCITBX_ASSERT_HACK_OP(x, next) \
-SCITBX_ASSERT_HACK_A.with_current_value((x), #x).SCITBX_ASSERT_HACK_ ## next
-
-#define SCITBX_ASSERT(bool) \
-  if (!(bool)) throw ::scitbx::error(__FILE__, __LINE__,\
-    "SCITBX_ASSERT(" # bool ") failure.").SCITBX_ASSERT_HACK_A
+#define SCITBX_ASSERT(assertion)\
+  SMART_ASSERT(scitbx::error, SCITBX_ASSERT, assertion)
 
 #endif // SCITBX_ERROR_H
