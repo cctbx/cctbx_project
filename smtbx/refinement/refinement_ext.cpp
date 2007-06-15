5f345fa769db3b560eda7a0f50926059b36afdfa
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-06-15 14:49:40 +0000

    - remove the wrapper for the defunct u_cart special position object in the smtbx

--- smtbx/refinement/refinement_ext.cpp
+++ smtbx/refinement/refinement_ext.cpp
@@ -3,13 +3,11 @@
 namespace smtbx { namespace refinement { namespace boost_python {
 
   void wrap_minimization();
-  void wrap_u_cart_special_position_constraints();
 
   namespace {
 
     void init_module() {
       wrap_minimization();
-      wrap_u_cart_special_position_constraints();
     }
 
   } // namespace anonymous
