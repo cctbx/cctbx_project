4b55428ace99ec69217f1576c499dc5abb65b39f
Pavel Afonine <pafonine@users.noreply.github.com>
2007-06-21 16:00:01 +0000

    accomodate COMPLETE re-done for IAS code

--- mmtbx/mmtbx/dbe/__init__.py
+++ mmtbx/mmtbx/dbe/__init__.py
@@ -41,12 +41,6 @@ dbe_master_params = iotbx.phil.parse("""\
      scaling = *volume sigma
        .type=choice(multi=False)
   }
-  restraints {
-    selection = D1 D2 D3 D4 D5 D6 D7 D8 D9 D10
-      .type = strings
-    weight_scale = 10.0
-      .type = float
-  }
 """)
 
 ias_scattering_dict = {
