5df39e6d3ee2d5a175e69ef35abae38f068fe2fa
Pavel Afonine <pafonine@users.noreply.github.com>
2007-07-05 18:16:38 +0000

    add tolerance; phenix -> mmtbx

--- mmtbx/mmtbx/tst_pdbtools.py
+++ mmtbx/mmtbx/tst_pdbtools.py
@@ -42,7 +42,7 @@ def run(file_name = "phe_e.pdb"):
   output = "modified.pdb"
   xrsp_init = xray_structure_plus(file_name = file_name)
   base = \
-      "phenix.pdbtools %s output.pdb.file_name=%s --quiet "%(file_name, output)
+      "mmtbx.pdbtools %s output.pdb.file_name=%s --quiet "%(file_name, output)
   for selection_str in [None, "chain A or chain C"]:
     selection = xrsp_init.selection(selection_strings = selection_str)
     if(selection_str is None):
@@ -88,134 +88,147 @@ def run(file_name = "phe_e.pdb"):
   cmd = base
   check_all_none(cmd, xrsp_init, output)
 
-def check_adp_rand(cmd, xrsp_init, output, selection, selection_str):
+def check_adp_rand(cmd, xrsp_init, output, selection, selection_str,
+                                                              tolerance=1.e-3):
   easy_run.call(cmd)
   xrsp = xray_structure_plus(file_name = output)
-  assert approx_equal(xrsp.occ,        xrsp_init.occ)
-  assert approx_equal(xrsp.sites_cart, xrsp_init.sites_cart)
-  assert approx_equal(xrsp.use_u_iso,  xrsp_init.use_u_iso)
-  assert approx_equal(xrsp.use_u_aniso,xrsp_init.use_u_aniso)
-  assert approx_equal(xrsp.u_iso_not_used,  xrsp_init.u_iso_not_used)
-  assert approx_equal(xrsp.u_cart_not_used, xrsp_init.u_cart_not_used)
+  assert approx_equal(xrsp.occ,        xrsp_init.occ,tolerance)
+  assert approx_equal(xrsp.sites_cart, xrsp_init.sites_cart,tolerance)
+  assert approx_equal(xrsp.use_u_iso,  xrsp_init.use_u_iso,tolerance)
+  assert approx_equal(xrsp.use_u_aniso,xrsp_init.use_u_aniso,tolerance)
+  assert approx_equal(xrsp.u_iso_not_used,  xrsp_init.u_iso_not_used,tolerance)
+  assert approx_equal(xrsp.u_cart_not_used,xrsp_init.u_cart_not_used,tolerance)
   if(selection_str is None):
-    assert not_approx_equal(xrsp.u_iso_used,  xrsp_init.u_iso_used)
-    assert not_approx_equal(xrsp.u_cart_used, xrsp_init.u_cart_used)
+    assert not_approx_equal(xrsp.u_iso_used,  xrsp_init.u_iso_used,tolerance)
+    assert not_approx_equal(xrsp.u_cart_used, xrsp_init.u_cart_used,tolerance)
   else:
     arg1 = xrsp.u_iso_used.select(selection.select(xrsp.use_u_iso))
     arg2 = xrsp_init.u_iso_used.select(selection.select(xrsp_init.use_u_iso))
-    if(arg1.size() > 0): assert not_approx_equal(arg1, arg2)
+    if(arg1.size() > 0): assert not_approx_equal(arg1, arg2,tolerance)
     arg1 =xrsp.u_cart_used.select(selection.select(xrsp.use_u_aniso))
     arg2 =xrsp_init.u_cart_used.select(selection.select(xrsp_init.use_u_aniso))
-    if(arg1.size() > 0): assert not_approx_equal(arg1, arg2)
+    if(arg1.size() > 0): assert not_approx_equal(arg1, arg2,tolerance)
   easy_run.call("rm -rf %s "%output)
 
-def check_adp_set_b_iso(cmd, xrsp_init, output, selection, selection_str):
+def check_adp_set_b_iso(cmd, xrsp_init, output, selection, selection_str,
+                                                              tolerance=1.e-3):
   easy_run.call(cmd)
   xrsp = xray_structure_plus(file_name = output)
-  assert approx_equal(xrsp.occ,        xrsp_init.occ)
-  assert approx_equal(xrsp.sites_cart, xrsp_init.sites_cart)
-  assert approx_equal(xrsp.use_u_iso,  xrsp_init.use_u_iso)
-  assert approx_equal(xrsp.use_u_aniso,xrsp_init.use_u_aniso)
-  assert approx_equal(xrsp.u_iso_not_used,  xrsp_init.u_iso_not_used)
-  assert approx_equal(xrsp.u_cart_not_used, xrsp_init.u_cart_not_used)
+  assert approx_equal(xrsp.occ,        xrsp_init.occ,tolerance)
+  assert approx_equal(xrsp.sites_cart, xrsp_init.sites_cart,tolerance)
+  assert approx_equal(xrsp.use_u_iso,  xrsp_init.use_u_iso,tolerance)
+  assert approx_equal(xrsp.use_u_aniso,xrsp_init.use_u_aniso,tolerance)
+  assert approx_equal(xrsp.u_iso_not_used,  xrsp_init.u_iso_not_used,tolerance)
+  assert approx_equal(xrsp.u_cart_not_used,xrsp_init.u_cart_not_used,tolerance)
   if(selection_str is None):
-    assert not_approx_equal(xrsp.u_iso_used,  xrsp_init.u_iso_used)
-    assert approx_equal(xrsp.u_cart, xrsp_init.u_cart)
+    assert not_approx_equal(xrsp.u_iso_used,  xrsp_init.u_iso_used,tolerance)
+    assert approx_equal(xrsp.u_cart, xrsp_init.u_cart,tolerance)
   else:
     arg1 = xrsp.u_iso_used.select(selection.select(xrsp.use_u_iso))
     arg2 = xrsp_init.u_iso_used.select(selection.select(xrsp_init.use_u_iso))
-    if(arg1.size() > 0): assert not_approx_equal(arg1, arg2)
-    assert approx_equal(xrsp.u_cart, xrsp_init.u_cart)
+    if(arg1.size() > 0): assert not_approx_equal(arg1, arg2,tolerance)
+    assert approx_equal(xrsp.u_cart, xrsp_init.u_cart,tolerance)
   easy_run.call("rm -rf %s "%output)
 
-def check_adp_to_iso(cmd, xrsp_init, output, selection, selection_str):
+def check_adp_to_iso(cmd, xrsp_init, output, selection, selection_str,
+                                                              tolerance=1.e-3):
   easy_run.call(cmd)
   xrsp = xray_structure_plus(file_name = output)
-  assert approx_equal(xrsp.occ,        xrsp_init.occ)
-  assert approx_equal(xrsp.sites_cart, xrsp_init.sites_cart)
-  assert not_approx_equal(xrsp.use_u_iso,  xrsp_init.use_u_iso)
-  assert not_approx_equal(xrsp.use_u_aniso,xrsp_init.use_u_aniso)
+  assert approx_equal(xrsp.occ,        xrsp_init.occ,tolerance)
+  assert approx_equal(xrsp.sites_cart, xrsp_init.sites_cart,tolerance)
+  assert not_approx_equal(xrsp.use_u_iso,  xrsp_init.use_u_iso,tolerance)
+  assert not_approx_equal(xrsp.use_u_aniso,xrsp_init.use_u_aniso,tolerance)
   assert xrsp.u_iso_not_used.size() == 0
   assert xrsp_init.u_iso_not_used.size() > 0
   assert xrsp.u_cart_used.size() == 0
   assert xrsp_init.u_cart_used.size() > 0
   easy_run.call("rm -rf %s "%output)
 
-def check_adp_to_aniso(cmd, xrsp_init, output, selection, selection_str):
+def check_adp_to_aniso(cmd, xrsp_init, output, selection, selection_str,
+                                                              tolerance=1.e-3):
   easy_run.call(cmd)
   xrsp = xray_structure_plus(file_name = output)
-  assert approx_equal(xrsp.occ,        xrsp_init.occ)
-  assert approx_equal(xrsp.sites_cart, xrsp_init.sites_cart)
+  assert approx_equal(xrsp.occ,        xrsp_init.occ,tolerance)
+  assert approx_equal(xrsp.sites_cart, xrsp_init.sites_cart,tolerance)
   if(selection_str is None):
-    assert not_approx_equal(xrsp.use_u_iso,  xrsp_init.use_u_iso)
-    assert not_approx_equal(xrsp.use_u_aniso,xrsp_init.use_u_aniso)
+    assert not_approx_equal(xrsp.use_u_iso,  xrsp_init.use_u_iso,tolerance)
+    assert not_approx_equal(xrsp.use_u_aniso,xrsp_init.use_u_aniso,tolerance)
     assert xrsp.u_iso_used.size() == 0
     assert xrsp_init.u_iso_used.size() > 0
     assert xrsp.u_cart_not_used.size() == 0
     assert xrsp_init.u_cart_not_used.size() > 0
   else:
-    assert approx_equal(xrsp.use_u_iso,  xrsp_init.use_u_iso)
-    assert approx_equal(xrsp.use_u_aniso,xrsp_init.use_u_aniso)
+    assert approx_equal(xrsp.use_u_iso,  xrsp_init.use_u_iso,tolerance)
+    assert approx_equal(xrsp.use_u_aniso,xrsp_init.use_u_aniso,tolerance)
   easy_run.call("rm -rf %s "%output)
 
-def check_sites_shake(cmd, xrsp_init, output, selection, selection_str, shake):
+def check_sites_shake(cmd, xrsp_init, output, selection, selection_str, shake,
+                                                              tolerance=1.e-3):
   easy_run.call(cmd)
   xrsp = xray_structure_plus(file_name = output)
-  assert approx_equal(xrsp.occ,    xrsp_init.occ)
-  assert approx_equal(xrsp.u_iso,  xrsp_init.u_iso)
-  assert approx_equal(xrsp.u_cart, xrsp_init.u_cart)
+  assert approx_equal(xrsp.occ,    xrsp_init.occ,tolerance)
+  assert approx_equal(xrsp.u_iso,  xrsp_init.u_iso,tolerance)
+  assert approx_equal(xrsp.u_cart, xrsp_init.u_cart,tolerance)
   if(selection_str is None):
     diff = xrsp.sites_cart - xrsp_init.sites_cart
-    assert approx_equal(math.sqrt(flex.mean(diff.dot())), shake, 1.e-3)
+    assert approx_equal(
+                      math.sqrt(flex.mean(diff.dot())), shake, 1.e-3,tolerance)
   else:
     diff = xrsp.sites_cart - xrsp_init.sites_cart
-    assert approx_equal(math.sqrt(flex.mean(diff.select(selection).dot())),
-                                                                  shake, 1.e-3)
-    assert approx_equal(math.sqrt(flex.mean(diff.select(~selection).dot())),0.)
+    assert approx_equal(
+              math.sqrt(flex.mean(diff.select(selection).dot())), shake, 1.e-3)
+    assert approx_equal(
+              math.sqrt(flex.mean(diff.select(~selection).dot())),0.,tolerance)
   easy_run.call("rm -rf %s "%output)
 
-def check_sites_rt(cmd, xrsp_init, output, selection, selection_str):
+def check_sites_rt(cmd, xrsp_init, output, selection, selection_str,
+                                                              tolerance=1.e-3):
   easy_run.call(cmd)
   xrsp = xray_structure_plus(file_name = output)
-  assert approx_equal(xrsp.occ,    xrsp_init.occ)
-  assert approx_equal(xrsp.u_iso,  xrsp_init.u_iso)
-  assert approx_equal(xrsp.u_cart, xrsp_init.u_cart)
+  assert approx_equal(xrsp.occ,    xrsp_init.occ,tolerance)
+  assert approx_equal(xrsp.u_iso,  xrsp_init.u_iso,tolerance)
+  assert approx_equal(xrsp.u_cart, xrsp_init.u_cart,tolerance)
   if(selection_str is None):
     diff = xrsp.sites_cart - xrsp_init.sites_cart
     assert math.sqrt(flex.mean(diff.dot())) > 1.0
   else:
     diff = xrsp.sites_cart - xrsp_init.sites_cart
     assert math.sqrt(flex.mean(diff.select(selection).dot())) > 1.0
-    assert approx_equal(math.sqrt(flex.mean(diff.select(~selection).dot())),0.)
+    assert approx_equal(
+              math.sqrt(flex.mean(diff.select(~selection).dot())),0.,tolerance)
   easy_run.call("rm -rf %s "%output)
 
-def check_occ_randomize(cmd, xrsp_init, output, selection,selection_str):
+def check_occ_randomize(cmd, xrsp_init, output, selection,selection_str,
+                                                              tolerance=1.e-3):
   easy_run.call(cmd)
   xrsp = xray_structure_plus(file_name = output)
-  assert approx_equal(xrsp.sites_cart,xrsp_init.sites_cart)
-  assert approx_equal(xrsp.u_iso,     xrsp_init.u_iso)
-  assert approx_equal(xrsp.u_cart,    xrsp_init.u_cart)
+  assert approx_equal(xrsp.sites_cart,xrsp_init.sites_cart,tolerance)
+  assert approx_equal(xrsp.u_iso,     xrsp_init.u_iso,tolerance)
+  assert approx_equal(xrsp.u_cart,    xrsp_init.u_cart,tolerance)
   if(selection_str is None):
     diff = flex.abs(xrsp.occ - xrsp_init.occ)
     assert flex.mean(diff) > 0.3
   else:
     diff = flex.abs(xrsp.occ - xrsp_init.occ)
     assert flex.mean(diff) > 0.3
-    assert approx_equal(flex.mean(diff.select(~selection)),0.)
+    assert approx_equal(flex.mean(diff.select(~selection)),0.,tolerance)
   easy_run.call("rm -rf %s "%output)
 
 def check_remove_selection(cmd, xrsp_init, output, selection, selection_str,
-                                                          remove_selection_str):
+                                         remove_selection_str,tolerance=1.e-3):
   easy_run.call(cmd)
   xrsp = xray_structure_plus(file_name = output)
   remove_selection = ~xrsp_init.selection(
                                       selection_strings = remove_selection_str)
   assert remove_selection.size() > remove_selection.count(True)
   assert approx_equal(xrsp.sites_cart,
-                      xrsp_init.sites_cart.select(remove_selection))
-  assert approx_equal(xrsp.occ,      xrsp_init.occ.select(remove_selection))
-  assert approx_equal(xrsp.u_iso,    xrsp_init.u_iso.select(remove_selection))
-  assert approx_equal(xrsp.u_cart,   xrsp_init.u_cart.select(remove_selection))
+                      xrsp_init.sites_cart.select(remove_selection),tolerance)
+  assert approx_equal(
+                    xrsp.occ, xrsp_init.occ.select(remove_selection),tolerance)
+  assert approx_equal(
+                xrsp.u_iso, xrsp_init.u_iso.select(remove_selection),tolerance)
+  assert approx_equal(
+              xrsp.u_cart, xrsp_init.u_cart.select(remove_selection),tolerance)
   sct1 = xrsp_init.xray_structure.scatterers().extract_scattering_types()
   assert sct1.count("C") > 0
   sct2 = xrsp.xray_structure.scatterers().extract_scattering_types()
@@ -224,15 +237,15 @@ def check_remove_selection(cmd, xrsp_init, output, selection, selection_str,
   assert sct1.size() > sct2.size()
   easy_run.call("rm -rf %s "%output)
 
-def check_all_none(cmd, xrsp_init, output):
+def check_all_none(cmd, xrsp_init, output, tolerance=1.e-3):
   easy_run.call(cmd)
   xrsp = xray_structure_plus(file_name = output)
-  assert approx_equal(xrsp.occ,         xrsp_init.occ)
-  assert approx_equal(xrsp.sites_cart,  xrsp_init.sites_cart)
-  assert approx_equal(xrsp.use_u_iso,   xrsp_init.use_u_iso)
-  assert approx_equal(xrsp.use_u_aniso, xrsp_init.use_u_aniso)
-  assert approx_equal(xrsp.u_iso,       xrsp_init.u_iso)
-  assert approx_equal(xrsp.u_cart,      xrsp_init.u_cart)
+  assert approx_equal(xrsp.occ,         xrsp_init.occ,tolerance)
+  assert approx_equal(xrsp.sites_cart,  xrsp_init.sites_cart,tolerance)
+  assert approx_equal(xrsp.use_u_iso,   xrsp_init.use_u_iso,tolerance)
+  assert approx_equal(xrsp.use_u_aniso, xrsp_init.use_u_aniso,tolerance)
+  assert approx_equal(xrsp.u_iso,       xrsp_init.u_iso,tolerance)
+  assert approx_equal(xrsp.u_cart,      xrsp_init.u_cart,tolerance)
   easy_run.call("rm -rf %s "%output)
 
 if (__name__ == "__main__"):
