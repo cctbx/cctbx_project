2d645de1fcc15ea621ea52b1f74ba11b5a639771
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-07-13 00:42:45 +0000

    - more flexible script, by using the cmd.extend from PyMol

--- cctbx/cctbx/examples/view_fft_map.py
+++ cctbx/cctbx/examples/view_fft_map.py
@@ -24,7 +24,7 @@ from chempy.map import Map
 from pymol import cmd
 from pymol import cgo
 
-def show_map(unit_cell, map_covering_unit_cell, label):
+def show_map(unit_cell, map_covering_unit_cell, label, level):
   map_grid = map_covering_unit_cell.focus()
   print "map_grid:", map_grid
   ucell_params = unit_cell.parameters()
@@ -41,7 +41,7 @@ def show_map(unit_cell, map_covering_unit_cell, label):
                     list(map_grid), first, last)
   cmd.load_map(map, label+"_cell")
   print "map loaded into PyMol"
-  cmd.isomesh(label+"_con", label+"_cell", 3.0) # create mesh
+  cmd.isomesh(label+"_con", label+"_cell", level) # create mesh
   cmd.color('gray', label+"_cell") # color wire frame
   cmd.set('auto_zoom', '0')    # disable zooming
   cmd.set('ortho', '1')        # orthoscopic projects
@@ -60,14 +60,14 @@ def show_peaks(unit_cell, clusters, radius=2.0):
               + [radius*height/height0])
   cmd.load_cgo(go, "peaks")
 
-def show_fft():
+def show_fft(file="map_coeff.pickle", map_level=3.0):
   cmd.delete("all")
-  map_coeff = easy_pickle.load("map_coeff.pickle")
+  map_coeff = easy_pickle.load(file)
   map_coeff.show_summary()
   fft_map = map_coeff.fft_map(
     symmetry_flags=maptbx.use_space_group_symmetry)
   print "map gridding:", fft_map.n_real()
-  show_map(fft_map.unit_cell(), fft_map.real_map(), "fft_map")
+  show_map(fft_map.unit_cell(), fft_map.real_map(), "fft_map", map_level)
   clusters = fft_map.tags().peak_search(
     parameters=maptbx.peak_search_parameters(
       min_distance_sym_equiv=3.0,
@@ -78,4 +78,4 @@ def show_fft():
   print
 
 if (__name__ == "pymol"):
-  show_fft()
+  cmd.extend("show_fft", show_fft)
