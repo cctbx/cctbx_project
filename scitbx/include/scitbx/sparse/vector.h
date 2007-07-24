3c942391802d0aa42e7cc7b43e9076a56eb4a4fc
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-07-24 02:10:16 +0000

    - Sparse vector, matrices and LU decomposition of the latter, plus very basic example / test in C++ - to do: Python interface and more thorough tests - foreseen applications: elimination of variables given a general set of constraints

---
+++ scitbx/include/scitbx/sparse/vector.h
@@ -0,0 +1,292 @@
+#ifndef SCITBX_SPARSE_VECTOR_H
+#define SCITBX_SPARSE_VECTOR_H
+
+#include <memory>
+#include <algorithm>
+#include <limits>
+#include <boost/operators.hpp>
+#include <scitbx/error.h>
+#include <scitbx/array_family/shared.h>
+
+namespace scitbx { namespace sparse {
+
+
+/** A sparse vector represented as a sequence of records,
+each containing the value and the index of a non-zero element.
+This sequence is not kept sorted. Most operation do however require that
+there are no duplicate index but since this is too expensive to enforce in
+general, we leave it to user code either to  enforce it by construction, or
+to call the member function "sort_indices" when appropriate. Any method
+requiring the abscence of duplicate will be annotated with:
+precondition: no duplicate.
+*/
+template<typename T>
+class vector
+{
+  public:
+    typedef T value_type;
+    typedef std::size_t index_type;
+    typedef std::ptrdiff_t index_difference_type;
+
+  private:
+    struct element {
+      index_type index;
+      value_type value;
+      element(index_type i, value_type x) : index(i), value(x) {}
+
+    };
+    struct indexes_less_than {
+      bool operator()(const element& e, const element& f) {
+        return e.index < f.index;
+      }
+    };
+    struct indexes_equal {
+      bool operator()(const element& e, const element& f) {
+        return e.index == f.index;
+      }
+    };
+    struct index_equal {
+      index_type i;
+      index_equal(index_type j) : i(j)
+      {}
+      bool operator()(const element& e) {
+        return e.index == i;
+      }
+    };
+
+    typedef af::shared<element> container_type;
+
+    container_type elements;
+    index_type _size;
+
+  public:
+    /// Const iterator over the records
+    class const_iterator
+      : public boost::forward_iterator_helper<const_iterator, value_type const>
+    {
+        typename container_type::const_iterator p;
+
+      public:
+        const_iterator()
+        {}
+
+        const_iterator(typename container_type::const_iterator q)
+          : p(q)
+        {}
+
+        value_type operator*() const {
+          return p->value;
+        }
+
+        bool operator==(const_iterator const& i) const {
+          return p == i.p;
+        }
+
+        const_iterator& operator++() {
+          p++;
+          return *this;
+        }
+
+        index_type index() const {
+          return p->index;
+        }
+    };
+
+    /// Iterator over the records
+    class iterator
+      : public boost::forward_iterator_helper<iterator, value_type>
+    {
+      typename container_type::iterator p;
+
+      public:
+        iterator()
+        {}
+
+        iterator(typename container_type::iterator q)
+          : p(q)
+        {}
+
+        value_type& operator*() {
+          return p->value;
+        }
+
+        bool operator==(iterator const& i) const {
+          return p == i.p;
+        }
+
+        iterator& operator++() {
+          p++;
+          return *this;
+        }
+
+        index_type index() const {
+          return p->index;
+        }
+    };
+
+    /// A reference to an element of given index
+    /** This the type of object returned by v[i] for a vector v and an index i
+    */
+    class element_reference
+    {
+      protected:
+        vector& v;
+        index_type i;
+
+      public:
+        /// Construct the reference to the element of index j in u
+        element_reference(vector& u, index_type j) : v(u), i(j)
+        {}
+
+        /* Without the destructor, the constructor would not be inlined
+        by g++ 4.0 */
+        ~element_reference()
+        {}
+
+        /// Triggered by using the value of v[i]
+        operator value_type() {
+          typename container_type::iterator p = std::find_if(
+            v.elements.rbegin(), v.elements.rend(), index_equal(i));
+          return *p;
+        }
+
+        /// Triggered by an assignment v[i] = ...
+        value_type operator=(value_type x) {
+          if (x != 0) v.elements.push_back(element(i,x));
+          return x;
+        }
+    };
+
+    /// Construct a vector of size 0
+    vector() {}
+
+    /// Construct a zero vector of size n
+    vector(index_type n) : _size(n) {}
+
+    /// An iterator pointing to the first record
+    const_iterator begin() const {
+      return const_iterator(elements.begin());
+    }
+
+    /// An iterator pointing past the last record
+    const_iterator end() const {
+      return const_iterator(elements.end());
+    }
+
+    /// An iterator pointing to the first record
+    iterator begin() {
+      return iterator(elements.begin());
+    }
+
+    /// An iterator pointing past the last record
+    iterator end() {
+      return iterator(elements.end());
+    }
+
+    /// Dimension of the vector, i.e. number of zero or non-zero elements
+    index_type size() const {
+      return _size;
+    }
+
+    /// Subscripting
+    /** Assignment v[i] = ... may introduce a duplicate index, a problem
+    solved for the getter v[i] by returning the last record with the
+    desired index.
+    */
+    element_reference operator[](index_type i) {
+      return element_reference(*this, i);
+    }
+
+    /// Fill the given dense vector.
+    /** precondition: no duplicate */
+    void fill_dense_vector(af::shared<T>& w) const {
+      SCITBX_ASSERT(w.size() == size())
+                   ( w.size() )( size() );
+      for(const_iterator q =  begin(); q != end(); q++) {
+        w[q.index()] = *q;
+      }
+    }
+
+    /// Element-wise comparison of a and b with the given absolute tolerance
+    friend
+    bool approx_equal(vector const& a,
+                      vector const& b,
+                      value_type tol=std::numeric_limits<value_type>::epsilon()
+                      )
+    {
+      SCITBX_ASSERT(a.size() == b.size())
+                   ( a.size() )( b.size() );
+      a.sort_indices(); b.sort_indices();
+      const_iterator p,q;
+      for (p=a.begin(), q=b.begin(); p != a.end() && q != b.end(); p++, q++) {
+        if (std::abs(*p - *q) > tol) return false;
+      }
+      return p == a.end() && q == b.end();
+    }
+
+    /// The dense vector corresponding to this
+    /** precondition: no duplicate */
+    af::shared<T> as_dense_vector() const {
+      af::shared<T> result;
+      fill_dense_vector(result);
+      return result;
+    }
+
+    /// A copy of this, copying the elements.
+    /** Since the copy constructor makes a shallow copy, thanks to
+    the shallow copy semantic of af::shared, this is ver necessary a
+    member function. */
+    vector deep_copy() const {
+      vector result(size());
+      result.elements = elements.deep_copy();
+      return result;
+    }
+
+    /// Fill the given dense vector with a permutation of this
+    /** precondition: no duplicate */
+    template<class PermutationType>
+    void fill_dense_vector_with_permutation(af::shared<T>& w,
+                                            PermutationType const& perm) const
+    {
+      SCITBX_ASSERT(w.size() == perm.size() && perm.size() == size())
+                   ( w.size() )( perm.size() )( size() );
+      for(const_iterator q =  begin(); q != end(); q++) {
+        w[ perm[q.index()] ] = *q;
+      }
+    }
+
+    /// Remove duplicate indices and sort records by increasing indices.
+    /** The last record is kept in case of duplicates. */
+    vector const& sort_indices() const {
+      container_type new_elements;
+      std::back_insert_iterator<container_type> inserter(new_elements);
+      std::unique_copy(elements.rbegin(), elements.rend(),
+                       inserter, indexes_equal());
+      std::sort(new_elements.begin(), new_elements.end(), indexes_less_than());
+      // so as to honour the const qualifier...
+      const_cast<vector*>(this)->elements = new_elements;
+      return *this;
+    }
+
+    /// Permute the elements of this, in place
+    /** Return this object, for convenient chaining of operations */
+    template<class PermutationType>
+    vector& permute(PermutationType const& permutation) {
+      SCITBX_ASSERT(size() == permutation.size())
+                   ( size() )( permutation.size() );
+      for (typename container_type::iterator p=elements.begin();
+           p != elements.end(); p++)
+      {
+        p->index = permutation[p->index];
+      }
+      return *this;
+    }
+};
+
+
+
+}}
+
+
+
+#endif
---
+++ scitbx/sparse/examples/tst_sparse.h
@@ -0,0 +1,94 @@
+#include <scitbx/sparse/vector.h>
+#include <scitbx/sparse/matrix.h>
+#include <scitbx/sparse/io.h>
+#include <scitbx/sparse/lu_factorization.h>
+#include <iostream>
+
+namespace scitbx { namespace sparse {
+
+void exercise_vector() {
+  using namespace scitbx;
+  typedef sparse::vector<double> sparse_vec;
+  sparse_vec u(3);
+    u[0] = 6.;
+    u[2] = 8.;
+  std::cout << sparse::dense_display(u);
+  std::cout << std::endl;
+  sparse_vec v(8);
+    v[1] = 1.;
+    v[4] = 2.;
+    v[6] =-1.;
+  std::cout << sparse::dense_display(v);
+  std::cout << std::endl;
+  std::cout << sparse::compressed_display(v);
+  std::cout << std::endl;
+  for (sparse_vec::iterator i=v.begin(); i != v.end(); i++) {
+    std::cout << i.index() << ", ";
+  }
+  std::cout << std::endl;
+  for (sparse_vec::iterator i=v.begin(); i != v.end() && i.index() < 5; i++) {
+    std::cout << i.index() << ", ";
+  }
+}
+
+void exercise_matrix() {
+  using namespace scitbx;
+  sparse::matrix<double> a(3,3);
+  a(0,0) = 1.;
+  a(0,2) = 2.;
+  a(1,0) = 3.;
+  a(2,1) = 4.;
+  std::cout << "a = " << dense_display(a);
+  sparse::vector<double> v1(3);
+  v1[0] = -1.;
+  std::cout << "\nv1 = " << dense_display(v1);
+  std::cout << "\na v1 = " << dense_display(a*v1);
+  sparse::vector<double> v2(3);
+  v2[1] = 2.;
+  v2[2] = 3.;
+  std::cout << "\nv2 = " << dense_display(v2);
+  std::cout << "\na v2 = " << dense_display(a*v2);
+}
+
+void exercise_lu() {
+  /* mathematica
+    a := SparseArray[ { {3, j_}  -> 1.5 - j/5,
+                        {7, j_}  -> -0.8 + j/5,
+                        {_, 5}   -> 2.1,
+                        {i_, i_} -> i },
+                      {8, 8} ]
+  */
+  using namespace scitbx;
+  std::cout << std::endl << std::endl;
+  sparse::matrix<double> a(8,8);
+  for (int j=1; j <= a.n_cols(); j++) {
+    sparse::vector<double> &c = a.col(j-1);
+    for(int i=1; i <= a.n_rows(); i++) {
+      switch(i) {
+        case 3:
+          c[i-1] = 1.5 - j/5.;
+          break;
+        case 7:
+          c[i-1] = -0.8 + j/5.;
+          break;
+        default:
+          if      (j==5) c[i-1] = 2.1;
+          else if (i==j) c[i-1] = i;
+      }
+    }
+  }
+  sparse::gilbert_peierls_lu_factorization< sparse::matrix<double> > lu(a);
+  std::cout << std::setw(6) << lu;
+  SCITBX_ASSERT(sparse::approx_equal(
+                lu.l() * lu.u(),
+                lu.factored().deep_copy().permute_rows(lu.rows_permutation()),
+                1e-6));
+}
+
+void exercise() {
+  exercise_vector();
+  exercise_matrix();
+  exercise_lu();
+}
+
+}} // namespace scitbx::sparse
