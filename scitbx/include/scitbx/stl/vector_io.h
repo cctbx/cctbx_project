cbcf2f7d273517e4dc1cb824304e1f01d81a193e
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-07-24 01:57:18 +0000

    - output of std::vector

---
+++ scitbx/include/scitbx/stl/vector_io.h
@@ -0,0 +1,16 @@
+#ifndef SCITBX_STD_VECTOR_IO_H
+#define SCITBX_STD_VECTOR_IO_H
+
+#include <ostream>
+#include <vector>
+
+namespace scitbx {
+
+template <typename ElementType>
+std::ostream& operator << (std::ostream& o, std::vector<ElementType> const& a) {
+  return o << af::const_ref<ElementType>(&a[0], a.size());
+}
+  
+}
+
+#endif
