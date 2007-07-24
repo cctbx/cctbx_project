aa840701d00a9defc99445a2b46a5e4c22acaa0a
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-07-24 01:54:44 +0000

    - add reverse iterators to make scitbx arrays better STL citizens

--- scitbx/include/scitbx/array_family/detail/ref_helpers.h
+++ scitbx/include/scitbx/array_family/detail/ref_helpers.h
@@ -1,20 +1,78 @@
 // Included from ref.h
 // DO NOT INCLUDE THIS FILE DIRECTLY.
 
+#include <boost/operators.hpp>
+
+namespace scitbx { namespace af { namespace detail {
+
+  template<class T>
+  class ref_reverse_iterator
+    : public boost::forward_iterator_helper<ref_reverse_iterator<T>, T>
+  {
+    T *p;
+    public:
+      ref_reverse_iterator(T* beg) : p(beg-1)
+      {}
+
+      T& operator*() {
+        return *p;
+      }
+
+      bool operator==(ref_reverse_iterator const& i) const {
+        return p == i.p;
+      }
+
+      ref_reverse_iterator& operator++() {
+        p--;
+        return *this;
+      }
+  };
+
+  template<class T>
+  class ref_const_reverse_iterator
+    : public boost::forward_iterator_helper<ref_const_reverse_iterator<T>, T>
+  {
+    T const *p;
+    public:
+      ref_const_reverse_iterator(T const* beg) : p(beg-1)
+      {}
+
+      T operator*() {
+        return *p;
+      }
+
+      bool operator==(ref_const_reverse_iterator const& i) const {
+        return p == i.p;
+      }
+
+      ref_const_reverse_iterator& operator++() {
+        p--;
+        return *this;
+      }
+  };
+
+}}}
+
 #define SCITBX_ARRAY_FAMILY_TYPEDEFS \
-typedef ElementType        value_type; \
-typedef ElementType*       iterator; \
-typedef const ElementType* const_iterator; \
-typedef ElementType&       reference; \
-typedef ElementType const& const_reference; \
-typedef std::size_t        size_type; \
-typedef std::ptrdiff_t     difference_type;
+typedef ElementType                                     value_type; \
+typedef ElementType*                                    iterator; \
+typedef const ElementType*                              const_iterator; \
+typedef detail::ref_reverse_iterator<ElementType>       reverse_iterator; \
+typedef detail::ref_const_reverse_iterator<ElementType> const_reverse_iterator; \
+typedef ElementType&                                    reference; \
+typedef ElementType const&                              const_reference; \
+typedef std::size_t                                     size_type; \
+typedef std::ptrdiff_t                                  difference_type;
 
 #define SCITBX_ARRAY_FAMILY_BEGIN_END_ETC(this_type, beg, sz) \
 ElementType* begin() { return beg; } \
 const ElementType* begin() const { return beg; } \
 ElementType* end() { return beg+sz; } \
 const ElementType* end() const { return beg+sz; } \
+reverse_iterator rbegin() { return reverse_iterator(beg+sz); } \
+const_reverse_iterator rbegin() const { return const_reverse_iterator(beg+sz); } \
+reverse_iterator rend() { return reverse_iterator(beg); } \
+const_reverse_iterator rend() const { return const_reverse_iterator(beg); } \
 ElementType& front() { return beg[0]; } \
 ElementType const& front() const { return beg[0]; } \
 ElementType& back() { return beg[sz-1]; } \
