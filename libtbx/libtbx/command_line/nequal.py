000841a83b08374473ca3069435bbe8c8e4cbc9f
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2007-08-01 06:00:11 +0000

    new utility: like uniq but with counts

---
+++ libtbx/libtbx/command_line/nequal.py
@@ -0,0 +1,44 @@
+from libtbx.utils import Usage
+import fileinput
+import sys, os
+
+def run(command_name=os.environ.get(
+          "LIBTBX_DISPATCHER_NAME", "libtbx.nequal")):
+  if (sys.argv[1:] in [["-h"], ["--help"]]):
+    raise Usage("%s [file ...]" % command_name + """
+  Similar to the Unix uniq command, but each output line is
+  prefixed with the number of identical consecutive lines.
+  Example command:
+    grep Warning log_file | sort | %s
+  Example output:
+    12: Warning: missing file'
+     9: Warning: missing directory'
+     1: Warning: unknown file""" % command_name)
+  buffer = []
+  prev = None
+  n = 0
+  for line in fileinput.input():
+    if (prev is None):
+      prev = line
+      n = 1
+    elif (line != prev):
+      buffer.append((n, prev))
+      prev = line
+      n = 1
+    else:
+      n += 1
+  if (n != 0):
+    buffer.append((n, prev))
+  if (len(buffer) != 0):
+    def cmp_buffer_entries(a, b):
+      result = cmp(b[0], a[0])
+      if (result == 0):
+        result = cmp(a[1], b[1])
+      return result
+    buffer.sort(cmp_buffer_entries)
+    n_fmt = "%%%dd: " % len("%d" % buffer[0][0])
+    for n,line in buffer:
+      sys.stdout.write(n_fmt % n + line)
+
+if (__name__ == "__main__"):
+  run()
