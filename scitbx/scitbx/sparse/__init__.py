af0b4a8af540a3bc21af5ea1976bbaee4ba11560
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-08-03 13:43:59 +0000

    - complete overhaul of directories layout - C++ examples gone - Boost.Python interface - Python test cases

---
+++ scitbx/scitbx/sparse/__init__.py
@@ -0,0 +1,15 @@
+import boost.python
+from scitbx_sparse_ext import *
+from scitbx.array_family import flex
+
+class _matrix(boost.python.injector, matrix):
+
+  def cols(self):
+    for j in xrange(self.n_cols): yield self.col(j)
+
+  def as_dense_matrix(self):
+    result = flex.double(flex.grid(self.n_rows, self.n_cols))
+    for j,c in enumerate(self.cols()):
+      for i,val in c:
+        result[i,j] = val
+    return result
