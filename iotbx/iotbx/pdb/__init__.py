1d5f4e8c5d446dbcc8948a682b77879b99263009
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2007-08-12 06:03:06 +0000

    body of rna_dna_atom_names_interpretation moved from Python to C++ (reduces the runtime for processing about 9100 residues from more than 1.6 seconds to less than 0.2 seconds)

--- iotbx/iotbx/pdb/__init__.py
+++ iotbx/iotbx/pdb/__init__.py
@@ -109,69 +109,15 @@ rna_dna_atom_names_reference_to_mon_lib_translation_dict = {
 class rna_dna_atom_names_interpretation(object):
 
   def __init__(self, residue_name, atom_names):
-    assert residue_name in ["?A", "?C", "?G",
-                            "A", "C", "G", "U",
-                            "DA", "DC", "DG", "DT", "T"]
-    if (residue_name == "T"): residue_name = "DT"
-    infos = []
-    have_o2prime = False
-    have_ho2prime = False
-    i_atom_h2primeprime = []
-    have_phosphate = False
-    have_op3_or_hop3 = False
-    have_ho3prime = False
-    i_atom_ho5prime = []
-    for i_atom,atom_name in enumerate(atom_names):
-      info = rna_dna_atom_names_info(atom_name=atom_name)
-      infos.append(info)
-      if (info.is_o2prime()):
-        have_o2prime = True
-      elif (info.is_ho2prime()):
-        have_ho2prime = True
-      elif (info.is_h2primeprime()):
-        i_atom_h2primeprime.append(i_atom)
-      if (info.is_in_phosphate_group()):
-        have_phosphate = True
-        if (info.is_op3_or_hop3()):
-          have_op3_or_hop3 = True
-      if (info.is_ho5prime()):
-        i_atom_ho5prime.append(i_atom)
-      if (info.is_ho3prime()):
-        have_ho3prime = True
-    if (have_phosphate):
-      for i_atom in i_atom_ho5prime:
-        assert infos[i_atom].change_ho5prime_to_hop3()
-        have_op3_or_hop3 = True
-    if (residue_name[0] == "?"):
-      if (have_o2prime):
-        residue_name = residue_name[1]
-        if (not have_ho2prime):
-          for i_atom in i_atom_h2primeprime:
-            assert infos[i_atom].change_h2primeprime_to_ho2prime()
-          have_ho2prime = True
-      elif (len(i_atom_h2primeprime) != 0):
-        residue_name = "D" + residue_name[1]
-      elif (have_ho2prime):
-        residue_name = residue_name[1]
-      else:
-        residue_name = "D" + residue_name[1]
-    n_expected = 0
-    n_unexpected = 0
-    for info in infos:
-      if (   info.reference_name is None
-          or not info.is_compatible_with(residue_name=residue_name)):
-        info.change_to_unknown()
-        n_unexpected += 1
-      else:
-        n_expected += 1
+    if (residue_name == "T"):
+      residue_name = "DT"
+    else:
+      assert residue_name in ["?A", "?C", "?G",
+                              "A", "C", "G", "U",
+                              "DA", "DC", "DG", "DT", "T"]
     self.residue_name = residue_name
     self.atom_names = atom_names
-    self.infos = infos
-    self.have_phosphate = have_phosphate
-    self.have_op3_or_hop3 = have_op3_or_hop3
-    self.have_ho3prime = have_ho3prime
-    self.n_expected = n_expected
-    self.n_unexpected = n_unexpected
+    rna_dna_atom_names_interpretation_core(self)
 
   def unexpected_atom_names(self):
     result = []
