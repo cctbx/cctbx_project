c3bf67869c803211be2923f844235a8e1b009091
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2007-08-18 19:36:28 +0000

    show TAG, cctbx_bundle_TAG, or libtbx.env.command_version_suffix

---
+++ cctbx/cctbx/command_line/version.py
@@ -0,0 +1,18 @@
+import libtbx.load_env
+import os
+
+def run():
+  version = None
+  for tag_file in ["TAG", "cctbx_bundle_TAG"]:
+    tag_path = libtbx.env.under_dist("libtbx", os.path.join("..", tag_file))
+    if (os.path.isfile(tag_path)):
+      try: version = open(tag_path).read().strip()
+      except KeyboardInterrupt: raise
+      except: pass
+      else: break
+  if (version is None):
+    version = libtbx.env.command_version_suffix
+  print version
+
+if (__name__ == "__main__"):
+  run()
