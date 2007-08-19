68815348d200b0b2fadff7111f3113f77f180ee2
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2007-08-19 01:47:46 +0000

    update md5 hexdigest for mersenne_twister.hpp which has changed only because of the move from CVS to SVN

--- scitbx/libtbx_refresh.py
+++ scitbx/libtbx_refresh.py
@@ -12,8 +12,9 @@ if (self.env.is_ready_for_build()):
     path=self.env.under_dist(
       module_name="boost", path="boost/random/mersenne_twister.hpp"),
     expected_md5_hexdigests=[
-      "a2533c79a21f0f773f2e0d29a37371b0", # revision 1.21
-      "378432b5b280c9c0c894f7c80f0dad92", # revision 1.20
+      "a2533c79a21f0f773f2e0d29a37371b0", # CVS revision 1.21
+      "378432b5b280c9c0c894f7c80f0dad92", # CVS revision 1.20
+      "1fe430a94f330e36dca14ef2a553dba1", # SVN revision 37030 (== CVS 1.20)
     ],
     hints=[
       "  Files to review:",
