b79d5e72579d5807686b1a67791ea10f103c23c4
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-09-06 17:33:09 +0000

    reorganise the tests in a more OO way so as to cleanly reintroduce the test on degenerate edges as suggested by Ralf

--- scitbx/scitbx/iso_surface/tst_iso_surface.py
+++ scitbx/scitbx/iso_surface/tst_iso_surface.py
@@ -7,111 +7,154 @@ from math import sin, cos
 from scitbx import matrix
 import sys
 
-def exercise(f, err, nx, ny, nz, iso_level, verbose):
-  # construct a map of f
-  dx, dy, dz = 1/nx, 1/ny, 1/nz
-  map = flex.double(flex.grid((nx, ny, nz)))
-  loop = flex.nested_loop(end=(nx,ny,nz))
-  while not loop.over():
-    p = loop()
-    map[p] = f(p[0]/nx, p[1]/ny, p[2]/nz)
-    loop.incr()
 
-  # triangulation of the iso-surface of the map
-  s = iso_surface.triangulation(map, iso_level, (dx, dy, dz))
+class triangulation_test_case(object):
 
-  # the value of f on the vertices v shall not differ from iso_level by more
-  # than ||1/2 f''(v).h|| where h=(dx,dy,dz)
-  bad_vertices = []
-  for i,v in enumerate(s.vertices):
-    val = f(v[0], v[1], v[2])
-    tol = err(v[0], v[1], v[2], dx, dy, dz)
-    if not approx_equal(val, iso_level, tol):
-      bad_vertices.append((v,val))
-  assert not bad_vertices, bad_vertices_err(bad_vertices)
+  def __init__(self, func, grid_size):
+    """ Construct a map of func with the given grid_size """
+    self.func = func
+    self.grid_size = grid_size
 
-  # consistency check on the triangulation
-  # Note: a comprehensive check of the graph being triangulated is
-  # not attempted
-  degenerates = []
-  for v1, v2, v3 in s.triangles:
-    assert v1 != v2 and v2 != v3 and v3 != v1
-    for a,b in ((v1,v2), (v2,v3), (v3,v1)):
-      if s.vertices[a] == s.vertices[b]: degenerates.append((a,b))
-  if verbose:
-    if degenerates:
-      print "Degenerate edges for the isosurface of %s:" % f
-      print degenerates
+    nx, ny, nz = grid_size
+    self.grid_cell = 1/nx, 1/ny, 1/nz
 
-  vertices = {}
-  edges = {}
-  for v1,v2,v3 in s.triangles:
-    vertices[v1] = vertices[v2] = vertices[v3] = 1
-    for a,b in ((v1,v2), (v2,v3), (v3,v1)):
-      if a < b: e = (a,b)
-      else: e = (b,a)
-      edges[e] = edges.setdefault(e,0) + 1
-  assert len(vertices) == len(s.vertices)
-  for e,p in edges.iteritems():
-    assert p in (1,2)
-    if not is_near_boundary(s.vertices[e[0]], s.vertices[e[1]], (nx,ny,nz)):
-      assert p == 2
+    map = flex.double(flex.grid(grid_size))
+    loop = flex.nested_loop(end=grid_size)
+    while not loop.over():
+      p = loop()
+      map[p] = func((p[0]/nx, p[1]/ny, p[2]/nz))
+      loop.incr()
+    self.map = map
 
-  # consistency check on the normals
-  assert len(s.normals) == len(s.vertices)
-  for v,n in zip(s.vertices, s.normals):
-    v, n = matrix.col(v), matrix.col(n)
-    assert approx_equal(abs(n), 1, 1e-12)
-    outward = v + 0.01*n
-    inward  = v - 0.01*n
-    assert f(*outward) > iso_level > f(*inward)
+  def run(self, iso_level, verbose):
+    """ Test triangulation of the iso-surface at the given iso-level """
+    f = self.func
 
-def is_near_boundary(v1,v2,n):
-  eps = 1e-15
-  for c1, c2, m in zip(v1, v2, n):
-    if c1 != c2: continue
-    if (   abs(c1) < eps       or abs(c1 - 1) < eps
-        or abs(c1 - 1/m) < eps or abs(c1 - (1 - 1/m)) < eps ): return True
-  return False
+    # triangulation of the iso-surface of the map
+    s = iso_surface.triangulation(self.map, iso_level, self.grid_cell)
 
-def bad_vertices_err(seq):
-  msg = ["Vertices with value not close enough to threshold %f:" % iso_level]
-  for vertex, val in bad_vertices:
-    msg.append( "\t(%f, %f, %f) -> %f" % (v+(val,)) )
-  return "\n".join(msg)
+    # the value of f on the vertices v shall not differ from iso_level by more
+    # than ||1/2 f''(v).h|| where h=(dx,dy,dz)
+    bad_vertices = []
+    for i,v in enumerate(s.vertices):
+      val = f(v)
+      tol = f.second_order_error(v, self.grid_cell)
+      if not approx_equal(val, iso_level, tol):
+        bad_vertices.append((v,val))
+    assert not bad_vertices, self.bad_vertices_err(bad_vertices, iso_level)
 
-def elliptic(x,y,z):
-  return x*x + 2*y*y + 3*z*z
+    # consistency check on the triangulation
+    # Note: a comprehensive check of the graph being triangulated is
+    # not attempted
+    degenerates = []
+    for v1, v2, v3 in s.triangles:
+      assert v1 != v2 and v2 != v3 and v3 != v1
+      for a,b in ((v1,v2), (v2,v3), (v3,v1)):
+        if s.vertices[a] == s.vertices[b]: degenerates.append((a,b))
+    if verbose:
+      if degenerates:
+        print "Degenerate edges for the isosurface of %s:" % f
+        print degenerates
+    self.degenerate_edges = degenerates
 
-def elliptic_error(x,y,z, hx,hy,hz):
-  return 0.5*abs(elliptic(hx,hy,hz))
+    vertices = {}
+    edges = {}
+    for v1,v2,v3 in s.triangles:
+      vertices[v1] = vertices[v2] = vertices[v3] = 1
+      for a,b in ((v1,v2), (v2,v3), (v3,v1)):
+        if a < b: e = (a,b)
+        else: e = (b,a)
+        edges[e] = edges.setdefault(e,0) + 1
+    assert len(vertices) == len(s.vertices)
+    for e,p in edges.iteritems():
+      assert p in (1,2)
+      if not self.is_near_boundary(s.vertices[e[0]], s.vertices[e[1]]):
+        assert p == 2
 
-def hyperbolic(x,y,z):
-  return x*x - y*y - z*z
+    # consistency check on the normals
+    assert len(s.normals) == len(s.vertices)
+    for v,n in zip(s.vertices, s.normals):
+      v, n = matrix.col(v), matrix.col(n)
+      assert approx_equal(abs(n), 1, 1e-12)
+      outward = v + 0.01*n
+      inward  = v - 0.01*n
+      assert f(outward) > iso_level > f(inward)
 
-def hyperbolic_error(x,y,z, hx,hy,hz):
-  return 0.5*abs(hyperbolic(hx,hy,hz))
+  def is_near_boundary(self, v1, v2):
+    delta = matrix.col(self.grid_cell)/2
+    for c1, c2, m, eps in zip(v1, v2, self.grid_size, delta):
+      if c1 != c2: continue
+      if (   abs(c1) < eps       or abs(c1 - 1) < eps
+          or abs(c1 - 1/m) < eps or abs(c1 - (1 - 1/m)) < eps ): return True
+    return False
 
-def sinusoidal(x,y,z):
-  return sin(x*y + y*z + z*x)
+  def bad_vertices_err(self, bad_vertices, iso_level):
+    msg = ["Vertices with value not close enough to threshold %f:" % iso_level]
+    for vertex, val in bad_vertices:
+      msg.append( "\t(%f, %f, %f) -> %f" % (v+(val,)) )
+    return "\n".join(msg)
 
-def sinusoidal_error(x,y,z, hx,hy,hz):
-  s = sin(x*y + y*z + z*x)
-  c = cos(x*y + y*z + z*x)
-  return 0.5*abs( ( -(y+z)**2 * hx*hx -(z+x)**2 * hy*hy -(x+y)**2 * hz*hz )*s
-                  + 2*(c - (z+x)*(y+z)*s) * hx*hy
-                  + 2*(c - (x+y)*(z+x)*s) * hy*hz
-                  + 2*(c - (x+y)*(y+z)*s) * hz*hx
-                )
+
+class elliptic(object):
+
+  def __call__(self, p):
+    x,y,z = p
+    return x*x + 2*y*y + 3*z*z
+
+  def second_order_error(self, p, h):
+    return 0.5*abs(self(h))
+
+class hyperbolic(object):
+
+  def __call__(self, p):
+    x,y,z = p
+    return x*x - y*y - z*z
+
+  def second_order_error(self, p, h):
+    return 0.5*abs(self(h))
+
+class sinusoidal(object):
+
+  def __call__(self, p):
+    x,y,z = p
+    return sin(x*y + y*z + z*x)
+
+  def second_order_error(self, p, h):
+    x,y,z = p
+    hx,hy,hz = h
+    s = sin(x*y + y*z + z*x)
+    c = cos(x*y + y*z + z*x)
+    return 0.5*abs( ( -(y+z)**2 * hx*hx -(z+x)**2 * hy*hy -(x+y)**2 * hz*hz )*s
+                    + 2*(c - (z+x)*(y+z)*s) * hx*hy
+                    + 2*(c - (x+y)*(z+x)*s) * hy*hz
+                    + 2*(c - (x+y)*(y+z)*s) * hz*hx
+                  )
 
 def run(args):
   verbose = "--verbose" in args
-  exercise(elliptic, elliptic_error, nx=50, ny=40, nz=30, iso_level=3,
-    verbose=verbose)
-  exercise(hyperbolic, hyperbolic_error, nx=50, ny=40, nz=30, iso_level=3,
-    verbose=verbose)
-  exercise(sinusoidal, sinusoidal_error, nx=50, ny=40, nz=30, iso_level=3,
-    verbose=verbose)
+  grid_size = (50, 40, 30)
+
+  test = triangulation_test_case(elliptic(), grid_size)
+  test.run(iso_level=3, verbose=verbose)
+  assert test.degenerate_edges == [(303, 418), (418, 407), (407, 303),
+                                   (303, 407), (418, 303), (407, 418),
+                                   (654, 830), (830, 822), (822, 654),
+                                   (654, 822), (830, 654), (715, 909),
+                                   (909, 890), (890, 715), (715, 890),
+                                   (909, 715), (822, 830), (890, 909),
+                                   (1898, 2185), (2185, 2177), (2177, 1898),
+                                   (1898, 2177), (2185, 1898), (1966, 2278),
+                                   (2278, 2261), (2261, 1966), (1966, 2261),
+                                   (2278, 1966), (2177, 2185), (2261, 2278)]
+
+  test = triangulation_test_case(hyperbolic(), grid_size)
+  test.run(iso_level=3, verbose=verbose)
+  assert test.degenerate_edges == []
+
+  test = triangulation_test_case(sinusoidal(), grid_size)
+  test.run(iso_level=3, verbose=verbose)
+  assert test.degenerate_edges == []
+
   print format_cpu_times()
 
 if __name__ == '__main__':
