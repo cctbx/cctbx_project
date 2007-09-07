8498f5cc23b132194aa8eb1aa4d6e95ff170af3f
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-09-07 20:57:43 +0000

    remove redundant import

--- gltbx/wx_viewer.py
+++ gltbx/wx_viewer.py
@@ -1,6 +1,5 @@
 from __future__ import division
 from __future__ import generators
-from libtbx import forward_compatibility
 
 # This code is based on:
 #   http://lists.wxwidgets.org/archive/wxPython-users/msg11078.html
