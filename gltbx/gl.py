0bbe8d50267194350e495c54cd77ae518e999d39
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-09-11 23:26:27 +0000

    - OpenGL 1.3 functions glLoadMatrix[fd] and glMultTransposeMatrix[fd] - OpenGL 1.2 GL_RESCALE_NORMAL

--- gltbx/gl.py
+++ gltbx/gl.py
@@ -1,3 +1,19 @@
 import boost.python
 ext = boost.python.import_ext("gltbx_gl_ext")
 from gltbx_gl_ext import *
+
+def __function_taking_transposed_matrix(f):
+  def wrapper(m):
+    mt = m[0:13:4] + m[1:14:4] + m[2:15:4] + m[3:16:4]
+    f(mt)
+  return wrapper
+
+glLoadTransposeMatrixf = __function_taking_transposed_matrix(glLoadMatrixf)
+glLoadTransposeMatrixd = __function_taking_transposed_matrix(glLoadMatrixd)
+glMultTransposeMatrixf = __function_taking_transposed_matrix(glMultMatrixf)
+glMultTransposeMatrixd = __function_taking_transposed_matrix(glMultMatrixd)
+
+if GL_VERSION >= GL_VERSION_1_2:
+  GL_RESCALE_NORMAL = 0x803A
+else:
+  GL_RESCALE_NORMAL = GL_NORMALIZE
