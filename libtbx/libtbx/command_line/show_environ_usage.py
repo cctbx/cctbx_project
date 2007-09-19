a3c01760aa60533bf6151c2470a3293ec27397da
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2007-09-19 04:21:30 +0000

    new command libtbx.show_environ_usage

---
+++ libtbx/libtbx/command_line/show_environ_usage.py
@@ -0,0 +1,22 @@
+import sys
+
+def run(args):
+  assert len(args) == 0
+  print """
+LIBTBX_DISABLE_TRACEBACKLIMIT
+  If set, Sorry and Usage exceptions are shown with the full traceback.
+
+LIBTBX_VALGRIND
+  Run "libtbx.valgrind python" for more information.
+
+LIBTBX_PRINT_TRACE
+  If set, print trace of all Python code executed.
+  This can lead to very large output.
+
+LIBTBX_NATIVE_TAR
+  Inspected by libtbx.bundle_as_selfx to find alternative tar command.
+  Example: setenv LIBTBX_NATIVE_TAR $HOME/bin/tar
+"""
+
+if (__name__ == "__main__"):
+  run(sys.argv[1:])
