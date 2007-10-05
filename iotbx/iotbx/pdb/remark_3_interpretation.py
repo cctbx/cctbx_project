6ff0be05105c7f730e2d2d0de071a385cd0d07e0
Pavel Afonine <pafonine@users.noreply.github.com>
2007-10-05 03:49:32 +0000

    review TLS extraction; add new extraction for R, sigmas, resolutions used in refinement

--- iotbx/iotbx/pdb/remark_3_interpretation.py
+++ iotbx/iotbx/pdb/remark_3_interpretation.py
@@ -28,7 +28,7 @@ REMARK   3
 REMARK   3   TLS GROUP :     2
 REMARK   3    NUMBER OF COMPONENTS GROUP :    1
 REMARK   3    COMPONENTS        C SSSEQI   TO  C SSSEQI
-REMARK   3    RESIDUE RANGE :   B     1        B   348
+REMARK   3    RESIDUE RANGE :   B XYZW1-       B XY348+
 REMARK   3    ORIGIN FOR THE GROUP (A):  16.4920  -1.1810  59.3130
 REMARK   3    T TENSOR
 REMARK   3      T11:   0.0412 T22:   0.0118
@@ -96,7 +96,7 @@ def extract_tls_parameters(remark_3_records, file_name = ""):
     T11,T22,T33,T12,T13,T23 = None,None,None,None,None,None
     L11,L22,L33,L12,L13,L23 = None,None,None,None,None,None
     S11,S12,S13,S21,S22,S23,S31,S32,S33= None,None,None,None,None,None,None,None,None
-    sel_string = None
+    sel_str = None
     for rec in one:
       if(rec.startswith("REMARK   3    NUMBER OF COMPONENTS GROUP :")):
          n_components = None
@@ -106,58 +106,8 @@ def extract_tls_parameters(remark_3_records, file_name = ""):
              file_name
            return []
       if(rec.startswith("REMARK   3    RESIDUE RANGE :")):
-         if(len(rec.split()) == 9):
-            rr = rec.split()
-            chain_first = rr[5]
-            residue_number_first = None
-            try: residue_number_first = int(rr[6])
-            except:
-              raise ValueError, "Cannot extract first residue number in residue range: %s\n  %s"%(
-                file_name, rec)
-              return []
-            chain_second = rr[7]
-            #if(chain_first != chain_second):
-            #   raise RuntimeError("chain_first != chain_second: %s %s" % (chain_first,chain_second))
-            residue_number_second = None
-            try: residue_number_second = int(rr[8])
-            except:
-              raise ValueError, "Cannot extract second residue number in residue range: %s\n  %s"%(
-                file_name, rec)
-              return []
-            if(chain_first.strip() == chain_second.strip() and
-               residue_number_first > residue_number_second):
-              raise ValueError, \
-                "residue_number_first > residue_number_second, %s\n%s"%(
-                file_name,rec)
-              return []
-            if([residue_number_first,residue_number_second].count(None) == 0):
-              r_range.append([chain_first,residue_number_first,
-                              chain_second,residue_number_second])
-         elif(len(rec.split()) == 7):
-            rr = rec.split()
-            chain_first = " "
-            residue_number_first = None
-            try: residue_number_first = int(rr[5])
-            except:
-              raise ValueError, "Cannot extract first residue number in residue range: %s\n  %s"%(
-                file_name, rec)
-              return []
-            chain_second = " "
-            residue_number_second = None
-            try: residue_number_second = int(rr[6])
-            except:
-              raise ValueError, "Cannot extract second residue number in residue range: %s\n  %s"%(
-                file_name, rec)
-              return []
-            if(chain_first.strip() == chain_second.strip() and
-               residue_number_first > residue_number_second):
-              raise ValueError, \
-                "residue_number_first > residue_number_second, %s\n%s"%(
-                file_name,rec)
-              return []
-            if([residue_number_first,residue_number_second].count(None) == 0):
-              r_range.append([chain_first,residue_number_first,
-                              chain_second,residue_number_second])
+        ch1,res1,ch2,res2 = rec[32:33], rec[34:40], rec[47:48], rec[49:55]
+        r_range.append([ch1,res1,ch2,res2])
       if(rec.startswith("REMARK   3    ORIGIN FOR THE GROUP (A):")):
          try: x = float(rec.split()[7])
          except ValueError:
@@ -294,249 +244,51 @@ def extract_tls_parameters(remark_3_records, file_name = ""):
              return []
          S=[S11, S12, S13, S21, S22, S23, S31, S32, S33]
     for rr in r_range:
-      if(rr[0] != " " and rr[0]==rr[2]):
-         if(sel_string is None):
-           sel_string = "(chain "+rr[0] + " and resid "+str(rr[1]) + ":"+str(rr[3])+")"
-         else:
-           sel_string += " or (chain "+rr[0] + " and resid "+str(rr[1]) + ":"+str(rr[3])+")"
+      ch1 = rr[0].strip()
+      ch2 = rr[2].strip()
+      res1 = rr[1].strip()
+      res2 = rr[3].strip()
+      if(rr[0]==rr[2]):
+        if(sel_str is None):
+          if(ch1 == ""):
+            sel_str = "(resid %s:%s)" % (res1,res2)
+          else:
+            sel_str = "(chain %s and resid %s:%s)" % (ch1,res1,res2)
+        else:
+          if(ch1 == ""):
+            sel_str += " or (resid %s:%s)" % (res1,res2)
+          else:
+            sel_str += " or (chain %s and resid %s:%s)" % (ch1,res1,res2)
       elif(rr[0] != rr[2]):
-         if(sel_string is None):
-           sel_string = "(chain "+rr[0] + " and resid "+str(rr[1]) + "-"+")" + \
-                        " or (chain "+rr[2] + " and resid -"+str(rr[3]) +")"
-         else:
-           sel_string = " or (chain "+rr[0] + " and resid "+str(rr[1]) + "-"+")" + \
-                        " or (chain "+rr[2] + " and resid -"+str(rr[3]) +")"
-      else:
-         if(sel_string is None):
-           sel_string = "(resid "+str(rr[1]) + ":"+str(rr[3])+")"
-         else:
-           sel_string += " or (resid "+str(rr[1]) + ":"+str(rr[3])+")"
-    if(sel_string is not None or
+        if(sel_str is None):
+          if(ch1 == ""):
+            sel_str = "(resid %s- ) or (resid -%s)" % (res1,res2)
+          else:
+            sel_str = "(chain %s and resid %s- ) or (chain %s and resid -%s)"%(
+              ch1,res1,ch2,res2)
+        else:
+          if(ch1 == ""):
+            sel_str += " or (resid %s- ) or (resid -%s)" % (res1,res2)
+          else:
+            sel_str += \
+              " or (chain %s and resid %s- ) or (chain %s and resid -%s)" %(
+              ch1,res1,ch2,res2)
+    if(sel_str is not None or
        T.count(None) > 0 or
        L.count(None) > 0 or
        S.count(None) > 0):
-      tls_params.append(tls(T=T,L=L,S=S,origin=origin,selection_string=sel_string))
+      tls_params.append(tls(T=T,L=L,S=S,origin=origin,selection_string=sel_str))
   return tls_params
 
-
-def XXXextract_tls_parameters(remark_3_records):
-# T = (T11, T22, T33, T12, T13, T23)
-# L = (L11, L22, L33, L12, L13, L23)
-# S = (S11, S12, S13, S21, S22, S23, S31, S32, S33)
-  T = []
-  L = []
-  S = []
-  origin = []
-  residue_range = []
-  number_of_tls_groups = 0
-  group_counter = 0
-  for record in remark_3_records:
-    assert record[0:10] == "REMARK   3"
-    srecord = record.split()
-    lsrecord = len(srecord)
-#=====> extract number of TLS groups
-    if(record.count("NUMBER OF TLS GROUPS")*record.count(":") == 1):
-      try: number_of_tls_groups = int(srecord[7])
-      except:
-        try: number_of_tls_groups = int(srecord[6])
-        except:
-          try: number_of_tls_groups = int(srecord[lsrecord-1])
-          except:
-            if(srecord[lsrecord-1][0] == ":"):
-              try: number_of_tls_groups = int(srecord[lsrecord-1][1:])
-              except ValueError: return None
-            elif(srecord[lsrecord-1].count("S:") == 1):
-              start_position = srecord[lsrecord-1].index(":")+1
-              try: number_of_tls_groups = int(srecord[lsrecord-1][start_position:])
-              except ValueError: return None
-            else:
-              return None
-#=====> extract currents TLS group number
-    if(record.count("TLS GROUP")*record.count(":") == 1 and
-       record.count("NUMBER OF TLS GROUPS") == 0):
-      try:
-        group_number = int(srecord[5])
-        group_counter += 1
-      except:
-        try:
-          group_number = int(srecord[4])
-          group_counter += 1
-        except:
-          try:
-            group_number = int(srecord[lsrecord-1])
-            group_counter += 1
-          except:
-            if(srecord[lsrecord-1][0] == ":"):
-              try:
-                group_number = int(srecord[lsrecord-1][1:])
-                group_counter += 1
-              except ValueError: return None
-            elif(srecord[lsrecord-1].count("P:") == 1):
-              start_position = srecord[lsrecord-1].index(":")+1
-              try:
-                group_number = int(srecord[lsrecord-1][start_position:])
-                group_counter += 1
-              except ValueError: return None
-            else:
-              return None
-#=====> extract current residue range
-    if(record.count("RESIDUE RANGE ")*record.count(":") == 1):
-      rec = list(srecord[5:9])
-      try:
-        rec[1]
-      except IndexError: return None
-      try:
-        int(rec[1])
-      except ValueError: return None
-      try:
-        rec[3]
-      except IndexError: return None
-      try:
-        int(rec[3])
-      except ValueError: return None
-      residue_range.append(list(srecord[5:9]))
-    elif(record.count("RESIDUE RANGE:") == 1):
-      rec = list(srecord[4:8])
-      try:
-        rec[1]
-      except IndexError: return None
-      try:
-        int(rec[1])
-      except ValueError: return None
-      try:
-        rec[3]
-      except IndexError: return None
-      try:
-        int(rec[3])
-      except ValueError: return None
-      residue_range.append(list(srecord[4:8]))
-#=====> extract current origin
-    if(record.count("ORIGIN FOR THE GROUP") == 1):
-      try: x = float(srecord[lsrecord-3])
-      except ValueError: return None
-      try: y = float(srecord[lsrecord-2])
-      except ValueError: return None
-      try: z = float(srecord[lsrecord-1])
-      except ValueError: return None
-      origin.append([x,y,z])
-#=====> extract current T
-    check = record.count("T11:")*record.count("T22:")
-    if(check == 1 and lsrecord == 6):
-      T11=None
-      T22=None
-      T33=None
-      T12=None
-      T13=None
-      T23=None
-      try: T11 = float(srecord[lsrecord-3])
-      except ValueError: return None
-      try: T22 = float(srecord[lsrecord-1])
-      except ValueError: return None
-    check = record.count("T33:")*record.count("T12:")
-    if(check == 1 and lsrecord == 6):
-      assert T11 is not None and T22 is not None
-      assert T33 is None and T12 is None and T13 is None and T23 is None
-      try: T33 = float(srecord[lsrecord-3])
-      except ValueError: return None
-      try: T12 = float(srecord[lsrecord-1])
-      except ValueError: return None
-    check = record.count("T13:")*record.count("T23:")
-    if(check == 1 and lsrecord == 6):
-      assert T11 is not None and T22 is not None
-      assert T33 is not None and T12 is not None
-      assert T13 is None and T23 is None
-      try: T13 = float(srecord[lsrecord-3])
-      except ValueError: return None
-      try: T23 = float(srecord[lsrecord-1])
-      except ValueError: return None
-      T.append([T11, T22, T33, T12, T13, T23])
-#=====> extract current L
-    check = record.count("L11:")*record.count("L22:")
-    if(check == 1 and lsrecord == 6):
-      L11=None
-      L22=None
-      L33=None
-      L12=None
-      L13=None
-      L23=None
-      try: L11 = float(srecord[lsrecord-3])
-      except ValueError: return None
-      try: L22 = float(srecord[lsrecord-1])
-      except ValueError: return None
-    check = record.count("L33:")*record.count("L12:")
-    if(check == 1 and lsrecord == 6):
-      assert L11 is not None and L22 is not None
-      assert L33 is None and L12 is None and L13 is None and L23 is None
-      try: L33 = float(srecord[lsrecord-3])
-      except ValueError: return None
-      try: L12 = float(srecord[lsrecord-1])
-      except ValueError: return None
-    check = record.count("L13:")*record.count("L23:")
-    if(check == 1 and lsrecord == 6):
-      assert L11 is not None and L22 is not None
-      assert L33 is not None and L12 is not None
-      assert L13 is None and L23 is None
-      try: L13 = float(srecord[lsrecord-3])
-      except ValueError: return None
-      try: L23 = float(srecord[lsrecord-1])
-      except ValueError: return None
-      L.append([L11, L22, L33, L12, L13, L23])
-#=====> extract current S
-    check = record.count("S11:")*record.count("S12:")*record.count("S13:")
-    if(check == 1 and lsrecord == 8):
-      S11=None
-      S12=None
-      S13=None
-      S21=None
-      S22=None
-      S23=None
-      S31=None
-      S32=None
-      S33=None
-      try: S11 = float(srecord[lsrecord-5])
-      except ValueError: return None
-      try: S12 = float(srecord[lsrecord-3])
-      except ValueError: return None
-      try: S13 = float(srecord[lsrecord-1])
-      except ValueError: return None
-    check = record.count("S21:")*record.count("S22:")*record.count("S23:")
-    if(check == 1 and lsrecord == 8):
-      assert S11 is not None and S12 is not None and S13 is not None
-      assert S21 is None and S22 is None and S23 is None
-      assert S31 is None and S32 is None and S33 is None
-      try: S21 = float(srecord[lsrecord-5])
-      except ValueError: return None
-      try: S22 = float(srecord[lsrecord-3])
-      except ValueError: return None
-      try: S23 = float(srecord[lsrecord-1])
-      except ValueError: return None
-    check = record.count("S31:")*record.count("S32:")*record.count("S33:")
-    if(check == 1 and lsrecord == 8):
-      assert S11 is not None and S12 is not None and S13 is not None
-      assert S21 is not None and S22 is not None and S23 is not None
-      assert S31 is None and S32 is None and S33 is None
-      try: S31 = float(srecord[lsrecord-5])
-      except ValueError: return None
-      try: S32 = float(srecord[lsrecord-3])
-      except ValueError: return None
-      try: S33 = float(srecord[lsrecord-1])
-      except ValueError: return None
-      S.append([S11, S12, S13, S21, S22, S23, S31, S32, S33])
-
-  assert group_counter == number_of_tls_groups == len(T)
-  #assert len(T) == len(L) == len(S) == len(origin) == len(residue_range)
-  #XXX self.pdb_residue_range: do this smarter
-  tls_params = []
-  for t,l,s,o,rr in zip(T,L,S,origin,residue_range):
-      tls_params.append(tls(T=t,L=l,S=s,origin=o,pdb_residue_range=rr))
-  return tls_params
 
 def get_program(st):
+  st = st.upper()
   program = None
   programs = ["SHELX", "CNS", "PROLSQ", "FROG", "REFMAC", "XPLOR", "X-PLOR",
               "NUCLSQ", "MOPRO", "MOLLY", "PROFFT", "TNT", "CORELS", "GROMOS"
               "XTALVIEW", "RESTRAIN", "GPRLSA", "ARP", "CNX", "EREF",
-              "NMREF", "GSAS", "BUSTER","SOLVE", "CCP4", "NUCLIN", "MAIN"]
+              "NMREF", "GSAS", "BUSTER","SOLVE", "CCP4", "NUCLIN", "MAIN",
+              "PHENIX", "PHENIX.REFINE"]
   diamond = "DIAMOND"
   if(st[0:6] == "REMARK"):
      st_split = st.split()
