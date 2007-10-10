d14acf17970f1e2282bec654eaff59d484240bb1
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2007-10-10 21:57:52 +0000

    don't swallow KeyboardInterrupt; minor cosmetics

--- libtbx/libtbx/assert_utils.py
+++ libtbx/libtbx/assert_utils.py
@@ -4,6 +4,7 @@ def is_numeric(x):
   try:
     x+1.
     return True
+  except KeyboardInterrupt: raise
   except:
     return False
 
@@ -16,9 +17,10 @@ if __name__ == '__main__':
   assert not is_numeric((1,))
   try:
     from scitbx.array_family import flex
-    assert is_numeric(flex.double((1,2,3)))
   except ImportError:
     import sys
     print >> sys.stderr, "scitbx library not available:" \
                          "some tests were skipped"
+  else:
+    assert is_numeric(flex.double((1,2,3)))
   print 'OK'
