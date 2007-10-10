9942bc69e1ee6e2818044c14919da896f08f1cfd
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-10-10 10:17:40 +0000

    - modifications before renaming

--- smtbx/ab_initio/charge_flipping.h
+++ smtbx/ab_initio/charge_flipping.h
@@ -1,10 +1,10 @@
-#ifndef SMTBX_AB_INITIO_CHARGE_FLIPPING_H
-#define SMTBX_AB_INITIO_CHARGE_FLIPPING_H
+#ifndef SMTBX_AB_INITIO_DENSITY_MODIFICATION_H
+#define SMTBX_AB_INITIO_DENSITY_MODIFICATION_H
 
 #include <scitbx/array_family/ref.h>
 #include <scitbx/array_family/loops.h>
 
-namespace smtbx { namespace ab_initio {
+namespace smtbx { namespace ab_initio { namespace density_modification {
 
   namespace af = scitbx::af;
 
@@ -20,6 +20,25 @@ namespace smtbx { namespace ab_initio {
     }
   }
 
-}}
+  template<class FloatType, class AccessorType>
+  void low_density_elimination_in_place_tanaka_et_al_2001(
+    af::ref<FloatType, AccessorType> rho,
+    FloatType rho_s)
+  {
+      typedef typename AccessorType::index_type index_type;
+      for (af::nested_loop<index_type> i(rho.accessor().focus());
+           !i.over(); i.incr())
+      {
+        if (rho(i()) <= 0) {
+          rho(i()) = 0;
+        }
+        else {
+          FloatType x = rho(i())/rho_s;
+          rho(i()) *= 1 - std::exp(-0.5*x*x);
+        }
+      }
+  }
+  
+}}} // namespace smtbx::ab_initio:: density_modification
 
 #endif
