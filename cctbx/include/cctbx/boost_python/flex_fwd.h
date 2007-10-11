681818b18da886ed927415f597b3e2b40c005776
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2007-10-11 22:21:10 +0000

    work around weird gcc 3.2 problem (triggered by boost svn revison 39792)

--- cctbx/include/cctbx/boost_python/flex_fwd.h
+++ cctbx/include/cctbx/boost_python/flex_fwd.h
@@ -3,6 +3,13 @@
 
 #include <scitbx/array_family/boost_python/flex_fwd.h>
 
+#if defined(__GNUC__) && __GNUC__ == 3 && __GNUC_MINOR__ == 2
+// work around weird gcc 3.2 problem:
+//   def.hpp must be included before boost/math/special_functions/atanh.hpp
+//   (as of boost svn revison 39792)
+#include <boost/python/def.hpp>
+#endif
+
 #include <cctbx/maptbx/accessors/c_grid_p1.h>
 #include <cctbx/maptbx/accessors/c_grid_padded_p1.h>
 #include <cctbx/miller.h>
