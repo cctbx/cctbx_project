3daeb10db35c14814ef14d4edf0c5eb43bf6f682
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2007-10-20 05:37:45 +0000

    simple check of line count read from stdin, to replace cumbersome wc -l | sed 's/ //g' | libtbx.assert_stdin gymnastics

---
+++ libtbx/libtbx/command_line/assert_line_count.py
@@ -0,0 +1,9 @@
+import sys
+
+def run(args):
+  assert len(args) == 1
+  expected_count = int(args[0])
+  assert len(sys.stdin.read().splitlines()) == expected_count
+
+if (__name__ == "__main__"):
+  run(sys.argv[1:])
