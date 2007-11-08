c1ffe6f4d8396306f0602c3d9dca8c325255b3b7
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-11-08 12:00:37 +0000

    - make __call__ into an iterator which yields the results of the function applied to each element of the iterable passed to __call__ (in the same order)

--- libtbx/libtbx/symmetric_multi_processing.py
+++ libtbx/libtbx/symmetric_multi_processing.py
@@ -10,9 +10,11 @@ time being, only UNIX systems are supported, through the use of 'fork'.
 """
 
 from __future__ import generators, division
+from libtbx import forward_compatibility
 from libtbx.utils import tupleize
+from libtbx import easy_pickle
 import sys, os, select
-import cStringIO
+import cStringIO, cPickle
 
 
 class parallelized_function(object):
@@ -27,15 +29,18 @@ class parallelized_function(object):
     self.timeout = timeout
     self.printout = printout
     self.child_pid_for_out_fd = {}
+    self.index_of_pid = {}
+    self.next_i = 0
+    self.result_queue = []
     self.debug = False
 
   def child_out_fd(self):
     return self.child_pid_for_out_fd.keys()
   child_out_fd = property(child_out_fd)
 
-  def poll(self, block):
-    if block and self.debug: print "** waiting for a child to finish **"
-    if block:
+  def poll(self):
+    if len(self.child_out_fd) == self.max_children:
+      if self.debug: print "** waiting for a child to finish **"
       inputs, outputs, errors = select.select(self.child_out_fd, [], [])
     else:
       inputs, outputs, errors = select.select(self.child_out_fd, [], [],
@@ -50,12 +55,24 @@ class parallelized_function(object):
     for fd in inputs:
       f = os.fdopen(fd)
       msg = f.read() # block only shortly because of the child buffering
-      print >> self.printout, msg,
+      l = int(msg[:128], 16)
+      result = cPickle.loads(msg[128:128+l])
+      print >> self.printout, msg[128+l:],
       pid = self.child_pid_for_out_fd.pop(fd)
+      j = self.index_of_pid.pop(pid)
       os.waitpid(pid, 0) # to make sure the child is fully cleaned up
+      if self.debug: print "#%i -> %s" % (j, result)
+      self.result_queue.append((j, result))
+    for i, (j,x) in enumerate(sorted(self.result_queue)):
+      if j == self.next_i:
+        self.next_i += 1
+        del self.result_queue[i]
+        yield x
+      else:
+        break
 
   def __call__(self, iterable):
-    for x in iterable:
+    for i,x in enumerate(iterable):
       args = tupleize(x)
       r,w = os.pipe()
       pid = os.fork()
@@ -64,21 +81,33 @@ class parallelized_function(object):
         if self.debug: print "parent: from %i, child %i: to %i" % (r, pid, w)
         os.close(w)
         self.child_pid_for_out_fd[r] = pid
-        self.poll(block=len(self.child_out_fd) == self.max_children)
+        self.index_of_pid[pid] = i
+        for result in self.poll():
+          yield result
       else:
         # child
         os.close(r)
         # fully buffer the print-out of the wrapped function
-        sys.stdout = cStringIO.StringIO()
-        self.func(*args)
+        output = cStringIO.StringIO()
+        sys.stdout, stdout = output, sys.stdout
+        result = self.func(*args)
+        sys.stdout = stdout
         if self.debug: print "\tchild writing to %i" % w
         f = os.fdopen(w, 'w')
-        print >> f, sys.stdout.getvalue(),
+        pickled_result = easy_pickle.pickled(result)
+        msg = ''.join((
+          "%128x" % len(pickled_result),
+          pickled_result,
+          output.getvalue()))
+        print >> f, msg,
         try:f.close()
         except KeyboardInterrupt: raise
         except:pass
         os._exit(0)
-    while self.child_out_fd: self.poll(block=False)
+    while self.child_out_fd:
+      for result in self.poll():
+        yield result
+
 
 def exercise():
   import math, time, re
@@ -103,12 +132,20 @@ def exercise():
     ref_printout = zip(xrange(1,11), [ "%.2f" % s for s in ref_results ])
     f = parallelized_function(func, max_children=2,
                               printout=cStringIO.StringIO())
-    f(xrange(1,11))
+    if 0:
+      f.debug = True
+      f.timeout = 0.1
+    results = list(f(xrange(1,11)))
+    if 0:
+      print results
+      print f.printout.getvalue()
+    assert approx_equal(results, ref_results, eps=1e-3)
     matches = [ result_pat.search(li)
                 for li in f.printout.getvalue().strip().split('\n') ]
     printout = [ (int(m.group(1)), m.group(2)) for m in matches ]
     printout.sort()
     assert printout == ref_printout
+    sys.stdout.flush()
   except NotImplementedError:
     print "Skipped!"
 
