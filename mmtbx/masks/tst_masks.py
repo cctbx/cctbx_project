5d9372ac528458d0d73f48843363f88eaab1c3a3
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2007-11-15 05:48:52 +0000

    use approx_equal() to compensate for rounding errors

--- mmtbx/masks/tst_masks.py
+++ mmtbx/masks/tst_masks.py
@@ -207,8 +207,9 @@ def exercise_3():
   mm3 = masks.manager(miller_array   = f_calc,
                       xray_structure = xs)
   assert list(xs.scatterers().extract_occupancies()) == [0.0, 0.0]
-  assert flex.abs(mm3.compute_f_mask().data()).min_max_mean().as_tuple() == \
-    (0.0, 0.0, 0.0)
+  assert approx_equal(
+    flex.abs(mm3.compute_f_mask().data()).min_max_mean().as_tuple(),
+    (0.0, 0.0, 0.0))
 
 def exercise_centrics(space_group_info, n_sites=10):
   structure = random_structure.xray_structure(
