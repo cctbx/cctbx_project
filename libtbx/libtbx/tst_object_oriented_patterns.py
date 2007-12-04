875a9d7325f0410ed9d840df9a5f7f348a386de8
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-12-04 23:23:47 +0000

    - memoization of function and method result - extend injector to accept any attribute definition

--- libtbx/libtbx/tst_object_oriented_patterns.py
+++ libtbx/libtbx/tst_object_oriented_patterns.py
@@ -2,13 +2,17 @@ from libtbx import object_oriented_patterns as oop
 
 def exercise_injector():
   class a(object):
+    """ doc for a """
     def __init__(self, i): self.i = i
     def get(self): return self.i
     def set(self, i): self.i = i
 
   class a_extension(oop.injector, a):
+    """ doc for extension of a """
+    var = 1
     def get_square(self): return self.get() * self.get()
 
+  assert a.var == 1
   o = a(2)
   assert o.get() == 2
   assert o.get_square() == 4
@@ -32,10 +36,69 @@ def exercise_injector():
     class d_extension(oop.injector, d):
       def get_square(self): return 0
   except AssertionError, err:
-    assert str(err) == "class d has already a method get_square"
+    assert str(err) == "class d has already attribute 'get_square'"
+
+def exercise_memoize():
+  diagnostic = []
+  def f(x):
+    diagnostic.append('+')
+    return x+1
+
+  mf = oop.memoize(f)
+  assert mf(0) == 1
+  assert diagnostic == ['+']
+  assert mf(0) == 1
+  assert diagnostic == ['+']
+  assert mf(1) == 2
+  assert diagnostic == ['+']*2
+  assert mf(0) == 1
+  assert diagnostic == ['+']*2
+  assert mf(1) == 2
+  assert diagnostic == ['+']*2
+
+  class foo(object):
+    def __init__(self, a):
+      self.a = a
+    def f(self, x):
+      diagnostic.append('+')
+      return self.a + x
+    f = oop.memoize_method(f)
+
+  diagnostic = []
+  o1 = foo(1)
+  o2 = foo(2)
+  assert o1.f(4) == 5
+  assert diagnostic == ['+']
+  assert o2.f(5) == 7
+  assert diagnostic == ['+']*2
+  assert o1.f(2) == 3
+  assert diagnostic == ['+']*3
+  assert o1.f(4) == 5
+  assert diagnostic == ['+']*3
+  assert o2.f(5) == 7
+  assert diagnostic == ['+']*3
+  o3 = o1
+  assert o3.f(4) == 5
+  assert diagnostic == ['+']*3
+  assert o3.f(1) == 2
+  assert diagnostic == ['+']*4
+  assert o1.f(1) == 2
+  assert diagnostic == ['+']*4
+
+  class _foo_extension(oop.injector, foo):
+    def g(self, n):
+      return 'x'*n
+    g = oop.memoize_method(g)
+
+  efoo = foo(0)
+  assert efoo.g(2) == "xx"
+  assert efoo.g(3) == "xxx"
+  assert efoo.g(2) == "xx"
+  assert efoo._memoized_g.cached == {(2,):'xx', (3,): 'xxx'}
 
 def run():
   exercise_injector()
+  exercise_memoize()
   print 'OK'
 
 if __name__ == '__main__':
