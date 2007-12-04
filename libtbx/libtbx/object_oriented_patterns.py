ff5e3b03688ddc81426d7511385efd687ef5eab7
Luc J. Bourhis <luc_j_bourhis@mac.com>
2007-12-04 23:23:47 +0000

    - memoization of function and method result - extend injector to accept any attribute definition

--- libtbx/libtbx/object_oriented_patterns.py
+++ libtbx/libtbx/object_oriented_patterns.py
@@ -1,5 +1,7 @@
 """ A library of object-oriented patterns """
 
+import new
+
 class injector:
   """ Injection of new methods into an existing class
 
@@ -16,7 +18,7 @@ class injector:
   obj.new_method(...)
   obj = another_existing_class(...)
   obj.new_method(...)
-  # class 'some_descriptive_name' is an empty shell with no use by itself.
+  # class 'some_descriptive_text' is an empty shell with no use by itself.
 
   * motivation *
 
@@ -37,8 +39,46 @@ class injector:
     def __init__(cls, classname, bases, classdict):
       for target_class in bases[1:]:
         for name, attribute in classdict.items():
-          if not callable(attribute): continue
+          if name in ('__module__', '__doc__'): continue
           assert not hasattr(target_class, name), (
-            "class %s has already a method %s"
+            "class %s has already attribute '%s'"
             % (target_class.__name__, name))
           setattr(target_class, name, attribute)
+
+
+class memoize(object):
+  """ Memoize the result returned by a function """
+
+  def __init__(self, func):
+    self.cached = {}
+    self.func = func
+
+  def __call__(self, *args, **kwds):
+    if kwds:
+      return self.func(*args, **kwds)
+    try:
+      return self.cached[args]
+    except KeyError:
+      self.cached[args] = result = self.func(*args)
+      return result
+    except TypeError:
+      return self.func(*args)
+
+
+class memoize_method(object):
+  """ Memoize the result returned by a bound method """
+
+  def __init__(self, meth):
+    self.cache = '_memoized_%s' % meth.__name__
+    self.meth = meth
+
+  def __get__(self, obj, type=None):
+    if obj is None:
+      return self
+    try:
+      return getattr(obj, self.cache)
+    except AttributeError:
+      func = new.instancemethod(self.meth, obj, type)
+      memoized = memoize(func)
+      setattr(obj, self.cache, memoized)
+      return memoized
