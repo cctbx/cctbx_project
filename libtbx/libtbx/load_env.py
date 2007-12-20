3e9acd92c909e04dc4432dd8041aeb3a3cbe432b
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2007-12-20 20:34:27 +0000

    define libtbx.env.dispatcher_name

--- libtbx/libtbx/load_env.py
+++ libtbx/libtbx/load_env.py
@@ -1,4 +1,6 @@
 import libtbx
 import libtbx.config
+import os
 libtbx.env = libtbx.config.unpickle()
 libtbx.env.set_os_environ_all_dist()
+libtbx.env.dispatcher_name = os.environ.get("LIBTBX_DISPATCHER_NAME")
