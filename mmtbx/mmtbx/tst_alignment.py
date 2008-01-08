f6e9d3741df0bc5e19f7c64b8589e198a15b1bba
Pavel Afonine <pafonine@users.noreply.github.com>
2008-01-08 06:02:26 +0000

    extract exact match selections and test

--- mmtbx/mmtbx/tst_alignment.py
+++ mmtbx/mmtbx/tst_alignment.py
@@ -5,45 +5,45 @@ from scitbx.array_family import flex
 def exercise():
   #
   i_seqs, j_seqs = align("EASYA",
-                         "AETSYT").extract_alignment().get_selections()
+                         "AETSYT").extract_alignment().exact_match_selections()
   assert i_seqs == flex.size_t([0, 2, 3]) and j_seqs == flex.size_t([1, 3, 4])
   #
   i_seqs, j_seqs = align("AAAGGTT",
-                         "AAATT").extract_alignment().get_selections()
+                         "AAATT").extract_alignment().exact_match_selections()
   assert i_seqs == flex.size_t([0, 1, 2, 5, 6]) and \
          j_seqs == flex.size_t([0, 1, 2, 3, 4])
   #
   i_seqs, j_seqs = align("AESD",
-                         "AEDK").extract_alignment().get_selections()
+                         "AEDK").extract_alignment().exact_match_selections()
   assert i_seqs == flex.size_t([0, 1]) and j_seqs == flex.size_t([0, 1])
   #
   i_seqs, j_seqs = align("EASY",
-                         "YSAE").extract_alignment().get_selections()
+                         "YSAE").extract_alignment().exact_match_selections()
   assert i_seqs.size()==0 and j_seqs.size()==0
   #
   i_seqs, j_seqs = align("EASY",
-                         "KMT").extract_alignment().get_selections()
+                         "KMT").extract_alignment().exact_match_selections()
   assert i_seqs.size()==0 and j_seqs.size()==0
   #
   i_seqs, j_seqs = align("EASY",
-                         "KMST").extract_alignment().get_selections()
+                         "KMST").extract_alignment().exact_match_selections()
   assert i_seqs == flex.size_t([2]) and j_seqs == flex.size_t([2])
   #
   i_seqs, j_seqs = align("EASY",
-                         "KMMST").extract_alignment().get_selections()
+                         "KMMST").extract_alignment().exact_match_selections()
   assert i_seqs == flex.size_t([2]) and j_seqs == flex.size_t([3])
   #
   i_seqs, j_seqs = align("EASY",
-                         "EATY").extract_alignment().get_selections()
+                         "EATY").extract_alignment().exact_match_selections()
   assert i_seqs == flex.size_t([0, 1, 3]) and j_seqs == flex.size_t([0, 1, 3])
   #
   i_seqs, j_seqs = align("EASIEST",
-                         "EATY").extract_alignment().get_selections()
+                         "EATY").extract_alignment().exact_match_selections()
   assert i_seqs == flex.size_t([0, 1]) and j_seqs == flex.size_t([0, 1])
   #
   i_seqs, j_seqs = align("EEEEEASIEST",
                          "EEEATYRRIESQQEIES"
-                        ).extract_alignment().get_selections()
+                        ).extract_alignment().exact_match_selections()
   assert i_seqs == flex.size_t([0, 1, 2, 7, 8, 9]) and \
          j_seqs == flex.size_t([0, 1, 2, 8, 9, 10])
 
