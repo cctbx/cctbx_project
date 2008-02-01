3a7125635d6f6f15ec3616344c888c30e8320652
Pavel Afonine <afonine@users.noreply.github.com>
2008-02-01 06:25:48 +0000

    build H into a difference map: half of way...

--- mmtbx/mmtbx/hydrogens/find.py
+++ mmtbx/mmtbx/hydrogens/find.py
@@ -1,61 +1,78 @@
 import sys, math
 from cctbx.array_family import flex
-import iotbx.pdb
-import mmtbx.f_model
-from cctbx import miller
 from mmtbx import find_peaks
-from mmtbx import monomer_library
-import mmtbx.monomer_library.pdb_interpretation
-import mmtbx.monomer_library.server
 from mmtbx import utils
+import iotbx.phil
+from scitbx import matrix
+
+master_params_part1 = iotbx.phil.parse("""\
+map_type = mFobs-DFmodel
+  .type = str
+  .help = Map type to be used to find hydrogens
+map_cutoff = 2.0
+  .type = float
+  .help = Map cutoff
+""")
+
+master_params_part2 = find_peaks.master_params.fetch(iotbx.phil.parse("""\
+use_sigma_scaled_maps = True
+resolution_factor = 1./4.
+map_next_to_model
+{
+  min_model_peak_dist = 0.7
+  max_model_peak_dist = 1.15
+  min_peak_peak_dist = 1.0
+  use_hydrogens = True
+}
+peak_search
+{
+  peak_search_level = 3
+  min_cross_distance = 1.0
+}
+"""))
+
+def all_master_params():
+  return iotbx.phil.parse("""\
+    include scope mmtbx.hydrogens.find.master_params_part1
+    include scope mmtbx.hydrogens.find.master_params_part2
+""", process_includes=True)
 
 class h_peak(object):
   def __init__(self, site_frac,
                      height,
                      dist,
-                     site_frac_o,
-                     atom_attribute_o):
+                     scatterer_o,
+                     atom_attribute_o,
+                     i_seq_o):
     self.site_frac        = site_frac
     self.height           = height
     self.dist             = dist
-    self.site_frac_o      = site_frac_o
+    self.scatterer_o      = scatterer_o
     self.atom_attribute_o = atom_attribute_o
+    self.i_seq_o          = i_seq_o
 
-def angle(o,h1,h2):
+def water_bond_angle(o,h1,h2):
   result = None
   a = h1[0]-o[0], h1[1]-o[1], h1[2]-o[2]
   b = h2[0]-o[0], h2[1]-o[1], h2[2]-o[2]
-  ab = a[0]*b[0]+a[1]*b[1]+a[2]*b[2]
-  a_abs = math.sqrt(a[0]*a[0]+a[1]*a[1]+a[2]*a[2])
-  b_abs = math.sqrt(b[0]*b[0]+b[1]*b[1]+b[2]*b[2])
-  a_abs_b_abs = a_abs * b_abs
-  if(a_abs_b_abs != 0):
-    result = math.acos(ab/(a_abs_b_abs))*180./math.pi
+  a = matrix.col(a)
+  b = matrix.col(b)
+  return a.angle(b, deg=True)
+
+def find_hydrogen_peaks(fmodel,
+                        atom_attributes_list,
+                        params,
+                        log):
+  fp_manager = find_peaks.manager(fmodel     = fmodel,
+                                  map_type   = params.map_type,
+                                  map_cutoff = params.map_cutoff,
+                                  params     = params,
+                                  log        = log)
+  result = fp_manager.peaks_mapped()
+  fp_manager.show_mapped(atom_attributes_list = atom_attributes_list)
   return result
 
-def find_hydrogens(fmodel,
-                   atom_attributes_list,
-                   map_type,
-                   map_cutoff,
-                   dist_min,
-                   dist_max,
-                   peak_search_level):
-  fp_params = find_peaks.master_params.extract()
-  fp_params.peak_search.peak_search_level = peak_search_level
-  fp_params.map_next_to_model.min_model_peak_dist = dist_min
-  fp_params.map_next_to_model.min_peak_peak_dist = 1.0
-  fp_params.peak_search.min_cross_distance = 1.0
-  fp_params.map_next_to_model.use_hydrogens = True
-  fp_params.map_next_to_model.max_model_peak_dist = dist_max
-  fp_manager = find_peaks.manager(fmodel     = fmodel,
-                                  map_type   = map_type,
-                                  map_cutoff = map_cutoff,
-                                  params     = fp_params,
-                                  log        = None)
-  fp_manager.show_mapped(atom_attributes_list = atom_attributes_list)
-  return fp_manager.peaks_mapped()
-
-def extract_hoh_peaks(peaks, atom_attributes_list, xray_structure):
+def extract_hoh_peaks(peaks, atom_attributes_list, xray_structure, log):
   scatterers = xray_structure.scatterers()
   assert scatterers.size() == len(atom_attributes_list)
   assert peaks.sites.size() == peaks.heights.size()
@@ -64,150 +81,75 @@ def extract_hoh_peaks(peaks, atom_attributes_list, xray_structure):
   sites = peaks.sites.select(perm)
   heights = peaks.heights.select(perm)
   iseqs_of_closest_atoms = peaks.iseqs_of_closest_atoms.select(perm)
-
-  print "\n\nWater H:\n"
-  for s, h, i_seq in zip(sites, heights, iseqs_of_closest_atoms):
-    aa = atom_attributes_list[i_seq]
-    if(aa.resName in ["HOH","SOL","SOLV","WAT","DOD","TIP3"]):
-      d = xray_structure.unit_cell().distance(s, scatterers[i_seq].site)
-      print "peak= %8.3f closest distance to %s = %8.3f"%(
-        h, aa.pdb_format(), d)
-  ####
-  print
-  top = {}
+  result = {}
   unit_cell = xray_structure.unit_cell()
+  get_class = iotbx.pdb.common_residue_names_get_class
   for s, h, i_seq in zip(sites, heights, iseqs_of_closest_atoms):
     aa = atom_attributes_list[i_seq]
-    if(aa.resName in ["HOH","SOL","SOLV","WAT","DOD","TIP3"]):
+    if(get_class(name = aa.resName) == "common_water"):
+      scatterer_o = scatterers[i_seq]
       hp = h_peak(
         site_frac        = s,
         height           = h,
-        dist             = unit_cell.distance(s, scatterers[i_seq].site),
-        site_frac_o      = scatterers[i_seq].site,
-        atom_attribute_o = aa)
-      top.setdefault(i_seq,[]).append(hp)
-  for key in top.keys():
-    print atom_attributes_list[int(key)].pdb_format()
-    sz = len(top[key])
-    for j, jhp in enumerate(top[key]):
+        dist             = unit_cell.distance(s, scatterer_o.site),
+        scatterer_o      = scatterer_o,
+        atom_attribute_o = aa,
+        i_seq_o          = i_seq)
+      result.setdefault(i_seq,[]).append(hp)
+  for key in result.keys():
+    print >> log, atom_attributes_list[int(key)].pdb_format()
+    sz = len(result[key])
+    for j, jhp in enumerate(result[key]):
       if(sz == 1):
-        print "  ", jhp.height, jhp.dist
+        print >> log, "  ", jhp.height, jhp.dist
       else:
         angles = []
-        for k, khp in enumerate(top[key]):
+        for k, khp in enumerate(result[key]):
           if(j != k):
-            assert jhp.site_frac_o == khp.site_frac_o
-            angl = angle(o=unit_cell.orthogonalize(jhp.site_frac_o),
-                                h1=unit_cell.orthogonalize(jhp.site_frac),
-                                h2=unit_cell.orthogonalize(khp.site_frac))
+            assert jhp.scatterer_o.site == khp.scatterer_o.site
+            angl = water_bond_angle(
+              o=unit_cell.orthogonalize(jhp.scatterer_o.site),
+              h1=unit_cell.orthogonalize(jhp.site_frac),
+              h2=unit_cell.orthogonalize(khp.site_frac))
             angles.append(angl)
-            print "  ", jhp.height, jhp.dist, angl
+            print >> log, "  ", jhp.height, jhp.dist, angl
+  return result
 
+def rebuild_hydrogens(h_peaks, model):
+  items = h_peaks.items()
+  items.sort()
+  items.reverse()
+  h_peaks_as_tuple = [(key, value) for key, value in items]
+  # add H ato xray_structure and aal
+  # important: asume atoms are aded from tail to head of the list
+  #            asume only one bonded atom is added
+  sca = model.xray_structure.scatterers()
+  aal = model.atom_attributres_list
+  xh_ct = model
+  for hp in h_peaks_as_tuple:
+    i = hp[0]
+    label = "H1"
+    sc = xray.scatterer(label           = label,
+                        scattering_type = "H",
+                        site            = hp[1].site_frac,
+                        u               = hp.scatterer_o.u_iso, # XXX if aniso, then convert to iso
+                        occupancy       = hp.scatterer_o.occupancy)
+    sca = sca[:i+1]+sc+sca[i+1:]
+    #HETATM   35  H1  HOH S   4       7.424  12.171   7.250  1.00 15.00           H
+    fmt = "HETATM    0  %2s  HOH%2s%4s    %8.3f%8.3f%8.3f%6.2f%6.2f%12s"
 
-#def add_hydrogens():
+  # invalidate relevant memebrs of model
 
-#def exercise():
-#  xrs_exact = iotbx.pdb.input(file_name = "m.pdb").xray_structure_simple()
-#  xrs_part = iotbx.pdb.input(file_name = "m_omitH.pdb").xray_structure_simple()
-#  miller_set = miller.build_set(
-#    crystal_symmetry = xrs_exact.crystal_symmetry(),
-#    anomalous_flag   = False,
-#    d_min            = 1.0)
-#  f_obs = abs(miller_set.structure_factors_from_scatterers(
-#    xray_structure = xrs_exact,
-#    algorithm      = "direct",
-#    cos_sin_table  = False).f_calc())
-#  sf_par = mmtbx.f_model.sf_and_grads_accuracy_params.extract()
-#  sf_par.algorithm = "direct"
-#  sf_par.cos_sin_table = False
-#  flags = f_obs.array(data=flex.bool(f_obs.data().size(),False))
-#  fmodel = mmtbx.f_model.manager(
-#    xray_structure               = xrs_part,
-#    sf_and_grads_accuracy_params = sf_par,
-#    r_free_flags                 = flags,
-#    target_name                  = "ls_wunit_k1",
-#    f_obs                        = f_obs)
-#  #
-#  mon_lib_srv = monomer_library.server.server()
-#  ener_lib = monomer_library.server.ener_lib()
-#  processed_pdb_file = monomer_library.pdb_interpretation.process(
-#    mon_lib_srv              = mon_lib_srv,
-#    ener_lib                 = ener_lib,
-#    file_name                = "m_omitH.pdb")
-#  aal = processed_pdb_file.all_chain_proxies.stage_1.atom_attributes_list
-#  #
-#  peaks = find_hydrogens(fmodel = fmodel,
-#                         atom_attributes_list = aal,
-#                         map_type = "mFo-DFc",
-#                         map_cutoff = 3,
-#                         dist_min = 0.7,
-#                         dist_max = 1.3)
-
-def exercise_real(pdb_file_name = "ald.pdb",
-                  cif_file_name = "ald.cif",
-                  mtz_file_name = "ald.mtz",
-                  pkl_file_name = "fmodel_ald.pickle"):
-  if 1:
-    cif_objects = None
-    if(cif_file_name is not None):
-      cif_objects = [].append((cif_file_name,
-        mmtbx.monomer_library.server.read_cif(file_name = cif_file_name)))
-    processed_pdb_files_srv = utils.process_pdb_file_srv(
-      cif_objects = cif_objects)
-    processed_pdb_file, pdb_inp = \
-      processed_pdb_files_srv.process_pdb_files(
-        pdb_file_names = [pdb_file_name])
-    aal = processed_pdb_file.all_chain_proxies.stage_1.atom_attributes_list
-    xrs = processed_pdb_file.xray_structure(show_summary = False)
-    #
-  if 0:
-    from iotbx import reflection_file_reader
-    reflection_file = reflection_file_reader.any_reflection_file(
-      file_name = mtz_file_name)
-    reflection_file_as_miller_arrays = \
-      reflection_file.as_miller_arrays(merge_equivalents=True)
-    for miller_array in reflection_file_as_miller_arrays:
-      label=miller_array.info().labels[0]
-      if label in ['I-obs']:
-        f_obs = miller_array
-      if label in ['R-free-flags']:
-        flags = miller_array
-    f_obs = f_obs.f_sq_as_f()
-    flags = flags.array(data = flags.data() == 1)
-
-    sf_par = mmtbx.f_model.sf_and_grads_accuracy_params.extract()
-    sf_par.algorithm = "fft"
-    sf_par.cos_sin_table = True
-    sf_par.grid_resolution_factor=1/4.
-    fmodel = mmtbx.f_model.manager(
-      xray_structure               = xrs,
-      sf_and_grads_accuracy_params = sf_par,
-      r_free_flags                 = flags,
-      target_name                  = "ls_wunit_k1",
-      f_obs                        = f_obs)
-    print fmodel.r_work(), fmodel.r_free()
-    fmodel.update_solvent_and_scale()
-    #
-    from libtbx import easy_pickle
-    easy_pickle.dump(pkl_file_name, (fmodel))
-    #
-  ###
-  from libtbx import easy_pickle
-  fmodel = easy_pickle.load(pkl_file_name)
-  print fmodel.r_work(), fmodel.r_free()
-  ##
-  peaks = find_hydrogens(fmodel = fmodel,
-                         atom_attributes_list = aal,
-                         map_type = "mFo-DFc",
-                         map_cutoff = 2,
-                         dist_min = 0.7,
-                         dist_max = 1.15,
-                         peak_search_level = 3)
-  extract_hoh_peaks(peaks = peaks, atom_attributes_list = aal, xray_structure = xrs)
-
-
-if (__name__ == "__main__"):
-  exercise_real()
-
-
+def run(fmodel, model, log):
+  params = all_master_params().extract()
+  peaks = find_hydrogen_peaks(
+    fmodel               = fmodel,
+    atom_attributes_list = model.atom_attributes_list,
+    params               = params,
+    log                  = log)
+  h_peaks = extract_hoh_peaks(
+    peaks                = peaks,
+    atom_attributes_list = model.atom_attributes_list,
+    xray_structure       = model.xray_structure,
+    log                  = log)
 
