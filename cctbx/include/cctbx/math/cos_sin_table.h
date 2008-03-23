921cdd9c5b5d91e45bac55e402fd71b1c47d5eb0
Randy Read <randyread@users.noreply.github.com>
2008-03-23 21:45:27 +0000

    Add Rob's cos_sin_lin_interp_table to gain smoothness and accuracy.

--- cctbx/include/cctbx/math/cos_sin_table.h
+++ cctbx/include/cctbx/math/cos_sin_table.h
@@ -59,10 +59,78 @@ namespace cctbx { namespace math {
       FloatType* values_;
   };
 
+  template <typename FloatType=double>
+  class cos_sin_lin_interp_table
+  {
+    public:
+      cos_sin_lin_interp_table() {}
+
+      cos_sin_lin_interp_table(int n_points)
+      :
+        n_points_(n_points),
+        n_points_4_(n_points/4),
+        values_memory_(new FloatType[n_points_+n_points_4_+1]),
+        values_(values_memory_.get()),
+        diffvalues_memory_(new FloatType[n_points_+n_points_4_+1]),
+        diffvalues_(diffvalues_memory_.get())
+      {
+        CCTBX_ASSERT(n_points % 4 == 0);
+        using scitbx::constants::two_pi;
+        for(int i=0;i<=n_points_+n_points_4_;i++)
+        {
+          values_[i] = std::sin(i*two_pi/n_points_);
+          diffvalues_[i] = std::sin((i+1)*two_pi/n_points_)
+                         - std::sin(i*two_pi/n_points_);
+        }
+      }
+
+      int
+      n_points() const { return n_points_; }
+
+      std::complex<FloatType>
+      get(FloatType const& unary_angle) const
+      {
+        FloatType dec = unary_angle*n_points_;
+        if (dec<0.0)
+        {
+          dec = -dec;
+          int i = static_cast<int>(dec); // floor it to an integer
+          // get fraction between the ceiling and the floor of fval
+          FloatType frac = dec - i;
+          // return interpolated value between the i-th and the (i+1)th entry
+          i = i % n_points_;
+
+          return std::complex<FloatType>(
+              values_[i+n_points_4_] + frac*diffvalues_[i+n_points_4_],
+              - values_[i] -frac*diffvalues_[i]);
+        }
+        else
+        {
+          int i = static_cast<int>(dec); // floor it to an integer
+          FloatType frac = dec - i;
+          // return interpolated value between the i-th and the (i+1)th entry
+          i = i % n_points_;
+
+          return std::complex<FloatType>(
+              values_[i+n_points_4_] + frac*diffvalues_[i+n_points_4_],
+              values_[i] + frac*diffvalues_[i]);
+        }
+      }
+    private:
+      int n_points_;
+      int n_points_4_;
+      boost::shared_array<FloatType> values_memory_;
+      FloatType* values_;
+      boost::shared_array<FloatType> diffvalues_memory_;
+      FloatType* diffvalues_;
+  };
+
+
 #if defined(__sgi) && !defined(__GNUC__)
   namespace work_around_edg_typename_handling {
     typedef cos_sin_exact<double> cos_sin_exact_double;
     typedef cos_sin_table<double> cos_sin_table_double;
+    typedef cos_sin_lin_interp_table<double> cos_sin_lin_interp_table_double;
   }
 #endif
 
