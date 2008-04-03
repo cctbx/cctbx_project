8d1135a09c4026da2269196392c0fbd59b505ba5
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2008-04-03 22:04:52 +0000

    new --write-pdb-file=FILE option with related secondary options

--- iotbx/iotbx/command_line/pdb.hierarchy.py
+++ iotbx/iotbx/command_line/pdb.hierarchy.py
@@ -35,16 +35,54 @@ def run(args, command_name="phenix.pdb.hierarchy"):
       default="",
       help="prefix for all output lines",
       metavar="STRING")
+    .option(None, "--write_pdb_file",
+      action="store",
+      type="string",
+      default=None,
+      help="write hierarchy as PDB coordinate section to file",
+      metavar="FILE")
+    .option(None, "--reset_serial_first_value",
+      action="store",
+      type="int",
+      default=None,
+      help="resets atom serial numbers before writing PDB file",
+      metavar="INT")
+    .option(None, "--interleaved_conf",
+      action="store",
+      type="int",
+      default=0,
+      help="interleave alt. conf. when writing PDB file; possible choices are:"
+        " 0 (not interleaved),"
+        " 1 (interleaved atom names but not resnames),"
+        " 2 (fully interleaved)",
+      metavar="INT")
+    .option(None, "--no_anisou",
+      action="store_true",
+      default=False,
+      help="suppress ANISOU records when writing PDB file")
+    .option(None, "--no_sigatm",
+      action="store_true",
+      default=False,
+      help="suppress SIGATM records when writing PDB file")
   ).process(args=args)
   co = command_line.options
   for file_name in command_line.args:
     if (not os.path.isfile(file_name)): continue
-    execute(
+    pdb_objs = execute(
       file_name=file_name,
       prefix=co.prefix,
       residue_groups_max_show=co.residue_groups_max_show,
       duplicate_atom_labels_max_show=co.duplicate_atom_labels_max_show,
       level_id=co.details)
+    if (co.write_pdb_file is not None):
+      pdb_objs.hierarchy.write_pdb_file(
+        file_name=co.write_pdb_file,
+        append_end=True,
+        interleaved_conf=co.interleaved_conf,
+        atoms_reset_serial_first_value=co.reset_serial_first_value,
+        sigatm=not co.no_sigatm,
+        anisou=not co.no_anisou,
+        siguij=not co.no_anisou)
     print co.prefix.rstrip()
 
 def execute(
