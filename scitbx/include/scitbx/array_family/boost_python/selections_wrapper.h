783666049ed5313fc3391deca90b4c1e0584fd54
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2008-04-05 15:50:06 +0000

    work around Tru64 cxx 6.5 compiler crash

--- scitbx/include/scitbx/array_family/boost_python/selections_wrapper.h
+++ scitbx/include/scitbx/array_family/boost_python/selections_wrapper.h
@@ -2,7 +2,6 @@
 #define SCITBX_ARRAY_FAMILY_BOOST_PYTHON_SELECTIONS_WRAPPER_H
 
 #include <boost/python/args.hpp>
-#include <boost/python/overloads.hpp>
 #include <scitbx/array_family/selections.h>
 
 namespace scitbx { namespace af { namespace boost_python {
@@ -10,24 +9,40 @@ namespace scitbx { namespace af { namespace boost_python {
   template <typename ElementType>
   struct select_wrappers
   {
-    BOOST_PYTHON_FUNCTION_OVERLOADS(select_overloads, select, 2, 3)
+    static shared<ElementType>
+    with_flags(
+      const_ref<ElementType> const& self,
+      const_ref<bool> const& flags)
+    {
+      return select(self, flags);
+    }
+
+    static shared<ElementType>
+    with_indices(
+      const_ref<ElementType> const& self,
+      const_ref<std::size_t> const& indices)
+    {
+      return select(self, indices, false);
+    }
+
+    static shared<ElementType>
+    with_indices_reverse(
+      const_ref<ElementType> const& self,
+      const_ref<std::size_t> const& indices,
+      bool reverse)
+    {
+      return select(self, indices, reverse);
+    }
 
     template <typename ArrayWrapper>
     static void
     wrap(ArrayWrapper& aw)
     {
       using namespace boost::python;
-      aw.def("select",
-          (shared<ElementType>(*)(
-            const_ref<ElementType> const&,
-            const_ref<bool> const&)) select, (
-              arg_("self"), arg_("flags")))
-        .def("select",
-          (shared<ElementType>(*)(
-            const_ref<ElementType> const&,
-            const_ref<std::size_t> const&,
-            bool)) select, select_overloads((
-              arg_("self"), arg_("indices"), arg_("reverse")=false)))
+      aw.def("select", with_flags, (arg_("self"), arg_("flags")))
+        .def("select", with_indices, (arg_("self"), arg_("indices")))
+        .def("select", with_indices_reverse, (
+          arg_("self"), arg_("indices"), arg_("reverse")))
       ;
     }
   };
