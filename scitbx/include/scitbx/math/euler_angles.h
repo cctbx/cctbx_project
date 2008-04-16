7563bd8a5ee407327b0bc93073e743f6c1a6baa5
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2008-04-16 20:37:42 +0000

    keyword argument support for euler_angles.h functions; consistently preserve argument names in function signatures

--- scitbx/include/scitbx/math/euler_angles.h
+++ scitbx/include/scitbx/math/euler_angles.h
@@ -2,36 +2,43 @@
 #define SCITBX_MATH_EULER_ANGLES_H
 
 #include <scitbx/constants.h>
-#include <scitbx/vec3.h>
 #include <scitbx/mat3.h>
 
 namespace scitbx {  namespace math {  namespace euler_angles {
-    
+
+/* Mathematica code:
+     rx={{1,0,0},{0,cx,-sx},{0,sx,cx}}
+     ry={{cy,0,sy},{0,1,0},{-sy,0,cy}}
+     rz={{cz,-sz,0},{sz,cz,0},{0,0,1}}
+     rz1={{cz1,-sz1,0},{sz1,cz1,0},{0,0,1}}
+     rz3={{cz3,-sz3,0},{sz3,cz3,0},{0,0,1}}
+ */
+
 template< typename FloatType >
 const scitbx::mat3< FloatType >
-xyz_matrix( const FloatType x_deg, const FloatType y_deg, const FloatType z_deg )
+xyz_matrix( const FloatType ax, const FloatType ay, const FloatType az )
 {
-    FloatType x_rad( scitbx::deg_as_rad( x_deg ) );
-    FloatType y_rad( scitbx::deg_as_rad( y_deg ) );
-    FloatType z_rad( scitbx::deg_as_rad( z_deg ) );
-    
+    FloatType x_rad( scitbx::deg_as_rad( ax ) );
+    FloatType y_rad( scitbx::deg_as_rad( ay ) );
+    FloatType z_rad( scitbx::deg_as_rad( az ) );
+
     FloatType sin_x( std::sin( x_rad ) );
     FloatType sin_y( std::sin( y_rad ) );
     FloatType sin_z( std::sin( z_rad ) );
-    
+
     FloatType cos_x( std::cos( x_rad ) );
     FloatType cos_y( std::cos( y_rad ) );
     FloatType cos_z( std::cos( z_rad ) );
-    
+
     return scitbx::mat3< FloatType >(
              cos_y * cos_z,
             -cos_y * sin_z,
              sin_y,
-            
+
              cos_x * sin_z  +  sin_x * sin_y * cos_z,
              cos_x * cos_z  -  sin_x * sin_y * sin_z,
             -sin_x * cos_y,
-            
+
              sin_x * sin_z  -  cos_x * sin_y * cos_z,
              sin_x * cos_z  +  cos_x * sin_y * sin_z,
              cos_x * cos_y );
@@ -49,7 +56,7 @@ xyz_angles( const scitbx::mat3< FloatType >& m, const FloatType eps = 1.0e-12 )
                 scitbx::rad_as_deg( scitbx::constants::pi_2 ),
                 scitbx::rad_as_deg( 0.0 ) );
     }
-    
+
     else if ( m( 0, 2 ) < ( -1 + eps ) )
     {
         return scitbx::vec3< FloatType >(
@@ -57,7 +64,7 @@ xyz_angles( const scitbx::mat3< FloatType >& m, const FloatType eps = 1.0e-12 )
                 scitbx::rad_as_deg( -scitbx::constants::pi_2 ),
                 scitbx::rad_as_deg( 0.0 ) );
     }
-    
+
     else
     {
         return scitbx::vec3< FloatType >(
@@ -70,29 +77,29 @@ xyz_angles( const scitbx::mat3< FloatType >& m, const FloatType eps = 1.0e-12 )
 
 template< typename FloatType >
 const scitbx::mat3< FloatType >
-yzx_matrix( const FloatType y_deg, const FloatType z_deg, const FloatType x_deg )
+yzx_matrix( const FloatType ay, const FloatType az, const FloatType ax )
 {
-    FloatType y_rad( scitbx::deg_as_rad( y_deg ) );
-    FloatType z_rad( scitbx::deg_as_rad( z_deg ) );
-    FloatType x_rad( scitbx::deg_as_rad( x_deg ) );
-    
+    FloatType y_rad( scitbx::deg_as_rad( ay ) );
+    FloatType z_rad( scitbx::deg_as_rad( az ) );
+    FloatType x_rad( scitbx::deg_as_rad( ax ) );
+
     FloatType sin_y( std::sin( y_rad ) );
     FloatType sin_z( std::sin( z_rad ) );
     FloatType sin_x( std::sin( x_rad ) );
-    
+
     FloatType cos_y( std::cos( y_rad ) );
     FloatType cos_z( std::cos( z_rad ) );
     FloatType cos_x( std::cos( x_rad ) );
-    
+
     return scitbx::mat3< FloatType >(
              cos_y * cos_z,
              sin_x * sin_y  -  cos_x * cos_y * sin_z,
              cos_x * sin_y  +  cos_y * sin_x * sin_z,
-            
+
              sin_z,
              cos_x * cos_z,
             -cos_z * sin_x,
-            
+
             -cos_z * sin_y,
              cos_y * sin_x  +  cos_x * sin_y * sin_z,
              cos_x * cos_y  -  sin_x * sin_y * sin_z );
@@ -110,7 +117,7 @@ yzx_angles( const scitbx::mat3< FloatType >& m, const FloatType eps = 1.0e-12 )
                 scitbx::rad_as_deg( scitbx::constants::pi_2 ),
                 scitbx::rad_as_deg( 0.0 ) );
     }
-    
+
     else if ( m( 1, 0 ) < ( -1 + eps ) )
     {
         return scitbx::vec3< FloatType >(
@@ -118,7 +125,7 @@ yzx_angles( const scitbx::mat3< FloatType >& m, const FloatType eps = 1.0e-12 )
                 scitbx::rad_as_deg( -scitbx::constants::pi_2 ),
                 scitbx::rad_as_deg( 0.0 ) );
     }
-    
+
     else
     {
         return scitbx::vec3< FloatType >(
@@ -131,29 +138,29 @@ yzx_angles( const scitbx::mat3< FloatType >& m, const FloatType eps = 1.0e-12 )
 
 template< typename FloatType >
 const scitbx::mat3< FloatType >
-zyz_matrix( const FloatType z1_deg, const FloatType y2_deg, const FloatType z3_deg )
+zyz_matrix( const FloatType az1, const FloatType ay, const FloatType az3 )
 {
-    FloatType z1_rad( scitbx::deg_as_rad( z1_deg ) );
-    FloatType y2_rad( scitbx::deg_as_rad( y2_deg ) );
-    FloatType z3_rad( scitbx::deg_as_rad( z3_deg ) );
-    
+    FloatType z1_rad( scitbx::deg_as_rad( az1 ) );
+    FloatType y2_rad( scitbx::deg_as_rad( ay ) );
+    FloatType z3_rad( scitbx::deg_as_rad( az3 ) );
+
     FloatType sin_z1( std::sin( z1_rad ) );
     FloatType sin_y2( std::sin( y2_rad ) );
     FloatType sin_z3( std::sin( z3_rad ) );
-    
+
     FloatType cos_z1( std::cos( z1_rad ) );
     FloatType cos_y2( std::cos( y2_rad ) );
     FloatType cos_z3( std::cos( z3_rad ) );
-    
+
     return scitbx::mat3< FloatType >(
              cos_y2 * cos_z1 * cos_z3  -  sin_z1 * sin_z3,
             -cos_z3 * sin_z1  -  cos_y2 * cos_z1 * sin_z3,
              cos_z1 * sin_y2,
-            
+
              cos_y2 * cos_z3 * sin_z1  +  cos_z1 * sin_z3,
              cos_z1 * cos_z3  -  cos_y2 * sin_z1 * sin_z3,
              sin_y2 * sin_z1,
-            
+
             -cos_z3 * sin_y2,
              sin_y2 * sin_z3,
              cos_y2 );
@@ -171,7 +178,7 @@ zyz_angles( const scitbx::mat3< FloatType >& m, const FloatType eps = 1.0e-12 )
                 scitbx::rad_as_deg( 0.0 ),
                 scitbx::rad_as_deg( 0.0 ) );
     }
-    
+
     else if ( m( 2, 2 ) < ( -1 + eps ) )
     {
         return scitbx::vec3< FloatType >(
@@ -179,7 +186,7 @@ zyz_angles( const scitbx::mat3< FloatType >& m, const FloatType eps = 1.0e-12 )
                 scitbx::rad_as_deg( scitbx::constants::pi ),
                 scitbx::rad_as_deg( 0.0 ) );
     }
-    
+
     else
     {
         return scitbx::vec3< FloatType >(
@@ -188,7 +195,7 @@ zyz_angles( const scitbx::mat3< FloatType >& m, const FloatType eps = 1.0e-12 )
                 scitbx::rad_as_deg( std::atan2( m(2, 1), -m(2, 0) ) ) );
     }
 }
-    
+
 }  }  } //namespace scitbx::math::euler_angles
 
 #endif /*SCITBX_MATH_EULER_ANGLES_H*/
