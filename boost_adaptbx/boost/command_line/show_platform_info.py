38ebb594e132b84918ba1570d8fb1284c69afa6e
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2008-04-18 02:29:37 +0000

    get number of processors via sysctl() or sysconf() if possible (this covers Tru64 Unix)

--- boost_adaptbx/boost/command_line/show_platform_info.py
+++ boost_adaptbx/boost/command_line/show_platform_info.py
@@ -1,10 +1,13 @@
 # LIBTBX_SET_DISPATCHER_NAME boost_adaptbx.show_platform_info
 
 import boost.python
+from libtbx import introspection
 import sys
 
 def run():
   sys.stdout.write(boost.python.platform_info)
+  print "number of processors:", introspection.number_of_processors(
+    return_value_if_unknown="unknown")
   print "sys.byteorder:", sys.byteorder
   try: import thread
   except ImportError: print "import thread: NO"
