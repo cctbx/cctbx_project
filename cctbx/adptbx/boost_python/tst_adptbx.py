8a5bf596978ee6ac1e08e3445d6ae93ffa77b361
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2008-04-19 19:51:46 +0000

    exercise_ellipsoid(): local random number generator with fixed seed, to avoid rare failures; missing "assert" added

--- cctbx/adptbx/boost_python/tst_adptbx.py
+++ cctbx/adptbx/boost_python/tst_adptbx.py
@@ -258,20 +258,21 @@ def exercise_eigen(n_trials=100):
         diag = [random.random()] * 3
       exercise_eigen_core(diag)
 
-def exercise_ellipsoid():
-  for i in xrange(100):
-    half_lengths = matrix.col([ random.random() for k in xrange(3) ])
+def exercise_ellipsoid(n_trials=100, n_sub_trials=10):
+  rnd = random.Random(0)
+  for i in xrange(n_trials):
+    half_lengths = matrix.col([ 0.1 + rnd.random() for k in xrange(3) ])
     r = scitbx.math.euler_angles_as_matrix(
-      [ random.uniform(0, 360) for i in xrange(3) ], deg=True)
+      [ rnd.uniform(0, 360) for i in xrange(3) ], deg=True)
     u_cart = r.transpose() * matrix.diag([ x**2 for x in half_lengths ]) * r
     es = adptbx.eigensystem(u_cart.as_sym_mat3())
     t = adptbx.sphere_to_ellipsoid_transform(es)
     m = matrix.sqr(t.matrix())
-    for j in xrange(10):
-      x = matrix.col([ random.uniform(0,1) for k in xrange(3) ]).normalize()
+    for j in xrange(n_sub_trials):
+      x = matrix.col([ rnd.random() for k in xrange(3) ]).normalize()
       y = m*x
       c = y.transpose() * u_cart.inverse() * y
-      approx_equal(c[0], 1)
+      assert approx_equal(c[0], 1)
   r = scitbx.math.euler_angles_as_matrix((30, 115, 260), deg=True)
   u_cart = r.transpose() * matrix.diag((-1, 0.1, 1)) * r
   es = adptbx.eigensystem(u_cart.as_sym_mat3())
