4a68f7270cfd3d4068d765ca0340ea1a0820610a
Luc J. Bourhis <luc_j_bourhis@mac.com>
2008-04-21 15:29:02 +0000

    - to run all the smtbx tests

---
+++ smtbx/run_tests.py
@@ -0,0 +1,15 @@
+from libtbx import test_utils
+import libtbx.load_env
+
+def run():
+  tst_list = (
+    "$D/ab_initio/tests/tst_ab_initio_ext.py",
+    "$D/ab_initio/tests/tst_charge_flipping.py",
+    "$D/refinement/tests/tst_minimization_at_random.py"
+    )
+  build_dir = libtbx.env.under_build("smtbx")
+  dist_dir = libtbx.env.dist_path("smtbx")
+  test_utils.run_tests(build_dir, dist_dir, tst_list)
+  
+if __name__ == '__main__':
+  run()
