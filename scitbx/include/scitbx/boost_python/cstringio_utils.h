9de0529c3af41dd2c1e65e2a76224b775c981dff
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2008-05-14 20:25:43 +0000

    cstringio code moved to scitbx/boost_python/cstringio_utils.h

--- scitbx/include/scitbx/boost_python/cstringio_utils.h
+++ scitbx/include/scitbx/boost_python/cstringio_utils.h
@@ -1,6 +1,7 @@
 #ifndef SCITBX_BOOST_PYTHON_CSTRINGIO_UTILS_H
 #define SCITBX_BOOST_PYTHON_CSTRINGIO_UTILS_H
 
+#include <stdexcept>
 #include <cStringIO.h>
 
 namespace scitbx { namespace boost_python {
@@ -14,6 +15,30 @@ namespace scitbx { namespace boost_python {
     char* m = const_cast<char*>(module_name);
     char* c = const_cast<char*>(cobject_name);
     PycStringIO = reinterpret_cast<PycStringIO_CAPI*>(PyCObject_Import(m, c));
+  }
+
+  inline
+  void
+  cstringio_output_check(PyObject* sio)
+  {
+    if (!PycStringIO_OutputCheck(sio)) {
+      throw std::invalid_argument(
+        "cstringio argument must be a cStringIO.StringIO instance.");
+    }
+  }
+
+  inline
+  void
+  cstringio_cwrite(PyObject* sio, const char* s, unsigned n)
+  {
+    PycStringIO->cwrite(
+      sio,
+#if PY_VERSION_HEX >= 0x02050000
+      s,
+#else
+      const_cast<char*>(s),
+#endif
+      static_cast<boost::python::ssize_t>(n));
   }
 
 }} // namespace scitbx::boost_python
