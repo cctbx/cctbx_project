daf99ed219b284c05b7b43df79a2401ad8772ff6
Pavel Afonine <afonine@users.noreply.github.com>
2008-05-20 19:03:24 +0000

    bug fix: outliers rejection was always performed but not applied to actual data; for twin refinement it's still not aailabe and the change is made for compatibility of twin f_model.

--- mmtbx/mmtbx/refinement/__init__.py
+++ mmtbx/mmtbx/refinement/__init__.py
@@ -1,6 +1,7 @@
 from libtbx import adopt_init_args
 import cctbx.array_family.flex
 from mmtbx.refinement import print_statistics
+from mmtbx import utils
 
 class monitors(object):
   def __init__(self, params,
@@ -33,6 +34,9 @@ class monitors(object):
       self.target_weights = target_weights
     if(model is not None): self.model = model
     if(fmodels is not None): self.fmodels = fmodels
+    utils.assert_xray_structures_equal(
+      x1 = self.fmodels.fmodel_xray().xray_structure,
+      x2 = self.model.xray_structure)
     self.monitor_xray.collect(
       model                        = self.model,
       fmodel                       = self.fmodels.fmodel_xray(),
@@ -42,6 +46,9 @@ class monitors(object):
       target_weights               = self.target_weights,
       rigid_body_shift_accumulator = rigid_body_shift_accumulator)
     if(self.monitor_neutron is not None):
+      utils.assert_xray_structures_equal(
+        x1 = self.fmodels.fmodel_neutron().xray_structure,
+        x2 = self.model.xray_structure)
       self.monitor_neutron.collect(
         model                        = self.model,
         fmodel                       = self.fmodels.fmodel_neutron(),
