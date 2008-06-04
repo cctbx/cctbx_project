c15abbcf0efc51389b6059f371508fdc3edae832
Luc J. Bourhis <luc_j_bourhis@mac.com>
2008-06-04 23:36:41 +0000

    add more cctbx common monikers to the smtbx namespace

--- smtbx/import_cctbx.h
+++ smtbx/import_cctbx.h
@@ -1,11 +1,13 @@
 #ifndef SMTBX_IMPORT_CCTBX_H
 #define SMTBX_IMPORT_CCTBX_H
 
-namespace smtbx { 
-  namespace uctbx = cctbx::uctbx;
+namespace smtbx {
   namespace adptbx = cctbx::adptbx;
+  namespace uctbx = cctbx::uctbx;
+  namespace sgtbx = cctbx::sgtbx;
+  namespace xray = cctbx::xray;
   using cctbx::cartesian;
-  using cctbx::error;
-}  
+  using cctbx::fractional;
+}
 
 #endif
