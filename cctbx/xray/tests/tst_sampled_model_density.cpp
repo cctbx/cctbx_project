752c51fa13a265aaabb3df039259960517650112
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2008-06-06 18:01:32 +0000

    xray/tests/tst_sampled_model_density.cpp: use scitbx::math::basic_statistics to replace output of complete map

--- cctbx/xray/tests/tst_sampled_model_density.cpp
+++ cctbx/xray/tests/tst_sampled_model_density.cpp
@@ -1,11 +1,15 @@
 #include "sampled_model_density.h"
+#include <scitbx/math/basic_statistics.h>
 
 int main() {
   sampled_model_density_test_case test;
   std::cout
     << "sampled model density and main compiled together into this executable"
     << std::endl;
-  std::cout << test.sampled_model_density->real_map().as_1d().const_ref()
-            << std::endl;
-
+  scitbx::math::basic_statistics<double> stats(
+    test.sampled_model_density->real_map().as_1d().const_ref());
+  std::cout << "map points: " << stats.n << std::endl;
+  std::cout << "    min:    " << stats.min << std::endl;
+  std::cout << "    max:    " << stats.max << std::endl;
+  std::cout << "    mean:   " << stats.mean << std::endl;
 }
