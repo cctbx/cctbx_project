0c5e40ac05c2b2f575d5c5640e3aa55b8b21bc54
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2008-06-12 19:48:41 +0000

    fftpack timings.py: show wall-clock times if num_threads > 1

--- scitbx/fftpack/boost_python/timings.py
+++ scitbx/fftpack/boost_python/timings.py
@@ -6,16 +6,28 @@ from libtbx.utils import time_log
 import random
 import sys
 
-def run(n_iterations=3):
+def run(args, num_threads=0, n_iterations=3):
   show_times = libtbx.utils.show_times()
-  assert len(sys.argv) in (1,2)
-  if (len(sys.argv) > 1):
-    n_iterations = int(sys.argv[1])
+  assert len(args) <= 2, "[num_threads, [n_iterations]]"
+  if (len(args) > 0):
+    num_threads = int(args[0])
+    if (len(args) > 1):
+      n_iterations = int(args[1])
+  #
+  print "default omptbx.env.num_threads:", omptbx.env.num_threads
+  if (num_threads > 0):
+    print "num_threads from command line:", num_threads
+    omptbx.env.num_threads = num_threads
+    print "working omptbx.env.num_threads:", omptbx.env.num_threads
+  use_wall_clock = (omptbx.env.num_threads > 1)
+  print "use_wall_clock:", use_wall_clock
+  #
   rfft = fftpack.real_to_complex_3d((2*3*5*7,3*4*5*7,3*4*5*5))
   print "rfft.m_real():", rfft.m_real()
-  t_map = time_log("map")
-  t_fill = time_log("fill")
-  t_fft = time_log("fft")
+  #
+  t_map = time_log(label="map", use_wall_clock=use_wall_clock)
+  t_fill = time_log(label="fill", use_wall_clock=use_wall_clock)
+  t_fft = time_log(label="fft", use_wall_clock=use_wall_clock)
   print t_map.legend
   for i_iteration in xrange(n_iterations):
     t_map.start()
@@ -31,4 +43,4 @@ def run(n_iterations=3):
   show_times()
 
 if (__name__ == "__main__"):
-  run()
+  run(sys.argv[1:])
