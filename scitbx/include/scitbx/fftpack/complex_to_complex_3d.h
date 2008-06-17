a0eed0c19058c913b02bcf85c0a89c6ba53cb29f
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2008-06-17 06:53:34 +0000

    scitbx/fftpack/complex_to_complex_3d.h: OpenMP support equivalent to real_to_complex_3d.h

--- scitbx/include/scitbx/fftpack/complex_to_complex_3d.h
+++ scitbx/include/scitbx/fftpack/complex_to_complex_3d.h
@@ -1,9 +1,11 @@
 #ifndef SCITBX_FFTPACK_COMPLEX_TO_COMPLEX_3D_H
 #define SCITBX_FFTPACK_COMPLEX_TO_COMPLEX_3D_H
 
-#include <scitbx/error.h>
-#include <scitbx/array_family/tiny_reductions.h>
 #include <scitbx/fftpack/complex_to_complex.h>
+#include <scitbx/error.h>
+#include <omptbx/omp_or_stubs.h>
+
+//#define SCITBX_FFTPACK_COMPLEX_TO_COMPLEX_3D_NO_PRAGMA_OMP
 
 namespace scitbx { namespace fftpack {
 
@@ -37,28 +39,45 @@ namespace scitbx { namespace fftpack {
       /*! See also: complex_to_complex
        */
       template <typename MapType>
-      void forward(MapType map)
+      void forward(
+        MapType map,
+        real_type* scratch=0)
       {
-        transform(select_sign<forward_tag>(), map);
+        transform(select_sign<forward_tag>(), map, scratch);
       }
       //! In-place "backward" Fourier transformation.
       /*! See also: complex_to_complex
        */
       template <typename MapType>
-      void backward(MapType map)
+      void backward(
+        MapType map,
+        real_type* scratch=0)
       {
-        transform(select_sign<backward_tag>(), map);
+        transform(select_sign<backward_tag>(), map, scratch);
       }
+
     protected:
       // This accepts complex or real maps.
       template <typename Tag, typename MapType>
-      void transform(select_sign<Tag> tag, MapType map) {
+      void
+      transform(
+        select_sign<Tag> tag,
+        MapType map,
+        real_type* scratch)
+      {
         typedef typename MapType::value_type real_or_complex_type;
-        transform(tag, map, real_or_complex_type());
+        transform(tag, map, scratch, real_or_complex_type());
       }
+
       // Cast map of real to map of complex.
       template <typename Tag, typename MapType>
-      void transform(select_sign<Tag> tag, MapType map, real_type) {
+      void
+      transform(
+        select_sign<Tag> tag,
+        MapType map,
+        real_type* scratch,
+        real_type)
+      {
         typedef typename MapType::accessor_type accessor_type;
         accessor_type dim_real(map.accessor());
         if (dim_real[2] % 2 != 0) {
@@ -67,55 +86,85 @@ namespace scitbx { namespace fftpack {
         af::ref<complex_type, accessor_type> cmap(
           reinterpret_cast<complex_type*>(map.begin()),
           accessor_type(dim_real[0], dim_real[1], dim_real[2] / 2));
-        transform(tag, cmap, complex_type());
+        transform(tag, cmap, scratch, complex_type());
       }
+
       // Core routine always works on complex maps.
       template <typename Tag, typename MapType>
-      void transform(select_sign<Tag> tag, MapType map, complex_type) {
+      void
+      transform(
+        select_sign<Tag> tag,
+        MapType map,
+        real_type* scratch,
+        complex_type)
+      {
   // FUTURE: move out of class body
   {
-    complex_type* seq = &(*(seq_.begin()));
-    for (std::size_t iz = 0; iz < fft1d_[2].n(); iz++) {
-      for (std::size_t iy = 0; iy < fft1d_[1].n(); iy++) {
-        std::size_t ix;
-        for (ix = 0; ix < fft1d_[0].n(); ix++) {
-          seq[ix] = map(ix, iy, iz);
+    int nx = fft1d_[0].n();
+    int ny = fft1d_[1].n();
+    int nz = fft1d_[2].n();
+    int seq_size = 2 * std::max(std::max(nx, ny), nz);
+    scitbx::auto_array<real_type> seq_and_scratch;
+    omp_set_dynamic(0);
+#if !defined(SCITBX_FFTPACK_COMPLEX_TO_COMPLEX_3D_NO_PRAGMA_OMP)
+    #pragma omp parallel
+#endif
+    {
+      int num_threads = omp_get_num_threads();
+      int i_thread = omp_get_thread_num();
+#if !defined(SCITBX_FFTPACK_COMPLEX_TO_COMPLEX_3D_NO_PRAGMA_OMP)
+      #pragma omp single
+#endif
+      {
+        seq_and_scratch = scitbx::auto_array<real_type>(
+          new real_type[2 * seq_size * num_threads]);
+      }
+      real_type* scratch = seq_and_scratch.get() + 2 * seq_size * i_thread;
+      complex_type* seq = reinterpret_cast<complex_type*>(scratch + seq_size);
+#if !defined(SCITBX_FFTPACK_COMPLEX_TO_COMPLEX_3D_NO_PRAGMA_OMP)
+      #pragma omp for
+#endif
+      for (int iz = 0; iz < nz; iz++) {
+        for (int iy = 0; iy < ny; iy++) {
+          for (int ix = 0; ix < nx; ix++) {
+            seq[ix] = map(ix, iy, iz);
+          }
+          // Transform along x (slow direction)
+          fft1d_[0].transform(tag, seq, scratch);
+          for (int ix = 0; ix < nx; ix++) {
+            map(ix, iy, iz) = seq[ix];
+          }
         }
-        // Transform along x (slow direction)
-        fft1d_[0].transform(tag, seq);
-        for (ix = 0; ix < fft1d_[0].n(); ix++) {
-          map(ix, iy, iz) = seq[ix];
+        for (int ix = 0; ix < nx; ix++) {
+          for (int iy = 0; iy < ny; iy++) {
+            seq[iy] = map(ix, iy, iz);
+          }
+          // Transform along y (medium direction)
+          fft1d_[1].transform(tag, seq, scratch);
+          for (int iy = 0; iy < ny; iy++) {
+            map(ix, iy, iz) = seq[iy];
+          }
         }
       }
-      for (std::size_t ix = 0; ix < fft1d_[0].n(); ix++) {
-        std::size_t iy;
-        for (iy = 0; iy < fft1d_[1].n(); iy++) {
-          seq[iy] = map(ix, iy, iz);
+#if !defined(SCITBX_FFTPACK_COMPLEX_TO_COMPLEX_3D_NO_PRAGMA_OMP)
+      #pragma omp for
+#endif
+      for (int ix = 0; ix < nx; ix++) {
+        for (int iy = 0; iy < ny; iy++) {
+          // Transform along z (fast direction)
+          fft1d_[2].transform(tag, &map(ix, iy, 0), scratch);
         }
-        // Transform along y (medium direction)
-        fft1d_[1].transform(tag, seq);
-        for (iy = 0; iy < fft1d_[1].n(); iy++) {
-          map(ix, iy, iz) = seq[iy];
-        }
-      }
-    }
-    for (std::size_t ix = 0; ix < fft1d_[0].n(); ix++) {
-      for (std::size_t iy = 0; iy < fft1d_[1].n(); iy++) {
-        // Transform along z (fast direction)
-        fft1d_[2].transform(tag, &map(ix, iy, 0));
       }
     }
   }
       }
     private:
       af::tiny<complex_to_complex<real_type, complex_type>, 3> fft1d_;
-      af::shared<complex_type> seq_;
   };
 
   template <typename RealType, typename ComplexType>
   complex_to_complex_3d<RealType, ComplexType
     >::complex_to_complex_3d(const af::int3& n)
-    : seq_(af::max(n))
   {
     for(std::size_t i=0;i<3;i++) {
       fft1d_[i] = complex_to_complex<real_type, complex_type>(n[i]);
@@ -125,7 +174,6 @@ namespace scitbx { namespace fftpack {
   template <typename RealType, typename ComplexType>
   complex_to_complex_3d<RealType, ComplexType
     >::complex_to_complex_3d(std::size_t n0, std::size_t n1, std::size_t n2)
-    : seq_(af::max(af::tiny<std::size_t, 3>(n0, n1, n2)))
   {
     fft1d_[0] = complex_to_complex<real_type, complex_type>(n0);
     fft1d_[1] = complex_to_complex<real_type, complex_type>(n1);
