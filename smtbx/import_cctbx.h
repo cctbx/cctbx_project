7c490e337de721de0ce0120aae267fd8908ec824
Luc J. Bourhis <luc_j_bourhis@mac.com>
2008-06-23 13:23:05 +0000

    define cctbx namespaces so that this header can be included anywhere

--- smtbx/import_cctbx.h
+++ smtbx/import_cctbx.h
@@ -1,6 +1,13 @@
 #ifndef SMTBX_IMPORT_CCTBX_H
 #define SMTBX_IMPORT_CCTBX_H
 
+namespace cctbx {
+  namespace adptbx {}
+  namespace uctbx {}
+  namespace sgtbx {}
+  namespace xray {}
+}
+
 namespace smtbx {
   namespace adptbx = cctbx::adptbx;
   namespace uctbx = cctbx::uctbx;
