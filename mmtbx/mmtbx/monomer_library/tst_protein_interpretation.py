fdb370a961f4ed60a6ac42404987002082e658ea
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2008-06-25 05:36:54 +0000

    pdb_interpretation.py: detect protein HXT and apply COOH modification instead of COO

--- mmtbx/mmtbx/monomer_library/tst_protein_interpretation.py
+++ mmtbx/mmtbx/monomer_library/tst_protein_interpretation.py
@@ -13,7 +13,7 @@ expected_results = {
 "ile_nterm_all_h_1v1c_v2":
   ['ILE%NH3', 21, [], 'peptide', True, None],
 "met_cterm_all_h_1hz3_v2":
-  ['MET%COO', 18, [' HXT'], 'peptide', True, None],
+  ['MET%COOH', 19, [], 'peptide', True, None],
 "his_chain_all_h_1g7e_v2":
   ['HIS', 18, [], 'peptide', None, None],
 "arg_nterm_all_h_5pti_v2":
@@ -37,7 +37,7 @@ expected_results = {
 "glu_nterm_all_h_2bic_v2":
   ['GLU%NH3', 17, [' HE2'], 'peptide', True, None],
 "ile_cterm_all_h_1pog_v3":
-  ['ILE%COO', 20, [' HXT'], 'peptide', True, None],
+  ['ILE%COOH', 21, [], 'peptide', True, None],
 "met_nterm_all_h_1qqi_v2":
   ['MET%NH3', 19, [], 'peptide', True, None],
 "ser_chain_all_h_1o8t_v2":
@@ -45,7 +45,7 @@ expected_results = {
 "ala_nterm_all_h_1blq_v2":
   ['ALA%NH3', 12, [], 'peptide', True, None],
 "phe_cterm_all_h_2jnr_v2":
-  ['PHE%COO', 21, [' HXT'], 'peptide', True, None],
+  ['PHE%COOH', 22, [], 'peptide', True, None],
 "val_nterm_all_h_1ylb_v2":
   ['VAL%NH3', 18, [], 'peptide', True, None],
 "met_chain_all_h_1ozo_v3":
@@ -63,7 +63,7 @@ expected_results = {
 "arg_nterm_all_h_1dpu_v3":
   ['ARG%NH3', 26, [], 'peptide', True, None],
 "val_cterm_all_h_1cz2_v2":
-  ['VAL%COO', 17, [' HXT'], 'peptide', True, None],
+  ['VAL%COOH', 18, [], 'peptide', True, None],
 "gly_nterm_all_h_3ins_v2":
   ['GLY%NH3', 9, [], 'peptide', True, None],
 "met_nterm_all_h_2j46_v3":
@@ -79,7 +79,7 @@ expected_results = {
 "lys_nterm_all_h_2bvb_v2":
   ['LYS%NH3', 24, [], 'peptide', True, None],
 "asn_cterm_all_h_2cdx_v2":
-  ['ASN%COO', 15, [' HXT'], 'peptide', True, None],
+  ['ASN%COOH', 16, [], 'peptide', True, None],
 "lys_chain_all_h_1o8t_v3":
   ['LYS', 22, [], 'peptide', None, None],
 "thr_nterm_all_h_2g3q_v2":
@@ -109,9 +109,9 @@ expected_results = {
 "ala_chain_all_h_1ozo_v2":
   ['ALA', 10, [], 'peptide', None, None],
 "ala_cterm_all_h_1eio_v2":
-  ['ALA%COO', 11, [' HXT'], 'peptide', True, None],
+  ['ALA%COOH', 12, [], 'peptide', True, None],
 "asp_cterm_all_h_1kb8_v2":
-  ['ASP%COO', 13, [' HD2', ' HXT'], 'peptide', True, None],
+  ['ASP%COOH', 14, [' HD2'], 'peptide', True, None],
 "ser_nterm_all_h_1gkt_v2":
   ['SER%NH3', 13, [], 'peptide', True, None],
 "ile_nterm_all_h_1ghx_v3":
@@ -211,7 +211,7 @@ expected_results = {
 "glu_chain_all_h_1tmr_v3":
   ['GLU', 15, [' HE2'], 'peptide', None, None],
 "lys_cterm_all_h_1aze_v3":
-  ['LYS%COO', 23, [' HXT'], 'peptide', True, None],
+  ['LYS%COOH', 24, [], 'peptide', True, None],
 "glu_chain_all_h_1bm4_v2":
   ['GLU', 15, [' HE2'], 'peptide', None, None],
 "trp_chain_all_h_1hdp_v3":
@@ -227,11 +227,11 @@ expected_results = {
 "ser_chain_all_h_1o8t_v3":
   ['SER', 11, [], 'peptide', None, None],
 "arg_cterm_all_h_1mmc_v3":
-  ['ARG%COO', 25, [' HXT'], 'peptide', True, None],
+  ['ARG%COOH', 26, [], 'peptide', True, None],
 "pro_nterm_all_h_2lhb_v3":
   ['PRO%NH2', 16, [], 'peptide', True, None],
 "tyr_cterm_all_h_1fu6_v3":
-  ['TYR%COO', 22, [' HXT'], 'peptide', True, None],
+  ['TYR%COOH', 23, [], 'peptide', True, None],
 "val_chain_all_h_1ozo_v2":
   ['VAL', 16, [], 'peptide', None, None],
 "ile_nterm_all_h_1p57_v3":
@@ -241,11 +241,11 @@ expected_results = {
 "asp_chain_all_h_2izb_v3":
   ['ASP', 12, [' HD2'], 'peptide', None, None],
 "lys_cterm_all_h_1gkt_v2":
-  ['LYS%COO', 23, [' DXT'], 'peptide', True, None],
+  ['LYS%COOH', 24, [], 'peptide', True, None],
 "gln_chain_all_h_1o8t_v3":
   ['GLN', 17, [], 'peptide', None, None],
 "ser_cterm_all_h_2jnr_v3":
-  ['SER%COO', 12, [' HXT'], 'peptide', True, None],
+  ['SER%COOH', 13, [], 'peptide', True, None],
 "lys_nterm_all_h_1io5_v3":
   ['LYS%NH3', 24, [], 'peptide', True, None],
 "ile_nterm_all_h_1tkw_v3":
@@ -253,9 +253,9 @@ expected_results = {
 "gln_nterm_all_h_1fmy_v3":
   ['GLN%NH3', 19, [], 'peptide', True, None],
 "met_cterm_all_h_1hz3_v3":
-  ['MET%COO', 18, [' HXT'], 'peptide', True, None],
+  ['MET%COOH', 19, [], 'peptide', True, None],
 "lys_cterm_all_h_1aze_v2":
-  ['LYS%COO', 23, [' HXT'], 'peptide', True, None],
+  ['LYS%COOH', 24, [], 'peptide', True, None],
 "val_nterm_all_h_2mb5_v3":
   ['VAL%NH3', 18, [], 'peptide', True, None],
 "val_nterm_all_h_1ylb_v3":
@@ -271,7 +271,7 @@ expected_results = {
 "arg_nterm_all_h_1i9f_v3":
   ['ARG%NH3', 26, [], 'peptide', True, None],
 "asp_cterm_all_h_1kb8_v3":
-  ['ASP%COO', 13, [' HD2', ' HXT'], 'peptide', True, None],
+  ['ASP%COOH', 14, [' HD2'], 'peptide', True, None],
 "met_chain_all_h_1ozo_v2":
   ['MET', 17, [], 'peptide', None, None],
 "trp_nterm_all_h_1fi6_v3":
@@ -285,11 +285,11 @@ expected_results = {
 "cys_nterm_all_h_1b9q_v3":
   ['CYS%NH3', 13, [], 'peptide', True, None],
 "leu_cterm_all_h_2oyw_v3":
-  ['LEU%COO', 20, [' HXT'], 'peptide', True, None],
+  ['LEU%COOH', 21, [], 'peptide', True, None],
 "trp_chain_all_h_1cx1_v3":
   ['TRP', 24, [], 'peptide', None, None],
 "ala_cterm_all_h_1jdk_v2":
-  ['ALA%COO', 11, [' HC '], 'peptide', True, None],
+  ['ALA%COOH', 12, [], 'peptide', True, None],
 "arg_nterm_all_h_5pti_v3":
   ['ARG%NH3', 26, [], 'peptide', True, None],
 "gly_chain_all_h_1ozo_v3":
@@ -305,13 +305,13 @@ expected_results = {
 "phe_chain_all_h_1hdp_v3":
   ['PHE', 20, [], 'peptide', None, None],
 "arg_cterm_all_h_1mmc_v2":
-  ['ARG%COO', 25, [' HXT'], 'peptide', True, None],
+  ['ARG%COOH', 26, [], 'peptide', True, None],
 "ile_nterm_all_h_1ntp_v2":
   ['ILE%NH3', 21, [], 'peptide', True, None],
 "met_nterm_all_h_2j46_v2":
   ['MET%NH3', 19, [], 'peptide', True, None],
 "asn_cterm_all_h_2cdx_v3":
-  ['ASN%COO', 15, [' HXT'], 'peptide', True, None],
+  ['ASN%COOH', 16, [], 'peptide', True, None],
 "lys_nterm_all_h_2bvb_v3":
   ['LYS%NH3', 24, [], 'peptide', True, None],
 "glu_nterm_all_h_1agg_v2":
@@ -321,7 +321,7 @@ expected_results = {
 "ala_nterm_all_h_1vcx_v2":
   ['ALA%NH3', 12, [], 'peptide', True, None],
 "ala_cterm_all_h_1eio_v3":
-  ['ALA%COO', 11, [' HXT'], 'peptide', True, None],
+  ['ALA%COOH', 12, [], 'peptide', True, None],
 "ser_nterm_all_h_1bl1_v2":
   ['SER%NH3', 13, [], 'peptide', True, None],
 "ile_chain_all_h_1ozo_v3":
@@ -335,7 +335,7 @@ expected_results = {
 "met_nterm_all_h_1c15_v2":
   ['MET%NH3', 19, [], 'peptide', True, None],
 "trp_cterm_all_h_6cmh_v2":
-  ['TRP%COO', 25, [' HXT'], 'peptide', True, None],
+  ['TRP%COOH', 26, [], 'peptide', True, None],
 "leu_chain_all_h_2sn3_v2":
   ['LEU', 19, [], 'peptide', None, None],
 "leu_nterm_all_h_2bun_v2":
@@ -345,7 +345,7 @@ expected_results = {
 "met_nterm_all_h_1d7q_v2":
   ['MET%NH2NOTPRO', 19, [], 'peptide', None, None],
 "val_cterm_all_h_1cz2_v3":
-  ['VAL%COO', 17, [' HXT'], 'peptide', True, None],
+  ['VAL%COOH', 18, [], 'peptide', True, None],
 "asn_nterm_all_h_1w4g_v2":
   ['ASN%NH3', 16, [], 'peptide', True, None],
 "ala_nterm_all_h_1blq_v3":
@@ -355,9 +355,9 @@ expected_results = {
 "glu_chain_all_h_1tmr_v2":
   ['GLU', 15, [' HE1'], 'peptide', None, None],
 "gly_cterm_all_h_1sbu_v3":
-  ['GLY%COO', 8, [' HXT'], 'peptide', True, None],
+  ['GLY%COOH', 9, [], 'peptide', True, None],
 "leu_cterm_all_h_2oyw_v2":
-  ['LEU%COO', 20, [' HXT'], 'peptide', True, None],
+  ['LEU%COOH', 21, [], 'peptide', True, None],
 "met_nterm_all_h_1euw_v3":
   ['MET%NH3', 19, [], 'peptide', True, None],
 "gly_nterm_all_h_3ins_v3":
@@ -367,7 +367,7 @@ expected_results = {
 "gly_chain_all_h_1ozo_v2":
   ['GLY', 7, [], 'peptide', None, None],
 "ala_cterm_all_h_1jdk_v3":
-  ['ALA%COO', 11, [' HXT'], 'peptide', True, None],
+  ['ALA%COOH', 12, [], 'peptide', True, None],
 "leu_chain_all_h_1ozo_v2":
   ['LEU', 19, [], 'peptide', None, None],
 "thr_chain_all_h_1o8t_v2":
@@ -375,19 +375,19 @@ expected_results = {
 "his_nterm_all_h_1c17_v2":
   ['HIS%NH3', 20, [], 'peptide', True, None],
 "gly_cterm_all_h_1sbu_v2":
-  ['GLY%COO', 8, [' HXT'], 'peptide', True, None],
+  ['GLY%COOH', 9, [], 'peptide', True, None],
 "tyr_cterm_all_h_1fu6_v2":
-  ['TYR%COO', 22, [' HXT'], 'peptide', True, None],
+  ['TYR%COOH', 23, [], 'peptide', True, None],
 "tyr_chain_all_h_1cx1_v2":
   ['TYR', 21, [], 'peptide', None, None],
 "lys_cterm_all_h_1gkt_v3":
-  ['LYS%COO', 23, [' DXT'], 'peptide', True, None],
+  ['LYS%COOH', 24, [], 'peptide', True, None],
 "ser_cterm_all_h_2jnr_v2":
-  ['SER%COO', 12, [' HXT'], 'peptide', True, None],
+  ['SER%COOH', 13, [], 'peptide', True, None],
 "trp_cterm_all_h_6cmh_v3":
-  ['TRP%COO', 25, [' HXT'], 'peptide', True, None],
+  ['TRP%COOH', 26, [], 'peptide', True, None],
 "phe_cterm_all_h_2jnr_v3":
-  ['PHE%COO', 21, [' HXT'], 'peptide', True, None],
+  ['PHE%COOH', 22, [], 'peptide', True, None],
 "leu_nterm_all_h_1edv_v2":
   ['LEU%NH3', 21, [], 'peptide', True, None],
 "asp_chain_all_h_1kgl_v2":
@@ -395,7 +395,7 @@ expected_results = {
 "lys_nterm_all_h_1gcc_v3":
   ['LYS%NH3', 24, [], 'peptide', True, None],
 "ile_cterm_all_h_1pog_v2":
-  ['ILE%COO', 20, [' HXT'], 'peptide', True, None],
+  ['ILE%COOH', 21, [], 'peptide', True, None],
 "phe_chain_all_h_1hdp_v2":
   ['PHE', 20, [], 'peptide', None, None],
 "pro_chain_all_h_1hdp_v3":
@@ -409,7 +409,7 @@ expected_results = {
 "phe_n_ter_c_ter_with_hydrogens":
   ['PHE%COO%NH3', 23, [], 'peptide', True, None],
 "mse_cterm_all_h_1hz3_v2":
-  ['MSE%COO', 18, [' HXT'], 'peptide', True, None],
+  ['MSE%COOH', 19, [], 'peptide', True, None],
 "mse_nterm_all_h_1qqi_v2":
   ['MSE%NH3', 19, [], 'peptide', True, None],
 "mse_chain_all_h_1ozo_v3":
@@ -421,7 +421,7 @@ expected_results = {
 "mse_nterm_all_h_1qqi_v3":
   ['MSE%NH3', 19, [], 'peptide', True, None],
 "mse_cterm_all_h_1hz3_v3":
-  ['MSE%COO', 18, [' HXT'], 'peptide', True, None],
+  ['MSE%COOH', 19, [], 'peptide', True, None],
 "mse_nterm_all_h_1euw_v2":
   ['MSE%NH3', 19, [], 'peptide', True, None],
 "mse_chain_all_h_1ozo_v2":
