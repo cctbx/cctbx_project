8f3c96a64a9d26254b286dc92f3dd8ae34f339dc
Luc J. Bourhis <luc_j_bourhis@mac.com>
2008-07-07 15:03:25 +0000

    flex_fwd_h support for the smtbx

---
+++ smtbx/libtbx_refresh.py
@@ -0,0 +1,11 @@
+import os
+
+if self.env.is_ready_for_build():
+  message_template = '  Generating C++ files in:\n    "%s"'
+  #flex_fwd.h
+  from smtbx.source_generators import flex_fwd_h
+  target_dir = self.env.under_build("include/smtbx/boost_python")
+  print message_template % target_dir
+  if not os.path.isdir(target_dir):
+    os.makedirs(target_dir)
+  flex_fwd_h.run(target_dir)
