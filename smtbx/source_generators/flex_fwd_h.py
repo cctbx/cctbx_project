35dfbc013d95b9ed712ddc64c9d6ccbbb802254f
Luc J. Bourhis <luc_j_bourhis@mac.com>
2008-07-07 15:03:25 +0000

    flex_fwd_h support for the smtbx

---
+++ smtbx/source_generators/flex_fwd_h.py
@@ -0,0 +1,41 @@
+from scitbx.source_generators.flex_fwd_h import write
+
+this = "cctbx.source_generators.flex_fwd_h"
+
+common_code = """\
+#ifndef SMTBX_BOOST_PYTHON_FLEX_FWD_H
+#define SMTBX_BOOST_PYTHON_FLEX_FWD_H
+
+#include <cctbx/boost_python/flex_fwd.h>
+
+%s#endif // SMTBX_BOOST_PYTHON_FLEX_FWD_H
+"""
+
+full_code = """\
+%s
+
+#if defined(__sgi) && !defined(__GNUC__)
+
+namespace smtbx { namespace boost_python {
+
+  template <typename T>
+  struct flex_fwd
+  {
+  };
+
+  inline void
+  flex_fwd_types()
+  {
+  }
+
+}} // namespace cctbx::boost_python
+
+#endif // defined(__sgi) && !defined(__GNUC__)
+
+"""
+
+def run(target_dir):
+  write(this, target_dir, common_code, full_code)
+
+if (__name__ == "__main__"):
+  run(".")
