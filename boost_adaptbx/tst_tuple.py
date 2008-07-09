a2232b254a0ee80ffcc532afb4d15a4d5a44223f
Luc J. Bourhis <luc_j_bourhis@mac.com>
2008-07-09 10:15:37 +0000

    - read boost.tuple.exercise.__doc__ as a mean of triggering a crash in the previous revision of this code - crash identified as being caused by get_pytype: so disable it for that conversion - test that boost.tuple.exercise.__doc__ is correct for good measure

--- boost_adaptbx/tst_tuple.py
+++ boost_adaptbx/tst_tuple.py
@@ -1,6 +1,16 @@
+import re
 import boost.tuple
 
 def exercise():
+  doc = boost.tuple.exercise.__doc__
+  doc = re.sub("\s+", "", doc)
+  assert re.match("exercise\(\(int\)arg1\)->object:"
+                  "C\+\+signature:"
+                  "boost::tuples::tuple<int,double,"
+                  "(boost::tuples::null_type,)+ boost::tuples::null_type >"
+                  "exercise\(int\)",
+                  doc,
+                  re.X)
   assert boost.tuple.exercise(1) == (2, 0.5)
   assert boost.tuple.exercise(2) == (4, 1)
 
