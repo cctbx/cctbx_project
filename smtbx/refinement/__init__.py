1f699e88b3756ce1a433f9e90b693d8899f9f75a
Luc J. Bourhis <luc_j_bourhis@mac.com>
2008-08-06 00:33:44 +0000

    move the extension of xray.structure to the __init__.py for better visibility

--- smtbx/refinement/__init__.py
+++ smtbx/refinement/__init__.py
@@ -2,3 +2,11 @@ import smtbx.refinement.ext
 import smtbx.refinement.tests
 import smtbx.refinement.minimization
 import smtbx.refinement.barriers
+
+from libtbx import object_oriented_patterns as oop
+import cctbx.xray
+
+class smtbx_structure_enhancements(oop.injector, cctbx.xray.structure):
+
+  def parameter_map(self):
+    return smtbx.refinement.ext.parameter_map(self.scatterers())
