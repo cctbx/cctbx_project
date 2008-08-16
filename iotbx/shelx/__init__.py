3a5f3dcb14523a5ce3b3e08ac5cf87eb97d9581d
Luc J. Bourhis <luc_j_bourhis@mac.com>
2008-08-16 01:16:29 +0000

    - put builders in their own modules - dependencies go only in the direction builders > parsers > lexer - refactor a bit

--- iotbx/shelx/__init__.py
+++ iotbx/shelx/__init__.py
@@ -1,3 +1,4 @@
 from iotbx.shelx.errors import *
-from iotbx.shelx.parsers import *
 from iotbx.shelx.lexer import *
+from iotbx.shelx.parsers import *
+from iotbx.shelx.builders import *
---
+++ iotbx/shelx/builders.py
@@ -0,0 +1,47 @@
+from cctbx import crystal
+from cctbx import xray
+
+from iotbx.shelx import util
+
+class crystal_symmetry_builder(object):
+
+  def make_crystal_symmetry(self, unit_cell, space_group):
+    self.crystal_symmetry = crystal.symmetry(unit_cell=unit_cell,
+                                             space_group=space_group)
+
+
+class crystal_structure_builder(crystal_symmetry_builder,
+                                util.behaviour_of_variable):
+
+  def __init__(self,
+               set_grad_flags=True,
+               min_distance_sym_equiv=0.5):
+    super(crystal_structure_builder, self).__init__()
+    self.set_grad_flags = set_grad_flags
+    self.min_distance_sym_equiv = min_distance_sym_equiv
+
+  def make_structure(self):
+    self.structure = xray.structure(
+      special_position_settings=crystal.special_position_settings(
+        crystal_symmetry=self.crystal_symmetry,
+        min_distance_sym_equiv=self.min_distance_sym_equiv))
+
+  def add_scatterer(self, scatterer, behaviour_of_variable):
+    """ If the parameter set_grad_flags passed to the constructor was True,
+        the scatterer.flags.grad_xxx() will be set to True
+        if the corresponding variables have been found to be refined
+        by the parser using this builder.
+    """
+    if self.set_grad_flags:
+      f = scatterer.flags
+      if behaviour_of_variable[0:3].count(self.fixed) != 3:
+        f.set_grad_site(True)
+      if behaviour_of_variable[3] != self.fixed:
+        f.set_grad_occupancy(True)
+      if f.use_u_iso():
+        if behaviour_of_variable[4] != self.fixed:
+          f.set_grad_u_iso(True)
+      else:
+        if behaviour_of_variable[-6:].count(self.fixed) != 3:
+          f.set_grad_u_aniso(True)
+    self.structure.add_scatterer(scatterer)
