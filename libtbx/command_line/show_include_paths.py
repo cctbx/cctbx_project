3b38658b7af250bff50ad61b9b4994868a6b14af
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2008-08-23 15:34:31 +0000

    libtbx.show_include_paths enhancements: no arguments: show list of keys (e.g. cctbx_common_includes); use os.pathsep; raise error if os.pathsep in include path names

--- libtbx/command_line/show_include_paths.py
+++ libtbx/command_line/show_include_paths.py
@@ -3,6 +3,14 @@ from libtbx.str_utils import show_string
 import libtbx.load_env
 import sys
 
+def read_include_paths(file_name="include_paths"):
+  result = []
+  for line in open(libtbx.env.under_build(file_name)).read().splitlines():
+    flds = line.split(None, 1)
+    assert len(flds) == 2
+    result.append(flds)
+  return result
+
 def run(args):
   remaining_args = []
   prohibit_white_space = False
@@ -11,27 +19,22 @@ def run(args):
       prohibit_white_space = True
     else:
       remaining_args.append(arg)
-  args = remaining_args
-  def process_key(target_key):
-    n_hits = 0
-    for line in open(libtbx.env.under_build(
-                  "include_paths")).read().splitlines():
-      flds = line.split(None, 1)
-      assert len(flds) == 2
-      source_key, path = flds
-      if (target_key is None or source_key == target_key):
-        if (prohibit_white_space and len(path.split()) != 1):
-          raise Sorry(
-            "Include path contains white-space: %s" % show_string(path))
-        print flds[1]
-        n_hits += 1
-    if (n_hits == 0):
-      raise Sorry("No such include path: %s" % show_string(arg))
-  if (len(args) == 0):
-    process_key(target_key=None)
+  include_paths = read_include_paths()
+  if (len(remaining_args) == 0):
+    for key,path in include_paths:
+      print key
   else:
-    for arg in args:
-      process_key(target_key=arg)
+    for target_key in remaining_args:
+      n_hits = 0
+      for key,path in include_paths:
+        if (key == target_key):
+          if (prohibit_white_space and len(path.split()) != 1):
+            raise Sorry(
+              "Include path contains white-space: %s" % show_string(path))
+          print path
+          n_hits += 1
+      if (n_hits == 0):
+        raise Sorry("No such include path: %s" % show_string(arg))
 
 if (__name__ == "__main__"):
   run(sys.argv[1:])
