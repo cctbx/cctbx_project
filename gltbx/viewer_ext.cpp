73dc5e631552021e4a8746af399cb6bb2130093b
Nathaniel Echols <nathaniel.echols@gmail.com>
2008-09-10 00:39:53 +0000

    gltbx viewer: new draw_bonds function

--- gltbx/viewer_ext.cpp
+++ gltbx/viewer_ext.cpp
@@ -3,8 +3,9 @@
 #include <boost/python/args.hpp>
 #include <boost/python/overloads.hpp>
 
+#include <gltbx/error.h>
 #include <scitbx/vec3.h>
-#include <gltbx/include_opengl.h>
+#include <set>
 
 namespace gltbx { namespace viewer {
 
@@ -18,6 +19,8 @@ namespace gltbx { namespace viewer {
     af::const_ref< bool > const& points_visible,
     double cross_radius=0.25)
   {
+    GLTBX_ASSERT(atom_colors.size() == points.size());
+    GLTBX_ASSERT(points_visible.size() == points.size());
     double f = cross_radius;
     for (unsigned i_seq = 0; i_seq < points.size(); i_seq++) {
       if (! points_visible[i_seq]) continue;
@@ -35,6 +38,37 @@ namespace gltbx { namespace viewer {
       glVertex3f(x,y,z+f);
       glEnd();
     }
+    handle_error();
+  }
+
+  void
+  draw_bonds (
+    af::const_ref< scitbx::vec3<double> > const& points,
+    af::const_ref< std::set< unsigned > > const& bonds,
+    af::const_ref< scitbx::vec3<double> > const& atom_colors,
+    af::const_ref< bool > const& bonds_visible)
+  {
+    GLTBX_ASSERT(bonds.size() == points.size());
+    GLTBX_ASSERT(atom_colors.size() == points.size());
+    GLTBX_ASSERT(bonds_visible.size() == points.size());
+    for (unsigned i_seq = 0; i_seq < points.size(); i_seq++) {
+      if (! bonds_visible[i_seq]) continue;
+      scitbx::vec3<double> const& pi = points[i_seq];
+      typedef std::set< unsigned >::const_iterator it;
+      it j_seqs_end = bonds[i_seq].end();
+      for (it j_seq = bonds[i_seq].begin(); j_seq != j_seqs_end; j_seq++) {
+        if (! bonds_visible[*j_seq]) continue;
+        scitbx::vec3<double> const& pj = points[*j_seq];
+        scitbx::vec3<double> const& c = atom_colors[i_seq];
+        glColor3f(c[0], c[1], c[2]);
+        scitbx::vec3<double> midpoint = (pi + pj) / 2.0;
+        glBegin(GL_LINES);
+        glVertex3f(pi[0], pi[1], pi[2]);
+        glVertex3f(midpoint[0], midpoint[1], midpoint[2]);
+        glEnd();
+      }
+    }
+    handle_error();
   }
 
   BOOST_PYTHON_FUNCTION_OVERLOADS(draw_points_overloads, draw_points, 3, 4)
@@ -48,6 +82,11 @@ namespace gltbx { namespace viewer {
       arg_("atom_colors"),
       arg_("points_visible"),
       arg_("cross_radius")=0.25)));
+    def("draw_bonds", draw_bonds, (
+      arg_("points"),
+      arg_("bonds"),
+      arg_("atom_colors"),
+      arg_("bonds_visible")));
   }
 
 }} // namespace gltbx::viewer
