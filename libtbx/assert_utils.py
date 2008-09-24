b66b624683be276c0f994be188183c35def33335
Luc J. Bourhis <luc_j_bourhis@mac.com>
2008-09-24 21:16:06 +0000

    tidy up and assert that exception of given type is raised

--- libtbx/assert_utils.py
+++ libtbx/assert_utils.py
@@ -1,12 +1,30 @@
 """ Functions useful to write assertions, especially preconditions """
 
 def is_numeric(x):
-  try:
-    x+1.
-    return True
+  try: x+1.
   except KeyboardInterrupt: raise
-  except:
-    return False
+  except: return False
+  else: return True
+
+def is_string(s):
+  try: s+""
+  except KeyboardInterrupt: raise
+  except: return False
+  else: return True
+
+class shall_raise(object):
+
+  def __init__(self, func, *exceptions):
+    self.func = func
+    self.exceptions = exceptions
+
+  def __call__(self, *args, **kwds):
+    try: self.func(*args, **kwds)
+    except KeyboardInterrupt: raise
+    except:
+      import sys
+      return sys.exc_info()[0] in self.exceptions
+    else: return False
 
 
 if __name__ == '__main__':
@@ -23,4 +41,10 @@ if __name__ == '__main__':
                          "some tests were skipped"
   else:
     assert is_numeric(flex.double((1,2,3)))
+
+  def f(x,y,z):
+    z/(x-y)
+  assert shall_raise(f, ZeroDivisionError)(1,1,1)
+  assert not shall_raise(f, ZeroDivisionError)(x=1, y=0, z=0)
+
   print 'OK'
