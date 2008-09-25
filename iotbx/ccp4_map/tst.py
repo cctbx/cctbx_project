e7c6ff21dbeb85db2ecb62390cde05197c43a8ac
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2008-09-25 17:24:18 +0000

    ciotbx.ccp4_map: reading code based on clipper/ccp4/ccp4_map_io.cpp (rough initial version, needs to be cleaned up)

--- iotbx/ccp4_map/tst.py
+++ iotbx/ccp4_map/tst.py
@@ -1,24 +1,28 @@
-from cctbx.array_family import flex
+from cctbx import maptbx
 import iotbx.ccp4_map
+from libtbx.test_utils import approx_equal
 import sys
 
 def run(args):
   for file_name in args:
     print file_name
     m = iotbx.ccp4_map.map_reader(file_name=file_name)
-    d = m.data
-    print d
-    print flex.min(d), flex.max(d)
+    print "header_min: ", m.header_min
+    print "header_max: ", m.header_min
+    print "header_mean:", m.header_mean
+    print "header_rms: ", m.header_rms
+    map_stats = maptbx.statistics(m.data)
+    assert approx_equal(map_stats.min(), m.header_min)
+    assert approx_equal(map_stats.max(), m.header_max)
+    assert approx_equal(map_stats.mean(), m.header_mean)
+    if (m.header_rms != 0):
+      assert approx_equal(map_stats.sigma(), m.header_rms)
     print m.space_group
     print m.unit_cell
-    print m.map_min
-    print m.map_max
     print m.map_grid
     print m.map_origin
     print m.axes_order
     print m.map_dim
-    print m.average
-    print m.standard_deviation
 
 if (__name__ == "__main__"):
   run(sys.argv[1:])
