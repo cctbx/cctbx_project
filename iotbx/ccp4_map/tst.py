1eff4d9af69a64580c8c8eaa83a46942fbbc2744
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2008-09-25 19:49:59 +0000

    iotbx.ccp4_map: fairly complete reader

--- iotbx/ccp4_map/tst.py
+++ iotbx/ccp4_map/tst.py
@@ -1,28 +1,47 @@
-from cctbx import maptbx
 import iotbx.ccp4_map
+from cctbx import maptbx
 from libtbx.test_utils import approx_equal
+from libtbx.utils import format_cpu_times
+import libtbx.load_env
 import sys
 
+def exercise_with_tst_input_map():
+  file_name = libtbx.env.under_dist(
+    module_name="iotbx",
+    path="ccp4_map/tst_input.map")
+  m = iotbx.ccp4_map.map_reader(file_name=file_name)
+  assert approx_equal(m.header_min, -0.422722190619)
+  assert approx_equal(m.header_max, 0.335603952408)
+  assert approx_equal(m.header_mean, 0)
+  assert approx_equal(m.header_rms, 0.140116646886)
+  assert approx_equal(m.unit_cell_parameters, (
+    82.095001220703125, 37.453998565673828, 69.636001586914062,
+    90.0, 101.47599792480469, 90.0))
+  assert m.space_group_number == 5
+  assert m.data.origin() == (0, 0, 0)
+  assert m.data.all() == (8, 16, 16)
+  assert not m.data.is_padded()
+
 def run(args):
+  exercise_with_tst_input_map()
   for file_name in args:
     print file_name
     m = iotbx.ccp4_map.map_reader(file_name=file_name)
     print "header_min: ", m.header_min
-    print "header_max: ", m.header_min
+    print "header_max: ", m.header_max
     print "header_mean:", m.header_mean
     print "header_rms: ", m.header_rms
+    print "unit cell parameters:", m.unit_cell_parameters
+    print "space group number:  ", m.space_group_number
+    print "map origin:", m.data.origin()
+    print "map grid:  ", m.data.all()
     map_stats = maptbx.statistics(m.data)
     assert approx_equal(map_stats.min(), m.header_min)
     assert approx_equal(map_stats.max(), m.header_max)
     assert approx_equal(map_stats.mean(), m.header_mean)
     if (m.header_rms != 0):
       assert approx_equal(map_stats.sigma(), m.header_rms)
-    print m.space_group
-    print m.unit_cell
-    print m.map_grid
-    print m.map_origin
-    print m.axes_order
-    print m.map_dim
+  print format_cpu_times()
 
 if (__name__ == "__main__"):
   run(sys.argv[1:])
