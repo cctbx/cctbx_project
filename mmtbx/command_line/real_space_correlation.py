deeb281e97e994c3fcf7fd3ba2b3f7e1899153ec
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2008-10-27 05:41:08 +0000

    phenix.real_space_correlation generated directly from mmtbx

--- mmtbx/command_line/real_space_correlation.py
+++ mmtbx/command_line/real_space_correlation.py
@@ -1,6 +1,9 @@
+# LIBTBX_SET_DISPATCHER_NAME phenix.real_space_correlation
+
 from mmtbx import real_space_correlation
 import sys
 
 if(__name__ == "__main__"):
-  real_space_correlation.cmd_run(args         = sys.argv[1:],
-                                 command_name = "mmtbx.real_space_correlation")
+  real_space_correlation.cmd_run(
+    args         = sys.argv[1:],
+    command_name = "phenix.real_space_correlation")
