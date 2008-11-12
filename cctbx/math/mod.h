07d647a7b520bc79c94aa4e12ed7cbc30853129f
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2008-11-12 23:57:58 +0000

    cctbx/math/mod.h: enable softer transition (to restore labelit build): using scitbx::math::mod_positive; etc.

--- cctbx/math/mod.h
+++ cctbx/math/mod.h
@@ -1,5 +1,22 @@
-#error This header has been deprecated: \
-       please #include <scitbx/math/modulo.h> instead \
-       and don't forget to replace math::mod_positive \
-       or cctbx::math::mod_positive to scitbx::math::mod_positive \
-       and so on for the other 2 functions defined in that header
+//! backward compatibility 2008-11-12 (whole file)
+/*! This header has been deprecated:
+    Please #include <scitbx/math/modulo.h> instead
+    and don't forget to replace math::mod_positive or
+    cctbx::math::mod_positive with scitbx::math::mod_positive
+    and similarly for other functions.
+ */
+
+#ifndef CCTBX_MATH_MOD_H
+#define CCTBX_MATH_MOD_H
+
+#include <scitbx/math/modulo.h>
+
+namespace cctbx { namespace math {
+
+  using scitbx::math::mod_positive;
+  using scitbx::math::mod_short;
+  using scitbx::math::fmod_short;
+
+}} // namespace cctbx::math
+
+#endif // CCTBX_MATH_MOD_H
