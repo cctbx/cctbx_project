fbfec3d27f87a32b687e422d123b7674d2a1c57d
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2008-11-15 02:35:11 +0000

    libtbx/thread_utils.py: support first_callback and .start_and_wait_for_first_callback(); new gltbx/wx_motion_on_tab.py example

---
+++ gltbx/wx_motion_on_tab.py
@@ -0,0 +1,98 @@
+"""\
+Example:
+  - use results from a function with callback (motion() in this example)
+  - return from callback only after an event in the GUI (Tab-key)
+"""
+
+from gltbx import wx_viewer
+from libtbx.thread_utils import thread_with_callback_and_wait
+from scitbx.rigid_body_dynamics.free_motion_reference_impl import \
+  create_triangle_with_center_of_mass_at_origin
+from scitbx.math import minimum_covering_sphere, sphere_3d
+from scitbx.array_family import flex
+from scitbx import matrix
+import wx
+import sys
+
+class viewer(wx_viewer.show_points_and_lines_mixin):
+
+  def __init__(self, *args, **kwds):
+    super(viewer, self).__init__(*args, **kwds)
+
+  def set_points_and_lines(self):
+    self.child_thread = thread_with_callback_and_wait(
+      run = motion,
+      callback = self.motion_callback,
+      first_callback = self.first_motion_callback)
+    self.child_thread.start_and_wait_for_first_callback()
+
+  def first_motion_callback(self, points):
+    self.points = flex.vec3_double(points)
+    self.labels = ["A", "B", "C"]
+    def add_line(i, j, color):
+      line = (i,j)
+      self.line_i_seqs.append(line)
+      self.line_colors[line] = color
+    add_line(0, 1, (1,0,0))
+    add_line(1, 2, (0,1,0))
+    add_line(2, 0, (0,0,1))
+    mcs = minimum_covering_sphere(self.points, epsilon=1.e-2)
+    self.minimum_covering_sphere = sphere_3d(
+      center=matrix.col(mcs.center())+matrix.col((0.5,0.5,0.5)),
+      radius=mcs.radius()*2.0)
+    self.flag_show_minimum_covering_sphere = False
+    self.flag_show_rotation_center = False
+
+  def motion_callback(self, points):
+    wx.PostEvent(self, wx_viewer.ViewerUpdateEvent(points))
+
+  def OnUpdate (self, event) :
+    for i in xrange(len(event.data)):
+      self.points[i] = event.data[i]
+    self.labels_display_list = None
+    self.lines_display_list = None
+    self.points_display_list = None
+    self.OnRedraw()
+
+  def tab_callback(self, shift_down=False, control_down=False):
+    self.child_thread.resume()
+
+  def CleanupBeforeFrameClose(self):
+    self.child_thread.resume(last_iteration=True)
+
+class App(wx_viewer.App):
+
+  def __init__(self, args):
+    assert len(args) == 0
+    super(App, self).__init__(title="Motion Viewer")
+
+  def init_view_objects(self):
+    box = wx.BoxSizer(wx.VERTICAL)
+    self.view_objects = viewer(self.frame, size=(600,600))
+    self.view_objects.set_points_and_lines()
+    box.Add(self.view_objects, wx.EXPAND, wx.EXPAND)
+    self.frame.SetSizer(box)
+    box.SetSizeHints(self.frame)
+
+def run(args):
+  App(args).MainLoop()
+
+def motion(n_steps=20, callback=None):
+  points = create_triangle_with_center_of_mass_at_origin()
+  shift = matrix.col((0.1,0.1,0.1))
+  i_step = 0
+  while (callback is not None or i_step != n_steps):
+    if (callback is None):
+      print [point.elems for point in points]
+    else:
+      status = callback(points)
+      if status == False :
+        break
+    for i in xrange(len(points)):
+      points[i] += shift
+    i_step += 1
+    if (i_step % 10 == 0):
+      shift = -shift
+
+if (__name__ == "__main__"):
+  run(sys.argv[1:])
--- libtbx/thread_utils.py
+++ libtbx/thread_utils.py
@@ -1,55 +1,94 @@
-
-import time
-import threading
 import Queue
+import threading
 
-class thread_with_callback_and_wait (threading.Thread) :
-  def __init__ (self, run_function, callback_function, run_args=(),
-      run_kwds={}) :
-    self.f = run_function
-    self.args = run_args
-    self.kwds = run_kwds
-    self.kwds['callback'] = self._callback
-    self.cb = callback_function
-    self.q = Queue.Queue(0)
-    self._aborted = False
+class thread_with_callback_and_wait(threading.Thread):
+
+  def __init__ (self,
+          run,
+          callback,
+          first_callback=None,
+          run_args=(),
+          run_kwds={}) :
+    self._run = run
+    self._run_args = run_args
+    self._run_kwds = dict(run_kwds) # copy, to avoid side effects
+    self._run_kwds['callback'] = self._callback_proxy
+    self._callback = callback
+    self._first_callback = first_callback
+    self._caller_wait = Queue.Queue(0)
+    self._queue = Queue.Queue(0)
     threading.Thread.__init__(self)
 
-  def run (self) :
-    self.f(*self.args, **self.kwds)
+  def run(self):
+    self._run(*self._run_args, **self._run_kwds)
 
-  def _callback (self, *args, **kwds) :
-    cb_status = self.cb(*args, **kwds)
-    if cb_status is not None :
-      return cb_status
-    while True :
-      go_ahead = self.q.get()
-      if go_ahead :
-        return True
-      else :
-        return False
+  def start_and_wait_for_first_callback(self):
+    self.start()
+    self._caller_wait.get()
 
-  def resume (self) :
-    self.q.put(True)
+  def _callback_proxy(self, *args, **kwds):
+    if (self._first_callback is not None):
+      result = self._first_callback(*args, **kwds)
+      self._first_callback = None
+      self._caller_wait.put(None)
+    else:
+      result = self._callback(*args, **kwds)
+    if (result == False):
+      return False
+    last_iteration = self._queue.get()
+    if (last_iteration):
+      return False
+    return result
 
-  def abort (self) :
-    self.q.put(False)
+  def resume(self, last_iteration=False):
+    if (self.is_alive()):
+      self._queue.put(last_iteration)
+    return self
 
-def thread_test() :
-  t = thread_with_callback_and_wait(tst_run, tst_cb)
-  t.start()
-  return t
+def exercise() :
 
-def tst_run (callback=None) :
-  for i in xrange(0, 10) :
-    print "run: %d" % i
-    if callback is not None :
-      status = callback(i)
-      if status == False :
-        break
-  print "done."
-  return True
+  def first_callback(i):
+    collected.append(-i*10)
 
-def tst_cb (i) :
-  print "  callback: %d" % i
-  return True
+  def callback(i):
+    collected.append(i*10)
+    return (i < 5)
+
+  def run(n=3, callback=None):
+    i = 1
+    while (callback is not None or i <= n):
+      collected.append(i)
+      if (callback is not None):
+        status = callback(i)
+        if (status == False):
+          break
+      i += 1
+
+  collected = []
+  run()
+  assert collected == [1,2,3]
+
+  for callback1,expected in [
+        (None, [1,10]),
+        (first_callback, [1,-10])]:
+    for n_resume in xrange(7):
+      collected = []
+      t = thread_with_callback_and_wait(
+        run=run,
+        callback=callback,
+        first_callback=callback1)
+      if (callback1 is None):
+        t.start()
+      else:
+        t.start_and_wait_for_first_callback()
+      for i in xrange(n_resume):
+        t.resume()
+      t.resume(last_iteration=True).join()
+      assert collected == expected
+      if (n_resume < 4):
+        expected.extend([n_resume+2, (n_resume+2)*10])
+
+  print "OK"
+
+if (__name__ == "__main__"):
+  exercise()
