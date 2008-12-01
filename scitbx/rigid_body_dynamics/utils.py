c2e56a086968ad0954f0857471f38ab3b102e9f0
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2008-12-01 19:32:24 +0000

    scitbx/rigid_body_dynamics: e_kin_from_model code moved from tst_joint_lib.py to utils.py

--- scitbx/rigid_body_dynamics/utils.py
+++ scitbx/rigid_body_dynamics/utils.py
@@ -66,3 +66,26 @@ class featherstone_system_model(object):
       model.Ttree.append(Ttree)
       model.Xtree.append(T_as_X(Ttree))
       model.I.append(B.I)
+
+def spatial_velocities_from_model(model, q, qd):
+  result = [None] * model.NB
+  Xup = [None] * model.NB
+  for i in xrange(model.NB):
+    XJ, S = featherstone.jcalc( model.pitch[i], q[i], qd[i] )
+    if (S is None):
+      vJ = qd[i]
+    else:
+      vJ = S*qd[i]
+    Xup[i] = XJ * model.Xtree[i]
+    if model.parent[i] == -1:
+      result[i] = vJ
+    else:
+      result[i] = Xup[i]*result[model.parent[i]] + vJ
+  return result
+
+def e_kin_from_model(model, q, qd):
+  result = 0
+  for I_spatial,v_spatial in zip(
+        model.I, spatial_velocities_from_model(model, q, qd)):
+    result += kinetic_energy(I_spatial=I_spatial, v_spatial=v_spatial)
+  return result
