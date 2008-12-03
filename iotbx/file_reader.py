02853286c8022a5f154e70f8254ef3702d945d6e
Nathaniel Echols <nathaniel.echols@gmail.com>
2008-12-03 01:26:29 +0000

    updated iotbx.file_reader

--- iotbx/file_reader.py
+++ iotbx/file_reader.py
@@ -6,6 +6,8 @@ from iotbx.reflection_file_reader import any_reflection_file
 from iotbx.reflection_file_utils import reflection_file_server
 from libtbx.utils import Sorry
 
+standard_file_types = ["hkl", "map", "pdb", "cif", "phil", "seq"]
+
 standard_file_extensions = {
   'pdb'  : ["pdb", "ent"],
   'hkl'  : ["mtz", "hkl", "sca", "cns", "xplor"],
@@ -24,16 +26,6 @@ standard_file_descriptions = {
   'phil' : "Parameters"
 }
 
-def get_file_extension (file_name) :
-  base_file = file_name
-
-  if len(file_name) < 3 :
-    return ""
-  if file_name[-3:] == ".gz" :
-    base_file = file_name[:-3]
-  file_ext = re.sub(".*\.", "", base_file)
-  return file_ext
-
 class any_file (object) :
   def __init__ (
       self,
@@ -51,79 +43,63 @@ class any_file (object) :
     self.file_size = os.path.getsize(file_name)
     self.get_processed_file = get_processed_file
 
-    file_ext = get_file_extension(file_name)
+    (file_base, file_ext) = os.path.splitext(file_name)
     try :
-      if file_ext in standard_file_extensions['pdb'] :
-        self.try_as_pdb()
-      elif file_ext in standard_file_extensions['hkl'] :
-        self.try_as_hkl()
-      elif file_ext in standard_file_extensions['phil'] :
-        self.try_as_phil()
-      elif file_ext in standard_file_extensions['seq'] :
-        self.try_as_seq()
-      elif file_ext in standard_file_extensions['cif'] :
-        self.try_as_cif()
-    except Exception :
-      pass
+      for file_type in standard_file_types :
+        if file_ext[1:] in standard_file_extensions[file_type] :
+          read_method = getattr(self, "try_as_%s" % file_type)
+          read_method()
+          break
+    except Exception, e :
+      self.file_type = None
 
-    if not self.file_type :
+    if self.file_type is None :
       self.try_all_types()
 
   def try_as_pdb (self) :
-    if self.file_type != None : return False
+    if self.file_type is not None : return False
     if is_pdb_file(self.file_name) :
       self.file_type = "pdb"
-      if self.get_processed_file :
-        pass
 
   def try_as_hkl (self) :
-    if self.file_type != None : return False
-    try :
-      hkl_file = any_reflection_file(self.file_name)
-      self.file_type = "hkl"
-      self.file_object = hkl_file
-      self.file_server = reflection_file_server(
-        crystal_symmetry=None,
-        force_symmetry=True,
-        reflection_files=[hkl_file],
-        err=sys.stderr)
-    except Exception :
-      pass
+    if self.file_type is not None : return False
+    hkl_file = any_reflection_file(self.file_name)
+    self.file_server = reflection_file_server(
+      crystal_symmetry=None,
+      force_symmetry=True,
+      reflection_files=[hkl_file],
+      err=sys.stderr)
+    self.file_type = "hkl"
+    self.file_object = hkl_file
 
   def try_as_cif (self) :
-    if self.file_type != None : return False
-    try :
-      cif_object = server.read_cif(file_name=self.file_name)
-      self.file_type = "cif"
-      self.file_object = cif_object
-    except Exception :
-      pass
+    if self.file_type is not None : return False
+    cif_object = server.read_cif(file_name=self.file_name)
+    self.file_type = "cif"
+    self.file_object = cif_object
 
   def try_as_phil (self) :
-    if self.file_type != None : return False
-    try :
-      phil_object = iotbx.phil.parse(file_name=self.file_name)
-      self.file_type = "phil"
-      self.file_object = phil_object
-    except Exception :
-      pass
+    if self.file_type is not None : return False
+    phil_object = iotbx.phil.parse(file_name=self.file_name)
+    self.file_type = "phil"
+    self.file_object = phil_object
 
   def try_as_seq (self) :
-    if self.file_type != None : return False
-    pass
+    if self.file_type is not None : return False
+    self.file_type = "seq"
 
   def try_as_map (self) :
-    if self.file_type != None : return False
+    if self.file_type is not None : return False
     pass
 
   def try_all_types (self) :
-    self.try_as_pdb()
-    self.try_as_hkl()
-    self.try_as_cif()
-    self.try_as_phil()
-    self.try_as_seq()
-    self.try_as_map()
-
+    for filetype in standard_file_types :
+      read_method = getattr(self, "try_as_%s" % filetype)
+      try :
+        read_method()
+      except Exception, e :
+        pass
+      
   def file_info (self, show_file_size=True) :
     file_size_str = ""
     if show_file_size :
