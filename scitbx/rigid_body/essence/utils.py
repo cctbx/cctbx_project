02597bf299e8afb3243beba4391d2c3584a4f27e
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2008-12-18 06:22:32 +0000

    new scitbx/rigid_body/essence: simplified copies of some files in proto directory

---
+++ scitbx/rigid_body/essence/utils.py
@@ -0,0 +1,91 @@
+from scitbx.rigid_body.essence import featherstone
+from scitbx import matrix
+
+def center_of_mass_from_sites(sites):
+  assert len(sites) != 0
+  result = matrix.col((0,0,0))
+  for site in sites:
+    result += site
+  result /= len(sites)
+  return result
+
+def inertia_from_sites(sites, pivot):
+  m = [0] * 9
+  for site in sites:
+    x,y,z = site - pivot
+    m[0] += y*y+z*z
+    m[4] += x*x+z*z
+    m[8] += x*x+y*y
+    m[1] -= x*y
+    m[2] -= x*z
+    m[5] -= y*z
+  m[3] = m[1]
+  m[6] = m[2]
+  m[7] = m[5]
+  return matrix.sqr(m)
+
+def spatial_inertia_from_sites(
+      sites,
+      mass=None,
+      center_of_mass=None,
+      alignment_T=None):
+  if (mass is None):
+    mass = len(sites)
+  if (center_of_mass is None):
+    center_of_mass = center_of_mass_from_sites(sites=sites)
+  inertia = inertia_from_sites(sites=sites, pivot=center_of_mass)
+  if (alignment_T is not None):
+    center_of_mass = alignment_T * center_of_mass
+    inertia = alignment_T.r * inertia * alignment_T.r.transpose()
+  return featherstone.mcI(m=mass, c=center_of_mass, I=inertia)
+
+def kinetic_energy(I_spatial, v_spatial):
+  "RBDA Eq. 2.67"
+  return 0.5 * v_spatial.dot(I_spatial * v_spatial)
+
+def T_as_X(Tps):
+  return featherstone.Xrot(Tps.r) \
+       * featherstone.Xtrans(-Tps.r.transpose() * Tps.t)
+
+class featherstone_system_model(object):
+
+  def __init__(model, bodies):
+    model.NB = len(bodies)
+    model.pitch = []
+    model.parent =[]
+    model.Ttree = []
+    model.Xtree = []
+    model.I = []
+    for B in bodies:
+      model.pitch.append(B.J)
+      model.parent.append(B.parent)
+      if (B.parent == -1):
+        Ttree = B.A.T0b
+      else:
+        Ttree = B.A.T0b * bodies[B.parent].A.Tb0
+      model.Ttree.append(Ttree)
+      model.Xtree.append(T_as_X(Ttree))
+      model.I.append(B.I)
+
+def spatial_velocities_from_model(model, q, qd):
+  result = [None] * model.NB
+  Xup = [None] * model.NB
+  for i in xrange(model.NB):
+    XJ, S = featherstone.jcalc( model.pitch[i], q[i] )
+    if (S is None):
+      vJ = qd[i]
+    else:
+      vJ = S*qd[i]
+    Xup[i] = XJ * model.Xtree[i]
+    if model.parent[i] == -1:
+      result[i] = vJ
+    else:
+      result[i] = Xup[i]*result[model.parent[i]] + vJ
+  return result
+
+def e_kin_from_model(model, q, qd):
+  result = 0
+  for I_spatial,v_spatial in zip(
+        model.I, spatial_velocities_from_model(model, q, qd)):
+    result += kinetic_energy(I_spatial=I_spatial, v_spatial=v_spatial)
+  return result
---
+++ scitbx/rigid_body/essence/wx_molecules.py
@@ -0,0 +1,118 @@
+from scitbx.rigid_body.essence import tst_molecules
+from scitbx.math import minimum_covering_sphere, sphere_3d
+from scitbx.array_family import flex
+from gltbx import wx_viewer
+from libtbx.utils import Usage
+import wx
+import sys
+
+class viewer(wx_viewer.show_points_and_lines_mixin):
+
+  def __init__(self, *args, **kwds):
+    super(viewer, self).__init__(*args, **kwds)
+
+  def set_points(self):
+    self.points.clear()
+    for B,AJA in zip(self.sim.bodies, self.sim.AJA_accu):
+      for s in B.sites:
+        self.points.append(AJA * s)
+    self.labels_display_list = None
+    self.lines_display_list = None
+    self.points_display_list = None
+
+  def set_points_and_lines(self, simulation_factory_index, n_zigzag):
+    if (simulation_factory_index == 0):
+      self.sim = tst_molecules.simulation_zigzag(NB=n_zigzag)
+    else:
+      self.sim = tst_molecules.simulation_factories[simulation_factory_index]()
+    self.points = flex.vec3_double()
+    self.set_points()
+    def add_line(i, j, color):
+      line = (i,j)
+      self.line_i_seqs.append(line)
+      self.line_colors[line] = color
+    self.labels = []
+    B_off = []
+    for B in self.sim.bodies:
+      B_off.append(len(self.labels))
+      self.labels.extend(B.labels)
+      def add_off(i):
+        if (i < 0): return B_off[B.parent+1] + i
+        else:       return B_off[-1] + i
+      for bond in B.bonds:
+        i,j = [add_off(b) for b in bond]
+        add_line(i, j, (1,0,0))
+    mcs = minimum_covering_sphere(self.points, epsilon=1.e-2)
+    self.minimum_covering_sphere = sphere_3d(
+      center=mcs.center(), radius=mcs.radius()*1.3)
+    self.flag_show_minimum_covering_sphere = False
+    self.flag_show_rotation_center = False
+    self.steps_per_tab = 1
+    self.show_key_stroke_help()
+
+  def show_key_stroke_help(self):
+    print "Press and hold Tab key to run the simulation."
+    print "Press Shift-Tab to increase speed."
+    print "Press Ctrl-Tab  to decrease speed."
+    print "Press M for minimization."
+
+  def process_key_stroke(self, key):
+    if (key == ord("M")):
+      return self.minimization()
+    print "No action for this key stroke."
+    self.show_key_stroke_help()
+
+  def tab_callback(self, shift_down=False, control_down=False):
+    if (shift_down or control_down):
+      if (shift_down):
+        self.steps_per_tab = min(256, self.steps_per_tab * 2)
+      else:
+        self.steps_per_tab = max(1, self.steps_per_tab // 2)
+      print "Steps per Tab:", self.steps_per_tab
+      return
+    self.sim.dynamics_step(delta_t=0.05)
+    self.set_points()
+    self.OnRedraw()
+
+  def minimization(self):
+    print "Minimization:"
+    print "  start e_pot:", self.sim.e_pot
+    self.sim.minimization(
+      max_iterations=10,
+      callback_after_step=self.minimization_callback)
+    print "  final e_pot:", self.sim.e_pot
+
+  def minimization_callback(self, minimizer):
+    print "        e_pot:", self.sim.e_pot
+    self.set_points()
+    self.OnRedraw()
+
+class App(wx_viewer.App):
+
+  def __init__(self, args):
+    n = len(tst_molecules.simulation_factories)
+    if (len(args) not in [1,2]):
+      raise Usage("scitbx.python wx_molecules.py sim_index [n_zigzag]")
+    self.simulation_factory_index = int(args[0])
+    if (len(args) > 1):
+      self.n_zigzag = int(args[1])
+    else:
+      self.n_zigzag = 10
+    assert 0 <= self.simulation_factory_index < n
+    super(App, self).__init__(title="wx_molecules")
+
+  def init_view_objects(self):
+    box = wx.BoxSizer(wx.VERTICAL)
+    self.view_objects = viewer(self.frame, size=(600,600))
+    self.view_objects.set_points_and_lines(
+      simulation_factory_index=self.simulation_factory_index,
+      n_zigzag=self.n_zigzag)
+    box.Add(self.view_objects, wx.EXPAND, wx.EXPAND)
+    self.frame.SetSizer(box)
+    box.SetSizeHints(self.frame)
+
+def run(args):
+  App(args).MainLoop()
+
+if (__name__ == "__main__"):
+  run(sys.argv[1:])
