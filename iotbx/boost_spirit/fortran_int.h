1f71595bc5f39204f9685eba0e59553fc2e02816
Luc J. Bourhis <luc_j_bourhis@mac.com>
2008-12-19 13:32:37 +0000

    - I had dug out a buggy version - templatize with the numerical types to be more like the standard Boost.Spirit parsers

--- iotbx/boost_spirit/fortran_int.h
+++ iotbx/boost_spirit/fortran_int.h
@@ -2,52 +2,32 @@
 #define IOTBX_BOOST_SPIRIT_FORTRAN_INT_H
 
 #include <boost/spirit/include/classic_parser.hpp>
-#include <boost/spirit/include/classic_scanner.hpp>
-#include <boost/spirit/include/classic_actions.hpp>
+#include <iotbx/boost_spirit/fortran_util.h>
 
 namespace iotbx { namespace boost_spirit {
 
 using namespace boost::spirit::classic;
 
-template <int Width>
-struct fortran_int_parser : parser<fortran_int_parser<Width> >
+template <typename IntegralType, int Width>
+struct fortran_int_parser : parser<fortran_int_parser<IntegralType, Width> >
 {
   BOOST_STATIC_ASSERT(Width > 0);
 
-  typedef fortran_int_parser<Width> self_t;
+  typedef fortran_int_parser<IntegralType, Width> self_t;
 
   template <typename ScannerType>
   struct result
   {
-    typedef typename match_result<ScannerType, int>::type type;
+    typedef typename match_result<ScannerType, IntegralType>::type type;
   };
 
   template <typename ScannerType>
   typename parser_result<self_t, ScannerType>::type
   parse(ScannerType const &scan) const {
     typename ScannerType::iterator_t first = scan.first;
-    int n = 0;
-    for(; n < Width; ++n, ++scan) {
-      if (scan.at_end()) return scan.no_match();
-      if (*scan != ' ') break;
-    }
-    if (n == Width) return scan.no_match();
-    int sign = +1;
-    char ch = *scan;
-    if (ch == '-') sign = -1;
-    if (ch == '-' || ch == '+') {
-        if (++n == Width) return scan.no_match();
-        ++scan;
-        if (scan.at_end()) return scan.no_match();
-    }
-    int res = 0;
-    for (; n < Width; ++n, ++scan) {
-      char ch = *scan;
-      if (ch < '0' || ch > '9') return scan.no_match();
-      res = 10*res + int(ch - '0');
-    }
-    if (sign == -1) res = -res;
-    return scan.create_match(Width, res, first, first + Width);
+    fortran_signed_int_extractor<IntegralType, Width> extractor(scan);
+    if (extractor.failed) return scan.no_match();
+    return scan.create_match(Width, extractor.value, first, first + Width);
   }
 };
 
--- iotbx/boost_spirit/fortran_real_fixed.h
+++ iotbx/boost_spirit/fortran_real_fixed.h
@@ -1,23 +1,24 @@
 #ifndef IOTBX_BOOST_SPIRIT_FORTRAN_REAL_FIXED_H
 #define IOTBX_BOOST_SPIRIT_FORTRAN_REAL_FIXED_H
 
-#include <iotbx/boost_spirit/fortran_int.h>
 #include <boost/spirit/include/classic_numerics.hpp>
+#include <iotbx/boost_spirit/fortran_util.h>
 
 namespace iotbx { namespace boost_spirit {
 
 using namespace boost::spirit;
 
-template <int Width, int FracDigits>
-struct fortran_real_fixed : parser<fortran_real_fixed<Width, FracDigits> >
+template <typename FloatingPointType, int Width, int FracDigits>
+struct fortran_real_fixed_parser
+  : parser<fortran_real_fixed_parser<FloatingPointType, Width, FracDigits> >
 {
   BOOST_STATIC_ASSERT(Width > 0);
   static const int WholeDigits = Width - FracDigits - 1; // dot always printed
   BOOST_STATIC_ASSERT(WholeDigits > 0); // Never .123 style
 
-  typedef fortran_real_fixed<Width, FracDigits> self_t;
+  typedef fortran_real_fixed_parser<FloatingPointType, Width, FracDigits>
+          self_t;
 
-  typedef fortran_int_parser<WholeDigits> whole_part_parser;
   typedef uint_parser<unsigned, /*Radix=*/10,
                       /*MinDigits=*/FracDigits, /*MaxDigits=*/FracDigits>
           frac_part_parser;
@@ -25,30 +26,29 @@ struct fortran_real_fixed : parser<fortran_real_fixed<Width, FracDigits> >
   template <typename ScannerType>
   struct result
   {
-    typedef typename match_result<ScannerType, double>::type type;
+    typedef typename match_result<ScannerType, FloatingPointType>::type type;
   };
 
   template <typename ScannerType>
   typename parser_result<self_t, ScannerType>::type
   parse(ScannerType const &scan) const {
     typename ScannerType::iterator_t first = scan.first;
-    whole_part_parser whole_part_p;
-    typename parser_result<self_t, ScannerType>::type
-      whole_part_result = whole_part_p.parse(scan);
-    if (!whole_part_result) return whole_part_result;
-    double whole_part = double(whole_part_result.value());
+    fortran_signed_int_extractor<int, WholeDigits> whole_part_extractor(scan);
+    if (whole_part_extractor.failed) return scan.no_match();
+    FloatingPointType whole_part = whole_part_extractor.value;
+    bool negative = whole_part_extractor.negative;
 
     if (*scan != '.') return scan.no_match();
     ++scan;
 
     frac_part_parser frac_part_p;
-    typename parser_result<self_t, ScannerType>::type
+    typename parser_result<frac_part_parser, ScannerType>::type
       frac_part_result = frac_part_p.parse(scan);
-    if (!frac_part_result) return frac_part_result;
-    double frac_part = double(frac_part_result.value())
-                        * std::pow(10., -FracDigits);
-    double res = whole_part >= 0 ? whole_part + frac_part
-                                 : whole_part - frac_part;
+    if (!frac_part_result) return scan.no_match();
+    FloatingPointType frac_part = FloatingPointType(frac_part_result.value())
+                                    * std::pow(10., -FracDigits);
+    FloatingPointType res = negative ? whole_part - frac_part
+                                     : whole_part + frac_part;
     return scan.create_match(Width, res, first, first + Width);
   }
 };
