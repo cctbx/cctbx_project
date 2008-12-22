4a049c3e0241b0f9653141513da43f1524fe1c30
Luc J. Bourhis <luc_j_bourhis@mac.com>
2008-12-22 08:57:10 +0000

    - a bit more archaeology and polishing to dig out a more useful version: one which relies on the input editing format so as to be compatible with a FORTRAN program doing "read" with a given format (previous version relied on the output editing format, i.e. write) - note for myself: summer 2003, project qcd34 - put everything under one single header

--- iotbx/boost_spirit/tst_parsers.cpp
+++ iotbx/boost_spirit/tst_parsers.cpp
@@ -1,8 +1,9 @@
-#include <iotbx/boost_spirit/fortran_int.h>
-#include <iotbx/boost_spirit/fortran_real_fixed.h>
+#include <iotbx/boost_spirit/fortran_numerics.h>
+
 #include <boost/spirit/include/classic_core.hpp>
 #include <boost/spirit/include/classic_assign_actor.hpp>
 #include <boost/spirit/include/classic_push_back_actor.hpp>
+
 #include <iotbx/error.h>
 #include <boost/optional.hpp>
 #include <string>
@@ -11,32 +12,26 @@
 
 void exercise_fortran_int() {
   using namespace iotbx::boost_spirit;
-  fortran_int_parser<int, 4> i4_p;
+  fortran_int_parser<int, /*Width=*/4> i4_p;
   std::vector<std::string> input;
   typedef boost::optional<int> opt_t;
   std::vector<opt_t> output;
   input.push_back("   1");  output.push_back(1);
+  input.push_back("  1");  output.push_back(1);
+  input.push_back(" 1");  output.push_back(1);
+  input.push_back("1");  output.push_back(1);
+  input.push_back("  1 ");  output.push_back(1);
   input.push_back("  12");  output.push_back(12);
   input.push_back(" 123");  output.push_back(123);
   input.push_back("1234");  output.push_back(1234);
-  input.push_back("    1");  output.push_back(opt_t());
-  input.push_back("   12");  output.push_back(1);
-  input.push_back("  123");  output.push_back(12);
-  input.push_back(" 1234");  output.push_back(123);
-  input.push_back("12345"); output.push_back(1234);
-  input.push_back("1234a"); output.push_back(1234);
-  input.push_back("  -1");  output.push_back(-1);
-  input.push_back(" -12");  output.push_back(-12);
+  input.push_back(" 1 2");  output.push_back(12);
+  input.push_back(" -1");  output.push_back(-1);
   input.push_back("-123");  output.push_back(-123);
-  input.push_back("-1234"); output.push_back(-123);
-  input.push_back("-123a"); output.push_back(-123);
-  input.push_back("    ");  output.push_back(opt_t());
-  input.push_back("  a ");  output.push_back(opt_t());
-  input.push_back("--12");  output.push_back(opt_t());
-  input.push_back(" 1 2");  output.push_back(opt_t());
-  input.push_back("   -");  output.push_back(opt_t());
-  input.push_back("123");  output.push_back(opt_t());
-  input.push_back("123");  output.push_back(opt_t());
+  input.push_back("- 12");  output.push_back(-12);
+  input.push_back("- 1 ");  output.push_back(-1);
+  input.push_back("    ");  output.push_back(0);
+  input.push_back("-1-2");  output.push_back(opt_t());
+  input.push_back("1.024");  output.push_back(opt_t());
   for (int i=0; i < input.size(); ++i) {
     opt_t res;
     parse_info<std::string::iterator> info = parse(
@@ -45,8 +40,9 @@ void exercise_fortran_int() {
                since white spaces are significant */
     if (output[i]) {
       IOTBX_ASSERT(info.hit)(i)(input[i]);
-      IOTBX_ASSERT(info.length == 4)(i)(input[i])(info.length);
-      IOTBX_ASSERT(res == output[i])(i)(input[i])(res);
+      IOTBX_ASSERT(info.length == std::min(input[i].length(), std::size_t(4)))
+                  (i)(input[i])(info.length);
+      IOTBX_ASSERT(res == output[i])(i)(input[i])(*res);
     }
     else {
       IOTBX_ASSERT(!info.hit)(i)(input[i]);
@@ -55,10 +51,10 @@ void exercise_fortran_int() {
   {
     std::vector<int> results;
     parse_info<> info = parse(
-      "  12  34", i4_p[push_back_a(results)] >> i4_p[push_back_a(results)]);
+      " 1 2- 34", i4_p[push_back_a(results)] >> i4_p[push_back_a(results)]);
     IOTBX_ASSERT(results.size() == 2)(results.size());
     IOTBX_ASSERT(results[0] == 12)(results[0]);
-    IOTBX_ASSERT(results[1] == 34)(results[1]);
+    IOTBX_ASSERT(results[1] == -34)(results[1]);
   }
   {
     std::vector<int> results;
@@ -72,19 +68,28 @@ void exercise_fortran_int() {
 
 void exercise_fortran_real_fixed() {
   using namespace iotbx::boost_spirit;
-  fortran_real_fixed_parser<double, /*Width=*/6, /*FracDigits=*/3> f63_p;
+  fortran_real_parser<double, /*Width=*/6, /*FracDigits=*/3> f63_p;
   std::vector<std::string> input;
   typedef boost::optional<double> opt_t;
   std::vector<opt_t> output;
-  input.push_back(" 1.024");  output.push_back(1.024);
-  input.push_back("21.024");  output.push_back(21.024);
-  input.push_back("-1.024");  output.push_back(-1.024);
-  input.push_back(" 1.0245");  output.push_back(1.024);
-  input.push_back(" 1.024a");  output.push_back(1.024);
-  input.push_back("-0.256");  output.push_back(-0.256);
-  input.push_back("  1.024");  output.push_back(opt_t());
-  input.push_back("123.024");  output.push_back(opt_t());
-  input.push_back("123.24");  output.push_back(opt_t());
+  input.push_back("1."); output.push_back(1);
+  input.push_back("12."); output.push_back(12);
+  input.push_back("123."); output.push_back(123);
+  input.push_back("12345."); output.push_back(12345);
+  input.push_back("-1 3 ."); output.push_back(-13);
+  input.push_back("1024"); output.push_back(1.024);
+  input.push_back("102.4"); output.push_back(102.4);
+  input.push_back(".1024"); output.push_back(0.1024);
+  input.push_back("16+2"); output.push_back(1.6);
+  input.push_back("16e+2"); output.push_back(1.6);
+  input.push_back("16e2"); output.push_back(1.6);
+  input.push_back("16.+2"); output.push_back(1600);
+  input.push_back("16.e+2"); output.push_back(1600);
+  input.push_back("16.e2"); output.push_back(1600);
+  input.push_back("16-2"); output.push_back(16.e-5);
+  input.push_back("16e-2"); output.push_back(16.e-5);
+  input.push_back("16.-2"); output.push_back(0.16);
+  input.push_back("16.e-2"); output.push_back(0.16);
   for (int i=0; i < input.size(); ++i) {
     opt_t res;
     parse_info<std::string::iterator> info = parse(
@@ -92,7 +97,8 @@ void exercise_fortran_real_fixed() {
       f63_p[assign_a(res)]);
     if (output[i]) {
       IOTBX_ASSERT(info.hit)(i)(input[i]);
-      IOTBX_ASSERT(info.length == 6)(i)(input[i])(info.length);
+      IOTBX_ASSERT(info.length == std::min(input[i].length(), std::size_t(6)))
+                  (i)(input[i])(info.length);
       IOTBX_ASSERT(res == output[i])(i)(input[i])(*res);
     }
     else {
@@ -111,24 +117,29 @@ void exercise_fortran_real_fixed() {
   {
     std::vector<double> results;
     parse_info<> info = parse(
-      "11.024-2.048a",
+      "0.1024-0.1289a",
       f63_p[push_back_a(results)] >> f63_p[push_back_a(results)]);
-    IOTBX_ASSERT(results.size() == 2)(results.size());
-    IOTBX_ASSERT(results[0] == 11.024)(results[0]);
-    IOTBX_ASSERT(results[1] == -2.048)(results[1]);
+    IOTBX_ASSERT(info.hit);
+    IOTBX_ASSERT(results[0] == 0.1024)(results[0]);
+    IOTBX_ASSERT(results[1] == -0.128)(results[1]);
   }
   {
     std::vector<double> results;
     parse_info<> info = parse(
-      "11.02422.048a",
+      "0.10240.2048a",
       f63_p[push_back_a(results)] >> f63_p[push_back_a(results)]);
-    IOTBX_ASSERT(results.size() == 2)(results.size());
-    IOTBX_ASSERT(results[0] == 11.024)(results[0]);
-    IOTBX_ASSERT(results[1] == 22.048)(results[1]);
+    IOTBX_ASSERT(info.hit);
+    IOTBX_ASSERT(results[0] == 0.1024)(results[0]);
+    IOTBX_ASSERT(results[1] == 0.2048)(results[1]);
   }
   {
-    parse_info<> info = parse("11.024222.048 ", f63_p >> f63_p);
-    IOTBX_ASSERT(!info.hit);
+    std::vector<double> results;
+    parse_info<> info = parse(
+      "-16.-22",
+      f63_p[push_back_a(results)] >> f63_p[push_back_a(results)]);
+    IOTBX_ASSERT(info.hit);
+    IOTBX_ASSERT(results[0] == -0.16)(results[0]);
+    IOTBX_ASSERT(results[1] == 0.002)(results[1]);
   }
 }
 
