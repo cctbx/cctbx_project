ee6cdb62cf7a1f1e3d3ae49a662046bdd574d8a9
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-01-26 23:01:06 +0000

    generation of random sparse matrices with a given sparsity

--- scitbx/sparse/boost_python/sparse_ext.cpp
+++ scitbx/sparse/boost_python/sparse_ext.cpp
@@ -6,10 +6,12 @@ void wrap_vector();
 void wrap_matrix();
 void wrap_lu_factorization();
 void wrap_approx_equal();
+void wrap_random();
 
 void init_module() {
   wrap_vector();
   wrap_matrix();
+  wrap_random();
   wrap_approx_equal();
   wrap_lu_factorization();
 }
