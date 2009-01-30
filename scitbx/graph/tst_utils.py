bdc68222eb99158e357b623d7acd4b8e041d2566
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2009-01-30 08:56:07 +0000

    tst_utils.py: bogus edge_list_includes_all_implied() removed

--- scitbx/graph/tst_utils.py
+++ scitbx/graph/tst_utils.py
@@ -1,17 +1,6 @@
 from scitbx.graph import utils
 import sys
 
-def edge_list_includes_all_implied(n_vertices, edge_list):
-  es = utils.construct_edge_sets(
-    n_vertices=n_vertices, edge_list=edge_list)
-  bbes = utils.bond_bending_edge_sets(edge_sets=es)
-  el = utils.extract_edge_list(edge_sets=es)
-  bbel = utils.extract_edge_list(edge_sets=bbes)
-  assert len(set(el) - set(bbel)) == 0
-  piel = utils.potential_implied_edge_list(
-    edge_sets=es, bond_bending_edge_sets=bbes)
-  return piel == []
-
 def run(args):
   assert len(args) == 0
   #
@@ -26,8 +15,6 @@ def run(args):
   piel = utils.potential_implied_edge_list(
     edge_sets=es, bond_bending_edge_sets=bbes)
   assert len(piel) == 0
-  assert edge_list_includes_all_implied(
-    n_vertices=n_vertices, edge_list=bbel+piel)
   #
   el = [(0,1), (1,2), (2,3)]
   n_vertices = 4
@@ -37,11 +24,30 @@ def run(args):
   assert len(bbes) == n_vertices
   bbel = utils.extract_edge_list(edge_sets=bbes)
   assert bbel == [(0,1), (0,2), (1,2), (1,3), (2,3)]
+  assert set(el).issubset(set(bbel))
   piel = utils.potential_implied_edge_list(
     edge_sets=es, bond_bending_edge_sets=bbes)
   assert piel == [(0,3)]
-  assert edge_list_includes_all_implied(
-    n_vertices=n_vertices, edge_list=bbel+piel)
+  assert set(bbel).isdisjoint(set(piel))
+  #
+  el = [(0,1), (1,2), (2,3), (3,4), (4,5), (5,6), (6,7)]
+  n_vertices = 8
+  es = utils.construct_edge_sets(n_vertices=n_vertices, edge_list=el)
+  bbes = utils.bond_bending_edge_sets(edge_sets=es)
+  bbel = utils.extract_edge_list(edge_sets=bbes)
+  assert bbel == [
+    (0, 1), (0, 2),
+    (1, 2), (1, 3),
+    (2, 3), (2, 4),
+    (3, 4), (3, 5),
+    (4, 5), (4, 6),
+    (5, 6), (5, 7),
+    (6, 7)]
+  assert set(el).issubset(set(bbel))
+  piel = utils.potential_implied_edge_list(
+    edge_sets=es, bond_bending_edge_sets=bbes)
+  assert piel == [(0,3), (1,4), (2,5), (3,6), (4,7)]
+  assert set(bbel).isdisjoint(set(piel))
   #
   el = [(0,1), (1,2), (2,3), (0,3)]
   n_vertices = 4
@@ -49,11 +55,10 @@ def run(args):
   bbes = utils.bond_bending_edge_sets(edge_sets=es)
   bbel = utils.extract_edge_list(edge_sets=bbes)
   assert bbel == [(0,1), (0,2), (0,3), (1,2), (1,3), (2,3)]
+  assert set(el).issubset(set(bbel))
   piel = utils.potential_implied_edge_list(
     edge_sets=es, bond_bending_edge_sets=bbes)
   assert piel == []
-  assert edge_list_includes_all_implied(
-    n_vertices=n_vertices, edge_list=bbel+piel)
   #
   el = [(0,1), (1,2), (2,3), (3,4), (4,5), (0,5)]
   n_vertices = 6
@@ -66,12 +71,11 @@ def run(args):
     (2,3), (2,4),
     (3,4), (3,5),
     (4,5)]
+  assert set(el).issubset(set(bbel))
   piel = utils.potential_implied_edge_list(
     edge_sets=es, bond_bending_edge_sets=bbes)
   assert piel == [(0,3), (1,4), (2,5)]
-  assert len(set(bbel).intersection(set(piel))) == 0
-  assert edge_list_includes_all_implied(
-    n_vertices=n_vertices, edge_list=bbel+piel)
+  assert set(bbel).isdisjoint(set(piel))
   #
   el = [(0,1), (1,2), (2,3), (2,6), (3,4), (3,7), (4,5), (0,5)]
   n_vertices = 8
@@ -84,12 +88,11 @@ def run(args):
     (2,3), (2,4), (2,6), (2,7),
     (3,4), (3,5), (3,6), (3,7),
     (4,5), (4,7)]
+  assert set(el).issubset(set(bbel))
   piel = utils.potential_implied_edge_list(
     edge_sets=es, bond_bending_edge_sets=bbes)
   assert piel == [(0,3), (0,6), (1,4), (1,7), (2,5), (4,6), (5,7), (6,7)]
-  assert len(set(bbel).intersection(set(piel))) == 0
-  assert edge_list_includes_all_implied(
-    n_vertices=n_vertices, edge_list=bbel+piel)
+  assert set(bbel).isdisjoint(set(piel))
   #
   print "OK"
 
