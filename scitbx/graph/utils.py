3c761ba95851360bc99c944f633a9e127a27d89f
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2009-01-30 07:36:42 +0000

    scitbx/graph/utils.py: new potential_implied_edge_list(); not fully vetted, checked in for testing

--- scitbx/graph/utils.py
+++ scitbx/graph/utils.py
@@ -6,10 +6,16 @@ def construct_edge_sets(n_vertices, edge_list):
     result[j].add(i)
   return result
 
-def bond_bending_edge_sets(edge_sets):
+def extract_edge_list(edge_sets):
   result = []
-  for edge_set in edge_sets:
-    result.append(set(edge_set))
+  for i,edge_set in enumerate(edge_sets):
+    for j in sorted(edge_set):
+      if (j < i): continue
+      result.append((i,j))
+  return result
+
+def bond_bending_edge_sets(edge_sets):
+  result = [set(edge_set) for edge_set in edge_sets]
   for i,edge_set in enumerate(edge_sets):
     for j in edge_set:
       if (j < i): continue
@@ -21,10 +27,22 @@ def bond_bending_edge_sets(edge_sets):
         result[j].add(k)
   return result
 
-def extract_edge_list(edge_sets):
-  result = []
+def potential_implied_one_way_edge_sets(edge_sets, bond_bending_edge_sets):
+  result = [set() for i in xrange(len(edge_sets))]
   for i,edge_set in enumerate(edge_sets):
-    for j in sorted(edge_set):
-      if (j < i): continue
-      result.append((i,j))
+    for j in edge_set:
+      if (j == i): continue
+      for k in edge_sets[j]:
+        if (k == j): continue
+        if (k == i): continue
+        for l in edge_sets[k]:
+          if (l == i): continue
+          if (l in bond_bending_edge_sets[i]): continue
+          if (i < l): result[i].add(l)
+          else:       result[l].add(i)
   return result
+
+def potential_implied_edge_list(edge_sets, bond_bending_edge_sets):
+  return extract_edge_list(
+    edge_sets=potential_implied_one_way_edge_sets(
+      edge_sets=edge_sets, bond_bending_edge_sets=bond_bending_edge_sets))
