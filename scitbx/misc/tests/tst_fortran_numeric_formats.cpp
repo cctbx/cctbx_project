45a0d9380dd1d034814320c3de9ce9034ada365a
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-02-02 10:59:02 +0000

    move fortran numeric format parsers for boost spirit to the scitbx

--- scitbx/misc/tests/tst_fortran_numeric_formats.cpp
+++ scitbx/misc/tests/tst_fortran_numeric_formats.cpp
@@ -1,18 +1,18 @@
-#include <iotbx/boost_spirit/fortran_numerics.h>
+#include <scitbx/misc/fortran_numeric_formats.h>
 
 #include <boost/spirit/include/classic_core.hpp>
 #include <boost/spirit/include/classic_assign_actor.hpp>
 #include <boost/spirit/include/classic_push_back_actor.hpp>
 
-#include <iotbx/error.h>
+#include <scitbx/error.h>
 #include <boost/optional.hpp>
 #include <string>
 #include <vector>
 #include <iostream>
 
-void exercise_fortran_int() {
-  using namespace iotbx::boost_spirit;
-  fortran_int_parser<int, /*Width=*/4, /*StrictWidth=*/false> i4_p;
+template <class parser_type>
+void exercise_fortran_int(parser_type const &i4_p) {
+  using namespace scitbx::boost_spirit_classic;
   std::vector<std::string> input;
   typedef boost::optional<int> opt_t;
   std::vector<opt_t> output;
@@ -39,49 +39,50 @@ void exercise_fortran_int() {
       /* Note: those parsers must be use only for character-level parsing
                since white spaces are significant */
     if (output[i]) {
-      IOTBX_ASSERT(info.hit)(i)(input[i]);
-      IOTBX_ASSERT(info.length == std::min(input[i].length(), std::size_t(4)))
+      SCITBX_ASSERT(info.hit)(i)(input[i]);
+      SCITBX_ASSERT(info.length == std::min(input[i].length(), std::size_t(4)))
                   (i)(input[i])(info.length);
-      IOTBX_ASSERT(res == output[i])(i)(input[i])(*res);
+      SCITBX_ASSERT(res == output[i])(i)(input[i])(*res);
     }
     else {
-      IOTBX_ASSERT(!info.hit)(i)(input[i]);
+      SCITBX_ASSERT(!info.hit)(i)(input[i]);
     }
   }
   {
     std::vector<int> results;
     parse_info<> info = parse(
       " 1 2- 34", i4_p[push_back_a(results)] >> i4_p[push_back_a(results)]);
-    IOTBX_ASSERT(results.size() == 2)(results.size());
-    IOTBX_ASSERT(results[0] == 12)(results[0]);
-    IOTBX_ASSERT(results[1] == -34)(results[1]);
+    SCITBX_ASSERT(results.size() == 2)(results.size());
+    SCITBX_ASSERT(results[0] == 12)(results[0]);
+    SCITBX_ASSERT(results[1] == -34)(results[1]);
   }
   {
     std::vector<int> results;
     parse_info<> info = parse(
       "1234-567", i4_p[push_back_a(results)] >> i4_p[push_back_a(results)]);
-    IOTBX_ASSERT(results.size() == 2)(results.size());
-    IOTBX_ASSERT(results[0] == 1234)(results[0]);
-    IOTBX_ASSERT(results[1] == -567)(results[1]);
-  }
-  {
-    fortran_int_parser<int, /*Width=*/5, /*StrictWidth=*/true> i5_p;
-    int i;
-    parse_info<> info;
-    info = parse("1", i5_p[assign_a(i)]);
-    IOTBX_ASSERT(!info.hit);
-    info = parse("   1", i5_p[assign_a(i)]);
-    IOTBX_ASSERT(!info.hit);
-    info = parse("    1", i5_p[assign_a(i)]);
-    IOTBX_ASSERT(info.hit);
-    IOTBX_ASSERT(i == 1);
+    SCITBX_ASSERT(results.size() == 2)(results.size());
+    SCITBX_ASSERT(results[0] == 1234)(results[0]);
+    SCITBX_ASSERT(results[1] == -567)(results[1]);
   }
 }
 
-void exercise_fortran_real_fixed() {
-  using namespace iotbx::boost_spirit;
-  fortran_real_parser<
-    double, /*Width=*/6, /*FracDigits=*/3, /*StrictWidth=*/false> f63_p;
+void exercise_fortran_int_2() {
+  using namespace scitbx::boost_spirit_classic;
+  fortran_int_parser<int, /*Width=*/5, /*StrictWidth=*/true> i5_p;
+  int i;
+  parse_info<> info;
+  info = parse("1", i5_p[assign_a(i)]);
+  SCITBX_ASSERT(!info.hit);
+  info = parse("   1", i5_p[assign_a(i)]);
+  SCITBX_ASSERT(!info.hit);
+  info = parse("    1", i5_p[assign_a(i)]);
+  SCITBX_ASSERT(info.hit);
+  SCITBX_ASSERT(i == 1);
+}
+
+template <class parser_type>
+void exercise_fortran_real_fixed(parser_type const &f63_p) {
+  using namespace scitbx::boost_spirit_classic;
   std::vector<std::string> input;
   typedef boost::optional<double> opt_t;
   std::vector<opt_t> output;
@@ -110,13 +111,13 @@ void exercise_fortran_real_fixed() {
       input[i].begin(), input[i].end(),
       f63_p[assign_a(res)]);
     if (output[i]) {
-      IOTBX_ASSERT(info.hit)(i)(input[i]);
-      IOTBX_ASSERT(info.length == std::min(input[i].length(), std::size_t(6)))
+      SCITBX_ASSERT(info.hit)(i)(input[i]);
+      SCITBX_ASSERT(info.length == std::min(input[i].length(), std::size_t(6)))
                   (i)(input[i])(info.length);
-      IOTBX_ASSERT(res == output[i])(i)(input[i])(*res);
+      SCITBX_ASSERT(res == output[i])(i)(input[i])(*res);
     }
     else {
-      IOTBX_ASSERT(!info.hit)(i)(input[i]);
+      SCITBX_ASSERT(!info.hit)(i)(input[i]);
     }
   }
   {
@@ -124,53 +125,78 @@ void exercise_fortran_real_fixed() {
     parse_info<> info = parse(
       " 1.024 2.048",
       f63_p[push_back_a(results)] >> f63_p[push_back_a(results)]);
-    IOTBX_ASSERT(results.size() == 2)(results.size());
-    IOTBX_ASSERT(results[0] == 1.024)(results[0]);
-    IOTBX_ASSERT(results[1] == 2.048)(results[1]);
+    SCITBX_ASSERT(results.size() == 2)(results.size());
+    SCITBX_ASSERT(results[0] == 1.024)(results[0]);
+    SCITBX_ASSERT(results[1] == 2.048)(results[1]);
   }
   {
     std::vector<double> results;
     parse_info<> info = parse(
       "0.1024-0.1289a",
       f63_p[push_back_a(results)] >> f63_p[push_back_a(results)]);
-    IOTBX_ASSERT(info.hit);
-    IOTBX_ASSERT(results[0] == 0.1024)(results[0]);
-    IOTBX_ASSERT(results[1] == -0.128)(results[1]);
+    SCITBX_ASSERT(info.hit);
+    SCITBX_ASSERT(results[0] == 0.1024)(results[0]);
+    SCITBX_ASSERT(results[1] == -0.128)(results[1]);
   }
   {
     std::vector<double> results;
     parse_info<> info = parse(
       "0.10240.2048a",
       f63_p[push_back_a(results)] >> f63_p[push_back_a(results)]);
-    IOTBX_ASSERT(info.hit);
-    IOTBX_ASSERT(results[0] == 0.1024)(results[0]);
-    IOTBX_ASSERT(results[1] == 0.2048)(results[1]);
+    SCITBX_ASSERT(info.hit);
+    SCITBX_ASSERT(results[0] == 0.1024)(results[0]);
+    SCITBX_ASSERT(results[1] == 0.2048)(results[1]);
   }
   {
     std::vector<double> results;
     parse_info<> info = parse(
       "-16.-22",
       f63_p[push_back_a(results)] >> f63_p[push_back_a(results)]);
-    IOTBX_ASSERT(info.hit);
-    IOTBX_ASSERT(results[0] == -0.16)(results[0]);
-    IOTBX_ASSERT(results[1] == 0.002)(results[1]);
+    SCITBX_ASSERT(info.hit);
+    SCITBX_ASSERT(results[0] == -0.16)(results[0]);
+    SCITBX_ASSERT(results[1] == 0.002)(results[1]);
   }
+}
 
+void exercise_fortran_real_fixed_2() {
+  using namespace scitbx::boost_spirit_classic;
   fortran_real_parser<
     double, /*Width=*/5, /*FracDigits=*/1,/*StrictWidth=*/true> f5_1_p;
   double x;
   parse_info<> info;
   info = parse("1.", f5_1_p[assign_a(x)]);
-  IOTBX_ASSERT(!info.hit);
+  SCITBX_ASSERT(!info.hit);
   info = parse("  1.", f5_1_p[assign_a(x)]);
-  IOTBX_ASSERT(!info.hit);
+  SCITBX_ASSERT(!info.hit);
   info = parse("   1.", f5_1_p[assign_a(x)]);
-  IOTBX_ASSERT(info.hit);
-  IOTBX_ASSERT(x == 1);
+  SCITBX_ASSERT(info.hit);
+  SCITBX_ASSERT(x == 1);
 }
 
 int main() {
-  exercise_fortran_int();
-  exercise_fortran_real_fixed();
+  {
+    using namespace scitbx::boost_spirit_classic;
+    fortran_int_parser<int, /*Width=*/4, /*StrictWidth=*/false> i4_p;
+    exercise_fortran_int(i4_p);
+  }
+  {
+    using namespace scitbx::boost_spirit_classic::dynamic;
+    fortran_int_parser<int> i4_p(/*Width=*/4, /*StrictWidth=*/false);
+    exercise_fortran_int(i4_p);
+  }
+  exercise_fortran_int_2();
+  {
+    using namespace scitbx::boost_spirit_classic;
+    fortran_real_parser<
+      double, /*Width=*/6, /*FracDigits=*/3, /*StrictWidth=*/false> f63_p;
+    exercise_fortran_real_fixed(f63_p);
+  }
+  {
+    using namespace scitbx::boost_spirit_classic::dynamic;
+    fortran_real_parser<double> f63_p(
+      /*Width=*/6, /*FracDigits=*/3, /*StrictWidth=*/false);
+    exercise_fortran_real_fixed(f63_p);
+  }
+  exercise_fortran_real_fixed_2();
   std::cout << "OK" << std::endl;
 }
