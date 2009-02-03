5f93f67c5724620ba516ec77c365597a98619fcb
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-02-03 15:37:36 +0000

    - rewrite the hklf parser with standard C++ stream manipulation (easier to understand by mere mortals and since no backtracking is needed, a Boost.Spirit parser is largely overkill)

--- iotbx/shelx/shelx_ext.cpp
+++ iotbx/shelx/shelx_ext.cpp
@@ -4,10 +4,36 @@
 #include <boost/python/args.hpp>
 
 #include <iotbx/shelx/hklf_simple.h>
-//#define IOTBX_SHELX_HKLF_SPIRIT_DEBUG
 #include <iotbx/shelx/hklf.h>
+#include <strstream>
 
 namespace iotbx { namespace shelx { namespace boost_python {
+
+  struct hklf_reader
+  {
+    hklf_reader(std::string const &content, bool strict) {
+      std::istringstream input(content);
+      iotbx::shelx::hklf_reader delegate(input, strict);
+      indices_ = delegate.indices();
+      data_ = delegate.data();
+      sigmas_ = delegate.sigmas();
+      extra_ = delegate.alphas();
+    }
+
+    scitbx::af::shared<cctbx::miller::index<> > indices_;
+    scitbx::af::shared<double> data_, sigmas_;
+    scitbx::af::shared<int> extra_; // batch numbers or phases
+
+    scitbx::af::shared<cctbx::miller::index<> > indices() { return indices_; };
+
+    scitbx::af::shared<double> data() { return data_; }
+
+    scitbx::af::shared<double> sigmas() { return sigmas_; }
+
+    scitbx::af::shared<int> alphas() { return extra_; }
+
+    scitbx::af::shared<int> batch_numbers() { return extra_; }
+  };
 
   template<class WrappedType>
   struct any_hklf_reader_wrapper
