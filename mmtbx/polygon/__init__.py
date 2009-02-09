24af30d148a353d28acdcd3528304485e2b47b27
Pavel Afonine <pafonine@users.noreply.github.com>
2009-02-09 04:00:37 +0000

    use Phil, add tests

--- mmtbx/polygon/__init__.py
+++ mmtbx/polygon/__init__.py
@@ -2,6 +2,8 @@ from scitbx.array_family import flex
 from libtbx import group_args, smart_open
 import iotbx.phil
 import mmtbx.polygon
+import libtbx, os
+
 
 master_params = iotbx.phil.parse("""\
 polygon {
@@ -22,10 +24,20 @@ polygon {
   max_reject_fraction = 0.1
     .type = float
     .help = Fraction of models allowed to be rejected as outliers.
+  max_models_for_default_filter = 1000
+    .type = int
   filter
     .multiple = True
     .help = Selection keys.
   {
+    key = nseq npdb code dhigh dlow nrefl rfact rfree bndav bndmx \
+          angav angmx dihav dihmx plnav plnmx chrav chrmx nbdis adpmn \
+          adpav adpmx occmn occav occmx natoms natiso nonpos nmod nsg \
+          acell bcell ccell alpha beta gamma data twin anom bwils \
+          compl cmp-t cmp-l bulkk bulkb noutl progr year rhgpdb rlwpdb \
+          sigma t-set t-flag rpdb rfpdb vol/1000 mw(kda) cmatthw rmout \
+          rmfav rmgen mol:pno wat/res
+      .type = choice(multi=False)
     value_min = None
       .type = float
     value_max = None
@@ -60,7 +72,7 @@ def select_database_dict_by_keys(select_keys, database_dict):
     result.setdefault(key, database_dict[key])
   return result
 
-def _convert_to_numeric(values):
+def convert_to_numeric(values):
   is_digit_selection = flex.bool()
   for kv in values:
     flag = True
@@ -83,7 +95,7 @@ def _convert_to_numeric(values):
       new_values.append(float(kv))
   return new_values
 
-def _select_dict(database_dict, selection):
+def select_dict(database_dict, selection):
   result = {}
   for key in database_dict.keys():
     result.setdefault(key, database_dict[key].select(selection))
@@ -91,16 +103,16 @@ def _select_dict(database_dict, selection):
 
 def order_by_value(database_dict, key, reverse = False):
   values = database_dict[key]
-  new_values = _convert_to_numeric(values = values)
+  new_values = convert_to_numeric(values = values)
   selection = flex.sort_permutation(new_values, reverse = reverse)
-  return _select_dict(database_dict = database_dict, selection = selection)
+  return select_dict(database_dict = database_dict, selection = selection)
 
 def leave_all_available_entries(database_dict, keys):
   selection = flex.bool(database_dict[keys[0]].size(), True)
   for key in keys:
     values = database_dict[key]
     selection &= values != "none"
-  return _select_dict(database_dict = database_dict, selection = selection)
+  return select_dict(database_dict = database_dict, selection = selection)
 
 def filter_database(database_dict, key, value_min = None, value_max = None,
                     target_value = None):
@@ -111,27 +123,29 @@ def filter_database(database_dict, key, value_min = None, value_max = None,
   else:
     assert [value_min,value_max].count(None) < 2
     if([value_min,value_max].count(None) == 0): assert value_min < value_max
-    new_values = _convert_to_numeric(values = values)
+    new_values = convert_to_numeric(values = values)
     selection = flex.bool(new_values.size(), True)
     if(value_min is not None):
+      if(isinstance(new_values, flex.int)): value_min = int(value_min)
       selection &= new_values > value_min
     if(value_max is not None):
+      if(isinstance(new_values, flex.int)): value_max = int(value_max)
       selection &= new_values < value_max
-  return _select_dict(database_dict = database_dict, selection = selection)
+  return select_dict(database_dict = database_dict, selection = selection)
 
 def filter_histogram_of_key_value(database_dict, key, max_reject_fraction,
-                                  edge_tolerance_small = 1.e-4):
+                                  edge_tolerance_small = 1.e-4, n_slots = 3):
   values = database_dict[key]
-  new_values = _convert_to_numeric(values = values)
+  new_values = convert_to_numeric(values = values)
   #print flex.min(new_values), flex.max(new_values)
   size = new_values.size()
   #print size
   if(size == 0): return
   while True:
     values = database_dict[key]
-    new_values = _convert_to_numeric(values = values)
+    new_values = convert_to_numeric(values = values)
     selection = flex.bool(new_values.size(), True)
-    histogram = flex.histogram(data = new_values, n_slots = 3)
+    histogram = flex.histogram(data = new_values, n_slots = n_slots)
     l = histogram.data_min()
     for i, s in enumerate(histogram.slots()):
       r = histogram.data_min() + histogram.slot_width() * (i+1)
@@ -146,10 +160,12 @@ def filter_histogram_of_key_value(database_dict, key, max_reject_fraction,
     #print leave, remove
     if(remove == 0): break
     if(size - leave > int(size * max_reject_fraction)): break
-    database_dict = _select_dict(database_dict = database_dict, selection = selection)
+    database_dict = select_dict(database_dict = database_dict,
+                                selection     = selection)
   return database_dict
 
 def show_histogram(data, n_slots):
+  normalization_scale = data.size() / n_slots
   histogram = flex.histogram(data = data, n_slots = n_slots)
   l = histogram.data_min()
   for i, s in enumerate(histogram.slots()):
@@ -157,32 +173,61 @@ def show_histogram(data, n_slots):
     print "%8.4f %8.4f %d" % (l, r, s)
     l = r
 
-def polygon(file_name,
-            selected_keys,
-            filter_keys_and_targets,
-            max_reject_fraction,
-            n_histogram_slots):
+def apply_default_filter(database_dict, d_min, max_models_for_default_filter,
+                         key = "dhigh"):
+  database_dict = order_by_value(database_dict = database_dict, key = key)
+  values = flex.double()
+  for v in database_dict[key]: values.append(float(v))
+  diff = flex.abs(values-d_min)
+  min_val = flex.min(diff)
+  i_min_sel = (diff == min_val).iselection()
+  assert i_min_sel.size() > 0
+  i_min = i_min_sel[i_min_sel.size()/2]
+  i_l = max(0, i_min-max_models_for_default_filter/2)
+  i_r = min(values.size()-1, i_min+max_models_for_default_filter/2)
+  selection = flex.bool(values.size(), False)
+  for i in xrange(i_l,i_r): selection[i] = True
+  return select_dict(database_dict = database_dict, selection = selection)
+
+def polygon(params = master_params.extract(), d_min = None,
+            show_histograms = True):
+  if(params.polygon.database_file_name is None):
+    file_name = libtbx.env.find_in_repositories(
+      relative_path = "chem_data/polygon_data/pdb_2009-01-14_ord.txt",
+      test = os.path.isfile)
+  else:
+    file_name = params.polygon.database_file_name
+    assert os.path.isfile(file_name)
   database_dict = extract_database_from_file(file_name = file_name)
   result = leave_all_available_entries(
     database_dict = database_dict,
-    keys          = selected_keys)
-  for filter_keys_and_target in filter_keys_and_targets:
-    result = filter_database(
-      database_dict = result,
-      key           = filter_keys_and_target.key,
-      value_min     = filter_keys_and_target.value_min,
-      value_max     = filter_keys_and_target.value_max,
-      target_value  = filter_keys_and_target.target_value)
+    keys          = params.polygon.keys_to_show)
+  filters = params.polygon.filter
+  if((len(filters) == 0 or (len(filters) == 1 and filters[0].key == "dhigh" and
+     [filter.value_min, filter.value_max].count(None) == 2)) and
+     d_min is not None):
+    result = apply_default_filter(database_dict = result, d_min = d_min,
+      max_models_for_default_filter =
+        params.polygon.max_models_for_default_filter)
+  else:
+    for filter in filters:
+      result = filter_database(
+        database_dict = result,
+        key           = filter.key,
+        value_min     = filter.value_min,
+        value_max     = filter.value_max,
+        target_value  = filter.target_value)
   result = select_database_dict_by_keys(
-    select_keys   = selected_keys,
+    select_keys   = params.polygon.keys_to_show,
     database_dict = result)
-  for selected_key in selected_keys:
+  for key_to_show in params.polygon.keys_to_show:
     result = filter_histogram_of_key_value(
       database_dict       = result,
-      key                 = selected_key,
-      max_reject_fraction = max_reject_fraction)
-  for selected_key in selected_keys:
-    print selected_key
-    show_histogram(data = _convert_to_numeric(values = result[selected_key]),
-                   n_slots = n_histogram_slots)
-
+      key                 = key_to_show,
+      max_reject_fraction = params.polygon.max_reject_fraction)
+  if(show_histograms):
+    for selected_key in params.polygon.keys_to_show:
+      print selected_key
+      show_histogram(data    = convert_to_numeric(values=result[selected_key]),
+                     n_slots = params.polygon.number_of_histogram_slots)
+  return result
--- mmtbx/polygon/tst.py
+++ mmtbx/polygon/tst.py
@@ -1,26 +1,68 @@
 from scitbx.array_family import flex
-from libtbx import group_args, smart_open
 import iotbx.phil
 from mmtbx import polygon
 import libtbx, os
 
+params1 = iotbx.phil.parse("""\
+polygon {
+  keys_to_show = *rfact *rfree *bndav *angav *adpav
+  filter
+  {
+    key = *dhigh
+    value_min = 2
+    value_max = 2.5
+  }
+  filter
+  {
+    key = *natoms
+    value_min = 0
+    value_max = 50000
+  }
+  filter
+  {
+    key = *data
+    target_value = fobs_x
+  }
+}
+""")
 
-def run():
-  c1 = group_args(key = "dhigh",  value_min = 2,    value_max = 2.5,  target_value  = None)
-  c2 = group_args(key = "natoms", value_min = 0,    value_max = 50000, target_value = None)
-  c3 = group_args(key = "data",   value_min = None, value_max = None, target_value  = "fobs_x")
-  filter_keys_and_targets = [c1,c2, c3]
+params2 = iotbx.phil.parse("""\
+polygon {
+  keys_to_show = *rfact *rfree *bndav *angav *adpav
+}
+""")
 
-  database_file = libtbx.env.find_in_repositories(
-    relative_path = "chem_data/polygon_data/pdb_2009-01-14_ord.txt",
-    test          = os.path.isfile)
+def example_1():
+  # show selected characteristics, apply pre-defined filters
+  pr, unused_definitions = polygon.master_params.fetch(sources = [params1],
+    track_unused_definitions = True)
+  polygon.polygon(params = pr.extract())
 
-  polygon.polygon(file_name               = database_file,
-                  selected_keys           = ["rfact", "rfree", "angav", "bndav"],
-                  filter_keys_and_targets = filter_keys_and_targets,
-                  max_reject_fraction     = 0.1,
-                  n_histogram_slots       = 10)
+def example_2():
+  # show selected characteristics for the whole database
+  pr, unused_definitions = polygon.master_params.fetch(sources = [params2],
+    track_unused_definitions = True)
+  polygon.polygon(params = pr.extract())
+
+def example_3():
+  # show selected characteristics, apply default selection
+  # d_min comes from the structure that we are comparing against database
+  pr, unused_definitions = polygon.master_params.fetch(sources = [params2],
+    track_unused_definitions = True)
+  polygon.polygon(params = pr.extract(),
+                  d_min  = 2.0)
 
 
 if (__name__ == "__main__"):
-  run()
+  file_name = libtbx.env.find_in_repositories(
+      relative_path = "chem_data/polygon_data/pdb_2009-01-14_ord.txt",
+      test = os.path.isfile)
+  if(file_name is None):
+    print "Skip POLYGON test: database file is not available."
+    return
+  print "\nEXAMPLE 1:"
+  example_1()
+  print "\nEXAMPLE 2:"
+  example_2()
+  print "\nEXAMPLE 3:"
+  example_3()
