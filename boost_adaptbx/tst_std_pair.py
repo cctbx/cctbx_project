139b0f9c338030ec410e8a78629a1fabf5ac956e
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-02-11 11:08:00 +0000

    - conversion of Python tuple to std::pair

--- boost_adaptbx/tst_std_pair.py
+++ boost_adaptbx/tst_std_pair.py
@@ -1,8 +1,31 @@
 import boost.std_pair
+from libtbx.test_utils import Exception_expected
 
 def run():
-  boost.std_pair.exercise(2) == (4,1.)
-  boost.std_pair.exercise(4) == (16,2.)
+  boost.std_pair.exercise(( 1, 2)) == ( 2, 1.)
+  boost.std_pair.exercise((-2, 4)) == (-4, 2.)
+
+  try:
+    boost.std_pair.exercise(1)
+  except Exception, err:
+    assert err.__class__.__name__ == 'ArgumentError'
+  else:
+    raise Exception_expected
+
+  try:
+    boost.std_pair.exercise((1,))
+  except Exception, err:
+    assert err.__class__.__name__ == 'ArgumentError'
+  else:
+    raise Exception_expected
+
+  try:
+    boost.std_pair.exercise((1,2,3,))
+  except Exception, err:
+    assert err.__class__.__name__ == 'ArgumentError'
+  else:
+    raise Exception_expected
+
   print "OK"
 
 if __name__ == '__main__':
