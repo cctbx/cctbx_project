8fbe5abea9c880a302825c19e796bf3a157f578a
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-02-11 11:08:00 +0000

    - conversion of Python tuple to std::pair

--- boost_adaptbx/std_pair_conversion.h
+++ boost_adaptbx/std_pair_conversion.h
@@ -2,6 +2,7 @@
 #define BOOST_ADAPTBX_STD_PAIR_CONVERSION_H
 
 #include <boost/python/tuple.hpp>
+#include <boost/python/extract.hpp>
 #include <boost/python/to_python_converter.hpp>
 
 namespace boost_adaptbx { namespace std_pair_conversions {
@@ -31,6 +32,53 @@ namespace boost_adaptbx { namespace std_pair_conversions {
       >();
     }
   };
+
+  template <typename T, typename U>
+  struct from_tuple
+  {
+    from_tuple() {
+      using namespace boost::python::converter;
+      registry::push_back(&convertible,
+                          &construct,
+                          boost::python::type_id<std::pair<T,U> >()
+                          #ifdef BOOST_PYTHON_SUPPORTS_PY_SIGNATURES
+                          , get_pytype
+                          #endif
+      );
+    }
+
+    static const PyTypeObject *get_pytype() { return &PyTuple_Type; }
+
+    static void *convertible(PyObject *o) {
+      using namespace boost::python;
+      if (!PyTuple_Check(o) || PyTuple_GET_SIZE(o) != 2) return 0;
+      return o;
+    }
+
+    static void construct(
+      PyObject *o,
+      boost::python::converter::rvalue_from_python_stage1_data *data)
+    {
+      using boost::python::extract;
+      using namespace boost::python::converter;
+      PyObject *first  = PyTuple_GET_ITEM(o, 0),
+               *second = PyTuple_GET_ITEM(o, 1);
+      void *storage =
+        ((rvalue_from_python_storage<std::pair<T,U> >*) data)->storage.bytes;
+      new (storage) std::pair<T,U>(extract<T>(first), extract<U>(second));
+      data->convertible = storage;
+    }
+  };
+
+  template <typename T, typename U>
+  struct to_and_from_tuple
+  {
+    to_and_from_tuple() {
+      to_tuple<T,U>();
+      from_tuple<T,U>();
+    }
+  };
+
 
 }}
 
--- boost_adaptbx/std_pair_ext.cpp
+++ boost_adaptbx/std_pair_ext.cpp
@@ -2,20 +2,15 @@
 #include <boost/python/def.hpp>
 #include <boost_adaptbx/std_pair_conversion.h>
 #include <boost_adaptbx/std_pair_fwd.h>
-#include <boost_adaptbx/optional_fwd.h>
 
 namespace {
-  std::pair<int, double> exercise(int i) {
-    return std::make_pair(i*2, i/2.);
+  std::pair<int, double> exercise(std::pair<int, double> const &p) {
+    return std::make_pair(p.first*2, p.second/2.);
   }
 }
 
 BOOST_PYTHON_MODULE(std_pair_ext)
 {
-  using boost_adaptbx::std_pair_conversions::to_tuple;
-  using boost::optional;
-
-  to_tuple<int, double>();
-
+  boost_adaptbx::std_pair_conversions::to_and_from_tuple<int, double>();
   boost::python::def("exercise", exercise);
 }
