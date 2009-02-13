d9b993b88ebc18390709c3a933dc3d5856a1e4ec
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2009-02-13 00:52:37 +0000

    scitbx/graph/tardy_tree.py: find_paths(): re-worked to produce data structure which should be more suitable for the next steps

--- scitbx/graph/tst_tardy_tree_find_paths.py
+++ scitbx/graph/tst_tardy_tree_find_paths.py
@@ -4,19 +4,38 @@ from scitbx.graph import utils
 from libtbx.utils import null_out
 import sys
 
-def exercise_simple_loops(out, loop_size_max=8):
+def exercise_minimal():
+  edge_sets = utils.construct_edge_sets(n_vertices=1, edge_list=[])
+  assert find_paths(edge_sets=edge_sets, iv=0) == {0: {}}
+  edge_sets = utils.construct_edge_sets(n_vertices=2, edge_list=[(0,1)])
+  for iv in [0,1]:
+    assert find_paths(edge_sets=edge_sets, iv=iv) == {0: {1: []}, 1: {0: []}}
+
+def exercise_simple_loops(loop_size_max=8):
   for n_vertices in xrange(3, loop_size_max+1):
     edge_list = [tuple(sorted((i,(i+1)%n_vertices)))
       for i in xrange(n_vertices)]
     edge_sets = utils.construct_edge_sets(
       n_vertices=n_vertices, edge_list=edge_list)
-    jps = find_paths(edge_sets=edge_sets, out=out)
+    jv_kv_paths = find_paths(edge_sets=edge_sets, iv=0)
+    assert len(jv_kv_paths[0]) == 2
     if (n_vertices <= 6):
-      assert len(jps[0]) == 2
-      for jp in jps[0]:
-        assert len(jp[1][0]) == n_vertices-2
+      jv_kv_paths[0][1] == n_vertices-2
+      jv_kv_paths[0][n_vertices-1] == n_vertices-2
     else:
-      assert len(jps[0]) == 0
+      jv_kv_paths[0][1] == 0
+      jv_kv_paths[0][n_vertices-1] == 0
+    if (n_vertices == 3):
+      assert jv_kv_paths == {
+        0: {1: [[2]], 2: [[1]]},
+        1: {0: [], 2: []},
+        2: {0: [], 1: []}}
+    elif (n_vertices == 4):
+      assert jv_kv_paths == {
+        0: {1: [[3, 2]], 3: [[1, 2]]},
+        1: {0: [], 2: [[3]]},
+        2: {1: [], 3: []},
+        3: {0: [], 2: [[1]]}}
 
 def three_archs_grow_edge_list(edge_list, offs, size):
   result = list(edge_list)
@@ -63,11 +82,20 @@ def exericse_three_archs(out, arch_size_max=8):
         s = (arch_size_1 < 6) \
           + (arch_size_2 < 6) \
           + (arch_size_3 < 6)
-        jps = find_paths(edge_sets=es, out=out)
-        assert len(jps[1]) == s
-        if (len(jps[1]) == 3):
-          inferred_is_rigid = sum([len(jp[1][0]) for jp in jps[1]]) < 7
-          assert inferred_is_rigid == is_rigid
+        for iv in [0,1]:
+          jv_kv_paths = find_paths(edge_sets=es, iv=iv)
+          kv_paths = jv_kv_paths.get(1-iv, [])
+          assert len(kv_paths) == s
+          if (len(kv_paths) == 3):
+            sum_len = 0
+            for paths in kv_paths.values():
+              assert len(paths) in [0,1]
+              if (len(paths) == 1):
+                sum_len += len(paths[0])
+            inferred_is_rigid = sum_len < 7
+            assert inferred_is_rigid == is_rigid
+          else:
+            assert not is_rigid
 
 def run(args):
   assert args in [[], ["--verbose"]]
@@ -75,7 +103,8 @@ def run(args):
     out = null_out()
   else:
     out = sys.stdout
-  exercise_simple_loops(out=out)
+  exercise_minimal()
+  exercise_simple_loops()
   exericse_three_archs(out=out)
   print "OK"
 
