3295197f81139f9cd68fbfb018039277783eb61e
Nicholas Sauter <nksauter@lbl.gov>
2009-02-25 04:49:05 +0000

    New bravais lattice convenience class

--- cctbx/sgtbx/bravais_types.py
+++ cctbx/sgtbx/bravais_types.py
@@ -32,6 +32,64 @@ centric = (
 "F m -3 m",
 )
 
+class bravais_lattice(object):
+  """Reference:  Table 1.2 (page 4) and Table 2.1.1 (page 13) of
+  International Tables for Crystallography, Volume A Space Group Symmetry,
+  ed. Theo Hahn, Fourth, revised edition, Kluwer Academic Publishers, 1996.
+  """
+
+  def __init__(self,symbol):
+    from cctbx import sgtbx
+    self.space_group_info = sgtbx.space_group_info(symbol)
+    self.space_group = self.space_group_info.group()
+    self.crystal_system = self.space_group.crystal_system()
+    self.centring_symbol = self.space_group.conventional_centring_type_symbol()
+
+  crystal_family_symbols = {"Triclinic":'a',
+                            "Monoclinic":'m',
+                            "Orthorhombic":'o',
+                            "Tetragonal":'t',
+                            "Trigonal":'h',
+                            "Hexagonal":'h',
+                            "Cubic":'c'
+                            }
+
+  def __str__(self):
+    return "%s%s"%(self.crystal_family_symbols[self.crystal_system],
+                   self.centring_symbol)
+
+def tst_bravais_types():
+  from cctbx import sgtbx
+  from cctbx.sgtbx.subgroups import subgroups
+
+  bravais_types_reference = {"aP":2,"mP":8,"mC":5,"mA":0,"mI":0,"mB":0,"oP":30,"oC":11,
+                             "oA":4,"oB":0,"oI":9,"oF":5,"tP":49,"tI":19,"hP":45,"hR":7,
+                             "cP":15,"cI":10,"cF":11}
+
+  bravais_types_tally = {}
+  for key in bravais_types_reference: bravais_types_tally[key]=0
+
+  crystal_systems_reference = {"Triclinic":2,"Monoclinic":13,"Orthorhombic":59,"Tetragonal":68,
+                               "Trigonal":25,"Hexagonal":27,"Cubic":36}
+  crystal_systems_tally = {}
+  for key in crystal_systems_reference: crystal_systems_tally[key]=0
+
+  for space_group_number in xrange(1,231):
+    GC = bravais_lattice(space_group_number)
+    bravais_types_tally[str(GC)]+=1
+    crystal_systems_tally[GC.crystal_system]+=1
+
+    # idea, not fully implemented--more extensive testing by generating all subgroups
+    if False:
+      subgrs = subgroups(parent_group_info).groups_parent_setting()
+      for subgroup in subgrs:
+        subgroup_info = sgtbx.space_group_info(group=subgroup)
+        subgroup_info.show_summary()
+        bravais_lattice(subgroup_info.type().lookup_symbol())
+
+  return (bravais_types_tally==bravais_types_reference and
+          crystal_systems_tally==crystal_systems_reference)
+
 def exercise():
   from cctbx import sgtbx
   import sys
@@ -43,6 +101,7 @@ def exercise():
     for symbol in centric:
       print "/* %s */ %d," % (
         symbol, sgtbx.space_group_info(symbol=symbol).type().number())
+  assert tst_bravais_types()
   print "OK"
 
 if (__name__ == "__main__"):
