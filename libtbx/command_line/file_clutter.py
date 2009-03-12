4cf606960dcc389d7abdfe3cd10d4996c488fbbb
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-03-12 21:39:29 +0000

    extend libtbx.clean_clutter and refactor cleaning to support that

---
+++ libtbx/command_line/file_clutter.py
@@ -0,0 +1,78 @@
+import sys, os
+
+class file_clutter(object):
+
+  def __init__(self, path):
+    self.path = path
+    self.is_executable = os.access(path, os.X_OK)
+    self.dos_format = False
+    self.n_tabs_or_trailing_whitespace = 0
+    self.n_trailing_empty_lines = 0
+    self.missing_eol = False
+    stream = open(path, "rb").read()
+    if (len(stream) > 0):
+      if (stream[-1] != "\n"):
+        self.missing_eol = True
+      else:
+        stream = stream[:-1]
+      text = stream.split("\n")
+      for line in text:
+        if (line.endswith("\r")):
+          line = line[:-1]
+          self.dos_format = True
+        clean_line = line.expandtabs().rstrip()
+        if (clean_line != line): self.n_tabs_or_trailing_whitespace += 1
+        if (len(clean_line) == 0): self.n_trailing_empty_lines += 1
+        else: self.n_trailing_empty_lines = 0
+
+  def is_cluttered(self, flag_x):
+    return ((self.is_executable and flag_x)
+            or self.dos_format
+            or self.n_tabs_or_trailing_whitespace > 0
+            or self.n_trailing_empty_lines > 1
+            or self.missing_eol)
+
+  def status(self, flag_x):
+    status = ""
+    if (self.is_executable and flag_x
+        and self.path.lower().find("command_line") < 0
+        and not self.path.endswith(".csh")
+        and not self.path.endswith(".sh")):
+      status += "is executable, "
+    if (self.dos_format):
+      status += "dos format, "
+    if (self.n_tabs_or_trailing_whitespace > 0):
+      status += "tabs or trailing whitespace=%d, " \
+             % self.n_tabs_or_trailing_whitespace
+    if (self.n_trailing_empty_lines > 1):
+      status += "trailing empty lines=%d, " % self.n_trailing_empty_lines
+    if (self.missing_eol):
+      status += "missing end-of-line, "
+    return status
+
+  def show(self, flag_x):
+    status = self.status(flag_x)
+    if status: print "%s: %s" % (self.path, status)
+
+
+def is_text_file(file_name):
+  name = file_name.lower()
+  for extension in (".c", ".cpp", ".h", ".hpp", ".py", ".java", ".params",
+                    ".dox", ".txt", ".html", ".csh", ".sh", ".cif"):
+    if (name.endswith(extension)): return True
+  return False
+
+def gather(paths):
+  clutter = []
+  for path in paths:
+    if (not os.path.exists(path)):
+      print >> sys.stderr, "No such file or directory:", path
+    elif (os.path.isfile(path)):
+      clutter.append(file_clutter(path))
+    else:
+      for root, dirs, files in os.walk(path):
+        for f in files:
+          if (is_text_file(f)):
+            path = os.path.normpath(os.path.join(root, f))
+            clutter.append(file_clutter(path))
+  return clutter
--- libtbx/command_line/find_clutter.py
+++ libtbx/command_line/find_clutter.py
@@ -1,56 +1,5 @@
-import sys, os
-
-def show_status(path, flag_x):
-  is_executable = os.access(path, os.X_OK)
-  dos_format = False
-  n_tabs_or_trailing_whitespace = 0
-  n_trailing_empty_lines = 0
-  missing_eol = False
-  stream = open(path, "rb").read()
-  if (len(stream) > 0):
-    if (stream[-1] != "\n"):
-      missing_eol = True
-    else:
-      stream = stream[:-1]
-    text = stream.split("\n")
-    for line in text:
-      if (line.endswith("\r")):
-        line = line[:-1]
-        dos_format = True
-      clean_line = line.expandtabs().rstrip()
-      if (clean_line != line): n_tabs_or_trailing_whitespace += 1
-      if (len(clean_line) == 0): n_trailing_empty_lines += 1
-      else: n_trailing_empty_lines = 0
-  status = ""
-  if (is_executable and flag_x
-      and path.lower().find("command_line") < 0
-      and not path.endswith(".csh")
-      and not path.endswith(".sh")):
-    status += "is executable, "
-  if (dos_format):
-    status += "dos format, "
-  if (n_tabs_or_trailing_whitespace > 0):
-    status += "tabs or trailing whitespace=%d, " \
-           % n_tabs_or_trailing_whitespace
-  if (n_trailing_empty_lines > 1):
-    status += "trailing empty lines=%d, " % n_trailing_empty_lines
-  if (missing_eol):
-    status += "missing end-of-line, "
-  if (len(status) > 0):
-    print "%s:" % path, status[:-2]
-
-def is_text_file(file_name):
-  name = file_name.lower()
-  for extension in (".c", ".cpp", ".h", ".hpp", ".py", ".java", ".params",
-                    ".dox", ".txt", ".html", ".csh", ".sh", ".cif"):
-    if (name.endswith(extension)): return True
-  return False
-
-def visitor(flag_x, dirname, names):
-  for file_name in names:
-    if (is_text_file(file_name)):
-      path = os.path.normpath(os.path.join(dirname, file_name))
-      show_status(path, flag_x)
+import sys
+import libtbx.command_line.file_clutter
 
 def run(args):
   flag_x = False
@@ -61,13 +10,8 @@ def run(args):
     else:
       paths.append(arg)
   if (len(paths) == 0): paths = ["."]
-  for path in paths:
-    if (not os.path.exists(path)):
-      print >> sys.stderr, "No such file or directory:", path
-    elif (os.path.isfile(path)):
-      show_status(path, flag_x)
-    else:
-      os.path.walk(path, visitor, flag_x)
+  for cluttered_file in libtbx.command_line.file_clutter.gather(paths):
+    cluttered_file.show(flag_x)
 
 if (__name__ == "__main__"):
   run(sys.argv[1:])
