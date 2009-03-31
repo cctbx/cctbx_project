d0bf7c6b9fad1952e6a5227b62c4c6f760aae36e
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2009-03-31 20:02:54 +0000

    libtbx.full_command_path -> libtbx.show_full_command_path

--- libtbx/command_line/show_full_command_path.py
+++ libtbx/command_line/show_full_command_path.py
@@ -1,10 +1,9 @@
-
 import libtbx.path
 import sys
 
 def run (args) :
   assert len(args) == 1
-  print libtbx.path.full_command_path(args[0])
+  print libtbx.path.full_command_path(command=args[0])
 
 if __name__ == "__main__" :
   run(sys.argv[1:])
