3c002f1cfccc73fba11781e905ef2767247afcd2
Richard Gildea <richard.gildea@diamond.ac.uk>
2009-04-08 13:22:07 +0000

    libtbx/command_line/find_clutter.py: add -ndos option to libtbx.find_clutter to ignore dos format warnings

--- libtbx/command_line/find_clutter.py
+++ libtbx/command_line/find_clutter.py
@@ -3,15 +3,18 @@ import libtbx.command_line.file_clutter
 
 def run(args):
   flag_x = False
+  flag_dos_format = True
   paths = []
   for arg in args:
     if (arg == "-x"):
       flag_x = True
+    elif (arg == "-ndos"):
+      flag_dos_format = False
     else:
       paths.append(arg)
   if (len(paths) == 0): paths = ["."]
   for cluttered_file in libtbx.command_line.file_clutter.gather(paths):
-    cluttered_file.show(flag_x)
+    cluttered_file.show(flag_x, flag_dos_format)
 
 if (__name__ == "__main__"):
   run(sys.argv[1:])
