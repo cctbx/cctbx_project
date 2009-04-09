132ed5782c9ebebfe12ca7d79fcd5775018b574d
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-04-09 12:25:36 +0000

    C99 copysign

---
+++ scitbx/math/copysign.h
@@ -0,0 +1,34 @@
+#ifndef SCITBX_MATH_COPY_SIGN_H
+#define SCITBX_MATH_COPY_SIGN_H
+
+#if defined(__GNUC__) && __GNUC__ == 4
+  #include <math.h>
+  namespace scitbx { namespace math {
+    double copysign(double x, double y) {
+      return ::copysign(x,y);
+    }
+    float copysign(float x, float y) {
+      return ::copysignf(x,y);
+    }
+  }}
+#elif defined(BOOST_MSVC) && (BOOST_MSVC >= 1310)
+  // VC++ 7.1 or newer
+  #include <math.h>
+  #include <boost/math/special_functions/sign.hpp>
+  namespace scitbx { namespace math {
+    // VC++ doesn't provide the float version
+    double copysign(double x, double y) {
+      return ::_copysign(x,y);
+    }
+    float copysign(float x, float y) {
+      return boost::math::copysign(x,y);
+    }
+  }}
+#else
+  #include <boost/math/special_functions/sign.hpp>
+  namespace scitbx { namespace math {
+    using boost::math::copysign;
+  }}
+#endif
+
+#endif // GUARD
