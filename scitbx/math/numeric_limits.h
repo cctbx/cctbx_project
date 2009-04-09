48455419b210babe883068d9904b4678f5aed4b4
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-04-09 12:26:24 +0000

    new scitbx::math::numeric_limits

---
+++ scitbx/math/numeric_limits.h
@@ -0,0 +1,26 @@
+#ifndef SCITBX_MATH_NUMERIC_LIMITS
+#define SCITBX_MATH_NUMERIC_LIMITS
+
+#include <limits>
+#include <cmath>
+
+namespace scitbx { namespace math {
+
+/// Extension of std::numeric_limits for LAPACK-style floating point info
+template <typename T>
+class numeric_limits : public std::numeric_limits<T>
+{
+  public:
+    /// Safe minimum, such that 1/safe_min does not overflow
+    static T safe_min() {
+      return std::pow(T(std::numeric_limits<T>::radix),
+                      std::numeric_limits<T>::min_exponent);
+    }
+
+
+};
+
+
+}} // scitbx::math
+
+#endif // GUARD
