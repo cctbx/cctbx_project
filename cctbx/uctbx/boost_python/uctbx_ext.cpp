46163d2cc93bfa5a2c35f6f18f271e7dd40c932e
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2009-04-24 18:36:41 +0000

    cctbx/uctbx/boost_python/uctbx_ext.cpp: Visual C++ 7.1 compatibility

--- cctbx/uctbx/boost_python/uctbx_ext.cpp
+++ cctbx/uctbx/boost_python/uctbx_ext.cpp
@@ -97,7 +97,9 @@ namespace {
         .def("fractionalization_matrix", &w_t::fractionalization_matrix, ccr())
         .def("orthogonalization_matrix", &w_t::orthogonalization_matrix, ccr())
         .def("grid_index_as_site_cart_matrix",
-          &w_t::grid_index_as_site_cart_matrix<int>, (arg_("gridding")))
+          (uc_mat3 (w_t::*)(scitbx::vec3<int> const&) const)
+            &w_t::grid_index_as_site_cart_matrix,
+              (arg_("gridding")))
         .def("fractionalize",
           (scitbx::vec3<double>(w_t::*)(scitbx::vec3<double> const&) const)
           &w_t::fractionalize, (
