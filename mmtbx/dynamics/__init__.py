ea194a3d286e92c11bc6f1d5b7a2d50af5081b40
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2009-04-27 00:13:58 +0000

    mmtbx/dynamics/__init__.py: new kinetic_energy_as_temperature(), temperature_as_kinetic_energy()

--- mmtbx/dynamics/__init__.py
+++ mmtbx/dynamics/__init__.py
@@ -4,13 +4,21 @@ import boost.python
 ext = boost.python.import_ext("mmtbx_dynamics_ext")
 from mmtbx_dynamics_ext import *
 
+def kinetic_energy_as_temperature(dof, e):
+  from mmtbx.dynamics.constants import boltzmann_constant_akma as k
+  return e / (0.5 * k * dof)
+
+def temperature_as_kinetic_energy(dof, t):
+  from mmtbx.dynamics.constants import boltzmann_constant_akma as k
+  return t * (0.5 * k * dof)
+
 class kinetic_energy_and_temperature(object):
 
   def __init__(O, velocities, masses):
-    from mmtbx.dynamics.constants import boltzmann_constant_akma
     O.kinetic_energy = kinetic_energy(velocities=velocities, masses=masses)
     dof = 3 * velocities.size()
     if (dof == 0):
       O.temperature = 0
     else:
-      O.temperature = 2 * O.kinetic_energy / (dof * boltzmann_constant_akma)
+      O.temperature = kinetic_energy_as_temperature(
+        dof=dof, e=O.kinetic_energy)
