8709f9631db3eab6eafefd105411649512c377cb
Pavel Afonine <pafonine@users.noreply.github.com>
2009-04-29 05:23:52 +0000

    new tool to compute fo-fo maps, fixes in cartesian.py

--- mmtbx/maps.py
+++ mmtbx/maps.py
@@ -45,6 +45,7 @@ import libtbx.load_env
 from mmtbx import utils
 from mmtbx import max_lik
 from mmtbx.max_lik import maxlik
+from iotbx.pdb import combine_unique_pdb_files
 
 
 map_params_str ="""\
@@ -459,3 +460,213 @@ class interpreter:
       force_symmetry=True,
       reflection_files=self.reflection_files,
       err=self.log)
+
+### Fo-Fo map calculation code
+fo_minus_fo_master_params_str = """\
+f_obs_1_file_name = None
+  .type = str
+  .help = File with Fobs data
+  .expert_level = 1
+f_obs_1_label = None
+  .type = str
+  .expert_level = 1
+
+f_obs_2_file_name = None
+  .type = str
+  .help = File with Fobs data
+  .expert_level = 1
+f_obs_2_label = None
+  .type = str
+  .expert_level = 1
+high_resolution = None
+  .type = float
+  .help = High resolution data cutoff
+  .expert_level = 1
+low_resolution = None
+  .type = float
+  .help = Low resolution data cutoff
+  .expert_level = 1
+sigma_cutoff = None
+  .type = float
+  .help = Fobs sigma cutoff
+  .expert_level = 1
+phase_source = None
+  .type = str
+  .help = PDB file with a model or reflection file with the phases
+  .expert_level = 1
+scattering_table = *xray neutron
+  .type = choice(multi=False)
+  .help = Choices of scattering table for structure factors calculations
+  .expert_level=1
+"""
+def fo_minus_fo_master_params():
+  return iotbx.phil.parse(fo_minus_fo_master_params_str, process_includes=False)
+
+def compute_fo_minus_fo_map(data_arrays, xray_structure, log, silent):
+  fmodels = []
+  for i_seq, d in enumerate(data_arrays):
+    if(not silent):
+      print >> log, "Data set: %d"%i_seq
+    r_free_flags = d.array(data = flex.bool(d.data().size(), False))
+    fmodel = mmtbx.f_model.manager(
+      xray_structure = xray_structure,
+      r_free_flags   = r_free_flags,
+      target_name    = "ls_wunit_k1",
+      f_obs          = d)
+    fmodel.remove_outliers()
+    fmodel.update_solvent_and_scale()
+    if(not silent):
+      fmodel.info().show_rfactors_targets_scales_overall()
+      print >> log
+    fmodels.append(fmodel)
+  # prepare Fobs for map calculation (apply scaling):
+  f_obss = []
+  for fmodel in fmodels:
+    obs = fmodel.f_obs
+    fb_cart  = fmodel.fb_cart()
+    scale_k2 = fmodel.scale_k2()
+    f_obs_scale   = 1.0 / fb_cart * scale_k2
+    obs = miller.array(miller_set = fmodel.f_model(),
+                       data       = obs.data()*f_obs_scale)
+    f_obss.append(obs)
+  # given two Fobs sets, make them one-to-one matching, get phases and map coefficients
+  # Note: f_calc below is just f_calc from atoms (no bulk solvent etc applied)
+  fobs_1, f_model = f_obss[0].common_sets(other = fmodels[0].f_model())
+  fobs_1, fobs_2 = fobs_1.common_sets(other = f_obss[1])
+  fobs_1, f_model = fobs_1.common_sets(other = f_model)
+  assert fobs_2.indices().all_eq(fobs_1.indices())
+  assert f_model.indices().all_eq(fobs_1.indices())
+  # scale again
+  scale_k1 = 1
+  den = flex.sum(flex.abs(fobs_2.data())*flex.abs(fobs_2.data()))
+  if(den != 0):
+    scale_k1 = flex.sum(flex.abs(fobs_1.data())*flex.abs(fobs_2.data())) / den
+  # map coefficients
+  diff = miller.array(
+    miller_set = f_model,
+    data       = fobs_1.data()-fobs_2.data()*scale_k1)
+  def phase_transfer(miller_array, phase_source):
+    tmp = miller.array(miller_set = miller_array,
+      data = flex.double(miller_array.indices().size(), 1)
+      ).phase_transfer(phase_source = phase_source)
+    return miller.array(miller_set = miller_array,
+      data = miller_array.data() * tmp.data() )
+  map_coeff = phase_transfer(
+    miller_array = diff,
+    phase_source = f_model)
+  # output MTZ file with map coefficients
+  class map_coeffs_mtz_label_manager:
+    def __init__(self, amplitudes, phases):
+      self._amplitudes = amplitudes
+      self._phases = phases
+    def amplitudes(self):
+      return self._amplitudes
+    def phases(self, root_label, anomalous_sign=None):
+      assert anomalous_sign is None or not anomalous_sign
+      return self._phases
+  mtz_history_buffer = flex.std_string()
+  lbl_mgr = map_coeffs_mtz_label_manager(amplitudes = "FoFo", phases = "PHFc")
+  if(map_coeff.anomalous_flag()):
+    map_coeff = map_coeff.average_bijvoet_mates()
+  mtz_dataset = map_coeff.as_mtz_dataset(
+    column_root_label=lbl_mgr.amplitudes(),
+    label_decorator=lbl_mgr)
+  mtz_history_buffer.append("> column label %s = phenix %s" % (
+      lbl_mgr.amplitudes(), "FoFoPHFc"))
+  file_name = "FoFoPHFc.mtz"
+  mtz_history_buffer.append("file name %s"%file_name)
+  mtz_object = mtz_dataset.mtz_object()
+  mtz_object.add_history(mtz_history_buffer)
+  mtz_object.write(file_name=file_name)
+
+def run_fobs_minus_fobs_map(args, command_name = "phenix.fobs_minus_fobs_map"):
+  if(len(args) == 0): args = ["--help"]
+  examples = """Examples:
+
+phenix.fobs_minus_fobs_map f_obs_1_file=data1.mtz f_obs_2_file=data2.sca \
+f_obs_1_label=FOBS1 f_obs_2_label=FOBS2 model.pdb
+
+phenix.fobs_minus_fobs_map f_obs_1_file=data.mtz f_obs_2_file=data.mtz \
+f_obs_1_label=FOBS1 f_obs_2_label=FOBS2 phase_source=model.pdb \
+high_res=2.0 sigma_cutoff=2 scattering_table=neutron"""
+
+  command_line = (iotbx_option_parser(
+    usage="%s [options]" % command_name,
+    description=examples)
+    .option("--silent",
+      action="store_true",
+      help="Suppress output to the screen.")
+    ).process(args=args)
+  #
+  log = sys.stdout
+  if(not command_line.options.silent):
+    utils.print_header("phenix.fobs_minus_fobs_map", out = log)
+    print >> log, "Command line arguments: "
+    print >> log, args
+    print >> log
+  #
+  processed_args = utils.process_command_line_args(args = command_line.args,
+    master_params = fo_minus_fo_master_params(), log = log)
+  crystal_symmetry = processed_args.crystal_symmetry
+  if(crystal_symmetry is None):
+    raise Sorry("No crystal symmetry found.")
+  if(len(processed_args.pdb_file_names) == 0):
+    raise Sorry("No PDB file found.")
+  pdb_file_names = processed_args.pdb_file_names
+  params = processed_args.params
+  #
+  if(not command_line.options.silent):
+    print >> log, "*** Parameters:"
+    params.show(out = log)
+    print >> log
+  params = params.extract()
+  # Extaract Fobs1, Fobs2
+  if([params.f_obs_1_file_name,params.f_obs_2_file_name].count(None)==2):
+    raise Sorry("No reflection data file found.")
+  f_obss = []
+  for file_name, label in zip([params.f_obs_1_file_name,params.f_obs_2_file_name],
+                              [params.f_obs_1_label,params.f_obs_2_label]):
+    reflection_file = reflection_file_reader.any_reflection_file(
+      file_name = file_name, ensure_read_access = False)
+    reflection_file_server = reflection_file_utils.reflection_file_server(
+      crystal_symmetry = crystal_symmetry,
+      force_symmetry   = True,
+      reflection_files = [reflection_file],
+      err              = StringIO())
+    parameters = utils.data_and_flags.extract()
+    if(label is not None):
+      parameters.labels = [label]
+    determine_data_and_flags_result = utils.determine_data_and_flags(
+        reflection_file_server  = reflection_file_server,
+        parameters              = parameters,
+        keep_going              = True,
+        log                     = StringIO())
+    f_obss.append(determine_data_and_flags_result.f_obs)
+  if(len(f_obss)!=2):
+    raise Sorry(" ".join(errors))
+  if(not command_line.options.silent):
+    for ifobs, fobs in enumerate(f_obss):
+      print >> log, "*** Summary for data set %d:"%ifobs
+      fobs.show_comprehensive_summary(f = log)
+      print >> log
+  pdb_combined = combine_unique_pdb_files(file_names = pdb_file_names)
+  pdb_combined.report_non_unique(out = log)
+  if(len(pdb_combined.unique_file_names) == 0):
+    raise Sorry("No coordinate file given.")
+  xray_structure = iotbx.pdb.input(source_info = None, lines =
+    pdb_combined.raw_records).xray_structure_simple()
+  if(not command_line.options.silent):
+    print >> log, "*** Model summary:"
+    xray_structure.show_summary(f = log)
+    print >> log
+  f_obss[0] = f_obss[0].resolution_filter(d_min = params.high_resolution,
+    d_max = params.low_resolution)
+  f_obss[1] = f_obss[1].resolution_filter(d_min = params.high_resolution,
+    d_max = params.low_resolution)
+  if(params.sigma_cutoff is not None):
+    for i in [0,1]:
+      if(f_obss[i].sigmas() is not None):
+        sel = f_obss[i].data() > f_obss[i].sigmas()*params.sigma_cutoff
+        f_obss[i] = f_obss[i].select(sel)
+  compute_fo_minus_fo_map(data_arrays = f_obss, xray_structure = xray_structure,
+    log = log, silent = command_line.options.silent)
