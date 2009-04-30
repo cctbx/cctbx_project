16c904f94a24355a43316a4ddf03e65ad48da057
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2009-04-30 03:05:41 +0000

    iotbx.crystal_symmetry_from_any: show also as CRYST1 record

--- iotbx/command_line/crystal_symmetry_from_any.py
+++ iotbx/command_line/crystal_symmetry_from_any.py
@@ -1,14 +1,18 @@
 from iotbx import crystal_symmetry_from_any
+from iotbx.pdb import format_cryst1_and_scale_records
 from iotbx import format
 import sys
 
-def run():
-  assert len(sys.argv) == 2
-  crystal_symmetry = crystal_symmetry_from_any.extract_from(sys.argv[1])
+def run(args):
+  assert len(args) == 1
+  crystal_symmetry = crystal_symmetry_from_any.extract_from(args[0])
   if (crystal_symmetry is None):
     raise RuntimeError, \
       "Unknown file format or unit cell and/or space group missing from file."
   format.crystal_symmetry(crystal_symmetry)
+  print format_cryst1_and_scale_records(
+    crystal_symmetry=crystal_symmetry,
+    write_scale_records=True)
 
 if (__name__ == "__main__"):
-  run()
+  run(args=sys.argv[1:])
