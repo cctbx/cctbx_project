fa7e13ac94f339bd7323ddd9fce06a038925d06e
Richard Gildea <richard.gildea@diamond.ac.uk>
2009-05-03 12:23:49 +0000

    smtbx/refinement/restraints/adp_restraints.py: code wasn't correct if optional pair_sym_tables argument was not None, now tested.

--- smtbx/refinement/restraints/adp_restraints.py
+++ smtbx/refinement/restraints/adp_restraints.py
@@ -3,8 +3,9 @@ from cctbx import crystal
 from cctbx import adp_restraints
 
 class adp_similarity_restraints(object):
-  def __init__(self, xray_structure, pair_sym_table=None, i_seqs=None,
+  def __init__(self, xray_structure=None, pair_sym_table=None, i_seqs=None,
                sigma=0.04, sigma_terminal=None, buffer_thickness=3.5):
+    assert [xray_structure, pair_sym_table].count(None) == 1
     if sigma_terminal is None: sigma_terminal = 2 * sigma
     proxies = adp_restraints.shared_adp_similarity_proxy()
     if pair_sym_table is None:
@@ -14,7 +15,7 @@ class adp_similarity_restraints(object):
       pair_asu_table.add_covalent_pairs(
         scattering_types, exclude_scattering_types=flex.std_string(("H","D")))
       pair_sym_table = pair_asu_table.extract_pair_sym_table()
-      connectivity = pair_sym_table.full_simple_connectivity()
+    connectivity = pair_sym_table.full_simple_connectivity()
 
     for i_seq, j_seq_dict in enumerate(pair_sym_table):
       if i_seqs is not None and i_seq not in i_seqs: continue
@@ -33,11 +34,12 @@ class adp_similarity_restraints(object):
     self.proxies = proxies
 
 class rigid_bond_restraints(object):
-  def __init__(self, xray_structure, pair_sym_table=None, i_seqs=None,
+  def __init__(self, xray_structure=None, pair_sym_table=None, i_seqs=None,
                sigma_12=0.01, sigma_13=None, buffer_thickness=3.5):
     """ sigma_12 and sigma_13 are the effective standard deviations used for
         1,2- and 1,3-distances respectively
     """
+    assert [xray_structure, pair_sym_table].count(None) == 1
     if sigma_13 is None: sigma_13 = sigma_12
     proxies = adp_restraints.shared_rigid_bond_proxy()
     if pair_sym_table is None:
@@ -47,7 +49,7 @@ class rigid_bond_restraints(object):
       pair_asu_table.add_covalent_pairs(
         scattering_types, exclude_scattering_types=flex.std_string(("H","D")))
       pair_sym_table = pair_asu_table.extract_pair_sym_table()
-      connectivity = pair_sym_table.full_simple_connectivity()
+    connectivity = pair_sym_table.full_simple_connectivity()
 
     for i_seq, j_seq_dict in enumerate(pair_sym_table):
       if i_seqs is not None and i_seq not in i_seqs: continue
@@ -84,18 +86,18 @@ class isotropic_adp_restraints(object):
                sigma=0.1, sigma_terminal=None, buffer_thickness=3.5):
     if sigma_terminal is None: sigma_terminal = 2 * sigma
     proxies = adp_restraints.shared_isotropic_adp_proxy()
+    scattering_types = xray_structure.scatterers().extract_scattering_types()
     if pair_sym_table is None:
       asu_mappings = xray_structure.asu_mappings(buffer_thickness=buffer_thickness)
       pair_asu_table = crystal.pair_asu_table(asu_mappings=asu_mappings)
-      scattering_types = xray_structure.scatterers().extract_scattering_types()
       pair_asu_table.add_covalent_pairs(
         scattering_types, exclude_scattering_types=flex.std_string(("H","D")))
       pair_sym_table = pair_asu_table.extract_pair_sym_table()
-      connectivity = pair_sym_table.full_simple_connectivity()
+    connectivity = pair_sym_table.full_simple_connectivity()
 
     for i_seq , neighbours in enumerate(connectivity):
       if i_seqs is not None and i_seq not in i_seqs: continue
-      elif scattering_types[i_seq] == 'H': continue
+      elif scattering_types[i_seq] in ('H','D'): continue
       if neighbours.size() <= 1:
         weight = 1/(sigma_terminal*sigma_terminal)
       else:
--- smtbx/refinement/restraints/tests/tst_adp_restraints.py
+++ smtbx/refinement/restraints/tests/tst_adp_restraints.py
@@ -1,6 +1,8 @@
 from libtbx.test_utils import approx_equal
 from smtbx.refinement.restraints import adp_restraints
 from iotbx import shelx
+from cctbx.array_family import flex
+from cctbx import crystal
 import cStringIO
 
 def trial_structure():
@@ -13,62 +15,86 @@ def trial_structure():
   l_xs.parse()
   return l_xs.builder.structure
 
+def get_pair_sym_table(xray_structure):
+  asu_mappings = xray_structure.asu_mappings(buffer_thickness=3.5)
+  pair_asu_table = crystal.pair_asu_table(asu_mappings=asu_mappings)
+  scattering_types = xray_structure.scatterers().extract_scattering_types()
+  pair_asu_table.add_covalent_pairs(
+    scattering_types, exclude_scattering_types=flex.std_string(("H","D")))
+  return pair_asu_table.extract_pair_sym_table()
+
 def exercise_adp_similarity():
   xray_structure = trial_structure()
-  restraints = \
-    adp_restraints.adp_similarity_restraints(
-      xray_structure=xray_structure)
-  assert restraints.proxies.size() == 24
-  i_seqs = (9,14,28,32,36,38)
-  restraints = \
-    adp_restraints.adp_similarity_restraints(
-      xray_structure=xray_structure,
-      i_seqs=i_seqs)
-  expected_i_seqs = ((9,32),(14,36),(32,36),(36,38))
-  expected_weights = (625,156.25,625,625)
-  proxies = restraints.proxies
-  assert proxies.size() == len(expected_i_seqs)
-  for i in range(proxies.size()):
-    assert approx_equal(proxies[i].i_seqs, expected_i_seqs[i])
-    assert approx_equal(proxies[i].weight, expected_weights[i])
+  pair_sym_table = get_pair_sym_table(xray_structure)
+  for table in (None,pair_sym_table):
+    if table is None: xs = xray_structure
+    else: xs = None
+    restraints = \
+      adp_restraints.adp_similarity_restraints(
+        xray_structure=xs,
+        pair_sym_table=table)
+    assert restraints.proxies.size() == 24
+    i_seqs = (9,14,28,32,36,38)
+    restraints = \
+      adp_restraints.adp_similarity_restraints(
+        xray_structure=xs,
+        pair_sym_table=table,
+        i_seqs=i_seqs)
+    expected_i_seqs = ((9,32),(14,36),(32,36),(36,38))
+    expected_weights = (625,156.25,625,625)
+    proxies = restraints.proxies
+    assert proxies.size() == len(expected_i_seqs)
+    for i in range(proxies.size()):
+      assert approx_equal(proxies[i].i_seqs, expected_i_seqs[i])
+      assert approx_equal(proxies[i].weight, expected_weights[i])
 
 def exercise_rigid_bond():
   xray_structure = trial_structure()
-  restraints = \
-    adp_restraints.rigid_bond_restraints(
-      xray_structure=xray_structure)
-  assert restraints.proxies.size() == 60
-  i_seqs = (9,14,28,32,36,38)
-  restraints = \
-    adp_restraints.rigid_bond_restraints(
-      xray_structure=xray_structure,
-      i_seqs=i_seqs)
-  expected_i_seqs = (
-    (9,32),(9,36),(14,36),(14,32),(14,38),(32,36),(32,38),(36,38))
-  expected_weights = [10000]*len(expected_i_seqs)
-  proxies = restraints.proxies
-  assert proxies.size() == len(expected_i_seqs)
-  for i in range(proxies.size()):
-    assert approx_equal(proxies[i].i_seqs, expected_i_seqs[i])
-    assert approx_equal(proxies[i].weight, expected_weights[i])
+  pair_sym_table = get_pair_sym_table(xray_structure)
+  for table in (None,pair_sym_table):
+    if table is None: xs = xray_structure
+    else: xs = None
+    restraints = \
+      adp_restraints.rigid_bond_restraints(
+        xray_structure=xs,
+        pair_sym_table=table)
+    assert restraints.proxies.size() == 60
+    i_seqs = (9,14,28,32,36,38)
+    restraints = \
+      adp_restraints.rigid_bond_restraints(
+        xray_structure=xs,
+        pair_sym_table=table,
+        i_seqs=i_seqs)
+    expected_i_seqs = (
+      (9,32),(9,36),(14,36),(14,32),(14,38),(32,36),(32,38),(36,38))
+    expected_weights = [10000]*len(expected_i_seqs)
+    proxies = restraints.proxies
+    assert proxies.size() == len(expected_i_seqs)
+    for i in range(proxies.size()):
+      assert approx_equal(proxies[i].i_seqs, expected_i_seqs[i])
+      assert approx_equal(proxies[i].weight, expected_weights[i])
 
 def exercise_isotropic_adp():
   xray_structure = trial_structure()
-  restraints = \
-    adp_restraints.isotropic_adp_restraints(
-      xray_structure=xray_structure)
-  assert restraints.proxies.size() == 23
-  i_seqs = (9,14,28,32,36,38)
-  expected_weights = (100,25,100,100,100,100)
-  restraints = \
-    adp_restraints.isotropic_adp_restraints(
-      xray_structure=xray_structure,
-      i_seqs=i_seqs)
-  proxies = restraints.proxies
-  assert proxies.size() == len(i_seqs)
-  for i in range(proxies.size()):
-    assert approx_equal(proxies[i].i_seq, i_seqs[i])
-    assert approx_equal(proxies[i].weight, expected_weights[i])
+  pair_sym_table = get_pair_sym_table(xray_structure)
+  for table in (None,pair_sym_table):
+    restraints = \
+      adp_restraints.isotropic_adp_restraints(
+        xray_structure=xray_structure,
+        pair_sym_table=table)
+    assert restraints.proxies.size() == 23
+    i_seqs = (9,14,28,32,36,38)
+    expected_weights = (100,25,100,100,100,100)
+    restraints = \
+      adp_restraints.isotropic_adp_restraints(
+        xray_structure=xray_structure,
+        pair_sym_table=table,
+        i_seqs=i_seqs)
+    proxies = restraints.proxies
+    assert proxies.size() == len(i_seqs)
+    for i in range(proxies.size()):
+      assert approx_equal(proxies[i].i_seq, i_seqs[i])
+      assert approx_equal(proxies[i].weight, expected_weights[i])
 
 sucrose = """
 CELL 0.71073 7.783 8.7364 10.9002 90 102.984 90
