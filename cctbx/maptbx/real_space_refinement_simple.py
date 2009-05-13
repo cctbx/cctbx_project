e392aa541bfb8a0ec007333d8039a396d5e91141
Pavel Afonine <pafonine@users.noreply.github.com>
2009-05-13 18:44:04 +0000

    whole map based rsr (very slow)

--- cctbx/maptbx/real_space_refinement_simple.py
+++ cctbx/maptbx/real_space_refinement_simple.py
@@ -1,18 +1,21 @@
 from cctbx import maptbx
 from cctbx.array_family import flex
 import scitbx.lbfgs
+from cctbx import miller
 
 class lbfgs(object):
 
   def __init__(O,
-        geometry_restraints_manager,
         sites_cart,
         density_map,
         gradients_delta,
         real_space_weight_scale,
+        unit_cell,
+        geometry_restraints_manager = None,
         lbfgs_termination_params=None,
         lbfgs_exception_handling_params=None):
     O.gr = geometry_restraints_manager
+    O.unit_cell = unit_cell
     O.density_map = density_map
     O.gradients_delta = gradients_delta
     O.rs_weight_scale = real_space_weight_scale
@@ -28,27 +31,118 @@ class lbfgs(object):
 
   def compute_functional_and_gradients(O):
     sites_cart = flex.vec3_double(O.x)
-    unit_cell = O.gr.crystal_symmetry.unit_cell()
-    gr_e = O.gr.energies_sites(sites_cart=sites_cart, compute_gradients=True)
-    gr_f = gr_e.target
-    gr_g = gr_e.gradients
+    gr_e, gr_f, gr_g = [None]*3
+    if(O.gr is not None):
+      gr_e = O.gr.energies_sites(sites_cart=sites_cart, compute_gradients=True)
+      gr_f = gr_e.target
+      gr_g = gr_e.gradients
     rs_f = -maptbx.real_space_target_simple(
-      unit_cell=unit_cell,
+      unit_cell=O.unit_cell,
       density_map=O.density_map,
       sites_cart=sites_cart)
     rs_g = maptbx.real_space_gradients_simple(
-      unit_cell=unit_cell,
+      unit_cell=O.unit_cell,
       density_map=O.density_map,
       sites_cart=sites_cart,
       delta=O.gradients_delta)
     rs_g *= -1
-    if (O.rs_weight is None):
+    if (O.rs_weight is None and O.gr is not None):
       rms_gr_g = flex.mean_sq(gr_g.as_double())**0.5
       rms_rs_g = flex.mean_sq(rs_g.as_double())**0.5
       if (rms_rs_g < 1):
         O.rs_weight = 1
       else:
         O.rs_weight = rms_gr_g / rms_rs_g * O.rs_weight_scale
-    f = gr_f + rs_f * O.rs_weight
-    g = gr_g + rs_g * O.rs_weight
+    if(O.gr is not None):
+      f = gr_f + rs_f * O.rs_weight
+      g = gr_g + rs_g * O.rs_weight
+    else:
+      f = rs_f
+      g = rs_g
     return f, g.as_double()
+
+class minimization(object):
+  def __init__(self,
+        xray_structure,
+        miller_array,
+        crystal_gridding,
+        map_target,
+        max_iterations,
+        min_iterations,
+        step):
+    self.step = step
+    self.sites_cart = xray_structure.sites_cart()
+    self.x = self.sites_cart.as_double()
+    self.xray_structure = xray_structure
+    self.map_target = map_target
+    self.miller_array = miller_array
+    self.crystal_gridding = crystal_gridding
+    self.minimizer = scitbx.lbfgs.run(
+      target_evaluator=self,
+      termination_params=scitbx.lbfgs.termination_parameters(
+        max_iterations=max_iterations,
+        min_iterations=min_iterations),
+      exception_handling_params=scitbx.lbfgs.exception_handling_parameters(
+        ignore_line_search_failed_rounding_errors=True,
+        ignore_line_search_failed_step_at_lower_bound=True,
+        ignore_line_search_failed_maxfev=True))
+    self.sites_cart.clear()
+    self.sites_cart.extend(flex.vec3_double(self.x))
+    self.xray_structure = self.xray_structure.replace_sites_cart(self.sites_cart)
+
+  def compute_functional_and_gradients(self):
+    self.sites_cart = flex.vec3_double(self.x)
+    self.xray_structure = self.xray_structure.replace_sites_cart(self.sites_cart)
+    self.map_current = self.compute_map()
+    f = 0
+    g = flex.vec3_double(self.sites_cart.size(), (0,0,0))
+    f += self.target()
+    g = self.gradients()
+    return f, g.as_double()
+
+  def target(self):
+    r = self.map_target-self.map_current
+    r = r*r
+    r = flex.sum(r)
+    return r
+
+  def gradients(self):
+    g = flex.vec3_double()
+    uc = self.xray_structure.unit_cell()
+    r = 2 * (self.map_target - self.map_current)
+    step_x = flex.double([self.step,0,0])
+    step_y = flex.double([0,self.step,0])
+    step_z = flex.double([0,0,self.step])
+    for site_cart in self.xray_structure.sites_cart():
+      site_cart = flex.double(site_cart)
+      sxp = uc.fractionalize(tuple(site_cart+step_x))
+      sxm = uc.fractionalize(tuple(site_cart-step_x))
+      syp = uc.fractionalize(tuple(site_cart+step_y))
+      sym = uc.fractionalize(tuple(site_cart-step_y))
+      szp = uc.fractionalize(tuple(site_cart+step_z))
+      szm = uc.fractionalize(tuple(site_cart-step_z))
+      gx = (self.map_target.eight_point_interpolation(sxp) -
+            self.map_target.eight_point_interpolation(sxm)) / (2*self.step)
+      gy = (self.map_target.eight_point_interpolation(syp) -
+            self.map_target.eight_point_interpolation(sym)) / (2*self.step)
+      gz = (self.map_target.eight_point_interpolation(szp) -
+            self.map_target.eight_point_interpolation(szm)) / (2*self.step)
+      if 0: # Equivalent to lines below
+        s = 2*(map_target.eight_point_interpolation(site)-
+          map_current.eight_point_interpolation(site))
+        g.append([gx*s,gy*s,gz*s])
+      site_frac = uc.fractionalize(tuple(site_cart))
+      x = (gx*r).eight_point_interpolation(site_frac)
+      y = (gy*r).eight_point_interpolation(site_frac)
+      z = (gz*r).eight_point_interpolation(site_frac)
+      g.append([x,y,z])
+    return g
+
+  def compute_map(self):
+    map_coefficients = self.miller_array.structure_factors_from_scatterers(
+      xray_structure = self.xray_structure).f_calc()
+    fft_map = miller.fft_map(crystal_gridding     = self.crystal_gridding,
+                             fourier_coefficients = map_coefficients)
+    fft_map.apply_sigma_scaling()
+    result = fft_map.real_map_unpadded()
+    return result
--- cctbx/maptbx/tst_real_space_refinement_simple.py
+++ cctbx/maptbx/tst_real_space_refinement_simple.py
@@ -9,6 +9,10 @@ from cctbx import uctbx
 from cctbx.array_family import flex
 from scitbx.graph import tst_tardy_pdb
 import sys
+from cctbx.development import random_structure
+from cctbx import sgtbx
+from cctbx import adptbx
+import random
 
 def construct_geometry_restraints_manager(test_case):
   sites = test_case.sites
@@ -73,6 +77,7 @@ def run(args):
     geo_manager.energies_sites(sites_cart=structure.sites_cart()).show()
     minimized = real_space_refinement_simple.lbfgs(
       geometry_restraints_manager=geo_manager,
+      unit_cell=structure.unit_cell(),
       sites_cart=structure.sites_cart(),
       density_map=fft_map.real_map(),
       gradients_delta=d_min/4,
@@ -84,5 +89,49 @@ def run(args):
     print
   print "OK"
 
+def run_minimization():
+  if (1):
+    random.seed(0)
+    flex.set_random_seed(0)
+  def compute_map(xray_structure, d_min=1.5, resolution_factor=1./4):
+    fc = xray_structure.structure_factors(d_min = d_min).f_calc()
+    fft_map = fc.fft_map(resolution_factor=resolution_factor)
+    fft_map.apply_sigma_scaling()
+    result = fft_map.real_map_unpadded()
+    return result, fc, fft_map
+  xrs = random_structure.xray_structure(
+    space_group_info = sgtbx.space_group_info("P1"),
+    unit_cell        = (10, 20, 30, 70, 80, 120),
+    elements         =(("O","N","C")*10),
+    volume_per_atom  = 50,
+    min_distance     = 2,
+    u_iso            = adptbx.b_as_u(10.),
+    use_u_iso        = True)
+  map_target,tmp,tmp = compute_map(xray_structure = xrs)
+  xrs_sh = xrs.deep_copy_scatterers()
+  xrs_sh.shake_sites_in_place(mean_distance=0.5)
+  start_error = flex.mean(xrs.distances(other = xrs_sh))
+  print "Start:", start_error
+  map_current, miller_array, crystal_gridding = compute_map(xray_structure = xrs_sh)
+  steps1 = [1.0,0.5,0.25,0.1,0.05,0.01,0.001]
+  steps1.reverse()
+  steps2 = [1.0,0.5,0.25,0.1,0.05,0.01,0.001]
+  for step in steps1+steps2:
+    minimized = real_space_refinement_simple.minimization(
+      xray_structure   = xrs_sh,
+      miller_array     = miller_array,
+      crystal_gridding = crystal_gridding,
+      map_target       = map_target,
+      max_iterations   = 500,
+      min_iterations   = 25,
+      step             = step)
+    xrs_sh = minimized.xray_structure
+    map_current = minimized.map_current
+    final_error = flex.mean(xrs.distances(other = minimized.xray_structure))
+    print "Final:", final_error
+  assert start_error >= 0.5
+  assert final_error < 0.021
+
 if (__name__ == "__main__"):
   run(args=sys.argv[1:])
+  run_minimization()
