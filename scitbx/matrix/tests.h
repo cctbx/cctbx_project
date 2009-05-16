6067d9e1accb55f7346263ddd2f41a288bbd0ae9
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-05-16 23:51:24 +0000

    Extend normality test to rectangular matrices

--- scitbx/matrix/tests.h
+++ scitbx/matrix/tests.h
@@ -10,19 +10,37 @@
 
 namespace scitbx { namespace matrix {
 
+/// A measure of whether the columns and/or the rows of u
+/// form an orthonormal system.
+/** Reference: DLQT02.F and DQRT02.F in LAPACK tests (c.f. very end of those
+    files) */
 template <typename T>
 T normality_ratio(af::const_ref<T, af::mat_grid> const &u,
                   T eps=std::numeric_limits<T>::epsilon())
 {
-  SCITBX_ASSERT(u.is_square());
-  int n = u.n_rows();
-  af::versa<T, af::c_grid<2> > delta = af::matrix_multiply(
-        u, af::matrix_transpose(u).ref());
-  af::ref<T, af::c_grid<2> > delta_ = delta.ref();
-  for (int i=0; i<n; ++i) delta_(i,i)--;
-  return (norm_1(delta.const_ref())/n)/eps;
+  typedef af::versa<T, af::mat_grid> matrix_t;
+  typedef af::const_ref<T, af::mat_grid> matrix_const_ref_t;
+  typedef af::ref<T, af::mat_grid> matrix_ref_t;
+
+  int m=u.n_rows(), n = u.n_columns();
+  matrix_t ut_= af::matrix_transpose(u);
+  matrix_const_ref_t ut = ut_.const_ref();
+  if (m <= n) { // only the rows can be tested
+    matrix_t delta = af::matrix_multiply(u, ut);
+    matrix_ref_t delta_ = delta.ref();
+    for (int i=0; i<m; ++i) delta_(i,i) -= 1;
+    return (norm_1(delta.const_ref())/n)/eps;
+  }
+  else { // only the columns can be tested
+    matrix_t delta = af::matrix_multiply(ut, u);
+    matrix_ref_t delta_ = delta.ref();
+    for (int i=0; i<n; ++i) delta_(i,i) -= 1;
+    return (norm_1(delta.const_ref())/m)/eps;
+  }
 }
 
+
+/// A measure the relative difference between a and b
 template <typename T>
 T equality_ratio(af::const_ref<T, af::mat_grid> const &a,
                  af::const_ref<T, af::mat_grid> const &b,
