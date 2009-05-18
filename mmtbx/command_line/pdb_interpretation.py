129f4cd8a2245e660f916d55fe03c42527880968
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2009-05-18 21:50:24 +0000

    mmtbx/command_line/pdb_interpretation.py: new option --write-tardy-geo-files

--- mmtbx/command_line/pdb_interpretation.py
+++ mmtbx/command_line/pdb_interpretation.py
@@ -13,6 +13,9 @@ def run(args):
     .option(None, "--write_geo_files",
       action="store_true",
       default=False)
+    .option(None, "--write_tardy_geo_files",
+      action="store_true",
+      default=False)
     .option(None, "--strict_conflict_handling",
       action="store_true",
       default=False)
@@ -24,25 +27,37 @@ def run(args):
     substitute_non_crystallographic_unit_cell_if_necessary=True,
     return_all_processed_pdb_files=True)
   print
-  if (co.write_geo_files):
+  if (   co.write_geo_files
+      or co.write_tardy_geo_files):
     for processed_pdb_file in processed_pdb_files:
       acp = processed_pdb_file.all_chain_proxies
       source = acp.pdb_inp.source_info()
-      sites_cart = acp.sites_cart_exact()
-      site_labels = [atom.id_str() for atom in acp.pdb_atoms]
-      geo = processed_pdb_file.geometry_restraints_manager()
       assert source.startswith("file ")
       pdb_file_name = source[5:]
-      geo_file_name = op.basename(pdb_file_name) + ".geo"
-      print "Writing file: %s" % show_string(geo_file_name)
-      f = open(geo_file_name, "w")
-      print >> f, "# Geometry restraints for file:"
-      print >> f, "#   %s" % show_string(pdb_file_name)
-      print >> f, "#", date_and_time()
-      print >> f
-      geo.show_sorted(sites_cart=sites_cart, site_labels=site_labels, f=f)
-      f.close()
-      print
+      sites_cart = acp.sites_cart_exact()
+      site_labels = [atom.id_str() for atom in acp.pdb_atoms]
+      def write_geo(label, geo, geo_file_name):
+        f = open(geo_file_name, "w")
+        print >> f, "# %sgeometry restraints for file:" % label
+        print >> f, "#   %s" % show_string(pdb_file_name)
+        print >> f, "#", date_and_time()
+        print >> f
+        geo.show_sorted(
+          sites_cart=sites_cart, site_labels=site_labels, f=f)
+        f.close()
+      geo = processed_pdb_file.geometry_restraints_manager()
+      if (co.write_geo_files):
+        geo_file_name = op.basename(pdb_file_name) + ".geo"
+        print "Writing file: %s" % show_string(geo_file_name)
+        write_geo(label="", geo=geo, geo_file_name=geo_file_name)
+        print
+      if (co.write_tardy_geo_files):
+        geo_file_name = op.basename(pdb_file_name) + ".tardy.geo"
+        print "Writing file: %s" % show_string(geo_file_name)
+        tardy_tree = geo.construct_tardy_tree(sites_cart=sites_cart)
+        reduced_geo = geo.reduce_for_tardy(tardy_tree=tardy_tree)
+        write_geo(label="tardy ", geo=reduced_geo, geo_file_name=geo_file_name)
+        print
 
 if (__name__ == "__main__"):
   run(args=sys.argv[1:])
