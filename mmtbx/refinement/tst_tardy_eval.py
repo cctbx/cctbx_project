f9ac5826e9ecd6bd6d87965c9589be4da61ff3e0
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2009-05-20 17:07:12 +0000

    mmtbx/refinement/tst_tardy_eval.py: plots for annealing runs; plots for minimization runs reworked (fewer plots)

--- mmtbx/refinement/tst_tardy_eval.py
+++ mmtbx/refinement/tst_tardy_eval.py
@@ -2,15 +2,19 @@ from __future__ import division
 from mmtbx.refinement import tst_tardy_comprehensive
 from mmtbx.refinement import tst_tardy_pdb
 from scitbx.array_family import flex
+from libtbx.math_utils import iceil
 from libtbx import dict_with_default_0, group_args
 import sys
 
 try: import reportlab
 except ImportError: reportlab = None
+if (reportlab is None):
+  print "Skipping generation of plots: reportlab not available."
 
 class plot_grid(object):
 
   def __init__(O, grid, top_label, margin=20, top_label_space=40):
+    if (reportlab is None): return
     from reportlab.graphics.shapes import Group, String
     from reportlab.lib import pagesizes
     O.grid = grid
@@ -25,14 +29,18 @@ class plot_grid(object):
       fontSize=16,
       textAnchor="middle"))
 
-  def process(O, grid_ij, label, data):
+  def process(O, grid_ij, xy_max, label, data):
+    if (reportlab is None): return
     from reportlab.graphics.shapes import Group, String
-    lp = O.line_plot(data=data)
+    lp = O.line_plot(xy_max, data=data)
     gr = Group(lp)
     i,j = grid_ij
-    tx, ty = O.margin + i * (O.page_size[0] - 2 * O.margin) / O.grid[0], \
-             O.margin + j * (O.page_size[1] - 2 * O.margin
-                                            - O.top_label_space) / O.grid[1]
+    assert 0 <= i < O.grid[0]
+    assert 0 <= j < O.grid[1]
+    i = O.grid[0] - 1 - i
+    tx, ty = O.margin + j * (O.page_size[0] - 2 * O.margin) / O.grid[1], \
+             O.margin + i * (O.page_size[1] - 2 * O.margin
+                                            - O.top_label_space) / O.grid[0]
     gr.translate(tx, ty)
     O.top_group.add(gr)
     O.top_group.add(String(
@@ -42,7 +50,8 @@ class plot_grid(object):
       fontSize=12,
       textAnchor="middle"))
 
-  def line_plot(O, data):
+  def line_plot(O, xy_max, data):
+    if (reportlab is None): return
     from reportlab.graphics.charts.lineplots import LinePlot
     from reportlab.graphics.widgets.markers import makeMarker
     from reportlab.lib import colors
@@ -51,7 +60,7 @@ class plot_grid(object):
     lp.y = 40
     lp.height = 120
     lp.width = 120
-    lp.data = [[(0,0),(4,4)], data]
+    lp.data = [[(0,0),(xy_max,xy_max)], data]
     lp.lines[0].strokeColor = colors.Color(*[0.8]*3)
     lp.lines[0].strokeWidth = 0.5
     lp.lines[1].strokeColor = colors.white
@@ -62,24 +71,26 @@ class plot_grid(object):
     lp.strokeColor = colors.Color(*[0.8]*3)
     lp.strokeWidth = 1
     lp.xValueAxis.valueMin = 0
-    lp.xValueAxis.valueMax = 4
-    lp.xValueAxis.valueSteps = range(5)
+    lp.xValueAxis.valueMax = xy_max
+    lp.xValueAxis.valueSteps = range(xy_max+1)
     lp.xValueAxis.strokeWidth = 1
     lp.xValueAxis.tickDown = 3
     lp.xValueAxis.labels.fontSize = 10
     lp.yValueAxis.valueMin = 0
-    lp.yValueAxis.valueMax = 4
-    lp.yValueAxis.valueSteps = range(5)
+    lp.yValueAxis.valueMax = xy_max
+    lp.yValueAxis.valueSteps = range(xy_max+1)
     lp.yValueAxis.strokeWidth = 1
     lp.yValueAxis.tickLeft = 3
     lp.yValueAxis.labels.fontSize = 10
     return lp
 
   def new_canvas(O, file_name):
+    if (reportlab is None): return
     from reportlab.pdfgen.canvas import Canvas
     return Canvas(filename=file_name, pagesize=O.page_size)
 
   def draw_to_canvas(O, canvas):
+    if (reportlab is None): return
     from reportlab.graphics.shapes import Drawing
     from reportlab.graphics import renderPDF
     drawing = Drawing(*O.page_size)
@@ -87,6 +98,7 @@ class plot_grid(object):
     renderPDF.draw(drawing=drawing, canvas=canvas, x=0, y=0)
 
   def write_to_file(O, file_name):
+    if (reportlab is None): return
     canvas = O.new_canvas(file_name=file_name)
     O.draw_to_canvas(canvas=canvas)
     canvas.showPage()
@@ -95,25 +107,29 @@ class plot_grid(object):
 class multi_page_plots(object):
 
   def __init__(O, file_name):
+    if (reportlab is None): return
     O.file_name = file_name
     O.canvas = None
 
   def add_page(O, page):
+    if (reportlab is None): return
     if (O.canvas is None):
       O.canvas = page.new_canvas(file_name=O.file_name)
     page.draw_to_canvas(canvas=O.canvas)
     O.canvas.showPage()
 
   def write_to_file(O):
+    if (reportlab is None): return
     O.canvas.save()
 
-def rmsd_start_final_plots(
+def rmsd_start_final_plots_minimization(
       tst_tardy_pdb_params,
+      parameter_trial_table,
       cp_n_trials,
       rmsds,
       rmsd_n_n,
       write_separate_pages):
-  ttd = dict(tst_tardy_comprehensive.common_parameter_trial_table)
+  ttd = dict(parameter_trial_table)
   plot_data = {}
   for h in ttd["structure_factors_high_resolution"]:
     plot_data[h] = {}
@@ -131,7 +147,7 @@ def rmsd_start_final_plots(
   for cp_i_trial in xrange(cp_n_trials):
     tst_tardy_comprehensive.set_parameters(
       params=p,
-      trial_table=tst_tardy_comprehensive.common_parameter_trial_table,
+      trial_table=parameter_trial_table,
       cp_i_trial=cp_i_trial)
     plot = plot_data[
       p.structure_factors_high_resolution][
@@ -143,35 +159,33 @@ def rmsd_start_final_plots(
       plot.rmsd_final.append(rmsd[-1])
       plot.rmsd_n.append(rmsd[min(len(rmsd)-1, rmsd_n_n)])
   #
-  if (reportlab is None):
-    print "Skipping generation of plots: reportlab not available."
-    return
-  #
   mpp = multi_page_plots(file_name="plots_h_e.pdf")
   for e in  ttd["emulate_cartesian"]:
+    top_label = "emulate_cartesian = %s" % str(e)
+    print top_label
+    page = plot_grid(grid=(4,3), top_label=top_label)
     w_d_ranks_rn = {}
     w_d_ranks_rf = {}
     for d in ttd["real_space_gradients_delta_resolution_factor"]:
       for w in ttd["real_space_target_weight"]:
         w_d_ranks_rn[(w,d)] = []
         w_d_ranks_rf[(w,d)] = []
-    for h in ttd["structure_factors_high_resolution"]:
-      top_label = "h%.0f_e%d" % (h, int(e))
-      print top_label
-      page = plot_grid(grid=(3,4), top_label=top_label)
+    for i_h,h in enumerate(ttd["structure_factors_high_resolution"]):
+      plot_xy_max = iceil(h * 1.3)
       page_rn = []
       page_rf = []
-      for i,d in enumerate(ttd["real_space_gradients_delta_resolution_factor"]):
-        for j,w in enumerate(ttd["real_space_target_weight"]):
+      for d in ttd["real_space_gradients_delta_resolution_factor"]:
+        for i_w,w in enumerate(ttd["real_space_target_weight"]):
           pd = plot_data[h][e][d][w]
           rn = flex.mean(pd.rmsd_n)
           rf = flex.mean(pd.rmsd_final)
           page_rn.append((rn, (w,d)))
           page_rf.append((rf, (w,d)))
-          label = "w%04.0f_d%.0f: %d=%.2f, %.2f" % (w, d*100, rmsd_n_n, rn, rf)
+          label = "h%.2f_w%04.0f: %d=%.2f, %.2f" % (h, w, rmsd_n_n, rn, rf)
           print "  ", label
           page.process(
-            grid_ij=(i,j),
+            grid_ij=(i_h,i_w),
+            xy_max=plot_xy_max,
             label=label,
             data=zip(pd.rmsd_start, pd.rmsd_final))
       def cmp_rx(a, b):
@@ -185,9 +199,9 @@ def rmsd_start_final_plots(
         w_d_ranks_rn[w_d].append(i)
       for i,(r,w_d) in enumerate(page_rf):
         w_d_ranks_rf[w_d].append(i)
-      if (write_separate_pages):
-        page.write_to_file(file_name="plot_%s.pdf" % top_label)
-      mpp.add_page(page=page)
+    if (write_separate_pages):
+      page.write_to_file(file_name="plot_%s.pdf" % top_label)
+    mpp.add_page(page=page)
     w_d_ranks_rn = w_d_ranks_rn.items()
     w_d_ranks_rf = w_d_ranks_rf.items()
     def cmp_w_d_ranks(a, b):
@@ -201,7 +215,7 @@ def rmsd_start_final_plots(
       return result
     w_d_ranks_rn.sort(cmp_w_d_ranks)
     w_d_ranks_rf.sort(cmp_w_d_ranks)
-    print "e=%d" % int(e)
+    print "emulate_cartesian = %s" % str(e)
     for prefix,w_d_ranks in [("rn:", w_d_ranks_rn),
                              ("rf:", w_d_ranks_rf)]:
       for w_d,ranks in w_d_ranks:
@@ -211,11 +225,95 @@ def rmsd_start_final_plots(
       print
   mpp.write_to_file()
 
+def rmsd_start_final_plots_annealing(
+      tst_tardy_pdb_params,
+      parameter_trial_table,
+      cp_n_trials,
+      rmsds,
+      write_separate_pages):
+  ttd = dict(parameter_trial_table)
+  plot_data = {}
+  for h in ttd["structure_factors_high_resolution"]:
+    plot_data[h] = {}
+    for e in  ttd["emulate_cartesian"]:
+      plot_data[h][e] = {}
+      for d in ttd["real_space_gradients_delta_resolution_factor"]:
+        plot_data[h][e][d] = {}
+        for w in ttd["real_space_target_weight"]:
+          plot_data[h][e][d][w] = {}
+          for t in ttd["start_temperature_kelvin"]:
+            plot_data[h][e][d][w][t] = {}
+            for c in ttd["number_of_cooling_steps"]:
+              plot_data[h][e][d][w][t][c] = group_args(
+                rmsd_start=flex.double(),
+                rmsd_final=flex.double())
+  #
+  p = tst_tardy_pdb_params
+  for cp_i_trial in xrange(cp_n_trials):
+    tst_tardy_comprehensive.set_parameters(
+      params=p,
+      trial_table=parameter_trial_table,
+      cp_i_trial=cp_i_trial)
+    plot = plot_data[
+      p.structure_factors_high_resolution][
+      p.emulate_cartesian][
+      p.real_space_gradients_delta_resolution_factor][
+      p.real_space_target_weight][
+      p.start_temperature_kelvin][
+      p.number_of_cooling_steps]
+    for rmsd in rmsds[cp_i_trial]:
+      plot.rmsd_start.append(rmsd[0])
+      plot.rmsd_final.append(rmsd[-1])
+  #
+  mpp = multi_page_plots(file_name="plots_h_e.pdf")
+  for h in ttd["structure_factors_high_resolution"]:
+    for e in  ttd["emulate_cartesian"]:
+      top_label = "high resol. %.2f, emulate_cartesian = %s" % (h, str(e))
+      print top_label
+      page = plot_grid(grid=(4,3), top_label=top_label)
+      plot_xy_max = iceil(h * 1.3)
+      for d in ttd["real_space_gradients_delta_resolution_factor"]:
+        for i_w,w in enumerate(ttd["real_space_target_weight"]):
+          for i_t,t in enumerate(ttd["start_temperature_kelvin"]):
+            for i_c,c in enumerate(ttd["number_of_cooling_steps"]):
+              pd = plot_data[h][e][d][w][t][c]
+              rf = flex.mean(pd.rmsd_final)
+              label = "w%04.0f_t%.0f_c%d: %.2f" % (w, t, c, rf)
+              print "  ", label
+              page.process(
+                grid_ij=(i_t*2+i_c, i_w),
+                xy_max=plot_xy_max,
+                label=label,
+                data=zip(pd.rmsd_start, pd.rmsd_final))
+      if (write_separate_pages):
+        page.write_to_file(file_name="plot_%s.pdf" % top_label)
+      mpp.add_page(page=page)
+  mpp.write_to_file()
+
 def run(args):
+  algorithm = None
+  for line in open(args[0]).read().splitlines():
+    if (line == "algorithm = *minimization annealing"):
+      algorithm = "minimization"
+      break
+    elif (line == "algorithm = minimization *annealing"):
+      algorithm = "annealing"
+      break
+  else:
+    raise RuntimeError("algorithm = minimization or annealing not found.")
+  #
   tst_tardy_pdb_master_phil = tst_tardy_pdb.get_master_phil()
   tst_tardy_pdb_params = tst_tardy_pdb_master_phil.extract()
+  if (algorithm == "minimization"):
+    parameter_trial_table \
+      = tst_tardy_comprehensive.common_parameter_trial_table
+  elif (algorithm == "annealing"):
+    parameter_trial_table \
+      = tst_tardy_comprehensive.annealing_parameter_trial_table
+  else:
+    raise AssertionError
   cp_n_trials = tst_tardy_comprehensive.number_of_trials(
-    table=tst_tardy_comprehensive.common_parameter_trial_table)
+    table=parameter_trial_table)
   #
   random_seed_rmsd = []
   for cp_i_trial in xrange(cp_n_trials):
@@ -245,12 +343,20 @@ def run(args):
     rmsds.append([random_seed_rmsd[cp_i_trial][random_seed]
       for random_seed in xrange(len(random_seeds_found))])
   #
-  if (1):
-    rmsd_start_final_plots(
+  if (algorithm == "minimization"):
+    rmsd_start_final_plots_minimization(
       tst_tardy_pdb_params=tst_tardy_pdb_params,
+      parameter_trial_table=parameter_trial_table,
       cp_n_trials=cp_n_trials,
       rmsds=rmsds,
       rmsd_n_n=50,
+      write_separate_pages=False)
+  elif (algorithm == "annealing"):
+    rmsd_start_final_plots_annealing(
+      tst_tardy_pdb_params=tst_tardy_pdb_params,
+      parameter_trial_table=parameter_trial_table,
+      cp_n_trials=cp_n_trials,
+      rmsds=rmsds,
       write_separate_pages=False)
 
 if (__name__ == "__main__"):
