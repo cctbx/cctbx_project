bfff063f738cda7c7c28ade913d6f8dc9633bcd4
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-05-25 11:23:50 +0000

    integration with VALGRIND_OPTS=-q

--- libtbx/command_line/valgrind.sh
+++ libtbx/command_line/valgrind.sh
@@ -1,11 +1,26 @@
 #! /bin/sh
-valgrind --version
+verbose="yes"
+if [ -n "$VALGRIND_OPTS" ]; then
+  for opt in $VALGRIND_OPTS; do
+    if [ "$opt" == "-q" ]; then
+      verbose="no"
+      break
+    fi
+  done
+fi
+if [ "$verbose" == "yes" ]; then
+  valgrind --version
+else
+  valgrind --version > /dev/null 2>&1
+fi
 if [ $? -ne 0 ]; then exit 1; fi
 if [ ! -n "$LIBTBX_VALGRIND" ]; then
-  echo "### LIBTBX_VALGRIND not set: using default."
-  echo "### To override, define LIBTBX_VALGRIND"
-  echo "### before calling $LIBTBX_DISPATCHER_NAME."
-  opt=""
+  if [ "$verbose" == "yes" ]; then
+    echo "### LIBTBX_VALGRIND not set: using default."
+    echo "### To override, define LIBTBX_VALGRIND"
+    echo "### before calling $LIBTBX_DISPATCHER_NAME."
+  fi
+  opt="z"
   if [ "`uname`" = Darwin ]; then
     opt=" --trace-children=yes --auto-run-dsymutil=yes"
   fi
@@ -13,7 +28,9 @@ if [ ! -n "$LIBTBX_VALGRIND" ]; then
   if [ $? -ne 0 ]; then exit 1; fi
   export LIBTBX_VALGRIND
 fi
-echo "LIBTBX_VALGRIND=$LIBTBX_VALGRIND"
+if [ "$verbose" == "yes" ]; then
+  echo "LIBTBX_VALGRIND=$LIBTBX_VALGRIND"
+fi
 if [ $? -ne 0 ]; then exit 1; fi
 LIBTBX__VALGRIND_FLAG__=1
 export LIBTBX__VALGRIND_FLAG__
