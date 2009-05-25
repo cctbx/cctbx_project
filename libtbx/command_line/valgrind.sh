224e9e73cf4a26a370b46f3dfbba262a5d8bf7c7
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-05-25 11:19:37 +0000

    keep up with the DARWIN branch

--- libtbx/command_line/valgrind.sh
+++ libtbx/command_line/valgrind.sh
@@ -7,7 +7,7 @@ if [ ! -n "$LIBTBX_VALGRIND" ]; then
   echo "### before calling $LIBTBX_DISPATCHER_NAME."
   opt=""
   if [ "`uname`" = Darwin ]; then
-    opt=" --trace-children=yes"
+    opt=" --trace-children=yes --auto-run-dsymutil=yes"
   fi
   LIBTBX_VALGRIND="valgrind --tool=memcheck$opt --suppressions=`libtbx.show_dist_paths libtbx`/valgrind-python24.supp"
   if [ $? -ne 0 ]; then exit 1; fi
