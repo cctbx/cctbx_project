0301f929a2e44e13de83b52f3bd301abf8ce2717
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2009-05-29 05:08:42 +0000

    cctbx/maptbx/real_space_refinement_simple.py: Pavel's completely unrelated implementation moved to separate cctbx/maptbx/real_space_refinement_pavel.py; balancing of gradients removed from real_space_refinement_simple.py (test still needs work to be conclusive)

--- cctbx/maptbx/tst_real_space_refinement_simple.py
+++ cctbx/maptbx/tst_real_space_refinement_simple.py
@@ -8,11 +8,8 @@ import cctbx.crystal.coordination_sequences
 from cctbx import uctbx
 from cctbx.array_family import flex
 from scitbx.graph import tst_tardy_pdb
-import sys
-from cctbx.development import random_structure
-from cctbx import sgtbx
-from cctbx import adptbx
 import random
+import sys
 
 def construct_geometry_restraints_manager(test_case):
   sites = test_case.sites
@@ -54,84 +51,58 @@ def construct_geometry_restraints_manager(test_case):
     nonbonded_function=geometry_restraints.prolsq_repulsion_function(),
     max_reasonable_bond_distance=10)
 
-def run(args):
-  d_min = 3
-  test_cases = tst_tardy_pdb.select_test_cases(tags_or_indices=args)
-  for test_case in test_cases:
-    print "test case %d: %s" % (test_case.index, test_case.tag)
-    sites_cart, geo_manager = construct_geometry_restraints_manager(
-      test_case=test_case)
-    scatterers = flex.xray_scatterer(
-      sites_cart.size(), xray.scatterer(scattering_type="C"))
-    for sc,lbl in zip(scatterers, test_case.labels):
-      sc.label = lbl
-    structure = xray.structure(
-      crystal_symmetry=geo_manager.crystal_symmetry,
-      scatterers=scatterers)
-    structure.set_sites_cart(sites_cart=sites_cart)
-    f_calc = structure.structure_factors(
-      d_min=d_min, anomalous_flag=False).f_calc()
-    fft_map = f_calc.fft_map()
-    fft_map.apply_sigma_scaling()
+def exercise_lbfgs(test_case, use_geo, d_min=2):
+  sites_cart, geo_manager = construct_geometry_restraints_manager(
+    test_case=test_case)
+  scatterers = flex.xray_scatterer(
+    sites_cart.size(), xray.scatterer(scattering_type="C"))
+  for sc,lbl in zip(scatterers, test_case.labels):
+    sc.label = lbl
+  structure = xray.structure(
+    crystal_symmetry=geo_manager.crystal_symmetry,
+    scatterers=scatterers)
+  structure.set_sites_cart(sites_cart=sites_cart)
+  f_calc = structure.structure_factors(
+    d_min=d_min, anomalous_flag=False).f_calc()
+  fft_map = f_calc.fft_map()
+  fft_map.apply_sigma_scaling()
+  if (use_geo):
     structure.shake_sites_in_place(rms_difference=0.1)
     geo_manager.energies_sites(sites_cart=structure.sites_cart()).show()
     minimized = real_space_refinement_simple.lbfgs(
-      geometry_restraints_manager=geo_manager,
-      unit_cell=structure.unit_cell(),
       sites_cart=structure.sites_cart(),
       density_map=fft_map.real_map(),
-      gradients_delta=d_min/4,
-      real_space_weight_scale=0.01)
+      gradients_delta=d_min/3,
+      geometry_restraints_manager=geo_manager,
+      real_space_weight=0.01)
     structure.set_sites_cart(sites_cart=minimized.sites_cart)
     geo_manager.energies_sites(sites_cart=structure.sites_cart()).show()
-    print "f, |g|, w:", \
-      minimized.f, flex.mean_sq(minimized.g)**0.5, minimized.rs_weight
-    print
-  print "OK"
+  else:
+    minimized = real_space_refinement_simple.lbfgs(
+      sites_cart=structure.sites_cart(),
+      density_map=fft_map.real_map(),
+      gradients_delta=d_min/3,
+      unit_cell=structure.unit_cell())
+  print "f, |g|, w:", \
+    minimized.f, flex.mean_sq(minimized.g)**0.5, minimized.rs_weight
+  print
+  return minimized
 
-def run_minimization():
+def run(args):
   if (1):
     random.seed(0)
     flex.set_random_seed(0)
-  def compute_map(xray_structure, d_min=1.5, resolution_factor=1./4):
-    fc = xray_structure.structure_factors(d_min = d_min).f_calc()
-    fft_map = fc.fft_map(resolution_factor=resolution_factor)
-    fft_map.apply_sigma_scaling()
-    result = fft_map.real_map_unpadded()
-    return result, fc, fft_map
-  xrs = random_structure.xray_structure(
-    space_group_info = sgtbx.space_group_info("P1"),
-    unit_cell        = (10, 20, 30, 70, 80, 120),
-    elements         =(("O","N","C")*10),
-    volume_per_atom  = 50,
-    min_distance     = 2,
-    u_iso            = adptbx.b_as_u(10.),
-    use_u_iso        = True)
-  map_target,tmp,tmp = compute_map(xray_structure = xrs)
-  xrs_sh = xrs.deep_copy_scatterers()
-  xrs_sh.shake_sites_in_place(mean_distance=0.5)
-  start_error = flex.mean(xrs.distances(other = xrs_sh))
-  print "Start:", start_error
-  map_current, miller_array, crystal_gridding = compute_map(xray_structure = xrs_sh)
-  steps1 = [1.0,0.5,0.25,0.1,0.05,0.01,0.001]
-  steps1.reverse()
-  steps2 = [1.0,0.5,0.25,0.1,0.05,0.01,0.001]
-  for step in steps1+steps2:
-    minimized = real_space_refinement_simple.minimization(
-      xray_structure   = xrs_sh,
-      miller_array     = miller_array,
-      crystal_gridding = crystal_gridding,
-      map_target       = map_target,
-      max_iterations   = 500,
-      min_iterations   = 25,
-      step             = step)
-    xrs_sh = minimized.xray_structure
-    map_current = minimized.map_current
-    final_error = flex.mean(xrs.distances(other = minimized.xray_structure))
-    print "Final:", final_error
-  assert start_error >= 0.5
-  assert final_error < 0.021
+  test_cases = tst_tardy_pdb.select_test_cases(tags_or_indices=args)
+  for test_case in test_cases:
+    print "test case %d: %s" % (test_case.index, test_case.tag)
+    minimized = []
+    for use_geo in [False, True]:
+      minimized.append(exercise_lbfgs(test_case, use_geo=use_geo))
+    f0 =  minimized[0].f
+    f1 =  minimized[1].f / minimized[1].rs_weight
+    assert f1 > f0 * 1.1
+    assert f1 < f0 * 0.7
+  print "OK"
 
 if (__name__ == "__main__"):
   run(args=sys.argv[1:])
-  run_minimization()
