8d4a18ec2f856f6f5c9444404b5c10a3c88a113a
Nathaniel Echols <nathaniel.echols@gmail.com>
2009-06-15 22:19:15 +0000

    modules for handling xml-rpc communiation between cctbx-based apps and other python-extensible programs (coot, pymol, etc.)

---
+++ libtbx/xmlrpc_server_example.py
@@ -0,0 +1,122 @@
+
+# This is an example of how a 3rd-party program with Python embedded, such
+# as Coot or PyMOL, can be interfaced with CCTBX-based software.  Something
+# much like this is used for the Phenix GUI extensions to those programs.
+# I haven't tried this with any other software, but anything with a reasonably
+# recent version of Python and support for either persistent Python threads
+# or some sort of timer callback should be able to use it.
+
+import os, sys, time, string, signal
+import xmlrpclib
+
+try :
+  from SimpleXMLRPCServer import SimpleXMLRPCServer
+  class external_xmlrpc_server (SimpleXMLRPCServer) :
+    def __init__ (self, addr, cctbx_interface) :
+      self.cctbx_interface = cctbx_interface
+      SimpleXMLRPCServer.__init__(self, addr, logRequests=0)
+
+    def _dispatch (self, method, params) :
+      if not self.cctbx_interface.enable_xmlrpc :
+        return -1
+      result = -1
+      func = getattr(self.cctbx_interface, method, None)
+      if not callable(func) :
+        print "%s is not a callable object!" % method
+      else :
+        result = func(*params)
+        if result is None :
+          result = -1
+      return result
+
+  class external_xmlrpc_interface (object) :
+    def __init__ (self, program_id, auto_start=True, verbose=False) :
+      self.enable_xmlrpc = True
+      self.xmlrpc_server = None
+      self.cctbx_server = None
+      self.verbose = verbose
+      self.timeout = string.atoi(os.environ.get("CCTBX_XMLRPC_TIMEOUT", "250"))
+      self.program_id = program_id
+      self.supported_modules = []
+      self.setup_modules()
+      self.setup_server()
+      if auto_start :
+        self.start_server()
+
+    def setup_modules (self) :
+      pass
+
+    def add_module (self, module_object=None, module_path=None) :
+      if module_object is not None :
+        self.supported_modules.append(module_object)
+      elif module_path is not None :
+        module_object = __import__(module_path)
+        self.supported_modules.append(module_object)
+
+    def setup_server (self) :
+      port = os.environ.get("CCTBX_%s_PORT" % self.program_id, None)
+      if port is not None :
+        self.port = string.atoi(port)
+        self.xmlrpc_server = external_xmlrpc_server(("localhost", self.port),
+                                                    self)
+      cctbx_port = os.environ.get("CCTBX_XMLRPC_PORT", None)
+      if cctbx_port is not None :
+        uri = "http://localhost:%d/RPC2" % cctbx_port
+        self.cctbx_server = xmlrpclib.ServerProxy(uri=uri)
+
+    def start_server (self) :
+      if self.xmlrpc_server is not None :
+        print "XML-RPC server started on port %d" % self.port
+        self.xmlrpc_server.serve_forever()
+
+    def start_server_in_separate_thread (self) :
+      import threading
+      t = threading.Thread(target=self.start_server)
+      t.setDaemon(1)
+      t.start()
+
+    def timeout_func (self, *args) :
+      if self.xmlrpc_server is not None :
+        self.xmlrpc_server.handle_request()
+      return True
+
+    def is_alive (self) :
+      return True
+
+    # XXX: this should be replaced by the proper quit function for the program
+    # being extended - e.g. cmd.quit() in PyMOL.
+    def quit (self) :
+      print "quitting"
+      sys.stdout.flush()
+      os.kill(os.getpid(), signal.SIGKILL)
+
+    def __getattr__ (self, name) :
+      for module_object in self.supported_modules :
+        if hasattr(module_object, name) :
+          return getattr(module_object, name)
+      return None
+
+except KeyboardInterrupt :
+  raise
+except ImportError :
+  def external_xmlrpc_server (*args, **kwds) :
+    raise Exception("SimpleXMLRPCServer not available on this platform.")
+
+  def external_cctbx_interface (*args, **kwds) :
+    raise Exception("SimpleXMLRPCServer not available on this platform.")
+
+if __name__ == "__main__" :
+  class test_module (object) :
+    def echo_test (self) :
+      print "hello, world!"
+      sys.stdout.flush()
+      return True
+
+#  os.environ["CCTBX_TEST_PORT"] = "48000"
+  test_server = external_xmlrpc_interface("TEST", auto_start=False,
+                                          verbose=False)
+  module_object = test_module()
+  test_server.add_module(module_object)
+  test_server.start_server()
+
+#---end
---
+++ libtbx/xmlrpc_utils.py
@@ -0,0 +1,238 @@
+
+# see also xmlrpc_server_example.py
+
+# XXX: The ServerProxy here is based on the xmlrpclib ServerProxy
+# object, but rewritten from scratch to cache requests which failed due
+# to a connection error and retry them later.  I'm not sure why I can't
+# just subclass it - I suspect it's a "feature" of old-style classes.
+#
+# XXX: Note that using the hacked ServerProxy violates the intended behavior
+# of the XML-RPC protocol.  Therefore, this module allows either the original
+# or modified version to be used - the original is left as the default.
+#
+#
+# Original copyright information:
+#
+# XML-RPC CLIENT LIBRARY
+# $Id: xmlrpclib.py 65467 2008-08-04 00:50:11Z brett.cannon $
+#
+# an XML-RPC client interface for Python.
+#
+# the marshalling and response parser code can also be used to
+# implement XML-RPC servers.
+#
+# --------------------------------------------------------------------
+# The XML-RPC client interface is
+#
+# Copyright (c) 1999-2002 by Secret Labs AB
+# Copyright (c) 1999-2002 by Fredrik Lundh
+#
+# By obtaining, using, and/or copying this software and/or its
+# associated documentation, you agree that you have read, understood,
+# and will comply with the following terms and conditions:
+#
+# Permission to use, copy, modify, and distribute this software and
+# its associated documentation for any purpose and without fee is
+# hereby granted, provided that the above copyright notice appears in
+# all copies, and that both that copyright notice and this permission
+# notice appear in supporting documentation, and that the name of
+# Secret Labs AB or the author not be used in advertising or publicity
+# pertaining to distribution of the software without specific, written
+# prior permission.
+#
+# SECRET LABS AB AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD
+# TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANT-
+# ABILITY AND FITNESS.  IN NO EVENT SHALL SECRET LABS AB OR THE AUTHOR
+# BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY
+# DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+# WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+# OF THIS SOFTWARE.
+# --------------------------------------------------------------------
+
+import os, sys, time, random, string
+import xmlrpclib
+import subprocess
+import threading
+from libtbx import adopt_init_args
+from libtbx import thread_utils
+import libtbx.load_env
+from libtbx.utils import Sorry
+
+class ServerProxy (object) :
+  def __init__(self, uri, transport=None, encoding=None, verbose=0,
+               allow_none=0, use_datetime=0):
+    self._pending = []
+    # establish a "logical" server connection
+
+    # get the url
+    import urllib
+    type, uri = urllib.splittype(uri)
+    if type not in ("http", "https"):
+      raise IOError, "unsupported XML-RPC protocol"
+    self.__host, self.__handler = urllib.splithost(uri)
+    if not self.__handler:
+      self.__handler = "/RPC2"
+
+    if transport is None:
+      if type == "https":
+        transport = xmlrpclib.SafeTransport(use_datetime=use_datetime)
+      else:
+        transport = xmlrpclib.Transport(use_datetime=use_datetime)
+    self.__transport = transport
+
+    self.__encoding = encoding
+    self.__verbose = verbose
+    self.__allow_none = allow_none
+
+  def __request(self, methodname, params):
+    self._pending.append((methodname, params))
+    return self.flush_requests()
+
+  def flush_requests (self) :
+    result = None
+    while len(self._pending) > 0 :
+      (methodname, params) = self._pending.pop(0)
+      # call a method on the remote server
+      try :
+        request = xmlrpclib.dumps(params, methodname,
+                                  encoding=self.__encoding,
+                                  allow_none=self.__allow_none)
+
+        response = self.__transport.request(
+            self.__host,
+            self.__handler,
+            request,
+            verbose=self.__verbose
+        )
+
+        if len(response) == 1 :
+          result = response[0]
+      except KeyboardInterrupt :
+        raise
+      except Exception, e :
+        if str(e).startswith("[Errno 61]") :
+          self._pending.insert(0, (methodname, params))
+          break
+        elif str(e).startswith("<ProtocolError ") :
+          self._pending = []
+          break
+        else :
+          print str(e)
+          raise
+    return result
+
+
+  def __repr__(self):
+      return (
+          "<ServerProxy for %s%s>" %
+          (self.__host, self.__handler)
+          )
+
+  __str__ = __repr__
+
+  # note: to call a remote object with an non-standard name, use
+  # result getattr(server, "strange-python-name")(args)
+
+  def __getattr__(self, name):
+      # magic method dispatcher
+      return xmlrpclib._Method(self.__request, name)
+
+#-----------------------------------------------------------------------
+class external_program_thread (threading.Thread) :
+  def __init__ (self, command, program_id, log=None) :
+    adopt_init_args(self, locals())
+    if self.log is None :
+      self.log = sys.stdout
+    threading.Thread.__init__(self)
+    self._alive = True
+
+  def run (self) :
+    p = subprocess.Popen(args=[self.command], stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE, shell=True)
+    while True :
+      if p.poll() is not None :
+        break
+      output = p.stdout.readline()
+      if output is not None and output != "" :
+        self.log.write(output)
+        self.log.flush()
+    self._alive = False
+
+  # XXX: this is probably a bad idea
+  def is_alive (self) :
+    return self._alive
+
+class external_program_server (object) :
+  port_ranges = [ (40001, 40840),
+                  (46000, 46999) ]
+  def __init__ (self, command, program_id, timeout, cache_requests=False,
+                local_port=None, log=None) :
+    adopt_init_args(self, locals())
+    self._process = None
+    self._server = None
+    self.initialize_server()
+
+  def initialize_server (self) :
+    if self._process is None and self._server is None :
+      valid_ports = []
+      for (start, end) in self.port_ranges :
+        valid_ports.extend([ n for n in range(start, end) ])
+      i = int(random.random() * (len(valid_ports) - 1))
+      self._port = valid_ports[i]
+      prog_port_env = "CCTBX_%s_PORT" % string.upper(self.program_id)
+      os.environ[prog_port_env] = str(self._port)
+      if self.timeout is not None :
+        os.environ["CCTBX_XMLRPC_TIMEOUT"] = str(self.timeout)
+      if self.local_port is not None :
+        os.environ["CCTBX_XMLRPC_PORT"] = str(self.local_port)
+      self._process = external_program_thread(command=self.command,
+                                              program_id=self.program_id,
+                                              log=self.log)
+      self._process.start()
+      if self.cache_requests :
+        proxy_class = ServerProxy
+      else :
+        proxy_class = xmlrpclib.ServerProxy
+      self._server = proxy_class(uri="http://localhost:%d/RPC2" %
+                                             self._port)
+
+  def flush_requests (self) :
+    if self.cache_requests :
+      return False
+    elif self._server is not None :
+      return self._server.flush_requests()
+
+  def restart (self) :
+    self._process = None
+    self._server = None
+    self.initialize_server()
+
+  def is_alive (self) :
+    if self._process is None or self._server is None :
+      return False
+    try :
+      status = self._server.is_alive()
+    except KeyboardInterrupt :
+      raise
+    except Exception :
+      return False
+    else :
+      if status is None :
+        return False
+      else :
+        return True
+
+  def get_port (self) :
+    return self._port
+
+  def _ignore (self, *args, **kwds) :
+    return True
+
+  def __getattr__ (self, name) :
+    if self._process is None or self._server is None :
+      return self._ignore
+    else :
+      return getattr(self._server, name)
+
+#---end
