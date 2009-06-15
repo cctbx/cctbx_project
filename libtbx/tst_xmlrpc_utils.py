072b6c85bdad1e0f05d8c4d86eb457a493c00aab
Nathaniel Echols <nathaniel.echols@gmail.com>
2009-06-15 22:19:15 +0000

    modules for handling xml-rpc communiation between cctbx-based apps and other python-extensible programs (coot, pymol, etc.)

---
+++ libtbx/tst_xmlrpc_utils.py
@@ -0,0 +1,40 @@
+
+import os, sys, time
+import libtbx.load_env
+from libtbx import xmlrpc_utils
+import cStringIO
+
+def exercise () :
+  ext_module = libtbx.env.find_in_repositories(
+    relative_path="libtbx/xmlrpc_server_example.py",
+    test=os.path.isfile)
+  full_cmd = "libtbx.python %s" % ext_module
+  log = cStringIO.StringIO()
+  server = xmlrpc_utils.external_program_server(command=full_cmd,
+                                                program_id="TEST",
+                                                timeout=None,
+                                                cache_requests=True,
+                                                log=log)
+  server.echo_test()
+  time.sleep(1)
+  run_server_tests(server)
+  port = server.get_port()
+  assert log.getvalue() == """XML-RPC server started on port %d
+hello, world!
+hello, world!
+quitting
+""" % port
+  server.restart()
+  time.sleep(1)
+  run_server_tests(server)
+  print "OK"
+
+def run_server_tests (server) :
+  assert server.echo_test() == True
+  assert server.is_alive() == True
+  server.quit()
+  assert server.is_alive() == False
+  assert server.echo_test() is None
+
+if __name__ == "__main__" :
+  exercise()
