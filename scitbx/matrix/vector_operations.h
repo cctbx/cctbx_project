78761950618f587c98e458d4f4082e1319614371
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-06-27 21:16:39 +0000

    Matrix-vector operations (and those vector-vector needed for implementation): BLAS2 and more

---
+++ scitbx/matrix/vector_operations.h
@@ -0,0 +1,23 @@
+#ifndef SCITBX_MATRIX_VECTOR_OPERATIONS_H
+#define SCITBX_MATRIX_VECTOR_OPERATIONS_H
+
+namespace scitbx { namespace matrix {
+
+  /// x(0:n) /= alpha
+  template <typename T>
+  void scale_vector(int n, T *x, T alpha) {
+    if (alpha == 0.) std::fill_n(x, n, 0.);
+    else if (alpha != 1.) for (int i=0; i<n; ++i) x[i] *= alpha;
+  }
+
+  /// Euclidean scalar product of x(0:n) and y(0:n)
+  template <typename T>
+  T dot(int n, T const *x, T const *y) {
+    T s = 0;
+    for (int i=0; i<n; ++i) s += x[i] * y[i];
+    return s;
+  }
+
+}}
+
+#endif // GUARD
