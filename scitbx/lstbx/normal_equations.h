b199f01ea6edf3d3d37d15d6f289913b83d0d944
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-06-27 21:20:33 +0000

    New least-squares toolbox; first contribution: normal equations

---
+++ scitbx/lstbx/normal_equations.h
@@ -0,0 +1,345 @@
+/// Tools implementing the Gauss-Newton method for non-linear least-squares.
+
+#ifndef SCITBX_GAUSS_NEWTON_H
+#define SCITBX_GAUSS_NEWTON_H
+
+#include <scitbx/array_family/shared.h>
+#include <scitbx/array_family/owning_ref.h>
+#include <scitbx/matrix/cholesky.h>
+#include <scitbx/matrix/matrix_vector_operations.h>
+#include <boost/tuple/tuple.hpp>
+
+namespace scitbx { namespace lstbx {
+
+#define SCITBX_LSTBX_DECLARE_ARRAY_TYPE(FloatType)                            \
+    typedef FloatType scalar_t;                                               \
+    typedef af::ref_owning_versa<scalar_t,                                    \
+                                 matrix::packed_u_accessor>                   \
+            symmetric_matrix_owning_ref_t;                                    \
+    typedef af::ref<scalar_t,                                                 \
+                    matrix::packed_u_accessor>                                \
+            symmetric_matrix_ref_t;                                           \
+    typedef af::versa<scalar_t,                                               \
+                      matrix::packed_u_accessor>                              \
+            symmetric_matrix_t;                                               \
+    typedef af::ref_owning_versa<FloatType, af::mat_grid> matrix_owning_ref_t;\
+    typedef af::ref<FloatType, af::mat_grid> matrix_ref_t;                    \
+    typedef af::ref_owning_shared<scalar_t> vector_owning_ref_t;              \
+    typedef af::shared<scalar_t> vector_t;                                    \
+    typedef af::ref<scalar_t> vector_ref_t;
+
+
+  /// Normal equations for linear least-squares
+  /** The least-squares target reads
+
+      \f$ L(x) = \| A x - b \|^2 \f$
+
+      where the norm is diagonal-weighted
+
+      \f$ \| y \|^2 = \sum_i w_i y_i^2 \f$
+  */
+  template <typename FloatType>
+  class normal_equations_for_linear_least_squares
+  {
+  public:
+    SCITBX_LSTBX_DECLARE_ARRAY_TYPE(FloatType);
+
+    /// Construct a linear least-squares problem with the given number of unknowns
+    normal_equations_for_linear_least_squares(int n_parameters)
+      : n_params(n_parameters), a(n_params), right_hand_side(n_params)
+    {}
+
+    /// Add the equation \f$ A_i.^T x = b_i \f$
+    void add_equation(scalar_t b,
+                      af::const_ref<scalar_t> const &a_row,
+                      scalar_t w)
+    {
+      double *p = a.begin();
+      for (int i=0; i<n_params; ++i)  {
+        right_hand_side[i] += w * a_row[i] * b;
+        for (int j=i; j<n_params; ++j) *p++ += w * a_row[i] * a_row[j];
+      }
+    }
+
+    boost::tuple<symmetric_matrix_t, vector_t> equations() {
+      return boost::make_tuple(a.array(), right_hand_side.array());
+    }
+
+  private:
+    int n_params;
+    symmetric_matrix_owning_ref_t a;
+    vector_owning_ref_t right_hand_side;
+  };
+
+
+  /// Normal equations for generic non-linear least-squares
+  /** The least-squares target reads
+
+      \f$ L(x) = \sum r(x)^2 \f$
+
+      where \f$ r \f$ is the vector of residuals.
+  */
+  template <typename FloatType>
+  class normal_equations_for_generic_residuals
+  {
+  public:
+    SCITBX_LSTBX_DECLARE_ARRAY_TYPE(FloatType);
+
+    normal_equations_for_generic_residuals(int n_parameters)
+      : n_params(n_parameters), a(n_params), b(n_params)
+    {}
+
+    void add_datum(scalar_t r, af::const_ref<scalar_t> const &grad_r) {
+      r_sq += r*r;
+      double *p = a.begin();
+      for (int i=0; i<n_params; ++i) {
+        b[i] -= r*grad_r[i];
+        for (int j=i; j<n_params; ++j) *p++ += grad_r[i]*grad_r[j];
+      }
+    }
+
+    boost::tuple<symmetric_matrix_t, vector_t> equations() {
+      return boost::make_tuple(a.array(), b.array());
+    }
+
+  private:
+    int n_params;
+    scalar_t r_sq;
+    symmetric_matrix_owning_ref_t a;
+    vector_owning_ref_t b;
+  };
+
+
+  /// Normal equations for least-squares fit with an overall scale.
+  /** The least-squares target reads
+
+      \f$ L(K, x) = \sum w ( K y_c(x) - y_o )^2 \f$
+
+      where the both of \f$ y_c(x) \f$ and \f$ y_o \f$ are vectors,
+      respectively the model to fit to the data.
+
+      One takes advantage of the separability of the problem:
+
+        - step 1: \f$ K^*(x) = \argmin_K L(K, x) \f$
+
+        - step 2: Build the Newton equations for the problem
+                  \f$ \min_x L(K^*(x), x) \f$
+
+          in the Gauss approximation of small residuals.
+  */
+  template <typename FloatType>
+  class normal_equations_separating_scale_factor
+  {
+  public:
+    SCITBX_LSTBX_DECLARE_ARRAY_TYPE(FloatType);
+
+    /// Construct a least-squares problem with the given number of parameters
+    /** That is the length of the vector \f$ x \f$ */
+    normal_equations_separating_scale_factor(int n_parameters)
+      : yo_dot_yc(0), yc_sq(0), sum_w(0),
+        n_params(n_parameters),
+        a(n_parameters),
+        yo_dot_grad_yc(n_parameters),
+        yc_dot_grad_yc(n_parameters)
+    {}
+
+    /// Add the data yo with the weight w and the corresponding value of the
+    /// model yc and its gradient.
+    void add_datum(scalar_t yc, af::const_ref<scalar_t> const &grad_yc,
+                     scalar_t yo, scalar_t w)
+      {
+        SCITBX_ASSERT(grad_yc.size() == n_params);
+        add_datum(yc, grad_yc.begin(), yo, w);
+      }
+
+    /// Overload for when efficiency is paramount.
+    void add_datum(scalar_t yc, scalar_t *grad_yc, scalar_t yo, scalar_t w) {
+      sum_w += w;
+      yo_dot_yc += w * yo * yc;
+      yc_sq += w * yc * yc;
+      double *pa = a.begin();
+      for (int i=0; i<n_params; ++i) for (int j=i; j<n_params; ++j) {
+        *pa++ += w * grad_yc[i] * grad_yc[j];
+      }
+      for (int i=0; i<n_params; ++i) {
+        yo_dot_grad_yc[i] += w * yo * grad_yc[i];
+        yc_dot_grad_yc[i] += w * yc * grad_yc[i];
+      }
+    }
+
+    /// The separately optimised value of the scale factor, \f$ K^*(x) \f$
+    scalar_t optimised_scale_factor() {
+      return yo_dot_yc/yc_sq;
+    }
+
+    /// The normal equation as a pair (A,b), the normal matrix A and
+    /// the right-hand side b.
+    boost::tuple<symmetric_matrix_t, vector_t> equations() {
+      scalar_t inv_k = 1/optimised_scale_factor();
+      scalar_t inv_yc_norm = 1/std::sqrt(yc_sq);
+      vector_ref_t alpha = yc_dot_grad_yc.ref();
+      vector_owning_ref_t b = yo_dot_grad_yc;
+      for (int i=0; i<n_params; ++i) {
+        scalar_t yo_dot_grad_yc_i = yo_dot_grad_yc[i];
+        scalar_t yc_dot_grad_yc_i = yc_dot_grad_yc[i];
+        b[i] = inv_k*yo_dot_grad_yc_i - yc_dot_grad_yc_i;
+        alpha[i] = (2*yc_dot_grad_yc_i - inv_k*yo_dot_grad_yc_i)*inv_yc_norm;
+      }
+      double *pa = a.begin();
+      for (int i=0; i<n_params; ++i) for (int j=i; j<n_params; ++j) {
+        *pa++ -= alpha[i]*alpha[j];
+      }
+      return boost::make_tuple(a.array(), b.array());
+    }
+
+    /// Ready this for another computation of the normal equations
+    void reset() {
+      yo_dot_yc = 0; yc_sq = 0; sum_w = 0;
+      std::fill(a.begin(), a.end(), scalar_t(0));
+      std::fill(yo_dot_grad_yc.begin(), yo_dot_grad_yc.end(), scalar_t(0));
+      std::fill(yc_dot_grad_yc.begin(), yc_dot_grad_yc.end(), scalar_t(0));
+    }
+
+  private:
+    scalar_t yo_dot_yc, yc_sq, sum_w;
+    int n_params;
+    symmetric_matrix_owning_ref_t a; // normal matrix stored
+                                     // as packed upper diagonal
+    vector_owning_ref_t yo_dot_grad_yc;
+    vector_owning_ref_t yc_dot_grad_yc;
+  };
+
+
+
+  /// Normal equations for least-squares with a separable linear part
+  /** The least-squares target reads
+
+   \f$ L(a, x) = \sum w ( y_o - Y_c(x) a )
+
+   Thus the model \f$ y_c \f$ fitted to the data \f$ y_o \f$ is the linear
+   combination of some \f$ y_{c,1}, \cdots, y_{c,p} \f$ with coefficients
+   \f$ a_1, \cdots, a_p \f$.
+
+   The separability of the problem is exploited by
+
+   - first computing \f$ a^* = \argmin_a L(a, x) \f$,
+
+   - and then building the Newton equations for \f$ L(a^*(x), x) \f$
+     in the Gauss approximation of small residuals.
+   */
+  template <typename FloatType>
+  class normal_equations_separating_linear_part
+  {
+  public:
+    SCITBX_LSTBX_DECLARE_ARRAY_TYPE(FloatType);
+
+    normal_equations_separating_linear_part(int n_parameters, int n_bases)
+      : n(n_parameters), p(n_bases),
+        linear_part(p),
+        grad_yc_trans_grad_yc(n*(n+1)/2 * p*p),
+        yo_trans_grad_yc(af::mat_grid(n,p)),
+        yc_trans_grad_yc(n*p*p),
+        a(n), b(n),
+        has_solved_separable_linear_part(false)
+    {}
+
+    void add_datum(af::const_ref<scalar_t> const &yc,
+                   af::const_ref<scalar_t, af::mat_grid> const &grad_yc,
+                   scalar_t yo, scalar_t w)
+    {
+      sum_w += w;
+      linear_part.add_equation(yo, yc, w);
+
+      double *q = grad_yc_trans_grad_yc.begin();
+      for (int i=0; i<n; ++i) for (int j=i; j<n; ++j) {
+        for (int k=0; k<p; ++k) for (int l=0; l<p; ++l) {
+          *q++ += w * grad_yc(i,k) * grad_yc(j,l);
+        }
+      }
+
+      double *yo_tr_grad_yc = yo_trans_grad_yc.begin();
+      double *yc_tr_grad_yc = yc_trans_grad_yc.begin();
+      for (int i=0; i<n; ++i) {
+        for (int k=0; k<p; ++k) {
+          *yo_tr_grad_yc++ += w * yo * grad_yc(i,k);
+          for (int l=0; l<p; ++l) {
+            *yc_tr_grad_yc++ += w * yc(k) * grad_yc(i,l);
+          }
+        }
+      }
+    }
+
+    vector_t optimal_linear_coefficients() {
+      if(!has_solved_separable_linear_part) optimise_separable_linear_part();
+      return a_star.array();
+    }
+
+    boost::tuple<symmetric_matrix_t, vector_t> equations() {
+      using namespace scitbx::matrix;
+      if(!has_solved_separable_linear_part) optimise_separable_linear_part();
+
+      int sym_p = p*(p+1)/2;
+      scalar_t *a_star_ = a_star.begin();
+      scalar_t *alpha0 = yo_trans_grad_yc.begin();
+
+      scalar_t *alpha = alpha0;
+      scalar_t *yc_tr_grad_yc = yc_trans_grad_yc.begin();
+      for (int i=0; i<n; ++i, yc_tr_grad_yc += p*p, alpha += p)
+      {
+        scalar_t *beta = alpha;
+        matrix_vector(p, p, yc_tr_grad_yc, a_star_, beta, -1., 1.);
+        b[i] = dot(p, a_star_, beta);
+        matrix_transposed_vector(p, p, yc_tr_grad_yc, a_star_, alpha, -1., 1.);
+        forward_substitution_given_transpose(p, lin_u.begin(), alpha);
+      }
+
+      scalar_t *a_ = a.begin();
+      alpha = alpha0;
+      scalar_t *grad_yc_tr_grad_yc = grad_yc_trans_grad_yc.begin();
+      for (int i=0; i<n; ++i) for (int j=i; j<n; ++j) {
+        scalar_t a_ij = quadratic_form(p, grad_yc_tr_grad_yc, a_star_)
+                        -dot(p, &alpha[i*p], &alpha[j*p]);
+        *a_++ = a_ij;
+        grad_yc_tr_grad_yc += p*p;
+      }
+
+      return boost::make_tuple(a.array(), b.array());
+    }
+
+  private:
+    void optimise_separable_linear_part() {
+      using namespace scitbx::matrix;
+      symmetric_matrix_owning_ref_t lin_a;
+      vector_owning_ref_t lin_b;
+      boost::tie(lin_a, lin_b) = linear_part.equations();
+      cholesky::u_transpose_u_decomposition_in_place<scalar_t> chol(lin_a);
+      SCITBX_ASSERT(!chol.failure);
+      lin_u = lin_a.array();
+      cholesky::solve_in_place::using_u_transpose_u(lin_u, lin_b);
+      a_star = lin_b.array();
+      has_solved_separable_linear_part = true;
+    }
+
+    int n;
+    int p;
+
+    normal_equations_for_linear_least_squares<scalar_t> linear_part;
+
+    scalar_t sum_w;
+    af::ref_owning_shared<scalar_t> grad_yc_trans_grad_yc;
+    matrix_owning_ref_t yo_trans_grad_yc;
+    af::ref_owning_shared<scalar_t> yc_trans_grad_yc;
+
+    vector_owning_ref_t b;
+    symmetric_matrix_owning_ref_t a;
+
+    bool has_solved_separable_linear_part;
+    symmetric_matrix_owning_ref_t lin_u;
+    vector_owning_ref_t a_star;
+  };
+
+
+
+}}
+
+#endif // GUARD
---
+++ scitbx/lstbx/tests/tst_normal_equations.cpp
@@ -0,0 +1,152 @@
+#include <scitbx/lstbx/normal_equations.h>
+#include <scitbx/array_family/misc_functions.h>
+#include <scitbx/array_family/initialiser.h>
+#include <scitbx/array_family/simple_io.h>
+#include <iostream>
+
+using namespace scitbx;
+using scitbx::fn::approx_equal;
+
+template <template<typename> class NormalEquationsType>
+struct linear_polynomial_fit
+{
+  double noise;
+  static int const n_data = 10, n_params=3;
+  typedef NormalEquationsType<double> normal_eqns_t;
+  typedef typename normal_eqns_t::symmetric_matrix_owning_ref_t
+          symmetric_matrix_owning_ref_t;
+  typedef typename normal_eqns_t::vector_owning_ref_t
+          vector_owning_ref_t;
+  typedef matrix::cholesky::gill_murray_wright_decomposition_in_place<double>
+          cholesky_t;
+  normal_eqns_t ls;
+
+  linear_polynomial_fit(double noise_)
+    : noise(noise_), ls(n_params)
+  {}
+
+  void compute(double a, double b, double c) {
+    double sign = +1;
+    double w = 1.;
+    for (int i=0; i<n_data; ++i) {
+      double t = i * 1./n_data;
+      double yo = 2*t*t*(1-t) + noise*sign;
+      sign = -sign;
+      double t2=t*t, t3=t*t2;
+      double yc = -t3 + a*t2 + b*t + c;
+      double grad_yc[n_params] = { t2, t, 1 };
+      ls.add_datum(yc, grad_yc, yo, w);
+    }
+  }
+
+  static void exercise()
+  {
+    // the optimal scale factor is 2
+    double const tol=1e-15;
+    {
+      linear_polynomial_fit fit(1e-5);
+      fit.compute(0.5, 0.3, 0.2);
+      symmetric_matrix_owning_ref_t a;
+      vector_owning_ref_t b;
+      boost::tie(a, b) = fit.ls.equations();
+      // C.f. Mathematica notebook tst_normal_equations for how
+      // these reference values have been obtained.
+      symmetric_matrix_owning_ref_t a0(3);
+      af::init(a0) = 1.3178042921566215, 1.5732149143108503, 1.3858589230294014 ,
+                                         1.902836060198941 , 1.4304395500957026 ,
+                                                             0.05219586632944103;
+      vector_owning_ref_t b0(3);
+      af::init(b0) =  0.32134261313408674,
+                      0.3650836430078277 ,
+                     -0.06662453951574325;
+      SCITBX_ASSERT(a.all_approx_equal(a0, 5e-14));
+      SCITBX_ASSERT(b.all_approx_equal(b0, 5e-14));
+    }
+  }
+};
+
+struct linear_combination_fit
+{
+  static int const n=4, p=3, n_data=10;
+  double noise;
+  typedef lstbx::normal_equations_separating_linear_part<double> normal_eqns_t;
+  typedef normal_eqns_t::symmetric_matrix_owning_ref_t
+          symmetric_matrix_owning_ref_t;
+  typedef normal_eqns_t::vector_owning_ref_t
+          vector_owning_ref_t;
+  typedef normal_eqns_t::vector_t vector_t;
+  normal_eqns_t ls;
+
+  linear_combination_fit(double noise_)
+    : noise(noise_), ls(n, p)
+  {}
+
+  void compute(double x1, double x2, double x3, double x4) {
+    double sign = +1;
+    double w = 1.;
+    for (int i=0; i<n_data; ++i) {
+      double t0 = i * 1./n_data;
+      af::tiny<double, p> t(t0, 1-t0, -t0) ;
+      double yo = noise*sign;
+      for (int k=0; k<p; ++k) yo += t[k]*(1 + t[k]*(-1 + 2*t[k]));
+      sign = -sign;
+
+      af::tiny<double, p> yc;;
+      af::versa<double, af::mat_grid> grad_yc_(af::mat_grid(n,p));
+      af::ref<double, af::mat_grid> grad_yc = grad_yc_.ref();
+      for (int k=0; k<p; ++k) {
+        double t1 = t[k];
+        double t2 = t1*t1;
+        double t3 = t1*t2;
+        double t4 = t2*t2;
+        yc[k] = x4*t4 + x3*t3 + x2*t2 + x1*t1;
+        grad_yc(0, k) += t1;
+        grad_yc(1, k) += t2;
+        grad_yc(2, k) += t3;
+        grad_yc(3, k) += t4;
+      }
+      ls.add_datum(yc.ref(), grad_yc, yo, w);
+    }
+  }
+
+  static void exercise() {
+    linear_combination_fit fit(1e-5);
+    fit.compute(1, 2, 3, 4);
+    vector_t a_star = fit.ls.optimal_linear_coefficients();
+    vector_t a_star_0(p);
+    af::init(a_star_0) = -0.210323669833525,
+                          0.208033764252907,
+                         -0.0543166780808711;
+    SCITBX_ASSERT(a_star.all_approx_equal(a_star_0, 1e-14));
+    symmetric_matrix_owning_ref_t a;
+    vector_owning_ref_t b;
+    boost::tie(a, b) = fit.ls.equations();
+
+    vector_owning_ref_t b0(n);
+    af::init(b0) = 0.00542544409277865,   0.00539923674666925,
+                   0.000326266092452357, -0.00430067896586856;
+    SCITBX_ASSERT(b.all_approx_equal(b0, 1e-14));
+
+    symmetric_matrix_owning_ref_t a0(n);
+    af::init(a0) =
+      0.00300136362055691, 0.00227988574887292, 0.000520356169835869,
+                                                          -0.000924189883757925,
+
+                           0.00222887407045411, 0.000625283216528185,
+                                                          -0.000803561698174109,
+
+                                                0.0000743095490471244,
+                                                          -0.000416896289395314,
+
+                                                           -0.000129669204394118;
+    SCITBX_ASSERT(a.all_approx_equal(a0, 1e-14))(a.const_ref());
+  }
+};
+
+int main() {
+  linear_polynomial_fit<
+    lstbx::normal_equations_separating_scale_factor>::exercise();
+  linear_combination_fit::exercise();
+  std::cout << "OK\n";
+  return 0;
+}
