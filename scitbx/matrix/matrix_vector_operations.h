1ba82b436d4c3a1b9f9daf560a6e5b54f64bb0bf
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-06-27 21:16:39 +0000

    Matrix-vector operations (and those vector-vector needed for implementation): BLAS2 and more

---
+++ scitbx/matrix/matrix_vector_operations.h
@@ -0,0 +1,122 @@
+#ifndef SCITBX_MATRIX_MATRIX_VECTOR_OPERATIONS_H
+#define SCITBX_MATRIX_MATRIX_VECTOR_OPERATIONS_H
+
+#include <scitbx/matrix/vector_operations.h>
+
+namespace scitbx { namespace matrix {
+
+  /// y := alpha A x + beta y for a general m x n matrix A
+  /** A shall be passed in row-major layout to a */
+  template <typename T>
+  void matrix_vector(int m, int n,
+                     T const *a, T const *x, T *y,
+                     T alpha=1., T beta=0.)
+  {
+    scale_vector(m, y, beta);
+    if (alpha == 0.) return;
+    for (int i=0; i<m; ++i) {
+      T ax_i = 0;
+      for (int j=0; j<n; ++j) {
+        T a_ij = *a++;
+        ax_i += a_ij * x[j];
+      }
+      y[i] += alpha * ax_i;
+    }
+  }
+
+  /// y := alpha A^T x + beta y for a general m x n matrix A
+  /** A shall be passed in row-major layout to a */
+  template <typename T>
+  void matrix_transposed_vector(int m, int n,
+                                T const *a, T const *x, T *y,
+                                T alpha=1., T beta=0.)
+  {
+    scale_vector(m, y, beta);
+    if (alpha == 0.) return;
+    for (int i=0; i<m; ++i) {
+      T alpha_x_i = alpha * x[i];
+      for (int j=0; j<n; ++j) {
+        T a_ij = *a++;
+        y[j] += a_ij * alpha_x_i;
+      }
+    }
+  }
+
+  /// y := alpha A x + beta y for symmetric matrix A
+  /** The upper diagonal of A packed by row is passed in the range
+      [ a, a + n*(n+1)/2 )
+   */
+  template <typename T>
+  void symmetric_packed_u_vector(int n,
+                                 T const *a, T const *x, T *y,
+                                 T alpha=1., T beta=0.)
+  {
+    scale_vector(n, y, beta);
+    if (alpha == 0.) return;
+    for (unsigned i=0; i<n; ++i) {
+      T alpha_x_i = alpha * x[i];
+      T ax_i = 0;
+      T a_ii = *a++;
+      y[i] += a_ii * alpha_x_i;
+      for (unsigned j=i+1; j<n; ++j) {
+        T a_ij = *a++, &a_ji = a_ij;
+        ax_i += a_ij * x[j];
+        y[j] += a_ji * alpha_x_i;
+      }
+      y[i] += alpha * ax_i;
+    }
+  }
+
+
+  /// A := alpha x x^T + A for symmetric matrix A
+  /** The upper diagonal of A packed by rows shall be passed in the range
+      [ a, a +  n*(n+1)/2 )
+   */
+  template <typename T>
+  void symmetric_packed_u_rank_1_update(int n,
+                                        T *a, T const *x,
+                                        T alpha=1.)
+  {
+    for (int i=0; i<n; ++i) {
+      T alpha_x_i = alpha * x[i];
+      for (int j=i; j<n; ++j) {
+        *a++ += alpha_x_i * x[j];
+      }
+    }
+  }
+
+  /// x^T A x for symmetric matrix A
+  /** The upper diagonal of A packed by rows shall be passed in the range
+   [ a, a +  n*(n+1)/2 )
+   */
+  template <typename T>
+  T quadratic_form_packed_u(int n, T const *a, T const *x) {
+    T diag = 0., off_diag = 0.;
+    for (int i=0; i<n; ++i) {
+      T a_ii = *a++;
+      diag += x[i] * a_ii * x[i];
+      T off_diag_i = 0.;
+      for (int j=i+1; j<n; ++j) {
+        T a_ij = *a++;
+        off_diag_i += a_ij * x[j];
+      }
+      off_diag += x[i] * off_diag_i;
+    }
+    return diag + 2.*off_diag;
+  }
+
+  /// x^T A x for a square matrix A
+  /** A shall be passed as a in row-major layout */
+  template <typename T>
+  T quadratic_form(int n, T const *a, T const *x) {
+    T s = 0;
+    for (int i=0; i<n; ++i) for (int j=0; j<n; ++j) {
+      T a_ij = *a++;
+      s += x[i] * a_ij * x[j];
+    }
+    return s;
+  }
+
+}}
+
+#endif // GUARD
