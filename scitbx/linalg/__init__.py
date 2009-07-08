34a3fab280b2a5cb160dd671a6d6d4ae7ff7ff79
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-07-08 22:07:42 +0000

    support for scitbx.matrix by equality_ratio test

--- scitbx/linalg/__init__.py
+++ scitbx/linalg/__init__.py
@@ -1,8 +1,20 @@
 from scitbx.linalg.ext import *
 from scitbx.array_family import flex
+import scitbx.matrix
 
 class random_normal_matrix_generator(ext.random_normal_matrix_generator):
 
   def __init__(self, m, n):
     super(random_normal_matrix_generator, self).__init__(m, n)
     self.state = flex.random_generator.getstate()
+
+
+def matrix_equality_ratio(a, b, eps=None):
+  if isinstance(a, scitbx.matrix.rec):
+    a = a.as_flex_double_matrix()
+  if isinstance(b, scitbx.matrix.rec):
+    b = b.as_flex_double_matrix()
+  if eps is not None:
+    return ext.matrix_equality_ratio(a, b, eps)
+  else:
+    return ext.matrix_equality_ratio(a, b)
