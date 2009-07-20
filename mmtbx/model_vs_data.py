564fd2983a13546484a4e19e83203bacd06c9275
Pavel Afonine <pafonine@users.noreply.github.com>
2009-07-20 23:49:41 +0000

    enable new mask in model_vs_data

--- mmtbx/model_vs_data.py
+++ mmtbx/model_vs_data.py
@@ -550,12 +550,9 @@ def run(args,
   bss_params.b_sol_grid_search_min = 20.0
   bss_params.k_sol_step = 0.3
   bss_params.b_sol_step = 30.0
-  mp = masks.mask_master_params.extract() # XXX remove once Marat adds a fix.
-  mp.use_asu_masks = False                # XXX remove once Marat adds a fix.
   fmodel = utils.fmodel_simple(xray_structures = xray_structures,
                                f_obs           = f_obs,
                                r_free_flags    = r_free_flags,
-                               mask_params     = mp, # XXX remove once Marat adds a fix.
                                bss_params      = bss_params)
   n_outl = f_obs.data().size() - fmodel.f_obs.data().size()
   mvd_obj.collect(data =
