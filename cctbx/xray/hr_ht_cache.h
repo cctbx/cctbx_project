ac5262982b1cf368418dc6d5af206abd9b0f3e8c
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-07-22 12:55:04 +0000

    - set f_h_inv_t to 1 if is_origin_centric is true   rationale: client code may wish to handle only origin centric space-group   as a special case whereas origin centric miller indices in other centric   space group settings are handled generically (performance hit is negligeable   as origin centric miller indices are a plane, i.e. measure zero) - documentation

--- cctbx/xray/hr_ht_cache.h
+++ cctbx/xray/hr_ht_cache.h
@@ -5,6 +5,7 @@
 
 namespace cctbx { namespace xray { namespace structure_factors {
 
+  /// A pair \f$(hR, ht)\f$ where \f$(R \mid t)\f$ is a symmetry element.
   template <typename FloatType>
   struct hr_ht_group
   {
@@ -16,32 +17,46 @@ namespace cctbx { namespace xray { namespace structure_factors {
       ht(ht_)
     {}
 
+    /// \f$hR\f$
     miller::index<> hr;
+
+    /// \f$ht\f$
     FloatType ht;
   };
 
+
+  /** \brief The miller indices \f$h R\f$ and the scalars \f$h t\f$
+       where \f$(R \mid t)\f$ spans the elements of a space group \f$G\f$.
+
+    A centric space group is partition as the coset decomposition
+    \f$G = G' \bigcup \bar{1}G''\f$ and only the elements of G' are considered.
+   */
   template <typename FloatType>
   struct hr_ht_cache
   {
     typedef FloatType f_t;
 
+    /// Construct the cache using std::cos and std::sin.
     hr_ht_cache(sgtbx::space_group const& space_group,
                 miller::index<> const& h)
     {
       init(space_group, h);
     }
 
+    /// Construct the cache using the functor cos_sin
+    /** That functor implements \f$\theta \mapsto \exp i 2\pi \theta\f$ */
     template<class CosSinType>
     hr_ht_cache(CosSinType const &cos_sin,
                 sgtbx::space_group const& space_group,
                 miller::index<> const& h)
     {
       init(space_group, h);
-      if (!is_origin_centric && is_centric) {
-        f_h_inv_t = cos_sin.get(h_inv_t);
+      if (is_centric) {
+        f_h_inv_t = !is_origin_centric ? cos_sin.get(h_inv_t) : 1;
       }
     }
 
+    /// Implementation of the constructors
     void init(sgtbx::space_group const& space_group,
               miller::index<> const& h)
     {
@@ -64,11 +79,27 @@ namespace cctbx { namespace xray { namespace structure_factors {
       }
     }
 
+    /// Whether the space group is centric
     bool is_centric;
+
+    /// Whether the reflection is origin centric in the given space group
+    /** i.e. whether h_inv_t is zero */
     bool is_origin_centric;
+
+    /// Order of the centring translation subgroup
     f_t ltr_factor;
+
+    /// \f$h t_{\bar{1}}\f$ where \f$t_{\bar{1}}\f$ is the translation part
+    /// of the inversion.
+    /** Only defined for a centric space-group and zero for an origin centric one
+     */
     f_t h_inv_t;
+
+    /// \f$ \exp i 2 \pi h t_{\bar{1}} \f$
+    /** Only defined for a centric space group and 1 for an origin centric one */
     std::complex<f_t> f_h_inv_t;
+
+    /// The list of pairs \f$(hR, ht)\f$.
     af::small<hr_ht_group<f_t>, sgtbx::n_max_repr_rot_mx> groups;
   };
 
