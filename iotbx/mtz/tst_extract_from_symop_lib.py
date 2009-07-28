5e8546ac52f66410cac82323e29a1d20312a3040
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2009-07-28 01:15:45 +0000

    iotbx/mtz/tst_extract_from_symop_lib.py: check ccp4_id_counts, ccp4_symbol_counts

--- iotbx/mtz/tst_extract_from_symop_lib.py
+++ iotbx/mtz/tst_extract_from_symop_lib.py
@@ -1,6 +1,7 @@
 from iotbx.mtz import extract_from_symop_lib
 from cctbx import sgtbx
 from libtbx.utils import format_cpu_times
+import libtbx
 import sys
 
 def exercise_230():
@@ -18,12 +19,16 @@ def exercise_230():
 
 def exercise_symop_lib_recycling():
   file_iter = open(extract_from_symop_lib.ccp4io_symop_lib_path)
+  ccp4_id_counts = libtbx.dict_with_default_0()
+  ccp4_symbol_counts = libtbx.dict_with_default_0()
   for line in file_iter:
     flds = line.split(None, 4)
     ccp4_id = flds[0]
+    ccp4_id_counts[ccp4_id] += 1
     space_group_number = int(ccp4_id[-3:])
     order_z = int(flds[1])
     given_ccp4_symbol = flds[3]
+    ccp4_symbol_counts[given_ccp4_symbol] += 1
     group = extract_from_symop_lib.collect_symops(
       file_iter=file_iter, order_z=order_z)
     assert group.order_z() == order_z
@@ -32,6 +37,17 @@ def exercise_symop_lib_recycling():
       space_group_info=space_group_info)
     assert retrieved_ccp4_symbol == given_ccp4_symbol
     assert space_group_info.type().number() == space_group_number
+  for ccp4_id,count in ccp4_id_counts.items():
+    if (count != 1):
+      raise RuntimeError(
+        'ccp4 id "%s" appears %d times (should be unique).'
+          % (ccp4_id, count))
+  ccp4_symbol_counts = libtbx.dict_with_default_0()
+  for ccp4_symbol,count in ccp4_symbol_counts.items():
+    if (count != 1):
+      raise RuntimeError(
+        'ccp4 symbol "%s" appears %d times (should be unique).'
+          % (ccp4_symbol, count))
 
 def exercise(args):
   assert len(args) == 0
