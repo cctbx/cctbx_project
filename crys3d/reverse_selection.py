f9e27737e803230b9a6fe18ee7b5d52758205d3f
Nathaniel Echols <nathaniel.echols@gmail.com>
2009-07-29 22:52:35 +0000

    moved mouse selection handling to new module with partial tests; started range selections

---
+++ crys3d/reverse_selection.py
@@ -0,0 +1,435 @@
+# XXX: internal data for mouse selections in wx_selection_editor.py
+
+from scitbx.array_family import flex
+from libtbx.utils import Sorry
+from libtbx import adopt_init_args
+
+class chain_selection_info (object) :
+  def __init__ (self, chain_id, resseq_start, resseq_end) :
+    adopt_init_args(self, locals())
+    self.ranges = set()
+    self.ranges_by_altloc = {}
+    self.remove_ranges = set()
+    self.remove_ranges_by_altloc = {}
+
+  def add_range (self, start, end, altloc=None) :
+    assert start >= self.resseq_start and start <= self.resseq_end
+    assert end >= self.resseq_start and end <= self.resseq_end
+    if altloc is None :
+      self.ranges |= set([ x for x in range(start, end+1)])
+    else :
+      ranges = self.ranges_by_altloc.get(altloc, set())
+      ranges |= set([ x for x in range(start, end+1)])
+      remove_ranges = self.remove_ranges_by_altloc.get(altloc, set())
+      remove_ranges -= ranges
+      self.remove_ranges_by_altloc[altloc] = remove_ranges
+      self.ranges_by_altloc[altloc] = ranges
+
+  def remove_range (self, start, end, altloc=None) :
+    assert start >= self.resseq_start and start <= self.resseq_end
+    assert end >= self.resseq_start and end <= self.resseq_end
+    if altloc is None :
+      self.remove_ranges |= set([ x for x in range(start, end+1)])
+      self.ranges -= self.remove_ranges
+    else :
+      remove_ranges = self.remove_ranges_by_altloc.get(altloc, set())
+      remove_ranges |= set([ x for x in range(start, end+1)])
+      ranges = self.ranges_by_altloc.get(altloc, set())
+      ranges -= remove_ranges
+      self.remove_ranges_by_altloc[altloc] = remove_ranges
+      if len(ranges) == 0 :
+        self.ranges_by_altloc.pop(altloc)
+      else :
+        self.ranges_by_altloc[altloc] = ranges
+    if len(self.ranges) == 0 and len(self.ranges_by_altloc) == 0 :
+      return True
+
+  def __str__ (self) :
+    sele_str = "chain '%s'" % self.chain_id
+    #if len(self.ranges) > 0 :
+    return sele_str
+
+class residue_selection_info (object) :
+  def __init__ (self, chain_id, resid, altloc=None) :
+    adopt_init_args(self, locals())
+
+  def __str__ (self) :
+    if self.altloc is None :
+      return "chain '%s' and resid '%s'" % (self.chain_id, self.resid)
+    else :
+      return "chain '%s' and resid '%s' and altloc '%s'" % (self.chain_id,
+        self.resid, self.altloc)
+
+class atom_selection_info (object) :
+  def __init__ (self, i_seq, atom) : #chain_id, resid, atom_name, altloc) :
+    adopt_init_args(self, locals())
+
+  def __str__ (self) :
+    atom = self.atom
+    altloc = atom.altloc
+    if altloc == "" :
+      altloc = " "
+    return ("chain '%s' and resid '%s' and name '%s' and altloc '%s'" %
+      (atom.chain_id, atom.resid(), atom.name, altloc))
+
+class mouse_selection_manager (object) :
+  def __init__ (self) :
+    self.saved_selection = "none"
+    self.selection_string = "none"
+    self._selection_callback = None
+    self.start_i_seq = None
+    self.end_i_seq = None
+
+  def selection_callback (self, selection_string, atom_selection) :
+    if self._selection_callback is not None :
+      self._selection_callback(selection_string, atom_selection)
+
+  def set_selection_callback (self, callback) :
+    assert hasattr(callback, "__call__") or callback is None
+    self._selection_callback = callback
+
+  def set_mmtbx_selection_function (self, mmtbx_selection_function) :
+    self.mmtbx_selection_function = mmtbx_selection_function
+
+  def update_selection_handlers (self, pdb_hierarchy,
+      mmtbx_selection_function) :
+    self.mmtbx_selection_function = mmtbx_selection_function
+    self.selection_cache = pdb_hierarchy.atom_selection_cache()
+    self.atom_selection = flex.bool()
+    #--- XXX: for testing only
+    if not hasattr(self, "atom_index") :
+      self.atom_index = []
+      for atom in pdb_hierarchy.atoms_with_labels() :
+        self.atom_index.append(atom)
+    if not hasattr(self, "pdb_hierarchy") :
+      self.pdb_hierarchy = pdb_hierarchy
+    #---
+    self._chain_ends = {}
+    for chain in pdb_hierarchy.chains() :
+      if len(chain.conformers()) == 1 :
+        residues = chain.residues()
+        resseq_start = residues[0].resseq_as_int()
+        resseq_end = residues[-1].resseq_as_int()
+      else :
+        resseq_start = 100000
+        resseq_end = -100000
+        for conf in chain.conformers() :
+          residues = conf.residues()
+          if residues[0].resseq_as_int() < resseq_start :
+            resseq_start = residues[0].resseq_as_int()
+          if residues[-1].resseq_as_int() > resseq_end :
+            resseq_end = residues[-1].resseq_as_int()
+      self._chain_ends[chain.id] = (resseq_start, resseq_end)
+    self.clear_selection()
+
+  def clear_selection (self) :
+    self.selected_chains = {}
+    self.deselected_chains = {}
+    self.selected_atoms = []
+    self.deselected_atoms = []
+    self.selected_residues = []
+    self.deselected_residues = []
+    self.apply_selection(self.saved_selection)
+
+  def selection_size (self) :
+    return self.selection_i_seqs.size()
+
+  def apply_selection (self, selection_string) :
+    atom_selection = self.get_atom_selection(selection_string)
+    if atom_selection is None :
+      self.selection_string = "none"
+      atom_selection = self.selection_cache.selection("none")
+    else :
+      self.selection_string = selection_string
+    self.atom_selection = atom_selection
+    self.selection_i_seqs = atom_selection.iselection()
+    if self.selection_callback is not None :
+      self.selection_callback(self.selection_string, self.atom_selection)
+    if atom_selection is None :
+      raise Sorry("Invalid selection '%s'."%selection_string)
+
+  def get_atom_selection (self, selection_string) :
+    try :
+      if self.mmtbx_selection_function is not None :
+        atom_selection = self.mmtbx_selection_function(
+          string=selection_string,
+          cache=self.selection_cache)
+      else :
+        atom_selection = self.selection_cache.selection(selection_string)
+    except KeyboardInterrupt :
+      raise
+    except Exception, e :
+      print e
+      atom_selection =None
+    return atom_selection
+
+  def revert_selection (self) :
+    self.apply_selection(self.saved_selection)
+
+  def set_initial_selection (self, selection_string) :
+    self.saved_selection = selection_string
+    self.apply_selection(selection_string)
+
+  def reset_range_selection (self, i_seq) :
+    self.start_i_seq = None
+
+  def start_range_selection (self, i_seq) :
+    self.start_i_seq = i_seq
+
+  def end_range_selection (self, i_seq, deselect, ignore_altloc) :
+    success = False
+    print i_seq
+    if self.start_i_seq is not None :
+      start_atom = self.atom_index[self.start_i_seq]
+      end_atom = self.atom_index[i_seq]
+      if (start_atom.chain_id == end_atom.chain_id and
+          (ignore_altloc or start_atom.altloc == end_atom.altloc)) :
+        chain_id = start_atom.chain_id
+        if chain_id in self.selected_chains :
+          chain_info = self.selected_chains[chain_id]
+        else :
+          (resseq_start, resseq_end) = self._chain_ends[chain_id]
+          chain_info = chain_selection_info(chain_id, resseq_start, resseq_end)
+        altloc = None
+        if not ignore_altloc :
+          altloc = start_atom.altloc
+        start_range = start_atom.resseq_as_int()
+        end_range = end_atom.resseq_as_int()
+        if start_range > end_range :
+          start_range = end_atom.resseq_as_int()
+          end_range = start_atom.resseq_as_int()
+        if deselect :
+          remove_chain = chain_info.remove_range(start_range, end_range,
+            altloc)
+          if remove_chain :
+            self.selection_chains.pop(chain_id)
+        else :
+          chain_info.add_range(start_range, end_range, altloc)
+          self.selected_chains[chain_id] = chain_info
+        success = True
+        self.construct_selection()
+    self.reset_range_selection()
+    return success
+
+  def toggle_chain_selection (self, i_seq) :
+    atom = self.atom_index[i_seq]
+    chain_id = atom.chain_id
+    if chain_id in self.selected_chains :
+      chain_info = self.selected_chains.pop(chain_id)
+      chain_sel = self.selection_cache.selection(str(chain_info))
+      self.remove_redundant_residues(chain_sel, self.deselected_residues)
+      self.remove_redundant_atoms(chain_sel, self.deselected_atoms)
+    else :
+      (resseq_start, resseq_end) = self._chain_ends[chain_id]
+      chain_info = chain_selection_info(chain_id, resseq_start, resseq_end)
+      self.selected_chains[chain_id] = chain_info
+      chain_sel = self.selection_cache.selection(str(chain_info))
+      self.remove_redundant_residues(chain_sel, self.selected_residues)
+      self.remove_redundant_atoms(chain_sel, self.selected_atoms)
+    self.construct_selection()
+
+  def remove_redundant_residues (self, main_selection, residue_list) :
+    i = 0
+    while i < len(residue_list) :
+      residue_info = residue_list[i]
+      resi_sel = self.selection_cache.selection(str(residue_info))
+      if main_selection.is_super_set(resi_sel) :
+        residue_list.pop(i)
+      else :
+        i += 1
+
+  def remove_redundant_atoms (self, main_selection, atom_list) :
+    i = 0
+    while i < len(atom_list) :
+      i_seq = atom_list[i]
+      if main_selection[i_seq] :
+        atom_list.pop(i)
+      else :
+        i += 1
+
+  def toggle_residue_selection (self, i_seq, ignore_altloc=True) :
+    atom = self.atom_index[i_seq]
+    if ignore_altloc :
+      resi_info = residue_selection_info(atom.chain_id, atom.resid())
+    else :
+      resi_info = residue_selection_info(atom.chain_id, atom.resid(),
+        atom.altloc)
+    resi_sel = self.selection_cache.selection(str(resi_info))
+    if self.atom_selection.is_super_set(resi_sel) :
+      self.deselected_residues.append(resi_info)
+      self.remove_redundant_residues(resi_sel, self.selected_residues)
+    else :
+      self.selected_residues.append(resi_info)
+      self.remove_redundant_residues(resi_sel, self.deselected_residues)
+    self.remove_redundant_atoms(resi_sel, self.selected_atoms)
+    self.remove_redundant_atoms(resi_sel, self.deselected_atoms)
+    self.construct_selection()
+
+  def toggle_atom_selection (self, i_seq) :
+    atom = self.atom_index[i_seq]
+    if self.atom_selection[i_seq] : #and not i_seq in self.deselected_atoms :
+      self.deselected_atoms.append(i_seq)
+      if i_seq in self.selected_atoms :
+        self.selected_atoms.remove(i_seq)
+    elif not i_seq in self.selected_atoms :
+      self.selected_atoms.append(i_seq)
+      if i_seq in self.deselected_atoms :
+        self.deselected_atoms.remove(i_seq)
+    self.construct_selection()
+
+  def construct_selection (self) :
+    final_selection = ""
+    # Part 1: stuff we want
+    clauses = []
+    for chain_id, chain_info in self.selected_chains.iteritems() :
+      clauses.append(str(chain_info))
+    chains_selection_str = assemble_selection_clauses(clauses)
+    selection1 = self.get_atom_selection(chains_selection_str)
+    deselection1 = selection1.__invert__()
+    self.remove_redundant_residues(selection1, self.selected_residues)
+    self.remove_redundant_residues(deselection1, self.deselected_residues)
+    for residue_info in self.selected_residues :
+      residue_selection_str = str(residue_info)
+      clauses.append(residue_selection_str)
+    chains_and_resi_sel_str = assemble_selection_clauses(clauses)
+    selection2 = self.get_atom_selection(chains_and_resi_sel_str)
+    deselection2 = selection2.__invert__()
+    self.remove_redundant_atoms(selection2, self.selected_atoms)
+    self.remove_redundant_atoms(deselection2, self.deselected_atoms)
+    for i_seq in self.selected_atoms :
+      atom_info = atom_selection_info(i_seq, self.atom_index[i_seq])
+      clauses.append(str(atom_info))
+    positive_selection = assemble_selection_clauses(clauses)
+    # Part 2: stuff we don't want
+    clauses = []
+    for residue_info in self.deselected_residues :
+      residue_selection_str = str(residue_info)
+      clauses.append(residue_selection_str)
+    for i_seq in self.deselected_atoms :
+      atom_info = atom_selection_info(i_seq, self.atom_index[i_seq])
+      clauses.append(str(atom_info))
+    negative_selection = assemble_selection_clauses(clauses)
+    # assemble final selection
+    if positive_selection != "" :
+      if negative_selection != "" :
+        final_selection = "(%s) and not (%s)" % (positive_selection,
+                                                 negative_selection)
+      else :
+        final_selection = positive_selection
+    else :
+      final_selection = "none"
+    self.apply_selection(final_selection)
+
+def assemble_selection_clauses (clauses) :
+  if len(clauses) == 0 :
+    return ""
+  elif len(clauses) == 1 : # not necessary, but looks nicer
+    return clauses[0]
+  else :
+    return "(" + ") or (".join(clauses) + ")"
+
+########################################################################
+def exercise () :
+  from mmtbx.monomer_library import pdb_interpretation
+  import cStringIO
+  open("tmp.pdb", "w").write("""\
+CRYST1   50.800   50.800  155.300  90.00  90.00  90.00 P 43 21 2     8
+ATOM      4  N   SER A   1       8.753  29.755  61.685  1.00 49.13
+ATOM      5  CA  SER A   1       9.242  30.200  62.974  1.00 46.62
+ANISOU    5  CA  SER A   1    343    490   2719    -45   -169    617
+ATOM      6  C   SER A   1      10.453  29.500  63.579  1.00 41.99
+ATOM      7  O   SER A   1      10.593  29.607  64.814  1.00 43.24
+ANISOU    7  O   SER A   1    343    490   2719    -45   -169    617
+ATOM      8  CB  SER A   1       8.052  30.189  63.974  1.00 53.00
+ATOM      9  OG  SER A   1       7.294  31.409  63.930  1.00 57.79
+ATOM     10  N   ARG A   2      11.360  28.819  62.827  1.00 36.48
+ATOM     11  CA  ARG A   2      12.548  28.316  63.532  1.00 30.20
+ATOM     12  C   ARG A   2      13.502  29.501  63.500  1.00 25.54
+ATOM     13  O   ARG A   2      13.730  30.037  62.407  1.00 23.86
+ATOM     14  CB  ARG A   2      13.241  27.119  62.861  1.00 27.44
+ATOM     15  CG  ARG A   2      12.412  25.849  62.964  1.00 23.66
+ATOM     16  CD  ARG A   2      13.267  24.651  63.266  1.00 23.98
+ATOM     17  NE  ARG A   2      13.948  24.115  62.135  1.00 22.71
+ATOM     18  CZ  ARG A   2      15.114  23.487  62.201  1.00 21.38
+ATOM     19  NH1 ARG A   2      15.845  23.331  63.301  1.00 19.34
+ATOM     20  NH2 ARG A   2      15.575  23.030  61.051  1.00 26.66
+ATOM     21  N   PRO A   3J     13.947  29.997  64.680  1.00 22.94
+ATOM     22  CA  PRO A   3J     14.902  31.100  64.827  1.00 20.19
+ATOM     23  C   PRO A   3J     16.195  30.718  64.086  1.00 18.44
+ATOM     24  O   PRO A   3J     16.545  29.521  64.086  1.00 19.76
+ATOM     25  CB  PRO A   3J     15.133  31.218  66.313  1.00 19.17
+ATOM     26  CG  PRO A   3J     14.065  30.364  66.951  1.00 15.12
+ATOM     27  CD  PRO A   3J     13.816  29.289  65.966  1.00 19.56
+ATOM     28  N  AILE A   4      16.953  31.648  63.512  1.00 15.29
+ATOM     29  CA AILE A   4      18.243  31.372  62.859  1.00 14.32
+ATOM     30  C  AILE A   4      19.233  32.112  63.743  1.00 13.54
+ATOM     31  O  AILE A   4      19.105  33.315  64.009  1.00 11.84
+ATOM     32  CB AILE A   4      18.298  31.951  61.406  1.00 13.62
+ATOM     33  CG1AILE A   4      17.157  31.300  60.620  1.00 18.39
+ATOM     34  CG2AILE A   4      19.661  31.747  60.743  1.00 13.64
+ATOM     35  CD1AILE A   4      16.879  32.102  59.355  1.00 16.69
+ATOM     28  N  BILE A   4      16.953  31.648  63.512  1.00 15.29
+ATOM     29  CA BILE A   4      18.243  31.372  62.859  1.00 14.32
+ATOM     30  C  BILE A   4      19.233  32.112  63.743  1.00 13.54
+ATOM     31  O  BILE A   4      19.105  33.315  64.009  1.00 11.84
+ATOM     32  CB BILE A   4      18.298  31.951  61.406  1.00 13.62
+ATOM     33  CG1BILE A   4      17.157  31.300  60.620  1.00 18.39
+ATOM     34  CG2BILE A   4      19.661  31.747  60.743  1.00 13.64
+ATOM1200035  CD1BILE A   4      16.879  32.102  59.355  1.00 16.69
+HETATM 1475  S   SO4 S 188      31.424  42.923  60.396  1.00 55.69           S4+
+HETATM 1476  O1  SO4 S 188      31.631  41.513  60.336  1.00 59.84           O1-
+HETATM 1477  O2  SO4 S 188      32.533  43.699  59.932  1.00 49.98           O1-
+HETATM 1478  O3  SO4 S 188      31.128  43.217  61.738  1.00 59.44           O1-
+HETATM 1479  O4  SO4 S 188      30.353  43.201  59.539  1.00 60.54           O1-
+HETATM 1480  O   HOH W 200      29.478  23.354  61.364  1.00  8.67      WATE
+END""")
+  out = cStringIO.StringIO()
+  processed_pdb_file = pdb_interpretation.run(args=["tmp.pdb"], log=out)
+  m = mouse_selection_manager()
+  pdb_hierarchy = processed_pdb_file.all_chain_proxies.pdb_hierarchy
+  pdb_hierarchy.atoms().reset_i_seq()
+  m.update_selection_handlers(
+    pdb_hierarchy=pdb_hierarchy,
+    mmtbx_selection_function=processed_pdb_file.all_chain_proxies.selection)
+  assert m.selection_size() == 0
+  m.apply_selection("chain A")
+  assert m.selection_size() == 40
+  m.clear_selection()
+  m.toggle_chain_selection(5)
+  assert m.selection_size() == 40
+  m.toggle_residue_selection(10)
+  assert m.selection_size() == 29
+  m.toggle_atom_selection(10) # XXX: doesn't work!
+  assert m.selection_size() == 29
+  m.toggle_atom_selection(20)
+  assert m.selection_size() == 28
+
+  from iotbx import pdb
+  pdb_hierarchy = pdb.input(source_info=None, lines=flex.split_lines("""\
+HETATM 4049  O   HOH W   1       2.954  13.042  11.632  1.00 37.53           O
+HETATM 4050  O   HOH W   2       5.539  14.595  10.951  1.00 31.25           O
+HETATM 4051  O   HOH W   3      -2.971  14.661  14.669  1.00 38.68           O
+HETATM 4052  O   HOH W   4       6.281  34.000   7.684  1.00 39.58           O
+HETATM 4053  O   HOH W   5      16.004   9.039  10.335  1.00 37.31           O
+HETATM 4054  O   HOH W   6       2.144   5.718  20.447  1.00 49.77           O
+HETATM 4055  O   HOH W   7      -1.180  10.517  14.630  1.00 32.95           O
+HETATM 4056  O   HOH W   8       9.227   8.636  12.535  1.00 32.52           O
+HETATM 4057  O   HOH W   9      11.070  -0.570  15.047  1.00 30.24           O
+HETATM 4058  O   HOH W  10      15.630  -6.169  12.853  1.00 31.08           O
+HETATM 4059  O   HOH W  11      14.854  -8.299  16.887  1.00 32.65           O
+HETATM 4060  O   HOH W  12      27.586   0.391  24.184  1.00 31.29           O
+HETATM 4061  O   HOH W  13       3.240   7.801  38.401  1.00 32.09           O
+END""")).construct_hierarchy()
+  m = mouse_selection_manager()
+  pdb_hierarchy.atoms().reset_i_seq()
+  m.update_selection_handlers(pdb_hierarchy=pdb_hierarchy,
+    mmtbx_selection_function=None)
+  assert m.selection_size() == 0
+  m.apply_selection("chain W")
+  assert m.selection_size() == 13
+  print "OK"
+
+if __name__ == "__main__" :
+  exercise()
+
+#---end
