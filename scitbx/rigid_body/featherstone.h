25037260763e543b3f96424d3ae1fab5a76a294b
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2009-07-30 02:35:05 +0000

    scitbx/rigid_body/featherstone.h: bug fix

--- scitbx/rigid_body/featherstone.h
+++ scitbx/rigid_body/featherstone.h
@@ -107,7 +107,7 @@ namespace scitbx { namespace rigid_body { namespace featherstone {
           af::tiny<ft, 6> const& vp = result[body->parent];
           vec3<ft> r_va = cb_up.r * vec3<ft>(&vp[0]);
           vec3<ft> vl = cb_up.r * vec3<ft>(&vp[3]) + cb_up.t.cross(r_va);
-          res_ib = spatial_lib::as_tiny_6(r_va, vl);
+          res_ib += spatial_lib::as_tiny_6(r_va, vl);
         }
       }
       return result;
