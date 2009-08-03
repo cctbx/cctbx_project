ea3209a734384ab42989e7ee56f53a17950ba08f
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2009-08-03 00:36:57 +0000

    scitbx/rigid_body: essence and C++ version: all code which is independent of potential_obj moved from tardy.{py,h} to featherstone.{py,h}, tardy model inherits from featherstone system model

--- scitbx/rigid_body/ext.cpp
+++ scitbx/rigid_body/ext.cpp
@@ -16,12 +16,50 @@ namespace scitbx { namespace rigid_body { namespace ext {
     typedef featherstone::system_model<> wt;
     typedef wt::ft ft;
 
+    static
+    boost::python::object
+    sum_of_masses_in_each_tree(
+      wt const& O)
+    {
+      af::shared<std::pair<int, double> >
+        somiet = O.sum_of_masses_in_each_tree();
+      return boost_python::array_as_list(somiet.begin(), somiet.size());
+    }
+
     static void
     wrap()
     {
       using namespace boost::python;
+      typedef return_value_policy<copy_const_reference> ccr;
       object none;
-      class_<wt>("featherstone_system_model", no_init)
+      class_<wt, boost::noncopyable>("featherstone_system_model", no_init)
+        .def_readonly("number_of_trees", &wt::number_of_trees)
+        .def_readonly("degrees_of_freedom", &wt::degrees_of_freedom)
+        .def_readonly("q_packed_size", &wt::q_packed_size)
+        .def("bodies_size", &wt::bodies_size)
+        .def("root_indices", &wt::root_indices)
+        .def("pack_q", &wt::pack_q)
+        .def("unpack_q", &wt::unpack_q, (arg_("q_packed")))
+        .def("pack_qd", &wt::pack_qd)
+        .def("unpack_qd", &wt::unpack_qd, (arg_("qd_packed")))
+        .def("number_of_sites_in_each_tree",
+          &wt::number_of_sites_in_each_tree)
+        .def("sum_of_masses_in_each_tree", sum_of_masses_in_each_tree)
+        .def("mean_linear_velocity", &wt::mean_linear_velocity, (
+          arg_("number_of_sites_in_each_tree")))
+        .def("subtract_from_linear_velocities",
+          &wt::subtract_from_linear_velocities, (
+            arg_("number_of_sites_in_each_tree"),
+            arg_("value")))
+        .def("e_kin", &wt::e_kin, ccr())
+        .def("reset_e_kin", &wt::reset_e_kin, (
+           arg_("e_kin_target"),
+           arg_("e_kin_epsilon")=1e-12))
+        .def("assign_zero_velocities", &wt::assign_zero_velocities)
+        .def("assign_random_velocities", &wt::assign_random_velocities, (
+           arg_("e_kin_target")=none,
+           arg_("e_kin_epsilon")=1e-12,
+           arg_("random_gauss")=none))
         .def("inverse_dynamics_packed", &wt::inverse_dynamics_packed, (
           arg_("qdd_packed")=none,
           arg_("f_ext_packed")=none,
@@ -41,57 +79,16 @@ namespace scitbx { namespace rigid_body { namespace ext {
     typedef tardy::model<> wt;
     typedef wt::ft ft;
 
-    static
-    boost::python::list
-    xxx_spatial_inertia(
-      wt const& O)
-    {
-      boost::python::list result;
-      unsigned nb = O.bodies_size();
-      for(unsigned ib=0;ib<nb;ib++) {
-        result.append(O.bodies[ib]->i_spatial);
-      }
-      return result;
-    }
-
-    static
-    boost::python::list
-    xxx_spatial_velocities(
-      wt& O)
-    {
-      boost::python::list result;
-      af::shared<af::tiny<ft, 6> >
-        sv = O.featherstone_system_model().spatial_velocities();
-      SCITBX_ASSERT(sv.size() == O.bodies_size());
-      unsigned nb = O.bodies_size();
-      for(unsigned ib=0;ib<nb;ib++) {
-        result.append(boost_python::array_as_list(
-          sv[ib].begin(), sv[ib].size()));
-      }
-      return result;
-    }
-
-    static
-    boost::python::object
-    sum_of_masses_in_each_tree(
-      wt const& O)
-    {
-      af::shared<std::pair<int, double> >
-        somiet = O.sum_of_masses_in_each_tree();
-      return boost_python::array_as_list(somiet.begin(), somiet.size());
-    }
-
     static void
     wrap()
     {
       using namespace boost::python;
       typedef return_value_policy<copy_const_reference> ccr;
       object none;
-      class_<wt, boost::noncopyable>("tardy_model", no_init)
-        .def("bodies_size", &wt::bodies_size)
-        .def_readonly("number_of_trees", &wt::number_of_trees)
-        .def_readonly("degrees_of_freedom", &wt::degrees_of_freedom)
-        .def_readonly("q_packed_size", &wt::q_packed_size)
+      class_<wt,
+             bases<featherstone::system_model<ft> >,
+             boost::noncopyable
+            >("tardy_model", no_init)
         .def(init<
           object const&,
           af::shared<vec3<ft> > const&,
@@ -107,38 +104,12 @@ namespace scitbx { namespace rigid_body { namespace ext {
             arg_("near_singular_hinges_angular_tolerance_deg")=5)))
         .def("flag_positions_as_changed", &wt::flag_positions_as_changed)
         .def("flag_velocities_as_changed", &wt::flag_velocities_as_changed)
-        .def("root_indices", &wt::root_indices)
-        .def("pack_q", &wt::pack_q)
-        .def("unpack_q", &wt::unpack_q, (arg_("q_packed")))
-        .def("pack_qd", &wt::pack_qd)
-        .def("unpack_qd", &wt::unpack_qd, (arg_("qd_packed")))
-        .def("number_of_sites_in_each_tree",
-          &wt::number_of_sites_in_each_tree)
-        .def("sum_of_masses_in_each_tree", sum_of_masses_in_each_tree)
-        .def("mean_linear_velocity", &wt::mean_linear_velocity, (
-          arg_("number_of_sites_in_each_tree")))
-        .def("subtract_from_linear_velocities",
-          &wt::subtract_from_linear_velocities, (
-            arg_("number_of_sites_in_each_tree"),
-            arg_("value")))
-        .def("featherstone_system_model", &wt::featherstone_system_model,ccr())
         .def("sites_moved", &wt::sites_moved, ccr())
         .def("e_pot", &wt::e_pot, ccr())
         .def("d_e_pot_d_sites", &wt::d_e_pot_d_sites, ccr())
         .def("d_e_pot_d_q_packed", &wt::d_e_pot_d_q_packed)
-        .def("e_kin", &wt::e_kin, ccr())
         .def("e_tot", &wt::e_tot)
-        .def("reset_e_kin", &wt::reset_e_kin, (
-           arg_("e_kin_target"),
-           arg_("e_kin_epsilon")=1e-12))
-        .def("assign_zero_velocities", &wt::assign_zero_velocities)
-        .def("assign_random_velocities", &wt::assign_random_velocities, (
-           arg_("e_kin_target")=none,
-           arg_("e_kin_epsilon")=1e-12,
-           arg_("random_gauss")=none))
         .def("dynamics_step", &wt::dynamics_step, (arg_("delta_t")))
-        .def("xxx_spatial_inertia", xxx_spatial_inertia)
-        .def("xxx_spatial_velocities", xxx_spatial_velocities)
       ;
     }
   };
