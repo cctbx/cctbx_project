e514348ca32cfaa23c2b747acc09ecdd1edd0249
mmarat <mmarat@users.noreply.github.com>
2009-08-04 22:09:51 +0000

    Added change of basis to mask test. Added option to test against list of groups from a file. Tested with about 1000 group symbols

---
+++ mmtbx/masks/util.h
@@ -0,0 +1,13 @@
+#ifndef MMTBX_MASKS_UTIL_H
+#define MMTBX_MASKS_UTIL_H
+
+namespace mmtbx { namespace masks {
+
+  // DO NOT USE
+  void generate_groups(std::set<std::string> &halls,
+      const std::string &group_symbol, int ncb);
+
+} } // namespace mmtbx::masks
+
+
+#endif
