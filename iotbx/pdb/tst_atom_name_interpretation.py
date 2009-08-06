0a8932f896183d2692436afcaea19b08a2a323ee
Jeffrey Headd <jjheadd@users.noreply.github.com>
2009-08-06 18:43:57 +0000

    added missing_atom_names routine to atom_name_interpretation.py and the supporting test code

--- iotbx/pdb/tst_atom_name_interpretation.py
+++ iotbx/pdb/tst_atom_name_interpretation.py
@@ -7,12 +7,25 @@ def exercise():
   atom_names = ["CA", "C", "C", "X", "HB1", "HB2", "HB3"]
   matched = interpreters["LEU"].match_atom_names(atom_names)
   s = StringIO()
+  
   matched.show_problems(out=s, prefix=">")
   assert not show_diff(s.getvalue(), """\
 >unexpected atom names: "X"
 >multiple matches: expected pattern=C  names="C", "C"
 >mutually exclusive: 1hB 3hB
 """)
+
+  ala_names = ["N", "CA", "C", "O", "CB"]
+  ala_missing_names = ["CA", "CB", "O"]
+  ala_with_h = ["N","CA","C","O","CB","HA","HB1","HB2","HB3","H"]
+  ala_matched = interpreters["ALA"].match_atom_names(ala_names)
+  ala_missing = interpreters["ALA"].match_atom_names(ala_missing_names)
+  ala_h = interpreters["ALA"].match_atom_names(ala_with_h)
+  assert ala_matched.missing_atom_names(ignore_hydrogen=True) == set(())
+  assert ala_missing.missing_atom_names(ignore_hydrogen=True) == set(("C","N"))
+  assert ala_h.missing_atom_names(ignore_hydrogen=False) == set(())
+
+  
   print "OK"
 
 if (__name__ == "__main__"):
