e6b6ec21d8eee93e74dcdbc737ed67e7474d9b9d
Pavel Afonine <afonine@users.noreply.github.com>
2009-08-11 19:30:11 +0000

    adopt twin-bulk-solvent-and-scaling

--- mmtbx/f_model/f_model_ext.cpp
+++ mmtbx/f_model/f_model_ext.cpp
@@ -16,14 +16,15 @@ namespace {
   {
     return boost::python::make_tuple(self.f_calc,
                                      self.f_mask,
-                                     self.b_cart,
+                                     self.u_star,
                                      self.k_sol,
                                      self.b_sol,
                                      self.hkl,
                                      self.uc,
                                      self.f_model,
                                      self.f_bulk,
-                                     self.fb_cart,
+                                     self.f_aniso,
+                                     self.f_b_sol,
                                      self.ss);
   }
 
@@ -40,12 +41,12 @@ namespace {
            scitbx::sym_mat3<double>               const&,
            double                                 const&,
            double                                 const&,
-           af::const_ref<cctbx::miller::index<> > const&,
+           af::shared<cctbx::miller::index<> >    const&,
            cctbx::uctbx::unit_cell                const&,
            af::shared<double>                     const& >(
                                                         (arg_("f_calc"),
                                                          arg_("f_mask"),
-                                                         arg_("b_cart"),
+                                                         arg_("u_star"),
                                                          arg_("k_sol"),
                                                          arg_("b_sol"),
                                                          arg_("hkl"),
@@ -53,14 +54,15 @@ namespace {
                                                          arg_("ss"))))
       .add_property("f_calc",        make_getter(&core<>::f_calc,       rbv()))
       .add_property("f_mask",        make_getter(&core<>::f_mask,       rbv()))
-      .add_property("b_cart",        make_getter(&core<>::b_cart,       rbv()))
+      .add_property("u_star",        make_getter(&core<>::u_star,       rbv()))
       .add_property("k_sol",         make_getter(&core<>::k_sol,        rbv()))
       .add_property("b_sol",         make_getter(&core<>::b_sol,        rbv()))
       .add_property("hkl",           make_getter(&core<>::hkl,          rbv()))
       .add_property("uc",            make_getter(&core<>::uc,           rbv()))
       .add_property("f_model",       make_getter(&core<>::f_model,      rbv()))
       .add_property("f_bulk",        make_getter(&core<>::f_bulk,       rbv()))
-      .add_property("fb_cart",       make_getter(&core<>::fb_cart,      rbv()))
+      .add_property("f_aniso",       make_getter(&core<>::f_aniso,      rbv()))
+      .add_property("f_b_sol",       make_getter(&core<>::f_b_sol,      rbv()))
       .add_property("ss",            make_getter(&core<>::ss,           rbv()))
       .enable_pickling()
       .def("__getinitargs__", getinitargs)
