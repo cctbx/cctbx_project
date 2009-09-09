877fc8a0d7db6089d586e93ac8ebaa09517aeab2
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-09-09 15:54:20 +0000

    Pure Python wrapper holding onto the matrix decomposed in place (fix the bug reported by Billy Poon <BKPoon@lbl.gov> on 2009/9/9)

--- scitbx/linalg/tests/tst_matrix.py
+++ scitbx/linalg/tests/tst_matrix.py
@@ -20,9 +20,20 @@ def exercise_random_normal_matrix():
     eig_v = scitbx.linalg.eigensystem.real_symmetric(
       a.matrix_transpose().matrix_multiply(a))
     assert approx_equal(list(eig_u.values()), [9, 4, 1] + [0,]*(m-3))
-    assert approx_equal(list(eig_v.values()), [9, 4, 1] + [0,]*(n-3))
+    assert approx_equal(list(eig_v.values()),  [9, 4, 1] + [0,]*(n-3))
+
+def exercise_householder():
+  a = flex.double([
+    ( 4.0, 1.0,-1.0, 2.0),
+    (1.0, 4.0, 1.0,-1.0),
+    (-1.0, 1.0, 4.0, 1.0),
+    (2.0,-1.0, 1.0, 4.0)])
+  qr = scitbx.linalg.householder_qr_decomposition(a, may_accumulate_q=True)
+  del a
+  q = qr.q()
 
 def run():
+  exercise_householder()
   exercise_random_normal_matrix()
   print 'OK'
 
