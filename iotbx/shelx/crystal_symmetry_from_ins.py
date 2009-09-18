b0e8a6402809e50fdaa407c42f16d8f32f44a8b5
Richard Gildea <richard.gildea@diamond.ac.uk>
2009-09-18 12:14:47 +0000

    iotbx/shelx/crystal_symmetry_from_ins.py: small change to backward compatibility message for consistency

--- iotbx/shelx/crystal_symmetry_from_ins.py
+++ iotbx/shelx/crystal_symmetry_from_ins.py
@@ -1,10 +1,10 @@
 from iotbx import shelx
 
 def extract_from(file_name=None, file=None, max_characters=100000):
-  # XXX leave max_characters keyword for back-compatibility 17-09-09
+  # XXX backward compatibility 2009-09-17 - keep max_characters keyword
   assert [file_name, file].count(None) == 1
   stream = shelx.command_stream(file=file, filename=file_name)
-  l = shelx.crystal_symmetry_parser(stream,
-                                    builder=shelx.crystal_symmetry_builder())
-  l.parse()
-  return l.builder.crystal_symmetry
+  parser = shelx.crystal_symmetry_parser(
+    stream, builder=shelx.crystal_symmetry_builder())
+  parser.parse()
+  return parser.builder.crystal_symmetry
