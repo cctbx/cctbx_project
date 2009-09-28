286145fa62aeea260975cd6cd83412f51e28ea3b
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-09-28 10:53:15 +0000

    Fix bug reported by Ralf

--- boost_adaptbx/tests/tst_stderr_stdout.py
+++ boost_adaptbx/tests/tst_stderr_stdout.py
@@ -1,10 +1,21 @@
+import libtbx.object_oriented_patterns as oop
 import boost.python_file
 import sys
 import gc
 
+class without_tell(oop.proxy):
+  """ sys.stdout and sys.stderr don't have a functional method 'tell',
+  except on MacOS X. This proxy brings the latter back into the ranks """
+
+  def tell(self):
+    raise NotImplementedError("Test of stdout/stderr / C++ stream bridge")
+
+
 def run():
   ext = boost.python.import_ext("python_file_test_ext")
-  ext.call_with_stderr_stdout_do_nothing(sys.stderr, sys.stdout)
+  ext.call_with_stderr_stdout_do_nothing(
+    without_tell(sys.stderr), # bug trigger on MacOS X
+    sys.stdout)
   gc.collect()
   print "OK"
 
