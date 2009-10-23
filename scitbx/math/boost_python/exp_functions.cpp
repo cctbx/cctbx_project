9848b3e8a47fcb3e19946c9f031cf486fdbe1fa9
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2009-10-23 02:22:09 +0000

    new scitbx/math/jacks_expf.h (single-precision exp function, much faster than libc expf), with result and speed comparisons in scitbx/math/boost_python/tst_exp_functions.py

---
+++ scitbx/math/boost_python/exp_functions.cpp
@@ -0,0 +1,97 @@
+#include <scitbx/array_family/boost_python/flex_fwd.h>
+
+#include <boost/python/def.hpp>
+#include <boost/python/args.hpp>
+
+#include <scitbx/math/jacks_expf.h>
+#include <scitbx/array_family/shared.h>
+#include <boost_adaptbx/error_utils.h>
+
+namespace scitbx { namespace math {
+
+namespace {
+
+  af::shared<float>
+  jacks_expf_a(
+    af::const_ref<float> const& array_of_float)
+  {
+    af::shared<float> result(
+      array_of_float.size(),
+      af::init_functor_null<float>());
+    for(std::size_t i=0;i<array_of_float.size();i++) {
+      result[i] = jacks_expf(array_of_float[i]);
+    }
+    return result;
+  }
+
+  union float_unsigned
+  {
+    float f;
+    unsigned u;
+  };
+
+  namespace float_bits {
+    static const unsigned sign = 0x80000000U;
+    static const unsigned expo = 0x7F800000U;
+    static const unsigned mant = 0x007FFFFFU;
+    static const unsigned expo_size = 8U;
+    static const unsigned mant_size = 23U;
+    static const int expo_min = -127;
+    static const int expo_max = 127;
+    static const int expo_bias = 127;
+  }
+
+  af::shared<float>
+  exercise_jacks_expf(
+    bool negative_sign,
+    int exponent,
+    unsigned mantissa_step_size,
+    unsigned j_sample)
+  {
+    ASSERTBX(sizeof(float) == 4);
+    ASSERTBX(sizeof(int) == 4);
+    ASSERTBX(exponent >= float_bits::expo_min);
+    ASSERTBX(exponent <= float_bits::expo_max);
+    float_unsigned value;
+    value.f = -3.9572185e-13f;
+    ASSERTBX(value.u == 2866726279U);
+      // expected to be true for IEEE 754 binary32
+    af::shared<float> result;
+    value.u = 0U;
+    if (negative_sign) {
+      value.u |= float_bits::sign;
+    }
+    unsigned uexp = static_cast<unsigned>(exponent + float_bits::expo_bias);
+    value.u |= uexp << float_bits::mant_size;
+    unsigned j_end = 1U << float_bits::mant_size;
+    float je;
+    for(unsigned j=0;j<j_end;j+=mantissa_step_size) {
+      value.u &= float_bits::sign | float_bits::expo;
+      value.u |= j;
+      je = jacks_expf(value.f);
+      if (j == 0 || j == j_sample) {
+        result.push_back(value.f);
+        result.push_back(je);
+      }
+    }
+    result.push_back(value.f);
+    result.push_back(je);
+    return result;
+  }
+
+} // namespace <anonymous>
+
+namespace boost_python {
+
+  void wrap_exp_functions()
+  {
+    using namespace boost::python;
+    def("jacks_expf", jacks_expf_a, (arg("array_of_float")));
+    def("exercise_jacks_expf", exercise_jacks_expf, (
+      arg("negative_sign"),
+      arg("exponent"),
+      arg("mantissa_step_size"),
+      arg("j_sample")));
+  }
+
+}}} // namespace scitbx::math::boost_python
---
+++ scitbx/math/boost_python/tst_exp_functions.py
@@ -0,0 +1,71 @@
+import scitbx.math
+from scitbx.array_family import flex
+from libtbx.test_utils import Exception_expected, approx_equal
+from libtbx.utils import format_cpu_times
+import time
+import sys
+
+def exercise_with_random_arguments(n_arguments, n_iterations):
+  mt = flex.mersenne_twister(seed=0)
+  d = mt.random_double(size=n_arguments)*100-50
+  f = flex.float(tuple(d))
+  print "showing wall clock times!"
+  t0 = time.time()
+  for i_iteration in xrange(n_iterations):
+    jef = scitbx.math.jacks_expf(array_of_float=f)
+  print "jacks_expf(): %.2f s" % (time.time()-t0)
+  for i_iteration in xrange(n_iterations):
+    sef = flex.exp(f)
+  print "std::exp(float): %.2f s" % (time.time()-t0)
+  t0 = time.time()
+  for i_iteration in xrange(n_iterations):
+    sed = flex.exp(d)
+  print "std::exp(double): %.2f s" % (time.time()-t0)
+  assert sed.all_gt(0)
+  for ef in [jef, sef]:
+    max_rel_err = flex.max(flex.abs((ef.as_double() - sed) / sed))
+    assert approx_equal(max_rel_err, 0, eps=1e-5)
+
+def run(args):
+  assert args in [[], ["--comprehensive"]]
+  if (len(args) == 0):
+    comprehensive = False
+  else:
+    comprehensive = True
+  if (not comprehensive):
+    n_arguments = 100000
+    n_iterations = 10
+    exponents = [-127, -1, 0, 1, 5, 6]
+    mantissa_step_size = 100000
+    j_sample = 300000
+  else:
+    n_arguments = 300000
+    n_iterations = 20
+    exponents = range(-127, 7)
+    mantissa_step_size = 1
+    j_sample = 300000
+  exercise_with_random_arguments(
+    n_arguments=n_arguments,
+    n_iterations=n_iterations)
+  for negative_sign in [False, True]:
+    for exponent in exponents:
+      if (comprehensive): print exponent
+      try:
+        r = scitbx.math.exercise_jacks_expf(
+          negative_sign=negative_sign,
+          exponent=exponent,
+          mantissa_step_size=mantissa_step_size,
+          j_sample=j_sample)
+        if (comprehensive): print list(r)
+      except RuntimeError, e:
+        assert str(e) == "jacks_expf(): function argument out of range."
+        assert not negative_sign
+        break
+    else:
+      if (not negative_sign):
+        raise Exception_expected
+  #
+  print format_cpu_times()
+
+if (__name__ == "__main__"):
+  run(args=sys.argv[1:])
