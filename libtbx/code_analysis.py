5d780f59584a9949148bbc275e38eee1a4d7de43
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-10-26 00:16:47 +0000

    Comment lines statistics (only implemented FORTRAN)

---
+++ libtbx/code_analysis.py
@@ -0,0 +1,52 @@
+""" Tools to statically analyse source code in various languages """
+
+import os
+import re
+import operator
+from glob import glob
+
+
+class comments(object):
+  """ Information about comment lines """
+
+  def __init__(self, files, debug=False):
+    """ Construct the information for the given files """
+    self.debug = debug
+    self.commented_lines = 0
+    self.lines = 0
+    for f in files:
+      for p in glob(f):
+        p = os.path.expanduser(p)
+        root, ext = os.path.splitext(p)
+        ext = ext.lower()
+        if ext in ('.f', '.fpp', '.inc', '.f77', '.f90'):
+          self.process_fortran(p)
+        else:
+          raise NotImplemented(ext)
+
+  fortran_start_of_line = re.compile(r'^ (?: C | \* | !) \W* \w', re.X)
+  fortran_bang_anywhere = re.compile(r'! \W* \w', re.X)
+
+  def process_fortran(self, file_path):
+    n = 0
+    for line in open(file_path):
+      self.lines += 1
+      if self.fortran_start_of_line.search(line):
+        n += 1
+        if self.debug: print self.lines, line
+        continue
+      bang = line.rfind('!')
+      if bang < 0: continue
+      single_quote, double_quote = line.rfind("'"), line.rfind('"')
+      if single_quote >= 0 or double_quote >= 0:
+        if single_quote >= 0 and double_quote >= 0:
+          quote = max(single_quote, double_quote)
+        elif single_quote >= 0:
+          quote = single_quote
+        else:
+          quote = double_quote
+        if quote < bang: continue
+      if self.fortran_bang_anywhere.search(line, pos=bang):
+        n += 1
+        if self.debug: print self.lines, line
+    self.commented_lines += n
---
+++ libtbx/command_line/analyse_code_comments.py
@@ -0,0 +1,20 @@
+from __future__ import division
+from libtbx import code_analysis
+from libtbx.option_parser import option_parser
+
+def run(args, debug=False):
+  comments = code_analysis.comments(args, debug=debug)
+  print comments.commented_lines, comments.lines,\
+        round(comments.commented_lines/comments.lines * 100, 1)
+
+if __name__ == '__main__':
+  import sys
+  command_line = (option_parser(
+    usage="",
+    description="")
+    .option(None, "--debug",
+            dest='debug',
+            action="store_true",
+            default=False)
+  ).process(args=sys.argv[1:])
+  run(command_line.args, **command_line.options.__dict__)
