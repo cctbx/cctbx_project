e03fc161f3e85151d9dc9a7ad680dd56985e35ce
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-10-26 00:21:41 +0000

    Example of seminvariants exploration

---
+++ cctbx/examples/list_non_principal_continuous_shifts.py
@@ -0,0 +1,23 @@
+import libtbx.load_env
+from cctbx import sgtbx
+import os
+
+def run():
+  """ List seminvariants whose continuous shifts aren't principal,
+      drawing from the spacegroup settings specified in phenix_regression
+  """
+  namespace = {}
+  execfile(os.path.join(libtbx.env.find_in_repositories("phenix_regression"),
+                        "settings.py"),
+           namespace)
+  for setting in namespace['settings']:
+    sgi = sgtbx.space_group_info(setting)
+    seminvar = sgtbx.structure_seminvariants(sgi.group())
+    if seminvar.continuous_shifts_are_principal(): continue
+    print str(sgi)
+    for vm in seminvar.vectors_and_moduli():
+      if vm.m != 0: continue
+      print "\t", vm.v
+
+if __name__ == '__main__':
+  run()
