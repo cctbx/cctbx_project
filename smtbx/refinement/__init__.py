fd9d45aaaaed3894f2cf2496caf0d5ec090eaf3a
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-10-26 00:23:24 +0000

    Get rid of the the injector pattern (too difficult for users to find features)

--- smtbx/refinement/__init__.py
+++ smtbx/refinement/__init__.py
@@ -1,14 +1,6 @@
 import boost.python
-boost.python.import_ext("smtbx_refinement_ext")
-from smtbx_refinement_ext import *
+ext = boost.python.import_ext("smtbx_refinement_ext")
+from ext import *
 import smtbx.refinement.tests
 import smtbx.refinement.minimization
 import smtbx.refinement.barriers
-
-from libtbx import object_oriented_patterns as oop
-import cctbx.xray
-
-class smtbx_structure_enhancements(oop.injector, cctbx.xray.structure):
-
-  def parameter_map(self):
-    return smtbx.refinement.parameter_map(self.scatterers())
