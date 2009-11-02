4cd6a845d00f7bc9e5752fcf32c8db8c7ba87069
Luc J. Bourhis <luc_j_bourhis@mac.com>
2009-11-02 14:34:40 +0000

    Skip F vs F^2 on platforms with gcc 4.1.1

--- smtbx/structure_factors/direct/tests/tst_standard_xray.py
+++ smtbx/structure_factors/direct/tests/tst_standard_xray.py
@@ -172,7 +172,16 @@ class custom_vs_std_test_case(test_case):
 
 class f_vs_f_sq_test_case(test_case):
 
+  skip_warning = True
+
   def do_exercise(self, verbose=False):
+    import boost.python
+    vers = boost.python.gcc_version()
+    if vers and str(vers) == "4.1.1":
+      if self.__class__.skip_warning:
+        print "Skip F vs F^2 test because platform with GCC %s" % vers
+        self.__class__.skip_warning = False
+      return
     xs = self.xs
     indices = self.miller_indices(xs.space_group_info())
     f_linearisation = (
