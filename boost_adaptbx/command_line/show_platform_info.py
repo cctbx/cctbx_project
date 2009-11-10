6247e2cf453d206119a36171a9679853a4e91db4
Nathaniel Echols <nathaniel.echols@gmail.com>
2009-11-10 20:26:37 +0000

    allow redirection of show_platform_info output (for error reporting in phenix gui)

--- boost_adaptbx/command_line/show_platform_info.py
+++ boost_adaptbx/command_line/show_platform_info.py
@@ -5,29 +5,29 @@ from libtbx import introspection
 import platform
 import sys, os
 
-def run():
-  sys.stdout.write(boost.python.platform_info)
-  print "os.name:", os.name
-  print "sys.platform:", sys.platform
-  print "sys.byteorder:", sys.byteorder
-  print "platform.platform():", platform.platform()
-  print "platform.architecture():", platform.architecture()
+def run (out=sys.stdout):
+  out.write(boost.python.platform_info)
+  print >> out, "os.name:", os.name
+  print >> out, "sys.platform:", sys.platform
+  print >> out, "sys.byteorder:", sys.byteorder
+  print >> out, "platform.platform():", platform.platform()
+  print >> out, "platform.architecture():", platform.architecture()
   for attr in ["division_by_zero", "invalid", "overflow"]:
     attr = "floating_point_exceptions.%s_trapped" % attr
-    print "%s:" % attr, eval("boost.python.%s" % attr)
-  print "number of processors:", introspection.number_of_processors(
+    print >> out, "%s:" % attr, eval("boost.python.%s" % attr)
+  print >> out, "number of processors:", introspection.number_of_processors(
     return_value_if_unknown="unknown")
   introspection.machine_memory_info().show()
   try: import thread
-  except ImportError: print "import thread: NO"
-  else: print "import thread: OK"
+  except ImportError: print >> out, "import thread: NO"
+  else: print >> out, "import thread: OK"
   c = getattr(boost.python.ext, "str_or_unicode_as_char_list", None)
   if (c is not None):
-    print '"hello" =', c("hello")
-    print 'u"hello" =', c(u"hello")
+    print >> out, '"hello" =', c("hello")
+    print >> out, 'u"hello" =', c(u"hello")
     e = u"\u00C5".encode("utf-8", "strict")
-    print 'u"\u00C5" =', c(u"\u00C5"), 'as utf-8 =', c(e)
-    print "LATIN CAPITAL LETTER A WITH RING ABOVE =", e
+    print >> out, 'u"\u00C5" =', c(u"\u00C5"), 'as utf-8 =', c(e)
+    print >> out, "LATIN CAPITAL LETTER A WITH RING ABOVE =", e
 
 if (__name__ == "__main__"):
   run()
