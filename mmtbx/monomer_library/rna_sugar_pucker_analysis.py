d2dfb99d2b8af1d6b66b45ac3be25993e3012cd6
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2009-11-16 19:14:40 +0000

    mmtbx/monomer_library/rna_sugar_pucker_analysis.py: use O3' distance to C1'-outbound line as a fallback if P from the following residue is not available

--- mmtbx/monomer_library/rna_sugar_pucker_analysis.py
+++ mmtbx/monomer_library/rna_sugar_pucker_analysis.py
@@ -19,85 +19,124 @@ master_phil = libtbx.phil.parse("""\
     .type = float
   p_distance_c1p_outbound_line_2p_max = 2.9
     .type = float
+  o3p_distance_c1p_outbound_line_2p_max = 1.6
+    .type = float
   bond_detection_distance_tolerance = 0.5
     .type = float
 """)
 
 class evaluate(object):
 
-  def __init__(self,
+  __slots__ = [
+    "epsilon",
+    "delta",
+    "p_distance_c1p_outbound_line",
+    "o3p_distance_c1p_outbound_line",
+    "is_2p_epsilon",
+    "is_2p_delta",
+    "is_2p_p_distance_c1p_outbound_line",
+    "is_2p_o3p_distance_c1p_outbound_line",
+    "is_2p"]
+
+  def assign(O, **keyword_args):
+    for slot in evaluate.__slots__:
+      setattr(O, slot, keyword_args.get(slot, None))
+
+  def __init__(O,
         params,
         residue_1_deoxy_ribo_atom_dict,
         residue_1_c1p_outbound_atom,
         residue_2_p_atom):
-    if (   residue_1_c1p_outbound_atom is None
-        or residue_2_p_atom is None):
-      distances = None
+    def v(key): return matrix.col(residue_1_deoxy_ribo_atom_dict[key].xyz)
+    c1p = v("C1'")
+    o3p = v("O3'")
+    c3p = v("C3'")
+    c4p = v("C4'")
+    c5p = v("C5'")
+    bonded_sites = [
+      (o3p, c3p),
+      (c3p, c4p),
+      (c5p, c4p)]
+    distances = [abs(s2-s1) for s1,s2 in bonded_sites]
+    if (residue_2_p_atom is None):
+      p = None
     else:
       p = matrix.col(residue_2_p_atom.xyz)
-      def v(key): return matrix.col(residue_1_deoxy_ribo_atom_dict[key].xyz)
-      c1p = v("C1'")
-      o3p = v("O3'")
-      c3p = v("C3'")
-      c4p = v("C4'")
-      c5p = v("C5'")
+      distances.append(abs(p-o3p))
+    if (residue_1_c1p_outbound_atom is None):
+      c1p_outbound = None
+    else:
       c1p_outbound = matrix.col(residue_1_c1p_outbound_atom.xyz)
-      bonded_sites = [
-        (p, o3p),
-        (o3p, c3p),
-        (c3p, c4p),
-        (c1p, c1p_outbound),
-        (c5p, c4p)]
-      distances = [abs(s2-s1) for s1,s2 in bonded_sites]
-    if (   distances is None
-        or min(distances) < params.bond_min_distance
+      distances.append(abs(c1p-c1p_outbound))
+    if (   min(distances) < params.bond_min_distance
         or max(distances) > params.bond_max_distance):
+      O.assign()
+      return
+    #
+    def dihe(sites): return normalize_angle(geometry_restraints.dihedral(
+      sites=sites, angle_ideal=0, weight=1).angle_model, deg=True)
+    if (p is None):
       epsilon = None
-      delta = None
+    else:
+      epsilon = dihe([p, o3p, c3p, c4p])
+    delta = dihe([c5p, c4p, c3p, o3p])
+    if (c1p_outbound is None):
       p_distance_c1p_outbound_line = None
+      o3p_distance_c1p_outbound_line = None
+    else:
+      if (p is None):
+        p_distance_c1p_outbound_line = None
+      else:
+        p_distance_c1p_outbound_line = scitbx.math.line_given_points(
+          points=(c1p, c1p_outbound)).distance_sq(point=p)**0.5
+      o3p_distance_c1p_outbound_line = scitbx.math.line_given_points(
+        points=(c1p, c1p_outbound)).distance_sq(point=o3p)**0.5
+    n_decisions = 0
+    if (   epsilon is None
+        or params.epsilon_range_not_2p_min is None
+        or params.epsilon_range_not_2p_max is None):
       is_2p_epsilon = None
+    else:
+      is_2p_epsilon = (   epsilon < params.epsilon_range_not_2p_min
+                       or epsilon > params.epsilon_range_not_2p_max)
+      n_decisions += 1
+    if (   params.delta_range_2p_min is None
+        or params.delta_range_2p_max is None):
       is_2p_delta = None
+    else:
+      is_2p_delta = (    delta >= params.delta_range_2p_min
+                     and delta <= params.delta_range_2p_max)
+      n_decisions += 1
+    if (   p_distance_c1p_outbound_line is None
+        or params.p_distance_c1p_outbound_line_2p_max is None):
       is_2p_p_distance_c1p_outbound_line = None
+    else:
+      is_2p_p_distance_c1p_outbound_line = (
+          p_distance_c1p_outbound_line
+        < params.p_distance_c1p_outbound_line_2p_max)
+      n_decisions += 1
+    if (   o3p_distance_c1p_outbound_line is None
+        or params.o3p_distance_c1p_outbound_line_2p_max is None):
+      is_2p_o3p_distance_c1p_outbound_line = None
+    else:
+      is_2p_o3p_distance_c1p_outbound_line = (
+          o3p_distance_c1p_outbound_line
+        < params.o3p_distance_c1p_outbound_line_2p_max)
+      n_decisions += 1
+    if (n_decisions == 0):
       is_2p = None
     else:
-      def a(sites): return normalize_angle(geometry_restraints.dihedral(
-        sites=sites, angle_ideal=0, weight=1).angle_model, deg=True)
-      epsilon = a([p, o3p, c3p, c4p])
-      delta = a([c5p, c4p, c3p, o3p])
-      p_distance_c1p_outbound_line = scitbx.math.line_given_points(
-        points=(c1p, c1p_outbound)).distance_sq(point=p)**0.5
-      n_decisions = 0
-      if (   params.epsilon_range_not_2p_min is None
-          or params.epsilon_range_not_2p_max is None):
-        is_2p_epsilon = None
-      else:
-        is_2p_epsilon = (   epsilon < params.epsilon_range_not_2p_min
-                         or epsilon > params.epsilon_range_not_2p_max)
-        n_decisions += 1
-      if (   params.delta_range_2p_min is None
-          or params.delta_range_2p_max is None):
-        is_2p_delta = None
-      else:
-        is_2p_delta = (    delta >= params.delta_range_2p_min
-                       and delta <= params.delta_range_2p_max)
-        n_decisions += 1
-      if (params.p_distance_c1p_outbound_line_2p_max is None):
-        is_2p_p_distance_c1p_outbound_line = None
-      else:
-        is_2p_p_distance_c1p_outbound_line = (
-            p_distance_c1p_outbound_line
-          < params.p_distance_c1p_outbound_line_2p_max)
-        n_decisions += 1
-      if (n_decisions == 0):
-        is_2p = None
-      else:
-        is_2p = (   is_2p_epsilon
-                 or is_2p_delta
-                 or is_2p_p_distance_c1p_outbound_line)
-    self.epsilon = epsilon
-    self.delta = delta
-    self.p_distance_c1p_outbound_line = p_distance_c1p_outbound_line
-    self.is_2p_epsilon = is_2p_epsilon
-    self.is_2p_delta = is_2p_delta
-    self.is_2p_p_distance_c1p_outbound_line =is_2p_p_distance_c1p_outbound_line
-    self.is_2p = is_2p
+      perp_distance = is_2p_p_distance_c1p_outbound_line
+      if (perp_distance is None):
+        perp_distance = is_2p_o3p_distance_c1p_outbound_line
+      is_2p = (is_2p_epsilon or is_2p_delta or perp_distance)
+    O.assign(
+      epsilon=epsilon,
+      delta=delta,
+      p_distance_c1p_outbound_line=p_distance_c1p_outbound_line,
+      o3p_distance_c1p_outbound_line=o3p_distance_c1p_outbound_line,
+      is_2p_epsilon=is_2p_epsilon,
+      is_2p_delta=is_2p_delta,
+      is_2p_p_distance_c1p_outbound_line=is_2p_p_distance_c1p_outbound_line,
+      is_2p_o3p_distance_c1p_outbound_line=is_2p_o3p_distance_c1p_outbound_line,
+      is_2p=is_2p)
