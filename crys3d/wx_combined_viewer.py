edfb68b576eb8f4ef1287e59b959a9ce9d6a706a
Nathaniel Echols <nathaniel.echols@gmail.com>
2009-11-22 01:20:35 +0000

    use mouse wheel to set map levels

--- crys3d/wx_combined_viewer.py
+++ crys3d/wx_combined_viewer.py
@@ -19,7 +19,7 @@ viewer_phil = iotbx.phil.parse("""
 """, process_includes=True)
 
 class map_data (object) :
-  def __init__ (self, map) :
+  def __init__ (self, map, is_difference_map=False) :
     adopt_init_args(self, locals())
     self.unit_cell = map.unit_cell()
     o = self.unit_cell.orthogonalization_matrix()
@@ -39,6 +39,16 @@ class map_data (object) :
     self.iso_levels = levels
     if len(self.colors) != len(levels) :
       self.colors = [ (1.0,1.0,1.0) for x in levels ]
+
+  def increment_iso_levels (self, inc) :
+    for i, cutoff in enumerate(self.iso_levels) :
+      if self.is_difference_map :
+        if cutoff < 0 :
+          self.iso_levels[i] -= inc
+        elif cutoff > 0 :
+          self.iso_levels[i] += inc
+      else :
+        self.iso_levels[i] += inc
 
   def set_colors (self, colors) :
     assert len(self.iso_levels) == len(colors)
@@ -93,7 +103,7 @@ class map_scene (object) :
       va.draw_triangles(triangulation.triangles)
     glPopMatrix()
 
-
+#-----------------------------------------------------------------------
 class map_viewer_mixin (wxGLWindow) :
   initialize_map_viewer_super = True
   def __init__ (self, *args, **kwds) :
@@ -107,6 +117,7 @@ class map_viewer_mixin (wxGLWindow) :
     self.map_ctrls = {}
     # user settings
     self.mesh_line_width = 0.25 # very buggy on OS X + NVidia (and ???)
+    self.selected_map_id = None
     self.update_maps = False
     self.flag_show_maps = True
     self.flag_smooth_lines = True
@@ -176,10 +187,10 @@ class map_viewer_mixin (wxGLWindow) :
   def process_key_stroke (self, key) :
     pass
 
-  def add_map (self, map_id, map) :
+  def add_map (self, map_id, map, is_difference_map=False) :
     if map_id in self.map_ids :
       self.delete_map(map_id)
-    map_object = map_data(map)
+    map_object = map_data(map, is_difference_map)
     self.map_ids.append(map_id)
     self.map_objects.append(map_object)
     self.show_object[map_id] = True
@@ -242,6 +253,25 @@ class map_viewer_mixin (wxGLWindow) :
     map.set_colors(colors)
     self.update_maps = True
 
+  def set_selected_map (self, map_id) :
+    if map_id is None :
+      self.selected_map_id = None
+    else :
+      self.selected_map_id = map_id
+    for other_id, ctrl_frame in self.map_ctrls.iteritems() :
+      if ctrl_frame is not None :
+        ctrl_frame.refresh_attach_mouse()
+
+  def get_selected_map (self) :
+    return self.get_map(self.selected_map_id)
+
+  def increment_map_iso_levels (self, inc) :
+    if self.get_selected_map() is not None :
+      self.get_selected_map().increment_iso_levels(inc)
+      if self.map_ctrls.get(self.selected_map_id) is not None :
+        self.map_ctrls[self.selected_map_id].refresh_iso_levels()
+      self.update_maps = True
+
   def update_map_scenes (self) :
     for object_id, map in self.iter_maps() :
       scene = map.get_scene_data(self.rotation_center)
@@ -261,6 +291,7 @@ class map_viewer_mixin (wxGLWindow) :
       if self.show_object[map_id] :
         scene.draw_mesh()
 
+#-----------------------------------------------------------------------
 class model_and_map_viewer (selection_editor_mixin, map_viewer_mixin) :
   initialize_map_viewer_super = False
   def __init__ (self, *args, **kwds) :
@@ -270,7 +301,9 @@ class model_and_map_viewer (selection_editor_mixin, map_viewer_mixin) :
 
   def OnMouseWheel (self, event) :
     scale = event.GetWheelRotation()
-    if event.ShiftDown() :
+    if self.selected_map_id is not None and not event.AltDown() :
+      self.increment_map_iso_levels(scale * 0.1)
+    elif event.ShiftDown() :
       self.fog_end_offset -= scale
     else :
       self.slab_scale += 0.01 * scale
@@ -329,6 +362,7 @@ class model_and_map_viewer (selection_editor_mixin, map_viewer_mixin) :
       if current_object_id == object_id :
         self.show_object[current_object_id] = show_object
 
+########################################################################
 class MapEditor (wx.MiniFrame) :
   def __init__ (self, parent, map_object, map_id) :
     import wx.lib.colourselect
@@ -359,8 +393,13 @@ class MapEditor (wx.MiniFrame) :
       level_sizer.Add(color_ctrl, 0, wx.ALL, 5)
       main_sizer.Add(level_sizer, 1, wx.ALL, 5)
       self.ctrls.append((slider, level_txt, color_ctrl))
+    self.attach_mouse_wheel = wx.CheckBox(panel,
+      label="Attach mouse wheel to this map")
+    self.attach_mouse_wheel.SetValue(map_id == parent.selected_map_id)
+    main_sizer.Add(self.attach_mouse_wheel, 0, wx.ALL, 5)
     main_sizer.Fit(panel)
     self.Fit()
+    self.Bind(wx.EVT_CHECKBOX, self.OnAttachMouse, self.attach_mouse_wheel)
     self.Bind(wx.EVT_SLIDER, self.OnUpdate)
     self.Bind(wx.lib.colourselect.EVT_COLOURSELECT, self.OnUpdate)
     self.Bind(wx.EVT_CLOSE, self.OnClose)
@@ -385,6 +424,22 @@ class MapEditor (wx.MiniFrame) :
     self.Destroy()
 
   def OnDestroy (self, event) :
-    self.parent.map_ctrls[self.map_id] = None
+    self.parent.map_ctrls.pop(self.map_id)
+
+  def refresh_iso_levels (self) :
+    levels = self.map_object.iso_levels
+    assert len(levels) == len(self.ctrls)
+    for i, (s, t, c) in enumerate(self.ctrls) :
+      s.SetValue(int(10 * levels[i]))
+      t.SetValue("%.2f" % levels[i])
+
+  def refresh_attach_mouse (self) :
+    self.attach_mouse_wheel.SetValue(self.map_id==self.parent.selected_map_id)
+
+  def OnAttachMouse (self, event) :
+    if self.attach_mouse_wheel.GetValue() == True :
+      self.parent.set_selected_map(self.map_id)
+    else :
+      self.parent.set_selected_map(None)
 
 #---end
