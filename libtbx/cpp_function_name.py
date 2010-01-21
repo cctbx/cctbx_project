7dacf8b53ecda5387e8a1bb445dc269f4702afb9
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-01-21 10:34:49 +0000

    Demangling of C++ symbols where c++filt is available

---
+++ libtbx/cpp_function_name.py
@@ -0,0 +1,23 @@
+from libtbx import easy_run
+
+def demangle(symbol):
+  """ Return the demangled C++ function name corresponding to symbol
+      on platforms featuring c++filt. On those which do not, return symbol
+      untouched
+  """
+  try:
+    result = easy_run.fully_buffered("c++filt %s" % symbol)\
+                     .raise_if_errors()\
+                     .stdout_lines[0]
+  except AssertionError:
+    result = symbol
+  return result
+
+if __name__ == '__main__':
+  def exercise_demangle():
+    assert (demangle("__ZN5iotbx2xd17master_file_input8on_error"
+                     "EPKcRKNS_10flex_bison8locationEPv")
+            == "iotbx::xd::master_file_input::on_error"
+               "(char const*, iotbx::flex_bison::location const&, void*)")
+
+  exercise_demangle()
