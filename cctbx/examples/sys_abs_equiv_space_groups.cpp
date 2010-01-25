bcb689caf1984f519625f21d07bcc26e968110a8
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2010-01-25 23:11:39 +0000

    new cctbx/examples/find_sys_abs_equiv_space_groups.py; output archived as cctbx/sgtbx/sys_abs_equiv.py,h for quick access

---
+++ cctbx/examples/sys_abs_equiv_space_groups.cpp
@@ -0,0 +1,19 @@
+#include <cctbx/sgtbx/sys_abs_equiv.h>
+#include <iostream>
+
+int
+main()
+{
+  using std::cout;
+  using std::endl;
+  unsigned const** tab = cctbx::sgtbx::sys_abs_equiv::space_group_numbers;
+  cout << tab[0] << endl;
+  cout << tab[1][1] << endl;
+  cout << tab[2][1] << endl;
+  for(unsigned i=1;i<=tab[75][0];i++) {
+    cout << " " << tab[75][i];
+  }
+  cout << endl;
+  cout << tab[230] << endl;
+  return 0;
+}
--- cctbx/run_examples.py
+++ cctbx/run_examples.py
@@ -10,6 +10,7 @@ def run():
   "$D/examples/space_subgroups.py",
   "$D/examples/trigonal_r_vs_tetragonal_i.py",
   "$D/examples/quartz_structure.py",
+  "$D/examples/find_sys_abs_equiv_space_groups.py",
   "$D/examples/find_distances_using_cpp_objects.py",
   "$D/examples/analyze_adp.py",
   "$D/examples/g_exp_i_partial_derivatives.py",
