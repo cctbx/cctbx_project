7e49d92ba2b63c7ae366bea3f25e03d70f26c997
Richard Gildea <richard.gildea@diamond.ac.uk>
2010-01-27 16:02:09 +0000

    smtbx/masks: fixes/improvements for structure factor calculation: only add f_000_s component to nonzero points of masked_diff_map; a method for faster convergence; add check for convergence

--- smtbx/masks/__init__.py
+++ smtbx/masks/__init__.py
@@ -3,6 +3,7 @@ from cctbx import maptbx
 from cctbx import miller
 from cctbx import xray
 from cctbx.array_family import flex
+from scitbx.math import approx_equal_relatively
 
 class mask(object):
   def __init__(self, xray_structure, observations):
@@ -43,8 +44,8 @@ class mask(object):
       gridding_n_real=self.crystal_gridding.n_real(),
       solvent_radius=solvent_radius,
       shrink_truncation_radius=shrink_truncation_radius)
-    self.solvent_accessible_volume = float(
-      self.mask.data.count(1))/self.mask.data.size() * self.xray_structure.unit_cell().volume()
+    self.solvent_accessible_volume = float(self.mask.data.count(1)) \
+        / self.mask.data.size() * self.xray_structure.unit_cell().volume()
     print "Solvent accessible volume = %.1f [%.1f%%]" %(
       self.solvent_accessible_volume, 100.*
       self.solvent_accessible_volume/xs.unit_cell().volume())
@@ -64,25 +65,59 @@ class mask(object):
       1./self.scale_factor, self.f_calc)
     self.fft_scale = self.xray_structure.unit_cell().volume()\
               / self.crystal_gridding.n_grid_points()
+    epsilon_for_min_residual = 2
     for i in range(max_cycles):
-      diff_map = miller.fft_map(self.crystal_gridding, f_obs_minus_f_calc, self.f_000_s)
+      diff_map = miller.fft_map(self.crystal_gridding, f_obs_minus_f_calc)
       diff_map.apply_volume_scaling()
       stats = diff_map.statistics()
       masked_diff_map = diff_map.real_map_unpadded().set_selected(
         self.mask.data.as_double() == 0, 0)
       self.f_000_cell = flex.sum(diff_map.real_map_unpadded()) * self.fft_scale
       self.f_000 = flex.sum(masked_diff_map) * self.fft_scale
-      self.f_000_s = self.f_000 * (
-        float(masked_diff_map.size())/(masked_diff_map.size() - self.mask.data.count(1)))
-      #print "F000 cell: %.1f" %self.f_000_cell
-      #print "F000 void: %.1f" %self.f_000_s
-      masked_diff_map += self.f_000_s/self.xray_structure.unit_cell().volume()
+      f_minus_000 = flex.sum(diff_map.real_map_unpadded().set_selected(
+        self.mask.data.as_double() == 1, 0)) * self.fft_scale
+      f_000_s = self.f_000 * (
+        float(masked_diff_map.size()) /
+        (masked_diff_map.size() - self.mask.data.count(1)))
+      print "F000 void: %.1f" %f_000_s
+      if (self.f_000_s is not None and
+          approx_equal_relatively(self.f_000_s, f_000_s, 0.0001)):
+        break # we have reached convergence
+      else:
+        self.f_000_s = f_000_s
+      masked_diff_map.add_selected(
+        self.mask.data.as_double() == 1,
+        self.f_000_s/self.xray_structure.unit_cell().volume())
+      if 0:
+        from crys3d import wx_map_viewer
+        wx_map_viewer.display(
+          title="masked diff_map",
+          raw_map=masked_diff_map.as_double(),
+          unit_cell=f_obs.unit_cell())
       self._f_mask = f_obs.structure_factors_from_map(map=masked_diff_map)
       self._f_mask *= self.fft_scale
-      f_model = self.f_model()
+      epsilons = (i/10 for i in range(int(epsilon_for_min_residual*10), 9, -2))
+      scales = []
+      residuals = []
+      min_residual = 1000
+      for epsilon in epsilons:
+        f_model_ = self.f_model(epsilon=epsilon)
+        scale = f_obs.quick_scale_factor_approximation(
+          f_model_, cutoff_factor=0.1)
+        residual = flex.sum(flex.abs(
+          1./scale * flex.abs(f_obs.data())- flex.abs(f_model_.data()))) \
+                 / flex.sum(1./scale * flex.abs(f_obs.data()))
+        scales.append(scale)
+        residuals.append(residual)
+        min_residual = min(min_residual, residual)
+        if min_residual == residual:
+          scale_for_min_residual = scale
+          epsilon_for_min_residual = epsilon
+      print "epsilon: %.1f" %epsilon_for_min_residual
+      self.scale_factor = scale_for_min_residual
+      f_model = self.f_model(epsilon=epsilon_for_min_residual)
       f_obs_minus_f_calc = f_obs.phase_transfer(f_model).f_obs_minus_f_calc(
         1./self.scale_factor, self.f_calc)
-    print "F000 cell: %.1f" %self.f_000_cell
     print "F000 void: %.1f" %self.f_000_s
     self.masked_diff_map = masked_diff_map
     return self._f_mask
@@ -91,23 +126,28 @@ class mask(object):
     assert self._f_mask is not None
     return self._f_mask
 
-  def f_model(self):
+  def f_model(self, epsilon=None):
     assert self._f_mask is not None
-    data = self.f_calc.data()+self.f_mask().data()
-    return miller.array(miller_set=self.f_calc, data=data)
+    f_calc = self.f_calc
+    f_mask = self.f_mask()
+    if epsilon is None:
+      data = f_calc.data() + f_mask.data()
+    else:
+      data = f_calc.data() + epsilon * f_mask.data()
+    return miller.array(miller_set=f_calc, data=data)
 
   def modified_structure_factors(self):
     """Subtracts the solvent contribution from the observed structure
     factors to obtain modified structure factors, suitable for refinement
     with other refinement programs such as ShelXL"""
-    f_obs = self.observations.as_amplitude_array()
-    f_model = self.f_model()
-    f_obs = f_obs.phase_transfer(phase_source=f_model)
     assert self._f_mask is not None
     f_mask = self.f_mask()
+    f_model = self.f_model()
+    f_obs = self.observations.as_amplitude_array()
+    scale_factor = self.scale_factor
+    f_obs = f_obs.phase_transfer(phase_source=f_model)
     modified_f_obs = miller.array(
       miller_set=f_obs,
-      data=(f_obs.data() - f_mask.data()*self.scale_factor),
+      data=(f_obs.data() - f_mask.data()*scale_factor),
       sigmas=f_obs.sigmas())
-    modified_fo_sq = modified_f_obs.as_intensity_array()
-    return modified_fo_sq
+    return modified_f_obs.as_intensity_array()
