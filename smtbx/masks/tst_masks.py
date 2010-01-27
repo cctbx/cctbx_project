a823c0342220d3d7b36ad8dafba66689f0c6578e
Richard Gildea <richard.gildea@diamond.ac.uk>
2010-01-27 16:02:09 +0000

    smtbx/masks: fixes/improvements for structure factor calculation: only add f_000_s component to nonzero points of masked_diff_map; a method for faster convergence; add check for convergence

--- smtbx/masks/tst_masks.py
+++ smtbx/masks/tst_masks.py
@@ -1,11 +1,8 @@
+from cctbx import crystal, miller, uctbx, xray
+from cctbx.array_family import flex
+from iotbx import reflection_file_utils, reflection_file_reader
 from iotbx import shelx
 from smtbx import masks
-from cctbx import miller
-from cctbx import xray
-from cctbx.array_family import flex
-from iotbx import reflection_file_utils
-from iotbx import reflection_file_reader
-
 from libtbx.test_utils import approx_equal
 
 from cStringIO import StringIO
@@ -19,7 +16,8 @@ def exercise_masks(xs, fo_sq,
   print "solvent_radius: %f" %solvent_radius
   print "shrink_truncation_radius: %f" %shrink_truncation_radius
   mask.compute(solvent_radius=solvent_radius,
-               shrink_truncation_radius=shrink_truncation_radius)
+               shrink_truncation_radius=shrink_truncation_radius,
+               resolution_factor=1./4)
   f_mask = mask.structure_factors(max_cycles=10)
   f_model = mask.f_model()
   # write modified structure factors as shelxl hkl
@@ -39,19 +37,26 @@ def exercise_masks(xs, fo_sq,
     f_calc = sf(xs, f_obs).f_calc()
     f_model = mask.f_model()
     scale_factor = f_obs.quick_scale_factor_approximation(
-      f_model, cutoff_factor=0.8)
+      f_model, cutoff_factor=0.1)
     # f_obs - f_calc
     k = f_obs.quick_scale_factor_approximation(f_calc, cutoff_factor=0.8)
     f_obs_minus_f_calc = f_obs.f_obs_minus_f_calc(1./k, f_calc)
     diff_map_calc = miller.fft_map(mask.crystal_gridding, f_obs_minus_f_calc)
     diff_map_calc.apply_volume_scaling()
+    # f_mask
+    mask_map = miller.fft_map(mask.crystal_gridding, f_mask)
+    mask_map.apply_volume_scaling()
+    # f_model
+    model_map = miller.fft_map(mask.crystal_gridding, f_model)
+    model_map.apply_volume_scaling()
     # f_obs - f_model
     f_obs_minus_f_model = f_obs.f_obs_minus_f_calc(1./scale_factor, f_model)
     diff_map_model = miller.fft_map(mask.crystal_gridding, f_obs_minus_f_model)
     diff_map_model.apply_volume_scaling()
-    # f_model
-    model_map = miller.fft_map(mask.crystal_gridding, f_model)
-    model_map.apply_volume_scaling()
+    # f_obs - f_model
+    modified_fo_sq_map = miller.fft_map(
+      mask.crystal_gridding, modified_fo_sq.as_amplitude_array().phase_transfer(f_calc))
+    modified_fo_sq_map.apply_volume_scaling()
     # view the maps
     from crys3d import wx_map_viewer
     wx_map_viewer.display(
@@ -63,18 +68,26 @@ def exercise_masks(xs, fo_sq,
       raw_map=diff_map_calc.real_map(),
       unit_cell=f_obs.unit_cell())
     wx_map_viewer.display(
-      title="f_obs - f_model",
-      raw_map=diff_map_model.real_map(),
+      title="f_mask",
+      raw_map=mask_map.real_map(),
       unit_cell=f_obs.unit_cell())
     wx_map_viewer.display(
       title="f_model",
       raw_map=model_map.real_map(),
       unit_cell=f_obs.unit_cell())
+    wx_map_viewer.display(
+      title="f_obs - f_model",
+      raw_map=diff_map_model.real_map(),
+      unit_cell=f_obs.unit_cell())
+    wx_map_viewer.display(
+      title="modified_fo_sq",
+      raw_map=modified_fo_sq_map.real_map(),
+      unit_cell=f_obs.unit_cell())
   return
 
 def run(ins_name, hkl_name,
-        solvent_radius=1.4,
-        shrink_truncation_radius=1.4,
+        solvent_radius=1.3,
+        shrink_truncation_radius=1.3,
         debug=False):
   xs = xray.structure.from_shelx(filename=ins_name)
   reflections_server = reflection_file_utils.reflection_file_server(
@@ -92,4 +105,5 @@ def run(ins_name, hkl_name,
 
 if __name__ == '__main__':
   import sys
-  run(*(sys.argv[1:]))
+  args = sys.argv[1:]
+  run(*args)
