ff1d367f3a1a5ae248d83b135aeef67113e945cb
Richard Gildea <richard.gildea@diamond.ac.uk>
2010-02-01 15:25:00 +0000

    cctbx/masks: vdw_radii_from_xray_structure(): allow optional dictionary of radii to be passed

--- cctbx/masks/__init__.py
+++ cctbx/masks/__init__.py
@@ -5,17 +5,23 @@ from cctbx_masks_ext import *
 from cctbx.array_family import flex
 from cctbx.eltbx import van_der_waals_radii
 
-def vdw_radii_from_xray_structure(xray_structure):
+def vdw_radii_from_xray_structure(xray_structure, table=None):
   # XXX use scattering dictionary and set_selected
   # XXX use monomer library definitions for radii
   unknown = []
   atom_radii = flex.double()
   for i_seq, scatterer in enumerate(xray_structure.scatterers()):
     try:
-      atom_radii.append(
-        van_der_waals_radii.vdw.table[scatterer.element_symbol()])
-    except:
+      if table is not None:
+        radius = table.get(scatterer.element_symbol())
+        if radius is None:
+          radius = van_der_waals_radii.vdw.table[scatterer.element_symbol()]
+      else:
+        radius = van_der_waals_radii.vdw.table[scatterer.element_symbol()]
+    except KeyError:
       unknown.append(scatterer.element_symbol())
+    else:
+      atom_radii.append(radius)
   if(len(unknown) > 0):
     raise RuntimeError("Atoms with unknown van der Waals radius: ",unknown)
   return atom_radii
