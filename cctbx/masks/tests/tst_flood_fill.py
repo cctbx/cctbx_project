aac873291c6f3105f5c5f5d815f3448e42badc0e
Richard Gildea <richard.gildea@diamond.ac.uk>
2010-02-01 15:16:05 +0000

    test for masks flood_fill

---
+++ cctbx/masks/tests/tst_flood_fill.py
@@ -0,0 +1,26 @@
+from cctbx.array_family import flex
+from cctbx import maptbx, masks, uctbx
+
+def exercise_flood_fill():
+  uc = uctbx.unit_cell('10 10 10 90 90 90')
+  gridding = maptbx.crystal_gridding(
+    unit_cell=uc,
+    pre_determined_n_real=(5,5,5))
+  corner_cube = (0,4,20,24,100,104,120,124) # cube across all 8 corners
+  centre_blob = (37,38,39,42,43,62,63,67,68)
+  data = flex.int(flex.grid(gridding.n_real()))
+  for i in (corner_cube + centre_blob): data[i] = 1
+  masks.flood_fill(data)
+  assert data.count(0) == 108
+  for i in corner_cube: assert data[i] == 2
+  for i in centre_blob: assert data[i] == 3
+  if 0:
+    from crys3d import wx_map_viewer
+    wx_map_viewer.display(raw_map=data.as_double(), unit_cell=uc, wires=False)
+
+def run():
+  exercise_flood_fill()
+  print "OK"
+
+if __name__ == '__main__':
+  run()
