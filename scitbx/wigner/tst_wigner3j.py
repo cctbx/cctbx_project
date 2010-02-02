40079962f2f27a15c9845ace2a3a626c02bf5d8a
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2010-02-02 19:30:13 +0000

    scitbx/wigner: misc. cleanup (functionality unchanged)

--- scitbx/wigner/tst_wigner3j.py
+++ scitbx/wigner/tst_wigner3j.py
@@ -1,4 +1,4 @@
-from scitbx import wigner 
+from scitbx import wigner
 import math
 
 def tst1():
@@ -15,7 +15,7 @@ def tst1():
   value = wigner_fast.compute(2,2,3,1,2,-3)
   assert abs(result-value)<1e-6
   assert abs(w.get_value()-value)<1e-6
-  
+
   result=math.sqrt(15.0/52003.0)*(-5.0)
   w=wigner.wigner3j(5, 8, 10, 3, -3, 0)
   value = wigner_fast.compute(5, 8, 10, 3, -3, 0)
@@ -23,11 +23,11 @@ def tst1():
   assert abs(w.get_value()-value)<1e-6
 
   w=wigner.wigner3j(5,8,10,2,4,-6)
-  result=-(32.0/3.0)*math.sqrt(26.0/2860165.0) 
+  result=-(32.0/3.0)*math.sqrt(26.0/2860165.0)
   value = wigner_fast.compute(5,8,10,2,4,-6)
   assert abs(result-value)<1e-6
   assert abs(w.get_value()-value)<1e-6
-  
+
 if __name__=="__main__":
   tst1()
   print "OK"
