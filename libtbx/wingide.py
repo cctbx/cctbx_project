ff8c447cc88020119da621e43d64bb87a85645cf
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-03-04 14:23:07 +0000

    Watchpoints for Wing IDE debugger

---
+++ libtbx/wingide.py
@@ -0,0 +1,21 @@
+""" Tools for the Python integrated development environment Wing IDE """
+
+def log(stop=False, **kwds):
+  """ Helper to ease putting watchpoints
+
+  Synopsis:
+    - set a conditional breakpoint
+    - write the condition as
+        some_condition and log(some_variable=some_variable, ...)
+    - debug the code
+
+  The debugger won't stop at that breakpoint but it will log name and values
+  of the specified variables each time it passes over that breakpoint if
+  and only if the given condition is true.
+
+  Remark:
+    Passing stop=True makes it a real breakpoint
+  """
+  for k,v in kwds.iteritems():
+    print "%s = %s" % (k,v)
+  return stop
