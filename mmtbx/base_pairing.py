5d27e1157a049745f931e27b8dd74fe0cead8f57
Jeffrey Headd <jjheadd@users.noreply.github.com>
2010-04-21 20:02:18 +0000

    added get_pair_type function

--- mmtbx/base_pairing.py
+++ mmtbx/base_pairing.py
@@ -61,14 +61,36 @@ class pair_database (object) :
       raise RuntimeError("No entry for base pair %s with type %s (H=%s)." %
         (base_pair, pair_type, str(use_hydrogens)))
 
-  def get_pair_type (self, base_pair, atom_pairs) :
-    return None # TODO
+  def get_pair_type (self, base_pair, atom_pairs, use_hydrogens=False) :
+    return_pair_type = None
+    if use_hydrogens :
+      db = self._h_bond_pairs
+    else:
+      db = self._pseudo_bond_pairs
+    for pair_type in db:
+      if base_pair in db[pair_type]:
+        if db[pair_type][base_pair].sort(key=sort_tuple) == atom_pairs.sort(key=sort_tuple):
+          if return_pair_type is None:
+            return_pair_type = pair_type
+          else:
+            raise RuntimeError("Redundant entries found for base pair %s." % base_pair)
+      elif base_pair[::-1] in db[pair_type]:
+        if db[pair_type][base_pair[::-1]].sort(key=sort_tuple) == \
+           invert_pairs(atom_pairs).sort(key=sort_tuple):
+          if return_pair_type is None:
+            return_pair_type = pair_type
+          else:
+            raise RuntimeError("Redundant entries found for base pair %s." % base_pair)
+    return return_pair_type
 
 def invert_pairs(h_bond_atoms):
   inverted_h_bond_atoms = []
   for pair in h_bond_atoms:
     inverted_h_bond_atoms.append((pair[1],pair[0]))
   return inverted_h_bond_atoms
+
+def sort_tuple(tuple):
+  return tuple[0]
 
 db = pair_database()
 
@@ -80,6 +102,9 @@ def exercise () :
   assert (db.get_atoms("AU", "WWT", True) == [('H61', 'O4'), ('N1', 'H3')])
   assert (db.get_atoms("GC", "WWT", False) == [('O6', 'N4'), ('N1', 'N3'),
     ('N2', 'O2')])
+  assert db.get_pair_type("AU", [('H61', 'O4'), ('N1', 'H3')], True) == "WWT"
+  assert db.get_pair_type("AU", [('N1', 'H3'), ('H61', 'O4')], True) == "WWT"
+  assert db.get_pair_type("CG", [('N4', 'O6'), ('N3', 'N1'), ('O2', 'N2')], False) == "WWT"
   print "OK"
 
 if __name__ == "__main__" :
