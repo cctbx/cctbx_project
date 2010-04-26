47266f932d64a904433229a3d46e50990112d1dc
Jeffrey Headd <jjheadd@users.noreply.github.com>
2010-04-26 23:55:22 +0000

    added user-defined planarity restraints for use with RNA bases

--- mmtbx/monomer_library/tst_selection.py
+++ mmtbx/monomer_library/tst_selection.py
@@ -162,16 +162,16 @@ def exercise_atom_selections():
   assert list(isel("peptide backbone")) \
       == list(isel("peptide and (name ca or name c or name o or name n)"))
   assert list(isel("nucleotide backbone")) \
-      == [52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 63,
-          72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 83,
+      == [52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
+          72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
           95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105]
   assert list(isel("backbone and (rna or dna)")) \
       == list(isel("nucleotide backbone"))
   assert list(isel("phosphate")) \
       == [52, 53, 54, 55, 60, 72, 73, 74, 75, 80, 95, 96, 97, 98, 103]
   assert list(isel("ribose")) \
-      == [56, 57, 58, 59, 61, 63,
-          76, 77, 78, 79, 81, 83,
+      == [56, 57, 58, 59, 61, 62, 63,
+          76, 77, 78, 79, 81, 82, 83,
           99, 100, 101, 102, 104, 105]
   assert list(isel("phosphate or ribose")) == list(isel("nuc backbone"))
   assert list(isel("backbone resname glu")) \
