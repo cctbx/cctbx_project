6affa32daa3fff2743f0b7c4026d5760bb4e4ab7
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2010-05-02 14:43:39 +0000

    libtbx/command_line/show_number_of_processors.py: add libtbx parent dir to sys.path, to make command work without having to set PYTHONPATH externally

--- libtbx/command_line/show_number_of_processors.py
+++ libtbx/command_line/show_number_of_processors.py
@@ -1,7 +1,13 @@
-import libtbx.introspection
 import sys
 
 def run(args):
+  import os.path as op
+  dn = op.dirname
+  d = dn(dn(__file__))
+  d,b = op.split(d)
+  if (b == "libtbx" and op.isdir(d)):
+    sys.path.insert(0, d)
+  import libtbx.introspection
   n = libtbx.introspection.number_of_processors(return_value_if_unknown=None)
   if (n is not None or len(args) == 0):
     print n
