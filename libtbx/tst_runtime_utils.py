fcc7d6dc6991c0eaa1dfd5cf00347c07f8e1f546
Nathaniel Echols <nathaniel.echols@gmail.com>
2010-05-07 22:57:48 +0000

    fixed broken runtime_utils test

--- libtbx/tst_runtime_utils.py
+++ libtbx/tst_runtime_utils.py
@@ -17,10 +17,14 @@ def exercise () :
   run = runtime_utils.simple_run(output_dir)
   params.output_dir = output_dir
   params.buffer_stdout = False
+  params.tmp_dir = output_dir
 #  driver = runtime_utils.detached_process_driver(output_dir, run)
   params.run_file = os.path.join(output_dir, "run.pkl")
+  eff_file = os.path.join(output_dir, "run.eff")
+  working_phil = runtime_utils.process_master_phil.format(python_object=params)
+  working_phil.show(out=open(eff_file, "w"))
   easy_pickle.dump(params.run_file, run)
-  easy_run.call("libtbx.start_process %s &" % params.run_file)
+  easy_run.call("libtbx.start_process %s &" % eff_file) #params.run_file)
   client = runtime_utils.simple_client(params)
   client.run()
   assert (client.out.getvalue() == """\
