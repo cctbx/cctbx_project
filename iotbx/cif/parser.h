9b0e6a785addf13048836158be285d1241f7ea11
Richard Gildea <richard.gildea@diamond.ac.uk>
2010-05-14 11:15:31 +0000

    iotbx/cif: a compiled version of the ANTLR3 parser

---
+++ iotbx/cif/parser.h
@@ -0,0 +1,42 @@
+#ifndef IOTBX_CIF_PARSER_H
+#define IOTBX_CIF_PARSER_H
+
+#include <istream>
+#include <string>
+
+#include <iotbx/cif/cif2Lexer.h>
+#include <iotbx/cif/cif2Parser.h>
+#include <boost/python/object.hpp>
+
+namespace iotbx { namespace cif {
+
+class parser
+{
+
+
+  public:
+
+    parser(std::string filename, boost::python::object& builder)
+    {
+      input = antlr3AsciiFileStreamNew(pANTLR3_UINT8(filename.c_str()));
+      if (input == NULL)
+      {
+        input = antlr3NewAsciiStringInPlaceStream(pANTLR3_UINT8(filename.c_str()), filename.size(), pANTLR3_UINT8("memory"));
+      }
+      lxr = cif2LexerNew(input);
+      tstream = antlr3CommonTokenStreamSourceNew(ANTLR3_SIZE_HINT, TOKENSOURCE(lxr));
+      psr = cif2ParserNew(tstream);
+      psr->parse(psr, builder);
+    }
+
+  private:
+      pANTLR3_COMMON_TOKEN_STREAM       tstream;
+      pANTLR3_INPUT_STREAM input;
+      pcif2Lexer lxr;
+      pcif2Parser psr;
+
+};
+
+}} // namespace iotbx::cif
+
+#endif //GUARD
