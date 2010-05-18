02b4462143799cff9529a1d89001ca21f6b9103c
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-05-18 13:15:31 +0000

    brush up code

--- scitbx/random/variate_generator.h
+++ scitbx/random/variate_generator.h
@@ -12,30 +12,29 @@ namespace scitbx { namespace random {
       in addition to single numbers.
    */
   template<class Engine, class Distribution>
-  struct variate_generator : public boost::variate_generator<Engine, Distribution>
+  struct variate_generator
+        : public boost::variate_generator<Engine, Distribution>
   {
   private:
     typedef boost::variate_generator<Engine, Distribution> base_class;
 
   public:
     typedef typename base_class::result_type result_type;
-    variate_generator(Engine e, Distribution d)
-      : base_class(e, d) {}
 
-    result_type
-    operator()()
-    {
+    variate_generator(Engine e, Distribution d) : base_class(e, d) {}
+
+    result_type operator()() {
       return base_class::operator()();
     }
-    af::shared<result_type>
-    operator()(std::size_t size)
-    {
-      af::shared<result_type> result(size, af::init_functor_null<result_type>());
-      result_type* r = result.begin();
-      for(std::size_t i=0;i<size;i++) *r++ = base_class::operator()();
+
+    af::shared<result_type> operator()(std::size_t size) {
+      af::reserve reserved(size);
+      af::shared<result_type> result(reserved);
+      for(std::size_t i=0; i<size; ++i) result.push_back((*this)());
       return result;
     }
-
   };
+
 }} // scitbx::random
+
 #endif // GUARD
