cd0ab50cc4ea6d26f6afc5380384cadb3a9f9243
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-05-18 13:16:13 +0000

    major overhaul of package scitbx.random In C++:   - factory function to make variate_generator's   - new 'wrap_distribution_and_variate' tool to reduce the boiler plate for each     distribution   - last 3 items in a header random.h for reuse in other Boost.Python bindings In Python:   - variate_generator are now Python generators (next & __iter__)   - class 'variate_factory' and a singleton instance for other modules     to plug into the new framework Both:   - add Bernoulli distribution for proof of concept   - rename class as mersenne_twister_19937

--- scitbx/random/boost_python/random_ext.cpp
+++ scitbx/random/boost_python/random_ext.cpp
@@ -1,11 +1,34 @@
+#include <scitbx/random/mersenne_twister.h>
+
 #include <boost/python/module.hpp>
+#include <boost/python/class.hpp>
+#include <boost/python/args.hpp>
 
 namespace scitbx { namespace random { namespace boost_python {
 
   void wrap_random();
 
+  /*! This is intentionally kept useless as a generator
+   in order to discourage use directly from python.
+   Instead use variate_generators.
+   */
+  struct mt19937_wrappers
+  {
+    typedef boost_random::mt19937 wt;
+
+    static void wrap() {
+      using namespace boost::python;
+      class_<wt>("mersenne_twister_19937", no_init)
+        .def(init<wt::result_type>(arg("value")))
+        .def("seed", (void(wt::*)()) &wt::seed)
+        .def("seed", (void(wt::*)(const wt::result_type&)) &wt::seed)
+        ;
+    }
+  };
+
   namespace {
     void init_module() {
+      mt19937_wrappers::wrap();
       wrap_random();
     }
   }
--- scitbx/random/ext.py
+++
@@ -1,3 +0,0 @@
-import boost.python
-boost.python.import_ext("scitbx_random_ext")
-from scitbx_random_ext import *
