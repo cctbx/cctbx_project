625ab72401ffc14f9b38585d62001b71c9ab2c4c
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-05-18 13:16:30 +0000

    Python binding for boost::uniform_real

--- scitbx/random/boost_python/basic_distributions.cpp
+++ scitbx/random/boost_python/basic_distributions.cpp
@@ -1,3 +1,4 @@
+#include <boost/random/uniform_real.hpp>
 #include <boost/random/normal_distribution.hpp>
 #include <boost/random/bernoulli_distribution.hpp>
 
@@ -6,6 +7,22 @@
 namespace scitbx { namespace random { namespace boost_python {
 
 namespace {
+
+  struct uniform
+  {
+    typedef boost::uniform_real<double> wt;
+
+    static std::string name() { return "uniform"; }
+
+    static void wrap_specific(boost::python::class_<wt> &klass) {
+      using namespace boost::python;
+      klass
+        .def(init<double, double>((arg("min"), arg("max"))))
+        .add_property("min", &wt::min)
+        .add_property("min", &wt::min)
+      ;
+    }
+  };
 
   struct normal
   {
@@ -44,6 +61,7 @@ namespace {
 
   void wrap_random()
   {
+    wrap_distribution_and_variate<uniform>();
     wrap_distribution_and_variate<normal>();
     wrap_distribution_and_variate<bernoulli>();
   }
