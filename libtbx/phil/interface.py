3367d4df3612edabc470b61ca3dbd7310d189dc5
Nathaniel Echols <nathaniel.echols@gmail.com>
2010-06-03 23:24:34 +0000

    fix for persistent sources list bug

--- libtbx/phil/interface.py
+++ libtbx/phil/interface.py
@@ -78,7 +78,8 @@ class index (object) :
       sources = []
     if extra_phil != "" :
       self.merge_phil(phil_string=extra_phil, rebuild_index=False)
-    final_phil = self.master_phil.fetch(sources=[self.working_phil]+sources)
+    final_phil = self.master_phil.fetch(sources=[self.working_phil] +
+      list(sources))
     if diff_only :
       output_phil = self.master_phil.fetch_diff(source=final_phil)
     else :
