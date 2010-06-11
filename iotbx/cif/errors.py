a4954bbbaa28fa615487bfa21d48ce881a7850bc
Richard Gildea <richard.gildea@diamond.ac.uk>
2010-06-11 12:17:16 +0000

    iotbx/cif/validation.py: validation against DDL2 dictionaries (TODO: add DDL2-specific test case)

--- iotbx/cif/errors.py
+++ iotbx/cif/errors.py
@@ -12,9 +12,11 @@ english = {
 
   # warnings
   1001: "Unknown data item: '%(key)s' not present in dictionary",
+  1002: "Warning: case-sensitive match failure for value '%(value)s' for '%(key)s'",
+  1003: "Warning: obsolete definition: '%(key)s' replaced by '%(related_item)s'",
 
   # Type errors
-  2001: "Type error for %(key)s: '%(value)s' is not interpretable as type '%(data_type)s'",
+  2001: "Type error for %(key)s: '%(value)s' is not interpretable as type '%(item_type)s'",
   2002: "Type error for %(key)s: su is not allowed",
 
   # Enumeration errors
@@ -23,13 +25,18 @@ english = {
 
   # Related errors
   2201: "Both data item '%(key)s' and exclusive alternate '%(related_item)s' present in data block",
+  2202: "Associated value item '%(related_item)s' missing for '%(key)s'",
+
+  # Dependent errors
+  2301: "Dependent item '%(dependent)s' not present in data block for '%(key)s'",
+
 
   # Loop errors
   2501: "Invalid loop: data item '%(key)s' cannot be declared in a looped list",
   2502: "Invalid loop: data names from multiple categories present",
-  2503: "Invalid loop: value '%(value)s' present in _list_link_child '%(child)s' but not found in _list_link_parent '%(parent)s'",
-  2504: "Invalid loop: missing _list_parent for loop containing '%(child)s': '%(parent)s' required but not present in data block",
-  2505: "Invalid loop: missing _list_reference for loop containing '%(key)s': '%(reference)s' required but not present",
+  2503: "Invalid loop: value '%(value)s' present in child '%(child)s' but not found in parent '%(parent)s'",
+  2504: "Invalid loop: missing parent for loop containing '%(child)s': '%(parent)s' required but not present in data block",
+  2505: "Invalid loop: missing reference for loop containing '%(key)s': '%(reference)s' required but not present",
   2506: "data item '%(key)s' can only be declared in a looped list",
 
   }
