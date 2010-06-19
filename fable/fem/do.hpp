9a91aca9abdb2aa2e8c432a637a2f70d7bed0891
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2010-06-19 16:54:26 +0000

    cout.py: fem_do_safe=True,False option

--- fable/fem/do.hpp
+++ fable/fem/do.hpp
@@ -1,9 +1,34 @@
 #ifndef FEM_DO_HPP
 #define FEM_DO_HPP
 
+#define FEM_DO(i, f, l) for(i=f; i<=l; i++)
+
 namespace fem {
 
-  class do_
+  class do_safe
+  {
+    int* i_;
+    int l_;
+
+    public:
+
+      do_safe(int* i, int f, int l) : i_(i), l_(l) { (*i) = f; }
+
+      operator
+      bool() { return (*i_) <= l_; }
+
+      void
+      incr() { (*i_)++; }
+  };
+
+} // namespace fem
+
+#define FEM_DO_SAFE(i, f, l) \
+  for(fem::do_safe fem_do(&(i), f, l);fem_do;fem_do.incr())
+
+namespace fem {
+
+  class dostep
   {
     int* i_;
     int l_;
@@ -11,7 +36,7 @@ namespace fem {
 
     public:
 
-      do_(int* i, int f, int l, int s=1) : i_(i), l_(l), s_(s) { (*i) = f; }
+      dostep(int* i, int f, int l, int s) : i_(i), l_(l), s_(s) { (*i) = f; }
 
       operator bool()
       {
@@ -25,10 +50,7 @@ namespace fem {
 
 } // namespace fem
 
-#define FEM_DO(i, f, l) \
-  for(i=f; i<=l; i++)
-
 #define FEM_DOSTEP(i, f, l, s) \
-  for(fem::do_ fem_do(&(i), f, l, s);fem_do;fem_do.incr())
+  for(fem::dostep fem_do(&(i), f, l, s);fem_do;fem_do.incr())
 
 #endif // GUARD
