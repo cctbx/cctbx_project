b332de1cad7ec60a39092346a99c31e6ff12cff6
Nicholas Sauter <nksauter@lbl.gov>
2010-07-02 20:51:40 +0000

    CBF: Expose direct string-to-flex_int array conversion to the Python layer, code is courtesy Graeme Winter

--- cbflib_adaptbx/include/cbflib_adaptbx/buffer_based_service.h
+++ cbflib_adaptbx/include/cbflib_adaptbx/buffer_based_service.h
@@ -1,11 +1,13 @@
 #ifndef BUF_BASED_SVC_H
 #define BUF_BASED_SVC_H
 #include <cstddef>
+#include <vector>
 
 namespace iotbx {
   namespace detectors {
 
-void buffer_uncompress(char*, std::size_t, int*);
+void buffer_uncompress(const char*, std::size_t, int*);
+std::vector<char> buffer_compress(const int*, const std::size_t&);
 
   }//namespace detectors
 }//namespace iotbx
