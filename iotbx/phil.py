f45a255ecbfd2eb8484af1f87bd59ffd7b38e088
Nathaniel Echols <nathaniel.echols@gmail.com>
2010-07-07 19:14:56 +0000

    moved gui utility functions from phenix svn

--- iotbx/phil.py
+++ iotbx/phil.py
@@ -1,8 +1,10 @@
 from cctbx import sgtbx
 from cctbx import uctbx
 import libtbx.phil
+from libtbx.utils import Sorry, import_python_object
 from libtbx.phil import tokenizer
 from libtbx import Auto
+import sys, os
 
 class unit_cell_converters(object):
 
@@ -73,3 +75,71 @@ def read_default(
     params_extension=params_extension,
     converter_registry=converter_registry,
     process_includes=process_includes)
+
+# Utilities for Phenix GUI
+class setup_app_generic (object) :
+  def __init__ (self, master_phil_path) :
+    container = import_python_object(
+      import_path=master_phil_path,
+      error_prefix="",
+      target_must_be="",
+      where_str="")
+    master_phil = container.object
+    if master_phil is None :
+      raise Sorry("Couldn't start program using specified phil object (%s)!" %
+        master_phil_path)
+    if hasattr(master_phil, "__call__") :
+      master_phil = master_phil()
+    elif isinstance(master_phil, str) :
+      master_phil = parse(master_phil, process_includes=True)
+    else :
+      assert type(master_phil).__name__ == "scope"
+    self.master_phil = master_phil
+
+  def __call__ (self, args) :
+    (working_phil, options, unused_args) = parse_command_line_phil_args(
+      args=args,
+      master_phil=self.master_phil,
+      command_name="phenix",
+      usage_opts=["[model.pdb]", "[data.mtz]"],
+      app_options=None,
+      home_scope="")
+    return (self.master_phil,working_phil,options, unused_args)
+
+def parse_command_line_phil_args (args, master_phil, command_name, usage_opts,
+    app_options, home_scope, log=sys.stdout) :
+  sources = []
+  unused_args = []
+  interpreter = libtbx.phil.command_line.argument_interpreter(
+    master_phil=master_phil,
+    home_scope=home_scope)
+  for arg in args :
+    if os.path.isfile(arg) :
+      try :
+        user_phil = parse(file_name=arg)
+        sources.append(user_phil)
+      except Exception, e :
+        unused_args.append(os.path.abspath(arg))
+    elif arg != "" and not arg.startswith("-") :
+      try :
+        params = interpreter.process(arg=arg)
+      except RuntimeError :
+        print >> log, "%s does not appear to be a parameter definition" % arg
+        unused_args.append(arg)
+      else :
+        sources.append(params)
+    elif arg != "" :
+      unused_args.append(arg)
+  try :
+    working_phil = master_phil.fetch_diff(sources=sources)
+  except Exception, e :
+    print >> log, "Error incorporating parameters from user-specified file(s):"
+    print >> log, str(e)
+    print >> log, "Will revert to default parameters for this run."
+    working_phil = master_phil.fetch()
+
+  assert working_phil is not None
+  if "--debug" in args :
+    working_phil.show()
+  cmdline_opts = None
+  return (working_phil, cmdline_opts, unused_args)
