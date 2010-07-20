d648d3a1a3348c187c637c2dc4feee72064a4855
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2010-07-20 22:51:53 +0000

    scitbx/linalg/tests/tst_eigensystem.py: comparison of eigensystem and lapack_dsyev times

--- scitbx/linalg/boost_python/lapack_dsyev_bpl.cpp
+++ scitbx/linalg/boost_python/lapack_dsyev_bpl.cpp
@@ -3,6 +3,7 @@
 #include <boost/python.hpp>
 
 #include <lapack_fem/dsyev.hpp>
+#include <scitbx/sym_mat3.h>
 #include <scitbx/array_family/versa.h>
 #include <scitbx/array_family/accessors/c_grid.h>
 #include <boost/scoped_array.hpp>
@@ -40,12 +41,33 @@ namespace lapack_fem { namespace boost_python {
     return info;
   }
 
+  scitbx::vec3<double>
+  time_dsyev(
+    scitbx::sym_mat3<double> const& m, std::size_t n_repetitions)
+  {
+    SCITBX_ASSERT(n_repetitions % 2 == 0);
+    scitbx::vec3<double> result(0,0,0);
+    for(std::size_t i=0;i<n_repetitions/2;i++) {
+      for(std::size_t j=0;j<2;j++) {
+        scitbx::vec3<double> w;
+        scitbx::mat3<double> a(m);
+        dsyev_wrapper("V", "U",
+          af::ref<double, af::c_grid<2> >(a.begin(), af::c_grid<2>(3,3)),
+          w.ref());
+        if (j == 0) result += w;
+        else        result -= w;
+      }
+    }
+    return result / static_cast<double>(n_repetitions);
+  }
+
   void
   wrap()
   {
     using namespace boost::python;
     def("lapack_dsyev", dsyev_wrapper, (
       arg("jobz"), arg("uplo"), arg("a"), arg("w")));
+    def("time_lapack_dsyev", time_dsyev);
   }
 
 }} // namespace lapack_fem::boost_python
