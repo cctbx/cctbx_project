84bac017cd805306b5c5d05b4586ea1ad9b69aaa
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2010-07-25 08:39:39 +0000

    fable/fem/common.hpp: removing dependency on boost::any; should also reduce compile-time and runtime overhead, although it may be insignificant

--- fable/fem/common.hpp
+++ fable/fem/common.hpp
@@ -6,8 +6,6 @@
 #include <fem/inquire_chain.hpp>
 #include <fem/open_chain.hpp>
 #include <fem/utils/misc.hpp>
-#include <boost/any.hpp>
-#include <boost/shared_ptr.hpp>
 
 namespace fem {
 
@@ -16,7 +14,65 @@ namespace fem {
     fem::io io;
   };
 
-  typedef boost::any cmn_sve;
+  struct cmn_sve
+  {
+    private:
+      // simplified boost::any, with pointer holder instead of value holder
+      struct placeholder
+      {
+        virtual ~placeholder() {}
+      };
+
+      template <typename T>
+      struct holder : placeholder
+      {
+        T* ptr;
+
+        holder(T* ptr_) : ptr(ptr_) {}
+
+        ~holder() { delete ptr; }
+
+        private:
+          holder& operator=(holder const&);
+          holder(holder const&);
+      };
+
+      placeholder* content;
+
+      cmn_sve(cmn_sve const&);
+      cmn_sve& operator=(cmn_sve const&);
+
+      public:
+
+    cmn_sve() : content(0) {}
+
+    ~cmn_sve() { delete content; }
+
+    bool
+    is_called_first_time() { return (content == 0); }
+
+    template <typename T>
+    void
+    construct()
+    {
+      content = new holder<T>(new T);
+    }
+
+    template <typename T, typename D>
+    void
+    construct(
+      D const& dynamic_parameters)
+    {
+      content = new holder<T>(new T(dynamic_parameters));
+    }
+
+    template <typename T>
+    T&
+    get()
+    {
+      return *(static_cast<holder<T>*>(content)->ptr);
+    }
+  };
 
   template <typename T>
   void
@@ -27,22 +83,18 @@ namespace fem {
 } // namespace fem
 
 #define FEM_CMN_SVE(FUNC) \
-  bool is_called_first_time = cmn.FUNC##_sve.empty(); \
+  bool is_called_first_time = cmn.FUNC##_sve.is_called_first_time(); \
   if (is_called_first_time) { \
-    cmn.FUNC##_sve = boost::shared_ptr<FUNC##_save>( \
-      new FUNC##_save); \
+    cmn.FUNC##_sve.construct<FUNC##_save>(); \
   } \
-  FUNC##_save& sve = *boost::any_cast< \
-    boost::shared_ptr<FUNC##_save> >(cmn.FUNC##_sve).get(); \
+  FUNC##_save& sve = cmn.FUNC##_sve.get<FUNC##_save>(); \
   fem::no_operation_to_avoid_unused_variable_warning(sve)
 
 #define FEM_CMN_SVE_DYNAMIC_PARAMETERS(FUNC) \
-  bool is_called_first_time = cmn.FUNC##_sve.empty(); \
+  bool is_called_first_time = cmn.FUNC##_sve.is_called_first_time(); \
   if (is_called_first_time) { \
-    cmn.FUNC##_sve = boost::shared_ptr<FUNC##_save>( \
-      new FUNC##_save(cmn.dynamic_parameters)); \
+    cmn.FUNC##_sve.construct<FUNC##_save>(cmn.dynamic_parameters); \
   } \
-  FUNC##_save& sve = *boost::any_cast< \
-    boost::shared_ptr<FUNC##_save> >(cmn.FUNC##_sve).get()
+  FUNC##_save& sve = cmn.FUNC##_sve.get<FUNC##_save>()
 
 #endif // GUARD
