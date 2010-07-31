f6fdade777ae14d76de95053a1781fb0857ca915
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2010-07-31 07:46:15 +0000

    new scitbx.r3_utils.clash_detector_simple, used in iotbx/examples/pdb_tardy_conf_sampling_simple.py

--- scitbx/run_tests.py
+++ scitbx/run_tests.py
@@ -37,6 +37,7 @@ def run():
   "$D/array_family/boost_python/tst_shared.py",
   "$D/array_family/boost_python/tst_integer_offsets_vs_pointers.py",
   "$D/array_family/boost_python/tst_cost_of_m_handle_in_af_shared.py",
+  "$D/tst_r3_utils.py",
   "$D/matrix.py",
   "$D/python_utils/tst_random_transform.py",
   "$D/python_utils/tst_graph.py",
