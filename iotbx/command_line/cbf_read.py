0cb4a4e29be9e9bcd0e1b3c47d64db1678ce8316
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2010-08-03 18:49:28 +0000

    new iotbx/command_line/cbf_read.py: quick-and-dirty start

---
+++ iotbx/command_line/cbf_read.py
@@ -0,0 +1,31 @@
+import iotbx.cif
+import iotbx.cif.validation
+import libtbx.load_env
+import sys, os
+op = os.path
+
+def run(args):
+  dic_path = libtbx.env.find_in_repositories(
+    relative_path="cbflib/doc/cif_img_1.5.4_28Jul07.dic",
+    test=op.isfile)
+  if (dic_path is not None):
+    cif_dic = iotbx.cif.validation.smart_load_dictionary(file_path=dic_path)
+  else:
+    cif_dic = None
+  for file_name in args:
+    raw = open(file_name, "rb").read()
+    pattern = "--CIF-BINARY-FORMAT-SECTION--"
+    i = raw.find(pattern)
+    if (i >= 0):
+      j = raw.rfind(pattern+"--")
+    sliced = raw[:i] + "\n" + raw[j:]
+    cif = iotbx.cif.fast_reader(input_string=sliced)
+    model = cif.model()
+    im1 = model["image_1"]
+    print im1["_array_data.data"]
+    if (cif_dic is not None):
+      model.validate(cif_dic)
+
+if (__name__ == "__main__"):
+  import sys
+  run(args=sys.argv[1:])
