c9e4eed7c097cd665683b2411ed7f89f50e1ab44
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2010-08-03 08:34:33 +0000

    scitbx/linalg/boost_python: wrapper for lapack DGESVD

--- scitbx/linalg/boost_python/lapack_fem_bpl.cpp
+++ scitbx/linalg/boost_python/lapack_fem_bpl.cpp
@@ -4,8 +4,11 @@
 
 #include <scitbx/sym_mat3.h>
 #include <scitbx/array_family/versa.h>
+#include <scitbx/array_family/shared.h>
 #include <scitbx/array_family/accessors/c_grid.h>
 #include <boost/scoped_array.hpp>
+
+namespace boost_python_meta_ext { struct holder {}; }
 
 // simlar to time_eigensystem_real_symmetric()
 #define SCITBX_LOC(fxx) \
@@ -31,11 +34,52 @@
 
 #if defined(SCITBX_LAPACK_FEM)
 
+#include <lapack_fem/dgesvd.hpp>
 #include <lapack_fem/dsyev.hpp>
 
 namespace scitbx { namespace lapack { namespace boost_python {
 
-  lapack_fem::common cmn;
+  lapack_dgesvd_fem::common dgesvd_cmn;
+  lapack_dsyev_fem::common dsyev_cmn;
+
+  boost::python::object
+  dgesvd_fem_wrapper(
+    af::ref<double, af::c_grid<2> > const& a)
+  {
+    int m = a.accessor()[1];
+    int n = a.accessor()[0];
+    SCITBX_ASSERT(m > 0);
+    SCITBX_ASSERT(n > 0);
+    int lda = fem::max(1,m);
+    af::shared<double> s(fem::min(m,n), 0.);
+    af::versa<double, af::c_grid<2> > u(af::c_grid<2>(m, m), 0.);
+    af::versa<double, af::c_grid<2> > vt(af::c_grid<2>(n, n), 0.);
+    int lwork = fem::max(1,3*fem::min(m,n)+fem::max(m,n),5*fem::min(m,n));
+    boost::scoped_array<double> work(new double[lwork]);
+    int info;
+    lapack_dgesvd_fem::dgesvd(
+      dgesvd_cmn,
+      /*jobu*/ "A",
+      /*jobvt*/ "A",
+      m,
+      n,
+      a[0],
+      lda,
+      s[0],
+      u[0],
+      /*ldu*/ m,
+      vt[0],
+      /*ldvt*/ n,
+      work[0],
+      lwork,
+      info);
+    boost::python::object result((boost_python_meta_ext::holder()));
+    result.attr("s") = s;
+    result.attr("u") = u;
+    result.attr("vt") = vt;
+    result.attr("info") = info;
+    return result;
+  }
 
   int
   dsyev_fem_wrapper(
@@ -50,8 +94,8 @@ namespace scitbx { namespace lapack { namespace boost_python {
     int lwork = std::max(1, 3*n-1);
     boost::scoped_array<double> work(new double[lwork]);
     int info;
-    lapack_fem::dsyev(
-      cmn,
+    lapack_dsyev_fem::dsyev(
+      dsyev_cmn,
       fem::str_cref(jobz.data(), jobz.size()),
       fem::str_cref(uplo.data(), uplo.size()),
       n,
@@ -133,6 +177,8 @@ namespace scitbx { namespace lapack { namespace boost_python {
   {
     using namespace boost::python;
 #if defined(SCITBX_LAPACK_FEM)
+    def("lapack_dgesvd_fem", dgesvd_fem_wrapper, (
+      arg("a")));
     def("lapack_dsyev_fem", dsyev_fem_wrapper, (
       arg("jobz"), arg("uplo"), arg("a"), arg("w")));
     def("time_lapack_dsyev_fem", time_dsyev_fem);
