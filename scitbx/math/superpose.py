bd0df97d8c5934fcca5eeb6ee3436e8ca6509ca1
Peter Zwart <phzwart@users.noreply.github.com>
2010-08-19 01:15:40 +0000

    Added NSD target engine

--- scitbx/math/superpose.py
+++ scitbx/math/superpose.py
@@ -3,6 +3,7 @@ from scitbx.math import superpose_kearsley_rotation
 from scitbx import matrix
 from stdlib import math
 from scitbx.array_family import flex
+from stdlib import math as smath
 
 def kearsley_rotation(reference_sites, other_sites):
   """
@@ -75,3 +76,83 @@ class least_squares_fit(object):
 
   def rt(self):
     return matrix.rt(tuple_r_t=(self.r, self.t))
+
+
+
+"""
+The NSD engine is a simple implementation of the normalized spatial discrepancy
+as discussed by Kochin & Svergun J. Appl. Cryst 2001, 34, 33-41.
+
+It can be used as a target for structure super positioning when no point-point correspondence is known.
+"""
+
+class nsd_engine(object):
+  def __init__(self, fixed, d_fixed=None, d_moving=None):
+    self.fixed = fixed
+    self.d_fixed = d_fixed
+    self.d_moving = d_moving
+    if self.d_fixed is None:
+      self.d_fixed = self.get_mean_distance(self.fixed)
+
+
+  def get_mean_distance(self,xyz):
+    N = xyz.size()
+    d = 0
+    count=0
+    for ii in range(N):
+      for jj in range(ii+1,N):
+        dd = flex.double(xyz[ii])-flex.double(xyz[jj])
+        d += dd.norm()
+        count += 1
+    d = d / count
+    return d*d
+
+
+  def nsd(self,moving,d_moving=None):
+    if d_moving is None:
+      self.d_moving = self.get_mean_distance(moving)
+    else:
+      self.d_moving = d_moving
+
+    # loop over all sites in fixed, find the minimum for each site
+    tot_rho_mf = 0
+    tot_rho_fm = 0
+    for site in moving:
+      dd = self.fixed-site
+      dd = flex.min( dd.norms() )
+      tot_rho_mf+=dd*dd
+
+    for site in self.fixed:
+      dd = moving-site
+      dd = flex.min( dd.norms() )
+      tot_rho_fm+=dd
+    tot_rho_fm = tot_rho_fm / (self.fixed.size()*self.d_fixed )
+    tot_rho_mf = tot_rho_mf / (moving.size()*self.d_moving )
+    result = smath.sqrt((tot_rho_fm+tot_rho_mf)/2.0)
+    return result
+
+
+
+def tst_nsd():
+  moving1 = flex.vec3_double()
+  moving2 = flex.vec3_double()
+  fixed  = flex.vec3_double()
+  for ii in range(20):
+    noise = flex.random_double(3)*2-1.0
+    xyz = flex.random_double(3)*5
+    fixed.append( list(xyz) )
+    moving1.append(  list(xyz + noise/10) )
+    moving2.append(  list(xyz + noise/2) )
+
+  ne = nsd_engine(fixed)
+  a = ne.nsd(fixed)
+  b = ne.nsd(moving1)
+  c = ne.nsd(moving2)
+  assert abs(a)<1e-6
+  assert(b<=c)
+
+
+
+if __name__ == "__main__":
+  tst_nsd()
+  print "OK"
