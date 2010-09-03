a38d2cce1915d8dccb0189869032972de0cdd4e2
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2010-09-03 16:48:43 +0000

    fable: changing all "prcd" to "fproc"

--- fable/command_line/insert_write_at_start_of_each_procedure.py
+++ fable/command_line/insert_write_at_start_of_each_procedure.py
@@ -7,16 +7,16 @@ def run(args):
   op = os.path
   n_files_changed = 0
   for file_name in args[1:]:
-    all_prcds = fable.read.process(file_names=[op.abspath(file_name)])
+    all_fprocs = fable.read.process(file_names=[op.abspath(file_name)])
     insert_info = []
-    for prcd in all_prcds.all_in_input_order:
-      if (prcd.prcd_type == "blockdata"): continue
-      if (len(prcd.executable) == 0):
-        print "WARNING: no executable statements in %s" % prcd.name.value
+    for fproc in all_fprocs.all_in_input_order:
+      if (fproc.fproc_type == "blockdata"): continue
+      if (len(fproc.executable) == 0):
+        print "WARNING: no executable statements in %s" % fproc.name.value
       else:
-        sl0 = prcd.executable[0].ssl.source_line_cluster[0]
+        sl0 = fproc.executable[0].ssl.source_line_cluster[0]
         assert sl0.file_name == file_name
-        insert_info.append((prcd.name.value, sl0.line_number))
+        insert_info.append((fproc.name.value, sl0.line_number))
     if (len(insert_info) != 0):
       insert_info.reverse()
       lines = open(file_name).read().splitlines()
