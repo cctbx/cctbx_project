a255fdf84c8db4bbba0b8ae3b45fcbe099686218
Haiguang <hgliu@users.noreply.github.com>
2010-09-23 01:44:16 +0000

    adding 2d zernike moment boost code

---
+++ scitbx/math/boost_python/2d_zernike_moments.cpp
@@ -0,0 +1,78 @@
+#include <boost/python/module.hpp>
+#include <boost/python/class.hpp>
+#include <boost/python/args.hpp>
+
+#include <scitbx/math/zernike.h>
+#include <scitbx/math/2d_zernike_moments.h>
+
+namespace scitbx { namespace math {
+namespace {
+
+//
+//
+  struct two_d_grid_wrapper
+  {
+    typedef grid_2d  < double > w_t;
+    static void
+    wrap()
+    {
+      using namespace boost::python;
+      class_<w_t>("two_d_grid", no_init)
+        .def( init<
+                   int const&,
+                   int const&
+                  >
+             ((
+                arg("np"),
+                arg("n_max")
+             ))
+            )
+        .def("get_ss", &w_t::get_ss)
+        .def("get_ss", &w_t::get_all_ss)
+        .def("clean_space", &w_t::clean_space)
+        .def("construct_space_sum",&w_t::construct_space_sum)
+      ;
+    }
+  };
+
+//
+//
+  struct two_d_moments_wrapper
+  {
+    typedef zernike_2d_moments  < double > w_t;
+    static void
+    wrap()
+    {
+      using namespace boost::python;
+      class_<w_t>("two_d_zernike_moments", no_init)
+        .def( init<
+                   grid_2d<double>,
+                   int const&
+                  >
+             ((
+                arg("grid"),
+                arg("nmax")
+             ))
+            )
+        .def("moments", &w_t::all_moments)
+        .def("get_moment",&w_t::get_moment)
+        .def("calc_moments",&w_t::calc_moments)
+        .def("update_ss",&w_t::update_ss)
+      ;
+    }
+  };
+
+//
+//
+
+} //namespace <anonymous>
+
+namespace boost_python {
+
+  void wrap_2d_zernike_mom()
+  {
+    two_d_moments_wrapper::wrap();
+    two_d_grid_wrapper::wrap();
+  }
+
+}}}
