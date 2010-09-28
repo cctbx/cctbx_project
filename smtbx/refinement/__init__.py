f931699c4612b4800755f29170dc215ec7b616d0
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-09-28 08:20:35 +0000

    Prune the dead wood that LBFGS refinement has become.

--- smtbx/refinement/__init__.py
+++
@@ -1,6 +0,0 @@
-import boost.python
-ext = boost.python.import_ext("smtbx_refinement_ext")
-from smtbx_refinement_ext import *
-import smtbx.refinement.tests
-import smtbx.refinement.minimization
-import smtbx.refinement.barriers
