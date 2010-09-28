e52982a0b392e887ef8732cf188a8b002b85d6c9
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-09-28 08:14:08 +0000

    site, u and occupancy = crystallographic parameter; record scatterer too; array of scatterer_parameters for Python

--- smtbx/refinement/constraints/scatterer_parameters.h
+++ smtbx/refinement/constraints/scatterer_parameters.h
@@ -8,15 +8,26 @@ namespace smtbx { namespace refinement { namespace constraints {
 /// The constraints::parameters corresponding to a single scatterer
 struct scatterer_parameters
 {
-  parameter *site, *occupancy, *u;
+  typedef crystallographic_parameter::scatterer_type scatterer_type;
 
-  scatterer_parameters()
-    : site(0), occupancy(0), u(0)
+  scatterer_type const *scatterer;
+  crystallographic_parameter *site, *occupancy, *u;
+
+  scatterer_parameters() {}
+
+  scatterer_parameters(scatterer_type const *scatterer)
+    : scatterer(scatterer),
+      site(0), occupancy(0), u(0)
   {}
 
-  scatterer_parameters(parameter *site, parameter *occupancy, parameter *u)
-    : site(site), occupancy(occupancy), u(u)
+  scatterer_parameters(scatterer_type const *scatterer,
+                       crystallographic_parameter *site,
+                       crystallographic_parameter *occupancy,
+                       crystallographic_parameter *u)
+    : scatterer(scatterer),
+      site(site), occupancy(occupancy), u(u)
   {}
+
 };
 
 
