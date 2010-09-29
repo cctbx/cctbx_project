57514b9768bcd6dde50234ae2f50b4c21776cb8e
Jeffrey Headd <jjheadd@users.noreply.github.com>
2010-09-29 23:33:15 +0000

    updated reference_model test

--- mmtbx/refinement/tst_reference_model.py
+++ mmtbx/refinement/tst_reference_model.py
@@ -46,6 +46,26 @@ ATOM   5499  CD1 LEU C 237      16.055  65.956  29.107  1.00 13.10           C
 ATOM   5500  CD2 LEU C 237      18.455  66.271  28.343  1.00 11.63           C
 """.splitlines()
 
+reference_raw_records_alt_seq = """\
+CRYST1   40.688   71.918   93.213 108.16  93.25  90.40 P 1           4
+ATOM   5485  N   ASN C 246      16.417  72.834  29.095  1.00  7.17           N
+ATOM   5486  CA  ASN C 246      15.051  72.312  29.173  1.00  7.74           C
+ATOM   5487  C   ASN C 246      15.000  70.818  29.431  1.00  7.38           C
+ATOM   5488  O   ASN C 246      14.047  70.141  29.024  1.00  7.80           O
+ATOM   5489  CB  ASN C 246      14.281  72.645  27.887  1.00  8.78           C
+ATOM   5490  CG  ASN C 246      12.769  72.657  28.088  1.00 13.44           C
+ATOM   5491  OD1 ASN C 246      12.265  73.196  29.082  1.00 20.19           O
+ATOM   5492  ND2 ASN C 246      12.032  72.114  27.109  1.00 16.07           N
+ATOM   5493  N   LEU C 247      16.010  70.282  30.134  1.00  6.60           N
+ATOM   5494  CA  LEU C 247      16.122  68.825  30.270  1.00  7.41           C
+ATOM   5495  C   LEU C 247      16.481  68.430  31.697  1.00  6.01           C
+ATOM   5496  O   LEU C 247      15.944  67.448  32.224  1.00  6.47           O
+ATOM   5497  CB  LEU C 247      17.151  68.239  29.297  1.00  8.10           C
+ATOM   5498  CG  LEU C 247      17.384  66.726  29.347  1.00 10.94           C
+ATOM   5499  CD1 LEU C 247      16.055  65.956  29.107  1.00 13.10           C
+ATOM   5500  CD2 LEU C 247      18.455  66.271  28.343  1.00 11.63           C
+""".splitlines()
+
 def get_master_phil():
   return iotbx.phil.parse(
     input_string="""\
@@ -94,6 +114,26 @@ reference_model
 }
 """)
 
+def get_master_phil_alt_seq():
+  return iotbx.phil.parse("""
+reference_model
+{
+   reference_group
+   .multiple=True
+   .optional=True
+  {
+    reference= chain C and resseq 246:247
+      .type=str
+      .short_caption=Reference selection
+      .style = selection
+    selection= chain C and resid 236:237
+      .type=str
+      .short_caption=Restrained selection
+      .style = selection
+  }
+}
+""")
+
 def exercise_reference_model(args, mon_lib_srv, ener_lib):
   master_phil = get_master_phil()
   #print master_phil.show()
@@ -119,6 +159,13 @@ def exercise_reference_model(args, mon_lib_srv, ener_lib):
     file_name=None,
     raw_records=reference_raw_records,
     for_dihedral_reference=True)
+  processed_pdb_file_ref_alt_seq = monomer_library.pdb_interpretation.process(
+    mon_lib_srv=mon_lib_srv,
+    ener_lib=ener_lib,
+    file_name=None,
+    raw_records=reference_raw_records_alt_seq,
+    for_dihedral_reference=True)
+  pdb_hierarchy_ref_alt_seq = processed_pdb_file_ref_alt_seq.all_chain_proxies.pdb_hierarchy
   geometry_ref = processed_pdb_file_ref.geometry_restraints_manager()
   sites_cart_ref = processed_pdb_file_ref.all_chain_proxies.sites_cart
   pdb_hierarchy_ref=processed_pdb_file_ref.all_chain_proxies.pdb_hierarchy
@@ -159,6 +206,16 @@ def exercise_reference_model(args, mon_lib_srv, ener_lib):
   assert len(reference_dihedral_proxies) == len(dihedral_hash)
   for rdp in reference_dihedral_proxies:
     assert rdp.limit == work_params.reference_model.limit
+
+  master_phil_alt_seq = get_master_phil_alt_seq()
+  work_params_alt_seq = master_phil_alt_seq.extract()
+  match_map = reference_model.process_reference_groups(
+                pdb_hierarchy,
+                pdb_hierarchy_ref_alt_seq,
+                work_params_alt_seq.reference_model)
+  assert match_map == \
+  {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 11,
+   12: 12, 13: 13, 14: 14, 15: 15}
 
   r = rotalyze()
   rot_list_model, coot_model = r.analyze_pdb(
