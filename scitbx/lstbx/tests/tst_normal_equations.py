8700788f409e1c7b8390721524a6128bf017c447
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-10-01 13:35:29 +0000

    new member function to add equations specified by a sparse design matrix; expose add_equation and add_equations to Python

--- scitbx/lstbx/tests/tst_normal_equations.py
+++ scitbx/lstbx/tests/tst_normal_equations.py
@@ -1,5 +1,39 @@
 from scitbx.array_family import flex
-from scitbx import lstbx
+from scitbx import lstbx, sparse
+from libtbx.test_utils import approx_equal
+
+def exercise_basic_normal_equations():
+  py_eqs = [ ( 1, (-1,  0,  0),  1),
+             ( 2, ( 2, -1,  0),  3),
+             (-1, ( 0,  2,  1),  2),
+             (-2, ( 0,  1,  0), -2),
+             ]
+
+  eqs_0 = lstbx.normal_equations(3)
+  for b, a, w in py_eqs:
+    eqs_0.add_equation(right_hand_side=b,
+                       design_matrix_row=flex.double(a),
+                       weight=w)
+
+  eqs_1 = lstbx.normal_equations(3)
+  b = flex.double()
+  w = flex.double()
+  a = sparse.matrix(len(py_eqs), 3)
+  for i, (b_, a_, w_) in enumerate(py_eqs):
+    b.append(b_)
+    w.append(w_)
+    for j in xrange(3):
+      if a_[j]: a[i, j] = a_[j]
+  eqs_1.add_equations(right_hand_side=b, design_matrix=a, weights=w)
+
+  assert approx_equal(
+    eqs_0.normal_matrix_packed_u, eqs_1.normal_matrix_packed_u, eps=1e-15)
+  assert approx_equal(
+    eqs_0.right_hand_side, eqs_1.right_hand_side, eps=1e-15)
+  assert approx_equal(
+    list(eqs_0.normal_matrix_packed_u), [ 13, -6, 0, 9, 4, 2 ], eps=1e-15)
+  assert approx_equal(
+    list(eqs_0.right_hand_side), [ 11, -6, -2 ], eps=1e-15)
 
 def exercise_normal_equations_separating_scale_factor():
   eqs = lstbx.normal_equations_separating_scale_factor(3)
@@ -13,6 +47,7 @@ def exercise_normal_equations_separating_scale_factor():
   assert list(b) == [0, 0, 0]
 
 def run():
+  exercise_basic_normal_equations()
   exercise_normal_equations_separating_scale_factor()
   print 'OK'
 
