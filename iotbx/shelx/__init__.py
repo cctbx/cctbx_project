4ea45e0600fd21487ae5fab56c65cba1e585700a
Richard Gildea <richard.gildea@diamond.ac.uk>
2010-10-01 09:23:15 +0000

    iotbx/shelx: remove stray iotbx.shelx.builders import

--- iotbx/shelx/__init__.py
+++ iotbx/shelx/__init__.py
@@ -1,7 +1,6 @@
 from iotbx.shelx.errors import *
 from iotbx.shelx.lexer import *
 from iotbx.shelx.parsers import *
-from iotbx.shelx.builders import *
 
 import boost.python
 ext = boost.python.import_ext("iotbx_shelx_ext")
