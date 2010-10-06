9979f58185de847f845a51c5ef3b58b2e5a95b7c
Nicholas Sauter <nksauter@lbl.gov>
2010-10-06 00:02:26 +0000

    Better clarity in understanding whether an image data array needs to be transformed by reversing columns or rows, or transposing.

--- cbflib_adaptbx/detectors/cbf_adaptor.cpp
+++ cbflib_adaptbx/detectors/cbf_adaptor.cpp
@@ -15,7 +15,7 @@ namespace af = scitbx::af;
 namespace ide = iotbx::detectors;
 
 bool
-ide::CBFAdaptor::file_is_transposed(){
+ide::CBFAdaptor::file_is_transposed() const{
     std::string elem,precedence;
     const char *temp_value;
     cbf_failnez ( cbf_find_category(cbf_h,"array_structure_list") );
@@ -29,8 +29,8 @@ ide::CBFAdaptor::file_is_transposed(){
     cbf_failnez ( cbf_get_value(cbf_h,&temp_value) );
     precedence = std::string(temp_value);
 
-    if (precedence=="1") return false;
-    if (precedence=="2") return true;
+    if (precedence=="1") return false; //ELEMENT_X precedence=1 means it is fastest
+    if (precedence=="2") return true;  //ELEMENT_X precedence=2 means it is slow
     throw iotbx::detectors::Error ("Unable to determine precedence of ELEMENT_X");
 }
 
@@ -60,4 +60,39 @@ ide::CBFAdaptor::raster_description(){
     }
 
     return result;
+}
+
+ide::transform_flags
+ide::CBFAdaptor::transform_flags() const{
+    std::string precedence,direction;
+    const char *temp_value;
+    ide::transform_flags T;
+    T.transpose = file_is_transposed();
+    cbf_failnez ( cbf_find_category(cbf_h,"array_structure_list") );
+
+      cbf_failnez ( cbf_rewind_column(cbf_h) );
+      cbf_failnez ( cbf_find_column(cbf_h,"precedence") );
+      cbf_failnez ( cbf_rewind_row(cbf_h) );
+      precedence = "1";
+      cbf_failnez ( cbf_find_row(cbf_h,precedence.c_str()) );
+      cbf_failnez ( cbf_rewind_column(cbf_h) );
+      cbf_failnez ( cbf_find_column(cbf_h,"direction") );
+      cbf_failnez ( cbf_get_value(cbf_h,&temp_value) );
+      direction = std::string(temp_value);
+      SCITBX_ASSERT( direction == "increasing" || direction == "decreasing" );
+      T.reverse_fast = (direction=="decreasing");
+
+      cbf_failnez ( cbf_rewind_column(cbf_h) );
+      cbf_failnez ( cbf_find_column(cbf_h,"precedence") );
+      cbf_failnez ( cbf_rewind_row(cbf_h) );
+      precedence = "2";
+      cbf_failnez ( cbf_find_row(cbf_h,precedence.c_str()) );
+      cbf_failnez ( cbf_rewind_column(cbf_h) );
+      cbf_failnez ( cbf_find_column(cbf_h,"direction") );
+      cbf_failnez ( cbf_get_value(cbf_h,&temp_value) );
+      direction = std::string(temp_value);
+      SCITBX_ASSERT( direction == "increasing" || direction == "decreasing" );
+      T.reverse_slow = (direction=="decreasing");
+
+    return T;
 }
