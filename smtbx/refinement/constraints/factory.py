31c669bba43b9904728d65e707b5c23dc542c58b
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-10-29 13:50:38 +0000

    Constraints factory module for smtbx

---
+++ smtbx/refinement/constraints/factory.py
@@ -0,0 +1,6 @@
+""" The factory module to be used with iotbx builders to make smtbx-aware
+constraints objects to pass to class constraints.reparametrisation
+at construction time.
+"""
+from smtbx.refinement.constraints.adp import *
+from smtbx.refinement.constraints.geometrical_hydrogens import *
