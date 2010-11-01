5c54a9555eb2459dab9af43ecd49c2bd8f51b237
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-11-01 00:13:54 +0000

    Implement classic stopping criteria; change expectations of the class

--- scitbx/lstbx/normal_eqns_solving.py
+++ scitbx/lstbx/normal_eqns_solving.py
@@ -10,6 +10,7 @@ class iterations(object):
   track_shifts = False
   track_gradients = False
   track_all = False
+  n_max_iterations = 100
 
   def __init__(self, normal_eqns, **kwds):
     """ normal_eqns, thereafter denoted as e, shall feature:
@@ -19,60 +20,68 @@ class iterations(object):
               the value of the L.S. objective
           - e.gradient:
               the gradient of the objective wrt the independent parameters
-          - e.gradient_norm_reference:
-              a scalar to compare the norm of the gradient to
+
+        It is assumed that the objective is properly scaled to be
+        between 0 and 1 (or approximately so), so that this class can
+        meaningfully thresholds the norm of the gradients to watch for
+        convergence.
 
         An instance of this class accumulates the following information:
-        L.S. objective and gradient, relative gradient norm
-        (i.e. gradient norm over the gradient norm reference),
-        and L.S. scale factor if the normal equations feature one.
+        L.S. objective and gradient, and L.S. scale factor
+        if the normal equations feature one.
     """
     self.normal_eqns = normal_eqns
     libtbx.adopt_optional_init_args(self, kwds)
     self.objectives = flex.double()
     self.gradients = []
-    self.relative_gradient_norms = flex.double()
+    self.gradient_norms = []
     self.shifts = []
     if hasattr(normal_eqns, "scale_factor"):
       self.scale_factors = flex.double()
     else:
       self.scale_factors = None
 
+  class n_iterations(libtbx.property):
+    def fget(self): return len(self.objectives)
+
   def accumulate_normal_equation_properties(self):
     self.objectives.append(self.normal_eqns.objective)
     g = self.normal_eqns.gradient
-    self.relative_gradient_norms.append(
-      g.norm()/self.normal_eqns.gradient_norm_reference)
+    self.gradient_norms.append(g.norm_1())
     if self.scale_factors is not None:
       self.scale_factors.append(self.normal_eqns.scale_factor)
     if self.track_all or self.track_gradients:
-      self.gradients.append(self.normal_eqns.gradient)
+      self.gradients.append(g)
 
   def accumulate_solution_properties(self):
     if self.track_all or self.track_shifts:
-      self.shifts.append(self.normal_eqns.shifts)
+      self.shifts.append(self.normal_eqns.shifts.deep_copy())
 
   def accumulate(self):
     self.accumulate_normal_equation_properties()
     self.accumulate_solution_properties()
 
   def do(self,
-         relative_gradient_norm_threshold=None,
-         n_iterations=None):
-    assert (n_iterations is not None
-            or relative_gradient_norm_threshold is not None)
-    if n_iterations is None:
-      counter = itertools.count()
-    else:
-      counter = xrange(n_iterations)
-    for i in counter:
+         gradient_threshold=None,
+         shift_threshold=None,
+         n_iterations = None):
+    """ Iterate, using classic stopping criteria (c.f. e.g.
+        Methods for non-linear least-squares problems,
+        K. Madsen, H.B. Nielsen, O. Tingleff,
+        http://www2.imm.dtu.dk/pubdb/views/edoc_download.php/3215/pdf/imm3215.pdf
+    """
+    eps_1 = gradient_threshold
+    eps_2 = shift_threshold
+    for i in xrange(n_iterations or self.n_max_iterations):
       self.next()
-      if relative_gradient_norm_threshold is None: continue
-      if self.relative_gradient_norms[-1] < relative_gradient_norm_threshold:
+      h = self.normal_eqns.shifts
+      if (eps_1 is not None and self.gradient_norms[-1] <= eps_1):
         break
+      if eps_2 is not None:
+        lxl = self.normal_eqns.parameter_vector_norm
+        if h.norm() <= eps_2*(lxl + eps_2):
+          break
 
-  class n_iterations(libtbx.property):
-    def fget(self): return len(self.objectives)
 
 class naive_iterations(iterations):
 
