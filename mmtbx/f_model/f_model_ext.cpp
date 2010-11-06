4446a3fcdf18562faeb104becd30424acd6b19d6
Pavel Afonine <pafonine@users.noreply.github.com>
2010-11-06 02:02:48 +0000

    adding Fpart and using Toms mask

--- mmtbx/f_model/f_model_ext.cpp
+++ mmtbx/f_model/f_model_ext.cpp
@@ -16,9 +16,12 @@ namespace {
   {
     return boost::python::make_tuple(self.f_calc,
                                      self.f_mask,
-                                     self.u_star,
                                      self.k_sol,
                                      self.b_sol,
+                                     self.f_part,
+                                     self.k_part,
+                                     self.b_part,
+                                     self.u_star,
                                      self.hkl,
                                      self.uc,
                                      self.f_model,
@@ -38,25 +41,34 @@ namespace {
       .def(init<
            af::shared<std::complex<double> >      const&,
            af::shared<std::complex<double> >      const&,
+           double                                 const&,
+           double                                 const&,
+           af::shared<std::complex<double> >      const&,
+           double                                 const&,
+           double                                 const&,
            scitbx::sym_mat3<double>               const&,
-           double                                 const&,
-           double                                 const&,
            af::shared<cctbx::miller::index<> >    const&,
            cctbx::uctbx::unit_cell                const&,
            af::shared<double>                     const& >(
                                                         (arg("f_calc"),
                                                          arg("f_mask"),
-                                                         arg("u_star"),
                                                          arg("k_sol"),
                                                          arg("b_sol"),
+                                                         arg("f_part_base"),
+                                                         arg("k_part"),
+                                                         arg("b_part"),
+                                                         arg("u_star"),
                                                          arg("hkl"),
                                                          arg("uc"),
                                                          arg("ss"))))
       .add_property("f_calc",        make_getter(&core<>::f_calc,       rbv()))
       .add_property("f_mask",        make_getter(&core<>::f_mask,       rbv()))
-      .add_property("u_star",        make_getter(&core<>::u_star,       rbv()))
       .add_property("k_sol",         make_getter(&core<>::k_sol,        rbv()))
       .add_property("b_sol",         make_getter(&core<>::b_sol,        rbv()))
+      .add_property("f_part",        make_getter(&core<>::f_part,       rbv()))
+      .add_property("k_part",        make_getter(&core<>::k_part,       rbv()))
+      .add_property("b_part",        make_getter(&core<>::b_part,       rbv()))
+      .add_property("u_star",        make_getter(&core<>::u_star,       rbv()))
       .add_property("hkl",           make_getter(&core<>::hkl,          rbv()))
       .add_property("uc",            make_getter(&core<>::uc,           rbv()))
       .add_property("f_model",       make_getter(&core<>::f_model,      rbv()))
