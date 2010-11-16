a1d1ea6de2b97b9303532e235176ac56bdb440e4
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2010-11-16 19:26:34 +0000

    new libtbx.find_unused_imports_crude

---
+++ libtbx/command_line/find_unused_imports_crude.py
@@ -0,0 +1,113 @@
+import os
+op = os.path
+
+def find_unused_imports(py_lines):
+  imports_to_ignore = set([
+    "import libtbx.forward_compatibility",
+    "  import libtbx.start_print_trace",
+    "    import libtbx.callbacks"])
+  combined_lines = []
+  block = []
+  for line in py_lines:
+    if (line in imports_to_ignore):
+      continue
+    l = line.strip()
+    if (not l.endswith("\\")):
+      block.append(l)
+      combined_lines.append(" ".join(block))
+      block = []
+    else:
+      block.append(l[:-1])
+  if (len(block) != 0):
+    combined_lines.append("".join(block))
+  imported_names_dict = {}
+  non_import_lines = []
+  for l in combined_lines:
+    def split():
+      return l.replace(",", " , ").split()
+    if (l.startswith("import ")):
+      if (l.endswith(" # import dependency")):
+        continue
+      if (l.endswith(" # implicit import")):
+        continue
+      flds = split()
+    elif (l.startswith("from ")):
+      if (l.startswith("from __future__ ")):
+        continue
+      flds = split()
+      for i,fld in enumerate(flds):
+        if (fld == "import"):
+          flds = flds[i:]
+          break
+      else:
+        continue
+    else:
+      non_import_lines.append(l)
+      continue
+    assert flds[0] == "import", flds
+    flds = flds[1:]
+    flds.append(",")
+    #
+    def collect(flds):
+      if (len(flds) == 1):
+        name = flds[0]
+      elif (len(flds) == 3):
+        name = flds[2]
+      else:
+        return
+      if (name == "libtbx.load_env"):
+        name = "env"
+      else:
+        name = name.split(".")[-1] # XXX very crude
+      if (name != "*" and name not in imported_names_dict):
+        imported_names_dict[name] = len(imported_names_dict)
+    i = 0
+    for j,fld in enumerate(flds):
+      if (fld == ","):
+        if (j > i):
+          collect(flds[i:j])
+        i = j+1
+  idc = "_0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  imported_names = set(imported_names_dict.keys())
+  used_names = set()
+  for l in non_import_lines:
+    filtered = []
+    for c in l:
+      if (idc.find(c) < 0):
+        c = " "
+      filtered.append(c)
+    flds = "".join(filtered).split()
+    used_names.update(imported_names.intersection(flds))
+  unused_names = list(imported_names - used_names)
+  def cmp_input_order(a, b):
+    return cmp(*(imported_names_dict[a], imported_names_dict[b]))
+  unused_names.sort(cmp_input_order)
+  return unused_names
+
+def show_unused_imports(file_name):
+  unused_imports = find_unused_imports(
+    py_lines=open(file_name).read().splitlines())
+  if (len(unused_imports) != 0):
+    print "%s: %s" % (file_name, ", ".join(unused_imports))
+    print
+
+def walk_func(not_used, dirname, names):
+  for name in names:
+    if (not name.endswith(".py")):
+      continue
+    file_name = op.join(dirname, name)
+    if (op.isfile(file_name)):
+      show_unused_imports(file_name)
+
+def run(args):
+  if (len(args) == 0):
+    args = ["."]
+  for arg in args:
+    if (op.isdir(arg)):
+      op.walk(arg, walk_func, None)
+    elif (op.isfile(arg)):
+      show_unused_imports(file_name=arg)
+
+if (__name__ == "__main__"):
+  import sys
+  run(args=sys.argv[1:])
