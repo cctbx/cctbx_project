fb6a9001432b0f19d31524710991e8f1244f953c
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-12-08 00:21:01 +0000

    Move C++ lstbx tests to Python (discard test for separable case as fix is pending)

--- scitbx/lstbx/tests/tst_normal_equations.py
+++ scitbx/lstbx/tests/tst_normal_equations.py
@@ -1,10 +1,11 @@
 from scitbx.array_family import flex
 from scitbx import sparse
 from scitbx.lstbx import normal_eqns, normal_eqns_solving
-from libtbx.test_utils import approx_equal
+from libtbx.test_utils import approx_equal, Exception_expected
+import libtbx
 import itertools
 
-def exercise_basic_normal_equations():
+def exercise_linear_normal_equations():
   py_eqs = [ ( 1, (-1,  0,  0),  1),
              ( 2, ( 2, -1,  0),  3),
              (-1, ( 0,  2,  1),  2),
@@ -37,31 +38,107 @@ def exercise_basic_normal_equations():
   assert approx_equal(
     list(eqs_0.right_hand_side()), [ 11, -6, -2 ], eps=1e-15)
 
-def exercise_normal_equations_separating_scale_factor():
-  eqs = lstbx.non_linear_ls_with_separable_scale_factor(3)
-  eqs.add_equation(y_calc=1.1,
-                   grad_y_calc=flex.double((1, 2, 3)),
-                   y_obs=1,
-                   weight=1)
-  eqs.add_equation(y_calc=2.2,
-                   grad_y_calc=flex.double((2, 3, 1)),
-                   y_obs=2,
-                   weight=1)
-  eqs.add_equation(y_calc=3.3,
-                   grad_y_calc=flex.double((3, 1, 2)),
-                   y_obs=3,
-                   weight=1)
-  eqs.add_equation(y_calc=4.4,
-                   grad_y_calc=flex.double((1, 3, 2)),
-                   y_obs=4,
-                   weight=1)
-  eqs.finalise()
-  a, b = eqs.step_equations()
-  assert a.size() == 6
-  assert b.size() == 3
-  eqs.step_equations().solve()
-  assert eqs.step_equations().solved
 
+class polynomial_fit(normal_eqns.non_linear_ls_with_separable_scale_factor,
+                     normal_eqns.non_linear_ls_mixin):
+
+  noise = 1e-5
+  n_data = 10
+  x_0 = flex.double((0.5, 0.3, 0.2))
+
+  def __init__(self, **kwds):
+    super(polynomial_fit, self).__init__(n_parameters=3, normalised=False)
+    libtbx.adopt_optional_init_args(self, kwds)
+    self.t = t = flex.double_range(self.n_data)/self.n_data
+    noise = self.noise*flex.double([ (-1)**i for i in xrange(self.n_data) ])
+    self.yo = 2.*t**2.*(1-t) + noise
+    self.one = flex.double(self.n_data, 1)
+    self.t2 = t**2
+    self.t3 = t**3
+    self.restart()
+
+  def restart(self):
+    self.x = self.x_0.deep_copy()
+    self.old_x = None
+
+  def step_forward(self):
+    self.old_x = self.x.deep_copy()
+    self.x += self.step()
+
+  def step_backward(self):
+    assert self.old_x is not None
+    self.x, self.old_x = self.old_x, None
+
+  def parameter_vector_norm(self):
+    return self.x.norm()
+
+  def build_up(self, objective_only=False):
+    a, b, c = self.x
+    one, t, t2, t3 = self.one, self.t, self.t2, self.t3
+    yc = -t3 + a*t2 + b*t + c
+    grad_yc = (t2, t, one)
+    jacobian_yc = flex.double(flex.grid(self.n_data, self.n_parameters))
+    for j, der_r in enumerate(grad_yc):
+      jacobian_yc.matrix_paste_column_in_place(der_r, j)
+    self.add_equations(yc, jacobian_yc, self.yo, weights=None)
+    self.finalise()
+
+def exercise_non_linear_ls_with_separable_scale_factor():
+  test = polynomial_fit()
+  test.build_up()
+  # Reference values computed in tst_normal_equations.nb
+  eps = 5e-14
+  assert approx_equal(test.optimal_scale_factor(), 0.6148971786833856, eps)
+  assert approx_equal(test.objective(), 0.039642707534326034, eps)
+
+
+  assert not test.step_equations().solved
+  try:
+    test.step_equations().cholesky_factor_packed_u()
+    raise Exception_expected
+  except RuntimeError:
+    pass
+  try:
+    test.step_equations().solution()
+    raise Exception_expected
+  except RuntimeError:
+    pass
+
+  assert approx_equal(
+    list(test.step_equations().normal_matrix_packed_u()),
+    [ 0.371944193675858, 0.39066546997866547  , 0.10797294655500618,
+                           0.41859250354804045, 0.08077629438075473,
+                                                0.19767268057900367 ],
+    eps)
+  assert approx_equal(
+    list(test.step_equations().right_hand_side()),
+    [ 0.12149917297914861, 0.13803759252793774, -0.025190641142579157 ],
+    eps)
+
+  test.step_equations().solve()
+
+  assert test.step_equations().solved
+  try:
+    test.step_equations().normal_matrix_packed_u()
+    raise Exception_expected
+  except RuntimeError:
+    pass
+  try:
+    test.step_equations().right_hand_side()
+    raise Exception_expected
+  except RuntimeError:
+    pass
+
+  assert approx_equal(
+    list(test.step_equations().cholesky_factor_packed_u()),
+    [ 0.6098722765266986, 0.6405693208478925   ,  0.1770418999366983 ,
+                            0.09090351333425013, -0.3589664912436558 ,
+                                                  0.19357661121640218 ],
+    eps)
+  assert approx_equal(
+    list(test.step_equations().solution()),
+    [ 1.2878697604109028, -0.7727798877778043, -0.5151113342942297 ],
+    eps=1e-12)
 
 
 class exponential_fit(
@@ -70,12 +147,13 @@ class exponential_fit(
 
   """ Model M(x, t) = x3 e^{x1 t} + x4 e^{x2 t} """
 
+  n_data = 45
   arg_min = flex.double((-4, -5, 4, -4))
   x_0     = flex.double((-1, -2, 1, -1))
 
   def __init__(self):
     super(exponential_fit, self).__init__(n_parameters=4)
-    self.t = 0.02*flex.double_range(1, 46)
+    self.t = 0.02*flex.double_range(1, self.n_data + 1)
     self.y = flex.double((
       0.090542, 0.124569, 0.179367, 0.195654, 0.269707, 0.286027, 0.289892,
       0.317475, 0.308191, 0.336995, 0.348371, 0.321337, 0.299423, 0.338972,
@@ -85,12 +163,12 @@ class exponential_fit(
       0.091868, 0.128926, 0.119273, 0.115997, 0.105831, 0.075261, 0.068387,
       0.090823, 0.085205, 0.067203
       ))
+    assert len(self.y) == len(self.t)
     self.restart()
 
   def restart(self):
     self.x = self.x_0.deep_copy()
     self.old_x = None
-    self.old_objective = None
 
   def parameter_vector_norm(self):
     return self.x.norm()
@@ -110,7 +188,7 @@ class exponential_fit(
                 self.t*x4*exp_x2_t,
                 exp_x1_t,
                 exp_x2_t)
-      jacobian = flex.double(flex.grid(len(self.t), len(self.x)))
+      jacobian = flex.double(flex.grid(self.n_data, self.n_parameters))
       for j, der_r in enumerate(grad_r):
         jacobian.matrix_paste_column_in_place(der_r, j)
       self.add_equations(residuals, jacobian, weights=None)
@@ -147,8 +225,8 @@ def run():
   plot = '--plot' in sys.argv[1:]
   t = exponential_fit()
   exercise_levenberg_marquardt(t, plot)
-  exercise_basic_normal_equations()
-  exercise_normal_equations_separating_scale_factor()
+  exercise_linear_normal_equations()
+  exercise_non_linear_ls_with_separable_scale_factor()
   print 'OK'
 
 if __name__ == '__main__':
