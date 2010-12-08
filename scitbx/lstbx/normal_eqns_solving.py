e24ab3865d71625a2a8a0ed903b187afc462931e
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-12-08 00:17:51 +0000

    Refined normal equations solving framework (smtbx broken)

--- scitbx/lstbx/normal_eqns_solving.py
+++ scitbx/lstbx/normal_eqns_solving.py
@@ -32,6 +32,8 @@ class journaled_normal_eqns(object):
       self.journal.shifts_history = []
     else:
       self.journal.shifts_history = None
+    self.journal.shifts_norm_history = flex.double()
+    self.journal.parameter_vector_norm_history = flex.double()
     if hasattr(normal_eqns, "scale_factor"):
       self.journal.scale_factor_history = flex.double()
     else:
@@ -42,6 +44,8 @@ class journaled_normal_eqns(object):
 
   def build_up(self):
     self.actual.build_up()
+    self.journal.parameter_vector_norm_history.append(
+      self.actual.parameter_vector_norm)
     self.journal.objective_history.append(self.actual.objective)
     self.journal.gradient_norm_history.append(self.actual.gradient.norm_inf())
     if self.journal.gradient_history is not None:
@@ -51,97 +55,72 @@ class journaled_normal_eqns(object):
 
   def solve(self):
     self.actual.solve()
+    self.journal.shifts_norm_history.append(self.actual.shifts.norm())
     if self.journal.shifts_history is not None:
       self.journal.shifts_history.append(self.actual.shifts.deep_copy())
 
   def apply_shifts(self):
     self.actual.apply_shifts()
 
+  def solve_and_apply_shifts(self):
+    self.solve()
+    self.apply_shifts()
+
 
 class iterations(object):
+  """ Iterations to solve a L.S. minimisation problem.
+
+  It is assumed that the objective function is properly scaled to be
+  between 0 and 1 (or approximately so), so that this class can
+  meaningfully thresholds the norm of the gradients to watch for
+  convergence.
+
+  Use classic stopping criteria: c.f. e.g.
+  Methods for non-linear least-squares problems,
+  K. Madsen, H.B. Nielsen, O. Tingleff,
+  http://www2.imm.dtu.dk/pubdb/views/edoc_download.php/3215/pdf/imm3215.pdf
+  """
 
   track_shifts = False
   track_gradients = False
   track_all = False
   n_max_iterations = 100
-  norm_of_gradient = flex.double.norm_inf
+  gradient_threshold = None
+  shift_threshold = None
 
   def __init__(self, normal_eqns, **kwds):
-    """ normal_eqns, thereafter denoted as e, shall feature:
-          - e.build_up():
-              build the normal equations
-          - e.objective:
-              the value of the L.S. objective
-          - e.gradient:
-              the gradient of the objective wrt the independent parameters
-
-        It is assumed that the objective is properly scaled to be
-        between 0 and 1 (or approximately so), so that this class can
-        meaningfully thresholds the norm of the gradients to watch for
-        convergence.
-
-        An instance of this class accumulates the following information:
-        L.S. objective and gradient, and L.S. scale factor
-        if the normal equations feature one.
     """
-    self.normal_eqns = normal_eqns
+    """
     libtbx.adopt_optional_init_args(self, kwds)
-    self.objectives = flex.double()
-    self.gradients = []
-    self.gradient_norms = []
-    self.shifts = []
-    if hasattr(normal_eqns, "scale_factor"):
-      self.scale_factors = flex.double()
-    else:
-      self.scale_factors = None
+    if self.track_all: self.track_shifts = self.track_gradients = True
+    self.normal_eqns = journaled_normal_eqns(normal_eqns, self,
+                                             self.track_gradients,
+                                             self.track_shifts)
+    self.do()
 
-  class n_iterations(libtbx.property):
-    def fget(self): return len(self.objectives)
+  def has_gradient_converged_to_zero(self):
+    eps_1 = self.gradient_threshold
+    return eps_1 is not None and self.gradient_norm_history[-1] <= eps_1
 
-  def accumulate_normal_equation_properties(self):
-    self.objectives.append(self.normal_eqns.objective)
-    g = self.normal_eqns.gradient
-    self.gradient_norms.append(self.norm_of_gradient(g))
-    if self.scale_factors is not None:
-      self.scale_factors.append(self.normal_eqns.scale_factor)
-    if self.track_all or self.track_gradients:
-      self.gradients.append(g)
+  def had_too_small_a_step(self):
+    eps_2 = self.shift_threshold
+    if eps_2 is None: return False
+    x = self.parameter_vector_norm_history[-1]
+    h = self.shifts_norm_history[-1]
+    return h <= eps_2*(x + eps_2)
 
-  def accumulate_solution_properties(self):
-    if self.track_all or self.track_shifts:
-      self.shifts.append(self.normal_eqns.shifts.deep_copy())
-
-  def accumulate(self):
-    self.accumulate_normal_equation_properties()
-    self.accumulate_solution_properties()
-
-  def do(self,
-         gradient_threshold=None,
-         shift_threshold=None,
-         n_iterations = None):
-    """ Iterate, using classic stopping criteria (c.f. e.g.
-        Methods for non-linear least-squares problems,
-        K. Madsen, H.B. Nielsen, O. Tingleff,
-        http://www2.imm.dtu.dk/pubdb/views/edoc_download.php/3215/pdf/imm3215.pdf
-    """
-    eps_1 = gradient_threshold
-    eps_2 = shift_threshold
-    for i in xrange(n_iterations or self.n_max_iterations):
-      self.next()
-      h = self.normal_eqns.shifts
-      if (eps_1 is not None and self.gradient_norms[-1] <= eps_1):
-        break
-      if eps_2 is not None:
-        lxl = self.normal_eqns.parameter_vector_norm
-        if h.norm() <= eps_2*(lxl + eps_2):
-          break
+  def do(self):
+    raise NotImplementedError
 
 
 class naive_iterations(iterations):
 
-  def next(self):
-    self.normal_eqns.build_up()
-    self.accumulate_normal_equation_properties()
-    self.normal_eqns.solve_and_apply_shifts()
-    self.accumulate_solution_properties()
-    return self
+  def do(self):
+    self.n_iterations = 0
+    while self.n_iterations <= self.n_max_iterations:
+      self.normal_eqns.build_up()
+      if self.has_gradient_converged_to_zero(): break
+      self.normal_eqns.solve()
+      if self.had_too_small_a_step(): break
+      self.normal_eqns.apply_shifts()
+      self.n_iterations += 1
