b5f3790f1f83b8c5ed623b27d1f44e66b015e172
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2010-12-09 03:08:57 +0000

    iotbx/scalepack: correct_rhombohedral_setting_if_necessary=not have_r_with_axes_info() to be safer

--- iotbx/scalepack/crystal_symmetry_from_hkl.py
+++ iotbx/scalepack/crystal_symmetry_from_hkl.py
@@ -3,11 +3,7 @@ from iotbx.scalepack import no_merge_original_index
 from cctbx import crystal
 
 def extract_from_merge(file):
-  scalepack_file = merge.reader(file, header_only=True)
-  return crystal.symmetry(
-    unit_cell=scalepack_file.unit_cell,
-    space_group_info=scalepack_file.space_group_info,
-    correct_rhombohedral_setting_if_necessary=True)
+  return merge.reader(file, header_only=True).crystal_symmetry()
 
 def extract_from_no_merge_original_index(file_name):
   scalepack_file = no_merge_original_index.reader(file_name, header_only=True)
