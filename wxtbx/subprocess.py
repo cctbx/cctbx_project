e2755dedf92f1f7ca4a15a48946782963aed7ffc
Nathaniel Echols <nathaniel.echols@gmail.com>
2010-12-11 19:08:00 +0000

    handling of long-running processes from gui (abstraction of phenix gui modules), with simple 'busy' dialog for minimally interactive tasks

---
+++ wxtbx/subprocess.py
@@ -0,0 +1,273 @@
+
+from wx.lib.agw import pyprogress
+from libtbx import thread_utils
+from libtbx.utils import Sorry
+import wx
+
+JOB_START_ID = wx.NewId()
+LOG_UPDATE_ID = wx.NewId()
+CALLBACK_ID = wx.NewId()
+JOB_EXCEPTION_ID = wx.NewId()
+JOB_KILLED_ID = wx.NewId()
+JOB_COMPLETE_ID = wx.NewId()
+
+class SubprocessEvent (wx.PyEvent) :
+  event_id = None
+
+  def __init__ (self, data, **kwds) :
+    self.data = data
+    self.__dict__.update(kwds)
+    wx.PyEvent.__init__(self)
+    self.SetEventType(self.event_id)
+
+class JobStartEvent (SubprocessEvent) :
+  event_id = JOB_START_ID
+
+class LogEvent (SubprocessEvent) :
+  event_id = LOG_UPDATE_ID
+
+class JobExceptionEvent (SubprocessEvent) :
+  event_id = JOB_EXCEPTION_ID
+
+class JobKilledEvent (SubprocessEvent) :
+  event_id = JOB_KILLED_ID
+
+class JobCompleteEvent (SubprocessEvent) :
+  event_id = JOB_COMPLETE_ID
+
+class CallbackEvent (SubprocessEvent) :
+  event_id = CALLBACK_ID
+
+def setup_stdout_logging_event (window, OnPrint) :
+  window.Connect(-1, -1, LOG_UPDATE_ID, OnPrint)
+
+def setup_process_gui_events (
+    window,
+    OnStart=None,
+    OnPrint=None,
+    OnUpdate=None,
+    OnExcept=None,
+    OnAbort=None,
+    OnComplete=None) :
+  if OnStart is not None :
+    assert hasattr(OnStart, "__call__")
+    window.Connect(-1, -1, JOB_START_ID, OnStart)
+  if OnPrint is not None :
+    assert hasattr(OnPrint, "__call__")
+    window.Connect(-1, -1, LOG_UPDATE_ID, OnPrint)
+  if OnUpdate is not None :
+    assert hasattr(OnUpdate, "__call__")
+    window.Connect(-1, -1, CALLBACK_ID, OnUpdate)
+  if OnExcept is not None :
+    assert hasattr(OnExcept, "__call__")
+    window.Connect(-1, -1, JOB_EXCEPTION_ID, OnExcept)
+  if OnAbort is not None :
+    assert hasattr(OnAbort, "__call__")
+    window.Connect(-1, -1, JOB_KILLED_ID, OnAbort)
+  if OnComplete is not None :
+    assert hasattr(OnComplete, "__call__")
+    window.Connect(-1, -1, JOB_COMPLETE_ID, OnComplete)
+
+class event_agent (object) :
+  def __init__ (self, window, **kwds) :
+    self.window = window
+    self.__dict__.update(kwds)
+
+  def get_kwds (self) :
+    return {}
+
+  def callback_start (self, data) :
+    kwds = self.get_kwds()
+    event = JobStartEvent(data, **kwds)
+    wx.PostEvent(self.window, event)
+
+  def callback_stdout (self, data) :
+    kwds = self.get_kwds()
+    event = LogEvent(data, **kwds)
+    wx.PostEvent(self.window, event)
+
+  def callback_error (self, error, traceback_info) :
+    kwds = self.get_kwds()
+    event = JobExceptionEvent((error, traceback_info), **kwds)
+    wx.PostEvent(self.window, event)
+
+  def callback_abort (self) :
+    kwds = self.get_kwds()
+    event = JobKilledEvent(None, **kwds)
+    wx.PostEvent(self.window, event)
+
+  def callback_final (self, result) :
+    kwds = self.get_kwds()
+    event = JobCompleteEvent(result, **kwds)
+    wx.PostEvent(self.window, event)
+
+  def callback_other (self, data) :
+    kwds = self.get_kwds()
+    event = CallbackEvent(data, **kwds)
+    wx.PostEvent(self.window, event)
+
+# this just adds event posting callbacks to the original class
+class process_with_gui_callbacks (thread_utils.process_with_callbacks) :
+  def __init__ (self, proxy, target, args=(), kwargs={}, buffer_stdout=True) :
+    thread_utils.process_with_callbacks.__init__(self,
+      target = target,
+      args=args,
+      kwargs=kwargs,
+      callback_stdout = proxy.callback_stdout,
+      callback_final  = proxy.callback_final,
+      callback_err    = proxy.callback_error,
+      callback_abort  = proxy.callback_abort,
+      callback_other  = proxy.callback_other,
+      buffer_stdout   = buffer_stdout)
+
+  def set_job (self, job) :
+    pass
+
+class simple_gui_process (process_with_gui_callbacks) :
+  def __init__ (self, window, target, args=(), kwargs={}) :
+    proxy = event_agent(window, None, None)
+    process_with_gui_callbacks.__init__(self,
+      proxy=proxy,
+      target=target,
+      args=args,
+      kwargs=kwargs,
+      buffer_stdout=True)
+
+class ThreadProgressDialog (pyprogress.PyProgress) :
+  def __init__ (self, parent, title, message) :
+    pyprogress.PyProgress.__init__(self, parent, -1, title, message,
+      style=wx.PD_ELAPSED_TIME|wx.PD_APP_MODAL)
+    self.SetGaugeProportion(0.15)
+    self.SetGaugeSteps(50)
+    self.SetGaugeBackground(wx.Colour(235, 235, 235))
+    self.SetFirstGradientColour(wx.Colour(235,235,235))
+    self.SetSecondGradientColour(wx.Colour(120, 200, 255))
+
+def run_function_as_thread_in_dialog (thread_function, title, message) :
+  dlg = ThreadProgressDialog(None, title, message)
+  t = thread_utils.simple_task_thread(thread_function, dlg)
+  t.start()
+  while True :
+    if t.is_complete() or t.exception_raised() :
+      #dlg.Destroy()
+      dlg.Hide()
+      break
+    else :
+      dlg.UpdatePulse()
+    wx.MilliSleep(30)
+  dlg.Destroy()
+  wx.SafeYield()
+  if t.exception_raised() :
+    raise RuntimeError("An exception occurred while running this process: %s" %
+      t.get_error())
+  return t.return_value
+
+# TODO
+class ProcessDialog (wx.Dialog) :
+  def __init__ (self, parent, message, caption, callback=None) :
+    wx.Dialog.__init__(self,
+      parent=parent,
+      title=caption,
+      style=wx.RAISED_BORDER|wx.CAPTION)
+    self.callback = callback
+    self.process = None
+    szr = wx.BoxSizer(wx.VERTICAL)
+    self.SetSizer(szr)
+    szr2 = wx.BoxSizer(wx.VERTICAL)
+    szr.Add(szr2, 1, wx.ALL, 5)
+    msg_txt = wx.StaticText(self, -1, message)
+    msg_txt.Wrap(400)
+    szr2.Add(msg_txt, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5)
+    self.gauge = wx.Gauge(parent=self)
+    self.gauge.SetRange(100)
+    szr2.Add(self.gauge, 1, wx.ALL|wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL, 5)
+    abort_btn = wx.Button(parent=self,
+      label="Abort")
+    self.Bind(wx.EVT_BUTTON, self.OnAbort, abort_btn)
+    szr2.Add(abort_btn, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5)
+    szr.Fit(self)
+    self.Centre(wx.BOTH)
+
+  def run (self, process) :
+    self.process = process
+    self.process.start()
+    self.gauge.Pulse()
+    return self.ShowModal()
+
+  def OnAbort (self, event) :
+    self.process.abort()
+    self.EndModal(wx.ID_CANCEL)
+
+  def OnError (self, event) :
+    try :
+      if isinstance(event.data, Exception) :
+        raise event.data
+      elif isinstance(event.data, tuple) :
+        exception, traceback = event.data
+        print exception
+        print traceback
+        raise RuntimeError("Original error: %s" % str(exception))
+      else :
+        raise Sorry("error in child process: %s" % str(event.data))
+    finally :
+      self.EndModal(wx.ID_CANCEL)
+
+  def OnComplete (self, event) :
+    try :
+      if (self.callback is not None) :
+        self.callback(event.data)
+    finally :
+      self._result = event.data
+      self.EndModal(wx.ID_OK)
+
+  def get_result (self) :
+    return getattr(self, "_result", None)
+
+def run_function_as_process_in_dialog (
+    parent,
+    thread_function,
+    title,
+    message,
+    callback=None) :
+  dlg = ProcessDialog(
+    parent=parent,
+    message=message,
+    caption=title,
+    callback=callback)
+  setup_process_gui_events(
+    window=dlg,
+    OnExcept=dlg.OnError,
+    OnComplete=dlg.OnComplete)
+  cb = event_agent(dlg)
+  p = thread_utils.process_with_callbacks(
+    target=thread_function,
+    callback_final=cb.callback_final,
+    callback_err=cb.callback_error,
+    buffer_stdout=True)
+  result = None
+  if (dlg.run(p) == wx.ID_OK) :
+    result = dlg.get_result()
+  wx.CallAfter(dlg.Destroy)
+  return result
+
+if (__name__ == "__main__") :
+  from libtbx.test_utils import approx_equal
+  import math
+  def test_function (*args, **kwds) :
+    n = 0
+    for i in range(25000) :
+      x = math.sqrt(i)
+      print x
+      n += x
+    return n
+  app = wx.App(0)
+  result = run_function_as_process_in_dialog(
+    parent=None,
+    thread_function=test_function,
+    title="Test subprocess",
+    message="Running test function as separate process...",
+    callback=None)
+  if (result is not None) :
+    print result
+    assert approx_equal(result, 2635152.11891, eps=0.0001)
+  wx.Yield()
