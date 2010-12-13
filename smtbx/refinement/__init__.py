35a8d1e3a9ac08479e1289a3e24887de2d7a527d
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-12-13 12:05:43 +0000

    new smtbx.refinement.model and its construction from shelx files

---
+++ smtbx/refinement/__init__.py
@@ -0,0 +1,30 @@
+from smtbx.refinement import constraints, least_squares
+import smtbx.utils
+
+class model(object):
+
+  def from_shelx(self, *args, **kwds):
+    from iotbx.shelx import smtbx_refinement_model_from as _
+    return _(self, *args, **kwds)
+  from_shelx = classmethod(from_shelx)
+
+  def __init__(self, fo_sq, xray_structure,
+               constraints, restraints_manager, weighting_scheme):
+    self.fo_sq = fo_sq
+    self.xray_structure = xray_structure
+    self.constraints = constraints
+    self.restraints_manager = restraints_manager
+    self.weighting_scheme = weighting_scheme
+    self.connectivity_table = smtbx.utils.connectivity_table(
+      self.xray_structure)
+
+  def least_squares(self):
+    reparametrisation = constraints.reparametrisation(
+      self.xray_structure,
+      self.constraints,
+      self.connectivity_table)
+    return least_squares.normal_equations(
+      self.fo_sq,
+      reparametrisation,
+      restraints_manager=self.restraints_manager,
+      weighting_scheme=self.weighting_scheme)
