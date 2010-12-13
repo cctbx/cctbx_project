eee814d7ee1bf8a9a6b69da3e2d2c788f397cb3d
Luc J. Bourhis <luc_j_bourhis@mac.com>
2010-12-13 12:05:43 +0000

    new smtbx.refinement.model and its construction from shelx files

--- iotbx/shelx/__init__.py
+++ iotbx/shelx/__init__.py
@@ -14,3 +14,51 @@ def cctbx_xray_structure_from(cls, file=None, filename=None,
   stream = atom_parser(stream.filtered_commands(), builder)
   stream.parse()
   return builder.structure
+
+def smtbx_refinement_model_from(cls, ins_or_res=None, hkl=None):
+  import os
+  from iotbx.reflection_file_reader import any_reflection_file
+  from iotbx.builders \
+       import weighted_constrained_restrained_crystal_structure_builder
+  import smtbx.refinement.constraints.factory
+  import smtbx.refinement.weighting_schemes
+
+  assert ins_or_res is not None or hkl is not None
+
+  if ins_or_res is None:
+    root, _ = os.path.splitext(hkl)
+    ins = "%s.ins" % root
+    res = "%s.res" % root
+    ins_exists = os.path.isfile(ins)
+    res_exists = os.path.isfile(res)
+    assert ins_exists or res_exists
+    if res_exists: ins_or_res = res
+    elif ins_exists: ins_or_res = ins
+  elif hkl is None:
+    root, _ = os.path.splitext(ins_or_res)
+    hkl = "%.hkl" % root
+    assert os.path.isfile(hkl)
+
+  builder = weighted_constrained_restrained_crystal_structure_builder(
+    constraint_factory=smtbx.refinement.constraints.factory,
+    weighting_scheme_factory=smtbx.refinement.weighting_schemes)
+  stream = command_stream(filename=ins_or_res)
+  stream = crystal_symmetry_parser(stream, builder)
+  stream = afix_parser(stream.filtered_commands(), builder)
+  stream = atom_parser(stream.filtered_commands(), builder)
+  stream = restraint_parser(stream.filtered_commands(), builder)
+  stream = instruction_parser(stream.filtered_commands(), builder)
+  stream.parse()
+
+  hklf = stream.instructions['hklf']
+  assert hklf['n'] == 4
+  assert 'matrix' not in hklf
+  fo_sq = any_reflection_file("%s=hklf4" % hkl)\
+         .as_miller_arrays(crystal_symmetry=builder.structure)[0]\
+         .merge_equivalents().array()
+
+  return cls(fo_sq,
+             builder.structure,
+             builder.constraints,
+             builder.restraints_manager,
+             builder.weighting_scheme)
