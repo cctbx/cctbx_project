30d0a2b33566d6f32b88e16c107423dd26f4553f
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2010-12-20 08:49:34 +0000

    cctbx/examples/fft_map_electron_density_around_atom.py: matplotlib plots

--- cctbx/examples/fft_map_electron_density_around_atom.py
+++ cctbx/examples/fft_map_electron_density_around_atom.py
@@ -2,13 +2,19 @@ from __future__ import division
 from cctbx import xray
 from cctbx import miller
 from cctbx import crystal
-from cctbx.array_family import flex
 from cctbx import maptbx
+from cctbx.adptbx import u_as_b
+from cctbx.array_family import flex
 from scitbx import matrix
 from libtbx.math_utils import ifloor
 from libtbx.test_utils import approx_equal
 
-def rho_stats(xray_structure, d_min, resolution_factor, electron_sum_radius):
+def rho_stats(
+      xray_structure,
+      d_min,
+      resolution_factor,
+      electron_sum_radius,
+      zero_out_f000):
   n_real = []
   n_half_plus = []
   n_half_minus = []
@@ -39,16 +45,15 @@ def rho_stats(xray_structure, d_min, resolution_factor, electron_sum_radius):
     xray_structure=xray_structure,
     algorithm="direct",
     cos_sin_table=False).f_calc()
+  if (zero_out_f000):
+    f_calc.data()[0] = 0j
   #
   unit_cell_volume = xray_structure.unit_cell().volume()
   voxel_volume = unit_cell_volume / n_real_product
   number_of_miller_indices = []
   rho_max = []
   electron_sums_around_atoms = []
-  plot_file = open("rho_r=%3.1f_f=%4.2f_u=%4.2f.xy" % (
-    d_min,
-    resolution_factor,
-    xray_structure.scatterers()[0].u_iso), "w")
+  densities_along_x = []
   for f in [f_calc, f_calc.resolution_filter(d_min=d_min)]:
     assert f.indices()[0] == (0,0,0)
     number_of_miller_indices.append(f.indices().size())
@@ -73,45 +78,119 @@ def rho_stats(xray_structure, d_min, resolution_factor, electron_sum_radius):
     a = xray_structure.unit_cell().parameters()[0]
     nx = n_real[0]
     nxh = nx//2
+    dax = []
     for ix in xrange(-nxh,nxh+1):
-      print >> plot_file, a*ix/nx, rho[(ix%nx,0,0)]
-    print >> plot_file, "&"
+      dax.append((a*ix/nx, rho[(ix%nx,0,0)]))
+    densities_along_x.append(dax)
   #
-  print "%3.1f %4.2f %-12s %5d %5d | %5.2f %5.2f | %5.2f %5.2f" % (
-    d_min,
-    resolution_factor,
-    n_real,
-    number_of_miller_indices[0],
-    number_of_miller_indices[1],
-    electron_sums_around_atoms[0],
-    electron_sums_around_atoms[1],
-    rho_max[0],
-    rho_max[1])
+  print \
+    "%3.1f %4.2f %-12s %5d %5d | %6.3f %6.3f | %6.3f %6.3f | %4.2f %5.1f" % (
+      d_min,
+      resolution_factor,
+      n_real,
+      number_of_miller_indices[0],
+      number_of_miller_indices[1],
+      electron_sums_around_atoms[0],
+      electron_sums_around_atoms[1],
+      rho_max[0],
+      rho_max[1],
+      f_calc.data()[0].real,
+      u_as_b(xray_structure.scatterers()[0].u_iso))
+  #
+  return densities_along_x
 
-def loop_res_fac(u_iso):
-  electron_sum_radius = 2
-  xray_structure = xray.structure(
+table_header = """\
+                          hkl     |    electrons  |     rho max
+res fac  grid           all Ewald |    all  Ewald |    all  Ewald | F000  Biso\
+"""
+
+def build_xray_structure_with_carbon_at_origin(a, b):
+  result = xray.structure(
     crystal_symmetry=crystal.symmetry(
-      unit_cell=(10,10,10,90,90,90),
+      unit_cell=(a,a,a,90,90,90),
       space_group_symbol="P1"))
-  xray_structure.add_scatterer(xray.scatterer(
-    scattering_type="C", site=(0,0,0), u=u_iso))
+  result.add_scatterer(xray.scatterer(
+    scattering_type="C", site=(0,0,0), b=b))
+  return result
+
+def loop_res_fac(b, electron_sum_radius=2, zero_out_f000=False):
+  xray_structure = build_xray_structure_with_carbon_at_origin(a=10, b=b)
   xray_structure.show_scatterers()
-  print """\
-                          hkl     |   electrons |    rho max
-res fac  grid           all Ewald |   all Ewald |   all Ewald"""
+  print table_header
   for d_min in [4, 3, 2, 1]:
     for resolution_factor in [1/2, 1/3, 1/4]:
-      rho_stats(
+      densities_along_x = rho_stats(
         xray_structure=xray_structure,
         d_min=d_min,
         resolution_factor=resolution_factor,
-        electron_sum_radius=electron_sum_radius)
+        electron_sum_radius=electron_sum_radius,
+        zero_out_f000=False)
 
-def run():
-  for u_iso in [0, 0.05, 0.25]:
-    loop_res_fac(u_iso=u_iso)
+def text_only():
+  for b in [0, 5, 20]:
+    loop_res_fac(b=b)
+
+def matplotlib_plots(dry_run):
+  if (not dry_run):
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+  print table_header
+  a = 5
+  gr = 3
+  gc = 4
+  def p(i, r, f, b, z, ym):
+    xs = build_xray_structure_with_carbon_at_origin(a=a, b=b)
+    densities_along_x = rho_stats(
+      xray_structure=xs,
+      d_min=r,
+      resolution_factor=f,
+      electron_sum_radius=2,
+      zero_out_f000=bool(z))
+    if (dry_run):
+      return
+    ax = fig.add_subplot(gr, gc, i)
+    for dax,code in reversed(zip(densities_along_x, ["b-", "r-"])):
+      x_values = [x for x,y in dax]
+      y_values = [y for x,y in dax]
+      ax.plot(x_values, y_values, code)
+    ax.axis([-a/2, a/2, -1, ym])
+    if (i > 8):
+      for label in ax.get_xticklabels():
+        label.set_fontsize(8)
+    else:
+      for label in ax.get_xticklabels():
+        label.set_visible(False)
+    if (i % 4 == 1):
+      for label in ax.get_yticklabels():
+        label.set_fontsize(8)
+    else:
+      for label in ax.get_yticklabels():
+        label.set_visible(False)
+    j = len(y_values)//2
+    y0 = y_values[j]
+    if (y0 > ym):
+      ax.text(0.5, ym*0.8, "h=%.2f" % y0, fontsize=8, color="b")
+  i = 1
+  for r in [1, 2, 3]:
+    for f in [1/3, 1/4]:
+      p(i, r, f, 0, 0, 10)
+      i += 1
+    p(i, r, 1/4, 20, 0, 10)
+    i += 1
+    p(i, r, 1/4, 20, 1, 10)
+    i += 1
+  if (not dry_run):
+    plt.show()
+
+def run(args):
+  assert args in [[], ["plots"]]
+  dry_run = False
+  if (len(args) == 0):
+    text_only()
+    dry_run = True
+  matplotlib_plots(dry_run=dry_run)
   print "OK"
 
 if (__name__ == "__main__"):
-  run()
+  import sys
+  run(args=sys.argv[1:])
