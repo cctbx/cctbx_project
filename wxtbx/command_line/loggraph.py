aaf98203e960b6cde191a20e46686f27859b0339
Nathaniel Echols <nathaniel.echols@gmail.com>
2011-01-12 23:05:07 +0000

    loggraph utility

---
+++ wxtbx/command_line/loggraph.py
@@ -0,0 +1,19 @@
+# LIBTBX_PRE_DISPATCHER_INCLUDE_SH PHENIX_GUI_ENVIRONMENT=1
+# LIBTBX_PRE_DISPATCHER_INCLUDE_SH export PHENIX_GUI_ENVIRONMENT
+
+from wxtbx import plots
+import wx
+import sys
+
+def run (args) :
+  logfile = args[0]
+  app = wx.App(0)
+  frame = plots.loggraph(parent=None,
+    title="Loggraph test",
+    tables=None,
+    processed_lines=open(logfile).readlines())
+  frame.Show()
+  app.MainLoop()
+
+if __name__ == "__main__" :
+  run(sys.argv[1:])
