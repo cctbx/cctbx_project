74ed96d4e31b91b0b3bf0d6cb93e16dd60d8adbe
Nathaniel Echols <nathaniel.echols@gmail.com>
2011-01-27 17:53:01 +0000

    consolidated and moved ramachandran restraints to new mmtbx.geometry_restraints framework

--- mmtbx/geometry_restraints/ramachandran.h
+++ mmtbx/geometry_restraints/ramachandran.h
@@ -17,6 +17,8 @@
 
 namespace mmtbx { namespace ramachandran {
   namespace af = scitbx::af;
+  using cctbx::geometry_restraints::dihedral;
+  namespace gr = cctbx::geometry_restraints;
 
   double convert_angle (double theta) {
     if (theta > 180) {
@@ -27,6 +29,7 @@ namespace mmtbx { namespace ramachandran {
     return theta;
   }
 
+  // COOT-like restraints (rama_potential=emsley)
   class lookup_table
   {
     public:
@@ -135,7 +138,6 @@ namespace mmtbx { namespace ramachandran {
       {
         MMTBX_ASSERT(gradient_array.size() == sites_cart.size());
         MMTBX_ASSERT(epsilon > 0.0);
-        using cctbx::geometry_restraints::dihedral;
         af::tiny<scitbx::vec3<double>, 4> phi_sites;
         af::tiny<scitbx::vec3<double>, 4> psi_sites;
         for (unsigned i = 0; i < 4; i++) {
@@ -177,7 +179,6 @@ namespace mmtbx { namespace ramachandran {
       {
         MMTBX_ASSERT(gradient_array.size() == sites_cart.size());
         MMTBX_ASSERT(epsilon > 0.0);
-        using cctbx::geometry_restraints::dihedral;
         af::tiny<scitbx::vec3<double>, 4> phi_sites;
         af::tiny<scitbx::vec3<double>, 4> psi_sites;
         for (unsigned k = 0; k < 4; k++) {
@@ -236,40 +237,92 @@ namespace mmtbx { namespace ramachandran {
       }
   };
 
-  void init_module()
+  // QUANTA-like harmonic restraints (rama_potential=oldfield)
+  class target_and_gradients
   {
-    using namespace boost::python;
-    class_<lookup_table>("lookup_table", no_init)
-      .def(init<af::const_ref< double >,
-                int,
-                double>((
-        arg("values"),
-        arg("n_angles"),
-        arg("scale_allowed")=1.0)))
-      .def("get_score", &lookup_table::get_score, (
-        arg("phi"),
-        arg("psi")))
-      .def("get_energy", &lookup_table::get_energy, (
-        arg("phi"),
-        arg("psi")))
-      .def("compute_gradients", &lookup_table::compute_gradients, (
-        arg("gradient_array"),
-        arg("sites_cart"),
-        arg("i_seqs"),
-        arg("weight")=1.0,
-        arg("epsilon")=0.1))
-      .def("compute_gradients_finite_differences",
-        &lookup_table::compute_gradients_finite_differences, (
-        arg("gradient_array"),
-        arg("sites_cart"),
-        arg("i_seqs"),
-        arg("weight")=1.0,
-        arg("epsilon")=0.01));
-  }
+    public:
+      target_and_gradients (
+        af::ref<scitbx::vec3<double> > const& gradient_array,
+        double const& phi_target,
+        double const& psi_target,
+        double const& weight,
+        af::const_ref<scitbx::vec3<double> > const& rama_table,
+        af::const_ref<scitbx::vec3<double> > const& sites_cart,
+        af::tiny<unsigned, 5> const& i_seqs)
+      {
+        MMTBX_ASSERT(gradient_array.size() == sites_cart.size());
+        gradients_.resize(sites_cart.size(), scitbx::vec3<double>(0,0,0));
+        af::tiny<scitbx::vec3<double>, 4> phi_sites;
+        af::tiny<scitbx::vec3<double>, 4> psi_sites;
+        for (unsigned i = 0; i < 4; i++) {
+          phi_sites[i] = sites_cart[i_seqs[i]];
+          psi_sites[i] = sites_cart[i_seqs[i+1]];
+        }
+        dihedral phi(phi_sites, phi_target, weight);
+        dihedral psi(psi_sites, psi_target, weight);
+        target_ = phi.residual()+psi.residual();
+        af::tiny<scitbx::vec3<double>, 4> d_phi_d_xyz = phi.gradients();
+        af::tiny<scitbx::vec3<double>, 4> d_psi_d_xyz = psi.gradients();
+        for (unsigned k = 0; k < 5; k++) {
+          std::size_t i_seq = i_seqs[k];
+          if(k < 4) gradient_array[i_seq] += d_phi_d_xyz[k] ;
+          if(k > 0) gradient_array[i_seq] += d_psi_d_xyz[k-1] ;
+        }
+      }
+
+      double target() { return target_; }
+
+      af::shared<scitbx::vec3<double> > gradients() { return gradients_; }
+
+    private :
+      double target_;
+      af::shared<scitbx::vec3<double> > gradients_;
+  };
+
+  template <typename FloatType>
+  af::tiny<FloatType, 3>
+    target_phi_psi(af::const_ref<scitbx::vec3<double> > const& rama_table,
+                   af::const_ref<scitbx::vec3<double> > const& sites_cart,
+                   af::tiny<unsigned, 5> const& i_seqs)
+  {
+    af::tiny<scitbx::vec3<double>, 4> phi_sites;
+    af::tiny<scitbx::vec3<double>, 4> psi_sites;
+    for (unsigned i = 0; i < 4; i++) {
+      phi_sites[i] = sites_cart[i_seqs[i]];
+      psi_sites[i] = sites_cart[i_seqs[i+1]];
+    }
+    dihedral phi1(phi_sites, 0, 1.0);
+    dihedral psi1(psi_sites, 0, 1.0);
+    double phi_deg = phi1.angle_model;
+    double psi_deg = psi1.angle_model;
+    double phi_t=phi_deg;
+    double psi_t=psi_deg;
+    double dist_to_current = 1.e+9;
+    double score_current = 0;
+    af::shared<double> distances;
+    distances.resize(rama_table.size(), 0);
+    for(int i=0; i<rama_table.size(); i++) {
+      scitbx::vec3<double> point = rama_table[i];
+      double d1 = gr::angle_delta_deg(point[0],phi_deg);
+      double d2 = gr::angle_delta_deg(point[1],psi_deg);
+      double d = std::sqrt(d1 * d1 + d2 * d2);
+      distances[i] = d;
+      if(d<dist_to_current) {
+        dist_to_current = d;
+        score_current = point[2];
+      }
+    }
+    double dist_to_allowed = 1.e+9;
+    for(int i=0; i<rama_table.size(); i++) {
+      scitbx::vec3<double> point = rama_table[i];
+      double d = distances[i];
+      if(point[2] >= score_current && d<dist_to_allowed) {
+        dist_to_allowed = d;
+        phi_t = point[0];
+        psi_t = point[1];
+      }
+    }
+    return af::tiny<double, 3> (phi_t,psi_t,dist_to_allowed) ;
+  };
 
 }} // namespace mmtbx::ramachandran
-
-BOOST_PYTHON_MODULE(mmtbx_ramachandran_ext)
-{
-  mmtbx::ramachandran::init_module();
-}
