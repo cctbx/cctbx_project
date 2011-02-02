8e06d80ece2679dc3ce55a98bf32f6deb0554ad3
Richard Gildea <richard.gildea@diamond.ac.uk>
2011-02-02 19:48:33 +0000

    iotbx/cif/tests/crawler.py: keep track of number of parsing errors

--- iotbx/cif/tests/crawler.py
+++ iotbx/cif/tests/crawler.py
@@ -5,39 +5,50 @@ from libtbx.utils import time_log
 import libtbx.load_env
 from iotbx.option_parser import option_parser
 from iotbx import cif
-from cctbx import miller, xray
 
-def crawl(directory, file_ext,
-          build_miller_arrays=False,
-          build_xray_structure=False):
-  timer = time_log("parsing")
-  error_count = 0
-  for root, dirs, files in os.walk(directory):
-    cif_g = glob.glob(os.path.join(root, "*.%s" %file_ext))
-    files_to_read = cif_g
-    for path in files_to_read:
-      timer.start()
-      try:
-        run_once(path, build_miller_arrays=build_miller_arrays,
-                 build_xray_structure=build_xray_structure)
-      except Exception, e:
-        print "error reading %s" %path
-        print e
-        error_count += 1
-      timer.stop()
-  print
-  print "%i files read (%i with errors)" %(timer.n, error_count)
-  print timer.legend
-  print timer.report()
-  sys.stdout.flush()
+class crawl(object):
+  def __init__(self, directory, file_ext,
+               build_miller_arrays=False,
+               build_xray_structure=False):
+    timer = time_log("parsing")
+    error_count = 0
+    self.parsing_error_count = 0
+    for root, dirs, files in os.walk(directory):
+      cif_g = glob.glob(os.path.join(root, "*.%s" %file_ext))
+      files_to_read = cif_g
+      for path in files_to_read:
+        timer.start()
+        try:
+          reader = self.run_once(path, build_miller_arrays=build_miller_arrays,
+                            build_xray_structure=build_xray_structure)
+
+        except Exception, e:
+          print "error reading %s" %path
+          print e
+          error_count += 1
+        timer.stop()
+    print
+    print "%i files read (%i with building errors and %i with parsing errors)" %(
+      timer.n, error_count, self.parsing_error_count)
+    print timer.legend
+    print timer.report()
+    sys.stdout.flush()
+
+  def run_once(self, file_path, build_miller_arrays=False,
+               build_xray_structure=False):
+    reader = cif.reader(file_path=file_path, max_errors=10)
+    if reader.error_count(): self.parsing_error_count += 1
+    if build_xray_structure:
+      xs = reader.build_crystal_structure()
+    elif build_miller_arrays:
+      ma = reader.build_miller_arrays()
 
 def run_once(file_path, build_miller_arrays=False, build_xray_structure=False):
+  reader = cif.reader(file_path=file_path, max_errors=10)
   if build_xray_structure:
-    xs = xray.structure.from_cif(file_path=file_path)
+    xs = reader.build_crystal_structure()
   elif build_miller_arrays:
-    ma = miller.array.from_cif(file_path=file_path)
-  else:
-    cm = cif.reader(file_path=file_path, max_errors=10).model()
+    ma = reader.build_miller_arrays()
 
 def run(args, out=sys.stdout):
   assert len(args) > 0
