de66d692bcbb7c7f57402c13383fa240bf49e846
Nathaniel Echols <nathaniel.echols@gmail.com>
2011-02-08 18:55:43 +0000

    use template for catmull-rom spline function, now accepts either vec2_double or vec3_double

--- scitbx/math/boost_python/interpolation.cpp
+++ scitbx/math/boost_python/interpolation.cpp
@@ -3,24 +3,25 @@
 #include <boost/python/args.hpp>
 
 #include <scitbx/math/interpolation.h>
+#include <scitbx/vec3.h>
+#include <scitbx/vec2.h>
 
 namespace scitbx { namespace math {
 
 namespace {
 
-  struct interpolation_wrappers
+  template <typename PointType>
+  void wrap_splines()
   {
-    static void
-    wrap()
-    {
-      using namespace boost::python;
-      def("interpolate_catmull_rom_spline", interpolate_catmull_rom_spline, (
-        arg("p0"),
-        arg("p1"),
-        arg("p2"),
-        arg("p3")));
-    }
-  };
+    using namespace boost::python;
+    def("interpolate_catmull_rom_spline",
+      interpolate_catmull_rom_spline<PointType>, (
+      arg("p0"),
+      arg("p1"),
+      arg("p2"),
+      arg("p3"),
+      arg("n_points")));
+  }
 
 } // namespace <anonymous>
 
@@ -28,7 +29,8 @@ namespace boost_python {
 
   void wrap_interpolation()
   {
-    interpolation_wrappers::wrap();
+    wrap_splines< scitbx::vec2<double> >();
+    wrap_splines< scitbx::vec3<double> >();
   }
 
 }}} // namespace scitbx::math::boost_python
