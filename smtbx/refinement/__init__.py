d71167cca6739fa3b19348652adb1ad0b69db4ad
Luc J. Bourhis <luc_j_bourhis@mac.com>
2011-02-10 13:16:07 +0000

    add a method 'from_cif' to smtbx.refinement.model

--- smtbx/refinement/__init__.py
+++ smtbx/refinement/__init__.py
@@ -1,4 +1,5 @@
-from smtbx.refinement import constraints, least_squares
+from cctbx import xray
+from smtbx.refinement import constraints, least_squares, restraints
 import smtbx.utils
 
 class model(object):
@@ -7,6 +8,22 @@ class model(object):
     from iotbx.shelx import smtbx_refinement_model_from as _
     return _(cls, *args, **kwds)
   from_shelx = classmethod(from_shelx)
+
+  def from_cif(cls, model, reflections):
+    """
+    We could try to read in the weighting scheme.
+    As for constraints and restraints, the CIF format does not support them
+    yet
+    """
+    from iotbx.reflection_file_reader import any_reflection_file
+    xs = xray.structure.from_cif(file_path=model)
+    ma = any_reflection_file(reflections).as_miller_arrays()[0]
+    assert ma.is_xray_intensity_array()
+    return cls(fo_sq=ma, xray_structure=xs,
+               constraints=[],
+               restraints_manager=restraints.manager(),
+               weighting_scheme=least_squares.sigma_weighting())
+  from_cif=classmethod(from_cif)
 
   def __init__(self, fo_sq, xray_structure,
                constraints, restraints_manager, weighting_scheme):
@@ -23,7 +40,7 @@ class model(object):
       self.xray_structure,
       self.constraints,
       self.connectivity_table)
-    return least_squares.normal_equations(
+    return least_squares.crystallographic_ls(
       self.fo_sq,
       reparametrisation,
       restraints_manager=self.restraints_manager,
