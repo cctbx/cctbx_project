40f144a43083e2ff84bde78036b5d1b988ae41c5
Nathaniel Echols <nathaniel.echols@gmail.com>
2011-02-12 00:38:46 +0000

    detached function from iotbx.reflection_file_editor, added simple unit tests

--- cctbx/r_free_utils.py
+++ cctbx/r_free_utils.py
@@ -1,4 +1,5 @@
 
+from libtbx.math_utils import iceil
 from itertools import count
 import random
 
@@ -29,6 +30,7 @@ generate_r_free_params_str = """\
 """
 
 def assign_random_r_free_flags (n_refl, fraction_free) :
+  assert (fraction_free > 0) and (fraction_free < 0.5)
   from scitbx.array_family import flex
   from libtbx.math_utils import iround
   group_size = 1/(fraction_free)
@@ -47,6 +49,8 @@ def assign_random_r_free_flags (n_refl, fraction_free) :
   return result
 
 def assign_r_free_flags_by_shells (n_refl, fraction_free, n_bins) :
+  assert (fraction_free > 0) and (fraction_free < 0.5)
+  assert (n_bins > 1) and (n_bins < n_refl) # XXX this should be smarter
   from scitbx.array_family import flex
   from libtbx.math_utils import iround
   n_free = iround(n_refl * fraction_free)
@@ -61,3 +65,35 @@ def assign_r_free_flags_by_shells (n_refl, fraction_free, n_bins) :
     flags.resize(min(n_refl, flags.size()+half_n_work_per_bin), False)
   flags.resize(n_refl, False)
   return flags
+
+def export_r_free_flags (flags, test_flag_value) :
+  assert (test_flag_value == True) or isinstance(test_flag_value, int)
+  from scitbx.array_family import flex
+  if (isinstance(flags, flex.bool)) :
+    test_flag_value = True
+  else :
+    assert isinstance(flags, flex.int)
+  unique_values = set(flags)
+  if len(unique_values) > 2 : # XXX: is this safe?
+    return flags
+  new_flags = flex.int(flags.size())
+  for i in range(flags.size()) :
+    if flags[i] == test_flag_value :
+      new_flags[i] = 0
+    else :
+      new_flags[i] = iceil(random.random() * 19)
+  return new_flags
+
+def exercise () :
+  flags_1 = assign_random_r_free_flags(n_refl=100000, fraction_free=0.05)
+  assert (flags_1.count(True) == 5000)
+  flags_2 = assign_r_free_flags_by_shells(n_refl=100000,
+    fraction_free=0.05,
+    n_bins=50)
+  assert (flags_2.count(True) == 5000)
+  ccp4_flags = export_r_free_flags(flags_1, True)
+  assert (ccp4_flags.count(0) == flags_1.count(True))
+
+if (__name__ == "__main__") :
+  exercise()
+  print "OK"
