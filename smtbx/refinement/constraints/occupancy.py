2e2389cdd0bdb905d8acf46083a3e3f846fafaab
Richard Gildea <richard.gildea@diamond.ac.uk>
2011-02-13 11:46:45 +0000

    smtbx/refinement/constraints/occupancy.py: fix typo

--- smtbx/refinement/constraints/occupancy.py
+++ smtbx/refinement/constraints/occupancy.py
@@ -9,7 +9,7 @@ class dependent_occupancy(object):
     if (len(var_refs) + len(var_minus_one_refs)) == 0:
       raise InvalidConstraint("at least one atom is expected")
     self.as_var = var_refs
-    self.as_one_munus_var = var_minus_one_refs
+    self.as_one_minus_var = var_minus_one_refs
 
   def __eq__(self, other):
     if self.indices != other.indices:  return False
@@ -21,8 +21,8 @@ class dependent_occupancy(object):
       sc_idx = self.as_var[0][0]
       ratio = 1./self.as_var[0][1]
     else:
-      sc_idx = self.as_one_munus_var[0][0]
-      ratio = 1./self.as_one_munus_var[0][1]
+      sc_idx = self.as_one_minus_var[0][0]
+      ratio = 1./self.as_one_minus_var[0][1]
     occupancy = reparametrisation.add_new_occupancy_parameter(sc_idx)
     for sc in self.as_var:
       if sc[0] == sc_idx:  continue
@@ -35,7 +35,7 @@ class dependent_occupancy(object):
       reparametrisation.asu_scatterer_parameters[sc[0]].occupancy = param
       reparametrisation.shared_occupancies[sc[0]] = occupancy
     as_one = len(self.as_var) == 0  # only if both lists are not empty
-    for sc in self.as_one_munus_var:
+    for sc in self.as_one_minus_var:
       if sc[0] == sc_idx:  continue
       param = reparametrisation.add(
         _.dependent_occupancy,
