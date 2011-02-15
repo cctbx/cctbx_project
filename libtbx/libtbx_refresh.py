f1e76d0dfb28addde31ad157a6f6cd1763588091
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2011-02-15 19:56:45 +0000

    dox/rst/installation.txt updated, obsolete libtbx/sourceforge_checkout.py removed

--- libtbx/libtbx_refresh.py
+++
@@ -1,8 +0,0 @@
-# copy script to enable checkout even if libtbx sources are removed already
-source = self.env.under_dist("libtbx", "sourceforge_checkout.py")
-copy = self.env.under_build("sourceforge_checkout.py")
-open(copy, "w").write(open(source).read())
-#
-self.env.write_dispatcher_in_bin(
-  source_file=copy,
-  target_file="libtbx.sourceforge_checkout")
