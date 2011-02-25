b8336f3750541483cf15a333400518ec1673f447
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2011-02-25 04:33:19 +0000

    pickle_import_trigger added to pickled states of C++ extension classes with __setstate__ methods injected from Python

--- scitbx/array_family/shared.py
+++ scitbx/array_family/shared.py
@@ -5,18 +5,23 @@ boost.python.import_ext("scitbx_array_family_shared_ext")
 from scitbx_array_family_shared_ext import *
 import scitbx_array_family_shared_ext as ext
 
+class pickle_import_trigger(object): pass
+
 class _stl_set_unsigned(boost.python.injector, ext.stl_set_unsigned):
 
   def __getstate__(self): # XXX slow, move to C++
-    version = 1
+    version = 2
     list_of_lists = []
     for elem in self:
       list_of_lists.append(list(elem))
-    return (version, list_of_lists)
+    return (version, pickle_import_trigger(), list_of_lists)
 
   def __setstate__(self, state):
-    assert len(state) == 2
-    assert state[0] == 1 # version
-    list_of_lists = state[1]
+    assert len(state) >= 2
+    version = state[0]
+    if   (version == 1): assert len(state) == 2
+    elif (version == 2): assert len(state) == 3
+    else: raise RuntimeError("Unknown version of pickled state.")
+    list_of_lists = state[-1]
     for elem in list_of_lists:
       self.append(scitbx.stl.set.unsigned(elem))
