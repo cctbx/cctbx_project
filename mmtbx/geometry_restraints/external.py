1c9b48f2e6c5245c1417ec9a11f306a322fce8aa
Nathaniel Echols <nathaniel.echols@gmail.com>
2011-02-28 21:35:11 +0000

    bug fix

--- mmtbx/geometry_restraints/external.py
+++ mmtbx/geometry_restraints/external.py
@@ -46,6 +46,7 @@ def get_rosetta_manager (pdb_hierarchy,
                          params,
                          log=sys.stdout) :
   import rosetta_adaptbx # import dependency
+  rosetta_adaptbx.init()
   from rosetta_adaptbx import scoring
   manager = scoring.manager(
     pdb_hierarchy=pdb_hierarchy,
