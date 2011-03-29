25a7f786cc47be7327edc8b0425a78e11d51258d
Richard Gildea <richard.gildea@diamond.ac.uk>
2011-03-29 18:43:55 +0000

    iotbx/cif: new geometry.py to give a home to geometry-related cif tools

--- iotbx/cif/tests/tst_geometry.py
+++ iotbx/cif/tests/tst_geometry.py
@@ -1,7 +1,7 @@
 from cctbx import crystal, xray
 from cctbx.array_family import flex
 from libtbx.test_utils import show_diff
-import iotbx.cif
+from iotbx.cif import geometry
 from cStringIO import StringIO
 
 def exercise_cif_from_cctbx():
@@ -12,7 +12,7 @@ def exercise_cif_from_cctbx():
       xray.scatterer("Si", (1/2.,1/2.,1/3.)),
       xray.scatterer("O", (0.197,-0.197,0.83333))]))
   s = StringIO()
-  loop = iotbx.cif.distances_as_cif_loop(
+  loop = geometry.distances_as_cif_loop(
     quartz.pair_asu_table(distance_cutoff=2),
     site_labels=quartz.scatterers().extract_labels(),
     sites_frac=quartz.sites_frac()).loop
@@ -30,7 +30,7 @@ loop_
 
 """)
   s = StringIO()
-  loop = iotbx.cif.angles_as_cif_loop(
+  loop = geometry.angles_as_cif_loop(
     quartz.pair_asu_table(distance_cutoff=2),
     site_labels=quartz.scatterers().extract_labels(),
     sites_frac=quartz.sites_frac()).loop
