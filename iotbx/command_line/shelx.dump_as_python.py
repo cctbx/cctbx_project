511ae442a8259053cbbf91a7696846f9c050f675
Luc J. Bourhis <luc_j_bourhis@mac.com>
2011-03-30 08:43:42 +0000

    Be loose!

--- iotbx/command_line/shelx.dump_as_python.py
+++ iotbx/command_line/shelx.dump_as_python.py
@@ -1,7 +1,8 @@
 from cctbx import xray
 
 def run(file_name, exclude_hydrogens=False):
-  xs = xray.structure.from_shelx(filename=file_name)
+  xs = xray.structure.from_shelx(filename=file_name,
+                                 strictly_shelxl=False)
   if exclude_hydrogens:
     xs = xs.select(xs.hd_selection(), negate=True)
   print repr(xs)
