5ebadb599a1879d7aa60a11ec3d83267ec04135c
Oleg Dolomanov <pcxod@users.noreply.github.com>
2011-04-06 16:54:31 +0000

    a convenience method for custom copy of observations with new twin fractions/components is added

--- cctbx/xray/observations/__init__.py
+++ cctbx/xray/observations/__init__.py
@@ -1,1 +1,17 @@
 from cctbx_xray_observations_ext import *
+
+def custom_copy(obs, twin_fractions=None, twin_components=None):
+  """ Creates a copy of the observation object and with new twin fractions
+  and twin components
+  """
+  twf = ()
+  if twin_fractions is not None:
+    twf = twin_fractions
+  twc = ()
+  if twin_components is not None:
+    twc = twin_components
+  result = observations(obs, twf, twc)
+  result.fo_sq = obs.fo_sq
+  result.ref_twin_fractions = twin_fractions
+  result.ref_twin_components = twin_components
+  return result
