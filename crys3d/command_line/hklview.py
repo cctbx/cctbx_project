4fd85e61b9ee9cafc6eec13bb6e9448e01b43ab1
Nathaniel Echols <nathaniel.echols@gmail.com>
2011-04-23 02:06:40 +0000

    separate class for array data; graphical file/array selection; bug fixes and test code

--- crys3d/command_line/hklview.py
+++ crys3d/command_line/hklview.py
@@ -4,16 +4,31 @@
 from crys3d.wx_hklview import *
 import sys
 
-if (__name__ == "__main__") :
+def run (args) :
   from iotbx import file_reader
-  f = file_reader.any_file(sys.argv[-1])
   ma = None
-  for array in f.file_server.miller_arrays :
-    if array.is_xray_amplitude_array() or array.is_xray_intensity_array() :
-      ma = array
-      break
+  if (len(args) == 0) :
+    from cctbx import miller, crystal
+    from cctbx.array_family import flex
+    xs = crystal.symmetry((3,3,5,90,90,120), "P6")
+    mi = flex.miller_index([
+      (0,0,1),(0,0,2),(0,0,3),
+      (0,1,0),(0,2,0),(0,3,0),
+      (1,1,0),(1,2,0),(1,3,0)])
+    d = flex.double([1.0, 2.0, 3.0, 5.0, 10.0, 15.0, 6.0, 9.0, 12.0])
+    s = miller.set(xs, mi, anomalous_flag=False)
+    ma = s.array(data=d).set_info("test")
+  else :
+    f = file_reader.any_file(args[-1])
+    for array in f.file_server.miller_arrays :
+      if array.is_xray_amplitude_array() or array.is_xray_intensity_array() :
+        ma = array
+        break
   a = wx.App(0)
   f = HKLViewFrame(None, -1, "HKL viewer", size=(1024,768))
   f.set_miller_array(ma)
   f.Show()
   a.MainLoop()
+
+if (__name__ == "__main__") :
+  run(sys.argv[1:])
