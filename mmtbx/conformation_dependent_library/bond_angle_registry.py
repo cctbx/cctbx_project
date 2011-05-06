2fa1ddb145f834ad37c590bb9961c8dd257bdde6
Nigel W Moriarty <nwmoriarty@lbl.gov>
2011-05-06 23:31:43 +0000

    FAST CDL restraints update

---
+++ mmtbx/conformation_dependent_library/bond_angle_registry.py
@@ -0,0 +1,14 @@
+class bond_angle_registry(dict):
+  def __init__(self):
+    pass
+
+  def __repr__(self):
+    outl = "CDL Bond Angle Registry"
+    for i, residue in enumerate(sorted(self)):
+      outl += "\n  %s : %7.2f" % (str(residue),
+                                  self[residue].angle_ideal,
+                                  )
+    return outl
+
+if __name__=="__main__":
+  pass
