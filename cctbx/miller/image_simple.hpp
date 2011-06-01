b2cbf5dddba67714dd01b4df1ceb0191a6a6c8c0
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2011-06-01 01:06:08 +0000

    cctbx/miller/image_simple.hpp: point_spread option

--- cctbx/miller/image_simple.hpp
+++ cctbx/miller/image_simple.hpp
@@ -19,15 +19,19 @@ namespace cctbx { namespace miller {
     double ewald_proximity,
     double detector_distance,
     scitbx::vec2<double> detector_size,
-    scitbx::vec2<int> detector_pixels)
+    scitbx::vec2<int> detector_pixels,
+    unsigned point_spread)
   {
     TBXX_ASSERT(ewald_radius > 0);
     TBXX_ASSERT(detector_pixels.const_ref().all_gt(0));
+    TBXX_ASSERT(point_spread > 0);
     int dpx = detector_pixels[0];
     int dpy = detector_pixels[1];
     std::string result(dpx * dpy * 3, static_cast<char>(255U));
     double dsx = detector_size[0];
     double dsy = detector_size[1];
+    unsigned ps2 = point_spread / 2;
+    bool point_spread_is_even_value = (point_spread % 2 == 0);
     typedef scitbx::vec3<double> v3d;
     for(std::size_t ih=0;ih<miller_indices.size();ih++) {
       v3d rv = unit_cell.reciprocal_space_vector(miller_indices[ih]);
@@ -43,18 +47,26 @@ namespace cctbx { namespace miller {
           double dy = rvre[1] * d;
           if (   std::abs(dx) <= dsx/2
               && std::abs(dy) <= dsy/2) {
-            using scitbx::math::iround;
-            int px = std::min(iround((dx/dsx + 1/2.) * dpx), dpx-1);
-            int py = std::min(iround((dy/dsy + 1/2.) * dpy), dpy-1);
-            for(int i=-1;i<=1;i++) {
-              int pi = px + i;
+            using scitbx::math::ifloor;
+            double pxf = (dx/dsx + 0.5) * dpx;
+            double pyf = (dy/dsy + 0.5) * dpy;
+            int pxi = ifloor(pxf);
+            int pyi = ifloor(pyf);
+            int pxb = pxi - ps2;
+            int pyb = pyi - ps2;
+            if (point_spread_is_even_value) {
+              if (pxf - pxi > 0.5) pxb++;
+              if (pyf - pyi > 0.5) pyb++;
+            }
+            for(int i=0;i<point_spread;i++) {
+              int pi = pxb + i;
               if (pi < 0 || pi >= dpx) continue;
-              for(int j=-1;j<=1;j++) {
-                int pj = py + j;
+              for(int j=0;j<point_spread;j++) {
+                int pj = pyb + j;
                 if (pj < 0 || pj >= dpy) continue;
-                int pijk = (pi*dpy+pj)*3;
-                for(int k=0;k<3;k++,pijk++) {
-                  result[pijk] = static_cast<char>(0U);
+                int pij = (pi*dpy+pj)*3;
+                for(int k=0;k<3;k++) {
+                  result[pij+k] = static_cast<char>(0U);
                 }
               }
             }
