51a7f60d2e221b6a61a000f7605cc4b342e26e4d
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2011-06-18 04:26:41 +0000

    cctbx/miller/image_simple.hpp: store_spots option

--- cctbx/miller/image_simple.hpp
+++ cctbx/miller/image_simple.hpp
@@ -10,92 +10,116 @@
 
 namespace cctbx { namespace miller {
 
-  inline
-  af::versa<int, af::flex_grid<> >
-  image_simple(
-    uctbx::unit_cell const& unit_cell,
-    af::const_ref<index<> > const& miller_indices,
-    scitbx::mat3<double> const& crystal_rotation_matrix,
-    double ewald_radius,
-    double ewald_proximity,
-    int signal_max,
-    double detector_distance,
-    scitbx::vec2<double> detector_size,
-    scitbx::vec2<int> detector_pixels,
-    unsigned point_spread,
-    double gaussian_falloff_scale)
+  struct image_simple
   {
-    TBXX_ASSERT(ewald_radius > 0);
-    TBXX_ASSERT(detector_size.const_ref().all_gt(0));
-    TBXX_ASSERT(detector_pixels.const_ref().all_gt(0));
-    TBXX_ASSERT(point_spread > 0);
-    TBXX_ASSERT(gaussian_falloff_scale >= 0);
-    int dpx = detector_pixels[0];
-    int dpy = detector_pixels[1];
-    af::versa<int, af::flex_grid<> > result(af::flex_grid<>(dpx, dpy), 0);
-    int* result_beg = result.begin();
-    double dsx = detector_size[0];
-    double dsy = detector_size[1];
-    unsigned point_spread_half = point_spread / 2;
-    bool point_spread_is_even_value = (point_spread % 2 == 0);
-    double circle_radius_sq = point_spread * std::max(dsx/dpx, dsy/dpy) / 2;
-    circle_radius_sq *= circle_radius_sq;
-    TBXX_ASSERT(circle_radius_sq != 0);
-    typedef scitbx::vec3<double> v3d;
-    for(std::size_t ih=0;ih<miller_indices.size();ih++) {
-      v3d rv = unit_cell.reciprocal_space_vector(miller_indices[ih]);
-      v3d rvre = crystal_rotation_matrix * rv;
-      rvre[2] += ewald_radius; // direct beam anti-parallel (0,0,1)
-      double rvre_len = rvre.length();
-      double rvre_proximity = rvre_len / ewald_radius;
-      if (std::abs(1-rvre_proximity) <= ewald_proximity) {
-        // http://en.wikipedia.org/wiki/Line-plane_intersection
-        if (rvre[2] > 0) {
-          double d = -detector_distance / rvre[2];
-          double dx = rvre[0] * d;
-          double dy = rvre[1] * d;
-          if (   std::abs(dx) <= dsx/2
-              && std::abs(dy) <= dsy/2) {
-            using scitbx::math::ifloor;
-            double pxf = (dx/dsx + 0.5) * dpx;
-            double pyf = (dy/dsy + 0.5) * dpy;
-            int pxi = ifloor(pxf);
-            int pyi = ifloor(pyf);
-            int pxb = pxi - point_spread_half;
-            int pyb = pyi - point_spread_half;
-            if (point_spread_is_even_value) {
-              if (pxf - pxi > 0.5) pxb++;
-              if (pyf - pyi > 0.5) pyb++;
-            }
-            double gauss_arg_term = -gaussian_falloff_scale / circle_radius_sq;
-            int signal = signal_max;
-            for(int i=0;i<=point_spread;i++) {
-              int pi = pxb + i;
-              if (pi < 0 || pi >= dpx) continue;
-              int pi0 = pi * dpy;
-              for(int j=0;j<=point_spread;j++) {
-                int pj = pyb + j;
-                if (pj < 0 || pj >= dpy) continue;
-                if (point_spread > 2) {
-                  double pcx = ((pi + 0.5) / dpx - 0.5) * dsx - dx;
-                  double pcy = ((pj + 0.5) / dpy - 0.5) * dsy - dy;
-                  double pc_sq = pcx*pcx + pcy*pcy;
-                  if (pc_sq > circle_radius_sq) continue;
-                  if (gaussian_falloff_scale != 0) {
-                    double falloff_factor = std::exp(pc_sq * gauss_arg_term);
-                    signal = static_cast<int>(
-                      signal_max * falloff_factor + 0.5);
+    bool store_spots;
+    bool set_pixels;
+    af::shared<scitbx::vec3<double> > spots;
+    af::versa<int, af::flex_grid<> > pixels;
+
+    image_simple(
+      bool store_spots_,
+      bool set_pixels_)
+    :
+      store_spots(store_spots_),
+      set_pixels(set_pixels_)
+    {
+      TBXX_ASSERT(store_spots || set_pixels);
+    }
+
+    image_simple&
+    compute(
+      uctbx::unit_cell const& unit_cell,
+      af::const_ref<index<> > const& miller_indices,
+      scitbx::mat3<double> const& crystal_rotation_matrix,
+      double ewald_radius,
+      double ewald_proximity,
+      int signal_max,
+      double detector_distance,
+      scitbx::vec2<double> detector_size,
+      scitbx::vec2<int> detector_pixels,
+      unsigned point_spread,
+      double gaussian_falloff_scale)
+    {
+      TBXX_ASSERT(ewald_radius > 0);
+      TBXX_ASSERT(detector_size.const_ref().all_gt(0));
+      TBXX_ASSERT(detector_pixels.const_ref().all_gt(0));
+      TBXX_ASSERT(point_spread > 0);
+      TBXX_ASSERT(gaussian_falloff_scale >= 0);
+      int dpx = detector_pixels[0];
+      int dpy = detector_pixels[1];
+      if (set_pixels) {
+        pixels.resize(af::flex_grid<>(dpx, dpy), 0);
+      }
+      int* pixels_beg = pixels.begin();
+      double dsx = detector_size[0];
+      double dsy = detector_size[1];
+      unsigned point_spread_half = point_spread / 2;
+      bool point_spread_is_even_value = (point_spread % 2 == 0);
+      double circle_radius_sq = point_spread * std::max(dsx/dpx, dsy/dpy) / 2;
+      circle_radius_sq *= circle_radius_sq;
+      TBXX_ASSERT(circle_radius_sq != 0);
+      typedef scitbx::vec3<double> v3d;
+      for(std::size_t ih=0;ih<miller_indices.size();ih++) {
+        v3d rv = unit_cell.reciprocal_space_vector(miller_indices[ih]);
+        v3d rvre = crystal_rotation_matrix * rv;
+        rvre[2] += ewald_radius; // direct beam anti-parallel (0,0,1)
+        double rvre_len = rvre.length();
+        double rvre_proximity = rvre_len / ewald_radius;
+        if (std::abs(1-rvre_proximity) <= ewald_proximity) {
+          // http://en.wikipedia.org/wiki/Line-plane_intersection
+          if (rvre[2] > 0) {
+            double d = -detector_distance / rvre[2];
+            double dx = rvre[0] * d;
+            double dy = rvre[1] * d;
+            if (   std::abs(dx) <= dsx/2
+                && std::abs(dy) <= dsy/2) {
+              using scitbx::math::ifloor;
+              double pxf = (dx/dsx + 0.5) * dpx;
+              double pyf = (dy/dsy + 0.5) * dpy;
+              if (store_spots) {
+                spots.push_back(scitbx::vec3<double>(pxf, pyf, 0));
+              }
+              if (!set_pixels) continue;
+              int pxi = ifloor(pxf);
+              int pyi = ifloor(pyf);
+              int pxb = pxi - point_spread_half;
+              int pyb = pyi - point_spread_half;
+              if (point_spread_is_even_value) {
+                if (pxf - pxi > 0.5) pxb++;
+                if (pyf - pyi > 0.5) pyb++;
+              }
+              double gauss_arg_term = -gaussian_falloff_scale
+                                    / circle_radius_sq;
+              int signal = signal_max;
+              for(int i=0;i<=point_spread;i++) {
+                int pi = pxb + i;
+                if (pi < 0 || pi >= dpx) continue;
+                int pi0 = pi * dpy;
+                for(int j=0;j<=point_spread;j++) {
+                  int pj = pyb + j;
+                  if (pj < 0 || pj >= dpy) continue;
+                  if (point_spread > 2) {
+                    double pcx = ((pi + 0.5) / dpx - 0.5) * dsx - dx;
+                    double pcy = ((pj + 0.5) / dpy - 0.5) * dsy - dy;
+                    double pc_sq = pcx*pcx + pcy*pcy;
+                    if (pc_sq > circle_radius_sq) continue;
+                    if (gaussian_falloff_scale != 0) {
+                      double falloff_factor = std::exp(pc_sq * gauss_arg_term);
+                      signal = static_cast<int>(
+                        signal_max * falloff_factor + 0.5);
+                    }
                   }
+                  pixels[pi0+pj] = signal;
                 }
-                result[pi0+pj] = signal;
               }
             }
           }
         }
       }
+      return *this;
     }
-    return result;
-  }
+  };
 
 }} // namespace cctbx::miller
 
