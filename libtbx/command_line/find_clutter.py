b371c9590a74d72b9e8093251680945b4fd7b8e2
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2011-06-22 21:23:17 +0000

    libtbx/command_line/find_clutter.py: report bare excepts

--- libtbx/command_line/find_clutter.py
+++ libtbx/command_line/find_clutter.py
@@ -17,28 +17,41 @@ def run(args):
       paths.append(arg)
   if (len(paths) == 0): paths = ["."]
   n_is_cluttered = 0
+  n_bare_excepts = 0
   n_has_unused_imports = 0
-  first_print = True
+  message_lines = []
   for info in gather(paths=paths, find_unused_imports=not flag_ni):
     if (info.is_cluttered(flag_x=flag_x)):
       n_is_cluttered += 1
-      if (first_print):
-        first_print = False
-        print
+    if (info.n_bare_excepts > 0):
+      n_bare_excepts += info.n_bare_excepts
     if (info.has_unused_imports()):
       n_has_unused_imports += 1
-      if (first_print):
-        first_print = False
-        print
-    info.show(flag_x, flag_dos_format)
+    info.show(
+      flag_x=flag_x,
+      flag_dos_format=flag_dos_format,
+      append=message_lines.append)
   please_use = []
   if (n_is_cluttered != 0):
     please_use.append("libtbx.clean_clutter")
   if (n_has_unused_imports != 0):
     please_use.append("libtbx.find_unused_imports_crude")
   if (len(please_use) != 0):
+    message_lines.append("")
+    message_lines.append(
+      "*** To clean up please use: %s ***" % ", ".join(please_use))
+  if (n_bare_excepts > 0):
+    message_lines.append("")
+    message_lines.extend("""\
+*** Please change bare excepts: ***
+      Usually best:
+        except Exception:
+      Rarely necessary:
+        except: # intentional
+""".splitlines())
+  if (len(message_lines) != 0):
     print
-    print "*** To clean up please use: %s ***" % ", ".join(please_use)
+    print "\n".join(message_lines)
     print
 
 if (__name__ == "__main__"):
