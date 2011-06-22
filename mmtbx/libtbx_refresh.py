ea3ff83a3a21fad47647c40d8163b4805fe71e75
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2011-06-22 07:39:50 +0000

    mmtbx/monomer_library: restoring revisions 13292 and 13293, with one extra underscore in line 259 of server.py (bug fix by Richard Gildea)

--- mmtbx/libtbx_refresh.py
+++ mmtbx/libtbx_refresh.py
@@ -1,3 +1,3 @@
-#self.remove_obsolete_pyc_if_possible(pyc_file_names=[
-#  "monomer_library/mmCIF.pyc", # XXX backward compatibility 2011-06-21
-#])
+self.remove_obsolete_pyc_if_possible(pyc_file_names=[
+  "monomer_library/mmCIF.pyc", # XXX backward compatibility 2011-06-21
+])
