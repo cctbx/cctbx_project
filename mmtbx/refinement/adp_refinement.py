097e936d5e28e6f907cb60de13cbbca64a0a298c
Pavel Afonine <afonine@users.noreply.github.com>
2011-06-28 02:20:57 +0000

    small bug fix in xyz weight optimization; new adp weight optimization

--- mmtbx/refinement/adp_refinement.py
+++ mmtbx/refinement/adp_refinement.py
@@ -6,7 +6,10 @@ from mmtbx.tls import tools
 from mmtbx.refinement import print_statistics
 import scitbx.lbfgs
 from libtbx.test_utils import approx_equal
+from libtbx import adopt_init_args
 from libtbx.utils import user_plus_sys_time
+from mmtbx import utils
+from cctbx import adptbx
 
 time_adp_refinement_py = 0.0
 
@@ -210,109 +213,196 @@ class manager(object):
           log                      = log)
     time_adp_refinement_py += timer.elapsed()
 
-def refine_adp(model,
-               fmodels,
-               target_weights,
-               individual_adp_params,
-               adp_restraints_params,
-               h_params,
-               log,
-               all_params):
-  model.set_refine_individual_adp()
-  print_statistics.make_sub_header(text="Individual ADP refinement", out = log)
-  lbfgs_termination_params = scitbx.lbfgs.termination_parameters(
-    max_iterations = individual_adp_params.iso.max_number_of_iterations)
-  assert fmodels.fmodel_xray().xray_structure is model.xray_structure
-  if(target_weights is not None and target_weights.twp.optimize_wxu):
-    save_scatterers = fmodels.fmodel_xray().xray_structure.\
-      deep_copy_scatterers().scatterers()
-    print >> log, "Start r_free = %6.4f"%fmodels.fmodel_xray().r_free()
-    new_scatterers = None
-    scaler_values = [1.,1.5,2.,2.5,3.,3.5,4.,4.5,5.,6.,7.,8.,9.,10.]
-  else: scaler_values = [1.,]
-  r_free = 1.e+9
-  cntr = 0
-  if(target_weights is not None):
-    w = target_weights.adp_weights_result.w
-  for scaler in scaler_values:
-    if(target_weights is not None and target_weights.twp.optimize_wxu):
-      fmodels.fmodel_xray().xray_structure.replace_scatterers(
+class refine_adp(object):
+
+  def __init__(self, model, fmodels, target_weights, individual_adp_params,
+               adp_restraints_params, h_params, log, all_params):
+    adopt_init_args(self, locals())
+    # define similarity width for r-free
+    d_min = fmodels.fmodel_xray().f_obs().d_min()
+    if  (d_min<=1.5):               r_free_range_width = 0.
+    elif(d_min>1.5 and d_min<=2.0): r_free_range_width = 0.5
+    elif(d_min>2.0 and d_min<=2.5): r_free_range_width = 1.0
+    elif(d_min>2.5 and d_min<=3.0): r_free_range_width = 1.5
+    elif(d_min>3.0):                r_free_range_width = 2.0
+    # r-free-r-work gap value
+    if  (d_min<=1.5):               r_free_r_work_gap = 4.0
+    elif(d_min>1.5 and d_min<=2.0): r_free_r_work_gap = 5.0
+    elif(d_min>2.0 and d_min<=2.5): r_free_r_work_gap = 6.0
+    elif(d_min>2.5 and d_min<=3.0): r_free_r_work_gap = 6.0
+    elif(d_min>3.0):                r_free_r_work_gap = 7.0
+    #
+    print_statistics.make_sub_header(text="Individual ADP refinement", out = log)
+    assert fmodels.fmodel_xray().xray_structure is model.xray_structure
+    #
+    fmodels.create_target_functors()
+    assert approx_equal(self.fmodels.fmodel_xray().target_w(),
+      self.fmodels.target_functor_result_xray(
+        compute_gradients=False).target_work())
+    rw     = flex.double()
+    rf     = flex.double()
+    rfrw   = flex.double()
+    deltab = flex.double()
+    w      = flex.double()
+    if(self.target_weights is not None):
+      fmth ="    R-FACTORS      <Bi-Bj>  <B>   WEIGHT       TARGETS"
+      print >> self.log, fmth
+      print >> self.log, " work  free  delta                           data restr"
+    else:
+      print >> self.log, "Unresrained refinement..."
+    save_scatterers = self.fmodels.fmodel_xray().xray_structure.\
+        deep_copy_scatterers().scatterers()
+    if(self.target_weights is not None):
+      default_weight = self.target_weights.adp_weights_result.wx*\
+          self.target_weights.adp_weights_result.wx_scale
+      if(self.target_weights.twp.optimize_adp_weight):
+        wx_scale = [0.,0.0625,0.125,0.25,0.5,0.75,1.,1.125,1.25,1.5,1.75,2.,2.5,
+          3.,3.5,4.,4.5,5.]
+        trial_weights = list( flex.double(wx_scale)*self.target_weights.xyz_weights_result.wx )
+        wx_scale = 1
+      else:
+        trial_weights = [self.target_weights.adp_weights_result.wx]
+        wx_scale = self.target_weights.adp_weights_result.wx_scale
+    else:
+      default_weight = 1
+      trial_weights = [1]
+      wx_scale = 1
+    self.show(weight=default_weight)
+    #
+    for weight in trial_weights:
+      if(self.target_weights is not None):
+        self.fmodels.fmodel_xray().xray_structure.replace_scatterers(
+          save_scatterers.deep_copy())
+        self.fmodels.update_xray_structure(
+          xray_structure = self.fmodels.fmodel_xray().xray_structure,
+          update_f_calc  = True)
+        self.target_weights.adp_weights_result.wx = weight
+        self.target_weights.adp_weights_result.wx_scale = wx_scale
+      minimized = self.minimize()
+      wt = weight*wx_scale
+      rw_,rf_,rfrw_,deltab_,w_ = self.show(weight=wt)
+      if(rw_ is not None):
+        rw     .append(rw_  )
+        rf     .append(rf_  )
+        rfrw   .append(rfrw_)
+        deltab .append(deltab_)
+        w      .append(w_   )
+    #
+    if(len(trial_weights)>1):
+      # sort by r-free
+      sel = flex.sort_permutation(rf)
+      rw,rf,rfrw,deltab,w=self.select(
+        rw=rw,rf=rf,rfrw=rfrw,deltab=deltab,w=w,sel=sel)
+      # select equally good results
+      sel = (rf <= rf[0]+r_free_range_width)
+      rw,rf,rfrw,deltab,w= self.select(
+        rw=rw,rf=rf,rfrw=rfrw,deltab=deltab,w=w,sel=sel)
+      # filter by rfree-rwork
+      rw,rf,rfrw,deltab,w = self.score(rw=rw,rf=rf,rfrw=rfrw,deltab=deltab,w=w,
+        score_target=rfrw,score_target_value=r_free_r_work_gap,
+        secondary_target=deltab)
+      # filter by <Bi-Bj>
+      rw,rf,rfrw,deltab,w = self.score(rw=rw,rf=rf,rfrw=rfrw,deltab=deltab,w=w,
+        score_target=deltab,score_target_value=10)
+      # select the result with lowest rfree
+      sel = flex.sort_permutation(rf)
+      rw,rf,rfrw,deltab,w= self.select(
+        rw=rw,rf=rf,rfrw=rfrw,deltab=deltab,w=w,sel=sel)
+      #
+      w_best = w[0]
+      print >> self.log, "Best weight: %8.3f"%w_best
+      #
+      self.target_weights.adp_weights_result.wx = w_best
+      self.target_weights.adp_weights_result.wx_scale = 1
+      self.fmodels.fmodel_xray().xray_structure.replace_scatterers(
         save_scatterers.deep_copy())
-      fmodels.update_xray_structure(update_f_calc = True)
-      target_weights.adp_weights_result.w = w / scaler
+      self.fmodels.update_xray_structure(
+        xray_structure = self.fmodels.fmodel_xray().xray_structure,
+        update_f_calc  = True)
+      print >> self.log, "Accepted refinement result:"
+      minimized = self.minimize()
+      self.show(weight=w_best)
+    #
+    assert approx_equal(self.fmodels.fmodel_xray().target_w(),
+       self.fmodels.target_functor_result_xray(
+         compute_gradients=False).target_work())
+    self.model.xray_structure = self.fmodels.fmodel_xray().xray_structure
+    #
+
+  def show(self, weight = None, prefix = "", show_neutron=True):
+    deltab = self.model.rms_b_iso_or_b_equiv_bonded()
+    r_work = self.fmodels.fmodel_xray().r_work()*100.
+    r_free = self.fmodels.fmodel_xray().r_free()*100.
+    mean_b = flex.mean(
+      self.model.xray_structure.extract_u_iso_or_u_equiv())*adptbx.u_as_b(1)
+    if(deltab is None):
+      print >> self.log, "  r_work=%5.2f r_free=%5.2f"%(r_work, r_free)
+      return [None,]*5
+    if(len(prefix.strip())>0): prefix += " "
+    format = prefix+"%5.2f %5.2f %6.2f %6.3f  %6.3f %6.3f   %6.3f"
+    print >> self.log, format%(r_work,r_free,r_free-r_work,deltab,mean_b,weight,
+      self.fmodels.fmodel_xray().target_w())
+    if(show_neutron and self.fmodels.fmodel_neutron() is not None):
+      print >> self.log
+      print >> self.log, "Neutron data: r_work=%5.2f r_free=%5.2f"%(
+        self.fmodels.fmodel_neutron().r_work()*100.,
+        self.fmodels.fmodel_neutron().r_free()*100.)
+    return r_work,r_free,r_free-r_work,deltab,weight
+
+  def score(self, rw, rf, rfrw, deltab, w, score_target, score_target_value,
+            secondary_target=None):
+    sel  = score_target < score_target_value
+    sel &= score_target > 0
+    if(sel.count(True)>0):
+      rw,rf,rfrw,deltab,w = self.select(
+        rw=rw,rf=rf,rfrw=rfrw,deltab=deltab,w=w, sel=sel)
+    else:
+      if(secondary_target is None):
+        sel = flex.sort_permutation(score_target)
+      else:
+        sel = flex.sort_permutation(secondary_target)
+      rw,rf,rfrw,deltab,w = self.select(
+        rw=rw,rf=rf,rfrw=rfrw,deltab=deltab,w=w, sel=sel)
+      #
+      rw     = flex.double([rw    [0]])
+      rf     = flex.double([rf    [0]])
+      rfrw   = flex.double([rfrw  [0]])
+      deltab = flex.double([deltab[0]])
+      w      = flex.double([w     [0]])
+    return rw, rf, rfrw, deltab, w
+
+  def select(self, rw, rf, rfrw, deltab, w, sel):
+    rw     = rw    .select(sel)
+    rf     = rf    .select(sel)
+    rfrw   = rfrw  .select(sel)
+    deltab = deltab.select(sel)
+    w      = w     .select(sel)
+    return rw, rf, rfrw, deltab, w
+
+  def minimize(self):
+    utils.assert_xray_structures_equal(
+      x1 = self.fmodels.fmodel_xray().xray_structure,
+      x2 = self.model.xray_structure)
+    self.model.set_refine_individual_adp()
+    lbfgs_termination_params = scitbx.lbfgs.termination_parameters(
+      max_iterations = self.individual_adp_params.iso.max_number_of_iterations)
     is_neutron_scat_table = False
-    if(all_params.main.scattering_table == "neutron"):
+    if(self.all_params.main.scattering_table == "neutron"):
       is_neutron_scat_table = True
     minimized = minimization.lbfgs(
-      restraints_manager       = model.restraints_manager,
-      fmodels                  = fmodels,
-      model                    = model,
+      restraints_manager       = self.model.restraints_manager,
+      fmodels                  = self.fmodels,
+      model                    = self.model,
       refine_adp               = True,
       is_neutron_scat_table    = is_neutron_scat_table,
       lbfgs_termination_params = lbfgs_termination_params,
-      iso_restraints           = adp_restraints_params.iso,
+      iso_restraints           = self.adp_restraints_params.iso,
       verbose                  = 0,
-      target_weights           = target_weights,
-      h_params                 = h_params)
-    r_free_ = fmodels.fmodel_xray().r_free()
-    if(target_weights is not None and target_weights.twp.optimize_wxu):
-      print >> log, "scale= %8.4f total_weight= %8.4f r_free= %6.4f"%(scaler,
-        target_weights.adp_weights_result.wx, r_free_), cntr
-    if(r_free_ < r_free):
-      r_free = r_free_
-      new_scatterers = fmodels.fmodel_xray().xray_structure.scatterers(
-        ).deep_copy()
-      cntr = 0
-    elif(r_free != 1.e+9):
-      cntr += 1
-    if(cntr == 3): break
-    assert fmodels.fmodel_xray().xray_structure is model.xray_structure
-    assert minimized.xray_structure is model.xray_structure
-  #########
-  if(target_weights is not None and target_weights.twp.optimize_wxu):
-    scaler_values = [1./1.5,1./2.,1./2.5,1./3.,1./3.5,1./4.,1./4.5,1./5.]
-    cntr = 0
-    for scaler in scaler_values:
-      if(target_weights is not None and target_weights.twp.optimize_wxu):
-        fmodels.fmodel_xray().xray_structure.replace_scatterers(
-          save_scatterers.deep_copy())
-        fmodels.update_xray_structure(update_f_calc = True)
-        target_weights.adp_weights_result.w = w / scaler
-      is_neutron_scat_table = False
-      if(all_params.main.scattering_table == "neutron"):
-        is_neutron_scat_table = True
-      minimized = minimization.lbfgs(
-        restraints_manager       = model.restraints_manager,
-        fmodels                  = fmodels,
-        model                    = model,
-        refine_adp               = True,
-        lbfgs_termination_params = lbfgs_termination_params,
-        iso_restraints           = adp_restraints_params.iso,
-        verbose                  = 0,
-        target_weights           = target_weights,
-        is_neutron_scat_table    = is_neutron_scat_table,
-        h_params                 = h_params)
-      r_free_ = fmodels.fmodel_xray().r_free()
-      if(target_weights.twp.optimize_wxu):
-        print >> log, "scale= %8.4f total_weight= %8.4f r_free= %6.4f"%(scaler,
-          target_weights.adp_weights_result.wx, r_free_), cntr
-      if(r_free_ < r_free):
-        r_free = r_free_
-        new_scatterers = fmodels.fmodel_xray().xray_structure.scatterers(
-          ).deep_copy()
-        cntr = 0
-      elif(r_free != 1.e+9):
-        cntr += 1
-      if(cntr == 3): break
-      assert fmodels.fmodel_xray().xray_structure is model.xray_structure
-      assert minimized.xray_structure is model.xray_structure
-  #########
-  if(target_weights is not None and target_weights.twp.optimize_wxu):
-    print >> log, "Best r_free =  %6.4f"%r_free
-    fmodels.fmodel_xray().xray_structure.replace_scatterers(new_scatterers)
-    fmodels.update_xray_structure(update_f_calc = True)
-    assert approx_equal(fmodels.fmodel_xray().r_free(), r_free)
-  else: fmodels.update_xray_structure(update_f_calc = True)
-  if(target_weights is not None and not target_weights.twp.optimize_wxu):
-    minimized.monitor.show(message = "LBFGS minimization", log  = log)
-    #monitors.collect(step = str(macro_cycle)+"_adp:") XXX
+      target_weights           = self.target_weights,
+      h_params                 = self.h_params)
+    self.model.xray_structure = self.fmodels.fmodel_xray().xray_structure
+    assert minimized.xray_structure is self.model.xray_structure
+    utils.assert_xray_structures_equal(
+      x1 = minimized.xray_structure,
+      x2 = self.model.xray_structure)
+    return minimized
