247d8a6fad26777d9a891c7a1285243ad1c3f5a8
Nicholas Sauter <nksauter@lbl.gov>
2011-06-29 22:10:06 +0000

    Simple script returns OK status if mp_spotfinder_server is alive.

---
+++ spotfinder/servers/mp_spotfinder_server_is_alive.py
@@ -0,0 +1,14 @@
+import httplib
+
+def get_spotfinder_url(host,port):
+  testurl = "%s:%d"%(host,port)
+  Connection = httplib.HTTPConnection(testurl,strict=True)
+  Connection.putrequest('POST',"/spotfinder")
+  Connection.putheader('Expect',"200")
+  Connection.endheaders()
+  Connection.send("")
+  Response = Connection.getresponse()
+  print Response.status,Response.reason
+  Connection.close()
+
+get_spotfinder_url("localhost",8125)
