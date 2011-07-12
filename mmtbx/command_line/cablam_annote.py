6d009a6c02aefea2d9a77b8d7b81ec0f2a38783a
Christopher J Williams <chrisjwilliams@users.noreply.github.com>
2011-07-12 21:36:35 +0000

    initial check-in of phenix.cablam_annote and phenix.cablam_training commandline access

---
+++ mmtbx/command_line/cablam_annote.py
@@ -0,0 +1,7 @@
+# LIBTBX_SET_DISPATCHER_NAME phenix.cablam_annote
+
+import sys
+from mmtbx.cablam import cablam_annote
+
+if __name__ == "__main__":
+  cablam_annote.run()
---
+++ mmtbx/command_line/cablam_training.py
@@ -0,0 +1,7 @@
+# LIBTBX_SET_DISPATCHER_NAME phenix.cablam_training
+
+import sys
+from mmtbx.cablam import cablam_training
+
+if __name__ == "__main__":
+  cablam_training.run()
