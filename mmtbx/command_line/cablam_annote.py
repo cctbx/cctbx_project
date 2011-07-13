e312b24477b67fc326d31de5a1d1d06cc7d25bdf
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2011-07-13 02:29:18 +0000

    mmtbx/command_line/cablam_*.py: removing unused imports

--- mmtbx/command_line/cablam_annote.py
+++ mmtbx/command_line/cablam_annote.py
@@ -1,6 +1,5 @@
 # LIBTBX_SET_DISPATCHER_NAME phenix.cablam_annote
 
-import sys
 from mmtbx.cablam import cablam_annote
 
 if __name__ == "__main__":
--- mmtbx/command_line/cablam_training.py
+++ mmtbx/command_line/cablam_training.py
@@ -1,6 +1,5 @@
 # LIBTBX_SET_DISPATCHER_NAME phenix.cablam_training
 
-import sys
 from mmtbx.cablam import cablam_training
 
 if __name__ == "__main__":
