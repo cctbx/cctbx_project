02246ffeef23774fccde3944641d40db2b301f1d
Nicholas Sauter <nksauter@lbl.gov>
2011-07-13 03:51:58 +0000

    Spotfinder refactoring:  detector and synchrotron beamline information is now in the iotbx.

---
+++ iotbx/detectors/context/config_detector.py
@@ -0,0 +1,235 @@
+import re,time,copy
+
+#------------------ALS-----------------------
+'''
+Prior to March 2004:
+Beamline 5.0.1 ADSC Q4U  s/n 401
+Beamline 5.0.2 ADSC Q210 s/n 447
+Beamline 5.0.3 ADSC Q4R  s/n 423
+Beamline 8.2.1 ADSC Q210 s/n 445
+Beamline 8.2.2 ADSC Q315 s/n 905
+After March 2004:
+Beamline 5.0.1 ADSC Q210 s/n 447
+Beamline 5.0.2 ADSC Q315 s/n 913
+Beamline 5.0.3 ADSC Q4R  s/n 423
+Beamline 8.2.1 ADSC Q210 s/n 445
+Beamline 8.2.1 ADSC Q315 s/n 925
+Beamline 8.2.2 ADSC Q315 s/n 905
+Beamline 8.3.1 ADSC Q210 s/n 442
+Beamline 12.3.2 ADSC Q315 s/n 907
+After Aug 2006:
+Beamline 5.0.3 ADSC Q315r  s/n 923
+Installed Nov 2009:
+Beamline 5.0.1 ADSC Q315r s/n 931'''
+
+known_als_detectors = [401,423,445,447,905,913,923,925,913]
+known_als831_detectors = [907,442]
+
+def als_beam_rules(iobj): #take an ADSC image object
+
+  beam_center_convention = 1
+  #The only use for this default of 1 is for the swap_beam simulation
+  # in the pipeline server.  All other known ALS images will adhere to
+  # the convention of 0, if the following DENZO overrides are used:
+
+  for tag,search,datatype in [
+          ('BEAM_CENTER_X','DENZO_BEAM_CENTER_X',float),
+          ('BEAM_CENTER_Y','DENZO_BEAM_CENTER_Y',float),
+          ('BEAM_CENTER_X','DENZO_XBEAM',float),
+          ('BEAM_CENTER_Y','DENZO_YBEAM',float),
+          ]:
+          pattern = re.compile(search+'='+r'(.*);')
+          matches = pattern.findall(iobj.header)
+          if len(matches)>0:
+            beam_center_convention = 0
+            iobj.parameters[tag] = datatype(matches[-1])
+  return beam_center_convention
+
+def als_beamline831_rules(iobj):
+  beam_center_convention = 5
+  return beam_center_convention
+
+#-------------------Other synchrotrons-------------------------
+'''
+Denzo = header has special records for Denzo beam
+Beam = 0 is LABELIT default
+Regression = there is a corresponding entry in LABELIT regression suite
+
+Successful Indexing:
+ADSC s/n 402  Web Denzo Beam5 Regression
+ADSC s/n 403  SSRL BL11-3 Beam5
+ADSC s/n 406  CHESS F1, Marian Szebenyi, reverse phi, Beam5
+ADSC s/n 409  CHESS F1 upper upside-down detector, NOT reverse phi, Beam5
+ADSC s/n 410  NSLS X9B
+ADSC s/n 411  SSRL BL1-5 Beam5
+ADSC s/n 413  Web [ESRF ID14-2 Q4] Beam0 Beam_very_close_to_center Regression
+ADSC Q4  414  CHESS F3, Marian Szebenyi, reverse phi or Pringle-Shen, Beam 5
+ADSC s/n 415  APS: BIOCARS 14-BM-C Beam0 (Web case 83995)
+ADSC s/n 418  NSLS X4A
+ADSC s/n 420  Web [ESRF ID14-3 Q4R] Beam0 Regression
+ADSC Q4R 428  Web Beam0 Regression ESRF
+ADSC s/n 429  Web Beam0 (no strong evidence for 0) Regression
+ADSC 210 441  CHESS A1, Marian Szebenyi, reverse phi after 10/2004, Beam5
+ADSC 210 443  APS IMCA-CAT 17ID, Xaiochun Yang, Beam0
+ADSC 210 446  BNL X6A, Vivian Stojanoff, Beam5
+ADSC 210 448  CHESS F2, Marian Szebenyi, reverse phi, Beam5
+ADSC 210r 457 Australian Synchrotron Tom Caradoc-Davies, reverse phi, beam on center
+ADSC 270 471  CHESS F1, Marian Szebenyi, reverse phi, Beam5
+ADSC 210 901  SSRL BL9-2 Beam5
+ADSC s/n 902  SSRL BL11-1 Beam5
+ADSC s/n 903  Web unknown location Beam5
+ADSC s/n 904  APS NE-CAT [24-ID-C or 24-BM-B] Beam0 (JCSG 2003Nov beam not really tested; close to center)
+ADSC s/n 908  SSRL BL9-1 Beam5
+ADSC s/n 910  APS BioCARS 14-BM-C installed before August 2007.
+ADSC s/n 911  APS [24-ID-C or 24-BM-B] (NE-CAT), Beam0
+ADSC s/n 914  APS ID19
+ADSC s/n 916  APS 24-ID-E (NE-CAT), Beam0
+ADSC s/n 917  [ESRF ID23-1 Q315]
+ADSC s/n 918  [ESRF ID29   Q315]
+ADSC s/n 919  [ESRF ID14-4 Q315]
+ADSC s/n 924  ESRF BM30:A French Beamline. Reverse Phi
+ADSC 315r 928 Australian Synchrotron 3ID microfocus, Tom Caradoc-Davis, reverse phi, beam on center
+              Looking from the source towards the detector our goniometer is horizontal
+              and on the left-hand side of the beam. Facing the goniometer, a positive
+              rotation turns the air-bearing clockwise.
+Unsuccessful:
+ADSC s/n 415  Web Regression Submitted virus images have wrong center - off by mm
+                  Impossible to determine convention.  Subsequent images indexed
+                  correctly; see above.
+ADSC s/n 416  Web Beam center off by many cm; no indexing possible
+ADSC s/n 444  Web Error upon image read
+ADSC s/n 444  [ESRF ID14-1 Q210]
+ADSC s/n 910  APS ID19 Beam0 beam perfectly centered, but it looks like
+                  the phi axis is vertical. Not tested. Beginning in 2005,
+                  ID19 had detector s/n 914.
+
+Olof Svenson: all ESRF beamlines have the same coordinate system.
+detector serial numbes listed at
+http://www.esrf.fr/UsersAndScience/Experiments/MX/Software/PXSOFT/Denzo/
+ID23-2 Mar 225, serial #5
+ID13   Mar 133, serial #8
+'''
+
+def ADSC910_at_BioCARS(iobj):
+  if iobj.serial_number != 910: return False
+  record_date = iobj.parameters["DATE"]
+  record_tse = time.mktime(time.strptime(record_date))
+  #pending further information, assume 910 at BioCARS beginning in 2007
+  cutoff_this = time.mktime(time.strptime("Mon Jan 01 00:00:00 2007"))
+  return record_tse > cutoff_this
+
+def other_beamlines(iobj,passthru_convention):
+  beam5 = [402,403,406,409,410,411,414,418,441,446,448,471,901,902,903,908]
+  beam0 = [413,415,420,428,429,443,444,457,904,914,916,917,918,919,924,928]
+  alld = beam5+beam0+known_als_detectors+known_als831_detectors
+  if iobj.serial_number in beam5:
+    beam_center_convention = 5
+  elif ADSC910_at_BioCARS(iobj):
+    beam_center_convention = 0
+  elif iobj.serial_number not in alld:
+    print "WARNING (possibly fatal): new beamline; coordinate system unknown. Please contact the authors"
+    beam_center_convention = passthru_convention
+  else:
+    beam_center_convention = passthru_convention
+  return beam_center_convention
+
+reference_information_ADSC_detectors = [
+{'type':'Q4U', 'pixels_unbinned':10616832, 'pixels_binned':2654208},
+{'type':'Q4R', 'pixels_unbinned':10616832, 'pixels_binned':2654208},
+{'type':'Q210', 'pixels_unbinned':33554432, 'pixels_binned':8388608},
+{'type':'Q315', 'pixels_unbinned':75497472, 'pixels_binned':18874368},
+]
+
+def set_convention(value,phil_params):
+  if phil_params.convention_override != None:
+     phil_params.spot_convention = copy.copy(
+      phil_params.convention_override)
+  else:
+    phil_params.spot_convention = value
+
+def beam_center_convention_from_image_object(imageobject,phil_params):
+
+    if imageobject.vendortype == "ADSC":
+      if imageobject.serial_number in known_als_detectors:
+        beam_center_convention = als_beam_rules(imageobject)
+      else:
+        beam_center_convention = 0
+      set_convention(0,phil_params)
+      if imageobject.serial_number in known_als831_detectors:
+        beam_center_convention = als_beamline831_rules(imageobject)
+
+      beam_center_convention = other_beamlines(imageobject,
+        passthru_convention = beam_center_convention)
+
+    elif imageobject.vendortype == "CBF":
+      beam_center_convention = 0
+      set_convention(0,phil_params)
+
+    elif imageobject.vendortype == "MacScience":
+      beam_center_convention = 0
+      set_convention(0,phil_params)
+
+    elif imageobject.vendortype == "Bruker Proteus CCD":
+      beam_center_convention = 0
+      set_convention(0,phil_params)
+
+    elif imageobject.vendortype == "RigakuSaturn":
+      beam_center_convention = 5
+      set_convention(0,phil_params)
+
+    elif imageobject.vendortype=="MARCCD":
+      '''Explanation: there are only two test datasets:
+         /net/adder/raid1/sauter/marccd/brunzelle  4 5 6 (serial#=='1')
+         /net/adder/raid1/sauter/marccd/flav  1 3 (no serial #)
+           For orientation, When looking at the detector from the source,
+           the beamstop shadow comes in from the right of the image
+           and the beam is about 15 pixels below the center.
+           This is a mosaic image from our 225cm 3x3 detector - It is 3072x3072
+           pixels.The format is exactly the same as the 2k marCCD images -
+           just more pixels. The crystals is flavodoxin and the images were
+           taken at the ESRF BM14 by Martin Walsh
+         ...listed with the beam_center conventions that support these data.
+         Have to choose one, so choose 1.  Later figure out if these
+         were collected at different beamlines'''
+      beam_center_convention = 1
+      set_convention(0,phil_params)
+
+      '''For SSRL's MarCCD, serial number 11, there is a different beam
+      convention.  We assume here SSRL has the only MarCCD with this
+      serial number.  There isn't any information from other beamlines
+      to tell if the SSRL convention is general.'''
+      try:
+        from labelit.detectors.mar import CompleteMarHeader
+        C = CompleteMarHeader(imageobject)
+        #C.dumpHeader()
+        if C.get_serial_number() in ['11']:
+          beam_center_convention = 5
+          #print "Using SSRL Mar CCD beam_center system"
+      except Exception:pass
+
+    elif imageobject.vendortype=="MARIP":
+      beam_center_convention = 0
+      set_convention(0,phil_params)
+      #but note coordinate transformation going to mosflm
+    elif imageobject.vendortype=="RAXIS":
+      beam_center_convention = 2
+      set_convention(2,phil_params)
+
+    else: beam_center_convention = None
+
+    if imageobject.vendortype == "CBF" and \
+       imageobject.size1==2527 and imageobject.size2==2463:
+       imageobject.vendortype = "Pilatus-6M"
+    if imageobject.vendortype == "Pilatus" and \
+       imageobject.size1==1679 and imageobject.size2==1475:
+       imageobject.vendortype = "Pilatus-2M"
+    if imageobject.vendortype in ["Pilatus-6M","Pilatus-2M"]:
+       beam_center_convention = 0
+       set_convention(0,phil_params)
+       if phil_params.distl.minimum_signal_height==None:
+          phil_params.distl.minimum_signal_height=2.5
+       if phil_params.distl.minimum_spot_area==None or \
+          phil_params.distl.minimum_spot_area > 5:
+          phil_params.distl.minimum_spot_area=5
+
+    return beam_center_convention
---
+++ iotbx/detectors/context/endstation.py
@@ -0,0 +1,248 @@
+import math,time
+from iotbx.detectors.context.camera_convention import Cameras
+from iotbx.detectors.context.config_detector import ADSC910_at_BioCARS
+
+class EndStation:
+  def __init__(self):
+    #set defaults
+    self.mos = {}
+    self.mos['mosflm_detector']="""#detector-take defaults\n"""
+    self.mos['mosflm_beamline']=beamlines['ALS']
+    self.set_camera_convention(1)
+    self.set_rotation_axis("ROTATION HORIZ ANTI")
+
+  def set_camera_convention(self,number):
+    self.cam_con = Cameras(number)
+
+  def camera_convention(self,):
+    return self.cam_con
+
+  def set_rotation_axis(self,axis):
+    if type(axis) == type("string"):
+      self.rot_axi = rotation_lookup[axis]
+      self.rot_axi_string = axis
+    else: #tuple type of 3-component vector elements of normalized direction
+      self.rot_axi= axis
+
+  def rotation_axis(self):
+    return self.rot_axi
+
+  def mosflm(self): return self.mos
+
+beamlines = {
+"ALS":
+"""#beam
+SYNCHROTRON POLARIZATION 0.9
+DIVERGENCE 0.100 0.020
+DISPERSION 0.0001
+""",
+"Australian":
+"""#beam
+SYNCHROTRON POLAR 0.9
+DIVERGENCE 0.11 0.001
+DISPER 0.001
+""",
+"CHESS":
+"""#beam
+SYNCHROTRON POLAR 0.89
+DIVE 0.030 0.010
+DISPER 0.0025
+""",
+"RAXIS":
+"""#beam
+""",
+}
+
+rotation_lookup = {
+"ROTATION HORIZ ANTI":(0,1,0),
+"ROTATION HORIZ CLOCK":(0,-1,0), #reverse phi
+"ROTATION VERT ANTI":(-1,0,0),
+"ROTATION VERT CLOCK":(1,0,0),
+}
+#ALS Quantum 4:
+#omega 90 rotation horiz anti fast horiz origin ur rectang type adsc
+#rmin 5 rmax 188.0 ymax 188.0 xmax 188.0 xscan 188.0 yscan 188.0
+
+def EndStation_from_ImageObject(imageobject,phil_params):
+  endstation = EndStation()
+  endstation.set_camera_convention(1)
+  endstation.set_rotation_axis("ROTATION HORIZ ANTI")
+
+  if imageobject.vendortype == "Bruker Proteus CCD":
+     endstation.set_rotation_axis("ROTATION VERT ANTI")
+     print "BRUKER rotation", endstation.rot_axi
+
+  if imageobject.vendortype == "RAXIS":
+     endstation.set_rotation_axis("ROTATION VERT CLOCK")
+
+  #clockwise horizontal phi at most CHESS beamlines
+  #also at Australian Synchrotron
+  if imageobject.vendortype == "ADSC" and \
+     imageobject.serial_number in [406,414,441,448,457,471,924,928]:
+     endstation.set_rotation_axis("ROTATION HORIZ CLOCK")
+
+  if imageobject.vendortype == "ADSC" and imageobject.serial_number == 910:
+    if ADSC910_at_BioCARS(imageobject):
+      endstation.set_rotation_axis("ROTATION HORIZ CLOCK")
+    else:
+      endstation.set_rotation_axis("ROTATION VERT ANTI") # just a hypothesis
+  #vertical phi at APS 19ID
+  if imageobject.vendortype == "ADSC" and \
+     imageobject.serial_number in [914]:
+     endstation.set_rotation_axis("ROTATION HORIZ CLOCK")
+
+  if imageobject.vendortype == "RigakuSaturn":
+     endstation.set_rotation_axis("ROTATION VERT ANTI")
+
+  #change in phi axis rotation at CHESS A1
+  if imageobject.vendortype == "ADSC" and \
+     imageobject.serial_number in [441]:
+       record_date = imageobject.parameters["DATE"]
+       record_tse = time.mktime(time.strptime(record_date))
+       cutoff_441 = time.mktime(time.strptime("Fri Oct 01 00:00:00 2004"))
+       if record_tse < cutoff_441:
+         endstation.set_rotation_axis("ROTATION HORIZ ANTI")
+
+  # Special cases:  Pringle-Shen goniometer at CHESS F3
+  if imageobject.vendortype == "ADSC" and \
+     imageobject.serial_number in [414] and \
+     phil_params.goniometer_rotation.lower().find(
+       'pringle-shen')>=0 and \
+     imageobject.parameters.has_key('AXIS') and \
+     imageobject.parameters['AXIS']=='phi':
+       endstation.set_rotation_axis("ROTATION VERT ANTI")
+       if imageobject.parameters.has_key('OMEGA') and \
+          imageobject.parameters['OMEGA'] != 0.0 :
+          omega = imageobject.parameters['OMEGA']
+          from iotbx.detectors import rotate_vector_around
+          endstation.set_rotation_axis(
+             rotate_vector_around(endstation.rotation_axis(),
+                        (0,1,0),-omega*math.pi/180.)
+          )
+  '''
+  #tested for CHESS F1 s/n 406:
+  SCANNER ROTATION HORIZ CLOCK FAST horizontal ORIGIN UR RECT TYPE ADSC
+  LIMITS RMIN 5 RMAX 137.2 XMAX 96.5 YMAX 97.5 XSCAN 94.0 YSCAN 94.0
+  BACKSTOP RADIUS 7.00 CENTRE 90.100 91.500
+  ADCOFFSET 20
+  NULLPIX 0
+  GAIN 0.300
+  #tested for CHESS F3 Pringle-Shen
+  SCANNER ROTATION VERT ANTI FAST horizontal ORIGIN UR RECT TYPE ADSC
+  LIMITS RMIN 5 RMAX 143.3 XMAX 106.8 YMAX 95.7 XSCAN 94.0 YSCAN 94.0
+  BACKSTOP RADIUS 4.00 CENTRE 106.600 94.200
+  GAIN 0.500
+  BIAS 5
+  '''
+  if imageobject.vendortype == "MacScience":
+    if imageobject.size1==3000:
+      endstation.mos['mosflm_detector'] = """DETECTOR DIP2030\n"""
+      endstation.set_rotation_axis("ROTATION HORIZ CLOCK")
+
+  if imageobject.vendortype == "MARCCD":
+   if imageobject.size1*imageobject.bin>=4096:
+     parameters = {'sz' : imageobject.size1*imageobject.bin,
+                   'pix': imageobject.pixel_size / imageobject.bin}
+     parameters['scan']=parameters['pix']*parameters['sz']/2
+
+     endstation.mos['mosflm_detector'] = """#MARCCD detector
+LIMITS XMIN 0 XMAX xmax_tag YMIN 0 YMAX ymax_tag xscan %(scan)d yscan %(scan)d
+SIZE %(sz)d %(sz)d HEADER 1 byte 4096
+PIXEL %(pix)f
+NULLPIX 0
+#4Kx4K MarCCD format is unknown to MOSFLM, which apparently defaults
+#the nullpix to 10.  This is a problem for weak-background images.
+"""%parameters
+     '''the correct xmax_tag and ymax_tag are added later in the interface module'''
+
+   #identification of specific beamlines at Spring8 with Reversephi:
+     # BL41XU Mar MX225HE--Serial number 40
+     # BL32XU Mar MX225HE--Serial number 31
+   # rely on detector serial number, uncertain how to decode geometric description
+   # of rotation axis within the header.
+   if imageobject.parameters["DETECTOR_SN"] in [31,40]:
+     endstation.set_rotation_axis("ROTATION HORIZ CLOCK")
+     endstation.mos['mosflm_detector'] = """
+# Specific implementation for Spring8 BL41XU Mar MX225HE
+DETECTOR MARCCD
+DETECTOR REVERSEPHI
+SIZE 3072 3072
+PIXEL 0.07324 0.07324
+LIMITS RMIN 2 RMAX 159.1 XMAX 159.1 YMAX 112.5 XSCAN 159.1 YSCAN 159.1
+"""
+     endstation.mos['mosflm_beamline'] = """GAIN 0.37
+POLARISATION 0.99
+DIVE 0.0001 0.00001
+DISPER 0.0001
+!offset 0.0 0.0
+"""
+
+  if imageobject.vendortype == "ADSC" and \
+     endstation.rot_axi_string!="ROTATION HORIZ ANTI":
+
+     # Rough idea of the MOSFLM LIMITS
+     mmsize = imageobject.size1 * imageobject.pixel_size
+     maxx = max( abs(imageobject.beamx-mmsize), abs(imageobject.beamx) )
+     maxy = max( abs(imageobject.beamy-mmsize), abs(imageobject.beamy) )
+     parameters = {'rotation': endstation.rot_axi_string,
+                   'rmax': math.sqrt(maxx*maxx + maxy*maxy),
+                   'xmax': maxx,
+                   'ymax': maxy,
+                   'xscan': mmsize/2.,
+                   'yscan': mmsize/2.}
+     if imageobject.serial_number in [457,928]: parameters['gain']=0.32
+     else: parameters['gain']=0.30
+
+     endstation.mos['mosflm_detector'] = """#detector
+SCANNER %(rotation)s FAST horizontal ORIGIN UR RECT TYPE ADSC
+LIMITS RMIN 5 RMAX %(rmax).1f XMAX %(xmax).1f YMAX %(ymax).1f XSCAN %(xscan).1f YSCAN %(yscan).1f
+GAIN %(gain).3f
+"""%parameters
+
+     if imageobject.serial_number in [457,928]:
+       endstation.mos['mosflm_beamline'] = beamlines['Australian']
+     else:
+       endstation.mos['mosflm_beamline'] = beamlines['CHESS']
+
+  if imageobject.vendortype == "ADSC" and imageobject.serial_number == 910:
+    if ADSC910_at_BioCARS(imageobject):
+      endstation.mos['mosflm_detector']=endstation.mos['mosflm_detector']+\
+       "#BIOCARS 14-BM-C S/N=910"
+
+  if imageobject.vendortype in [ "RAXIS" ]:
+     endstation.mos['mosflm_detector'] = """#detector
+ADCOFFSet 5
+"""
+     if imageobject.serial_number.lower().find('dr. r-axis iv')==0:
+       endstation.mos['mosflm_detector']=endstation.mos['mosflm_detector']+\
+                                         'DETECTOR RAXISIV'
+       #At least for MOSFLM 6.2.4, this seems to be important because it
+       #  allows the program to accept large standard deviations for spots
+       #  (above 32767).  Otherwise MOSFLM can crash with SERIOUS ERROR message.
+
+     # Both DTrek and Raxis formats have "RAXIS" vendortype but only Raxis has "head" attribute
+     if imageobject.__dict__.has_key("head") and \
+        imageobject.head['Device'].lower().find('r-axis2')==0:
+       endstation.mos['mosflm_detector']=endstation.mos['mosflm_detector']+\
+                                         'detector raxis'
+     endstation.mos['mosflm_beamline'] = beamlines['RAXIS']
+
+  if imageobject.vendortype in [ "CBF" ]:
+     endstation.mos['mosflm_detector'] = """#detector cbf"""
+
+  if imageobject.vendortype in [ "Pilatus-6M" ]:
+     endstation.mos['mosflm_detector'] = """#detector Pilatus-6M"""
+     #flags mosflm interface to include the start & angle fix, mosflm 7.0.3 & below
+
+  if imageobject.vendortype in [ "MARIP" ]:
+     endstation.mos['mosflm_detector'] = """#detector
+ADCOFFSet 5
+"""#Just a guess: image plates require an offset to insure stability
+   # over long integration sweeps.  Example: TM0064/11_20_01/1c3p3
+
+  #additional information for universally specifying image size
+  endstation.mos['mosflm_detector'] = endstation.mos['mosflm_detector'] + \
+"\n#UIS_PIXEL %(pix)f\n#UIS_SIZE %(sz)d"%{
+'sz' : imageobject.size1*imageobject.bin,
+'pix': imageobject.pixel_size / imageobject.bin}
+  return endstation
