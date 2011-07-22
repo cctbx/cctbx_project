a4625486d721bb79be71e22bfd44500cb8c9e4d6
Luc J. Bourhis <luc_j_bourhis@mac.com>
2011-07-22 10:41:14 +0000

    Relative difference of mean square displacements for a pair of atoms

--- cctbx/adptbx/boost_python/tst_hirshfeld.py
+++ cctbx/adptbx/boost_python/tst_hirshfeld.py
@@ -1,16 +1,17 @@
-from cctbx import adp_restraints, uctbx, adptbx
+from cctbx import adp_restraints, uctbx, adptbx, sgtbx
 from scitbx import random, linalg, matrix
 from libtbx.test_utils import approx_equal
 
-def exercise(n_trials):
-  site_coord = random.variate(random.uniform_distribution(0, 1))
-  u_eigenval = random.variate(random.uniform_distribution(0.0005, 0.003))
-  g_eigenval = random.variate(random.uniform_distribution(1,10))
-  symm_mat = linalg.random_normal_matrix_generator(3,3)\
-           .symmetric_matrix_with_eigenvalues
-  def as_sym_mat3(packed_u):
-    return matrix.sqr(packed_u.matrix_packed_u_as_symmetric()).as_sym_mat3()
+site_coord = random.variate(random.uniform_distribution(0, 1))
+u_eigenval = random.variate(random.uniform_distribution(0.0005, 0.003))
+g_eigenval = random.variate(random.uniform_distribution(1,10))
+symm_mat = linalg.random_normal_matrix_generator(3,3)\
+         .symmetric_matrix_with_eigenvalues
+def as_sym_mat3(packed_u):
+  return matrix.sqr(packed_u.matrix_packed_u_as_symmetric()).as_sym_mat3()
 
+
+def exercise_mean_square_displacement(n_trials):
   # check adptbx.mean_square_displacement_difference
   # against adp_restraints.rigid_bond_pair
   for i in xrange(n_trials):
@@ -50,8 +51,64 @@ def exercise(n_trials):
     r = (taylor_diff - finite_diff)/finite_diff
     assert abs(r) < 0.001, r
 
+def exercise_hirshfeld_relative_difference(n_trials):
+  operators = [ sgtbx.rt_mx(),
+                sgtbx.rt_mx('-x, y, -z'),
+                sgtbx.rt_mx('y, z, x') ]
+
+  # check against adptbx.mean_square_displacement_difference
+  for op in operators:
+    for i in xrange(n_trials):
+      x1 = matrix.col(site_coord(3))
+      x2 = matrix.col(site_coord(3))
+      u1 = matrix.col(as_sym_mat3(symm_mat(u_eigenval(3))))
+      u2 = matrix.col(as_sym_mat3(symm_mat(u_eigenval(3))))
+      g  = as_sym_mat3(symm_mat(g_eigenval(3)))
+      uc = uctbx.unit_cell(metrical_matrix=g)
+      hirshfeld = adptbx.mean_square_displacement(uc, x1 - matrix.col(op(x2)))
+      h1 = hirshfeld(u1).value
+      h2 = hirshfeld(op(u2)).value
+      r = adptbx.relative_hirshfeld_difference(uc, x1, u1, x2, u2, op).value
+      assert approx_equal(r, 2*(h1 - h2)/(h1 + h2), eps=1e-12)
+
+  # check gradients with finite difference
+  s = 1e-4
+  for op in operators:
+    for i in xrange(n_trials):
+      x1 = matrix.col(site_coord(3))
+      dx1 = matrix.col(site_coord(3))*s
+      x2 = matrix.col(site_coord(3))
+      dx2 = matrix.col(site_coord(3))*s
+      u1 = matrix.col(as_sym_mat3(symm_mat(u_eigenval(3))))
+      du1 = matrix.col(as_sym_mat3(symm_mat(u_eigenval(3))))*s
+      u2 = matrix.col(as_sym_mat3(symm_mat(u_eigenval(3))))
+      du2 = matrix.col(as_sym_mat3(symm_mat(u_eigenval(3))))*s
+      g  = matrix.col(as_sym_mat3(symm_mat(g_eigenval(3))))
+      dg = matrix.col(as_sym_mat3(symm_mat(g_eigenval(3))))*s
+      uc = uctbx.unit_cell(metrical_matrix=g)
+      r = adptbx.relative_hirshfeld_difference(uc, x1, u1, x2, u2, op)
+      uc_p = uctbx.unit_cell(metrical_matrix=g+dg)
+      r_p = adptbx.relative_hirshfeld_difference(uc_p,
+                                                 x1+dx1, u1+du1,
+                                                 x2+dx2, u2+du2, op).value
+      duc_p_params = matrix.col(uc_p.parameters()) - matrix.col(uc.parameters())
+      uc_m = uctbx.unit_cell(metrical_matrix=g-dg)
+      r_m = adptbx.relative_hirshfeld_difference(uc_m,
+                                                 x1-dx1, u1-du1,
+                                                 x2-dx2, u2-du2, op).value
+      duc_m_params = matrix.col(uc_m.parameters()) - matrix.col(uc.parameters())
+      finite_diff = (r_p - r_m)/2
+      eta = tuple(dx1) + tuple(dx2) + tuple(du1) + tuple(du2)
+      taylor_diff = (
+        matrix.col(r.grad_sites_adps).dot(matrix.col(eta))
+        + matrix.col(r.grad_unit_cell_params).dot(
+          (duc_p_params - duc_m_params)/2))
+      r = (taylor_diff - finite_diff)/finite_diff
+      assert abs(r) < 0.001, r
+
 def run():
-  exercise(n_trials=50)
+  exercise_mean_square_displacement(n_trials=50)
+  exercise_hirshfeld_relative_difference(n_trials=50)
   print 'OK'
 
 if __name__ == '__main__':
