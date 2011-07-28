738d7804ebdf547d88dbaae6a9c9841b589f2a6b
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2011-07-28 22:23:58 +0000

    cctbx/miller/reindexing.py: new assistant.show_summary() method; cctbx/libtbx_refresh.py: remove obsolete miller.pyc

--- cctbx/miller/tst_reindexing.py
+++ cctbx/miller/tst_reindexing.py
@@ -22,9 +22,11 @@ def run(args):
     assert matches.pairs().column(0).all_eq(flex.size_t_range(k.size()))
     miis_cb = matches.pairs().column(1)
     assert perm.select(miis).all_eq(miis_cb)
-  for cb_op, perm, inv_perm in zip(ra.cb_ops, ra.perms, ra.inv_perms):
-    check_cb_op_perm(cb_op, perm)
-    check_cb_op_perm(cb_op.inverse(), inv_perm)
+  def check_ra():
+    for cb_op, perm, inv_perm in zip(ra.cb_ops, ra.perms, ra.inv_perms):
+      check_cb_op_perm(cb_op, perm)
+      check_cb_op_perm(cb_op.inverse(), inv_perm)
+  check_ra()
   assert ra.i_j_multiplication_table == [
     [0, 1, 2, 3, 4, 5],
     [1, 2, 0, 4, 5, 3],
@@ -39,6 +41,55 @@ def run(args):
     [3, 4, 5, 0, 2, 1],
     [4, 5, 3, 1, 0, 2],
     [5, 3, 4, 2, 1, 0]]
+  from libtbx.test_utils import show_diff
+  from cStringIO import StringIO
+  sio = StringIO()
+  assert ra.show_summary(out=sio, prefix=": ") is ra
+  assert not show_diff(sio.getvalue(), """\
+: Lattice symmetry: R 3 2 :R (No. 155)
+: Intensity symmetry: P 1 (No. 1)
+:
+: Indexing ambiguities:
+:   k,l,h         3-fold    invariants:    4
+:   l,h,k         3-fold    invariants:    4
+:   -k,-h,-l      2-fold    invariants:    4
+:   -l,-k,-h      2-fold    invariants:    4
+:   -h,-l,-k      2-fold    invariants:    4
+""")
+  #
+  ra = miller.reindexing.assistant(
+    lattice_group=ms.space_group(),
+    intensity_group=ms.space_group(),
+    miller_indices=ra.miller_indices)
+  check_ra()
+  sio = StringIO()
+  assert ra.show_summary(out=sio) is ra
+  assert not show_diff(sio.getvalue(), """\
+Lattice symmetry: R 3 2 :R (No. 155)
+Intensity symmetry: R 3 2 :R (No. 155)
+
+No indexing ambiguity.
+""")
+  assert ra.i_j_multiplication_table == [[0]]
+  assert ra.i_j_inv_multiplication_table == [[0]]
+  #
+  ra = miller.reindexing.assistant(
+    lattice_group=ms.space_group(),
+    intensity_group=sgtbx.space_group_info(symbol="R 3 :R").group(),
+    miller_indices=ra.miller_indices)
+  check_ra()
+  sio = StringIO()
+  assert ra.show_summary(out=sio) is ra
+  assert not show_diff(sio.getvalue(), """\
+Lattice symmetry: R 3 2 :R (No. 155)
+Intensity symmetry: R 3 :R (No. 146)
+
+Indexing ambiguity:
+  -h,-l,-k      2-fold    invariants:    4
+""")
+  assert ra.i_j_multiplication_table == [[0, 1], [1, 0]]
+  assert ra.i_j_inv_multiplication_table == [[0, 1], [1, 0]]
+  #
   print "OK"
 
 if (__name__ == "__main__"):
