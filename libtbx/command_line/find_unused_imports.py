2afc75184240510899531319560487a41d502291
Luc J. Bourhis <luc_j_bourhis@mac.com>
2011-07-31 21:41:12 +0000

    New libtbx.find_unused_imports and supporting library

---
+++ libtbx/command_line/find_unused_imports.py
@@ -0,0 +1,30 @@
+import os, fnmatch
+from libtbx import python_code_parsing
+
+def run(args):
+  if not args: args = [ '.' ]
+  work = set()
+  arg_filenames = []
+  for arg in args:
+    if os.path.isdir(arg):
+      for dirpath, dirnames, filenames in os.walk(arg):
+        work.update( os.path.join(dirpath, f)
+                     for f in fnmatch.filter(filenames, '*.py') )
+    else:
+      arg_filenames.append(arg)
+  work.update(fnmatch.filter(arg_filenames, '*.py'))
+  for filename in work:
+    unused = python_code_parsing.unused_imports(
+      python_source_filename=filename,
+      ignored_imports=('libtbx.load_env'),
+      ignored_imports_from=('__future__',),
+      ignore_imports_flagged_by_comments=('# import dependency',
+                                          '# implicit import'))
+    if unused:
+      print 'In file %s:' % filename
+      print unused
+
+
+if __name__ == '__main__':
+  import sys
+  run(args=sys.argv[1:])
