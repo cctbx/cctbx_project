317a53bcf4226b901cf7fa6a99fa906c3ec4c8a9
Luc J. Bourhis <luc_j_bourhis@mac.com>
2011-08-10 14:52:25 +0000

    Overhaul of origin fixing restraint. Phase 2. Allow different weights to be used for each scatterer. To do so, split the code in two orthogonal parts: (i) alteration of the normal matrix, (ii) computations of the weights for each scatterers by an abstract method in base class to be overriden: 2 alternatives at the moment: homogeneous weights (for debugging, i.e. to keep the existing tests on singular spaces working), atomic number weights (a reasonably robust choice which should fit the bill as compared to ShelX for most structures). Todo: Flack and Schwarzenbach weighting, for the moment a mere ghost shell.

--- smtbx/refinement/restraints/boost_python/origin_fixing.cpp
+++ smtbx/refinement/restraints/boost_python/origin_fixing.cpp
@@ -1,7 +1,13 @@
+#include <boost/python/class.hpp>
+#include <boost/python/return_value_policy.hpp>
+#include <boost/python/return_by_value.hpp>
+#include <boost/python/make_function.hpp>
+
 #include <smtbx/refinement/restraints/origin_fixing.h>
 
 namespace smtbx { namespace refinement { namespace restraints {
   namespace boost_python {
+
 
   template <typename FloatType>
   struct origin_fixing_wrapper
@@ -9,18 +15,38 @@ namespace smtbx { namespace refinement { namespace restraints {
     typedef origin_fixing<FloatType> wt;
     typedef typename wt::scalar_t scalar_t;
 
+    struct origin_fixing_scaffold : wt, boost::python::wrapper<wt>
+    {
+      origin_fixing_scaffold(sgtbx::space_group const &space_group)
+      : wt(space_group)
+      {}
+
+      af::shared<scalar_t>
+      weights(lstbx::normal_equations::linear_ls<scalar_t> &normal_eqns,
+              scitbx::sparse::matrix<scalar_t> const
+              &jacobian_transpose_matching_grad_fc,
+              af::shared<constraints::scatterer_parameters> const &params)
+      {
+        return this->get_override("weights")(normal_eqns,
+                                             jacobian_transpose_matching_grad_fc,
+                                             params);
+      }
+    };
+
     static boost::python::tuple singular_directions(wt const &self) {
       using namespace boost::python;
-      switch (self.singular_directions.size()) {
+      af::small<af::shared<scalar_t>, 3> const
+      &whole_deltas = self.singular_directions();
+      switch (whole_deltas.size()) {
         case 3:
-          return make_tuple(self.singular_directions[0],
-                            self.singular_directions[1],
-                            self.singular_directions[2]);
+          return make_tuple(whole_deltas[0],
+                            whole_deltas[1],
+                            whole_deltas[2]);
         case 2:
-          return make_tuple(self.singular_directions[0],
-                            self.singular_directions[1]);
+          return make_tuple(whole_deltas[0],
+                            whole_deltas[1]);
         case 1:
-          return make_tuple(self.singular_directions[0]);
+          return make_tuple(whole_deltas[0]);
         case 0:
         default:
           return boost::python::tuple();
@@ -30,16 +56,20 @@ namespace smtbx { namespace refinement { namespace restraints {
 
     static void wrap(char const *name) {
       using namespace boost::python;
-      class_<wt>(name, no_init)
-        .def(init<sgtbx::space_group const &,
-                  af::const_ref<constraints::scatterer_parameters> const &,
-                  scalar_t>
-             ((arg("space_group"),
-               arg("all_scatterer_parameters"),
-               arg("relative_weight"))))
+      return_value_policy<return_by_value> rbv;
+      class_<origin_fixing_scaffold, boost::noncopyable>(name, no_init)
+        .def(init<sgtbx::space_group const &>())
         .def("add_to", &wt::add_to,
              (arg("normal_equations"),
-              arg("jacobian_transpose_matching_grad_fc")))
+              arg("jacobian_transpose_matching_grad_fc"),
+              arg("scatterer_parameters")))
+        .def("weights", pure_virtual(&wt::weights),
+             (arg("normal_equations"),
+              arg("jacobian_transpose_matching_grad_fc"),
+              arg("scatterer_parameters")))
+        .add_property("origin_shifts",
+                      make_function(&wt::origin_shifts, rbv))
+        .add_property("has_floating_directions",  &wt::has_floating_directions)
         .add_property("singular_directions", singular_directions)
         ;
     }
@@ -47,7 +77,6 @@ namespace smtbx { namespace refinement { namespace restraints {
 
   void wrap_origin_fixing_restraints() {
     origin_fixing_wrapper<double>::wrap("origin_fixing");
-
   }
 
 
--- smtbx/refinement/restraints/origin_fixing.h
+++ smtbx/refinement/restraints/origin_fixing.h
@@ -1,7 +1,7 @@
 #ifndef SMTBX_REFINEMENT_RESTRAINTS
 #define SMTBX_REFINEMENT_RESTRAINTS
 
-#include <scitbx/math/accumulators.h>
+#include <scitbx/vec3.h>
 #include <scitbx/lstbx/normal_equations.h>
 #include <scitbx/sparse/matrix.h>
 
@@ -19,125 +19,118 @@ namespace smtbx { namespace refinement { namespace restraints {
   /** \brief Restraints to prevent the structure to freely move as a whole
    along the continuous shift directions if any.
 
-   The implementation has a key limitation: the continuous shift directions
-   must lie along unit cell axes if there are atoms on special positions on
-   axes parallel to continuous shift directions.
-   It will therefore work for all space-groups in standard settings
-   but it will not work in, e.g., a setting of R3 with
-   the 3-fold axis along the direction (1,1,1), as in the space group
-   Hall: R 3 (-y+z, x+z, -x+y+z), with an atom at the position (0.5, 0.5, 0.5).
-   As far as we know, this is a limitation of the programs ShelXL
-   and Crystals too. In order to keep it simple, an exception is thrown if
-   a continuous shift directions is found to be non-trivial, whether there are
-   atoms on the wrong kind of special positions or not.
+   The code uses as much as possible the notations of the reference paper.
 
-   If \f$A\f$ is the original normal matrix, the restraint replace \f$A\f$
-   by \f$A + \mu I\f$. The weight \f$\mu\f$ of the restraints is dynamically
-   adjusted to \f$A\f$, so as to fullfill two requirements:
-
-   (a) \f$A + \mu I\f$ is non-singular enough that the L.S. problem can
-   be reliably solved; and
-
-   (b) \f$(A + \mu I)^{-1}\f$ is approximately the pseudo-inverse of \f$A\f$.
-
-   Requirement (b) is essential to obtaining faithful parameter esd's and
-   correlations.
+   Reference:
+   Flack, H. D., and Schwarzenbach, D.
+   On the use of least-square restraints for origin fixing in polar space groups.
+   Acta Cryst. A 44 (1988), 499Ð506.
    */
   template <typename FloatType>
   class origin_fixing
   {
   public:
     typedef FloatType scalar_t;
+    typedef scitbx::vec3<scalar_t> vec3_t;
 
-    scalar_t relative_weight;
-
-    /// Singular directions in the space of crystallographic parameters
-    af::small<af::shared<scalar_t>, 3> singular_directions;
-
-    af::small<scitbx::vec3<scalar_t>, 3> origin_shifts;
-
-    /// A mask pinpointing which parameters span the singular space
-    af::shared<bool> singular_space;
+  private:
+    af::small<vec3_t, 3> deltas;
+    af::small<af::shared<scalar_t>, 3> whole_deltas;
 
   public:
-    /// Construct with the given relative weight
-    /** It precomputes the singular vectors of the normal matrix
-        for the crystallographic parameters.
-     */
-    origin_fixing(
-      sgtbx::space_group const &space_group,
-      af::const_ref<constraints::scatterer_parameters> const &params,
-      scalar_t relative_weight)
 
-      : relative_weight(relative_weight)
+    /// Independent directions in which the whole structure may freely float
+    af::small<vec3_t, 3> const &origin_shifts() const {
+      return deltas;
+    }
+
+    /// Whether there are directions in which a structure may freely float
+    bool has_floating_directions() const {
+      return deltas.size();
+    }
+
+    /// Singular direction of the normal matrix for crystallographic parameters
+    /** That is before the reparametrisation triggered by constraints
+        is applied.
+     */
+    af::small<af::shared<scalar_t>, 3> const &singular_directions() const {
+      return whole_deltas;
+    }
+
+    /** \brief Precomputes the origin shifts for the given space group.
+     */
+    origin_fixing(sgtbx::space_group const &space_group)
     {
-      // Floating origin restraints: pre-compute singular directions
       sgtbx::structure_seminvariants seminvariants(space_group);
-      SMTBX_ASSERT(seminvariants.continuous_shifts_are_principal());
       af::small<sgtbx::ss_vec_mod, 3> const
       &vm = seminvariants.vectors_and_moduli();
       for (int i=0; i<vm.size(); ++i) {
         if (vm[i].m != 0) continue;
         // allowed continuous origin shift:
         scitbx::vec3<scalar_t> v(vm[i].v[0], vm[i].v[1], vm[i].v[2]);
-        origin_shifts.push_back(v);
-
-        // Fill singular_dir with the singular direction corresponding to v
-        af::shared<scalar_t> singular_dir(af::reserve(5*params.size()));
-        std::back_insert_iterator< af::shared<scalar_t> > g(singular_dir);
-        for (std::size_t i=0; i<params.size(); ++i) {
-          BOOST_FOREACH (constraints::asu_parameter const *p,
-                         params[i].ordered())
-          {
-            if (p == params[i].site) std::copy(v.begin(), v.end(), g);
-            else std::fill_n(g, p->size(), scalar_t(0));
-          }
-        }
-
-        // Save it
-        singular_directions.push_back(singular_dir);
-      }
-
-      // Pinpoint coordinates participating in the singularity
-      if (singular_directions.size()) {
-        int n = singular_directions[0].size();
-        singular_space = af::shared<bool>(n, false);
-        for (int i=0; i<n; ++i) {
-          for (int j=0; j<singular_directions.size(); ++j) {
-            singular_space[i] = singular_space[i] || singular_directions[j][i];
-          }
-        }
+        deltas.push_back(v);
       }
     }
+
+    ~origin_fixing()
+    {}
+
+    /// Scatterer weights
+    /** Scatterer weights are the \f$a_m\f$ in the reference paper.
+        The i-th element of scatterer_weights shall be the weight
+        for the scatterer corresponding to the i-th element of params.
+        This member function will be called from member function add_to.
+     */
+    virtual af::shared<scalar_t>
+    weights(lstbx::normal_equations::linear_ls<scalar_t> &normal_eqns,
+            scitbx::sparse::matrix<scalar_t> const
+            &jacobian_transpose_matching_grad_fc,
+            af::shared<constraints::scatterer_parameters> const &params) = 0;
 
     /// Add floating origin restraints to the given normal equations
     /** The Jacobian is that of the reparametrisation of crystallographic
         parameters as function of the independent parameters that the
-        normal equations are built with
+        normal equations are built with.
      */
     void add_to(lstbx::normal_equations::linear_ls<scalar_t> &normal_eqns,
-                scitbx::sparse::matrix<FloatType> const
-                &jacobian_transpose_matching_grad_fc)
+                scitbx::sparse::matrix<scalar_t> const
+                &jacobian_transpose_matching_grad_fc,
+                af::shared<constraints::scatterer_parameters> const &params)
     {
-      if (!relative_weight) return;
-      if (!singular_space.size()) return;
-      af::ref<scalar_t, af::packed_u_accessor>
-      a = normal_eqns.normal_matrix().ref();
-      scitbx::math::accumulator::min_max_accumulator<scalar_t> acc(a(0,0));
-      for (int i=1; i<a.n_rows(); ++i) {
-        if (singular_space[i]) acc(a(i,i));
+      if (!has_floating_directions()) return;
+
+      // construct singular vectors
+      whole_deltas.clear();
+      af::shared<scalar_t>
+      scatterer_weights = weights(normal_eqns,
+                  jacobian_transpose_matching_grad_fc,
+                  params);
+      SMTBX_ASSERT(params.size() == scatterer_weights.size())
+                  (params.size())
+                  (scatterer_weights.size());
+      for (int k=0; k<deltas.size(); ++k) {
+        af::shared<scalar_t> whole_delta(af::reserve(5*params.size()));
+        std::back_insert_iterator< af::shared<scalar_t> > g(whole_delta);
+        for (std::size_t i=0; i<params.size(); ++i) {
+          BOOST_FOREACH (constraints::asu_parameter const *p,
+                         params[i].ordered())
+          {
+            vec3_t v = scatterer_weights[i]*deltas[k];
+            if (p == params[i].site) std::copy(v.begin(), v.end(), g);
+            else std::fill_n(g, p->size(), scalar_t(0));
+          }
+        }
+        whole_deltas.push_back(whole_delta);
       }
-      scalar_t w = relative_weight * acc.max();
-      for (int i=0; i<singular_directions.size(); ++i) {
-        af::shared<scalar_t> reparametrised_singular_dir =
-        jacobian_transpose_matching_grad_fc*singular_directions[i].const_ref();
-        normal_eqns.add_equation(0, reparametrised_singular_dir.const_ref(), w);
+      for (int i=0; i<whole_deltas.size(); ++i) {
+        af::shared<scalar_t> reparametrised_delta =
+        jacobian_transpose_matching_grad_fc*whole_deltas[i].const_ref();
+        normal_eqns.add_equation(0, reparametrised_delta.const_ref(), 1);
       }
     }
   };
 
 }}}
-
 
 
 #endif // Guard
