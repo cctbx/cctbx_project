6ab44406f929f397b308dec7a6a6dfaab1dafc08
Jeffrey Headd <metalheadd@users.noreply.github.com>
2011-09-07 23:04:31 +0000

    added initial DEN source code

---
+++ mmtbx/den/boost_python/den_ext.cpp
@@ -0,0 +1,82 @@
+
+#include <cctbx/boost_python/flex_fwd.h>
+
+#include <boost/python/module.hpp>
+#include <boost/python/def.hpp>
+#include <boost/python/class.hpp>
+#include <boost/python/args.hpp>
+#include <boost/python/return_value_policy.hpp>
+#include <boost/python/return_by_value.hpp>
+#include <boost/optional.hpp>
+#include <scitbx/array_family/boost_python/shared_wrapper.h>
+
+#include <mmtbx/den/den.h>
+
+namespace mmtbx { namespace den {
+namespace {
+
+  void wrap_simple_restraints()
+  {
+    using namespace boost::python;
+    typedef return_value_policy<return_by_value> rbv;
+    typedef den_simple_proxy w_t;
+    class_<w_t>("den_simple_proxy", no_init)
+      .def(init<
+        af::tiny<unsigned, 2> const&, double, double, double >((
+          arg("i_seqs"),
+          arg("eq_distance"),
+          arg("eq_distance_start"),
+          arg("weight"))))
+      .add_property("i_seqs", make_getter(&w_t::i_seqs, rbv()))
+      .def_readwrite("eq_distance", &w_t::eq_distance)
+      .def_readwrite("eq_distance_start", &w_t::eq_distance_start)
+      .def_readwrite("weight", &w_t::weight)
+    ;
+    {
+      typedef return_internal_reference<> rir;
+      scitbx::af::boost_python::shared_wrapper<den_simple_proxy, rir>::wrap(
+        "shared_den_simple_proxy");
+    }
+
+    def("den_simple_residual_sum",
+      (double(*)(
+        af::const_ref<scitbx::vec3<double> > const&,
+        af::const_ref<den_simple_proxy> const&,
+        af::ref<scitbx::vec3<double> > const&,
+        double,
+        double))
+      den_simple_residual_sum, (
+      arg("sites_cart"),
+      arg("proxies"),
+      arg("gradient_array"),
+      arg("den_weight")=1.0));
+
+    def("den_update_eq_distances",
+    (void(*)(
+      af::const_ref<scitbx::vec3<double> > const&,
+      af::ref<den_simple_proxy> const&,
+      double,
+      double))
+    den_update_eq_distances, (
+    arg("sites_cart"),
+    arg("proxies"),
+    arg("gamma"),
+    arg("kappa")));
+  }
+} // namespace anonymous
+
+namespace boost_python {
+
+  void wrap_den() {
+    wrap_simple_restraints();
+  }
+
+} // namespace boost_python
+
+}} //namespace mmtbx::den
+
+BOOST_PYTHON_MODULE(mmtbx_den_restraints_ext)
+{
+  mmtbx::den::boost_python::wrap_den();
+}
+
---
+++ mmtbx/den/den.h
@@ -0,0 +1,92 @@
+#include <mmtbx/error.h>
+#include <cctbx/geometry_restraints/bond.h>
+#include <cctbx/geometry/geometry.h>
+
+#include <cmath>
+#include <set>
+#include <iostream>
+
+namespace mmtbx { namespace den {
+
+  namespace af = scitbx::af;
+  using cctbx::geometry_restraints::bond;
+  using cctbx::geometry_restraints::bond_simple_proxy;
+
+  struct den_simple_proxy : bond_simple_proxy
+  {
+    den_simple_proxy() {}
+
+    den_simple_proxy(
+      af::tiny<unsigned, 2> const& i_seqs_,
+      double eq_distance_,
+      double eq_distance_start_,
+      double weight_)
+    :
+      i_seqs(i_seqs_),
+      eq_distance(eq_distance_),
+      eq_distance_start(eq_distance_start_),
+      weight(weight_)
+    {
+      MMTBX_ASSERT((eq_distance > 0) && (eq_distance_start > 0));
+    }
+
+    af::tiny<unsigned, 2> i_seqs;
+    double eq_distance;
+    double eq_distance_start;
+    double weight;
+  };
+  
+  inline
+  double
+  den_simple_residual_sum(
+    af::const_ref<scitbx::vec3<double> > const& sites_cart,
+    af::const_ref<den_simple_proxy> const& proxies,
+    af::ref<scitbx::vec3<double> > const& gradient_array,
+    double den_weight=1.0)
+  {
+    double residual_sum = 0;
+    double slack = 0.0;
+    for (std::size_t i = 0; i < proxies.size(); i++) {
+      den_simple_proxy proxy = proxies[i];
+      af::tiny<scitbx::vec3<double>, 2> sites;
+      af::tiny<unsigned, 2> const& i_seqs = proxy.i_seqs;
+      sites[0] = sites_cart[ i_seqs[0] ];
+      sites[1] = sites_cart[ i_seqs[1] ];
+      bond restraint(sites, proxy.eq_distance, proxy.weight, slack);
+      double residual = restraint.residual();
+      double grad_factor = den_weight;
+      residual_sum += residual;
+      if (gradient_array.size() != 0) {
+        af::tiny<scitbx::vec3<double>, 2> gradients = restraint.gradients();
+        gradient_array[ i_seqs[0] ] += gradients[0] * grad_factor;
+        gradient_array[ i_seqs[1] ] += gradients[1] * grad_factor;
+      }
+    }
+    return residual_sum;
+  }
+
+  inline
+  void
+  den_update_eq_distances(
+    af::const_ref<scitbx::vec3<double> > const& sites_cart,
+    af::ref<den_simple_proxy> const& proxies,
+    double gamma,
+    double kappa)
+  {
+    double slack = 0.0;
+    for (std::size_t i = 0; i < proxies.size(); i++) {
+      den_simple_proxy proxy = proxies[i];
+      af::tiny<scitbx::vec3<double>, 2> sites;
+      af::tiny<unsigned, 2> const& i_seqs = proxy.i_seqs;
+      sites[0] = sites_cart[ i_seqs[0] ];
+      sites[1] = sites_cart[ i_seqs[1] ];
+      bond restraint(sites, proxy.eq_distance, proxy.weight, slack);
+      double distance_model = restraint.distance_model;
+      double new_eq_dist = ((1.0-kappa)*proxy.eq_distance) + 
+                           ( kappa * 
+                             ( (gamma*distance_model) +
+                               (1.0-gamma)*proxy.eq_distance_start ));
+      proxies[i].eq_distance = new_eq_dist;
+    }
+  }
+}}
