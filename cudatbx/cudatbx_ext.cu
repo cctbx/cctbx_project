69556d139bac1eba4c75b2ec2bcebf5e7bd62204
Billy Poon <bkpoon@lbl.gov>
2011-09-13 21:26:03 +0000

    Initial setup for toolbox (missed file)

---
+++ cudatbx/cudatbx_ext.cu
@@ -0,0 +1,24 @@
+#include <boost/python/module.hpp>
+#include <boost/python/def.hpp>
+#include <boost/python/args.hpp>
+
+#include <cudatbx/cuda_base.h>
+
+namespace cudatbx {
+namespace {
+
+namespace boost_python {
+
+  void wrap_functions()
+  {
+    boost::python::def("number_of_gpus",&number_of_gpus);
+    boost::python::def("reset_gpu",&reset_gpu);
+  }
+
+}}
+
+BOOST_PYTHON_MODULE(cudatbx_ext)
+{
+  cudatbx::boost_python::wrap_functions();
+}
+}
