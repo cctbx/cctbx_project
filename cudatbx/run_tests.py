99c395a78619f8d5111ea2c183799ca733d18df0
Billy Poon <bkpoon@lbl.gov>
2011-09-14 20:59:20 +0000

    Added run_tests.py

---
+++ cudatbx/run_tests.py
@@ -0,0 +1,15 @@
+from libtbx import test_utils
+import libtbx.load_env
+
+def run():
+  tst_list = (
+  "$D/math/special_functions/tst_spherical_bessel_jn.py",
+  )
+
+  build_dir = libtbx.env.under_build("cudatbx")
+  dist_dir = libtbx.env.dist_path("cudatbx")
+
+  test_utils.run_tests(build_dir, dist_dir, tst_list)
+
+if (__name__ == "__main__"):
+  run()
