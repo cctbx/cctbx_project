f2ef77e35ce0a8bac40e57feffca3fa5641ffafa
Richard Gildea <richard.gildea@diamond.ac.uk>
2011-09-15 03:43:23 +0000

    ucif/utils.h: bug fix: print only first 20 characters of input after error

--- ucif/utils.h
+++ ucif/utils.h
@@ -398,8 +398,7 @@ lexer_displayRecognitionError (pANTLR3_BASE_RECOGNIZER recognizer, pANTLR3_UINT8
       {
         message << "near char(" << std::hex << (ANTLR3_UINT8)(ex->c) << ") :\n";
       }
-      message << "\t" << std::setprecision(20) << (pANTLR3_UINT8)(ex->index) << "\n";
-      message << "\t" << std::setprecision(std::min(width, 20)) << (pANTLR3_UINT8)(ex->index) << "\n";
+      message << "\t" << std::string((const char*)(ex->index), std::min(width, 20)) << "\n";
     }
     else
     {
@@ -414,7 +413,7 @@ lexer_displayRecognitionError (pANTLR3_BASE_RECOGNIZER recognizer, pANTLR3_UINT8
       if (width >= 1)
       {
         message << "looks like this:\n\t\t"
-                << std::setprecision(std::min(width, 20))
+                << std::string((const char*)(ex->index), std::min(width, 20))
                 << (pANTLR3_UINT8)(lexer->rec->state->tokenStartCharIndex)
                 << "\n";
       }
