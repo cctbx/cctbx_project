c8a3c1d18c859fbb77ccdfe46462501ec2e73b5d
Nathaniel Echols <nathaniel.echols@gmail.com>
2011-09-22 19:10:08 +0000

    command to execute a pickled callable

---
+++ libtbx/command_line/run_pickled_function.py
@@ -0,0 +1,14 @@
+
+from libtbx import easy_pickle
+import os
+import sys
+
+def run (args) :
+  assert (len(args) > 0)
+  assert os.path.isfile(args[0])
+  pickle_file = args[0]
+  func = easy_pickle.load(pickle_file)
+  return func()
+
+if (__name__ == "__main__") :
+  run(sys.argv[1:])
