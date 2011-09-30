e682574b291e24648b728d46e9127e587e83daa1
Richard Gildea <richard.gildea@diamond.ac.uk>
2011-09-30 20:01:43 +0000

    add compilation of ucif/examples to tests

--- ucif/examples/build_cif_parser.bat
+++ ucif/examples/build_cif_parser.bat
@@ -1,7 +1,9 @@
-SET LIBTBX_ANTLR3=..\..\..\antlr3
+if not defined LIBTBX_ANTLR3 SET LIBTBX_ANTLR3=..\..\..\antlr3
+if not defined LIBTBX_UCIF SET LIBTBX_UCIF=..\
 cl /DNOMINMAX /EHsc /DANTLR3_NODEBUGGER /Fe./cif_parser ^
-/I..\..\ /I %LIBTBX_ANTLR3%\include\ /I%LIBTBX_ANTLR3%\ ^
-main.cpp ..\cifLexer.cpp ..\cifParser.cpp ..\cifWalker.cpp ^
+/I%LIBTBX_UCIF%\..\ /I%LIBTBX_ANTLR3%\include\ /I%LIBTBX_ANTLR3%\ ^
+%LIBTBX_UCIF%\examples\main.cpp %LIBTBX_UCIF%\cifLexer.cpp ^
+%LIBTBX_UCIF%\cifParser.cpp %LIBTBX_UCIF%\cifWalker.cpp ^
   "%LIBTBX_ANTLR3%\src\antlr3baserecognizer.c" ^
   "%LIBTBX_ANTLR3%\src\antlr3basetree.c" ^
   "%LIBTBX_ANTLR3%\src\antlr3basetreeadaptor.c" ^
