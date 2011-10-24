f67e609b03124b9c833cdc1423d9d792fe10ad28
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2011-10-24 04:56:46 +0000

    fable/test/sf_times.py: multiple gcc versions, with and without Intel Math Libs

--- fable/test/sf_times.py
+++ fable/test/sf_times.py
@@ -1,15 +1,31 @@
 from libtbx.test_utils import approx_equal
 from libtbx.utils import Usage
 from libtbx import easy_run
-from libtbx import easy_pickle
-from libtbx.path import full_command_path
 import libtbx.load_env
 import platform
+import time
 import sys, os
 op = os.path
 
 __this_script__ = "cctbx_project/fable/test/sf_times.py"
        # based on  cctbx_project/compcomm/newsletter09/sf_times.py
+
+setup_dir = "/net/cci/setup/Linux"
+
+ifort_versions = ["intel111.sh"]
+
+icc_versions = [
+  "icc91.sh",
+  "icc101.sh",
+  "intel111.sh",
+  "icc121.sh"]
+
+gcc_versions = [
+  "gcc-4.2.4_fc8.sh",
+  "gcc-4.3.6_fc8.sh",
+  "gcc-4.4.6_fc8.sh",
+  "gcc-4.5.3_fc8.sh",
+  "gcc-4.6.1_fc8.sh"]
 
 fortran_template = r"""C %(this_script)s
 
@@ -162,21 +178,36 @@ def compare_with_cctbx_structure_factors(n_scatt, n_refl, output_lines):
   for f1,f2 in zip(fc.data(), fc2.data()):
     assert approx_equal(f1, f2, eps=1e-5)
 
-def build_run(a_out, n_scatt, n_refl, build_cmd, check_max_a_b):
+def build_run(
+      setup_cmd, ld_preload_flag, n_scatt, n_refl, build_cmd, check_max_a_b):
   if (op.isfile("a.out")):
     os.remove("a.out")
   assert not op.isfile("a.out")
-  if (a_out is None):
-    buffers = easy_run.fully_buffered(command=build_cmd)
-    if (buffers.format_errors_if_any() is not None):
-      return None
-  else:
-    open("a.out", "w").write(a_out)
-    os.chmod("a.out", 0755)
+  buffers = easy_run.fully_buffered(command=build_cmd)
+  msg = buffers.format_errors_if_any()
+  if (msg is not None):
+    if (0):
+      print build_cmd
+      print
+      print msg
+      print
+      STOP()
+    return None
   assert op.isfile("a.out")
-  run_cmd = "/usr/bin/time -p ./a.out"
+  run_cmd = setup_cmd
+  if (ld_preload_flag):
+    run_cmd += 'env LD_PRELOAD='\
+    '"/net/marbles/raid1/rwgk/dist/opt_resources/linux64/libimf.so:"'\
+    '"/net/marbles/raid1/rwgk/dist/opt_resources/linux64/libirc.so" '
+  run_cmd += '/usr/bin/time -p ./a.out'
   buffers = easy_run.fully_buffered(command=run_cmd)
-  assert len(buffers.stderr_lines) == 3
+  if (len(buffers.stderr_lines) != 3):
+    print "v"*79
+    print "\n".join(buffers.stderr_lines)
+    print "^"*79
+    raise RuntimeError(
+      "Unexpected number of output lines"
+      " (3 expected; acutal output see above).")
   if (n_scatt <= 10 and n_refl <= 100):
     assert len(buffers.stdout_lines) == n_scatt + n_refl
   else:
@@ -201,30 +232,30 @@ def build_run(a_out, n_scatt, n_refl, build_cmd, check_max_a_b):
   return utime
 
 def write_build_run(
-      a_out, n_scatt, n_refl, real, lang, build_cmd, replace_cos, replace_exp):
-  if (a_out is None):
-    this_script = __this_script__
-    for_txt = fortran_template % vars()
-    if (replace_cos):
-      for_txt = for_txt.replace(
-        "COS(arg)",
-        "arg / (abs(arg)+1.0)")
-    if (replace_exp):
-      for_txt = for_txt.replace(
-        "EXP(arg)",
-        "max(0.0, 1.0 - arg*arg)")
-    for_txt = for_txt.replace("REAL", real)
-    open("tmp.f", "w").write(for_txt)
-    from fable import cout
-    cpp_txt = cout.process(
-      file_names=["tmp.f"],
-      namespace="sf_test",
-      fem_do_safe=False,
-      inline_all=True)
-    open("tmp.cpp", "w").write("\n".join(cpp_txt)+"\n")
-  if (lang == "for"):
+      setup_cmd, ld_preload_flag, n_scatt, n_refl, real, lang, build_cmd,
+      replace_cos, replace_exp):
+  this_script = __this_script__
+  for_txt = fortran_template % vars()
+  if (replace_cos):
+    for_txt = for_txt.replace(
+      "COS(arg)",
+      "arg / (abs(arg)+1.0)")
+  if (replace_exp):
+    for_txt = for_txt.replace(
+      "EXP(arg)",
+      "max(0.0, 1.0 - arg*arg)")
+  for_txt = for_txt.replace("REAL", real)
+  open("tmp.f", "w").write(for_txt)
+  from fable import cout
+  cpp_txt = cout.process(
+    file_names=["tmp.f"],
+    namespace="sf_test",
+    fem_do_safe=False,
+    inline_all=True)
+  open("tmp.cpp", "w").write("\n".join(cpp_txt)+"\n")
+  if (lang.lower() == "f"):
     build_cmd += " tmp.f"
-  elif (lang == "cpp"):
+  elif (lang.lower() == "c"):
     from fable import simple_compilation
     comp_env = simple_compilation.environment()
     build_cmd += " " \
@@ -233,7 +264,8 @@ def write_build_run(
   else:
     raise RuntimeError('Unknown lang: "%s"' % lang)
   return build_run(
-    a_out=a_out,
+    setup_cmd=setup_cmd,
+    ld_preload_flag=ld_preload_flag,
     n_scatt=n_scatt,
     n_refl=n_refl,
     build_cmd=build_cmd,
@@ -242,151 +274,121 @@ def write_build_run(
 def run_combinations(
       compiler_versions,
       all_utimes,
-      a_out_archive,
-      write_a_out_archive,
       n_scatt,
       n_refl,
       compiler_build_opts_list,
       real_list):
-  for lang,compiler,build_opts in compiler_build_opts_list:
-    if (write_a_out_archive):
-      have_compiler = (full_command_path(command=compiler) is not None)
-      if (not have_compiler):
-        compiler_version = "n/a"
+  for lang,setup_sh_list,compiler,build_opts in compiler_build_opts_list:
+    for setup_sh in setup_sh_list:
+      if (setup_sh is None):
+        setup_cmd = ""
       else:
-        compiler_version = easy_run.fully_buffered(
-          command=compiler+" --version",
-          join_stdout_stderr=True).stdout_lines[0]
-      compiler_versions.append(compiler_version)
-      a_out = None
-    build_cmd = " ".join([compiler, build_opts])
-    print build_cmd
-    utimes = []
-    for real in real_list:
-      print "  %s" % real
-      for replace_cos in [False, True]:
-        print "    replace_cos", replace_cos
-        for replace_exp in [False, True]:
-          print "      replace_exp", replace_exp
-          sys.stdout.flush()
-          a_out_key = (build_cmd, replace_cos, replace_exp)
-          if (not write_a_out_archive):
-            compiler_version, a_out = a_out_archive.get(a_out_key, (
-              "n/a", None))
-          if (compiler_version != "n/a"):
-            utime = write_build_run(
-              a_out=a_out,
-              n_scatt=n_scatt,
-              n_refl=n_refl,
-              real=real,
-              lang=lang,
-              build_cmd=build_cmd,
-              replace_cos=replace_cos,
-              replace_exp=replace_exp)
-            if (utime is not None):
-              print "        %4.2f" % utime
-              if (write_a_out_archive):
-                a_out_archive[a_out_key] = (
-                  compiler_version,
-                  open("a.out", "rb").read())
-            else:
-              utime = -1.0
-              print "        err"
-          else:
-            utime = -1.0
-            print "        n/a"
-          utimes.append(utime)
-          sys.stdout.flush()
-    all_utimes.append((utimes, build_cmd))
-
-def gcc_static_is_available():
-  tmp_cpp = """\
-#include <iostream>
-#include <cmath>
-int
-main()
-{
-  std::cout << static_cast<int>(std::cos(0.0)) << std::endl;
-  return 0;
-}
-"""
-  open("tmp.cpp", "w").write(tmp_cpp)
-  buffers = easy_run.fully_buffered(command="g++ -O tmp.cpp")
-  buffers.raise_if_errors_or_output()
-  buffers = easy_run.fully_buffered(command="g++ -static -O tmp.cpp")
-  if (len(buffers.stderr_lines) != 0):
-    return False
-  return True
+        setup_cmd = ". %s/%s; " % (setup_dir, setup_sh)
+      compiler_version = easy_run.fully_buffered(
+        command=setup_cmd+compiler+" --version",
+        join_stdout_stderr=True).stdout_lines[0]
+      if (lang in ["f", "c"]):
+        ld_preload_flags = [False, True]
+      else:
+        ld_preload_flags = [False]
+      for ld_preload_flag in ld_preload_flags:
+        iml = ["", " Intel Math Lib"][int(ld_preload_flag)]
+        compiler_versions.append(compiler_version + iml)
+        build_cmd = " ".join([setup_cmd+compiler, build_opts])
+        print build_cmd
+        utimes = []
+        for real in real_list:
+          print "  %s" % real
+          for replace_cos in [False, True]:
+            print "    replace_cos", replace_cos
+            for replace_exp in [False, True]:
+              print "      replace_exp", replace_exp
+              sys.stdout.flush()
+              if (compiler_version != "n/a"):
+                utime = write_build_run(
+                  setup_cmd=setup_cmd,
+                  ld_preload_flag=ld_preload_flag,
+                  n_scatt=n_scatt,
+                  n_refl=n_refl,
+                  real=real,
+                  lang=lang,
+                  build_cmd=build_cmd,
+                  replace_cos=replace_cos,
+                  replace_exp=replace_exp)
+                if (utime is not None):
+                  print "        %4.2f" % utime
+                else:
+                  utime = -1.0
+                  print "        err"
+              else:
+                utime = -1.0
+                print "        n/a"
+              utimes.append(utime)
+              sys.stdout.flush()
+        all_utimes.append((utimes, build_cmd + iml))
 
 def usage():
-  raise Usage(
-    "fable.python sf_times.py unit_test|quick|production|a_out_archive.pickle")
+  raise Usage("fable.python sf_times.py unit_test|quick|production")
 
 def run(args):
   if (len(args) != 1): usage()
+  t_start = time.time()
   build_platform = platform.platform()
   build_node = platform.node()
   compiler_versions = []
-  gcc_static = "-static "
-  a_out_archive = {}
-  write_a_out_archive = True
   if (args[0] == "unit_test"):
     n_scatt, n_refl = 10, 100
   elif (args[0] == "quick"):
     n_scatt, n_refl = 100, 1000
   elif (args[0] == "production"):
     n_scatt, n_refl = 2000, 20000
-  elif (op.isfile(args[0])):
-    n_scatt, n_refl, \
-    build_platform, build_node, \
-    compiler_versions, gcc_static, a_out_archive = easy_pickle.load(
-      file_name=args[0])
-    write_a_out_archive = False
   else:
     usage()
-  if (write_a_out_archive and not gcc_static_is_available()):
-    gcc_static = ""
+  gcc_sh = [None] + gcc_versions
+  icc_sh = icc_versions
+  if (args[0] == "quick"):
+    gcc_sh = gcc_sh[:2]
+    icc_sh = icc_sh[:1]
   all_utimes = []
   run_combinations(
     compiler_versions,
     all_utimes,
-    a_out_archive=a_out_archive,
-    write_a_out_archive=write_a_out_archive,
     n_scatt=n_scatt,
     n_refl=n_refl,
     compiler_build_opts_list=[
-      ("for", "ifort", "-O"),
-      ("for", "gfortran", gcc_static+"-O -ffast-math"),
-      ("for", "gfortran", gcc_static+"-O -ffast-math -march=native"),
-      ("for", "g77", gcc_static+"-O -ffast-math"),
-      ("cpp", "icpc", "-static -O"),
-      ("cpp", "g++", gcc_static+"-O -ffast-math"),
-      ("cpp", "g++", gcc_static+"-O -ffast-math -march=native")],
+      ("F", ifort_versions, "ifort", "-O"),
+      ("f", gcc_sh, "gfortran", "-O -ffast-math"),
+      ("f", gcc_sh, "gfortran", "-O -ffast-math -march=native"),
+      ("C", icc_sh, "icpc", "-O"),
+      ("c", gcc_sh, "g++", "-O -ffast-math"),
+      ("c", gcc_sh, "g++", "-O -ffast-math -march=native"),
+      ("c", [None], "clang++", "-O -U__GXX_WEAK__ -ffast-math"),
+      ("c", [None], "clang++", "-O -U__GXX_WEAK__ -ffast-math -march=native")],
     real_list=["real*4", "real*8"])
-  if (write_a_out_archive and len(a_out_archive) != 0):
-    print "Writing file: a_out_archive.pickle"
-    easy_pickle.dump(
-      file_name="a_out_archive.pickle",
-      obj=(
-        n_scatt,
-        n_refl,
-        build_platform,
-        build_node,
-        compiler_versions,
-        gcc_static,
-        a_out_archive))
   print
   print "current_platform:", platform.platform()
   print "current_node:", platform.node()
   print "build_platform:", build_platform
   print "build_node:", build_node
-  print 'gcc_static: "%s"' % gcc_static
   for compiler_version in compiler_versions:
     print "compiler:", compiler_version
   print "n_scatt * n_refl: %d * %d" % (n_scatt, n_refl)
+  print '''\
+"s" or "d": single-precision or double-precision floating-point variables
+"E" or "e": using the library exp(arg) function or "max(0.0, 1.0 - arg*arg)"
+"C" or "c": using the library cos(arg) function or "arg / (abs(arg)+1.0)"'''
+  print "  sEC    seC    sEc    sec    dEC    deC    dEc    dec"
+  useful_utimes = []
   for utimes,build_cmd in all_utimes:
     if (max(utimes) != -1.0):
       print " ".join(["%6.2f" % u for u in utimes]), build_cmd
+      useful_utimes.append((utimes,build_cmd))
+  if (len(useful_utimes) > 1):
+    print "Relative to first:"
+    for utimes,build_cmd in useful_utimes:
+      print " ".join(["%6.2f" % (u/max(u0,0.01))
+        for u,u0 in zip(utimes,useful_utimes[0][0])]), build_cmd
+  print "Wall clock time: %.2f s" % (time.time()-t_start)
 
 if (__name__ == "__main__"):
   run(args=sys.argv[1:])
