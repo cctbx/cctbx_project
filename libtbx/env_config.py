08775adce4737758eb655390f31db5ca4e58f2ec
Luc J. Bourhis <luc_j_bourhis@mac.com>
2011-11-05 17:40:16 +0000

    [Windows] Work around the apparent inability of the FOR command to handle double quotes for the executable by spinning off a separate batch file

--- libtbx/env_config.py
+++ libtbx/env_config.py
@@ -299,6 +299,11 @@ class windows_setpaths(common_setpaths):
 
   def __init__(self, env, suffix):
     common_setpaths.__init__(self, env, "bat", suffix)
+    p = env.under_build("path_util.bat", return_relocatable_path=True)
+    if not p.isfile():
+      f = p.open('w')
+      print >>f, '"%s" "%s" %%*' % (abs(env.python_exe), abs(env.path_utility))
+      f.close()
 
   def setenv(self, var_name, val):
     print >> self.s, '  set %s=%s' % (var_name, val)
@@ -307,11 +312,9 @@ class windows_setpaths(common_setpaths):
   def update_path(self, var_name, val):
     val = os.pathsep.join([ abs(p) for p in val ])
     fmt = '''\
-  for /F "delims=" %%%%i in ('%s "%s" %s %s "%s"') do set %s=%%%%i'''
+  for /F "delims=" %%%%i in ('path_util.bat %s %s "%s"') do set %s=%%%%i'''
     for f,action in [(self.s, "prepend"), (self.u, "delete")]:
       print >> f, fmt % (
-        abs(self.env.python_exe),
-        abs(self.env.path_utility),
         action,
         var_name,
         val,
