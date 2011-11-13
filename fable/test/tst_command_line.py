f55971b83b381d815be89e4e2922c5b2767a9417
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2011-11-13 05:04:51 +0000

    fable/test/tst_command_line.py: quote paths which may contain spaces

--- fable/test/tst_command_line.py
+++ fable/test/tst_command_line.py
@@ -6,29 +6,30 @@ def run(args):
   op = os.path
   t_dir = libtbx.env.under_dist(
     module_name="fable", path="test/valid", test=op.isdir)
+  assert t_dir.find('"') < 0
   n_errors = 0
   for command,expected_output_fragment in [
-        ("fable.split %ssubroutine_1.f", "program_prog.f"),
-        ("fable.read --each %swrite_star.f", "Success: 1"),
-        ("fable.read --warnings %sequivalence_mixed.f",
-          "Warning: EQUIVALENCE cluster with mixed data types: integer, real:"),
-        ("fable.show_calls --write-graphviz-dot=tmp.dot"
-          " --top-procedure=sub %sexternal_arg_layers.f",
-            "exch->exch_imp"),
-        ("fable.show_calls %sdependency_cycle.f", "sub1 sub2"),
-        ("fable.fem_include_search_paths --with-quotes", "fable"),
-        ("fable.cout --each %swrite_star.f", "return fem::main_with_catch"),
-        ("fable.cout %scommon_variants.f",
+        ('fable.split "%ssubroutine_1.f"', 'program_prog.f'),
+        ('fable.read --each "%swrite_star.f"', 'Success: 1'),
+        ('fable.read --warnings "%sequivalence_mixed.f"',
+          'Warning: EQUIVALENCE cluster with mixed data types: integer, real:'),
+        ('fable.show_calls --write-graphviz-dot=tmp.dot'
+          ' --top-procedure=sub "%sexternal_arg_layers.f"',
+            'exch->exch_imp'),
+        ('fable.show_calls "%sdependency_cycle.f"', 'sub1 sub2'),
+        ('fable.fem_include_search_paths --with-quotes', 'fable'),
+        ('fable.cout --each "%swrite_star.f"', 'return fem::main_with_catch'),
+        ('fable.cout "%scommon_variants.f"',
           'Writing file: "fable_cout_common_report"'),
-        ("fable.cout %ssubroutine_3.f --top-procedure=sub3"
-         " --fortran-file_comments",
-          "nums(i) = i * 20;"),
-        ("fable.cout --compile %swrite_star.f",
-          "placeholder_please_replace::program_prog);"),
-        ("fable.cout --namespace=test --run %swrite_star.f",
-          "test::program_prog);"),
-        ("fable.cout --example", "  -3   1  -5"),
-        ('fable.cout %sdynamic_parameters_1.f'
+        ('fable.cout "%ssubroutine_3.f" --top-procedure=sub3'
+         ' --fortran-file_comments',
+          'nums(i) = i * 20;'),
+        ('fable.cout --compile "%swrite_star.f"',
+          'placeholder_please_replace::program_prog);'),
+        ('fable.cout --namespace=test --run "%swrite_star.f"',
+          'test::program_prog);'),
+        ('fable.cout --example', '  -3   1  -5'),
+        ('fable.cout "%sdynamic_parameters_1.f"'
           ' --dynamic-parameter="int root_size=1"',
             "const int root_size = cmn.dynamic_params.root_size;")]:
     if (expected_output_fragment.find("fable_cout_common_report") >= 0):
