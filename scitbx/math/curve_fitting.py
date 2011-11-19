9f29a1e7294fb0418e3e1961e34e5ca0722eb038
Richard Gildea <richard.gildea@diamond.ac.uk>
2011-11-19 01:38:27 +0000

    scitbx/math/curve_fitting.py: fitting of multiple gaussians

--- scitbx/math/curve_fitting.py
+++ scitbx/math/curve_fitting.py
@@ -1,5 +1,7 @@
+import itertools
 import math
 
+from libtbx import adopt_init_args
 import scitbx.lbfgs
 from scitbx.array_family import flex
 
@@ -41,6 +43,15 @@ class univariate_polynomial_fit(object):
   #def callback_after_step(self, minimizer):
     #print list(self.x)
 
+
+class gaussian(object):
+
+  def __init__(self, scale, mu, sigma):
+    """Simple wrapper for the parameters associated with a gaussian
+         f(x) = scale * exp(-(x - mu)**2 / (2 * sigma**2))
+    """
+    adopt_init_args(self, locals())
+
 class single_gaussian_fit(object):
 
   def __init__(self, x_obs, y_obs):
@@ -58,27 +69,77 @@ class single_gaussian_fit(object):
     # quick estimate of scale and mean to give the optimiser a helping hand
     scale = y_obs[max_i]
     mu = x_obs[max_i]
-    self.x = flex.double([scale, mu, 1]) # can we make a simple estimate of sigma too?
+    sigma = 1 # can we make a simple estimate of sigma too?
+    fit = gaussian_fit(x_obs, y_obs, [gaussian(scale, mu, sigma)])
+    self.scale = fit.gaussians[0].scale
+    self.mu = fit.gaussians[0].mu
+    self.sigma = fit.gaussians[0].sigma
+
+class gaussian_fit(object):
+
+  def __init__(self, x_obs, y_obs, starting_gaussians):
+    """Fit one or more gaussians to points (x_obs, y_obs):
+         f(x) = sum_i(A_i exp(-(x - mu_i)**2 / (2 * sigma_i**2)))
+
+       :param x_obs: x-coordinates of the data
+       :type x_obs: flex.double
+       :param y_obs: y-coordinates of the data
+       :type y_obs: flex.double
+       :param gaussian: a list or tuple of gaussian objects
+       :type gaussian: list
+    """
+    self.x_obs = x_obs
+    self.y_obs = y_obs
+    self.n_gaussians = len(starting_gaussians)
+    assert self.n_gaussians > 0
+    max_i = flex.max_index(y_obs)
+    self.x = flex.double(list(itertools.chain(
+      *[(g.scale, g.mu, math.sqrt(1/(2*g.sigma**2))) for g in starting_gaussians])))
+    # run the minimizer
     self.minimizer = scitbx.lbfgs.run(target_evaluator=self)
-    self.scale, self.mu, S = tuple(self.x)
-    self.sigma = math.sqrt(1/(2 * S**2))
-    del self.x
+    # prepare the results
+    self.gaussians = []
+    for i in range(self.n_gaussians):
+      scale, mu, S = self.x[i*3:i*3+3]
+      sigma = math.sqrt(1/(2 * S**2))
+      self.gaussians.append(gaussian(scale, mu, sigma))
+
+  def compute_y_calc(self):
+    y_calc = flex.double(self.x_obs.size())
+    for i in range(self.n_gaussians):
+      scale, mu, S = tuple(self.x[i*3:i*3+3])
+      # reparametrisation:
+      #   S**2 = 1/(2 * sigma**2)
+      y_calc += scale * flex.exp(-flex.pow2(self.x_obs-mu) * S**2)
+    return y_calc
 
   def compute_functional_and_gradients(self):
-    scale, mu, S = tuple(self.x)
-    # reparametrisation:
-    #   S**2 = 1/(2 * sigma**2)
-    exponential_part = flex.exp(-flex.pow2(self.x_obs-mu) * S**2)
-    y_calc = scale * exponential_part
+    y_calc = self.compute_y_calc()
     delta_y = self.y_obs - y_calc
     f = flex.sum(flex.pow2(delta_y))
-    g = flex.double((
-      -2 * flex.sum(delta_y * exponential_part),
-      -2 * flex.sum(delta_y * 2 * S**2 * scale * exponential_part * (self.x_obs - mu)),
-      -2 * flex.sum(delta_y * - 2 * scale * S * exponential_part * flex.pow2(mu - self.x_obs))
-    ))
+    g = flex.double()
+    for i in range(self.n_gaussians):
+      scale, mu, S = tuple(self.x[i*3:i*3+3])
+      exponential_part = flex.exp(-flex.pow2(self.x_obs-mu) * S**2)
+      g.extend(flex.double((
+        -2 * flex.sum(delta_y * exponential_part),
+        -2 * flex.sum(delta_y * 2 * S**2 * scale * exponential_part * (self.x_obs - mu)),
+        -2 * flex.sum(delta_y * - 2 * scale * S * exponential_part * flex.pow2(mu - self.x_obs))
+      )))
     return f, g
 
   #def callback_after_step(self, minimizer):
-    #print list(self.x)
+    #for i in range(self.n_gaussians):
+      #scale, mu, S = tuple(self.x[i*3:i*3+3])
+      #sigma = math.sqrt(1/(2 * S**2))
+      #print scale, mu, sigma
 
+  def pyplot(self):
+    from matplotlib import pyplot
+    pyplot.plot(self.x_obs, self.y_obs)
+    pyplot.plot(self.x_obs, self.compute_y_calc())
+    for i in range(self.n_gaussians):
+      scale, mu, S = tuple(self.x[i*3:i*3+3])
+      y_calc = scale * flex.exp(-flex.pow2(self.x_obs-mu) * S**2)
+      pyplot.plot(self.x_obs, y_calc)
+    pyplot.show()
--- scitbx/math/tests/tst_curve_fitting.py
+++ scitbx/math/tests/tst_curve_fitting.py
@@ -51,7 +51,7 @@ def run():
   mu = 3.2
   sigma = 0.1
   x = flex.double(frange(2, 4, 0.01))
-  y = scale * flex.exp(-flex.pow2(x - mu)/(2 * sigma**2))
+  y = scale * flex.exp(-flex.pow2(x - mu) / (2 * sigma**2))
   # need to be careful to only use values of y > 0
   eps = 1e-15
   x = flex.double([x[i] for i in range(x.size()) if y[i] > eps])
@@ -60,6 +60,69 @@ def run():
   c, b, a = fit.params
   assert approx_equal(mu, -b/(2*a))
   assert approx_equal(sigma*sigma, -1/(2*a))
+
+  # test multiple gaussian fits
+  gaussians = [curve_fitting.gaussian(0.3989538, 3.7499764, 0.7500268),
+               curve_fitting.gaussian(0.7978957, 6.0000004, 0.5000078)]
+  x = flex.double(frange(0, 10, 0.1))
+  y = flex.double(x.size())
+  for i in range(len(gaussians)):
+    g = gaussians[i]
+    scale, mu, sigma = g.scale, g.mu, g.sigma
+    y += scale * flex.exp(-flex.pow2(x - mu) / (2 * sigma**2))
+
+  starting_gaussians = [
+    curve_fitting.gaussian(1, 4, 1),
+    curve_fitting.gaussian(1, 5, 1)]
+  fit = curve_fitting.gaussian_fit(x, y, starting_gaussians)
+  for g1, g2 in zip(gaussians, fit.gaussians):
+    assert approx_equal(g1.scale, g2.scale, eps=1e-4)
+    assert approx_equal(g1.mu, g2.mu, eps=1e-4)
+    assert approx_equal(g1.sigma, g2.sigma, eps=1e-4)
+
+  if 0:
+    from matplotlib import pyplot
+    start_y = flex.double(x.size())
+    for i in range(len(starting_gaussians)):
+      g = starting_gaussians[i]
+      scale, mu, sigma = g.scale, g.mu, g.sigma
+      start_y += scale * flex.exp(-flex.pow2(x - mu) / (2 * sigma**2))
+    pyplot.plot(x, start_y)
+    fit.pyplot()
+
+
+  # use example of 5-gaussian fit from here:
+  # http://research.stowers-institute.org/efg/R/Statistics/MixturesOfDistributions/index.htm
+  gaussians = [curve_fitting.gaussian(0.10516252, 23.32727, 2.436638),
+               curve_fitting.gaussian(0.46462715, 33.09053, 2.997594),
+               curve_fitting.gaussian(0.29827916, 41.27244, 4.274585),
+               curve_fitting.gaussian(0.08986616, 51.24468, 5.077521),
+               curve_fitting.gaussian(0.04206501, 61.31818, 7.070303)]
+
+  x = flex.double(frange(0, 80, 0.1))
+  y = flex.double(x.size())
+  for i in range(len(gaussians)):
+    g = gaussians[i]
+    scale, mu, sigma = g.scale, g.mu, g.sigma
+    y += scale * flex.exp(-flex.pow2(x - mu) / (2 * sigma**2))
+
+  starting_gaussians = [curve_fitting.gaussian(1, 20, 2.1),
+                        curve_fitting.gaussian(1, 30, 2.8),
+                        curve_fitting.gaussian(1, 40, 2.2),
+                        curve_fitting.gaussian(1, 51, 1.2),
+                        curve_fitting.gaussian(1, 60, 2.3)]
+  fit = curve_fitting.gaussian_fit(x, y, starting_gaussians)
+  y_calc = fit.compute_y_calc()
+  assert approx_equal(y, y_calc, eps=1e-3)
+
+  if 0:
+    from matplotlib import pyplot
+    start_y = flex.double(x.size())
+    for i in range(len(starting_gaussians)):
+      g = starting_gaussians[i]
+      scale, mu, sigma = g.scale, g.mu, g.sigma
+    fit.pyplot()
+
 
 if (__name__ == "__main__"):
   run()
