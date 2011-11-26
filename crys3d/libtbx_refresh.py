80c70f7c38791b20812d55775f2c6fd5f629b497
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2011-11-26 06:03:11 +0000

    crys3d/libtbx_refresh.py: avoid qttbx import by using libtbx.env.dist_path(), because the qttbx import has implicit dependencies on scitbx and gltbx extensions

--- crys3d/libtbx_refresh.py
+++ crys3d/libtbx_refresh.py
@@ -1,7 +1,9 @@
 try:
   from PyQt4 import uic
-  from crys3d import qttbx
-  print "Processing *.ui files in qttbx"
-  uic.compileUiDir(qttbx.__path__[0], recurse=True)
 except ImportError:
   pass
+else:
+  import libtbx.load_env
+  ui_dir = libtbx.env.under_dist(module_name="crys3d", path="qttbx")
+  print '  Processing *.ui files in "%s"' % ui_dir
+  uic.compileUiDir(ui_dir, recurse=True)
