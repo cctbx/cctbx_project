a0001d2b2be25458e0517ece794e97dcf79297b7
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2011-12-03 00:44:51 +0000

    libtbx/command_line/show_dist_paths.py: new --dirname option

--- libtbx/command_line/show_dist_paths.py
+++ libtbx/command_line/show_dist_paths.py
@@ -1,13 +1,25 @@
-import libtbx.load_env
-import sys
-
-def run():
-  if (len(sys.argv) == 1):
+def run(args):
+  import libtbx.load_env
+  remaining_args = []
+  dirname_count = 0
+  for arg in args:
+    if (arg == "--dirname"):
+      dirname_count += 1
+    else:
+      remaining_args.append(arg)
+  def show(path):
+    if (path is not None):
+      from os.path import dirname
+      for _ in xrange(dirname_count):
+        path = dirname(path)
+    print path
+  if (len(remaining_args) == 0):
     for path in libtbx.env.dist_paths():
-      print path
+      show(path)
   else:
-    for arg in sys.argv[1:]:
-      print libtbx.env.dist_path(arg, None)
+    for arg in remaining_args:
+      show(libtbx.env.dist_path(module_name=arg, default=None))
 
 if (__name__ == "__main__"):
-  run()
+  import sys
+  run(args=sys.argv[1:])
