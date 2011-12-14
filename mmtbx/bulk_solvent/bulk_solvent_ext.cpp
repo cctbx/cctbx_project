369252722f36dfbe89632cf4a29092a297728f60
Pavel Afonine <afonine@users.noreply.github.com>
2011-12-14 00:08:16 +0000

    new fast analytical solution for overall iso/aniso scale and bulk-solvent scale

--- mmtbx/bulk_solvent/bulk_solvent_ext.cpp
+++ mmtbx/bulk_solvent/bulk_solvent_ext.cpp
@@ -7,6 +7,8 @@
 #include <mmtbx/bulk_solvent/bulk_solvent.h>
 #include <mmtbx/f_model/f_model.h>
 
+#include <boost/python/return_value_policy.hpp>
+#include <boost/python/return_by_value.hpp>
 
 namespace mmtbx { namespace bulk_solvent {
 namespace {
@@ -15,6 +17,8 @@ namespace {
   {
     using namespace boost::python;
     using boost::python::arg;
+    typedef return_value_policy<return_by_value> rbv;
+
     class_<target_gradients_aniso_ml>("target_gradients_aniso_ml",
                              init<af::const_ref<double> const&,
                                   af::const_ref< std::complex<double> > const&,
@@ -69,6 +73,41 @@ namespace {
       .def("grad_k_sols", &bulk_solvent_and_aniso_scale_target_and_grads_ls<>::grad_k_sols)
       .def("grad_b_sol", &bulk_solvent_and_aniso_scale_target_and_grads_ls<>::grad_b_sol)
    ;
+   //
+   class_<overall_and_bulk_solvent_scale_coefficients_analytical<> >(
+      "overall_and_bulk_solvent_scale_coefficients_analytical")
+      .def(init<
+           af::const_ref<double> const&,
+           af::const_ref<std::complex<double> > const&,
+           af::const_ref<std::complex<double> > const&,
+           af::const_ref<bool> const&>(
+             (arg("f_obs"),
+              arg("f_calc"),
+              arg("f_mask"),
+              arg("selection"))))
+      .add_property("k", make_getter(
+        &overall_and_bulk_solvent_scale_coefficients_analytical<>::k, rbv()))
+      .add_property("x", make_getter(
+        &overall_and_bulk_solvent_scale_coefficients_analytical<>::x, rbv()))
+      .add_property("t", make_getter(
+        &overall_and_bulk_solvent_scale_coefficients_analytical<>::t, rbv()))
+   ;
+
+   class_<aniso_u_scaler<> >("aniso_u_scaler")
+      .def(init<
+           af::const_ref<std::complex<double> > const&,
+           af::const_ref<double> const&,
+           af::const_ref<cctbx::miller::index<> > const& >(
+             (arg("fm"),
+              arg("fo"),
+              arg("miller_indices"))))
+      .add_property("M",  make_getter(&aniso_u_scaler<>::M,  rbv()))
+      .add_property("b",  make_getter(&aniso_u_scaler<>::b,  rbv()))
+      .add_property("u_star", make_getter(&aniso_u_scaler<>::u_star, rbv()))
+      .add_property("overall_anisotropic_scale",
+        make_getter(&aniso_u_scaler<>::overall_anisotropic_scale, rbv()))
+   ;
+   //
     def("r_factor",
       (double(*)
         (af::const_ref<double> const&,
