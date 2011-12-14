af1ed4ce1b7d1967bab06d649cc1dc91c4401f8f
Nigel W Moriarty <nwmoriarty@users.noreply.github.com>
2011-12-14 23:20:36 +0000

    added inverted chiral hydrogen correction

--- mmtbx/monomer_library/pdb_interpretation.py
+++ mmtbx/monomer_library/pdb_interpretation.py
@@ -97,6 +97,9 @@ master_params_str = """\
     .help = Use Conformation Dependent Library (CDL) \
       for geometry minimization restraints
     .style = bold
+  correct_hydrogens = False
+    .type = bool
+    .short_caption = Correct the hydrogen positions trapped in chirals
   apply_cif_modification
     .optional = True
     .multiple = True
@@ -3767,6 +3770,94 @@ def show_residue_groups(residue_groups, log, prefix, max_items):
   n = len(residue_groups) - max_items
   if (n > 0):
     print >> log, prefix+"... (remaining %d not shown)" % n
+
+def correct_hydrogen_geometries(hierarchy,
+                                xray_structure=None,
+                                verbose=False,
+                                ):
+  from cctbx import geometry
+  
+  def get_bonded(atom_group, atom, exclude=[]):
+    min_d2=1000
+    min_atom=None
+    for atom1 in atom_group.atoms():
+      if atom1.name.strip()==atom.name.strip(): continue
+      process=False
+      for atom2 in exclude:
+        if atom1.name.strip()==atom2.name.strip():
+          break
+      else:
+        process=True
+      if not process: continue
+      if(atom.element.strip() in ["H", "D", "T"] and
+         atom1.element.strip() in ["H", "D", "T"]
+         ): continue
+      d2 = (atom.xyz[0]-atom1.xyz[0])**2
+      d2 += (atom.xyz[1]-atom1.xyz[1])**2
+      d2 += (atom.xyz[2]-atom1.xyz[2])**2
+      if d2<min_d2:
+        min_d2=d2
+        min_atom=atom1
+    if min_atom:
+      return [min_atom]
+    else:
+      return []
+
+  def get_angles(atom_group, atom, verbose=False):
+    bad_hydrogen_count = 0
+    corrected_hydrogen_count = 0
+    bonded = get_bonded(atom_group, atom)
+    if not bonded:
+      bad_hydrogen_count+=1
+      if verbose: print 'not bonded: %s' % atom.format_atom_record()
+      return bad_hydrogen_count, corrected_hydrogen_count
+    for ba in bonded:
+      angled = get_bonded(atom_group, ba, exclude=[atom])
+    if not angled:
+      bad_hydrogen_count+=1
+      if verbose: print 'not angled: %s' % atom.format_atom_record()
+      return bad_hydrogen_count, corrected_hydrogen_count
+    if angled[0].element.strip() in ["H", "D", "T"]:
+      return bad_hydrogen_count, corrected_hydrogen_count
+    try: angle = geometry.angle((atom.xyz,ba.xyz,angled[0].xyz)).angle_model
+    except:
+      print '  Bad angle "%s"' % (atom.format_atom_record()[:26])
+      bad_hydrogen_count +=1
+      return bad_hydrogen_count, corrected_hydrogen_count
+    if angle<85.:
+      xyz=[0,0,0]
+      xyz[0] = ba.xyz[0]*2-atom.xyz[0]
+      xyz[1] = ba.xyz[1]*2-atom.xyz[1]
+      xyz[2] = ba.xyz[2]*2-atom.xyz[2]
+      angle = geometry.angle((xyz,ba.xyz,angled[0].xyz)).angle_model
+      if angle>95.:
+        atom.xyz = tuple(xyz)
+        if verbose: print '  Inverted "%s" ' % (atom.format_atom_record()[:26])
+        corrected_hydrogen_count+=1
+    return bad_hydrogen_count, corrected_hydrogen_count
+
+  bad_hydrogen_count=0
+  corrected_hydrogen_count=0
+  if len(hierarchy.models())>1:
+    print "  \nPDB files with more than one model are ignored\n"
+    return bad_hydrogen_count, corrected_hydrogen_count
+  for model in hierarchy.models():
+    for chain in model.chains():
+      for residue_group in chain.residue_groups():
+        #
+        # needs to be removed
+        #
+        #if len(residue_group.atom_groups())>1: continue
+        for atom_group_i, atom_group in enumerate(residue_group.atom_groups()):
+          for i, atom in enumerate(atom_group.atoms()):
+            if atom.element.strip() in ["H", "D", "T"]:
+              rc = get_angles(atom_group, atom, verbose=verbose)
+              bad_hydrogen_count += rc[0]
+              corrected_hydrogen_count += rc[1]
+  if xray_structure:
+    sites_cart = hierarchy.atoms().extract_xyz()
+    xray_structure.set_sites_cart(sites_cart)
+  return bad_hydrogen_count, corrected_hydrogen_count
 
 class process(object):
 
