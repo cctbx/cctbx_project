b0ef8776aa32f1fceab6ccb9dab36233841f391c
Nathaniel Echols <nathaniel.echols@gmail.com>
2011-12-15 21:58:41 +0000

    added iotbx.pdb.split_models

---
+++ iotbx/command_line/pdb_split_models.py
@@ -0,0 +1,113 @@
+# LIBTBX_SET_DISPATCHER_NAME iotbx.pdb.split_models
+
+from libtbx.utils import Sorry, Usage, null_out
+import os
+import sys
+
+master_phil = """
+split_models
+  .short_caption = Split multi-model PDB file
+  .caption = This utility will separate a multi-model PDB file (such as an \
+    NMR ensemble) into individual files for each model.  The output files \
+    will be named similarly to the input file but ending in _1.pdb, _2.pdb, \
+    etc.
+  .style = auto_align box caption_img:icons/custom/iotbx.pdb.join_fragment_files.png
+{
+  file_name = None
+    .type = path
+    .short_caption = PDB file
+    .style = file_type:pdb input_file bold
+  output_dir = None
+    .type = path
+    .short_caption = Output directory
+    .style = directory default_cwd bold
+}
+"""
+
+def run (args=(), params=None, out=None) :
+  if (out is None) :
+    out = sys.stdout
+  if (params is None) :
+    if (len(args) == 0) :
+      raise Usage("""
+iotbx.pdb.split_models ensemble.pdb [output_dir=/path/...]
+
+Splits a multi-model PDB file into separate files for each model.
+""")
+    import iotbx.phil
+    cmdline = iotbx.phil.process_command_line_with_files(
+      args=args,
+      master_phil_string=master_phil,
+      pdb_file_def="split_models.file_name",
+      directory_def="split_models.output_dir")
+    params = cmdline.work.extract()
+    validate_params(params)
+  from iotbx import file_reader
+  pdb_in = file_reader.any_file(params.split_models.file_name, force_type="pdb")
+  pdb_in.check_file_type("pdb")
+  hierarchy = pdb_in.file_object.construct_hierarchy()
+  if (len(hierarchy.models()) <= 1) :
+    raise Sorry("The PDB file %s already has a single model." %
+      params.split_models.file_name)
+  pdb_rel_path = os.path.basename(params.split_models.file_name)
+  if (pdb_rel_path.endswith(".gz")) :
+    pdb_rel_path = pdb_rel_path[:-3]
+  elif (pdb_rel_path.endswith(".Z")) :
+    pdb_rel_path = pdb_rel_path[:-2]
+  base_name = os.path.splitext(pdb_rel_path)[0]
+  if (params.split_models.output_dir is None) :
+    params.split_models.output_dir = os.getcwd()
+  output_base = os.path.join(params.split_models.output_dir, base_name)
+  return split_models(
+    hierarchy=hierarchy,
+    crystal_symmetry=pdb_in.file_object.crystal_symmetry(),
+    output_base=output_base,
+    original_file=params.split_models.file_name,
+    log=out)
+
+def split_models (hierarchy,
+                  crystal_symmetry,
+                  output_base,
+                  original_file=None,
+                  log=None) :
+  if (log is None) : log = null_out()
+  import iotbx.pdb.hierarchy
+  n_models = len(hierarchy.models())
+  file_names = []
+  for k, model in enumerate(hierarchy.models(), start=1) :
+    new_hierarchy = iotbx.pdb.hierarchy.root()
+    new_hierarchy.append_model(model.detached_copy())
+    if (model.id == "") :
+      model_id = str(k)
+    else :
+      model_id = model.id.strip()
+    output_file = "%s_%s.pdb" % (output_base, model_id)
+    f = open(output_file, "w")
+    if (crystal_symmetry is not None) :
+      print >> f, iotbx.pdb.format_cryst1_and_scale_records(
+        crystal_symmetry=crystal_symmetry,
+        write_scale_records=True)
+    print >> f, "REMARK Model %d of %d" % (k, n_models)
+    if (original_file is not None) :
+      print >> f, "REMARK Original file:"
+      print >> f, "REMARK   %s" % original_file
+    f.write(new_hierarchy.as_pdb_string())
+    f.close()
+    file_names.append(output_file)
+    print >> log, "Wrote %s" % output_file
+  return file_names
+
+def validate_params (params) :
+  if (params.split_models.file_name is None) :
+    raise Sorry("Please specify a PDB file to split!")
+  elif (not os.path.isfile(params.split_models.file_name)) :
+    raise Sorry("The PDB file '%s' does not exist or is not a file." %
+      params.split_models.file_name)
+  if (params.split_models.output_dir is not None) :
+    if (not os.path.isdir(params.split_models.output_dir)) :
+      raise Sorry(("The specified output directory '%s' does not exist or is "+
+        "not a directory.") % params.split_models.output_dir)
+  return True
+
+if (__name__ == "__main__") :
+  run(sys.argv[1:])
---
+++ iotbx/regression/tst_split_models.py
@@ -0,0 +1,60 @@
+
+import os
+
+def exercise () :
+  if (os.path.isfile("multi_model_1.pdb")) :
+    os.remove("multi_model_1.pdb")
+  if (os.path.isfile("multi_model_2.pdb")) :
+    os.remove("multi_model_2.pdb")
+  f = open("multi_model.pdb", "w")
+  f.write("""\
+CRYST1    1.000    1.000    1.000  90.00  90.00  90.00 P 1           1
+ORIGX1      1.000000  0.000000  0.000000        0.00000
+ORIGX2      0.000000  1.000000  0.000000        0.00000
+ORIGX3      0.000000  0.000000  1.000000        0.00000
+SCALE1      1.000000  0.000000  0.000000        0.00000
+SCALE2      0.000000  1.000000  0.000000        0.00000
+SCALE3      0.000000  0.000000  1.000000        0.00000
+MODEL        1
+ATOM      1  N   GLY P  -1     -22.866  -2.627  15.217  1.00  0.00           N
+ATOM      2  CA  GLY P  -1     -22.714  -3.068  16.621  1.00  0.00           C
+ATOM      3  C   GLY P  -1     -21.276  -3.457  16.936  1.00  0.00           C
+ATOM      4  O   GLY P  -1     -20.538  -3.887  16.047  1.00  0.00           O
+ATOM      5  H1  GLY P  -1     -22.583  -3.364  14.590  1.00  0.00           H
+ATOM      6  H2  GLY P  -1     -22.293  -1.817  15.040  1.00  0.00           H
+ATOM      7  H3  GLY P  -1     -23.828  -2.392  15.027  1.00  0.00           H
+ATOM      8  HA2 GLY P  -1     -23.016  -2.261  17.288  1.00  0.00           H
+ATOM      9  HA3 GLY P  -1     -23.352  -3.933  16.803  1.00  0.00           H
+ENDMDL
+MODEL        2
+ATOM      1  N   GLY P  -1       7.889 -28.444  15.733  1.00  0.00           N
+ATOM      2  CA  GLY P  -1       7.573 -27.021  15.481  1.00  0.00           C
+ATOM      3  C   GLY P  -1       6.614 -26.458  16.523  1.00  0.00           C
+ATOM      4  O   GLY P  -1       5.922 -27.210  17.215  1.00  0.00           O
+ATOM      5  H1  GLY P  -1       8.513 -28.794  15.023  1.00  0.00           H
+ATOM      6  H2  GLY P  -1       7.043 -28.994  15.722  1.00  0.00           H
+ATOM      7  H3  GLY P  -1       8.329 -28.551  16.633  1.00  0.00           H
+ATOM      8  HA2 GLY P  -1       8.492 -26.437  15.501  1.00  0.00           H
+ATOM      9  HA3 GLY P  -1       7.112 -26.918  14.499  1.00  0.00           H
+ENDMDL
+END""")
+  f.close()
+  from iotbx import file_reader
+  from libtbx import easy_run
+  from libtbx.test_utils import approx_equal
+  out = easy_run.fully_buffered("iotbx.pdb.split_models multi_model.pdb")
+  assert (len(out.stdout_lines) == 2) and (len(out.stderr_lines) == 0)
+  assert (os.path.isfile("multi_model_1.pdb") and
+          os.path.isfile("multi_model_2.pdb"))
+  sites = []
+  for file in ["multi_model_1.pdb", "multi_model_2.pdb"] :
+    pdb_in = file_reader.any_file(file, force_type="pdb")
+    pdb_in.assert_file_type("pdb")
+    h = pdb_in.file_object.construct_hierarchy()
+    assert (len(h.models()) == 1)
+    sites.append(h.atoms().extract_xyz())
+  assert approx_equal(sites[0].rms_difference(sites[1]), 38.9216638)
+
+if (__name__ == "__main__") :
+  exercise()
+  print "OK"
