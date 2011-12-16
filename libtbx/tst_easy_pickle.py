e71869567c2924101d44411384df739743308b34
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2011-12-16 07:13:53 +0000

    libtbx/easy_pickle.py: use HIGHEST_PROTOCOL (as suggested by Richard)

--- libtbx/tst_easy_pickle.py
+++ libtbx/tst_easy_pickle.py
@@ -1,24 +1,34 @@
-def exercise(n):
+def exercise(n, use_dumps=False):
   from libtbx import easy_pickle
   import time
   obj = []
   for i in xrange(n):
     obj.append([i,i])
   for dgz in ["", ".gz"]:
-    file_name = "test.dat"+dgz
-    print file_name
     t0 = time.time()
-    easy_pickle.dump(file_name=file_name, obj=obj)
+    if (use_dumps):
+      print "dumps/loads"
+      pickle_string = easy_pickle.dumps(obj=obj)
+    else:
+      file_name = "test.dat"+dgz
+      print file_name
+      easy_pickle.dump(file_name=file_name, obj=obj)
     print "  dump: %.2f s" % (time.time()-t0)
     del obj
     t0 = time.time()
-    obj = easy_pickle.load(file_name=file_name)
+    if (use_dumps):
+      obj = easy_pickle.loads(pickle_string)
+    else:
+      obj = easy_pickle.load(file_name=file_name)
     print "  load buffered: %.2f s" % (time.time()-t0)
-    del obj
-    t0 = time.time()
-    obj = easy_pickle.load(
-      file_name=file_name, faster_but_using_more_memory=False)
-    print "  load direct: %.2f s" % (time.time()-t0)
+    if (use_dumps):
+      break
+    else:
+      del obj
+      t0 = time.time()
+      obj = easy_pickle.load(
+        file_name=file_name, faster_but_using_more_memory=False)
+      print "  load direct: %.2f s" % (time.time()-t0)
 
 def run(args):
   assert len(args) in [0,1]
@@ -27,7 +37,8 @@ def run(args):
   else:
     n = int(args[0])
     assert n >= 0
-  exercise(n)
+  for use_dumps in [False, True]:
+    exercise(n, use_dumps)
   print "OK"
 
 if (__name__ == "__main__"):
