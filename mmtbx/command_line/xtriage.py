7ec81dedc3b5bbbfd1805ee7ebe61a5364cee59c
Nathaniel Echols <nathaniel.echols@gmail.com>
2011-12-20 23:14:50 +0000

    remove obsolete gui launching code

--- mmtbx/command_line/xtriage.py
+++ mmtbx/command_line/xtriage.py
@@ -1,20 +1,7 @@
 # LIBTBX_SET_DISPATCHER_NAME phenix.xtriage
-# LIBTBX_PRE_DISPATCHER_INCLUDE_SH PHENIX_GUI_ENVIRONMENT=1
-# LIBTBX_PRE_DISPATCHER_INCLUDE_SH export PHENIX_GUI_ENVIRONMENT
 
 from mmtbx.scaling import xtriage
 import sys
 
 if (__name__ == "__main__"):
-  if "--gui" in sys.argv :
-    try :
-      from phenix.command_line import xtriage_gui
-      xtriage_gui.run(args=sys.argv[1:])
-    except ImportError :
-      print "PHENIX GUI is not installed."
-      sys.exit()
-    except Exception, e :
-      print e
-      sys.exit()
-  else :
-    xtriage.run(args=sys.argv[1:])
+  xtriage.run(args=sys.argv[1:])
