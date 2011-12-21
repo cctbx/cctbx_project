ca7703d9bbabf0ddeba704a04fc759d73efa47f8
Ralf W. Grosse-Kunstleve <rwgk@users.noreply.github.com>
2011-12-21 08:38:53 +0000

    cctbx/crystal/__init__.py: show_distances: prefix symmetry operations with "sym=" (for easy grep)

--- cctbx/crystal/__init__.py
+++ cctbx/crystal/__init__.py
@@ -820,7 +820,7 @@ class show_distances(object):
         formatted_site = [" %7.4f" % x for x in site_frac_ji]
       s += " (" + ",".join(formatted_site) +")"
       if (not rt_mx_ji.is_unit_mx()):
-        s += " " + str(rt_mx_ji)
+        s += " sym=" + str(rt_mx_ji)
       print >> out, s
 
       if first_time_i_seq and d.pair_count == 0:
