2d1a0350293858d2a0a2cf2103bce0b000f9c2b3
Pavel Afonine <pafonine@users.noreply.github.com>
2012-01-06 22:25:13 +0000

    use finer-grained reciprocal space sampling; rationalize constructor

--- mmtbx/bulk_solvent/scaler.py
+++ mmtbx/bulk_solvent/scaler.py
@@ -8,7 +8,8 @@ import boost.python
 ext = boost.python.import_ext("mmtbx_f_model_ext")
 from cctbx import sgtbx
 from mmtbx.bulk_solvent import bulk_solvent_and_scaling
-
+import scitbx.math
+import mmtbx.f_model
 
 def sliding_window_average(x, offset):
   result = flex.double(x.size(), -1)
@@ -118,40 +119,99 @@ class core(object):
       bulk_solvent_scale        = self.bulk_solvent_scale)
     return bulk_solvent.r_factor(self.f_obs.data(), core_data.f_model)
 
+def determine_n_bins(
+  n_refl,
+  n_refl_per_bin,
+  max_n_bins,
+  min_refl_per_bin,
+  min_n_bins=1):
+    assert n_refl_per_bin > 0
+    n_refl_per_bin = min(n_refl, iround(n_refl_per_bin))
+    result = max(1, iround(n_refl / max(1, n_refl_per_bin)))
+    result = max(result, min(min_n_bins, iround(n_refl / min_refl_per_bin)))
+    result = min(max_n_bins, result)
+    return result
+
+def split(mi, r1=5, r2=10, m1=500, m2=100, m3=10):
+  d_spacings = mi.d_spacings().data()
+  s1 = d_spacings <= r1
+  s2 = (d_spacings > r1) & (d_spacings <= r2)
+  s3 = d_spacings > r2
+  c1 = s1.count(True)
+  c2 = s2.count(True)
+  c3 = s3.count(True)
+  #
+  if(c1>m1 and c2>m2 and c3>m3):
+    s1 = d_spacings <= r1
+    s2 = (d_spacings > r1) & (d_spacings <= r2)
+    s3 = d_spacings > r2
+  elif(c1<m1 and c2>m2 and c3>m3):
+    s1 = None
+    s2 = d_spacings <= r2
+    s3 = d_spacings > r2
+  elif(c1>m1 and c2>m2 and c3<m3):
+    s1 = d_spacings <= r1
+    s2 = d_spacings > r1
+    s3 = None
+  elif(c1>m1 and c2<m2):
+    s1 = flex.bool(mi.data().size(),True)
+    s2 = None
+    s3 = None
+  elif(c1<m1 and c2<m2):
+    s1 = None
+    s2 = None
+    s3 = flex.bool(mi.data().size(),True)
+  else:
+    assert 0 # should never get here (if it does - it's a bug)
+  nb1,nb2,nb3 = None,None,None
+  if(s1 is not None):
+    nb1 = determine_n_bins(
+      n_refl           = s1.count(True),
+      n_refl_per_bin   = m1,
+      max_n_bins       = 100,
+      min_refl_per_bin = m1)
+  if(s2 is not None):
+    nb2 = determine_n_bins(
+      n_refl           = s2.count(True),
+      n_refl_per_bin   = m2,
+      max_n_bins       = 50,
+      min_refl_per_bin = m2)
+  if(s3 is not None):
+    nb3 = determine_n_bins(
+      n_refl           = s3.count(True),
+      n_refl_per_bin   = m3,
+      max_n_bins       = 10,
+      min_refl_per_bin = m3)
+  cntr = 0
+  for s in [s1,s2,s3]:
+    if(s is not None):
+      cntr += s.count(True)
+  assert cntr == mi.data().size()
+  return ((s1,nb1), (s2,nb2), (s3,nb3))
+
 class run(object):
   def __init__(self,
                f_obs,
                f_calc, # can be a sum: f_calc=f_hydrogens+f_calc+f_part
                f_mask, # only one shell is supported
                ss,
-               low_high_resolution=5.0, # should not be changed
                number_of_cycles=10, # termination occures much earlier
-               min_refl_per_bin_low=50,
-               min_refl_per_bin_high=250,
-               estimate_k_sol_and_b_sol=False, # not used, for information only
+               estimate_k_sol_and_b_sol=True, # not used, for information only
                use_minimization_for_aniso_scaling=False, # just in case...
                log=None,
                verbose=False):
+    self.use_minimization_for_aniso_scaling = use_minimization_for_aniso_scaling
+    self.log = log
+    self.verbose = verbose
     if(log is None): log = sys.stdout
     if(verbose):
       print >> log, \
         "Overall, iso- and anisotropic scaling and bulk-solvent modeling:"
-    def determine_n_bins(
-      n_refl,
-      n_refl_per_bin,
-      max_n_bins,
-      min_refl_per_bin,
-      min_n_bins=1):
-        assert n_refl_per_bin > 0
-        n_refl_per_bin = min(n_refl, iround(n_refl_per_bin))
-        result = max(1, iround(n_refl / max(1, n_refl_per_bin)))
-        result = max(result, min(min_n_bins, iround(n_refl / min_refl_per_bin)))
-        result = min(max_n_bins, result)
-        return result
+    bins_and_selections = split(mi=f_obs, r1=5, r2=10, m1=500, m2=100, m3=10)
     point_group = sgtbx.space_group_info(
       symbol=f_obs.space_group().type().lookup_symbol()
       ).group().build_derived_point_group()
-    adp_constraints = sgtbx.tensor_rank_2_constraints(
+    self.adp_constraints = sgtbx.tensor_rank_2_constraints(
       space_group=point_group,
       reciprocal_space=True)
     scalar_scale = bulk_solvent.scale(f_obs.data(), f_calc.data())
@@ -165,44 +225,13 @@ class run(object):
       overall_anisotropic_scale = flex.double(f_obs.size(), 1),
       bulk_solvent_scale = flex.double(f_obs.size(), 0),
       ss                 = ss)
-    d_spacings = self.core.f_obs.d_spacings().data()
-    low_res_sel = d_spacings > low_high_resolution
-    high_res_sel = ~low_res_sel
-    all_sel = flex.bool(f_obs.indices().size(), True)
-    #
-    n_bins_low = determine_n_bins(
-      n_refl           = low_res_sel.count(True),
-      n_refl_per_bin   = 50,
-      max_n_bins       = 5000,
-      min_refl_per_bin = min_refl_per_bin_low)
-    n_bins_high = determine_n_bins(
-      n_refl           = high_res_sel.count(True),
-      n_refl_per_bin   = 500,
-      max_n_bins       = 10,
-      min_refl_per_bin = min_refl_per_bin_high)
-    cl,ch,clh = None,None,None
-    if(low_res_sel.count(True)<min_refl_per_bin_low):
-      clh = self.core
-      clh.f_obs.setup_binner(reflections_per_bin = 0, n_bins = n_bins_high)
-      low_res_sel = flex.bool(f_obs.indices().size(), True)
-      if(verbose): print >> log, "  number of bins: %d"%n_bins_high
-    elif(high_res_sel.count(True)<min_refl_per_bin_high):
-      clh = self.core
-      print f_obs.data().size()
-      clh.f_obs.setup_binner(reflections_per_bin = 0, n_bins = n_bins_low)
-      low_res_sel = flex.bool(f_obs.indices().size(), True)
-      if(verbose): print >> log, "  number of bins: %d"%n_bins_low
-    else:
-      cl = self.core.select(selection=low_res_sel)
-      ch = self.core.select(selection=high_res_sel)
-      cl.f_obs.setup_binner(reflections_per_bin = 0, n_bins = n_bins_low)
-      ch.f_obs.setup_binner(reflections_per_bin = 0, n_bins = n_bins_high)
-      if(verbose):
-        print >> log, "  number of low resolution bins  (d_min>%4.1fA): %d"%(
-          low_high_resolution,n_bins_low)
-        print >> log, "  number of high resolution bins (d_min<%4.1fA): %d"%(
-          low_high_resolution,n_bins_high)
-    #
+    cores_and_selections = []
+    for i_bas, bas in enumerate(bins_and_selections):
+      if(bas[0] is not None):
+        core_selected = self.core.select(selection=bas[0])
+        cores_and_selections.append([bas[0], core_selected])
+        core_selected.f_obs.setup_binner(reflections_per_bin=0, n_bins = bas[1])
+        print bas[1], bas[0].count(True)
     if(verbose):
       print >> log, "  r_start: %6.4f"%self.core.r_factor()
     for cycle in xrange(number_of_cycles):
@@ -214,100 +243,25 @@ class run(object):
       # anisotropic scaling
       if(verbose):
         print >> log, "      anisotropic scaling:"
-      if(not use_minimization_for_aniso_scaling):
-        # try exp_anal
-        obj = bulk_solvent.aniso_u_scaler(
-          f_model        = self.core.f_model_no_scale().data(),
-          f_obs          = self.core.f_obs.data(),
-          miller_indices = self.core.f_obs.indices(),
-          adp_constraint_matrix = adp_constraints.gradient_sum_matrix())
-        u_star = adp_constraints.all_params(tuple(obj.u_star_independent))
-        b_cart = adptbx.u_as_b(adptbx.u_star_as_u_cart(
-          self.core.f_obs.unit_cell(), u_star))
-        overall_anisotropic_scale_expanal = ext.overall_anisotropic_scale(
-          self.core.f_obs.indices(), u_star)
-        r_expanal = self.core.try_overall_anisotropic_scale(
-          scale_array = overall_anisotropic_scale_expanal)
-        # try poly
-        obj = bulk_solvent.aniso_u_scaler(
-          f_model        = self.core.f_model_no_scale().data(),
-          f_obs          = self.core.f_obs.data(),
-          miller_indices = self.core.f_obs.indices(),
-          unit_cell      = f_obs.unit_cell())
-        overall_anisotropic_scale_poly = ext.overall_anisotropic_scale(
-          self.core.f_obs.indices(), obj.a, self.core.f_obs.unit_cell())
-        r_poly = self.core.try_overall_anisotropic_scale(
-          scale_array = overall_anisotropic_scale_poly)
-        if(r_poly<r_expanal):
-          overall_anisotropic_scale = overall_anisotropic_scale_poly
-          b_cart = obj.a # not really b_cart
-        else:
-          overall_anisotropic_scale = overall_anisotropic_scale_expanal
-        print >> log, "        r_poly   : %6.4f"%r_poly
-        print >> log, "        r_expanal: %6.4f"%r_expanal
-        self.core.update(overall_anisotropic_scale = overall_anisotropic_scale)
-      else:
-        zero = f_calc.customized_copy(data =
-          flex.complex_double(f_obs.data().size(), 0))
-        fm = mmtbx.f_model.core(
-          f_calc  = self.core.f_model_no_scale(),
-          f_mask  = zero,
-          k_sols  = [0.],
-          b_sol   = 0.,
-          f_part1 = None,
-          f_part2 = None,
-          u_star  = [0,0,0,0,0,0],
-          fmodel  = None,
-          ss      = None)
-        obj = bulk_solvent_and_scaling.u_star_minimizer(
-          fmodel_core_data = fm,
-          f_obs            = f_obs,
-          u_initial        = [0,0,0,0,0,0],
-          refine_u         = True,
-          min_iterations   = 500,
-          max_iterations   = 500,
-          symmetry_constraints_on_b_cart = True,
-          u_min_max = 500.,
-          u_min_min =-500.)
-        u_star = obj.u_min
-        overall_anisotropic_scale = ext.overall_anisotropic_scale(
-          self.core.f_obs.indices(), u_star)
-        b_cart = adptbx.u_as_b(adptbx.u_star_as_u_cart(
-          self.core.f_obs.unit_cell(), u_star))
-        self.core.update(overall_anisotropic_scale = overall_anisotropic_scale)
-      r_aniso = self.core.r_factor()
-      if(verbose):
-        print >> log, "        r_final  : %6.4f"%r_aniso
-        if(len(b_cart)<=6):
-          print >> log, "        b_cart(11,22,33,12,13,23):",\
-            ",".join([str("%8.4f"%i).strip() for i in b_cart])
-        else:
-          print >> log, "        a:",\
-            ",".join([str("%8.4f"%i).strip() for i in b_cart])
+      self.anisotropic_scaling() # order IS important
       # bulk-solvent and overall isotropic scale
       overall_scale = flex.double(f_obs.size(), -1)
       bulk_solvent_scale = flex.double(f_obs.size(), -1)
       ssi = flex.double()
       x = flex.double()
-      if(clh is None):
-        scale = self.core.overall_anisotropic_scale.select(low_res_sel) #* self.core.scale_k1
-        # low-resolution
-        a,b,c,d = self.scale(core=cl, selection=low_res_sel, scale=scale)
-        ssi,x = a,b
-        overall_scale.set_selected(low_res_sel, c)
-        bulk_solvent_scale.set_selected(low_res_sel, d)
-        # high-resolution
-        scale = self.core.overall_anisotropic_scale.select(high_res_sel) #* self.core.scale_k1
-        a,b,c,d = self.scale(core=ch,selection=high_res_sel, scale=scale)
-        ssi.extend(a)
-        x.extend(b)
-        overall_scale.set_selected(high_res_sel, c)
-        bulk_solvent_scale.set_selected(high_res_sel, d)
-      else:
-        scale = self.core.overall_anisotropic_scale #* self.core.scale_k1
-        a,b,overall_scale,bulk_solvent_scale = self.scale(core=clh,
-          selection=all_sel, scale=scale)
-        ssi,x = a,b
+      for i_cas, cas in enumerate(cores_and_selections):
+        sel = cas[0]
+        scale = self.core.overall_anisotropic_scale.select(sel)
+        a,b,c,d = self.scale(core=cas[1], selection=sel, scale=scale)
+        if(i_cas == 0):
+          ssi,x = a,b
+          overall_scale.set_selected(sel, c)
+          bulk_solvent_scale.set_selected(sel, d)
+        else:
+          ssi.extend(a)
+          x.extend(b)
+          overall_scale.set_selected(sel, c)
+          bulk_solvent_scale.set_selected(sel, d)
       assert (overall_scale < 0).count(True) == 0
       assert (bulk_solvent_scale < 0).count(True) == 0
       self.core.update(
@@ -316,6 +270,7 @@ class run(object):
       if(verbose):
         print >> log, "      bulk-solvent:"
         print >> log, "        r        : %6.4f"%self.core.r_factor()
+      #self.anisotropic_scaling() # XXX order IS important
       r_final = self.core.r_factor()
       if((r_start<=r_final) or
          (r_start>r_final and abs(r_start-r_final)<1.e-4)):
@@ -331,7 +286,7 @@ class run(object):
                              sliding_window_average(x=x,offset=4)):
         print >> log, "%8.6f %8.6f %8.6f %8.6f %8.6f %8.6f"%(a,b, c,d,e,f)
       add = False
-      for i in [0,1,2,3,4]:
+      for i in [0]:#[0,1,2,3,4]:
         if i == 0:
           sel = x>0
           xi = x.select(sel)
@@ -343,6 +298,84 @@ class run(object):
           ssii = ssi.select(sel)
         r = scitbx.math.gaussian_fit_1d_analytical(x = ssii, y = xi)
         print >> log, "k_sol, b_sol: %5.3f %7.2f"%(r.a, r.b)
+    #
+    #bulk_solvent_scale = r.a*flex.exp(-ss*r.b)
+    #self.core.update(
+    #    bulk_solvent_scale = bulk_solvent_scale)
+    #print "LOOK:", self.core.r_factor()
+
+  def anisotropic_scaling(self):
+    if(not self.use_minimization_for_aniso_scaling):
+      # try exp_anal
+      obj = bulk_solvent.aniso_u_scaler(
+        f_model        = self.core.f_model_no_scale().data(),
+        f_obs          = self.core.f_obs.data(),
+        miller_indices = self.core.f_obs.indices(),
+        adp_constraint_matrix = self.adp_constraints.gradient_sum_matrix())
+      u_star = self.adp_constraints.all_params(tuple(obj.u_star_independent))
+      b_cart = adptbx.u_as_b(adptbx.u_star_as_u_cart(
+        self.core.f_obs.unit_cell(), u_star))
+      overall_anisotropic_scale_expanal = ext.overall_anisotropic_scale(
+        self.core.f_obs.indices(), u_star)
+      r_expanal = self.core.try_overall_anisotropic_scale(
+        scale_array = overall_anisotropic_scale_expanal)
+      # try poly
+      obj = bulk_solvent.aniso_u_scaler(
+        f_model        = self.core.f_model_no_scale().data(),
+        f_obs          = self.core.f_obs.data(),
+        miller_indices = self.core.f_obs.indices(),
+        unit_cell      = self.core.f_obs.unit_cell())
+      overall_anisotropic_scale_poly = ext.overall_anisotropic_scale(
+        self.core.f_obs.indices(), obj.a, self.core.f_obs.unit_cell())
+      r_poly = self.core.try_overall_anisotropic_scale(
+        scale_array = overall_anisotropic_scale_poly)
+      if(r_poly<r_expanal):
+        overall_anisotropic_scale = overall_anisotropic_scale_poly
+        b_cart = obj.a # not really b_cart
+      else:
+        overall_anisotropic_scale = overall_anisotropic_scale_expanal
+      if(self.verbose):
+        print >> self.log, "        r_poly   : %6.4f"%r_poly
+        print >> self.log, "        r_expanal: %6.4f"%r_expanal
+      self.core.update(overall_anisotropic_scale = overall_anisotropic_scale)
+    else:
+      zero = self.core.f_calc.customized_copy(data =
+        flex.complex_double(self.core.f_obs.data().size(), 0))
+      fm = mmtbx.f_model.core(
+        f_calc  = self.core.f_model_no_scale(),
+        f_mask  = zero,
+        k_sols  = [0.],
+        b_sol   = 0.,
+        f_part1 = None,
+        f_part2 = None,
+        u_star  = [0,0,0,0,0,0],
+        fmodel  = None,
+        ss      = None)
+      obj = bulk_solvent_and_scaling.u_star_minimizer(
+        fmodel_core_data = fm,
+        f_obs            = self.core.f_obs,
+        u_initial        = [0,0,0,0,0,0],
+        refine_u         = True,
+        min_iterations   = 500,
+        max_iterations   = 500,
+        symmetry_constraints_on_b_cart = True,
+        u_min_max = 500.,
+        u_min_min =-500.)
+      u_star = obj.u_min
+      overall_anisotropic_scale = ext.overall_anisotropic_scale(
+        self.core.f_obs.indices(), u_star)
+      b_cart = adptbx.u_as_b(adptbx.u_star_as_u_cart(
+        self.core.f_obs.unit_cell(), u_star))
+      self.core.update(overall_anisotropic_scale = overall_anisotropic_scale)
+    r_aniso = self.core.r_factor()
+    if(self.verbose):
+      print >> self.log, "        r_final  : %6.4f"%r_aniso
+      if(len(b_cart)<=6):
+        print >> self.log, "        b_cart(11,22,33,12,13,23):",\
+          ",".join([str("%8.4f"%i).strip() for i in b_cart])
+      else:
+        print >> self.log, "        a:",\
+          ",".join([str("%8.4f"%i).strip() for i in b_cart])
 
   def scale(self, core, selection, scale):
     f_obs  = core.f_obs
