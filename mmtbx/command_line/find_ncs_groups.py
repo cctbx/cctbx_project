297ec1d435f1f0c21258fe95f3cca45c5eeeeb88
Jeffrey Headd <jjheadd@users.noreply.github.com>
2012-01-10 18:00:35 +0000

    added quick command line function to automatically determine NCS groups

---
+++ mmtbx/command_line/find_ncs_groups.py
@@ -0,0 +1,24 @@
+# LIBTBX_SET_DISPATCHER_NAMEW mmtbx.find_ncs_groups
+
+import sys
+import time
+from mmtbx.torsion_restraints import torsion_ncs
+from iotbx import pdb
+
+def run(args):
+  file_name = args[0]
+  pdb_io = pdb.input(file_name)
+  pdb_hierarchy = pdb_io.construct_hierarchy()
+  pdb_hierarchy.atoms().reset_i_seq()
+  ncs_groups = torsion_ncs.determine_ncs_groups(
+                 pdb_hierarchy=pdb_hierarchy)
+  for i, group in enumerate(ncs_groups):
+    print "Group %d:" % (i+1)
+    for chain in group:
+      print "  "+chain
+    print
+
+if (__name__ == "__main__"):
+  t0 = time.time()
+  run(args=sys.argv[1:])
+  print "Time: %10.3f"%(time.time()-t0)
