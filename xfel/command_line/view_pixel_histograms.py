126f896d5f92ad0f31e5290990803eda4975f646
Richard Gildea <richard.gildea@diamond.ac.uk>
2012-01-19 01:42:30 +0000

    xfel/command_line/view_pixel_histograms.py: reorganisation of code

--- xfel/command_line/view_pixel_histograms.py
+++ xfel/command_line/view_pixel_histograms.py
@@ -6,7 +6,6 @@ import sys
 
 from libtbx import easy_pickle
 from libtbx.option_parser import option_parser
-from libtbx.utils import frange
 from scitbx.array_family import flex
 from scitbx.math import curve_fitting
 
@@ -57,141 +56,134 @@ def run(args):
   d = easy_pickle.load(path)
   args = args[1:]
 
-  if roi is not None:
-
-    summed_hist = None
-    for i in range(roi[2], roi[3]):
-      for j in range(roi[0], roi[1]):
-        if summed_hist is None:
-          summed_hist = d[(i,j)]
-        else:
-          summed_hist.update(d[(i,j)])
-
-    title = str(roi)
-    plot(hist, window_title=window_title, title=title,log_scale=log_scale,
-         normalise=normalise, save_image=save_image, fit_gaussians=fit_gaussians)
-    return
-
+  pixels = None
   if len(args) > 0:
-    for arg in args:
-      pixel = eval(arg)
+    pixels = [eval(arg) for arg in args]
+    for pixel in pixels:
       assert isinstance(pixel, tuple)
       assert len(pixel) == 2
-      hist = d[pixel]
-      print pixel
-      title = str(pixel)
-      plot(hist, window_title=window_title, title=title,log_scale=log_scale,
-           normalise=normalise, save_image=save_image, fit_gaussians=fit_gaussians)
-  else:
-    pixels = sorted(d.keys())
-    if starting_pixel is not None:
-      pixels = pixels[pixels.index(starting_pixel):]
+
+  #if roi is not None:
+
+    #summed_hist = None
+    #for i in range(roi[2], roi[3]):
+      #for j in range(roi[0], roi[1]):
+        #if summed_hist is None:
+          #summed_hist = d[(i,j)]
+        #else:
+          #summed_hist.update(d[(i,j)])
+
+    #title = str(roi)
+    #plot(hist, window_title=window_title, title=title,log_scale=log_scale,
+         #normalise=normalise, save_image=save_image, fit_gaussians=fit_gaussians)
+    #return
+
+  histograms = pixel_histograms(d)
+  histograms.plot(
+    pixels=pixels, starting_pixel=starting_pixel, fit_gaussians=fit_gaussians,
+    window_title=window_title, log_scale=log_scale, save_image=save_image)
+
+
+class pixel_histograms(object):
+
+  def __init__(self, histograms):
+    self.histograms = histograms
+
+  def plot(self, pixels=None, starting_pixel=None, fit_gaussians=True,
+           window_title=None, log_scale=False, save_image=False):
+    normalise=False # XXX
+    assert [pixels, starting_pixel].count(None) > 0
+    if pixels is None:
+      pixels = sorted(self.histograms.keys())
+      if starting_pixel is not None:
+        pixels = pixels[pixels.index(starting_pixel):]
     for pixel in pixels:
-      hist = d[pixel]
+      hist = self.histograms[pixel]
       print pixel
       title = str(pixel)
-      plot(hist, window_title=window_title, title=title,log_scale=log_scale,
-           normalise=normalise, save_image=save_image, fit_gaussians=fit_gaussians)
+      self.plot_one_histogram(
+        hist, window_title=window_title, title=title,log_scale=log_scale,
+        normalise=normalise, save_image=save_image)
+      if fit_gaussians:
+        gaussians = self.fit_one_histogram(hist)
 
-def plot(hist, window_title=None, title=None,
-         log_scale=False, normalise=False, save_image=False,
-         fit_gaussians=False):
-  pyplot.clf() # clear current figure
-  if log_scale:
-    pyplot.yscale("log")
+        x = hist.slot_centers()
+        y_calc = flex.double(x.size(), 0)
+        for g in gaussians:
+          print g.params
+          y = g(x)
+          y_calc += y
+          pyplot.plot(x, y)
+        pyplot.plot(x, y_calc)
 
-  slots = hist.slots().as_double()
-  if normalise:
-    normalisation = (flex.sum(slots) + hist.n_out_of_slot_range()) / 1e5
-    print "normalising by factor: ", normalisation
-    slots /= normalisation
-  bins, data = hist_outline(hist)
-  pyplot.plot(bins, data, '-k')
-  #pyplot.bar(hist.slot_centers()-0.5*hist.slot_width(), slots, width=hist.slot_width())
-  pyplot.xlim(hist.data_min(), hist.data_max())
-  pyplot.suptitle(title)
-  data_min = min([slot.low_cutoff for slot in hist.slot_infos() if slot.n > 0])
-  data_max = max([slot.low_cutoff for slot in hist.slot_infos() if slot.n > 0])
-  pyplot.xlim(data_min, data_max)
+      if save_image:
+        pyplot.savefig("%s.png" %title)
+      else:
+        pyplot.show()
 
-  if fit_gaussians:
 
-    import scitbx.lbfgs
+  def plot_one_histogram(self, histogram,
+                         window_title=None, title=None,
+                         log_scale=False, normalise=False, save_image=False):
+    pyplot.clf() # clear current figure
+    if log_scale:
+      pyplot.yscale("log")
 
+    slots = histogram.slots().as_double()
+    if normalise:
+      normalisation = (flex.sum(slots) + histogram.n_out_of_slot_range()) / 1e5
+      print "normalising by factor: ", normalisation
+      slots /= normalisation
+    bins, data = hist_outline(histogram)
+    pyplot.plot(bins, data, '-k')
+    #pyplot.bar(hist.slot_centers()-0.5*hist.slot_width(), slots, width=hist.slot_width())
+    pyplot.xlim(histogram.data_min(), histogram.data_max())
+    pyplot.suptitle(title)
+    data_min = min([slot.low_cutoff for slot in histogram.slot_infos() if slot.n > 0])
+    data_max = max([slot.low_cutoff for slot in histogram.slot_infos() if slot.n > 0])
+    pyplot.xlim(data_min, data_max)
+
+  def fit_one_histogram(self, histogram):
     n_gaussians_to_fit = 2
-
     fitted_gaussians = []
 
+    # XXX These parameter should be configurable
     lower_fit_thresholds = [-1000, 20, 50]
     upper_fit_thresholds = [20, 40, 70]
     estimated_peak_positions = [0, 30, 60]
 
-    x = hist.slot_centers()
-    y = hist.slots().as_double()
+    x = histogram.slot_centers()
+    y = histogram.slots().as_double()
 
     for i in range(n_gaussians_to_fit):
       lower_threshold = lower_fit_thresholds[i]
       upper_threshold = upper_fit_thresholds[i]
       mean = estimated_peak_positions[i]
-
-      fit = peak_fit(hist, lower_threshold, upper_threshold, mean)
-
-      #diff_peak_position = estimated_peak_positions[i] - fit.functions[0].params[1]
-      #if abs(diff_peak_position) > 1:
-        #lower_threshold += diff_peak_position
-        #upper_threshold += diff_peak_position
-        #mean += diff_peak_position
-        #print "Refitting peak: delta peak position: %.2f" %diff_peak_position
-        #print fit.functions[0].params
-        #fit = peak_fit(hist, lower_threshold, upper_threshold, mean)
-        #print fit.functions[0].params
-
+      fit = self.single_peak_fit(histogram, lower_threshold, upper_threshold, mean)
       fitted_gaussians += fit.functions
 
-    y_calc = flex.double(x.size(), 0)
-    for g in fitted_gaussians:
-      #print "%.3f %.3f %.3f %.3f" %(g.a, g.b, g.c, g.area())
-      #scale = g.a
-      #mu = g.b
-      #sigma = g.c
-      #print scale, mu, sigma
-      #y = scale * flex.exp(-flex.pow2(x - mu) / (2 * sigma**2))
-      print g.params
-      y = g(x)
-      y_calc += y
-      pyplot.plot(x, y)
-    pyplot.plot(x, y_calc)
-    if log_scale:
-      pyplot.ylim(ymin=0.1)
-  pyplot.show()
+    return fitted_gaussians
 
-  if save_image:
-    pyplot.savefig("%s.png" %title)
-  else:
-    pyplot.show()
-  return
+  def single_peak_fit(self, hist, lower_threshold, upper_threshold, mean):
+    starting_gaussians = [curve_fitting.gaussian(a=100, b=mean, c=5)]
+    lower_slot = 0
+    for slot in hist.slot_centers():
+      lower_slot += 1
+      if slot > lower_threshold: break
+    upper_slot = 0
+    for slot in hist.slot_centers():
+      upper_slot += 1
+      if slot > upper_threshold: break
 
-def peak_fit(hist, lower_threshold, upper_threshold, mean):
-  starting_gaussians = [curve_fitting.gaussian(a=100, b=mean, c=5)]
-  lower_slot = 0
-  for slot in hist.slot_centers():
-    lower_slot += 1
-    if slot > lower_threshold: break
-  upper_slot = 0
-  for slot in hist.slot_centers():
-    upper_slot += 1
-    if slot > upper_threshold: break
-
-  x = hist.slot_centers()
-  y = hist.slots().as_double()
-  if 1:
-    fit = curve_fitting.lbfgs_minimiser(
-      starting_gaussians, x[lower_slot:upper_slot], y[lower_slot:upper_slot])
-  else:
-    fit = curve_fitting.cma_es_minimiser(
-      starting_gaussians, x[lower_slot:upper_slot], y[lower_slot:upper_slot])
-  return fit
-
+    x = hist.slot_centers()
+    y = hist.slots().as_double()
+    if 1:
+      fit = curve_fitting.lbfgs_minimiser(
+        starting_gaussians, x[lower_slot:upper_slot], y[lower_slot:upper_slot])
+    else:
+      fit = curve_fitting.cma_es_minimiser(
+        starting_gaussians, x[lower_slot:upper_slot], y[lower_slot:upper_slot])
+    return fit
 
 
 def hist_outline(hist):
