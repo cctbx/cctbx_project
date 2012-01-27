1f127cfa3e006a9c2509fd81a4e7e2ec98766a14
Richard Gildea <richard.gildea@diamond.ac.uk>
2012-01-27 22:47:09 +0000

    place legend above figure rather than within

--- xfel/command_line/overlay_spectra.py
+++ xfel/command_line/overlay_spectra.py
@@ -11,7 +11,11 @@ def run(args):
     x = flex.double(flex.std_string(x))
     y = flex.double(flex.std_string(y))
     pyplot.plot(x, y/flex.max(y), label=filename)
-  pyplot.legend(loc=3) # lower left
+  legend = pyplot.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+                         ncol=2, mode="expand", borderaxespad=0.)
+  for t in legend.get_texts():
+    t.set_fontsize(13)
+
   pyplot.show()
 
 if __name__ == '__main__':
