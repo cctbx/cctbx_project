c4036c2f03386c1bee5e69f6a005006a1fa55d08
Johan Hattne <johan@hattne.se>
2012-02-01 23:06:32 +0000

    Initial attempt at supporting multitile detectors.

--- iotbx/detectors/detectors_ext.cpp
+++ iotbx/detectors/detectors_ext.cpp
@@ -454,8 +454,9 @@ BOOST_PYTHON_MODULE(iotbx_detectors_ext)
           ))
       .def("prep_string",&iotbx::detectors::display::generic_flex_image::prep_string)
 
-.def("add_rotation_translation",&iotbx::detectors::display::generic_flex_image::add_rotation_translation)
-.def("picture_to_readout",&iotbx::detectors::display::generic_flex_image::picture_to_readout_f)
+      .def("add_rotation_translation",&iotbx::detectors::display::generic_flex_image::add_rotation_translation)
+      .def("picture_to_readout",&iotbx::detectors::display::generic_flex_image::picture_to_readout_f)
+      .def("setWindowCart", &iotbx::detectors::display::generic_flex_image::setWindowCart)
   ;
 
   class_<iotbx::detectors::display::Color>("Color", no_init)
