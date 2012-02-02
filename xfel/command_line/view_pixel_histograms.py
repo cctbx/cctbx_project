f0cd67c16bfd5b3358549920ad4509804bd54852
Richard Gildea <richard.gildea@diamond.ac.uk>
2012-02-02 22:30:28 +0000

    more general finding of secondary histogram peaks

--- xfel/command_line/view_pixel_histograms.py
+++ xfel/command_line/view_pixel_histograms.py
@@ -39,6 +39,10 @@ def run(args):
                           action="store_true",
                           default=False,
                           help="Fit gaussians to the peaks.")
+                  .option("--estimated_gain",
+                          type="float",
+                          default=30,
+                          help="The approximate position of the one photon peak.")
                   ).process(args=args)
   log_scale = command_line.options.log_scale
   fit_gaussians = command_line.options.fit_gaussians
@@ -46,6 +50,7 @@ def run(args):
   normalise = command_line.options.normalise
   save_image = command_line.options.save
   starting_pixel = command_line.options.start
+  estimated_gain = command_line.options.estimated_gain
   if starting_pixel is not None:
     starting_pixel = eval(starting_pixel)
     assert isinstance(starting_pixel, tuple)
@@ -78,7 +83,7 @@ def run(args):
          #normalise=normalise, save_image=save_image, fit_gaussians=fit_gaussians)
     #return
 
-  histograms = pixel_histograms(d)
+  histograms = pixel_histograms(d, estimated_gain=estimated_gain)
   histograms.plot(
     pixels=pixels, starting_pixel=starting_pixel, fit_gaussians=fit_gaussians,
     window_title=window_title, log_scale=log_scale, save_image=save_image)
@@ -86,8 +91,9 @@ def run(args):
 
 class pixel_histograms(object):
 
-  def __init__(self, histograms):
+  def __init__(self, histograms, estimated_gain=30):
     self.histograms = histograms
+    self.estimated_gain = estimated_gain
 
   def plot(self, pixels=None, starting_pixel=None, fit_gaussians=True,
            window_title=None, log_scale=False, save_image=False):
@@ -118,6 +124,7 @@ class pixel_histograms(object):
     if log_scale:
       pyplot.ylim(ymin=0.1)
     hist = self.histograms[pixel]
+    #pyplot.ylim(ymax=flex.max(hist.slots()))
     gaussians = self.fit_one_histogram(pixel)
     x = hist.slot_centers()
     y_calc = flex.double(x.size(), 0)
@@ -127,13 +134,12 @@ class pixel_histograms(object):
       y_calc += y
       pyplot.plot(x, y)
     #print "Peak height ratio: %.2f" %(gaussians[0].params[0]/gaussians[1].params[0])
-    params = gaussians[0].params
-    g = curve_fitting.gaussian(params[0], 0, params[2])
-    pyplot.plot(x, g(x))
     pyplot.plot(x, y_calc)
+    # Plot the fit residuals
     xlim = pyplot.xlim() # store for reuse below
     pyplot.subplot(212)
     pyplot.plot(x, hist.slots().as_double() - y_calc)
+
     pyplot.xlim(xlim)
 
   def plot_one_histogram(self, histogram,
@@ -156,42 +162,58 @@ class pixel_histograms(object):
     data_max = max([slot.low_cutoff for slot in histogram.slot_infos() if slot.n > 0])
     pyplot.xlim(data_min, data_max)
 
-  def fit_one_histogram(self, pixel):
+  def fit_one_histogram(self, pixel, n_gaussians=2):
+    #n_gaussians = 1
     histogram = self.histograms[pixel]
-    n_gaussians_to_fit = 2
     fitted_gaussians = []
-
-    # XXX These parameter should be configurable
-    lower_fit_thresholds = [-1000, 20, 50]
-    upper_fit_thresholds = [20, 40, 70]
-    estimated_peak_positions = [0, 30, 60]
 
     slot_centers = histogram.slot_centers()
     slots = histogram.slots().as_double()
 
-    for i in range(n_gaussians_to_fit):
-      lower_threshold = lower_fit_thresholds[i]
-      upper_threshold = upper_fit_thresholds[i]
-      mean = estimated_peak_positions[i]
-      #max_freq = 0
-      #max_slot_i = 0
-      #if i > 0: # XXX
-        #for j, slot_center in enumerate(slot_centers):
-          #if slot_center < lower_threshold: continue
-          #if slot_center > upper_threshold: break
-          #max_freq = max(max_freq, slots[j])
-          #if max_freq == slots[j]:
-            #max_slot_i = j
-        #mean = slot_centers[max_slot_i]
-        #lower_threshold = mean - 8
-        #upper_threshold = mean + 8
-      fit = self.single_peak_fit(histogram, lower_threshold, upper_threshold, mean)
+    zero_peak_gaussian = None
+    for i in range(n_gaussians):
+      if i == 0:
+        lower_threshold = -1000
+        upper_threshold = 0.4 * self.estimated_gain
+        mean = 0
+        fit = self.single_peak_fit(histogram, lower_threshold, upper_threshold, mean,
+                                   zero_peak_gaussian=zero_peak_gaussian)
+        hist_max = flex.max(histogram.slots())
+        if abs(fit.functions[0].params[0] - hist_max)/hist_max > 0.1:
+          upper_threshold = 0.3 * self.estimated_gain
+          fit = self.single_peak_fit(histogram, lower_threshold, upper_threshold, mean,
+                                     zero_peak_gaussian=zero_peak_gaussian)
+      else:
+        lower_threshold = self.estimated_gain * i - 0.2 * self.estimated_gain
+        upper_threshold = self.estimated_gain * i + 0.33 * self.estimated_gain
+        y_obs = histogram.slots().as_double()
+        x = histogram.slot_centers()
+        y_calc = flex.double(y_obs.size(), 0)
+        for g in fitted_gaussians:
+          y_calc += g(x)
+        residual = y_obs - y_calc
+        # triangular smoothing of residual to find peak position
+        residual = sliding_average(residual)
+        residual = sliding_average(residual)
+        # we assume that the peaks are separated by at least 4 sigma
+        four_sigma = abs(4 * fitted_gaussians[0].params[2])
+        slot_i = histogram.get_i_slot(four_sigma)
+        max_slot_i = flex.max_index(residual[slot_i:]) + slot_i
+        mean = slot_centers[max_slot_i]
+        lower_threshold = mean - 0.3 * (mean - fitted_gaussians[0].params[1])
+        upper_threshold = mean + 0.4 * (mean - fitted_gaussians[0].params[1])
+        print lower_threshold, mean, upper_threshold
+        #zero_peak_gaussian = None
+        fit = self.single_peak_fit(histogram, lower_threshold, upper_threshold, mean,
+                                   zero_peak_gaussian=zero_peak_gaussian)
       fitted_gaussians += fit.functions
+      if i == 0: zero_peak_gaussian = fit.functions[0]
 
     return fitted_gaussians
 
-  def single_peak_fit(self, hist, lower_threshold, upper_threshold, mean):
-    starting_gaussians = [curve_fitting.gaussian(a=100, b=mean, c=5)]
+  def single_peak_fit(self, hist, lower_threshold, upper_threshold, mean,
+                      zero_peak_gaussian=None):
+    starting_gaussians = [curve_fitting.gaussian(a=100, b=mean, c=3)]
     lower_slot = 0
     for slot in hist.slot_centers():
       lower_slot += 1
@@ -203,6 +225,8 @@ class pixel_histograms(object):
 
     x = hist.slot_centers()
     y = hist.slots().as_double()
+    if zero_peak_gaussian is not None:
+      y -= zero_peak_gaussian(x)
     if 1:
       fit = curve_fitting.lbfgs_minimiser(
         starting_gaussians, x[lower_slot:upper_slot], y[lower_slot:upper_slot])
@@ -219,6 +243,15 @@ class pixel_histograms(object):
   def pixels(self):
     for pixel in self.histograms.keys():
       yield pixel
+
+def sliding_average(y):
+  y_trimmed = y[1:-1]
+  y_right_shifted = y[:-2]
+  y_left_shifted = y[2:]
+  averaged = (y_trimmed + y_right_shifted + y_left_shifted) * (1/3)
+  averaged.insert(0, y[0])
+  averaged.append(y[-1])
+  return averaged
 
 
 def hist_outline(hist):
