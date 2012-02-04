59f075d60b6622f59d183c832493aa76a44f1ee5
Nicholas Sauter <nksauter@lbl.gov>
2012-02-04 07:16:36 +0000

    Refactor the image viewer, pushing some coordinate-related functions down to the detector image base class.  This allows future support of tiled detectors wherein coordinate transformations are handled behind an API.

--- iotbx/detectors/detectorbase.py
+++ iotbx/detectors/detectorbase.py
@@ -165,3 +165,71 @@ CCD_IMAGE_SATURATION=65535;
     print >> out, "Rotation width: %.2f deg."%self.deltaphi
     print >> out, "Beam center: x=%.2f mm  y=%.2f mm"%(self.beamx,self.beamy)
     print >> out, "Wavelength: %f Ang."%self.wavelength
+
+  # code developed for the image viewer. phil_parameters is a scope extract
+  def initialize_viewer_properties(self,phil_parameters):
+
+    self._invert_beam_center = False
+    from iotbx.detectors.context.config_detector import \
+      beam_center_convention_from_image_object
+    bc = beam_center_convention_from_image_object(self,phil_parameters)
+    print "beam center convention: %d" % bc
+    # FIXME what about 2-4 & 6-7?
+    if (bc == 0) :
+      self._invert_beam_center = True
+      self._invert_y = True
+    elif (bc == 1) :
+      self._invert_y = False
+    elif (bc == 5) :
+      self._invert_y = True
+
+    self.image_size_fast = self.size2 # width
+    self.image_size_slow = self.size1 # height
+    self.pixel_resolution = self.pixel_size
+
+  def detector_coords_as_image_coords_float (self, x, y) :
+    """
+    Convert absolute detector position (in mm) to floating-value image pixel coordinates.
+    """
+    dw = self.image_size_fast * self.pixel_resolution
+    dh = self.image_size_slow * self.pixel_resolution
+    x_frac = x / dw
+    if (self._invert_y) :
+      y_frac = - ((y / dh) - 1.0)
+    else :
+      y_frac = y / dh
+    return x_frac * self.image_size_fast, \
+           y_frac * self.image_size_slow
+
+  def detector_coords_as_image_coords (self, x, y) :
+    """
+    Convert absolute detector position (in mm) to integer-value image pixel coordinates.
+    """
+    x_point,y_point = self.detector_coords_as_image_coords_float(x,y)
+    return (int(x_point), int(y_point))
+
+  def image_coords_as_detector_coords (self, x, y) :
+    """
+    Convert image pixel coordinates to absolute position on the detector
+    (in mm).
+    """
+    dw = self.image_size_fast * self.pixel_resolution
+    dh = self.image_size_slow * self.pixel_resolution
+    x_frac = x / self.image_size_fast
+    y_frac = y / self.image_size_slow
+    x_detector = x_frac * dw
+    if (self._invert_y) :
+      y_detector = (1.0 - y_frac) * dh
+    else :
+      y_detector = y_frac * dh
+    return x_detector, y_detector
+
+  def get_beam_center_mm (self) :
+    # FIXME Pilatus and ADSC images appear to have different conventions???
+    if (self._invert_beam_center) :
+      center_x = self.beamy
+      center_y = self.beamx
+    else :
+      center_x = self.beamx
+      center_y = self.beamy
+    return center_x, center_y
