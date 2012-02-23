27d53ec9a3dddaadf27eeb452134213df9afff49
Nathaniel Echols <nathaniel.echols@gmail.com>
2012-02-23 16:39:04 +0000

    search PDB for similar unit cells

---
+++ mmtbx/command_line/search_pdb_symmetry.py
@@ -0,0 +1,85 @@
+
+import iotbx.phil
+from libtbx.utils import Sorry, Usage
+import sys
+
+master_phil = iotbx.phil.parse("""
+symmetry_search
+  .caption = This utility allows you to search the PDB for structures with \
+    similar unit cell parameters.  Crystallization artifacts due to \
+    impurities in the protein solution can often be detected this way, if the \
+    protein which actually crystallized has been solved before.
+  .style = auto_align caption_img:icons/custom/db_lookup.png
+{
+  file_name = None
+    .type = path
+    .short_caption = File name (PDB or MTZ)
+  unit_cell = None
+    .type = unit_cell
+  space_group = None
+    .type = space_group
+  max_rmsd = None
+    .type = float
+    .short_caption = Max. RMSD to consider
+  max_hits_to_display = 50
+    .type = int
+    .short_caption = Max. number of hits
+}
+""")
+
+def run (args=(), params=None, out=sys.stdout) :
+  if (len(args) > 0) :
+    cmdline = iotbx.phil.process_command_line_with_files(
+      args=args,
+      master_phil=master_phil,
+      pdb_file_def="symmetry_search.file_name",
+      reflection_file_def="symmetry_search.file_name")
+    params = cmdline.work.extract().symmetry_search
+  elif (params is None) :
+    raise Usage("""mmtbx.search_pdb_symmetry [file] [space_group] [unit_cell]
+  Utility for finding similar unit cells deposited in the PDB.
+""")
+  else :
+    params = params.symmetry_search
+  from mmtbx import pdb_symmetry
+  from iotbx import crystal_symmetry_from_any
+  from cctbx import crystal
+  db = pdb_symmetry.load_db()
+  if (params.file_name is not None) :
+    symm = crystal_symmetry_from_any.extract_from(file_name=params.file_name)
+    if (symm is None) :
+      raise Sorry("The file %s does not include symmetry information." %
+        params.file_name)
+    elif (symm.space_group() is None) or (symm.unit_cell() is None) :
+      raise Sorry("Incomplete symmetry information in %s." % params.file_name)
+  else :
+    symm = crystal.symmetry(
+      unit_cell=params.unit_cell,
+      space_group_info=params.space_group)
+  print >> out, ""
+  print >> out, "Input symmetry:"
+  symm.show_summary()
+  scores = pdb_symmetry.symmetry_search(db, symm, max_rmsd=params.max_rmsd)
+  niggli_cell = symm.niggli_cell().unit_cell().parameters()
+  print >> out, ""
+  print >> out, "Top %d matches (sorted by RMSD):"
+  for scored in scores[:params.max_hits_to_display] :
+    print >> out, "%s (rmsd = %.3f, volume ratio = %.2f)" % \
+      (scored.entry.pdb_id, scored.rmsd, scored.volume_ratio)
+    print >> out, "    Unit cell: %8.3f %8.3f %8.3f %8.3f %8.3f %8.3f" % \
+      scored.entry.crystal_symmetry.unit_cell().parameters()
+    print >> out, "  Niggli cell: %8.3f %8.3f %8.3f %8.3f %8.3f %8.3f" % \
+      scored.entry.niggli_cell.unit_cell().parameters()
+    print >> out, "  Target cell: %8.3f %8.3f %8.3f %8.3f %8.3f %8.3f" % \
+      niggli_cell
+    print >> out, ""
+
+def validate_params (params) :
+  params = params.symmetry_search
+  have_symm = (not None in [params.unit_cell, params.space_group])
+  if (not have_symm) and (params.file_name is None) :
+    raise Sorry("Missing or incomplete symmetry information.")
+  return True
+
+if (__name__ == "__main__") :
+  run(sys.argv[1:])
---
+++ mmtbx/pdb_symmetry.py
@@ -0,0 +1,96 @@
+
+import libtbx.load_env
+from libtbx import easy_pickle
+from libtbx import group_args
+from math import sqrt
+import os
+
+def parse_database (file_name) :
+  from cctbx import crystal
+  from cctbx import sgtbx
+  from cctbx import uctbx
+  db = []
+  lines = open(file_name).readlines()
+  process = False
+  for line in lines :
+    line = line.strip()
+    if (process) :
+      fields = line.split()
+      pdb_id = fields[0]
+      try :
+        uc = uctbx.unit_cell([ float(x.replace(",","")) for x in fields[2:8] ])
+      except RuntimeError, e :
+        print "Unit cell error:"
+        print line
+        continue
+      try :
+        sg = sgtbx.space_group_info(" ".join(fields[8:-1]))
+      except RuntimeError, e :
+        print "Unrecognized space group:"
+        print line
+        continue
+      try :
+        symm = crystal.symmetry(unit_cell=uc, space_group_info=sg)
+      except AssertionError :
+        print "Incompatible unit cell parameters:"
+        print line
+        continue
+      niggli_symm = symm.niggli_cell()
+      db.append(group_args(
+        pdb_id=pdb_id,
+        crystal_symmetry=symm,
+        niggli_cell=niggli_symm))
+    elif (line.startswith("-----")) :
+      process = True
+  return db
+
+db_ = None
+def load_db (save_pickle=False) :
+  global db_
+  if (db_ is not None) :
+    return db_
+  pkl_file_name = libtbx.env.find_in_repositories(
+    relative_path="chem_data/pdb/crystal.pkl",
+    test=os.path.isfile)
+  txt_file_name = libtbx.env.find_in_repositories(
+    relative_path="chem_data/pdb/crystal.idx",
+    test=os.path.isfile)
+  if (pkl_file_name is None) or (save_pickle) :
+    assert (txt_file_name is not None)
+    db_ = parse_database(txt_file_name)
+    if (save_pickle) :
+      easy_pickle.dump(txt_file_name[:-4] + ".pkl", db_)
+  else :
+    db_ = easy_pickle.load(pkl_file_name)
+  return db_
+
+def rms_difference (params1, params2) :
+  assert (len(params1) == len(params2) != 0)
+  sum = 0
+  n = 0
+  for x1, x2 in zip(params1, params2) :
+    sum += (x1-x2)**2
+  return sqrt(sum / len(params1))
+
+def symmetry_search (
+    symmetry_db,
+    crystal_symmetry,
+    max_rmsd=None) :
+  scores = []
+  niggli_cell = crystal_symmetry.niggli_cell()
+  input_volume = niggli_cell.unit_cell().volume()
+  niggli_uc = niggli_cell.unit_cell().parameters()
+  for entry in symmetry_db :
+    entry_volume = entry.niggli_cell.unit_cell().volume()
+    entry_uc = entry.niggli_cell.unit_cell().parameters()
+    rmsd = rms_difference(niggli_uc, entry_uc)
+    if (max_rmsd is not None) and (rmsd > max_rmsd) :
+      continue
+    scores.append(group_args(entry=entry,
+      rmsd=rmsd,
+      volume_ratio=entry_volume/input_volume))
+  scores.sort(lambda x,y: cmp(x.rmsd, y.rmsd))
+  return scores
+
+if (__name__ == "__main__") :
+  load_db(save_pickle=True)
