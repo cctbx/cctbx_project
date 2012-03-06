5455551bc8f6659f256aca7c6b8b9bdeca0d02f3
Nicholas Sauter <nksauter@lbl.gov>
2012-03-06 21:49:24 +0000

    Better Hamamatsu and KEK serial number implementations.

--- iotbx/detectors/hamamatsu.py
+++ iotbx/detectors/hamamatsu.py
@@ -16,3 +16,29 @@ class HamamatsuImage(ADSCImage):
     DetectorImageBase.__init__(self,filename)
     self.vendortype = "Hamamatsu"
 
+  def readHeader(self):
+    ADSCImage.readHeader(self,external_keys=[("DETECTOR_NAME","DETECTOR_NAME",str)])
+    assert self.parameters["DETECTOR_NAME"].lower().find("hamamatsu")>=0
+
+    #above code validates the Hamamatsu signature, as in
+    """HEADER_BYTES=512;
+DIM=2;
+BYTE_ORDER=little_endian;
+TYPE=unsigned_short;
+SIZE1=2352;
+SIZE2=2352;
+PIXEL_SIZE=0.050000;
+BIN=1x1;
+DETECTOR_NAME=Hamamatsu C10158DK;
+DATE=Tue Jun 14 15:31:08 2011;
+TIME=0.33;
+DISTANCE=144.000;
+OSC_RANGE=1.000;
+OMEGA=1.000;
+OSC_START=0.000;
+TWOTHETA=0.000;
+AXIS=Omega;
+WAVELENGTH=1.00000;
+BEAM_CENTER_X=58.425;
+BEAM_CENTER_Y=58.775;
+"""
