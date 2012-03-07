81edda7c1ba3c099be2641269083acef0dd5157a
Nathaniel Echols <nathaniel.echols@gmail.com>
2012-03-07 22:56:43 +0000

    bug fixes and test for mmtbx.prune_model

---
+++ mmtbx/regression/tst_prune_model.py
@@ -0,0 +1,22 @@
+
+import libtbx.load_env
+from libtbx import easy_run
+import os
+
+def exercise ():
+  pdb_file = libtbx.env.find_in_repositories(
+    relative_path="phenix_regression/pdb/prune.pdb",
+    test=os.path.isfile)
+  mtz_file = libtbx.env.find_in_repositories(
+    relative_path="phenix_regression/reflection_files/prune_data.mtz",
+    test=os.path.isfile)
+  if (None in [pdb_file, mtz_file]) :
+    print "Input files not found, skipping test"
+    return
+  output = easy_run.fully_buffered("mmtbx.prune_model %s %s" % (pdb_file,
+    mtz_file)).stdout_lines
+  assert ("Removed 3 residues and 1 sidechains")
+
+if (__name__ == "__main__") :
+  exercise()
+  print "OK"
