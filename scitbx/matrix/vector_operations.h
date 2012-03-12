cd705ff21bd998e5981fd71f84662f5676f333e7
Luc J. Bourhis <luc_j_bourhis@mac.com>
2012-03-12 16:07:39 +0000

    Make scale_vector and dot inline and fix documentation for the former

--- scitbx/matrix/vector_operations.h
+++ scitbx/matrix/vector_operations.h
@@ -3,8 +3,9 @@
 
 namespace scitbx { namespace matrix {
 
-  /// x(0:n) /= alpha
+  /// x(0:n) *= alpha
   template <typename T>
+  inline
   void scale_vector(int n, T *x, T alpha) {
     if (alpha == 0) std::fill_n(x, n, T(0));
     else if (alpha != 1) for (int i=0; i<n; ++i) x[i] *= alpha;
@@ -12,6 +13,7 @@ namespace scitbx { namespace matrix {
 
   /// Euclidean scalar product of x(0:n) and y(0:n)
   template <typename T>
+  inline
   T dot(int n, T const *x, T const *y) {
     T s = 0;
     for (int i=0; i<n; ++i) s += x[i] * y[i];
