d3da1b592dc92415a9f26ec68241afb857dcd1dd
Luc J. Bourhis <luc_j_bourhis@mac.com>
2012-03-12 16:07:23 +0000

    LIBTBX_BUILD reset to empty string by sourcing setpaths.sh: that broke that script. Thus move to XCode specific environment variables.

--- libtbx/development/xcode_build_phase.sh
+++ libtbx/development/xcode_build_phase.sh
@@ -2,7 +2,7 @@ cd $cctbxroot
 
 ##############################################################################
 
-${LIBTBX_BUILD}/bin/python -u <<PYSCRIPT
+${XCODE_CCTBX_BUILD}/bin/python -u <<PYSCRIPT
 import sys, os, re
 import libtbx.load_env
 from libtbx.command_line import scons
@@ -20,14 +20,14 @@ libtbx.env.build_options.report()
 
 print
 print '-'*80
-os.chdir(os.environ['LIBTBX_BUILD'])
-sys.argv[1:] = os.environ['SCONS_OPTIONS'].split()
-if os.environ.get('SCONS_LIB_TARGET'):
-  libs = os.environ['SCONS_LIB_TARGET'].split()
+os.chdir(os.environ['XCODE_CCTBX_BUILD'])
+sys.argv[1:] = os.environ['XCODE_SCONS_OPTIONS'].split()
+if os.environ.get('XCODE_SCONS_LIB_TARGET'):
+  libs = os.environ['XCODE_SCONS_LIB_TARGET'].split()
   sys.argv.extend([ "lib/%s.so" % lib for lib in libs ])
   print "warning: only library being built: %s" % ', '.join(libs)
-elif os.environ.get('SCONS_PROGRAM_TARGET'):
-  programs = os.environ['SCONS_PROGRAM_TARGET'].split()
+elif os.environ.get('XCODE_SCONS_PROGRAM_TARGET'):
+  programs = os.environ['XCODE_SCONS_PROGRAM_TARGET'].split()
   sys.argv.extend(programs)
   print "warning: only program being built: %s" % ', '.join(programs)
 scons.run()
