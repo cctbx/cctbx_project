f208dd55a6826481a59c785301dc2abab4d777e5
Nathaniel Echols <nathaniel.echols@gmail.com>
2012-03-20 22:05:26 +0000

    added show_summary method for ccp4 map files

--- iotbx/ccp4_map/__init__.py
+++ iotbx/ccp4_map/__init__.py
@@ -1,6 +1,20 @@
 import cctbx.array_family.flex # import dependency
+import sys
 
 import boost.python
 ext = boost.python.import_ext("iotbx_ccp4_map_ext")
 from iotbx_ccp4_map_ext import *
 import iotbx_ccp4_map_ext as ext
+
+class _(boost.python.injector, ext.map_reader) :
+  def show_summary (self, out=None, prefix="") :
+    if (out is None) : out = sys.stdout
+    print >> out, prefix + "header_min: ", self.header_min
+    print >> out, prefix + "header_max: ", self.header_max
+    print >> out, prefix + "header_mean:", self.header_mean
+    print >> out, prefix + "header_rms: ", self.header_rms
+    print >> out, prefix + "unit cell grid:", self.unit_cell_grid
+    print >> out, prefix + "unit cell parameters:", self.unit_cell_parameters
+    print >> out, prefix + "space group number:  ", self.space_group_number
+    print >> out, prefix + "map origin:", self.data.origin()
+    print >> out, prefix + "map grid:  ", self.data.all()
