b1044188818e7f89554e85bd9e00454ec31ff48e
Nathaniel Echols <nathaniel.echols@gmail.com>
2012-03-27 18:39:07 +0000

    allow Windows bundles to be grouped into a single directory

--- libtbx/command_line/start_binary_bundle.py
+++ libtbx/command_line/start_binary_bundle.py
@@ -7,17 +7,29 @@ def run(args):
   if (len(os.listdir(".")) != 0):
     print "Please use this command only in an empty directory."
     return
-  if (len(args) != 2):
-    print "usage: libtbx.start_binary_bundle bundle_name top_modules"
+  if (len(args) != 2) and (len(args) != 3) :
+    print "usage: libtbx.start_binary_bundle bundle_name top_modules [--single_directory]"
     return
-  bundle_name, top_modules = args
+  single_dir = False
+  if (len(args) == 2) :
+    bundle_name, top_modules = args
+  else :
+    if (args[2] != "--single_directory") :
+      print "usage: libtbx.start_binary_bundle bundle_name top_modules [--single_directory]"
+      return
+    bundle_name, top_modules = args[:2]
+    single_dir = True
+  if (single_dir) :
+    os.mkdir(bundle_name)
+    os.chdir(bundle_name)
   copy_all.run(bundle_name)
   if (os.name == "nt"):
     install_script = bundle_name+"_install_script.bat"
     open(install_script, "w").write(
       install_bat.create_script(
         bundle=bundle_name,
-        top_modules=top_modules))
+        top_modules=top_modules,
+        single_dir=single_dir))
   else:
     install_script = bundle_name+"_install_script.csh"
     open(install_script, "w").write(
