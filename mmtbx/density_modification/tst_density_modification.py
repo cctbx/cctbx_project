c071ee382f5ee7af05372b2fbfaa3d0a0b4f5a88
Pavel Afonine <pafonine@users.noreply.github.com>
2012-03-29 17:47:17 +0000

    adjust minor change in tolerances

--- mmtbx/density_modification/tst_density_modification.py
+++ mmtbx/density_modification/tst_density_modification.py
@@ -65,8 +65,8 @@ def exercise_density_modification():
   result = easy_run.fully_buffered(command=cmd).raise_if_errors()
   assert result.stdout_lines[-5].startswith('Starting dm/model correlation:')
   assert result.stdout_lines[-4].startswith('Final dm/model correlation:')
-  assert approx_equal(float(result.stdout_lines[-5].split()[-1]), 0.619448)
-  assert approx_equal(float(result.stdout_lines[-4].split()[-1]), 0.789672, 5e-4)
+  assert approx_equal(float(result.stdout_lines[-5].split()[-1]), 0.618415)
+  assert approx_equal(float(result.stdout_lines[-4].split()[-1]), 0.789281, 5e-4)
 
 def run():
   exercise_density_modification()
