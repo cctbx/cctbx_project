90ad9ebdc564ef78d2eb9819ec36b5d1fd34c23a
David Waterman <david.waterman@stfc.ac.uk>
2012-04-24 12:51:16 +0000

    Beginning to transfer Python code from detector_model in bpcx_regression repo to C++ in CCTBX. Not yet incorporated in the build system; committed for safekeeping.

---
+++ rstbx/bpcx/detector_model/sensor.cpp
@@ -0,0 +1,32 @@
+#include "sensor.h"
+
+rstbx::detector_model::sensor::sensor(
+            const scitbx::vec3<double>& _origin,
+            const scitbx::vec3<double>& _dir1,
+            const scitbx::vec3<double>& _dir2,
+            const scitbx::vec2<double>& _lim1,
+            const scitbx::vec2<double>& _lim2):
+    origin(_origin),
+    dir1(_dir1),
+    dir2(_dir2),
+    lim1(_lim1),
+    lim2(_lim2),
+    normal(dir1.cross(dir2)),
+    D()
+{
+    distance = origin * normal;
+    update();    
+}
+
+// other getters are inline
+scitbx::mat3<double> rstbx::detector_model::sensor::get_d() const
+{
+    return D.inverse();
+}
+
+void rstbx::detector_model::sensor::update()
+{
+    D.set_column(0, dir1);
+    D.set_column(1, dir2);
+    D.set_column(2, origin);
+}
