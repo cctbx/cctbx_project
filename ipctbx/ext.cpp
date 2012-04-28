0c8b08f96c0946e557dc4587f0cf71d513b63b0a
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2012-04-28 00:16:42 +0000

    ipctbx/ext.cpp: new copy_to_new_segment() and copy_from_segment()

--- ipctbx/ext.cpp
+++ ipctbx/ext.cpp
@@ -1,50 +1,60 @@
 #include <boost/python.hpp>
+
+#include <scitbx/array_family/shared.h>
+
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <errno.h>
 
+namespace ipctbx {
+
+namespace af = scitbx::af;
+
+template <typename ElementType>
 int
-exercise_create_segment(
-  std::size_t number_of_integers)
+copy_to_new_segment(
+  af::const_ref<ElementType> const& data)
 {
   int shmid = shmget(
     IPC_PRIVATE,
-    number_of_integers * sizeof(int),
+    data.size() * sizeof(ElementType),
     IPC_CREAT|IPC_EXCL|S_IRUSR|S_IWUSR);
   if (shmid == -1){
     std::string msg = strerror(errno);
     throw std::runtime_error("shmget() failure: " + msg);
   }
-  void* ptr = shmat(shmid, 0, 0);
-  if (ptr == reinterpret_cast<void *>(-1)) {
+  void* void_ptr = shmat(shmid, 0, 0);
+  if (void_ptr == reinterpret_cast<void *>(-1)) {
     std::string msg = strerror(errno);
     throw std::runtime_error("shmat() failure: " + msg);
   }
-  int* data = static_cast<int*>(ptr);
-  for (std::size_t i=0;i<number_of_integers;i++) {
-    data[i] = static_cast<int>(i % 13);
-  }
+  ElementType* data_ptr = static_cast<ElementType*>(void_ptr);
+  std::copy(data.begin(), data.end(), data_ptr);
   return shmid;
 }
 
-std::size_t
-exercise_read_segment(
-  int shmid,
-  std::size_t number_of_integers)
+template <typename ElementType>
+af::shared<ElementType>
+copy_from_segment(
+  int shmid)
 {
-  std::size_t result = 0;
-  void* ptr = shmat(shmid, 0, SHM_RDONLY);
-  if (ptr == reinterpret_cast<void *>(-1)) {
+  void* void_ptr = shmat(shmid, 0, SHM_RDONLY);
+  if (void_ptr == reinterpret_cast<void *>(-1)) {
     std::string msg = strerror(errno);
     throw std::runtime_error("shmat() failure: " + msg);
   }
-  int const* data = static_cast<int const*>(ptr);
-  for (std::size_t i=0;i<number_of_integers;i++){
-    if (data[i] == static_cast<int>(i % 13)) {
-      result++;
-    }
+  struct shmid_ds shm_stat;
+  if (shmctl(shmid, IPC_STAT, &shm_stat) != 0) {
+    std::string msg = strerror(errno);
+    throw std::runtime_error("shmctl(IPC_STAT) failure: " + msg);
   }
-  return result;
+  std::size_t data_size = shm_stat.shm_segsz / sizeof(ElementType);
+  if (data_size * sizeof(ElementType) != shm_stat.shm_segsz) {
+    throw std::runtime_error(
+      "copy_from_segment() failure: unexpected segment size");
+  }
+  ElementType const* data_ptr = static_cast<ElementType const*>(void_ptr);
+  return af::shared<ElementType>(data_ptr, data_ptr+data_size);
 }
 
 boost::python::list
@@ -61,8 +71,8 @@ list_segment_ids(
     throw std::runtime_error("shmctl(SHM_INFO) failure: " + msg);
   }
   for (int i=0;i<=max_id;i++) {
-    struct shmid_ds shm_segment;
-    int shmid = shmctl(i, SHM_STAT, &shm_segment);
+    struct shmid_ds shm_stat;
+    int shmid = shmctl(i, SHM_STAT, &shm_stat);
     if (shmid < 0) {
       if (i == 0 && max_id == 0) break;
       if (ignore_errors) continue;
@@ -70,7 +80,7 @@ list_segment_ids(
       throw std::runtime_error("shmctl(SHM_STAT) failure: " + msg);
     }
     if (attached_status == 0
-        || ((attached_status < 0) == (shm_segment.shm_nattch == 0))) {
+        || ((attached_status < 0) == (shm_stat.shm_nattch == 0))) {
       result.append(shmid);
     }
   }
@@ -90,15 +100,25 @@ shmctl_rmid(
   return true;
 }
 
-BOOST_PYTHON_MODULE(ipctbx_ext)
+void
+wrap_all()
 {
   using namespace boost::python;
-  def("exercise_create_segment", exercise_create_segment);
-  def("exercise_read_segment", exercise_read_segment);
+  def("copy_to_new_segment", copy_to_new_segment<int>, (arg("data")));
+  def("copy_to_new_segment", copy_to_new_segment<double>, (arg("data")));
+  def("copy_from_segment_int", copy_from_segment<int>, (arg("shmid")));
+  def("copy_from_segment_double", copy_from_segment<double>, (arg("shmid")));
   def("list_segment_ids", list_segment_ids, (
     arg("attached_status")=0,
     arg("ignore_errors")=false));
   def("shmctl_rmid", shmctl_rmid, (
     arg("shmid"),
     arg("ignore_errors")=false));
+}
+
+} // namespace ipctbx
+
+BOOST_PYTHON_MODULE(ipctbx_ext)
+{
+  ipctbx::wrap_all();
 }
