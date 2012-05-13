2dfb6d04f38275ccc761113540356f357d6a93fe
David Waterman <david.waterman@stfc.ac.uk>
2012-05-13 09:05:34 +0000

    required import for conversion of Python types to correct C++ types on construction

--- rstbx/bpcx/__init__.py
+++ rstbx/bpcx/__init__.py
@@ -2,6 +2,7 @@
 #
 # from rstbx.bpcx import sensor
 
+from cctbx.array_family import flex # import dependency
 import boost.python
 ext = boost.python.import_ext("rstbx_bpcx_detector_model_ext")
 from rstbx_bpcx_detector_model_ext import *
