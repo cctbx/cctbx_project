c590a8178307f7210772b333cd59a8d0e9bfb2b8
Johan Hattne <johan@hattne.se>
2012-05-14 16:39:25 +0000

    Revised dpack(): beam center bug fix, retired redundant size parameters, and better PEP 8 conformance.

--- xfel/cxi/cspad_ana/cspad_tbx.py
+++ xfel/cxi/cspad_ana/cspad_tbx.py
@@ -312,73 +312,68 @@ def CsPadElement(data3d, qn, config):
   return quadrant
 
 
-def dpack(active_areas         = None,
-          beam_center_x        = None,
-          beam_center_y        = None,
-          ccd_image_saturation = None,
-          data                 = None,
-          distance             = None,
-          pixel_size           = pixel_size,
-          saturated_value      = None,
-          size1                = None,
-          size2                = None,
-          timestamp            = None,
-          wavelength           = None,
-          xtal_target          = None):
-  """XXX Check completeness.  Should fill in sensible defaults.  Must
-  have data."""
+def dpack(active_areas=None,
+          beam_center_x=None,
+          beam_center_y=None,
+          ccd_image_saturation=None,
+          data=None,
+          distance=None,
+          pixel_size=pixel_size,
+          saturated_value=None,
+          timestamp=None,
+          wavelength=None,
+          xtal_target=None):
+  """XXX Check completeness.  Should fill in sensible defaults."""
 
-  if (data is None):
-    return (None)
+  # Must have data.
+  if data is None:
+    return None
 
   # Create a time stamp of the current time if none was supplied.
-  if (timestamp is None):
+  if timestamp is None:
     timestamp = evt_timestamp()
 
   # For unknown historical reasons, the dictionary must contain both
   # CCD_IMAGE_SATURATION and SATURATED_VALUE items.
-  if (ccd_image_saturation is None):
+  if ccd_image_saturation is None:
     ccd_image_saturation = dynamic_range
-  if (saturated_value is None):
+  if saturated_value is None:
     saturated_value = dynamic_range
 
-  # The size must match the image dimensions.  The length along the
-  # slow axis is size1, the length along the fast axis is size2.
-  if (size1 is None):
-    size1 = data.focus()[0]
-  if (size2 is None):
-    size2 = data.focus()[1]
-
-  # The slow (vertical) axis is x, while size2 is the fast
-  # (horizontal) y-axis.
-  if (beam_center_x is None):
-    beam_center_x = pixel_size * size2
-  if (beam_center_y is None):
-    beam_center_y = pixel_size * size1
+  # By default, the beam center is the center of the image.  The slow
+  # (vertical) and fast (horizontal) axes correspond to x and y,
+  # respectively.
+  if beam_center_x is None:
+    beam_center_x = pixel_size * data.focus()[1] / 2
+  if beam_center_y is None:
+    beam_center_y = pixel_size * data.focus()[0] / 2
 
   # There is no sensible default for the active areas, distance, nor
   # wavelength.  XXX But setting wavelength to zero may be disastrous?
-  if (active_areas is None):
+  if active_areas is None:
     active_areas = flex.int()
-  if (distance is None):
+  if distance is None:
     distance = 0
-  if (wavelength is None):
+  if wavelength is None:
     wavelength = 0
 
-  return (dict(ACTIVE_AREAS         = active_areas,
-               BEAM_CENTER_X        = beam_center_x,
-               BEAM_CENTER_Y        = beam_center_y,
-               CCD_IMAGE_SATURATION = ccd_image_saturation,
-               DATA                 = data,
-               DISTANCE             = distance,
-               PIXEL_SIZE           = pixel_size,
-               SATURATED_VALUE      = saturated_value,
-               SIZE1                = size1,
-               SIZE2                = size2,
-               TIMESTAMP            = timestamp,
-               SEQUENCE_NUMBER      = 0, # XXX Deprecated
-               WAVELENGTH           = wavelength,
-               xtal_target          = xtal_target))
+  # The size must match the image dimensions.  The length along the
+  # slow (vertical) axis is SIZE1, the length along the fast
+  # (horizontal) axis is SIZE2.
+  return {'ACTIVE_AREAS': active_areas,
+          'BEAM_CENTER_X': beam_center_x,
+          'BEAM_CENTER_Y': beam_center_y,
+          'CCD_IMAGE_SATURATION': ccd_image_saturation,
+          'DATA': data,
+          'DISTANCE': distance,
+          'PIXEL_SIZE': pixel_size,
+          'SATURATED_VALUE': saturated_value,
+          'SIZE1': data.focus()[0],
+          'SIZE2': data.focus()[1],
+          'TIMESTAMP': timestamp,
+          'SEQUENCE_NUMBER': 0, # XXX Deprecated
+          'WAVELENGTH': wavelength,
+          'xtal_target': xtal_target}
 
 
 def dwritef(d, dirname=None, basename=None):
