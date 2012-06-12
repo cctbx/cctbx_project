720d340ab83eddadf4643c865f309f526fb9c0dd
Pavel Afonine <pafonine@users.noreply.github.com>
2012-06-12 17:08:00 +0000

    allow non-centered CC calculation (that is don't make mean of arrays to be equal to zero)

--- scitbx/math/linear_correlation.h
+++ scitbx/math/linear_correlation.h
@@ -16,7 +16,8 @@ namespace scitbx { namespace math {
       linear_correlation(
         af::const_ref<OtherFloatType> const& x,
         af::const_ref<OtherFloatType> const& y,
-        FloatType const& epsilon=1.e-15)
+        FloatType const& epsilon=1.e-15,
+        bool const& subtract_mean=true)
       :
         is_well_defined_(false),
         n_(x.size()),
@@ -30,10 +31,12 @@ namespace scitbx { namespace math {
       {
         SCITBX_ASSERT(x.size() == y.size());
         if (n_ != 0) {
-          for(std::size_t i=0;i<n_;i++) mean_x_ += x[i];
-          for(std::size_t i=0;i<n_;i++) mean_y_ += y[i];
-          mean_x_ /= n_;
-          mean_y_ /= n_;
+          if(subtract_mean) {
+            for(std::size_t i=0;i<n_;i++) mean_x_ += x[i];
+            for(std::size_t i=0;i<n_;i++) mean_y_ += y[i];
+            mean_x_ /= n_;
+            mean_y_ /= n_;
+          }
           for(std::size_t i=0;i<n_;i++) {
             FloatType delta_x = x[i] - mean_x_;
             FloatType delta_y = y[i] - mean_y_;
