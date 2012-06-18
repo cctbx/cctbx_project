3e0c4ce1585380681576ae6e1ea2d86d60b36bef
David Waterman <david.waterman@stfc.ac.uk>
2012-06-18 09:44:21 +0000

    added overloaded setter methods to sensor, exposed to python via BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS. Also removed unused file with trivial main function for testing sensor

--- rstbx/bpcx/detector_model/sensor.h
+++ rstbx/bpcx/detector_model/sensor.h
@@ -31,9 +31,17 @@ class sensor {
 
   //setters
 
-  /* Not yet implemented. Each must call update(). set all three
+  /* Each must call update(). set all three
      properties of the sensor at once (i.e. dir1, dir2, origin) as we
      want to maintain the same "shape" coordinate frame. */
+  void set_frame(const scitbx::vec3<double>& origin);
+
+  void set_frame(const scitbx::vec3<double>& origin,
+                 const scitbx::vec3<double>& dir1);
+
+  void set_frame(const scitbx::vec3<double>& origin,
+                 const scitbx::vec3<double>& dir1,
+                 const scitbx::vec3<double>& dir2);
 
  private:
 
