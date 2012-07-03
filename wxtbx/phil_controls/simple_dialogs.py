c3ce1cd3f2b552ec6450a3d6279a06bf5f52920b
Nathaniel Echols <nathaniel.echols@gmail.com>
2012-07-03 22:07:31 +0000

    more functionality, including rotation/translation of coordinates

--- wxtbx/phil_controls/simple_dialogs.py
+++ wxtbx/phil_controls/simple_dialogs.py
@@ -93,6 +93,76 @@ def get_integer_value (**kwds) :
   dlg = IntegerDialog(**kwds)
   return get_phil_value_from_dialog(dlg)
 
+class RTDialog (wx.Dialog) :
+  def __init__ (self, *args, **kwds) :
+    kwds = dict(kwds)
+    style = kwds.get('style', 0)
+    style |= wx.CAPTION|wx.CLOSE_BOX|wx.RAISED_BORDER| \
+      wx.WS_EX_VALIDATE_RECURSIVELY
+    kwds['style'] = style
+    wx.Dialog.__init__(self, *args, **kwds)
+    szr = wx.BoxSizer(wx.VERTICAL)
+    self.SetSizer(szr)
+    self._r_ctrls = []
+    self._t_ctrls = []
+    txt = wx.StaticText(self, -1,
+      "Please enter rotation and translation operations to apply.  All "+
+      "values must be defined.")
+    szr.Add(txt, 0, wx.ALL, 5)
+    grid = wx.FlexGridSizer(rows=4, cols=4)
+    szr.Add(grid, 0, wx.ALL, 5)
+    label1 = wx.StaticText(self, -1, "Rotation:")
+    grid.Add(label1, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+    for i in range(3) :
+      if (i > 0) :
+        grid.Add((1,1))
+      for j in range(3) :
+        ctrl = floatctrl.FloatCtrl(
+          parent=self,
+          name="Rotation",
+          value=0)
+        ctrl.SetOptional(False)
+        grid.Add(ctrl, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+        self._r_ctrls.append(ctrl)
+    label2 = wx.StaticText(self, -1, "Translation:")
+    grid.Add(label2, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+    for i in range(3) :
+      ctrl = floatctrl.FloatCtrl(
+        parent=self,
+        name="Translation",
+        value=0)
+      ctrl.SetOptional(False)
+      grid.Add(ctrl, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+      self._t_ctrls.append(ctrl)
+    ok_btn = wx.Button(self, wx.ID_OK)
+    cancel_btn = wx.Button(self, wx.ID_CANCEL)
+    btn_szr = wx.StdDialogButtonSizer()
+    btn_szr.Add(cancel_btn, 0, wx.ALL, 5)
+    btn_szr.Add(ok_btn, 0, wx.ALL, 5)
+    ok_btn.SetDefault()
+    btn_szr.Realize()
+    szr.Add(btn_szr, 0, wx.ALL|wx.ALIGN_RIGHT, 5)
+    self.Fit()
+    self.Centre(wx.BOTH)
+
+  def GetMatrix (self) :
+    from scitbx import matrix
+    r = [ c.GetPhilValue() for c in self._r_ctrls ]
+    t = [ c.GetPhilValue() for c in self._t_ctrls ]
+    return matrix.rt((r,t))
+
+def get_rt_matrix (parent=None) :
+  dlg = RTDialog(
+    parent=parent,
+    title="Rotation/translation operator")
+  rt = None
+  if (dlg.ShowModal() == wx.ID_OK) :
+    rt = dlg.GetMatrix()
+  wx.CallAfter(dlg.Destroy)
+  if (rt is None) :
+    raise Abort()
+  return rt
+
 if (__name__ == "__main__") :
   app = wx.App(0)
   value1 = get_float_value(
@@ -114,3 +184,8 @@ if (__name__ == "__main__") :
     label="Symmetry operator",
     value=None)
   print get_phil_value_from_dialog(dlg)
+  dlg = RTDialog(
+    parent=None,
+    title="Rotation/translation operator")
+  if (dlg.ShowModal() == wx.ID_OK) :
+    rt = dlg.GetMatrix()
