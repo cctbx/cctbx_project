f82e48c9d201837592af9d4f421db7acf366c486
Nathaniel Echols <nathaniel.echols@gmail.com>
2012-07-06 17:15:17 +0000

    moved infrastructure code from phenix tree (since it's needed elsewhere)

---
+++ libtbx/program_utils/result.py
@@ -0,0 +1,81 @@
+
+from libtbx.program_utils import statistics_info
+from libtbx import adopt_init_args
+import os
+
+class program_result (object) :
+  def __init__ (self,
+                program_name,
+                job_title,
+                directory=None,
+                log_file=None,
+                input_files=(),
+                pdb_files=(),
+                map_file=None,
+                data_file=None,
+                cif_files=(),
+                phil_files=(),
+                other_files=(),
+                statistics={}) :
+    adopt_init_args(self, locals())
+
+  def get_statistic (self, name) :
+    return self.statistics.get(name, None)
+
+  def get_pdb_files (self) :
+    return [ self.get_file_path(fn) for fn in self.pdb_files ]
+
+  def get_pdb_file (self) :
+    assert (len(self.pdb_files) <= 1)
+    if (len(self.pdb_files) == 0) :
+      return None
+    return self.get_file_path(self.pdb_files[0])
+
+  def show_summary (self, out) :
+    pass
+
+  def get_file_path (self, file_name) :
+    if (file_name is None) :
+      return None
+    elif (os.path.isabs(file_name)) :
+      return file_name
+    else :
+      assert (self.directory is not None)
+      return os.path.join(self.directory, file_name)
+
+  def get_map_file (self) :
+    return self.get_file_path(self.map_file)
+
+  def get_data_file (self) :
+    return self.get_file_path(self.data_file)
+
+  def r_free (self) :
+    return self.get_statistic("r_free")
+
+  def format_statistics (self, stat_keys) :
+    formatted = []
+    for stat_name in stat_keys :
+      stat_value = self.get_statistic(stat_name)
+      if (stat_value is not None) :
+        stat_label = statistics_info.keys_and_labels.get(stat_name, stat_name)
+        format = statistics_info.get_format(stat_label)
+        formatted.append((stat_label, format % stat_value))
+    return formatted
+
+  def get_pdb_file_caption (self) :
+    return "Model"
+
+  def get_map_file_caption (self) :
+    return "Map coefficients"
+
+  def finish_job (self) :
+    output_files = []
+    for file_name in self.pdb_files :
+      output_files.append((file_name, self.get_pdb_file_caption()))
+    for file_name in self.map_file :
+      output_files.append((file_name, self.get_map_file_caption()))
+    stats = self.get_final_stats()
+    return output_files, stats
+
+  def get_final_stats (self) :
+    return []
---
+++ libtbx/program_utils/statistics_info.py
@@ -0,0 +1,101 @@
+
+# Rules of thumb for interpreting program statistics (used in Phenix GUI)
+
+# format:
+#   label min max quality strict_cutoff
+threshold_values = [
+  ("R-work", 0.4, 0.45, "fair", True),
+  ("R-work", 0.55, 1, "bad", True),
+  ("R-work", 0.0, 0.4, "good", True),
+  ("R-free", 0.4, 0.45, "fair", True),
+  ("R-free", 0.55, 1, "bad", True),
+  ("R-free", 0.0, 0.4, "good", True),
+  ("Skew", 0.15, 1, "good", True),
+  ("RMS(bonds)", 0.0, 0.016, "good", True),
+  ("RMS(bonds)", 0.016, 0.02, "fair", False),
+  ("RMS(bonds)", 0.02, 999, "poor", False),
+  ("RMS(angles)", 0.0, 1.6, "good", True),
+  ("RMS(angles)", 1.6, 2.0, "fair", False),
+  ("RMS(angles)", 2.0, 180, "poor", False),
+  ("Ramachandran outliers", 0.0, 0.2, "good", False),
+  ("Ramachandran outliers", 0.2, 0.5, "fair", False),
+  ("Ramachandran outliers", 0.5, 1.0, "poor", False),
+  ("Ramachandran outliers", 1.0, 100.0, "bad", False),
+  ("Ramachandran favored", 98.0, 100.0, "good", False),
+  ("Ramachandran favored", 95.0, 97.999, "fair", False),
+  ("Ramachandran favored", 90.0, 94.999, "poor", False),
+  ("Ramachandran favored", 0.0, 89.9999, "bad", False),
+  ("Clashscore", 0.0, 19.999, "good", False),
+  ("Clashscore", 20.0, 39.999, "fair", False),
+  ("Clashscore", 40.0, 59.999, "poor", False),
+  ("Clashscore", 60.0, 999, "bad", False),
+  ("Rotamer outliers", 0.0, 1.999, "good", False),
+  ("Rotamer outliers", 2.0, 5.0, "fair", False),
+  ("Rotamer outliers", 5.0, 10.0, "poor", False),
+  ("Rotamer outliers", 10.0, 100.0, "bad", False),
+  # XXX backwards compatibility
+  ("RMSbonds", 0.0, 0.016, "good", True),
+  ("RMSbonds", 0.016, 0.02, "fair", False),
+  ("RMSbonds", 0.02, 999, "poor", False),
+  ("RMSangles", 0.0, 1.6, "good", True),
+  ("RMSangles", 1.6, 2.0, "fair", False),
+  ("RMSangles", 2.0, 180, "poor", False),
+  ("CC", 0.0, 0.3, "bad", False),
+  ("CC", 0.3, 0.5, "poor", False),
+  ("CC", 0.5, 0.7, "fair", False),
+  ("CC", 0.7, 1.0, "good", False),
+  ("Model-map CC", 0.0, 0.3, "bad", False),
+  ("Model-map CC", 0.3, 0.5, "poor", False),
+  ("Model-map CC", 0.5, 0.7, "fair", False),
+  ("Model-map CC", 0.7, 1.0, "good", False),
+  ("Residues", 0, 0, "bad", False),
+]
+
+precisions = [
+  ("R-work", 4),
+  ("R-free", 4),
+  ("RMS(bonds)", 4),
+  ("RMS(angles)", 3),
+  ("FOM", 3),
+  ("Figure of Merit", 3),
+  ("Skew", 2),
+  ("Skewness", 2),
+  ("Avg. B-factor", 2),
+  ("Ramachandran favored", 2),
+  ("Ramachandran outliers", 2),
+  ("Rotamer outliers", 2),
+  ("Clashscore", 1),
+  # XXX backwards compatibility
+  ("RMSbonds", 4),
+  ("RMSangles", 3),
+  ("CC", 2),
+  ("Model-map CC", 2),
+]
+
+keys_and_labels = {
+  "r_free" : "R-free",
+  "r_work" : "R-work",
+  "rms_bonds" : "RMS(bonds)",
+  "rms_angles" : "RMS(angles)",
+  "map_cc" : "Model-map CC",
+  "cc" : "CC",
+  "n_res" : "Residues",
+  "rama_out" : "Ramachandran outliers",
+  "rama_fav" : "Ramachandran favored",
+  "rota_out" : "Rotamer outliers",
+  "clashscore" : "Clashscore",
+  "skew" : "Skew",
+  "fom" : "Figure of merit",
+}
+
+precisions_dict_ = None
+def get_format (stat_name, default="%g") :
+  global precisions_dict_
+  if (precisions_dict_ is None) :
+    precisions_dict_ = dict(precisions)
+  if (stat_name in precisions_dict_) :
+    return "%%.%df" % precisions_dict_[stat_name]
+  elif (stat_name in keys_and_labels) :
+    stat_label = keys_and_labels[stat_name]
+    return "%%.%df" % precisions_dict_[stat_label]
+  return default
