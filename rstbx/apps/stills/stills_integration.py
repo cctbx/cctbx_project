67e2840d2371ebdaf578d1364fb985c51505152a
Nicholas Sauter <nksauter@lbl.gov>
2012-07-23 19:22:55 +0000

    Migrate BPCX development code into rstbx.

---
+++ rstbx/apps/stills/stills_integration.py
@@ -0,0 +1,174 @@
+"""Specialization code for 2011 JCSG pilot experiment; not general use"""
+import pickle,math
+from labelit.preferences import labelit_commands,labelit_phil
+from labelit.command_line.default_param import establish_dict_for_refinement
+from labelit.dptbx.autoindex import index_and_refine
+from labelit.command_line.stats_index import best_character_to_IndexPrinter
+from labelit.command_line.stats_index import AutoIndexOrganizer
+
+class Empty:pass
+
+class api:
+  def __init__(self,*args):
+    #application programmer interface for screen, using identical inputs
+    # to the command line interface
+    args = list(args)
+    labelit_phil.merge_command_line(args)
+    E = Empty()
+    E.argv=['Empty']
+    for x in xrange(len(args)):
+      E.argv.append(args[x])
+    E.horizons_phil=labelit_commands
+
+    self.establish_dict_for_refinement = establish_dict_for_refinement
+    self.index_and_refine = index_and_refine
+    self.best_character_to_IndexPrinter = best_character_to_IndexPrinter
+
+    self.Org = AutoIndexOrganizer(
+      verbose=labelit_commands.distl.bins.verbose,argument_module=E)
+    self.Org.setIndexingDelegate(self.index_and_integrate)
+      # legacy:algorithm control could be excercised here
+    self.horizons_phil = labelit_commands
+
+  def __call__(self):
+    return self.Org.process()
+
+  def index_and_integrate(self,frames,files,spotfinder_results):
+    self.frames = frames
+    self.files = files
+    self.spotfinder_results = spotfinder_results
+    pd = self.establish_dict_for_refinement(frames,spotfinder_results)
+    #------------------------------------------------------------
+    ai,P = self.index_and_refine(pd,files,spotfinder_results,0)
+    self.indexing_ai = ai
+    #------------------------------------------------------------
+    if labelit_commands.compatibility_allow==False:
+      M = self.best_character_to_IndexPrinter(ai,P,pd,horizons_phil=self.horizons_phil)
+    else:
+      from labelit.diffraction.compatibility import \
+        best_compatibility_to_IndexPrinter
+      M = best_compatibility_to_IndexPrinter(ai,P,pd,files,spotfinder_results,horizons_phil=self.horizons_phil)
+    #------------------------------------------------------------
+    if labelit_commands.__dict__.has_key("writer"):
+      labelit_commands.writer.make_image_plots_detail(
+        ai=ai,pd=pd,inframes=files,spotfinder_results=spotfinder_results)
+    if not labelit_commands.index_only:
+      IC = IntegrateCharacters(M,pd)
+      IC.write_mosflm_matrices()
+      IC.find_best()
+      IC.show()
+    return pd
+
+  def create_case_only(self,frames,file,spotfinder_results):
+    self.pd = self.establish_dict_for_refinement(frames)
+
+  def analyze_one(self,solution):
+    inputpd = self.Org.process()
+
+    settings = pickle.load(open("LABELIT_possible","rb"))
+    setting = [setting for setting in settings if setting["counter"]==solution][0]
+
+    from labelit.preferences import labelit_commands as param
+
+    pixel_size = float(inputpd['pixel_size'])
+    self.pixel_size = pixel_size
+
+    from labelit.dptbx import AutoIndexEngine, Parameters
+    ai = AutoIndexEngine(inputpd['endstation'])
+
+    P = Parameters(xbeam=setting["refined x beam"],ybeam=setting["refined y beam"],
+             distance=setting["refined distance"],twotheta=float(inputpd["twotheta"]))
+    ai.setBase(P)
+    ai.setWavelength(float(inputpd['wavelength']))
+    ai.setMaxcell(float(inputpd['ref_maxcel']))
+    print "Deltaphi is",float(inputpd['deltaphi'])
+    ai.setDeltaphi(float(inputpd['deltaphi'])*math.pi/180.)
+    ai.setMosaicity(setting["mosaicity"])
+    ai.setOrientation(setting["orient"])
+    #why aren't hexagonal constraints applied here???
+    print inputpd["osc_start"]
+
+    image_centers = [(math.pi/180.)*float(x) for x in inputpd["osc_start"].values()]
+
+    limiting_resolution = param.distl_highres_limit
+    print "Limiting resolution",limiting_resolution
+
+    #predict the spots
+    spots = ai.predict_all(image_centers[0],limiting_resolution)
+    pre2m = spots.vec3()
+    self.pre2m = pre2m
+
+    hkllist = spots.hkl()
+    cell = ai.getOrientation().unit_cell()
+    print cell
+    for hkl in hkllist:
+      #print "%25s %5.2f"%(str(hkl),cell.d(hkl))
+      assert cell.d(hkl)>=limiting_resolution
+    print "Number of hkls:",(hkllist).size(),
+    print "all inside the %4.2f Angstrom limiting sphere."%limiting_resolution
+    print "The unit cell is",cell
+    self.solution_setting_ai = ai
+    self.solution_pd = inputpd
+    self.image_centers = image_centers
+    return [ai.getOrientation().unit_cell(),hkllist]
+
+  def parameters(self):
+    image_centers = [(math.pi/180.)*float(x) for x in self.solution_pd["osc_start"].values()]
+    P = dict( image_center_radians = image_centers,
+              wavelength = float(self.solution_pd['wavelength']),
+              deltaphi = float(self.solution_pd['deltaphi']),
+              mosaicity_degrees = self.solution_setting_ai.getMosaicity(),
+              orientation = self.solution_setting_ai.getOrientation(),
+              )
+    return P
+
+from libtbx.development.timers import Timer
+from rstbx.dials_core.integration_core import integration_core
+from rstbx.apps.stills.simple_integration import IntegrationMetaProcedure as base_class
+class IntegrationMetaProcedure(base_class):
+  def __init__(self,inputs,backcompat_horizons_phil): # inputs is an instance of class api
+    self.horizons_phil = backcompat_horizons_phil
+    integration_core.__init__(self)
+    self.inputai = inputs.solution_setting_ai #C++ autoindex engine
+    self.indexing_ai = inputs.indexing_ai
+
+    #Note...here we may be working with a non-primitive cell, so
+    # must work with systematic absences...not implemented yet.
+    if self.indexing_ai.getData().size() < 40: return # initial protection
+
+    self.inputpd = inputs.solution_pd #parameter dictionary
+    self.inputframes = inputs.frames
+    self.imagefiles = inputs.files
+    self.spotfinder = inputs.spotfinder_results
+    self.frame_numbers = self.spotfinder.pd['osc_start'].keys()
+    self.frame_numbers.sort()
+
+    self.image_centers = inputs.image_centers
+    print "IMAGE CENTERS",self.image_centers
+
+    # initial resolution from DISTL
+    resolution_est = float(self.inputpd['resolution_inspection'])
+    print "initial resolution from DISTL",resolution_est
+
+    # resolution limit of the strong spots used for indexing
+    resolution_str = self.indexing_ai.high().reciprocal_spacing
+    resolution = max(resolution_est,resolution_str)
+    print "resolution limit of the strong spots used for indexing",resolution
+    self.limiting_resolution = resolution
+
+    #print "resolution: %.2f %.2f"%(resolution_est,resolution_str)
+    self.pixel_size = inputs.pixel_size
+    self.set_pixel_size(inputs.pixel_size)
+    self.set_detector_size(int(self.inputpd["size1"]),int(self.inputpd["size2"]))
+
+    self.pre2m = inputs.pre2m
+    self.set_up_mask_focus()
+    self.initialize_increments()
+    T = Timer("concept")
+    from cctbx import sgtbx
+    self.integration_concept(image_number = 0,
+        cb_op_to_primitive = sgtbx.change_of_basis_op(), #identity; supports only primitive lattices
+        verbose_cv = self.horizons_phil.indexing.verbose_cv,
+        background_factor = self.horizons_phil.integration.background_factor)
+    T = Timer("proper")
+    self.integration_proper()
---
+++ rstbx/outlier_spots/fit_distribution.py
@@ -0,0 +1,184 @@
+import math
+import scitbx.math
+from scitbx import lbfgs
+from scitbx.array_family import flex
+
+class fit_cdf(object):
+  """
+  =============================================================================
+  Class fits a distribution according to its cumulative distribution function
+
+  Arguments:
+    x_data - measured property (list)
+    y_data - fraction of points with measured property (cdf) (list)
+    distribution - type of distribution to be fit
+
+  Useful accessible attributes:
+    self.x - the final parameters (list)
+    self.distribution - the distribution being modeled (see notes)
+
+  Notes:
+    The available distributions are,
+      gaussian
+      rayleigh
+      rice
+    The distribution argument should not have quotes since it is used directly
+    to access the class.
+    The lbfgs method is used to minimize, f = (predicted - observed)^2
+  -----------------------------------------------------------------------------
+  """
+  def __init__(self,x_data=None,y_data=None,distribution=None,
+               max_iterations=50):
+    # setup data
+    assert(len(x_data) == len(y_data))
+    self.n = len(x_data)
+    self.x_data = flex.double(x_data)
+    self.y_data = flex.double(y_data)
+    if (flex.max(self.y_data) > 1.0):
+      print "The cumulative distribution function (y_data) should only have values be between 0 and 1."
+      exit()
+
+    # intialize distribution with guess
+    self.distribution = distribution()
+    self.distribution.estimate_parameters_from_cdf(x_data=self.x_data,
+                                                   y_data=self.y_data)
+    self.x = self.distribution.get_parameters()
+
+    # optimize parameters
+    self.minimizer = lbfgs.run(target_evaluator=self)
+
+    # set optimized parameters
+    self.distribution.set_parameters(p=self.x)
+
+  def compute_functional_and_gradients(self):
+    # caculate difference between predicted and observed values
+    self.distribution.set_parameters(p=self.x)
+    predicted = flex.double(self.n)
+    for i in xrange(self.n):
+      predicted[i] = self.distribution.cdf(x=self.x_data[i])
+    difference = predicted - self.y_data
+
+    # target function for minimization is sum of rmsd
+    f = flex.sum(flex.sqrt(difference*difference))
+    gradients = flex.double(len(self.x))
+    for i in xrange(self.n):
+      g_i = self.distribution.cdf_gradients(x=self.x_data[i])
+      for j in xrange(len(self.x)):
+        gradients[j] = gradients[j] + difference[i]*g_i[j]
+    gradients = 2.0*gradients
+    return f,gradients
+
+# =============================================================================
+class rayleigh(object):
+  """
+  =============================================================================
+  Class models a 1-d Rayleigh distribution using one parameter, sigma.
+
+              x                x^2
+    pdf = --------- exp(- ------------)
+           sigma^2         2 sigma^2
+
+                        x^2
+    cdf = 1 - exp(- -----------)
+                     2 sigma^2
+
+  The derivative of the cdf with respect to sigma is,
+
+      d(cdf)          x^2               x^2             x
+    ---------- = - --------- exp( - -----------) = - ------- pdf
+     d(sigma)       sigma^3          2 sigma^2        sigma
+
+  Methods:
+    set_parameters
+    get_parameters
+    estimate_parameters_from_cdf
+    pdf
+    cdf
+    d_cdf_d_sigma
+    d_cdf_d_sigma_finite
+    cdf_gradients
+  -----------------------------------------------------------------------------
+  """
+  def __init__(self,mean=None,sigma=None):
+
+    # parameter for Rayleigh distribution
+    if (sigma is None):
+      sigma = 1.0
+    self.sigma = sigma
+
+  def set_parameters(self,p=None):
+    """
+    Function sets the all the parameters
+    """
+    assert(len(p) == 1)
+    self.sigma = float(p[0])
+
+  def get_parameters(self):
+    """
+    Function returns all the parameters
+    """
+    return flex.double([self.sigma])
+
+  def estimate_parameters_from_cdf(self,x_data=None,y_data=None):
+    """
+    Function estimates the parameter values based on the data (cdf)
+    """
+    # sigma is the mode of the distribution
+    # approximate with the median (cdf = 0.5)
+    midpoint = None
+    for i in xrange(len(x_data)):
+      if (y_data[i] > 0.5):
+        midpoint = i
+        break
+    if (midpoint is None):
+      midpoint = len(x_data) - 1
+    self.sigma = x_data[midpoint]
+
+  def pdf(self,x=None):
+    """
+    Function returns the probability density function at x
+    """
+    x_sigma = x/self.sigma
+    f = (x_sigma/self.sigma)*math.exp(-0.5*x_sigma*x_sigma)
+    return f
+
+  def cdf(self,x=None):
+    """
+    Function returns the cumulative distribution function at x
+    """
+    x_sigma = x/self.sigma
+    f = 1.0 - math.exp(-0.5*x_sigma*x_sigma)
+    return f
+
+  def inv_cdf(self,cdf=None):
+    """
+    Function returns the inverse cumulative distribution function at cdf
+    """
+    return math.sqrt(-2.*self.sigma*self.sigma*math.log(1.-cdf))
+
+  def d_cdf_d_sigma(self,x=None):
+    """
+    Function returns the derivative of the cdf at x with respect to the
+    standard deviation
+    """
+    df = -(x/self.sigma)*self.pdf(x=x)
+    return df
+
+  def d_cdf_d_sigma_finite(self,x=None,delta=0.00001):
+    """
+    Function returns the derivative of the cdf at x with respect to the
+    standard deviation
+    """
+    sigma0 = self.sigma
+    self.sigma = sigma0 + delta
+    f1 = self.cdf(x=x)
+    self.sigma = sigma0 - delta
+    f2 = self.cdf(x=x)
+    self.sigma = sigma0
+    return (f1-f2)/(2.0*delta)
+
+  def cdf_gradients(self,x=None):
+    """
+    Function returns a flex.double containing all derivatives
+    """
+    return flex.double([self.d_cdf_d_sigma(x)])
