e26023fb38985087771b431de81c92d6df429aca
Nathaniel Echols <nathaniel.echols@gmail.com>
2012-07-27 14:42:58 +0000

    command-line launcher for pdb statistics viewer

---
+++ wxtbx/command_line/pdb_statistics.py
@@ -0,0 +1,12 @@
+# LIBTBX_SET_DISPATCHER_NAME mmtbx.pdb_statistics
+# LIBTBX_PRE_DISPATCHER_INCLUDE_SH export PHENIX_GUI_ENVIRONMENT=1
+# LIBTBX_PRE_DISPATCHER_INCLUDE_SH export BOOST_ADAPTBX_FPE_DEFAULT=1
+
+from wxtbx.polygon_db_viewer import ConfigFrame
+import wx
+
+if (__name__ == "__main__") :
+  app = wx.App(0)
+  frame = ConfigFrame(None, -1, "PDB statistics from POLYGON database")
+  frame.Show()
+  app.MainLoop()
