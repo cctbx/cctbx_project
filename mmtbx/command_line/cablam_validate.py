2c2677c497f755e1c8c28fd5ab2796e4368d7181
Christopher J Williams <chrisjwilliams@users.noreply.github.com>
2012-08-03 22:05:06 +0000

    Fixed cablam_validate commandline so it works

--- mmtbx/command_line/cablam_validate.py
+++ mmtbx/command_line/cablam_validate.py
@@ -1,7 +1,7 @@
-# LIBTBX_SET_DISPATCHER_NAME phenix.cablam_training
+# LIBTBX_SET_DISPATCHER_NAME phenix.cablam_validate
 
 import sys
 from mmtbx.cablam import cablam_validate
 
 if __name__ == "__main__":
-  cablam_validate.run([sys.argv[1:])
+  cablam_validate.run(sys.argv[1:])
