666da65aa5c5c68d0b9264d7591e58a1b3089ee1
Nicholas Sauter <nksauter@lbl.gov>
2012-08-05 02:12:43 +0000

    Slip viewer is ported to rstbx/slip_viewer as announced at 2012 ACA Meeting.

---
+++ rstbx/command_line/slip_viewer.py
@@ -0,0 +1,143 @@
+# -*- Mode: Python; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 8 -*-
+#
+# LIBTBX_SET_DISPATCHER_NAME rstbx.image_viewer
+# LIBTBX_PRE_DISPATCHER_INCLUDE_SH export PHENIX_GUI_ENVIRONMENT=1
+# LIBTBX_PRE_DISPATCHER_INCLUDE_SH export BOOST_ADAPTBX_FPE_DEFAULT=1
+#
+# $Id: slip_viewer.py 319 2012-05-11 21:06:01Z rjgildea $
+
+import os
+import sys
+import wx
+
+import iotbx.detectors
+from rstbx.slip_viewer.frame import XrayFrame
+from libtbx import phil
+from spotfinder import phil_str
+from spotfinder.command_line.signal_strength import additional_spotfinder_phil_defs
+
+
+master_str="""
+anti_aliasing = False
+  .type = bool
+  .help = "Enable anti-aliasing"
+effective_metrology = None
+  .type = path
+  .help = "Read effective metrology from PATH"
+beam_center = True
+  .type = bool
+  .help = "Mark beam center"
+show_integration_results = False
+  .type = bool
+  .help = "Show integration results"
+show_spotfinder_results = False
+  .type = bool
+  .help = "Show spotfinder results"
+show_effective_tiling = False
+  .type = bool
+  .help = "Show the effective tiling of the detector"
+"""
+
+
+def modify_the_iotbx_detector_list():
+  from iotbx import detectors
+  try:
+    from cxi_xdr_xes.cftbx.detector.cspad_detector import CSPadDetector
+    from cxi_xdr_xes.cftbx.detector.generic_detector import GenericDetector
+    detectors.all_image_types.append(CSPadDetector)
+    detectors.all_image_types.append(GenericDetector)
+  except ImportError: pass
+
+# store default ImageFactory function
+defaultImageFactory = iotbx.detectors.ImageFactory
+
+def ImageFactory(filename):
+  try:
+    return NpyImageFactory(filename)
+  except Exception, e:
+    return defaultImageFactory(filename)
+
+# install modified ImageFactory function from above
+iotbx.detectors.ImageFactory = ImageFactory
+
+# The NpyImage requires some special treatment for creating an instance
+def NpyImageFactory(filename):
+  global parameters
+  from iotbx.detectors.npy import NpyImage
+  from spotfinder.applications.xfel import cxi_phil
+  args = [filename,
+          "distl.detector_format_version=CXI 5.1",
+          "viewer.powder_arcs.show=False",
+          "viewer.powder_arcs.code=3n9c",
+         ]
+  params = cxi_phil.cxi_versioned_extract(args)
+  horizons_phil = params.persist.commands
+  if isinstance(filename, basestring) and os.path.isfile(filename):
+    I = NpyImage(filename)
+  else:
+    print "This is not a file; assume the data are in the defined dictionary format"
+    I = NpyImage(filename, source_data=params.indexing.data)
+  I.readHeader(horizons_phil)
+  I.translate_tiles(horizons_phil)
+  return I
+
+
+def run(argv=None):
+  if (argv is None):
+    argv = sys.argv
+
+  # XXX Could/should handle effective metrology the same way, except
+  # it does not have a single scope.
+  work_phil = phil.process_command_line(
+    args=argv[1:],
+    master_string=master_str + phil_str + additional_spotfinder_phil_defs)
+  work_params = work_phil.work.extract()
+
+  app = wx.App(0)
+  wx.SystemOptions.SetOptionInt("osx.openfiledialog.always-show-types", 1)
+  frame = XrayFrame(None, -1, "X-ray image display", size=(800,720))
+
+  # Update initial settings with values from the command line.  Needs
+  # to be done before image is loaded (but after the frame is
+  # instantiated).
+  frame.params = work_params
+  frame.pyslip.tiles.user_requests_antialiasing = work_params.anti_aliasing
+
+  frame.settings_frame.panel.center_ctrl.SetValue(
+    work_params.beam_center)
+  frame.settings_frame.panel.integ_ctrl.SetValue(
+    work_params.show_integration_results)
+  frame.settings_frame.panel.spots_ctrl.SetValue(
+    work_params.show_spotfinder_results)
+  frame.settings.show_effective_tiling = work_params.show_effective_tiling
+  frame.settings_frame.panel.collect_values()
+
+  if (work_params.effective_metrology is not None):
+    from cxi_xdr_xes.cftbx.detector.metrology2phil import master_phil
+    from cxi_xdr_xes.cftbx.detector.metrology import metrology_as_transformation_matrices
+
+    stream = open(work_params.effective_metrology)
+    metrology_phil = master_phil.fetch(sources=[phil.parse(stream.read())])
+    stream.close()
+    frame.metrology_matrices = metrology_as_transformation_matrices(
+      metrology_phil.extract())
+
+  paths = work_phil.remaining_args
+  if (len(paths) == 1 and os.path.basename(paths[0]) == "DISTL_pickle"):
+    assert os.path.isfile(paths[0])
+    frame.load_distl_output(paths[0])
+  elif (len(paths) > 0):
+    for path in paths:
+      assert os.path.isfile(path)
+      frame.add_file_name_or_data(path)
+    frame.load_image(paths[0])
+
+  frame.Show()
+  app.MainLoop()
+
+  return 0
+
+
+if (__name__ == "__main__"):
+  modify_the_iotbx_detector_list()
+  sys.exit(run())
---
+++ rstbx/slip_viewer/tile_generation.py
@@ -0,0 +1,319 @@
+# -*- Mode: Python; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 8 -*-
+#
+# $Id: tile_generation.py 321 2012-05-15 19:31:42Z sauter $
+
+import math
+import wx
+######
+# Base class for a tile object - handles access to tiles.
+######
+'''Jan '12 ... make it so the rstbx widget actually zooms in DONE
+           ... draggable area large enough for whole image DONE
+           ... remove the dependency on map tile library geological DONE
+           ... provide cache  DONE
+           ... put in negative zoom levels  DONE
+           ... tighten up the flex image  -- refactor  DONE
+           ... connect the open filename command line option to the tile generator DONE
+           ... check in the code so far.  Put most of it in cxi xdr xes DONE
+           ... provide cache invalidation DONE
+           ... give mechanism to change brightness; color scheme
+'''
+class _Tiles(object):
+    # maximum number of tiles held in each level cache
+    MaxTileList = 512
+
+    def __init__(self, filename):
+        (self.tile_size_x, self.tile_size_y) = (256,256)
+        self.levels = [-3,-2,-1,0,1,2,3,4,5]
+
+        # set min and max tile levels
+        self.min_level = -3
+        self.max_level = 5
+        self.extent = (-180.0, 180., -166.66 , 166.66) #longitude & latitude limits
+        self.set_image(filename)
+        self.current_brightness = 1.0
+        self.current_color_scheme = 0
+        self.user_requests_antialiasing = False
+
+    def set_image(self, file_name_or_data, metrology_matrices=None):
+
+        self.reset_the_cache()
+        if file_name_or_data is None:
+          self.raw_image = None
+          return
+        if type(file_name_or_data) is type(""):
+          from iotbx.detectors import ImageFactory
+          self.raw_image = ImageFactory(file_name_or_data)
+        else:
+          self.raw_image = file_name_or_data._raw
+        self.raw_image.read()
+        #print "SETTING NEW IMAGE",self.raw_image.filename
+
+        # XXX Since there doesn't seem to be a good way to refresh the
+        # image (yet), the metrology has to be applied here, and not
+        # in frame.py.
+
+        if (self.raw_image.implements_metrology_matrices() and
+            metrology_matrices is not None):
+            self.raw_image.apply_metrology_from_matrices(metrology_matrices)
+
+        self.flex_image = self.raw_image.get_flex_image(
+            brightness=self.current_brightness / 100)
+        self.flex_image.adjust(color_scheme=self.current_color_scheme)
+
+    def update_brightness(self,b,color_scheme=0):
+        self.flex_image= self.raw_image.get_flex_image(brightness=b/100.)
+        self.update_color_scheme(color_scheme)
+        self.current_brightness = b
+
+    def update_color_scheme(self,color_scheme=0):
+        self.flex_image.adjust(color_scheme)
+        self.reset_the_cache()
+        self.UseLevel(self.zoom_level)
+        self.current_color_scheme = color_scheme
+
+    def reset_the_cache(self):
+
+        # setup the tile caches and Least Recently Used lists
+        self.cache = {}
+        self.lru = {}
+        for l in self.levels:
+            self.cache[l] = {}
+            self.lru[l] = []
+
+    def flex_image_get_tile(self,x,y):
+      # The supports_rotated_tiles_antialiasing_recommended flag in
+      # the C++ FlexImage class indicates whether the underlying image
+      # instance supports tilted readouts.  Anti-aliasing only makes
+      # sense if it does.
+      if self.raw_image is not None and \
+        self.zoom_level >=2 and \
+        self.flex_image.supports_rotated_tiles_antialiasing_recommended and \
+        self.user_requests_antialiasing:
+        # much more computationally intensive to prepare nice-looking pictures of tilted readout
+        self.flex_image.setZoom(self.zoom_level+1)
+        fraction = 512./self.flex_image.size1()/(2**(self.zoom_level+1))
+        self.flex_image.setWindowCart(  y, x, fraction )
+        self.flex_image.prep_string()
+        w,h = self.flex_image.ex_size2(), self.flex_image.ex_size1()
+        assert w==512
+        assert h==512
+        wx_image = wx.EmptyImage(w/2,h/2)
+        import Image
+        I = Image.fromstring("RGB",(512,512),self.flex_image.export_string)
+        J = I.resize((256,256),Image.ANTIALIAS)
+        wx_image.SetData(J.tostring())
+        return wx_image.ConvertToBitmap()
+      elif self.raw_image is not None:
+        self.flex_image.setZoom(self.zoom_level)
+        fraction = 256./self.flex_image.size1()/(2**self.zoom_level)
+        self.flex_image.setWindowCart(  y, x, fraction )
+        self.flex_image.prep_string()
+        w,h = self.flex_image.ex_size2(), self.flex_image.ex_size1()
+        assert w==256
+        assert h==256
+        wx_image = wx.EmptyImage(w,h)
+        wx_image.SetData(self.flex_image.export_string)
+        return wx_image.ConvertToBitmap()
+      else:
+        wx_image = wx.EmptyImage(256,256)
+        return wx_image.ConvertToBitmap()
+
+    def get_binning(self):
+      if self.zoom_level>=0: return 1.
+      return 2.**-self.zoom_level
+
+    def UseLevel(self, n):
+        """Prepare to serve tiles from the required level.
+
+        n  The required level
+
+        Returns a tuple (map_width, map_height, ppd_x, ppd_y) if succesful,
+        else None.  The width/height values are pixels.  The ppd_? values are
+        pixels-per-degree values for X and Y direction.
+        """
+        # try to get cache for this level, no cache means no level
+        #print "IN USE LEVEL",n
+        try:
+            self.tile_cache = self.cache[n]
+            self.tile_list = self.lru[n]
+        except KeyError:
+            return None
+        self.zoom_level = n
+        if self.raw_image is None: #dummy values if there is no image
+          self.center_x_lon = self.center_y_lat = 500.
+          return (1024,1024,1.,1.)
+        self.num_tiles_x = int(math.ceil((self.flex_image.size1()*(2**self.zoom_level))/256.))
+        self.num_tiles_y = int(math.ceil((self.flex_image.size2()*(2**self.zoom_level))/256.))
+        self.ppd_x = 2.**self.zoom_level
+        self.ppd_y = 2.**self.zoom_level
+        #print "USELEVEL %d # tiles: %d %d"%(n,self.num_tiles_x,self.num_tiles_y)
+        #print "USELEVEL %d returning"%n,(self.tile_size_x * self.num_tiles_x,
+        #        self.tile_size_y * self.num_tiles_y,
+        #        self.ppd_x, self.ppd_y)
+        # The longitude & latitude coordinates at the image center:
+        self.center_x_lon = self.extent[0] + (1./self.ppd_x) * (0 +
+          self.flex_image.size2() * (2**self.zoom_level) / 2.
+          )
+        self.center_y_lat = self.extent[3] - (1./self.ppd_y) * (0 +
+          self.flex_image.size1() * (2**self.zoom_level) / 2.
+          )
+        # The 2+num_tiles is just a trick to get PySlip to think the map is
+        # slightly larger, allowing zoom level -3 to be properly framed:
+        # ....for larger display sizes it is necessary to increase this...
+        # ....can tile_generator get the display size & figure it out?
+        return (self.tile_size_x * (2+self.num_tiles_x),
+                self.tile_size_y * (2+self.num_tiles_y),
+                self.ppd_x, self.ppd_y)
+
+    def get_initial_instrument_centering_within_picture_as_lon_lat(self):
+      import sys
+      if sys.platform.lower().find("linux") >= 0:
+        if self.raw_image.__class__.__name__.find("CSPadDetector") >= 0:
+          return 0.,0.
+        else:
+          return self.center_x_lon-self.extent[0], self.center_y_lat-self.extent[3]
+      else:
+        if self.raw_image.__class__.__name__.find("CSPadDetector") >= 0:
+          return self.extent[0], self.extent[3]
+        else:
+          return self.center_x_lon, self.center_y_lat
+
+    def GetTile(self, x, y):
+        #from libtbx.development.timers import Timer
+        #T = Timer("get tile")
+        """Get bitmap for tile at tile coords (x, y).
+
+        x  X coord of tile required (tile coordinates)
+        y  Y coord of tile required (tile coordinates)
+
+        Returns bitmap object containing the tile image.
+        Tile coordinates are measured from map top-left.
+        """
+        try:
+            # if tile in cache, return it from there
+            pic = self.tile_cache[(x, y)]
+            index = self.tile_list.index((x, y))
+            del self.tile_list[index]
+        except KeyError:
+            pic = self.flex_image_get_tile(x,y)
+            self.tile_cache[(x, y)] = pic
+
+        self.tile_list.insert(0, (x, y))
+        if len(self.tile_cache)>=self.MaxTileList:
+          del self.tile_cache[self.tile_list[-1]]
+          del self.tile_list[-1]
+        return pic
+
+    def get_flex_pixel_coordinates(self, lon, lat):
+      fast_picture_coord_pixel_scale, slow_picture_coord_pixel_scale = \
+        self.lon_lat_to_picture_fast_slow(lon,lat)
+      if self.flex_image.supports_rotated_tiles_antialiasing_recommended: # for generic_flex_image
+        tilted = self.flex_image.picture_to_readout(
+          slow_picture_coord_pixel_scale,fast_picture_coord_pixel_scale)
+        return tilted
+      else: # standard flex_image
+        return slow_picture_coord_pixel_scale,fast_picture_coord_pixel_scale
+
+    def lon_lat_to_picture_fast_slow(self,longitude,latitude):
+      # input latitude and longitude in degrees (conceptually)
+      # output fast and slow picture coordinates in units of detector pixels
+      # slow is pointing down (x).  fast is pointing right (y).
+      return \
+        (float(self.raw_image.size2)/2.) - \
+        (self.center_x_lon - float(longitude)),  \
+        (float(self.raw_image.size1)/2.) - \
+        (float(latitude) - self.center_y_lat)
+
+    def picture_fast_slow_to_lon_lat(self,pic_fast_pixel,pic_slow_pixel):
+      # inverse of the preceding function
+      return \
+        (float(self.raw_image.size2)/2.) - self.center_x_lon - pic_fast_pixel, \
+        (float(self.raw_image.size1)/2.) + self.center_y_lat - pic_slow_pixel
+
+    def picture_fast_slow_to_map_relative(self,pic_fast_pixel,pic_slow_pixel):
+      #return up/down, left/right map relative coords for pyslip layers
+      return pic_fast_pixel+self.extent[0],-pic_slow_pixel+self.extent[3]
+
+    def vec_picture_fast_slow_to_map_relative(self,vector):
+      value = []
+      for vec in vector:
+        value.append(self.picture_fast_slow_to_map_relative(vec[0],vec[1]))
+      return value
+
+    def get_spotfinder_data(self, params):
+
+      pointdata = []
+      test_pattern = False
+      if test_pattern is True and self.raw_image.__class__.__name__.find("CSPadDetector") >= 0:
+        key_count = -1
+        for key, asic in self.raw_image._tiles.iteritems():
+          key_count += 1
+          focus = asic.focus()
+          for slow in xrange(0,focus[0],20):
+            for fast in xrange(0,focus[1],20):
+              slowpic,fastpic = self.flex_image.tile_readout_to_picture(key_count,slow,fast)
+              mr1,mr2 = self.picture_fast_slow_to_map_relative(fastpic,slowpic)
+              pointdata.append((mr1,mr2,{"data":key}))
+
+      elif (self.raw_image.__class__.__name__.find("CSPadDetector") >= 0):
+        from cxi_xdr_xes.cftbx.spotfinder.speckfinder import spotfind_readout
+
+        key_count = -1
+        for key, asic in self.raw_image._tiles.iteritems():
+          key_count += 1
+          indexing = spotfind_readout(
+              readout=asic,
+              peripheral_margin=params.spotfinder.peripheral_margin)
+
+          for spot in indexing:
+            slow = int(round(spot[0]))
+            fast = int(round(spot[1]))
+
+            slowpic,fastpic = self.flex_image.tile_readout_to_picture(key_count,slow,fast)
+            mr1,mr2 = self.picture_fast_slow_to_map_relative(fastpic,slowpic)
+            pointdata.append((mr1,mr2,{"data":key}))
+
+      else:
+        from spotfinder.command_line.signal_strength import master_params
+        working_params = master_params.fetch(sources = []) #placeholder for runtime mods
+        working_params.show(expert_level=1)
+        distl_params = working_params.extract()
+
+        spotfinder,frameno = self.raw_image.get_spotfinder(distl_params)
+        spots = spotfinder.images[frameno]["spots_total"]
+        for spot in spots:
+          mr = self.picture_fast_slow_to_map_relative(
+              spot.max_pxl_y() + 0.5, spot.max_pxl_x() + 0.5)
+#             spot.ctr_mass_y() + 0.5, spot.ctr_mass_x() + 0.5)
+          pointdata.append(mr)
+      return pointdata
+
+    def get_effective_tiling_data(self, params):
+      box_data = []
+      text_data = []
+      if hasattr(self.raw_image, 'get_tile_manager'):
+        IT = self.raw_image.get_tile_manager(params).effective_tiling_as_flex_int()
+        for i in xrange(len(IT) // 4):
+          tile = IT[4*i:4*i+4]
+          attributes = {'color': '#0000FFA0', 'width': 1, 'closed': False}
+          box_data.append(
+              ((self.picture_fast_slow_to_map_relative(tile[1], tile[0]),
+                self.picture_fast_slow_to_map_relative(tile[1], tile[2])),
+               attributes))
+          box_data.append(
+              ((self.picture_fast_slow_to_map_relative(tile[1], tile[0]),
+                self.picture_fast_slow_to_map_relative(tile[3], tile[0])),
+               attributes))
+          box_data.append(
+              ((self.picture_fast_slow_to_map_relative(tile[1], tile[2]),
+                self.picture_fast_slow_to_map_relative(tile[3], tile[2])),
+               attributes))
+          box_data.append(
+              ((self.picture_fast_slow_to_map_relative(tile[3], tile[0]),
+                self.picture_fast_slow_to_map_relative(tile[3], tile[2])),
+               attributes))
+          txt_x, txt_y = self.picture_fast_slow_to_map_relative(
+              (tile[1]+tile[3])//2, (tile[0]+tile[2])//2)
+          text_data.append((txt_x, txt_y, "%i" %i))
+      return box_data, text_data
