bd9259f81abc16b7d00739dc939332103f421e03
Pavel Afonine <pafonine@users.noreply.github.com>
2012-08-27 20:24:07 +0000

    use reference sites restrains in minimization

--- mmtbx/geometry_restraints/tst_reference_coordinate.py
+++ mmtbx/geometry_restraints/tst_reference_coordinate.py
@@ -6,7 +6,6 @@ import iotbx.pdb
 from cctbx.array_family import flex
 from cctbx import adp_restraints # import dependency
 from mmtbx.monomer_library import server, pdb_interpretation
-import mmtbx.restraints
 from cStringIO import StringIO
 
 def simple_pdb () :
@@ -186,11 +185,12 @@ def exercise_2():
     strict_conflict_handling = True,
     force_symmetry           = True,
     log                      = None)
-  geometry = processed_pdb_file.geometry_restraints_manager()
-  restraints_manager = mmtbx.restraints.manager(geometry = geometry,
-    normalization=True)
+  grm = processed_pdb_file.geometry_restraints_manager()
+  xrs2 = processed_pdb_file.xray_structure(show_summary = False)
   awl2 = processed_pdb_file.all_chain_proxies.pdb_hierarchy.atoms_with_labels()
-  ph3 = iotbx.pdb.input(source_info=None, lines=pdb_str_3).construct_hierarchy()
+  pdb_inp3 = iotbx.pdb.input(source_info=None, lines=pdb_str_3)
+  xrs3 = pdb_inp3.xray_structure_simple()
+  ph3 = pdb_inp3.construct_hierarchy()
   ph3.atoms().reset_i_seq()
   awl3 =  ph3.atoms_with_labels()
   sites_cart_reference = flex.vec3_double()
@@ -204,10 +204,28 @@ def exercise_2():
       selection.append(a2.i_seq)
       sites_cart_reference.append(a3.xyz)
   assert selection.size() == len(reference_names)
-  # Next step would be:
-  #   restraints_manager.add_reference_restraints(
-  #     sites_cart_reference = sites_cart_reference,
-  #     selection = selection)
+  #
+  grm.generic_restraints_manager.add_reference_restraints(
+    sites_cart = sites_cart_reference,
+    selection = selection)
+  #
+  d1 = flex.mean(flex.sqrt((xrs2.sites_cart() - xrs3.sites_cart()).dot()))
+  print "distance start: %6.4f"%d1
+  from cctbx import geometry_restraints
+  import mmtbx.command_line.geometry_minimization
+  import scitbx.lbfgs
+  grf = geometry_restraints.flags.flags(default=True)
+  sites_cart = xrs2.sites_cart()
+  minimized = mmtbx.command_line.geometry_minimization.lbfgs(
+    sites_cart                  = sites_cart,
+    geometry_restraints_manager = grm,
+    sites_cart_selection        = flex.bool(sites_cart.size(), selection),
+    geometry_restraints_flags   = grf,
+    lbfgs_termination_params = scitbx.lbfgs.termination_parameters(
+      max_iterations=5000))
+  xrs2.set_sites_cart(sites_cart = sites_cart)
+  d2 = flex.mean(flex.sqrt((xrs2.sites_cart() - xrs3.sites_cart()).dot()))
+  print "distance final: %6.4f"%d2
 
 if (__name__ == "__main__") :
   exercise_1()
