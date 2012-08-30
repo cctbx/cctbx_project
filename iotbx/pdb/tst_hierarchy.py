059197a1b02efc25be4c60d627f412a9cd15df3d
Pavel Afonine <pafonine@users.noreply.github.com>
2012-08-30 02:34:47 +0000

    missing test added; name changed to be more specific

--- iotbx/pdb/tst_hierarchy.py
+++ iotbx/pdb/tst_hierarchy.py
@@ -5992,6 +5992,67 @@ ATOM     59  OXT TYR A   6       9.056   2.078   5.240  1.00 17.49           O
 TER
 """)
 
+def exercise_get_peptide_c_alpha_selection():
+  pdb_inp = pdb.input(source_info=None, lines=flex.split_lines("""\
+MODEL        0
+ATOM      1  C   MET A   1
+ATOM      2  CA AMET A   1
+BREAK
+ATOM      3  N   GLY A   2
+ATOM      4  CA AGLY A   2
+ENDMDL
+MODEL        1
+ATOM      1  N   MET A   1
+ATOM      2  CA AMET A   1
+ATOM      3  N   ALA B   2
+ATOM      4  CA AALA B   2
+ENDMDL
+MODEL        2
+HETATM    6 CA  AION B   1      32.360  11.092  17.308  0.92 35.96          CA2+
+HETATM    7 CA   ION B   2      30.822  10.665  17.190  1.00 36.87
+ENDMDL
+"""))
+  h = pdb_inp.construct_hierarchy()
+  h.atoms().reset_i_seq()
+  assert list(h.get_peptide_c_alpha_selection()) == [1, 3, 5, 7]
+  #
+  pdb_inp = pdb.input(source_info=None, lines=flex.split_lines("""\
+ATOM      1  N   THR A   2      -3.791  -8.769  29.092  1.00 24.15           N
+ATOM      2  CA  THR A   2      -3.627  -7.675  28.090  1.00 25.97           C
+ATOM      3  C   THR A   2      -2.202  -7.127  28.152  1.00 24.18           C
+ATOM      4  O   THR A   2      -1.633  -6.984  29.233  1.00 24.71           O
+ATOM      5  CB  THR A   2      -4.627  -6.527  28.357  1.00 26.50           C
+ATOM      6  OG1 THR A   2      -5.961  -7.056  28.404  1.00 28.79           O
+ATOM      7  CG2 THR A   2      -4.548  -5.486  27.255  1.00 27.05           C
+ATOM      8  N   LYS A   3      -1.629  -6.832  26.988  1.00 24.44           N
+ATOM     10  C   LYS A   3      -0.196  -4.896  27.485  1.00 23.66           C
+ATOM     11  O   LYS A   3      -1.094  -4.084  27.265  1.00 23.75           O
+ATOM     12  CB  LYS A   3       0.199  -6.262  25.438  1.00 26.61           C
+ATOM      9  CA ALYS A   3      -0.266  -6.307  26.901  1.00 25.16           C
+ATOM     13  CG ALYS A   3       0.312  -7.619  24.754  0.50 27.88           C
+ATOM     15  CD ALYS A   3       1.436  -8.454  25.347  0.50 27.58           C
+ATOM     17  CE ALYS A   3       1.585  -9.783  24.621  0.50 28.69           C
+ATOM     19  NZ ALYS A   3       0.362 -10.624  24.732  0.50 28.63           N
+ATOM      9  CA BLYS A   3      -0.266  -6.307  26.901  1.00 25.16           C
+ATOM     14  CG BLYS A   3       0.201  -7.603  24.718  0.50 27.66           C
+ATOM     16  CD BLYS A   3       1.205  -8.570  25.325  0.50 27.30           C
+ATOM     18  CE BLYS A   3       1.213  -9.893  24.575  0.50 28.17           C
+ATOM     20  NZ BLYS A   3       2.149 -10.873  25.188  0.50 27.40           N
+ATOM     21  N   LYS A   4       0.873  -4.612  28.225  1.00 22.24           N
+ATOM     22  CA  LYS A   4       1.068  -3.295  28.826  1.00 21.81           C
+ATOM     23  C   LYS A   4       2.337  -2.642  28.295  1.00 19.26           C
+ATOM     24  O   LYS A   4       3.417  -3.243  28.310  1.00 18.66           O
+ATOM     25  CB  LYS A   4       1.156  -3.398  30.354  1.00 23.29           C
+ATOM     26  CG  LYS A   4      -0.170  -3.685  31.031  1.00 27.60           C
+ATOM     27  CD  LYS A   4      -0.049  -3.681  32.551  1.00 32.16           C
+ATOM     28  CE  LYS A   4       0.797  -4.842  33.052  1.00 33.04           C
+ATOM     29  NZ  LYS A   4       0.827  -4.892  34.541  1.00 36.05           N
+"""))
+  h = pdb_inp.construct_hierarchy()
+  h.atoms().reset_i_seq()
+  assert list(h.get_peptide_c_alpha_selection()) == [1, 11, 16, 22]
+
+
 def exercise(args):
   comprehensive = "--comprehensive" in args
   forever = "--forever" in args
@@ -6006,6 +6067,7 @@ def exercise(args):
       prev = key
   phenix_regression_pdb_file_names = get_phenix_regression_pdb_file_names()
   while True:
+    exercise_get_peptide_c_alpha_selection()
     exercise_adopt_xray_structure()
     exercise_atom()
     exercise_atom_group()
