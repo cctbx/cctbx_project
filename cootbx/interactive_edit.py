53831e22cd9bd19f99c9c5afbd52bad0fad2d49f
Nathaniel Echols <nathaniel.echols@gmail.com>
2012-09-05 23:45:30 +0000

    added button to generate new maps for edited model

--- cootbx/interactive_edit.py
+++ cootbx/interactive_edit.py
@@ -6,6 +6,8 @@ import time
 import os
 import sys
 
+t_wait = 250 # in milliseconds
+
 #-----------------------------------------------------------------------
 # Phenix side
 def start_coot_and_wait (
@@ -20,6 +22,7 @@ def start_coot_and_wait (
   if (work_dir is None) : work_dir = os.getcwd()
   if (not os.path.isdir(work_dir)) :
     os.makedirs(work_dir)
+  import mmtbx.maps.utils
   from libtbx.str_utils import make_header
   from libtbx import easy_run
   from libtbx import group_args
@@ -43,14 +46,27 @@ def start_coot_and_wait (
     str(time.asctime())
   base_dir = os.path.dirname(pdb_file)
   tmp_file = os.path.join(base_dir, "coot_out_tmp.pdb")
+  edit_file = os.path.join(base_dir, "coot_tmp_edits.pdb")
+  maps_file = os.path.join(base_dir, ".NEW_MAPS")
   while (True) :
     if (os.path.isfile(tmp_file)) :
       print >> log, "  Coot editing complete at %s" % str(time.asctime())
       break
+    elif (os.path.isfile(maps_file)) :
+      t1 = time.time()
+      assert os.path.isfile(edit_file)
+      mmtbx.maps.utils.create_map_from_pdb_and_mtz(
+        pdb_file=edit_file,
+        mtz_file=data_file,
+        output_file=os.path.join(base_dir, "maps_for_coot.mtz"),
+        fill=True,
+        out=log)
+      t2 = time.time()
+      print >> log, "Calculated new map coefficients in %.1fs" % (t2-t1)
+      os.remove(maps_file)
     else :
-      time.sleep(1)
+      time.sleep(t_wait/1000.)
   shutil.move(tmp_file, "coot_out.pdb")
-  import mmtbx.maps.utils
   mmtbx.maps.utils.create_map_from_pdb_and_mtz(
     pdb_file="coot_out.pdb",
     mtz_file=data_file,
@@ -79,7 +95,6 @@ class manager (object) :
     import gtk
     import coot
     import coot_python
-    auto_read_make_and_draw_maps(map_file)
     toolbar = coot_python.main_toolbar()
     assert (toolbar is not None)
     return_button = gtk.ToolButton()
@@ -88,15 +103,43 @@ class manager (object) :
     toolbar.insert(return_button, -1)
     return_button.connect("clicked", self.OnReturn)
     return_button.show()
+    maps_button = gtk.ToolButton()
+    maps_button.set_label("Fetch new maps")
+    maps_button.set_is_important(True)
+    toolbar.insert(maps_button, -1)
+    maps_button.connect("clicked", self.OnNewMaps)
+    maps_button.show()
     self._imol = read_pdb(pdb_file)
     set_molecule_bonds_colour_map_rotation(self._imol, 30)
+    self._map_mols = []
+    self.load_maps(map_file)
     dialog = gtk.MessageDialog(None, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO,
       gtk.BUTTONS_OK, "You may now edit your model to prepare for further "+
       "building and refinement.   When you are finished, "+
       "click the \"Save model and return\" button on the toolbar, then close "+
-      "Coot.")
+      "Coot.  If you want to recalculate the map coefficients, click \"Fetch "+
+      "new maps\".")
     dialog.run()
     dialog.destroy()
+
+  def load_maps (self, map_file) :
+    if (len(self._map_mols) > 0) :
+      set_colour_map_rotation_for_map(0)
+      for imol in self._map_mols :
+        close_molecule(imol)
+    else :
+      set_colour_map_rotation_for_map(10)
+    print "Loading %s" % map_file
+    self._map_mols = []
+    map_imol = auto_read_make_and_draw_maps(map_file)
+    if (isinstance(map_imol, int)) :
+      # XXX this may be dangerous, but auto_read_make_and_draw_maps only returns
+      # the last imol
+      self._map_mols = [ map_imol - 1, map_imol ]
+    else :
+      self._map_mols = map_imol
+    set_imol_refinement_map(self._map_mols[0])
+    set_scrollable_map(self._map_mols[0])
 
   def OnReturn (self, *args) :
     import coot
@@ -120,3 +163,34 @@ class manager (object) :
       "The PDB file has been saved to %s.  You may now close Coot." % pdb_out)
     dialog.run()
     dialog.destroy()
+
+  def OnNewMaps (self, *args) :
+    import coot
+    import gtk
+    import gobject
+    dir_name = os.path.dirname(self.file_name)
+    pdb_out = os.path.join(dir_name, "coot_tmp_edits.pdb")
+    maps_in = os.path.join(dir_name, "maps_for_coot.mtz")
+    if (os.path.exists(maps_in)) :
+      os.remove(maps_in)
+    write_pdb_file(self._imol, pdb_out)
+    dialog = gtk.MessageDialog(None, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO,
+      gtk.BUTTONS_OK_CANCEL,
+      "Phenix will generate new maps; these will be automatically loaded "+
+      "when complete.")
+    response = dialog.run()
+    if (response == gtk.RESPONSE_OK) :
+      print "WRITING .NEW_MAPS"
+      open(os.path.join(dir_name, ".NEW_MAPS"), "w").write("1")
+      gobject.timeout_add(t_wait, self.OnWaitForMaps)
+    dialog.destroy()
+
+  def OnWaitForMaps (self, *args) :
+    # TODO a spinner or progress bar might be nice here...
+    dir_name = os.path.dirname(self.file_name)
+    maps_in = os.path.join(dir_name, "maps_for_coot.mtz")
+    if (os.path.isfile(maps_in)) :
+      self.load_maps(maps_in)
+      os.remove(maps_in)
+      return False # this kills the timeout
+    return True
