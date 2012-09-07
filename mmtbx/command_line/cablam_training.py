5e437a008adabf71e55beb99db02d985ea9bff49
Christopher J Williams <chrisjwilliams@users.noreply.github.com>
2012-09-07 19:56:06 +0000

    cablam_training commandline updated for phil compliance

--- mmtbx/command_line/cablam_training.py
+++ mmtbx/command_line/cablam_training.py
@@ -1,7 +1,8 @@
 from __future__ import division
 # LIBTBX_SET_DISPATCHER_NAME phenix.cablam_training
 
+import sys
 from mmtbx.cablam import cablam_training
 
 if __name__ == "__main__":
-  cablam_training.run()
+  cablam_training.run(sys.argv[1:])
