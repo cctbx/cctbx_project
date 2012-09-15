1f1e630475a9b3b1f7384d7a4c66b14ec80a3c53
Luc J. Bourhis <luc_j_bourhis@mac.com>
2012-09-15 16:33:50 +0000

    Replace relative paths by absolute ones in error messages.
    Motivation: the C++ source files that are generated within cctbx_build
    are processed by SCons with their paths relative to that build directory
    whereas the C++ source files inside cctbx_project are handled by SCons
    with their absolute path. Unfortunately, Xcode does not recognize compiler
    errors that feature relative a path. As a result, the click-to-jump to the
    error location does not work for C++ source files generated inside cctbx_build
    which make it more difficult to debug the code generation. Hence this script.
    
    Note: this script is now entirely written in Python, using the
    subprocess module to create a parent/child pair: the child process runs SCons
    while the parent process filters the output of the child.

---
+++ libtbx/development/xcode_build_phase.py
@@ -0,0 +1,60 @@
+import sys, os, re
+
+def run_scons():
+  import libtbx.load_env
+  from libtbx import easy_run
+  from libtbx.command_line import scons
+
+  print; print; print '-'*80
+
+  m = re.search(r"^(\d+ \. \d+ \. \d+) .*? \[\s*GCC\s* (\d+ \. \d+ \. \d+)",
+                sys.version,
+                re.X|re.M|re.S)
+  print 'Python %s (compiled with gcc %s)' % m.groups()
+  print
+
+  print "** %s **" % libtbx.env.build_path.basename()
+  libtbx.env.build_options.report()
+
+  print
+  print '-'*80
+  for compiler in ('gcc', 'clang',):
+    print easy_run.fully_buffered('type %s' % compiler).stdout_lines[0]
+  print '-'*80
+
+  os.chdir(os.environ['XCODE_CCTBX_BUILD'])
+  sys.argv[1:] = os.environ['XCODE_SCONS_OPTIONS'].split()
+  if os.environ.get('XCODE_SCONS_LIB_TARGET'):
+    libs = os.environ['XCODE_SCONS_LIB_TARGET'].split()
+    sys.argv.extend([ "lib/%s.so" % lib for lib in libs ])
+    print "warning: only library being built: %s" % ', '.join(libs)
+  elif os.environ.get('XCODE_SCONS_PROGRAM_TARGET'):
+    programs = os.environ['XCODE_SCONS_PROGRAM_TARGET'].split()
+    sys.argv.extend(programs)
+    print "warning: only program being built: %s" % ', '.join(programs)
+  scons.run()
+
+def run_filtered_scons():
+  import libtbx.load_env
+  import subprocess
+  proc = subprocess.Popen([ "%s/libtbx.python" % abs(libtbx.env.bin_path),
+                            '-u', __file__, 'child'],
+                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  error_pat = re.compile(r'^(.*? : \d* : \d* : \s* error:)', re.X)
+  prefix = abs(libtbx.env.build_path) + '/'
+  for li in proc.stdout:
+    m = error_pat.search(li)
+    if m and not m.group(1).startswith('/'):
+      sys.stdout.write(prefix)
+    sys.stdout.write(li)
+    sys.stdout.flush()
+  proc.wait()
+  return proc.returncode
+
+if __name__ == '__main__':
+  if sys.argv[1] == "child":
+    run_scons()
+  elif sys.argv[1] == "parent":
+    sys.exit(run_filtered_scons())
+  else:
+    print "Usage: %s [child|parent]" % os.path.basename(__file__)
