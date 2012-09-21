f476ef95d78f6d27b879eaf2d0401647f71a1c50
Nicholas Sauter <nksauter@lbl.gov>
2012-09-21 22:24:00 +0000

    better

--- xfel/cxi/cxi_cc.py
+++ xfel/cxi/cxi_cc.py
@@ -146,6 +146,7 @@ def run_cc(params,output):
        if this_label.find("fobs")>=0:
          print >>output, this_label,array.observation_type()
          uniform.append(array.as_intensity_array())
+         continue
        if this_label.find("imean")>=0:
          print >>output, this_label,array.observation_type()
          uniform.append(array.as_intensity_array())
