f0dc5652c2627b166320c48dea1a2e3a8e6fe811
Nathaniel Echols <nathaniel.echols@gmail.com>
2012-10-08 00:27:08 +0000

    program to calculate CC* and model/data statistics (as described in Karplus & Diederichs 2012); needs more testing

---
+++ mmtbx/command_line/cc_star.py
@@ -0,0 +1,77 @@
+
+from __future__ import division
+from libtbx.utils import Sorry
+import libtbx.phil
+import sys
+
+def merging_and_model_statistics (
+    fmodel, unmerged_i_obs, i_obs=None, n_bins=20) :
+  from iotbx.command_line import merging_statistics
+  if (i_obs is None) :
+    i_obs = fmodel.f_obs().f_as_f_sq()
+  if (not i_obs.is_unique_set_under_symmetry()) :
+    i_obs = i_obs.merge_equivalents().array()
+  if (i_obs.anomalous_flag()) :
+    i_obs = i_obs.average_bijvoet_mates()
+  f_model = fmodel.f_model()
+  work_sel = f_model.customized_copy(data=fmodel.arrays.work_sel)
+  free_sel = f_model.customized_copy(data=fmodel.arrays.free_sel)
+  if (f_model.anomalous_flag()) :
+    f_model = f_model.average_bijvoet_mates()
+    work_sel = work_sel.average_bijvoet_mates()
+    free_sel = free_sel.average_bijvoet_mates()
+  i_obs, work_sel = i_obs.common_sets(other=work_sel)
+  i_obs, free_sel = i_obs.common_sets(other=free_sel)
+  i_obs, f_model = i_obs.common_sets(other=f_model)
+  i_calc = abs(f_model).f_as_f_sq()
+  d_max, d_min = i_calc.d_max_min()
+  model_arrays = merging_statistics.model_based_arrays(
+    i_obs=i_obs,
+    i_calc=i_calc,
+    work_sel=work_sel,
+    free_sel=free_sel)
+  return merging_statistics.dataset_statistics(
+    i_obs=unmerged_i_obs,
+    crystal_symmetry=i_calc,
+    d_min=d_min,
+    d_max=d_max,
+    n_bins=n_bins,
+    model_arrays=model_arrays)
+
+master_phil = libtbx.phil.parse("""
+include scope mmtbx.utils.cmdline_input_phil_str
+unmerged_data = None
+  .type = path
+unmerged_labels = None
+  .type = str
+""", process_includes=True)
+
+def run (args, out=sys.stdout) :
+  import mmtbx.utils
+  from iotbx.command_line import merging_statistics
+  cmdline = mmtbx.utils.cmdline_load_pdb_and_data(
+    args=args,
+    master_phil=master_phil,
+    process_pdb_file=False,
+    scattering_table="n_gaussian",
+    out=out)
+  params = cmdline.params
+  fmodel = cmdline.fmodel
+  if (params.unmerged_data is None) :
+    raise Sorry("Please specify unmerged_data file")
+  unmerged_i_obs = merging_statistics.select_data(
+    file_name=params.unmerged_data,
+    data_labels=params.unmerged_labels,
+    out=out)
+  stats = merging_and_model_statistics(
+    fmodel=fmodel,
+    unmerged_i_obs=unmerged_i_obs)
+  stats.show_cc_star(out=out)
+  print >> out, ""
+  print >> out, "Reference:"
+  print >> out, "  Karplus PA & Diederichs K (2012) Science 336:1030-3."
+  print >> out, ""
+  return stats
+
+if (__name__ == "__main__") :
+  run(sys.argv[1:])
