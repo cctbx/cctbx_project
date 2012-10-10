235a3a770d2d6595feb6b57aee0989a41b33217b
Richard Gildea <richard.gildea@diamond.ac.uk>
2012-10-10 22:31:40 +0000

    new dataset_statistics.as_cif_block()

--- iotbx/regression/tst_merging_statistics.py
+++ iotbx/regression/tst_merging_statistics.py
@@ -1,9 +1,11 @@
-
 from __future__ import division
-from libtbx import easy_run
 import libtbx.load_env
+from libtbx.test_utils import approx_equal
+from iotbx.command_line import merging_statistics
+from cctbx.array_family import flex
 import os
 import sys
+from cStringIO import StringIO
 
 def exercise (debug=False) :
   if (not libtbx.env.has_module("phenix_regression")) :
@@ -13,7 +15,6 @@ def exercise (debug=False) :
     relative_path="phenix_regression/wizards/p9_se_w2.sca",
     test=os.path.isfile)
   args = [
-    "phenix.merging_statistics",
     hkl_file,
     "space_group=I4",
     "unit_cell=113.949,113.949,32.474,90,90,90",
@@ -22,10 +23,17 @@ def exercise (debug=False) :
   if (debug) :
     args.append("debug=True")
     print " ".join(args)
-  result = easy_run.fully_buffered(" ".join(args)).raise_if_errors()
-  assert ("R-merge: 0.073" in result.stdout_lines)
-  assert ("R-meas:  0.079" in result.stdout_lines)
-  print "OK"
+  out = StringIO()
+  result = merging_statistics.run(args, out=out)
+  assert ("R-merge: 0.073" in out.getvalue())
+  assert ("R-meas:  0.079" in out.getvalue())
+  cif_block = result.as_cif_block()
+  assert "_reflns_shell" in cif_block
+  assert approx_equal(float(cif_block["_reflns.pdbx_Rpim_I_all"]), 0.0295498162694)
+  assert approx_equal(
+    flex.int(cif_block["_reflns_shell.number_measured_all"]),
+    [15737, 15728, 15668, 15371, 14996, 14771, 13899, 13549, 13206, 12528])
 
 if (__name__ == "__main__") :
   exercise(debug=("--debug" in sys.argv))
+  print "OK"
