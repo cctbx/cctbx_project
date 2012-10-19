6db9bd3592f796460640bcfe078616fee0da1172
Gabor Bunkoczi <gbunkoczi@users.noreply.github.com>
2012-10-19 16:34:45 +0000

    Generic Cartesian product iterator: compile time type and dimension setup, runtime execution

---
+++ scitbx/math/cartesian_product_fixed_size.hpp
@@ -0,0 +1,296 @@
+#ifndef SCITBX_MATH_CARTESIAN_PRODUCT_FIXED_H
+#define SCITBX_MATH_CARTESIAN_PRODUCT_FIXED_H
+
+#include <boost/fusion/container/vector.hpp>
+#include <boost/fusion/include/vector.hpp>
+#include <boost/fusion/container/vector/vector_fwd.hpp>
+#include <boost/fusion/include/vector_fwd.hpp>
+#include <boost/fusion/container/vector/convert.hpp>
+#include <boost/fusion/include/as_vector.hpp>
+#include <boost/fusion/adapted/mpl.hpp>
+#include <boost/fusion/include/mpl.hpp>
+#include <boost/fusion/algorithm/transformation/transform.hpp>
+#include <boost/fusion/include/transform.hpp>
+#include <boost/fusion/sequence/comparison/equal_to.hpp>
+#include <boost/fusion/include/equal_to.hpp>
+#include <boost/fusion/algorithm/query/any.hpp>
+#include <boost/fusion/include/any.hpp>
+#include <boost/fusion/sequence/intrinsic/size.hpp>
+#include <boost/fusion/include/size.hpp>
+
+#include <boost/mpl/transform.hpp>
+#include <boost/mpl/size.hpp>
+
+#include <boost/type_traits/remove_reference.hpp>
+
+namespace scitbx
+{
+
+namespace math
+{
+
+namespace cartesian_product
+{
+
+template< class Iterator >
+struct iterated_range
+{
+  typedef Iterator iterator_type;
+  iterator_type begin;
+  iterator_type end;
+
+  iterated_range();
+  iterated_range(const iterator_type& begin, const iterator_type& end);
+  ~iterated_range();
+};
+
+template< class Iterator >
+bool operator ==(
+  const iterated_range< Iterator >& left,
+  const iterated_range< Iterator >& right
+  );
+
+template< class Iterator >
+struct counter
+{
+  typedef Iterator iterator_type;
+  typedef typename iterator_type::value_type value_type;
+  typedef iterated_range< iterator_type > range_type;
+  typedef counter< iterator_type > counter_type;
+  
+  range_type range;
+  iterator_type current;
+
+  counter();
+  counter(const range_type& range);
+  ~counter();
+};
+
+template< class Iterator >
+bool operator ==(
+  const counter< Iterator >& left,
+  const counter< Iterator >& right
+  );
+
+template< typename CounterVector, unsigned Depth >
+struct increment_fast_back
+{
+  static bool process(CounterVector& cv);
+};
+
+template< typename CounterVector >
+struct increment_fast_back< CounterVector, 0 >
+{
+  static bool process(CounterVector& cv);
+};
+
+template< typename CounterVector, unsigned Depth >
+struct increment_fast_front
+{
+  static bool process(CounterVector& cv);
+};
+
+template< typename CounterVector >
+struct increment_fast_front< CounterVector, 0 >
+{
+  static bool process(CounterVector& cv);
+};
+
+template<
+  typename IteratorTypeList,
+  template<class, unsigned> class IterationOrder = increment_fast_back
+  >
+class fixed_size_iterator
+{
+private:
+  struct get_value_type
+  {
+    template< class Any >
+    struct apply
+    {
+      typedef typename Any::value_type type;
+    };
+  };
+
+  struct get_counter_type
+  {
+    template< class Iterator >
+    struct apply
+    {
+      typedef typename counter< Iterator >::counter_type type;
+    };
+  };
+
+  struct get_range_type
+  {
+    template< class Counter >
+    struct apply
+    {
+      typedef typename Counter::range_type type;
+    };
+  };
+
+  struct is_counter_empty
+  {
+    template<typename Counter>
+    bool operator()(const Counter& counter) const
+    {
+      return counter.range.begin == counter.range.end;
+    }
+  };
+
+  struct get_counter_for_range
+  {
+    template< typename Signature > struct result;
+
+    template< typename Transformation, class Range >
+    struct result< Transformation( Range ) >
+    {
+      typedef typename Range::iterator_type iterator_type;
+      typedef typename counter< iterator_type >::counter_type type;
+    };
+
+    template< typename Transformation, class Range >
+    struct result< Transformation( Range& ) >
+    {
+      typedef typename Range::iterator_type iterator_type;
+      typedef typename counter< iterator_type >::counter_type type;
+    };
+
+    template< class Range >
+    typename result< get_counter_for_range( Range ) >::type 
+    operator()(const Range& range) const
+    {
+      return typename result< get_counter_for_range( Range ) >::type( range );
+    }
+  };
+
+  struct get_counter_current_value
+  {
+    template< typename Signature > struct result;
+
+    template< typename Transformation, class Counter >
+    struct result< Transformation( Counter ) >
+    {
+      typedef typename Counter::value_type type;
+    };
+
+    template< typename Transformation, class Counter >
+    struct result< Transformation( Counter& ) >
+    {
+      typedef typename Counter::value_type type;
+    };
+
+    template< class Counter >
+    typename result< get_counter_current_value( Counter ) >::type 
+    operator()(const Counter& counter) const
+    {
+      return *counter.current;
+    }
+  };
+
+public:
+  typedef fixed_size_iterator< IteratorTypeList, IterationOrder > iterator_type;
+  typedef typename boost::mpl::transform<
+    IteratorTypeList,
+    get_value_type
+    >::type mpl_value_vector_type;
+  typedef typename boost::mpl::transform<
+    IteratorTypeList,
+    get_counter_type
+    >::type mpl_counter_vector_type;
+  typedef typename boost::mpl::transform<
+    mpl_counter_vector_type,
+    get_range_type
+    >::type mpl_range_vector_type;
+
+  typedef typename boost::fusion::result_of::as_vector<
+    mpl_value_vector_type
+    >::type value_type;
+  typedef typename boost::fusion::result_of::as_vector<
+    mpl_counter_vector_type
+    >::type counter_vector_type;
+  typedef typename boost::fusion::result_of::as_vector<
+    mpl_range_vector_type
+    >::type range_vector_type;
+  typedef IterationOrder<
+    counter_vector_type,
+    boost::mpl::size< IteratorTypeList >::value - 1
+    > order_type;
+
+private:
+  counter_vector_type counter_vector_;
+  bool finished_;
+  value_type current_value_;
+
+public:
+  fixed_size_iterator();
+  fixed_size_iterator(const range_vector_type& rv);
+  ~fixed_size_iterator();
+
+  iterator_type& operator ++();
+  iterator_type operator ++(int);
+  bool operator ==(const iterator_type& rhs) const;
+  bool operator !=(const iterator_type& rhs) const;
+  value_type& operator *();
+
+public:
+  static const iterator_type end;
+};
+
+template<
+  typename ContainerTypeList,
+  template<class, unsigned> class IterationOrder = increment_fast_back
+  >
+class from_containers
+{
+private:
+  struct get_iterator_type
+  {
+    template< class Container >
+    struct apply
+    {
+      typedef typename Container::const_iterator type;
+    };
+  };
+
+public:
+  typedef typename boost::mpl::transform<
+    ContainerTypeList,
+    get_iterator_type
+    >::type mpl_iterator_vector_type;
+  typedef fixed_size_iterator< mpl_iterator_vector_type, IterationOrder >
+    cartesian_iterator_type;
+  typedef typename cartesian_iterator_type::range_vector_type
+    initializer_type;
+};
+
+struct get_range_from_container
+{
+  template< typename Signature > struct result;
+
+  template< typename Transformation, class Container >
+  struct result< Transformation( Container ) >
+  {
+    typedef typename Container::const_iterator iterator_type;
+    typedef typename counter< iterator_type >::range_type type;
+  };
+
+  template< class Container >
+  typename result< get_range_from_container( Container ) >::type 
+  operator()(const Container& container) const
+  {
+    typedef typename result< get_range_from_container( Container ) >::type
+      result_type;
+    return result_type( container.begin(), container.end() );
+  }
+};
+
+#include "cartesian_product_fixed_size.hxx"
+
+} // namespace cartesian_product
+} // namespace geometry
+} // namespace mmtbx
+
+#endif // SCITBX_MATH_CARTESIAN_PRODUCT_FIXED_H
+
---
+++ scitbx/math/cartesian_product_fixed_size.hxx
@@ -0,0 +1,232 @@
+template< class Iterator >
+iterated_range< Iterator >::iterated_range()
+  : end( begin )
+{}
+
+template< class Iterator >
+iterated_range< Iterator >::iterated_range(
+  const Iterator& begin_,
+  const Iterator& end_
+  )
+  : begin( begin_ ), end( end_ )
+{}
+
+template< class Iterator >
+iterated_range< Iterator >::~iterated_range()
+{}
+
+template< class Iterator >
+bool
+operator ==(
+  const iterated_range< Iterator >& left,
+  const iterated_range< Iterator >& right
+  )
+{
+  return ( left.begin == right.begin ) && ( left.end == right.end );
+}
+
+template< class Iterator >
+counter< Iterator >::counter()
+  : current( range.begin )
+{}
+
+template< class Iterator >
+counter< Iterator >::counter(const range_type& range_)
+  : range( range_ ), current( range.begin )
+{}
+
+template< class Iterator >
+counter< Iterator >::~counter()
+{}
+
+template< class Iterator >
+bool
+operator ==(const counter< Iterator >& left, const counter< Iterator >& right)
+{
+  return ( left.current == right.current ) && ( left.range == right.range );
+}
+
+template< typename CounterVector, unsigned Depth >
+bool
+increment_fast_back< CounterVector, Depth >::process(CounterVector& cv)
+{
+  typedef typename boost::remove_reference<
+    typename boost::fusion::result_of::at_c< CounterVector, Depth >::type
+    >::type counter_type;
+  counter_type& mycounter = boost::fusion::at_c< Depth >( cv );
+  ++( mycounter.current );
+  
+  if ( mycounter.current == mycounter.range.end )
+  {
+    mycounter.current = mycounter.range.begin;
+    return increment_fast_back< CounterVector, Depth - 1 >::process( cv );
+  }
+  else
+  {
+    return false;
+  }
+}
+
+template< typename CounterVector >
+bool
+increment_fast_back< CounterVector, 0 >::process(CounterVector& cv)
+{
+  typedef typename boost::remove_reference<
+    typename boost::fusion::result_of::at_c< CounterVector, 0 >::type
+    >::type counter_type;
+  counter_type& mycounter = boost::fusion::at_c< 0 >( cv );
+  ++( mycounter.current );
+
+  return mycounter.current == mycounter.range.end;
+}
+
+template< typename CounterVector, unsigned Depth >
+bool
+increment_fast_front< CounterVector, Depth >::process(CounterVector& cv)
+{
+  typedef typename boost::fusion::result_of::size< CounterVector >::type
+    mpl_integral_constant;
+  typedef typename boost::remove_reference<
+    typename boost::fusion::result_of::at_c<
+      CounterVector,
+      mpl_integral_constant::value - Depth - 1
+      >::type
+    >::type counter_type;
+  counter_type& mycounter = boost::fusion::at_c<
+    mpl_integral_constant::value - Depth - 1
+    >( cv );
+  ++( mycounter.current );
+  
+  if ( mycounter.current == mycounter.range.end )
+  {
+    mycounter.current = mycounter.range.begin;
+    return increment_fast_front< CounterVector, Depth - 1 >::process( cv );
+  }
+  else
+  {
+    return false;
+  }
+}
+
+template< typename CounterVector >
+bool
+increment_fast_front< CounterVector, 0 >::process(CounterVector& cv)
+{
+  typedef typename boost::fusion::result_of::size< CounterVector >::type
+    mpl_integral_constant;
+  typedef typename boost::remove_reference<
+    typename boost::fusion::result_of::at_c<
+      CounterVector,
+      mpl_integral_constant::value - 1
+      >::type
+    >::type counter_type;
+  counter_type& mycounter = boost::fusion::at_c<
+    mpl_integral_constant::value - 1
+    >( cv );
+  ++( mycounter.current );
+
+  return mycounter.current == mycounter.range.end;
+}
+
+template<
+  typename IteratorTypeList,
+  template< class, unsigned > class IterationOrder
+  >
+fixed_size_iterator< IteratorTypeList, IterationOrder >::fixed_size_iterator()
+  : finished_( true )
+{}
+
+template<
+  typename IteratorTypeList,
+  template< class, unsigned > class IterationOrder
+  >
+fixed_size_iterator< IteratorTypeList, IterationOrder >::fixed_size_iterator(
+  const range_vector_type& rv
+  )
+  : counter_vector_( boost::fusion::transform( rv, get_counter_for_range() ) ),
+    finished_( boost::fusion::any( counter_vector_, is_counter_empty() ) )
+{
+}
+
+template<
+  typename IteratorTypeList,
+  template< class, unsigned > class IterationOrder
+  >
+fixed_size_iterator< IteratorTypeList, IterationOrder >::~fixed_size_iterator()
+{}
+
+template<
+  typename IteratorTypeList,
+  template< class, unsigned > class IterationOrder
+  >
+typename fixed_size_iterator< IteratorTypeList, IterationOrder >::iterator_type&
+fixed_size_iterator< IteratorTypeList, IterationOrder >::operator ++()
+{
+  finished_ = order_type::process( counter_vector_ );
+}
+
+template<
+  typename IteratorTypeList,
+  template< class, unsigned > class IterationOrder
+  >
+typename fixed_size_iterator< IteratorTypeList, IterationOrder >::iterator_type
+fixed_size_iterator< IteratorTypeList, IterationOrder >::operator ++(int)
+{
+  iterator_type tmp( *this );
+  operator ++();
+  return tmp;
+}
+
+template<
+  typename IteratorTypeList,
+  template< class, unsigned > class IterationOrder
+  >
+bool
+fixed_size_iterator< IteratorTypeList, IterationOrder >::operator ==(
+  const iterator_type& other
+  ) const
+{
+  if ( finished_  || other.finished_ )
+  {
+    return finished_ == other.finished_;
+  }
+  else
+  {
+    return counter_vector_ == other.counter_vector_;
+  }
+}
+
+template<
+  typename IteratorTypeList,
+  template< class, unsigned > class IterationOrder
+  >
+bool
+fixed_size_iterator< IteratorTypeList, IterationOrder >::operator !=(
+  const iterator_type& other
+  ) const
+{
+  return !( *this == other );
+}
+
+template<
+  typename IteratorTypeList,
+  template< class, unsigned > class IterationOrder
+  >
+typename fixed_size_iterator< IteratorTypeList, IterationOrder >::value_type&
+fixed_size_iterator< IteratorTypeList, IterationOrder >::operator *()
+{
+    current_value_ = boost::fusion::transform(
+      counter_vector_,
+      get_counter_current_value()
+      );
+    
+    return current_value_;
+}
+
+template<
+  typename IteratorTypeList,
+  template< class, unsigned > class IterationOrder
+  >
+const fixed_size_iterator< IteratorTypeList, IterationOrder >
+fixed_size_iterator< IteratorTypeList, IterationOrder >::end;
+
