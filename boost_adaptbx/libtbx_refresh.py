416656e5572fbc03e359744755dac79f5b80898e
Luc J. Bourhis <luc_j_bourhis@mac.com>
2012-10-19 05:53:50 +0000

    libtbx_refresh.py should not use libtbx.env
    Each libtbx_refresh.py is like the body of a method of the class
    libtbx.env_config.module as within each libtbx_refresh.py the
    variable "self" is available at file scope that refers to the
    instance of libtbx.env_config.module that is currently running
    in libtbx/env_config.py.
    
    Thus self.env shall be used instead of libtbx.env: forgetting to
    do so may result in the wrong environment being updated.

--- boost_adaptbx/libtbx_refresh.py
+++ boost_adaptbx/libtbx_refresh.py
@@ -1,18 +1,17 @@
 from __future__ import division
-import libtbx.load_env
 import re
 
 # Find Boost version at configuration time by parsing boost/version.hpp
 try:
-  boost_root = libtbx.env.find_in_repositories('boost',
-                                               return_relocatable_path=True)
+  boost_root = self.env.find_in_repositories('boost',
+                                             return_relocatable_path=True)
   version_pat = re.compile(r'^ \s* \#define \s+ BOOST_VERSION \s+ (\d+) \s* $',
                            re.X)
-  libtbx.env.boost_version = -1
+  self.env.boost_version = -1
   with (boost_root / 'boost' / 'version.hpp').open() as lines:
     for li in lines:
       m = version_pat.search(li)
       if m:
-        libtbx.env.boost_version = int(m.group(1))
+        self.env.boost_version = int(m.group(1))
 except IOError:
   pass
