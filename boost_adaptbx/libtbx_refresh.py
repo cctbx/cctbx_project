446736eeaec056910e03df3fda2a0dd461c5b187
Luc J. Bourhis <luc_j_bourhis@mac.com>
2012-10-22 04:07:36 +0000

    Handle the case of absent boost directory.
    This should close the last bug remaining, reported by Nat on Windows
    due to the particular way out nightly tests are performed are performed
    on that platform.

--- boost_adaptbx/libtbx_refresh.py
+++ boost_adaptbx/libtbx_refresh.py
@@ -3,11 +3,11 @@ import re
 
 # Find Boost version at configuration time by parsing boost/version.hpp
 try:
+  self.env.boost_version = -1
   boost_root = self.env.find_in_repositories('boost',
                                              return_relocatable_path=True)
   version_pat = re.compile(r'^ \s* \#define \s+ BOOST_VERSION \s+ (\d+) \s* $',
                            re.X)
-  self.env.boost_version = -1
   with (boost_root / 'boost' / 'version.hpp').open() as lines:
     for li in lines:
       m = version_pat.search(li)
@@ -15,3 +15,5 @@ try:
         self.env.boost_version = int(m.group(1))
 except IOError:
   pass
+except TypeError:
+  if boost_root is None: pass
