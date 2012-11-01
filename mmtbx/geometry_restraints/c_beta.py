2e7e429c764cc5cb0cbd657b6f809fb66c346010
Jeffrey Headd <jjheadd@users.noreply.github.com>
2012-11-01 02:44:03 +0000

    fixed handling of alternate conformers for c-beta restraints

--- mmtbx/geometry_restraints/c_beta.py
+++ mmtbx/geometry_restraints/c_beta.py
@@ -1,5 +1,4 @@
 from __future__ import division
-import iotbx.pdb
 import cctbx.geometry_restraints
 from cctbx.array_family import flex
 
@@ -15,56 +14,56 @@ def get_c_beta_torsion_proxies(pdb_hierarchy,
       True).iselection()
   c_beta_dihedral_proxies = \
       cctbx.geometry_restraints.shared_dihedral_proxy()
-  get_class = iotbx.pdb.common_residue_names_get_class
   for model in pdb_hierarchy.models():
     for chain in model.chains():
-      for conformer in chain.conformers():
-        for residue in conformer.residues():
-          if(get_class(residue.resname) == "common_amino_acid"):
-            N_atom = None
-            CA_atom = None
-            C_atom = None
-            CB_atom = None
-            for atom in residue.atoms():
-              if atom.name.strip() == "N":
-                N_atom = atom
-              elif atom.name.strip() == "CA":
-                CA_atom = atom
-              elif atom.name.strip() == "C":
-                C_atom = atom
-              elif atom.name.strip() == "CB":
-                CB_atom = atom
-            if ( (N_atom is not None) and
-                 (CA_atom is not None) and
-                 (C_atom is not None) and
-                 (CB_atom is not None) ):
-              if ( (N_atom.i_seq not in selection) or
-                   (CA_atom.i_seq not in selection) or
-                   (C_atom.i_seq not in selection) or
-                   (CB_atom.i_seq not in selection) ):
-                continue
-              dihedralNCAB, dihedralCNAB = get_cb_target_angle_pair(
-                                             resname=residue.resname)
-              #NCAB
-              i_seqs = [N_atom.i_seq,
-                        C_atom.i_seq,
-                        CA_atom.i_seq,
-                        CB_atom.i_seq]
-              dp_add = cctbx.geometry_restraints.dihedral_proxy(
-                i_seqs=i_seqs,
-                angle_ideal=dihedralNCAB,
-                weight=1/sigma**2)
-              c_beta_dihedral_proxies.append(dp_add)
-              #CNAB
-              i_seqs = [C_atom.i_seq,
-                        N_atom.i_seq,
-                        CA_atom.i_seq,
-                        CB_atom.i_seq]
-              dp_add = cctbx.geometry_restraints.dihedral_proxy(
-                i_seqs=i_seqs,
-                angle_ideal=dihedralCNAB,
-                weight=1/sigma**2)
-              c_beta_dihedral_proxies.append(dp_add)
+      for rg in chain.residue_groups():
+        for conformer in rg.conformers():
+          if conformer.is_protein():
+            for residue in conformer.residues():
+              N_atom = None
+              CA_atom = None
+              C_atom = None
+              CB_atom = None
+              for atom in residue.atoms():
+                if atom.name.strip() == "N":
+                  N_atom = atom
+                elif atom.name.strip() == "CA":
+                  CA_atom = atom
+                elif atom.name.strip() == "C":
+                  C_atom = atom
+                elif atom.name.strip() == "CB":
+                  CB_atom = atom
+              if ( (N_atom is not None) and
+                   (CA_atom is not None) and
+                   (C_atom is not None) and
+                   (CB_atom is not None) ):
+                if ( (N_atom.i_seq not in selection) or
+                     (CA_atom.i_seq not in selection) or
+                     (C_atom.i_seq not in selection) or
+                     (CB_atom.i_seq not in selection) ):
+                  continue
+                dihedralNCAB, dihedralCNAB = get_cb_target_angle_pair(
+                                               resname=residue.resname)
+                #NCAB
+                i_seqs = [N_atom.i_seq,
+                          C_atom.i_seq,
+                          CA_atom.i_seq,
+                          CB_atom.i_seq]
+                dp_add = cctbx.geometry_restraints.dihedral_proxy(
+                  i_seqs=i_seqs,
+                  angle_ideal=dihedralNCAB,
+                  weight=1/sigma**2)
+                c_beta_dihedral_proxies.append(dp_add)
+                #CNAB
+                i_seqs = [C_atom.i_seq,
+                          N_atom.i_seq,
+                          CA_atom.i_seq,
+                          CB_atom.i_seq]
+                dp_add = cctbx.geometry_restraints.dihedral_proxy(
+                  i_seqs=i_seqs,
+                  angle_ideal=dihedralCNAB,
+                  weight=1/sigma**2)
+                c_beta_dihedral_proxies.append(dp_add)
   return c_beta_dihedral_proxies
 
 def target_and_gradients(
