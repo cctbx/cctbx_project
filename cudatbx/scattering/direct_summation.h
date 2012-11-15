579475aec51a25e9a063bfae5ce9a6188009ee39
Billy Poon <bkpoon@lbl.gov>
2012-11-15 07:35:14 +0000

    direct_summation: refactored code for more flexibility

--- cudatbx/scattering/direct_summation.h
+++ cudatbx/scattering/direct_summation.h
@@ -40,8 +40,48 @@ namespace scattering {
     scitbx::af::shared<std::complex<double> > get_sum();
 
   private:
+    // functions for reorganizing data
+    void reorganize_coordinates
+      (const scitbx::af::const_ref<scitbx::vec3<double> >&);
+    void copy_solvent_weights(const scitbx::af::const_ref<double>&);
+    void reorganize_h(const scitbx::af::const_ref<scitbx::vec3<double> >&);
+    void reorganize_rt(const scitbx::af::const_ref<double>&,
+                       const scitbx::af::const_ref<scitbx::vec3<double> >&);
+    void convert_scatterers(const scitbx::af::const_ref<std::string>&,
+                            const cctbx::xray::scattering_type_registry&);
+
+    // xyz parameters
+    int n_xyz;
+    int padded_n_xyz;
+    int size_xyz;
+    fType * h_xyz, * d_xyz;
+
+    // solvent weight parameters
+    fType * d_solvent;
+
+    // reciprocal space parameters
+    int n_h;
+    int padded_n_h;
+    int size_h;
+    fType * h_h, * d_h;
+
+    // rotation & translation parameters
+    int n_rt;
+    int size_rt;
+    fType * h_rt, * d_rt;
+
+    // scatterer parameters
+    int * h_scattering_type, * d_scattering_type;
+    int n_types;
+    int n_terms;
+    int f_size;
+    fType * h_a, * d_a;
+    fType * h_b, * d_b;
+    fType * h_c, * d_c;
+
+    // structure factor parameters
     int sf_size;
-    fType* sf_real, * sf_imag;
+    fType * sf_real, * sf_imag;
   };
 
   /* ==========================================================================
--- cudatbx/scattering/form_factors.cuh
+++ cudatbx/scattering/form_factors.cuh
@@ -9,23 +9,30 @@ namespace scattering {
   // form factors
   const int max_types = 50;          // number of atom types
   const int max_terms = 10;          // maximum number of terms for form factor
-  __device__ __constant__ fType d_a[max_types * max_terms];
-  __device__ __constant__ fType d_b[max_types * max_terms];
-  __device__ __constant__ fType d_c[max_types];
-  __device__ __constant__ int d_n_types;
-  __device__ __constant__ int d_n_terms;
+  __device__ __constant__ fType dc_a[max_types * max_terms];
+  __device__ __constant__ fType dc_b[max_types * max_terms];
+  __device__ __constant__ fType dc_c[max_types];
+  __device__ __constant__ int dc_n_types;
+  __device__ __constant__ int dc_n_terms;
 
   template <typename floatType>
-  __device__ floatType form_factor(const int type, const floatType stol_sq) {
+  __device__ floatType form_factor(const floatType* a, const floatType* b,
+                                   const floatType& c, const int& n_terms,
+                                   const floatType& stol_sq) {
     floatType ff = 0.0;
-    for (int term=0; term<d_n_terms; term++) {
-      ff += d_a[type*d_n_terms + term] *
-        __expf(-d_b[type*d_n_terms + term] * stol_sq);
+    for (int term=0; term<n_terms; term++) {
+      ff += a[term] * __expf(-b[term] * stol_sq);
     }
-    ff += d_c[type];
-
+    ff += c;
     return ff;
   }
+
+  template <typename floatType>
+  __device__ floatType form_factor(const int& type, const floatType& stol_sq) {
+    return form_factor(&dc_a[type*dc_n_terms],&dc_b[type*dc_n_terms],
+                       dc_c[type],dc_n_terms,stol_sq);
+  }
+
 }
 }
 #endif // FORM_FACTORS_CUH
