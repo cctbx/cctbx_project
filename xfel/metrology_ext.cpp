f79802fe614cbdd2e93a736dac8cad9317e7d0d9
Nicholas Sauter <nksauter@lbl.gov>
2012-11-18 05:45:18 +0000

    XFEL CSPAD.  More infrastructure for positional refinement.

--- xfel/metrology_ext.cpp
+++ xfel/metrology_ext.cpp
@@ -102,6 +102,75 @@ struct mark2_iteration {
     }
   }
 
+  mark2_iteration(farray values, farray tox, farray toy, farray spotcx, farray spotcy,
+                  farray spotfx, farray spotfy,
+                  iarray master_tiles,iarray frames,int const& nframes):
+    values(values),tox(tox),toy(toy),spotcx(spotcx),spotcy(spotcy),
+    master_tiles(master_tiles),
+    model_calcx(spotcx.size(),scitbx::af::init_functor_null<double>()),
+    model_calcy(spotcx.size(),scitbx::af::init_functor_null<double>()),
+    functional(0.),
+    gradients_(3*64+2*nframes),
+    curvatures_(3*64+2*nframes)
+  {
+    SCITBX_ASSERT(tox.size()==64);
+    SCITBX_ASSERT(toy.size()==64);
+    SCITBX_ASSERT(values.size()==3*64+2*nframes);
+    for (int tidx=0; tidx < 64; ++tidx){
+      cosine.push_back(std::cos(values[128+tidx]*scitbx::constants::pi_180));
+      sine.push_back(std::sin(values[128+tidx]*scitbx::constants::pi_180));
+    }
+
+    for (int ridx=0; ridx < spotcx.size(); ++ridx){
+      int itile = master_tiles[ridx];
+      int frame_param_no = frames[ridx];//Poor programming; assumes simple permutation formula
+      calc_minus_To_x = spotcx[ridx] - tox[itile];
+      calc_minus_To_y = spotcy[ridx] - toy[itile];
+
+      rotated_o_x = calc_minus_To_x * cosine[itile] - calc_minus_To_y * sine[itile];
+      rotated_o_y = calc_minus_To_x * sine[itile] +   calc_minus_To_y * cosine[itile];
+
+      model_calcx[ridx] = rotated_o_x + (tox[itile] + values[2*itile]);
+      model_calcy[ridx] = rotated_o_y + (toy[itile] + values[2*itile+1]);
+
+      partial_partial_theta_x = -calc_minus_To_x * sine[itile] - calc_minus_To_y * cosine[itile];
+      partial_partial_theta_y =  calc_minus_To_x * cosine[itile] - calc_minus_To_y * sine[itile];
+
+      partial_sq_theta_x = -calc_minus_To_x * cosine[itile] + calc_minus_To_y * sine[itile];
+      partial_sq_theta_y = -calc_minus_To_x * sine[itile] - calc_minus_To_y * cosine[itile];
+
+      double delx = model_calcx[ridx] - spotfx[ridx];
+      double dely = model_calcy[ridx] - spotfy[ridx];
+      double delrsq(delx*delx + dely*dely);
+      functional += delrsq; // sum of square differences
+
+      gradients_[2*itile]  += 2. *  delx;
+      gradients_[2*itile+1]+= 2. *  dely;
+      if (frame_param_no < nframes){
+        gradients_[192+2*frame_param_no]  += 2. *  delx;
+        gradients_[193+2*frame_param_no]  += 2. *  dely;
+      }
+
+      gradients_[128+itile] += scitbx::constants::pi_180 * 2.* (
+        delx * partial_partial_theta_x +
+        dely * partial_partial_theta_y
+      );
+
+      curvatures_[2*itile] += 2.;
+      curvatures_[2*itile+1] += 2.;
+      if (frame_param_no < nframes){
+        curvatures_[192+2*frame_param_no] += 2.;
+        curvatures_[193+2*frame_param_no] += 2.;
+      }
+
+      curvatures_[128+itile] += scitbx::constants::pi_180 * scitbx::constants::pi_180 * 2. * (
+        ( partial_partial_theta_x*partial_partial_theta_x +
+          partial_partial_theta_y*partial_partial_theta_y ) +
+        ( delx*partial_sq_theta_x + dely*partial_sq_theta_y )
+      );
+    }
+  }
+
   double f(){ return functional; }
   farray gradients(){ return gradients_; }
   farray curvatures(){ return curvatures_; }
@@ -182,6 +251,13 @@ namespace boost_python { namespace {
         (arg_("values"),arg("tox"),arg_("toy"),arg_("spotcx"),arg_("spotcy"),
          arg_("spotfx"),arg_("spotfy"),
          arg_("master_tiles"))))
+      .def(init<mark2_iteration::farray, mark2_iteration::farray, mark2_iteration::farray,
+                mark2_iteration::farray, mark2_iteration::farray,
+                mark2_iteration::farray, mark2_iteration::farray,
+                mark2_iteration::iarray, mark2_iteration::iarray, int const& >(
+        (arg_("values"),arg("tox"),arg_("toy"),arg_("spotcx"),arg_("spotcy"),
+         arg_("spotfx"),arg_("spotfy"),
+         arg_("master_tiles"), arg_("frames"), arg_("nframes"))))
       .def("f",&mark2_iteration::f)
       .def("gradients",&mark2_iteration::gradients)
       .def("curvatures",&mark2_iteration::curvatures)
