5bce52f7a09bf30f539ccd42406a2d3a441f2c13
Nader Morshed <morshed.nader@gmail.com>
2012-11-20 00:11:53 +0000

    Updated find_and_build_ions

--- mmtbx/ions/build.py
+++ mmtbx/ions/build.py
@@ -11,7 +11,26 @@ def find_and_build_ions(manager, out = sys.stdout, debug = True,
   ions = manager.analyze_waters(out = out, debug = debug,
                                 show_only_map_outliers = show_only_map_outliers,
                                 candidates = candidates)
-  print ions
+  # Build in the identified ions
+  for i_seq, final_choices in ions:
+    if len(final_choices) < 1:
+      continue
+    atom = manager.pdb_atoms[i_seq]
+    if len(final_choices) > 1:
+      # Ambiguous results
+      pass
+    else:
+      final_choice = final_choices[0]
+      # print i_seq, final_choice
+      _change_identity(atom = atom,
+                       element = final_choice.element,
+                       charge = str(final_choice.charge))
+  return ions
 
-def add_ion(atom, xray_structure, identity):
-  pass
+def _change_identity(atom, element, charge):
+  atom.element = element
+  atom.charge = charge
+  atom.fetch_labels().resname = element
+  atom.name = element
+  atom.segid = "ION"
+  # print atom.id_str()
