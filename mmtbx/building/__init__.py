e258b7a00044dca45c869bf9f90347954cb53ac7
Nathaniel Echols <nathaniel.echols@gmail.com>
2012-12-02 19:58:48 +0000

    reverting to standard Python coding style, even though I still think 'self' is annoying

--- mmtbx/building/__init__.py
+++ mmtbx/building/__init__.py
@@ -46,7 +46,7 @@ def iter_residue_groups (hierarchy) :
 #-----------------------------------------------------------------------
 # MAP-RELATED
 class local_density_quality (object) :
-  def __init__ (O,
+  def __init__ (self,
       fofc_map,
       two_fofc_map,
       atom_selection,
@@ -56,29 +56,29 @@ class local_density_quality (object) :
     from scitbx.array_family import flex
     assert ((len(fofc_map) == len(two_fofc_map)) and
             (fofc_map.focus() == two_fofc_map.focus()))
-    O.atom_selection = atom_selection # assumes no HD already
-    assert (len(O.atom_selection) > 0)
-    O.sites_cart = xray_structure.sites_cart().select(O.atom_selection)
-    O.map_sel = maptbx.grid_indices_around_sites(
+    self.atom_selection = atom_selection # assumes no HD already
+    assert (len(self.atom_selection) > 0)
+    self.sites_cart = xray_structure.sites_cart().select(self.atom_selection)
+    self.map_sel = maptbx.grid_indices_around_sites(
       unit_cell=xray_structure.unit_cell(),
       fft_n_real=fofc_map.focus(),
       fft_m_real=fofc_map.all(),
-      sites_cart=O.sites_cart,
-      site_radii=flex.double(O.sites_cart.size(), radius))
-    assert (len(O.map_sel) > 0)
-    O.fofc_map_sel = fofc_map.select(O.map_sel)
-    O.two_fofc_map_sel = two_fofc_map.select(O.map_sel)
-    O.difference_map_levels = flex.double()
-    for site_cart in O.sites_cart :
+      sites_cart=self.sites_cart,
+      site_radii=flex.double(self.sites_cart.size(), radius))
+    assert (len(self.map_sel) > 0)
+    self.fofc_map_sel = fofc_map.select(self.map_sel)
+    self.two_fofc_map_sel = two_fofc_map.select(self.map_sel)
+    self.difference_map_levels = flex.double()
+    for site_cart in self.sites_cart :
       site_frac = xray_structure.unit_cell().fractionalize(site_cart=site_cart)
       fofc_map_value = fofc_map.tricubic_interpolation(site_frac)
-      O.difference_map_levels.append(fofc_map_value)
+      self.difference_map_levels.append(fofc_map_value)
 
-  def number_of_atoms_in_difference_holes (O, sigma_cutoff=-2.5) :
-    return (O.difference_map_levels <= sigma_cutoff).count(True)
+  def number_of_atoms_in_difference_holes (self, sigma_cutoff=-2.5) :
+    return (self.difference_map_levels <= sigma_cutoff).count(True)
 
-  def fraction_of_nearby_grid_points_above_cutoff (O, sigma_cutoff=2.5) :
-    return (O.fofc_map_sel >= sigma_cutoff).count(True) / len(O.fofc_map_sel)
+  def fraction_of_nearby_grid_points_above_cutoff (self, sigma_cutoff=2.5) :
+    return (self.fofc_map_sel >= sigma_cutoff).count(True) / len(self.fofc_map_sel)
 
 def get_difference_maps (fmodel, resolution_factor=0.25) :
   fofc_coeffs = fmodel.map_coefficients(map_type="mFo-DFc",
@@ -159,7 +159,7 @@ class box_build_refine_base (object) :
   Base class for handling functions associated with rebuilding and refinement
   of atoms in a box, with the building methods implemented elsewhere.
   """
-  def __init__ (O,
+  def __init__ (self,
       pdb_hierarchy,
       xray_structure,
       processed_pdb_file,
@@ -171,85 +171,85 @@ class box_build_refine_base (object) :
       resolution_factor=0.25,
       target_map_rsr=None,
       debug=False) :
-    adopt_init_args(O, locals())
+    adopt_init_args(self, locals())
     from mmtbx.command_line import real_space_refine
     import mmtbx.restraints
     import mmtbx.utils
     from scitbx.array_family import flex
-    if (O.processed_pdb_file is None) :
-      O.processed_pdb_file = reprocess_pdb(
+    if (self.processed_pdb_file is None) :
+      self.processed_pdb_file = reprocess_pdb(
         pdb_hierarchy=pdb_hierarchy,
         cif_objects=cif_objects,
         crystal_symmetry=xray_structure,
         out=out)
-    O.iselection = selection.iselection()
+    self.iselection = selection.iselection()
     grm = real_space_refine.get_geometry_restraints_manager(
-      processed_pdb_file = O.processed_pdb_file,
-      xray_structure     = O.xray_structure)
-    O.selection_within = xray_structure.selection_within(
+      processed_pdb_file = self.processed_pdb_file,
+      xray_structure     = self.xray_structure)
+    self.selection_within = xray_structure.selection_within(
       radius    = 5,#selection_buffer_radius,
       selection = selection)
-    O.box = mmtbx.utils.extract_box_around_model_and_map(
+    self.box = mmtbx.utils.extract_box_around_model_and_map(
       xray_structure   = xray_structure,
       pdb_hierarchy    = pdb_hierarchy,
       map_data         = target_map,
       map_data_2       = target_map_rsr,
-      selection        = O.selection_within,
+      selection        = self.selection_within,
       box_cushion      = 2)
-    O.hd_selection_box = O.box.xray_structure_box.hd_selection()
-    O.n_sites_box = O.box.xray_structure_box.sites_cart().size()
-    O.target_map_box = O.box.map_box
-    O.target_map_rsr_box = O.box.map_box_2
-    if (O.target_map_rsr_box is None) :
-      O.target_map_rsr_box = O.target_map_box
-    new_unit_cell = O.box.xray_structure_box.unit_cell()
-    O.geo_box = grm.geometry.select(O.box.selection_within).discard_symmetry(
-      new_unit_cell=new_unit_cell)
-    O.reference_manager_box = \
-      O.geo_box.generic_restraints_manager.reference_manager
-    O.box_restraints_manager = mmtbx.restraints.manager(
-      geometry      = O.geo_box,
+    self.hd_selection_box = self.box.xray_structure_box.hd_selection()
+    self.n_sites_box = self.box.xray_structure_box.sites_cart().size()
+    self.target_map_box = self.box.map_box
+    self.target_map_rsr_box = self.box.map_box_2
+    if (self.target_map_rsr_box is None) :
+      self.target_map_rsr_box = self.target_map_box
+    new_unit_cell = self.box.xray_structure_box.unit_cell()
+    self.geo_box = grm.geometry.select(self.box.selection_within
+      ).discard_symmetry(new_unit_cell=new_unit_cell)
+    self.reference_manager_box = \
+      self.geo_box.generic_restraints_manager.reference_manager
+    self.box_restraints_manager = mmtbx.restraints.manager(
+      geometry      = self.geo_box,
       normalization = True)
-    O.selection_all_box = flex.bool(O.n_sites_box, True)
-    O.selection_in_box = selection.select(O.selection_within).iselection()
-    O.others_in_box = flex.bool(O.n_sites_box, True).set_selected(
-      O.selection_in_box, False).iselection()
-    assert (len(O.selection_in_box.intersection(O.others_in_box)) == 0)
-    O.box_selected_hierarchy = O.box.pdb_hierarchy_box.select(
-      O.selection_in_box)
-    O.box_selected_hierarchy.atoms().reset_i_seq()
-    sites_cart_box = O.box.xray_structure_box.sites_cart()
-    O.atom_id_mapping = {}
-    for atom in O.box_selected_hierarchy.atoms() :
-      O.atom_id_mapping[atom.id_str()] = atom.i_seq
+    self.selection_all_box = flex.bool(self.n_sites_box, True)
+    self.selection_in_box = selection.select(self.selection_within).iselection()
+    self.others_in_box = flex.bool(self.n_sites_box, True).set_selected(
+      self.selection_in_box, False).iselection()
+    assert (len(self.selection_in_box.intersection(self.others_in_box)) == 0)
+    self.box_selected_hierarchy = self.box.pdb_hierarchy_box.select(
+      self.selection_in_box)
+    self.box_selected_hierarchy.atoms().reset_i_seq()
+    sites_cart_box = self.box.xray_structure_box.sites_cart()
+    self.atom_id_mapping = {}
+    for atom in self.box_selected_hierarchy.atoms() :
+      self.atom_id_mapping[atom.id_str()] = atom.i_seq
 
-  def get_selected_sites (O, selection=None, hydrogens=True) :
+  def get_selected_sites (self, selection=None, hydrogens=True) :
     from scitbx.array_family import flex
     if (selection is None) :
-      selection = O.selection_in_box
+      selection = self.selection_in_box
     if (type(selection).__name__ == 'size_t') :
-      selection = flex.bool(O.n_sites_box, False).set_selected(selection, True)
+      selection = flex.bool(self.n_sites_box, False).set_selected(selection, True)
     if (not hydrogens) :
-      selection &= ~O.hd_selection_box
-    return O.box.xray_structure_box.sites_cart().select(selection)
+      selection &= ~self.hd_selection_box
+    return self.box.xray_structure_box.sites_cart().select(selection)
 
-  def mean_density_at_sites (O, selection=None) :
+  def mean_density_at_sites (self, selection=None) :
     """
     Compute the mean density of the target map at coordinates for non-hydrogen
     atoms.
     """
     if (selection is None) :
-      selection = O.selection_in_box
-    unit_cell = O.box.xray_structure_box.unit_cell()
+      selection = self.selection_in_box
+    unit_cell = self.box.xray_structure_box.unit_cell()
     sum_density = n_heavy = 0
-    for site in O.get_selected_sites(selection=selection, hydrogens=False) :
+    for site in self.get_selected_sites(selection=selection, hydrogens=False) :
       site_frac = unit_cell.fractionalize(site)
-      sum_density += O.target_map_box.tricubic_interpolation(site_frac)
+      sum_density += self.target_map_box.tricubic_interpolation(site_frac)
       n_heavy += 1
     assert (n_heavy > 0)
     return sum_density / n_heavy
 
-  def cc_model_map (O, selection=None, radius=1.5) :
+  def cc_model_map (self, selection=None, radius=1.5) :
     """
     Calculate the correlation coefficient for the current model (in terms of
     F(calc) from the xray structure) and the target map, calculated at atomic
@@ -259,88 +259,88 @@ class box_build_refine_base (object) :
     """
     from scitbx.array_family import flex
     if (selection is None) :
-      selection = O.selection_in_box
-    fcalc = O.box.xray_structure_box.structure_factors(d_min=O.d_min).f_calc()
-    fc_fft_map = fcalc.fft_map(resolution_factor=O.resolution_factor)
+      selection = self.selection_in_box
+    fcalc = self.box.xray_structure_box.structure_factors(d_min=self.d_min).f_calc()
+    fc_fft_map = fcalc.fft_map(resolution_factor=self.resolution_factor)
     fc_map = fc_fft_map.apply_sigma_scaling().real_map_unpadded()
-    sites_selected = O.get_selected_sites(selection, hydrogens=False)
+    sites_selected = self.get_selected_sites(selection, hydrogens=False)
     assert (len(sites_selected) > 0)
     fc_values = flex.double()
     map_values = flex.double()
-    unit_cell = O.box.xray_structure_box.unit_cell()
+    unit_cell = self.box.xray_structure_box.unit_cell()
     for site in sites_selected :
       site_frac = unit_cell.fractionalize(site)
       fc_values.append(fc_map.tricubic_interpolation(site_frac))
-      map_values.append(O.target_map_box.tricubic_interpolation(site_frac))
+      map_values.append(self.target_map_box.tricubic_interpolation(site_frac))
     return flex.linear_correlation(
       x=map_values,
       y=fc_values).coefficient()
 
-  def restrain_atoms (O, selection, reference_sigma) :
+  def restrain_atoms (self, selection, reference_sigma) :
     """
     Apply harmonic reference restraints to the selected atoms, wiping out
     any previously existing harmonic restraints.
     """
     assert (reference_sigma > 0)
-    sites_cart_box = O.box.xray_structure_box.sites_cart()
+    sites_cart_box = self.box.xray_structure_box.sites_cart()
     reference_sites = sites_cart_box.select(selection)
-    O.reference_manager_box.remove_coordinate_restraints()
-    O.reference_manager_box.add_coordinate_restraints(
+    self.reference_manager_box.remove_coordinate_restraints()
+    self.reference_manager_box.add_coordinate_restraints(
         sites_cart = reference_sites,
         selection  = selection,
         sigma      = reference_sigma)
 
-  def real_space_refine (O, selection=None) :
+  def real_space_refine (self, selection=None) :
     """
     Run simple real-space refinement, returning the optimized weight for the
     map target (wx).
     """
     import mmtbx.refinement.real_space.individual_sites
     if (selection is None) :
-      selection = O.selection_all_box
+      selection = self.selection_all_box
     rsr_simple_refiner = mmtbx.refinement.real_space.individual_sites.simple(
-      target_map                  = O.target_map_rsr_box,
+      target_map                  = self.target_map_rsr_box,
       selection                   = selection,
-      real_space_gradients_delta  = O.d_min*O.resolution_factor,
+      real_space_gradients_delta  = self.d_min*self.resolution_factor,
       max_iterations              = 150,
-      geometry_restraints_manager = O.geo_box)
+      geometry_restraints_manager = self.geo_box)
     refined = mmtbx.refinement.real_space.individual_sites.refinery(
       refiner                  = rsr_simple_refiner,
-      xray_structure           = O.box.xray_structure_box,
+      xray_structure           = self.box.xray_structure_box,
       start_trial_weight_value = 1.0,
       rms_bonds_limit          = 0.01,
       rms_angles_limit         = 1.0)
-    O.update_coordinates(refined.sites_cart_result)
-    print >> O.out, ""
-    print >> O.out, \
+    self.update_coordinates(refined.sites_cart_result)
+    print >> self.out, ""
+    print >> self.out, \
       "  after real-space refinement: rms_bonds=%.3f  rms_angles=%.3f" % \
         (refined.rms_bonds_final, refined.rms_angles_final)
     return refined.weight_final
 
-  def update_coordinates (O, sites_cart) :
+  def update_coordinates (self, sites_cart) :
     """
     Set coordinates for the boxed xray_structure and pdb_hierarchy objects.
     """
-    O.box.xray_structure_box.set_sites_cart(sites_cart)
-    O.box.pdb_hierarchy_box.atoms().set_xyz(sites_cart)
+    self.box.xray_structure_box.set_sites_cart(sites_cart)
+    self.box.pdb_hierarchy_box.atoms().set_xyz(sites_cart)
 
-  def update_original_coordinates (O) :
+  def update_original_coordinates (self) :
     """
     Copies the new coordinates from the box to the selected atoms in the
     original xray_structure object, and returns the (complete) list of
     coordinates.
     """
     sites_cart_box_refined_shifted_back = \
-      O.box.xray_structure_box.sites_cart() + \
-      O.box.shift_to_map_boxed_sites_back
+      self.box.xray_structure_box.sites_cart() + \
+      self.box.shift_to_map_boxed_sites_back
     sites_cart_refined = sites_cart_box_refined_shifted_back.select(
-      O.selection_in_box)
-    assert (len(O.iselection) == len(sites_cart_refined))
-    sites_cart_moving = O.xray_structure.sites_cart().set_selected(
-       O.iselection, sites_cart_refined)
+      self.selection_in_box)
+    assert (len(self.iselection) == len(sites_cart_refined))
+    sites_cart_moving = self.xray_structure.sites_cart().set_selected(
+       self.iselection, sites_cart_refined)
     return sites_cart_moving
 
-  def geometry_minimization (O,
+  def geometry_minimization (self,
       selection=None,
       bond=True,
       nonbonded=True,
@@ -361,9 +361,9 @@ class box_build_refine_base (object) :
     from scitbx.array_family import flex
     import scitbx.lbfgs
     if (selection is None) :
-      selection = O.selection_in_box
+      selection = self.selection_in_box
     if (type(selection).__name__ == 'size_t') :
-      selection = flex.bool(O.n_sites_box, False).set_selected(selection, True)
+      selection = flex.bool(self.n_sites_box, False).set_selected(selection, True)
     lbfgs_termination_params = scitbx.lbfgs.termination_parameters(
       max_iterations = max_number_of_iterations)
     geometry_restraints_flags = geometry_restraints.flags.flags(
@@ -374,14 +374,14 @@ class box_build_refine_base (object) :
       chirality          = chirality,
       planarity          = planarity,
       generic_restraints = generic_restraints)
-    site_labels = O.box.xray_structure_box.scatterers().extract_labels()
+    site_labels = self.box.xray_structure_box.scatterers().extract_labels()
     for i in xrange(number_of_macro_cycles):
-      sites_cart = O.box.xray_structure_box.sites_cart()
+      sites_cart = self.box.xray_structure_box.sites_cart()
       exception_handling_params = scitbx.lbfgs.exception_handling_parameters(
         ignore_line_search_failed_step_at_lower_bound = True)
       minimized = geometry_minimization.lbfgs(
         sites_cart                  = sites_cart,
-        geometry_restraints_manager = O.geo_box,
+        geometry_restraints_manager = self.geo_box,
         geometry_restraints_flags   = geometry_restraints_flags,
         lbfgs_termination_params    = lbfgs_termination_params,
         lbfgs_exception_handling_params = exception_handling_params,
@@ -389,9 +389,9 @@ class box_build_refine_base (object) :
         rmsd_bonds_termination_cutoff = rmsd_bonds_termination_cutoff,
         rmsd_angles_termination_cutoff = rmsd_angles_termination_cutoff,
         site_labels=site_labels)
-      O.update_coordinates(sites_cart)
+      self.update_coordinates(sites_cart)
 
-  def anneal (O, simulated_annealing_params=None) :
+  def anneal (self, simulated_annealing_params=None) :
     """
     Run real-space simulated annealing using the target map (not the RSR map,
     if this is different).  In practice, the non-selection atoms in the box
@@ -400,24 +400,24 @@ class box_build_refine_base (object) :
     """
     from mmtbx.dynamics import simulated_annealing
     import mmtbx.utils
-    wx = O.real_space_refine(selection=O.selection_all_box)
-    if (O.debug) :
-      O.box.write_pdb_file("box_start.pdb")
+    wx = self.real_space_refine(selection=self.selection_all_box)
+    if (self.debug) :
+      self.box.write_pdb_file("box_start.pdb")
     states_collector = None
-    if (O.debug) :
+    if (self.debug) :
       states_collector = mmtbx.utils.states(
-        xray_structure=O.box.xray_structure_box,
-        pdb_hierarchy=O.box.pdb_hierarchy_box)
+        xray_structure=self.box.xray_structure_box,
+        pdb_hierarchy=self.box.pdb_hierarchy_box)
     simulated_annealing.run(
       params             = simulated_annealing_params,
       fmodel             = None,
-      xray_structure     = O.box.xray_structure_box,
+      xray_structure     = self.box.xray_structure_box,
       real_space         = True,
-      target_map         = O.target_map_box,
-      restraints_manager = O.box_restraints_manager,
+      target_map         = self.target_map_box,
+      restraints_manager = self.box_restraints_manager,
       wx                 = wx,
       wc                 = 1.0,
-      log                = O.out,
+      log                = self.out,
       verbose            = True,
       states_collector   = states_collector)
     if (states_collector is not None) :
@@ -447,7 +447,7 @@ def run_real_space_annealing (
   import iotbx.phil
   if (params is None) :
     params = iotbx.phil.parse(simulated_annealing.master_params_str).extract()
-  O = box_build_refine_base(
+  box = box_build_refine_base(
     xray_structure=xray_structure,
     pdb_hierarchy=pdb_hierarchy,
     selection=selection,
@@ -460,10 +460,10 @@ def run_real_space_annealing (
     out=out,
     debug=debug)
   if (reference_sigma is not None) :
-    O.restrain_atoms(
-      selection=O.others_in_box,
+    box.restrain_atoms(
+      selection=box.others_in_box,
       reference_sigma=reference_sigma)
-  O.anneal(simulated_annealing_params=params)
+  box.anneal(simulated_annealing_params=params)
   if (rsr_after_anneal) :
-    O.real_space_refine(selection=O.selection_in_box)
-  return O.update_original_coordinates()
+    box.real_space_refine(selection=box.selection_in_box)
+  return box.update_original_coordinates()
