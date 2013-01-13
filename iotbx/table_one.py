a67a0f0fcb8583031c58b9d2a5f11f311144456d
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-01-13 00:20:06 +0000

    more refactoring, added CIF tags

--- iotbx/table_one.py
+++ iotbx/table_one.py
@@ -9,6 +9,7 @@ to extract statistics for display.
 
 from __future__ import division
 from libtbx import slots_getstate_setstate
+from libtbx.utils import Sorry
 from libtbx import str_utils
 import re
 
@@ -18,7 +19,8 @@ angstrom = u"\u00C5".encode("utf-8", "strict").strip()
 # the template suggested by NSMB plus more, but not clear what other journals
 # expect.  the VTF paper does not list any explicit Table 1 requirements, but
 # the forthcoming PDB validation reports may change expectations.
-# TODO fill in CIF tags
+# TODO fill in missing CIF tags - some of these may not have standard
+# tag names yet
 keywords = [
   # (attr, label, format,  cif_tag)
   # data-only statistics
@@ -26,19 +28,19 @@ keywords = [
   ("d_max_min", "Resolution range (%s)" % angstrom, "%.3g - %.3g", None),
   ("space_group", "Space group", "%s", None),
   ("unit_cell", "Unit cell", "%g %g %g %g %g %g", None),
-  ("n_refl_all", "Total reflections", "%d", None),
-  ("n_refl", "Unique reflections", "%d", None),
-  ("multiplicity", "Multiplicity", "%.1f", None),
-  ("completeness", "Completeness (%)", "%.2f", None),
-  ("i_over_sigma", "Mean I/sigma(I)", "%.2f", None),
+  ("n_refl_all", "Total reflections", "%d", "_reflns.pdbx_number_measured_all"),
+  ("n_refl", "Unique reflections", "%d", "_reflns.number_obs"),
+  ("multiplicity", "Multiplicity", "%.1f", "_reflns.pdbx_redundancy"),
+  ("completeness", "Completeness (%)", "%.2f", "_reflns.percent_possible_obs"),
+  ("i_over_sigma", "Mean I/sigma(I)", "%.2f", "_reflns.pdbx_netI_over_sigmaI"),
   ("wilson_b", "Wilson B-factor", "%.2f", None),
-  ("r_sym", "R-merge", "%.4g", None),
-  ("r_meas", "R-meas", "%.4g", None),
-  ("cc_one_half", "CC1/2", "%.3g", None),
-  ("cc_star", "CC*", "%.3g", None),
+  ("r_sym", "R-merge", "%.4g", "_reflns.pdbx_Rmerge_I_obs"),
+  ("r_meas", "R-meas", "%.4g", "_reflns.pdbx_Rrim_I_obs"),
+  ("cc_one_half", "CC1/2", "%.3g", "_reflns.phenix_cc_1/2"),
+  ("cc_star", "CC*", "%.3g", "_reflns.phenix_cc_star"),
   # model-based statistics
-  ("r_work", "R-work", "%.4f", None),
-  ("r_free", "R-free", "%.4f", None),
+  ("r_work", "R-work", "%.4f", "_refine.ls_R_factor_R_work"),
+  ("r_free", "R-free", "%.4f", "_refine.ls_R_factor_R_free"),
   ("cc_work", "CC(work)", "%.3f", None),
   ("cc_free", "CC(free)", "%.3f", None),
   ("n_atoms", "Number of atoms", "%d", None),
@@ -55,29 +57,10 @@ keywords = [
   ("adp_mean_mm", "  macromolecules", "%.2f", None),
   ("adp_mean_lig", "  ligands", "%.2f", None),
   ("adp_mean_wat", "  solvent", "%.2f", None),
-  ("solvent_content", "Solvent content", "%.1f%%", None),
+#  ("solvent_content", "Solvent content", "%.1f%%", None),
 ]
 
 keyword_formats = dict([ (kw, fs) for (kw, label, fs, cif_tag) in keywords ])
-
-# XXX is this superfluous?
-def format_value (fs, value) :
-  try :
-    val_str = str_utils.format_value(fs, value, replace_none_with="").strip()
-  except Exception, e :
-    raise RuntimeError("Formatting error: %s, %s" % (fs, value))
-  else :
-    return val_str
-
-def format_d_max_min (d_max_min) :
-  """Format a resolution range (e.g. '30 - 2.56')"""
-  if (d_max_min is None) :
-    return ""
-  else :
-    (d_max, d_min) = d_max_min
-    d_max_str = "%.4g " % d_max
-    d_min_str = re.sub("\.$", ".0", re.sub("0*$", "", "%.3f" % d_min))
-    return "%s - %s" % (d_max_str, d_min_str)
 
 class column (slots_getstate_setstate) :
   """
@@ -124,3 +107,132 @@ class column (slots_getstate_setstate) :
       if (value is not None) :
         lines.append("%s %s" % (stat_name, value))
     return "\n".join(lines)
+
+class table (slots_getstate_setstate) :
+  """
+  Combined table of statistics for one or more structures published together.
+  """
+  __slots__ = ["text_field_separation", "columns"]
+
+  def __init__ (self, text_field_separation=4) :
+    self.text_field_separation = text_field_separation
+    self.columns = []
+
+  def add_column (self, col) :
+    self.columns.append(col)
+
+  def format_as_txt (self) :
+    rows = []
+    rows.append([""] + [ column.label for column in self.columns ])
+    for (stat_name, label, fstring, cif_tag) in keywords :
+      row = [label]
+      for column in self.columns :
+        row.append(column.format_stat(stat_name))
+      rows.append(row)
+    n_rows = len(keywords) + 1
+    n_cols = len(self.columns) + 1
+    columns = [ [ row[i] for row in rows ] for i in range(n_cols) ]
+    columns = [ resize_column(col, "right") for col in columns ]
+    table = [ [ col[j] for col in columns ] for j in range(n_rows) ]
+    sep = " " * self.text_field_separation
+    out = "\n".join([ sep.join(row) for row in table ])
+    return out
+
+  def format_as_csv (self) :
+    rows = []
+    rows.append([""] + [ column.label for column in self.columns ])
+    for (stat_name, label, fstring, cif_tag) in keywords :
+      row = [label]
+      for column in self.columns :
+        row.append(column.format_stat(stat_name))
+      rows.append(row)
+    return "\n".join([ ",".join(row) for row in rows ])
+
+  def format_as_rtf (self) :
+    try :
+      import PyRTF
+    except ImportError :
+      raise Sorry("The PyRTF module is not available.")
+    doc = PyRTF.Document()
+    ss = doc.StyleSheet
+    section = PyRTF.Section()
+    doc.Sections.append(section)
+    p = PyRTF.Paragraph( ss.ParagraphStyles.Heading2 )
+    p.append("Table 1.  Data collection and refinement statistics.")
+    section.append(p)
+    n_cols = len(self.columns) + 1
+    col_widths = [ PyRTF.TabPS.DEFAULT_WIDTH * 3 ] * n_cols
+    table = PyRTF.Table(*col_widths)
+    header = [ PyRTF.Cell(PyRTF.Paragraph("")) ]
+    for column in self.columns :
+      column_label = PyRTF.Paragraph(ss.ParagraphStyles.Heading2, column.label)
+      header.append(PyRTF.Cell(column_label))
+    table.AddRow(*header)
+    for (stat_name, label, fstring, cif_tag) in keywords :
+      row = [ PyRTF.Cell(PyRTF.Paragraph(ss.ParagraphStyles.Heading2, label)) ]
+      for column in self.columns :
+        txt = column.format_stat(stat_name)
+        p = PyRTF.Paragraph(ss.ParagraphStyles.Normal,
+          PyRTF.ParagraphPS(alignment=2))
+        p.append(txt)
+        row.append(PyRTF.Cell(p))
+      table.AddRow(*row)
+    section.append(table)
+    p = PyRTF.Paragraph(ss.ParagraphStyles.Normal)
+    p.append("Statistics for the highest-resolution shell are shown in "+
+      "parentheses.")
+    section.append(p)
+    return doc
+
+  def save_txt (self, file_name) :
+    f = open(file_name, "w")
+    out = self.format_as_txt()
+    f.write(out)
+    f.close()
+
+  def save_csv (self, file_name) :
+    f = open(file_name, "w")
+    out = self.format_as_csv()
+    f.write(out)
+    f.close()
+
+  def save_rtf (self, file_name) :
+    import PyRTF
+    DR = PyRTF.Renderer()
+    doc = self.format_as_rtf()
+    f = open(file_name, "w")
+    DR.Write(doc, f)
+    f.close()
+
+#-----------------------------------------------------------------------
+# UTILITY FUNCTIONS
+
+# XXX is this superfluous?
+def format_value (fs, value) :
+  try :
+    val_str = str_utils.format_value(fs, value, replace_none_with="").strip()
+  except Exception, e :
+    raise RuntimeError("Formatting error: %s, %s" % (fs, value))
+  else :
+    return val_str
+
+def format_d_max_min (d_max_min) :
+  """Format a resolution range (e.g. '30 - 2.56')"""
+  if (d_max_min is None) :
+    return ""
+  else :
+    (d_max, d_min) = d_max_min
+    d_max_str = "%.4g " % d_max
+    d_min_str = re.sub("\.$", ".0", re.sub("0*$", "", "%.3f" % d_min))
+    return "%s - %s" % (d_max_str, d_min_str)
+
+def resize_column (cell_values, alignment="right") :
+  max_width = max([ len(cell) for cell in cell_values ])
+  if (alignment == "right") :
+    fs = "%%%ds" % max_width
+    return [ fs % cell for cell in cell_values ]
+  elif (alignment == "left") :
+    fs = "%%%-ds" % max_width
+    return [ fs % cell for cell in cell_values ]
+  else :
+    raise RuntimeError("Alignemnt '%s' not supported." % alignment)
