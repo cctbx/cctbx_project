d95734e84462b2a4928c4f3ef0e244c2ae1a745d
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-01-21 22:21:31 +0000

    handle smouse wheel weirdness on Mac (and possible GTK, TBD)

--- wxtbx/__init__.py
+++ wxtbx/__init__.py
@@ -1,1 +1,13 @@
 from __future__ import division
+
+class MouseWheelTransparencyMixin (object) :
+  """
+  This mixin provides an event handler for passing the mouse wheel event to
+  the parent, presumably a ScrolledPanel or similar.  For this to happen, the
+  actual class must bind wx.EVT_MOUSEWHEEL to self.OnMouseWheel.
+  """
+  def OnMouseWheel (self, evt) :
+    parent = self.GetParent()
+    evt.SetId(parent.GetId())
+    evt.SetEventObject(parent)
+    parent.GetEventHandler().ProcessEvent(evt)
