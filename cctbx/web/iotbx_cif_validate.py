f825fc4e1d220df9b80c82d56daa6173aa503ea1
Richard Gildea <richard.gildea@diamond.ac.uk>
2013-02-01 18:59:16 +0000

    clean clutter

--- cctbx/web/iotbx_cif_validate.py
+++ cctbx/web/iotbx_cif_validate.py
@@ -1,15 +1,9 @@
 from __future__ import division
-from cctbx import euclidean_model_matching as emma
 from cctbx import crystal
-from cctbx import sgtbx
-from cctbx import uctbx
-from cctbx.web import io_utils
 from cctbx.web import cgi_utils
 import iotbx.cif
 from iotbx.cif import validation
 import iotbx.pdb.mmcif
-from itertools import count
-import sys
 
 def interpret_form_data(form):
   inp = cgi_utils.inp_from_form(form,
