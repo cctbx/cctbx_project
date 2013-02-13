d3789f448c688ffec4c55ce4f8fdec9d3193fcd9
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-02-13 22:32:12 +0000

    improved tools for querying the RCSB

---
+++ mmtbx/wwpdb/tst_rcsb_web_services.py
@@ -0,0 +1,18 @@
+
+# XXX depends on internet connectivity, so not run as part of standard tests
+
+from __future__ import division
+
+def exercise () :
+  from mmtbx.wwpdb import rcsb_web_services
+  lysozyme = """KVFGRCELAAAMKRHGLDNYRGYSLGNWVCAAKFESNFNTQATNRNTDGSTDYGILQINSRWWCNDGRTPGSRNLCNIPCSALLSSDITASVNCAKKIVSDGNGMNAWVAWRNRCKGTDVQAWIRGCRL"""
+  homologs = rcsb_web_services.sequence_search(lysozyme, d_max=2.0)
+  assert (len(homologs) > 500)
+  atp_binding = rcsb_web_services.chemical_id_search("ATP", protein_only=True)
+  assert (len(atp_binding) > 650)
+  report = rcsb_web_services.get_high_resolution_for_structures(atp_binding)
+  assert (len(report) == len(atp_binding)) and (len(report[0]) == 2)
+  print "OK"
+
+if (__name__ == "__main__") :
+  exercise()
