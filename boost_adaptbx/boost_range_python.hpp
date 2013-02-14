2a4d60e229a461136c4f069c6b98831a95c162fc
Gabor Bunkoczi <gbunkoczi@users.noreply.github.com>
2013-02-14 16:16:39 +0000

    Python export for boost::range

---
+++ boost_adaptbx/boost_range_python.hpp
@@ -0,0 +1,36 @@
+#ifndef BOOST_ADAPTBX_BOOST_RANGE_PYTHON_H
+#define BOOST_ADAPTBX_BOOST_RANGE_PYTHON_H
+
+#include <boost/range.hpp>
+
+#include <boost/python/class.hpp>
+#include <boost/python/iterator.hpp>
+
+namespace boost_adaptbx
+{
+
+namespace python
+{
+
+template< typename Range >
+struct generic_range_wrapper
+{
+  static void wrap(const char* name)
+  {
+    using namespace boost::python;
+
+    class_< Range >( name, no_init )
+      .def(
+        "__iter__",
+        boost::python::iterator< Range >()
+        )
+      .def( "__len__", boost::distance< Range > )
+      ;
+  }
+};
+
+} // namespace python
+} // namespace boost_adaptbx
+
+#endif
+
