c50474a927d9c7ae094300edc2e8d61da6238b34
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-02-20 20:38:32 +0000

    added support for wxPython on Linux (NOT YET TESTED)

--- auto_build/install_cctbx_base.py
+++ auto_build/install_cctbx_base.py
@@ -11,9 +11,21 @@ from __future__ import division
 from cctbx_installer_utils import *
 from package_defs import *
 from optparse import OptionParser
+import warnings
 import urllib2
+import time
 import os
 import sys
+
+# XXX CCTBX itself requires at least Python 2.5, but this script is intended
+# to bootstrap an installation on older systems as well
+if (not sys.version_info >= (2,3)) :
+  raise Exception("Python version 2.3 or greater required to run this script.")
+elif (sys.version_info < (2,4)) : # subprocess module not available
+  warnings.warn("You are running an obsolete version of Python; this script "+
+    "should still run, but not all functionality is available.",
+    DeprecationWarning)
+  time.sleep(2)
 
 class installer (object) :
   # XXX various defaults go here, to be overridden in subclasses
@@ -30,20 +42,21 @@ class installer (object) :
                 report problems to cctbx-dev@cci.lbl.gov
   ****************************************************************************
 """
+    dist_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
     parser = OptionParser()
     parser.add_option("--build_dir", dest="build_dir", action="store",
       help="Build directory", default=os.path.join(os.getcwd(), "cctbx_build"))
     parser.add_option("--src_dir", dest="src_dir", action="store",
       help="Source directory",
-      default=os.path.join(os.getcwd(), "cctbx_sources"))
+      default=dist_dir)
     parser.add_option("--tmp_dir", dest="tmp_dir", action="store",
       help="Temporary directory",
       default=os.path.join(os.getcwd(), "build_tmp"))
-    parser.add_option("--nproc", dest="nproc", action="store", type="int",
-      help="Number of processors", default=1)
-    parser.add_option("--verbose", dest="verbose", action="store_true",
+    parser.add_option("-p", "--nproc", dest="nproc", action="store",
+      type="int", help="Number of processors", default=1)
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
       help="Verbose output", default=False)
-    parser.add_option("--pkg_dir", dest="pkg_dir", action="store",
+    parser.add_option("--pkg_dir", dest="pkg_dirs", action="append",
       help="Directory with source packages", default=None)
     parser.add_option("--basic", dest="basic", action="store_true",
       help="Only install basic prerequisites", default=False)
@@ -56,20 +69,20 @@ class installer (object) :
       help="Build GUI dependencies", default=self.build_gui_dependencies)
     #parser.add_option("--use_system_python", dest="use_system_python",
     #  action="store_true", default=False)
-    parser.add_option("--all", dest="build_all", action="store_true",
+    parser.add_option("-a", "--all", dest="build_all", action="store_true",
       help="Build all recommended dependencies", default=False)
     parser.add_option("--scipy", dest="build_scipy", action="store_true",
       help="Build SciPy (requires Fortran compiler)", default=False)
-    parser.add_option("--debug", dest="debug", action="store_true",
+    parser.add_option("-g", "--debug", dest="debug", action="store_true",
       help="Build in debugging mode", default=False)
     parser.add_option("--no-download", dest="no_download", action="store_true",
       help="Use only local packages (no downloads)", default=False)
     options, args = parser.parse_args(args)
     # basic setup
     self.tmp_dir = options.tmp_dir
-    self.src_dir = options.src_dir
+    #self.src_dir = options.src_dir
     self.build_dir = options.build_dir
-    self.pkg_dir = options.pkg_dir
+    self.pkg_dirs = options.pkg_dirs
     self.base_dir = os.path.join(self.build_dir, "base")
     print >> log, "Setting up directories..."
     for dir_name in [self.tmp_dir,self.src_dir,self.build_dir,self.base_dir] :
@@ -103,26 +116,26 @@ class installer (object) :
       self.build_python_module_simple(
         pkg_url=BASE_CCI_PKG_URL,
         pkg_name=NUMPY_PKG,
-        pkg_name_simple="numpy",
+        pkg_name_label="numpy",
         confirm_import_module="numpy")
       self.build_python_module_simple(
         pkg_url=BASE_CCI_PKG_URL,
         pkg_name=BIOPYTHON_PKG,
-        pkg_name_simple="BioPython",
+        pkg_name_label="BioPython",
         confirm_import_module="Bio")
       if (options.build_scipy) :
         # XXX SciPy requires a Fortran compiler, so it's not built by default
         self.build_python_module_simple(
           pkg_url=BASE_CCI_PKG_URL,
           pkg_name=SCIPY_PKG,
-          pkg_name_simple="SciPy",
+          pkg_name_label="SciPy",
           confirm_import_module="scipy")
     if (options.labelit) or (options.build_gui) or (options.build_all) :
       self.build_imaging()
       self.build_python_module_simple(
         pkg_url=BASE_CCI_PKG_URL,
         pkg_name=REPORTLAB_PKG,
-        pkg_name_simple="reportlab",
+        pkg_name_label="reportlab",
         confirm_import_module="reportlab")
     if (options.xia2) or (options.build_all) :
       self.build_hdf5()
@@ -136,10 +149,21 @@ class installer (object) :
     if (log is None) : log = self.log
     return call(args, log=log)
 
+  def configure_and_build (self, config_args=(), log=None, make_args=()) :
+    self.call("./configure %s" % " ".join(list(config_args)), log=log)
+    self.call("make -j %d %s" % (self.nproc, " ".join(list(make_args))), log=log)
+    self.call("make install", log=log)
+
   def chdir (self, dir_name, log=None) :
     if (log is None) : log = self.log
     print >> log, "cd \"%s\"" % dir_name
     os.chdir(dir_name)
+
+  def touch_file (self, file_name) :
+    f = open(file_name, "w")
+    f.write("")
+    f.close()
+    assert os.path.isfile(file_name)
 
   def print_sep (self, char="-") :
     print >> self.log, ""
@@ -147,6 +171,7 @@ class installer (object) :
     print >> self.log, ""
 
   def start_building_package (self, pkg_name) :
+    os.chdir(self.tmp_dir)
     install_log = os.path.join(self.tmp_dir, pkg_name + "_install_log")
     print >> self.log, "Installing %s..." % pkg_name
     print >> self.log, "  log file is %s" % install_log
@@ -161,14 +186,17 @@ class installer (object) :
     src_in.close()
     src_out.close()
 
-  def fetch_package (self, pkg_url, pkg_name) :
+  def fetch_package (self, pkg_name, pkg_url=None) :
+    if (pkg_url is None) :
+      pkg_url = BASE_CCI_PKG_URL
     os.chdir(self.tmp_dir)
     print >> self.log, "  getting package %s..." % pkg_name
-    if (self.pkg_dir is not None) :
-      static_file = os.path.join(self.pkg_dir, pkg_name)
-      if (os.path.exists(static_file)) :
-        print >> self.log, "    using %s" % static_file
-        return static_file
+    if (len(self.pkg_dirs) > 0) :
+      for pkg_dir in self.pkg_dirs :
+        static_file = os.path.join(pkg_dir, pkg_name)
+        if (os.path.exists(static_file)) :
+          print >> self.log, "    using %s" % static_file
+          return static_file
     if (os.path.exists(pkg_name)) :
       print >> self.log, "    using ./%s" % pkg_name
       return os.path.join(self.tmp_dir, pkg_name)
@@ -187,12 +215,16 @@ class installer (object) :
       f.close()
       return os.path.join(self.tmp_dir, pkg_name)
 
+  def fetch_untar_and_chdir (self, pkg_name, pkg_url=None, log=None) :
+    if (log is None) : log = self.log
+    pkg = self.fetch_package(pkg_name=pkg_name, pkg_url=pkg_url)
+    pkg_dir = untar(pkg, log=log)
+    os.chdir(pkg_dir)
+
   def build_python (self) :
     log = self.start_building_package("Python")
     os.chdir(self.tmp_dir)
-    python_tarball = self.fetch_package(
-      pkg_url=BASE_CCI_PKG_URL,
-      pkg_name=PYTHON_PKG)
+    python_tarball = self.fetch_package(PYTHON_PKG)
     python_dir = untar(python_tarball)
     self.chdir(python_dir, log=log)
     if (sys.platform == "darwin") :
@@ -212,9 +244,7 @@ class installer (object) :
         self.base_dir, log=log)
     else :
       configure_args = ["--prefix=\"%s\"" % self.base_dir,]
-      self.call("./configure %s" % " ".join(configure_args), log=log)
-      self.call("make -j %d" % self.nproc, log=log)
-      self.call("make install", log=log)
+      self.configure_and_build(config_args=configure_args, log=log)
     log.close()
     self.python_exe = os.path.abspath(
       os.path.join(self.base_dir, "bin", "python"))
@@ -252,24 +282,22 @@ class installer (object) :
     os.environ['CPPFLAGS'] = self.cppflags_start
     os.environ['LDFLAGS'] = self.ldflags_start
 
-  def verify_python_module (self, pkg_name_simple, module_name) :
+  def verify_python_module (self, pkg_name_label, module_name) :
     os.chdir(self.tmp_dir) # very important for import to work!
-    self.log.write("  verifying %s installation..." % pkg_name_simple)
+    self.log.write("  verifying %s installation..." % pkg_name_label)
     self.call("%s -c 'import %s'" % (self.python_exe, module_name))
     print >> self.log, " OK"
 
   def build_python_module_simple (self,
       pkg_url,
       pkg_name,
-      pkg_name_simple,
+      pkg_name_label,
       callback_before_build=None,
       callback_after_build=None,
       confirm_import_module=None) :
-    os.chdir(self.tmp_dir)
-    pkg_log = self.start_building_package(pkg_name_simple)
-    pkg = self.fetch_package(pkg_url, pkg_name)
-    pkg_dir = untar(pkg, log=pkg_log)
-    os.chdir(pkg_dir)
+    pkg_log = self.start_building_package(pkg_name_label)
+    pkg = self.fetch_untar_and_chdir(pkg_name=pkg_name, pkg_url=pkg_url,
+      log=pkg_log)
     if (callback_before_build is not None) :
       assert callback_before_build(pkg_log), pkg_name
     debug_flag = ""
@@ -282,7 +310,7 @@ class installer (object) :
       assert callback_after_build(pkg_log), pkg_name
     os.chdir(self.tmp_dir)
     if (confirm_import_module is not None) :
-      self.verify_python_module(pkg_name_simple, confirm_import_module)
+      self.verify_python_module(pkg_name_label, confirm_import_module)
     self.print_sep()
 
   def build_imaging (self, patch_src=True) :
@@ -298,38 +326,34 @@ class installer (object) :
     self.build_python_module_simple(
       pkg_url=BASE_CCI_PKG_URL,
       pkg_name=IMAGING_PKG,
-      pkg_name_simple="Imaging",
+      pkg_name_label="Imaging",
       callback_before_build=patch_imaging_src,
       confirm_import_module="Image")
 
   def build_compiled_package_simple (self,
-      pkg_url,
       pkg_name,
-      pkg_name_simple) :
-    os.chdir(self.tmp_dir)
-    pkg_log = self.start_building_package(pkg_name_simple)
-    pkg = self.fetch_package(pkg_url, pkg_name)
-    pkg_dir = untar(pkg, log=pkg_log)
-    os.chdir(pkg_dir)
-    self.call("./configure --prefix=\"%s\"" % self.base_dir, log=pkg_log)
-    self.call("make -j %d" % self.nproc, log=pkg_log)
-    self.call("make install", log=pkg_log)
+      pkg_name_label,
+      pkg_url=None) :
+    pkg_log = self.start_building_package(pkg_name_label)
+    pkg = self.fetch_untar_and_chdir(pkg_name=pkg_name, pkg_url=pkg_url,
+      log=pkg_log)
+    self.configure_and_build(
+      config_args=["--prefix=\"%s\"" % self.base_dir,],
+      log=pkg_log)
     self.print_sep()
 
   def build_hdf5 (self) :
-    os.chdir(self.tmp_dir)
     pkg_log = self.start_building_package("HDF5")
-    pkg = self.fetch_package(BASE_XIA_PKG_URL, HDF5_PKG)
-    pkg_dir = untar(pkg, log=pkg_log)
-    os.chdir(pkg_dir)
+    self.fetch_untar_and_chdir(pkg_name=HDF5_PKG, pkg_url=BASE_XIA_PKG_URL,
+      log=pkg_log)
     print >> pkg_log, "Building base HDF5 library..."
-    self.call("./configure --prefix=\"%s\"" % self.base_dir, log=pkg_log)
-    self.call("make -j %d" % self.nproc, log=pkg_log)
-    self.call("make install", log=pkg_log)
+    self.configure_and_build(
+      config_args=["--prefix=\"%s\"" % self.base_dir,],
+      log=pkg_log)
     print >> pkg_log, "Building h5py..."
-    pkg = self.fetch_package(BASE_XIA_PKG_URL, H5PY_PKG)
-    pkg_dir = untar(pkg, log=pkg_log)
-    os.chdir(pkg_dir)
+    os.chdir(self.tmp_dir)
+    self.fetch_untar_and_chdir(pkg_url=BASE_XIA_PKG_URL, pkg_name=H5PY_PKG,
+      log=pkg_log)
     self.call("%s setup.py build --hdf5=\"%s\"" % (self.python_exe,
       self.base_dir), log=pkg_log)
     self.call("%s setup.py install" % (self.python_exe), log=pkg_log)
@@ -341,16 +365,138 @@ class installer (object) :
     self.build_compiled_package_simple(
       pkg_url=BASE_CCI_PKG_URL,
       pkg_name=FREETYPE_PKG,
-      pkg_name_simple="Freetype")
+      pkg_name_label="Freetype")
     self.include_dirs.append(os.path.join(self.base_dir, "include",
       "freetype2"))
+    self.build_compiled_package_simple(
+      pkg_url=BASE_CCI_PKG_URL,
+      pkg_name=LIBPNG_PKG,
+      pkg_name_label="libpng")
     if (sys.platform == "darwin") :
-      pass
+      # XXX nuke libpng dylibs - I forget why we need to do this
+      lib_dir = os.path.join(self.base_dir, "lib")
+      for file_name in os.listdir(lib_dir) :
+        if ("png" in file_name.lower()) and (file_name.endswith(".dylib")) :
+          full_path = os.path.join(lib_dir, file_name)
+          os.remove(full_path)
     else :
-      pass # TODO add roughly a dozen more libraries here...
+      self.build_wxpython_dependencies_linux()
+
+  def build_wxpython_dependencies_linux (self) :
+    pkg_config_dir = os.path.join(self.base_dir, "lib", "pkgconfig")
+    if (not os.path.isdir(pkg_config_dir)) :
+      os.makedirs(pkg_config_dir)
+    pkg_config_paths = [ pkg_config_dir ] + os.environ.get("PKG_CONFIG_PATH", [])
+    os.environ['PKG_CONFIG_PATH'] = ":".join(pkg_config_paths)
+    prefix_arg = "--prefix=\"%s\"" % self.base_dir
+    # gettext
+    pkg_log = self.start_building_package("gettext")
+    self.fetch_untar_and_chdir(pkg_name=GETTEXT_PKG, log=pkg_log)
+    os.chdir("gettext-runtime")
+    self.set_cppflags_ldflags_tmp()
+    gettext_conf_args = [ prefix_arg, "--disable-java", ]
+    self.configure_and_build(config_args=gettext_conf_args, log=pkg_log)
+    self.print_sep()
+    # glib
+    glib_log = self.start_building_package("glib")
+    self.fetch_untar_and_chdir(pkg_name=GLIB_PKG, log=glib_log)
+    msgfmt_bin = os.path.join(self.base_dir, "bin", "msgfmt")
+    gettext_bin = os.path.join(self.base_dir, "bin", "xgettext")
+    def touch_bin_files () :
+      self.touch_file(msgfmt_bin)
+      self.touch_file(gettext_bin)
+      self.call("chmod 744 \"%s\"" % msgfmt_bin)
+      self.call("chmod 744 \"%s\"" % gettext_bin)
+    touch_bin_files()
+    make_args = []
+    if (self.options.debug) : make_args.append("CFLAGS=-g")
+    self.configure_and_build(
+      config_args=[prefix_arg],
+      log=glib_log,
+      make_args=make_args)
+    os.remove(msgfmt_bin)
+    os.remove(gettext_bin)
+    self.print_sep()
+    # expat
+    expat_log = self.start_building_package("expat")
+    self.fetch_untar_and_chdir(pkg_name=EXPAT_PKG, log=expat_log)
+    self.configure_and_build(config_args=[prefix_arg], log=expat_log)
+    header_files = ["./lib/expat_external.h", "./lib/expat.h"]
+    for header in header_files :
+      self.call("./conftools/install-sh -c -m 644 .%s \"%s\"" % (header,
+        os.path.join(self.base_dir, "include")), log=expat_log)
+    # fontconfig
+    fc_log = self.start_building_package("fontconfig")
+    self.fetch_untar_and_chdir(pkg_name=FONTCONFIG_PKG, log=fc_log)
+    if (not os.path.isdir(os.path.join(self.base_dir, "share", "fonts"))) :
+      os.makedirs(os.path.join(self.base_dir, "share", "fonts"))
+    if (not os.path.isdir(os.path.join(self.base_dir,"etc","fonts"))) :
+      os.makedirs(os.path.join(self.base_dir,"etc","fonts"))
+    fc_config_args = [ prefix_arg,
+      "--disable-docs"
+      "--with-expat-includes=\"%s\"" % os.path.join(self.base_dir, "include"),
+      "--with-expat-lib=\"%s\"" % os.path.join(self.base_dir, "lib"),
+      "--with-add-fonts=\"%s\"" % os.path.join(self.base_dir,"share","fonts"),
+      "--with-confdir=\"%s\"" % os.path.join(self.base_dir,"etc","fonts"),
+      "--with-docdir=\"%s\"" % os.path.join(self.base_dir, "doc"),
+      "--with-freetype-config=freetype-config", ]
+    self.configure_and_build(config_args=fc_config_args, log=fc_log)
+    self.print_sep()
+    # render, xrender, xft
+    for pkg, name in zip([RENDER_PKG,XRENDER_PKG,XFT_PKG],
+                         ["render","Xrender","Xft"]) :
+      self.build_compiled_package_simple(pkg_name=pkg, pkg_name_label=name)
+    # pixman
+    pix_log = self.start_building_package("pixman")
+    self.fetch_untar_and_chdir(pkg_name=PIXMAN_PKG, log=pix_log)
+    self.configure_and_build(
+      config_args=[ prefix_arg, "--disable-gtk" ],
+      log=pix_log,
+      make_args=make_args) # re-used from above
+    self.print_sep()
+    # cairo, pango, atk
+    for pkg, name in zip([CAIRO_PKG, PANGO_PKG, ATK_PKG],
+                         ["cairo", "pango", "atk"]) :
+      self.build_compiled_package_simple(pkg_name=pkg, pkg_name_label=name)
+    # tiff
+    tiff_log = self.start_building_package("tiff")
+    self.fetch_untar_and_chdir(pkg_name=TIFF_PKG, log=tiff_log)
+    os.environ['MANSCHEME'] = "bsd-source-cat"
+    os.environ['DIR_MAN'] = os.path.join(self.base_dir, "man")
+    config_args = [ prefix_arg, "--noninteractive", "--with-LIBGL=no",
+                    "--with-LIBIMAGE=no" ]
+    self.configure_and_build(
+      config_args=config_args,
+      log=tiff_log,
+      make_args=make_args)
+    self.print_sep()
+    # gtk+
+    touch_bin_files()
+    gtk_log = self.start_building_package("gtk+")
+    self.fetch_untar_and_chdir(pkg_name=GTK_PKG, log=gtk_log)
+    self.call("./configure %s --without-libjpeg" % prefix_arg, log=gtk_log)
+    self.call("make -j %d SRC_SUBDIRS='gdk-pixbuf gdk gtk modules'" %
+      self.nproc, log=gtk_log)
+    self.call("make install SRC_SUBDIRS='gdk-pixbuf gdk gtk modules'",
+      log=gtk_log)
+    os.remove(msgfmt_bin)
+    os.remove(gettext_bin)
+    self.print_sep()
+    # gtk-engine
+    self.build_compiled_package_simple(pkg_name=GTK_ENGINE_PKG,
+      pkg_name_label="gtk-engine")
+    # fonts
+    fonts_log = self.start_building_package("fonts")
+    fonts_dir = os.path.join(self.base_dir, "share", "fonts")
+    if (not os.path.isdir(fonts_dir)) :
+      os.makedirs(fonts_dir)
+    os.chdir(fonts_dir)
+    pkg = self.fetch_package(pkg_name=FONT_PKG)
+    untar(pkg, log=fonts_log, verbose=True)
+    self.print_sep()
+    os.chdir(self.tmp_dir)
 
   def build_wxpython (self) :
-    os.chdir(self.tmp_dir)
     pkg_log = self.start_building_package("wxPython")
     pkg_name = WXPYTHON_PKG
     # XXX we don't entirely trust wxPython-2.9, but it would be preferrable for
@@ -361,7 +507,7 @@ class installer (object) :
         print >> self.log, "  running OS 10.6 or later, switching to wx 2.9"
         pkg_name = WXPYTHON_DEV_PKG
         cocoa = True
-    pkg = self.fetch_package(BASE_CCI_PKG_URL, pkg_name)
+    pkg = self.fetch_package(pkg_name)
     pkg_dir = untar(pkg, log=pkg_log)
     os.chdir(pkg_dir)
     # Stage 1: build wxWidgets libraries
@@ -429,7 +575,7 @@ class installer (object) :
     self.build_python_module_simple(
       pkg_url=BASE_CCI_PKG_URL,
       pkg_name=PYRTF_PKG,
-      pkg_name_simple="PyRTF",
+      pkg_name_label="PyRTF",
       confirm_import_module="PyRTF")
     self.set_cppflags_ldflags_tmp()
     # TODO we are patching the source in the CCTBX builds (but not the Phenix
@@ -438,6 +584,6 @@ class installer (object) :
     self.build_python_module_simple(
       pkg_url=BASE_CCI_PKG_URL,
       pkg_name=MATPLOTLIB_PKG,
-      pkg_name_simple="Matplotlib",
+      pkg_name_label="Matplotlib",
       confirm_import_module="matplotlib")
     self.restore_cppflags_ldflags()
