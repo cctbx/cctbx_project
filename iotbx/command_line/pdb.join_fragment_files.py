65151bcd2181e288d52e5c7ad7ddb6e9020b3afd
Richard Gildea <richard.gildea@diamond.ac.uk>
2013-02-22 19:03:30 +0000

    iotbx.pdb.join_fragment_files: new options to reset_atom_serial and to output as mmcif format

--- iotbx/command_line/pdb.join_fragment_files.py
+++ iotbx/command_line/pdb.join_fragment_files.py
@@ -1,9 +1,25 @@
 from __future__ import division
 import iotbx.pdb
+import iotbx.cif.model
+import iotbx.phil
+import libtbx
 from libtbx.utils import Usage, format_cpu_times
 import sys, os
 
+master_phil = iotbx.phil.parse("""
+join_fragment_files {
+  reset_atom_serial = False
+    .type = bool
+  model_file = None
+    .type = path
+    .multiple = True
+  format = mmcif pdb
+    .type = choice
+}
+""")
+
 def run(args,  command_name="iotbx.pdb.join_fragment_files"):
+  from iotbx import file_reader
   def usage():
     raise Usage("""\
 %s file1.pdb file2.pdb [...]
@@ -11,6 +27,7 @@ def run(args,  command_name="iotbx.pdb.join_fragment_files"):
 or define the environment variable
   PDB_MIRROR_PDB
 to join all fragment files in the PDB.""" % command_name)
+
   if (len(args) == 0 or args == ["--exercise"]):
     pdb_mirror_pdb = os.environ.get("PDB_MIRROR_PDB")
     if (pdb_mirror_pdb is None):
@@ -21,7 +38,7 @@ to join all fragment files in the PDB.""" % command_name)
         codes = line.split()
         joined = iotbx.pdb.join_fragment_files(
           file_names = [
-            os.path.join(pdb_mirror_pdb, code[1:3], "pdb%s.ent.gz" % code)
+            os.path.join(pdb_mirror_pdb, code[1:3], "pdb%s.ent.gz" % code).joined
               for code in codes])
         file_name_out = "%s_%s.pdb" % (codes[0], codes[-1])
         print "  writing:", file_name_out
@@ -31,10 +48,37 @@ to join all fragment files in the PDB.""" % command_name)
         if (len(args) != 0): break
     print format_cpu_times()
   else:
-    if (len(args) < 2): usage()
-    joined = iotbx.pdb.join_fragment_files(file_names=args)
-    print "\n".join(joined.info)
-    sys.stdout.write(joined.as_pdb_string(append_end=True))
+    sources = []
+    file_names = []
+    interpreter = master_phil.command_line_argument_interpreter()
+    input_file_type = None
+    for arg in args :
+      if os.path.isfile(arg) :
+        input_file = file_reader.any_file(arg)
+        if (input_file.file_type == "pdb") :
+          file_names.append(input_file)
+          sources.append(interpreter.process(arg="model_file=\"%s\"" % arg))
+        elif (input_file.file_type == "phil") :
+          sources.append(input_file.file_object)
+      else :
+        arg_phil = interpreter.process(arg=arg)
+        sources.append(arg_phil)
+    work_phil = master_phil.fetch(sources=sources)
+    work_params = work_phil.extract()
+    file_names = work_params.join_fragment_files.model_file
+    if (len(file_names) < 2): usage()
+    result = iotbx.pdb.join_fragment_files(file_names=file_names)
+    joined = result.joined
+    if work_params.join_fragment_files.reset_atom_serial:
+      joined.atoms_reset_serial()
+    if work_params.join_fragment_files.format in (None, libtbx.Auto, "pdb"):
+      print "\n".join(joined.info)
+      sys.stdout.write(joined.as_pdb_string(append_end=True))
+    elif work_params.join_fragment_files.format == "mmcif":
+      cif_object = iotbx.cif.model.cif()
+      cif_object["combined"] = joined.as_cif_block(
+        crystal_symmetry=result.crystal_symmetry)
+      cif_object.show(out=sys.stdout)
 
 if (__name__ == "__main__"):
   run(sys.argv[1:])
