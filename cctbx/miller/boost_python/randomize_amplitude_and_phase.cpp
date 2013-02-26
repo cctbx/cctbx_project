6d10f3f3e5bbc13253faa0507709f639ade85bbc
Pavel Afonine <afonine@users.noreply.github.com>
2013-02-26 23:50:19 +0000

    function to randomize amplitude and phase of complex number (e.g. structure factor)

---
+++ cctbx/miller/boost_python/randomize_amplitude_and_phase.cpp
@@ -0,0 +1,32 @@
+#include <scitbx/array_family/boost_python/shared_wrapper.h>
+#include <cctbx/miller/randomize_amplitude_and_phase.h>
+#include <boost/python/def.hpp>
+
+namespace cctbx { namespace miller { namespace boost_python {
+
+  template <typename ComplexType, typename FloatType>
+  struct randomize_amplitude_and_phase_wrapper
+  {
+    static void wrap() {
+      using namespace boost::python;
+      return_value_policy<return_by_value> rbv;
+
+      def("randomize_amplitude_and_phase",
+        (af::shared<ComplexType>(*)
+          (af::const_ref<ComplexType> const&,
+           FloatType const&,
+           FloatType const&,
+           int))
+             randomize_amplitude_and_phase, (
+               arg("data"),
+               arg("amplitude_error"),
+               arg("phase_error_deg"),
+               arg("random_seed")))
+      ;
+    }
+  };
+
+  void wrap_randomize_amplitude_and_phase() {
+    randomize_amplitude_and_phase_wrapper<std::complex<double>, double>::wrap();
+  }
+}}} // cctbx::miller::boostpython
---
+++ cctbx/miller/randomize_amplitude_and_phase.h
@@ -0,0 +1,39 @@
+#ifndef CCTBX_RANDOMIZE_AMPLITUDE_AND_PHASE_H
+#define CCTBX_RANDOMIZE_AMPLITUDE_AND_PHASE_H
+
+#include <cctbx/import_scitbx_af.h>
+#include <cctbx/error.h>
+#include <scitbx/random.h>
+
+namespace cctbx { namespace miller {
+
+  //Randomize complex number with prescribed target error in amplitude and phase
+  template <typename ComplexType, typename FloatType>
+  af::shared<ComplexType> randomize_amplitude_and_phase(
+    af::const_ref<ComplexType> const& data,
+    FloatType const& amplitude_error,
+    FloatType const& phase_error_deg,
+    int random_seed)
+  {
+    CCTBX_ASSERT(amplitude_error>=0 && amplitude_error<=1);
+    CCTBX_ASSERT(phase_error_deg>=0 && phase_error_deg<=90);
+    scitbx::random::mersenne_twister mt(random_seed);
+    af::shared<ComplexType>
+      result(data.size(), af::init_functor_null<ComplexType>());
+    for (std::size_t i=0; i<data.size(); i++) {
+      ComplexType d = data[i];
+      FloatType a = std::abs(d);
+      FloatType p = std::arg(d);
+      FloatType sc = 1;
+      if(mt.random_double() <= 0.5) sc=-1;
+      //sc *= (2 * mt.random_double()); //Why only works up to threshold=0.5?
+      a += a*amplitude_error*sc;
+      p += p*(phase_error_deg/90.)*sc;
+      result[i] = ComplexType(a*std::cos(p), a*std::sin(p));
+    }
+    return result;
+  };
+
+}} // namespace cctbx::miller
+
+#endif // CCTBX_RANDOMIZE_AMPLITUDE_AND_PHASE_H
