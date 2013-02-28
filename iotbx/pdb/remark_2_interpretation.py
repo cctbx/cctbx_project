fcfc8f4eb9b2c2e8309e3122aac4b3574e3b16fd
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-02-28 19:17:13 +0000

    more indentation cleanup

--- iotbx/pdb/remark_2_interpretation.py
+++ iotbx/pdb/remark_2_interpretation.py
@@ -5,23 +5,23 @@ def get_resolution(st):
         st.count("ANGSTROM")>0)
   ch = st.split()
   if(q1):
-     try:
-       result = float(ch[3])
-       assert result < 100.0 and result > 0.01
-     except KeyboardInterrupt: raise
-     except Exception:
-       pass
+    try:
+      result = float(ch[3])
+      assert result < 100.0 and result > 0.01
+    except KeyboardInterrupt: raise
+    except Exception:
+      pass
   return result
 
 def extract_resolution(remark_2_records):
   res_counter = 0
   resolutions = []
   for record in remark_2_records:
-      result = get_resolution(record)
-      if(result is not None):
-         resolutions.append(result)
-         res_counter +=1
+    result = get_resolution(record)
+    if(result is not None):
+      resolutions.append(result)
+      res_counter +=1
   if(res_counter == 1):
-     return resolutions
+    return resolutions
   else:
-     return None
+    return None
