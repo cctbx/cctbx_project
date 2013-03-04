dea280b9d23ac9e468fcad28ea704564793ef84f
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-03-04 16:03:19 +0000

    Detector now gives same interface as panel when only one panel is present as per Jira ticket. Otherwise methods throw exceptions.

--- dxtbx/model/detector.h
+++ dxtbx/model/detector.h
@@ -42,7 +42,7 @@ namespace dxtbx { namespace model {
   * The detector elements can be accessed in the same way as an array:
   *  detector[0] -> 1st detector panel.
   */
-  class Detector {
+  class DetectorBase {
   public:
 
     typedef std::pair<int, vec2<double> > coord_type;
@@ -53,19 +53,19 @@ namespace dxtbx { namespace model {
     typedef panel_list_type::iterator iterator;
 
     /** Default constructor */
-    Detector() {}
+    DetectorBase() {}
 
     /** Initialise the detector with singla panel */
-    Detector(const Panel &panel) {
+    DetectorBase(const Panel &panel) {
       panel_list_.push_back(panel);
     }
 
     /** Initialise the detector with an array of panels */
-    Detector(const panel_list_type &panel_list)
+    DetectorBase(const panel_list_type &panel_list)
       : panel_list_(panel_list) {}
 
     /** Virtual destructor */
-    virtual ~Detector() {}
+    virtual ~DetectorBase() {}
 
     /** Get the begin iterator */
     iterator begin() {
@@ -108,7 +108,7 @@ namespace dxtbx { namespace model {
     }
 
     /** Check the detector panels are the same */
-    bool operator==(const Detector &detector) {
+    bool operator==(const DetectorBase &detector) {
       bool same = panel_list_.size() == detector.panel_list_.size();
       if (same) {
         for (std::size_t i = 0; i < panel_list_.size(); ++i) {
@@ -119,7 +119,7 @@ namespace dxtbx { namespace model {
     }
 
     /** Check the detector panels are not the same */
-    bool operator!=(const Detector &detector) {
+    bool operator!=(const DetectorBase &detector) {
       return !(*this == detector);
     }
 
@@ -190,7 +190,7 @@ namespace dxtbx { namespace model {
 //      return false;
 //    }
 
-    friend std::ostream& operator<< (std::ostream& , const Detector&);
+    friend std::ostream& operator<< (std::ostream& , const DetectorBase&);
 
   protected:
 
@@ -209,8 +209,8 @@ namespace dxtbx { namespace model {
 //      typedef boost::geometry::model::polygon <point> polygon;
 
 //      // Get the rectange of detector points
-//      double6 rect_a = a.get_image_rectangle();
-//      double6 rect_b = b.get_image_rectangle();
+//      double6 rect_a(0, 0, 0, 0, 0, 0);// = a.get_image_rectangle();
+//      double6 rect_b(0, 0, 0, 0, 0, 0);// = b.get_image_rectangle();
 
 //      // Create a polygon for the panel a plane
 //      polygon poly_a;
@@ -237,7 +237,7 @@ namespace dxtbx { namespace model {
 
   /** Print the detector information to the ostream */
   inline
-  std::ostream& operator<< (std::ostream &os, const Detector &d) {
+  std::ostream& operator<< (std::ostream &os, const DetectorBase &d) {
     os << "Detector:\n";
     for (std::size_t i = 0; i < d.num_panels(); ++i) {
       std::stringstream ss;
@@ -251,6 +251,222 @@ namespace dxtbx { namespace model {
     }
     return os;
   }
+
+  /** Add interface to detector that is valid if only 1 panel is present */
+  class Detector : public DetectorBase {
+  public:
+
+    /** Default constructor */
+    Detector() {}
+
+    /** Initialise the detector with singla panel */
+    Detector(const Panel &panel)
+      : DetectorBase(panel) {}
+
+    /** Initialise the detector with an array of panels */
+    Detector(const panel_list_type &panel_list)
+      : DetectorBase(panel_list) {}
+
+    /** Virtual destructor */
+    virtual ~Detector() {}
+
+    /** Get the first panel type */
+    std::string get_type() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_type();
+    }
+
+    /** Set the first panel type */
+    void set_type(std::string type) {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      DetectorBase::panel_list_[0].set_type(type);
+    }
+
+    /** Get the first panel fast axis */
+    vec3 <double> get_fast_axis() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_fast_axis();
+    }
+
+    /** Get the first panel slow axis */
+    vec3 <double> get_slow_axis() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_slow_axis();
+    }
+
+    /** Get the first panel origin */
+    vec3 <double> get_origin() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_origin();
+    }
+
+    /** Get the first panel normal */
+    vec3 <double> get_normal() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_normal();
+    }
+
+    /** Get the first panel pixel size */
+    vec2 <double> get_pixel_size() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_pixel_size();
+    }
+
+    /** Set the first panel pixel size */
+    void set_pixel_size(vec2 <double> pixel_size) {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      DetectorBase::panel_list_[0].set_pixel_size(pixel_size);
+    }
+
+    /** Get the first panel image size */
+    vec2 <std::size_t> get_image_size() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_image_size();
+    }
+
+    /** Set the first panel image size */
+    void set_image_size(vec2 <std::size_t> image_size) {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      DetectorBase::panel_list_[0].set_image_size(image_size);
+    }
+
+    /** Get the first panel trusted range */
+    vec2 <double> get_trusted_range() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_trusted_range();
+    }
+
+    /** Set the first panel trusted range */
+    void set_trusted_range(vec2 <double> trusted_range) {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      DetectorBase::panel_list_[0].set_trusted_range(trusted_range);
+    }
+
+    /** Get the mask array */
+    shared_int4 get_mask() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_mask();
+    }
+
+    /** Set the first panel mask */
+    void set_mask(const shared_int4 &mask) {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      DetectorBase::panel_list_[0].set_mask(mask);
+    }
+
+    /** Add an element to the first panel mask */
+    void add_mask(int f0, int s0, int f1, int s1) {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      DetectorBase::panel_list_[0].add_mask(f0, s0, f1, s1);
+    }
+
+    /** Get the matrix of the first panel  coordinate system */
+    mat3 <double> get_d_matrix() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_d_matrix();
+    }
+
+    /** Get the first panel inverse d matrix */
+    mat3 <double> get_D_matrix() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_D_matrix();
+    }
+
+    /** Set the first panel origin, fast axis and slow axis */
+    void set_frame(vec3<double> fast_axis, vec3<double> slow_axis,
+        vec3<double> origin) {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      DetectorBase::panel_list_[0].set_frame(fast_axis, slow_axis, origin);
+    }
+
+    /** Get the distance from the sample to the first panel plane */
+    double get_distance() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_distance();
+    }
+
+    /** Get the first panel image size in millimeters */
+    vec2<double> get_image_size_mm() const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_image_size_mm();
+    }
+
+    /** Check the first panel value is valid */
+    bool is_value_in_trusted_range(double value) const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].is_value_in_trusted_range(value);
+    }
+
+    /** Check the first panel coordinate is valid */
+    bool is_coord_valid(vec2<double> xy) const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].is_coord_valid(xy);
+    }
+
+    /** Check the first panel coordinate is valid */
+    bool is_coord_valid_mm(vec2<double> xy) const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].is_coord_valid_mm(xy);
+    }
+
+    /** Get the beam centre in mm in the first panel  basis */
+    vec2<double> get_beam_centre(vec3<double> s0) const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_beam_centre(s0);
+    }
+
+    /** Get the first panel beam centre in lab coordinates */
+    vec3<double> get_beam_centre_lab(vec3<double> s0) const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_beam_centre_lab(s0);
+    }
+
+    /** Get the resolution at a given first panel pixel. */
+    double get_resolution_at_pixel(vec3<double> s0, double wavelength,
+        vec2<double> xy) {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_resolution_at_pixel(s0,
+        wavelength, xy);
+    }
+
+    /** Get the maximum resolution of the first panel. */
+    double get_max_resolution_at_corners(vec3<double> s0, double wavelength) {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_max_resolution_at_corners(s0,
+        wavelength);
+    }
+
+    /** Get the maximum resolution of a full circle on the first panel. */
+    double get_max_resolution_elipse(vec3<double> s0, double wavelength) {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_max_resolution_elipse(s0,
+        wavelength);
+    }
+
+    /** Get the first panel point (in mm) in lab coordinates */
+    vec3<double> get_lab_coord(vec2<double> xy) const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_lab_coord(xy);
+    }
+
+    /** Get the first panel point (in mm) in lab coordinates */
+    vec3<double> get_pixel_lab_coord(vec2<double> xy) const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].get_pixel_lab_coord(xy);
+    }
+
+    /** Map first panel coordinates in mm to pixels */
+    vec2<double> millimeter_to_pixel(vec2<double> xy) const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].millimeter_to_pixel(xy);
+    }
+
+    /** Map the first panel coordinates in pixels to millimeters */
+    vec2<double> pixel_to_millimeter(vec2<double> xy) const {
+      DXTBX_ASSERT(DetectorBase::panel_list_.size() == 1);
+      return DetectorBase::panel_list_[0].pixel_to_millimeter(xy);
+    }
+  };
 
 }} // namespace dxtbx::model
 
