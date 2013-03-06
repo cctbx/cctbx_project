d7f407407ad6f5fbaa59141f54a15d68ff46357d
Graeme Winter <graeme.winter@gmail.com>
2013-03-06 12:03:53 +0000

    Cleaning clutter; starting to define something useful i can test...

--- dxtbx/sweep.py
+++ dxtbx/sweep.py
@@ -10,8 +10,12 @@ from __future__ import division
 # Top level interface to the dxtbx: treatment of a sweep of images as a single
 # unit, incorporation of the registry and so on... N.B. factory functions will
 # be defined which can look up information from the file system etc.
+#
+# The classes in here should not be instantiated directly as they will depend
+# on the form of the data (a single file, a sequence of images) instead accessed
+# *only* from the factories.
 
-class sweep:
+class sweep_of_images:
     '''Definition for a sweep of images, defined to be a set of diffraction
     images in files with matching templates, or a volume formatted file.'''
 
@@ -42,7 +46,7 @@ class sweep:
         format_class = Registry.find(first_image)
         understanding = format_class.understand(first_image)
         format_instance = format_class(first_image)
-        
+
         if not self._beam_model:
             self._beam_model = format_instance.get_beam()
 
@@ -64,7 +68,7 @@ class sweep:
                 assert(other_instance.get_detector() == self._detector_model)
 
                 # cache as I will want this later on...
-                
+
                 if not image in scan_models:
                     scan_models[image] = other_instance.get_scan()
 
@@ -78,6 +82,59 @@ class sweep:
 
         self._format_class = format_class
 
+        # FIXME generate template, directory information in here... validate this
+        # against the image names... pull out image indices also and cache these.
+
+        import os
+        from sweep_filenames import template_regex
+
+        self._directory, first_image_name = os.path.split(first_image)
+        self._template, first_image_number = template_regex(first_image_name)
+
+        pfx = self._template.split('#')[0]
+        sfx = self._template.split('#')[-1]
+
+        self._image_numbers = [first_image_number]
+
+        for j, image in enumerate(list_of_images[1:]):
+            directory, image_name = os.path.split(image)
+            assert(directory == self._directory)
+            image_number = int(image_name.replace(pfx, '').replace(sfx, ''))
+            assert(image_number == first_image_number + 1 + j)
+            self._image_numbers.append(image_number)
+
+        self._image_number_range = (min(self._image_numbers),
+                                    max(self._image_numbers))
+
+        self._template_format = '%s%d%s' % (pfx, self._template.count('#'), sfx)
+
         return
-        
-    
+
+    def __getitem__(self, j):
+        '''Get the image data for image j.'''
+
+        assert(j >= self._image_number_range[0])
+        assert(j <= self._image_number_range[1])
+
+        import os
+
+        format_instance = self._format_class(
+            os.path.join(self._directory, self._template_format % j))
+
+        # FIXME add cache to store recently returned file data: should be
+        # configurable somehow so you can store in ram say the last 10 images
+        # or something...
+
+        return format_instance.get_raw_data()
+
+    def get_beam(self):
+        return self._beam_model
+
+    def get_detector(self):
+        return self._detector_model
+
+    def get_goniometer(self):
+        return self._goniometer_model
+
+    def get_scan(self):
+        return self._scan_model
