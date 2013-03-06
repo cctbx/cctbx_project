19b70e01f844e03dd04809adde4029e581dc3730
Graeme Winter <graeme.winter@gmail.com>
2013-03-06 11:33:45 +0000

    Add tests for sweep_filenames stuff

--- dxtbx/tst_dxtbx.py
+++ dxtbx/tst_dxtbx.py
@@ -43,6 +43,25 @@ def tst_dxtbx_models():
 
     return
 
+def tst_sweep():
+
+    from dxtbx.sweep_filenames import template_regex
+    
+    questions_answers = {
+        'foo_bar_001.img':'foo_bar_###.img',
+        'foo_bar001.img':'foo_bar###.img',
+        'foo_bar_1.8A_001.img':'foo_bar_1.8A_###.img',
+        'foo_bar.001':'foo_bar.###',
+        'foo_bar_001.img1000':'foo_bar_###.img1000',
+        'foo_bar_00001.img':'foo_bar_#####.img'
+        }
+
+    for filename in questions_answers:
+        answer = template_regex(filename)
+        assert answer[0] == questions_answers[filename]
+
+    print 'OK'
+
 if __name__ == '__main__':
     tst_dxtbx()
     tst_dxtbx_models()
