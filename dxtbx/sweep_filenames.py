4d61f1544e6c69f90552557d96a53501333b7a93
Graeme Winter <graeme.winter@gmail.com>
2013-03-06 11:33:16 +0000

    Tools to help with filename templates pulled from xia2 code: I know this is also somewhere in labelit or iotbx code; we need to merge the functionality of those

---
+++ dxtbx/sweep_filenames.py
@@ -0,0 +1,43 @@
+import string
+import re
+
+# filename template code stolen from xia2...
+
+# N.B. these are reversed patterns...
+
+patterns = [r'([0-9]{2,12})\.(.*)',
+            r'(.*)\.([0-9]{2,12})_(.*)',
+            r'(.*)\.([0-9]{2,12})(.*)']
+
+joiners = ['.', '_', '']
+
+compiled_patterns = [re.compile(pattern) for pattern in patterns]
+
+def template_regex(filename):
+    '''Try a bunch of templates to work out the most sensible. N.B. assumes
+    that the image index will be the last digits found in the file name.'''
+
+    rfilename = filename[::-1]
+
+    global patterns, compiled_patterns
+
+    for j, cp in enumerate(compiled_patterns):
+        match = cp.match(rfilename)
+        if not match:
+            continue
+        groups = match.groups()
+
+        if len(groups) == 3:
+            exten = '.' + groups[0][::-1]
+            digits = groups[1][::-1]
+            prefix = groups[2][::-1] + joiners[j]
+        else:
+            exten = ''
+            digits = groups[0][::-1]
+            prefix = groups[1][::-1] + joiners[j]
+            
+        template = prefix + ''.join(['#' for d in digits]) + exten
+        break
+        
+    return template, int(digits)
+
