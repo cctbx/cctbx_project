d9df77e01c3f59322437f12e98fa778c86d7ddbf
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-03-22 19:30:47 +0000

    script to create Mac app launchers using py2app

---
+++ auto_build/command_line/create_mac_app.py
@@ -0,0 +1,8 @@
+# LIBTBX_SET_DISPATCHER_NAME libtbx.create_mac_app
+
+from __future__ import division
+from auto_build.create_mac_app import run
+import sys
+
+if (__name__ == "__main__") :
+  sys.exit(run(sys.argv[1:]))
