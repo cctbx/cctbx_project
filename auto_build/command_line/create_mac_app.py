ce94b3e6ee992c72accf74da429e46fc793d530e
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-04-01 23:34:18 +0000

    relocated auto_build as sub-directory of libtbx (but still mostly dependency-free)

--- auto_build/command_line/create_mac_app.py
+++
@@ -1,8 +0,0 @@
-# LIBTBX_SET_DISPATCHER_NAME libtbx.create_mac_app
-
-from __future__ import division
-from auto_build.create_mac_app import run
-import sys
-
-if (__name__ == "__main__") :
-  sys.exit(run(sys.argv[1:]))
--- auto_build/command_line/create_windows_exe.py
+++
@@ -1,8 +0,0 @@
-# LIBTBX_SET_DISPATCHER_NAME libtbx.create_windows_exe
-
-from __future__ import division
-from auto_build.create_windows_exe import run
-import sys
-
-if (__name__ == "__main__") :
-  sys.exit(run(sys.argv[1:]))
