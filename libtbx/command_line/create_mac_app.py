2d2f5dd9c4cfbd1f1e2278b9e3cd83614b75c022
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-04-01 23:42:39 +0000

    cleaning up SVN mess, part 1: delete auto_build, etc.

--- libtbx/command_line/create_mac_app.py
+++
@@ -1,7 +0,0 @@
-
-from __future__ import division
-from libtbx.auto_build import create_mac_app
-import sys
-
-if (__name__ == "__main__") :
-  sys.exit(create_mac_app.run(sys.argv[1:]))
--- libtbx/command_line/create_windows_exe.py
+++
@@ -1,7 +0,0 @@
-
-from __future__ import division
-from libtbx.auto_build import create_windows_exe
-import sys
-
-if (__name__ == "__main__") :
-  sys.exit(create_windows_exe.run(sys.argv[1:]))
