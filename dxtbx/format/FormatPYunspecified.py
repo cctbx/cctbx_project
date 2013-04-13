ff2bff78ed09900efa5dfeae4f6b78f602b1df25
Nicholas Sauter <nksauter@lbl.gov>
2013-04-13 19:03:43 +0000

    dxtbx:  support for cctbx.xfel-style Npy images from CXI detector.

--- dxtbx/format/FormatPYunspecified.py
+++ dxtbx/format/FormatPYunspecified.py
@@ -2,24 +2,24 @@ from __future__ import division
 
 from dxtbx.format.FormatPY import FormatPY
 
-class FormatPYmultitile(FormatPY):
+class FormatPYunspecified(FormatPY):
 
     @staticmethod
     def understand(image_file):
         try:
-          stream = FormatPYmultitile.open_file(image_file, 'rb')
+          stream = FormatPYunspecified.open_file(image_file, 'rb')
           import cPickle as pickle
           data = pickle.load(stream)
         except IOError,e:
           return False
 
-        wanted_header_items = ['TILES','METROLOGY']
+        wanted_header_items = ['SIZE1','SIZE2']
 
         for header_item in wanted_header_items:
             if not header_item in data:
                 return False
 
-        unwanted_header_items = ['SIZE1','SIZE2']
+        unwanted_header_items = ['DETECTOR_FORMAT_VERSION']
 
         for header_item in unwanted_header_items:
             if header_item in data:
@@ -34,11 +34,27 @@ class FormatPYmultitile(FormatPY):
 
         FormatPY.__init__(self, image_file)
 
-    def _start(self):
+    def start_helper(self, version_token):
 
-        from cxi_xdr_xes.cftbx.detector.cspad_detector import CSPadDetector
-        self.detectorbase = CSPadDetector(self._image_file)
-        self.detectorbase.readHeader()
+        from spotfinder.applications.xfel import cxi_phil
+        from iotbx.detectors.npy import NpyImage
+        import os
+        args = [self._image_file,
+                version_token,
+                "viewer.powder_arcs.show=False",
+                "viewer.powder_arcs.code=3n9c",
+               ]
+
+        params = cxi_phil.cxi_versioned_extract(args)
+        horizons_phil = params.persist.commands
+        if isinstance(self._image_file, basestring) and os.path.isfile(self._image_file):
+          I = NpyImage(self._image_file)
+        else:
+          print "This is not a file; assume the data are in the defined dictionary format"
+          I = NpyImage(self._image_file, source_data=params.indexing.data)
+        I.readHeader(horizons_phil)
+        I.translate_tiles(horizons_phil)
+        self.detectorbase = I
 
     def _goniometer(self):
 
@@ -50,15 +66,15 @@ class FormatPYmultitile(FormatPY):
         return self._detector_factory.simple(
             sensor = 'PAD',
             distance = self.detectorbase.distance,
-            beam_centre = (self.detectorbase.beam_center[0],
-                           self.detectorbase.beam_center[1]),
+            beam_centre = (self.detectorbase.beamx,
+                           self.detectorbase.beamy),
             fast_direction = '+x',
             slow_direction = '-y',
-            pixel_size = (self.detectorbase._pixel_size[0],
-                          self.detectorbase._pixel_size[1]),
+            pixel_size = (self.detectorbase.pixel_size,
+                          self.detectorbase.pixel_size),
             image_size = (self.detectorbase.size1,
                           self.detectorbase.size2),
-            trusted_range = (0, self.detectorbase._saturation),
+            trusted_range = (0, self.detectorbase.saturation),
             mask = [])  # a list of dead rectangles
 
     def _beam(self):
@@ -77,9 +93,10 @@ class FormatPYmultitile(FormatPY):
           epochs = {1:0.}  # Later see if we can actually get the millisecond time stamp in here
           )
 
+
 if __name__ == '__main__':
 
     import sys
 
     for arg in sys.argv[1:]:
-        print FormatPYmultitile.understand(arg)
+        print FormatPYunspecified.understand(arg)
---
+++ dxtbx/format/TemplatePYCXI61.py
@@ -0,0 +1,22 @@
+from __future__ import division
+
+from dxtbx.format.FormatPYunspecified import FormatPYunspecified
+
+class FormatPYCXI61(FormatPYunspecified):
+
+    """PREFERENCE FILE.
+       Treats any Pickle-format file lacking a DETECTOR_FORMAT_VERSION key
+       automatically as format CXI 6.1.
+       Rename this file to FormatPYCXI61.py and put in dxtbx/format path.
+    """
+
+    def _start(self):
+
+        FormatPYunspecified.start_helper(self,version_token="distl.detector_format_version=CXI 6.1")
+
+if __name__ == '__main__':
+
+    import sys
+
+    for arg in sys.argv[1:]:
+        print FormatPYCXI61.understand(arg)
