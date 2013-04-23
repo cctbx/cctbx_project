adff3335d17e6090781e53d1f0a607f26d08b409
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-04-23 13:56:44 +0000

    Added files to read XDS GAIN.CBF/BLANK.CBF

---
+++ iotbx/xds/gain_cbf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env libtbx.python
+#
+# iotbx.xds.gain_cbf.py
+#
+#   James Parkhurst, Diamond Light Source, 2012/OCT/16
+#
+#   Class to read the GAIN.CBF files used in XDS
+#
+from __future__ import division
+
+import xds_cbf
+
+class reader(xds_cbf.reader):
+  """A class to read the GAIN.CBF files used in XDS"""
+  def __init__(self):
+    xds_cbf.reader.__init__(self)
+
+  def get_data(self, dim):
+    """Get the gain array from the file"""
+    from math import ceil
+    import numpy
+
+    # Get the image data
+    sampled_gain = xds_cbf.reader.get_data(self)
+    ny = int(ceil(dim[0] / sampled_gain.shape[0]))
+    nx = int(ceil(dim[1] / sampled_gain.shape[1]))
+
+    # Resize the gain image
+    i1 = numpy.array([range(dim[1])] * dim[0], dtype=numpy.int32)
+    j1 = numpy.array([range(dim[0])] * dim[1], dtype=numpy.int32).transpose()
+    i2 = numpy.divide(i1, nx)
+    j2 = numpy.divide(j1, ny)
+    gain = numpy.zeros(dim, dtype=numpy.float64)
+    gain[j1,i1] = sampled_gain[j2,i2] / 1000.0
+    return gain
+
+if __name__ == '__main__':
+    import sys
+    import numpy
+    handle = reader()
+    handle.read_file(sys.argv[1])
+    image = handle.get_data(sys.argv[2])
