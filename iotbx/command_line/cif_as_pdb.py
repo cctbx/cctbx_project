d488623839f6ed871f96e734b6fe6158e777e6d7
Richard Gildea <richard.gildea@diamond.ac.uk>
2013-04-23 23:37:54 +0000

    rename command to phenix.cif_as_pdb; output converted file in current working directory

--- iotbx/command_line/cif_as_pdb.py
+++ iotbx/command_line/cif_as_pdb.py
@@ -1,4 +1,5 @@
 from __future__ import division
+# LIBTBX_SET_DISPATCHER_NAME phenix.cif_as_pdb
 import os
 import iotbx.pdb
 import iotbx.pdb.mmcif
@@ -7,9 +8,11 @@ def run(args):
   for file_name in args:
     try:
       assert os.path.exists(file_name)
+      print "Converting %s to PDB format." %file_name
       cif_input = iotbx.pdb.mmcif.cif_input(file_name=file_name)
       hierarchy = cif_input.construct_hierarchy()
-      f = open(file_name+".pdb", "wb")
+      basename = os.path.splitext(os.path.basename(file_name))[0]
+      f = open(basename+".pdb", "wb")
       print >> f, hierarchy.as_pdb_string(
         crystal_symmetry=cif_input.crystal_symmetry())
       f.close()
