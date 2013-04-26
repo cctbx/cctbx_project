ecec6cbfc39873897fbf638d29a9604869c8eeef
Richard Gildea <richard.gildea@diamond.ac.uk>
2013-04-26 22:21:29 +0000

    new command xes.histograms

---
+++ xfel/command_line/xes_histograms.py
@@ -0,0 +1,11 @@
+from __future__ import division
+# LIBTBX_SET_DISPATCHER_NAME xes.histograms
+# LIBTBX_PRE_DISPATCHER_INCLUDE_SH PHENIX_GUI_ENVIRONMENT=1
+# LIBTBX_PRE_DISPATCHER_INCLUDE_SH export PHENIX_GUI_ENVIRONMENT
+
+import sys
+
+from xfel.cxi.cspad_ana import xes_histograms
+
+if __name__ == '__main__':
+  xes_histograms.run(sys.argv[1:])
