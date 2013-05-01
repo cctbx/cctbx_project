49291cc6dcabebefb3ed74fe9ed045fd9f9f3aaf
Richard Gildea <richard.gildea@diamond.ac.uk>
2013-05-01 05:28:23 +0000

    phil-ify smooth_spectrum.py - needs cleaning up but commiting current version so Johan can use it

--- xfel/command_line/smooth_spectrum.py
+++ xfel/command_line/smooth_spectrum.py
@@ -3,31 +3,58 @@ import math
 import os
 import sys
 
-from libtbx.option_parser import option_parser
+#from libtbx.option_parser import option_parser
+import iotbx.phil
 from scitbx.array_family import flex
 from scitbx import smoothing
 
+def pyplot_label_axes(xlabel="Pixel column", ylabel="Intensity", fontsize=20):
+  from matplotlib import pyplot
+  pyplot.ylabel("Intensity", fontsize=fontsize)
+  pyplot.xlabel("Pixel column", fontsize=fontsize)
+  axes = pyplot.axes()
+  for tick in axes.xaxis.get_ticklabels():
+    tick.set_fontsize(20)
+  for tick in axes.yaxis.get_ticklabels():
+    tick.set_fontsize(20)
+
+
+master_phil_str = """\
+smoothing {
+  method = *savitzky_golay fourier_filter
+    .type = choice
+  savitzky_golay {
+    half_window = 16
+      .type = int
+      .help = The number of values either side of a data point to use.
+    degree = 4
+      .type = int
+      .help = The degree of polynomial to fit to data points.
+  }
+  fourier_filter_cutoff = None
+    .type = int
+    .help = Cutoff frequency for Fourier filtering.
+}
+"""
+
+master_phil = iotbx.phil.parse(master_phil_str)
+
+
 def run(args):
+  processed = iotbx.phil.process_command_line(
+    args=args, master_string=master_phil_str)
+  args = processed.remaining_args
+  work_params = processed.work.extract().smoothing
+  savitzky_golay_half_window = work_params.savitzky_golay.half_window
+  savitzky_golay_degree = work_params.savitzky_golay.degree
+  fourier_cutoff = work_params.fourier_filter_cutoff
 
-  command_line = (option_parser()
-                  .option("--savitzky_golay_half_window", "--sg_half_window",
-                          type="int",
-                          help="The number of values either side of a data point"
-                              "to use.")
-                  .option("--savitzky_golay_degree", "--sg_degree",
-                          type="int",
-                          help="The degree of polynomial to fit to data points.")
-                  .option("--fourier_filter_cutoff",
-                          type="int",
-                          help="Cutoff frequency for Fourier filtering.")
-                  ).process(args=args)
-  args = command_line.args
-  savitzky_golay_half_window = command_line.options.savitzky_golay_half_window
-  savitzky_golay_degree = command_line.options.savitzky_golay_degree
-  fourier_cutoff = command_line.options.fourier_filter_cutoff
+  #assert (fourier_cutoff is not None or
+          #[savitzky_golay_degree, savitzky_golay_half_window].count(None) == 0)
 
-  assert (fourier_cutoff is not None or
-          [savitzky_golay_degree, savitzky_golay_half_window].count(None) == 0)
+  method = work_params.method
+  if method == "fourier_filter":
+    assert work_params.fourier_filter_cutoff is not None
 
   for i, filename in enumerate(args):
     print filename
@@ -35,24 +62,30 @@ def run(args):
     x, y = zip(*[line.split() for line in f.readlines() if not line.startswith("#")])
     x = flex.double(flex.std_string(x))
     y = flex.double(flex.std_string(y))
+    x = x[:-2]
+    y = y[:-2]
     x_orig = x.deep_copy()
     y_orig = y.deep_copy()
 
     x, y = interpolate(x, y)
 
-    if savitzky_golay_degree is not None:
+    if method == "savitzky_golay":
       x, y_smoothed = smoothing.savitzky_golay_filter(
         x, y, savitzky_golay_half_window, savitzky_golay_degree)
 
-    elif fourier_cutoff is not None:
+    elif method == "fourier_filter":
       x, y_smoothed = fourier_filter(x, y, cutoff_frequency=fourier_cutoff)
 
     from matplotlib import pyplot
+    fontsize = 20
     pyplot.plot(x_orig, y_orig, color='black', linestyle='dotted', linewidth=2)
+    #pyplot.plot(x_orig, y_orig, color='black', linewidth=2)
     pyplot.plot(x, y_smoothed, linewidth=2, color='red')
+    pyplot_label_axes()
     pyplot.show()
-    pyplot.plot(x, y - y_smoothed)
-    pyplot.show()
+    #pyplot.plot(x[:385], (y - y_smoothed)[:385], linewidth=2)
+    #pyplot_label_axes()
+    #pyplot.show()
 
     filename = os.path.join(os.path.dirname(filename), "smoothed_spectrum.txt")
     f = open(filename, "wb")
@@ -69,13 +102,17 @@ def run(args):
         del y[x_interp_size - i - 1]
         del y_smoothed[x_interp_size - i - 1]
 
-    signal_to_noise = estimate_signal_to_noise(x, y, y_smoothed)
+    x = x[10:-10]
+    y = y[10:-10]
+    y_smoothed = y_smoothed[10:-10]
 
-    pyplot.plot(x[:375], signal_to_noise[:375])
-    pyplot.show()
+    signal_to_noise = estimate_signal_to_noise(x, y, y_smoothed, plot=False)
+
+    #pyplot.plot(x[:375], signal_to_noise[:375])
+    #pyplot.show()
 
 
-def interpolate(x, y):
+def interpolate(x, y, half_window=10):
   perm = flex.sort_permutation(x)
   x = x.select(perm)
   y = y.select(perm)
@@ -85,13 +122,13 @@ def interpolate(x, y):
     x_all.append(x[i])
     y_all.append(y[i])
     if i < x.size()-1 and (x[i+1] - x[i]) > 1:
-      window_left = min(10, i)
-      window_right = min(10, x.size() - i)
+      window_left = min(half_window, i)
+      window_right = min(half_window, x.size() - i)
       x_ = x[i-window_left:i+window_right]
       y_ = y[i-window_left:i+window_right]
       from scitbx.math import curve_fitting
       # fit a 2nd order polynomial through the missing points
-      polynomial = curve_fitting.univariate_polynomial(1, 1, 1)
+      polynomial = curve_fitting.univariate_polynomial(1, 1)
       fit = curve_fitting.lbfgs_minimiser([polynomial], x_,y_).functions[0]
       missing_x = flex.double(range(int(x[i]+1), int(x[i+1])))
       x_all.extend(missing_x)
@@ -144,13 +181,18 @@ def estimate_signal_to_noise(x, y_noisy, y_smoothed, plot=False):
   #signal_to_noise.set_selected(x > 375, 0)
   if plot:
     from matplotlib import pyplot
-    pyplot.plot(x, y_noisy, linewidth=2)
-    pyplot.plot(x, y_smoothed, linewidth=2)
+    linewidth=2
+    pyplot.plot(x, y_noisy, linewidth=linewidth)
+    pyplot.plot(x, y_smoothed, linewidth=linewidth)
+    pyplot_label_axes()
     pyplot.show()
-    pyplot.plot(x, noise, linewidth=2)
-    pyplot.plot(x, flex.sqrt(sigma_sq), linewidth=2)
+    pyplot.plot(x, noise, linewidth=linewidth, label="noise")
+    pyplot.plot(x, flex.sqrt(sigma_sq), linewidth=linewidth, label="sigma")
+    pyplot_label_axes()
+    pyplot.legend(loc=2, prop={'size':20})
     pyplot.show()
-    pyplot.plot(x[:375], signal_to_noise[:375])
+    pyplot.plot(x, signal_to_noise, linewidth=linewidth)
+    pyplot_label_axes()
     pyplot.show()
 
   return signal_to_noise
