e44eae730171f826160f7bd654cd4470208edc5c
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-05-29 13:02:18 +0000

    Added python wrapper for pixel to millimeter stuff

---
+++ dxtbx/model/boost_python/pixel_to_millimeter.cc
@@ -0,0 +1,35 @@
+/*
+ * pixel_to_millimeter.cc
+ *
+ *  Copyright (C) 2013 Diamond Light Source
+ *
+ *  Author: James Parkhurst
+ *
+ *  This code is distributed under the BSD license, a copy of which is
+ *  included in the root directory of this package.
+ */
+#include <boost/python.hpp>
+#include <boost/python/def.hpp>
+#include <dxtbx/model/panel.h>
+#include <dxtbx/model/pixel_to_millimeter.h>
+
+namespace dxtbx { namespace model { namespace boost_python {
+
+  using namespace boost::python;
+
+  void export_pixel_to_millimeter()
+  {
+    class_<PxMmStrategy, boost::noncopyable>("PxMmStrategy", no_init)
+      .def("to_millimeter", &PxMmStrategy::to_millimeter, (
+        arg("panel"), arg("xy")))
+      .def("to_pixel", &PxMmStrategy::to_pixel, (
+        arg("panel"), arg("xy")));
+          
+    class_<SimplePxMmStrategy, bases<PxMmStrategy> >("SimplePxMmStrategy");
+
+    class_<ParallaxCorrectedPxMmStrategy, bases<SimplePxMmStrategy> >(
+      "ParallaxCorrectedPxMmStrategy", no_init)
+      .def(init<double>((arg("la"))));
+  }
+
+}}} // namespace = dxtbx::model::boost_python
