dce689a6c7ff01116922b60e7353166dc3144850
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-05-30 09:54:45 +0000

    Added support for new XPARM.XDS file in xparm.reader

--- iotbx/xds/xparm.py
+++ iotbx/xds/xparm.py
@@ -15,6 +15,50 @@ class reader:
     pass
 
   @staticmethod
+  def find_version(filename):
+    """Check the version if the given file is a (G)XPARM.XDS file.
+
+    If the file contains exactly 11 lines and 42 tokens, it is the old style
+    version 1 file. If the file starts with XPARM.XDS it is the new style
+    version 2 file.
+
+    Params:
+      filename The (G)XPARM.XDS filename
+
+    Returns:
+      The version or None if the file is not recognised
+
+    """
+    import os
+
+    # Check file contains 11 lines and 42 tokens
+    with open(filename, 'r') as file_handle:
+      tokens = []
+      old_style = True
+      for count, line in enumerate(file_handle):
+        line_tokens = line.split()
+        if count == 0:
+          if len(line_tokens) == 1 and line_tokens[0] == 'XPARM.XDS':
+              old_style=False
+        if old_style:
+          if count+1 > 11:
+            return None
+        else:
+          if count+1 > 14:
+            return None
+        tokens.extend(line_tokens)
+
+      if old_style:
+        if count+1 != 11 or len(tokens) != 42:
+          return None
+
+    # Is a (G)XPARM.XDS file
+    if old_style:
+      return 1
+    else:
+      return 2
+
+  @staticmethod
   def is_xparm_file(filename, check_filename = True):
     """Check if the given file is a (G)XPARM.XDS file.
 
@@ -28,44 +72,7 @@ class reader:
       True/False the file is a (G)XPARM.XDS file
 
     """
-    import os
-
-    # Check filename is (G)XPARM.XDS
-    basename = os.path.basename(filename)
-    if basename != 'XPARM.XDS' and basename != 'GXPARM.XDS' and check_filename:
-      return False
-
-    # Check file contains 11 lines and 42 tokens
-    with open(filename, 'r') as file_handle:
-      tokens = []
-      for count, line in enumerate(file_handle):
-        if count+1 > 11:
-          return False
-        tokens.extend(line.split())
-
-      if count+1 != 11 or len(tokens) != 42:
-        print 3
-        return False
-
-    # Is a (G)XPARM.XDS file
-    return True
-
-  def _read_lines(self, filename, check_filename = True):
-    """Read the (G)XPARM.XDS file.
-
-    If the file is not valid then return an IOError.
-
-    Params:
-      filename The path to the file
-
-    Returns:
-      The lines in the file as a list
-
-    """
-    if reader.is_xparm_file(filename, check_filename):
-      return open(filename, 'r').readlines()
-    else:
-      raise IOError("{0} is not a (G)XPARM.XDS file".format(filename))
+    return reader.find_version(filename) != None
 
   def read_file(self, filename, check_filename = True):
     """Read the XPARM.XDS/GXPARAM.XDS file.
@@ -77,16 +84,38 @@ class reader:
       filename The path to the file
 
     """
-    # Read the text from the file and split into an array of tokens
-    tokens = [l.split() for l in self._read_lines(filename, check_filename)]
 
-    # Read the parameters from the list of tokens
+    # Check version and read file
+    version = reader.find_version(filename)
+    if version != None:
+      tokens = [l.split() for l in open(filename, 'r').readlines()]
+    else:
+      raise IOError("{0} is not a (G)XPARM.XDS file".format(filename))
+
+    # Parse the tokens
+    if version == 1:
+      self.parse_version_1_tokens(tokens)
+    else:
+      self.parse_version_2_tokens(tokens)
+
+  def parse_version_1_tokens(self, tokens):
+    """Parse the version 1 tokens
+
+    Param:
+      tokens The list of tokens
+
+    """
+    # Scan and goniometer stuff
     self.starting_frame    = int(tokens[0][0])
     self.starting_angle    = float(tokens[0][1])
     self.oscillation_range = float(tokens[0][2])
     self.rotation_axis     = tuple(map(float, tokens[0][3:6]))
+
+    # Beam stuff
     self.wavelength        = float(tokens[1][0])
     self.beam_vector       = tuple(map(float, tokens[1][1:4]))
+
+    # Detector stuff
     self.detector_size     = tuple(map(int, tokens[2][0:2]))
     self.pixel_size        = tuple(map(float, tokens[2][2:4]))
     self.detector_distance = float(tokens[3][0])
@@ -94,8 +123,51 @@ class reader:
     self.detector_x_axis   = tuple(map(float, tokens[4]))
     self.detector_y_axis   = tuple(map(float, tokens[5]))
     self.detector_normal   = tuple(map(float, tokens[6]))
+
+    # Crystal stuff
     self.space_group       = int(tokens[7][0])
     self.unit_cell         = tuple(map(float, tokens[7][1:7]))
     self.unit_cell_a_axis  = tuple(map(float, tokens[8]))
     self.unit_cell_b_axis  = tuple(map(float, tokens[9]))
     self.unit_cell_c_axis  = tuple(map(float, tokens[10]))
+
+  def parse_version_2_tokens(self, tokens):
+    """Parse the version 2 tokens
+
+    Param:
+      tokens The list of tokens
+
+    """
+    # Scan and goniometer stuff
+    self.starting_frame    = int(tokens[1][0])
+    self.starting_angle    = float(tokens[1][1])
+    self.oscillation_range = float(tokens[1][2])
+    self.rotation_axis     = tuple(map(float, tokens[1][3:6]))
+
+    # Beam stuff
+    self.wavelength        = float(tokens[2][0])
+    self.beam_vector       = tuple(map(float, tokens[2][1:4]))
+
+    # Crystal stuff
+    self.space_group       = int(tokens[3][0])
+    self.unit_cell         = tuple(map(float, tokens[3][1:7]))
+    self.unit_cell_a_axis  = tuple(map(float, tokens[4]))
+    self.unit_cell_b_axis  = tuple(map(float, tokens[5]))
+    self.unit_cell_c_axis  = tuple(map(float, tokens[6]))
+
+    # Detector stuff
+    self.num_segments      = int(tokens[7][0])
+    self.detector_size     = tuple(map(int, tokens[7][0:2]))
+    self.pixel_size        = tuple(map(float, tokens[7][2:4]))
+    self.detector_origin   = tuple(map(float, tokens[8][0:2]))
+    self.detector_distance = float(tokens[8][2])
+    self.detector_x_axis   = tuple(map(float, tokens[9]))
+    self.detector_y_axis   = tuple(map(float, tokens[10]))
+    self.detector_normal   = tuple(map(float, tokens[11]))
+
+    # Loop through all the segments
+    self.segments = []
+    self.orientation = []
+    for i in range(self.num_segments):
+        self.segments.append(tuple(map(int, tokens[12+i*2])))
+        self.orientation.append(tuple(map(float, tokens[12+i*2+1])))
