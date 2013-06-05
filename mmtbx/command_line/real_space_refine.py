7d61bb6c17fca1e5dcc2b9932f3a419ae5451256
Pavel Afonine <pafonine@users.noreply.github.com>
2013-06-05 00:25:30 +0000

    use secondary structure restraints and morphing

--- mmtbx/command_line/real_space_refine.py
+++ mmtbx/command_line/real_space_refine.py
@@ -2,7 +2,7 @@ from __future__ import division
 # LIBTBX_SET_DISPATCHER_NAME phenix.real_space_refine
 
 from mmtbx.refinement import real_space
-import sys
+import sys, os
 from iotbx import file_reader
 from libtbx.utils import Sorry
 import iotbx.phil
@@ -19,14 +19,16 @@ import mmtbx.monomer_library.pdb_interpretation
 import mmtbx.restraints
 import mmtbx.refinement.real_space.driver
 import mmtbx.utils
+import mmtbx.secondary_structure
 
 if (1):
   random.seed(0)
   flex.set_random_seed(0)
-
+  
 # XXX TO BE MOVED OR RESTRUCTURED
 def get_processed_pdb_object(rama_potential, log,
       raw_records=None, pdb_file_name=None):
+  rama_potential = "emsley"
   assert [raw_records, pdb_file_name].count(None) == 1
   master_params = iotbx.phil.parse(
     input_string=mmtbx.monomer_library.pdb_interpretation.master_params_str,
@@ -34,8 +36,9 @@ def get_processed_pdb_object(rama_potential, log,
   if(rama_potential is not None):
     master_params.peptide_link.ramachandran_restraints=True
     master_params.peptide_link.rama_potential=rama_potential
+  master_params.nonbonded_weight=200
   mon_lib_srv = mmtbx.monomer_library.server.server()
-  return monomer_library.pdb_interpretation.process(
+  result = monomer_library.pdb_interpretation.process(
     mon_lib_srv              = monomer_library.server.server(),
     ener_lib                 = monomer_library.server.ener_lib(),
     params                   = master_params,
@@ -44,68 +47,82 @@ def get_processed_pdb_object(rama_potential, log,
     strict_conflict_handling = True,
     force_symmetry           = True,
     log                      = log)
+  return result
 
-def get_geometry_restraints_manager(processed_pdb_file, xray_structure):
+def get_geometry_restraints_manager(processed_pdb_file, xray_structure, log):
   sctr_keys=xray_structure.scattering_type_registry().type_count_dict().keys()
   has_hd = "H" in sctr_keys or "D" in sctr_keys
+  #
+  sec_str = mmtbx.secondary_structure.process_structure(
+    params             = None,
+    processed_pdb_file = processed_pdb_file,
+    tmp_dir            = os.getcwd(),
+    log                = log,
+    assume_hydrogens_all_missing=(not has_hd))
+  sec_str.initialize(log=log)
+  build_proxies = sec_str.create_hbond_proxies(
+    log          = log,
+    hbond_params = None)
+  hbond_params = build_proxies.proxies
+  hbond_params=None
+  #
   geometry = processed_pdb_file.geometry_restraints_manager(
     show_energies                = False,
     plain_pairs_radius           = 5,
+    hydrogen_bond_proxies        = hbond_params,
     assume_hydrogens_all_missing = not has_hd)
   restraints_manager = mmtbx.restraints.manager(
     geometry      = geometry,
     normalization = True)
   restraints_manager.crystal_symmetry = xray_structure.crystal_symmetry()
-  return restraints_manager
-#
-
-def process_inputs(args, log):
-  label = None
-  args_ = []
-  for arg in args:
-    if(arg.count("label=")):
-      label = arg.strip("label=")
-    else: args_.append(arg)
-  processed_pdb_file,xray_structure,ccp4_map,map_coefficients,pdb_file_name = \
-    [None,]*5
-  for arg in args_:
-    afo = file_reader.any_file(arg)
-    if(afo.file_type == "pdb"):
-      broadcast(m="Processing input PDB file:", log=log)
-      processed_pdb_file = get_processed_pdb_object(pdb_file_name=afo.file_name,
-        rama_potential=None, log = log)
-      xray_structure = processed_pdb_file.xray_structure(show_summary = True)
-      if(xray_structure is None):
-        raise Sorry("Cannot not extract xray_structure.")
-      pdb_file_name = afo.file_name
-    elif(afo.file_type == "ccp4_map"):
-      broadcast(m="Processing input CCP4 map file: %s"%afo.file_name, log=log)
-      ccp4_map = iotbx.ccp4_map.map_reader(file_name=afo.file_name)
-      ccp4_map.show_summary(prefix="  ")
-      #if(ccp4_map.space_group_number != 1):
-      #  raise Sorry("Map must be given in P1 box. Use phenix.map_box.")
-    elif(afo.file_type == "hkl"):
-      broadcast(m="Processing input file: %s"%afo.file_name, log=log)
-      miller_arrays = reflection_file_reader.any_reflection_file(file_name =
-        afo.file_name).as_miller_arrays()
-      print >> log, "  Available suitable arrays:"
-      suitable_arrays = []
-      suitable_labels = []
-      for ma in miller_arrays:
-        if(ma.is_complex_array()):
-          print >> log, "    ", ma.info().labels
-          suitable_arrays.append(ma)
-          suitable_labels.append(str(ma.info().labels))
-      if(  len(suitable_arrays) == 0): raise Sorry("No suitable arrays.")
-      elif(len(suitable_arrays) == 1): map_coefficients = ma
-      elif(len(suitable_arrays) >  1):
-        m="Multiple choices available: use 'label=' to select one of the above."
-        if(label is None): raise Sorry(m)
-        else:
-          for ma in miller_arrays:
-            if(ma.is_complex_array() and str(ma.info().labels) == label):
-              print >> log, "  Selected:", ma.info().labels
-              map_coefficients = ma
+  return restraints_manager  
+  
+def process_inputs(args, log=None):
+  got_pdb = False
+  got_hkl = False
+  got_map = False
+  processed_pdb_file = None
+  xray_structure     = None
+  ccp4_map           = None
+  map_coefficients   = None
+  pdb_file_name      = None
+  for arg in args :
+    if(os.path.isfile(arg)):
+      fro = file_reader.any_file(arg)
+      if(iotbx.pdb.is_pdb_file(arg)):
+        broadcast(m="Processing input PDB file:", log=log)
+        processed_pdb_file = get_processed_pdb_object(pdb_file_name=fro.file_name,
+          rama_potential=None, log = log)
+        xray_structure = processed_pdb_file.xray_structure(show_summary = True)
+        if(xray_structure is None):
+          raise Sorry("Cannot not extract xray_structure.")
+        pdb_file_name = fro.file_name
+        got_pdb = True
+      elif(fro.file_type == "ccp4_map"):
+        broadcast(m="Processing input CCP4 map file: %s"%fro.file_name, log=log)
+        ccp4_map = iotbx.ccp4_map.map_reader(file_name=fro.file_name)
+        ccp4_map.show_summary(prefix="  ")
+        got_map = True
+      else:
+        reflection_file = reflection_file_reader.any_reflection_file(
+          file_name=arg, ensure_read_access=False)
+        if(reflection_file.file_type() is not None):
+          map_coefficients = reflection_file_utils.extract_miller_array_from_file(
+            file_name = arg,
+            #label     = params.label,
+            type      = "complex",
+            log       = log)
+          got_hkl = True
+    else:
+      try: arg_phil = iotbx.phil.parse(arg)
+      except Exception: raise Sorry("Bad parameter: %s"%arg)
+      cmdline_phil.append(arg_phil)
+  if(not got_pdb): raise Sorry("PDB file is needed.")
+  if([got_map, got_hkl].count(True) == 2):
+    raise Sorry("Only one, map or map coefficients, file is needed.")
+  if([got_map, got_hkl].count(True) == 0):
+    raise Sorry("Map or map coefficients file is needed.")
+  #
   return group_args(
     processed_pdb_file = processed_pdb_file,
     xray_structure     = xray_structure,
@@ -191,7 +208,8 @@ def run(args, log = None, resolution_factor=1./4):
   broadcast(m="Creating geometry restraints:", log=log)
   geometry_restraints_manager = get_geometry_restraints_manager(
     processed_pdb_file = inputs.processed_pdb_file,
-    xray_structure     = inputs.xray_structure)
+    xray_structure     = inputs.xray_structure,
+    log                = log)
   target_map = extract_target_map_data_and_crystal_gridding(inputs=inputs,
     resolution_factor=resolution_factor)
   pdb_hierarchy = inputs.processed_pdb_file.all_chain_proxies.pdb_hierarchy
