c70ef3d6eb7cd9fa3e1bffbd9374a6ce566247d8
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-06-05 23:14:03 +0000

    refactoring so doc generation doesn't stall

--- xfel/command_line/plot_to_subpixel.py
+++ xfel/command_line/plot_to_subpixel.py
@@ -4,44 +4,49 @@ from __future__ import division
 import sys
 from scitbx.array_family import flex
 
-
-tile = flex.int()
-delx = flex.double()
-dely = flex.double()
-for line in sys.__stdin__.readlines():
-  if line.find("Tile")==0:
-    tokens = line.split()
-    #print tokens[1], int(tokens[1][:-1]),tokens
-    tile.append( int(tokens[1][:-1]) )
-    delx.append( float(tokens[7]) )
-    dely.append( float(tokens[9][:-1]) )
-    if len(tile)==64: break
-
-order = flex.sort_permutation(tile)
-for x in xrange(64):
-  #print tile[order[x]], delx[order[x]], dely[order[x]]
-  pass
-
-for x in xrange(8):
-  for y in xrange(8):
-    print "%5.2f,"%delx[order[x*8+y]], "%5.2f,"%dely[order[x*8+y]],
+def run (input=None) :
+  if (input is None) :
+    input = sys.__stdin__
+  tile = flex.int()
+  delx = flex.double()
+  dely = flex.double()
+  for line in input.readlines():
+    if line.find("Tile")==0:
+      tokens = line.split()
+      #print tokens[1], int(tokens[1][:-1]),tokens
+      tile.append( int(tokens[1][:-1]) )
+      delx.append( float(tokens[7]) )
+      dely.append( float(tokens[9][:-1]) )
+      if len(tile)==64: break
+  
+  order = flex.sort_permutation(tile)
+  for x in xrange(64):
+    #print tile[order[x]], delx[order[x]], dely[order[x]]
+    pass
+  
+  for x in xrange(8):
+    for y in xrange(8):
+      print "%5.2f,"%delx[order[x*8+y]], "%5.2f,"%dely[order[x*8+y]],
+    print
   print
-print
-
-for x in xrange(8):
-  for y in xrange(8):
-    print "%5.2f,"%dely[order[x*8+y]], "%5.2f,"%delx[order[x*8+y]],
+  
+  for x in xrange(8):
+    for y in xrange(8):
+      print "%5.2f,"%dely[order[x*8+y]], "%5.2f,"%delx[order[x*8+y]],
+    print
   print
-print
-
-for x in xrange(8):
-  for y in xrange(8):
-    print "%5.2f,"%-delx[order[x*8+y]], "%5.2f,"%-dely[order[x*8+y]],
+  
+  for x in xrange(8):
+    for y in xrange(8):
+      print "%5.2f,"%-delx[order[x*8+y]], "%5.2f,"%-dely[order[x*8+y]],
+    print
   print
-print
-
-for x in xrange(8):
-  for y in xrange(8):
-    print "%5.2f,"%-dely[order[x*8+y]], "%5.2f,"%-delx[order[x*8+y]],
+  
+  for x in xrange(8):
+    for y in xrange(8):
+      print "%5.2f,"%-dely[order[x*8+y]], "%5.2f,"%-delx[order[x*8+y]],
+    print
   print
-print
+
+if (__name__ == "__main__") :
+  run(sys.__stdin__)
