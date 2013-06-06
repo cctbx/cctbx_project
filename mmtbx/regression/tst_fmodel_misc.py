d438f017ce98d1bb07e7e38410cb5d87f0a7fea7
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-06-06 00:08:54 +0000

    added utility

--- mmtbx/regression/tst_fmodel_misc.py
+++ mmtbx/regression/tst_fmodel_misc.py
@@ -2,6 +2,7 @@
 from __future__ import division
 from libtbx.test_utils import approx_equal
 from libtbx.utils import null_out
+from libtbx import easy_run
 import os
 
 def exercise () :
@@ -13,6 +14,7 @@ def exercise () :
     os.remove("tst_fmodel_anomalous.mtz")
   pdb_file = make_fake_anomalous_data.write_pdb_input_cd_cl(
     file_base="tst_fmodel_anomalous")
+  # phenix.fmodel (with wavelength)
   args = [
     pdb_file,
     "high_resolution=1.0",
@@ -20,6 +22,7 @@ def exercise () :
     "label=F",
     "type=real",
     "output.file_name=tst_fmodel_anomalous.mtz",
+    "r_free_flags_fraction=0.1",
   ]
   fmodel.run(args=args, log=null_out())
   assert os.path.isfile("tst_fmodel_anomalous.mtz")
@@ -28,6 +31,11 @@ def exercise () :
   assert (array.anomalous_flag())
   anom_diffs = array.anomalous_differences()
   assert approx_equal(flex.max(anom_diffs.data()), 5.72, eps=0.01)
+  # mmtbx.fmodel_simple
+  result = easy_run.fully_buffered(
+    "mmtbx.fmodel_simple \"%s\" tst_fmodel_anomalous.mtz" % pdb_file
+    ).raise_if_errors()
+  assert (result.return_code == 0)
   print "OK"
 
 if (__name__ == "__main__") :
