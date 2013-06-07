5cb30e299f5b4ca223d8e2885684eb525c605de2
Richard Gildea <richard.gildea@diamond.ac.uk>
2013-06-07 13:11:21 +0000

    new command distl.sweep_strength - similar to distl.signal_strength, but acting on a sweep of images, and with tabulation of results and optional output of plots of distl results vs image number

---
+++ spotfinder/command_line/sweep_strength.py
@@ -0,0 +1,211 @@
+from __future__ import division
+# LIBTBX_SET_DISPATCHER_NAME distl.sweep_strength
+# LIBTBX_PRE_DISPATCHER_INCLUDE_SH export BOOST_ADAPTBX_FPE_DEFAULT=1
+import spotfinder
+import libtbx.phil
+from libtbx.utils import Sorry
+import sys, os
+from spotfinder.command_line.signal_strength import additional_spotfinder_phil_defs
+
+
+master_params = libtbx.phil.parse("""\
+distl {
+  res {
+    outer=None
+      .type=float
+      .help="High resolution limit in angstroms"
+    inner=None
+      .type=float
+      .help="Low resolution limit in angstroms"
+  }
+  plot {
+    file_name = None
+      .type = path
+      .help = "File path to output a plot of the distl results vs image number."
+              "Supported formats include png, jpeg, pdf, ps, eps, svg."
+  }
+  csv = None
+    .type = path
+    .help = "Optional file name for output of distl results in CSV format."
+  verbose = False
+    .type = bool
+    .help="Lengthy spot printout"
+  bins {
+    verbose = False
+      .type = bool
+      .help="Additional printout binned by resolution range"
+    N = 20
+      .type = int
+      .help="Maximum number of bins, but fewer can result if there are few spots"
+    corner = True
+      .type = bool
+      .help="Extend the binning all the way to detector corner, otherwise to outermost spot on first image"
+  }
+}
+
+%s
+"""%(spotfinder.labelit_related_commands)+additional_spotfinder_phil_defs)
+
+
+class Empty(object): pass
+
+def run_sweep_strength(image_file_names, params):
+  E = Empty()
+  E.argv=['Empty']
+  E.argv.extend(image_file_names)
+  return run_signal_strength_core(params,E)
+
+def run_signal_strength_core(params,E):
+  from spotfinder.applications.wrappers import DistlOrganizer
+  verbose = params.distl.verbose
+  if params.distl.res.inner!=None:
+    params.distl_lowres_limit = params.distl.res.inner
+  if params.distl.res.outer!=None:
+    params.force_method2_resolution_limit = params.distl.res.outer
+    params.distl_highres_limit = params.distl.res.outer
+
+  params.distl_force_binning = False
+  params.distl_permit_binning = False
+  params.wedgelimit = len(E.argv)
+  params.spotfinder_header_tests = False
+  Org = DistlOrganizer(verbose = False, argument_module=E,
+                       phil_params=params)
+  Org.printSpots()
+  return Org
+
+def table(spotfinder_results, keys):
+  headers = ['Image#']
+  headers.extend(keys)
+  rows = [headers]
+  for i in sorted(spotfinder_results.images.keys()):
+    spotfinder_one_image = spotfinder_results.images[i]
+    row = []
+    row.append("%i" %i)
+    for k in keys:
+      value = spotfinder_one_image[k]
+      try:
+        as_float = float(value)
+        as_int = int(value)
+        if float(as_int) == as_float:
+          row.append("%i" %as_int)
+        else:
+          row.append("%.2f" %as_float)
+      except (ValueError, TypeError):
+        row.append("%s" %value)
+    rows.append(row)
+  return rows
+
+def print_table(spotfinder_result, keys, out=None):
+  if out is None:
+    import sys
+    out = sys.stdout
+  from libtbx import table_utils
+  rows = table(spotfinder_result, keys)
+  print >> out, table_utils.format(rows, has_header=True)
+
+def as_columns(spotfinder_results):
+  d = {}
+  keys = (
+    'N_spots_total', 'N_spots_non-ice', 'N_spots_resolution',
+    'N_spots_unimodal', 'N_spots_inlier', 'intensity', 'area', 'neighbor',
+    'maxcel', 'resolution', 'distl_resolution', 'ice-ring_impact')
+
+  for i in sorted(spotfinder_results.images.keys()):
+    spotfinder_result = spotfinder_results.images[i]
+    for k in keys:
+      d.setdefault(k, [])
+      d[k].append(spotfinder_result[k])
+  return d
+
+def plot(spotfinder_results, file_name, format="png"):
+  from matplotlib import pyplot
+  columns = as_columns(spotfinder_results)
+  n_spots = columns.get('N_spots_inlier')
+  resolution = columns.get('resolution')
+  i_image = range(len(n_spots))
+  fig = pyplot.figure()
+  ax1 = fig.add_subplot(111)
+  ln1 = ax1.plot(i_image, n_spots, 'b--')
+  ax1.set_xlabel('Image #')
+  ax1.set_ylabel('# spots')
+  ax1.set_ylim(bottom=0)
+  ax2 = ax1.twinx()
+  ln2 = ax2.plot(i_image, resolution, 'r:')
+  ax2.set_ylabel(u'resolution (\u00c5)')
+  ax2.set_ylim(bottom=0)
+  pyplot.legend(ln1+ln2, ('# good spots', 'resolution (method 2)'), loc=0)
+  pyplot.savefig(file_name, dpi=600)
+
+def as_csv(spotfinder_results, out=None):
+  if out is None:
+    import sys
+    out = sys.stdout
+  columns = as_columns(spotfinder_results)
+  from iotbx import csv_utils
+  csv_utils.writer(out, columns.values(), field_names=columns.keys())
+
+
+def run(args, command_name="distl.sweep_strength"):
+  help_str="""\
+Similar to distl.signal_strength, but acting on a sweep of images, with
+tabulation of the results and optional output of results as CSV file and
+plots of number of spots and resolution with image number.
+"""
+
+  if (len(args) == 0 or args[0] in ["H","h","-H","-h","help","--help","-help"]):
+    print "usage:   %s image_prefix_*.img [parameter=value ...]" % command_name
+    print "example: %s lysozyme_*.img distl.minimum_spot_area=8 plot.file_name=lysozyme.pdf"%command_name
+    master_params.show(attributes_level=1,expert_level=1)
+    print help_str
+    return
+
+  print "%s: characterization of candidate Bragg spots"%command_name
+
+  phil_objects = []
+  argument_interpreter = master_params.command_line_argument_interpreter(
+    home_scope="distl")
+  image_file_names = []
+  moving_pdb_file_name = None
+
+  for arg in args:
+    if (os.path.isfile(arg)):
+      image_file_names.append(arg)
+    else:
+      try: command_line_params = argument_interpreter.process(arg=arg)
+      except KeyboardInterrupt: raise
+      except Exception: raise Sorry("Unknown file or keyword: %s" % arg)
+      else: phil_objects.append(command_line_params)
+
+  if len(image_file_names) < 2:
+    raise RuntimeError(
+      "Please provide more than one file. Alternatively use "
+      "distl.signal_strength to process a single image file.")
+
+  working_params = master_params.fetch(sources=phil_objects)
+  params = working_params.extract()
+
+  print "#Parameters used:"
+  print "#phil __ON__"
+  print
+  working_params = master_params.format(python_object=params)
+  working_params.show(expert_level=1)
+  print
+  print "#phil __OFF__"
+  print
+
+  from spotfinder.applications import signal_strength
+
+  spotfinder_results = run_sweep_strength(image_file_names, params)
+  print_table(spotfinder_results.S, keys=["N_spots_inlier", "resolution"])
+
+  csv_file_name = params.distl.csv
+  if csv_file_name is not None:
+    with open(csv_file_name, 'wb') as f:
+      as_csv(spotfinder_results.S, out=f)
+  plot_file_name = params.distl.plot.file_name
+  if plot_file_name is not None:
+    plot(spotfinder_results.S, file_name=plot_file_name)
+
+
+if (__name__ == "__main__"):
+  run(args=sys.argv[1:])
