91b1bb3c0231069a25c8787469adf203853120a7
Aaron S. Brewster <asbrewster@lbl.gov>
2013-06-17 23:55:13 +0000

    Change default averaging and hitfind image sizes for the MAR detector

--- xfel/cxi/cspad_ana/hitfinder_tbx.py
+++ xfel/cxi/cspad_ana/hitfinder_tbx.py
@@ -101,7 +101,7 @@ class distl_hitfinder(object):
       img_dim = (1765, 1765)
       pixel_size = cspad_tbx.pixel_size
     elif device == 'marccd':
-      img_dim = (2400, 2400)
+      img_dim = (4800, 4800)
       pixel_size = 0.079346
     else:
       raise RuntimeError("Unsupported device %s" % self.address)
