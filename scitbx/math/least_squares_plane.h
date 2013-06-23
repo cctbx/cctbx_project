1e01ef10b505df8c854cd6601def0c2d4d0361f8
Luc J. Bourhis <luc_j_bourhis@mac.com>
2013-06-23 21:37:58 +0000

    Fix bogus implementation.
    Document the class as well, spelling out very clearly the definition of the
    best plane it implements.

--- scitbx/math/least_squares_plane.h
+++ scitbx/math/least_squares_plane.h
@@ -3,9 +3,33 @@
 
 #include <scitbx/vec3.h>
 #include <scitbx/sym_mat3.h>
+#include <scitbx/math/accumulators.h>
+#include <scitbx/matrix/eigensystem.h>
 
 namespace scitbx { namespace math {
 
+/** "Best" plane through a set of points in dimension 3.
+
+The plane H implementd by this class is such that H minimises
+\f$ \sum_i d_i^2 \f$
+where \f$ d_i \f$ is the distance of the i-th point to H. The
+
+There is only one solution which is related to the first and
+second statistical moments of the set of points: the barycentre \f$ b \f$
+and the covariance tensor \f$ C \f$. Denoting by \f$ p_i \f$ the
+column-vector of coordinates of the i-th point, they are defined as
+
+- \f$ b = \frac{1}{n} \sum_i p_i \f$
+- \f$ C = \frac{1}{n} \sum_i (p_i - b)(p_i - b)^T \f$
+
+where \f$ n \f$ is the number of points.
+
+Then H is the hyperplane passing through \f$ b \f$ and whose normal \f$ n \f$ is
+the eigenvector corresponding to the smallest eigenvalue of \f$ C \f$.
+Thus the equation of the hyperplane is \f$ n^T p = d \f$
+where \f$ d = n^T b \f$ is the distance of H to the origin (this is
+the so-called Hessian normal form of the equation of H).
+*/
 template<typename ScalarType=double>
 class least_squares_plane
 {
@@ -14,32 +38,26 @@ class least_squares_plane
     typedef vec3<scalar_type> vector_type;
     typedef mat3<scalar_type> matrix_type;
 
-    least_squares_plane(af::const_ref<vector_type> const &points)
-    {
-      float_type xx=0, yy=0, zz=0, xy=0, xz=0, yz=0;
-      vector_type b(0,0,0);
-      for(int i=0; i < points.size(); ++i) {
-        vector_type const &p = points[i];
-        b += p;
-        float_type x=p[0], y=p[1], z=p[2];
-        xx += x*x;
-        yy += y*y;
-        zz += z*z;
-        xy += x*y;
-        xz += x*z;
-        yz += y*z;
-      }
-      sym_mat3<float_type> m(xx, yy, zz, xy, xz, yz);
-      vector_type u = m.inverse()*b;
-      d = 1/u.length();
-      n = d*u;
+    /// Construct for a set of points with the given 3-vector of coordinates
+    least_squares_plane(af::const_ref<vector_type> const &points) {
+      scitbx::math::accumulator::inertia_accumulator<scalar_type> acc;
+      for(int i=0; i<points.size(); ++i) acc(points[i]);
+      eigen_decomposition_type eigen(acc.covariance_matrix());
+      // eigenvalues are sorted in decreasing order
+      scalar_type *v = eigen.vectors().begin();
+      n = vector_type(v[6], v[7], v[8]).normalize();
+      d = n*acc.center_of_mass();
     }
 
+    /// Normal \f$ n \f$ to the plane
     vector_type const &normal() { return n; }
 
+    /// Distance \f$ d \f$ of the origin to the plane
     scalar_type distance_to_origin() { return d; }
 
   private:
+    typedef scitbx::matrix::eigensystem::real_symmetric<scalar_type>
+            eigen_decomposition_type;
     vector_type n;
     scalar_type d;
 };
