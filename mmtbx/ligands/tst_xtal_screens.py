1ae5dd0fa8813550bf6aa540ddc998bd624cfc65
Nader Morshed <nmorshed@users.noreply.github.com>
2013-07-02 23:34:47 +0000

    Added a few more negative tests and fixed the calls on the positive test cases

--- mmtbx/ligands/tst_xtal_screens.py
+++ mmtbx/ligands/tst_xtal_screens.py
@@ -5,34 +5,43 @@ def exercise () :
   from mmtbx.ligands import xtal_screens
   s = xtal_screens.server()
 
-  condition = s.get_condition("Crystal Screen", "4")
+  try:
+    condition = s.get_condition("chrysler", "1")
+    assert False
+  except RuntimeError:
+    pass
+
+  condition = s.get_condition("crystal_screen", "4")
   assert condition.pH() == 8.5
   assert condition.ligands() == ["TRS", "CL", "NH4", "SO4"]
 
-  condition = s.get_condition("Crystal Screen II", "E6")
+  condition = s.get_condition("crystal_screen", "E6")
   assert condition.pH() == 7.0
   assert condition.ligands() == ["IMD"]
 
-  condition = s.get_condition("Crystal Screen HT", "A9")
+  condition = s.get_condition("crystal_screen", "A9")
   assert condition.pH() == 5.6
   assert condition.ligands() == ["NH4", "ACT", "NA", "FLC", "PEG"]
 
-  condition = s.get_condition("Index", "5")
+  condition = s.get_condition("index", "5")
   assert condition.pH() == 7.5
   assert condition.ligands() == ["EPE", "NH4", "SO4"]
 
-  condition = s.get_condition("Index HT", "F12")
+  condition = s.get_condition("index", "F12")
   assert condition.pH() == 7.5
   assert condition.ligands() == ["NA", "CL", "EPE", "PEG"]
 
-  condition = s.get_condition("PEG/Ion", "48")
+  condition = s.get_condition("peg_ion", "48")
   assert condition.pH() == None
-  assert condition.ligands() == ["MLT", "LMR", "PEG"]
+  assert condition.ligands() == ["NH4", "FLC", "PEG"]
 
-  condition = s.get_condition("PEG/Ion 2", "49")
-  assert condition == None
+  try:
+    condition = s.get_condition("peg_ion", "49")
+    assert False
+  except RuntimeError:
+    pass
 
-  condition = s.get_condition("PEG/Ion HT", "G12")
+  condition = s.get_condition("peg_ion", "G12")
   assert condition.pH() == None
   assert condition.ligands() == ["CIT", "BTB", "TME", "PEG"]
 
