ca76e9ce024c50a431f875c6773a694d576608ed
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-07-16 22:49:28 +0000

    refactoring: detached geometry analysis from nested loop and main class, replaced custom math implementations with scitbx.matrix methods

--- mmtbx/validation/cbetadev.py
+++ mmtbx/validation/cbetadev.py
@@ -1,8 +1,9 @@
 from __future__ import division
-import sys, os, math
-from cctbx import geometry_restraints
 import iotbx.phil
+from scitbx.matrix import col, dihedral_angle, rotate_point_around_axis
 from libtbx.utils import Usage
+import os
+import sys
 
 def get_master_phil():
   return iotbx.phil.parse(
@@ -141,6 +142,8 @@ Example:
         for rg in chain.residue_groups():
           for i_cf,cf in enumerate(rg.conformers()):
             for i_residue,residue in enumerate(cf.residues()):
+              if (residue.resname == "GLY") :
+                continue
               is_first = (i_cf == 0)
               is_alt_conf = False
               relevant_atoms = {}
@@ -150,264 +153,59 @@ Example:
                   if (len(atom.parent().altloc) != 0):
                     is_alt_conf = True
               if ((is_first or is_alt_conf) and len(relevant_atoms) == 4):
-                resCA = relevant_atoms[" CA "]
-                resN  = relevant_atoms[" N  "]
-                resC  = relevant_atoms[" C  "]
-                resCB = relevant_atoms[" CB "]
-                dist, angleCAB, dihedralNCAB, angleNAB, dihedralCNAB, angleideal= \
-                  self.idealized_calpha_angles(residue=residue)
-                betaNCAB = self.construct_fourth(resN,
-                                                 resCA,
-                                                 resC,
-                                                 dist,
-                                                 angleCAB,
-                                                 dihedralNCAB,
-                                                 method="NCAB")
-                betaCNAB = self.construct_fourth(resN,
-                                                 resCA,
-                                                 resC,
-                                                 dist,
-                                                 angleNAB,
-                                                 dihedralCNAB,
-                                                 method="CNAB")
-                betaxyz = [(betaNCAB[0]+betaCNAB[0])/2,
-                           (betaNCAB[1]+betaCNAB[1])/2,
-                           (betaNCAB[2]+betaCNAB[2])/2]
-                betadist = self.distance(resCA.xyz,betaxyz)
-                if betadist == 0:
-                  continue
-                if(betadist != dist):
-                  distTemp = [(betaxyz[0]-resCA.xyz[0]),
-                              (betaxyz[1]-resCA.xyz[1]),
-                              (betaxyz[2]-resCA.xyz[2])]
-                  betaxyz = [(resCA.xyz[0]+distTemp[0]*dist/betadist),
-                             (resCA.xyz[1]+distTemp[1]*dist/betadist),
-                             (resCA.xyz[2]+distTemp[2]*dist/betadist)]
-                if(residue.resname != "GLY"):
-                  dev = self.distance(resCB.xyz,betaxyz)
-                  if(dev >=0.25 or outliers_only==False):
-                    if(dev >=0.25):
-                      cbetadev_ctr+=1
-                      self.num_outliers+=1
-                    d = geometry_restraints.dihedral(sites=[resN.xyz,resCA.xyz,betaxyz,resCB.xyz],
-                                                     angle_ideal=0,
-                                                     weight=1)
-                    dihedralNABB = d.angle_model
-
-                    #internal version of dihedral calculation used to test difference
-                    #dihedralTemp = dihedral4pt(resN.xyz,resCA.xyz,betaxyz,resCB.xyz)
-                    #dihedralNABB = dihedralTemp
-
-                    PDBfileStr = ""
-                    if(filename is not None):
-                      PDBfileStr = os.path.basename(filename)[:-4]
-                    if (is_alt_conf):
-                      altchar = cf.altloc.lower()
-                    else:
-                      altchar = " "
-                    res=residue.resname.lower()
-                    sub=chain.id
-                    if(len(sub)==1):
-                      sub=" "+sub
-                    resnum=residue.resid()
-                    occ = resCB.occ
-                    analysis += '%s :%s:%s:%s:%5s:%7.3f:%7.2f:%7.2f:%s:\n' % \
-                      (PDBfileStr,altchar,res,sub,resnum,dev,dihedralNABB,occ,altchar)
-                    if (dev >= 0.25):
-                      self.outliers += '%s :%s:%s:%s:%5s:%7.3f:%7.2f:%7.2f:%s:\n' % \
-                        (PDBfileStr,altchar,res,sub,resnum,dev,dihedralNABB,occ,altchar)
-                    key = altchar+res+sub+resnum
-                    self.beta_ideal[key] = betaxyz
-                    output_list.append([PDBfileStr,
-                                        altchar,
-                                        res,
-                                        sub,
-                                        resnum[0:4],
-                                        resnum[4:],
-                                        dev,
-                                        dihedralNABB,
-                                        occ,
-                                        altchar,
-                                        resCB.xyz])
-    summary = 'SUMMARY: %d C-beta Deviation >= 0.25 Angstrom (Goal: 0)' % (cbetadev_ctr)
-    self.summary = 'SUMMARY: %d C-beta Deviation >= 0.25 Angstrom (Goal: 0)' % (cbetadev_ctr)
+                result = calculate_ideal_and_deviation(
+                  relevant_atoms=relevant_atoms,
+                  resname=residue.resname)
+                dev = result.deviation
+                dihedralNABB = result.dihedral
+                betaxyz = result.ideal
+                if (dev is None) : continue
+                if(dev >=0.25 or outliers_only==False):
+                  if(dev >=0.25):
+                    cbetadev_ctr+=1
+                    self.num_outliers+=1
+                  PDBfileStr = ""
+                  if(filename is not None):
+                    PDBfileStr = os.path.basename(filename)[:-4]
+                  if (is_alt_conf):
+                    altchar = cf.altloc.lower()
+                  else:
+                    altchar = " "
+                  res=residue.resname.lower()
+                  sub=chain.id
+                  if(len(sub)==1):
+                    sub=" "+sub
+                  resnum=residue.resid()
+                  resCB = relevant_atoms[" CB "]
+                  occ = resCB.occ
+                  analysis += '%s :%s:%s:%s:%5s:%7.3f:%7.2f:%7.2f:%s:\n' % \
+                    (PDBfileStr,altchar,res,sub,resnum,dev,dihedralNABB,occ,
+                     altchar)
+                  if (dev >= 0.25):
+                    self.outliers += \
+                      '%s :%s:%s:%s:%5s:%7.3f:%7.2f:%7.2f:%s:\n' % \
+                      (PDBfileStr,altchar,res,sub,resnum,dev,dihedralNABB,occ,
+                       altchar)
+                  key = altchar+res+sub+resnum
+                  self.beta_ideal[key] = betaxyz
+                  output_list.append([PDBfileStr,
+                                      altchar,
+                                      res,
+                                      sub,
+                                      resnum[0:4],
+                                      resnum[4:],
+                                      dev,
+                                      dihedralNABB,
+                                      occ,
+                                      altchar,
+                                      resCB.xyz])
+    summary = 'SUMMARY: %d C-beta Deviation >= 0.25 Angstrom (Goal: 0)' % \
+      (cbetadev_ctr)
+    self.summary = 'SUMMARY: %d C-beta Deviation >= 0.25 Angstrom (Goal: 0)' %\
+      (cbetadev_ctr)
     self.analysis = analysis.rstrip()
     return self.analysis, summary, output_list
-  #}}}
 
-  #{{{ idealized_calpha_angles
-  def idealized_calpha_angles(self, residue):
-    if(residue.resname == "ALA"):
-      dist = 1.536
-      angleCAB = 110.1
-      dihedralNCAB = 122.9
-      angleNAB = 110.6
-      dihedralCNAB = -122.6
-      angleideal = 111.2
-    elif(residue.resname == "PRO"):
-      dist = 1.530
-      angleCAB = 112.2
-      dihedralNCAB = 115.1
-      angleNAB = 103.0
-      dihedralCNAB = -120.7
-      angleideal = 111.8
-    elif(residue.resname == "VAL") or (residue.resname == "THR") or (residue.resname == "ILE"):
-      dist = 1.540
-      angleCAB = 109.1
-      dihedralNCAB = 123.4
-      angleNAB = 111.5
-      dihedralCNAB = -122.0
-      angleideal = 111.2
-    elif(residue.resname == "GLY"):
-      dist = 1.10
-      angleCAB = 109.3
-      dihedralNCAB = 121.6
-      angleNAB = 109.3
-      dihedralCNAB = -121.6
-      angleideal = 112.5
-    else:
-      dist = 1.530
-      angleCAB = 110.1
-      dihedralNCAB = 122.8
-      angleNAB = 110.5
-      dihedralCNAB = -122.6
-      angleideal = 111.2
-    return dist, angleCAB, dihedralNCAB, angleNAB, dihedralCNAB, angleideal
-  #}}}
-
-  #{{{ construct_fourth
-  def construct_fourth(self, resN,resCA,resC,dist,angle,dihedral,method="NCAB"):
-    if (resN is not None and resCA is not None and resC is not None):
-      if (method is "NCAB"):
-        res0 = resN
-        res1 = resC
-        res2 = resCA
-      elif (method is "CNAB"):
-        res0 = resC
-        res1 = resN
-        res2 = resCA
-      a = [(res2.xyz[0] - res1.xyz[0]),(res2.xyz[1] - res1.xyz[1]),(res2.xyz[2] - res1.xyz[2])]
-      b = [(res0.xyz[0] - res1.xyz[0]),(res0.xyz[1] - res1.xyz[1]),(res0.xyz[2] - res1.xyz[2])]
-      c = self.cross(a,b)
-      cmag = self.mag(c)
-      if(cmag > 0.000001):
-        c=[(c[0]*dist/cmag),(c[1]*dist/cmag),(c[2]*dist/cmag)]
-      c=[(c[0] + res2.xyz[0]),(c[1] + res2.xyz[1]),(c[2] + res2.xyz[2])]
-      d = c
-      angledhdrl = dihedral - 90
-      a = [res1.xyz[0],res1.xyz[1],res1.xyz[2]]
-      b = [res2.xyz[0],res2.xyz[1],res2.xyz[2]]
-      newD = self.doaxisrot(d,angledhdrl,a,b)
-      a = [(newD[0]-res2.xyz[0]),(newD[1]-res2.xyz[1]),(newD[2]-res2.xyz[2])]
-      b = [(res1.xyz[0]-res2.xyz[0]),(res1.xyz[1]-res2.xyz[1]),(res1.xyz[2]-res2.xyz[2])]
-      c = self.cross(a,b)
-      cmag = self.mag(c)
-      if(cmag > 0.000001):
-        c=[(c[0]*dist/cmag),(c[1]*dist/cmag),(c[2]*dist/cmag)]
-      angledhdrl = 90 - angle;
-      a = [res2.xyz[0],res2.xyz[1],res2.xyz[2]]
-      c = [(c[0]+res2.xyz[0]),(c[1]+res2.xyz[1]),(c[2]+res2.xyz[2])]
-      b = c
-      return self.doaxisrot(newD,angledhdrl,a,b)
-  #}}}
-
-  #{{{ doaxisrot
-  def doaxisrot(self, d,theta,res1,res2):
-    LOK = True
-    xx = res2[0] - res1[0]
-    yy = res2[1] - res1[1]
-    zz = res2[2] - res1[2]
-    if(xx**2 + yy**2 + zz**2 > 0.000001):
-      cosn1 = xx/(xx**2 + yy**2 + zz**2)**.5
-      cosn2 = yy/(xx**2 + yy**2 + zz**2)**.5
-      cosn3 = zz/(xx**2 + yy**2 + zz**2)**.5
-    else:
-      cosn1 = xx
-      cosn2 = yy
-      cosn3 = zz
-    while(theta > 360):
-      theta = theta - 360
-    while(theta < -360):
-      theta = theta + 360
-    if(theta > 180):
-      theta = theta - 360
-    if(theta < -180):
-      theta = theta + 360
-    if(theta > -.0001 and theta < 0.0001):
-      LOK = False
-    if(LOK):
-      costheta = math.cos(theta*3.14159*2/360)
-      sintheta = math.sin(theta*3.14159*2/360)
-      a1 = [(cosn1**2 + (1-cosn1**2)*costheta),
-            (cosn1*cosn2*(1-costheta)+cosn3*sintheta),
-            (cosn1*cosn3*(1-costheta) - cosn2*sintheta)]
-      a2 = [(cosn1*cosn2*(1-costheta) - cosn3*sintheta),
-            (cosn2**2 + (1-cosn2**2)*costheta),
-            (cosn2*cosn3*(1-costheta) + cosn1*sintheta)]
-      a3 = [(cosn1*cosn3*(1-costheta)+cosn2*sintheta),
-            (cosn2*cosn3*(1-costheta)-cosn1*sintheta),
-            (cosn3**2+(1-cosn3**2)*costheta)]
-      f1 = [(d[0]-res2[0]),(d[1]-res2[1]),(d[2]-res2[2])]
-      f2 = [(f1[0]*a1[0] + f1[1]*a2[0] + f1[2]*a3[0]),
-            (f1[0]*a1[1] + f1[1]*a2[1] + f1[2]*a3[1]),
-            (f1[0]*a1[2]+f1[1]*a2[2]+f1[2]*a3[2])]
-      return [(f2[0]+res2[0]),(f2[1]+res2[1]),(f2[2]+res2[2])]
-  #}}}
-
-  #{{{ dihedral4pt
-  def dihedral4pt(self, p1,p2,p3,p4):
-    a = [(p1[0]-p2[0]),(p1[1]-p2[1]),(p1[2]-p2[2])]
-    b = [(p3[0]-p2[0]),(p3[1]-p2[1]),(p3[2]-p2[2])]
-    d = cross(a,b)
-    b = [(p2[0]-p3[0]),(p2[1]-p3[1]),(p2[2]-p3[2])]
-    c = [(p4[0]-p3[0]),(p4[1]-p3[1]),(p4[2]-p3[2])]
-    e = cross(b,c)
-    dot = dotProduct(d,e)
-    dmag = mag(d)
-    emag = mag(e)
-    if(dmag*emag < 0.0001):
-      angle = 0.0
-    else:
-      angle = math.acos( dot/(dmag*emag) )
-    anglehdrl = angle*360.0/(2*math.pi)
-    f = cross(d,b)
-    dot = dotProduct(f,e)
-    fmag = mag(f)
-    if(fmag*emag < 0.0001):
-      angle = 0.0
-    else:
-      angle = math.acos( dot/(fmag*emag) )
-    angle = angle*360.0/(2*math.pi)
-    if(angle > 90.0):
-      anglehdrl = -anglehdrl
-    return anglehdrl
-  #}}}
-
-  #{{{ mag
-  def mag(self, v):
-    return (v[0]**2 + v[1]**2 + v[2]**2)**0.5
-  #}}}
-
-  #{{{ cross
-  def cross(self, v1,v2):
-    newcoords = [0,0,0]
-    newcoords[0] = v1[1]*v2[2]-v1[2]*v2[1]
-    newcoords[1] = v1[2]*v2[0]-v1[0]*v2[2]
-    newcoords[2] = v1[0]*v2[1]-v1[1]*v2[0]
-    return newcoords
-  #}}}
-
-  #{{{ dotProduct
-  def dotProduct(self, v1,v2):
-    return (v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2])
-  #}}}
-
-  #{{{ distance
-  def distance(self, a,b):
-    return ((a[0]-b[0])**2+(a[1]-b[1])**2+(a[2]-b[2])**2)**0.5
-  #}}}
-
-  #{{{ get_functions
   def get_outlier_count(self):
     return self.num_outliers
 
@@ -422,3 +220,126 @@ Example:
 
   def get_beta_ideal(self):
     return self.beta_ideal
+
+class calculate_ideal_and_deviation (object) :
+  __slots__ = ["deviation", "ideal", "dihedral"]
+  def __init__ (self, relevant_atoms, resname) :
+    assert (resname != "GLY")
+    self.deviation = None
+    self.ideal = None
+    self.dihedral = None
+    resCA = relevant_atoms[" CA "]
+    resN  = relevant_atoms[" N  "]
+    resC  = relevant_atoms[" C  "]
+    resCB = relevant_atoms[" CB "]
+    dist, angleCAB, dihedralNCAB, angleNAB, dihedralCNAB, angleideal= \
+      idealized_calpha_angles(resname)
+    betaNCAB = construct_fourth(resN,
+                                resCA,
+                                resC,
+                                dist,
+                                angleCAB,
+                                dihedralNCAB,
+                                method="NCAB")
+    betaCNAB = construct_fourth(resN,
+                                resCA,
+                                resC,
+                                dist,
+                                angleNAB,
+                                dihedralCNAB,
+                                method="CNAB")
+    if (not None in [betaNCAB, betaCNAB]) :
+      betaxyz = (col(betaNCAB) + col(betaCNAB)) / 2
+      betadist = abs(col(resCA.xyz) - betaxyz)
+      if betadist != 0:
+        if(betadist != dist):
+          distTemp = betaxyz - col(resCA.xyz)
+          betaxyz = col(resCA.xyz) + distTemp * dist/betadist
+        self.deviation = abs(col(resCB.xyz) - betaxyz)
+        self.dihedral = dihedral_angle(
+          sites=[resN.xyz,resCA.xyz,betaxyz.elems,resCB.xyz], deg=True)
+        self.ideal = betaxyz.elems
+
+def idealized_calpha_angles(resname):
+  if (resname == "ALA"):
+    dist = 1.536
+    angleCAB = 110.1
+    dihedralNCAB = 122.9
+    angleNAB = 110.6
+    dihedralCNAB = -122.6
+    angleideal = 111.2
+  elif (resname == "PRO"):
+    dist = 1.530
+    angleCAB = 112.2
+    dihedralNCAB = 115.1
+    angleNAB = 103.0
+    dihedralCNAB = -120.7
+    angleideal = 111.8
+  elif (resname in ["VAL", "THR", "ILE"]) :
+    dist = 1.540
+    angleCAB = 109.1
+    dihedralNCAB = 123.4
+    angleNAB = 111.5
+    dihedralCNAB = -122.0
+    angleideal = 111.2
+  elif (resname == "GLY"):
+    dist = 1.10
+    angleCAB = 109.3
+    dihedralNCAB = 121.6
+    angleNAB = 109.3
+    dihedralCNAB = -121.6
+    angleideal = 112.5
+  else:
+    dist = 1.530
+    angleCAB = 110.1
+    dihedralNCAB = 122.8
+    angleNAB = 110.5
+    dihedralCNAB = -122.6
+    angleideal = 111.2
+  return dist, angleCAB, dihedralNCAB, angleNAB, dihedralCNAB, angleideal
+
+def construct_fourth(resN,resCA,resC,dist,angle,dihedral,method="NCAB"):
+  if (not None in [resN, resCA, resC]) :
+    if (method == "NCAB"):
+      res0 = resN
+      res1 = resC
+      res2 = resCA
+    elif (method == "CNAB"):
+      res0 = resC
+      res1 = resN
+      res2 = resCA
+    a = col(res2.xyz) - col(res1.xyz)
+    b = col(res0.xyz) - col(res1.xyz)
+    c = a.cross(b)
+    cmag = abs(c)
+    if(cmag > 0.000001):
+      c *= dist/cmag
+    c += col(res2.xyz)
+    d = c
+    angledhdrl = dihedral - 90
+    a = col(res1.xyz)
+    b = col(res2.xyz)
+    # XXX is there an equivalent method for 'col'?
+    newD = col(rotate_point_around_axis(
+      axis_point_1=res1.xyz,
+      axis_point_2=res2.xyz,
+      point=d.elems,
+      angle=angledhdrl,
+      deg=True))
+    a = newD - col(res2.xyz)
+    b = col(res1.xyz) - col(res2.xyz)
+    c = a.cross(b)
+    cmag = abs(c)
+    if(cmag > 0.000001):
+      c *= dist/cmag
+    angledhdrl = 90 - angle;
+    a = col(res2.xyz)
+    c += a
+    b = c
+    return rotate_point_around_axis(
+      axis_point_1=a.elems,
+      axis_point_2=b.elems,
+      point=newD.elems,
+      angle=angledhdrl,
+      deg=True)
+  return None
