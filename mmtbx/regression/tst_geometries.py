1c7ec62fffec607693e6df1e6c13ebcd4482d21a
Nader Morshed <nmorshed@users.noreply.github.com>
2013-07-17 20:49:05 +0000

    Added a test for mmtbx.ions.geometry.py

---
+++ mmtbx/regression/tst_geometries.py
@@ -0,0 +1,47 @@
+ # -*- coding: utf-8; py-indent-offset: 2 -*-
+from __future__ import division
+
+import os
+
+import libtbx
+from mmtbx.ions.geometry import find_coordination_geometry
+from mmtbx.ions import Manager
+from iotbx import file_reader
+from cctbx.eltbx import chemical_elements
+
+def exercise () :
+  if not libtbx.env.has_module("phenix_regression"):
+    print "Skipping {}".format(os.path.split(__file__)[1])
+    return
+
+  models = {
+    "3rva": (["octahedral"], ["trigonal_bipyramid"], ["square_planar"]),
+  }
+
+  for model, geometries in models.items():
+    pdb_path = libtbx.env.find_in_repositories(
+      relative_path = "phenix_regression/mmtbx/geometry/{}.pdb".format(model),
+      test = os.path.isfile)
+    pdb_in = file_reader.any_file(pdb_path, force_type = "pdb")
+    manager = Manager(
+      fmodel = None,
+      pdb_hierarchy = pdb_in.file_object.construct_hierarchy(),
+      xray_structure = pdb_in.file_object.xray_structure_simple(),
+      connectivity = pdb_in.file_object.extract_connectivity())
+
+    elements = chemical_elements.proper_upper_list()
+    elements = [element for element in elements
+                if element.strip().upper() not in ["O", "C", "H", "N"]]
+
+    metals = [i_seq for i_seq, atom in enumerate(manager.pdb_atoms)
+             if atom.fetch_labels().resname.strip().upper() in elements]
+    for metal, geometry in zip(metals, geometries):
+      contacts = manager.find_nearby_atoms(metal, filter_by_two_fofc = False)
+      found = find_coordination_geometry(contacts)
+      geometry_names = [i[0] for i in found]
+      assert geometry_names == geometry
+
+  print "OK"
+
+if __name__ == "__main__":
+  exercise()
