866464ebb727b24998c24255a536305fef9d2b24
Youval Dar <youdar@users.noreply.github.com>
2013-08-16 21:17:40 +0000

    Add --help to phenix.pdb.bioassembly_reconstruction command line

--- iotbx/command_line/bioassembly_reconstruction.py
+++ iotbx/command_line/bioassembly_reconstruction.py
@@ -10,7 +10,8 @@ def run(args, command_name="phenix.pdb.bioassembly_reconstruction"):
   ''' (string) -> file
   
   Reconstruction of the biological assembly multimer by applying
-  BIOMT transformation, from the pdb file, to all chains.
+  BIOMT transformation, from the pdb file, to all chains in file.
+  
   Produce a new pdb file containg the biological assembly multimer information
   
   Arguments:
@@ -18,16 +19,21 @@ def run(args, command_name="phenix.pdb.bioassembly_reconstruction"):
         if no file name is given, the output file will have the name 'copy_file_name.pdb'
         
   >>> phenix.pdb.bioassembly_reconstruction 'input_file_name.pdb'
-  file copy_input_file_name.pdb added in the current directory
+  bio-assembly reconstructed pdb file was added to your current directory
+  /path/copy_input_file_name.pdb
   
    >>> phenix.pdb.bioassembly_reconstruction 'input_file_name.pdb' 'output_file_name.pdb'
-  file output_file_name.pdb added in the current directory
+  bio-assembly reconstructed pdb file was added to your current directory
+  /path/output_file_name.pdb
   
   '''
   if (len(args) == 0): 
-    raise Sorry('No input filename is given. Please provide a pdb file name.')
+    raise Sorry('No input filename is given. Please provide a pdb file name. \n' + \
+                '>>> phenix.pdb.bioassembly_reconstruction "input_file_name.pdb"')
   elif (len(args) > 2):
     raise Sorry('To many input parameters are given')
+  elif args[0] in ['--help','-help','-h']:
+    print help(run)
   elif not os.path.isfile(args[0]):
     print 'There is no file {} in the current directory \n {}'.format(args[0],os.getcwd())
     raise Sorry('File name error')
