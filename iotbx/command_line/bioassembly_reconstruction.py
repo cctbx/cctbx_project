1574552aa2d377dbacb72a664dc6f094cbe3e5da
ydr <y0udar@users.noreply.github.com>
2013-08-20 20:32:07 +0000

    update handling of file names

--- iotbx/command_line/bioassembly_reconstruction.py
+++ iotbx/command_line/bioassembly_reconstruction.py
@@ -6,21 +6,21 @@ from libtbx.utils import Sorry
 from  iotbx.pdb.multimer_reconstruction import multimer
 import sys, os
 
-def run(args, command_name="phenix.pdb.bioassembly_reconstruction"):
+def bioassembly_reconstruction(args, command_name="phenix.pdb.bioassembly_reconstruction"):
   ''' (string) -> file
 
   Reconstruction of the biological assembly multimer by applying
-  BIOMT transformation, from the pdb file, to all chains in file.
+  BIOMT transformation, from the REMARK 350 in the pdb file, to all chains in file.
 
   Produce a new pdb file containg the biological assembly multimer information
 
   Arguments:
   args: a string containing the pdb file name, 'file_name.pdb' and an optional output file name
-        if no file name is given, the output file will have the name 'copy_file_name.pdb'
+        if no file name is given, the output file will have the name 'biological_assembly_file_name.pdb'
 
   >>> phenix.pdb.bioassembly_reconstruction 'input_file_name.pdb'
   bio-assembly reconstructed pdb file was added to your current directory
-  /path/copy_input_file_name.pdb
+  /path/biological_assembly_input_file_name.pdb
 
    >>> phenix.pdb.bioassembly_reconstruction 'input_file_name.pdb' 'output_file_name.pdb'
   bio-assembly reconstructed pdb file was added to your current directory
@@ -30,28 +30,26 @@ def run(args, command_name="phenix.pdb.bioassembly_reconstruction"):
   if (len(args) == 0):
     raise Sorry('No input filename is given. Please provide a pdb file name. \n' + \
                 '>>> phenix.pdb.bioassembly_reconstruction "input_file_name.pdb"')
+  elif ('--help' in args) or ('-h' in args):
+    print help(bioassembly_reconstruction)
   elif (len(args) > 2):
-    raise Sorry('To many input parameters are given')
-  elif args[0] in ['--help','-help','-h']:
-    print help(run)
+    raise Sorry('To many input parameters are given')  
   elif not os.path.isfile(args[0]):
     print 'There is no file {} in the current directory \n {}'.format(args[0],os.getcwd())
     raise Sorry('File name error')
   else:
-    m = multimer(args[0])
+    m = multimer(args[0],'ba')
+    # write the bio-assembly reconstructed file in current directory
     if len(args) == 2:
       # output file name given
-      output_file_name = args[1]
+      m.write(args[1])      
     else:
       # output file name not given
-      output_file_name = 'copy_' + args[0]
-    # write the bio-assembly reconstructed file in current directory
-    m.write(output_file_name)
+      m.write()
+    
     print 'bio-assembly reconstructed pdb file was added to your current directory'
-    print os.getcwd() + '/' + output_file_name
-
-
+    print os.getcwd() + '/' + m.pdb_output_file_name
 
 
 if (__name__ == "__main__"):
-  run(args=sys.argv[1:])
+  bioassembly_reconstruction(args=sys.argv[1:])
