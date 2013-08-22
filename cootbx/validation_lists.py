cdcba73d22954c212f97ead267b9c2ff434eac86
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-08-22 21:56:52 +0000

    code for displaying MolProbity outlier lists in Coot (ported from phenix tree)

---
+++ cootbx/validation_lists.py
@@ -0,0 +1,203 @@
+
+from __future__ import division
+import cPickle
+try :
+  import gobject
+except ImportError :
+  gobject = None
+import sys
+
+class coot_extension_gui (object) :
+  def __init__ (self, title) :
+    import gtk
+    self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+    scrolled_win = gtk.ScrolledWindow()
+    self.outside_vbox = gtk.VBox(False, 2)
+    self.inside_vbox = gtk.VBox(False, 0)
+    self.window.set_title(title)
+    self.inside_vbox.set_border_width(0)
+    self.window.add(self.outside_vbox)
+    self.outside_vbox.pack_start(scrolled_win, True, True, 0)
+    scrolled_win.add_with_viewport(self.inside_vbox)
+    scrolled_win.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+
+  def finish_window (self) :
+    import gtk
+    self.outside_vbox.set_border_width(2)
+    ok_button = gtk.Button("  Close  ")
+    self.outside_vbox.pack_end(ok_button, False, False, 0)
+    ok_button.connect("clicked", lambda b: self.destroy_window())
+    self.window.connect("delete_event", lambda a, b: self.destroy_window())
+    self.window.show_all()
+
+  def destroy_window (self, *args) :
+    self.window.destroy()
+    self.window = None
+
+  def confirm_data (self, data) :
+    for data_key in self.data_keys :
+      outlier_list = data.get(data_key)
+      if outlier_list is not None and len(outlier_list) > 0 :
+        return True
+    return False
+
+  def create_property_lists (self, data) :
+    import gtk
+    for data_key in self.data_keys :
+      outlier_list = data[data_key]
+      if outlier_list is None or len(outlier_list) == 0 :
+        continue
+      else :
+        frame = gtk.Frame(self.data_titles[data_key])
+        vbox = gtk.VBox(False, 2)
+        frame.set_border_width(6)
+        frame.add(vbox)
+        self.add_top_widgets(data_key, vbox)
+        self.inside_vbox.pack_start(frame, False, False, 5)
+        list_obj = residue_properties_list(
+          columns=self.data_names[data_key],
+          column_types=self.data_types[data_key],
+          rows=outlier_list,
+          box=vbox)
+
+# Molprobity result viewer
+class coot_molprobity_todo_list_gui (coot_extension_gui) :
+  data_keys = [ "rama", "rota", "cbeta", "probe" ]
+  data_titles = { "rama"  : "Ramachandran outliers",
+                  "rota"  : "Rotamer outliers",
+                  "cbeta" : "C-beta outliers",
+                  "probe" : "Severe clashes" }
+  data_names = { "rama"  : ["Chain", "Residue", "Name", "Score"],
+                 "rota"  : ["Chain", "Residue", "Name", "Score"],
+                 "cbeta" : ["Chain", "Residue", "Name", "Conf.", "Deviation"],
+                 "probe" : ["Atom 1", "Atom 2", "Overlap"] }
+  if (gobject is not None) :
+    data_types = { "rama" : [gobject.TYPE_STRING, gobject.TYPE_STRING,
+                             gobject.TYPE_STRING, gobject.TYPE_FLOAT,
+                             gobject.TYPE_PYOBJECT],
+                   "rota" : [gobject.TYPE_STRING, gobject.TYPE_STRING,
+                             gobject.TYPE_STRING, gobject.TYPE_FLOAT,
+                             gobject.TYPE_PYOBJECT],
+                   "cbeta" : [gobject.TYPE_STRING, gobject.TYPE_STRING,
+                              gobject.TYPE_STRING, gobject.TYPE_STRING,
+                              gobject.TYPE_FLOAT, gobject.TYPE_PYOBJECT],
+                   "probe" : [gobject.TYPE_STRING, gobject.TYPE_STRING,
+                              gobject.TYPE_FLOAT, gobject.TYPE_PYOBJECT] }
+  else :
+    data_types = dict([ (s, []) for s in ["rama","rota","cbeta","probe"] ])
+
+  def __init__ (self, data_file) :
+    data = load_pkl(data_file)
+    if not self.confirm_data(data) :
+      return
+    coot_extension_gui.__init__(self, "MolProbity to-do list")
+    self.dots_btn = None
+    self.dots2_btn = None
+    self._overlaps_only = True
+    self.window.set_default_size(420, 600)
+    self.create_property_lists(data)
+    self.finish_window()
+
+  def add_top_widgets (self, data_key, box) :
+    import gtk
+    if data_key == "probe" :
+      hbox = gtk.HBox(False, 2)
+      self.dots_btn = gtk.CheckButton("Show Probe dots")
+      hbox.pack_start(self.dots_btn, False, False, 5)
+      self.dots_btn.connect("toggled", self.toggle_probe_dots)
+      self.dots2_btn = gtk.CheckButton("Overlaps only")
+      hbox.pack_start(self.dots2_btn, False, False, 5)
+      self.dots2_btn.connect("toggled", self.toggle_all_probe_dots)
+      self.dots2_btn.set_active(True)
+      self.toggle_probe_dots()
+      box.pack_start(hbox, False, False, 0)
+
+  def toggle_probe_dots (self, *args) :
+    if self.dots_btn is not None :
+      show_dots = self.dots_btn.get_active()
+      overlaps_only = self.dots2_btn.get_active()
+      if show_dots :
+        self.dots2_btn.set_sensitive(True)
+      else :
+        self.dots2_btn.set_sensitive(False)
+      show_probe_dots(show_dots, overlaps_only)
+
+  def toggle_all_probe_dots (self, *args) :
+    if self.dots2_btn is not None :
+      self._overlaps_only = self.dots2_btn.get_active()
+      self.toggle_probe_dots()
+
+class rsc_todo_list_gui (coot_extension_gui) :
+  data_keys = ["by_res", "by_atom"]
+  data_titles = ["Real-space correlation by residue",
+                 "Real-space correlation by atom"]
+  data_names = {}
+  data_types = {}
+
+class residue_properties_list (object) :
+  def __init__ (self, columns, column_types, rows, box,
+      default_size=(380,200)) :
+    assert len(columns) == (len(column_types) - 1)
+    assert len(rows) == 0 or len(rows[0]) == len(column_types)
+    import gtk
+    self.liststore = gtk.ListStore(*column_types)
+    self.listmodel = gtk.TreeModelSort(self.liststore)
+    self.listctrl = gtk.TreeView(self.listmodel)
+    self.listctrl.column = [None]*len(columns)
+    self.listctrl.cell = [None]*len(columns)
+    for i, column_label in enumerate(columns) :
+      cell = gtk.CellRendererText()
+      column = gtk.TreeViewColumn(column_label)
+      self.listctrl.append_column(column)
+      column.set_sort_column_id(i)
+      column.pack_start(cell, True)
+      column.set_attributes(cell, text=i)
+    self.listctrl.get_selection().set_mode(gtk.SELECTION_SINGLE)
+    for row in rows :
+      self.listmodel.get_model().append(row)
+    self.listctrl.connect("cursor-changed", self.OnChange)
+    sw = gtk.ScrolledWindow()
+    w, h = default_size
+    if len(rows) > 10 :
+      sw.set_size_request(w, h)
+    else :
+      sw.set_size_request(w, 30 + (20 * len(rows)))
+    sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+    box.pack_start(sw, False, False, 5)
+    inside_vbox = gtk.VBox(False, 0)
+    sw.add(self.listctrl)
+
+  def OnChange (self, treeview) :
+    import coot # import dependency
+    selection = self.listctrl.get_selection()
+    (model, tree_iter) = selection.get_selected()
+    if tree_iter is not None :
+      row = model[tree_iter]
+      xyz = row[-1]
+      if isinstance(xyz, tuple) and len(xyz) == 3 :
+        set_rotation_centre(x, y, z)
+        set_zoom(30)
+        graphics_draw()
+
+def show_probe_dots (show_dots, overlaps_only) :
+  import coot # import dependency
+  n_objects = number_of_generic_objects()
+  sys.stdout.flush()
+  if show_dots :
+    for object_number in range(n_objects) :
+      obj_name = generic_object_name(object_number)
+      if overlaps_only and not obj_name in ["small overlap", "bad overlap"] :
+        sys.stdout.flush()
+        set_display_generic_object(object_number, 0)
+      else :
+        set_display_generic_object(object_number, 1)
+  else :
+    sys.stdout.flush()
+    for object_number in range(n_objects) :
+      set_display_generic_object(object_number, 0)
+
+def load_pkl (file_name) :
+  pkl = open(file_name, "rb")
+  data = cPickle.load(pkl)
+  pkl.close()
+  return data
