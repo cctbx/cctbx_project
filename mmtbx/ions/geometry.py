4892ab8526a91ad4b989ab94d8b00dd2282e1afc
Nader Morshed <morshed.nader@gmail.com>
2013-08-22 22:40:53 +0000

    Fixed the vector set for _pentagon() Added another geometry: "ring_miss", like a ring_pop, but missing an atom (Need a better name for both of these...) New fast method for identifying geometries, generates a list of expected angles and compares them to the actual angles - Similar to the old clunky method, but sources its info from the vector set

--- mmtbx/ions/geometry.py
+++ mmtbx/ions/geometry.py
@@ -301,32 +301,29 @@ def _tetrahedron():
     )
 
 def _octahedron():
-  return rec(
-    [1, 0, 0,
-     -1, 0, 0,
-     0, 1, 0,
-     0, -1, 0,
-     0, 0, 1,
-     0, 0, -1],
-     n = (6, 3)
-    )
+  return rec([
+    1, 0, 0,
+    -1, 0, 0,
+    0, 1, 0,
+    0, -1, 0,
+    0, 0, 1,
+    0, 0, -1,
+    ], n = (6, 3))
 
 def _trigonal_plane():
-  return rec(
-    [0, 1, 0,
-     -sqrt(3) / 2, -1 / 2, 0,
-     sqrt(3) / 2, -1 / 2, 0],
-     n = (3, 3)
-    )
+  return rec([
+    0, 1, 0,
+    -sqrt(3) / 2, -1 / 2, 0,
+    sqrt(3) / 2, -1 / 2, 0,
+    ], n = (3, 3))
 
 def _square_plane():
-  return rec(
-    [0, 1, 0,
-     0, -1, 0,
-     1, 0, 0,
-     -1, 0, 0],
-     n = (4, 3)
-    )
+  return rec([
+    0, 1, 0,
+    0, -1, 0,
+    1, 0, 0,
+    -1, 0, 0
+    ], n = (4, 3))
 
 def _concatenate(*args):
   lst = [i for arg in args for i in arg]
@@ -359,24 +356,53 @@ def _trigonal_prism():
 def _pentagon():
   # Taken from http://mathworld.wolfram.com/Pentagon.html
   c_1 = (sqrt(5) - 1) / 4
-  c_2 = (sqrt(5) - 1) / 4
+  c_2 = (sqrt(5) + 1) / 4
   s_1 = sqrt(10 + 2 * sqrt(5)) / 4
   s_2 = sqrt(10 - 2 * sqrt(5)) / 4
 
-  return rec(
-    [1, 0, 0,
-     c_1, s_1, 0,
-     -c_2, s_2, 0,
-     -c_2, -s_2, 0,
-     c_1, -s_1, 0],
-     n = (5, 3)
-    )
+  return rec([
+    1, 0, 0,
+    c_1, s_1, 0,
+    -c_2, s_2, 0,
+    -c_2, -s_2, 0,
+    c_1, -s_1, 0,
+    ], n = (5, 3))
 
 def _pentagonal_pyramid():
   return _pyramid(_pentagon())
 
 def _pentagonal_bipyramid():
   return _bipyramid(_pentagon())
+
+def _ring_miss_1():
+  return rec([
+    0, 1, 0,
+    0, -1, 0,
+    1, 0, 0,
+    0, 0, 1,
+    sqrt(2) / 2, sqrt(2) / 2, -1,
+    -sqrt(2) / 2, -sqrt(2) / 2, -1,
+  ], n = (6, 3))
+
+def _ring_miss_2():
+  return rec([
+    0, 1, 0,
+    0, -1, 0,
+    -1, 0, 0,
+    0, 0, 1,
+    sqrt(2) / 2, sqrt(2) / 2, -1,
+    -sqrt(2) / 2, -sqrt(2) / 2, -1,
+  ], n = (6, 3))
+
+def _ring_miss_3():
+  return rec([
+    0, 1, 0,
+    0, -1, 0,
+    -1, 0, 0,
+    1, 0, 0,
+    sqrt(2) / 2, sqrt(2) / 2, -1,
+    -sqrt(2) / 2, -sqrt(2) / 2, -1,
+  ], n = (6, 3))
 
 def _ring_pop():
   return _concatenate(
@@ -411,124 +437,59 @@ def _three_legs():
 
 SUPPORTED_GEOMETRIES = OrderedDict([
   (3, [
-    ("three_legs", _three_legs, [1, 1, 1], 15),
-    ("trigonal_plane", _trigonal_plane, [3, 1, 1], 15),
+    ("three_legs", _three_legs, 15),
+    ("trigonal_plane", _trigonal_plane, 15),
     ]),
   (4, [
-    ("tetrahedron", _tetrahedron, [1, 1, 1], 15),
-    ("square_plane", _square_plane, [4, 1, 1], 15),
-    ("trigonal_pyramid", _trigonal_pyramid, [3, 1, 1], 15),
-    ("see_saw", _see_saw, [1, 1, 1], 15),
+    ("tetrahedron", _tetrahedron, 15),
+    ("square_plane", _square_plane, 20),
+    ("trigonal_pyramid", _trigonal_pyramid, 15),
+    ("see_saw", _see_saw, 15),
     ]),
   (5, [
-    ("square_pyramid", _square_pyramid, [4, 1, 1], 15),
-    ("trigonal_bipyramid", _trigonal_bipyramid, [3, 2, 2], 15),
-    ("pentagon", _pentagon, [5, 1, 1], 15)
+    ("square_pyramid", _square_pyramid, 15),
+    ("trigonal_bipyramid", _trigonal_bipyramid, 15),
+    ("pentagon", _pentagon, 15)
     ]),
   (6, [
-    ("octahedron", _octahedron, [4, 4, 4], 20),
-    ("trigonal_prism", _trigonal_prism, [3, 1, 1], 15),
-    ("pentagonal_pyramid", _pentagonal_pyramid, [5, 1, 1], 20),
+    ("octahedron", _octahedron, 15),
+    ("trigonal_prism", _trigonal_prism, 15),
+    ("pentagonal_pyramid", _pentagonal_pyramid, 15),
+    ("ring_miss", _ring_miss_1, 15),
+    ("ring_miss", _ring_miss_2, 15),
+    ("ring_miss", _ring_miss_3, 15),
     ]),
   (7, [
-    ("pentagonal_bipyramid", _pentagonal_bipyramid, [5, 2, 2], 20),
-    ("ring_pop", _ring_pop, [2, 1, 1], 20), # better name, please.
+    ("pentagonal_bipyramid", _pentagonal_bipyramid, 15),
+    ("ring_pop", _ring_pop, 15), # better name, please.
     ]),
   ])
 
 SUPPORTED_GEOMETRY_NAMES = \
   [lst[0] for vals in SUPPORTED_GEOMETRIES.values() for lst in vals]
 
-def _norm(vec):
-  return sqrt(sum([i ** 2 for i in vec]))
+def _angles_deviation(vectors_a, vectors_b):
+  vectors_b = [rec((vectors_b[i], vectors_b[i + 1], vectors_b[i + 2]), n = (1, 3))
+               for i in xrange(0, len(vectors_b), 3)]
+  vectors_a = [rec((vectors_a[i], vectors_a[i + 1], vectors_a[i + 2]), n = (1, 3))
+               for i in xrange(0, len(vectors_a), 3)]
+  assert len(vectors_a) == len(vectors_b)
 
-def _rmsd_shape(a, b, power = 2):
-  assert a.n == b.n
-  c = list(b)
-  distances = []
-  for i in a:
-    c.sort(key = lambda x: _norm(i - x))
-    distances.append(_norm(i - c.pop(0)))
-  distances = [i ** power for i in distances]
-  return sqrt(sum(distances) / a.n[0])
+  angles_a = [vec.angle(vec_o, deg = True)
+              for index, vec in enumerate(vectors_a)
+              for vec_o in vectors_a[index + 1:]]
 
-def _angle(a, b, degree = False):
-  from math import acos, pi
-  angle = acos(a.dot(b) / (_norm(a) * _norm(b)))
-  if degree: angle *= 180 / pi
-  return angle
+  angles_b = [vec.angle(vec_o, deg = True)
+              for index, vec in enumerate(vectors_b)
+              for vec_o in vectors_b[index + 1:]]
 
-def _rmsa_shape(a, b, power = 2, degree = False):
-  assert a.n == b.n
-  c = b.as_list_of_lists()
-  angles = []
-  for i in a.as_list_of_lists():
-    i = col(i)
-    c.sort(key = lambda x: _angle(i, col(x), degree = degree))
-    angles.append(_angle(i, col(c.pop(0)), degree = degree))
+  angles_a.sort()
+  angles_b.sort()
 
-  angles = [i ** power for i in angles]
-  rmsa = sqrt(sum(angles) / a.n[0])
+  angle_deviation = sqrt(sum([(i - j) ** 2 for i, j in zip(angles_a, angles_b)])
+                   / len(angles_a))
 
-  return rmsa
-
-def _transform_shape(shape, scale, rotation):
-  from math import sin, cos
-  rot_x, rot_y, rot_z = rotation
-  mat_x = rec([1, 0, 0,
-               0, cos(rot_x), -sin(rot_x),
-               0, sin(rot_x), cos(rot_x)],
-              n = (3, 3))
-  mat_y = rec([cos(rot_y), 0, sin(rot_y),
-               0, 1, 0,
-               -sin(rot_y), 0, cos(rot_y)],
-              n = (3, 3))
-  mat_z = rec([cos(rot_z), -sin(rot_z), 0,
-               sin(rot_z), cos(rot_z), 0,
-               0, 0, 1],
-              n = (3, 3))
-  mat_r = mat_x * mat_y * mat_z
-
-  return rec([elem * scale for column in shape.as_list_of_lists()
-              for elem in mat_r * col(column)], n = shape.n)
-
-def sphere_minimizer(f, symmetry, step = 45):
-  def sphere_iterate():
-    for x in xrange(0, 360 // symmetry[0], step):
-      for y in xrange(0, 360 // symmetry[1], step):
-        for z in xrange(0, 360 // symmetry[2], step):
-          yield col([x, y, z])
-  return min(((x, f(x)) for x in sphere_iterate()), key = lambda x: x[-1])
-
-def _minimize_points(shape, target, symmetry = None):
-  def minimize_me(x):
-    return _rmsa_shape(shape, _transform_shape(target, 1, x), degree = True)
-  class minimize_me_class:
-    def __init__(self): pass
-    def target(self, vector): return minimize_me(vector)
-
-  if symmetry is None:
-    symmetry = [1, 1, 1]
-  m = sphere_minimizer(minimize_me, symmetry)
-  x = m[0]
-  from scitbx.direct_search_simulated_annealing import dssa
-  from scitbx.array_family import flex
-  x = flex.double([i for i in x])
-  starting_simplex = [
-    x + flex.double((0, 0, 0)),
-    x + flex.double((90, 0, 0)),
-    x + flex.double((0, 90, 0)),
-    x + flex.double((0, 0, 90)),
-  ]
-  dsa = dssa(
-    dimension = 3,
-    matrix = starting_simplex,
-    evaluator = minimize_me_class(),
-    further_opt = True,
-    )
-  x = dsa.get_solution()
-  f_x = minimize_me(x)
-  return f_x
+  return angle_deviation
 
 def find_coordination_geometry(nearby_atoms, minimizer_method = False,
                                cutoff = 2.9):
@@ -550,13 +511,14 @@ def find_coordination_geometry(nearby_atoms, minimizer_method = False,
     vectors = rec(
       [i for vector in filtered for i in vector],
       n = (len(filtered), 3))
+    # vectors = filtered
 
     n_vectors = vectors.n[0]
     if n_vectors not in SUPPORTED_GEOMETRIES:
       return geometries
 
-    for name, func, symmetry, rmsa_cutoff in SUPPORTED_GEOMETRIES[n_vectors]:
-      rmsa = _minimize_points(vectors, func(), symmetry = symmetry)
+    for name, func, rmsa_cutoff in SUPPORTED_GEOMETRIES[n_vectors]:
+      rmsa = _angles_deviation(vectors, func())
       # print name, rmsa
       if rmsa < rmsa_cutoff:
         geometries.append((name, rmsa))
