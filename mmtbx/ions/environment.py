dee54f9eb334a6a80734c0902171dc8f2928ed05
Nader Morshed <morshed.nader@gmail.com>
2013-08-23 19:12:39 +0000

    Added documentation to environment.py

--- mmtbx/ions/environment.py
+++ mmtbx/ions/environment.py
@@ -1,9 +1,13 @@
  # -*- coding: utf-8; py-indent-offset: 2 -*-
+"""
+Deals with examing the atoms around a site and recognizing distinct and useful
+chemical environments.
+"""
 from __future__ import division
 
 from iotbx.pdb import common_residue_names_get_class as get_class
 from libtbx.utils import Sorry
-from mmtbx import ions
+from mmtbx.ions import same_atom_different_altloc
 
 SUPPORTED_ENVIRONMENTS = set([
   "O", "N", "S", "CL",
@@ -16,10 +20,35 @@ SUPPORTED_ENVIRONMENTS = set([
 
 # XXX: Deal with alt loc environments?
 def get_environments(contact, manager):
+  """
+  Examines an atom contact object for chemical properties useful to ion
+  picking. These properties include degree of connectivitiy, presence in
+  carboxy and amide groups, disulfide bonds, and so on.
+
+  Parameters
+  ----------
+  contact: mmtbx.ions.atom_contact
+      The contact object to examine the chemical environment of. Generally
+      created by Manager.find_nearby_atoms.
+  manager: mmtbx.ions.Manager
+      The ions manager that created contact. Used for auxillary information such
+      as bond connectivity.
+
+  Returns
+  -------
+  set of str
+      A set of environments found for contact.
+
+  See Also
+  --------
+  SUPPORTED_ENVIRONMENTS
+      A complete list of environment types that this function recognizes.
+  """
   def _non_hydrogen_neighbors(seq):
-    return [other
-            for other in manager.connectivity[seq]
-            if manager.server.get_element(manager.pdb_atoms[other]) not in ["H"]]
+    return [
+      other
+      for other in manager.connectivity[seq]
+      if contact.server.get_element(manager.pdb_atoms[other]) not in ["H"]]
 
   def _n_non_altlocs(i_seqs):
     """
@@ -29,7 +58,7 @@ def get_environments(contact, manager):
     return len(
       [i_seq
        for index, i_seq in enumerate(i_seqs)
-       if all(not ions.same_atom_different_altloc(
+       if all(not same_atom_different_altloc(
            manager.pdb_atoms[i_seq],
            manager.pdb_atoms[j_seq])
            for j_seq in i_seqs[index + 1:])]
@@ -44,7 +73,7 @@ def get_environments(contact, manager):
     environments.add(element)
 
   i_seq = contact.atom.i_seq
-  neighbor_elements = [manager.server.get_element(manager.pdb_atoms[i])
+  neighbor_elements = [contact.server.get_element(manager.pdb_atoms[i])
                        for i in _non_hydrogen_neighbors(i_seq)]
 
   # Check for waters, sulfates, and phosphates
@@ -84,12 +113,12 @@ def get_environments(contact, manager):
   if element in ["O", "N"] and n_neighbors in [1]:
     for j_seq in _non_hydrogen_neighbors(i_seq):
       j_atom = manager.pdb_atoms[j_seq]
-      if manager.server.get_element(j_atom) in ["C"]:
+      if contact.server.get_element(j_atom) in ["C"]:
         for k_seq in manager.connectivity[j_seq]:
           k_neighbors = _non_hydrogen_neighbors(k_seq)
           k_atom = manager.pdb_atoms[k_seq]
           if k_seq != i_seq and _n_non_altlocs(k_neighbors) in [1]:
-            k_element = manager.server.get_element(k_atom)
+            k_element = contact.server.get_element(k_atom)
             if [element, k_element] in [["O", "O"]]:
               environments.add("Carboxy")
             elif [element, k_element] in [["N", "O"], ["O", "N"]]:
