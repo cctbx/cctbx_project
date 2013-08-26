08f6a28c1b58f7c254e5f9cac0ffb72550280e09
Nigel W Moriarty <nwmoriarty@lbl.gov>
2013-08-26 17:21:46 +0000

    an Iridium data set RDL start

---
+++ mmtbx/conformation_dependent_library/rdl_database.py
@@ -0,0 +1,9 @@
+from __future__ import division
+rdl_database = {
+  "TRP" : {
+    "mtt" : {
+        ("CA", "CB", "CG" ) : (115.6, 1.9), # (113.6, 1.9), 
+        ("CB", "CG", "CD1") : (124.9, 1.5), # (126.9, 1.5),
+        },
+      },
+    }
