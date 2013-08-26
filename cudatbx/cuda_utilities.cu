d2c9ef07f142e05ee75f34ed7402f43b4311db16
Billy Poon <bkpoon@lbl.gov>
2013-08-26 22:40:14 +0000

    cuda_timer: added class for timing GPU code

--- cudatbx/cuda_utilities.cu
+++ cudatbx/cuda_utilities.cu
@@ -1,5 +1,4 @@
-#include <cudatbx/cuda_utilities.h>
-#include <cudatbx/cuda_base.cuh>
+#include <cudatbx/cuda_utilities.cuh>
 
 namespace cudatbx {
 
@@ -13,5 +12,45 @@ namespace cudatbx {
     cudaSafeCall( cudaSetDevice(gpu_id) );
     cudaSafeCall( cudaDeviceReset() );
   }
+
+  /* ==========================================================================
+     Basic timer for CUDA using events, one stream only, no checks
+
+     Usage:
+
+       cuda_timer t;
+       t.start();
+
+       < run CUDA stuff >
+
+       t.stop();
+       float elapsed_time = t.get_elapsed_time();
+       std::cout << elapsed_time << "\n";
+  */
+  cudatbx::cuda_timer::cuda_timer() {
+    cudaEventCreate(&start_event);
+    cudaEventCreate(&stop_event);
+  }
+
+  cudatbx::cuda_timer::~cuda_timer() {
+    cudaEventDestroy(start_event);
+    cudaEventDestroy(stop_event);
+  }
+
+  void cudatbx::cuda_timer::start() {
+    cudaEventRecord(start_event);
+  }
+
+  void cudatbx::cuda_timer::stop() {
+    cudaEventRecord(stop_event);
+    cudaEventSynchronize(stop_event);
+    cudaEventElapsedTime(&elapsed_time, start_event, stop_event);
+  }
+
+  float cudatbx::cuda_timer::get_elapsed_time() {
+    return elapsed_time;
+  }
+
+  // ==========================================================================
 
 }
