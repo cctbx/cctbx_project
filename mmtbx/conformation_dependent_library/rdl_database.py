cd1a66d3f85462f86b40e7989f7dce47ea2b157a
Nigel W Moriarty <nwmoriarty@lbl.gov>
2013-08-27 16:01:27 +0000

    better data structure

--- mmtbx/conformation_dependent_library/rdl_database.py
+++ mmtbx/conformation_dependent_library/rdl_database.py
@@ -1,9 +1,13 @@
 from __future__ import division
 rdl_database = {
   "TRP" : {
-    "mtt" : {
-        ("CA", "CB", "CG" ) : (115.6, 1.9), # (113.6, 1.9), 
-        ("CB", "CG", "CD1") : (124.9, 1.5), # (126.9, 1.5),
+    "default" : { # values from restraints library
+        ("CA", "CB", "CG" ) : (113.6, 1.9), 
+        ("CB", "CG", "CD1") : (126.9, 1.5),
+        },
+    "p90" : {
+        ("CA", "CB", "CG" ) : (115.6, 1.9),
+        ("CB", "CG", "CD1") : (124.9, 1.5),
         },
       },
     }
