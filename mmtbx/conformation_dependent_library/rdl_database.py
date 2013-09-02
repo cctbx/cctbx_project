0b83e26255e58d3bc94acca2f897f5bc939b5898
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-09-02 23:20:45 +0000

    Clean clutter

--- mmtbx/conformation_dependent_library/rdl_database.py
+++ mmtbx/conformation_dependent_library/rdl_database.py
@@ -2,7 +2,7 @@ from __future__ import division
 rdl_database = {
   "TRP" : {
     "default" : { # values from restraints library
-        ("CA", "CB", "CG" ) : (113.6, 1.9), 
+        ("CA", "CB", "CG" ) : (113.6, 1.9),
         ("CB", "CG", "CD1") : (126.9, 1.5),
         },
     "p90" : {
