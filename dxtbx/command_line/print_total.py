bd87b25935f64f3feedf36e356c189409eebcd44
Johan Hattne <johan@hattne.se>
2013-09-06 07:38:08 +0000

    Clarify return value from Registry.find; use getter for image size.

--- dxtbx/command_line/print_total.py
+++ dxtbx/command_line/print_total.py
@@ -8,10 +8,10 @@ def print_total():
 
     for arg in sys.argv[1:]:
         print '=== %s ===' % arg
-        format_instance = Registry.find(arg)
-        print 'Using header reader: %s' % format_instance.__name__
-        i = format_instance(arg)
-        image_size = i.get_detector().image_size
+        format_class = Registry.find(arg)
+        print 'Using header reader: %s' % format_class.__name__
+        i = format_class(arg)
+        image_size = i.get_detector().get_image_size()
         print 'Total Counts:'
         total = sum(i.get_raw_data())
         print total
