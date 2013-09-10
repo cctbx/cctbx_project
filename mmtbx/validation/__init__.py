4527cde676fa6434687ba6157e5bbdebb9e32445
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-09-10 23:14:35 +0000

    MolProbity refactor, part 1: base classes, real-space and waters

--- mmtbx/validation/__init__.py
+++ mmtbx/validation/__init__.py
@@ -1,56 +1,359 @@
+
 from __future__ import division
+from libtbx import slots_getstate_setstate
+from libtbx.str_utils import format_value
+import sys
 
-from libtbx import adopt_init_args
+class entity (slots_getstate_setstate) :
+  """
+  Base class for all validation results.  This includes a boolean outlier flag,
+  the information used to zoom in the Phenix GUI (optional, but strongly
+  recommended), and some kind of numerical score (also optional, but strongly
+  recommended - although some analyses may require multiple distinct scores).
+  """
+  __slots__ = [
+    "atom_selection",
+    "xyz",
+    "outlier",
+    "score",
+  ]
+  score_format = "%s"
+  phenix_table_labels = []
+  molprobity_table_labels = []
 
-# XXX prototype for container for residue-specific validation information.
-# not currently used anywhere, but eventually this can be used to display
-# multi-criterion overviews of a structure (much more cleanly than in the
-# current PHENIX GUI code).
-class residue_info (object) :
-  def __init__ (self,
-                resname,
-                resid,
-                chain_id,
-                altloc,
-                xyz,
-                sec_str=None,
-                ramalyze_score=None,
-                rotalyze_score=None,
-                cbetadev_score=None,
-                clash_max=None,
-                bonds_max=None,
-                angles_max=None,
-                dihedral_max=None,
-                chiral_max=None,
-                plane_max=None,
-                rscc=None,
-                b_mean=None,
-                mean_2fofc=None,
-                mean_fc=None) :
-    adopt_init_args(self, locals())
+  def __init__ (self, **kwds) :
+    for name in kwds.keys() :
+      assert (name in self.__slots__), name
+    for name in self.__slots__ :
+      setattr(self, name, kwds.get(name, None))
 
-  def is_ramachandran_outlier (self) :
-    if (self.ramalyze_score is None) : return None
-    else : return (self.ramalyze_score < 0.002)
+  @staticmethod
+  def header () :
+    raise NotImplementedError()
 
-  def is_ramachandran_favored (self) :
-    if (self.ramalyze_score is None) : return None
-    else : return (self.ramalyze_score > 0.02)
+  def format_score (self, replace_none_with="None") :
+    return format_value(self.score_format, self.score,
+      replace_none_with=replace_none_with)
 
-  def is_rotalyze_outlier (self) :
-    if (self.rotalyze_score is None) : return None
-    else : return (self.rotalyze_score < 1.0)
+  def is_outlier (self) :
+    return self.outlier
 
-  def is_clash_outlier (self) :
-    return (self.clash_max >= 0.4)
+  def as_string (self, prefix="") :
+    raise NotImplementedError()
 
-  def is_cbeta_outlier (self) :
-    return (self.cbetadev_score > 0.25)
+  def __str__ (self) :
+    return self.as_string()
 
-  def get_atom_selection (self) :
-    if (self.altloc == "") :
-      return "(chain '%s' and resname %s and resid %s)" % (self.chain_id,
-        self.resname, self.resid)
+  def id_str (self, ignore_altloc=None) :
+    """
+    Returns a formatted (probably fixed-width) string describing the molecular
+    entity being validation, independent of the analysis type.
+    """
+    raise NotImplementedError()
+
+  def __hash__ (self) :
+    return self.id_str().__hash__()
+
+  def as_list (self) :
+    """
+    Optional; returns old format used by some tools in mmtbx.validation.
+    """
+    raise NotImplementedError()
+
+  def as_table_row_molprobity (self) :
+    """
+    Returns a list of formatted table cells for display by MolProbity.
+    """
+    raise NotImplementedError()
+
+  def as_table_row_phenix (self) :
+    """
+    Returns a list of formatted table cells for display by Phenix.
+    """
+    raise NotImplementedError()
+
+  def as_kinemage (self) :
+    """
+    Returns a kinemage string for displaying an outlier.
+    """
+    raise NotImplementedError()
+
+  def format_old (self) :
+    raise NotImplementedError()
+
+  def __eq__ (self, other) :
+    """
+    Compare two validation results to determine whether they correspond to the
+    same molecular entity and analysis type.  This is intended to be used for
+    analysis of a structure before-and-after refinement (etc.).
+    """
+    raise NotImplementedError()
+
+  def __cmp__ (self, other) :
+    return cmp(self.score, other.score)
+
+  def is_single_residue_object (self) :
+    raise NotImplementedError()
+
+__residue_attr__ = [
+  "chain_id",
+  "resseq",
+  "icode",
+  "resname",
+  "altloc",
+  "segid",
+]
+
+class residue (entity) :
+  """
+  Base class for validation information about a single residue, which depending
+  on context could mean either any one of the residue_group, atom_group, or
+  residue objects from the PDB hierarchy.
+  """
+  __slots__ = entity.__slots__ + __residue_attr__ + ["occupancy"]
+
+  def assert_all_attributes_defined (self) :
+    for name in self.__residue_attr__ :
+      assert (getattr(self, name) is not None) or (name == "segid")
+
+  def id_str (self, ignore_altloc=False) :
+    base = "%2s%4s%1s" % (self.chain_id, self.resseq, self.icode)
+    if (not ignore_altloc) :
+      base += "%1s" % self.altloc
     else :
-      return "(chain '%s' and resname %s and resid %s and altloc %s)" % \
-        (self.chain_id, self.resname, self.resid, self.altloc)
+      base += " "
+    base += "%3s" % self.resname
+    if (self.segid is not None) :
+      base += " segid='%4s'" % self.segid
+    return base
+
+  def residue_id (self, ignore_altloc=False) :
+    return self.id_str(ignore_altloc=ignore_altloc)
+
+  def simple_id (self) :
+    return ("%s%s%s" % (self.chain_id, self.resseq, self.icode)).strip()
+
+  # XXX probably needs to be flexible about altloc...
+  def is_same_residue (self, other, ignore_altloc=False) :
+    if hasattr(other, "residue_id") :
+      return (self.residue_id(ignore_altloc=ignore_altloc) ==
+              other.residue_id(ignore_altloc=ignore_altloc))
+    return (self.id_str(ignore_altloc=ignore_altloc) ==
+            other.id_str(ignore_altloc=ignore_altloc))
+
+  def is_same_residue_group (self, other) :
+    return ((self.chain_id == other.chain_id) and
+            (self.resseq == other.resseq) and
+            (self.icode == other.icode) and
+            (self.segid == other.segid))
+
+  def residue_group_id_str (self) :
+    return "%2s%4s%1s" % (self.chain_id, self.resseq, self.icode)
+
+  def __eq__ (self, other) :
+    assert type(self) == type(other), type(other)
+    return self.id_str() == other.id_str()
+
+  def is_single_residue_object (self) :
+    return True
+
+class atoms (entity) :
+  """
+  Base class for validation results involving a specific set of atoms, such
+  as covalent geometry restraints, clashes, etc.
+  """
+  __atoms_attr__ = [
+    "atoms_info",
+  ]
+  __slots__ = entity.__slots__ + __atoms_attr__
+
+  def n_atoms (self) :
+    return len(self.atoms_info)
+
+  def __eq__ (self, other) :
+    assert type(self) == type(other), type(other)
+    return sorted(self.atoms_info) == sorted(other.atoms_info)
+
+  def is_single_residue_object (self) :
+    return False
+
+class atom (entity) :
+  """
+  Base class for validation results for a single atom.  This is distinct from
+  the atom_info class above, which is used to track individual atoms within
+  a multi-atom validation result.
+  """
+  __atom_attr__ = __residue_attr__ + [
+    "name",
+    "element",
+    "b_iso",
+    "occupancy",
+    "i_seq", # XXX is this necessary?
+    # XXX do we need u_aniso too?  anything else?
+  ]
+  __slots__ = entity.__slots__ + __atom_attr__
+
+  def __init__ (self, **kwds) :
+    pdb_atom = kwds.get("pdb_atom", None)
+    if (pdb_atom is not None) :
+      del kwds['pdb_atom']
+    for name in kwds.keys() :
+      assert (name in self.__slots__), name
+    for name in self.__slots__ :
+      setattr(self, name, kwds.get(name, None))
+    if (pdb_atom is not None) :
+      labels = pdb_atom.fetch_labels()
+      self.chain_id = labels.chain_id
+      self.resseq = labels.resseq
+      self.icode = labels.icode
+      self.resname = labels.resname
+      self.altloc = labels.altloc
+      self.segid = labels.segid
+      self.name = pdb_atom.name
+      self.xyz = pdb_atom.xyz
+      self.occupancy = pdb_atom.occ
+      self.b_iso = pdb_atom.b
+      self.element = pdb_atom.element
+
+  def __eq__ (self, other) :
+    assert type(self) == type(other), type(other)
+    return self.id_str() == other.id_str()
+
+  def residue_group_id_str (self) :
+    return "%2s%4s%1s" % (self.chain_id, self.resseq, self.icode)
+
+  def is_single_residue_object (self) :
+    return True
+
+#-----------------------------------------------------------------------
+# Generic utility classes
+class atom_info (slots_getstate_setstate) :
+  """
+  Container for metadata for a single atom, in the context of validation
+  results involving multiple atoms.  Intended to be used as-is inside various
+  atoms classes.
+  """
+  __atom_attr__ = [
+    "name",
+    "element", # XXX is this necessary?
+    "xyz",
+    "symop",
+    "occupancy",
+  ]
+  __slots__ = __residue_attr__ + __atom_attr__
+
+  def __init__ (self, **kwds) :
+    for name in kwds.keys() :
+      assert (name in self.__slots__), name
+    for name in self.__slots__ :
+      setattr(self, name, kwds.get(name, None))
+
+  def id_str (self, ignore_altloc=True) :
+    base = "%2s%4s%1s" % (self.chain_id, self.resseq, self.icode)
+    if (not ignore_altloc) :
+      base += "%1s" % self.altloc
+    else :
+      base += " "
+    base += "%3s %4s" % (self.resname, self.name)
+    if (self.segid is not None) :
+      base += " segid='%4s'" % self.segid
+    return base
+
+  def __str__ (self) :
+    raise NotImplementedError() # FIXME
+
+  def __eq__ (self, other) :
+    assert type(self) == type(other), type(other)
+    return self.id_str() == other.id_str()
+
+  def __cmp__ (self, other) :
+    return cmp(self.id_str(), other.id_str())
+
+  def residue_group_id_str (self) :
+    return "%2s%4s%1s" % (self.chain_id, self.resseq, self.icode)
+
+#-----------------------------------------------------------------------
+# SPECIFIC RESULTS
+# FIXME move these to specific modules
+
+# rna_validate is a real mess, I've been meaning to work on this for a while
+# maybe split up pucker and suite analysis into separate classes?
+class rna_pucker (residue) : # TODO
+  pass
+
+class rna_suite (residue) : # TODO
+  pass
+
+#class rna_bond (bond) : # TODO
+#  pass
+
+#class rna_angle (angle) : # TODO
+#  pass
+
+# XXX others?
+class water (atom) : # TODO
+  pass
+
+class residue_bfactor (residue) : # TODO
+  pass
+
+#-----------------------------------------------------------------------
+class validation (slots_getstate_setstate) :
+  __slots__ = ["n_outliers", "n_total", "results"]
+  program_description = None
+  output_header = None
+  def __init__ (self) :
+    self.n_outliers = 0
+    self.n_total = 0
+    self.results = []
+
+  def get_outliers_count_and_fraction(self):
+    if (self.n_total != 0):
+      fraction = float(self.n_outliers) / self.n_total
+      assert fraction <= 1.0
+      return self.n_outliers, fraction
+    return 0, 0.
+
+  def get_outliers_goal (self) :
+    raise NotImplementedError()
+
+  def get_result_class (self) :
+    raise NotImplementedError()
+
+  def coot_todo (self) :
+    return ""
+
+  def show_old_output (self, out=sys.stdout, verbose=False) :
+    if (verbose) :
+      assert (self.output_header is not None)
+      print >> out, self.output_header
+    for result in self.results :
+      print >> out, result.format_old()
+    if (verbose) :
+      self.show_summary(out)
+
+  def show_summary (self, out=sys.stdout, prefix="") :
+    raise NotImplementedError()
+
+  def show (self, out=sys.stdout, prefix="  ", outliers_only=True,
+      verbose=True) :
+    if (len(self.results) > 0) :
+      print >> out, prefix + self.get_result_class().header()
+      for result in self.results :
+        if (not outliers_only) or (result.is_outlier()) :
+          print >> out, prefix + str(result)
+    self.show_summary(out=out, prefix=prefix)
+
+molprobity_cmdline_phil_str = """
+  model = None
+      .type = path
+      .help = "Model file (PDB or mmCIF)"
+
+  outliers_only = False
+    .type = bool
+    .help = "Only display outliers"
+
+  verbose = True
+    .type = bool
+    .help = '''Verbose'''
+"""
--- mmtbx/validation/waters.py
+++ mmtbx/validation/waters.py
@@ -1,74 +1,131 @@
 
 from __future__ import division
-from libtbx import slots_getstate_setstate
+from mmtbx.validation import atom, validation
+from mmtbx.validation import experimental
+from libtbx.str_utils import format_value
+import sys
 
-class water_properties (slots_getstate_setstate) :
-  __slots__ = [
-    "i_seq",
-    "id_str",
-    "site_cart",
-    "b_iso",
-    "occ",
+class water (atom) :
+  """
+  Container for information about a water atom, including electron density
+  properties.
+  """
+
+  __slots__ = atom.__slots__ + experimental.__real_space_attr__ + [
+    "anom",
     "nearest_contact",
-    "map_cc",
-    "two_fofc_map",
-    "fofc_map",
-    "anom_map",
     "n_hbonds",
   ]
-  def __init__ (self, **kwds) :
-    for name in self.__slots__ :
-      setattr(self, name, kwds.get(name, None))
+
+  @property
+  def cc (self) :
+    return self.score
+
+  @staticmethod
+  def header () :
+    return "%-20s  %6s  %4s  %6s  %6s  %6s  %5s" % ("atom", "b_iso", "occ",
+      "2Fo-Fc", "Fo-Fc", "Anom", "CC")
+
+  def id_str (self) :
+    return "%2s%4s%1s%1s %-4s" % (self.chain_id, self.resseq, self.icode,
+      self.altloc, self.name)
+
+  def as_string (self, prefix="", highlight_if_heavy=False) :
+    flag = ""
+    if highlight_if_heavy and self.is_heavy_atom() :
+      flag = " ***"
+    return "%-20s  %6.2f  %4.2f  %6.2f  %6.2f  %s  %5.3f%s" % (
+      self.id_str(), self.b_iso, self.occupancy, self.two_fofc, self.fofc,
+      format_value("%6.2f", self.anom, replace_none_with="---"), self.cc, flag)
 
   def is_bad_water (self) :
-    return ((self.map_cc < 0.8) or (self.fofc_map < -3.0) or (self.occ < 0.5)
-            or (self.two_fofc_map < 1.0))
+    return ((self.cc < 0.8) or (self.fofc < -3.0) or (self.occupancy < 0.5)
+            or (self.two_fofc < 1.0))
 
   def is_heavy_atom (self) :
-    return (self.fofc_map > 3.0) or (self.anom_map > 3.0)
+    return (self.fofc > 3.0) or (self.anom > 3.0) or (self.b_iso == 0)
 
-def analyze_waters (pdb_hierarchy, xray_structure, fmodel,
-    distance_cutoff=4.0) :
+class waters (validation) :
   """
-  Assess the properties of solvent atoms and return a list of objects
-  containing their attributes, including local environment and electron
-  density.
+  Assess the properties of solvent atoms, including local environment and
+  electron density.
   """
-  from mmtbx.real_space_correlation import extract_map_stats_for_single_atoms
-  from cctbx import adptbx
-  pdb_atoms = pdb_hierarchy.atoms()
-  assert (not pdb_atoms.extract_i_seq().all_eq(0))
-  unit_cell = xray_structure.unit_cell()
-  pair_asu_table = xray_structure.pair_asu_table(
-    distance_cutoff = distance_cutoff)
-  asu_mappings = pair_asu_table.asu_mappings()
-  asu_table = pair_asu_table.table()
-  u_isos = xray_structure.extract_u_iso_or_u_equiv()
-  occupancies = xray_structure.scatterers().extract_occupancies()
-  sites_cart = xray_structure.sites_cart()
-  sel_cache = pdb_hierarchy.atom_selection_cache()
-  water_sel = sel_cache.selection("resname HOH and name O")
-  map_stats = extract_map_stats_for_single_atoms(
-    pdb_atoms=pdb_atoms,
-    xray_structure=xray_structure,
-    fmodel=fmodel,
-    selection=water_sel)
-  waters = []
-  for i_seq, atom in enumerate(pdb_atoms) :
-    if (water_sel[i_seq]) :
-      asu_dict = asu_table[i_seq]
-      site_cart = sites_cart[i_seq]
-      water = water_properties(
-        i_seq=i_seq,
-        id_str=atom.id_str(),
-        site_cart=site_cart,
-        b_iso=adptbx.u_as_b(u_isos[i_seq]),
-        occ=occupancies[i_seq],
-        nearest_contact=None, #nearest_contact,
-        map_cc=map_stats.two_fofc_ccs[i_seq],
-        two_fofc_map=map_stats.two_fofc_values[i_seq],
-        fofc_map=map_stats.fofc_values[i_seq],
-        anom_map=map_stats.anom_values[i_seq],
-        n_hbonds=None) # TODO
-      waters.append(water)
-  return waters
+
+  __slots__ = validation.__slots__ + ["n_bad", "n_heavy"]
+
+  def get_result_class (self) : return water
+
+  def __init__ (self, pdb_hierarchy, xray_structure, fmodel,
+      distance_cutoff=4.0, collect_all=True) :
+    validation.__init__(self)
+    from mmtbx.real_space_correlation import extract_map_stats_for_single_atoms
+    from cctbx import adptbx
+    self.n_bad = 0
+    self.n_heavy = 0
+    pdb_atoms = pdb_hierarchy.atoms()
+    assert (not pdb_atoms.extract_i_seq().all_eq(0))
+    unit_cell = xray_structure.unit_cell()
+    pair_asu_table = xray_structure.pair_asu_table(
+      distance_cutoff = distance_cutoff)
+    asu_mappings = pair_asu_table.asu_mappings()
+    asu_table = pair_asu_table.table()
+    u_isos = xray_structure.extract_u_iso_or_u_equiv()
+    occupancies = xray_structure.scatterers().extract_occupancies()
+    sites_cart = xray_structure.sites_cart()
+    sel_cache = pdb_hierarchy.atom_selection_cache()
+    water_sel = sel_cache.selection("resname HOH and name O")
+    map_stats = extract_map_stats_for_single_atoms(
+      pdb_atoms=pdb_atoms,
+      xray_structure=xray_structure,
+      fmodel=fmodel,
+      selection=water_sel)
+    waters = []
+    for i_seq, atom in enumerate(pdb_atoms) :
+      if (water_sel[i_seq]) :
+        self.n_total += 1
+        asu_dict = asu_table[i_seq]
+        w = water(
+          pdb_atom=atom,
+          b_iso=adptbx.u_as_b(u_isos[i_seq]),
+          occupancy=occupancies[i_seq],
+          nearest_contact=None, #nearest_contact,
+          score=map_stats.two_fofc_ccs[i_seq],
+          two_fofc=map_stats.two_fofc_values[i_seq],
+          fofc=map_stats.fofc_values[i_seq],
+          anom=map_stats.anom_values[i_seq],
+          n_hbonds=None) # TODO
+        if (w.is_bad_water()) :
+          w.outlier = True
+          self.n_bad += 1
+        elif (w.is_heavy_atom()) :
+          w.outlier = True
+          self.n_heavy += 1
+        if (w.outlier) or (collect_all) :
+          self.results.append(w)
+    self.n_outliers = len(self.results)
+
+  def show_summary (self, out=sys.stdout, prefix="  ") :
+    if (self.n_bad > 0) :
+      print >> out, "%sPoorly ordered waters:  %4d" % (prefix, self.n_bad)
+    if (self.n_heavy > 0) :
+      print >> out, "%sMislabeled waters:      %4d" % (prefix, self.n_heavy)
+    if (self.n_bad == 0) and (self.n_heavy == 0) :
+      print >> out, "%sAll waters okay." % prefix
+
+  def show (self, out=sys.stdout, prefix="  ", verbose=True) :
+    if (len(self.results) > 0) :
+      if (self.n_bad > 0) :
+        print >> out, prefix + "Waters in poor density:"
+        print >> out, prefix + self.get_result_class().header()
+        for result in self.results :
+          if (result.is_bad_water()) :
+            print >> out, prefix + str(result)
+        print >> out, ""
+      if (self.n_heavy > 0) :
+        print >> out, prefix + "Possibly mislabeled atoms:"
+        print >> out, prefix + self.get_result_class().header()
+        for result in self.results :
+          if (result.is_heavy_atom()) :
+            print >> out, prefix + str(result)
+        print >> out, ""
+    self.show_summary(out=out, prefix=prefix)
