5bc3009bb6a2e1c27c7d33c5bcc511dca43620eb
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-09-16 10:48:26 +0000

    Updated XPARM test which was failing.

--- iotbx/xds/tests/tst_xparm.py
+++ iotbx/xds/tests/tst_xparm.py
@@ -24,7 +24,6 @@ class Test(object):
     handle = xparm.reader()
     assert handle.find_version(filename) == 2
     handle.read_file(filename)
-
     print 'OK'
 
     f = open_tmp_file(suffix='XPARM.XDS', mode='wb')
@@ -48,7 +47,9 @@ class Test(object):
       handle.detector_distance,
       handle.detector_x_axis,
       handle.detector_y_axis,
-      handle.detector_normal)
+      handle.detector_normal,
+      handle.segments,
+      handle.orientation)
     writer.write_file(f.name)
     handle_recycled = xparm.reader()
     # make sure we wrote out version 2
@@ -59,30 +60,30 @@ class Test(object):
 
       # Scan and goniometer stuff
       assert handle.starting_frame == 1
-      assert handle.starting_angle == 0.0
-      assert handle.oscillation_range == 0.2
-      assert handle.rotation_axis == (0.999964, 0.00201, 0.008234)
+      assert handle.starting_angle == 82.0
+      assert handle.oscillation_range == 0.1500
+      assert handle.rotation_axis == (0.999997, -0.001590, -0.001580)
 
       # Beam stuff
-      assert handle.wavelength == 0.9795
-      assert handle.beam_vector == (-0.001316, 0.001644, 1.020927)
+      assert handle.wavelength == 0.976250
+      assert handle.beam_vector == (0.001608, 0.004392, 1.024317)
 
       # Detector stuff
       assert handle.detector_size == (2463, 2527)
       assert handle.pixel_size == (0.172, 0.172)
 
-      assert handle.detector_distance == 191.594391
-      assert handle.detector_origin == (1237.948853, 1277.119141)
+      assert handle.detector_distance == 264.928955
+      assert handle.detector_origin == (1224.856812, 1187.870972)
       assert handle.detector_x_axis == (1.0, 0.0, 0.0)
       assert handle.detector_y_axis == (0.0, 1.0, 0.0)
       assert handle.detector_normal == (0.0, 0.0, 1.0)
 
       # Crystal stuff
-      assert handle.space_group == 1
-      assert handle.unit_cell == (39.7964, 42.3646, 42.4588, 90.152, 90.123, 89.985)
-      assert handle.unit_cell_a_axis == (5.487486, -39.117188, -4.846367)
-      assert handle.unit_cell_b_axis == (-35.442802, -7.695671, 21.893908)
-      assert handle.unit_cell_c_axis == (-22.425758, 1.410394, -36.025665)
+      assert handle.space_group == 75
+      assert handle.unit_cell == (57.7831, 57.7831, 150.0135, 90.000, 90.000, 90.000)
+      assert handle.unit_cell_a_axis == (-14.918090, -22.358297, 51.151196)
+      assert handle.unit_cell_b_axis == (-19.858326, 51.608330, 16.766487)
+      assert handle.unit_cell_c_axis == (-135.447952, -34.400188, -54.539391)
 
     # segment stuff
     assert handle_recycled.num_segments == 1
