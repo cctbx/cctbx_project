d6f6dc317217e77f7aaa6de34fe11ac32e985300
Gabor Bunkoczi <gbunkoczi@users.noreply.github.com>
2013-09-17 17:02:55 +0000

    Network-based data queue implemented with multiprocessing

---
+++ libtbx/queuing_system_utils/socket_queue.py
@@ -0,0 +1,158 @@
+from __future__ import division
+
+class MultiQueue(object):
+
+  def __init__(self):
+
+    self.queue_for = {}
+
+
+  def create(self, name):
+
+    import Queue
+    self.queue_for[ name ] = Queue.Queue()
+
+
+  def remove(self, name):
+
+    del self.queue_for[ name ]
+
+
+  def put(self, name, message):
+
+    self.queue_for[ name ].put( message )
+
+
+  def get(self, name):
+
+    return self.queue_for[ name ].get()
+
+
+  def get_nowait(self, name):
+
+    return self.queue_for[ name ].get_nowait()
+
+
+class Queue(object):
+
+  def __init__(self, server):
+
+    import socket
+    import os
+    self.identifier = "%s-%s-%s" % ( socket.getfqdn(), os.getpid(), id( self ) )
+
+    self.server = server
+    self.server.multiqueue.create( self.identifier )
+
+
+  def shutdown(self):
+
+    self.server.multiqueue.remove( self.identifier )
+    self.server = None
+
+
+  def put(self, value):
+
+    self.server.multiqueue.put( self.identifier, value )
+
+
+  def get(self):
+
+    return self.server.multiqueue.get( self.identifier )
+
+
+  def get_nowait(self):
+
+    return self.server.multiqueue.get_nowait( self.identifier )
+
+
+class Manager(object):
+
+  def __init__(self, manager):
+
+    self.manager = manager
+    self.multiqueue = manager.get() # caching
+
+
+  def Queue(self):
+
+    return Queue( server = self )
+
+
+  def __getstate__(self):
+
+    result = self.__dict__.copy()
+    result[ "address" ] = self.manager._address
+    result[ "authkey" ] = str( self.manager._authkey )
+    del result[ "manager" ]
+    del result[ "multiqueue" ]
+    return result
+
+
+  def __setstate__(self, result):
+
+    manager = self.get_client_manager(
+      address = result[ "address" ],
+      authkey = result[ "authkey" ],
+      )
+    result[ "manager" ] = manager
+    result[ "multiqueue" ] = manager.get()
+    del result[ "address" ]
+    del result[ "authkey" ]
+    self.__dict__ = result
+
+
+  @classmethod
+  def Server(cls, port, keylength = 16):
+
+    from multiprocessing.managers import BaseManager
+
+    class QManager(BaseManager):
+
+      pass
+
+    multiqueue = MultiQueue()
+    QManager.register( "get", lambda: multiqueue )
+
+    import socket
+    import string
+    import random
+
+    manager = QManager(
+      address = ( socket.getfqdn(), port ),
+      authkey = "".join(
+        random.choice( string.ascii_letters ) for i in range( keylength )
+        ),
+      )
+    manager.start()
+
+    return cls( manager = manager )
+
+
+  @classmethod
+  def Client(cls, server, port, authkey):
+
+    return cls(
+      manager = cls.get_client_manager(
+        address = (server, port ),
+        autkey = authkey,
+        )
+      )
+
+
+  @staticmethod
+  def get_client_manager(address, authkey):
+
+    from multiprocessing.managers import BaseManager
+
+    class QManager(BaseManager):
+
+      pass
+
+    QManager.register( "get" )
+
+    manager = QManager( address = address, authkey = authkey )
+    manager.connect()
+
+    return manager
+
