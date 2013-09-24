22fa2baea78be85cd9d8aac8e6d74f880fc0013a
Aaron S. Brewster <asbrewster@lbl.gov>
2013-09-24 17:03:06 +0000

    Format class for still shots without scans or goniometers
    Useful for dual inheritance with another Format class

---
+++ dxtbx/format/FormatStill.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# FormatStill.py
+#   Copyright (C) 2013 Diamond Light Source, Aaron Brewster, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is
+#   included in the root directory of this package.
+#
+# Root class for still shots.  A still shot has no goniomter and no
+# scan in their model, as these constructs are not meaningful.
+#
+from __future__ import division
+from dxtbx.format.Format import Format
+from dxtbx.model.detector import Detector
+from dxtbx.model.beam import Beam
+import exceptions
+
+class FormatStill(Format):
+    def setup(self):
+        '''Read the image file, construct the information which we will be
+        wanting about the experiment from this. N.B. in your implementation
+        of this you will probably want to make use of the static methods
+        below and probably add some format parsing code too. Please also keep
+        in mind that your implementation may be further subclassed by
+        someone else.
+
+        Do not create scan or or goniometer objects'''
+        self._start()
+        try:
+            detector_instance = self._detector()
+            assert(isinstance(detector_instance, Detector))
+            self._detector_instance = detector_instance
+
+            beam_instance = self._beam()
+            assert(isinstance(beam_instance, Beam))
+            self._beam_instance = beam_instance
+
+        except exceptions.Exception, e:
+            # FIXME ideally should not squash the errors here...
+            pass
+        finally:
+            self._end()
+
+        return
