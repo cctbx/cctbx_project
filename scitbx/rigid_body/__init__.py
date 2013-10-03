093ff5951e5d3474646e0938bd02b4163b1e1ffd
Pavel Afonine <pafonine@users.noreply.github.com>
2013-10-03 00:56:36 +0000

    Avoid code duplication: move euler code into scitbx.rigid_body

--- scitbx/rigid_body/__init__.py
+++ scitbx/rigid_body/__init__.py
@@ -4,6 +4,8 @@ import scitbx.array_family.flex # import dependency
 import boost.python
 ext = boost.python.import_ext("scitbx_rigid_body_ext")
 from scitbx_rigid_body_ext import *
+import math
+from scitbx import matrix
 
 class pickle_import_trigger(object): pass
 
@@ -37,3 +39,190 @@ class _(boost.python.injector, ext.tardy_model):
       tardy_model=O,
       max_iterations=max_iterations,
       callback_after_step=callback_after_step)
+
+def euler(phi, psi, the, convention):
+  if(convention == "zyz"):
+     result = rb_mat_zyz(the=the, psi=psi, phi=phi)
+  elif(convention == "xyz"):
+     result = rb_mat_xyz(the=the, psi=psi, phi=phi)
+  else:
+     raise RuntimeError("\nWrong rotation convention\n")
+  return result
+
+class rb_mat_zyz(object):
+
+   def __init__(self, the, psi, phi):
+     the = the * math.pi/180
+     psi = psi * math.pi/180
+     phi = phi * math.pi/180
+     self.c_psi = math.cos(psi)
+     self.c_the = math.cos(the)
+     self.c_phi = math.cos(phi)
+     self.s_psi = math.sin(psi)
+     self.s_the = math.sin(the)
+     self.s_phi = math.sin(phi)
+
+   def rot_mat(self):
+     c_psi = self.c_psi
+     c_the = self.c_the
+     c_phi = self.c_phi
+     s_psi = self.s_psi
+     s_the = self.s_the
+     s_phi = self.s_phi
+     r11 =  c_the*c_psi*c_phi - s_the*s_phi
+     r12 = -c_the*c_psi*s_phi - s_the*c_phi
+     r13 =  c_the*s_psi
+     r21 =  s_the*c_psi*c_phi + c_the*s_phi
+     r22 = -s_the*c_psi*s_phi + c_the*c_phi
+     r23 =  s_the*s_psi
+     r31 = -s_psi*c_phi
+     r32 =  s_psi*s_phi
+     r33 =  c_psi
+     rm = matrix.sqr((r11,r12,r13, r21,r22,r23, r31,r32,r33))
+     return rm
+
+   def r_the(self):
+     c_psi = self.c_psi
+     c_the = self.c_the
+     c_phi = self.c_phi
+     s_psi = self.s_psi
+     s_the = self.s_the
+     s_phi = self.s_phi
+     r11 = -s_the*c_psi*c_phi - c_the*s_phi
+     r12 =  s_the*c_psi*s_phi - c_the*c_phi
+     r13 = -s_the*s_psi
+     r21 =  c_the*c_psi*c_phi - s_the*s_phi
+     r22 = -c_the*c_psi*s_phi - s_the*c_phi
+     r23 =  c_the*s_psi
+     r31 = 0.0
+     r32 = 0.0
+     r33 = 0.0
+     rm = matrix.sqr((r11,r12,r13, r21,r22,r23, r31,r32,r33))
+     return rm
+
+   def r_psi(self):
+     c_psi = self.c_psi
+     c_the = self.c_the
+     c_phi = self.c_phi
+     s_psi = self.s_psi
+     s_the = self.s_the
+     s_phi = self.s_phi
+     r11 = -c_the*s_psi*c_phi
+     r12 =  c_the*s_psi*s_phi
+     r13 =  c_the*c_psi
+     r21 = -s_the*s_psi*c_phi
+     r22 =  s_the*s_psi*s_phi
+     r23 =  s_the*c_psi
+     r31 = -c_psi*c_phi
+     r32 =  c_psi*s_phi
+     r33 = -s_psi
+     rm = matrix.sqr((r11,r12,r13, r21,r22,r23, r31,r32,r33))
+     return rm
+
+   def r_phi(self):
+     c_psi = self.c_psi
+     c_the = self.c_the
+     c_phi = self.c_phi
+     s_psi = self.s_psi
+     s_the = self.s_the
+     s_phi = self.s_phi
+     r11 = -c_the*c_psi*s_phi - s_the*c_phi
+     r12 = -c_the*c_psi*c_phi + s_the*s_phi
+     r13 =  0.0
+     r21 = -s_the*c_psi*s_phi + c_the*c_phi
+     r22 = -s_the*c_psi*c_phi - c_the*s_phi
+     r23 =  0.0
+     r31 =  s_psi*s_phi
+     r32 =  s_psi*c_phi
+     r33 =  0.0
+     rm = matrix.sqr((r11,r12,r13, r21,r22,r23, r31,r32,r33))
+     return rm
+
+class rb_mat_xyz(object):
+
+   def __init__(self, phi, psi, the):
+     phi = phi * math.pi/180
+     psi = psi * math.pi/180
+     the = the * math.pi/180
+     self.c_psi = math.cos(psi)
+     self.c_phi = math.cos(phi)
+     self.c_the = math.cos(the)
+     self.s_psi = math.sin(psi)
+     self.s_phi = math.sin(phi)
+     self.s_the = math.sin(the)
+
+   def rot_mat(self):
+     c_psi = self.c_psi
+     c_the = self.c_the
+     c_phi = self.c_phi
+     s_psi = self.s_psi
+     s_the = self.s_the
+     s_phi = self.s_phi
+     r11 =  c_psi*c_phi
+     r12 = -c_psi*s_phi
+     r13 =  s_psi
+     r21 =  c_the*s_phi + s_the*s_psi*c_phi
+     r22 =  c_the*c_phi - s_the*s_psi*s_phi
+     r23 = -s_the*c_psi
+     r31 =  s_the*s_phi - c_the*s_psi*c_phi
+     r32 =  s_the*c_phi + c_the*s_psi*s_phi
+     r33 =  c_the*c_psi
+     rm = matrix.sqr((r11,r12,r13, r21,r22,r23, r31,r32,r33))
+     return rm
+
+   def r_phi(self):
+     c_psi = self.c_psi
+     c_the = self.c_the
+     c_phi = self.c_phi
+     s_psi = self.s_psi
+     s_the = self.s_the
+     s_phi = self.s_phi
+     r11 = -c_psi*s_phi
+     r12 = -c_psi*c_phi
+     r13 =  0.0
+     r21 =  c_the*c_phi - s_the*s_psi*s_phi
+     r22 = -c_the*s_phi - s_the*s_psi*c_phi
+     r23 =  0.0
+     r31 =  s_the*c_phi + c_the*s_psi*s_phi
+     r32 = -s_the*s_phi + c_the*s_psi*c_phi
+     r33 =  0.0
+     rm = matrix.sqr((r11,r12,r13, r21,r22,r23, r31,r32,r33))
+     return rm
+
+   def r_psi(self):
+     c_psi = self.c_psi
+     c_the = self.c_the
+     c_phi = self.c_phi
+     s_psi = self.s_psi
+     s_the = self.s_the
+     s_phi = self.s_phi
+     r11 = -s_psi*c_phi
+     r12 =  s_psi*s_phi
+     r13 =  c_psi
+     r21 =  s_the*c_psi*c_phi
+     r22 = -s_the*c_psi*s_phi
+     r23 =  s_the*s_psi
+     r31 = -c_the*c_psi*c_phi
+     r32 =  c_the*c_psi*s_phi
+     r33 = -c_the*s_psi
+     rm = matrix.sqr((r11,r12,r13, r21,r22,r23, r31,r32,r33))
+     return rm
+
+   def r_the(self):
+     c_psi = self.c_psi
+     c_the = self.c_the
+     c_phi = self.c_phi
+     s_psi = self.s_psi
+     s_the = self.s_the
+     s_phi = self.s_phi
+     r11 =  0.0
+     r12 =  0.0
+     r13 =  0.0
+     r21 = -s_the*s_phi+c_the*s_psi*c_phi
+     r22 = -s_the*c_phi-c_the*s_psi*s_phi
+     r23 = -c_the*c_psi
+     r31 =  c_the*s_phi+s_the*s_psi*c_phi
+     r32 =  c_the*c_phi-s_the*s_psi*s_phi
+     r33 = -s_the*c_psi
+     rm = matrix.sqr((r11,r12,r13, r21,r22,r23, r31,r32,r33))
+     return rm
