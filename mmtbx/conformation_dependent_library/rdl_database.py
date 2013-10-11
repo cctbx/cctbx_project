3c8f2ebefc6a331817b5657afef14e6a03251052
Nigel W Moriarty <nwmoriarty@lbl.gov>
2013-10-11 01:32:08 +0000

    testing for output of LINK records

--- mmtbx/conformation_dependent_library/rdl_database.py
+++ mmtbx/conformation_dependent_library/rdl_database.py
@@ -6,8 +6,17 @@ rdl_database = {
         ("CB", "CG", "CD1") : (126.9, 1.5),
         },
     "p90" : {
-        ("CA", "CB", "CG" ) : (115.6, 1.9),
+        ("CA", "CB", "CG" ) : (115.6, 1.9), 
         ("CB", "CG", "CD1") : (124.9, 1.5),
         },
       },
+  "MET" : {
+    "default" : { # values from restaints library
+      ("CG", "SD", "CE") : (100.9, 2.2),
+        }
+    "default" : {
+      ("CG", "SD", "CE") : (102.9, 2.2),
+        }
+  },
+  "ARG" : {},
     }
