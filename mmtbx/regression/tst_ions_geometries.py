1af5332d2e500cfc78b6035f45ecf07612606085
Nader Morshed <nmorshed@users.noreply.github.com>
2013-10-14 21:55:52 +0000

    Updated tst_ions_geometries so it passes again

--- mmtbx/regression/tst_ions_geometries.py
+++ mmtbx/regression/tst_ions_geometries.py
@@ -5,8 +5,9 @@ import os
 import sys
 
 import libtbx
+from mmtbx.command_line import fetch_pdb
 from mmtbx.ions.geometry import find_coordination_geometry
-from mmtbx.ions import Manager
+from mmtbx import ions
 from iotbx import file_reader
 from cctbx.eltbx import chemical_elements
 from collections import OrderedDict
@@ -17,13 +18,13 @@ def exercise () :
     return
 
   models = OrderedDict([
-    ("3rva", [["octahedron"], ["trigonal_bipyramid"], ["square_plane"]]),
+    ("3rva", [["ring_miss"], ["trigonal_bipyramid"], ["square_plane"]]),
     ("1mjh", [["octahedron"], ["octahedron"]]),
     ("4e1h", [["octahedron"], ["octahedron"], ["octahedron"]]),
     ("2xuz", [["trigonal_prism"]]),
     ("3zli", [["octahedron"], ["tetrahedron"],
               ["octahedron"], ["tetrahedron"],]),
-    ("3e0f", [["octahedron"], ["trigonal_pyramid"], ["square_pyramid"]]),
+    ("3e0f", [["ring_miss"], ["trigonal_pyramid"], ["square_pyramid"]]),
     ("3dkq", [["square_pyramid"], ["three_legs"], ["square_pyramid"]]),
     ("2o8q", [["octahedron"], ["octahedron"]]),
     ("1tgg", [["octahedron"], ["square_pyramid"], ["see_saw"]]),
@@ -37,37 +38,41 @@ def exercise () :
     ("3snm", [[], ["see_saw"]]),
     ("3qlq", [["ring_pop"], ["octahedron"], ["octahedron"], ["ring_pop"],
               ["octahedron"], ["ring_pop"], ["ring_pop"], ["octahedron"]]),
-    ("2gdf", [["square_pyramid"], ["pentagonal_pyramid"],
-              ["square_pyramid"], ["pentagonal_pyramid"]]),
+    ("2gdf", [["square_pyramid"], ["ring_miss"],
+              ["square_pyramid"], ["ring_miss"]]),
   ])
 
   for model, geometries in models.items():
     pdb_path = libtbx.env.find_in_repositories(
-      relative_path = os.path.join("phenix_regression", "mmtbx",
-                                   "geometry", model + ".pdb"),
-      test = os.path.isfile)
-    pdb_in = file_reader.any_file(pdb_path, force_type = "pdb")
-    manager = Manager(
+      relative_path = os.path.join(
+        "phenix_regression", "mmtbx", "geometry", model + ".pdb"),
+        test = os.path.isfile
+      )
+    pdb_in = file_reader.any_file(pdb_path).file_object
+    manager = ions.Manager(
       fmodel = None,
-      pdb_hierarchy = pdb_in.file_object.construct_hierarchy(),
-      xray_structure = pdb_in.file_object.xray_structure_simple(),
-      connectivity = pdb_in.file_object.extract_connectivity())
+      pdb_hierarchy = pdb_in.construct_hierarchy(),
+      xray_structure = pdb_in.xray_structure_simple(),
+      connectivity = pdb_in.extract_connectivity())
 
-    elements = chemical_elements.proper_upper_list()
-    elements = [element for element in elements
-                if element.strip().upper() not in ["O", "C", "H", "N"]]
+    elements = set(ions.DEFAULT_IONS + ions.TRANSITION_METALS)
+    elements.difference_update(["CL"])
 
     metals = [i_seq for i_seq, atom in enumerate(manager.pdb_atoms)
              if atom.fetch_labels().resname.strip().upper() in elements]
-    for index, metal, expected_geometry in zip(xrange(100), metals, geometries):
+    assert len(metals) == len(geometries)
+
+    for metal, expected_geometry in zip(metals, geometries):
       contacts = manager.find_nearby_atoms(metal, filter_by_two_fofc = False)
       found = find_coordination_geometry(contacts, minimizer_method = True)
       geometry_names = [i[0] for i in found]
+
       if geometry_names != expected_geometry:
-        print "Problem detecting geometries in", model, index
+        print "Problem detecting geometries in", model,
+        print manager.pdb_atoms[metal].id_str()
         print "Found geometries:", geometry_names
         print "Should be:", expected_geometry
-        sys.exit(1)
+        sys.exit()
 
   print "OK"
 
