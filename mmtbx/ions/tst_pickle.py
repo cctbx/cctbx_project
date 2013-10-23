f9f65aa8c40d3aa79a14a5a99e988b0d3ccb5a9e
Nader Morshed <morshed.nader@gmail.com>
2013-10-23 23:03:35 +0000

    Made tst_pickle.py test a larger set of values to make sure they are stored and re-set correctly

--- mmtbx/ions/tst_pickle.py
+++ mmtbx/ions/tst_pickle.py
@@ -55,8 +55,38 @@ def exercise():
     out = null_out
     )
 
-  for atom_prop in manager.atoms_to_props.values():
-    atom_prop = loads(dumps(atom_prop))
+  fo_map = manager.get_map("mFo")
+
+  for atom_props in manager.atoms_to_props.values():
+    new_atom_props = loads(dumps(atom_props))
+
+    # Test all of the values used by the machine learning module
+    assert atom_props.atom.id_str() == \
+      new_atom_props.atom.id_str()
+    assert atom_props.atom.segid.strip().upper() == \
+      new_atom_props.atom.segid.strip().upper()
+    assert atom_props.resname == new_atom_props.resname
+    assert atom_props.b_iso == new_atom_props.b_iso
+    assert atom_props.occ == new_atom_props.occ
+    assert atom_props.d_min == new_atom_props.d_min
+    assert atom_props.wavelength == new_atom_props.wavelength
+    assert len(atom_props.nearby_atoms) == len(new_atom_props.nearby_atoms)
+    assert atom_props.residue_counts == new_atom_props.residue_counts
+    assert atom_props.peak_2fofc == new_atom_props.peak_2fofc
+    assert atom_props.peak_fofc == new_atom_props.peak_fofc
+    assert atom_props.peak_anom == new_atom_props.peak_anom
+    assert atom_props.estimated_weight == new_atom_props.estimated_weight
+    assert atom_props.fp == new_atom_props.fp
+    assert atom_props.fpp == new_atom_props.fpp
+
+    geometry = ions.find_coordination_geometry(
+      atom_props.nearby_atoms, minimizer_method = True)
+    new_geometry = ions.find_coordination_geometry(
+      new_atom_props.nearby_atoms, minimizer_method = True)
+
+    assert geometry == new_geometry
+    assert ions.fit_gaussian(manager, atom_props.atom.xyz, fo_map) == \
+      ions.fit_gaussian(manager, new_atom_props.atom.xyz, fo_map)
 
   print "OK"
 
