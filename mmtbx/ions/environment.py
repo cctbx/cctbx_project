6b8e5eef2c0483bf384fa5afe48027452751ce6e
Nader Morshed <nmorshed@users.noreply.github.com>
2013-10-30 21:12:41 +0000

    Environment now collects info on the electron density, anomalous scattering, and geometry of an atom. Updated the relevant test

--- mmtbx/ions/environment.py
+++ mmtbx/ions/environment.py
@@ -6,10 +6,14 @@ chemical environments.
 from __future__ import division
 
 from collections import Counter
+from math import pi, cos, sin
 
 from iotbx.pdb import common_residue_names_get_class as get_class
 from libtbx.utils import Sorry
-from mmtbx.ions import same_atom_different_altloc
+from mmtbx import ions
+from mmtbx.ions import frange
+from scitbx.array_family import flex
+from scitbx.math import gaussian_fit_1d_analytical
 
 # Enums for the chemical environments supported by this module
 chem_carboxy = 1
@@ -28,15 +32,41 @@ chem_nitrogen = 13
 chem_sulfur = 14
 
 class Environment (object):
-  def __init__(self, contacts, manager):
+  def __init__(self, i_seq, contacts, manager,
+               chemistry = True, geometry = True,
+               electron_density = True, anomalous = True):
     # XXX: This constructor needs a better API...should we just take the i_seq
     # and generate all other information (Including the list of contacts) from
     # that and the manager?
-    self.chemistry_env = self._get_chemical_environment(contacts, manager)
-    # from mmtbx.ions import geometry
-    # self.geometry = geometry.find_coordination_geometry(contacts, minimizer_method = True)
-    # self.electron_density = None
-    # self.anomalous = None
+    if chemistry:
+      self.chemistry = self._get_chemical_environment(contacts, manager)
+    else:
+      self.chemistry = None
+
+    if geometry:
+      self.geometry = ions.geometry.find_coordination_geometry(
+        contacts, minimizer_method = True)
+    else:
+      geometry = None
+
+    if electron_density:
+      fo_map = None
+      self.electron_density = _fit_gaussian(
+        manager, manager.pdb_atoms[i_seq].xyz, fo_map)
+    else:
+      self.electron_density = None
+
+    if anomalous:
+      self.fp, self.fpp = manager.get_fp(i_seq), manager.get_fpp(i_seq)
+      self.wavelength = manager.wavelength
+    else:
+      self.fp, self.fpp = None, None
+      self.wavelength = manager.wavelength
+
+    # We can either store the contacts or generate a list of valences for all of
+    # the atom types we want to test. I'm choosing the former because it's more
+    # flexible down the road and contacts is pickable anyways.
+    self.contacts = contacts
     # self.valences = None
 
   def _get_chemical_environment(self, contacts, manager):
@@ -73,7 +103,8 @@ class Environment (object):
       """
       neighbors = []
       for other in manager.connectivity[seq]:
-        if manager.server.get_element(manager.pdb_atoms[other]) not in ["H"]:
+        other_atom = manager.pdb_atoms[other]
+        if manager.server.get_element(other_atom) not in ["H", "D"]:
           neighbors.append(other)
       return neighbors
 
@@ -86,7 +117,7 @@ class Environment (object):
       for index, i_seq in enumerate(i_seqs):
         no_alt_loc = True
         for j_seq in i_seqs[index + 1:]:
-          if same_atom_different_altloc(
+          if ions.same_atom_different_altloc(
               manager.pdb_atoms[i_seq], manager.pdb_atoms[j_seq]):
             no_alt_loc = False
             break
@@ -101,7 +132,7 @@ class Environment (object):
     for index, contact in enumerate(contacts):
       no_alt_loc = True
       for other_contact in contacts[index + 1:]:
-        if same_atom_different_altloc(contact.atom, other_contact.atom) and \
+        if ions.same_atom_different_altloc(contact.atom, other_contact.atom) and \
           contact.rt_mx == other_contact.rt_mx:
           no_alt_loc = False
           break
@@ -167,3 +198,89 @@ class Environment (object):
                   chem_env[chem_amide] += 1
 
     return chem_env
+
+def _get_points_within_radius(point, radius, radius_step = 0.2,
+                              angle_step = pi / 5):
+  """
+  Generates a list of points and their associated radius in steps around a
+  sphere.
+
+  Parameters
+  ----------
+  point: tuple of float, float, float
+      X, Y, Z, coordinates to center the sampling around.
+  radius: float
+      Max radius around the center to sample.
+  radius_step: float, optional
+      Steps along the radius to use when sampling.
+  angle_step: float, optional
+      Steps around each radii distance to use when sampling. Amount is in
+      radians.
+
+  Returns
+  -------
+  list of tuple of float, float, float
+      List of points to be sampled.
+  list of float
+      List of radii corresponding to each point.
+  """
+
+  points = [point]
+  radiuses = [0]
+  for r in frange(radius_step, radius, radius_step):
+    for theta in frange(-pi, pi, angle_step):
+      for phi in frange(-pi, pi, angle_step):
+        x = r * cos(theta) * sin(phi) + point[0]
+        y = r * sin(theta) * sin(phi) + point[1]
+        z = r * cos(phi) + point[2]
+        points.append((x, y, z))
+        radiuses.append(r)
+
+  return points, radiuses
+
+def _fit_gaussian(manager, site_cart, real_map, radius = 1.6):
+  """
+  Fit a gaussian function to the map around a site. Samples points in concentric
+  spheres up to radius away from the site.
+
+  f(x) = a * exp(-b * x ** 2)
+
+  Parameters
+  ----------
+  manager: mmtbx.ions.Manager
+  site_cart: tuple of float, float, float
+      The site's cartesian coordinates to sample the density around
+  real_map: ...
+      Real space map of the electron density in the unit cell.
+  radius: float, optional
+      The max radius to use for sampling.
+
+  Returns
+  -------
+  float
+      Height of gaussian curve.
+  float
+      Spread of guassian curve.
+  """
+  points, radiuses = _get_points_within_radius(site_cart, radius)
+
+  map_heights = \
+    [real_map.tricubic_interpolation(manager.unit_cell.fractionalize(i))
+     for i in points]
+
+  # Gaussian functions can't have negative values, filter sampled points below
+  # zero to allow us to find the analytical solution (radius = 2.0 is too big
+  # for most atoms anyways)
+  x, y = flex.double(), flex.double()
+  for rad, height in zip(radiuses, map_heights):
+    if height > 0:
+      x.append(rad)
+      y.append(height)
+
+  try:
+    fit = gaussian_fit_1d_analytical(x = x, y = y)
+  except RuntimeError as err:
+    print(err)
+    return 0., 0.
+  else:
+    return fit.a, fit.b
