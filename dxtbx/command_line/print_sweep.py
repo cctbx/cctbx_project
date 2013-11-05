33539b1c327b4496a02b6a67c06ec8bf1e7be586
Graeme Winter <graeme.winter@gmail.com>
2013-11-05 10:21:58 +0000

    refactor for new sweep access

--- dxtbx/command_line/print_sweep.py
+++ dxtbx/command_line/print_sweep.py
@@ -1,14 +1,14 @@
 from __future__ import division
 def print_sweep(list_of_images):
 
-    from dxtbx.sweep import SweepFactory
+    from dxtbx.imageset import ImageSetFactory
+    sweeps = ImageSetFactory.new(list_of_images)
 
-    s = SweepFactory.sweep(list_of_images)
-
-    print s.get_detector()
-    print s.get_beam()
-    print s.get_goniometer()
-    print s.get_scan()
+    for s in sweeps:
+        print s.get_detector()
+        print s.get_beam()
+        print s.get_goniometer()
+        print s.get_scan()
 
 if __name__ == '__main__':
     import sys
