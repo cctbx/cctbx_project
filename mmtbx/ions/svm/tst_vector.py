f52f52ea3b9c35114fadf1a0f3e5954fbd342369
Nader Morshed <morshed.nader@gmail.com>
2013-11-07 00:26:52 +0000

    Added a new submodule for svm-related code Moved in code to turn ChemicalEnvironment and ScatteringEnvironment into one long vector of features and an associated class Added a test for the ion_vector code

---
+++ mmtbx/ions/svm/tst_vector.py
@@ -0,0 +1,80 @@
+ # -*- coding: utf-8; py-indent-offset: 2 -*-
+
+from __future__ import division
+
+import os
+from pickle import loads, dumps
+from types import MethodType
+
+import libtbx
+from mmtbx.command_line.water_screen import master_phil
+from mmtbx import ions
+from mmtbx.ions.environment import ChemicalEnvironment, ScatteringEnvironment
+from mmtbx.ions.svm import ion_class, ion_vector
+from mmtbx.regression.make_fake_anomalous_data import generate_zinc_inputs
+import mmtbx.utils
+
+def exercise():
+  wavelength = 1.025
+  mtz_file, pdb_file = generate_zinc_inputs(anonymize = False)
+  null_out = libtbx.utils.null_out()
+
+  cmdline = mmtbx.utils.cmdline_load_pdb_and_data(
+    args = [pdb_file, mtz_file, "wavelength={}".format(wavelength),
+            "use_phaser=False"],
+    master_phil = master_phil,
+    out = null_out,
+    process_pdb_file = True,
+    create_fmodel = True,
+    prefer_anomalous = True
+    )
+
+  os.remove(pdb_file)
+  os.remove(mtz_file)
+
+  cmdline.xray_structure.set_inelastic_form_factors(
+    photon = cmdline.params.wavelength,
+    table = "sasaki"
+    )
+
+  cmdline.fmodel.update_xray_structure(
+    cmdline.xray_structure,
+    update_f_calc = True
+    )
+
+  manager = ions.create_manager(
+    pdb_hierarchy = cmdline.pdb_hierarchy,
+    fmodel = cmdline.fmodel,
+    geometry_restraints_manager = cmdline.geometry,
+    wavelength = cmdline.params.wavelength,
+    params = cmdline.params,
+    nproc = cmdline.params.nproc,
+    log = null_out
+    )
+
+  manager.validate_ions(
+    out = null_out
+    )
+
+  fo_map = manager.get_map("mFo")
+
+  for atom_props in manager.atoms_to_props.values():
+    chem_env = ChemicalEnvironment(
+      atom_props.i_seq,
+      manager.find_nearby_atoms(atom_props.i_seq, far_distance_cutoff = 3.5),
+      manager
+      )
+    scatter_env = ScatteringEnvironment(
+      atom_props.i_seq, manager, fo_map
+      )
+    vector = ion_vector(chem_env, scatter_env, server = manager.server)
+    resname = ion_class(chem_env)
+    assert vector is not None
+    assert resname != ""
+
+  del fo_map
+
+  print "OK"
+
+if __name__ == "__main__":
+  exercise()
