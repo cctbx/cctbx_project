10311b7758dce102939f7e4830649365112c7373
Nader Morshed <nmorshed@users.noreply.github.com>
2013-11-07 01:02:16 +0000

    Added a module for utility functions for ions

---
+++ mmtbx/ions/svm/utils.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8; py-indent-offset: 2 -*-
+"""
+Utility functions used within this module.
+"""
+
+def iterate_sites(pdb_hierarchy, split_sites = False, res_filter = None):
+  """
+  Returns a generator iterating over all atoms in pdb_hierarchy. Optionally
+  skips sites with alternate conformations and can filter by residue name.
+
+  Parameters
+  ----------
+  pdb_hierarchy: iotbx.pdb_hierarchy
+  split_sites: bool, optional
+      Indicates whether to iterate over sites with alternate conformations, by
+      default they are not included.
+  res_filter: list of str, optional
+      List of residue names to include, by default, all residues are examined.
+  """
+
+  for model in pdb_hierarchy.models():
+    for chain in model.chains():
+      for residue_group in chain.residue_groups():
+        for atom_group in residue_group.atom_groups():
+          if res_filter is None or \
+            atom_group.resname.strip().upper() in res_filter:
+            atoms = atom_group.atoms()
+            if len(atoms) == 1 or split_sites:
+              for atom in atoms:
+                yield atom
