959eded7ae2d40349d9c738b54e550c76c259995
Nader Morshed <morshed.nader@gmail.com>
2013-11-07 01:02:41 +0000

    Added a test for dump_sites.py

---
+++ mmtbx/ions/svm/tst_dump_sites.py
@@ -0,0 +1,36 @@
+ # -*- coding: utf-8; py-indent-offset: 2 -*-
+
+from __future__ import division
+
+import os
+
+from pickle import load
+
+import libtbx
+from mmtbx.regression.make_fake_anomalous_data import generate_zinc_inputs
+from mmtbx.ions.svm.dump_sites import main
+
+def exercise():
+  wavelength = 1.025
+  mtz_file, pdb_file = generate_zinc_inputs(anonymize = False)
+  null_out = libtbx.utils.null_out()
+  main(["skip_twin_detection=True", "use_phaser=False",
+        "input.pdb.file_name=" + pdb_file,
+        "input.xray_data.file_name=" + mtz_file,
+        "wavelength={}".format(wavelength)], out = null_out)
+
+  sites_path = os.path.splitext(pdb_file)[0] + "_sites.pkl"
+  sites = load(open(sites_path))
+
+  assert len(sites) == 7
+  for chem_env, scatter_env in sites:
+    assert chem_env is not None
+    assert scatter_env is not None
+
+  os.remove(pdb_file)
+  os.remove(mtz_file)
+
+  print "OK"
+
+if __name__ == "__main__":
+  exercise()
