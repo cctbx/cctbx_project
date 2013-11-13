cc93e29b2d9f8d70125189d8bc3fcdc70d270902
Nathaniel Echols <nathaniel.echols@gmail.com>
2013-11-13 00:33:05 +0000

    choice control for symmetry operators

--- wxtbx/phil_controls/symop.py
+++ wxtbx/phil_controls/symop.py
@@ -1,6 +1,7 @@
-from __future__ import division
 
+from __future__ import division
 from wxtbx.phil_controls.text_base import ValidatedTextCtrl, TextCtrlValidator
+from wxtbx import phil_controls
 from libtbx.utils import Sorry
 from libtbx import Auto
 import wx
@@ -43,6 +44,50 @@ class SymopValidator (TextCtrlValidator) :
     rt_mx = sgtbx.rt_mx(symbol=value)
     return value
 
+class SymopChoiceCtrl (wx.Choice, phil_controls.PhilCtrl) :
+  def __init__ (self, *args, **kwds) :
+    super(SymopChoiceCtrl, self).__init__(*args, **kwds)
+    self._space_group = None
+    self.Bind(wx.EVT_CHOICE, lambda evt: self.DoSendEvent(), self)
+
+  def SetSpaceGroup (self, space_group) :
+    if (type(space_group).__name__ == "space_group_info") :
+      space_group = space_group.group()
+    self._space_group = space_group
+    if (space_group is None) :
+      self.SetItems([])
+    else :
+      current_symop = self.GetStringSelection()
+      symops = [ str(smx) for smx in space_group.smx() ]
+      self.SetItems(symops)
+      if (current_symop in symops) :
+        self.SetStringSelection(current_symop)
+
+  def GetValue (self) :
+    """
+    Returns a rotation/translation matrix.
+    """
+    if (self._space_group is not None) :
+      selected_symop = self.GetStringSelection()
+      for smx in self._space_group.smx() :
+        if (str(smx) == selected_symop) :
+          return smx
+    return None
+
+  def GetPhilValue (self) :
+    """
+    Returns a string if defined, otherwise None.
+    """
+    sel = self.GetStringSelection()
+    if (sel == "") :
+      return None
+    return sel
+
+  def SetValue (self, value) :
+    items = self.GetItems()
+    if (str(value) in items) :
+      self.SetStringSelection(str(value))
+
 if (__name__ == "__main__") :
   app = wx.App(0)
   frame = wx.Frame(None, -1, "Symop control test")
@@ -50,11 +95,21 @@ if (__name__ == "__main__") :
   txt1 = wx.StaticText(panel, -1, "Symmetry operator:", pos=(100,180))
   sym_ctrl = SymopCtrl(panel, -1, pos=(300,180), size=(80,-1),
       name="Symmetry operator")
+  txt2 = wx.StaticText(panel, -1, "Symmetry operator:", pos=(100,240))
+  sym_ctrl_2 = SymopChoiceCtrl(panel, -1, pos=(300, 240), size=(160,-1),
+      name="Symmetry operator choice")
+  from cctbx import sgtbx
+  sym_ctrl_2.SetSpaceGroup(sgtbx.space_group_info("P6322"))
   btn = wx.Button(panel, -1, "Process input", pos=(400, 360))
   def OnOkay (evt) :
     symop = sym_ctrl.GetPhilValue()
-    print symop
+    symop2 = sym_ctrl_2.GetPhilValue()
+    print symop, symop2
+  def OnPhilCtrl (evt) :
+    symop3 = sym_ctrl_2.GetValue()
+    symop3.show_geometrical_elements()
   frame.Bind(wx.EVT_BUTTON, OnOkay, btn)
+  frame.Bind(phil_controls.EVT_PHIL_CONTROL, OnPhilCtrl, sym_ctrl_2)
   frame.Fit()
   frame.Show()
   app.MainLoop()
