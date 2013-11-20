e90bb90ecdae864811b96680fe042e09aa477b50
Johan Hattne <johan@hattne.se>
2013-11-20 10:09:08 +0000

    Feed input filenames through multiprocessing queue to allow for load balancing in the face of many invalid input files.  Thanks to Christian Roessler.  Closes #16.

--- xfel/command_line/cxi_merge.py
+++ xfel/command_line/cxi_merge.py
@@ -188,20 +188,19 @@ def load_result (file_name,
                  reference_cell,
                  params,
                  out) :
+  # If @p file_name cannot be read, the load_result() function returns
+  # @c None.
+
   print "Step 2.  Load pickle file into dictionary obj and filter on lattice & cell"
   """
   Take a pickle file, confirm that it contains the appropriate data, and
   check the lattice type and unit cell against the reference settings - if
   rejected, raises an exception (for tracking statistics).
   """
-  if (file_name.endswith("stats.pickle")) :
-    return None
-
   # Ignore corrupted pickle files.
   try:
     obj = easy_pickle.load(file_name=file_name)
   except Exception:
-    print "Skipping corrupted frame in", file_name
     return None
   if (not obj.has_key("observations")) :
     return None
@@ -287,7 +286,10 @@ class null_data (object) :
   included, to save pickling time during multiprocessing.)
   """
   def __init__ (self, file_name, log_out,
-                low_signal=False, wrong_bravais=False, wrong_cell=False) :
+                file_error=False,
+                low_signal=False,
+                wrong_bravais=False,
+                wrong_cell=False) :
     adopt_init_args(self, locals())
 
   def show_log_out (self, out) :
@@ -379,6 +381,7 @@ class scaling_manager (intensity_data) :
   def reset (self) :
     self.n_processed = 0
     self.n_accepted = 0
+    self.n_file_error = 0
     self.n_low_signal = 0
     self.n_wrong_bravais = 0
     self.n_wrong_cell = 0
@@ -408,7 +411,7 @@ class scaling_manager (intensity_data) :
     print >> self.log, "FINISHED MERGING"
     print >> self.log, "  Elapsed time: %.1fs" % (t2 - t1)
     print >> self.log, "  %d of %d integration files were accepted" % (
-      self.n_accepted, len(file_names))
+      self.n_accepted, len(file_names) - self.n_file_error)
     print >> self.log, "  %d rejected due to wrong Bravais group" % \
       self.n_wrong_bravais
     print >> self.log, "  %d rejected for unit cell outliers" % \
@@ -417,8 +420,9 @@ class scaling_manager (intensity_data) :
       self.n_low_signal
     print >> self.log, "  %d rejected due to poor correlation" % \
       self.n_low_corr
-    checksum = self.n_accepted + self.n_wrong_bravais + self.n_wrong_cell + \
-               self.n_low_signal + self.n_low_corr
+    checksum = self.n_accepted  + self.n_file_error \
+               + self.n_low_corr + self.n_low_signal \
+               + self.n_wrong_bravais + self.n_wrong_cell
     assert checksum == len(file_names)
 
   def _scale_all_parallel (self, file_names) :
@@ -439,23 +443,34 @@ class scaling_manager (intensity_data) :
     db_mgr = manager(self.params)
     db_mgr.initialize_db(self.i_model.indices())
 
+    # Input files are supplied to the scaling processes on demand by
+    # means of a queue.
+    #
+    # XXX The input queue may need to either allow non-blocking
+    # put():s or run in a separate process to prevent the procedure
+    # from blocking here if the list of file paths does not fit into
+    # the queue's buffer.
+    input_queue = multiprocessing.Manager().JoinableQueue()
+    for file_name in file_names:
+      input_queue.put(file_name)
+
     pool = multiprocessing.Pool(processes=nproc)
-    # Round-robin the frames through the process pool.  Each process
-    # accumulates its own statistics in serial, and the grand total is
-    # eventually collected by the main process' _add_all_frames()
-    # function.
+    # Each process accumulates its own statistics in serial, and the
+    # grand total is eventually collected by the main process'
+    # _add_all_frames() function.
     for i in xrange(nproc) :
       sm = scaling_manager(self.miller_set, self.i_model, self.params)
       pool.apply_async(
         func=sm,
-        args=[[file_names[j] for j in xrange(i, len(file_names), nproc)],
-              db_mgr],
+        args=[input_queue, db_mgr],
         callback=self._add_all_frames)
     pool.close()
     pool.join()
 
-    # Block until all database commands have been processed.
+    # Block until all database commands have been processed, and the
+    # input queue has been emptied.
     db_mgr.join()
+    input_queue.join()
 
 
   def _scale_all_serial (self, file_names, db_mgr) :
@@ -481,7 +496,9 @@ class scaling_manager (intensity_data) :
     #data.show_log_out(self.log)
     #self.log.flush()
     if (isinstance(data, null_data)) :
-      if (data.low_signal) :
+      if (data.file_error) :
+        self.n_file_error += 1
+      elif (data.low_signal) :
         self.n_low_signal += 1
       elif (data.wrong_bravais) :
         self.n_wrong_bravais += 1
@@ -518,6 +535,7 @@ class scaling_manager (intensity_data) :
     need a lock.
     """
     self.n_accepted += data.n_accepted
+    self.n_file_error += data.n_file_error
     self.n_low_corr += data.n_low_corr
     self.n_low_signal += data.n_low_signal
     self.n_processed += data.n_processed
@@ -641,9 +659,24 @@ class scaling_manager (intensity_data) :
     all_obs.show_summary(self.log, prefix="  ")
     return mtz_file, all_obs
 
-  def __call__ (self, file_names, db_mgr) :
+  def __call__ (self, input_queue, db_mgr) :
+    # Scale frames sequentially within the current process.  The
+    # return value is picked up by the callback.  See also
+    # self.scale_all_serial()
+    from Queue import Empty
+
     try :
-      return self._scale_all_serial(file_names, db_mgr)
+      while True:
+        try:
+          file_name = input_queue.get_nowait()
+        except Empty:
+          return self
+
+        scaled = self.scale_frame(file_name, db_mgr)
+        if scaled is not None:
+          self.add_frame(scaled)
+        input_queue.task_done()
+
     except Exception, e :
       print >> self.log, str(e)
       return None
@@ -665,9 +698,9 @@ class scaling_manager (intensity_data) :
         ref_bravais_type=self.ref_bravais_type,
         params=self.params,
         out=out)
-      if result is None: # load_result can return None for many reasons
+      if result is None:
         return null_data(
-        file_name=file_name, log_out=out.getvalue(), low_signal=True)
+          file_name=file_name, log_out=out.getvalue(), file_error=True)
     except OutlierCellError, e :
       print >> out, str(e)
       return null_data(
