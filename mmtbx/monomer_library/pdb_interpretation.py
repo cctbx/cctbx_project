1cd7f8616b67d8d25f63fd1602893b69d5e293cf
Pavel Afonine <pafonine@users.noreply.github.com>
2013-12-03 02:00:06 +0000

    use full_connectivty_table from grm

--- mmtbx/monomer_library/pdb_interpretation.py
+++ mmtbx/monomer_library/pdb_interpretation.py
@@ -4834,7 +4834,7 @@ class process(object):
             ramachandran_atom_selection=ramachandran_atom_selection,
             den_manager=den_manager,
             reference_manager=reference_manager,
-            log=self.log)   
+            log=self.log)
       if (self.log is not None):
         print >> self.log, \
           "  Time building geometry restraints manager: %.2f seconds" % (
@@ -4962,27 +4962,22 @@ class process(object):
           flush_log(self.log)
         if not self.for_dihedral_reference:
           self.clash_guard(hard_minimum_nonbonded_distance=hard_minimum_nonbonded_distance)
-
-        
         # Display nonbonded clashscore
         if show_nonbonded_clashscore:
           grm = self.geometry_restraints_manager()
           xrs = self.xray_structure()
           sites_cart = xrs.sites_cart()
           hd_sel = xrs.hd_selection()
-          table_bonds = grm.shell_sym_tables[0]
-          full_connectivty_table = table_bonds.full_simple_connectivity()
-          nb_clash_info = grm.get_nonbonded_clashscore(sites_cart=sites_cart,
-                                                       site_labels=site_labels,
-                                                       hd_sel=hd_sel,
-                                                       full_connectivty_table=full_connectivty_table)
+          nb_clash_info = grm.get_nonbonded_clashscore(
+            sites_cart=sites_cart,
+            site_labels=site_labels,
+            hd_sel=hd_sel)
           nb_clashscore_without_sym_op = nb_clash_info.nb_clashscore_without_sym_op
           nb_clash_due_to_sym_op = nb_clash_info.nb_clashscore_due_to_sym_op
-          print >> self.log, '\n  clashscore, without clashes due to symmetry operation: {0:.2f}'.format(
-            nb_clashscore_without_sym_op)
-          print >> self.log, '  clashscore, only due to symmetry operation: {0:.2f}\n'.format(
-            nb_clash_due_to_sym_op)
-
+          s='\n  clashscore, without clashes due to symmetry operation: {0:.2f}'
+          print >> self.log, s.format(nb_clashscore_without_sym_op)
+          s='  clashscore, only due to symmetry operation: {0:.2f}\n'
+          print >> self.log, s.format(nb_clash_due_to_sym_op)
     return self._geometry_restraints_manager
 
 
