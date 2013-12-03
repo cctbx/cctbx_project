ad772269a2865a971ed6d5fb5b87e04db49275b1
Graeme Winter <graeme.winter@gmail.com>
2013-12-03 12:18:05 +0000

    Method to import mosflm matrix and generate a crystal_model

--- cctbx/crystal/crystal_model/__init__.py
+++ cctbx/crystal/crystal_model/__init__.py
@@ -168,3 +168,61 @@ class crystal_model(object):
                             change_of_basis_op),
                           mosaicity=self.get_mosaicity())
     return other
+
+def crystal_model_from_mosflm_matrix(mosflm_A_matrix,
+                                     unit_cell=None,
+                                     wavelength=None,
+                                     space_group=None):
+  '''Create a crystal_model from a Mosflm A matrix (a*, b*, c*); N.B. assumes
+  the mosflm coordinate frame:
+
+                                                   /!
+                      Y-axis                      / !
+                        ^                        /  !
+                        !                       /   !
+                        !                      /    !
+                        !   /                 /  Xd !
+                        !  /                 / * ^  !
+                        ! /                  ! 3 !  !
+                        !/      X-ray beam   !   !  !
+                        /------------------------/--!---->X-axis
+                       /                     !  / *1!
+                    <-/-                     ! /    !
+                     /  \+ve phi             ! Yd  /
+                    /   /                    ! 2  /
+                   /                         ! * /
+                  Z-axis                  Ys ^ _/
+                Rotation                     ! /| Xs
+                 axis                        !/
+                                             O
+
+  Also assume that the mosaic spread is 0. If space_group is None spacegroup
+  will be assigned as P1.
+  '''
+
+  if not space_group:
+    space_group = SG('P1')
+
+  A_star = matrix.sqr(mosflm_A_matrix)
+  A = A_star.inverse()
+
+  if unit_cell:
+    unit_cell_constants = unit_cell.parameters()
+    a = matrix.col(A.elems[0:3])
+    wavelength = unit_cell_constants[0] / a.length()
+    A *= wavelength
+  elif wavelength:
+    A *= wavelength
+  else:
+    # assume user has pre-scaled
+    pass
+
+  a = A.elems[0:3]
+  b = A.elems[3:6]
+  c = A.elems[6:9]
+  rotate_mosflm_to_imgCIF = matrix.sqr((0, 0, 1, 0, 1, 0, -1, 0, 0))
+  _a = rotate_mosflm_to_imgCIF * a
+  _b = rotate_mosflm_to_imgCIF * b
+  _c = rotate_mosflm_to_imgCIF * c
+
+  return crystal_model(_a, _b, _c, space_group=space_group)
