9df7071f4e38b5e702eb21a395fb99a9c3b64339
Aaron S. Brewster <asbrewster@lbl.gov>
2013-12-11 01:34:25 +0000

    XPP support for converting image pickles to cbf files.  Lays groundwork for using dials to index cbf files directly from an xtc stream.
    Also enables cxi.display_metrology to show the metrology from
    an image file using the dxtbx detector model.

--- xfel/command_line/cxi_display_metrology.py
+++ xfel/command_line/cxi_display_metrology.py
@@ -9,7 +9,7 @@ import sys, os
 import libtbx.phil
 from libtbx.utils import Usage, Sorry
 from matplotlib import pyplot as plt
-from matplotlib.patches import Rectangle
+from matplotlib.patches import Rectangle,Polygon
 from xfel.cxi.cspad_ana.cspad_tbx import xpp_active_areas
 import sys
 
@@ -37,13 +37,35 @@ if (__name__ == "__main__") :
     master_phil.show()
     raise Usage("metrology must be defined (either metrology=XXX, or the name alone).")
 
+  fig = plt.figure()
+  ax = fig.add_subplot(111, aspect='equal')
+
   if os.path.isfile(params.metrology):
-    print "Not impl"
+    from dxtbx.format.Registry import Registry
+    from scitbx.matrix import col
+    reader = Registry.find(params.metrology)
+    img = reader(params.metrology)
+
+    detector = img.get_detector()
+
+    for i, panel in enumerate(detector):
+      size = panel.get_image_size()
+      p0 = col(panel.get_pixel_lab_coord((0,0)))
+      p1 = col(panel.get_pixel_lab_coord((size[0]-1,0)))
+      p2 = col(panel.get_pixel_lab_coord((size[0]-1,size[1]-1)))
+      p3 = col(panel.get_pixel_lab_coord((0,size[1]-1)))
+
+      v1 = p1-p0
+      v2 = p3-p0
+      vcen = ((v2/2) + (v1/2)) + p0
+
+      ax.add_patch(Polygon((p0[0:2],p1[0:2],p2[0:2],p3[0:2]), closed=True, color='green', fill=False, hatch='/'))
+      ax.annotate(i, vcen[0:2])
+
+      ax.set_xlim((-100, 100))
+      ax.set_ylim((-100, 100))
   else:
     active_areas = xpp_active_areas[params.metrology]['active_areas']
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, aspect='equal')
 
     for asic_number, (y1, x1, y2, x2) in enumerate([(active_areas[(i*4)+0]+1,
                                                      active_areas[(i*4)+1]+1,
@@ -56,4 +78,4 @@ if (__name__ == "__main__") :
     ax.set_ylim((0, 2000))
     ax.set_ylim(ax.get_ylim()[::-1])
 
-    plt.show()
+  plt.show()
