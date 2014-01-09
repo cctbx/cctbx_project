85ea42c80f1b8da8bbecf35455fd202ec9a45df2
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-01-09 17:42:14 +0000

    Readded NullSweep since some bits are hacked to ignore file input.

--- dxtbx/serialize/imageset.py
+++ dxtbx/serialize/imageset.py
@@ -10,6 +10,54 @@
 #  included in the root directory of this package.
 from __future__ import division
 
+class NullSweep(object):
+  ''' A class to facilitate reading in without access to images. '''
+  template = None
+  beam = None
+  detector = None
+  goniometer = None
+  scan = None
+  def __init__(self, template):
+    self.template = template
+  def get_template(self):
+    return self.template
+  def get_beam(self):
+    return self.beam
+  def set_beam(self, beam):
+    self.beam = beam
+  def get_detector(self):
+    return self.detector
+  def set_detector(self, detector):
+    self.detector = detector
+  def get_goniometer(self):
+    return self.goniometer
+  def set_goniometer(self, goniometer):
+    self.goniometer = goniometer
+  def get_scan(self):
+    return self.scan
+  def set_scan(self, scan):
+    self.scan = scan
+  def get_array_range(self):
+    assert self.scan is not None
+    return self.scan.get_array_range()
+  def __getitem__(self, item):
+    import dxtbx.model.scan
+    assert isinstance(item, slice)
+    sweep = NullSweep(self.template)
+    #scan = dxtbx.model.scan.Scan(self.get_scan())
+    scan = dxtbx.model.scan.Scan(
+        self.scan.get_image_range(),
+        self.scan.get_oscillation(),
+        self.scan.get_exposure_times(),
+        self.scan.get_epochs().deep_copy())
+    scan.set_image_range((item.start+1, item.stop))
+    sweep.set_scan(scan)
+    sweep.set_detector(self.get_detector())
+    sweep.set_goniometer(self.get_goniometer())
+    sweep.set_beam(self.get_beam())
+    return sweep
+
+
 def filename_to_absolute(filename):
   ''' Convert filenames to absolute form. '''
   from os.path import abspath
@@ -77,6 +125,8 @@ def imageset_to_dict(imageset):
     return imagesweep_to_dict(imageset)
   elif isinstance(imageset, ImageSet):
     return basic_imageset_to_dict(imageset)
+  elif isinstance(imageset, NullSweep):
+    return imagesweep_to_dict(imageset)
   else:
     raise TypeError("Unknown ImageSet Type")
 
@@ -118,7 +168,10 @@ def imagesweep_from_dict(d):
     image_range = scan_dict.get('image_range')
 
   # Construct the sweep
-  sweep = ImageSetFactory.from_template(template, image_range)[0]
+  try:
+    sweep = ImageSetFactory.from_template(template, image_range)[0]
+  except Exception:
+    sweep = NullSweep(template)
 
   # Get the existing models as dictionaries
   beam_dict = beam.to_dict(sweep.get_beam())
