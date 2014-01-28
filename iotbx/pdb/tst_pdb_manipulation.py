8338102acc314e632a3b453fe6be33856f4c9555
ydr <y0udar@users.noreply.github.com>
2014-01-28 06:18:26 +0000

    Reorgenize test Add missing cases Use temp folders for temp files

--- iotbx/pdb/tst_pdb_manipulation.py
+++ iotbx/pdb/tst_pdb_manipulation.py
@@ -2,17 +2,88 @@ from __future__ import division
 from iotbx import pdb
 from libtbx.test_utils import approx_equal
 from  iotbx.pdb.multimer_reconstruction import multimer
-import os
+import sys,os
+import unittest
+import shutil
+import tempfile
 
-def exercise_multimer_reconstruction():
-  '''
-  Test multimer reconstruction
 
-  - create a file, 'multimer_test_data.pdb', using the pdb_test_data
-  >>> v = multimer('multimer_test_data.pdb')
-  >>> v.assembled_multimer
-  '''
-  pdb_test_data = '''\
+class TestMultimerReconstruction(unittest.TestCase):
+  '''Test multimer reconstruction from BIOMT and MTRIX records'''
+
+
+  def setUp(self):
+    '''
+    Create temporary file: multimer_test_data.pdb
+    '''
+    self.currnet_dir = os.getcwd()
+    self.tempdir = tempfile.mkdtemp('tempdir')
+    os.chdir(self.tempdir)
+    # Write the test data into a file
+    open('multimer_test_data.pdb', 'w').write(pdb_test_data)
+
+  def test_MTRIX(self):
+    '''Test MTRIX record processing'''
+
+    cau_expected_results  = [
+    [1.0, 1.0, 1.0], [1.0, -1.0, 1.0], [-0.366025, 1.366025, 1.0], [-1.366025, 0.366025, 1.0],
+    [1.0, 1.5, 1.0], [94.618, -5.253, 91.582],
+    [94.618, -91.582, -5.253], [51.858229, 79.315053, 91.582], [-42.759771, 84.568053, 91.582],
+    [94.618, -4.753, 91.582], [62.395, 51.344, 80.786],
+    [62.395, -80.786, 51.344], [-13.267688, 79.70763, 80.786], [-75.662688, 28.36363, 80.786],
+    [62.395, 51.844, 80.786], [39.954, 51.526, 72.372],
+    [39.954, -72.372, 51.526], [-24.645804, 60.364163, 72.372], [-64.599804, 8.838163, 72.372],
+    [39.954, 52.026, 72.372]]
+
+    # use MTRIX data
+    cau_multimer_data = multimer('multimer_test_data.pdb','cau')
+    cau_multimer_xyz = cau_multimer_data.get_xyz()
+
+    cau_multimer_xyz.sort()
+    cau_expected_results.sort()
+    assert approx_equal(cau_expected_results,cau_multimer_xyz,eps=0.001)
+
+    # Test that the number of MTRIX record to process is correct
+    self.assertEqual(cau_multimer_data.number_of_transforms,4)
+
+  def test_BIOMT(self):
+    '''Test MTRIX record processing'''
+
+    ba_expected_results  = [
+    [1.0, 1.0, 1.0], [1.0, -1.0, 1.0], [1.0, 1.0, -1.0], [-1.0, 1.0, 1.0], [1.0, 1.0, -1.0],
+    [-1.0, 1.0, -1.0], [-0.366025, 1.366025, 1.0], [-1.366025, 0.366025, 1.0], [1.0, 1.5, 1.0],
+    [-1.366025, 0.366025, 0.0], [94.618, -5.253, 91.582], [94.618, -91.582, -5.253],
+    [91.582, -5.253, -94.618], [5.253, 94.618, 91.582], [91.582, -5.253, -94.618],
+    [5.253, 91.582, -94.618], [51.858229, 79.315053, 91.582], [-42.759771, 84.568053, 91.582],
+    [94.618, -4.753, 91.582], [-42.759771, 84.568053, 90.582], [62.395, 51.344, 80.786],
+    [62.395, -80.786, 51.344], [80.786, 51.344, -62.395], [-51.344, 62.395, 80.786],
+    [80.786, 51.344, -62.395], [-51.344, 80.786, -62.395], [-13.267688, 79.70763, 80.786],
+    [-75.662688, 28.36363, 80.786], [62.395, 51.844, 80.786], [-75.662688, 28.36363, 79.786],
+    [39.954, 51.526, 72.372], [39.954, -72.372, 51.526], [72.372, 51.526, -39.954],
+    [-51.526, 39.954, 72.372], [72.372, 51.526, -39.954], [-51.526, 72.372, -39.954],
+    [-24.645804, 60.364163, 72.372], [-64.599804, 8.838163, 72.372], [39.954, 52.026, 72.372],
+    [-64.599804, 8.838163, 71.372]]
+
+    # use BIOMT data
+    ba_multimer_data = multimer(
+      pdb_input_file_name='multimer_test_data.pdb',
+      reconstruction_type='ba')
+    ba_multimer_xyz = ba_multimer_data.get_xyz()
+
+    # The multimer processing is my chain, so we need to sort lists before we compare them
+    ba_multimer_xyz.sort()
+    ba_expected_results.sort()
+
+    assert approx_equal(ba_expected_results,ba_multimer_xyz,eps=0.001)
+    self.assertEqual(ba_multimer_data.number_of_transforms,9)
+
+  def tearDown(self):
+    '''remove temp files and folder'''
+    os.chdir(self.currnet_dir)
+    shutil.rmtree(self.tempdir)
+
+
+pdb_test_data = '''\
 REMARK   0 Test molecule with BIOMOLECULE: 1
 REMARK   0
 REMARK   0 The test will generate the biomolecule (the multimer assembly)
@@ -52,6 +123,9 @@ REMARK 350   BIOMT3  10  0.000000  0.000000  1.000000       -1.000000
 MTRIX1   1  1.000000  0.000000  0.000000        0.00000    1
 MTRIX2   1  0.000000  1.000000  0.000000        0.00000    1
 MTRIX3   1  0.000000  0.000000  1.000000        0.00000    1
+MTRIX1   1  1.000000  0.000000  0.000000        0.00000    1
+MTRIX2   1  0.000000  1.000000  0.000000        0.00000    1
+MTRIX3   1  0.000000  0.000000  1.000000        0.00000    1
 MTRIX1   2  1.000000  0.000000  0.000000        0.00000
 MTRIX2   2  0.000000  0.000000 -1.000000        0.00000
 MTRIX3   2  0.000000  1.000000  0.000000        0.00000
@@ -64,57 +138,14 @@ MTRIX3   4  0.000000  0.000000  1.000000        0.00000
 MTRIX1   5  1.000000  0.000000  0.000000        0.00000
 MTRIX2   5  0.000000  1.000000  0.000000        0.50000
 MTRIX3   5  0.000000  0.000000  1.000000        0.00000
-MTRIX1   6 -0.500000 -0.866025  0.000000        0.00000
-MTRIX2   6  0.866025 -0.500000  0.000000        0.00000
-MTRIX3   6  0.000000  0.000000  1.000000       -1.00000
+MTRIX1   6 -0.500000 -0.866025  0.000000        0.00000    1
+MTRIX2   6  0.866025 -0.500000  0.000000        0.00000    1
+MTRIX3   6  0.000000  0.000000  1.000000       -1.00000    1
 ATOM      1   N  ILE A  40       1.000   1.000   1.000  1.00162.33           C
 ATOM      2  CA  LEU A  40      94.618  -5.253  91.582  1.00 87.10           C
 ATOM      3   C  ARG B  40      62.395  51.344  80.786  1.00107.25           C
 HETATM    4  C1  EDO A  40      39.954  51.526  72.372  0.33 60.93           C
 '''
-  ba_expected_results  = [
-    [1.0, 1.0, 1.0], [1.0, -1.0, 1.0], [1.0, 1.0, -1.0], [-1.0, 1.0, 1.0], [1.0, 1.0, -1.0],
-    [-1.0, 1.0, -1.0], [-0.366025, 1.366025, 1.0], [-1.366025, 0.366025, 1.0], [1.0, 1.5, 1.0],
-    [-1.366025, 0.366025, 0.0], [94.618, -5.253, 91.582], [94.618, -91.582, -5.253],
-    [91.582, -5.253, -94.618], [5.253, 94.618, 91.582], [91.582, -5.253, -94.618],
-    [5.253, 91.582, -94.618], [51.858229, 79.315053, 91.582], [-42.759771, 84.568053, 91.582],
-    [94.618, -4.753, 91.582], [-42.759771, 84.568053, 90.582], [62.395, 51.344, 80.786],
-    [62.395, -80.786, 51.344], [80.786, 51.344, -62.395], [-51.344, 62.395, 80.786],
-    [80.786, 51.344, -62.395], [-51.344, 80.786, -62.395], [-13.267688, 79.70763, 80.786],
-    [-75.662688, 28.36363, 80.786], [62.395, 51.844, 80.786], [-75.662688, 28.36363, 79.786],
-    [39.954, 51.526, 72.372], [39.954, -72.372, 51.526], [72.372, 51.526, -39.954],
-    [-51.526, 39.954, 72.372], [72.372, 51.526, -39.954], [-51.526, 72.372, -39.954],
-    [-24.645804, 60.364163, 72.372], [-64.599804, 8.838163, 72.372], [39.954, 52.026, 72.372],
-    [-64.599804, 8.838163, 71.372]]
-  cau_expected_results  = [
-    [1.0, 1.0, 1.0], [1.0, -1.0, 1.0], [-0.366025, 1.366025, 1.0], [-1.366025, 0.366025, 1.0],
-    [1.0, 1.5, 1.0], [-1.366025, 0.366025, 0.0], [94.618, -5.253, 91.582],
-    [94.618, -91.582, -5.253], [51.858229, 79.315053, 91.582], [-42.759771, 84.568053, 91.582],
-    [94.618, -4.753, 91.582], [-42.759771, 84.568053, 90.582], [62.395, 51.344, 80.786],
-    [62.395, -80.786, 51.344], [-13.267688, 79.70763, 80.786], [-75.662688, 28.36363, 80.786],
-    [62.395, 51.844, 80.786], [-75.662688, 28.36363, 79.786], [39.954, 51.526, 72.372],
-    [39.954, -72.372, 51.526], [-24.645804, 60.364163, 72.372], [-64.599804, 8.838163, 72.372],
-    [39.954, 52.026, 72.372], [-64.599804, 8.838163, 71.372]]
-  # Write the test data into a file
-  f = open('multimer_test_data.pdb', 'w')
-  f.write(pdb_test_data)
-  f.close()
-  # use BIOMT data
-  ba_multimer_data = multimer('multimer_test_data.pdb','ba')
-  ba_multimer_xyz = ba_multimer_data.get_xyz()
-  # use MTRIX data
-  cau_multimer_data = multimer('multimer_test_data.pdb','cau')
-  cau_multimer_xyz = cau_multimer_data.get_xyz()
-  # The multimer processing is my chain, so we need to sort lists before we compare them
-  ba_multimer_xyz.sort()
-  ba_expected_results.sort()
-  cau_multimer_xyz.sort()
-  cau_expected_results.sort()
-  assert approx_equal(ba_expected_results,ba_multimer_xyz,eps=0.001)
-  assert approx_equal(cau_expected_results,cau_multimer_xyz,eps=0.001)
 
-def run():
-  exercise_multimer_reconstruction()
-
-if (__name__ == "__main__"):
-  run()
+if __name__ == "__main__":
+  unittest.main()
