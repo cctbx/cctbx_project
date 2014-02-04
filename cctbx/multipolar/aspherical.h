dfd473bbccf8c89fbc2ac1ef9ca3a80862c35bca
Nigel W Moriarty <nwmoriarty@users.noreply.github.com>
2014-02-04 21:30:15 +0000

    fixed SConsript, clean_clutter

--- cctbx/multipolar/aspherical.h
+++ cctbx/multipolar/aspherical.h
@@ -4,16 +4,21 @@
 
 #include <complex>
 
-class Aspherical{
-public:
-	Aspherical();
-	
-	void assign_atom_types(std::vector<size_t> &atomic_numbers, std::vector<double> &xyz, std::vector<std::string> &bank_atom_types,
-		                   std::vector<std::string> &local_coordinates);
-	void set_atom_types(std::vector<std::string> &atom_types, std::vector<std::string> &local_coordinates);
-	void calculate_f(std::vector<double> &xyz, std::vector<double> &occupancies, std::vector<double> &adps,
-		std::vector<int> &miller_indices, std::vector<std::complex<double> > &sf);
-};
+namespace cctbx {
+  class Aspherical{
+  public:
+    Aspherical();
 
-
-
+    void assign_atom_types(std::vector<size_t> &atomic_numbers,
+                           std::vector<double> &xyz,
+                           std::vector<std::string> &bank_atom_types,
+                           std::vector<std::string> &local_coordinates);
+    void set_atom_types(std::vector<std::string> &atom_types,
+                        std::vector<std::string> &local_coordinates);
+    void calculate_f(std::vector<double> &xyz,
+                     std::vector<double> &occupancies,
+                     std::vector<double> &adps,
+                     std::vector<int> &miller_indices,
+                     std::vector<std::complex<double> > &sf);
+  }; //class Aspherical
+} //namespace cctbx
