4b44f9b327a2e6a51e764c7cfc7780ecaead5d7d
Pavel Afonine <pafonine@users.noreply.github.com>
2014-02-10 22:08:14 +0000

    phenix.f000: tool to compute F000 given PDB model and mean solvent density (default=0.35)

---
+++ mmtbx/command_line/f000.py
@@ -0,0 +1,45 @@
+from __future__ import division
+# LIBTBX_SET_DISPATCHER_NAME phenix.f000
+
+import sys
+import iotbx.pdb
+from libtbx.utils import Sorry
+import mmtbx.utils
+
+legend = """phenix.f000: Given PDB file estimate F(0,0,0)
+
+How to run:
+  phenix.f000 model.pdb
+  phenix.f000 model.pdb mean_solvent_density=0.44
+"""
+
+master_params_str = """
+mean_solvent_density=0.35
+  .type=float
+"""
+
+def master_params():
+  return iotbx.phil.parse(master_params_str)
+
+def run(args, log=sys.stdout):
+  print >> log, "-"*79
+  print >> log, legend
+  print >> log, "-"*79
+  inputs = mmtbx.utils.process_command_line_args(args = args, 
+    master_params = master_params())
+  file_names = inputs.pdb_file_names
+  if(len(file_names) != 1): raise Sorry("One PDB file is expected.")
+  xrs = iotbx.pdb.input(file_name = file_names[0]).xray_structure_simple()
+  msd = inputs.params.extract().mean_solvent_density
+  f_000 = mmtbx.utils.f_000(xray_structure=xrs, 
+    mean_solvent_density=msd)
+  f_000_str = str("%-13.3f"%f_000.f_000).strip()
+  msd_str = str("%-6.3f"%msd).strip()
+  sf_str = str("%-6.3f"%f_000.solvent_fraction).strip()
+  msg = """
+Estimate of F(0,0,0)=%s given mean bulk-solvent density %s and fraction %s
+"""
+  print >> log, msg%(f_000_str, msd, sf_str)
+
+if (__name__ == "__main__"):
+  run(args=sys.argv[1:])
