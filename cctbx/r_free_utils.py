f00dbb25926bdb2be6ceb066a67fde7818409c99
Nathaniel Echols <nathaniel.echols@gmail.com>
2014-02-11 21:10:10 +0000

    function to fix R-free flag mismatches

--- cctbx/r_free_utils.py
+++ cctbx/r_free_utils.py
@@ -311,63 +311,24 @@ def get_r_free_as_bool (miller_array, test_flag_value=0) :
     assert isinstance(new_data, flex.bool)
     return miller_array.customized_copy(data=new_data, sigmas=None)
 
-def exercise () :
-  from cctbx import miller
-  from cctbx import crystal
-  from cctbx import sgtbx
-  from cctbx import uctbx
-  from scitbx.array_family import flex
-  from libtbx.test_utils import approx_equal
-  flags_1 = assign_random_r_free_flags(n_refl=100000, fraction_free=0.05)
-  assert (flags_1.count(True) == 5000)
-  flags_1_ccp4 = assign_random_r_free_flags(n_refl=100000, fraction_free=0.05,
-    format="ccp4")
-  # XXX this is the best we can do with the current method
-  assert (flags_1_ccp4.count(0) > 4000) and (flags_1_ccp4.count(0) < 6000)
-  flags_1_shelx = assign_random_r_free_flags(n_refl=100000, fraction_free=0.05,
-    format="shelx")
-  assert (flags_1_shelx.count(-1) == 5000)
-  flags_2 = assign_r_free_flags_by_shells(n_refl=100000,
-    fraction_free=0.05,
-    n_bins=50)
-  assert (flags_2.count(True) == 5000)
-  ccp4_flags = export_r_free_flags_for_ccp4(flags_1, True)
-  assert (ccp4_flags.count(0) == flags_1.count(True))
-  assert (flex.max(ccp4_flags) == 19)
-  shelx_flags = export_r_free_flags_for_shelx(flags_1, True)
-  assert ((shelx_flags==-1).all_eq((ccp4_flags==0)))
-  flags_3 = assign_random_r_free_flags(n_refl=100000, fraction_free=0.025)
-  assert (flags_3.count(True) == 2500)
-  ccp4_flags = export_r_free_flags_for_ccp4(flags_3, True)
-  assert (ccp4_flags.count(0) == flags_3.count(True))
-  assert (flex.max(ccp4_flags) == 39)
-  # now with an actual Miller array
-  symm = crystal.symmetry(
-    space_group_info=sgtbx.space_group_info("P212121"),
-    unit_cell=uctbx.unit_cell((6,7,8,90,90,90)))
-  set1 = miller.build_set(
-    crystal_symmetry=symm,
-    anomalous_flag=True,
-    d_min=1.0)
-  flags_4 = set1.generate_r_free_flags()
-  stats = get_r_free_stats(flags_4, True)
-  assert (19 <= stats[0] <= 25) # XXX is this even necessary?
-  # much larger for the last few tests
-  symm = crystal.symmetry(
-    space_group_info=sgtbx.space_group_info("P212121"),
-    unit_cell=uctbx.unit_cell((60,70,80,90,90,90)))
-  set1 = miller.build_set(
-    crystal_symmetry=symm,
-    anomalous_flag=True,
-    d_min=1.0)
-  flags_5 = set1.generate_r_free_flags(fraction=0.1, max_free=None)
-  flags_6 = adjust_fraction(flags_5, 0.15, log=null_out())
-  frac_6 = flags_6.data().count(True) / flags_6.data().size()
-  assert approx_equal(frac_6, 0.15, eps=0.001)
-  flags_7 = adjust_fraction(flags_5, 0.05, log=null_out())
-  frac_7 = flags_7.data().count(True) / flags_7.data().size()
-  assert approx_equal(frac_7, 0.05, eps=0.001)
-
-if (__name__ == "__main__") :
-  exercise()
-  print "OK"
+def remediate_mismatches (array, verbose=False, log=None) :
+  """
+  Given a set of R-free flags generated for anomalous data, detect any
+  mismatches between Friedel/Bijvoet mates, and move reflections to the
+  free set as needed to ensure consistency.
+  """
+  if (log is None) : log = null_out()
+  if (not array.anomalous_flag()) :
+    return array
+  assert array.is_bool_array()
+  array = array.map_to_asu()
+  orig_array = array.deep_copy()
+  array = array.as_non_anomalous_array()
+  merge = array.merge_equivalents(incompatible_flags_replacement=True)
+  if (merge.n_incompatible_flags > 0):
+    print >> log, "  %d reflections moved to test set" % \
+      merge.n_incompatible_flags
+  new_flags = merge.array().generate_bijvoet_mates().common_set(
+    other=orig_array)
+  assert len(new_flags.indices()) == len(orig_array.indices())
+  return new_flags
