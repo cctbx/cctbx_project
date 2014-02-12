a2cb2303ce0dbe771a660be4e5daf360e5868af1
Nader Morshed <nmorshed@users.noreply.github.com>
2014-02-12 00:49:44 +0000

    Fixed the wavelength for the zinc input to tst_classifier Fixed a bug where none of the waters were being examined in tst_classifier Changed the output of tst_classifier to be more clear when a prediction fails _get_classifier in mmtbx.ions.svm now returns a dict of options that should be passed to ion_vector (Controls which features to extract) _get_classifier now also returns a number for the predicted size of the array returned by ion_vector, preventing errors in classification when features are accidentally left out predict_ion now expects HOH to be explicitly mentioned if it is to be included in elements to select from

--- mmtbx/ions/svm/tst_classifier.py
+++ mmtbx/ions/svm/tst_classifier.py
@@ -14,9 +14,8 @@ from mmtbx.regression.make_fake_anomalous_data import generate_zinc_inputs, \
 import mmtbx.command_line
 
 def exercise () :
-  # XXX: Should CA's wavelength be 1.025, too?
   fns = [generate_calcium_inputs, generate_zinc_inputs]
-  wavelengths = [1.025, 1.025]
+  wavelengths = [1.025, 1.54]
 
   for fn, wavelength in zip(fns, wavelengths):
     mtz_file, pdb_file = fn(anonymize = True)
@@ -29,23 +28,14 @@ def exercise () :
       out = null_out,
       process_pdb_file = True,
       create_fmodel = True,
-      prefer_anomalous = True
+      prefer_anomalous = True,
+      set_inelastic_form_factors = "sasaki",
       )
 
     os.remove(pdb_file)
     os.remove(mtz_file)
     os.remove(os.path.splitext(mtz_file)[0] + "_fmodel.eff")
     os.remove(os.path.splitext(mtz_file)[0] + ".pdb")
-
-    cmdline.xray_structure.set_inelastic_form_factors(
-      photon = cmdline.params.wavelength,
-      table = "sasaki"
-      )
-
-    cmdline.fmodel.update_xray_structure(
-      cmdline.xray_structure,
-      update_f_calc = True
-      )
 
     manager = ions.create_manager(
       pdb_hierarchy = cmdline.pdb_hierarchy,
@@ -57,37 +47,54 @@ def exercise () :
       log = null_out
       )
 
-    manager.validate_ions(
-      out = null_out
-      )
+    # Build a list of properties of each water / ion site
+    waters = []
+    for chain in manager.pdb_hierarchy.only_model().chains():
+      for residue_group in chain.residue_groups():
+        atom_groups = residue_group.atom_groups()
+        if (len(atom_groups) > 1) : # alt conf, skip
+          continue
+        for atom_group in atom_groups :
+          # Check for non standard atoms in the residue
+          # Or a label indicating the residue is a water
+          resname = atom_group.resname.strip().upper()
+
+          if (resname in ions.WATER_RES_NAMES) :
+            atoms = atom_group.atoms()
+            if (len(atoms) == 1) : # otherwise it probably has hydrogens, skip
+              waters.append(atoms[0].i_seq)
+
+    assert len(waters) > 0
+
+    atom_props = [ions.AtomProperties(i_seq, manager) for i_seq in waters]
 
     fo_map = manager.get_map("mFo")
     fofc_map = manager.get_map("mFo-DFc")
     anom_map = manager.get_map("anom")
 
-    for atom_props in manager.atoms_to_props.values():
+    for atom_prop in atom_props:
       chem_env = ChemicalEnvironment(
-        atom_props.i_seq,
-        manager.find_nearby_atoms(atom_props.i_seq, far_distance_cutoff = 3.5),
+        atom_prop.i_seq,
+        manager.find_nearby_atoms(atom_prop.i_seq, far_distance_cutoff = 3.5),
         manager,
         )
       scatter_env = ScatteringEnvironment(
-        atom_props.i_seq, manager, fo_map, fofc_map, anom_map
+        atom_prop.i_seq, manager, fo_map, fofc_map, anom_map
         )
       resname = ion_class(chem_env)
       assert resname != ""
 
       predictions = predict_ion(chem_env, scatter_env,
-                                elements = ["ZN", "MN", "CA"])
+                                elements = ["HOH", "ZN", "CA"])
       if predictions is None:
         print "Could not load SVM classifier"
         print "Skipping {}".format(os.path.split(__file__)[1])
         return
 
-      if resname != prediction[0][0]:
-        print "Prediction did not match expected: {} != {}" \
-          .format(resname, prediction[0][0])
-        for element, prob in prediction:
+      if resname != predictions[0][0]:
+        print "Prediction ({}) did not match expected: {}" \
+          .format(predictions[0][0], resname)
+        for element, prob in predictions:
           print "  {}: {:.2f}".format(element, prob)
         sys.exit()
 
