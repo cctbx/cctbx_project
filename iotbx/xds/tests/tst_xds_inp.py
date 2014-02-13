2e1f61bce3c9379f0eef60c986f2dc4704b223e6
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-02-13 14:15:31 +0000

    xds_inp parser was broken except for very specific formatting of the input

--- iotbx/xds/tests/tst_xds_inp.py
+++ iotbx/xds/tests/tst_xds_inp.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from libtbx.test_utils import open_tmp_directory
 
 class Test(object):
 
@@ -10,47 +11,55 @@ class Test(object):
     from iotbx.xds import xds_inp
     import os
     import libtbx.load_env
-    try:
-        iotbx_dir = libtbx.env.dist_path('iotbx')
-    except KeyError, e:
-        print 'FAIL: dials_regression not configured'
-        return
+    iotbx_dir = libtbx.env.dist_path('iotbx')
 
     filename = os.path.join(iotbx_dir, 'xds', 'tests', 'XDS.INP')
     handle = xds_inp.reader()
     handle.read_file(filename)
 
-    handle.detector
-    handle.minimum_valid_pixel_value
-    handle.overload
-    handle.corrections
-    handle.direction_of_detector_x_axis
-    handle.direction_of_detector_y_axis
-    handle.trusted_region
-    handle.sensor_thickness
-    handle.untrusted_rectangle
-    handle.maximum_number_of_processor
-    handle.nx
-    handle.ny
-    handle.px
-    handle.py
-    handle.orgx
-    handle.orgy
-    handle.rotation_axis
-    handle.detector_distance
-    handle.xray_wavelength
-    handle.incident_beam_direction
-    handle.fraction_of_polarization
-    handle.polarization_plane_normal
-    handle.friedels_law
-    handle.name_template_of_data_frames
-    handle.starting_angle
-    handle.starting_frane
-    handle.include_resolution_range
-    handle.unit_cell_constants
-    handle.space_group_number
-    handle.max_fac_rmeas
-    handle.data_range
+    assert handle.detector == 'PILATUS'
+    assert handle.minimum_valid_pixel_value == 0
+    assert handle.overload == 1048500
+    assert handle.corrections == 'ALL'
+    assert handle.direction_of_detector_x_axis == [1.0, 0.0, 0.0]
+    assert handle.direction_of_detector_y_axis == [0.0, 1.0, 0.0]
+    assert handle.trusted_region == [0.0, 1.41]
+    assert handle.sensor_thickness == 0.32
+    assert handle.untrusted_rectangle == [
+      [487, 495, 0, 2528], [981, 989, 0, 2528], [1475, 1483, 0, 2528],
+      [1969, 1977, 0, 2528], [0, 2464, 195, 213], [0, 2464, 407, 425],
+      [0, 2464, 619, 637], [0, 2464, 831, 849], [0, 2464, 1043, 1061],
+      [0, 2464, 1255, 1273], [0, 2464, 1467, 1485], [0, 2464, 1679, 1697],
+      [0, 2464, 1891, 1909], [0, 2464, 2103, 2121], [0, 2464, 2315, 2333]]
+    assert handle.maximum_number_of_processor == 16
+    assert (handle.nx, handle.ny, handle.px, handle.py) == (2463, 2527, 0.172, 0.172)
+    assert (handle.orgx, handle.orgy) == (1279.1, 1235.3)
+    assert handle.rotation_axis == [1.0, 0.0, 0.0]
+    assert handle.detector_distance == 190.18
+    assert handle.xray_wavelength == 0.9795
+    assert handle.incident_beam_direction == [0.0, 0.0, 1.0]
+    assert handle.fraction_of_polarization == 0.99
+    assert handle.polarization_plane_normal == [0.0, 1.0, 0.0]
+    assert handle.friedels_law
+    assert handle.name_template_of_data_frames.endswith(
+      'X4_lots_M1S4_1_????.cbf')
+    assert handle.starting_angle == 0
+    assert handle.starting_frame == 1
+    assert handle.include_resolution_range == [30.0, 1.27]
+    assert handle.unit_cell_constants == [42.45, 42.45, 39.8, 90.0, 90.0, 90.0]
+    assert handle.space_group_number == 89
+    assert handle.max_fac_rmeas == 3.0
+    assert handle.data_range == [1]
+
+    filename_1 = os.path.join(iotbx_dir, 'xds', 'tests', 'XDS_2.INP')
+    tmp_dir = open_tmp_directory()
+    filename_2 = os.path.join(tmp_dir, 'XDS.INP')
+    with open(filename_2, 'wb') as f:
+      print >> f, open(filename_1, 'rb').read()
+    handle = xds_inp.reader()
+    handle.read_file(filename_2)
+    assert handle.corrections is None
+    assert handle.incident_beam_direction == [-0.003, 0.001, 1.032]
 
     print 'OK'
 
