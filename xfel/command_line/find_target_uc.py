aa137f7c0f5710b0f0e144628e0cd114114c8882
Oliver Zeldin <oliver.zeldin@gmail.com>
2014-02-14 21:29:27 +0000

    small updates for more options.

--- xfel/command_line/find_target_uc.py
+++ xfel/command_line/find_target_uc.py
@@ -7,19 +7,22 @@ def run(args):
   if args < 2: raise IOError("Must give at least one path to folder of pickles") 
   from cctbx.uctbx.determine_unit_cell.target_uc import target
   ucs = target(args.folders) 
-  ucs.cluster(args.t)
-  ucs.plot_clusters(ucs.clusters)
-  
+  ucs.cluster(args.t, args.m) # ^2 to convert to A^2 for G6 distance
+  ucs.plot_clusters(ucs.clusters) 
 
 if (__name__ == "__main__"):
   import sys
   import argparse
-  parser = argparse.ArgumentParser(description='Process some integers.')
+  parser = argparse.ArgumentParser(description=
+      'Find the best target cell from a collection of indexing pickles.')
   parser.add_argument('folders',  type=str, nargs='+',
                        help='One or more folers containing integration pickles.')
-  parser.add_argument('-t', type=float, default=50,
-                       help='threshold value for the clustering. Note that this is given in A, but will be squared for clustering, since the clustering occurs in G6 space. Default = 50')
+  parser.add_argument('-t', type=float, default=5000, 
+                       help='threshold value for the clustering. Default = 50')
+  parser.add_argument('-m', type=str, default='distance',
+      help='Clustering method for numpy clustering. Options are: inconsistent' + 
+           ', distance, maxclust, monocri, maxclust_monocrit')
 
   args = parser.parse_args()
   result = run(args)
-
+ 
