0111ae9e0ea1460cee11d7ea3d8414a910d96954
Pavel Afonine <afonine@users.noreply.github.com>
2014-02-20 16:20:16 +0000

    Deal with the fact that SHELXL Fourier sinthesis weighting scheme downweight unmodeled density; use improved filling missing reflections; more efficient (runtime and memory) solvent region filtering

--- mmtbx/maps/fem.py
+++ mmtbx/maps/fem.py
@@ -20,8 +20,13 @@ def get_map(mc, cg=None):
 
 def compute_map_and_combine(
       map_coeffs,
-      map_data):
+      map_data,
+      map_base):
   m = get_map(mc=map_coeffs)
+  #
+  m = maptbx.combine_and_maximize_maps(
+    map_data_1 = m, map_data_2 = map_base, n_real = map_base.all())
+  #
   maptbx.reset(
       data=m,
       substitute_value=0.0,
@@ -52,7 +57,7 @@ def intersection(m1,m2,thresholds=flex.double([i/10. for i in xrange(9)]+[1.]),
 
 class run(object):
 
-  def __init__(self, fmodel, use_omit=True):
+  def __init__(self, fmodel, use_omit=False, use_resolve=False):
     # make sure common B moved to k_total
     b_iso_min = flex.min(
       fmodel.xray_structure.extract_u_iso_or_u_equiv()*adptbx.u_as_b(1))
@@ -84,27 +89,26 @@ class run(object):
       unit_cell             = fmodel.f_obs().unit_cell(),
       space_group_info      = fmodel.f_obs().space_group_info(),
       pre_determined_n_real = bs_mask.all())
-    # Resolve DM map
-    self.mc_resolve = mmtbx.map_tools.resolve_dm_map(
-      fmodel       = fmodel,
-      map_coeffs   = self.mc_all_scales,
-      pdb_inp      = None,
-      use_model_hl = True,
-      fill         = True)
+    # Fill missing
+    missing_reflections_manager=None
+    if(use_resolve):
+      self.mc_filled = mmtbx.map_tools.fill_missing_f_obs_3(
+        coeffs=self.mc_all_scales, fmodel=fmodel)
+    else:
+      missing_reflections_manager = mmtbx.map_tools.model_missing_reflections(
+        coeffs=self.mc_all_scales, fmodel=fmodel)
+      missing = missing_reflections_manager.get_missing(deterministic=True)
+      self.mc_filled = self.mc_all_scales.complete_with(other=missing, scale=True)
+    #
+    map_base = get_map(mc=self.mc.complete_with(other=missing, scale=True),
+      cg=crystal_gridding)
     #XXX assert 100% complete XXX
-    mc_missing = self.mc_resolve.lone_set(self.mc)
-    m_resolve = get_map(self.mc_resolve, crystal_gridding)
-    # Resolve filter mask
-    m_resolve = m_resolve.set_selected(m_resolve< 0.25, 0)
-    m_resolve = m_resolve.set_selected(m_resolve>=0.25, 1)
-    s1 = bs_mask == 1
-    s0 = bs_mask == 0
-    assert s1.count(True)+s0.count(True) == bs_mask.size()
-    bs_mask = bs_mask.set_selected(s1, 0)
-    bs_mask = bs_mask.set_selected(s0, 1)
-    m_resolve = m_resolve + bs_mask
-    s2 = m_resolve > 1
-    m_resolve = m_resolve.set_selected(s2, 1)
+    m_resolve = map_base#get_map(self.mc_filled, crystal_gridding)
+    # Filter mask
+    m_resolve = maptbx.conditional_solvent_region_filter(
+      bulk_solvent_mask = bs_mask,
+      map_data          = m_resolve,
+      threshold         = 0.5) # XXX 0.25 ?
     # composite OMIT map
     m_omit = None
     if(use_omit):
@@ -112,10 +116,10 @@ class run(object):
         map_type                  = "mFo-DFc",
         crystal_gridding          = crystal_gridding,
         fmodel                    = fmodel.deep_copy(), # XXX
-        reset_to_zero_below_sigma = 0,
+        reset_to_zero_below_sigma = None,
         use_shelx_weight          = True)
       mc_omit = como.map_coefficients
-      mc_omit = mc_omit.complete_with(other=self.mc_resolve, scale=True)
+      mc_omit = mc_omit.complete_with(other=self.mc_filled, scale=True)
       m_omit = get_map(mc_omit, crystal_gridding)
       maptbx.reset(
         data=m_omit,
@@ -129,8 +133,14 @@ class run(object):
     # FEM loop 3
     self.map_result = None
     for i in [1,2,3]:
-      mc_w = loop_1_2(fmodel=fmodel, mc=self.mc, mc_fill=self.mc_resolve,
-        he_cycles=5, random_weigh_cycles=5)
+      mc_w = loop_1_2(
+        fmodel  = fmodel,
+        map_base=map_base,
+        mc      = self.mc,
+        mc_fill = self.mc_filled,
+        missing_reflections_manager = missing_reflections_manager,
+        he_cycles=5,
+        random_weigh_cycles=5)
       m=get_map(mc_w, crystal_gridding)
 
 #       ################
@@ -156,13 +166,14 @@ class run(object):
       m = m*m_resolve
       if(m_omit is not None): m = m*m_omit
       self.map_result = intersection(m1 = self.map_result, m2 = m)
-    self.mc_result = self.mc_resolve.structure_factors_from_map(
+    self.mc_result = self.mc_filled.structure_factors_from_map(
       map            = self.map_result,
       use_scale      = True,
       anomalous_flag = False,
       use_sg         = False)
 
-def loop_1_2(fmodel, mc, mc_fill, he_cycles=10, random_weigh_cycles=10):
+def loop_1_2(fmodel, map_base, mc, mc_fill=None, missing_reflections_manager=None,
+             he_cycles=10, random_weigh_cycles=10):
   mr = None
   for i in xrange(he_cycles):
     print i
@@ -175,11 +186,14 @@ def loop_1_2(fmodel, mc, mc_fill, he_cycles=10, random_weigh_cycles=10):
         weight_parameter = None)
       data = mc.data()*weight
       mc_w = mc.customized_copy(data = data)
-      if(mc_fill is not None):
+      if(missing_reflections_manager is not None):
+        mc_w = mc_w.complete_with(
+          other=missing_reflections_manager.get_missing(), scale=True)
+      else:
         mc_w = mc_w.complete_with(other=mc_fill, scale=True)
       if(random.choice([True, False])):
         mc_w = mc_w.select(flex.random_bool(mc_w.size(), 0.95))
-      m = compute_map_and_combine(map_coeffs=mc_w, map_data=m)
+      m = compute_map_and_combine(map_coeffs=mc_w, map_data=m, map_base=map_base)
     maptbx.reset(
       data=m,
       substitute_value=0.0,
