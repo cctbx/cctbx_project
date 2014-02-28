9d3dbd606db929a93ab8d042e09c0abb1852259c
Aaron S. Brewster <asbrewster@lbl.gov>
2014-02-28 01:57:32 +0000

    Refactor detector_format_version to use a dictionary to look up a version string based on a detector address and date.
    Use this dictionary to provide the reverse functionality (given
    a format version string, return a compatible address and data)
    
    Needed for making gain masks.

--- xfel/detector_formats.py
+++ xfel/detector_formats.py
@@ -16,12 +16,114 @@ def reverse_timestamp(timestamp):
   gmtime_tuple = time.strptime(tokens[0], '%Y-%m-%dT%H:%MZ%S')
   return (time.mktime(gmtime_tuple), float(tokens[1]))
 
+def _get_detector_format_version_dict():
+  from calendar import timegm
+  from time import strptime
+
+  # Note: one must take daylight savings into account when defining
+  # the cutoff-times for the LCLS runs.  The last shift of a run
+  # generally ends at 09:00 local time the day after the last day of
+  # the run.
+  f = '%Y-%m-%d, %H:%M %Z'
+
+  # We have no metrology for the first two LCLS runs
+  # LCLS started operation Oct 1, 2009
+  #timegm(strptime('2009-10-01, 02:00 UTC', f))
+  # LCLS run 1: until Dec 17, 2009
+  #timegm(strptime('2009-12-18, 01:00 UTC', f)):
+  # LCLS run 2: until Sep 15, 2010
+  #timegm(strptime('2010-09-16, 02:00 UTC', f)):
+
+  return {
+    'Sacla.MPCCD': {
+      'address':'Sacla.MPCCD',
+      'start_time':None,
+      'end_time':None
+    },
+    # LCLS run 3: until Mar 8, 2011
+    #
+    # 'CXI 3.1' corresponds to a quirky, old, and deprecated version
+    # of the cctbx.xfel pickle format.
+    'CXI 3.2': {
+      'address':'CxiDs1-0|Cspad-0',
+      'start_time':timegm(strptime('2010-09-16, 02:00 UTC', f)),
+      'end_time'  :timegm(strptime('2011-03-09, 02:00 UTC', f))
+    },
+    # LCLS run 4: until Oct 28, 2011
+    'CXI 4.1': {
+      'address':'CxiDs1-0|Cspad-0',
+      'start_time':timegm(strptime('2011-03-09, 02:00 UTC', f)),
+      'end_time'  :timegm(strptime('2011-10-29, 02:00 UTC', f))
+    },
+    # LCLS run 5: until May 31, 2012
+    'CXI 5.1': {
+      'address':'CxiDs1-0|Cspad-0',
+      'start_time':timegm(strptime('2011-10-29, 02:00 UTC', f)),
+      'end_time'  :timegm(strptime('2012-05-31, 02:00 UTC', f))
+    },
+    # LCLS run 6: until Dec 31, 2012
+    'CXI 6.1': {
+      'address':'CxiDs1-0|Cspad-0',
+      'start_time':timegm(strptime('2012-05-31, 02:00 UTC', f)),
+      'end_time'  :timegm(strptime('2013-01-01, 01:00 UTC', f))
+    },
+    # LCLS run 7: until Jul 31, 2013
+    'CXI 7.1': {
+      'address':'CxiDs1-0|Cspad-0',
+      'start_time':timegm(strptime('2013-01-01, 01:00 UTC', f)),
+      'end_time'  :timegm(strptime('2013-08-01, 02:00 UTC', f))
+    },
+    'CXI 7.d': {
+      'address':'CxiDsd-0|Cspad-0',
+      'start_time':timegm(strptime('2013-01-01, 01:00 UTC', f)),
+      'end_time'  :timegm(strptime('2013-08-01, 02:00 UTC', f))
+    },
+    'XPP 7.1': {
+      'address':'XppGon-0|Cspad-0',
+      'start_time':timegm(strptime('2013-01-01, 01:00 UTC', f)),
+      'end_time'  :timegm(strptime('2013-08-01, 02:00 UTC', f))
+    },
+    'XPP 7.marccd': {
+      'address':'XppGon-0|marccd-0',
+      'start_time':timegm(strptime('2013-01-01, 01:00 UTC', f)),
+      'end_time'  :timegm(strptime('2013-08-01, 02:00 UTC', f))
+    },
+    # LCLS run 8: until Mar 31, 2014
+    # CXI detector rebuilt in Jan 2014, before that use CXI 8.1 metrology.
+    'CXI 8.1': {
+      'address':'CxiDs1-0|Cspad-0',
+      'start_time':timegm(strptime('2013-08-01, 02:00 UTC', f)),
+      'end_time'  :timegm(strptime('2014-01-15, 02:00 UTC', f))
+    },
+    'CXI 8.2': {
+      'address':'CxiDs1-0|Cspad-0',
+      'start_time':timegm(strptime('2014-01-15, 02:00 UTC', f)),
+      'end_time'  :timegm(strptime('2014-03-31, 02:00 UTC', f))
+    },
+    'CXI 8.d': {
+      'address':'CxiDsd-0|Cspad-0',
+      'start_time':timegm(strptime('2013-08-01, 02:00 UTC', f)),
+      'end_time'  :timegm(strptime('2014-03-31, 02:00 UTC', f))
+    },
+    'XPP 8.1': {
+      'address':'XppGon-0|Cspad-0',
+      'start_time':timegm(strptime('2013-08-01, 02:00 UTC', f)),
+      'end_time'  :timegm(strptime('2014-03-31, 02:00 UTC', f))
+    },
+    'XPP 8.marccd': {
+      'address':'XppGon-0|marccd-0',
+      'start_time':timegm(strptime('2013-08-01, 02:00 UTC', f)),
+      'end_time'  :timegm(strptime('2014-03-31, 02:00 UTC', f))
+    }
+  }
+_detector_format_version_dict = _get_detector_format_version_dict()
+
 def detector_format_version(address, time):
   """The detector_format_version() function returns a format version
   string appropriate for the detector whose address is given by @p
   address at the time @p time.
 
-  @param address Address string XXX Que?!
+  @param address Full data source address of the DAQ device
   @param time    Time of the event, in number of seconds since
                  midnight, 1 January 1970 UTC (Unix time)
   @return        Format version string
@@ -30,78 +132,29 @@ def detector_format_version(address, time):
   from calendar import timegm
   from time import strptime
 
-  if address is None or time is None:
+  if address is None:
+    return None # time can be None if any time is valid for the format version
+
+  ret = None
+  for format_name, format in _detector_format_version_dict.iteritems():
+    if address == format['address'] and \
+       (format['start_time'] is None or time >  format['start_time']) and \
+       (format['end_time']   is None or time <= format['end_time']):
+      assert ret is None
+      ret = format_name
+
+  return ret
+
+def address_and_timestamp_from_detector_format_version(format_name):
+  """Reverse of detector_format_version()
+
+  @param format_name detector format version requested
+  @return tuple of address and end_time of this format version
+  """
+
+  if _detector_format_version_dict.has_key(format_name):
+    return _detector_format_version_dict[format_name]['address'], \
+           _detector_format_version_dict[format_name]['end_time']
+  else:
     return None
 
-  if address == 'Sacla.MPCCD':
-    return 'Sacla.MPCCD'
-
-  # Note: one must take daylight savings into account when defining
-  # the cutoff-times for the LCLS runs.  The last shift of a run
-  # generally ends at 09:00 local time the day after the last day of
-  # the run.
-  f = '%Y-%m-%d, %H:%M %Z'
-  if time < timegm(strptime('2009-10-01, 02:00 UTC', f)):
-    # LCLS started operation Oct 1, 2009
-    return None
-
-  elif time < timegm(strptime('2009-12-18, 01:00 UTC', f)):
-    # LCLS run 1: until Dec 17, 2009
-    return None
-
-  elif time < timegm(strptime('2010-09-16, 02:00 UTC', f)):
-    # LCLS run 2: until Sep 15, 2010
-    return None
-
-  elif time < timegm(strptime('2011-03-09, 02:00 UTC', f)):
-    # LCLS run 3: until Mar 8, 2011
-    #
-    # 'CXI 3.1' corresponds to a quirky, old, and deprecated version
-    # of the cctbx.xfel pickle format.
-    if address == 'CxiDs1-0|Cspad-0':
-      return 'CXI 3.2'
-
-  elif time < timegm(strptime('2011-10-29, 02:00 UTC', f)):
-    # LCLS run 4: until Oct 28, 2011
-    if address == 'CxiDs1-0|Cspad-0':
-      return 'CXI 4.1'
-
-  elif time < timegm(strptime('2012-05-31, 02:00 UTC', f)):
-    # LCLS run 5: until May 31, 2012
-    if address == 'CxiDs1-0|Cspad-0':
-      return 'CXI 5.1'
-
-  elif time < timegm(strptime('2013-01-01, 01:00 UTC', f)):
-    # LCLS run 6: until Dec 31, 2012
-    if address == 'CxiDs1-0|Cspad-0':
-      return 'CXI 6.1'
-
-  elif time < timegm(strptime('2013-08-01, 02:00 UTC', f)):
-    # LCLS run 7: until Jul 31, 2013
-    if address == 'CxiDs1-0|Cspad-0':
-      return 'CXI 7.1'
-    elif address == 'CxiDsd-0|Cspad-0':
-      return 'CXI 7.d'
-    elif address == 'XppGon-0|Cspad-0':
-      return 'XPP 7.1'
-    elif address == 'XppGon-0|marccd-0':
-      return 'XPP 7.marccd'
-
-  elif time < timegm(strptime('2014-01-15, 02:00 UTC', f)) and \
-       address == 'CxiDs1-0|Cspad-0':
-    # CXI detector rebuilt in Jan 2014, before that use CXI 8.1 metrology.
-    return 'CXI 8.1'
-
-
-  elif time < timegm(strptime('2014-03-31, 02:00 UTC', f)):
-    # LCLS run 8: until Mar 31, 2014
-    if address == 'CxiDs1-0|Cspad-0':
-      return 'CXI 8.2'
-    elif address == 'CxiDsd-0|Cspad-0':
-      return 'CXI 8.d'
-    elif address == 'XppGon-0|Cspad-0':
-      return 'XPP 8.1'
-    elif address == 'XppGon-0|marccd-0':
-      return 'XPP 8.marccd'
-
-  return None
