ebbb8652a8549a6835ce65eb9d43f0bf3be771b2
Youval Dar <youdar@users.noreply.github.com>
2014-03-04 07:29:20 +0000

    - Fix issue with testing refinement  convergence, due to numerical differences in sites_cart, between pdb hierarchy and xray-structure - Add doc strings - Change function to class

--- mmtbx/refinement/minimization_ncs_constraints.py
+++ mmtbx/refinement/minimization_ncs_constraints.py
@@ -3,11 +3,12 @@ from cctbx import xray
 import scitbx.lbfgs
 from libtbx import adopt_init_args
 from scitbx.array_family import flex
+from libtbx.utils import Sorry
 
 class lbfgs(object):
   def __init__(self,
         fmodel,
-        ncs_transformations_object=None,
+        ncs_to_asu_obj=None,
         ncs_atom_selection = None,
         finite_grad_differences_test = False,
         max_iterations=100,
@@ -15,12 +16,21 @@ class lbfgs(object):
         u_iso = False):
     """
     NCS constrained ADP and coordinates refinement
+
+    Arguments:
+    fmodel : fmodel of the complete ASU (Not the NCS fmodel)
+    ncs_to_asu_obj : A multimer object containing information
+                    on the NCS, ASU and MTRIX transformations
+    ncs_atom_selection : A flex bool array, selection of atoms in the NCS.
+    finite_grad_differences_test : When True, run grad differences test
+    sites : When True, refine using coordinates
+    u_iso : When True, B-factor (ADP) minimization
     """
     adopt_init_args(self, locals())
-    assert [self.sites, self.u_iso].count(True) == 1
+    if [self.sites, self.u_iso].count(True) != 1:
+      raise Sorry('Need to use either sites OR u_iso method!')
     self.fmodel.xray_structure.scatterers().flags_set_grads(state=False)
     self.x_target_functor = self.fmodel.target_functor()
-    self.ncs_to_asu = ncs_transformations_object # XXX
     # xray structure of NCS chains for self.x
     ncs_fmodel_xrs = self.fmodel.xray_structure.select(ncs_atom_selection)
     if(self.sites):
@@ -52,13 +62,21 @@ class lbfgs(object):
       update_f_calc  = True)
 
   def compute_functional_and_gradients(self, compute_gradients=True):
+    """
+    Argument:
+    compute_gradients : Bool, When True gradients are calculated
+
+    Returns:
+    target_work : float, value of target function, minimization cost function
+    g : When calculated, flex.double array, gradients of the target function
+    """
     self.update_fmodel()
     g = None
     tgx = self.x_target_functor(compute_gradients=compute_gradients)
     if(self.sites):
       if(compute_gradients):
-        gx = flex.vec3_double(tgx.\
-          gradients_wrt_atomic_parameters(site=True).packed())
+        gx = flex.vec3_double(
+          tgx.gradients_wrt_atomic_parameters(site=True).packed())
         g = self.grads_asu_to_one_ncs(grad=gx).as_double()
     if(self.u_iso):
       if(compute_gradients):
@@ -66,7 +84,8 @@ class lbfgs(object):
         g = self.grads_asu_to_one_ncs(grad=gx).as_double()
     if(self.finite_grad_differences_test and compute_gradients):
       self.finite_difference_test(g)
-    return tgx.target_work(), g
+    target_work = tgx.target_work()
+    return target_work, g
 
   def update_fmodel(self, x=None):
     if not x: x = self.x
@@ -82,10 +101,22 @@ class lbfgs(object):
       update_f_calc  = True)
 
   def refinable_params_one_ncs_to_asu(self, x=None):
+    """
+    For coordinates refinement: apply rotation and translation to x,
+                                build the ASU from the NCS
+    For B-factor refinement: create copies of the refinable parameters
+                             for each one of the NCSs
+
+    Argument:
+    x : sites coordinates of single NCS
+
+    returns:
+    flex.double array of refinable parameters for the complete ASU
+    """
     if(self.sites):
       assert x is not None
-      rotations = self.ncs_to_asu.rotation_matrices
-      translations =  self.ncs_to_asu.translation_vectors
+      rotations = self.ncs_to_asu_obj.rotation_matrices
+      translations =  self.ncs_to_asu_obj.translation_vectors
       assert len(rotations)==len(translations)
       new_x = list(x)
       x = flex.vec3_double(x)
@@ -94,10 +125,20 @@ class lbfgs(object):
         new_x += list(tmp_x.as_double())
       return flex.double(new_x)
     if(self.u_iso):
-      return flex.double(list(self.x)*(self.ncs_to_asu.number_of_transforms+1))
+      return flex.double(list(self.x)*(self.ncs_to_asu_obj.number_of_transforms+1))
 
   def grads_asu_to_one_ncs(self, grad):
-    n = self.ncs_to_asu.number_of_transforms
+    """(vec3_double) -> vec3_double
+    Average ASU target function gradients, with rotation when doing
+    coordinates refinement and without rotation when not
+
+    Argument:
+    grad : the gradient of the complete ASU
+
+    Returns:
+    g_ave : The average the gradients of all NCS copies in the ASU
+    """
+    n = self.ncs_to_asu_obj.number_of_transforms
     # gradients of the first NCS copy
     ncs_end = len(grad)//(n+1)
     assert ncs_end*(n+1)==len(grad)
@@ -105,7 +146,7 @@ class lbfgs(object):
     for i in range(n):
       g = grad[ncs_end*(i+1):ncs_end*(i+2)]
       if(self.sites):
-        rt = self.ncs_to_asu.rotation_matrices[i].transpose().elems
+        rt = self.ncs_to_asu_obj.rotation_matrices[i].transpose().elems
         g = rt*g
       g_ave += g
     g_ave = g_ave * (1./(n+1))
@@ -114,6 +155,13 @@ class lbfgs(object):
     return g_ave
 
   def finite_difference_test(self, g):
+    """
+    finite gradient test compares numerical estimate of the gradient
+    to the largest of the calculated gradients.
+
+    Argument:
+    g : flex array, target function gradient
+    """
     if(self.fmodel.r_work()>1.e-3):
       g = g.as_double()
       d = 1.e-3
