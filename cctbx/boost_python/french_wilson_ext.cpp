a6a52e26e8925388c40e7086220341ce91b65184
Pavel Afonine <pafonine@users.noreply.github.com>
2014-03-13 02:38:00 +0000

    python ports for individual functions

--- cctbx/boost_python/french_wilson_ext.cpp
+++ cctbx/boost_python/french_wilson_ext.cpp
@@ -1,27 +1,49 @@
 #include <boost/python/module.hpp>
 #include <boost/python/def.hpp>
 #include <cctbx/french_wilson.h>
+#include <scitbx/array_family/boost_python/shared_wrapper.h>
+#include <cctbx/import_scitbx_af.h>
 
-namespace cctbx {
-namespace {
+namespace cctbx { namespace boost_python {
 
   void init_module()
   {
     using namespace boost::python;
 
-    def("expectEFWacen",
+    def("expectEFW",
       (double(*)
         (double,
-         double)) expectEFWacen, (
+         double,
+         bool)) expectEFW, (
       arg("eosq"),
-      arg("sigesq")));
+      arg("sigesq"),
+      arg("centric")));
+
+    def("expectEsqFW",
+      (double(*)
+        (double,
+         double,
+         bool)) expectEsqFW, (
+      arg("eosq"),
+      arg("sigesq"),
+      arg("centric")));
+
+    def("is_FrenchWilson",
+      (bool(*)
+        (af::shared<double>,
+         af::shared<double>,
+         af::shared<bool>,
+         double)) is_FrenchWilson, (
+      arg("F"),
+      arg("SIGF"),
+      arg("is_centric"),
+      arg("eps")));
 
   }
 
-} // namespace <anonymous>
-} // namespace cctbx::maptbx::boost_python
+}}
 
 BOOST_PYTHON_MODULE(cctbx_ext)
 {
-  cctbx::init_module();
+  cctbx::boost_python::init_module();
 }
