4bb56a1819217cbdde2aeeba5630a5d71571df41
Johan Hattne <johan@hattne.se>
2014-03-17 23:13:20 +0000

    Cleaner method to determine path to shell script.  Thanks to rjgildea@.

--- xfel/command_line/cxi_pbs.py
+++ xfel/command_line/cxi_pbs.py
@@ -12,17 +12,14 @@ import sys
 def run(argv=None):
   import libtbx.load_env
 
-  from os import X_OK, access, path
+  from os import path
   from libtbx import easy_run
 
   if argv is None:
     argv = sys.argv
 
   # Absolute path to the executable Bourne-shell script.
-  pbs_sh = path.join(
-    libtbx.env.find_in_repositories('cctbx_project/xfel/cxi'),
-    'pbs.sh')
-  assert access(pbs_sh, X_OK)
+  pbs_sh = libtbx.env.under_dist('xfel', path.join('cxi', 'pbs.sh'))
 
   return easy_run.call(' '.join([pbs_sh] + argv[1:]))
 
