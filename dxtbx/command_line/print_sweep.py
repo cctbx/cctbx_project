04ccffbc0aea1aa04482ec57116fd242c5801ed7
Graeme Winter <graeme.winter@gmail.com>
2014-03-19 13:39:59 +0000

    Print derived quantity, e.g. beam centre

--- dxtbx/command_line/print_sweep.py
+++ dxtbx/command_line/print_sweep.py
@@ -10,6 +10,26 @@ def print_sweep(list_of_images):
     print s.get_goniometer()
     print s.get_scan()
 
+    # compute the beam centre... in mm... w.r.t. fast, slow axis
+
+    print 'Derived quantities:'
+
+    from scitbx import matrix
+
+    d = s.get_detector()[0]
+    b = s.get_beam()
+
+    o = matrix.col(d.get_origin())
+    f = matrix.col(d.get_fast_axis())
+    s = matrix.col(d.get_slow_axis())
+    s0 = matrix.col(b.get_direction())
+
+    n = f.cross(s)
+
+    beam_offset = o - o.dot(s0) * s0
+    print '    beam centre (mm, fast, slow): %.2f %.2f' % (- beam_offset.dot(f),
+                                                            - beam_offset.dot(s))
+
 if __name__ == '__main__':
   import sys
 
