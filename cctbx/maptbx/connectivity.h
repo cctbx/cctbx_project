3aa61be5c78e4aef91d72170e6aa94da4ae1ecc3
Oleg Sobolev <olegsobolev@users.noreply.github.com>
2014-03-19 18:51:53 +0000

    A tool to find connected regions on map. With tests.

--- cctbx/maptbx/connectivity.h
+++ cctbx/maptbx/connectivity.h
@@ -5,30 +5,106 @@
 
 namespace cctbx { namespace maptbx {
 
-//! Placeholder for map connectivity analysis code.
+//! Map connectivity analysis.
 
 class connectivity {
+
 public:
-  af::versa<double, af::c_grid<3> > map_new;
+  af::versa<int, af::c_grid<3> > map_new;
+
+  void
+  get_six_neighbours(
+    int const& x,
+    int const& y,
+    int const& z,
+    af::tiny<int, 3> const& dimensions,
+    af::versa<int, af::c_grid<2> > neighbours)
+  {
+    // x+-1
+    neighbours(0,0) = ((x+1<dimensions[0]) ? x+1 : 0);
+    neighbours(1,0) = ((x-1>=0) ? x-1 : dimensions[0]-1);
+    neighbours(0,1) = neighbours(1,1) = y;
+    neighbours(0,2) = neighbours(1,2) = z;
+    // y+-1
+    neighbours(2,1) = ((y+1<dimensions[1]) ? y+1 : 0);
+    neighbours(3,1) = ((y-1>=0) ? y-1 : dimensions[1]-1);
+    neighbours(2,0) = neighbours(3,0) = x;
+    neighbours(2,2) = neighbours(3,2) = z;
+    // z+-1
+    neighbours(4,2) = ((z+1<dimensions[2]) ? z+1 : 0);
+    neighbours(5,2) = ((z-1>=0) ? z-1 : dimensions[2]-1);
+    neighbours(4,0) = neighbours(5,0) = x;
+    neighbours(4,1) = neighbours(5,1) = y;
+  }
+
 
   connectivity(
     af::const_ref<double, af::c_grid<3> > const& map_data,
     double const& threshold)
   {
     af::tiny<int, 3> a = map_data.accessor();
-    map_new.resize(af::c_grid<3>(a), 0);
+    int pointer_empty=0, pointer_current=0, cur_reg = 0;
+
+    // estimating size of working array tempcoors. If this code fails with
+    // segmentation fault or reveal a bug, here is the first place to look.
+    // To make sure the cause is not here, just make tempcoors of map_data
+    // size and delete boundaries check (91 and 97th lines).
+    int maxside = ((a[0]>a[1]) ? a[0] : a[1]);
+    maxside = maxside>a[2] ? maxside : a[2];
+    int needed_size = 4*4*maxside*maxside;
+    af::versa<int,af::c_grid<2> >  tempcoors(af::c_grid<2>(needed_size,3));
+
+    map_new.resize(af::c_grid<3>(a), -1);
+    af::versa<int, af::c_grid<2> > neighbours(af::c_grid<2>(6,3));
+
     for (int i = 0; i < a[0]; i++) {
       for (int j = 0; j < a[1]; j++) {
         for (int k = 0; k < a[2]; k++) {
-          double rho = map_data(i,j,k);
-          if(rho < threshold) map_new(i,j,k) = 0;
-          else                map_new(i,j,k) = 1;
+          if (map_new(i,j,k)<0) {
+            if (map_data(i,j,k) > threshold) {
+              // got a new point, start filling
+              cur_reg += 1;
+              tempcoors(0,0) = i;
+              tempcoors(0,1) = j;
+              tempcoors(0,2) = k;
+              map_new(i,j,k) = cur_reg;
+              pointer_empty = 1;
+              pointer_current = 0;
+              while (pointer_empty != pointer_current) {
+                get_six_neighbours(tempcoors(pointer_current,0),
+                                   tempcoors(pointer_current,1),
+                                   tempcoors(pointer_current,2),
+                                   a,
+                                   neighbours);
+                for (int l = 0; l<6; l++) {
+                  //processing 6 neighbours
+                  int x = neighbours(l,0);
+                  int y = neighbours(l,1);
+                  int z = neighbours(l,2);
+                  if (map_new(x,y,z)<0) {
+                    if (map_data(x,y,z) > threshold) {
+                      map_new(x,y,z) = cur_reg;
+                      tempcoors(pointer_empty,0) = x;
+                      tempcoors(pointer_empty,1) = y;
+                      tempcoors(pointer_empty,2) = z;
+                      pointer_empty += 1;
+                      if (pointer_empty >= needed_size) pointer_empty = 0;
+                    }
+                    else map_new(x,y,z) = 0;
+                  }
+                }
+                pointer_current += 1;
+                if (pointer_current >= needed_size) pointer_current = 0;
+              }
+            }
+            else map_new(i,j,k) = 0;
+          }
         }
       }
     }
   }
 
-  af::versa<double, af::c_grid<3> > result() {return map_new;}
+  af::versa<int, af::c_grid<3> > result() {return map_new;}
 
 };
 
--- cctbx/regression/tst_connectivity.py
+++ cctbx/regression/tst_connectivity.py
@@ -2,25 +2,141 @@ from __future__ import division
 from scitbx.array_family import flex
 import iotbx.pdb
 from cctbx import maptbx
-from libtbx.test_utils import approx_equal
+from cctbx import miller
 
-def exercise():
+def getvs(cmap, threshold):
+  co = maptbx.connectivity(map_data=cmap, threshold=threshold)
+  map_result = co.result()
+  v=[0,0,0]
+  for i in range(3):
+    v[i] = (map_result==i).count(True)
+  return v
+
+
+def exercise1():
   pdb_str="""
-CRYST1   10.000   15.000   17.000  70.00 110.00 120.00 P 1
-HETATM    1  C    C      1       5.000   7.000  -9.000  1.00 20.00           C
+CRYST1   10.000  10.000   10.000  90.00  90.00  90.00 P 1
+HETATM    1  C    C      1       2.000   2.000   2.000  1.00 20.00           C
 END
 """
   pdb_inp = iotbx.pdb.input(source_info=None, lines=pdb_str)
   xrs = pdb_inp.xray_structure_simple()
+  cg = maptbx.crystal_gridding(unit_cell=xrs.unit_cell(),
+      pre_determined_n_real=(100,100,100),
+      space_group_info=xrs.space_group_info())
   fc = xrs.structure_factors(d_min = 1., algorithm = "direct").f_calc()
-  fft_map = fc.fft_map(resolution_factor = 0.2)
+  fft_map = miller.fft_map(crystal_gridding=cg, fourier_coefficients=fc)
+  map_data = fft_map.real_map_unpadded()
+  v = getvs(map_data, 100)
+  assert v[2] == 0
+  assert v[1] < 15000
+  assert v[0]+v[1]+v[2] == 1000000
+
+def exercise3():
+  pdb_str="""
+CRYST1   10.000  10.000   10.000  90.00  90.00  90.00 P 1
+HETATM    1  C    C      1       2.000   2.000   2.000  1.00  2.00           C
+HETATM    1  C    C      1       3.500   2.000   2.000  1.00  2.00           C
+END
+"""
+
+  pdb_inp = iotbx.pdb.input(source_info=None, lines=pdb_str)
+  xrs = pdb_inp.xray_structure_simple()
+  cg = maptbx.crystal_gridding(unit_cell=xrs.unit_cell(),
+      pre_determined_n_real=(100,100,100),
+      space_group_info=xrs.space_group_info())
+  fc = xrs.structure_factors(d_min = 1., algorithm = "direct").f_calc()
+  fft_map = miller.fft_map(crystal_gridding=cg, fourier_coefficients=fc)
   fft_map.apply_sigma_scaling()
   map_data = fft_map.real_map_unpadded()
-  co = maptbx.connectivity(map_data = map_data, threshold=1)
-  map_result = co.result()
-  print map_result.as_1d().min_max_mean().as_tuple()
-  print (map_result==1).count(True), (map_result==1).count(False)
-  print map_result.size()
+  #all filled
+  v = getvs(map_data, -100)
+  assert v == [0, 1000000, 0]
+  # can see one blob
+  v = getvs(map_data, 5)
+  assert v[0]+v[1]+v[2] == 1000000
+  assert v[2] == 0
+  # can see separate, approx equal volume bloobs
+  v = getvs(map_data, 10)
+  assert v[0]+v[1]+v[2] == 1000000
+  assert abs(v[1] - v[2]) < 5
+  # nothing to see
+  v = getvs(map_data, 1000)
+  assert v == [1000000, 0, 0]
+
+def exercise4():
+  cmap = flex.double(flex.grid(100,100,100))
+  cmap.fill(1)
+  for i in range(10,20):
+    for j in range(10,20):
+      for k in range(10,20):
+        cmap[i,j,k] = 10
+  v = getvs(cmap, 5)
+  assert v == [999000, 1000, 0]
+  #print "all filled"
+  v = getvs(cmap, -5)
+  assert v == [0,1000000,0]
+  #print "none filled"
+  v = getvs(cmap, 20)
+  assert v == [1000000,0,0]
+
+def exercise5():
+  #print "corner blob"
+  cmap = flex.double(flex.grid(100,100,100))
+  cmap.fill(0)
+  for i in range(100):
+    for j in range(100):
+      for k in range(100):
+        if (i<10 or i>=90) and (j<10 or j>=90) and (k<10 or k>=90):
+          cmap[i,j,k] = 10
+          #print i,j,k
+  v = getvs(cmap, 5)
+  assert v == [992000, 8000, 0]
+
+  #print "2 blobs"
+  cmap.fill(0)
+  for i in range(100):
+    for j in range(100):
+      for k in range(100):
+        if (5<i<10) and (5<j<10) and (5<k<10):
+          cmap[i,j,k] = 10
+        if (15<i<20) and (15<j<20) and (15<k<20):
+          cmap[i,j,k] = 20
+  v = getvs(cmap, 5)
+  assert v == [999872,64,64]
+  v = getvs(cmap, 15)
+  assert v == [999936, 64,0]
+
+  #print "endless blob"
+  cmap.fill(0)
+  for j in range(100):
+    for k in range(100):
+      cmap[5,j,k] = 10
+  v = getvs(cmap, 5)
+  assert v == [990000, 10000, 0]
+
+def exercise6():
+  cmap = flex.double(flex.grid(100,100,100))
+  #print "corner touch"
+  cmap.fill(0)
+  cmap[1,1,1] = cmap[2,2,2] = 10
+  v = getvs(cmap, 5)
+  assert v == [999998, 1, 1]
+  #print "edges touch"
+  cmap.fill(0)
+  cmap[1,1,1] = cmap[2,2,1] = 10
+  v = getvs(cmap, 5)
+  assert v == [999998, 1, 1]
+  #print "face touch"
+  cmap.fill(0)
+  cmap[1,1,1] = cmap[2,1,1] = 10
+  v = getvs(cmap, 5)
+  assert v == [999998, 2, 0]
+
 
 if __name__ == "__main__" :
-  exercise()
+  exercise1()
+  exercise3()
+  exercise4()
+  exercise5()
+  exercise6()
