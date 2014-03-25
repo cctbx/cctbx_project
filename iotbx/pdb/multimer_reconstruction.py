04ca6c062cc5a3e376bebd7976cbeef0237904e2
ydr <y0udar@users.noreply.github.com>
2014-03-25 19:40:14 +0000

    Change naming of new chains, created when building ASU

--- iotbx/pdb/multimer_reconstruction.py
+++ iotbx/pdb/multimer_reconstruction.py
@@ -129,7 +129,7 @@ class multimer(object):
 
     Create a dictionary
     keys: a string made of chain_name + str(i_transform number)
-    values: two letters and digits string combination
+    values: string (one or two chr long). letters and digits string combination
 
     The total number of new chains can be large (order of hundreds)
     Chain names might repeat themselves several times in a pdb file
@@ -137,10 +137,9 @@ class multimer(object):
     similar BIOMT/MTRIX transformation
 
     Arguments:
-    i_transform : an integer. the number of BIOMT/MTRIX transformation operations
-                  in the pdb object
-    nChains : an integer. the number of chains as interpreted by pdb.hierarchy.
-              input unique_chain_names : a set. a set of unique chain names
+    i_transform : (list) list of BIOMT/MTRIX transformation number
+    nChains : (integer) the number of chains as interpreted by pdb.hierarchy.
+    unique_chain_names : (set) a set of unique chain names
 
     Returns:
     new_names : a dictionary. {'A1': 'aa', 'A2': 'gq',....} map a chain name and
@@ -151,17 +150,39 @@ class multimer(object):
     '''
     # create list of character from which to assemble the list of names
     total_chains_number = len(i_transforms)*len(unique_chain_names)
-    # the number of character needed to produce new names
-    chr_number = int(math.sqrt(total_chains_number)) + 1
-    # build character list
-    chr_list = list(string.ascii_letters) + list(string.digits)
-    # take only as many characters as needed
-    chr_list = chr_list[:chr_number]
-    dictionary_values = set([ x+y for x in chr_list for y in chr_list])
-    dictinary_key = set([x+str(y) for x in unique_chain_names for y in i_transforms])
+    # start naming chains with a single letter
+    chr_list1 = list(set(string.ascii_uppercase) - unique_chain_names)
+    chr_list2 = list(set(string.ascii_lowercase) - unique_chain_names)
+    chr_list1.sort()
+    chr_list2.sort()
+    new_names_list = chr_list1 + chr_list2
+    # check if we need more chain names
+    if len(new_names_list) < total_chains_number:
+      n_names =  total_chains_number - len(new_names_list)
+      # the number of character needed to produce new names
+      chr_number = int(math.sqrt(n_names)) + 1
+      # build character list
+      chr_list = list(string.ascii_uppercase) + \
+                 list(string.ascii_lowercase) + \
+                 list(string.digits)
+      # take only as many characters as needed
+      chr_list = chr_list[:chr_number]
+      extra_names = set([ x+y for x in chr_list for y in chr_list])
+      # make sure not using existing names
+      extra_names = list(extra_names - unique_chain_names)
+      extra_names.sort()
+      new_names_list.extend(extra_names)
+    assert len(new_names_list) >= total_chains_number
+    dictionary_values = new_names_list[:total_chains_number]
+    dictinary_key = list(set([x+str(y) for x in unique_chain_names for y in
+                         i_transforms]))
+    dictinary_key.sort()
     # create the dictionary
-    new_names_dictionary ={x:y for (x,y) in zip(dictinary_key,dictionary_values)}
+    zippedlists = zip(dictinary_key,dictionary_values)
+    new_names_dictionary ={x:y for (x,y) in zippedlists}
     return new_names_dictionary
+
+
 
   def sites_cart(self):
     '''(multimer) -> flex.vec3_double
