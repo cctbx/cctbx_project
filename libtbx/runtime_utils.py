b01bb7626554eb634485a2fb8fb76028347c840b
Nathaniel Echols <nathaniel.echols@gmail.com>
2014-04-07 04:52:36 +0000

    bug fix

--- libtbx/runtime_utils.py
+++ libtbx/runtime_utils.py
@@ -54,10 +54,12 @@ class simple_target (object) :
 class target_with_save_result (object) :
   def __init__ (self, args, file_name, output_dir=None, log_file=None,
       job_title=None) :
-    assert (isinstance(file_name, str))
+    assert (isinstance(file_name, str)) or isinstance(file_name, unicode)
     assert (isinstance(args, list) or isinstance(args, tuple))
-    assert (output_dir is None) or (isinstance(output_dir, str))
-    assert (log_file is None) or (isinstance(log_file, str))
+    assert ((output_dir is None) or isinstance(output_dir, str) or
+            isinstance(output_dir, unicode))
+    assert ((log_file is None) or isinstance(log_file, str) or
+            isinstance(log_file, unicode))
     adopt_init_args(self, locals())
     if (output_dir is None) :
       self.output_dir = os.getcwd()
