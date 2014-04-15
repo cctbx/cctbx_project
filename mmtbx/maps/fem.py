f07d44153078541b736264462939ce371cfd960c
Pavel Afonine <pafonine@users.noreply.github.com>
2014-04-15 23:39:39 +0000

    Make use of median map instead of intersection&averaging

--- mmtbx/maps/fem.py
+++ mmtbx/maps/fem.py
@@ -9,6 +9,7 @@ from cctbx import adptbx
 from libtbx.test_utils import approx_equal
 from mmtbx.maps import kick
 import sys
+from libtbx import adopt_init_args
 
 def get_map(mc, cg=None):
   if(cg is None):
@@ -20,51 +21,22 @@ def get_map(mc, cg=None):
   fft_map.apply_sigma_scaling()
   return fft_map.real_map_unpadded()
 
-def scale(x,y):
-  n = flex.sum( flex.abs(x)*flex.abs(y) )
-  d = flex.sum( flex.abs(y)*flex.abs(y) )
-  return n/d
-
-def compute_map_and_combine(
-      map_coeffs,
-      map_data,
-      cg,
-      intersection_thresholds):
-  m = get_map(mc=map_coeffs, cg=cg)
-  #maptbx.reset(
-  #    data=m,
-  #    substitute_value=0.0,
-  #    less_than_threshold=0.0,
-  #    greater_than_threshold=-9999,
-  #    use_and=True)
-  #maptbx.map_box_average(
-  #  map_data   = m,
-  #  cutoff     = 0.0,
-  #  index_span = 1)
-  if(map_data is None): map_data = m
-  else:
-    if(intersection_thresholds is not None):
-      map_data=intersection(m1=map_data,m2=m,thresholds=intersection_thresholds)
-    else:
-      map_data = (m+map_data)/2
-  return map_data
-
-def intersection(m1,m2,thresholds=flex.double([i/10. for i in xrange(9)]+[1.]),
-      average=True):
-  assert [m1,m2].count(None) != 2
-  if(  m1 is None): return m2
-  elif(m2 is None): return m1
-  else:
-    maptbx.intersection(
-      map_data_1 = m1,
-      map_data_2 = m2,
-      thresholds = thresholds,
-      average    = average)
-  return m1
+class counter(object):
+  def __init__(self, n1, n2, log):
+    adopt_init_args(self, locals())
+    self.n=0
+    assert self.n1*self.n2>0
+    self.progress_scale = 100./(self.n1*self.n2)
+  def show(self):
+    self.n += 1
+    self.log.write(
+      "\r%s %d%%" %("FEM loop: done so far:",
+      int(self.n*self.progress_scale)))
+    self.log.flush()
 
 class run(object):
 
-  def __init__(self, fmodel, use_omit=False, use_resolve=False, sharp=True):
+  def __init__(self, fmodel, signal_threshold, use_omit=False, sharp=True):
     # make sure common B moved to k_total
     if(sharp):
       b_iso_min = flex.min(
@@ -81,44 +53,16 @@ class run(object):
         map_type         = "2mFo-DFc",
         isotropize       = True,
         fill_missing     = False)
-    # compute bs mask and define gridding
+    # define gridding
     crystal_gridding = fmodel.f_obs().crystal_gridding(
       d_min              = fmodel.f_obs().d_min(),
       resolution_factor  = 0.25)
-    bs_mask = mmtbx.masks.mask_from_xray_structure(
-      xray_structure           = fmodel.xray_structure,
-      p1                       = True,
-      solvent_radius           = 1.,#4,
-      shrink_truncation_radius = 1.,#2,
-      for_structure_factors    = False,
-      n_real                   = crystal_gridding.n_real()).mask_data
-    crystal_gridding = maptbx.crystal_gridding(
-      unit_cell             = fmodel.f_obs().unit_cell(),
-      space_group_info      = fmodel.f_obs().space_group_info(),
-      pre_determined_n_real = bs_mask.all())
     # Fill missing
-    if(use_resolve):
-      self.mc_filled = mmtbx.map_tools.resolve_dm_map(
-        fmodel       = fmodel,
-        map_coeffs   = self.mc_iso,
-        mask_cycles  = 5,
-        minor_cycles = 5,
-        pdb_inp      = None,
-        use_model_hl = True,
-        fill         = True)
-    else:
-      missing_reflections_manager = mmtbx.map_tools.model_missing_reflections(
-        coeffs=self.mc_iso, fmodel=fmodel)
-      missing = missing_reflections_manager.get_missing(deterministic=True)
-      self.mc_filled = self.mc_iso.complete_with(other=missing, scale=True)
-    m_filter = kick.randomize_completeness(
-      map_coeffs       = self.mc_filled,
-      crystal_gridding = crystal_gridding)
-    # Filter masks
-    m_filter = maptbx.conditional_solvent_region_filter(
-      bulk_solvent_mask = bs_mask,
-      map_data          = m_filter,
-      threshold         = 0.5)
+    missing_reflections_manager = mmtbx.map_tools.model_missing_reflections(
+      coeffs=self.mc_iso, fmodel=fmodel)
+    missing = missing_reflections_manager.get_missing(deterministic=True)
+    self.mc_filled        = self.mc_iso.complete_with(other=missing, scale=True)
+    self.mc_filled_no_iso = self.mc.complete_with(other=missing, scale=True)
     # composite OMIT map
     m_omit = None
     if(use_omit):
@@ -139,67 +83,101 @@ class run(object):
         use_and=True)
       m_omit = m_omit.set_selected(m_omit< 0.5, 0)
       m_omit = m_omit.set_selected(m_omit>=0.5, 1)
-      m_filter = m_filter * m_omit
-    #
+    # Extra filter
+    m_filter = None
+    for mc in [self.mc, self.mc_iso, self.mc_filled, self.mc_filled_no_iso]:
+      m_filter_ = get_map(mc = mc, cg=crystal_gridding)
+      maptbx.binarize(
+        map_data               = m_filter_,
+        threshold              = 0.5,
+        substitute_value_below = 0,
+        substitute_value_above = 1)
+      if(m_filter is None): m_filter = m_filter_
+      else:                 m_filter = m_filter * m_filter_
     # FEM loop
-    wam = kick.weighted_average(fmodel=fmodel, map_coefficients=self.mc,
-      missing=self.mc_filled)#missing)
-    self.map_result=None
-    cntr = 0
-    n_loop_1 = 10
-    n_loop_2 = 10
-    progress_scale = 100./(n_loop_1*n_loop_2)
-    for it2 in xrange(n_loop_1):
-      m,cntr= fem_loop_2(wam=wam, n=n_loop_2, crystal_gridding=crystal_gridding,
-        cntr=cntr, progress_scale=progress_scale)
-      maptbx.reset(
-        data=m,
-        substitute_value=0.0,
-        less_than_threshold=0.25,
-        greater_than_threshold=-9999,
-        use_and=True)
-      if(sharp):
-        maptbx.sharpen(map_data=m, index_span=1, n_averages=2)
-      m  = maptbx.volume_scale(map = m,  n_bins = 10000).map_data()
-      self.map_result = intersection(m1 = self.map_result, m2 = m)
-      #
-      mc_ = self.mc_filled.structure_factors_from_map(
-        map            = self.map_result,
-        use_scale      = True,
-        anomalous_flag = False,
-        use_sg         = False)
-      m_=get_map(mc_, crystal_gridding)
-      m_ = m_.set_selected(m_< 1, 0)
-      m_ = m_.set_selected(m_>=1, 1)
-      m_filter = m_filter * m_
-      #
-    sys.stdout.write("\n")
-    sys.stdout.flush()
+    progress_counter = counter(n1=10, n2=16, log=sys.stdout)
+    self.map_result = fem_loop(
+      fmodel           = fmodel,
+      mc               = self.mc_iso,
+      n                = 10,
+      n_cycles         = 50,
+      n_macro_cycles   = 16,
+      signal_threshold = signal_threshold,
+      crystal_gridding = crystal_gridding,
+      m_filter         = m_filter,
+      progress_counter = progress_counter)
+    print
     #
-    self.map_result = self.map_result*m_filter
+    if(m_omit is not None):
+      self.map_result = self.map_result*m_omit
     self.mc_result = self.mc_filled.structure_factors_from_map(
       map            = self.map_result,
       use_scale      = True,
       anomalous_flag = False,
       use_sg         = False)
 
-def fem_loop_2(wam, n, crystal_gridding, n_cycles=100, progress_scale=None,
-               cntr=None, intersection_thresholds=None):
+def ccp4_map(map_data, unit_cell, space_group, n_real, file_name):
+  from iotbx import ccp4_map
+  ccp4_map.write_ccp4_map(
+      file_name=file_name,
+      unit_cell=unit_cell,
+      space_group=space_group,
+      #gridding_first=(0,0,0),# This causes a bug (map gets shifted)
+      #gridding_last=n_real,  # This causes a bug (map gets shifted)
+      map_data=map_data,
+      labels=flex.std_string([""]))
+
+def fem_loop(fmodel, mc, n, crystal_gridding, signal_threshold, n_cycles,
+             n_macro_cycles, m_filter, progress_counter=None):
+  mac = maptbx.map_accumulator(n_real = crystal_gridding.n_real())
+  for i in xrange(n_macro_cycles):
+    m = fem_loop_(fmodel=fmodel, mc=mc, n=n, crystal_gridding=crystal_gridding,
+      n_cycles=n_cycles, signal_threshold=signal_threshold,
+      progress_counter=progress_counter)
+    m = m*m_filter
+    mac.add(map_data=m)
+#    ccp4_map(map_data=m, unit_cell=mc.unit_cell(),
+#      space_group=mc.space_group(), n_real=m.all(), file_name="%s.ccp4"%str(i))
+#  ccp4_map(map_data=mm, unit_cell=mc.unit_cell(),
+#      space_group=mc.space_group(), n_real=m.all(), file_name="ave.ccp4")
+  return mac.as_median_map()
+
+def fem_loop_(fmodel, mc, n, crystal_gridding, n_cycles, signal_threshold,
+              progress_counter=None):
   m = None
+  average_peak_volume = None
   for it in xrange(n):
-    if(cntr is not None):
-      cntr+=1
-      sys.stdout.write(
-        "\r%s %d%%" %("FEM loop: done so far:", int(cntr*progress_scale)))
-      sys.stdout.flush()
+    if(progress_counter is not None): progress_counter.show()
+    #
+    missing_reflections_manager = mmtbx.map_tools.model_missing_reflections(
+      coeffs=mc, fmodel=fmodel)
+    missing = missing_reflections_manager.get_missing(
+      deterministic=random.choice([True, False]))
+    wam=kick.weighted_average(fmodel=fmodel,map_coefficients=mc,missing=missing)
+    #
     mc_wa = wam.random_weight_averaged_map_coefficients(
       random_scale  = 5.,
       random_seed   = int(random.random()*10000000),
       n_cycles      = n_cycles,
       fraction_keep = 0.95)
-    m = compute_map_and_combine(
-      map_coeffs = mc_wa,
-      map_data   = m,
-      cg         = crystal_gridding,
-      intersection_thresholds = intersection_thresholds)
-  return m, cntr
+    m_ = get_map(mc=mc_wa, cg=crystal_gridding)
+    maptbx.reset(
+      data                   = m_,
+      substitute_value       = 0.0,
+      less_than_threshold    = signal_threshold,
+      greater_than_threshold = -9999,
+      use_and                = True)
+    if(m is None):
+      m = m_
+      average_peak_volume = int(maptbx.peak_volume_estimate(
+        map_data         = m_,
+        sites_cart       = fmodel.xray_structure.sites_cart(),
+        crystal_symmetry = fmodel.xray_structure.crystal_symmetry(),
+        cutoff           = signal_threshold)*0.7)*+1
+    else:
+      m = m + m_
+  m = m / n
+  co = maptbx.connectivity(map_data=m, threshold=signal_threshold)
+  m = m*co.volume_cutoff_mask(volume_cutoff=average_peak_volume).as_double()
+  m = maptbx.volume_scale(map = m,  n_bins = 10000).map_data()
+  return m
