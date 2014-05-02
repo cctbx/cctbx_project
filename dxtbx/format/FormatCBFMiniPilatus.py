8a5cfac0994dbf1b1bd3ec9072a87cdd6b42101f
Graeme Winter <graeme.winter@gmail.com>
2014-05-02 10:41:47 +0000

    Some FIXME's and also an implementation which has the data for, but not the calculations for, the measured tile geometry...

--- dxtbx/format/FormatCBFMiniPilatus.py
+++ dxtbx/format/FormatCBFMiniPilatus.py
@@ -98,6 +98,12 @@ class FormatCBFMiniPilatus(FormatCBFMini):
         self._cif_header_dictionary['Count_cutoff'].split()[0])
     underload = -1
 
+    # FIXME take into consideration here the thickness of the sensor? also the
+    # wavelength of the radiation (which we have in the same file...)
+
+    # FIXME would also be very nice to be able to take into account the 
+    # misalignment of the individual modules given the calibration information...
+    
     detector = self._detector_factory.simple(
         'PAD', distance * 1000.0, (beam_x * pixel_x * 1000.0,
                                    beam_y * pixel_y * 1000.0), '+x', '-y',
