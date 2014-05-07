a36152cc2e1fc8d7b5da14e325837275ca3b719d
Pavel Afonine <pafonine@users.noreply.github.com>
2014-05-07 04:22:07 +0000

    Bug fix in mc averaging; improve FEM workflow

--- mmtbx/maps/fem.py
+++ mmtbx/maps/fem.py
@@ -87,9 +87,12 @@ class run(object):
     m_filter = None
     for mc in [self.mc, self.mc_iso, self.mc_filled, self.mc_filled_no_iso]:
       m_filter_ = get_map(mc = mc, cg=crystal_gridding)
+      msk = truncate_with_roots2(m=m_filter_,fmodel=fmodel,c1=0.5,c2=0.25,
+        cutoff=0.5,scale=0.7, as_int=True)
+      maptbx.truncate_special(mask=msk, map_data=m_filter_)
       maptbx.binarize(
         map_data               = m_filter_,
-        threshold              = 0.5,
+        threshold              = 0.0,
         substitute_value_below = 0,
         substitute_value_above = 1)
       if(m_filter is None): m_filter = m_filter_
@@ -98,6 +101,7 @@ class run(object):
     progress_counter = counter(n1=10, n2=16, log=sys.stdout)
     self.map_result = fem_loop(
       fmodel           = fmodel,
+      complete_set = self.mc_filled,
       mc               = self.mc_iso,
       n                = 10,
       n_cycles         = 50,
@@ -106,7 +110,51 @@ class run(object):
       crystal_gridding = crystal_gridding,
       m_filter         = m_filter,
       progress_counter = progress_counter)
-    print
+#
+    maptbx.sharpen(map_data=self.map_result, index_span=2, n_averages=1)
+#
+    mc = self.mc_filled.structure_factors_from_map(
+      map            = self.map_result,
+      use_scale      = True,
+      anomalous_flag = False,
+      use_sg         = False)
+    m = get_map(mc=mc, cg = crystal_gridding)
+
+    average_peak_volume = int(maptbx.peak_volume_estimate(
+      map_data         = m,
+      sites_cart       = fmodel.xray_structure.sites_cart(),
+      crystal_symmetry = fmodel.xray_structure.crystal_symmetry(),
+      cutoff           = 1)*0.7)
+    co = maptbx.connectivity(map_data=m, threshold=1.0)
+
+    F = co.volume_cutoff_mask(volume_cutoff=average_peak_volume).as_double()
+    self.map_result = self.map_result*F
+    self.map_result = maptbx.volume_scale(map = self.map_result,  n_bins = 10000).map_data()
+####
+    keep_interblob=True
+    F = None
+    for c1 in [0.95, 0.9, 0.85, 0.8,0.7,0.6,0.5,0.4,0.3,0.2]:
+      if(c1<0.9): keep_interblob=False
+      f = truncate_with_roots2(m=self.map_result,fmodel=fmodel,c1=c1,c2=c1-0.1,
+        cutoff=c1,scale=1, keep_interblob=keep_interblob)
+      if(F is None): F = f
+      else: F = F * f
+#
+    mc = self.mc_filled.structure_factors_from_map(
+      map            = self.map_result,
+      use_scale      = True,
+      anomalous_flag = False,
+      use_sg         = False)
+    m = get_map(mc=mc, cg = crystal_gridding)
+    keep_interblob=True
+    for c1 in [1.0,0.9,0.8,0.7,0.6,0.5]:
+      if(c1<0.9): keep_interblob=False
+      f = truncate_with_roots2(m=m,fmodel=fmodel,c1=c1,c2=c1-0.1,cutoff=c1,scale=1.0,
+        keep_interblob=keep_interblob)
+      if(F is None): F = f
+      else: F = F * f
+#
+    self.map_result = self.map_result*F
     #
     if(m_omit is not None):
       self.map_result = self.map_result*m_omit
@@ -128,56 +176,197 @@ def ccp4_map(map_data, unit_cell, space_group, n_real, file_name):
       labels=flex.std_string([""]))
 
 def fem_loop(fmodel, mc, n, crystal_gridding, signal_threshold, n_cycles,
-             n_macro_cycles, m_filter, progress_counter=None):
+             n_macro_cycles, m_filter, complete_set, progress_counter=None):
   mac = maptbx.map_accumulator(n_real = crystal_gridding.n_real())
+  missing_reflections_manager = mmtbx.map_tools.model_missing_reflections(
+    coeffs=mc, fmodel=fmodel)
+  weighted_average_manager = kick.weighted_average(
+    fmodel = fmodel, map_coefficients = mc)
+  #
+  b_sharp = get_b_sharp(fmodel=fmodel, mc=mc, n_cycles=n_cycles,
+      crystal_gridding=crystal_gridding, signal_threshold=signal_threshold,
+      weighted_average_manager=weighted_average_manager,
+      missing_reflections_manager=missing_reflections_manager)
+  print "b_sharp:", b_sharp
+  #
   for i in xrange(n_macro_cycles):
-    m = fem_loop_(fmodel=fmodel, mc=mc, n=n, crystal_gridding=crystal_gridding,
-      n_cycles=n_cycles, signal_threshold=signal_threshold,
-      progress_counter=progress_counter)
+    m = fem_loop_(
+      fmodel                      = fmodel,
+      complete_set = complete_set,
+      mc                          = mc,
+      n                           = n,
+      crystal_gridding            = crystal_gridding,
+      n_cycles                    = n_cycles,
+      signal_threshold            = signal_threshold,
+      progress_counter            = progress_counter,
+      missing_reflections_manager = missing_reflections_manager,
+      b_sharp                     = b_sharp,
+      weighted_average_manager    = weighted_average_manager)
     m = m*m_filter
     mac.add(map_data=m)
 #    ccp4_map(map_data=m, unit_cell=mc.unit_cell(),
 #      space_group=mc.space_group(), n_real=m.all(), file_name="%s.ccp4"%str(i))
 #  ccp4_map(map_data=mm, unit_cell=mc.unit_cell(),
 #      space_group=mc.space_group(), n_real=m.all(), file_name="ave.ccp4")
-  return mac.as_median_map()
+  m = mac.as_median_map()
+  m = maptbx.volume_scale(map = m,  n_bins = 10000).map_data()
+  return m
 
-def fem_loop_(fmodel, mc, n, crystal_gridding, n_cycles, signal_threshold,
-              progress_counter=None):
-  m = None
-  average_peak_volume = None
-  for it in xrange(n):
-    if(progress_counter is not None): progress_counter.show()
-    #
-    missing_reflections_manager = mmtbx.map_tools.model_missing_reflections(
-      coeffs=mc, fmodel=fmodel)
-    missing = missing_reflections_manager.get_missing(
-      deterministic=random.choice([True, False]))
-    wam=kick.weighted_average(fmodel=fmodel,map_coefficients=mc,missing=missing)
-    #
-    mc_wa = wam.random_weight_averaged_map_coefficients(
-      random_scale  = 5.,
-      random_seed   = int(random.random()*10000000),
-      n_cycles      = n_cycles,
-      fraction_keep = 0.95)
-    m_ = get_map(mc=mc_wa, cg=crystal_gridding)
+def one_iteration(
+      fmodel,
+      mc,
+      n_cycles,
+      crystal_gridding,
+      signal_threshold,
+      missing_reflections_manager,
+      b_sharp,
+      weighted_average_manager):
+  mc_wa = weighted_average_manager.random_weight_averaged_map_coefficients(
+    random_scale  = 5.,
+    random_seed   = int(random.random()*10000000),
+    n_cycles      = n_cycles,
+    missing       = missing_reflections_manager.get_missing_fast(),
+    fraction_keep = 0.95)
+  if(b_sharp is not None): # and random.choice([True, False])):
+  #if(b_sharp is not None and random.choice([True, False])):
+    mc_wa = mmtbx.maps.b_factor_sharpening_by_map_kurtosis_maximization(
+      map_coeffs = mc_wa, show = False, b_sharp_best = b_sharp)
+  m = get_map(mc=mc_wa, cg=crystal_gridding)
+  ####
+  #ccp4_map(map_data=m, unit_cell=mc.unit_cell(),
+  #    space_group=mc.space_group(),
+  #    n_real=m.all(),
+  #    file_name="%s.ccp4"%random.choice(xrange(10000)))
+  ####
+  if(signal_threshold is not None):
     maptbx.reset(
-      data                   = m_,
+      data                   = m,
       substitute_value       = 0.0,
       less_than_threshold    = signal_threshold,
       greater_than_threshold = -9999,
       use_and                = True)
-    if(m is None):
-      m = m_
-      average_peak_volume = int(maptbx.peak_volume_estimate(
-        map_data         = m_,
-        sites_cart       = fmodel.xray_structure.sites_cart(),
-        crystal_symmetry = fmodel.xray_structure.crystal_symmetry(),
-        cutoff           = signal_threshold)*0.7)*+1
-    else:
-      m = m + m_
-  m = m / n
-  co = maptbx.connectivity(map_data=m, threshold=signal_threshold)
-  m = m*co.volume_cutoff_mask(volume_cutoff=average_peak_volume).as_double()
-  m = maptbx.volume_scale(map = m,  n_bins = 10000).map_data()
   return m
+
+def fem_loop_(
+      fmodel,
+      mc,
+      n,
+      crystal_gridding,
+      n_cycles,
+      signal_threshold,
+      weighted_average_manager,
+      missing_reflections_manager,
+      b_sharp,
+      complete_set,
+      progress_counter=None,
+      cut_by_connectivity=True):
+  m = None
+  average_peak_volume = None
+  average_peak_volume_he = None
+  for it in xrange(n):
+    if(progress_counter is not None): progress_counter.show()
+    m_ = one_iteration(
+      fmodel           = fmodel,
+      mc               = mc,
+      n_cycles         = n_cycles,
+      crystal_gridding = crystal_gridding,
+      signal_threshold = signal_threshold,
+      b_sharp=b_sharp,
+      missing_reflections_manager=missing_reflections_manager,
+      weighted_average_manager = weighted_average_manager)
+    if(m is None): m = m_
+    else:          m = m + m_
+  m = m / n
+  #
+  if(0):
+    average_peak_volume = int(maptbx.peak_volume_estimate(
+      map_data         = m,
+      sites_cart       = fmodel.xray_structure.sites_cart(),
+      crystal_symmetry = fmodel.xray_structure.crystal_symmetry(),
+      cutoff           = signal_threshold)*0.7)
+    co = maptbx.connectivity(map_data=m, threshold=signal_threshold)
+    m = m*co.volume_cutoff_mask(volume_cutoff=average_peak_volume).as_double()
+    m = maptbx.volume_scale(map = m,  n_bins = 10000).map_data()
+  else:
+  #
+    if(cut_by_connectivity):
+      msk = truncate_with_roots2(
+        m=m,fmodel=fmodel,c1=0.5,c2=0.25,cutoff=0.5,scale=0.7, as_int=True)
+      maptbx.truncate_special(mask=msk, map_data=m)
+      #
+      F = None
+      for c1 in [0.7,0.6,]:
+        f = truncate_with_roots2(m=m,fmodel=fmodel,c1=c1,c2=c1-0.1,
+          cutoff=c1,scale=0.7, keep_interblob=True)
+        if(F is None): F = f
+        else: F = F * f
+      m = m * F
+      #
+    m = maptbx.volume_scale(map = m,  n_bins = 10000).map_data()
+    #if(random.choice([True, False, False])):
+    if(random.choice([True, False])):
+      F = None
+      keep_interblob=True
+      for c1 in [0.9, 0.85, 0.8, 0.7]:
+        if(c1<0.9): keep_interblob=False
+        f = truncate_with_roots2(m=m,fmodel=fmodel,c1=c1,c2=c1-0.1,
+          cutoff=c1,scale=1, keep_interblob=keep_interblob)
+        if(F is None): F = f
+        else: F = F * f
+      m = m * F
+  return m
+
+def truncate_with_roots(m, fmodel, c1, c2, cutoff, scale, keep_interblob=False):
+  assert c1>=c2
+  average_peak_volume = int(maptbx.peak_volume_estimate(
+    map_data         = m,
+    sites_cart       = fmodel.xray_structure.sites_cart(),
+    crystal_symmetry = fmodel.xray_structure.crystal_symmetry(),
+    cutoff           = cutoff)*scale)
+  co1 = maptbx.connectivity(map_data=m, threshold=c1)
+  co2 = maptbx.connectivity(map_data=m, threshold=c2)
+  res_mask = co2.noise_elimination_two_cutoffs(connectivity_t1=co1,
+    volume_threshold_t1=average_peak_volume, keep_interblob=keep_interblob).as_double()
+  return m*res_mask
+
+def truncate_with_roots2(m, fmodel, c1, c2, cutoff, scale, keep_interblob=False, as_int=False):
+  assert c1>=c2
+  average_peak_volume = int(maptbx.peak_volume_estimate(
+    map_data         = m,
+    sites_cart       = fmodel.xray_structure.sites_cart(),
+    crystal_symmetry = fmodel.xray_structure.crystal_symmetry(),
+    cutoff           = cutoff)*scale)
+  co1 = maptbx.connectivity(map_data=m, threshold=c1)
+  co2 = maptbx.connectivity(map_data=m, threshold=c2)
+  result = co2.noise_elimination_two_cutoffs(connectivity_t1=co1,
+    volume_threshold_t1=average_peak_volume, keep_interblob=keep_interblob)
+  if(as_int): return result
+  else:       return result.as_double()
+
+def get_b_sharp(fmodel, mc, n_cycles, crystal_gridding, signal_threshold,
+      weighted_average_manager, missing_reflections_manager):
+  result = flex.double()
+  #return None
+  for i in xrange(1):
+    mc_wa = weighted_average_manager.random_weight_averaged_map_coefficients(
+      random_scale  = 5.,
+      random_seed   = int(random.random()*10000000),
+      n_cycles      = n_cycles,
+      missing       = missing_reflections_manager.get_missing_fast(),
+      fraction_keep = 0.95)
+    m = get_map(mc=mc_wa, cg=crystal_gridding)
+    maptbx.reset(
+      data                   = m,
+      substitute_value       = 0.0,
+      less_than_threshold    = signal_threshold,
+      greater_than_threshold = -9999,
+      use_and                = True)
+    tmp = mc_wa.structure_factors_from_map(
+      map            = m,
+      use_scale      = True,
+      anomalous_flag = False,
+      use_sg         = False)
+    b =  mmtbx.maps.b_factor_sharpening_by_map_kurtosis_maximization(
+      map_coeffs=tmp, show=False, b_only=True)
+    result.append(b)
+  return flex.min(result)
