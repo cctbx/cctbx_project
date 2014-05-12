aa14e943b503b533b6bc88d93cdf67b88aa48ae4
Oliver Zeldin <oliver.zeldin@gmail.com>
2014-05-12 19:00:13 +0000

    cleanup

--- xfel/command_line/cxi_stream_to_pickle.py
+++ xfel/command_line/cxi_stream_to_pickle.py
@@ -14,31 +14,31 @@ import argparse
 parser = argparse.ArgumentParser(description=
                                  ('Create indexing pickles from a'
                                   'crystfel stream file.'))
-parser.add_argument('filename',  type=str, nargs=1,
+parser.add_argument('filename', type=str, nargs=1,
                     help='The filename of the stream file to be converted.')
-parser.add_argument('point_group',  type=str, nargs=1,
+parser.add_argument('point_group', type=str, nargs=1,
                     help='The space group to be assigned.')
-parser.add_argument('--tag',  type=str, nargs=1,
+parser.add_argument('--tag', type=str, nargs=1,
                     help="Prefix to be used for the indexing pickles.")
 args = parser.parse_args()
-file = args.filename[0]
+stream_file = args.filename[0]
 point_group = args.point_group[0]
 if args.tag:
     tag = args.tag[0]
 else:
-    tag = file[0].split('.')[0]
+    tag = stream_file[0].split('.')[0]
 
 
 EV_PER_A = 12398.4187
 # Regular expressions, set up so one can use groups to extract the data
 re_energy = re.compile("photon_energy_eV\s=\s([0-9]+\.[0-9]+)")
-re_uc     = re.compile("""Cell\sparameters\s
-                       ([0-9]+\.[0-9]+)\s # a
-                       ([0-9]+\.[0-9]+)\s # b
-                       ([0-9]+\.[0-9]+)\snm,\s # c
-                       ([0-9]+\.[0-9]+)\s # alpha
-                       ([0-9]+\.[0-9]+)\s # beta
-                       ([0-9]+\.[0-9]+) # gamma""", re.X)
+re_uc = re.compile("""Cell\sparameters\s
+                   ([0-9]+\.[0-9]+)\s # a
+                   ([0-9]+\.[0-9]+)\s # b
+                   ([0-9]+\.[0-9]+)\snm,\s # c
+                   ([0-9]+\.[0-9]+)\s # alpha
+                   ([0-9]+\.[0-9]+)\s # beta
+                   ([0-9]+\.[0-9]+) # gamma""", re.X)
 
 # groups 1-7: h,k,l,I,sig(i),peak, background,fs,ss
 re_miller = re.compile("""\s*(-?[0-9]{1,3})
@@ -50,7 +50,7 @@ re_miller = re.compile("""\s*(-?[0-9]{1,3})
                           \s*([0-9]+\.[0-9]+)
                           \s*([0-9]+\.[0-9]+) #fs, ssi""", re.X)
 re_lattice_type = re.compile("lattice_type\s=\s([a-zA-Z]+)")
-re_centering    = re.compile("centering\s=\s([A-Z])")
+re_centering = re.compile("centering\s=\s([A-Z])")
 
 # note the setting lattice is in nm^-1
 re_Astar = re.compile("""astar\s=\s*(-?\+?[0-9]+\.[0-9]+)
@@ -65,7 +65,8 @@ re_Cstar = re.compile("""cstar\s=\s*(-?\+?[0-9]+\.[0-9]+)
 
 
 def image_template():
-    return { 'Millers': [], 'Is': [], 'sigIs': [], 'location': []}
+    return {'Millers': [], 'Is': [], 'sigIs': [], 'location': []}
+
 
 def check_image(image):
   if things_in_image == set(image):
@@ -82,10 +83,10 @@ def crystFEL_to_CCTBX_coord_system(abasis, bbasis, cbasis):
   from scitbx.matrix import sqr
   from cctbx import crystal_orientation
 
-  aMat = sqr((abasis[0],bbasis[0],cbasis[0],
-              abasis[1],bbasis[1],cbasis[1],
-              abasis[2],bbasis[2],cbasis[2]))
-  ori = crystal_orientation.crystal_orientation(aMat, crystal_orientation.basis_type.reciprocal)
+  a_mat = sqr((abasis[0], bbasis[0], cbasis[0],
+              abasis[1], bbasis[1], cbasis[1],
+              abasis[2], bbasis[2], cbasis[2]))
+  ori = crystal_orientation.crystal_orientation(a_mat, crystal_orientation.basis_type.reciprocal)
   return ori
 
 
@@ -131,7 +132,7 @@ if __name__ == "__main__":
   things_in_image = {'Millers', 'Is', 'sigIs', 'unit cell', 'aStar', 'bStar',
                      'cStar', 'wavelength', 'centering', 'lattice_type',
                      'location'}
-  with open(file, "r+") as stream:
+  with open(stream_file, "r+") as stream:
     count = 1
     this_image = image_template()
     for line in stream:
@@ -148,22 +149,22 @@ if __name__ == "__main__":
 
       uc = re_uc.match(line)
       if uc:  # Start of a new crystal dictionary
-        if check_image(this_image): # i.e. it's a complete image dictionairy
+        if check_image(this_image):  # i.e. it's a complete image dictionairy
           make_int_pickle(this_image, "{}_{:04d}.pickle".format(tag, count))
           count += 1
           this_image = image_template()
-          this_image["unit cell"] = unit_cell((float(uc.group(1))*10, # nm to A
-                                               float(uc.group(2))*10,
-                                               float(uc.group(3))*10,
+          this_image["unit cell"] = unit_cell((float(uc.group(1)) * 10,  # nm to A
+                                               float(uc.group(2)) * 10,
+                                               float(uc.group(3)) * 10,
                                                float(uc.group(4)),
                                                float(uc.group(5)),
                                                float(uc.group(6))))
 
         else:
           this_image = image_template()
-          this_image["unit cell"] = unit_cell((float(uc.group(1))*10, # nm to A
-                                               float(uc.group(2))*10,
-                                               float(uc.group(3))*10,
+          this_image["unit cell"] = unit_cell((float(uc.group(1)) * 10,  # nm to A
+                                               float(uc.group(2)) * 10,
+                                               float(uc.group(3)) * 10,
                                                float(uc.group(4)),
                                                float(uc.group(5)),
                                                float(uc.group(6))))
@@ -171,7 +172,7 @@ if __name__ == "__main__":
 
       energy = re_energy.match(line)
       if energy:
-        this_image['wavelength'] = float(energy.group(1))/EV_PER_A
+        this_image['wavelength'] = float(energy.group(1)) / EV_PER_A
         continue
 
       lattice = re_lattice_type.match(line)
@@ -186,23 +187,23 @@ if __name__ == "__main__":
 
       astar = re_Astar.match(line)
       if astar:
-        this_image['aStar'] = [float(astar.group(1))/10,
-                               float(astar.group(2))/10,
-                               float(astar.group(3))/10]
+        this_image['aStar'] = [float(astar.group(1)) / 10,
+                               float(astar.group(2)) / 10,
+                               float(astar.group(3)) / 10]
         continue
 
       bstar = re_Bstar.match(line)
       if bstar:
-        this_image['bStar'] = [float(bstar.group(1))/10,
-                               float(bstar.group(2))/10,
-                               float(bstar.group(3))/10]
+        this_image['bStar'] = [float(bstar.group(1)) / 10,
+                               float(bstar.group(2)) / 10,
+                               float(bstar.group(3)) / 10]
         continue
 
       cstar = re_Cstar.match(line)
       if cstar:
-        this_image['cStar'] =[float(cstar.group(1))/10,
-                              float(cstar.group(2))/10,
-                              float(cstar.group(3))/10]
+        this_image['cStar'] = [float(cstar.group(1)) / 10,
+                               float(cstar.group(2)) / 10,
+                               float(cstar.group(3)) / 10]
 
         continue
 # After this for loop, we should have an array of dictionairies,
