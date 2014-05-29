5941afcd13ff735d304506984f0d2eaad243c2a7
Nathaniel Echols <nathaniel.echols@gmail.com>
2014-05-29 04:05:46 +0000

    docstrings and a few more functions for xtriage output

--- mmtbx/scaling/__init__.py
+++ mmtbx/scaling/__init__.py
@@ -1,3 +1,10 @@
+
+"""
+Base module for Xtriage and related scaling functionality; this imports the
+Boost.Python extensions into the local namespace, and provides core functions
+for displaying the results of Xtriage.
+"""
+
 from __future__ import division
 import cctbx.array_family.flex # import dependency
 from libtbx.str_utils import make_sub_header, make_header
@@ -17,34 +24,74 @@ class xtriage_output (slots_getstate_setstate) :
   """
   Base class for generic output wrappers.
   """
-  def show_header (self, text) :
+  def show_header (self, title) :
+    """
+    Start a new section with the specified title.
+    """
     raise NotImplementedError()
 
   def show_text (self, text) :
+    """
+    Show unformatted text.
+    """
     raise NotImplementedError()
 
   def show (self, text) :
     return self.show_text(text)
 
   def show_preformatted_text (self, text) :
+    """
+    Show text with spaces and line breaks preserved; in some contexts this
+    will be done using a monospaced font.
+    """
+    raise NotImplementedError()
+
+  def show_lines (self, text) :
+    """
+    Show partially formatted text, preserving paragraph breaks.
+    """
+    raise NotImplementedError()
+
+  def show_paragraph_header (self, text) :
+    """
+    Show a header/title for a paragraph or small block of text.
+    """
     raise NotImplementedError()
 
   def show_table (self, table, precision=6, indent=0) :
+    """
+    Display a formatted table.
+    """
     raise NotImplementedError()
 
   def show_plot (self, plot) :
+    """
+    Display a plot, if supported by the given output class.
+    """
     raise NotImplementedError()
 
   def newline (self) :
+    """
+    Print a newline and nothing else.
+    """
     raise NotImplementedError()
 
   def write (self, text) :
+    """
+    Support for generic filehandle methods.
+    """
     self.show(text)
 
   def flush (self) :
+    """
+    Support for generic filehandle methods.
+    """
     pass
 
 class printed_output (xtriage_output) :
+  """
+  Output class for displaying raw text with minimal formatting.
+  """
   __slots__ = ["out"]
   def __init__ (self, out) :
     assert hasattr(out, "write") and hasattr(out, "flush")
@@ -59,7 +106,13 @@ class printed_output (xtriage_output) :
   def show_text (self, text) :
     print >> self.out, text
 
+  def show_paragraph_header (self, text) :
+    print >> self.out, text + ":"
+
   def show_preformatted_text (self, text) :
+    print >> self.out, text
+
+  def show_lines (self, text) :
     print >> self.out, text
 
   def show_table (self, table, precision=6, indent=0) :
@@ -75,6 +128,12 @@ class printed_output (xtriage_output) :
     self.out.write(text)
 
 class xtriage_analysis (object) :
+  """
+  Base class for analyses performed by Xtriage.  This does not impose any
+  restrictions on content or functionality, but simply provides a show()
+  method suitable for either filehandle-like objects or objects derived from
+  the xtriage_output class.  Child classes should implement _show_impl.
+  """
   def show (self, out=None) :
     if out is None:
       out=sys.stdout
