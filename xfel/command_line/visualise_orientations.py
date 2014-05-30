ffb7c87928ccdefbb7342493ed3acb54fb71bc94
Oliver Zeldin <oliver.zeldin@gmail.com>
2014-05-30 00:35:18 +0000

    command line utility for visualising crystal orientations in a serial dataset

---
+++ xfel/command_line/visualise_orientations.py
@@ -0,0 +1,28 @@
+# LIBTBX_SET_DISPATCHER_NAME cluster.visualize_orientations
+from __future__ import division
+__author__ = 'zeldin'
+#from libtbx.utils import multi_out
+
+
+def run(_args):
+  if _args < 2:
+    raise IOError("Must give at least one path to folder of pickles")
+  import logging
+  from xfel.clustering.cluster import Cluster
+  FORMAT = '%(levelname)s %(module)s.%(funcName)s: %(message)s'
+  logging.basicConfig(level=logging.WARNING, format=FORMAT)
+
+  cluster = Cluster.from_directories(_args.folders,
+                                          'Command line visualisation')
+  logging.info("data imported")
+  cluster.visualise_orientational_distribution()
+
+if __name__ == "__main__":
+  import argparse
+
+  parser = argparse.ArgumentParser(description=('Find the best target cell from'
+                                                'a set of indexing pickles.'))
+  parser.add_argument('folders', type=str, nargs='+',
+                      help='One or more folers containing integration pickles.')
+  args = parser.parse_args()
+  result = run(args)
