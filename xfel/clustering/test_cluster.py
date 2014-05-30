6caf3273c7524bac21ac2e090c2a27d5429f8539
Oliver Zeldin <oliver.zeldin@gmail.com>
2014-05-30 00:24:59 +0000

    added du

--- xfel/clustering/test_cluster.py
+++ xfel/clustering/test_cluster.py
@@ -1,21 +1,26 @@
 
 from __future__ import division
-from Cluster import Cluster
 
 import logging
+
+from xfel.clustering.Cluster import Cluster
+
 FORMAT = '%(levelname)s %(module)s.%(funcName)s: %(message)s'
-logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+logging.basicConfig(level=logging.INFO, format=FORMAT)
 """ A test cluster script for playing with the class """
-path_to_some_integration_pickles = r"/usr/local/Dropbox/Stanford_Postdoc/CODING/Fraser_test_data"
+path_to_some_integration_pickles = r"/Volumes/Data/XFEL/SFC_May_2014_XPP/" \
+                                   r"processed_data/SFC_CRYO/Int_3.7_LongCell"
 
-test_cluster = Cluster.from_directories([path_to_some_integration_pickles],
-                                        'test_script')
+all_longcell = Cluster.from_directories([path_to_some_integration_pickles],
+                                        'SFC_longcell')
 logging.info("data imported")
-clust = test_cluster.point_group_filer('P222')
-clust = clust.ab_cluster(80, plot=False)
-big_cluster = max(clust, key=lambda x: len(x.members))
-
-best_data = big_cluster.total_intensity_filter(res=8, completeness_threshold=0.5, plot=False)
-
-print best_data.info
-best_data.dump_file_list()
+#clust = test_cluster.point_group_filter('P222')
+all_longcell.visualise_orientational_distribution()
+#big_cluster = all_longcell.ab_cluster(5000, plot=False, max_only=True)
+#
+#best_data = big_cluster.total_intensity_filter(res=3.7,
+#                                               completeness_threshold=0.95,
+#                                               plot=True)
+#
+#print big_cluster.info
+#big_cluster.dump_file_list()
