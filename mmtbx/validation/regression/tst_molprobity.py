b2f5d10f2c456f3114522a5140812570aa157eeb
Pavel Afonine <pafonine@users.noreply.github.com>
2014-06-11 23:57:43 +0000

    adjust to new mvd database (not sure!)

--- mmtbx/validation/regression/tst_molprobity.py
+++ mmtbx/validation/regression/tst_molprobity.py
@@ -39,7 +39,8 @@ def exercise_protein () :
   # percentiles
   out4 = StringIO()
   result.show_summary(out=out4, show_percentiles=True)
-  assert ("""  Clashscore            =  49.96 (percentile: 1.0)""" in
+  print out4.getvalue()
+  assert ("""  Clashscore            =  49.96 (percentile: 0.2)""" in
     out4.getvalue())
   # misc
   assert approx_equal(result.r_work(), 0.237) # from PDB header
