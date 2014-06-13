09e6f975bcacd750415f8f8a7e3813d279492a4f
Nathaniel Echols <nathaniel.echols@gmail.com>
2014-06-13 19:44:48 +0000

    bug fixes

--- mmtbx/regression/tst_real_space_correlation.py
+++ mmtbx/regression/tst_real_space_correlation.py
@@ -1,5 +1,10 @@
 
 from __future__ import division
+from mmtbx import real_space_correlation
+import mmtbx.utils
+from iotbx import file_reader
+import iotbx.pdb.hierarchy
+from scitbx.array_family import flex
 import libtbx.load_env
 from libtbx.test_utils import approx_equal
 from libtbx.utils import null_out
@@ -16,10 +21,6 @@ def exercise_1():
   if (None in [pdb_file, mtz_file]) :
     print "phenix_regression not found, skipping test"
     return False
-  from mmtbx import real_space_correlation
-  import mmtbx.utils
-  from iotbx import file_reader
-  from scitbx.array_family import flex
   pdb_in = file_reader.any_file(pdb_file)
   hierarchy = pdb_in.file_object.construct_hierarchy()
   hierarchy.atoms().reset_i_seq()
@@ -64,6 +65,87 @@ def exercise_1():
   assert (results[0].id_str == " A   GLY    1    N  ")
   return True
 
+# test with synthetic twinned data
+def exercise_2 () :
+  pdb_str = """\
+CRYST1   12.000    8.000   12.000  90.02  89.96  90.05 P 1           1
+ATOM     39  N   ASN A   6       5.514   2.664   4.856  1.00 11.99           N
+ATOM     40  CA  ASN A   6       6.831   2.310   4.318  1.00 12.30           C
+ATOM     41  C   ASN A   6       7.854   2.761   5.324  1.00 13.40           C
+ATOM     42  O   ASN A   6       8.219   3.943   5.374  1.00 13.92           O
+ATOM     43  CB  ASN A   6       7.065   3.016   2.993  1.00 12.13           C
+ATOM     44  CG  ASN A   6       5.961   2.735   2.003  1.00 12.77           C
+ATOM     45  OD1 ASN A   6       5.798   1.604   1.551  1.00 14.27           O
+ATOM     46  ND2 ASN A   6       5.195   3.747   1.679  1.00 10.07           N
+ATOM     47  N   TYR A   7       8.292   1.817   6.147  1.00 14.70           N
+ATOM     48  CA  TYR A   7       9.159   2.144   7.299  1.00 15.18           C
+ATOM     49  C   TYR A   7      10.603   2.331   6.885  1.00 15.91           C
+ATOM     50  O   TYR A   7      11.041   1.811   5.855  1.00 15.76           O
+ATOM     51  CB  TYR A   7       9.061   1.065   8.369  1.00 15.35           C
+ATOM     52  CG  TYR A   7       7.665   0.929   8.902  1.00 14.45           C
+ATOM     53  CD1 TYR A   7       6.771   0.021   8.327  1.00 15.68           C
+ATOM     54  CD2 TYR A   7       7.210   1.756   9.920  1.00 14.80           C
+ATOM     55  CE1 TYR A   7       5.480  -0.094   8.796  1.00 13.46           C
+ATOM     56  CE2 TYR A   7       5.904   1.649  10.416  1.00 14.33           C
+ATOM     57  CZ  TYR A   7       5.047   0.729   9.831  1.00 15.09           C
+ATOM     58  OH  TYR A   7       3.766   0.589  10.291  1.00 14.39           O
+ATOM     59  OXT TYR A   7      11.358   2.999   7.612  1.00 17.49           O
+TER
+ATOM      1  N   ASN B   6       1.414   5.113   6.019  1.00 12.99           N
+ATOM      2  CA  ASN B   6       2.720   4.776   5.445  1.00 13.30           C
+ATOM      3  C   ASN B   6       3.763   5.209   6.438  1.00 14.40           C
+ATOM      4  O   ASN B   6       4.125   6.391   6.507  1.00 14.92           O
+ATOM      5  CB  ASN B   6       2.922   5.513   4.131  1.00 13.13           C
+ATOM      6  CG  ASN B   6       1.798   5.250   3.160  1.00 13.77           C
+ATOM      7  OD1 ASN B   6       1.629   4.129   2.686  1.00 15.27           O
+ATOM      8  ND2 ASN B   6       1.022   6.266   2.875  1.00 11.07           N
+ATOM      9  N   TYR B   7       4.222   4.248   7.230  1.00 15.70           N
+ATOM     10  CA  TYR B   7       5.113   4.552   8.370  1.00 16.18           C
+ATOM     11  C   TYR B   7       6.547   4.754   7.929  1.00 16.91           C
+ATOM     12  O   TYR B   7       6.964   4.259   6.878  1.00 16.76           O
+ATOM     13  CB  TYR B   7       5.042   3.449   9.417  1.00 16.35           C
+ATOM     14  CG  TYR B   7       3.659   3.296   9.977  1.00 15.45           C
+ATOM     15  CD1 TYR B   7       2.756   2.398   9.402  1.00 16.68           C
+ATOM     16  CD2 TYR B   7       3.224   4.098  11.023  1.00 15.80           C
+ATOM     17  CE1 TYR B   7       1.476   2.267   9.896  1.00 14.46           C
+ATOM     18  CE2 TYR B   7       1.929   3.975  11.545  1.00 15.33           C
+ATOM     19  CZ  TYR B   7       1.063   3.065  10.959  1.00 16.09           C
+ATOM     20  OH  TYR B   7      -0.207   2.910  11.443  1.00 15.39           O
+ATOM     21  OXT TYR B   7       7.316   5.408   8.654  1.00 18.49           O
+END
+"""
+  pdb_in = iotbx.pdb.hierarchy.input(pdb_string=pdb_str)
+  xrs = pdb_in.input.xray_structure_simple()
+  fc = abs(xrs.structure_factors(d_min=1.5).f_calc())
+  fc = fc.set_observation_type_xray_amplitude()
+  sigf = flex.double(fc.size(), 0.1) + (fc.data() * 0.03)
+  fc = fc.customized_copy(sigmas=sigf)
+  # and now add twinning
+  fc_twin = fc.twin_data(twin_law='-l,-k,-h', alpha=0.4)
+  flags = fc_twin.generate_r_free_flags(use_lattice_symmetry=True)
+  fmodel = mmtbx.utils.fmodel_simple(
+    f_obs=fc_twin,
+    update_f_part1_for=None,
+    r_free_flags=flags,
+    xray_structures=[xrs],
+    scattering_table="n_gaussian")
+  assert (fmodel.twin_law is not None)
+  map_stats = real_space_correlation.extract_map_stats_for_single_atoms(
+    xray_structure=xrs,
+    pdb_atoms=pdb_in.hierarchy.atoms(),
+    fmodel=fmodel)
+  sel_cache = pdb_in.hierarchy.atom_selection_cache()
+  sel = sel_cache.selection("chain B")
+  map_stats_2 = real_space_correlation.extract_map_stats_for_single_atoms(
+    xray_structure=xrs,
+    pdb_atoms=pdb_in.hierarchy.atoms(),
+    fmodel=fmodel,
+    selection=sel)
+  map_stats_3 = real_space_correlation.map_statistics_for_atom_selection(
+    atom_selection=sel,
+    fmodel=fmodel)
+
 if (__name__ == "__main__") :
-  if (exercise_1()) :
-    print "OK"
+  exercise_1()
+  exercise_2()
+  print "OK"
--- mmtbx/validation/regression/tst_rna_validate.py
+++ mmtbx/validation/regression/tst_rna_validate.py
@@ -1,31 +1,96 @@
-#(jEdit options) :folding=explicit:collapseFolds=1:
 
 from __future__ import division
+from mmtbx.command_line import rna_validate
+import mmtbx.validation.rna_validate
+from iotbx import file_reader
+import iotbx.pdb.hierarchy
+from libtbx.easy_pickle import loads, dumps
 from libtbx.utils import null_out
 import libtbx.load_env
+from cStringIO import StringIO
 import sys, os
 
-#{{{ exercise_rna_validate
 def exercise_rna_validate():
+  # fragment from 3g8t
+  pdb_raw = """\
+ATOM   7975  P     G Q 140      10.347 137.422  73.792  1.00118.69           P
+ATOM   7976  OP1   G Q 140       9.348 138.439  74.195  1.00118.63           O
+ATOM   7977  OP2   G Q 140      11.208 137.681  72.617  1.00118.60           O
+ATOM   7978  O5'   G Q 140      11.286 137.083  75.051  1.00119.34           O
+ATOM   7979  C5'   G Q 140      11.015 137.599  76.358  1.00120.43           C
+ATOM   7980  C4'   G Q 140      11.617 136.733  77.454  1.00121.27           C
+ATOM   7981  O4'   G Q 140      11.723 135.361  77.003  1.00121.51           O
+ATOM   7982  C3'   G Q 140      13.038 137.080  77.885  1.00121.72           C
+ATOM   7983  O3'   G Q 140      13.050 138.143  78.836  1.00122.07           O
+ATOM   7984  C2'   G Q 140      13.517 135.770  78.511  1.00121.87           C
+ATOM   7985  O2'   G Q 140      13.160 135.640  79.877  1.00121.91           O
+ATOM   7986  C1'   G Q 140      12.793 134.717  77.671  1.00121.84           C
+ATOM   7987  N9    G Q 140      13.651 134.055  76.684  1.00121.95           N
+ATOM   7988  C8    G Q 140      14.226 134.620  75.568  1.00121.96           C
+ATOM   7989  N7    G Q 140      14.944 133.787  74.870  1.00121.97           N
+ATOM   7990  C5    G Q 140      14.843 132.588  75.563  1.00121.99           C
+ATOM   7991  C6    G Q 140      15.412 131.320  75.281  1.00122.03           C
+ATOM   7992  O6    G Q 140      16.142 131.004  74.331  1.00121.99           O
+ATOM   7993  N1    G Q 140      15.064 130.361  76.235  1.00122.12           N
+ATOM   7994  C2    G Q 140      14.264 130.602  77.331  1.00122.14           C
+ATOM   7995  N2    G Q 140      14.033 129.557  78.139  1.00122.06           N
+ATOM   7996  N3    G Q 140      13.721 131.787  77.604  1.00122.09           N
+ATOM   7997  C4    G Q 140      14.050 132.734  76.684  1.00122.00           C
+ATOM   7998  P     A Q 141      14.015 139.407  78.645  1.00122.32           P
+ATOM   7999  OP1   A Q 141      13.141 140.571  78.383  1.00122.35           O
+ATOM   8000  OP2   A Q 141      15.073 139.069  77.665  1.00122.37           O
+ATOM   8001  O5'   A Q 141      14.690 139.587  80.088  1.00122.53           O
+ATOM   8002  C5'   A Q 141      15.914 138.934  80.438  1.00122.90           C
+ATOM   8003  C4'   A Q 141      15.673 137.727  81.331  1.00123.23           C
+ATOM   8004  O4'   A Q 141      16.899 137.378  82.025  1.00123.39           O
+ATOM   8005  C3'   A Q 141      14.605 137.909  82.412  1.00123.36           C
+ATOM   8006  O3'   A Q 141      13.551 136.969  82.215  1.00123.31           O
+ATOM   8007  C2'   A Q 141      15.347 137.686  83.732  1.00123.49           C
+ATOM   8008  O2'   A Q 141      14.582 136.976  84.689  1.00123.46           O
+ATOM   8009  C1'   A Q 141      16.551 136.857  83.291  1.00123.61           C
+ATOM   8010  N9    A Q 141      17.714 136.912  84.186  1.00123.79           N
+ATOM   8011  C8    A Q 141      17.797 137.531  85.406  1.00123.80           C
+ATOM   8012  N7    A Q 141      18.964 137.415  85.992  1.00123.83           N
+ATOM   8013  C5    A Q 141      19.706 136.662  85.099  1.00123.90           C
+ATOM   8014  C6    A Q 141      21.037 136.187  85.130  1.00123.95           C
+ATOM   8015  N6    A Q 141      21.875 136.421  86.146  1.00123.97           N
+ATOM   8016  N1    A Q 141      21.476 135.460  84.076  1.00123.93           N
+ATOM   8017  C2    A Q 141      20.634 135.225  83.057  1.00123.90           C
+ATOM   8018  N3    A Q 141      19.364 135.620  82.917  1.00123.84           N
+ATOM   8019  C4    A Q 141      18.953 136.340  83.979  1.00123.84           C
+TER    8020        A Q 141
+"""
+  open("tst_rna_validate_1.pdb", "w").write(pdb_raw)
+  rv = rna_validate.run(args=["tst_rna_validate_1.pdb"], out=null_out())
+  assert len(rv.puckers.results) == 1
+  pickle_unpickle(rv)
+  # derived from 2goz
   regression_pdb = libtbx.env.find_in_repositories(
     relative_path="phenix_regression/pdb/pdb2goz_refmac_tls.ent",
     test=os.path.isfile)
   if (regression_pdb is None):
     print "Skipping exercise_regression(): input pdb (pdb2goz_refmac_tls.ent) not available"
     return
-  from mmtbx.command_line import rna_validate
   rv = rna_validate.run(args=[regression_pdb], out=null_out())
   assert len(rv.puckers.results) == 2
   assert len(rv.bonds.results) == 2
   assert len(rv.angles.results) == 14
   assert len(rv.suites.results) == 4
-  from iotbx.file_reader import any_file
-  pdb_in = any_file(regression_pdb)
-  import mmtbx.validation.rna_validate
+  pickle_unpickle(rv)
+  pdb_in = file_reader.any_file(regression_pdb)
   result = mmtbx.validation.rna_validate.rna_validation(
     pdb_hierarchy=pdb_in.file_object.construct_hierarchy(),
     geometry_restraints_manager=None,
     params=None)
+  pickle_unpickle(result)
+
+def pickle_unpickle (result) :
+  result2 = loads(dumps(result))
+  out1 = StringIO()
+  out2 = StringIO()
+  result.show(out=out1)
+  result2.show(out=out2)
+  assert (out1.getvalue() == out2.getvalue())
 
 def run():
   verbose = "--verbose" in sys.argv[1:]
