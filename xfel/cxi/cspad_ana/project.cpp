0a0679ae8fcf8c17f0e311e74d6269fc9471b57d
Muhamed Amin <mamin03@users.noreply.github.com>
2014-06-15 00:57:59 +0000

    Filter the two color spectra

--- xfel/cxi/cspad_ana/project.cpp
+++ xfel/cxi/cspad_ana/project.cpp
@@ -8,6 +8,7 @@ compute_projection(const flex_double& pixels,
                    const float sin,
                    const float cos)
 {
+//    using namespace std;
     int x=0; int y=0;
     float raw_data[limit][limit];
     shared_double oneD(limit,0);
@@ -19,11 +20,20 @@ compute_projection(const flex_double& pixels,
            y=int(round(-j*sin+i*cos))+int(round(sin*1023));
            raw_data[y][x]=pixels(i,j);
          }
+    int count=0;
     for (int i=0; i<limit; i++)
+      {
+       count=0;
        for (int j=0; j<limit; j++)
          {
+          if(raw_data[i][j]!=0)
+           {
            oneD[i]=oneD[i]+raw_data[i][j];
+           count++;
+           }
          }
-    return    boost::python::object(oneD);
+      if(count>0) oneD[i]=oneD[i]/count;
+      }
+         return  boost::python::object(oneD);
 
 }
