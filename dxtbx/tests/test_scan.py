713c310fecefb5ee763ad10413bb81243f1899b4
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-06-30 14:06:01 +0000

    add test case where image_to_index currently returns bogus answer (8): graeme to fix regex :)

--- dxtbx/tests/test_scan.py
+++ dxtbx/tests/test_scan.py
@@ -41,6 +41,8 @@ def work_helper_image_files():
   assert(scan_helper_image_files.image_to_template(
       'image_001.dat') == 'image_###.dat')
 
+  assert scan_helper_image_files.image_to_index("image_6.8kev_1_001.cbf") == 1
+
   return
 
 def work_helper_image_formats():
