b550b683f60a21eecfe04eae47b519ba03c1e0bf
Nigel W Moriarty <nwmoriarty@lbl.gov>
2014-08-05 22:13:11 +0000

    fixed dumb bug

--- mmtbx/command_line/afitt.py
+++ mmtbx/command_line/afitt.py
@@ -5,4 +5,11 @@ import sys
 from mmtbx.geometry_restraints import afitt
 
 if __name__ == "__main__":
-  afitt.run(args=sys.argv[1:])
+  print sys.argv
+  if len(sys.argv[1:])<3:
+    print '''
+  Usage:
+    mmtbx.afitt model_file_name restraints_file_name ligand_code
+    '''
+  else:
+    afitt.run2()
