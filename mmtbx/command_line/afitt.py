52e59af187c289e5a6eebcbf245899730f2314b3
Nigel W Moriarty <nwmoriarty@users.noreply.github.com>
2014-08-07 19:00:58 +0000

    better help message

--- mmtbx/command_line/afitt.py
+++ mmtbx/command_line/afitt.py
@@ -5,11 +5,18 @@ import sys
 from mmtbx.geometry_restraints import afitt
 
 if __name__ == "__main__":
-  print sys.argv
   if len(sys.argv[1:])<3:
     print '''
-  Usage:
-    mmtbx.afitt model_file_name restraints_file_name ligand_code
+usage: afitt.py [-h] [-ff FF] pdb_file cif_file ligand_names
+
+positional arguments:
+  pdb_file      pdb file
+  cif_file      cif file
+  ligand_names  3-letter ligand names separated by commas
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -ff FF        afitt theory: mmff94, mmff94s pm3 or am1
     '''
   else:
     afitt.run2()
