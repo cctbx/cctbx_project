78742e472abf2c967db7482d67a18a55a6ae9908
Aaron S. Brewster <asbrewster@lbl.gov>
2014-08-20 13:36:06 +0000

    Two utilities for printing info related to cctbx.xfel
    cxi.parameters: print all the default parmaters for cctbx.xfel phil files
    
    cxi.detector_format_versions: print out all known cctbx.xfel CSPAD detector
    format versions.  Or, given a detecor format version, print known quad and
    tile translations for that format version.

---
+++ xfel/command_line/cxi_parameters.py
@@ -0,0 +1,13 @@
+from __future__ import division
+#-*- Mode: Python; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 8 -*-
+#
+# LIBTBX_SET_DISPATCHER_NAME cxi.parameters
+#
+# Utility for printing out default parameters for cctbx.xfel
+#
+
+if __name__=='__main__':
+
+  from spotfinder.applications.xfel import cxi_phil
+  horizons_phil = cxi_phil.cxi_versioned_extract()
+  horizons_phil.persist.show()
