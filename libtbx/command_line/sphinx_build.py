fa8fcfd23f6cff588b45686773930775e44efbbc
David Waterman <david.waterman@stfc.ac.uk>
2014-08-20 14:11:47 +0000

    Works around crash while building documentation caused by import order of a scipy dependency

--- libtbx/command_line/sphinx_build.py
+++ libtbx/command_line/sphinx_build.py
@@ -1,6 +1,13 @@
 # LIBTBX_SET_DISPATCHER_NAME sphinx.build
 
 import sys
+try:
+  # try importing scipy.linalg before any cctbx modules, otherwise we
+  # sometimes get a segmentation fault/core dump if it is imported after.
+  # scipy.linalg is a dependency of e.g. xfel/clustering
+  import scipy.linalg # import dependency
+except ImportError:
+  pass
 
 if __name__ == '__main__':
     from sphinx import main
