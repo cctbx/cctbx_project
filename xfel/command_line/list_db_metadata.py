8ede313a1faab6044dc2233f42954428bfeb2320
Aaron S. Brewster <asbrewster@lbl.gov>
2014-08-21 22:29:39 +0000

    Catch bad import if logging not available

--- xfel/command_line/list_db_metadata.py
+++ xfel/command_line/list_db_metadata.py
@@ -3,7 +3,12 @@ from __future__ import division
 # LIBTBX_PRE_DISPATCHER_INCLUDE_SH export PHENIX_GUI_ENVIRONMENT=1
 # LIBTBX_PRE_DISPATCHER_INCLUDE_SH export BOOST_ADAPTBX_FPE_DEFAULT=1
 
-from cxi_xdr_xes.cftbx.cspad_ana import db as db
+from libtbx.utils import Sorry
 
 if (__name__ == "__main__") :
+  try:
+    from cxi_xdr_xes.cftbx.cspad_ana import db as db
+  except ImportError:
+    raise Sorry("Trial logging not supported for this installation. Conact the developers for access.")
+
   db.list_db_metadata()
