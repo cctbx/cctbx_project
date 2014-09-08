92788a8bb105691e2699ffb297a9760d7bae0cf8
Aaron S. Brewster <asbrewster@lbl.gov>
2014-09-08 23:36:22 +0000

    Demonstrate parsing of SLAC's new geometry format. Example usage: cxi.display_geometry <path_to_experiment>/calib/geometry/0-end.data

--- xfel/command_line/cxi_display_metrology.py
+++ xfel/command_line/cxi_display_metrology.py
@@ -43,27 +43,67 @@ if (__name__ == "__main__") :
   if os.path.isfile(params.metrology):
     from dxtbx.format.Registry import Registry
     from scitbx.matrix import col
-    reader = Registry.find(params.metrology)
-    img = reader(params.metrology)
+    try:
+      reader = Registry.find(params.metrology)
+    except IOError:
+      reader = None
 
-    detector = img.get_detector()
+    if reader is None:
+      # see if it's a SLAC geometry file
+      from PSCalib.GeometryAccess import GeometryAccess
+      try:
+        geometry = GeometryAccess(params.metrology)
+      except Exception, e:
+        raise Sorry("Can't parse this metrology file")
 
-    for i, panel in enumerate(detector):
-      size = panel.get_image_size()
-      p0 = col(panel.get_pixel_lab_coord((0,0)))
-      p1 = col(panel.get_pixel_lab_coord((size[0]-1,0)))
-      p2 = col(panel.get_pixel_lab_coord((size[0]-1,size[1]-1)))
-      p3 = col(panel.get_pixel_lab_coord((0,size[1]-1)))
+      # get pixel mappings to real space.
+      x, y, z = geometry.get_pixel_coords()
+      assert x.shape == y.shape == z.shape
+      assert len(x.shape) == 4
+      sensor_slow = x.shape[2]
+      sensor_fast = x.shape[3]
+      for quad_id in xrange(x.shape[0]):
+        for sensor_id in xrange(x.shape[1]):
+          p0 = col((x[quad_id,sensor_id,0,0]/1000,
+                    y[quad_id,sensor_id,0,0]/1000))
+          p1 = col((x[quad_id,sensor_id,sensor_slow-1,0]/1000,
+                    y[quad_id,sensor_id,sensor_slow-1,0]/1000))
+          p2 = col((x[quad_id,sensor_id,sensor_slow-1,sensor_fast-1]/1000,
+                    y[quad_id,sensor_id,sensor_slow-1,sensor_fast-1]/1000))
+          p3 = col((x[quad_id,sensor_id,0,sensor_fast-1]/1000,
+                    y[quad_id,sensor_id,0,sensor_fast-1]/1000))
 
-      v1 = p1-p0
-      v2 = p3-p0
-      vcen = ((v2/2) + (v1/2)) + p0
+          v1 = p1-p0
+          v2 = p3-p0
+          vcen = ((v2/2) + (v1/2)) + p0
 
-      ax.add_patch(Polygon((p0[0:2],p1[0:2],p2[0:2],p3[0:2]), closed=True, color='green', fill=False, hatch='/'))
-      ax.annotate(i, vcen[0:2])
+          ax.add_patch(Polygon((p0[0:2],p1[0:2],p2[0:2],p3[0:2]), closed=True, color='green', fill=False, hatch='/'))
+          ax.annotate("%d,%d"%(quad_id,sensor_id), vcen[0:2])
 
-    ax.set_xlim((-100, 100))
-    ax.set_ylim((-100, 100))
+      ax.set_xlim((-100, 100))
+      ax.set_ylim((-100, 100))
+
+    else:
+      img = reader(params.metrology)
+
+      detector = img.get_detector()
+
+      for i, panel in enumerate(detector):
+        size = panel.get_image_size()
+        p0 = col(panel.get_pixel_lab_coord((0,0)))
+        p1 = col(panel.get_pixel_lab_coord((size[0]-1,0)))
+        p2 = col(panel.get_pixel_lab_coord((size[0]-1,size[1]-1)))
+        p3 = col(panel.get_pixel_lab_coord((0,size[1]-1)))
+
+        v1 = p1-p0
+        v2 = p3-p0
+        vcen = ((v2/2) + (v1/2)) + p0
+
+        ax.add_patch(Polygon((p0[0:2],p1[0:2],p2[0:2],p3[0:2]), closed=True, color='green', fill=False, hatch='/'))
+        ax.annotate(i, vcen[0:2])
+
+        ax.set_xlim((-100, 100))
+        ax.set_ylim((-100, 100))
   else:
     active_areas = xpp_active_areas[params.metrology]['active_areas']
 
