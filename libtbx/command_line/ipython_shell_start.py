5acfbf035e19bec34af25536964d34e9e5860604
Oliver Zeldin <oliver.zeldin@gmail.com>
2014-09-09 01:24:37 +0000

    Changed so that it can accept command line args. e.d.
    libtbx.ipython notebook

--- libtbx/command_line/ipython_shell_start.py
+++ libtbx/command_line/ipython_shell_start.py
@@ -1,10 +1,11 @@
 from __future__ import division
-# LIBTBX_SET_DISPATCHER_NAME libtbx.ipython
+# LIBTBX_SET_DISPATCHER_NAME libtbx.zipyton
+import re
+import sys
 
-import IPython
-if (__name__ == "__main__"):
-  try:
-    IPython.Shell.start().mainloop()
-  except AttributeError:
-    from IPython import embed
-    embed()
+from IPython import start_ipython
+
+if __name__ == '__main__':
+      sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+      sys.exit(start_ipython())
+
