dac281daa91c9175db446d78720cb4407a0488da
Nathaniel Echols <nathaniel.echols@gmail.com>
2014-09-30 19:14:52 +0000

    more consistent behavior

--- iotbx/command_line/pdb_as_fasta.py
+++ iotbx/command_line/pdb_as_fasta.py
@@ -5,61 +5,58 @@ import sys, os, re
 
 def run(args=(), params=None, out=sys.stdout):
   import iotbx.pdb
+  import iotbx.phil
   if (params is None) :
-    for arg in args:
-      assert os.path.isfile(arg)
-      pdb_obj = iotbx.pdb.input(file_name=arg)
-      hierarchy = pdb_obj.construct_hierarchy()
-      for model in hierarchy.models():
-        for chain in model.chains():
-          for conformer in chain.conformers():
-            # FIXME it would be better to use the chain object
-            f = conformer.format_fasta()
-            if (f is not None):
-              print >> out, "\n".join(f)
-  else :
-    params = params.pdb_as_fasta
-    fasta_seqs = []
-    for pdb_file in params.file_name :
-      name_base = ""
-      if (len(params.file_name) > 1) :
-        name_base = "%s " % os.path.splitext(os.path.basename(pdb_file))[0]
-      assert os.path.isfile(pdb_file)
-      pdb_obj = iotbx.pdb.input(file_name=pdb_file)
-      hierarchy = pdb_obj.construct_hierarchy()
-      for model in hierarchy.models() :
-        for chain in model.chains() :
-          if (chain.is_protein() or chain.is_na(min_content=0.5)) :
-            if (params.pad_missing_residues) :
-              seq = chain.as_padded_sequence(
-                skip_insertions=(not params.include_insertion_residues))
-            elif (not params.include_insertion_residues) :
-              seq = chain.as_padded_sequence(skip_insertions=True,
-                pad=False)
-            else :
-              seq = "".join(chain.as_sequence())
-            if (params.ignore_missing_residues_at_start) :
-              seq = re.sub("^X*", "", seq)
-            seq_lines = []
-            k = 0
-            while (k < len(seq)) :
-              if ((k % 70) == 0) :
-                seq_lines.append("")
-              seq_lines[-1] += seq[k]
-              k += 1
-            seq = "\n".join(seq_lines)
-            fasta_seqs.append(">%schain '%2s'\n%s" % (name_base,chain.id, seq))
+    cmdline = iotbx.phil.process_command_line_with_files(
+      args=args,
+      master_phil_string=master_phil_str_template % "None",
+      pdb_file_def="pdb_as_fasta.file_name")
+    params = cmdline.work.extract()
+    validate_params(params)
+  params = params.pdb_as_fasta
+  fasta_seqs = []
+  for pdb_file in params.file_name :
+    name_base = ""
+    if (len(params.file_name) > 1) :
+      name_base = "%s " % os.path.splitext(os.path.basename(pdb_file))[0]
+    assert os.path.isfile(pdb_file)
+    pdb_obj = iotbx.pdb.input(file_name=pdb_file)
+    hierarchy = pdb_obj.construct_hierarchy()
+    for model in hierarchy.models() :
+      for chain in model.chains() :
+        if (chain.is_protein() or chain.is_na(min_content=0.5)) :
+          if (params.pad_missing_residues) :
+            seq = chain.as_padded_sequence(
+              skip_insertions=(not params.include_insertion_residues))
+          elif (not params.include_insertion_residues) :
+            seq = chain.as_padded_sequence(skip_insertions=True,
+              pad=False)
+          else :
+            seq = "".join(chain.as_sequence())
+          if (params.ignore_missing_residues_at_start) :
+            seq = re.sub("^X*", "", seq)
+          seq_lines = []
+          k = 0
+          while (k < len(seq)) :
+            if ((k % 70) == 0) :
+              seq_lines.append("")
+            seq_lines[-1] += seq[k]
+            k += 1
+          seq = "\n".join(seq_lines)
+          fasta_seqs.append(">%schain '%2s'\n%s" % (name_base,chain.id, seq))
+  if (params.output_file is not None) :
     f = open(params.output_file, "w")
     f.write("\n".join(fasta_seqs))
     f.close()
-    return params.output_file
+  else :
+    print "\n".join(fasta_seqs)
+  return params.output_file
 
 def validate_params (params) :
   if (len(params.pdb_as_fasta.file_name) == 0) :
     raise Sorry("No PDB files defined!")
 
-# XXX for phenix gui
-master_phil = libtbx.phil.parse("""
+master_phil_str_template = """
 pdb_as_fasta
   .short_caption = Extract sequence from PDB file(s)
   .caption = Only protein and nucleic acid chains are supported by this \
@@ -72,7 +69,7 @@ pdb_as_fasta
     .multiple = True
     .short_caption = PDB file
     .style = bold noauto file_type:pdb
-  output_file = pdb_sequences.fa
+  output_file = %s
     .type = path
     .style = bold noauto file_type:seq new_file
   pad_missing_residues = True
@@ -87,7 +84,10 @@ pdb_as_fasta
     .short_caption = Include insertion residues
     .style = bold noauto
 }
-""")
+"""
+
+# XXX for phenix gui
+master_phil = libtbx.phil.parse(master_phil_str_template % "pdb_sequences.fa")
 
 if (__name__ == "__main__"):
   run(sys.argv[1:])
