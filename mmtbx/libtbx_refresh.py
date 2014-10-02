099a51b15a81e4ec7868608bb2da35bf4a1de26f
Nathaniel Echols <nathaniel.echols@gmail.com>
2014-10-02 23:04:24 +0000

    automatically generate rotarama pickle files if chem_data is present

--- mmtbx/libtbx_refresh.py
+++ mmtbx/libtbx_refresh.py
@@ -2,3 +2,11 @@ from __future__ import division
 self.remove_obsolete_pyc_if_possible(pyc_file_names=[
   "monomer_library/mmCIF.pyc", # XXX backward compatibility 2011-06-21
 ])
+# XXX automatically generate rotarama pickle files if chem_data is present
+try :
+  import libtbx.load_env
+  if libtbx.env.find_in_repositories(relative_path="chem_data") :
+    import subprocess
+    subprocess.call("mmtbx.rebuild_rotarama_cache")
+except Exception, e :
+  print e
