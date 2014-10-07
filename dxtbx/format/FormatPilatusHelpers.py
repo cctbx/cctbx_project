cb67d53c782b662b14ad236c04a28aaba4e2f597
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-10-07 11:18:10 +0000

    compute number of modules and hence the mask for the dead area dynamically in order to enable reading of Pilatus images where a region of interest was used

--- dxtbx/format/FormatPilatusHelpers.py
+++ dxtbx/format/FormatPilatusHelpers.py
@@ -52,13 +52,42 @@ def determine_pilatus_mask(xdetector):
 
   size = xdetector[0].get_image_size()
 
-  if size == (2463, 2527):
-    return pilatus_6M_mask()
+  # Hardcoded module size and gap size
+  module_size_fast, module_size_slow = (487, 195)
+  gap_size_fast, gap_size_slow = (7, 17)
 
-  if size == (1475, 1679):
-    return pilatus_2M_mask()
+  # Edge dead areas not included, only gaps between modules matter
+  n_fast, remainder = divmod(size[0], module_size_fast)
+  assert (n_fast-1) * gap_size_fast == remainder
 
-  if size == (487, 619):
-    return pilatus_300K_mask()
+  n_slow, remainder = divmod(size[1], module_size_slow)
+  assert (n_slow-1) * gap_size_slow == remainder
 
-  raise RuntimeError, 'unknown image size: %d %d' % size
+  # Specify the dead areas between the modules, i.e. the rows and columns
+  # where there are no active pixels
+  mask = []
+  for i_fast in range(n_fast-1):
+    mask.append([
+      (i_fast+1) * module_size_fast + i_fast * gap_size_fast + 1,
+      (i_fast+1) * module_size_fast + i_fast * gap_size_fast + gap_size_fast,
+      1, size[1]
+    ])
+  for i_slow in range(n_slow-1):
+    mask.append([
+      1, size[0],
+      (i_slow+1) * module_size_slow + i_slow * gap_size_slow + 1,
+      (i_slow+1) * module_size_slow + i_slow * gap_size_slow + gap_size_slow
+    ])
+
+  return mask
+
+  #if size == (2463, 2527):
+    #return pilatus_6M_mask()
+
+  #if size == (1475, 1679):
+    #return pilatus_2M_mask()
+
+  #if size == (487, 619):
+    #return pilatus_300K_mask()
+
+  #raise RuntimeError, 'unknown image size: %d %d' % size
