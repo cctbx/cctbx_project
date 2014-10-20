e51b2d2edf496b83c681e588b75eaea4c5e7aaa3
Iris Young <idyoung@lbl.gov>
2014-10-20 20:30:33 +0000

    Added method for extracting ML values, if available, and reorganized.

--- xfel/command_line/frame_extractor.py
+++ xfel/command_line/frame_extractor.py
@@ -14,30 +14,33 @@ import iotbx.phil
 import cctbx
 
 class construct_frame(object):
-  def __init__(self, pickle_name, json_name):
+  def __init__(self, pickle_name, json_name, pixel_size):
     # assemble template and unpack files
-    template_pickle = {'mapped_predictions':[[]],
-                       'distance':0,
-                       'ybeam':0,
-                       'current_orientation':0,
-                       'ML_half_mosaicity_deg':[0],
+    template_pickle = {'correction_vectors':[[]],
                        'current_cb_op_to_primitive': 0,
+                       'current_orientation':0,
+                       'distance':0,
                        'effective_tiling':0,
+                       'mapped_predictions':[[]],
+                       'max_signal':0,
+                       'ML_domain_size_ang':[0],
+                       'ML_half_mosaicity_deg':[0],
+                       'mosaicity':0,
+                       'model_partialities':[None],
+                       'observations':[0],
+                       'pointgroup':0,
                        'residual':0,
                        'sa_parameters':['None'],
-                       'model_partialities':[None],
-                       'ML_domain_size_ang':[0],
-                       'mosaicity':0,
-                       'observations':[0],
                        'wavelength':0,
                        'xbeam':0,
-                       'pointgroup':0,
-                       'max_signal':0,
-                       'correction_vectors':[[0]]}
+                       'ybeam':0}
 
     self.frame = template_pickle
+    self.pixel_size = pixel_size
 
     importer = Importer([pickle_name, json_name], read_experiments=True, read_reflections=True)
+    if importer.unhandled:
+      print "unable to process:", importer.unhandled
 
     # load the integration.pickle file (reflection table) into memory
     self.reflections = flatten_reflections(importer.reflections)[0]
@@ -52,6 +55,70 @@ class construct_frame(object):
     self.scan = experiments.scan
     self.img_sweep = experiments.imageset
 
+  # experiment-dependent components ---------------------------------------------------------------------------
+
+  # get wavelength
+  def populate_wavelength(self):
+    assert self.beam_obj.get_wavelength() is not None, "no wavelength"
+    self.frame['wavelength'] = self.beam_obj.get_wavelength()
+
+  # get detector distance in mm
+  def populate_distance(self):
+    assert self.det[0].get_distance() is not None, "no detector distance"
+    self.frame['distance'] = self.det[0].get_distance()
+
+  # get xbeam and ybeam in mm
+  def populate_beam_dir(self):
+    assert self.beam_obj.get_s0() is not None, "no beam direction"
+    self.frame['xbeam'], self.frame['ybeam'] = self.det[0].get_beam_centre(self.beam_obj.get_s0())
+
+  # get max signal
+  def populate_max_signal(self):
+    pass
+
+  # get effective tiling
+  def populate_effective_tiling(self):
+    pass
+
+  # indicate simulated annealing parameters, if present
+  def populate_sa_params(self):
+    pass
+
+  # crystal-dependent components ------------------------------------------------------------------------------
+
+  # generate a crystal orientation object from the A* matrix
+  def populate_orientation(self):
+    assert self.xtal.get_A() is not None, "no crystal orientation matrix"
+    self.frame['current_orientation'] = [crystal_orientation(self.xtal.get_A().elems, True)]
+
+  # generate change-of-basis operation for current to primitive cell
+  def populate_op_to_primitive(self):
+    assert self.xtal.get_space_group() is not None, "no space group"
+    self.frame['current_cb_op_to_primitive'] = [self.xtal.get_space_group().z2p_op()]
+
+  # fetch the point group associated with the crystal
+  def populate_point_group(self):
+    assert self.xtal.get_space_group() is not None, "no space group"
+    self.frame['pointgroup'] = str(self.xtal.get_space_group().build_derived_point_group().info())
+
+  # get mosaicity
+  def populate_mosaicity(self):
+    assert self.xtal.get_mosaicity() is not None, "no mosaicity"
+    self.frame['mosaicity'] = self.xtal.get_mosaicity()
+
+  # get any available ML values
+  def populate_ML_values(self):
+    try:
+      self.frame['ML_half_mosaicity_deg'] = [self.xtal._ML_half_mosaicity_deg]
+    except AttributeError:
+      pass
+    try:
+      self.frame['ML_domain_size_ang'] = [self.xtal._ML_domain_size_ang]
+    except AttributeError:
+      pass
+
+  # observations-dependent components -------------------------------------------------------------------------
+
   # generate a miller array containing the Miller indices, intensities and variances for one frame
   def populate_observations(self):
     if self.reflections.has_key('intensity.prf.value'):
@@ -62,62 +129,55 @@ class construct_frame(object):
       variances = self.reflections['intensity.sum.variance']
     space_group = crystal.symmetry(self.xtal.get_unit_cell(), str(self.xtal.get_space_group().info()))
     miller_set = miller.set(space_group, self.reflections['miller_index'])
-    self.frame['observations'][0] = cctbx.miller.array(miller_set, intensities, variances)
+    self.frame['observations'][0] = cctbx.miller.array(miller_set, intensities, variances).set_observation_type_xray_intensity()
 
-  # convert a flex.vec3_double object with key 'xyzobs.px.value' to a flex.vec2_double object with key 'mapped_predictions'
+  # collect predicted spot positions
   def populate_pixel_positions(self):
-    assert self.reflections.has_key('xyzobs.px.value')
+    assert self.reflections.has_key('xyzcal.px'), "no calculated spot positions"
     self.frame['mapped_predictions'][0] = flex.vec2_double()
-    for i in range(len(self.reflections['xyzobs.px.value'])):
-      self.frame['mapped_predictions'][0].append((self.reflections['xyzobs.px.value'][i][0], self.reflections['xyzobs.px.value'][i][1]))
+    for i in range(len(self.reflections['xyzcal.px'])):
+      self.frame['mapped_predictions'][0].append(tuple(self.reflections['xyzcal.px'][i][0:2]))
 
+  # generate a list of dictionaries containing a series of corrections for each predicted reflection
+  def populate_corrections(self):
+    assert self.reflections.has_key('xyzobs.px.value') and self.reflections.has_key('xyzcal.px'), "no calculated or observed spot positions"
+    assert self.frame['xbeam'] is not 0 and self.frame['ybeam'] is not 0, "invalid beam center"
+    for idx in xrange(len(self.reflections['xyzobs.px.value'])):
+      if self.reflections['xyzcal.px'][idx][0:2] != self.reflections['xyzobs.px.value'][idx][0:2]:
+        theoret_center = 1765/2, 1765/2
+        refined_center = self.frame['xbeam']/self.pixel_size, self.frame['ybeam']/self.pixel_size # px to mm conversion
+        hkl = self.reflections['miller_index'][idx]
+        obsspot = tuple(self.reflections['xyzobs.px.value'][idx][0:2])
+        predspot = tuple(self.reflections['xyzcal.px'][idx][0:2])
+        self.frame['correction_vectors'][0].append({'refinedcenter':refined_center, 'hkl':hkl, 'setting_id':0, 'azimuthal':0, 'radial':0,
+          'obsspot':obsspot, 'obscenter':theoret_center, 'predspot':predspot})
 
-  # fetch the point group associated with the crystal
-  def populate_point_group(self):
-    assert self.xtal.get_space_group() is not None
-    self.frame['pointgroup'] = str(self.xtal.get_space_group().build_derived_point_group().info())
+  # get partialities
+  def populate_partialities(self):
+    pass
 
-  # get wavelength
-  def populate_wavelength(self):
-    assert self.beam_obj.get_wavelength() is not None
-    self.frame['wavelength'] = self.beam_obj.get_wavelength()
-
-  # get mosaicity
-  def populate_mosaicity(self):
-    assert self.xtal.get_mosaicity() is not None
-    self.frame['mosaicity'] = self.xtal.get_mosaicity()
-
-  # get distance in mm
-  def populate_distance(self):
-    assert self.det[0].get_distance() is not None
-    self.frame['distance'] = self.det[0].get_distance()
-
-  # get xbeam and ybeam in mm
-  def populate_beam_dir(self):
-    assert self.beam_obj.get_s0() is not None
-    self.frame['xbeam'], self.frame['ybeam'] = self.det[0].get_beam_centre(self.beam_obj.get_s0())
-
-  # generate a crystal orientation object from the A matrix
-  def populate_orientation(self):
-    assert self.xtal.get_A() is not None
-    self.frame['current_orientation'] = [crystal_orientation(self.xtal.get_A().elems, True)] # if buggy, check here first
-
-  # generate change-of-basis operation for current to primitive cell
-  def populate_op_to_primitive(self):
-    assert self.xtal.get_space_group() is not None
-    self.frame['current_cb_op_to_primitive'] = [self.xtal.get_space_group().z2p_op()]
+  # produce residuals
+  def populate_residuals(self):
+    pass
 
   # combine all of the above
   def make_frame(self):
-    self.populate_observations()
-    self.populate_pixel_positions()
-    self.populate_point_group()
     self.populate_wavelength()
-    self.populate_mosaicity()
     self.populate_distance()
     self.populate_beam_dir()
+    self.populate_max_signal()
+    self.populate_effective_tiling()
+    self.populate_sa_params()
     self.populate_orientation()
     self.populate_op_to_primitive()
+    self.populate_point_group()
+    self.populate_mosaicity()
+    self.populate_ML_values()
+    self.populate_observations()
+    self.populate_pixel_positions()
+    self.populate_corrections()
+    self.populate_partialities()
+    self.populate_residuals()
     return self.frame
 
 if __name__ == "__main__":
