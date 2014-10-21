5b3cf91ee6bbe4267651e094885e3014d9c60caa
Nicholas Sauter <nksauter@lbl.gov>
2014-10-21 16:27:14 +0000

    Allow cxi.xmerge to work for dials-formatted experiment.json input.

--- xfel/cxi/util.py
+++ xfel/cxi/util.py
@@ -1,8 +1,13 @@
 from __future__ import division
 import os
-def is_odd_numbered(file_name):
-      if (file_name.endswith("_00000.pickle")):
-        return int(os.path.basename(file_name).split("_00000.pickle")[0][-1])%2==1
-      elif (file_name.endswith(".pickle")):
-        return int(os.path.basename(file_name).split(".pickle")[0][-1])%2==1
 
+#for integration pickles:
+allowable_basename_endings = ["_00000.pickle",
+                              ".pickle",
+                              "_experiments.json"
+                             ]
+def is_odd_numbered(file_name):
+  for allowable in allowable_basename_endings:
+    if (file_name.endswith(allowable)):
+      return int(os.path.basename(file_name).split(allowable)[0][-1])%2==1
+  raise ValueError
