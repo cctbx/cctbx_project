5cbe9869755f714afc68ce0972b595bbb593394f
Ian Rees <ian@ianrees.net>
2014-10-23 23:55:38 +0000

    libtbx.find_clutter unused imports

--- dxtbx/format/FormatCBFMiniPilatusDLS6MSN100F.py
+++ dxtbx/format/FormatCBFMiniPilatusDLS6MSN100F.py
@@ -15,8 +15,9 @@ from dxtbx.model import ParallaxCorrectedPxMmStrategy
 from dxtbx.format.FormatPilatusHelpers import determine_pilatus_mask
 
 def read_cbf_image_as_double(cbf_image):
-  from scitbx.array_family import flex
-  from cbflib_adaptbx import uncompress, compress
+  # from scitbx.array_family import flex
+  # from cbflib_adaptbx import uncompress, compress
+  from cbflib_adaptbx import uncompress
   import binascii
 
   start_tag = binascii.unhexlify('0c1a04d5')
