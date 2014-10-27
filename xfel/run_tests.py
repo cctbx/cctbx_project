43c55ffba6930a9eb01788dadd4cdbcd298b4d51
Ian Rees <ian@ianrees.net>
2014-10-27 21:38:27 +0000

    Testing stubs for cctbx.xfel.

---
+++ xfel/run_tests.py
@@ -0,0 +1,15 @@
+from __future__ import division
+from libtbx import test_utils
+import libtbx.load_env
+
+tst_list = ["$D/tst_cxi_index.py"]
+
+def run():
+  build_dir = libtbx.env.under_build("xfel")
+  dist_dir = libtbx.env.dist_path("xfel")
+  test_utils.run_tests(build_dir, dist_dir, tst_list)
+
+if (__name__ == "__main__"):
+  run()
+
+# libtbx.run_tests_parallel module=xfel
