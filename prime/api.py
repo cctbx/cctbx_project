39cdc6191119495ad5cb9b710104d29a8fd83d81
Oliver Zeldin <oliver.zeldin@gmail.com>
2014-11-04 18:45:20 +0000

    changed return description following discussion with Mona

--- prime/api.py
+++ prime/api.py
@@ -1,7 +1,8 @@
 from __future__ import division
 
 def refine_many(frame_lst, input_file):
-  """ Run postrefienement.
+  """ Perform a full run on Prime, using the parameters specified in the `input_file`. Merging of fully-corrected intensities is not performed.
+
   :param frame_lst: list of dictionairies describing integration results.
   :param input_file: Prime .inp file. See XXXXXX for full description.
   Schema for elements of `frame_lst`:
@@ -13,6 +14,10 @@ def refine_many(frame_lst, input_file):
         - `xbeam`: x-location of beam centre
         - `ybeam`: y-location of beam centre
         - `wavelength`: the wavelength in Angstroms
-  :return: a list of tuples of the form: (G, B, rotx, roty, ry, rz, re, a, b, c, alpha, beta, gamma), describing the refined parameters of the images described by the list of dictionairies.
+  :return: A list of `miller_array` objects, containing the partiality-corrected (full-intensity equivalent) intensities, and associated errors for each of the images specified in the input dictionairy.
+
+  .. warning::
+     This will modify the `orientation` object to reflect the updated parameters. Deep copy first if you do not want this!
+
   """
   pass
