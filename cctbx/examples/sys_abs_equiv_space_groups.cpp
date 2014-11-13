ffda7d16047f08b6cad1b1eb48ff2dc43e05ecbe
Nathaniel Echols <nathaniel.echols@gmail.com>
2014-11-13 17:14:21 +0000

    clang fix

--- cctbx/examples/sys_abs_equiv_space_groups.cpp
+++ cctbx/examples/sys_abs_equiv_space_groups.cpp
@@ -7,13 +7,13 @@ main()
   using std::cout;
   using std::endl;
   unsigned const** tab = cctbx::sgtbx::sys_abs_equiv::space_group_numbers;
-  cout << tab[0] << endl;
+  cout << (*tab[0]) << endl; // XXX need to dereference when using clang
   cout << tab[1][1] << endl;
   cout << tab[2][1] << endl;
   for(unsigned i=1;i<=tab[75][0];i++) {
     cout << " " << tab[75][i];
   }
   cout << endl;
-  cout << tab[230] << endl;
+  cout << (*tab[230]) << endl; // XXX see above
   return 0;
 }
