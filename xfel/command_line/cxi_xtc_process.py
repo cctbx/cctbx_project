36d3aa2495e54f5b05bba1a1254c25de6bb7c170
Aaron S. Brewster <asbrewster@lbl.gov>
2014-11-20 18:31:11 +0000

    Add debugging to cxi.xtc_process to help diagnose some rare errors:
    Use debug.write_debug_files to have the program write litle diagnostic files for
    each event in the XTC stream to the folder specified by output.output_dir.  If an
    event causes an failure, the diagnostic file will not be complete.
    
    Use debug.use_debug_files to only process events that had previously failed.
    
    Use debug.event_timestamp to specify a specific timestamp to only process.

--- xfel/command_line/cxi_xtc_process.py
+++ xfel/command_line/cxi_xtc_process.py
@@ -7,6 +7,7 @@ from psana import *
 import os, sys
 from libtbx.utils import Sorry
 from libtbx.phil import parse
+from xfel.cxi.cspad_ana import cspad_tbx
 
 phil_scope = parse('''
   dispatch {
@@ -24,6 +25,24 @@ phil_scope = parse('''
     run_num = None
       .type = int
       .help = "Run number or run range to process"
+  }
+  output {
+    output_dir = "."
+      .type = str
+      .help = "Directory for output files"
+  }
+  debug {
+    write_debug_files = False
+      .type = bool
+      .help = "If true, will write out a tiny diagnostic file for each event before"
+      .help = "and after the event is processed"
+    use_debug_files = False
+      .type = bool
+      .help = "If true, will look for debug diagnostic files in the output_dir and"
+      .help = "only process events that crashed previously"
+    event_timestamp = None
+      .type = str
+      .help = "If set to be a timestamp, will only process the event that matches it"
   }
 ''', process_includes=True)
 
@@ -50,6 +69,8 @@ class Script(object):
     assert params.input.experiment is not None
     assert params.input.run_num is not None
 
+    print "Processing run %d of experiment %s using config file %s"%(params.input.run_num, params.input.experiment, params.input.cfg)
+
     from mpi4py import MPI
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank() # each process in MPI has a unique id, 0-indexed
@@ -74,11 +95,41 @@ class Script(object):
       # chop the list into pieces, depending on rank
       mytimes= times[rank*mylength:(rank+1)*mylength]
       for i in range(mylength):
+        ts = cspad_tbx.evt_timestamp((mytimes[i].seconds(),mytimes[i].nanoseconds()/1e6))
+        if params.debug.event_timestamp is not None and params.debug.event_timestamp != ts:
+          continue
+
+        ts_path = os.path.join(params.output.output_dir, "debug-" + ts + ".txt")
+
+        if params.debug.use_debug_files:
+          if not os.path.exists(ts_path):
+            print "Skipping event %s: no debug file found"%ts
+            continue
+
+          f = open(ts_path, "r")
+          if len(f.readlines()) > 1:
+            print "Skipping event %s: processed successfully previously"%ts
+            continue
+          f.close()
+          print "Accepted", ts
+
+        if params.debug.write_debug_files:
+          f = open(ts_path, "w")
+          f.write("about to process " + ts + "\n")
+          f.close()
+
+        # load the event.  This will cause any modules to be run.
         evt = run.event(mytimes[i])
+
+        if params.debug.write_debug_files:
+          f = open(ts_path, "a")
+          f.write("done\n")
+          f.close()
+
         id = evt.get(EventId)
         print "Event #",i," has id:",id
 
-        # nop since the module does all the work
+        # nop since the module does all the work.
 
 if __name__ == "__main__":
   script = Script()
