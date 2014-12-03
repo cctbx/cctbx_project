015109233d385a9b937a38b2effc0ed989fc627a
Pavel Afonine <afonine@users.noreply.github.com>
2014-12-03 22:48:24 +0000

    Consolidate tests, and also make test structure uniform and alike to others

--- iotbx/regression/ncs/tst.py
+++ iotbx/regression/ncs/tst.py
@@ -123,6 +123,132 @@ TER
 END
 """
 
+pdb_str_6 = """\
+CRYST1  577.812  448.715  468.790  90.00  90.00  90.00 P 1
+ATOM      1  CA  LYS A 151      10.766   9.333  12.905  1.00 44.22           C
+ATOM      2  CA  LYS A 152      10.117   9.159  11.610  1.00 49.42           C
+ATOM      3  CA  LYS A 153       9.099   8.000  11.562  1.00 46.15           C
+ATOM      4  CA  LYS A 154       8.000   8.202  11.065  1.00 52.97           C
+ATOM      5  CA  LYS A 155      11.146   9.065  10.474  1.00 41.68           C
+ATOM      6  CA  LYS A 156      10.547   9.007   9.084  1.00 55.55           C
+TER
+ATOM      7  CA  LYS B 157      11.545   9.413   8.000  1.00 72.27           C
+ATOM      8  CA  LYS B 158      12.277  10.718   8.343  1.00 75.78           C
+ATOM      9  CA  LYS B 159      11.349  11.791   8.809  1.00 75.88           C
+TER
+ATOM      7  CA  LYS F 157       2.154   3.953  16.298  1.00 72.27           C
+ATOM      8  CA  LYS F 158       2.014   3.732  17.811  1.00 75.78           C
+ATOM      9  CA  LYS F 159       2.558   2.413  18.250  1.00 75.88           C
+TER
+ATOM      7  CA  LYS D 157       4.334  10.965  12.119  1.00 72.27           C
+ATOM      8  CA  LYS D 158       4.057  11.980  13.238  1.00 75.78           C
+ATOM      9  CA  LYS D 159       3.177  11.427  14.310  1.00 75.88           C
+TER
+ATOM      1  CA  LYS C 151       6.855   8.667  15.730  1.00 44.22           C
+ATOM      2  CA  LYS C 152       5.891   8.459  14.655  1.00 49.42           C
+ATOM      3  CA  LYS C 153       6.103   7.155  13.858  1.00 46.15           C
+ATOM      4  CA  LYS C 154       5.138   6.438  13.633  1.00 52.97           C
+ATOM      5  CA  LYS C 155       5.801   9.685  13.736  1.00 41.68           C
+ATOM      6  CA  LYS C 156       4.731   9.594  12.667  1.00 55.55           C
+TER
+ATOM      1  CA  LYS E 151       6.987   4.106  17.432  1.00 44.22           C
+ATOM      2  CA  LYS E 152       6.017   3.539  16.502  1.00 49.42           C
+ATOM      3  CA  LYS E 153       6.497   3.492  15.036  1.00 46.15           C
+ATOM      4  CA  LYS E 154       6.348   2.458  14.400  1.00 52.97           C
+ATOM      5  CA  LYS E 155       4.647   4.221  16.634  1.00 41.68           C
+ATOM      6  CA  LYS E 156       3.552   3.605  15.788  1.00 55.55           C
+TER
+"""
+
+pdb_str_7 = """\
+CRYST1  577.812  448.715  468.790  90.00  90.00  90.00 P 1
+ATOM      1  CA  LYS A 151      10.766   9.333  12.905  1.00 44.22           C
+ATOM      2  CA  LYS A 152      10.117   9.159  11.610  1.00 49.42           C
+ATOM      3  CA  LYS A 153       9.099   8.000  11.562  1.00 46.15           C
+ATOM      4  CA  LYS A 154       8.000   8.202  11.065  1.00 52.97           C
+ATOM      5  CA  LYS A 155      11.146   9.065  10.474  1.00 41.68           C
+TER
+ATOM    222  CA  LEU X  40      94.618  -5.253  91.582  1.00 87.10           C
+ATOM    223  CA  ARG X  41      62.395  51.344  80.786  1.00107.25           C
+ATOM    224  CA  ARG X  42      62.395  41.344  80.786  1.00107.25           C
+TER
+ATOM      1  CA  THR D   1       8.111  11.080  10.645  1.00 20.00           C
+ATOM      2  CA  THR D   2       8.000   9.722  10.125  1.00 20.00           C
+ATOM      3  CA  THR D   3       8.075   8.694  11.249  1.00 20.00           C
+ATOM      4  CA  THR D   4       8.890   8.818  12.163  1.00 20.00           C
+TER
+ATOM      1  CA  LYS B 151       6.855   8.667  15.730  1.00 44.22           C
+ATOM      2  CA  LYS B 152       5.891   8.459  14.655  1.00 49.42           C
+ATOM      3  CA  LYS B 153       6.103   7.155  13.858  1.00 46.15           C
+ATOM      4  CA  LYS B 154       5.138   6.438  13.633  1.00 52.97           C
+ATOM      5  CA  LYS B 155       5.801   9.685  13.736  1.00 41.68           C
+TER
+ATOM      1  CA  LYS C 151       6.987   4.106  17.432  1.00 44.22           C
+ATOM      2  CA  LYS C 152       6.017   3.539  16.502  1.00 49.42           C
+ATOM      3  CA  LYS C 153       6.497   3.492  15.036  1.00 46.15           C
+ATOM      4  CA  LYS C 154       6.348   2.458  14.400  1.00 52.97           C
+ATOM      5  CA  LYS C 155       4.647   4.221  16.634  1.00 41.68           C
+TER
+ATOM    222  CA  LEU Y  40     194.618   5.253  81.582  1.00 87.10           C
+ATOM    223  CA  ARG Y  41     162.395  41.344  70.786  1.00107.25           C
+ATOM    224  CA  ARG Y  42     162.395  31.344  70.786  1.00107.25           C
+TER
+ATOM      1  CA  THR E   1       8.111 -10.645  11.080  1.00 20.00           C
+ATOM      2  CA  THR E   2       8.000 -10.125   9.722  1.00 20.00           C
+ATOM      3  CA  THR E   3       8.075 -11.249   8.694  1.00 20.00           C
+ATOM      4  CA  THR E   4       8.890 -12.163   8.818  1.00 20.00           C
+TER
+"""
+
+pdb_str_8 = """\
+ATOM      1  N   THR A   1      13.014  18.419   8.520  1.00 20.00           N
+ATOM      2  CA  THR A   1      12.903  17.061   8.000  1.00 20.00           C
+ATOM      3  C   THR A   1      12.978  16.033   9.124  1.00 20.00           C
+ATOM      4  O   THR A   1      13.793  16.157  10.038  1.00 20.00           O
+TER
+ATOM      1  N   THR C   1      10.325   8.000  14.368  1.00 20.00           N
+ATOM      2  CA  THR C   1      10.111   8.702  13.108  1.00 20.00           C
+ATOM      3  C   THR C   1      11.313   9.570  12.750  1.00 20.00           C
+ATOM      4  O   THR C   1      11.885  10.241  13.609  1.00 20.00           O
+TER
+ATOM      1  N   THR D   1      -0.430  15.458  18.495  1.00 20.00           N
+ATOM      2  CA  THR D   1       0.086  15.020  17.204  1.00 20.00           C
+ATOM      3  C   THR D   1       1.440  14.334  17.355  1.00 20.00           C
+ATOM      4  O   THR D   1       2.297  14.791  18.111  1.00 20.00           O
+TER
+ATOM      1  N   THR E   1       1.895   8.366  19.752  1.00 20.00           N
+ATOM      2  CA  THR E   1       1.682   9.068  18.491  1.00 20.00           C
+ATOM      3  C   THR E   1       2.884   9.935  18.133  1.00 20.00           C
+ATOM      4  O   THR E   1       3.455  10.606  18.993  1.00 20.00           O
+TER
+ATOM      1  N   THR F   1       8.346   7.308  15.936  1.00 20.00           N
+ATOM      2  CA  THR F   1       7.054   7.796  15.467  1.00 20.00           C
+ATOM      3  C   THR F   1       6.884   9.281  15.767  1.00 20.00           C
+ATOM      4  O   THR F   1       7.237   9.751  16.849  1.00 20.00           O
+TER
+ATOM      1  N   THR G   1       0.609  -0.560  24.094  1.00 20.00           N
+ATOM      2  CA  THR G   1       0.395   0.142  22.834  1.00 20.00           C
+ATOM      3  C   THR G   1       1.598   1.009  22.476  1.00 20.00           C
+ATOM      4  O   THR G   1       2.169   1.680  23.335  1.00 20.00           O
+TER
+ATOM      1  N   THR H   1       7.061  -1.617  20.279  1.00 20.00           N
+ATOM      2  CA  THR H   1       5.768  -1.130  19.810  1.00 20.00           C
+ATOM      3  C   THR H   1       5.599   0.356  20.109  1.00 20.00           C
+ATOM      4  O   THR H   1       5.950   0.825  21.191  1.00 20.00           O
+TER
+ATOM      1  N   THR I   1       8.722   4.822  16.665  1.00 20.00           N
+ATOM      2  CA  THR I   1       7.494   4.036  16.653  1.00 20.00           C
+ATOM      3  C   THR I   1       6.628   4.350  17.868  1.00 20.00           C
+ATOM      4  O   THR I   1       7.130   4.482  18.984  1.00 20.00           O
+TER
+ATOM      1  N   THR B   1       8.000  15.093  13.112  1.00 20.00           N
+ATOM      2  CA  THR B   1       8.516  14.654  11.820  1.00 20.00           C
+ATOM      3  C   THR B   1       9.870  13.968  11.972  1.00 20.00           C
+ATOM      4  O   THR B   1      10.727  14.426  12.727  1.00 20.00           O
+TER
+"""
+
+
 def exercise_00(prefix="iotbx_ncs_exercise_00"):
   pdb_file_name = "%s.pdb"%prefix
   ncs_params_str = """
@@ -263,8 +389,151 @@ def exercise_03(prefix="iotbx_ncs_exercise_03"):
   assert approx_equal(sel_copy_2.iselection(), ng.copies[1].iselection)
   assert approx_equal(sel_copy_3.iselection(), ng.copies[2].iselection)
 
+def exercise_04(prefix="iotbx_ncs_exercise_04"):
+  """
+  Testing one ncs group and master ncs with two chains of different length
+  and chains are not in order
+  """
+  ncs_inp = iotbx.ncs.input(pdb_string=pdb_str_6)
+  t = ncs_inp.ncs_to_asu_selection
+  assert t.keys() == ['chain A or chain B']
+  assert t.values() == [['chain C or chain D', 'chain E or chain F']]
+
+def exercise_05(prefix="iotbx_ncs_exercise_05"):
+  """
+  Make sure that phil selection overrides ncs grouping
+  """
+  phil_str = """\
+ncs_group {
+  master_selection = chain A
+  copy_selection = chain C
+}
+ncs_group {
+  master_selection = chain B
+  copy_selection = chain D
+}
+"""
+  ncs_inp = iotbx.ncs.input(
+    pdb_string=pdb_str_6,
+    ncs_phil_string=phil_str)
+  expected = {'chain A': ['chain C'], 'chain B': ['chain D']}
+  assert ncs_inp.ncs_to_asu_selection.keys(), expected.keys()
+  assert ncs_inp.ncs_to_asu_selection.values(), expected.values()
+
+def exercise_06(prefix="iotbx_ncs_exercise_06"):
+  """
+  Two groups, different number of chain in each group.
+  Two chains that are NOT ncs related
+  """
+  ncs_inp = iotbx.ncs.input(pdb_string=pdb_str_7)
+  t = ncs_inp.ncs_to_asu_selection
+  assert t.keys() == ['chain D', 'chain A']
+  assert t.values() == [['chain E'], ['chain B', 'chain C']]
+  assert ncs_inp.ncs_group_map[1][0] == {'chain A'}
+  assert ncs_inp.ncs_group_map[2][0] == {'chain D'}
+
+def exercise_07(prefix="iotbx_ncs_exercise_07"):
+  """
+  Test that minimal number of chains in master ncs are selected (not the
+  minimal number of transformations)
+  """
+  ncs_inp = iotbx.ncs.input(pdb_string=pdb_str_8)
+  t = ncs_inp.ncs_to_asu_selection
+  assert t.keys() == ['chain A']
+  assert t.values() == \
+    [['chain B', 'chain C', 'chain D', 'chain E',
+      'chain F', 'chain G', 'chain H', 'chain I']]
+
+def exercise_08(prefix="iotbx_ncs_exercise_08"):
+  """
+  Test that minimal number of transformations in master ncs are selected
+  (not the minimal number of chains)
+  """
+  ncs_inp = iotbx.ncs.input(
+    pdb_string=pdb_str_8,
+    use_minimal_master_ncs=False)
+  t = ncs_inp.ncs_to_asu_selection
+  assert t.keys()==['chain A or chain B or chain C']
+  assert t.values()==\
+    [['chain D or chain E or chain F',
+      'chain G or chain H or chain I']]
+
+def exercise_09(prefix="iotbx_ncs_exercise_09"):
+  """ ??? """
+  from mmtbx.ncs import ncs_search
+  pdb_inp = iotbx.pdb.input(source_info=None, lines=pdb_str_8)
+  ph = pdb_inp.construct_hierarchy()
+  #
+  chain_match_list = ncs_search.search_ncs_relations(
+    ph=ph,min_contig_length=0,min_percent=0)
+  # make sure that all possible chains compared
+  model  = ph.models()[0]
+  chain_ids = list({x.id for x in model.chains()})
+  chain_ids = sorted(chain_ids)
+  n_chains = len(chain_ids)
+  assert n_chains==9
+  assert len(chain_match_list)==8
+  #
+  match_dict = ncs_search.clean_chain_matching(chain_match_list,ph)
+  chains_info = ncs_search.get_chains_info(ph)
+  # Test minimal master NCS
+  transform_to_group,match_dict = ncs_search.minimal_master_ncs_grouping(
+  match_dict=match_dict)
+  group_dict = ncs_search.build_group_dict(
+    transform_to_group,match_dict,chains_info)
+  assert len(group_dict)==1
+  gr_obj = group_dict[('A',)]
+  assert len(gr_obj.transforms)==len(gr_obj.copies)
+  assert len(gr_obj.iselections)==len(gr_obj.copies)
+  expected = [['A'], ['B'], ['C'], ['D'], ['E'], ['F'], ['G'], ['H'], ['I']]
+  assert gr_obj.copies == expected
+  tr = gr_obj.transforms[0]
+  assert tr.r.is_r3_identity_matrix()
+  assert tr.t.is_col_zero()
+  tr = gr_obj.transforms[1]
+  assert not tr.r.is_r3_identity_matrix()
+  assert not tr.t.is_col_zero()
+
+def exercise_10(prefix="iotbx_ncs_exercise_10"):
+  """ Test minimal NCS operators """
+  from mmtbx.ncs import ncs_search
+  pdb_inp = iotbx.pdb.input(source_info=None, lines=pdb_str_8)
+  ph = pdb_inp.construct_hierarchy()
+  #
+  chain_match_list = ncs_search.search_ncs_relations(
+    ph=ph,min_contig_length=0,
+    min_percent=0,
+    use_minimal_master_ncs=False)
+  #
+  match_dict = ncs_search.clean_chain_matching(chain_match_list,ph)
+  chains_info = ncs_search.get_chains_info(ph)
+  #
+  transform_to_group,match_dict = ncs_search.minimal_ncs_operators_grouping(
+  match_dict=match_dict)
+  group_dict = ncs_search.build_group_dict(
+    transform_to_group,match_dict,chains_info)
+  assert len(group_dict)==1
+  gr_obj = group_dict[('A', 'B', 'C')]
+  assert len(gr_obj.transforms)==len(gr_obj.copies)
+  assert len(gr_obj.iselections)==len(gr_obj.copies)
+  expected = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]
+  assert gr_obj.copies==expected
+  tr = gr_obj.transforms[0]
+  assert tr.r.is_r3_identity_matrix()
+  assert tr.t.is_col_zero()
+  tr = gr_obj.transforms[1]
+  assert not tr.r.is_r3_identity_matrix()
+  assert not tr.t.is_col_zero()
+
 if (__name__ == "__main__"):
   exercise_00()
   exercise_01()
   exercise_02()
   exercise_03()
+  exercise_04()
+  exercise_05()
+  exercise_06()
+  exercise_07()
+  exercise_08()
+  exercise_09()
+  exercise_10()
