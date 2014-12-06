10eaa8345f91313f0988b3edf63d107e0e2ae6ed
Tom Terwilliger <tterwill@users.noreply.github.com>
2014-12-06 00:22:45 +0000

    Add mean_f_rms_f.py and test for it

---
+++ mmtbx/scaling/tst_mean_f_rms_f.py
@@ -0,0 +1,19 @@
+
+from __future__ import division
+import sys
+from libtbx.test_utils import approx_equal
+from libtbx.utils import null_out
+
+def exercise (args) :
+  if 'verbose' in args:
+    out=sys.stdout
+  else:
+    out=null_out()
+
+  print "Testing mean_f_rms_f....",
+  from mmtbx.scaling.mean_f_rms_f import test 
+  test(out=out)
+
+if (__name__ == "__main__") :
+  exercise(sys.argv[1:])
+  print "OK"
