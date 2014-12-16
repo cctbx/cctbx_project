74e5ea9abff88020171ae76fcda23cb55f7d2bce
Nicholas Sauter <nksauter@lbl.gov>
2014-12-16 04:12:07 +0000

    Faster Levenberg-Marquardt parameter fitting for XES per-pixel histogram distributions. 1) Fewer-iteration limit & coarser convergence criterion 2) Push the entire problem-specific build_up() function into C++.  Avoids evaluating the objective function and gradients in Python with flex arrays, as this is the rate limiting step in the calculation. 3) Problem-specific class doesn't have to be pure-Python; here it is implemented as a C++ class inheriting from scitbx::lstbx::non_linear_ls. 4) Avoid ever building up the whole Jacobian; save very substantial memory by evaluating it one row at a time and adding this gradient using scitbx::lstbx::non_linear_ls::add_equation(). 5) Avoid continual allocation and deallocation of temporary results; allocate all memory in the constructor.

---
+++ xfel/vonHamos/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import division
+import boost.python
+ext = boost.python.import_ext("xes_ext")
+from xes_ext import *
---
+++ xfel/vonHamos/xes_ext.cpp
@@ -0,0 +1,142 @@
+#include <cctbx/boost_python/flex_fwd.h>
+
+#include <boost/python/module.hpp>
+#include <boost/python/class.hpp>
+#include <boost/python/def.hpp>
+#include <scitbx/array_family/flex_types.h>
+#include <scitbx/array_family/shared.h>
+#include <scitbx/array_family/boost_python/shared_wrapper.h>
+
+#include <vector>
+#include <cmath>
+#include <stdexcept>
+#include <scitbx/lstbx/normal_equations.h>
+
+using namespace boost::python;
+namespace xfel{
+
+namespace xes { namespace example {
+  class gaussian_fit_inheriting_from_non_linear_ls: public scitbx::lstbx::normal_equations::non_linear_ls<double> {
+
+    public:
+      gaussian_fit_inheriting_from_non_linear_ls(int n_parameters):
+        scitbx::lstbx::normal_equations::non_linear_ls<double>(n_parameters),
+        jacobian_one_row(n_parameters)
+        {}
+
+      void set_cpp_data(scitbx::af::shared<double> x,
+                        scitbx::af::shared<double> y,
+                        double const& g,
+                        double const& s) {
+                        free_x=x, free_y=y, gain_to_sigma=g, sigmafac=s; sigmafac_sq=s*s;
+                        residual_terms_0 = std::vector<double>(x.size());
+                        residual_terms_1 = std::vector<double>(x.size());
+                        residuals = scitbx::af::shared<double>(x.size());
+      }
+
+      void access_cpp_build_up_directly(bool objective_only, scitbx::af::shared<double> current_values) {
+        fvec_callable(current_values);
+        if (objective_only) {
+          add_residuals(residuals.const_ref(), scitbx::af::shared<double>().const_ref());
+        }else{
+          // add one of the normal equations for each observation
+          for (int ix = 0; ix < free_x.size(); ++ix) {
+            /*
+              z_mean = current_values[0]
+              z_ampl = current_values[1]
+              z_sigm = current_values[2]
+              o_ampl = current_values[3]
+              o_mean = z_mean + z_sigm * GAIN_TO_SIGMA
+              o_sigm = z_sigm * SIGMAFAC
+              # leaving out small cross terms where one-photon peak influences
+              # derivatives with respect to z_mean and z_sigm.
+              # XXX not accounting for charge sharing at all
+             */
+            double udiff = free_x[ix] - current_values[0];
+            double Afactor = udiff/(current_values[2] * current_values[2]);
+            double Sfactor = Afactor * udiff/current_values[2];
+            jacobian_one_row[0] = ( residual_terms_0[ix] * Afactor );
+            jacobian_one_row[1] = ( residual_terms_0[ix] / current_values[1] );
+            jacobian_one_row[2] = ( residual_terms_0[ix] * Sfactor );
+            jacobian_one_row[3] = ( residual_terms_1[ix] / current_values[3] );
+
+            add_equation(residuals[ix], jacobian_one_row.const_ref(), 1.);
+          }
+        }
+      }
+
+      void
+      fvec_callable(scitbx::af::shared<double> &current_values) {
+        /*
+          z_mean = current_values[0]
+          z_ampl = current_values[1]
+          z_sigm = current_values[2]
+          o_ampl = current_values[3]
+          o_mean = z_mean + z_sigm * GAIN_TO_SIGMA
+          o_sigm = z_sigm * SIGMAFAC
+
+          # model minus obs
+          # sqrt2pi_inv = 1./math.sqrt(2.*math.pi)
+          # the gaussian function is sqrt2pi_inv * exp( - (x-mean)**2 / (2.*(sigma**2)))/sigma
+          # take off the coefficient sqrt2pi_inv / sigma, use ampl
+        */
+
+        for (int ix = 0; ix < free_x.size(); ++ix) {
+          double model=0;
+          //zero-photon Gaussian
+          double diff = free_x[ix] - current_values[0];
+          residual_terms_0[ix] = (
+            current_values[1] * std::exp( - (diff*diff) / (2. * current_values[2] * current_values[2])));
+          model += residual_terms_0[ix];
+
+          //one-photon Gaussian
+          diff = free_x[ix] - ( current_values[0] + current_values[2] * gain_to_sigma );
+          residual_terms_1[ix] = (
+            current_values[3] * std::exp( - (diff*diff) / (2. * current_values[2] * current_values[2] * sigmafac_sq)));
+          model += residual_terms_1[ix];
+
+          //terms = [
+          //ampl * flex.exp(-flex.pow2(free_x - mean) / (2.*sigm*sigm))
+          //for mean,ampl,sigm in [(z_mean,z_ampl,z_sigm),(o_mean,o_ampl,o_sigm)]]
+          //#print "residual", math.sqrt(flex.sum((model-free_y)*(model-free_y)))
+          residuals[ix] = ( model - free_y[ix] );
+        }
+      }
+
+    private:
+      scitbx::af::shared<double> free_x, free_y, jacobian_one_row, residuals;
+      double gain_to_sigma, sigmafac, sigmafac_sq;
+      std::vector<double> residual_terms_0, residual_terms_1;
+
+  };
+}} //xes::example
+
+namespace boost_python { namespace {
+
+  void
+  xes_ext_init_module() {
+    using namespace boost::python;
+
+    typedef return_value_policy<return_by_value> rbv;
+    typedef default_call_policies dcp;
+    typedef xes::example::gaussian_fit_inheriting_from_non_linear_ls wt;
+
+    class_<xes::example::gaussian_fit_inheriting_from_non_linear_ls,
+           bases<scitbx::lstbx::normal_equations::non_linear_ls<double> > >(
+      "gaussian_fit_inheriting_from_non_linear_ls", no_init)
+      .def(init<int>(arg("n_parameters")))
+      .def("access_cpp_build_up_directly",&wt::access_cpp_build_up_directly,
+        (arg("objective_only"),arg("current_values")))
+      .def("set_cpp_data",&wt::set_cpp_data,
+        (arg("free_x"),arg("free_y"),arg("gain_to_sigma"),arg("sigmafac")))
+    ;
+  }
+
+}
+}} // namespace xfel::boost_python::<anonymous>
+
+BOOST_PYTHON_MODULE(xes_ext)
+{
+  xfel::boost_python::xes_ext_init_module();
+
+}
