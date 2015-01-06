e8f24ca6cc45e0426f1a70fcd20e8b99715cf767
Youval Dar <youdar@users.noreply.github.com>
2015-01-06 22:55:40 +0000

    - Add method=cctbx to phenix.clashscore

--- mmtbx/command_line/clashscore.py
+++ mmtbx/command_line/clashscore.py
@@ -1,46 +1,81 @@
 # LIBTBX_SET_DISPATCHER_NAME phenix.clashscore
 
 from __future__ import division
+import mmtbx.monomer_library.pdb_interpretation as pdb_inter
+import cctbx.geometry_restraints.clash_score as clash_score
 import mmtbx.validation.clashscore
-import iotbx.phil
+from libtbx.utils import null_out
 from libtbx.utils import Usage
+import iotbx.phil
 import sys
 
-def get_master_phil () :
-  return iotbx.phil.parse("""
-model = None
-  .type = path
+master_phil_str = """
+  model = None
+    .type = path
+    .optional = False
+    .help = '''input PDB file'''
 
-verbose = True
-  .type = bool
+  cif = None
+    .type = path
+    .optional = True
+    .help = '''Optional Crystallographic Information File (CIF)'''
 
-keep_hydrogens = True
-  .type = bool
-  .help = '''Keep hydrogens in input file'''
+  verbose = True
+    .type = bool
 
-nuclear = False
-  .type = bool
-  .help = '''Use nuclear hydrogen positions'''
+  keep_hydrogens = True
+    .type = bool
+    .help = '''Keep hydrogens in input file'''
 
-time_limit = 120
-  .type = int
-  .help = '''Time limit (sec) for Reduce optimization'''
+  nuclear = False
+    .type = bool
+    .help = '''Use nuclear hydrogen positions'''
 
-b_factor_cutoff = None
-  .type = int
-  .help = '''B factor cutoff for use with MolProbity'''
-""")
+  time_limit = 120
+    .type = int
+    .help = '''Time limit (sec) for Reduce optimization'''
+
+  b_factor_cutoff = None
+    .type = int
+    .help = '''B factor cutoff for use with MolProbity'''
+
+  method = *molprobity cctbx
+    .type = choice(multi=False)
+    .help = '''Select molprobity or ccbtx nonbonded clashscore method'''
+
+  hard_minimum_nonbonded_distance = 0.0
+    .type = float
+    .help = '''Clash guard minimum distance'''
+
+  nonbonded_distance_threshold = None
+    .type = float
+
+  substitute_non_crystallographic_unit_cell_if_necessary = True
+    .type = bool
+
+  assume_hydrogens_all_missing = False
+    .type = bool
+    .help = '''If hydrogen atoms are missing, add them using phenix.reduce or
+    phenix.ready_set'''
+"""
 
 usage_string = """\
 phenix.clashscore file.pdb [params.eff] [options ...]
 
 Options:
 
-  model=input_file      input PDB file
-  keep_hydrogens=True   keep input hydrogen files (otherwise regenerate)
-  nuclear=False         use nuclear x-H distances and vdW radii
-  verbose=True          verbose text output
-  b_factor_cutoff=40    B factor cutoff for clash analysis
+  model=input_file          input PDB file
+  method=molprobity         Non-bonded clashscore method: molprobity or cctbx
+Option for molprobity clashscore:
+  keep_hydrogens=True       keep input hydrogen files (otherwise regenerate)
+  nuclear=False             use nuclear x-H distances and vdW radii
+  verbose=True              verbose text output
+  b_factor_cutoff=40        B factor cutoff for clash analysis
+Option for cctbx clashscore:
+  cif=input_file            optional cif file
+  assume_hydrogens_all_missing=False
+                            geometry restraints manager assume that model
+                            contains hydrogens.
 
 Example:
 
@@ -49,25 +84,60 @@ Example:
 """
 
 def run (args, out=sys.stdout, quiet=None) :
+  """
+  Calculates nonbonded clashscore using MolProbity (PROBE) or CCTBX
+  """
   cmdline = iotbx.phil.process_command_line_with_files(
     args=args,
-    master_phil=get_master_phil(),
+    master_phil_string=master_phil_str,
     pdb_file_def="model",
+    cif_file_def="cif",
     usage_string=usage_string)
   params = cmdline.work.extract()
+
   if (params.model is None) :
     raise Usage(usage_string)
-  pdb_in = cmdline.get_file(params.model, force_type="pdb")
-  hierarchy = pdb_in.file_object.hierarchy
-  result = mmtbx.validation.clashscore.clashscore(
-    pdb_hierarchy=hierarchy,
-    keep_hydrogens=params.keep_hydrogens,
-    nuclear=params.nuclear,
-    out=out,
-    verbose=params.verbose and not quiet,
-    b_factor_cutoff=params.b_factor_cutoff)
-  if params.verbose:
-    result.show_old_output(out=out)
+  if params.method == 'molprobity':
+    pdb_in = cmdline.get_file(params.model, force_type="pdb")
+    hierarchy = pdb_in.file_object.hierarchy
+    result = mmtbx.validation.clashscore.clashscore(
+      pdb_hierarchy=hierarchy,
+      keep_hydrogens=params.keep_hydrogens,
+      nuclear=params.nuclear,
+      out=out,
+      verbose=params.verbose and not quiet,
+      b_factor_cutoff=params.b_factor_cutoff)
+    if params.verbose:
+      result.show_old_output(out=out)
+  else:
+    assert params.method == 'cctbx'
+    pdb_file_name = [x for x in args if x.endswith('.pdb')]
+    cif_file_name = [x for x in args if x.endswith('.cif')]
+    assert pdb_file_name
+    files = [pdb_file_name[0]]
+    if cif_file_name:
+        files.append(cif_file_name[0])
+
+    pdb_processed_file = pdb_inter.run(
+      args=files,
+      assume_hydrogens_all_missing=params.assume_hydrogens_all_missing,
+      hard_minimum_nonbonded_distance=params.hard_minimum_nonbonded_distance,
+      nonbonded_distance_threshold=params.nonbonded_distance_threshold,
+      substitute_non_crystallographic_unit_cell_if_necessary
+      =params.substitute_non_crystallographic_unit_cell_if_necessary,
+      log=null_out())
+
+    grm = pdb_processed_file.geometry_restraints_manager()
+    xrs = pdb_processed_file.xray_structure()
+    sites_cart = xrs.sites_cart()
+    site_labels = xrs.scatterers().extract_labels()
+    hd_sel = xrs.hd_selection()
+
+    clash_score.info(
+      geometry_restraints_manager=grm,
+      sites_cart=sites_cart,
+      site_labels=site_labels,
+      hd_sel=hd_sel).show(log=out)
 
 if (__name__ == "__main__") :
   run(sys.argv[1:])
