b72787d81e8e549529a83ca21d9e74e57c530708
Oliver Zeldin <oliver.zeldin@gmail.com>
2015-01-21 23:30:21 +0000

    Moving graph processing code to SVN. Left the .git folder in place for archival.

---
+++ xfel/graph_proc/components.py
@@ -0,0 +1,231 @@
+""" Module for treating images as the vertices of a graph. Includes both Edge and Vertex (ImageNode) classes. """
+from __future__ import division
+from scitbx.matrix import sqr
+from cctbx.uctbx import unit_cell
+import numpy as np
+import math
+import logging
+from cctbx.array_family import flex
+from xfel.clustering.singleframe import SingleFrame
+
+class ImageNode(SingleFrame):
+  """ Extends a SingleFrame object to make it a vertex in a graph. Adds a list
+   of edges.
+  """
+
+  def __init__(self, *args, **kwargs):
+    """ Constructor is same as for SingleFrame object, but has additional
+    kwargs:
+    :param kwargs['scale']: default True. Specifies if the images should be scaled upon creation. Mainly switched off for testing.
+    :param kwargs['use_b']: default True. If false, only initialise the scale factor, not the B factor.
+    """
+    SingleFrame.__init__(self, *args, **kwargs)
+    if hasattr(self, 'miller_array'):  # i.e. if the above worked.
+      self.use_scales = kwargs.get('scale', True)
+      self.use_b = kwargs.get('use_b', True)
+      self.edges = []  # this is populated when the Graph is made.
+      self.partialities = self.calc_partiality(self.get_x0(),
+                                               update_wilson=self.use_scales)
+      self.scales = self.calc_scales(self.get_x0())
+      self.label = None  # to be used for classification after instantiation
+      self.source = None  # for testing, when we know the 'source' of the image
+      self.params = self.get_x0()
+
+
+  def calc_partiality(self, params_in, update_wilson=False):
+    """ Reuturn a partiality vector for all the reflections in self, using the
+    parameters defined in the array params.
+
+    :params: a tuple of the form appropriate for the crystal symetry, such as
+    one produced by get_x0()
+    :return: a list of partialities for the miller indicies in self.
+    """
+    from xfel.cxi.postrefine.mod_leastsqr import prep_output, \
+      get_crystal_orientation
+    from xfel.cxi.postrefine.mod_partiality \
+      import partiality_handler as PartialityHandler
+
+    # Expand to triclinic params, and unpack
+    params_all = prep_output(params_in, self.crystal_system)
+    G, B, rotx, roty, ry, rz, re, a, b, c, alpha, beta, gamma = params_all
+
+    # Create a unit cell based on params, not the initialised values.
+    try:
+      uc = unit_cell((a, b, c, alpha, beta, gamma))
+    except RuntimeError:
+      return  None
+      logging.warning("Could not create unit cell with ({}, {}, {}, {}, {}, {})"
+                      ". Resetting unit cell to original value."
+                      .format(a, b, c, alpha, beta, gamma))
+      uc = self.orientation.unit_cell()
+
+    ortho_matrix = uc.orthogonalization_matrix()
+    orientation_matrix = self.orientation.crystal_rotation_matrix()
+
+    crystal_init_orientation = get_crystal_orientation(ortho_matrix,
+                                                       orientation_matrix)
+    crystal_orientation_model = crystal_init_orientation \
+      .rotate_thru((1, 0, 0), rotx).rotate_thru((0, 1, 0), roty)
+    a_star = sqr(crystal_orientation_model.reciprocal_matrix())
+
+    # set up variables
+    miller_indices = self.miller_array.indices()
+    ph = PartialityHandler(self.wavelength, None)
+    bragg_angle_set = self.miller_array.two_theta(wavelength=self.wavelength) \
+      .data()
+    mm_predictions = self.pixel_size * self.mapped_predictions
+    alpha_angle_obs = flex.double([math.atan(abs(pred[0] - self.xbeam) /
+                                             abs(pred[1] - self.ybeam))
+                                   for pred in mm_predictions])
+    assert len(alpha_angle_obs) == len(self.miller_array.indices()), \
+      'Size of alpha angles and observations are not equal %6.0f, %6.0f' \
+      % (len(alpha_angle_obs), len(self.miller_array.indices()))
+
+    partiality = ph.calc_partiality_anisotropy_set(a_star, miller_indices, ry,
+                                                   rz, re, bragg_angle_set,
+                                                   alpha_angle_obs)
+    if update_wilson:
+      self.minus_2B, self.G, self.log_i, self.sinsqtheta_over_lambda_sq, \
+      self.wilson_err = self.init_calc_wilson(use_b_factor=self.use_b,
+                                              i_corrections=(1 / partiality))
+    return partiality
+
+  def calc_scales(self, params_in):
+    """ Calculate an array of scales based on scale, B and wavelength using the
+    equation $scale * exp(-2*B*(sin(theta)/wavelength)^2)$
+
+    Reuturn a scale vector for all the reflections in self, using the
+    parameters defined in the array params.
+
+    :params: a tuple of the form appropriate for the crystal symetry, such as
+    one produced by get_x0(). This method only uses params[0] (scale) and
+    params[1] (B)
+
+    :return: a list of scales for all the miller indicies in self
+    """
+    if self.use_scales:
+      scale = params_in[0]
+      B = params_in[1]
+      sin_sq_theta = self.miller_array.two_theta(wavelength=self.wavelength) \
+        .sin_theta_over_lambda_sq().data()
+
+      scales = scale * self.miller_array.data()
+      exp_arg = flex.double(-2 * B * sin_sq_theta)
+      return flex.double(flex.double(scales) * flex.exp(exp_arg))
+    else:
+      # Horrible way to get vector of ones...
+      return flex.double(self.miller_array.data()/self.miller_array.data())
+
+
+  def get_x0(self):
+    """
+    Return the initial estimages of parameters to be refined for this frame
+    :return: inital (G, B, rotx, roty, ry, rz, re, a, b, c, alpha, beta, gamma)
+    for lowest symetry, up to (G, B, rotx, roty, ry, rz, re, a) for cubic.
+    values, using:
+
+      - G: initial guess from linear regression
+      - B: initial guess from linear regression
+      - rotx: 0
+      - roty: 0
+      - ry: spot radius initial guess
+      - rz: spot radius initial guess
+      - re: 0.0026 (From Winkler et al. paper)
+      - [a, b, c, alpha, beta, gamma]: from initial indexing solution, dependent on crystal system:
+
+            Triclinic: (G, B, rotx, roty, ry, rz, re, a)
+            Monoclinic: (G, B, rotx, roty, ry, rz, re,a,b,c,beta])
+            Orthorhombic: (G, B, rotx, roty, ry, rz, re,a,b,c)
+            Tetragonal: (G, B, rotx, roty, ry, rz, re,a,c)
+            Trigonal or Hexagonal: (G, B, rotx, roty, ry, rz, re,a,c)
+            Cubic: (G, B, rotx, roty, ry, rz, re,a)
+    """
+    from xfel.cxi.postrefine.mod_leastsqr import prep_input
+    from xfel.cxi.postrefine.test_rs import calc_spot_radius
+
+    a_star = sqr(self.orientation.reciprocal_matrix())
+    miller_indices = self.miller_array.indices()
+    spot_radius = calc_spot_radius(a_star, miller_indices, self.wavelength)
+    x_init = [self.G, - 1 * self.minus_2B / 2, 0, 0,
+              spot_radius, spot_radius, 0.0026]
+    x_init.extend(self.uc)
+    x0_all = np.array(x_init)
+    x0 = prep_input(x0_all, self.crystal_system)
+    return x0
+
+
+class Edge:
+  """
+  .. note::
+    Developmental code. Do not use without contacting zeldin@stanford.edu
+
+  Defines an undirected edge in a graph. Contains the connecting vertices, and a weight.
+  """
+
+  def __init__(self, vertex_a, vertex_b, weight):
+    self.vertex_a = vertex_a
+    self.vertex_b = vertex_b
+    self.weight = weight
+    self.intra = None  # True means we assume it connects two edges from the
+                       # same class, False means it is an iter-class edge.
+
+  def mean_residual(self):
+    """
+    :return: the mean residual of the absolute value of the all the weigts on this edge.
+    """
+    return np.mean(np.abs(self.residuals()))
+
+  def other_vertex(self, vertex):
+    """
+    Simple method to get the other vertex along and edge.
+
+    :param vertex: a vertex that is on one end of this edge
+    :return: the vertex at the other end of the edge
+    """
+    assert vertex == self.vertex_a or vertex == self.vertex_b
+    if vertex is self.vertex_a:
+      return self.vertex_b
+    elif vertex is self.vertex_b:
+      return self.vertex_a
+
+  @staticmethod
+  def _calc_residuals(va, vb, pa, pb, sa, sb):
+
+    mtch_indcs = va.miller_array.match_indices(vb.miller_array,
+                                               assert_is_similar_symmetry=False)
+
+    va_selection = mtch_indcs.pair_selection(0)
+    vb_selection = mtch_indcs.pair_selection(1)
+
+    sp_a = pa.select(va_selection) * sa.select(va_selection)
+    sp_b = pb.select(vb_selection) * sb.select(vb_selection)
+
+    ia_over_ib = va.miller_array.data().select(va_selection) / \
+                 vb.miller_array.data().select(vb_selection)
+
+    residuals = (flex.log(sp_a) - flex.log(sp_b) - flex.log(ia_over_ib))
+    residuals = residuals.as_numpy_array()
+    #logging.debug("Mean Residual: {}".format(np.mean(residuals)))
+    return residuals[~np.isnan(residuals)]
+
+  def residuals(self):
+    """
+    Calculates the edge residual, as defined as the sum over all common miller indices of:
+      log(scale * partiality of a) - log(scale * partiality of b) - log(I_a/I_b)
+
+    :return: the residual score for this edge
+    """
+    # 1. Create flex selection array
+    # 2. Trim these so that they only contain common reflections
+    # 3. Calculate residual
+
+    partialities_a = self.vertex_a.partialities
+    partialities_b = self.vertex_b.partialities
+    scales_a = self.vertex_a.scales
+    scales_b = self.vertex_b.scales
+
+    return Edge._calc_residuals(self.vertex_a, self.vertex_b,
+                           partialities_a, partialities_b,
+                           scales_a, scales_b)
+
+
---
+++ xfel/graph_proc/multiproc_temp_parame_opt.py
@@ -0,0 +1,159 @@
+""" Tools for optimizing the parameters of the images in a graph to minimize the overall residual.
+**Author:**   Oliver Zeldin <zeldin@stanford.edu>
+"""
+from __future__ import division
+__author__ = 'zeldin'
+import logging
+import numpy as np
+from scipy.optimize import fmin_l_bfgs_b as lbfgs
+
+def loss_func(x):
+  """ return the contribution to the loss function of residual x. Placeholder for rebust methods to come """
+  return x**2
+
+
+def total_score(params, *args):
+  """ This is the function to be minimized by graph processing.
+
+  It returns the total residual given params, which is a tuple containing the parameters to refine for one image. Formated for use with the scipy minimization routines. There are thus between 5 and 12,
+
+  :param params: a list of the parameters for the minimisation as a tuple, only including vertices that have edges. See ImageNode.get_x0() for details of parameters.
+  :param *args:  a 2-tuple of the vertex to be minimised, cross_val. cross_val is a list of edge to leave out of the optimisation for cross-validation purposes.
+
+  :return: the total squared residual of the graph minimisation
+  """
+  from xfel.graph_proc.components import Edge
+  vertex = args[0]
+  cross_val = args[1]
+
+
+  assert vertex.partialities != vertex.calc_partiality(params), "params have not changed the partialities"
+  pa = vertex.calc_partiality(params)
+  sa = vertex.calc_scales(params)
+
+  # 2. Calculate all new residuals
+  total_sum = 0
+  for edge in vertex.edges:
+    if edge not in cross_val:
+      other_v = edge.other_vertex(vertex)
+      residuals = Edge._calc_residuals(vertex, other_v,
+                  pa, other_v.partialities,
+                  sa, other_v.scales)
+      total_sum += np.sum([loss_func(res) for res in residuals])
+
+
+  logging.debug("Total Score: {}".format(total_sum))
+  return total_sum
+
+
+def _calc_residuals(work_edges, test_edges):
+  """ Internal function to quickly calculate the sum of residuals for two lists of edges: work edges, and test edges. """
+
+  work_residual = 0
+  test_residual = 0
+  # Calculate total graph residual
+  for e in work_edges:
+      for r in e.residuals():
+        work_residual += loss_func(r)
+
+  for e in test_edges:
+      for r in e.residuals():
+        test_residual += loss_func(r)
+
+  return work_residual, test_residual
+
+
+def multiproc_wrapper(stuff):
+  """ Trivial wrapper for python multiprocessing """
+  args, kwargs = stuff
+  #print str(args)  + "\n" +  str(kwargs) + "\n----\n"
+  params, _, result = lbfgs(*args, **kwargs)
+  if result['warnflag'] != 0:
+    logging.warning('lbfgs failed')
+  return kwargs['args'][0], params
+
+
+def global_minimise(graph, nsteps=10, eta=10, cross_val=[None], nproc=None):
+  """
+  Perform a global minimisation on the total squared residuals on all the edges, as calculated by Edge.residuals(). Uses the L-BFGS algorithm.
+
+  Done by repetedly locally optimizing each node, and repeating this until convergence.
+
+  :param graph: the graph object to minimize.
+  :param nsteps: max number of iterations.
+  :param cross_val: a list of edge to leave out of the optimisation for cross-validation purposes.
+
+  :return: (end_residual, starting_residual) for the overall graph
+  """
+  if nproc != 1:
+    from multiprocessing import Pool
+    p = Pool(nproc)
+
+  # make test/work set
+  work_edges = []
+  test_edges = []
+  for e in graph.edges:
+    if e in cross_val:
+      test_edges.append(e)
+    else:
+      work_edges.append(e)
+
+  init_work_residual, init_test_residual = _calc_residuals(work_edges,
+                                                           test_edges)
+  logging.info("Starting work/test residual is {}/{}".format(init_work_residual,
+                                                            init_test_residual))
+
+  old_residual = float("inf")
+  new_residual = init_work_residual
+  n_int = 0
+  work_residuals = [init_work_residual]
+  test_residuals = [init_test_residual]
+  param_history = [[v.params for v in graph.members]]
+  while abs(old_residual - new_residual) > eta and n_int < nsteps:
+    all_args = []
+    if nproc != 1:
+      for v in graph.members:
+        all_args.append(((total_score, list(v.params)),  # args
+                         {'approx_grad':True, 'args':[v, cross_val], 'm': 10,
+                         'factr':1e7, 'epsilon':1e-8, 'iprint':0}))  # kwargs
+      new_params = p.map(multiproc_wrapper, all_args)
+    else:
+      new_params = []
+      for v in graph.members:
+        new_params.append(multiproc_wrapper((total_score, list(v.params)),  # args
+                         {'approx_grad':True, 'args':[v, cross_val], 'm': 10,
+                         'factr':1e7, 'epsilon':1e-8, 'iprint':0}))  # kwargs
+
+
+    # Update scales and partialities for each node that has edges.
+    for v, final_params in new_params:
+      v.params = final_params
+      v.partialties = v.calc_partiality(final_params)
+      v.scales = v.calc_scales(final_params)
+      v.G = final_params[0]
+      v.B = final_params[1]
+      if any(v.partialities == v.calc_partiality(v.get_x0())):
+        pchange = False
+      else:
+        pchange = True
+      if any(v.scales == v.calc_scales(v.get_x0())):
+        schange = False
+      else:
+        schange = True
+      logging.info("partialityi/scales changed: {}/{}".format(pchange,schange))
+    work_residual, test_residual = _calc_residuals(work_edges,
+                                                   test_edges)
+    work_residuals.append(work_residual)
+    test_residuals.append(test_residual)
+    n_int += 1
+    logging.info("Iteration {}: work/test residual is {}/{}".format(n_int,
+                                                                  work_residual,
+                                                                  test_residual))
+    if n_int == nsteps:
+      logging.warning("Reached max iterations")
+
+    old_residual = new_residual
+    new_residual = work_residual
+
+  p.terminate()
+  return work_residuals, test_residuals, param_history
