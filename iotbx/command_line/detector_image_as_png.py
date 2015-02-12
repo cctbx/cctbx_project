220c8c77811ca9d4c6a97059e61fcc27561721f0
Johan Hattne <johan@hattne.se>
2015-02-12 22:24:33 +0000

    Allow Pillow to be used in lieu of PIL See r20091.  Will this make Jenkins happy?

--- iotbx/command_line/detector_image_as_png.py
+++ iotbx/command_line/detector_image_as_png.py
@@ -40,7 +40,10 @@ class convert_image:
  def __init__(self,file_name,graphics_bin=1):#, output_file=None) :
   from rstbx.slip_viewer.slip_viewer_image_factory import SlipViewerImageFactory as ImageFactory
   from iotbx.detectors import ImageException
-  import Image
+  try :
+    from PIL import Image
+  except ImportError :
+    import Image
   try :
     img = ImageFactory(file_name)
   except ImageException, e :
