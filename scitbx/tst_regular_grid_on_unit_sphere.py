5674a97ab78b2f61fbaa84bef500a38cf018c92d
Pavel Afonine <afonine@users.noreply.github.com>
2015-02-18 19:51:11 +0000

    Rosca algorithm for uniform sampling of a unit sphere (and a test)

---
+++ scitbx/tst_regular_grid_on_unit_sphere.py
@@ -0,0 +1,11 @@
+from __future__ import division
+from scitbx import regular_grid_on_unit_sphere
+
+def run():
+  r = regular_grid_on_unit_sphere.rosca(m=9, hemisphere=False)
+  assert r.size() == 649
+  r = regular_grid_on_unit_sphere.rosca(m=9, hemisphere=True)
+  assert r.size() == 361
+
+if (__name__ == "__main__"):
+  run()
