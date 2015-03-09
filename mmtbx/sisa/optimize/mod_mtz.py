cacb6e57391d152b7907dd318cf66f2595b11fa2
Monarin Uervirojnangkoorn <monarin@users.noreply.github.com>
2015-03-09 22:51:54 +0000

    Fix formatting input miller arrays

--- mmtbx/sisa/optimize/mod_mtz.py
+++ mmtbx/sisa/optimize/mod_mtz.py
@@ -36,18 +36,19 @@ class mtz_handler(object):
     column_labels=file_content.column_labels()
     col_name=iparams.hkl.column_names.split(',')
 
-
-
     miller_arrays=reflection_file.as_miller_arrays()
     flex_centric_flags = miller_arrays[0].centric_flags().data()
     crystal_symmetry = crystal.symmetry(
         unit_cell=miller_arrays[0].unit_cell(), space_group=miller_arrays[0].space_group())
 
     #grab all required columns
-    flag_fp_found=0
-    flag_phib_found=0
-    flag_fom_found=0
-    flag_hl_found=0
+    flag_fp_found = 0
+    flag_phib_found = 0
+    flag_fom_found = 0
+    flag_hl_found = 0
+    ind_miller_array_fp = 0
+    ind_miller_array_phib = 0
+    ind_miller_array_fom = 0
     ind_miller_array_hl = 0
     for i in range(len(miller_arrays)):
       label_string = miller_arrays[i].info().label_string()
@@ -58,14 +59,17 @@ class mtz_handler(object):
         flex_fp_all=miller_arrays[i].data()
         flex_sigmas_all=miller_arrays[i].sigmas()
         flag_fp_found=1
+        ind_miller_array_fp = i
       elif labels[0]==col_name[2]:
         #grab PHIB
         flex_phib_all=miller_arrays[i].data()
         flag_phib_found=1
+        ind_miller_array_phib = i
       elif labels[0]==col_name[3]:
         #grab FOM
         flex_fom_all=miller_arrays[i].data()
         flag_fom_found=1
+        ind_miller_array_fom = i
       elif labels[0]==col_name[4]:
         #grab HLA,HLB,HLC,HLD
         flex_hl_all=miller_arrays[i].data()
@@ -84,21 +88,61 @@ class mtz_handler(object):
       print "couldn't find all required columns"
       sys.exit()
 
-    #get only indices available in the HL miller_array
-    miller_indices_hl = miller_arrays[ind_miller_array_hl].indices()
+    miller_indices_sel = miller_arrays[ind_miller_array_fp].indices()
+    print 'No. reflections for read-in miller arrays - indices:%6.0f fp:%6.0f phib:%6.0f fom:%6.0f HL:%6.0f)'%( \
+          len(miller_indices_sel), len(flex_fp_all), len(flex_phib_all), len(flex_fom_all), len(flex_hl_all))
 
-    #filter out any indices that don't match.
-    matches = miller.match_multi_indices(
-                  miller_indices_unique=miller_indices_hl,
-                  miller_indices=miller_arrays[0].indices())
+    miller_indices = flex.miller_index()
+    flex_fp = flex.double()
+    flex_sigmas = flex.double()
+    flex_phib = flex.double()
+    flex_fom = flex.double()
+    flex_hl = flex.hendrickson_lattman()
+    #format all miller arrays to the same length
+    for miller_index in miller_indices_sel:
+      fp_cn, phib_cn, fom_cn, hl_cn = (0,0,0,0)
 
-    miller_indices = flex.miller_index([miller_indices_hl[pair[0]] for pair in matches.pairs()])
-    flex_fp = flex.double([flex_fp_all[pair[1]] for pair in matches.pairs()])
-    flex_sigmas = flex.double([flex_sigmas_all[pair[1]] for pair in matches.pairs()])
-    flex_phib = flex.double([flex_phib_all[pair[0]] for pair in matches.pairs()])
-    flex_fom = flex.double([flex_fom_all[pair[0]] for pair in matches.pairs()])
-    flex_hl = flex.hendrickson_lattman([flex_hl_all[pair[0]] for pair in matches.pairs()])
+      matches = miller.match_multi_indices(
+                    miller_indices_unique=flex.miller_index([miller_index]),
+                    miller_indices=miller_arrays[ind_miller_array_fp].indices())
+      if len(matches.pairs()) > 0:
+        fp_cn = 1
+        fp = flex_fp_all[matches.pairs()[0][1]]
+        sigmas = flex_sigmas_all[matches.pairs()[0][1]]
 
+      matches = miller.match_multi_indices(
+                    miller_indices_unique=flex.miller_index([miller_index]),
+                    miller_indices=miller_arrays[ind_miller_array_phib].indices())
+      if len(matches.pairs()) > 0:
+        phib_cn = 1
+        phib = flex_phib_all[matches.pairs()[0][1]]
+
+      matches = miller.match_multi_indices(
+                    miller_indices_unique=flex.miller_index([miller_index]),
+                    miller_indices=miller_arrays[ind_miller_array_fom].indices())
+      if len(matches.pairs()) > 0:
+        fom_cn = 1
+        fom = flex_fom_all[matches.pairs()[0][1]]
+
+      matches = miller.match_multi_indices(
+                    miller_indices_unique=flex.miller_index([miller_index]),
+                    miller_indices=miller_arrays[ind_miller_array_hl].indices())
+      if len(matches.pairs()) > 0:
+        hl_cn = 1
+        hl = flex_hl_all[matches.pairs()[0][1]]
+
+      if (fp_cn + phib_cn + fom_cn + hl_cn) == 4:
+        miller_indices.append(miller_index)
+        flex_fp.append(fp)
+        flex_sigmas.append(sigmas)
+        flex_phib.append(phib)
+        flex_fom.append(fom)
+        flex_hl.append(hl)
+
+
+
+    print 'No. reflections after format - indices:%6.0f fp:%6.0f phib:%6.0f fom:%6.0f HL:%6.0f)'%( \
+          len(miller_indices), len(flex_fp), len(flex_phib), len(flex_fom), len(flex_hl))
     #get wilson_plot
     from mmtbx.scaling import xtriage
     from libtbx.utils import null_out
