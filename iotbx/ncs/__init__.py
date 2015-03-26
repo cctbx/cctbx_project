ee4d15454e4ebb74b9cd95a3fc41d8a339f9c8c5
Youval Dar <youdar@users.noreply.github.com>
2015-03-26 20:28:38 +0000

    - move ncs phil params into cctbx

--- iotbx/ncs/__init__.py
+++ iotbx/ncs/__init__.py
@@ -2,6 +2,108 @@ from __future__ import division
 import sys
 import ncs_preprocess
 
+simple_ncs_phil_params = """\
+simple_ncs_from_pdb
+  .short_caption = Simple NCS from PDB file
+{
+
+ pdb_in = None
+   .type = path
+   .help = 'Input PDB file to be used to identify ncs'
+   .short_caption = PDB file
+   .style = bold noauto OnChange:load_ncs_pdb_file file_type:pdb no_map
+ temp_dir = ""
+   .type = path
+   .help = "temporary directory (ncs_domain_pdb will be written there)"
+   .style = noauto
+ min_percent = 85.
+   .help = '''Threshold for similarity between chains, where similarity
+   define as: (number of matching res) / (number of res in longer chain)'''
+   .type = float
+   .short_caption = Min. percent identity
+   .expert_level = 0
+   .style = bold noauto
+ max_rmsd = 2.
+   .help = '''limit of rms difference between chains to be considered
+      as copies'''
+   .type = float
+    .short_caption = Max. RMSD
+   .expert_level = 0
+   .style = bold noauto
+ max_rmsd_user = None
+   .help = '''When given, overrides the max_rmsd'''
+   .type = float
+   .short_caption = User-specified Max. RMSD
+   .expert_level = 0
+   .style = bold noauto
+ ncs_domain_pdb_stem  = None
+   .type = str
+   .help = '''NCS domains will be written to ncs_domain_pdb_stem+"group_"+nn'''
+   .style = noauto
+ write_ncs_domain_pdb = False
+   .type = bool
+   .help = '''You can write out PDB files representing NCS domains for
+        density modification if you want'''
+   .style = noauto
+ domain_finding_parameters
+    .style = box auto_align noauto
+  {
+   match_radius = 4.0
+   .type = float
+   .help = '''max allow distance difference between pairs of matching
+        atoms of two residues'''
+   similarity_threshold = 0.95
+   .type=float
+   .help='''Threshold for similarity between matching chains.
+      A smaller value cause more chains to be grouped together and can lower
+      the number of common residues'''
+   min_contig_length = 10
+   .help = "segments < min_contig_length rejected"
+   .type=int
+ }
+ verbose = False
+   .type = bool
+   .help = "Verbose output"
+   .short_caption = Debugging output
+   .style = noauto
+ show_ncs_phil = False
+   .type = bool
+   .help = "Show phil parameters for the NCS groups"
+   .short_caption = Show phil parameters
+   .style = noauto
+ show_summary = False
+   .type = bool
+   .help = "Show NCS information summary"
+   .short_caption = Show NCS information summary
+   .style = noauto
+ raise_sorry = False
+   .type = bool
+   .help = "Raise sorry if problems"
+   .short_caption = raise sorry
+ debug = False
+   .type = bool
+   .help = "Debugging output"
+   .short_caption = Debugging output
+   .style = noauto
+ dry_run = False
+   .type = bool
+   .help = '''Just read in and check parameter names'''
+   .style = noauto
+ ncs_file_format = *restraints constraints
+   .type = choice(multi=False)
+   .help = '''control the .ncs file format
+                restraints:  "refinement.ncs.restraint_group"
+                constraints: "refinement.ncs.constraint_group"'''
+   .style = noauto
+ minimize_param = *chains transforms
+   .type = choice(multi=False)
+   .help = '''chains : minimize the number of NCS related chains in each NCS
+   groups.
+   transforms : minimize the number of NCS operations'''
+   .style = noauto
+    }
+"""
+
 
 def input(pdb_hierarchy_inp=None,
           pdb_inp=None,
