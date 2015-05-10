a260036e26faf2e4469d7b0f635e0ebf3d687bab
Tara Michels-Clark <tmichels-clark@lbl.gov>
2015-05-10 10:11:15 +0000

    changed filter to use a weighted average to find the peak center position, also checking for peaks at iron edge. Adjusted detector address to be more general from xtc stream. Added condition to use a one dimensional FEE spectrum.

--- xfel/cxi/cspad_ana/mod_spectrum_filter.py
+++ xfel/cxi/cspad_ana/mod_spectrum_filter.py
@@ -12,7 +12,16 @@ from libtbx import easy_pickle
 from psana import *
 
 class mod_spectrum_filter(object):
-  def __init__(self,address, spectrometer_dark_path, peak_one_position_min, peak_one_position_max, peak_two_position_min, peak_two_position_max, peak_ratio, peak_one_width, peak_two_width, normalized_peak_to_noise_ratio):
+  def __init__(self,address,
+               peak_one_position_min,
+               peak_one_position_max,
+               peak_two_position_min,
+               peak_two_position_max,
+               peak_one_width,
+               peak_two_width,
+               peak_ratio = 0.4,
+               normalized_peak_to_noise_ratio = 0.4,
+               spectrometer_dark_path = None):
     """The mod_spectrum_filter class constructor stores the parameters passed
     from the psana configuration file in instance variables.
 
@@ -25,8 +34,11 @@ class mod_spectrum_filter(object):
     #self.logger.setLevel(logging.INFO)
     #self.logging = logging
 
-    self.src = Source('DetInfo(%s)'%address)
-    self.dark = easy_pickle.load(spectrometer_dark_path)
+    self.src = Source('%s'%address)
+    if spectrometer_dark_path is not None:
+      self.dark = easy_pickle.load(spectrometer_dark_path)
+    else:
+      self.dark = None
     self.peak_one_position_min = int(peak_one_position_min)
     self.peak_one_position_max = int(peak_one_position_max)
     self.peak_two_position_min = int(peak_two_position_min)
@@ -54,9 +66,15 @@ class mod_spectrum_filter(object):
     #import pdb; pdb.set_trace()
     if (evt.get("skip_event")):
       return
-
     data = evt.get(Camera.FrameV1, self.src)
+    if data is None:
+      one_D = True
+      data = evt.get(Bld.BldDataSpectrometerV1, self.src)
+    else:
+      one_D = False
+
     timestamp = cspad_tbx.evt_timestamp(cspad_tbx.evt_time(evt)) # human readable format
+
     if data is None:
       self.nnodata +=1
       #self.logger.warning("event(): No spectrum data")
@@ -69,52 +87,71 @@ class mod_spectrum_filter(object):
 
     elif data is not None:
       self.nshots +=1
-      data = np.array(data.data16().astype(np.int32))
-      data = data - self.dark
-      data_split1 = data[:,:data.shape[1]//2]
-      data_split2 = data[:,data.shape[1]//2:]
-    # make a 1D trace of entire spectrum and each half to find peaks
-      spectrum  = np.sum(data,0)/data.shape[0]
-      spectrum1 = np.sum(data_split1,0)/data_split1.shape[0]
-      spectrum2 = np.sum(data_split2,0)/data_split2.shape[0]
+      if one_D:
+        data = np.array(data.hproj().astype(np.int32))
+        spectrum = data
+        spectrum1 = data[:data.shape[0]//2]
+        spectrum2 = data[data.shape[0]//2:]
+      else:
+        data = np.array(data.data16().astype(np.int32))
+        data_split1 = data[:,:data.shape[1]//2]
+        data_split2 = data[:,data.shape[1]//2:]
+        # make a 1D trace of entire spectrum and each half to find peaks
+        spectrum  = np.sum(data,0)/data.shape[0]
+        spectrum1 = np.sum(data_split1,0)/data_split1.shape[0]
+        spectrum2 = np.sum(data_split2,0)/data_split2.shape[0]
+      if 'dark' in locals():
+        data = data - self.dark
 
       peak_one = np.max(spectrum1)
       peak_two = np.max(spectrum2)
       peak_one_position = np.argmax(spectrum1)
-      # added 500 pixels to position since spectrum2 is from [500,1000] range
-      peak_two_position = np.argmax(spectrum2)+500
-
+      peak_two_position = np.argmax(spectrum2) + len(spectrum2)
     # define the limits of the regions between the two peaks
       peak_one_lower_limit = self.peak_one_position_min - self.peak_one_width
       peak_one_upper_limit = self.peak_one_position_max + self.peak_one_width
       peak_two_lower_limit = self.peak_two_position_min - self.peak_two_width
       peak_two_upper_limit = self.peak_two_position_max + self.peak_two_width
 
+      # the x-coordinate of the weighted center of peak region
+      weighted_peak_one_positions = []
+      for i in xrange(peak_one_lower_limit,peak_one_upper_limit):
+        weighted_peak_one_positions.append(spectrum[i]*i)
+
+      weighted_sum_peak_one = sum(weighted_peak_one_positions)
+      weighted_peak_one_center_position = weighted_sum_peak_one//sum(spectrum[peak_one_lower_limit:peak_one_upper_limit])
+
+      weighted_peak_two_positions = []
+      for i in xrange(peak_two_lower_limit,peak_two_upper_limit):
+        weighted_peak_two_positions.append(spectrum[i]*i)
+
+      weighted_sum_peak_two = sum(weighted_peak_two_positions)
+      weighted_peak_two_center_position = weighted_sum_peak_two//sum(spectrum[peak_two_lower_limit:peak_two_upper_limit])
     # normalized integrated peaks
       int_peak_one = np.sum(spectrum[peak_one_lower_limit:self.peak_one_position_max])/len(spectrum[peak_one_lower_limit:self.peak_one_position_max])
       int_peak_two = np.sum(spectrum[peak_two_lower_limit:self.peak_two_position_max])/len(spectrum[peak_two_lower_limit:self.peak_two_position_max])
-
     # normalized integrated regions between the peaks
       int_left_region = np.sum(spectrum[0:peak_one_lower_limit])/len(spectrum[0:peak_one_lower_limit])
       int_middle_region = np.sum(spectrum[peak_one_upper_limit:peak_two_lower_limit])/len(spectrum[peak_one_upper_limit:peak_two_lower_limit])
-      int_right_region = np.sum(spectrum[peak_two_lower_limit:])/len(spectrum[:peak_two_lower_limit])
+      int_right_region = np.sum(spectrum[peak_two_upper_limit:])/len(spectrum[:peak_two_upper_limit])
+      # now to do the filtering
       if peak_one/peak_two < self.peak_ratio or peak_one/peak_two > 1/self.peak_ratio:
         print "event(): too low"
         evt.put(skip_event_flag(), "skip_event")
         return
-      if peak_two_position < self.peak_two_position_min or peak_two_position > self.peak_two_position_max:
+      if weighted_peak_two_center_position < self.peak_two_position_min or weighted_peak_two_center_position > self.peak_two_position_max:
         print "event(): out of range high energy peak"
         evt.put(skip_event_flag(), "skip_event")
         return
-      if peak_one_position < self.peak_one_position_min or peak_one_position > self.peak_one_position_max:
+      if weighted_peak_one_center_position < self.peak_one_position_min or weighted_peak_one_center_position > self.peak_one_position_max:
         print "event(): out of range low energy peak"
         evt.put(skip_event_flag(), "skip_event")
         return
-      if int_left_region/int_peak_one > self.normalized_peak_to_noise_ratio:
+      if not one_D and (int_left_region/int_peak_one > self.normalized_peak_to_noise_ratio):
         print "event(): noisy left of low energy peak"
         evt.put(skip_event_flag(), "skip_event")
         return
-      if int_middle_region/int_peak_one > self.normalized_peak_to_noise_ratio:
+      if not one_D and (int_middle_region/int_peak_one > self.normalized_peak_to_noise_ratio):
         print "event(): noisy middle"
         evt.put(skip_event_flag(), "skip_event")
         return
@@ -122,8 +159,18 @@ class mod_spectrum_filter(object):
         #print "event(): noisy middle"
        # evt.put(skip_event_flag(), "skip_event")
        # return
-      if int_right_region/int_peak_two > self.normalized_peak_to_noise_ratio:
+      if not one_D and (int_right_region/int_peak_two > self.normalized_peak_to_noise_ratio):
         print "event(): noisy right of high energy peak"
+        evt.put(skip_event_flag(), "skip_event")
+        return
+
+      if one_D and (spectrum[data.shape[0]//2]>=spectrum[weighted_peak_one_center_position]):
+        print "event(): peak at iron edge"
+        evt.put(skip_event_flag(), "skip_event")
+        return
+
+      if one_D and (spectrum[data.shape[0]//2]>=spectrum[weighted_peak_two_center_position]):
+        print "event(): peak at iron edge"
         evt.put(skip_event_flag(), "skip_event")
         return
 
