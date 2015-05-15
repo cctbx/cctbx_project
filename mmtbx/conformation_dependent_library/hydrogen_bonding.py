8c181255ec563f262fe5bbb63c8fdd4a9d4b219e
Nigel W Moriarty <nwmoriarty@users.noreply.github.com>
2015-05-15 18:59:25 +0000

    added more glyco values

---
+++ mmtbx/conformation_dependent_library/hydrogen_bonding.py
@@ -0,0 +1,16 @@
+from __future__ import division
+
+hb_database = { # donor acceptor
+  ("F", "F") : 38.6, #(161.5 kJ/mol or 38.6 kcal/mol)
+  ("O", "N") :  6.9,
+  ("O", "O") :  5.0,
+  ("N", "N") :  3.1,
+  ("N", "O") :  1.9,
+  #("HOH", "OH+") : 4.3,
+  }
+
+def run():
+  print hb_database
+
+if __name__=="__main__":
+  run()#sys.argv[1])
