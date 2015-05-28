419ff956711e7a53fa88a142cb7993c347732191
Pavel Afonine <afonine@users.noreply.github.com>
2015-05-28 12:49:44 +0000

    First stab at coding correction for intensity factors (epsilon) in case of tNCS

--- mmtbx/ncs/tst_tncs.py
+++ mmtbx/ncs/tst_tncs.py
@@ -5,17 +5,20 @@ ext = boost.python.import_ext("mmtbx_ncs_ext")
 from mmtbx_ncs_ext import *
 from libtbx.test_utils import approx_equal
 
-def run():
+def esercise_00():
   p = pair(
     r = ([1,2,3,4,5,6,7,8,9]),
     t = ([10,11,12]),
     radius=13,
-    weight=14)
+    weight=14,
+    fracscat=15,
+    rho_mn=([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
   assert approx_equal(p.r,[1,2,3,4,5,6,7,8,9])
   assert approx_equal(p.t,[10,11,12])
   assert approx_equal(p.radius, 13)
   assert approx_equal(p.weight, 14)
-
+  assert approx_equal(p.fracscat, 15)
+  assert approx_equal(p.rho_mn,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
 
 if (__name__ == "__main__"):
-  run()
+  esercise_00()
