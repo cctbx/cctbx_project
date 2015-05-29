fa5ce02e2454c066ba2d6b66475eea87d487b287
Nathaniel Echols <nathaniel.echols@gmail.com>
2015-05-29 19:38:08 +0000

    refactor em_scoring, peaks plots now optionally displayed in window

--- mmtbx/ringer/em_scoring.py
+++ mmtbx/ringer/em_scoring.py
@@ -129,19 +129,15 @@ def calc_ratio(count_list, sampling_angle=5):
   zscore=(rotamer_count-mean)/(stdev+0.000000000000000000001)
   return rotamer_ratio, zscore
 
-def main (args):
-  parser = argparse.ArgumentParser()
-  parser.add_argument("files",nargs="*")
-  parser.add_argument("-s", "--Sampling_Angle", dest="sampling_angle", help="Don't mess with this unless you've also made the corresponding change in ringer. By default it is 5, which is identical to the default in ringer.", nargs='?', default=5)
-  parser.add_argument("-r", "--Residues", dest="residues")
-  args = parser.parse_args(args)
-  assert (len(args.files) > 0)
-  for file in args.files :
-    out_dir = file + ".output"
-    if (not os.path.isdir(out_dir)) :
-      os.makedirs(file+'.output')
+class main (object) :
+  def __init__ (self, file_name, sampling_angle=5, out=sys.stdout,
+      quiet=False) :
+    if (not quiet) :
+      out_dir = file_name + ".output"
+      if (not os.path.isdir(out_dir)) :
+        os.makedirs(file_name+'.output')
     Weird_residues=OrderedDict()
-    peak_count={}
+    self.peak_count={}
     residue_peak_count={}
     rotamer_ratios_residues={}
     zscores_residues={}
@@ -150,84 +146,123 @@ def main (args):
       rotamer_ratios_residues[i]=[]
       zscores_residues[i]=[]
     binned_peaks={}
-    zscores=[]
-    rotamer_ratios=[]
-
-    non_zero_thresholds=[]
-    waves, thresholds = parse_pickle(file)
-    length = len(waves)
-    peaks=OrderedDict()
+    self.zscores=[]
+    self.rotamer_ratios=[]
+    self.non_zero_thresholds=[]
+    waves, self.thresholds = parse_pickle(file_name)
+    self.length = len(waves)
+    self.peaks=OrderedDict()
         # calculate peaks and histogram
-    for threshold in thresholds:
-      print ""
-      print "===== Calculating Statistics for Threshold %.3f =====" % threshold
-      peaks[threshold]=Peaklist()
+    for threshold in self.thresholds:
+      if (not quiet) :
+        print >>out, ""
+        print >>out, "===== Calculating Statistics for Threshold %.3f =====" %\
+          threshold
+      self.peaks[threshold]=Peaklist()
       Weird_residues[threshold]=Peaklist()
-      peak_count[threshold] = [0]*72
+      self.peak_count[threshold] = [0]*72
       for i in Residue_codes:
         residue_peak_count[i][threshold]=[0]*72
       for i in waves:
-        peaks[threshold].append_lists(calculate_peaks(i, threshold))
-      for peak in peaks[threshold].get_peaks():
-        peak_count[threshold][peak.chi_value]+=1
+        self.peaks[threshold].append_lists(calculate_peaks(i, threshold))
+      for peak in self.peaks[threshold].get_peaks():
+        self.peak_count[threshold][peak.chi_value] += 1
         residue_peak_count[peak.resname][threshold][peak.chi_value]+=1
         if ((peak.chi_value<6) or (peak.chi_value>18 and peak.chi_value<30) or (peak.chi_value>42 and peak.chi_value<54) or (peak.chi_value>66)):
           Weird_residues[threshold].peaks.append(peak)
       # Calculate the binned peaks and ratios
-      binned_peaks[threshold] = calculate_binned_counts(peak_count[threshold], 60)
+      binned_peaks[threshold] = calculate_binned_counts(self.peak_count[threshold], 60)
       # print "For threshold %.3f" % threshold
       # print "Sample size = %d" % sum(binned_peaks[threshold])
       zscore_n, rotamer_ratio_n = statistic(binned_peaks[threshold])
       if rotamer_ratio_n==0:
         break
       for i in Residue_codes:
-        rotamer_ratios_residues_n, zscores_n = calc_ratio(residue_peak_count[i][threshold], args.sampling_angle)
+        rotamer_ratios_residues_n, zscores_n = calc_ratio(residue_peak_count[i][threshold], sampling_angle)
         rotamer_ratios_residues[i].append(rotamer_ratios_residues_n)
         zscores_residues[i].append(zscores_n)
-      non_zero_thresholds.append(threshold)
-      zscores.append(zscore_n)
-      rotamer_ratios.append(rotamer_ratio_n)
-      print "===== Plotting Histogram for Threshold %.3f =====" % threshold
-      plot_peaks(peak_count[threshold], file, threshold, 60, RMSD_statistic(peaks[threshold].peaks))
+      self.non_zero_thresholds.append(threshold)
+      self.zscores.append(zscore_n)
+      self.rotamer_ratios.append(rotamer_ratio_n)
+      if (not quiet) :
+        print "===== Plotting Histogram for Threshold %.3f =====" % threshold
+        plot_peaks(
+          peak_count=self.peak_count[threshold],
+          filename=file_name,
+          threshold=threshold,
+          first=60,
+          title=RMSD_statistic(self.peaks[threshold].peaks))
       # plot_rotamers(binned_peaks[threshold], file, threshold, args.first_rotamer)
     #   print "Outliers at threshold %.2f: %s" % (threshold, str(Weird_residues[threshold]))
-    print ""
-    print "===== Plotting Statistics Across Thresholds ====="
-    plot_progression(non_zero_thresholds, rotamer_ratios, file, [10*i/math.sqrt(length) for i in zscores])
+    if (not quiet) :
+      print ""
+      print "===== Plotting Statistics Across Thresholds ====="
+      plot_progression(
+        non_zero_thresholds=self.non_zero_thresholds,
+        rotamer_ratios=self.rotamer_ratios,
+        file_name=file_name,
+        zscores=self.all_scores)
     # for i in Residue_codes:
     #   plot_progression(non_zero_thresholds, rotamer_ratios_residues[i], file, zscores_residues[i], i)
-    print ""
-    print "===== Writing Pickles Out ====="
-    easy_pickle.dump(file+'.output/Outliers.pkl',Weird_residues)
-    print 'Wrote ' + file+'.output/Outliers.pkl'
-    easy_pickle.dump(file+'.output/rotamer_ratios.pkl', rotamer_ratios)
-    print 'Wrote ' + file+'.output/rotamer_ratios.pkl'
-    easy_pickle.dump(file+'.output/zscores.pkl', zscores)
-    print 'Wrote ' + file+'.output/zscores.pkl'
-    easy_pickle.dump(file+'.output/emringer_scores.pkl', [10*i/math.sqrt(length) for i in zscores])
-    print 'Wrote ' + file+'.output/emringer_scores.pkl'
-    easy_pickle.dump(file+'.output/thresholds.pkl', thresholds)
-    print 'Wrote ' + file+'.output/thresholds.pkl'
-    easy_pickle.dump(file+'.output/peak_counts.pkl', peak_count)
-    print 'Wrote ' + file+'.output/peak_counts.pkl'
-    for index, value in enumerate(zscores):
-      if value == max(zscores):
-        print ""
-        print "=====Final Statistics for Model/Map Pair====="
-        print "Optimal Threshold: %.3f" % non_zero_thresholds[index]
-        print "Rotamer-Ratio: %.3f" % rotamer_ratios[index]
-        print "Max Zscore: %.3f" % float(value)
-        print "Model Length: %d" % length
-        # print "Z-score/(length): %.8f" % (value/length)
-        print "EMRinger Score: %8f" % (10*value/math.sqrt(length))
-        break
-    #return float(10*max(zscores)/math.sqrt(length))
+      print ""
+      print "===== Writing Pickles Out ====="
+      easy_pickle.dump(file_name+'.output/Outliers.pkl',Weird_residues)
+      print 'Wrote ' + file_name+'.output/Outliers.pkl'
+      easy_pickle.dump(file_name+'.output/rotamer_ratios.pkl', self.rotamer_ratios)
+      print 'Wrote ' + file_name+'.output/rotamer_ratios.pkl'
+      easy_pickle.dump(file_name+'.output/zscores.pkl', self.zscores)
+      print 'Wrote ' + file_name+'.output/zscores.pkl'
+      easy_pickle.dump(file_name+'.output/emringer_scores.pkl', self.all_scores)
+      print 'Wrote ' + file_name+'.output/emringer_scores.pkl'
+      easy_pickle.dump(file_name+'.output/thresholds.pkl', self.thresholds)
+      print 'Wrote ' + file_name+'.output/thresholds.pkl'
+      easy_pickle.dump(file_name+'.output/peak_counts.pkl', self.peak_count)
+      print 'Wrote ' + file_name+'.output/peak_counts.pkl'
+    self.zscore_max = max(self.zscores)
+    self._zscore_max_index = self.zscores.index(self.zscore_max)
 
+  @property
+  def all_scores (self) :
+    return [ 10*z/math.sqrt(self.length) for z in self.zscores ]
+
+  @property
+  def optimal_threshold (self) :
+    return self.non_zero_thresholds[self._zscore_max_index]
+
+  @property
+  def rotamer_ratio (self) :
+    return self.rotamer_ratios[self._zscore_max_index]
+
+  @property
+  def score (self) :
+    """Overall EM-Ringer score"""
+    return (10 * self.zscore_max / math.sqrt(self.length))
+
+  def show_summary (self, out=sys.stdout) :
+    print >> out, ""
+    print >> out, "=====Final Statistics for Model/Map Pair====="
+    print >> out, "Optimal Threshold: %.3f" % self.optimal_threshold
+    print >> out, "Rotamer-Ratio: %.3f" % self.rotamer_ratio
+    print >> out, "Max Zscore: %.3f" % self.zscore_max
+    print >> out, "Model Length: %d" % self.length
+    # print "Z-score/(length): %.8f" % (value/length)
+    print >> out, "EMRinger Score: %8f" % self.score
+    return self
+
+  def draw_wx_peaks_plot (self, plot, i_threshold) :
+    threshold = self.thresholds[i_threshold]
+    plot.figure.clear()
+    _plot_peaks(
+      fig=plot.figure,
+      peak_count=self.peak_count[threshold],
+      threshold=threshold,
+      first=60,
+      title=RMSD_statistic(self.peaks[threshold].peaks))
 
 ########################################################################
 # GUI and Output
 
-def _plot_rotamers (fig, binned_output, filename, threshold, first) :
+def _plot_rotamers (fig, binned_output, threshold, first) :
   """Binned histogram"""
   colors=['blue','red']*3
   angles = range(6)
@@ -235,14 +270,14 @@ def _plot_rotamers (fig, binned_output, filename, threshold, first) :
   ax = fig.add_subplot(111)
   ax.bar(bin_angles, binned_output, align='center', color=colors, width=60)
 
-def plot_rotamers (binned_output, filename, threshold, first):
+def plot_rotamers (binned_output, threshold, first):
   import matplotlib.pyplot as plt
   fig = plt.figure(1)
   _plot_rotamers(fig, binned_output, filename, threshold, first)
   plt.savefig('%s.output/%.3f.Phenixed_Histogram.png' % (filename,threshold))
   plt.close()
 
-def _plot_peaks (fig, peak_count, filename, threshold, first, title=0):
+def _plot_peaks (fig, peak_count, threshold, first, title=0):
   # rcParams.update({'figure.autolayout': True})
   colors = ['#F15854']*6+['#5DA5DA']*13+['#F15854']*11+['#5DA5DA']*13+['#F15854']*11+['#5DA5DA']*13+['#F15854']*5
   ax = fig.add_subplot(111)
@@ -263,14 +298,14 @@ def _plot_peaks (fig, peak_count, filename, threshold, first, title=0):
 def plot_peaks (peak_count, filename, threshold, first, title=0) :
   import matplotlib.pyplot as plt
   fig = plt.figure(2, figsize=(5,4))
-  _plot_peaks(fig, peak_count, filename, threshold, first, title)
+  _plot_peaks(fig, peak_count, threshold, first, title)
   plt.savefig('%s.output/%.3f.histogram.png' % (filename,threshold))
   print 'Saved plot to %s.output/%.3f.histogram.png' % (filename,threshold)
   # print 'RMSD at threshold %.3f is %.1f' % (threshold,title)
   # print 'Wrote '+filename+'/%.3f.Phenix_allpeaks.png' % threshold
   plt.close()
 
-def _plot_progression(fig, non_zero_thresholds, rotamer_ratios, file, zscores,
+def _plot_progression(fig, non_zero_thresholds, rotamer_ratios, zscores,
     i="Total"):
   for j in range(len(zscores)):
     if zscores[j]>=0:
@@ -301,15 +336,46 @@ def _plot_progression(fig, non_zero_thresholds, rotamer_ratios, file, zscores,
   else:
     ax1.set_title("Threshold Scan", y=1.05)
 
-def plot_progression(non_zero_thresholds, rotamer_ratios, file, zscores,
+def plot_progression(non_zero_thresholds, rotamer_ratios, file_name, zscores,
     i="Total"):
   import matplotlib.pyplot as plt
   fig = plt.figure(3, figsize=(5.5,4))
-  _plot_progression(fig, non_zero_thresholds, rotamer_ratios, file, zscores, i)
-  plt.savefig('%s.output/%s.threshold_scan.png' % (file, i))
-  print 'Saved plot to %s.output/%s.threshold_scan.png' % (file, i)
+  _plot_progression(fig, non_zero_thresholds, rotamer_ratios, zscores, i)
+  plt.savefig('%s.output/%s.threshold_scan.png' % (file_name, i))
+  print 'Saved plot to %s.output/%s.threshold_scan.png' % (file_name, i)
   # print 'Wrote '+file+'/threshold_scan.png'
   plt.close()
 
+def run (args, out=sys.stdout) :
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files",nargs="*")
+    parser.add_argument("-s", "--Sampling_Angle", dest="sampling_angle", help="Don't mess with this unless you've also made the corresponding change in ringer. By default it is 5, which is identical to the default in ringer.", nargs='?', default=5)
+    parser.add_argument("-r", "--Residues", dest="residues")
+    parser.add_argument("--gui", dest="show_gui", action="store_true",
+      default=False)
+    args = parser.parse_args(args)
+    if (not args.show_gui) :
+      import matplotlib
+      matplotlib.use("Agg")
+    app = None
+    for file_name in args.files :
+      result = main(
+        file_name=file_name,
+        sampling_angle=args.sampling_angle,
+        out=out,
+        quiet=False).show_summary(out=out)
+      if (args.show_gui) :
+        import wxtbx.plots.emringer
+        import wxtbx.app
+        if (app is None) :
+          app = wxtbx.app.CCTBXApp(0)
+        f1 = wxtbx.plots.emringer.peaks_plot_frame(
+          parent=None,
+          title="Histograms for %s" % file_name)
+        f1.SetResult(result)
+        f1.Show()
+    if (args.show_gui) :
+      app.MainLoop()
+
 if __name__ == "__main__":
-  main(sys.argv[1:])
+  run(sys.argv[1:])
