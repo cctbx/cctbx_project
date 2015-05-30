cdf14dde18bc7845a3bfcae68439118f51f65127
Nathaniel Echols <nathaniel.echols@gmail.com>
2015-05-30 02:47:04 +0000

    added plot of overall statistics; more refactoring and separate cmdline tool

--- wxtbx/plots/emringer.py
+++ wxtbx/plots/emringer.py
@@ -11,11 +11,20 @@ import wx
 class peaks_plot (wxtbx.plots.plot_container) :
   pass
 
-class peaks_plot_frame (wxtbx.plots.plot_frame) :
+class emringer_plot_frame (wxtbx.plots.plot_frame) :
+  """Base class for all frames enclosing plots."""
   def __init__ (self, *args, **kwds) :
     self._result = None
     wxtbx.plots.plot_frame.__init__(self, *args, **kwds)
 
+  def SetResult (self, result) :
+    self._result = result
+    self._load_result()
+
+  def _load_result (self) :
+    raise NotImplementedError()
+
+class peaks_plot_frame (emringer_plot_frame) :
   def create_plot_panel (self) :
     return peaks_plot(self)
 
@@ -29,9 +38,8 @@ class peaks_plot_frame (wxtbx.plots.plot_frame) :
     szr.Add(self.threshold_choice, 0, wx.ALL, 5)
     self.Bind(wx.EVT_CHOICE, self.OnSetThreshold, self.threshold_choice)
 
-  def SetResult (self, result) :
-    self._result = result
-    self.threshold_choice.SetItems([ "%.3f" % x for x in result.thresholds ])
+  def _load_result (self) :
+    self.threshold_choice.SetItems(["%.3f"%x for x in self._result.thresholds])
     self._set_threshold(0)
 
   def OnSetThreshold (self, evt) :
@@ -42,4 +50,16 @@ class peaks_plot_frame (wxtbx.plots.plot_frame) :
     self._result.draw_wx_peaks_plot(
       plot=self.plot_panel,
       i_threshold=i)
+    self.plot_panel.canvas.draw()
+
+class threshold_plot (wxtbx.plots.plot_container) :
+  pass
+
+class threshold_plot_frame (emringer_plot_frame) :
+  def create_plot_panel (self) :
+    return threshold_plot(self)
+
+  def _load_result (self) :
+    assert (self._result is not None)
+    self._result.draw_wx_progression_plot(plot=self.plot_panel)
     self.plot_panel.canvas.draw()
