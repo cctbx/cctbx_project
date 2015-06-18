9c09782d9aff445ca32c00e656f2b73f3db33f1d
Tom Terwilliger <tterwill@users.noreply.github.com>
2015-06-18 01:15:12 +0000

    Add place_model_in_map

---
+++ mmtbx/regression/tst_place_model.py
@@ -0,0 +1,29 @@
+from __future__ import division
+
+import os,time 
+from mmtbx.command_line.place_model_in_map import run
+
+import libtbx.load_env
+
+pdb_file = libtbx.env.find_in_repositories(
+    relative_path="mmtbx/regression/place_model_in_map/std_full.pdb",
+    test=os.path.isfile)
+map_file= libtbx.env.find_in_repositories(
+    relative_path="mmtbx/regression/place_model_in_map/poor_map_2mFo-DFc.ccp4",
+    test=os.path.isfile)
+
+def tst_01():
+
+  from mmtbx.command_line.place_model_in_map import run
+  args=[pdb_file,map_file,"resolution=3"]
+  file_name,cc_to_map=run(args)
+  print "File: %s, CC to map: %5.2f " %(file_name,cc_to_map)
+  assert file_name=="placed_model.pdb"
+  assert "%4.2f" %(cc_to_map)=="0.76"
+
+
+if (__name__ == "__main__"):
+  t0=time.time()
+  tst_01()
+  print "Time: %6.4f"%(time.time()-t0)
+  print "test 1 OK"
