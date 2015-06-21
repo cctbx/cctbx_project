4094cd65a334618dda9a8372078874876cc39c73
Pavel Afonine <afonine@users.noreply.github.com>
2015-06-21 03:31:12 +0000

    Next generation of extensive local real-space refinemenet code

--- mmtbx/building/minimize_chain.py
+++ mmtbx/building/minimize_chain.py
@@ -74,12 +74,6 @@ master_phil = iotbx.phil.parse("""
           Strategy.  CA_only uses just CA atoms, all_atoms uses all
        .short_caption = CA only or all atoms
 
-     number_of_build_cycles = 4
-       .type = int
-       .short_caption = Build cycles
-       .help = Number of build cycles \
-           (each build cycle has several macro_cycles)
-
      number_of_macro_cycles = 5
        .type = int
        .short_caption = Number of overall cycles of minimization
@@ -100,7 +94,7 @@ master_phil = iotbx.phil.parse("""
        .short_caption = Number of trials
        .help = Number of trials
 
-     start_xyz_error = 10.0
+     start_xyz_error = 5.0
        .type = float
        .short_caption = Starting coordinate error
        .help = Starting coordinate error
@@ -130,60 +124,6 @@ def get_params(args,out=sys.stdout):
   print >>out,"\nMinimize_ca ... optimize a coarse-grain model in EM or low-resolution X-ray map"
   master_phil.format(python_object=params).show(out=out)
   return params
-
-def get_params_edits(n_ca=None,edit_file_name='bond_edits.eff',out=sys.stdout):
-
-  from mmtbx.monomer_library.pdb_interpretation import geometry_restraints_edits_str
-  edit_phil = iotbx.phil.parse(
-  """refinement.geometry_restraints.edits
-      { %s }
-  """ %geometry_restraints_edits_str
-  )
-  # Generate restraint file
-  f=open(edit_file_name,'w')
-  print >>f,"""
-  refinement.geometry_restraints.edits {
-    excessive_bond_distance_limit = 10
-  """
-
-  for i in xrange(1,n_ca+1):
-    if i+1 <=n_ca:
-     print >>f,"""
-      bond {
-
-      action = *add delete change
-      atom_selection_1 = chain E and resseq %d  and name CA
-      atom_selection_2 = chain E and resseq %d and name CA
-      distance_ideal = 3.8
-      sigma = 0.5
-      slack = None
-      symmetry_operation = None
-      }
-    """ %(i,i+1)
-
-    if i+2 <=n_ca:
-     print >>f,"""
-      bond {
-
-      action = *add delete change
-      atom_selection_1 = chain E and resseq %d  and name CA
-      atom_selection_2 = chain E and resseq %d and name CA
-      distance_ideal = 5.5
-      sigma = 0.5
-      slack = None
-      symmetry_operation = None
-      }
-    """ %(i,i+2)
-
-  print >>f,"""
-    }
-  """
-  f.close()
-  command_line = iotbx.phil.process_command_line_with_files(
-      args=[edit_file_name],master_phil=edit_phil)
-  params = command_line.work.extract()
-  edit_phil.format(python_object=params).show(out=out)
-  return params.refinement.geometry_restraints.edits
 
 def ccp4_map(crystal_symmetry, file_name, map_data):
   from iotbx import ccp4_map
@@ -218,37 +158,18 @@ def run_one_cycle(
     crystal_symmetry=None,
     params_edits=None,
     out=sys.stdout):
-
-
   hierarchy=pdb_inp.construct_hierarchy()
   hierarchy.atoms().reset_i_seq()
   xrs=pdb_inp.xray_structure_simple()
   if not pdb_string:
     pdb_string=hierarchy.as_pdb_string()
-  #
-  asc = hierarchy.atom_selection_cache()
-  sel_ca    = asc.selection(string="name CA")
-  sel_other = ~sel_ca
-  #
-
   # Initialize states accumulator
   states = mmtbx.utils.states(pdb_hierarchy=hierarchy, xray_structure=xrs)
   states.add(sites_cart = xrs.sites_cart())
-
   # Build geometry restraints
-  pi_params = monomer_library.pdb_interpretation.master_params
-  pi_params = pi_params.extract()
-  pi_params.max_reasonable_bond_distance=500
-  pi_params.nonbonded_weight=2000
-  #pi_params.peptide_link.ramachandran_restraints=True
-  #pi_params.peptide_link.rama_potential="emsley"
-  if 0:
-    pi_params.nonbonded_distance_cutoff=10
-    pi_params.vdw_1_4_factor=2.0
   processed_pdb_file = monomer_library.pdb_interpretation.process(
     mon_lib_srv              = monomer_library.server.server(),
     ener_lib                 = monomer_library.server.ener_lib(),
-    params                   = pi_params,
     raw_records              = pdb_string,
     strict_conflict_handling = True,
     force_symmetry           = True,
@@ -261,23 +182,18 @@ def run_one_cycle(
   restraints_manager = mmtbx.restraints.manager(
     geometry      = geometry,
     normalization = True)
-
   if params.control.verbose:
     geometry.write_geo_file()
-
   ear = mmtbx.refinement.real_space.expload_and_refine.run(
     xray_structure          = xrs,
     pdb_hierarchy           = hierarchy,
     map_data                = map_data,
     restraints_manager      = restraints_manager,
-    number_of_macro_cycles  = params.minimization.number_of_macro_cycles,
     target_bond_rmsd        = params.minimization.target_bond_rmsd,
     target_angle_rmsd       = params.minimization.target_angle_rmsd,
-    number_of_trials        = params.minimization.number_of_trials,
     xyz_shake               = params.minimization.start_xyz_error,
     states                  = states)
-
-  return ear.pdb_hierarchy, ear.xray_structure, ear.states, ear.scorer.target
+  return ear.pdb_hierarchy, ear.xray_structure, ear.states
 
 def run(args,
     map_data=None,
@@ -330,42 +246,21 @@ def run(args,
       pdb_string=f.getvalue()
       pdb_inp=iotbx.pdb.input(source_info=None, lines = pdb_string)
 
-  best_score=None
-  best_result=[]
-  for cycle in xrange(1,max(2,params.minimization.number_of_build_cycles+1)):
-    # and run one cycle
-    print >>out,"\nCycle %d of refinement" %(cycle)
-    pdb_hierarchy,xray_structure,states,score=run_one_cycle(
-      params=params,
-      map_data=map_data,
-      pdb_inp=pdb_inp,
-      pdb_string=pdb_string,
-      crystal_symmetry=crystal_symmetry,
-      params_edits=params_edits,
-      out=out)
-    if best_score is None or score > best_score:
-      best_score=score
-      best_result=[pdb_hierarchy,xray_structure,states,score]
-      print >>out,"Best score so far is: %7.2f" %(best_score)
-      # get new pdb string
-      from cStringIO import StringIO
-      f=StringIO()
-      if crystal_symmetry:
-        print >>f, iotbx.pdb.format_cryst1_record(
-           crystal_symmetry=crystal_symmetry)
-      print >>f, pdb_hierarchy.as_pdb_string()
-      pdb_string=f.getvalue()
+  pdb_hierarchy,xray_structure,states=run_one_cycle(
+    params=params,
+    map_data=map_data,
+    pdb_inp=pdb_inp,
+    pdb_string=pdb_string,
+    crystal_symmetry=crystal_symmetry,
+    params_edits=params_edits,
+    out=out)
 
-    # Redo pdb_inp as we are going to get the hierarchy later
-    pdb_inp=iotbx.pdb.input(source_info=None, lines = pdb_string)
-
-    if params.output_files.pdb_out:
-      f=open(params.output_files.pdb_out,'w')
-      print >>f, pdb_string
-      f.close()
-
+  if params.output_files.pdb_out:
+    f=open(params.output_files.pdb_out,'w')
+    print >>f, pdb_string
+    f.close()
   # all done
-  return best_result
+  return pdb_hierarchy,xray_structure,states
 
 if   (__name__ == "__main__"):
   args=sys.argv[1:]
