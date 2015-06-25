ee51b4f6d89216f152b1d0b7d334a94e4d6d640e
Nigel W Moriarty <nwmoriarty@users.noreply.github.com>
2015-06-25 23:24:43 +0000

    changed tests for CDL=True

--- mmtbx/building/alternate_conformations/tst_build_simple.py
+++ mmtbx/building/alternate_conformations/tst_build_simple.py
@@ -252,7 +252,9 @@ def exercise () :
   build_alt_confs_simple.run(args=args, out=null_out())
   rota_in = get_rotamers("tst_build_alt_confs_in.pdb")
   rota_out = get_rotamers("tst_build_alt_confs_out.pdb")
-  assert (rota_out == rota_in), "rotamers in %s not equal to out %s" % (
+  assert (rota_out == rota_in), "rotamers in %s not equal to out %s\n%s\n%s" % (
+    len(rota_in),
+    len(rota_out),
     rota_in,
     rota_out,
     )
