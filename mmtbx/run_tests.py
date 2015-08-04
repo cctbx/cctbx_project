fceaf8006c71d6d46e702d19e70b57216ae87040
Pavel Afonine <pafonine@users.noreply.github.com>
2015-08-04 19:35:54 +0000

    moving from prototype to actually running code, not there yet.

--- mmtbx/run_tests.py
+++ mmtbx/run_tests.py
@@ -257,7 +257,7 @@ tst_list = (
   #
   "$D/regression/tst_models_to_from_chains.py",
   "$D/regression/tst_helix_sheet_recs_as_pdb_files.py",
-  "$D/ncs/tst_tncs.py",
+  "$D/ncs/tst_ncs_pair.py",
   )
 
 def run():
