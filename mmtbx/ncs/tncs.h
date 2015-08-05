2657cca4cc4f374ec9ae9d5234552ea31cc8936e
Randy Read <randyread@users.noreply.github.com>
2015-08-05 18:36:29 +0000

    Reformat comments for clarity

--- mmtbx/ncs/tncs.h
+++ mmtbx/ncs/tncs.h
@@ -222,9 +222,10 @@ public:
       tncs_epsfac[r] = refl_epsfac; // result - tNCS epsilon factor
       if(do_target || do_gradient) {
         /* Compute effective gfun with half-triple summation rather than full
-           quadruple summation: assume NCS chosen closest to pure translation,
-           ignore other NCS+symm combinations pair off-diagonal contributions
-           from jncs>incs with jncs<incs
+           quadruple summation: 
+             assume NCS chosen closest to pure translation,
+               ignore other NCS+symm combinations 
+             pair off-diagonal contributions from jncs>incs with jncs<incs
         */
         // XXX PVA: epsn = eps = epsilon - symmetry factors, integers.
         double epsnSigmaN = epsilon[r]*tncs_epsfac[r]*SigmaN[r];
