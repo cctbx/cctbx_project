7ceb89feafef0dc1654ca44c4730642aba4aad96
Gabor Bunkoczi <gbunkoczi@users.noreply.github.com>
2015-09-02 19:05:58 +0000

    Missing code for structural_domain_search

--- mmtbx/geometry/shared_types.py
+++ mmtbx/geometry/shared_types.py
@@ -3,3 +3,13 @@ from __future__ import division
 import boost.python
 ext = boost.python.import_ext( "mmtbx_geometry_shared_types_ext" )
 from mmtbx_geometry_shared_types_ext import *
+
+def calculate_base_for_coordinates(xyzs):
+
+  if len( xyzs ) == 0:
+    return ( 0, 0, 0 )
+
+  else:
+    ( xs, ys, zs ) = xyzs.parts()
+    return ( min( xs ), min( ys ), min( zs ) )
+
