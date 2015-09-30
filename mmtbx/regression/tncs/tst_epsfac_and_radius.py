92020f5798d48b78866f895de771f6061a89a199
Pavel Afonine <pafonine@users.noreply.github.com>
2015-09-30 20:41:13 +0000

    tNCS set of tests

---
+++ mmtbx/regression/tncs/tst_epsfac_and_radius.py
@@ -0,0 +1,44 @@
+from __future__ import division
+import cctbx.array_family.flex # import dependency
+import boost.python
+ext = boost.python.import_ext("mmtbx_ncs_ext")
+import iotbx.pdb
+from mmtbx.ncs import tncs
+import libtbx.load_env
+import os, time
+
+def exercise_00(reflections_per_bin=150):
+  """
+  tncs_epsfac calculation with radius refinement
+  """
+  pdb_file = libtbx.env.find_in_repositories(
+    relative_path="mmtbx/regression/tncs/model_1.pdb",
+    test=os.path.isfile)
+  pdb_inp = iotbx.pdb.input(file_name=pdb_file)
+  xray_structure = pdb_inp.xray_structure_simple()
+  for b in [0, 50, 100, 200, 400, 800]:
+    xray_structure = xray_structure.set_b_iso(value=b)
+    print "B: %5.1f"%b
+    for d_min in [2,3,4,6,8]:
+      f_obs = abs(xray_structure.structure_factors(d_min=d_min).f_calc())
+      f_obs = f_obs.set_observation_type_xray_amplitude()
+      result = tncs.compute_eps_factor(
+        f_obs               = f_obs,
+        pdb_hierarchy       = pdb_inp.construct_hierarchy(),
+        reflections_per_bin = reflections_per_bin)
+      M2 = f_obs.second_moments_centric_acentric(
+        reflections_per_bin = reflections_per_bin)
+      M2_corr = f_obs.second_moments_centric_acentric(
+        reflections_per_bin = reflections_per_bin,
+        eps_fac = result.epsfac)
+      fmt="  d_min: %5.1f R: refined %4.1f estimate %4.1f 2nd Mom.:"
+      print fmt%(d_min, result.ncs_pairs[0].radius,
+        result.ncs_pairs[0].radius_estimate), "%s %4.2f"%M2[0], "%s %4.2f"%M2_corr[0]
+  # this shows summary for the result corresponding to last trial B and d_min
+  result.show_summary()
+
+if (__name__ == "__main__"):
+  t0 = time.time()
+  exercise_00()
+  print "Time: %6.3f"%(time.time()-t0)
+  print "OK"
---
+++ mmtbx/regression/tncs/tst_fd.py
@@ -0,0 +1,59 @@
+from __future__ import division
+import cctbx.array_family.flex # import dependency
+import boost.python
+ext = boost.python.import_ext("mmtbx_ncs_ext")
+import iotbx.pdb
+from scitbx.array_family import flex
+from mmtbx.ncs import tncs
+import libtbx.load_env
+import os, time
+
+def f_obs_and_tncs_pairs_from_pdb(file_name, reflections_per_bin):
+  pdb_file = libtbx.env.find_in_repositories(
+    relative_path="mmtbx/regression/tncs/%s"%file_name,
+    test=os.path.isfile)
+  pdb_inp = iotbx.pdb.input(file_name=pdb_file)
+  xray_structure = pdb_inp.xray_structure_simple()
+  xray_structure = xray_structure.set_b_iso(value=10)
+  f_obs = abs(xray_structure.structure_factors(d_min=2.0).f_calc())
+  f_obs.set_sigmas(sigmas = flex.double(f_obs.data().size(), 0.0))
+  reflections_per_bin = min(f_obs.data().size(), reflections_per_bin)
+  f_obs.setup_binner(reflections_per_bin = reflections_per_bin)
+  ncs_pairs = tncs.groups(
+    pdb_hierarchy    = pdb_inp.construct_hierarchy(),
+    crystal_symmetry = f_obs.crystal_symmetry()).ncs_pairs
+  tncs.initialize_rho_mn(
+    ncs_pairs       = ncs_pairs,
+    d_spacings_data = f_obs.d_spacings().data(),
+    binner          = f_obs.binner())
+  return f_obs, ncs_pairs
+
+def exercise_00(file_name, reflections_per_bin=500):
+  """
+  Finite differences test for radii.
+  """
+  f_obs, ncs_pairs = f_obs_and_tncs_pairs_from_pdb(file_name = file_name,
+    reflections_per_bin = reflections_per_bin)
+  tncs.finite_differences_grad_radius(ncs_pairs=ncs_pairs,
+    f_obs=f_obs, reflections_per_bin=reflections_per_bin, tolerance=1.e-3)
+
+def exercise_01(file_name, reflections_per_bin=5000):
+  """
+  Finite differences test for rho_mn.
+  """
+  f_obs, ncs_pairs = f_obs_and_tncs_pairs_from_pdb(file_name = file_name,
+    reflections_per_bin = reflections_per_bin)
+  tncs.finite_differences_rho_mn(ncs_pairs=ncs_pairs,
+    f_obs=f_obs, reflections_per_bin=reflections_per_bin, tolerance=0.05)
+
+def run():
+  for file_name in ["model_2.pdb", "model_4.pdb"]:
+    print file_name
+    exercise_00(file_name = file_name)
+    exercise_01(file_name = file_name)
+
+if (__name__ == "__main__"):
+  t0 = time.time()
+  run()
+  print "Time: %6.3f"%(time.time()-t0)
+  print "OK"
