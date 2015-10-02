2f1a9e34516444e683a21ef5bd17d228027ea165
Pavel Afonine <pafonine@users.noreply.github.com>
2015-10-02 19:37:03 +0000

    Exercise G-function

--- scitbx/math/tests/tst_g_function.py
+++ scitbx/math/tests/tst_g_function.py
@@ -1,20 +1,20 @@
 from __future__ import division
-from scitbx import math
-from stdlib import math as smath
+from scitbx import math as m
+from libtbx.test_utils import approx_equal
+import math
 
 def G(x):
   if(x != 0):
-    return 3*(smath.sin(x)-x*smath.cos(x))/x**3
+    return 3*(math.sin(x)-x*math.cos(x))/x**3
   else:
     return 1
 
 def exercise():
   x = 0
-  while x < 20:
-    gf = math.GfuncOfRSsqr_approx(x)
-    print "%6.4f %6.4f %6.4f"%(x, gf, G(x))
+  while x < 4.45: #undefined for larger arguments
+    gf = m.GfuncOfRSsqr_approx((x/(2.*math.pi))**2)
+    assert approx_equal(gf, G(x))
     x += 0.01
-
 
 if (__name__ == "__main__"):
   exercise()
