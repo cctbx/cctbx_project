37304aed4ecf7c91ec48fbc575f96d72a0b71fa1
Aaron S. Brewster <asbrewster@lbl.gov>
2015-10-02 17:22:23 +0000

    Move in a copy of dials/command_line/process.py since dials.process has been deperecated and we used it for xtc processing. This file will be changed to a stills-specific indexing program at some point.

--- xfel/command_line/xtc_process.py
+++ xfel/command_line/xtc_process.py
@@ -158,7 +158,7 @@ def __stupid_but_swig_safe__deepcopy__(self, memo):
   pass
 cbf_wrapper.__deepcopy__ = __stupid_but_swig_safe__deepcopy__
 
-from dials.command_line.process import Script as DialsProcessScript
+from xfel.command_line.dials_process import Script as DialsProcessScript
 class InMemScript(DialsProcessScript):
   """ Script to process XFEL data at LCLS """
   def __init__(self):
