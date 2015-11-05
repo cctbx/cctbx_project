495e29e5f9fd7707f834a9a575d70156eb0d3a27
Nicholas Sauter <nksauter@lbl.gov>
2015-11-05 00:33:49 +0000

    Toy simulator to trigger xpp data analysis.

---
+++ xfel/command_line/xpp_simulate.py
@@ -0,0 +1,49 @@
+# -*- mode: python; coding: utf-8; indent-tabs-mode: nil; python-indent: 2 -*-
+#
+# LIBTBX_SET_DISPATCHER_NAME xpp.simulate
+#
+# $Id: cxi_merge.py 22906 2015-09-15 22:32:08Z phyy-nx $
+
+from __future__ import division
+import iotbx.phil
+import sys
+
+master_phil="""
+data = None
+  .type = str
+  .multiple = True
+  .help = LCLS experiment, like xppi6113
+web {
+  user = None
+    .type = str
+  password = None
+    .type = str
+}
+speedup {
+  factor = 100
+    .type = float
+}
+runlimits = None
+  .type = ints(2)
+"""
+
+#-----------------------------------------------------------------------
+def run(args):
+  phil = iotbx.phil.process_command_line(args=args, master_string=master_phil).show()
+  work_params = phil.work.extract()
+  from xfel.xpp.simulate import phil_validation
+  phil_validation(work_params)
+  if ("--help" in args) :
+    libtbx.phil.parse(master_phil.show())
+    return
+
+  from xfel.xpp.simulate import application
+  application(work_params)
+
+if (__name__ == "__main__"):
+  result = run(args=sys.argv[1:])
+  if result is None:
+    sys.exit(1)
+
+# typical usage for experiment LI61:
+# xpp.simulate web.user=<LCLSuser> web.password=XXXXXXXX data=xppi6115 runlimits=86,144
