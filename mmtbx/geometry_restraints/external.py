910e7a6448f38046acabb7eb5b55deba5d5516b3
Oleg Sobolev <osobolev@lbl.gov>
2015-11-09 18:52:43 +0000

    remove rosetta_adaptbx

--- mmtbx/geometry_restraints/external.py
+++ mmtbx/geometry_restraints/external.py
@@ -2,7 +2,7 @@
 """
 Accessory module for interfacing phenix.refine (or similar programs) with
 various external third-party software such as Amber, AFITT, DivCon, or
-Rosetta.
+Rosetta (obsoleted and removed).
 """
 
 from __future__ import division
@@ -54,24 +54,5 @@ if (afitt_installed) :
       .expert_level = 3
     {
       include scope mmtbx.geometry_restraints.afitt.master_phil_str
-    }
-"""
-
-#
-rosetta_installed = False
-if libtbx.env.has_module("rosetta_adaptbx") :
-  build_dir = libtbx.env.under_build("rosetta_adaptbx")
-  if (build_dir is not None) and (os.path.isdir(build_dir)) :
-    rosetta_installed = True
-
-if rosetta_installed :
-  external_energy_params_str += """
-    rosetta
-      .help = Rosetta force field extension
-      .expert_level = 3
-    {
-      use_rosetta_energy = False
-        .type = bool
-      include scope rosetta_adaptbx.master_phil
     }
 """
