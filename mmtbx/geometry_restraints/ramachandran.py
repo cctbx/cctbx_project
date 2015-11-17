e7cf14928e2656298a3d766b4a844a4c46dfa932
Oleg Sobolev <olegsobolev@users.noreply.github.com>
2015-11-17 22:12:53 +0000

    Refactoring to make ramalyze spelling uniform

--- mmtbx/geometry_restraints/ramachandran.py
+++ mmtbx/geometry_restraints/ramachandran.py
@@ -107,7 +107,7 @@ class ramachandran_manager(object):
         hierarchy=selected_h,
         geometry=None):
       phi_atoms, psi_atoms = three.get_phi_psi_atoms()
-      rama_key = three.get_ramalyse_key()
+      rama_key = three.get_ramalyze_key()
       i_seqs = [atom.i_seq for atom in phi_atoms] + [psi_atoms[-1].i_seq]
       resnames = three.get_resnames()
       r_name = resnames[1]
