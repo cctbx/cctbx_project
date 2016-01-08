e4203b958c7d115583070c9a740b756a930d7f31
James Parkhurst <james.parkhurst@diamond.ac.uk>
2016-01-08 16:39:31 +0000

    Added checks in print_header and made Format class return None for beam/detctor/goniometer etc by default

--- dxtbx/command_line/print_header.py
+++ dxtbx/command_line/print_header.py
@@ -12,18 +12,34 @@ def print_header():
     format_class = Registry.find(arg)
     print 'Using header reader: %s' % format_class.__name__
     i = format_class(arg)
-    print i.get_beam()
-    print i.get_goniometer()
-    print i.get_detector()
-    print i.get_scan()
+    beam = i.get_beam()
+    goniometer = i.get_goniometer()
+    detector = i.get_detector()
+    scan = i.get_scan()
+    if beam is None:
+      print 'No beam model found'
+    else:
+      print beam
+    if detector is None:
+      print 'No detector model found'
+    else:
+      print detector
+    if goniometer is None:
+      print 'No goniometer model found'
+    else:
+      print goniometer
+    if scan is None:
+      print 'No scan model found'
+    else:
+      print scan
     from dxtbx.format.FormatMultiImage import FormatMultiImage
     if not issubclass(format_class, FormatMultiImage):
       try:
         d = i.get_raw_data()
-        d = d.set_selected((d < 0), 0)
+        d.set_selected((d < 0), 0)
         print 'Total Counts: %d' % flex.sum(d)
       except AttributeError, e:
-        print e
+        print "Could not read image data"
 
 if __name__ == '__main__':
   print_header()
