90c77489b3cb7b46ecca1cd789faf0b95a64d274
James Parkhurst <james.parkhurst@diamond.ac.uk>
2016-01-20 11:35:59 +0000

    Fixed bug in h5py string length

--- dxtbx/format/FormatHDFEigerNearlyNexus.py
+++ dxtbx/format/FormatHDFEigerNearlyNexus.py
@@ -175,11 +175,12 @@ class EigerNXmxFixer(object):
     group['module_offset'].attrs['depends_on'] = '/entry/instrument/detector/transformations/translation'
 
     # Create detector depends_on
+    depends_on = '/entry/instrument/detector/transformations/translation'
     create_scalar(
       handle['/entry/instrument/detector'],
       'depends_on',
-      'S1',
-      '/entry/instrument/detector/transformations/translation')
+      'S%d' % len(depends_on),
+      depends_on)
 
     # Add detector position
     # print "Using /entry/instrument/detector/geometry/translation/distances as transformation"
