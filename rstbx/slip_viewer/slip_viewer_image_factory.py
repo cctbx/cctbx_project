40f04443a510ade949fe589f99d6ab3697432b84
Aaron S. Brewster <asbrewster@lbl.gov>
2016-01-21 18:12:23 +0000

    Refactor to use dxtbx. Code only used in dials_regression to test iotbx and dxtbx and in iotbx/command_line/detector_image_as_png.py. The latter could be refactored to use dxtbx and this code could be moved to dials_regression in the future.

--- rstbx/slip_viewer/slip_viewer_image_factory.py
+++ rstbx/slip_viewer/slip_viewer_image_factory.py
@@ -1,5 +1,4 @@
 from __future__ import division
-import os
 import iotbx.detectors
 
 # store default ImageFactory function
@@ -11,24 +10,8 @@ def SlipViewerImageFactory(filename):
   except Exception, e:
     return defaultImageFactory(filename)
 
-# The NpyImage requires some special treatment for creating an instance
+# Use the dxtbx class as it handles all possible variance of NPY images
 def NpyImageFactory(filename):
-
-  from iotbx.detectors.npy import NpyImage
-  from spotfinder.applications.xfel import cxi_phil
-  args = [filename,
-          "distl.detector_format_version=CXI 7.1",
-          "viewer.powder_arcs.show=False",
-          "viewer.powder_arcs.code=3n9c",
-         ]
-  params = cxi_phil.cxi_versioned_extract(args)
-  horizons_phil = params.persist.commands
-  if isinstance(filename, basestring) and os.path.isfile(filename):
-    I = NpyImage(filename)
-  else:
-    print "This is not a file; assume the data are in the defined dictionary format"
-    I = NpyImage(filename, source_data=params.indexing.data)
-  I.readHeader(horizons_phil)
-  I.translate_tiles(horizons_phil)
-  return I
-
+  from dxtbx.format.FormatPYunspecified import FormatPYunspecified
+  img = FormatPYunspecified(filename)
+  return img.get_detectorbase()
