577168c8dd23a6fb9a23756bb3b79fff7db3b769
Pavel Afonine <afonine@users.noreply.github.com>
2016-01-28 02:28:11 +0000

    Enable non-uniform bulk-solvent model with any number of (ksol,Bsol) and Fmask. Not in production yet. Simply code that makes it possible.

--- mmtbx/f_model/f_model.h
+++ mmtbx/f_model/f_model.h
@@ -96,13 +96,12 @@ class core
 {
   public:
     af::shared<ComplexType>                f_calc;
-    FloatType                              b_sol;
     af::shared<ComplexType>                f_part1;
     af::shared<ComplexType>                f_part2;
     scitbx::sym_mat3<FloatType>            u_star;
     af::shared<cctbx::miller::index<> >    hkl;
     cctbx::uctbx::unit_cell                uc;
-    af::shared<FloatType>                  ss, f_b_sol;
+    af::shared<FloatType>                  ss;
     af::shared<ComplexType>                f_model, f_bulk, f_mask_one;
     mat3<FloatType>                        a;
     af::shared<FloatType>                  k_isotropic;
@@ -121,9 +120,19 @@ class core
       return k_sols_[i];
     }
 
-    af::small<FloatType,max_n_shells> k_sols() const
+    FloatType b_sol(unsigned short i) const
+    {
+      return b_sols_[i];
+    }
+
+    af::shared<FloatType> k_sols() const
     {
       return k_sols_;
+    }
+
+    af::shared<FloatType> b_sols() const
+    {
+      return b_sols_;
     }
 
     af::shared<ComplexType> f_mask() const
@@ -137,18 +146,17 @@ class core
       return shell_f_masks_[i];
     }
 
-    af::small<af::shared<ComplexType>,max_n_shells> shell_f_masks() const
+    af::shared<af::shared<ComplexType> > shell_f_masks() const
     {
       return shell_f_masks_;
     }
 
-
     core() {}
 
     core(af::shared<ComplexType>                const& f_calc_,
-         af::small< af::shared<ComplexType>, max_n_shells> const& f_masks,
-         af::small<FloatType, max_n_shells>     const& k_sols,
-         FloatType                              const& b_sol_,
+         af::shared< af::shared<ComplexType> > const& f_masks,
+         af::shared<FloatType>     const& k_sols,
+         af::shared<FloatType>     const& b_sols,
          af::shared<ComplexType>                const& f_part1_,
          af::shared<ComplexType>                const& f_part2_,
          scitbx::sym_mat3<FloatType>            const& u_star_,
@@ -157,11 +165,10 @@ class core
          af::shared<FloatType>                  const& ss_)
     :
       f_calc(f_calc_),
-      b_sol(b_sol_), u_star(u_star_), hkl(hkl_),uc(uc_), ss(ss_),
+      u_star(u_star_), hkl(hkl_),uc(uc_), ss(ss_),
       k_anisotropic(hkl_.size(), af::init_functor_null<FloatType>()),
       f_bulk(hkl_.size(), af::init_functor_null<ComplexType>()),
       f_model(hkl_.size(), af::init_functor_null<ComplexType>()),
-      f_b_sol(hkl_.size(), af::init_functor_null<FloatType>()),
       a(uc_.fractionalization_matrix()),
       f_part1(f_part1_), f_part2(f_part2_)
     {
@@ -171,29 +178,32 @@ class core
       MMTBX_ASSERT(f_calc.size() == f_part1_.size());
       MMTBX_ASSERT(f_calc.size() == f_part2_.size());
       MMTBX_ASSERT(k_sols.size() == f_masks.size());
+      MMTBX_ASSERT(k_sols.size() == b_sols.size());
       k_sols_ = k_sols;
+      b_sols_ = b_sols;
       shell_f_masks_ = f_masks;
       MMTBX_ASSERT( this->n_shells() >= 1U );
       for(short j=0; j<shell_f_masks_.size(); ++j)
         MMTBX_ASSERT(f_calc.size() == this->shell_f_masks_[j].size());
       FloatType*   k_anisotropic_  = k_anisotropic.begin();
-      FloatType*   f_b_sol_  = f_b_sol.begin();
       FloatType*   ss__      = ss.begin();
       ComplexType* f_bulk_   = f_bulk.begin();
       ComplexType* f_model_  = f_model.begin();
       ComplexType* f_calc__  = f_calc.begin();
       ComplexType* f_part1__ = f_part1.begin();
       ComplexType* f_part2__ = f_part2.begin();
-      scitbx::af::small<ComplexType*,max_n_shells> f_masks__;
+      scitbx::af::shared<ComplexType*> f_masks__;
       for(short j=0; j<shell_f_masks_.size(); ++j)
         f_masks__.push_back(shell_f_masks_[j].begin());
       for(std::size_t i=0; i < hkl.size(); i++) {
         k_anisotropic_[i] = k_anisotropic_one_h(hkl[i], u_star);
-        f_b_sol_[i] = f_b_exp_one_h(ss__[i], b_sol);
         // TODO: optimize
-        f_bulk_[i] = f_k_bexp_f_one_h(f_masks__[0][i], f_b_sol_[i], k_sols_[0]);
-        for(short j=1; j<k_sols_.size(); ++j)
-          f_bulk_[i] +=f_k_bexp_f_one_h(f_masks__[j][i],f_b_sol_[i],k_sols_[j]);
+        FloatType fbs = f_b_exp_one_h(ss__[i], b_sols_[0]);
+        f_bulk_[i] = f_k_bexp_f_one_h(f_masks__[0][i], fbs, k_sols_[0]);
+        for(short j=1; j<k_sols_.size(); ++j) {
+          FloatType fbs = f_b_exp_one_h(ss__[i], b_sols_[j]);
+          f_bulk_[i] +=f_k_bexp_f_one_h(f_masks__[j][i],fbs,k_sols_[j]);
+        }
         f_model_[i] = f_model_one_h(
           f_calc__[i], f_bulk_[i], f_part1_[i], f_part2__[i], k_anisotropic_[i]);
       }
@@ -253,9 +263,9 @@ class core
        {
          return k_anisotropic_h * (f_calc_h + f_bulk_h + f_part1_h + f_part2_h);
        }
-
-       scitbx::af::small<af::shared<ComplexType>, max_n_shells> shell_f_masks_;
-       scitbx::af::small<FloatType,max_n_shells> k_sols_;
+       scitbx::af::shared<af::shared<ComplexType> > shell_f_masks_;
+       scitbx::af::shared<FloatType> k_sols_;
+       scitbx::af::shared<FloatType> b_sols_;
 };
 
 template <typename FloatType=double,
--- mmtbx/f_model/f_model_ext.cpp
+++ mmtbx/f_model/f_model_ext.cpp
@@ -17,7 +17,7 @@ namespace {
     return boost::python::make_tuple(self.f_calc,
                                      self.f_mask(),
                                      self.k_sols(),
-                                     self.b_sol,
+                                     self.b_sols(),
                                      self.f_part1,
                                      self.f_part2,
                                      self.u_star,
@@ -26,7 +26,6 @@ namespace {
                                      self.f_model,
                                      self.f_bulk,
                                      self.k_anisotropic,
-                                     self.f_b_sol,
                                      self.ss,
                                      self.f_model_no_aniso_scale);
   }
@@ -37,21 +36,22 @@ namespace {
     using boost::python::arg;
 
     boost::python::to_python_converter<
-      af::small< af::shared<std::complex<double> >, max_n_shells> ,
+      af::shared< af::shared<std::complex<double> > > ,
       scitbx::boost_python::container_conversions::to_tuple<
-        af::small< af::shared<std::complex<double> >, max_n_shells>   > >();
+        af::shared< af::shared<std::complex<double> > >   > >();
 
     scitbx::boost_python::container_conversions::from_python_sequence<
-      af::small< af::shared<std::complex<double> >, max_n_shells>,
+      af::shared< af::shared<std::complex<double> > >,
       scitbx::boost_python::container_conversions::fixed_capacity_policy>();
+
 
     typedef return_value_policy<return_by_value> rbv;
     class_<core<> >("core")
       .def(init<
            af::shared<std::complex<double> >      const&,
-           af::small< af::shared<std::complex<double> >, max_n_shells > const&,
-           af::small< double, max_n_shells>       const&,
-           double                                 const&,
+           af::shared< af::shared<std::complex<double> > > const&,
+           af::shared< double>       const&,
+           af::shared< double >       const&,
            af::shared<std::complex<double> >      const&,
            af::shared<std::complex<double> >      const&,
            scitbx::sym_mat3<double>               const&,
@@ -61,7 +61,7 @@ namespace {
                                                         (arg("f_calc"),
                                                          arg("shell_f_masks"),
                                                          arg("k_sols"),
-                                                         arg("b_sol"),
+                                                         arg("b_sols"),
                                                          arg("f_part1"),
                                                          arg("f_part2"),
                                                          arg("u_star"),
@@ -84,7 +84,6 @@ namespace {
                                           arg("f_part1"),
                                           arg("f_part2"))))
       .add_property("f_calc",        make_getter(&core<>::f_calc,       rbv()))
-      .add_property("b_sol",         make_getter(&core<>::b_sol,        rbv()))
       .add_property("f_part1",       make_getter(&core<>::f_part1,      rbv()))
       .add_property("f_part2",       make_getter(&core<>::f_part2,      rbv()))
       .add_property("u_star",        make_getter(&core<>::u_star,       rbv()))
@@ -93,12 +92,13 @@ namespace {
       .add_property("f_model",       make_getter(&core<>::f_model,      rbv()))
       .add_property("f_bulk",        make_getter(&core<>::f_bulk,       rbv()))
       .add_property("k_anisotropic", make_getter(&core<>::k_anisotropic,rbv()))
-      .add_property("f_b_sol",       make_getter(&core<>::f_b_sol,      rbv()))
       .add_property("ss",            make_getter(&core<>::ss,           rbv()))
       .add_property("f_model_no_aniso_scale", make_getter(&core<>::f_model_no_aniso_scale, rbv()))
       .def("n_shells", &core<>::n_shells)
       .def("k_sol", &core<>::k_sol)
       .def("k_sols", &core<>::k_sols)
+      .def("b_sols", &core<>::b_sols)
+      .def("b_sols", &core<>::b_sols)
       .def("f_mask", &core<>::f_mask)
       .def("shell_f_mask", &core<>::shell_f_mask)   // XXX re-name to f_mask
       .def("shell_f_masks", &core<>::shell_f_masks) // XXX re-name to f_masks
