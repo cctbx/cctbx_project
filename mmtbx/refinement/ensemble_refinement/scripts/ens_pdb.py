8a89db912591aaa35c3815588dc9ca4d565783de
Tom Burnley <tomburnley@users.noreply.github.com>
2016-02-08 11:01:20 +0000

    Add scripts for downsampling ensembles and pymol tools

---
+++ mmtbx/refinement/ensemble_refinement/scripts/ens_pdb.py
@@ -0,0 +1,376 @@
+# PDB manipulation tools for ensemble models
+# Tom Burnley
+
+import sys, os.path, re, pickle
+
+def remove_HOH(start_pdb, noHOH_pdb):
+
+  print "Removing HOH..."
+
+  openpdb = open(start_pdb,"r")
+  noHOHpdb = open(noHOH_pdb, 'w')
+  find_HOH = re.compile('HOH')
+
+  for line in openpdb:
+    if not find_HOH.search(line):
+      noHOHpdb.write(line)
+
+def find_number_model(open_pdb):
+  find_MODEL = re.compile('MODEL')
+  max_model_number = 0
+  for line in open_pdb:
+    if find_MODEL.match(line):
+      model_number = int(line.split()[-1])
+      if model_number > max_model_number:
+        max_model_number = model_number
+  
+  print "Number of models in PDB file : ", max_model_number
+  return max_model_number
+
+def find_header(open_pdb, num_pdb):  
+  print "Getting header info..................."
+  find_CRYST1 = re.compile('CRYST')
+  find_SCALE = re.compile('SCALE')
+  for line in open_pdb:
+    if find_CRYST1.search(line):
+      num_pdb.write(line)
+    if find_SCALE.search(line):
+      num_pdb.write(line)
+
+def model_parse(open_pdb, num_pdb, model_num, new_model_num, start_pdb):
+  model_num = str(model_num)
+  find_MODEL = re.compile('MODEL')
+  find_ENDMDL = re.compile('ENDMDL')
+  open_pdb = open(start_pdb,"r")
+  for line in open_pdb:
+    if find_MODEL.search(line):
+      items = line.split()
+      if items[1] == model_num:
+        newN = str(new_model_num)
+        if new_model_num < 10:
+          new_line = str('MODEL        '+ newN+'\n')
+        elif new_model_num < 100:
+          new_line = str('MODEL       '+ newN+'\n')
+        elif new_model_num < 1000:
+          new_line = str('MODEL      '+ newN+'\n')  
+        num_pdb.write(new_line)
+        for line in open_pdb:  
+          if find_ENDMDL.search(line):
+            num_pdb.write(line)
+            return
+          else:
+            num_pdb.write(line)
+
+def remove_specific_HOH(open_pdb,open_water_list,wat_pdb):
+  water_list = []
+  for wat_num in open_water_list:
+    x = wat_num.split()
+    water_list.append(x[0])
+  
+  find_HOH = re.compile('HOH')
+  
+  for line in open_pdb:
+    if not find_HOH.search(line):
+      wat_pdb.write(line)
+    else:
+      linesplit = line.split()
+      if (len(linesplit) > 5):
+        if (linesplit[4] in water_list) or (linesplit[5] in water_list):
+          print linesplit
+          wat_pdb.write(line)
+
+def parse_bfactor_occ_infomation(open_pdb, start_pdb):
+  print "Parsing B and Occ information"
+  find_MODEL = re.compile('MODEL')
+  find_ENDMDL = re.compile('ENDMDL')
+  #
+  b_total_array = []
+  b_model_array = []
+  q_total_array = []
+  q_model_array = []
+  for line in open_pdb:
+    if find_MODEL.search(line):
+      if len(b_model_array) > 0:
+        b_total_array.append(b_model_array)
+        b_model_array=[]
+        q_total_array.append(q_model_array)
+        q_model_array=[]
+    items = line.split()
+    if items[0] == 'ATOM' or items[0] == 'HETATM':
+      b_model_array.append(float(items[-3]))
+      q_model_array.append(float(items[-4]))
+  #Save last model b's
+  b_total_array.append(b_model_array)
+  q_total_array.append(q_model_array)
+  b_and_q={'b_array' : b_total_array, 'q_array' : q_total_array}
+  #Pickle b_and_q information
+#  pickle_name = start_pdb+"_b_q_pickle.pkl"
+#  pickle_jar = open(pickle_name, 'wb')
+#  pickle.dump(b_and_q, pickle_jar)
+#  pickle_jar.close()
+  print b_and_q['b_array']
+
+def parse_specific_pdb(open_pdb, start_pdb, num_pdb, last_model, new_model_num=1):
+  model_num = str(last_model)
+  find_MODEL = re.compile('MODEL')
+  find_ENDMDL = re.compile('ENDMDL')
+  open_pdb = open(start_pdb,"r")
+  for line in open_pdb:
+    if find_MODEL.search(line):
+      items = line.split()
+      if items[1] == model_num:
+        newN = str(new_model_num)
+        if new_model_num < 10:
+          new_line = str('MODEL        '+ newN+'\n')
+        elif new_model_num < 100:
+          new_line = str('MODEL       '+ newN+'\n')
+        elif new_model_num < 1000:
+          new_line = str('MODEL      '+ newN+'\n')  
+          
+        num_pdb.write(new_line)
+        for line in open_pdb:  
+          if find_ENDMDL.search(line):
+            num_pdb.write(line)
+            return
+          else:
+            num_pdb.write(line)
+
+def col_swap(open_pdb, open_second_pdb, get_col, replace_col, new_pdb):
+  if get_col == 'q':
+    get_range = [54,60]
+  else:
+    get_range = [60,66]
+  
+  if replace_col == 'q':
+    replace_range = [54,60]
+  else:
+    replace_range = [60,66]
+  
+  get_col_info=[]
+  for line in open_second_pdb:
+    if len(line) > 66:
+      get_col_info.append(line[get_range[0]:get_range[1]])
+  
+  n = 0
+  for line in open_pdb:
+    if len(line) > 66:
+      new_pdb.write((line[0:(replace_range[0])] + get_col_info[n] + line[(replace_range[1]):]))
+      n+=1
+    else:
+      new_pdb.write(line)
+  
+  print "Replaced : ",  replace_col, " with : ", get_col
+  
+def remove_anisou(start_pdb, fin_pdb):
+  print "Removing ANISOU..."
+  openpdb = open(start_pdb,"r")
+  finpdb = open(fin_pdb, 'w')
+  find_ANISOU = re.compile('ANISOU')
+
+  for line in openpdb:
+    if not find_ANISOU.search(line):
+      finpdb.write(line)
+
+def phxgro_to_phx(start_pdb, fin_pdb):
+  openpdb = open(start_pdb,"r")
+  finpdb = open(fin_pdb, 'w')
+  
+  #Change res names
+  residue_to_change = {'CY2':'CYS', 'HIB':'HIS', 'LYH':'LYS', 'TRY':'TRP'}
+  
+  for line in openpdb:
+    if len(line) > 66:
+      residue_name =  line[17:20]
+      if residue_name in residue_to_change:
+        line = line[0:17] + residue_to_change[residue_name] + line[20:]
+      if residue_name == 'HOH':
+        water_atom_name = line[13:16]
+        new_w_a_n = water_atom_name.replace('W', "")
+        new_w_a_n = new_w_a_n + " "
+        line = line[0:13] + new_w_a_n + line[16:]
+
+    finpdb.write(line)
+
+def phx_to_phxgro(start_pdb, fin_pdb):
+  openpdb = open(start_pdb,"r")
+  finpdb = open(fin_pdb, 'w')
+  
+  #Change res names
+  residue_to_change = {'CYS':'CY2', 'HIS':'HIB', 'LYS':'LYH', 'TRP':'TRY'}
+  
+  for line in openpdb:
+    if len(line) > 66:
+      residue_name =  line[17:20]
+      if residue_name in residue_to_change:
+        line = line[0:17] + residue_to_change[residue_name] + line[20:]
+      if residue_name == 'HOH':
+        water_atom_name = line[13:16]
+        if water_atom_name == "O  ":
+          new_w_a_n = "OW "
+        if water_atom_name == "H1 ":
+          new_w_a_n = "HW1"
+        if water_atom_name == "H2 ":
+          new_w_a_n = "HW2"
+        line = line[0:13] + new_w_a_n + line[16:]
+
+    finpdb.write(line)
+
+def extra_conformation(start_pdb, fin_pdb):
+  open_pdb = open(start_pdb,"r")
+  fin_pdb = open(fin_pdb, 'w')
+  
+  find_ATOM = re.compile('ATOM')
+  
+  for line in open_pdb:
+    if len(line) > 66 and find_ATOM.search(line):
+      print line
+      new_lineA = line[0:16] + 'A' + line[17:56] + '0.50' + line[60:]
+      new_lineB = line[0:16] + 'B' + line[17:56] + '0.50' + line[60:]
+      print new_lineA
+      print new_lineB
+      fin_pdb.write(new_lineA)
+      fin_pdb.write(new_lineB)
+    else:
+      fin_pdb.write(line)
+def main():
+  
+  print "\n\n\
+=========================== Ensemble PDB Display Tools ========================="
+  if len(sys.argv) < 2:
+    print "\n\nSpecify pdb file at command line:\n"
+    print "    python ens_pdb.py my.pdb\n"
+    return
+  else:
+    print "1. Downsample ensemble"
+    print "2. Remove explicit solvent atoms"
+    print "3. Downsample and remove solvent"
+#    print "4. Remove specific water from list"
+#    print "5. Parse B-factor and Occupancy info"
+#    print "6. Last ensemble model to single pdb"
+#    print "7. Swap B and Q cols"
+#    print "8. Remove ANISOU lines"
+#    print "9. Convert phx_gro to phx"
+#    print "10. Convert phx to phx_gro"
+#    print "11. Add extra conformation"
+    option = str(raw_input("Input option number : "))
+    
+    if option == str("1") or option == str("3"):
+      start_pdb = sys.argv[1]
+      open_pdb = open(start_pdb,"r")
+      
+      number_models = find_number_model(open_pdb)
+      
+      target_number = raw_input("Number models for output (default 25) : ")
+      try:
+        target_number = int(target_number)
+        div = int(number_models / target_number)
+      except ValueError:
+        target_number = 25
+        div = int(number_models / 25)
+      
+      fin_pdb = str(str(target_number) + "_dwnsmp_" + start_pdb)
+      num_pdb = open(fin_pdb, 'w')
+      
+      open_pdb = open(start_pdb,"r")
+      find_header(open_pdb = open_pdb, num_pdb = num_pdb)
+      
+      model_num = int(1)
+      new_model_num = int(1)
+      
+      while new_model_num <= target_number:
+        print "Parsing model  :", model_num
+        model_parse(open_pdb, num_pdb, model_num, new_model_num, start_pdb)
+        model_num = model_num + div
+        new_model_num += 1
+      
+      num_pdb.close()
+      if option == str("1"):
+        return
+      
+    if option == str("2"):
+      start_pdb = sys.argv[1]
+      fin_pdb = str("noHOH_" + start_pdb)
+      remove_HOH(start_pdb, fin_pdb)
+      return
+
+    if option == str("3"):
+      noHOH_pdb = str("noHOH_" + fin_pdb)
+      remove_HOH(fin_pdb, noHOH_pdb)
+      return
+    
+    if option == str("4"):
+      print "Remove specific HOH"
+      if len(sys.argv) < 3:
+        print "\n\nERROR - specify water list at command line after PDB file!\n\n"
+        main()
+      start_pdb = sys.argv[1]
+      open_pdb = open(start_pdb,"r")
+      water_list = sys.argv[2]
+      open_water_list = open(water_list,"r")
+      wat_pdb = open('goodHOH.pdb', 'w')
+      remove_specific_HOH(open_pdb,open_water_list,wat_pdb)
+    
+    if option == str("5"):
+      start_pdb = sys.argv[1]
+      open_pdb = open(start_pdb,"r")
+      parse_bfactor_occ_infomation(open_pdb, start_pdb)
+
+    if option == str("6"):
+      start_pdb = sys.argv[1]
+      open_pdb = open(start_pdb,"r")
+      last_model = raw_input("Parse x model)...     ")
+      fin_pdb = str(last_model + "_" + start_pdb)
+      num_pdb = open(fin_pdb, 'w')
+      find_header(open_pdb, num_pdb)
+      parse_specific_pdb(open_pdb, start_pdb, num_pdb, last_model, new_model_num=1)
+    
+    if option == str("7"):
+      start_pdb = sys.argv[1]
+      open_pdb = open(start_pdb,"r")
+      if len(sys.argv) > 2:
+        second_pdb = sys.argv[2]
+      else:
+        second_pdb = raw_input("Second pdb import infomation from..... : ")
+      open_second_pdb = open(second_pdb,"r")
+      get_col = raw_input("Import 'b' or 'q' column from second pdb  : ")
+      if get_col != 'q':
+        get_col = 'b'
+      print "Importing col : ", get_col
+      replace_col = raw_input("Replace 'b' or 'q' column from first pdb  : ")
+      if replace_col != 'q':
+        replace_col = 'b'
+      new_pdb_name = start_pdb + "_swap_" + replace_col + "_with_" + get_col + ".pdb"
+      new_pdb = open(new_pdb_name, 'w')
+      col_swap(open_pdb, open_second_pdb, get_col, replace_col, new_pdb)
+    
+    if option == str("8"):
+      start_pdb = sys.argv[1]
+      fin_pdb = str("noANISOU_" + start_pdb)
+      remove_anisou(start_pdb, fin_pdb)
+      return
+    
+    if option == str("9"):
+      start_pdb = sys.argv[1]
+      fin_pdb = str("phxgro_to_phx_" + start_pdb)
+      phxgro_to_phx(start_pdb, fin_pdb)
+      return
+            
+    if option == str("10"):
+      start_pdb = sys.argv[1]
+      fin_pdb = str("phx_to_phxgro" + start_pdb)
+      phx_to_phxgro(start_pdb, fin_pdb)
+      return
+    
+    if option == str("11"):
+      start_pdb = sys.argv[1]
+      fin_pdb = str("extra_con" + start_pdb)
+      extra_conformation(start_pdb, fin_pdb)
+      return
+    
+    else:
+      print "\n\nPlease choose from list : "
+      main() 
+
+if __name__ == '__main__':
+  main()
+
---
+++ mmtbx/refinement/ensemble_refinement/scripts/ens_tools.py
@@ -0,0 +1,429 @@
+# Ensemble tools for pymol
+# Tom Burnley
+
+import math, sys
+from pymol import cmd, stored
+
+class LogWriter:
+      def __init__(self, stdout, filename):
+          self.stdout = stdout
+          self.logfile = file(filename, 'a')
+
+      def write(self, text):
+          self.stdout.write(text)
+          self.logfile.write(text)
+
+      def close(self):
+          self.stdout.close()
+          self.logfile.close()
+
+def print_array_stats(array = None,
+                      log = None):
+    if array is not None:
+        if len(array) == 0: array = [0.0]
+        n         = len(array)
+        mean      = sum(array) / len(array)
+        maximum   = max(array)
+        minimum   = min(array)
+        print >> log, "n         : %4d" % n
+        print >> log, "mean      : %4.3f" % mean
+        print >> log, "min       : %4.3f" % minimum
+        print >> log, "max       : %4.3f" % maximum
+
+# Return distance between two coords
+def distance(x,y):
+    return math.sqrt((x[0]-y[0])*(x[0]-y[0]) + (x[1]-y[1])*(x[1]-y[1]) + (x[2]-y[2])*(x[2]-y[2]))
+
+def ens_measure(pk1 = None,
+                pk2 = None,
+                pk3 = None,
+                pk4 = None,
+                name = None,
+                log = None,
+                verbose = True):
+    '''
+DESCRIPTION
+ 
+    Statistics from ensemble structure measurements.  If:
+      2 selections give = distance
+      3 selections give = angle
+      4 selections give = dihedral angle
+ 
+USAGE
+ 
+    ens_measure pk1, pk2, pk3, pk4, name, log, verbose
+ 
+ARGUMENTS
+ 
+    log = name of log file
+    verbose = prints individual measurements
+ 
+ EXAMPLE
+ 
+    ens_measure atom1, atom2, name = 'measure', log 'ens.log'
+  '''
+    print >> log, '\nEnsemble measurement'
+
+    if [pk1, pk2, pk3, pk4].count(None) > 2:
+        print '\nERROR: Please supply at least 2 seletions'
+        return
+    number_models = cmd.count_states(pk1)
+    measurements = []
+    
+    # distance
+    if [pk1, pk2, pk3, pk4].count(None) == 2:
+        print >> log, 'Distance'
+        if name == None: name = 'ens_distance'
+        # display as object
+        cmd.distance(name = name,
+                     selection1 = pk1,
+                     selection2 = pk2)
+
+        # get individual values
+        for n in xrange(number_models):
+            measurements.append( cmd.get_distance(pk1, pk2, n+1) )
+        assert len(measurements) == number_models
+
+    # angle
+    if [pk1, pk2, pk3, pk4].count(None) == 1:
+        print >> log, 'Angle'
+        # display as object
+        if name == None: name = 'ens_angle'
+        cmd.angle(name = name,
+                  selection1 = pk1,
+                  selection2 = pk2,
+                  selection3 = pk3)
+
+        # get individual values
+        for n in xrange(number_models):
+            measurements.append( cmd.get_angle(atom1 = pk1, 
+                                               atom2 = pk2, 
+                                               atom3 = pk3, 
+                                               state = n+1) )
+        assert len(measurements) == number_models
+
+    # Dihedral angle
+    if [pk1, pk2, pk3, pk4].count(None) == 0:
+        print >> log, 'Dihedral angle'
+        # display as object
+        if name == None: name = 'ens_dihedral'
+        cmd.dihedral(name = name,
+                  selection1 = pk1,
+                  selection2 = pk2,
+                  selection3 = pk3,
+                  selection4 = pk4)
+
+        # get individual values
+        for n in xrange(number_models):
+            measurements.append( cmd.get_dihedral(atom1 = pk1,
+                                                  atom2 = pk2,
+                                                  atom3 = pk3,
+                                                  atom4 = pk4,
+                                                  state = n+1) )
+        assert len(measurements) == number_models
+
+    # print stats
+    if verbose:
+        print >> log, ' State  Value'
+        for n, measurement in enumerate(measurements):
+            print >> log, '  %4d  %3.3f '%(n+1, measurement)
+
+    print >> log, '\nMeasurement statistics'
+    print_array_stats(array                 = measurements,
+                      log                   = log)
+
+def ens_rmsd(ens_selection,
+             ref_selection,
+             log_name = None):
+    '''
+DESCRIPTION
+ 
+    Prints RMSD per structure in ensemble w.r.t. a reference structure
+ 
+USAGE
+ 
+    ens_rmsd ensemble_selection, reference_selection, name, log,
+ 
+ARGUMENTS
+ 
+    log = name of log file
+    verbose = calculates structure by structure RMSD for ensemble w.r.t. a single reference structure
+ 
+ EXAMPLE
+ 
+    ens_rmsd ensemble_selection, reference_selection, name = 'rmsd', log 'ens.log'
+  '''
+    
+    if log_name == None:
+      log = LogWriter(sys.stdout, 'log.txt')
+    else:
+      log = LogWriter(sys.stdout, log_name+'.txt')
+    
+    # initialize arrays
+    ens_selection = ens_selection + ' and not resn hoh'
+    ref_selection = ref_selection + ' and not resn hoh'
+    rmsd_states = []
+    mean_coords = None
+    number_models = cmd.count_states(ens_selection)
+
+    # get models, mean coords
+    print >> log, '\nRMSD by state'
+    print >> log, '\n State | RMSD'
+    for i in range(number_models):
+      ens_coords = cmd.get_model(ens_selection,state=i+1).get_coord_list()
+      ref_coords = cmd.get_model(ref_selection,state=1).get_coord_list()
+      atom_sqr_dev = []
+      for atom in xrange(len(ref_coords)):
+        x = ref_coords[atom]
+        y = ens_coords[atom]
+        atom_sqr_dev.append(distance(x,y)**2)
+      rmsd = math.sqrt(sum(atom_sqr_dev) / len(atom_sqr_dev))
+      rmsd_states.append(rmsd)
+      print >> log, ' %5d | %5.3f '%(i+1, rmsd)
+    
+    
+    print_array_stats(array                 = rmsd_states,
+                      log                   = log)
+    print '\nRMSD all states : %5.3f '%(cmd.rms(ens_selection, ref_selection))
+
+def ens_rmsf(selection,
+             rmsf_spectrum = False,
+             mean_structure = False,
+             mean_per_resi = True,
+             log_name = None):
+    '''
+DESCRIPTION
+ 
+    Generates and colors structure by RMSF statistics, calculated from mulistate ensembles
+ 
+USAGE
+ 
+    ens_rmsf selection, sigma_rmsf_spectrum, mean_structure, histogram_number_bins, log_name
+ 
+ARGUMENTS
+ 
+    sigma_rmsf_spectrum = overwrite q col with sigma RMSF (per atom), color by q
+    mean_structure = generate new single state structure with mean coords
+    histogram_number_bins = number of bins for output histograms
+    log_name = name of log file
+ 
+EXAMPLE
+ 
+    ens_rmsf protein, True, True, 25, 'ens_rmsf.log'
+  '''
+    if log_name == None:
+      log = LogWriter(sys.stdout, 'log.txt')
+    else:
+      log = LogWriter(sys.stdout, log_name+'.txt')
+    print >> log, "\nEnsemble RMSF"
+    print >> log, "N.B. waters excluded"
+    print >> log, "N.B. B column information from first model"
+    print >> log, "Rmsf (Angstrom) [w.r.t mean structure]"
+    print >> log, "B_atom (Angstrom^2) [atomic Bfactor used in simulation]"
+    print >> log, "B_rmsf (Angstrom^2) [rmsf converted to Bfactor]"
+
+    # initialize arrays
+    selection = selection + ' and not resn hoh'
+    models = []
+    mean_coords = None
+    number_models = cmd.count_states(selection)
+    r_number_models = 1.0 / number_models
+
+    # get models, mean coords
+    for i in range(number_models):
+      models.append(cmd.get_model(selection,state=i+1))
+      coords_for_mean = map( lambda x: [x[0]*r_number_models,x[1]*r_number_models,x[2]*r_number_models],models[i].get_coord_list())
+      if mean_coords == None:
+        mean_coords = coords_for_mean
+      else:
+        n = []
+        for mean, for_mean in zip(mean_coords, coords_for_mean):
+          mean = map(sum, zip(mean,for_mean))
+          n.append(mean)
+        mean_coords = n
+
+    # calculate RMSF w.r.t. mean structure
+    rmsf_coord = [0.0]*len(mean_coords)
+    for i in range(number_models):
+      coord_array = models[i].get_coord_list()
+      for i_seq, xyz in enumerate(coord_array):
+        rmsf_coord[i_seq] += distance(xyz, mean_coords[i_seq])**2
+    rmsf_coord = map(lambda x: (x / number_models)**0.5, rmsf_coord)
+
+    # Generate new model object with average xyz coord
+    if mean_structure:
+      mean_structure_name = 'mean_xyz_'+selection
+      cmd.create(mean_structure_name, selection, 1)
+      stored.xyz = map(lambda v:[v[0],v[1],v[2]],mean_coords)
+      cmd.alter_state(1, mean_structure_name,'(x,y,z) = stored.xyz.pop(0)')
+
+    # convert to B factor (A^2)
+    rmsf_as_b_coord = map(lambda x: x**2 * ((8.0 * math.pi**2) / 3.0), rmsf_coord)
+
+    # get atomic b factor info
+    atom_b = [at.b for at in models[0].atom]
+    b_atom_plus_b_rsmf = map(sum,zip(atom_b,rmsf_as_b_coord))
+
+    # Colour by rmsf
+    if rmsf_spectrum:
+#      cmd.color('grey', 'all')
+#      cmd.alter('all','q=0')
+      atom = models[0].atom
+
+      for n, atom in enumerate(models[0].atom):
+        atom_sel = 'id ' + str(atom.id)
+        atom_action = 'q = ' + str(rmsf_sigma[n])
+        cmd.alter(atom_sel,atom_action)
+
+      print "\n\nQ infomation updated with RMSF sigma\n"
+      cmd.spectrum('q',selection = selection)
+
+      if mean_structure:
+        cmd.spectrum('q', selection = mean_structure_name)
+    
+    else:
+      for n, atom in enumerate(models[0].atom):
+        atom_sel = 'id ' + str(atom.id)
+        atom_action = 'q = ' + str(rmsf_coord[n])
+        cmd.alter(atom_sel,atom_action)
+
+      print "\n\nQ infomation updated with RMSF (Angstrom)\n"
+      cmd.spectrum('q',selection = selection)
+    
+    # array stats
+    print >> log, '\nB_atom (A^2): '
+    print_array_stats(array                 = atom_b,
+                      log                   = log)
+    print >> log, '\nRmsf (A): '
+    print_array_stats(array                 = rmsf_coord,
+                      log                   = log)
+    print >> log, '\nB_rmsf (A^2): '
+    print_array_stats(array                 = rmsf_as_b_coord,
+                      log                   = log)
+    print >> log, '\nB_atom + B_rmsf (A^2):'
+    print_array_stats(array                 = b_atom_plus_b_rsmf,
+                      log                   = log)
+
+    # individual atom stats
+    print >> log, '\n\n Resi | Name | Chain   | Rmsf | B_rmsf | B_atom |    B_rmsf+B_atom\n'
+    for i_seq, b_factor in enumerate(atom_b):
+      print >> log, ' %7s %7s %7s | %8.3f | %8.3f %8.3f | %8.3f'%(
+                    models[0].atom[i_seq].resi,
+                    models[0].atom[i_seq].name,
+                    models[0].atom[i_seq].chain,
+                    rmsf_coord[i_seq],
+                    rmsf_as_b_coord[i_seq],
+                    b_factor,
+                    b_atom_plus_b_rsmf[i_seq])
+
+    if mean_per_resi:
+      print >> log, '\nMean per residue'
+      # update atom to include info
+      for n, atom in enumerate(models[0].atom):
+        atom.rmsf               = rmsf_coord[n]
+        atom.b_rmsf             = rmsf_as_b_coord[n]
+        atom.b_atom_plus_b_rsmf = atom.b + atom.b_rmsf
+
+      print >> log, ' Resi Resn | Atoms | Atom_rmsf | B_atom B_rmsf B_atom+B_rmsf'
+      def print_mean_residue():
+          print >> log, ' %4s %5s|   %3d |  %8.3f | %8.3f %8.3f %8.3f '%(
+                current_resi,
+                current_resn,
+                len(res_b),
+                sum(res_rmsf) / len(res_rmsf),
+                sum(res_b) / len(res_b),
+                sum(res_b_rmsf) / len(res_b_rmsf),
+                sum(res_b_atom_plus_b_rsmf) / len(res_b_atom_plus_b_rsmf) )
+              
+      current_resi = models[0].atom[0].resi
+      current_resn = models[0].atom[0].resn
+      res_rmsf = []
+      res_b = []
+      res_b_rmsf = []
+      res_b_atom_plus_b_rsmf = []
+
+      for atom in models[0].atom:
+        if atom.resi == current_resi:
+          assert atom.resn == current_resn
+          res_rmsf.append(atom.rmsf)
+          res_b.append(atom.b)
+          res_b_rmsf.append(atom.b_rmsf)
+          res_b_atom_plus_b_rsmf.append(atom.b_atom_plus_b_rsmf)
+        else:
+          print_mean_residue()
+          current_resi = atom.resi
+          current_resn = atom.resn
+          res_rmsf = [atom.rmsf]
+          res_b = [atom.b]
+          res_b_rmsf = [atom.b_rmsf]
+          res_b_atom_plus_b_rsmf = [atom.b_atom_plus_b_rsmf]
+      print_mean_residue()
+
+def ens_prob():
+  print '\n\nEnsemble probability options'
+  # get models, mean coords
+  models = []
+  selection = 'all'
+  for i in range(cmd.count_states(selection)):
+    models.append(cmd.get_model(selection,state=i+1))
+
+  for n, model in enumerate(models):
+    residues = model.get_residues()
+    for residue in residues:
+      # Get individual atom info
+      q_list = []
+      q_list_mc  = []
+      q_list_sc = []
+      for i_seq in range (residue[0], residue[1]):
+        # Ignore hydrogens
+        if model.atom[i_seq].symbol != 'H':
+          q_list.append(float(model.atom[i_seq].q))
+          if model.atom[i_seq].name in ['N','CA','C','O']:
+            q_list_mc.append(float(model.atom[i_seq].q))
+          else:
+            q_list_sc.append(float(model.atom[i_seq].q))
+
+      # Set probability per residue
+      # Mean p
+      if len(q_list) > 0:    p_new = sum(q_list) / len(q_list)
+      if len(q_list_mc) > 0: p_new_mc = sum(q_list_mc) / len(q_list_mc)
+      if len(q_list_sc) > 0: p_new_sc = sum(q_list_sc) / len(q_list_sc)
+
+#      # Joint
+      p_new = q_list[0]
+      for p in q_list[1:]:
+        p_new *= p
+
+#      # nll
+#      p_new = math.log(q_list[0])
+#      for p in q_list[1:]:
+#        p_new += math.log(max(p,0.001))
+#      p_new *= -1
+
+      if i_seq == residue[1]-1:
+        for i_seq in range (residue[0], residue[1]):
+          if True:
+            atom_sel = 'id ' + str(model.atom[i_seq].id) + ' and state ' + str(n+1)
+            atom_action = 'b = ' + str(p_new)
+            cmd.alter(atom_sel, atom_action)
+          else:
+            atom_sel = 'id ' + str(model.atom[i_seq].id) + ' and state ' + str(n+1)
+            if model.atom[i_seq].name in ['N','CA','C','O']:
+              atom_action = 'b = ' + str(p_new_mc)
+            else:
+              atom_action = 'b = ' + str(p_new_sc)
+            cmd.alter(atom_sel, atom_action)
+
+def print_names(selection):
+  print '\n\nSelection:\n\n\n\n\n'
+  selection_string = 'select sel_name, id '
+  for x in cmd.identify(selection,0):
+    print x
+    selection_string += string.strip(str(x) + '+')
+  print selection_string[:-1]
+
+cmd.extend('ens_measure',ens_measure)
+cmd.extend('ens_rmsf',ens_rmsf)
+cmd.extend('ens_rmsd',ens_rmsd)
+cmd.extend('ens_prob',ens_prob)
+cmd.extend('print_names',print_names)
