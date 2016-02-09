a6fff2d8cf95b6886c47ed73f8d56898567aaced
Luc J. Bourhis <luc_j_bourhis@mac.com>
2016-02-10 00:09:31 +0100

    Sum of non-linear L.S. objective with the same overall scale factor

--- scitbx/lstbx/normal_equations.h
+++ scitbx/lstbx/normal_equations.h
@@ -425,6 +425,28 @@ namespace scitbx { namespace lstbx { namespace normal_equations {
       }
     }
 
+    /// Addition in the sense of the L.S. objective functions
+    /**
+     *  The overall factor for this objective and other's objective are the same.
+     *  In term of normal equations, this appends other's equations to this.
+     */
+
+    non_linear_ls_with_separable_scale_factor
+    &operator+=(non_linear_ls_with_separable_scale_factor const &other) {
+      SCITBX_ASSERT(!finalised());
+      SCITBX_ASSERT(!other.finalised());
+      n_data += other.n_data;
+      yo_sq += other.yo_sq;
+      yo_dot_yc += other.yo_dot_yc;
+      yc_sq += other.yc_sq;
+
+      grad_yc_dot_grad_yc += other.grad_yc_dot_grad_yc;
+      yo_dot_grad_yc += other.yo_dot_grad_yc;
+      yc_dot_grad_yc += other.yc_dot_grad_yc;
+
+      return *this;
+    }
+
     /// \f$\sum w y_o^2\f$
     /** This is the normalisation that guarantees
         that \f$L(K, x)\f$ is between 0 and 1.
