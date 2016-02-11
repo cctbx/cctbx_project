5cf1346d930588a284f056d9e1df2056bf62fefc
Luc J. Bourhis <luc_j_bourhis@mac.com>
2016-02-11 14:10:32 +0000

    Improve documentation of weighting scheme classes

--- smtbx/refinement/weighting_schemes.h
+++ smtbx/refinement/weighting_schemes.h
@@ -9,6 +9,9 @@
 
 namespace smtbx { namespace refinement { namespace least_squares {
 
+  /// Given the list of computed and measured structure factors, with
+  /// their e.s.d., and the scale factor K (fo_sq ~ K fc_sq),
+  /// return the list of weights.
   template<typename T, class WeightingScheme>
   af::shared<T> weights(WeightingScheme const &weighting_scheme,
                         af::const_ref<T> const &fo_sq,
@@ -24,10 +27,12 @@ namespace smtbx { namespace refinement { namespace least_squares {
     return result;
   }
 
-
+  /// Pureley statistical weighting scheme
+  /** i.e. The weight for a datum is \f$1/\sigma^2\f$ */
   template <typename T>
   struct sigma_weighting
   {
+    /// Whether each weight is independent of the calculated structure factor
     bool f_calc_independent() const { return true; }
 
     T operator()(T fo_sq, T sigma, T fc_sq, T scale_factor) const {
@@ -36,10 +41,11 @@ namespace smtbx { namespace refinement { namespace least_squares {
     }
   };
 
-
+  /// Weights uniformly equal to 1
   template <typename T>
   struct unit_weighting
   {
+    /// Whether each weight is independent of the calculated structure factor
     bool f_calc_independent() const { return true; }
 
     T operator()(T fo_sq, T sigma, T fc_sq, T scale_factor) const {
@@ -47,7 +53,23 @@ namespace smtbx { namespace refinement { namespace least_squares {
     }
   };
 
+  /// Weighting scheme used in George Sheldrick's SHELXL
+  /** The weights are function of \f$F_o^2, F_c^2, \sigma(F_o^2)\f$
+      and the scale factor K:
 
+      \f[ w = \frac{ 1 }{ \sigma(F_o^2) + (a P)^2 + K b P) } \f]
+
+      where
+
+      \f[ P = 1/3 \max(F_o^2, 0)] + 2/3 K F_c^2 \f]
+
+    The formula differs from that in SHELXL documentation because SHELXL
+    uses \f$ F_o^2/K, \sigma(F_o^2)/K and F_c^2 \f$ in its full computation
+    of the weighted square of the residual:
+
+      \f[ w_{SHELXL}(F_o^2/K, \sigma/K, F_c^2) (F_o^2/K - F_c^2)^2
+         = w (F_o^2 - K F_c^2)^2 \f]
+   */
   template <typename T>
   struct mainstream_shelx_weighting
   {
@@ -57,17 +79,11 @@ namespace smtbx { namespace refinement { namespace least_squares {
       : a(a), b(b)
     {}
 
+    /// Whether each weight is independent of the calculated structure factor
     bool f_calc_independent() const { return false; }
 
-    /* Shelxl uses fo_sq/k, sigma/k, fc_sq in its calculation of the weights
-       whereas we use fo_sq, sigma and k*fc_sq. To keep our weights compatible
-       with shelxl values of a and b, we must include the factor of k to the
-       term (b*P) in order to make the weights linear wrt the scale factor:
 
-         w = 1/[sigma^2 * (a*P)^2 + k*a*P],
 
-         where P = [max(fo_sq, 0) + 2*k*fc_sq]
-     */
     T operator()(T fo_sq, T sigma, T fc_sq, T scale_factor) const {
       T p = (std::max(fo_sq, 0.) + 2*scale_factor*fc_sq)/3.;
       return 1./(sigma*sigma + std::pow(a*p, 2) + b*scale_factor*p);
