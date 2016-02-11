87b93dc3155fac380d4e67e2bf8cafc3600db01a
Luc J. Bourhis <luc_j_bourhis@mac.com>
2016-02-11 14:11:47 +0000

    Python keywords for arguments of build_normal_equations.__init__

--- smtbx/refinement/boost_python/least_squares.cpp
+++ smtbx/refinement/boost_python/least_squares.cpp
@@ -34,7 +34,10 @@ namespace smtbx { namespace refinement { namespace least_squares {
             // jacobian_transpose_matching_grad_fc
           cctbx::xray::extinction_correction<FloatType> const &, // exti
           optional<bool> // objective_only=false
-        >());
+        >((arg("normal_equations"), arg("reflections"), arg("f_mask"),
+           arg("weighting_scheme"), arg("scale_factor"),
+           arg("f_calc_function"), arg("jacobian_transpose_matching_grad_fc"),
+           arg("extinction"), arg("objective_only")=false)));
     }
 
     static void wrap(char const *name) {
