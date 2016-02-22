bd741815ecfac357000751dbe80aad80fcde09be
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-02-22 10:41:18 +0000

    As far as I can tell these are all unnecessary imports (buildbot complaint)

--- xfel/amo/pnccd_ana/pnccd_tbx.py
+++ xfel/amo/pnccd_ana/pnccd_tbx.py
@@ -3,10 +3,6 @@ from psana import *
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
-from scipy import optimize
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
