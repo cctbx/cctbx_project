ce97bc1a8a0037ccae5fb517b9c7c321b224450c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-02-23 13:31:09 +0000

    Enable detection of multiprocessing use. Flush cache on new instances. Cached BZ2File() objects are not transferred properly to new processes.

--- dxtbx/filecache_controller.py
+++ dxtbx/filecache_controller.py
@@ -9,6 +9,7 @@
 
 from __future__ import division
 import dxtbx.filecache
+import os
 import threading
 
 class simple_controller():
@@ -23,12 +24,16 @@ class simple_controller():
     # Lock for concurrent access
     self._lock = threading.Lock()
 
+    # Keep the current PID to detect the use of multiprocessing parallelization
+    # which breaks caching assumptions
+    self._pid = os.getpid()
+
   def __del__(self):
     '''Garbage collection.
        Since only a cache controller can run .open() on lazy cache objects,
        the lazy cache object can be told to close as soon as possible.
        File handles can still be open legitimately.'''
-    if self._cache is not None:
+    if self._cache is not None and self._pid == os.getpid():
       self._cache.close()
 
   def check(self, tag, open_method):
@@ -38,11 +43,21 @@ class simple_controller():
        Otherwise, create a cache first, using the passed open_method()
        function, which returns a (true) file handle.'''
     with self._lock:
+      currentpid = os.getpid()
+      if currentpid != self._pid:
+#       print "(%5s) Controller %s detected PID change, flushing %s" % (os.getpid(), format(id(self), '#x'), self._cache_tag)
+        # Change the PID and drop reference to the previous cache.
+        self._cache = None
+        self._cache_tag = None
+        self._pid = currentpid
+        # NB: Explicitly do not close the cache in this case. Would be pointless.
       if tag != self._cache_tag:
         if self._cache is not None:
-#         print "Controller %s closing previous cache on %s" % (format(id(self), '#x'), self._cache_tag)
+#         print "(%5s) Controller %s closing previous cache on %s" % (os.getpid(), format(id(self), '#x'), self._cache_tag)
           self._cache.close()
         self._cache_tag = tag
-#       print "Controller %s opening cache on %s" % (format(id(self), '#x'), self._cache_tag)
+#       print "(%5s) Controller %s opening cache on %s" % (os.getpid(), format(id(self), '#x'), self._cache_tag)
         self._cache = dxtbx.filecache.lazy_file_cache(open_method())
+#     else:
+#       print "(%5s) Controller %s reporting cache hit on %s" % (os.getpid(), format(id(self), '#x'), self._cache_tag)
       return self._cache.open()
