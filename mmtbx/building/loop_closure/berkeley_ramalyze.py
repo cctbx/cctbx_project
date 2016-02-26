081b49b71785eb3ba0e38e33582f86caf6a9fe4d
Oleg Sobolev <olegsobolev@users.noreply.github.com>
2016-02-26 18:08:12 +0000

    Example of ramalyze.

---
+++ mmtbx/building/loop_closure/berkeley_ramalyze.py
@@ -0,0 +1,16 @@
+import iotbx.pdb
+from mmtbx.building.loop_closure.utils import list_rama_outliers_h
+from mmtbx.rotamer import ramachandran_eval
+
+import sys
+
+def run(args, log=sys.stdout):
+  pdb_h = iotbx.pdb.input(source_info=None, file_name=args[0]).\
+      construct_hierarchy()
+  r = ramachandran_eval.RamachandranEval()
+  outp = list_rama_outliers_h(pdb_h, r)
+  print outp
+  print "END"
+
+if (__name__ == "__main__"):
+  run(sys.argv[1:])
