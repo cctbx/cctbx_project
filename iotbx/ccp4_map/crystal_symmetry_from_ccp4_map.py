e6e64c50772d96b1a7847909cfd76633febabfd0
Pavel Afonine <pafonine@users.noreply.github.com>
2016-03-07 19:11:48 +0000

    Ugly work-around to stop annoying std-output from c++ guts of ccp4 map reading library (NEVER i/o in c++!)

--- iotbx/ccp4_map/crystal_symmetry_from_ccp4_map.py
+++ iotbx/ccp4_map/crystal_symmetry_from_ccp4_map.py
@@ -3,7 +3,13 @@ import iotbx.ccp4_map
 from cctbx import crystal
 
 def extract_from(file_name):
+  # XXX This is to hide stdout from ccp4io code (ccp4_cmap_open) about input
+  # XXX being non-ccp4 map. That's why we never have io in c/c++ code!
+  import cStringIO
+  save_stdout = sys.stdout
+  sys.stdout = cStringIO.StringIO()
   m = iotbx.ccp4_map.map_reader(file_name=file_name)
+  sys.stdout = save_stdout
   ucp = m.unit_cell_parameters
   sgn = max(1, m.space_group_number)
   return crystal.symmetry(ucp, sgn)
