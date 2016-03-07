e036fa2f861b086d771638a415c46aba6bdf0dd6
Gabor Bunkoczi <gbunkoczi@users.noreply.github.com>
2016-03-07 17:20:50 +0000

    Fix clutter

--- libtbx/scheduling/file_queue.py
+++ libtbx/scheduling/file_queue.py
@@ -157,7 +157,7 @@ class Queue(object):
   def read_one_and_transfer_remanining_put_contents(self, fget, timeout):
 
     import shutil
-    
+
     with open( self._put_file, "r+b" ) as fput:
       _lock( fput, timeout )
 
