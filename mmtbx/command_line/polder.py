0e717aab912b74480145edd4275763165632f36c
Oleg Sobolev <osobolev@lbl.gov>
2016-03-15 17:50:34 +0000

    code review

--- mmtbx/command_line/polder.py
+++ mmtbx/command_line/polder.py
@@ -6,20 +6,26 @@ from cStringIO import StringIO
 import mmtbx.f_model
 import mmtbx.utils
 import mmtbx.masks
-from mmtbx import utils
+from mmtbx import utils # No need to import it twice. Previous import is better.
 from mmtbx import map_tools
 from iotbx import pdb
 from iotbx import ccp4_map
 from iotbx import file_reader
 from iotbx import reflection_file_utils
 import iotbx.phil
-import iotbx.pdb
+import iotbx.pdb # Second attempt to import it. This way is better.
 import iotbx.mtz
 from cctbx import xray
 from cctbx import maptbx
 from cctbx.array_family import flex
-from libtbx import phil
+from libtbx import phil # Second import. Previous is better.
 from libtbx.utils import Sorry
+
+# in general, imports as mmtbx.utils are preferable, because in is obvious
+# in the code what is called. There are surprising number of modules called 
+# "utils" in cctbx. On the opposite, from libtbx.utils import Sorry is 
+# ok, because Sorry is rather unique, and it would be excessively long
+# call otherwise.
 
 legend = """\
 
@@ -105,7 +111,13 @@ debug = False
   .help = Additional output: biased omit map (ligand used for mask calculation but omitted from model)
 """
 
+# it is not a good idea to call function exactly the same name as 
+# imported module. I'm not sure the presence of the function is justified
+# because it is literally a single call. On top of that, 
+# the name is too general and
+# does not provide any clue about what the function is doing.
 def ccp4_map(file_name, uc, sg, map_data):
+  # This is duplicated import, find it in the beginning of the file
   from iotbx import ccp4_map
   ccp4_map.write_ccp4_map(
     file_name   = file_name,
@@ -116,8 +128,10 @@ def ccp4_map(file_name, uc, sg, map_data):
 
 def output_map(f_obs,r_free_flags, xray_structure, mask_data, filename, params):
   f_calc = f_obs.structure_factors_from_scatterers(
-    xray_structure = xray_structure).f_calc()
+    xray_structure = xray_structure).f_calc() 
   mask = f_obs.structure_factors_from_map(map = mask_data,
+    # strings should be 80 chars long if there is no strong reason to breake
+    # this rule. Comment could be here.
     use_scale = True, anomalous_flag = False, use_sg = False) # is it really use_sg = false?
   fmodel = mmtbx.f_model.manager(
     f_obs        = f_obs,
@@ -125,6 +139,13 @@ def output_map(f_obs,r_free_flags, xray_structure, mask_data, filename, params):
     f_calc       = f_calc,
     f_mask       = mask)
   fmodel.update_all_scales()
+  # Generally, it is good idea to output everything somewhere, not straight
+  # to stdout.
+  # Especially when it is a funciton. If it needs to output anything,
+  # the "log" parameter could be supplied. If stdout output is desired,
+  # sys.stdout could be passed as value of "log" parameter.
+  # Then it would be 
+  # print >> log, "r_work=" ... etc.
   print "r_work=%6.4f r_free=%6.4f"%(fmodel.r_work(), fmodel.r_free())
   print "*"*79
   mc_diff = map_tools.electron_density_map(
@@ -161,16 +182,30 @@ def mask_from_xrs_unpadded(xray_structure, n_real):
   maptbx.unpad_in_place(map = mask)
   return mask
 
+# I think it would be useful to make it as convenient as possible to 
+# call this function from anywhere else. If it needs to return more than one 
+# thing, it could be rewritted as a class. In this case (of using this
+# functionality from somewhere else), remark about printing to log
+# becomes even more relevant.
 def polder(f_obs, r_free_flags, xray_structure, pdb_hierarchy, params):
+  # see above the remark about print statements.
   print "selecting atoms..."
   selection_bool = pdb_hierarchy.atom_selection_cache().selection(
     string = params.solvent_exclusion_mask_selection)
+  # it would be great to ouput parameter before you actually do selection.
+  # if it is bad, the user would look at traceback at this point
+  # and won't have a chance to see the parameter :)
   print "Selection:", params.solvent_exclusion_mask_selection
   n_selected = selection_bool.count(True)
   if(n_selected == 0):
     raise Sorry("No atoms where selected. Check selection syntax again.")
-  print "Atoms selected:", n_selected
+  print "Number of atoms selected:", n_selected
   ligand_str = pdb_hierarchy.select(selection_bool).as_pdb_string()
+  # I would strongly recommend against using "naked" prints at all, 
+  # i.e. print <variable name>. 
+  # It is extremely difficult to find them later when they need to be removed.
+  # Put something meaningful in front of them, like 
+  # print "atoms selected\n", ligand_str
   print ligand_str
   # when extracting cartesian coordinates, xray_structure needs to be in P1!
   sites_cart_ligand = xray_structure.select(selection_bool).expand_to_p1(
@@ -184,6 +219,8 @@ def polder(f_obs, r_free_flags, xray_structure, pdb_hierarchy, params):
     symmetry_flags    = maptbx.use_space_group_symmetry,
     resolution_factor = params.resolution_factor)
   # calculate mask using ALL atoms
+  # Style inconsistency: here we have spaces around "=" in parameter list,
+  # 10 lines above we don't ("sites_mod_positive=True")
   mask_data_all = mask_from_xrs_unpadded(xray_structure = xray_structure,
     n_real = crystal_gridding.n_real())
   print "R factors for unmodified input model and data:"
@@ -201,6 +238,9 @@ def polder(f_obs, r_free_flags, xray_structure, pdb_hierarchy, params):
   mc_diff_polder = output_map(f_obs, r_free_flags, xray_structure_noligand,
     mask_polder, filename = "polder", params = params)
   # calculate mask for structure without ligand
+  # Example of style inconsistency in one function call. And skipping spaces
+  # wasn't enough to keep the line shorter than 80 symbols. And anyway,
+  # the function call is two lines :)
   mask_data_omit = mask_from_xrs_unpadded(xray_structure=xray_structure_noligand,
     n_real = crystal_gridding.n_real())
   print "R factor when ligand is excluded for mask calculation:"
@@ -222,12 +262,17 @@ def polder(f_obs, r_free_flags, xray_structure, pdb_hierarchy, params):
   print "Finished."
 
 # parse through command line arguments
+# command_name parameter is not used. I don't see any reason to have it.
 def cmd_run(args, command_name):
   if(len(args)==0):
     print legend
     return
   print "phenix.polder is running..."
+  # There is no reason to make a new variable and not use it. Nothing is wrong
+  # with original master_params_str
   master_params = master_params_str
+  # The use of process_includes here is a bit misleadin. You don't have any
+  # includes in your phil.
   parsed = phil.parse(master_params_str, process_includes=True)
   inputs = mmtbx.utils.process_command_line_args(args = args,
     master_params = parsed)
@@ -235,6 +280,9 @@ def cmd_run(args, command_name):
   params = inputs.params.extract()
   if(params.scattering_table not in ["n_gaussian","wk1995","it1992","neutron",
     "electron"]):
+    # I bet you won't get here. I haven't check it, but most likely,
+    # if user provides wrong
+    # table here it will fail during phil parsing.
     raise Sorry("Incorrect scattering_table.")
   if(params.solvent_exclusion_mask_selection is None):
     raise Sorry("No selection for mask calculation found.")
@@ -257,6 +305,7 @@ def cmd_run(args, command_name):
       hkl_in.assert_file_type("hkl")
       reflection_files = [ hkl_in.file_object ]
   # crystal symmetry
+  # What is the reason to assing crystal_symmetry to None first?
   crystal_symmetry = None
   crystal_symmetry = inputs.crystal_symmetry
   if(crystal_symmetry is None):
@@ -272,6 +321,9 @@ def cmd_run(args, command_name):
     parameters.labels = params.data_labels
   if(params.r_free_flags_labels is not None):
     parameters.r_free_flags.label = params.r_free_flags_labels
+  # This name looks extremely like a function name due to a verb in it.
+  # In reality it is an object. Consider, e.g. another name:
+  # "determined_data_and_flags"
   determine_data_and_flags_result = mmtbx.utils.determine_data_and_flags(
     reflection_file_server = rfs,
     parameters             = parameters,
@@ -279,6 +331,9 @@ def cmd_run(args, command_name):
     log                    = StringIO())
   f_obs = determine_data_and_flags_result.f_obs
   r_free_flags = determine_data_and_flags_result.r_free_flags
+  # This assertion is wrong. It passes if f_obs is None, and r_free_flags is
+  # not None. I don't know if this situation is possible, but in this case
+  # the code will fail on the next line in printing f_obs labels.
   assert [f_obs, r_free_flags].count(None)<=1
   print "Input data:"
   print "  Iobs or Fobs:", f_obs.info().labels
@@ -290,6 +345,14 @@ def cmd_run(args, command_name):
     raise Sorry("Sphere radius out of range: must be between 3 A and 10 A")
   pdb_input = iotbx.pdb.input(file_name = params.model_file_name)
   pdb_hierarchy = pdb_input.construct_hierarchy()
+  # This way of extracting xray_structure is actually wrong. It will be 
+  # inconsistent with pdb_hierarchy, because pdb_hierarchy gets sorted and
+  # pdb_input.xray_structure_simple is not. A way to test it would be to
+  # put a side chain atoms in front of main chain in a residue and supply
+  # selection that would select side-chain atoms. It would actually make 
+  # a good test ;) I'm sure atoms selected in xray_structure 
+  # (in polder function) will be wrong.
+  # Use pdb_hierarchy.extract_xray_structure() instead.
   xray_structure = pdb_input.xray_structure_simple()
   mmtbx.utils.setup_scattering_dictionaries(
     scattering_table = params.scattering_table,
