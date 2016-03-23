6b7d086b87c9cab6f4da860f8ea6a4dc4f467108
Billy Poon <bkpoon@lbl.gov>
2016-03-23 23:14:50 +0000

    installation: in certain cases, PyQt4 expects ucs2, but our Python is built with ucs4; gracefully ignore error

--- crys3d/libtbx_refresh.py
+++ crys3d/libtbx_refresh.py
@@ -7,4 +7,8 @@ else:
   import libtbx.load_env
   ui_dir = libtbx.env.under_dist(module_name="crys3d", path="qttbx")
   print '  Processing *.ui files in "%s"' % ui_dir
-  uic.compileUiDir(ui_dir, recurse=True)
+  try:
+    # check in case PyQt4 looks for ucs2, our Python is built with ucs4
+    uic.compileUiDir(ui_dir, recurse=True)
+  except NameError,ImportError:
+    pass
