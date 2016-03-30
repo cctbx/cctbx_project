47ca8597a2fbbea029a396e6f4ba592bed0d3482
Billy Poon <bkpoon@lbl.gov>
2016-03-30 21:25:19 +0000

    installation: more lenient except condition for PyQt4 update error

--- crys3d/libtbx_refresh.py
+++ crys3d/libtbx_refresh.py
@@ -10,5 +10,5 @@ else:
   try:
     # check in case PyQt4 looks for ucs2, our Python is built with ucs4
     uic.compileUiDir(ui_dir, recurse=True)
-  except NameError,ImportError:
+  except BaseException:
     pass
