a2cce91cc04643d1900413b7eb14065b279e2606
Tom Terwilliger <terwilliger@lanl.gov>
2016-04-01 21:29:57 +0000

    Add test for chain_comparison

--- mmtbx/run_tests.py
+++ mmtbx/run_tests.py
@@ -123,6 +123,7 @@ tst_list = (
   "$D/conformation_dependent_library/test_rdl.py",
   "$D/conformation_dependent_library/test_hpdl.py",
   "$D/regression/tst_find_ss_structure.py",
+  "$D/regression/tst_chain_comparison.py",
   "$D/regression/tst_regularize_from_pdb.py",
   "$D/regression/tst_find_ncs.py",
   "$D/regression/tst_minimize_chain.py",
