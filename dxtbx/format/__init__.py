8df8766b90391813de21d928d8308880656d5da5
Richard Gildea <richard.gildea@diamond.ac.uk>
2016-04-15 12:42:52 +0000

    portable way of finding base/lib directory

--- dxtbx/format/__init__.py
+++ dxtbx/format/__init__.py
@@ -3,7 +3,7 @@ from __future__ import division
 # Pick up Dectris's LZ4 and bitshuffle compression plugins
 def setup_hdf5_plugin_path():
   import os, libtbx.load_env
-  plugin_path = os.path.abspath(os.path.join(abs(libtbx.env.python_exe), "..", "..", "lib"))
+  plugin_path = libtbx.env.under_base('lib')
   try:
     os.environ['HDF5_PLUGIN_PATH'] = os.environ['HDF5_PLUGIN_PATH'] + ":" + plugin_path
   except Exception:
