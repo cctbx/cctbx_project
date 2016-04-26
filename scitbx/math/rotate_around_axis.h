8ade88fe697842c3b0bdc8fd07875bd38effd351
Oleg Sobolev <osobolev@lbl.gov>
2016-04-26 17:54:35 +0000

    Overloading rotate_point_around_axis so that there is implementation that does not need sin-cos tables.

--- scitbx/math/rotate_around_axis.h
+++ scitbx/math/rotate_around_axis.h
@@ -52,6 +52,49 @@ rotate_point_around_axis(
     xma*(xlzlo-yl*dsa)+yma*(ylzlo+xl*dsa)+zma*(zlsq*oca+ca)+za);
 }
 
+// exact. Copy-paste from the code above. Here we don't need sin/cos tables.
+template <typename FloatType>
+vec3<FloatType>
+rotate_point_around_axis(
+  vec3<FloatType> const& axis_point_1,
+  vec3<FloatType> const& axis_point_2,
+  vec3<FloatType> const& point,
+  FloatType angle_rad)
+{
+  FloatType xa = axis_point_1[0];
+  FloatType ya = axis_point_1[1];
+  FloatType za = axis_point_1[2];
+  FloatType xb = axis_point_2[0];
+  FloatType yb = axis_point_2[1];
+  FloatType zb = axis_point_2[2];
+  FloatType xl = xb-xa;
+  FloatType yl = yb-ya;
+  FloatType zl = zb-za;
+  FloatType xlsq = xl*xl;
+  FloatType ylsq = yl*yl;
+  FloatType zlsq = zl*zl;
+  FloatType dlsq = xlsq + ylsq + zlsq;
+  // Use faster approximate functions instead. Standard functions can be enabled
+  // by overload.
+  FloatType dl  = std::sqrt(dlsq);
+  FloatType ca  = std::cos(angle_rad);
+  FloatType dsa = std::sin(angle_rad)/dl;
+  //FloatType dl  = math::approx_sqrt(dlsq);
+  // FloatType ca  = math::cos_table(cos_table,angle_rad,step,n,false);
+  // FloatType dsa = math::sin_table(sin_table,angle_rad,step,n,false)/dl;
+  FloatType oca = (1-ca)/dlsq;
+  FloatType xlylo = xl*yl*oca;
+  FloatType xlzlo = xl*zl*oca;
+  FloatType ylzlo = yl*zl*oca;
+  FloatType xma = point[0]-xa;
+  FloatType yma = point[1]-ya;
+  FloatType zma = point[2]-za;
+  return vec3<FloatType>(
+    xma*(xlsq*oca+ca) +yma*(xlylo-zl*dsa)+zma*(xlzlo+yl*dsa)+xa,
+    xma*(xlylo+zl*dsa)+yma*(ylsq*oca+ca) +zma*(ylzlo-xl*dsa)+ya,
+    xma*(xlzlo-yl*dsa)+yma*(ylzlo+xl*dsa)+zma*(zlsq*oca+ca)+za);
+}
+
 template <typename FloatType>
 void
 rotate_points_around_axis(
