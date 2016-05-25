f840faf1a5fdf668092efe67e60aab77c29cdc5e
Oleg Sobolev <osobolev@lbl.gov>
2016-05-25 09:44:50 +0000

    getting custom_gm in working order again.

--- mmtbx/building/loop_closure/custom_gm.py
+++ mmtbx/building/loop_closure/custom_gm.py
@@ -17,17 +17,18 @@ def run(args):
   # print dir(pdb_inp)
   xrs = pdb_inp.xray_structure_simple()
   outlier_selection_txt = mmtbx.building.loop_closure.utils. \
-    rama_outliers_selection(original_pdb_h, None, 1)
+    rama_outliers_selection(ref_h, None, 1)
   negate_selection = None
+  # asc = ref_h.atom_selection_cache()
   if outlier_selection_txt != "" and outlier_selection_txt is not None:
     negate_selection = "not (%s)" % outlier_selection_txt
-  sel = asc.selection(negate_selection)
+  # sel = asc.selection(negate_selection)
 
   minimize_wrapper_for_ramachandran(
       pdb_h,
       xrs,
       ref_h,
-      excl_string_selection=sel,
+      excl_string_selection=negate_selection,
       log=None,
       run_first_minimization_without_reference=True,
       oldfield_weight_scale=1,
