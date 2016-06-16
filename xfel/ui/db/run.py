4490149bc6121d9aef2faa6d60cb1601a6336d66
Aaron S. Brewster <asbrewster@lbl.gov>
2016-06-16 17:10:21 +0000

    XFEL DB
    Don't cache tags an rungroups on the run and trial objects, respectively.  Regenerate them each time .tags or .rungroups is accessed from the DB.

--- xfel/ui/db/run.py
+++ xfel/ui/db/run.py
@@ -5,14 +5,11 @@ class Run(db_proxy):
   def __init__(self, app, run_id = None, **kwargs):
     db_proxy.__init__(self, app, "%s_run" % app.params.experiment_tag, id = run_id, **kwargs)
     self.run_id = self.id
-    self._tags = None # delay querying the db for the tags until needed
 
   def __getattr__(self, name):
     # Called only if the property cannot be found
     if name == "tags":
-      if self._tags is None:
-        self._tags = self.app.get_run_tags(self.id)
-      return self._tags
+      return self.app.get_run_tags(self.id)
     else:
       return super(Run, self).__getattr__(name)
 
@@ -24,10 +21,8 @@ class Run(db_proxy):
     query = "INSERT INTO `%s_run_tag` (run_id, tag_id) VALUES (%d, %d)" % (
       self.app.params.experiment_tag, self.id, tag.id)
     self.app.execute_query(query, commit=True)
-    self._tags = self.app.get_run_tags(self.id)
 
   def remove_tag(self, tag):
     query = "DELETE FROM `%s_run_tag` WHERE run_id = %d AND tag_id = %s" % (
       self.app.params.experiment_tag, self.id, tag.id)
     self.app.execute_query(query, commit=True)
-    self._tags = self.app.get_run_tags(self.id)
--- xfel/ui/db/trial.py
+++ xfel/ui/db/trial.py
@@ -5,14 +5,11 @@ class Trial(db_proxy):
   def __init__(self, app, trial_id = None, **kwargs):
     db_proxy.__init__(self, app, "%s_trial" % app.params.experiment_tag, id = trial_id, **kwargs)
     self.trial_id = self.id
-    self._rungroups = None  # delay querying the db for the tags until needed
 
   def __getattr__(self, name):
     # Called only if the property cannot be found
     if name == "rungroups":
-      if self._rungroups is None:
-        self._rungroups = self.app.get_trial_rungroups(self.id)
-      return self._rungroups
+      return self.app.get_trial_rungroups(self.id)
     else:
       return super(Trial, self).__getattr__(name)
 
@@ -24,10 +21,8 @@ class Trial(db_proxy):
     query = "INSERT INTO `%s_trial_rungroup` (trial_id, rungroup_id) VALUES (%d, %d)" % (
       self.app.params.experiment_tag, self.id, rungroup.id)
     self.app.execute_query(query, commit=True)
-    self._rungroups = self.app.get_trial_rungroups(self.id)
 
   def remove_tag(self, rungroup):
     query = "DELETE FROM `%s_trial_rungroup` WHERE trial_id = %d AND rungroup_id = %s" % (
       self.app.params.experiment_tag, self.id, rungroup.id)
     self.app.execute_query(query, commit=True)
-    self._rungroups = self.app.get_trial_rungroups(self.id)
