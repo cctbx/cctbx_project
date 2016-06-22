e27618d3f7efc2052cec5a1240f7f25fc8d061cf
dcliebschner <dcliebschner@users.noreply.github.com>
2016-06-22 18:06:25 +0000

    parameterization for construction of H atoms

---
+++ mmtbx/hydrogens/hydrogen_parametrization.py
@@ -0,0 +1,324 @@
+from __future__ import division
+import sys
+import time
+import mmtbx.monomer_library.server
+import mmtbx.monomer_library.pdb_interpretation
+from mmtbx.monomer_library.pdb_interpretation import grand_master_phil_str
+import mmtbx.utils
+from mmtbx import monomer_library
+import iotbx.phil
+#from mmtbx import hydrogens
+from cctbx import geometry_restraints
+from scitbx import matrix
+#from libtbx.utils import Sorry
+from libtbx import adopt_init_args
+from libtbx import group_args
+from libtbx.utils import null_out
+from libtbx.utils import multi_out
+from stdlib import math
+
+import hydrogen_connectivity
+
+legend = """\
+phenix.hydrogen_parametrization:
+Computes the parameters for the compuationf of H atom positions
+based on their parent atoms
+
+Inputs:
+  - Model file in PDB format (other files are not supported)
+
+Usage examples:
+   phenix.hydrogen_parametrization model.pdb
+
+Output:
+   This code produces a dictionary
+   H atom index is the key, pointing to an object which contains necessary
+   parameters to build the H atom again (knowing the coordinates of
+   the parent atoms)
+   H (iseq) --> parametrization_info
+
+Important:
+  Hydrogen atoms need to be present in the file.
+  H atoms can be generated with phenix.reduce
+"""
+
+class parametrization_info(object):
+  def __init__(
+    self,
+    htype      = None,  # type of bond
+    a0         = None,  # parent atom index
+    a1         = None,  # 1-3 neighbor index
+    a2         = None,  # 1-3 or 1-4 neighbor index
+    a          = None,  # coefficient for reconstruction
+    b          = None,  # coefficient for reconstruction
+    h          = None,  # coefficient for reconstruction
+    chi        = None,  # angle
+    eps        = None,  # angle
+    dist_h     = None): # IDEAL distance
+    adopt_init_args(self, locals())
+
+
+# for every H atom, determine type of bond
+def get_h_parametrization(
+  connectivity, xray_structure, names, atoms_list):
+  sites_cart = xray_structure.sites_cart()
+  h_parametrization = {}
+  for ih in connectivity:
+    a0 = connectivity[ih][0]
+    dist_h = a0.dist_ideal
+    count_H, reduced_neighbs = a0.count_H, a0.reduced_neighbs
+    n_red_neigbs = len(reduced_neighbs)
+    rh = matrix.col(sites_cart[ih])
+    r0 = matrix.col(sites_cart[a0.iseq])
+  # case 2a and 2b, case 3
+    if((n_red_neigbs == 2) or
+      (n_red_neigbs == 3 and count_H == 0)):
+      a1, a2  = reduced_neighbs[0], reduced_neighbs[1]
+      r1 = matrix.col(sites_cart[a1.iseq])
+      r2 = matrix.col(sites_cart[a2.iseq])
+      uh0 = (rh - r0).normalize()
+      u10 = (r1 - r0).normalize()
+      u20 = (r2 - r0).normalize()
+      alpha0 = (u10).angle(u20)
+      alpha1 = (u10).angle(uh0)
+      alpha2 = (uh0).angle(u20)
+      sumang = math.degrees(alpha0) + math.degrees(alpha1) + math.degrees(alpha2)
+      c0, c1, c2 = math.cos(alpha0), math.cos(alpha1), math.cos(alpha2)
+      a, b = (c1-c0*c2)/(1-c0*c0), (c2-c0*c1)/(1-c0*c0)
+      h_parametrization[ih] = parametrization_info(
+        a0     = a0.iseq,
+        a1     = a1.iseq,
+        a2     = a2.iseq,
+        a      = a,
+        b      = b,
+        dist_h = dist_h)
+      if (sumang < 361 and sumang > 359):
+        h_parametrization[ih].htype = 'flat_2neigbs'
+      else:
+        cz = (math.sqrt(1-c1*c1-c2*c2-c0*c0+2*c0*c1*c2))/math.sin(alpha0)
+        h = cz/math.sin(alpha0)
+        #test if vector v points to same 'side' as uh0
+        if((u10.cross(u20)).dot(uh0) < 0):
+          h = -h
+        h_parametrization[ih].h = h
+        if (n_red_neigbs == 2): # case 2b
+          h_parametrization[ih].htype = '2neigbs'
+        elif (n_red_neigbs == 3): # case 3
+          h_parametrization[ih].htype = '3neigbs'
+    # case 1a
+    elif(n_red_neigbs == 1 and count_H == 1):
+      neigbs_14 = connectivity[ih][2]
+      a1 = reduced_neighbs[0]
+      b1, b2 = neigbs_14[0], neigbs_14[1]
+      r1 = matrix.col(sites_cart[a1.iseq])
+      rb1 = matrix.col(sites_cart[b1.iseq])
+      rb2 = matrix.col(sites_cart[b2.iseq])
+      # chose 1-4 neighbor which is closer - important!
+      if((rh-rb2).length() < (rh-rb1).length()):
+        neigbs_14[0], neigbs_14[1] = neigbs_14[1], neigbs_14[0]
+      rb1 = matrix.col(sites_cart[neigbs_14[0].iseq])
+      r2 = r0 + (r1 - rb1).normalize()
+      uh0 = (rh - r0).normalize()
+      u10 = (r1 - r0).normalize()
+      u20 = (r2 - r0).normalize()
+      alpha0 = (u10).angle(u20)
+      alpha1 = (u10).angle(uh0)
+      alpha2 = (uh0).angle(u20)
+      # TODO add singularity test - division by zero
+      c0, c1, c2 = math.cos(alpha0), math.cos(alpha1), math.cos(alpha2)
+      a, b = (c1-c0*c2)/(1-c0*c0), (c2-c0*c1)/(1-c0*c0)
+      cz = (math.sqrt(1-c1*c1-c2*c2-c0*c0+2*c0*c1*c2))/math.sin(alpha0)
+      h = cz/math.sin(alpha0)
+      # test if vector v points to same 'side' as uh0
+      if((u10.cross(u20)).dot(uh0) < 0):
+        h = -h
+      h_parametrization[ih] = parametrization_info(
+        htype  = 'alg1a',
+        a0     = a0.iseq,
+        a1     = a1.iseq,
+        a2     = neigbs_14[0].iseq,
+        a      = a,
+        b      = b,
+        h      = h,
+        dist_h = dist_h)
+    # case 1b
+    elif(n_red_neigbs == 1 and (count_H == 0 or count_H ==2)):
+      a1 = reduced_neighbs[0]
+      a, b = 1, 1
+      sec_neigbs = connectivity[ih][2]
+      b1 = sec_neigbs[0]
+      r1 = matrix.col(sites_cart[a1.iseq])
+      rb1 = matrix.col(sites_cart[b1.iseq])
+      dh = a0.dist_ideal
+      uh0 = (rh - r0).normalize()
+      u10 = (r1 - r0).normalize()
+      alpha2 = (u10).angle(uh0)
+      rb10 = rb1 - r1
+      v10 = (rb10 - ((rb10).dot(u10)) * u10).normalize()
+      if(v10.dot(uh0) < 0):
+        v10 = -v10
+        a = -1
+      w10 = (u10.cross(v10)).normalize()
+      if(w10.dot(uh0) < 0):
+        w10 = -w10
+        b   = -1
+      chi = math.asin((uh0).dot(w10))
+      c_chi, s_chi = math.cos(abs(chi)), math.sin(abs(chi))
+      rp = rh - dh * s_chi * w10
+      rp0 = rp - r0
+      eps = (rp0).angle(u10)
+      h_parametrization[ih] = parametrization_info(
+        htype  = 'alg1b',
+        a0     = a0.iseq,
+        a1     = a1.iseq,
+        a2     = b1.iseq,
+        a      = a,
+        b      = b,
+        chi    = chi,
+        eps    = eps,
+        dist_h = a0.dist_ideal)
+    else:
+      h_parametrization[ih] = parametrization_info(
+        htype  = 'unk',
+        a0     = a0.iseq,
+        a1     = a1.iseq)
+  return h_parametrization
+
+
+def generate_H_positions(xray_structure, ih, para_info):
+  sites_cart = xray_structure.sites_cart()
+  r0 = matrix.col(sites_cart[para_info.a0])
+  r1 = matrix.col(sites_cart[para_info.a1])
+  dh = para_info.dist_h
+  # case 2a
+  if (para_info.htype == 'flat_2neigbs'):
+    a, b = para_info.a, para_info.b
+    r2 = matrix.col(sites_cart[para_info.a2])
+    u10, u20 = (r1 - r0).normalize(), (r2 - r0).normalize()
+    uh0 = (a * u10 + b * u20).normalize()
+    rH_gen = r0 + dh * uh0
+    deltaH = (rH_gen - matrix.col(sites_cart[ih])).length()
+  # case 2b and 3
+  elif (para_info.htype == '2neigbs' or para_info.htype == '3neigbs'):
+    a, b, h= para_info.a, para_info.b, para_info.h
+    r2 = matrix.col(sites_cart[para_info.a2])
+    u10, u20 = (r1 - r0).normalize(), (r2 - r0).normalize()
+    v = u10.cross(u20)
+    uh0 = (a * u10 + b * u20 + h * v).normalize()
+    rH_gen = r0 + dh * uh0
+    deltaH = (rH_gen - matrix.col(sites_cart[ih])).length()
+  # case 1a
+  elif (para_info.htype == 'alg1a'):
+    a, b, h = para_info.a, para_info.b, para_info.h
+    rb1 = matrix.col(sites_cart[para_info.a2])
+    r2 = r0 + (r1 - rb1).normalize()
+    u10, u20 = (r1 - r0).normalize(), (r2 - r0).normalize()
+    v = u10.cross(u20)
+    uh0 = (a * u10 + b * u20 + h * v).normalize()
+    rH_gen = r0 + dh * uh0
+    deltaH = (rH_gen - matrix.col(sites_cart[ih])).length()
+  # case one H atom free rotation
+  elif (para_info.htype == 'alg1b'):
+    rb1 = matrix.col(sites_cart[para_info.a2])
+    a, b = para_info.a, para_info.b
+    chi, eps = para_info.chi, para_info.eps
+    u10 = (r1 - r0).normalize()
+    rb10 = rb1 - r1
+    v10 = a * (rb10 - ((rb10).dot(u10)) * u10).normalize()
+    w10 = b * (u10.cross(v10)).normalize()
+    c_chi, s_chi = math.cos(abs(chi)), math.sin(abs(chi))
+    c1, s1  = math.cos(eps), math.sin(eps)
+    rH_gen = r0 + dh * ((c1 * c_chi * u10) + (s1 * c_chi * v10) + (s_chi * w10))
+    deltaH = (rH_gen - matrix.col(sites_cart[ih])).length()
+  else:
+    deltaH, rH_gen = None, None
+  return group_args(
+    distance = deltaH,
+    rH_gen   = rH_gen)
+
+
+def run(args, out=sys.stdout):
+  log = multi_out()
+  log.register("stdout", out)
+  log_file_name = "hydrogen_parametrization.log"
+  logfile = open(log_file_name, "w")
+  log.register("logfile", logfile)
+  if (len(args) == 0):
+    print >>log, legend
+    return
+
+  print >> log, "phenix.hydrogen_parametrization is running..."
+  print >> log, "input parameters:\n", args
+
+# parse through params --> switch off CDL or not
+  params_line = grand_master_phil_str
+  params = iotbx.phil.parse(
+      input_string=params_line, process_includes=True).extract()
+  params.pdb_interpretation.restraints_library.cdl=False
+
+  processed_args = mmtbx.utils.process_command_line_args(
+    args=args, log=null_out())
+  pdb_filename = processed_args.pdb_file_names[0]
+  mon_lib_srv = monomer_library.server.server()
+  ener_lib = monomer_library.server.ener_lib()
+  if pdb_filename is not None:
+    processed_pdb_file = monomer_library.pdb_interpretation.process(
+      params         = params.pdb_interpretation,
+      mon_lib_srv    = mon_lib_srv,
+      ener_lib       = ener_lib,
+      file_name      = pdb_filename,
+      force_symmetry = True)
+
+  pdb_hierarchy = processed_pdb_file.all_chain_proxies.pdb_hierarchy
+  xray_structure = processed_pdb_file.xray_structure()
+  if pdb_filename is not None:
+    pdb_str = pdb_hierarchy.as_pdb_string()
+
+  geometry_restraints = processed_pdb_file.geometry_restraints_manager(
+    show_energies = False)
+  restraints_manager = mmtbx.restraints.manager(
+    geometry      = geometry_restraints,
+    normalization = False)
+
+  bond_proxies_simple, asu = restraints_manager.geometry.get_all_bond_proxies(
+    sites_cart = xray_structure.sites_cart())
+  angle_proxies = restraints_manager.geometry.get_all_angle_proxies()
+
+  names = list(pdb_hierarchy.atoms().extract_name())
+  sites_cart = xray_structure.sites_cart()
+  scatterers = xray_structure.scatterers()
+
+  atoms_list = list(pdb_hierarchy.atoms_with_labels())
+
+  print >>log, '\nNow determining connectivity table for H atoms...'
+  connectivity = hydrogen_connectivity.determine_H_neighbors(
+    bond_proxies   = bond_proxies_simple,
+    angle_proxies  = angle_proxies,
+    xray_structure = xray_structure)
+
+  print >>log, '\nNow determining the parametrization for H atoms...'
+  h_parametrization = get_h_parametrization(
+    connectivity   = connectivity,
+    xray_structure = xray_structure,
+    names          = names,
+    atoms_list     = atoms_list)
+
+  print >>log, '\nNow reconstructing H atoms...'
+  for ih in h_parametrization.keys():
+    residue = atoms_list[ih].resseq
+    hp = h_parametrization[ih]
+    h_obj = generate_H_positions(
+      xray_structure    = xray_structure,
+      ih                = ih,
+      para_info         = hp)
+    if(h_obj.distance is not None):
+      print >> log, hp.htype, 'atom:', names[ih]+' ('+str(ih)+ ') residue:', \
+        residue, 'distance:', h_obj.distance
+    else:
+      print >> log, hp.htype, 'atom:', names[ih]+' ('+str(ih)+ ') residue:', residue
+
+
+if (__name__ == "__main__"):
+  t0 = time.time()
+  run(sys.argv[1:])
+  print "Time:", round(time.time()-t0, 2)
