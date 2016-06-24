ab8fd312f9194ba52aad445559946b22f7a28b7b
Aaron S. Brewster <asbrewster@lbl.gov>
2016-06-24 19:27:43 +0000

    One color, monochromatic x-ray spectra filter moved in from cxi_xdr_xes

---
+++ xfel/command_line/fee_view.py
@@ -0,0 +1,106 @@
+from __future__ import division
+# -*- Mode: Python; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 8 -*-
+#
+# LIBTBX_SET_DISPATCHER_NAME cxi.fee_view
+#
+import sys, os
+import psana
+from psmon import publish
+from psmon.plots import Image, XYPlot
+from libtbx.phil import parse
+from xfel.cxi.spectra_filter import spectra_filter
+
+"""
+Example usage:
+cd ~/myrelease
+sit_setup
+psplot SPECTRUM FEE &
+cxi.fee_view params.phil selected_filter=best
+"""
+phil_scope = parse("""
+  selected_filter = None
+    .type = str
+    .help = Which filter to apply
+  runs = None
+    .type = str
+    .help = Set of runs to display, eg 96 or 95-114 or 95,97,99-101, etc.
+  experiment = None
+    .type = str
+    .help = Experiment name, e.g. cxid9114
+  include scope xfel.cxi.spectra_filter.phil_scope
+""", process_includes = True)
+
+def run(args):
+  user_phil = []
+  for arg in args:
+    if os.path.isfile(arg):
+      try:
+        user_phil.append(parse(file_name=arg))
+      except Exception, e:
+        print str(e)
+        raise Sorry("Couldn't parse phil file %s"%arg)
+    else:
+      try:
+        user_phil.append(parse(arg))
+      except Exception, e:
+        print str(e)
+        raise Sorry("Couldn't parse argument %s"%arg)
+  params = phil_scope.fetch(sources=user_phil).extract()
+
+  # cxid9114, source fee: FeeHxSpectrometer.0:Opal1000.1, downstream: CxiDg3.0:Opal1000.0
+  # cxig3614, source fee: FeeHxSpectrometer.0:OrcaFl40.0
+
+  src = psana.Source(params.spectra_filter.detector_address)
+  dataset_name = "exp=%s:run=%s:idx"%(params.experiment, params.runs)
+  print "Dataset string:", dataset_name
+  ds = psana.DataSource(dataset_name)
+  spf = spectra_filter(params)
+
+  if params.selected_filter == None:
+    filter_name = params.spectra_filter.filter[0].name
+  else:
+    filter_name = params.selected_filter
+
+  rank = 0
+  size = 1
+  max_events = sys.maxint
+
+  for run in ds.runs():
+    print "starting run", run.run()
+    # list of all events
+    times = run.times()
+
+    nevents = min(len(times),max_events)
+    # chop the list into pieces, depending on rank.  This assigns each process
+    # events such that the get every Nth event where N is the number of processes
+    mytimes = [times[i] for i in xrange(nevents) if (i+rank)%size == 0]
+
+    for i, t in enumerate(mytimes):
+      if i < 200: continue
+      evt = run.event(t)
+      accepted, data, spectrum, dc_offset, all_data, all_data_raw = spf.filter_event(evt, filter_name)
+
+      if not accepted:
+        continue
+
+      print "Publishing data for event", i
+
+      #header = "Event %d, m/f: %7.7f, f: %d"%(i, peak_max/flux, flux)
+      header = "Event %d"%(i)
+
+      if rank == 0:
+        fee = Image(header, "FEE", data) # make a 2D plot
+        publish.send("FEE", fee) # send to the display
+
+        spectrumplot = XYPlot(header, 'summed 1D trace', range(data.shape[1]), spectrum) # make a 1D plot
+        publish.send("SPECTRUM", spectrumplot) # send to the display
+
+        fee = Image(header, "DC_OFFSET", dc_offset) # make a 2D plot
+        publish.send("DC_OFFSET", fee) # send to the display
+        fee = Image(header, "ALL_FEE", all_data) # make a 2D plot
+        publish.send("ALL_FEE", fee) # send to the display
+        fee = Image(header, "ALL_FEE_RAW", all_data_raw) # make a 2D plot
+        publish.send("ALL_FEE_RAW", fee) # send to the display
+
+if __name__ == "__main__":
+  run(sys.argv[1:])
---
+++ xfel/cxi/spectra_filter.py
@@ -0,0 +1,100 @@
+from __future__ import division
+import psana
+import numpy as np
+from libtbx import easy_pickle
+from xfel.cxi.cspad_ana import cspad_tbx
+from libtbx.phil import parse
+from libtbx.utils import Sorry
+
+phil_scope = parse("""
+  spectra_filter {
+    name = None
+      .type = str
+      .help = Name of this set of filters
+    detector_address = None
+      .type = str
+      .help = Address for fee spectrometer, eg FeeHxSpectrometer.0:Opal1000.1
+    roi = None
+      .type = ints(size=4)
+      .help = Spectra region of interest in pixels (xmin, xmax, ymin, ymax)
+    background_roi = None
+      .type = ints(size=4)
+      .help = Background region of interest in pixels (xmin, xmax, ymin, ymax)
+    peak_range = None
+      .type = ints(size=2)
+      .help = Range of pixels for the peak region (xmin, xmax). ymin and ymax \
+              come from the roi.
+    background_path = None
+      .type = str
+      .help = Path to background pickle file
+    filter
+      .multiple = True
+      .help = Set of available filters
+    {
+      name = None
+        .type = str
+        .help = Name of this filter
+      flux_min = None
+        .type = float
+        .help = Minimum flux to accept an event, where flux is defined as \
+                sum(roi pixels-background pickle pixels) - mean(background roi pixels - background pickle pixels) \
+                None: do not filter with this parameter
+      flux_max = None
+        .type = float
+        .help = Maximum flux to accept an event. None: do not filter with this parameter
+      peak_ratio_min = None
+        .type = float
+        .help = Minimum peak ratio to accept an event, where the peak ratio is the peak_sum/flux, and peak_sum is \
+                sum(peak pixels-background pickle pixels) - mean(background roi pixels - background pickle pixels) \
+                None: do not filter with this parameter
+      peak_ratio_max = None
+        .type = float
+        .help = Maximum peak ratio to accept an event. None: do not filter with this parameter
+    }
+  }
+""")
+
+class spectra_filter(object):
+  def __init__(self, params):
+    self.src = psana.Source(params.spectra_filter.detector_address)
+    self.roi = params.spectra_filter.roi
+    self.bg_roi = params.spectra_filter.background_roi
+    self.dark_pickle = easy_pickle.load(params.spectra_filter.background_path)
+    self.peak_range = params.spectra_filter.peak_range
+    self.params = params
+
+  def filter_event(self, evt, filter_name):
+    filter_phils = [f for f in self.params.spectra_filter.filter if f.name == filter_name]
+    if len(filter_phils) == 0:
+      raise Sorry("Filter %s not found in phil file"%filter_name)
+    elif len(filter_phils) > 1:
+      raise Sorry("Ambiguous filter name: %s"%filter_name)
+
+    filter_phil = filter_phils[0]
+    return self._filter_event(evt, filter_phil.flux_min, filter_phil.flux_max, filter_phil.peak_ratio_min, filter_phil.peak_ratio_max)
+
+  def _filter_event(self, evt, flux_min, flux_max, peak_ratio_min, peak_ratio_max):
+    all_data = evt.get(psana.Camera.FrameV1, self.src)
+    if all_data is None:
+      print "No data"
+      return False, None, None, None, None, None
+    print cspad_tbx.evt_timestamp(cspad_tbx.evt_time(evt)),
+    xmin, xmax, ymin, ymax = self.bg_roi
+    dc_offset = np.array(all_data.data16().astype(np.int32))[ymin:ymax,xmin:xmax] - self.dark_pickle[ymin:ymax,xmin:xmax] #dc: direct current
+    xmin, xmax, ymin, ymax = self.roi
+    data_signal = np.array(all_data.data16().astype(np.int32))[ymin:ymax,xmin:xmax] - self.dark_pickle[ymin:ymax,xmin:xmax]
+    data = data_signal - np.mean(dc_offset)
+
+    # make a 1D trace
+    spectrum = np.sum(data, 0)
+    peak = spectrum[self.peak_range[0]-xmin:self.peak_range[1]-xmin]
+    peak_max = np.sum(peak)
+    flux = np.sum(spectrum)
+    print "Run", evt.run(), "peak max:", peak_max, "at", np.argmax(peak)+xmin, "px", "flux:", flux, "m/f:", peak_max/flux
+
+    if flux_min is not None and flux < flux_min: return False, None, None, None, None, None
+    if flux_max is not None and flux >= flux_max: return False, None, None, None, None, None
+    if peak_ratio_min is not None and peak_max/flux < peak_ratio_min: return False, None, None, None, None, None
+    if peak_ratio_max is not None and peak_max/flux >= peak_ratio_max: return False, None, None, None, None, None
+    all_data = all_data.data16().astype(np.int32)
+    return True, data, spectrum, dc_offset, all_data - self.dark_pickle, all_data
