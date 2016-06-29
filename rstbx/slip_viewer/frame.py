e2707cad57688b3882a5d49d7bc9fd6f887943eb
Iris Young <idyoung@lbl.gov>
2016-06-29 17:01:46 +0000

    raw_img.get_raw_data() may or may not be a tuple. Until this is consistent, make it a tuple.

--- rstbx/slip_viewer/frame.py
+++ rstbx/slip_viewer/frame.py
@@ -636,17 +636,20 @@ class XrayFrame (AppFrame,XFBaseClass) :
         # apply.
         raw_img = self.pyslip.tiles.raw_image
         detector = raw_img.get_detector()
+        data = raw_img.get_raw_data()
+        if not isinstance(data, tuple): # XXX should not need this test
+          data = (data,)
         if len(detector) > 1:
           from tile_generation import _get_flex_image_multipanel
           flex_img = _get_flex_image_multipanel(
             brightness=self.settings.brightness / 100,
             panels=detector,
-            raw_data=raw_img.get_raw_data())
+            raw_data=data)
         else:
           from tile_generation import _get_flex_image
           flex_img = _get_flex_image(
             brightness=self.settings.brightness / 100,
-            data=raw_img.get_raw_data()[0],
+            data=data[0],
             saturation=detector[0].get_trusted_range()[1],
             vendortype=raw_img.__class__.__name__)
 
