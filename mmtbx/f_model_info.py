a41ea67c35e8906143e163f942da48ba882e3072
Billy Poon <bkpoon@lbl.gov>
2016-07-13 18:13:03 +0000

    f_model_info: added file that just imports f_model.f_model_info to preserve backwards compatibility of phenix.refine GUI results

---
+++ mmtbx/f_model_info.py
@@ -0,0 +1,11 @@
+from __future__ import division
+from mmtbx.f_model.f_model_info import *
+
+'''
+This file is specifically to preserve backwards compatibility with earlier
+versions of the Phenix GUI where the class for storing results in a pickle,
+phenix.runtime.result_summary, has an import statement for this file.
+Pickling an instance of result_summary stored the location of this file in the
+cctbx hierarchy. The import statement in the result_summary class has been
+moved outside of the class definition to prevent future issues.
+'''
