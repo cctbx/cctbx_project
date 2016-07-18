c97a0adc11108900d82bf5531b065d39b31d557f
Iris Young <idyoung@lbl.gov>
2016-07-18 05:36:41 +0000

    Add more columns including %HQR, the percent of indexed images that are high quality/high resolution.

--- xfel/ui/command_line/print_run_stats.py
+++ xfel/ui/command_line/print_run_stats.py
@@ -24,27 +24,43 @@ def run(args):
   params = phil_scope.fetch(sources=user_phil).extract()
   print "Printing results for trial", params.trial, "using a hit cutoff of", params.hit_cutoff, "reflections"
   print
-  print "Run    N Hits   (%) N Indexed   (%) N Frames"
+  print " Run   N Hits   (%) N Indexed   (%) N High qual   (%)  %HQR   N Frames"
 
   hit_total = 0
   indexed_total = 0
+  high_quality_total = 0
   overall_total = 0
 
   app = xfel_db_application(params)
   for run_no in params.run:
-    timestamps, n_strong, average_i_sigi_low, average_i_sigi_high = HitrateStats(app, run_no, params.trial, params.rungroup)()
+    try:
+      timestamps, n_strong, average_i_sigi_low, average_i_sigi_high = HitrateStats(app, run_no, params.trial, params.rungroup, params.d_min)()
+    except Exception, e:
+      print "Coulnd't get run", run_no
+      continue
     n_hit = (n_strong >= params.hit_cutoff).count(True)
     n_indexed = (average_i_sigi_low > 0).count(True)
     n_total = len(timestamps)
-    print "% 4d  % 7d % 5.1f   % 7d % 5.1f  % 7d" % (run_no, n_hit, 100*n_hit/n_total, n_indexed, 100*n_indexed/n_total, n_total)
+    n_high_quality = (average_i_sigi_high > 0).count(True)
+    try:
+      print "% 4d  % 7d % 5.1f   % 7d % 5.1f     % 7d % 5.1f % 5.1f    % 7d " % (run_no, n_hit, 100*n_hit/n_total, n_indexed, 100*n_indexed/n_total, n_high_quality, 100*n_high_quality/n_total, 100*n_high_quality/n_indexed, n_total)
+    except ZeroDivisionError:
+      print "% 4d  % 7d % 5.1f   % 7d % 5.1f     % 7d % 5.1f % 5.1f    % 7d " % (run_no, n_hit, 0, n_indexed, 0, n_high_quality, 0, 0, n_total)
 
     hit_total += n_hit
     indexed_total += n_indexed
+    high_quality_total += n_high_quality
     overall_total += n_total
 
   if len(params.run) > 1:
     print "-" * 80
-    print "Total % 7d % 5.1f   % 7d % 5.1f  % 7d" % (hit_total, 100*hit_total/overall_total, indexed_total, 100*indexed_total/overall_total, overall_total)
+    try:
+      print "Total % 7d % 5.1f   % 7d % 5.1f     % 7d % 5.1f % 5.1f    % 7d " % (hit_total, 100*hit_total/overall_total, indexed_total, 100*indexed_total/overall_total, high_quality_total, 100*high_quality_total/overall_total, 100*high_quality_total/indexed_total, overall_total)
+    except ZeroDivisionError:
+      if overall_total == 0:
+        print "Total % 7d % 5.1f   % 7d % 5.1f     % 7d % 5.1f % 5.1f    % 7d " % (hit_total, 0, indexed_total, 0, 0, 0, 0, overall_total)
+      else:
+        print "Total % 7d % 5.1f   % 7d % 5.1f     % 7d % 5.1f % 5.1f    % 7d " % (hit_total, 100*hit_total/overall_total, indexed_total, 100*indexed_total/overall_total, high_quality_total, 100*high_quality_total/overall_total, 0, overall_total)
 
 if __name__ == "__main__":
   run(sys.argv[1:])
