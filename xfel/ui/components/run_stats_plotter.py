2f77996f431455c3daed88ceb55dda19198d2a8b
Iris Young <idyoung@lbl.gov>
2016-07-19 02:50:07 +0000

    In non-interactive mode, refuse to plot the image if there is no data (speeds up the XFEL UI). Expose parameters for adjusting the figure size and reduce the font size.

--- xfel/ui/components/run_stats_plotter.py
+++ xfel/ui/components/run_stats_plotter.py
@@ -46,21 +46,28 @@ def get_run_stats(timestamps,
           tuple_of_timestamp_boundaries,
           run_numbers)
 
-def plot_run_stats(stats, d_min, interactive=True):
+def plot_run_stats(stats, d_min, interactive=True, xsize=100, ysize=100):
   t, n_strong, hits, idx_rate, idx_low_sel, idx_high_sel, isigi_low, isigi_high, \
   window, lengths, boundaries, run_numbers = stats
+  if len(t) == 0:
+    return None
   f, (ax1, ax2, ax3, ax4) = plt.subplots(4, sharex=True, sharey=False)
   ax1.scatter(t.select(~idx_low_sel), n_strong.select(~idx_low_sel), edgecolors="none", color ='grey')
   ax1.scatter(t.select(idx_low_sel), n_strong.select(idx_low_sel), edgecolors="none", color='blue')
   ax1.axis('tight')
-  ax1.set_ylabel("strong spots\nblue: indexed\ngray: did not index")
+  ax1.set_ylabel("strong spots\nblue: indexed\ngray: did not index").set_fontsize(4)
   ax2.plot(t, idx_rate*100)
   ax2.axis('tight')
-  ax2.set_ylabel("indexing rate (%%)\nper %d frames" % window)
+  ax2.set_ylabel("indexing rate (%%)\nper %d frames" % window).set_fontsize(4)
   ax3.scatter(t, isigi_low, edgecolors="none", color='red')
   ax3.scatter(t, isigi_high, edgecolors="none", color='orange')
   ax3.axis('tight')
-  ax3.set_ylabel("signal-to-noise\nred: low res\nyellow: %3.1f Angstroms" % d_min)
+  ax3.set_ylabel("signal-to-noise\nred: low res\nyellow: %3.1f Angstroms" % d_min).set_fontsize(4)
+  for a in [ax1, ax2, ax3, ax4]:
+    xlab = a.get_xticklabels()
+    ylab = a.get_yticklabels()
+    for l in xlab + ylab:
+      l.set_fontsize(4)
   f.subplots_adjust(hspace=0)
   # add lines and text summaries at the timestamp boundaries
   for boundary in boundaries:
@@ -81,19 +88,21 @@ def plot_run_stats(stats, d_min, interactive=True):
     slice_idx_high_sel = idx_high_sel[start:end+1]
     n_idx_low = slice_idx_low_sel.count(True)
     n_idx_high = slice_idx_high_sel.count(True)
-    ax4.text(start_t, .9, "run %d" % run_numbers[idx])
-    ax4.text(start_t, .7, "%d f/%d h" % (lengths[idx], n_hits))
-    ax4.text(start_t, .5, "%d (%d) idx" % (n_idx_low, n_idx_high))
-    ax4.text(start_t, .3, "%-3.1f%% hit" % (100*n_hits/lengths[idx],))
+    ax4.text(start_t, .9, "run %d" % run_numbers[idx]).set_fontsize(4)
+    ax4.text(start_t, .7, "%d f/%d h" % (lengths[idx], n_hits)).set_fontsize(4)
+    ax4.text(start_t, .5, "%d (%d) idx" % (n_idx_low, n_idx_high)).set_fontsize(4)
+    ax4.text(start_t, .3, "%-3.1f%% hit" % (100*n_hits/lengths[idx],)).set_fontsize(4)
     ax4.text(start_t, .1, "%-3.1f (%-3.1f)%% idx" % \
-      (100*n_idx_low/lengths[idx], 100*n_idx_high/lengths[idx]))
-    ax4.set_xlabel("timestamp (s)\n# images shown as all (%3.1f Angstroms)" % d_min)
+      (100*n_idx_low/lengths[idx], 100*n_idx_high/lengths[idx])).set_fontsize(4)
+    ax4.set_xlabel("timestamp (s)\n# images shown as all (%3.1f Angstroms)" % d_min).set_fontsize(4)
     ax4.set_yticks([])
+    for item in [ax1, ax2, ax3, ax4]:
+      item.tick_params(labelsize=4)
     start += lengths[idx]
   if interactive:
     plt.show()
   else:
-    f.savefig("runstats_tmp.png", bbox_inches='tight')
+    f.savefig("runstats_tmp.png", bbox_inches='tight', figsize=(xsize, ysize), dpi=300)
     plt.close(f)
     return "runstats_tmp.png"
 
@@ -102,7 +111,9 @@ def plot_multirun_stats(runs,
                         d_min,
                         n_strong_cutoff=40,
                         interactive=False,
-                        compress_runs=True):
+                        compress_runs=True,
+                        xsize=100,
+                        ysize=100):
   tset = flex.double()
   nset = flex.int()
   I_sig_I_low_set = flex.double()
