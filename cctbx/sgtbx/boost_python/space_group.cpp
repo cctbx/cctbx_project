1b58041f1ad2ab9b39657dd5181c4bc8fad182ba
Robert Oeffner <robert@oeffner.net>
2016-07-26 14:47:06 +0000

    When restoring from pickling tidy the spacegroup order of operations because asu_mappings.find_i_sym(i_seq, rt_mx) rely on the order being retained. Do this in a setstate method that ignores its arguments and only calls make_tidy() on itself. Likewise the corresponding getstate() is a dummy returning an empty tuple.

--- cctbx/sgtbx/boost_python/space_group.cpp
+++ cctbx/sgtbx/boost_python/space_group.cpp
@@ -37,7 +37,23 @@ namespace {
     {
       return boost::python::make_tuple(o.type().hall_symbol());
     }
+/* Must tidy the spacegroup order of operations because 
+   asu_mappings.find_i_sym(i_seq, rt_mx) rely on the order being retained.
+   We do this in a setstate method that ignores its arguments and only calls make_tidy()
+   on itself. Likewise the corresponding getstate() is a dummy returning an empty tuple.
+*/
+    static boost::python::tuple
+      getstate(w_t const& o)
+    {
+      return boost::python::tuple();
+    }
 
+    static void
+      setstate(w_t& o, boost::python::tuple state)
+    {
+      o.make_tidy();
+    }
+      
     static void
     wrap()
     {
