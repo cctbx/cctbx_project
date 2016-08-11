daa05cfe5fd9968cccb71c46022b9551d0fc1ded
Graeme Winter <graeme.winter@gmail.com>
2016-08-11 13:06:47 +0000

    Added support for another instrument where this was a problem; FIXME we should probably make this more ... general

--- dxtbx/format/FormatSMVRigakuSaturnSN09040159.py
+++ dxtbx/format/FormatSMVRigakuSaturnSN09040159.py
@@ -12,9 +12,11 @@ from __future__ import division
 
 from scitbx import matrix
 
-from dxtbx.format.FormatSMVRigakuSaturn import FormatSMVRigakuSaturn
+from dxtbx.format.FormatSMVRigakuSaturnNoTS import FormatSMVRigakuSaturnNoTS
 
-class FormatSMVRigakuSaturn09040159(FormatSMVRigakuSaturn):
+SERIALS = ['09040159', '07510101']
+
+class FormatSMVRigakuSaturn09040159(FormatSMVRigakuSaturnNoTS):
   '''A class for reading SMV format Rigaku Saturn images, and correctly
   constructing a model for the experiment from this. This is the custom
   version for instrument with serial number 09040159 at CSHL.'''
@@ -25,7 +27,7 @@ class FormatSMVRigakuSaturn09040159(FormatSMVRigakuSaturn):
     i.e. we can make sense of it. Essentially that will be if it contains
     all of the keys we are looking for. Also checks the serial number.'''
 
-    size, header = FormatSMVRigakuSaturn.get_smv_header(image_file)
+    size, header = FormatSMVRigakuSaturnNoTS.get_smv_header(image_file)
 
     detector_prefix = header['DETECTOR_NAMES'].split()[0].strip()
     try:
@@ -33,7 +35,7 @@ class FormatSMVRigakuSaturn09040159(FormatSMVRigakuSaturn):
     except KeyError:
       return False
 
-    if serial_number != '09040159':
+    if not serial_number in SERIALS:
       return False
 
     return True
