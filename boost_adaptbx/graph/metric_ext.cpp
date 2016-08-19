d2ac69a86b4802ea7c1f6a1c87dd08c98af30ae2
Gabor Bunkoczi <gbunkoczi@users.noreply.github.com>
2016-08-19 11:40:50 +0000

    Export and tests for brandes_betweenness_centrality

---
+++ boost_adaptbx/graph/metric_ext.cpp
@@ -0,0 +1,111 @@
+#include <boost_adaptbx/graph/graph_type.hpp>
+#include <boost_adaptbx/graph/graph_export_adaptor.hpp>
+#include <boost_adaptbx/graph/vertex_map.hpp>
+
+#include <boost_adaptbx/exporting.hpp>
+
+#include <boost/python/module.hpp>
+#include <boost/python/tuple.hpp>
+#include <boost/python/dict.hpp>
+#include <boost/python/def.hpp>
+
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/betweenness_centrality.hpp>
+
+#include <boost/mpl/if.hpp>
+#include <boost/type_traits.hpp>
+
+namespace boost_adaptbx
+{
+namespace
+{
+
+template< typename Graph >
+struct brandes_betweenness_centrality_export
+{
+  typedef boost::graph_traits< Graph > graph_traits;
+  typedef typename graph_traits::vertex_iterator vertex_iterator;
+  typedef typename graph_traits::edge_iterator edge_iterator;
+  typedef typename graph_traits::vertex_descriptor vertex_descriptor;
+  typedef graph_export_adaptor::vertex_descriptor_converter< vertex_descriptor > converter;
+  typedef vertex_map::index_map< Graph > index_map_type;
+  typedef vertex_map::generic_vertex_map< Graph, double > vertex_centrality_map_type;
+  typedef edge_map::generic_edge_map< Graph, double > edge_centrality_map_type;
+  typedef edge_centrality_map_type weight_map_type;
+  typedef typename weight_map_type::property_map_type weight_property_map_type;
+
+  static boost::python::tuple brandes_betweenness_centrality(Graph const& graph)
+  {
+    using namespace boost;
+    index_map_type index_map( graph );
+    vertex_centrality_map_type vertex_centrality_map( graph );
+    edge_centrality_map_type edge_centrality_map( graph );
+    weight_map_type weight_map( graph );
+    weight_property_map_type wpropmap( weight_map.get() );
+    edge_iterator ei, ej;
+    vertex_iterator vi, vj;
+
+    double w;
+
+    for( boost::tie( ei, ej ) = boost::edges( graph ); ei != ej; ++ei )
+    {
+      w = boost::python::extract< double >( boost::get( boost::edge_weight, graph, *ei ) );
+      boost::put( wpropmap, *ei, w);
+    }
+
+    boost::brandes_betweenness_centrality(
+      graph,
+      centrality_map( vertex_centrality_map.get() ).
+      edge_centrality_map( edge_centrality_map.get() ).
+      vertex_index_map( index_map.get()  ).
+      weight_map( wpropmap )
+      );
+
+    boost::python::dict vertex_centrality_dict;
+    boost::python::dict edge_centrality_dict;
+
+    for( boost::tie( vi, vj ) = boost::vertices( graph ); vi != vj; ++vi )
+    {
+      vertex_centrality_dict[ converter::forward( *vi ) ] = boost::get(
+        vertex_centrality_map.get(),
+        *vi
+        );
+    }
+
+    for( boost::tie( ei, ej ) = boost::edges( graph ); ei != ej; ++ei )
+    {
+      edge_centrality_dict[ *ei ] = boost::get( edge_centrality_map.get(), *ei );
+    }
+
+    return boost::python::make_tuple( vertex_centrality_dict, edge_centrality_dict );
+  }
+
+  static void process()
+  {
+    using namespace boost::python;
+
+    def( "brandes_betweenness_centrality", brandes_betweenness_centrality, arg( "graph" ) );
+  }
+};
+
+struct metric_exporter
+{
+  template< typename Export >
+  void operator ()(boost::mpl::identity< Export > myexport) const
+  {
+    typedef typename Export::first graph_type;
+    typedef typename Export::second name_type;
+
+    brandes_betweenness_centrality_export< graph_type >::process();
+  }
+};
+
+} // namespace <anonymous>
+} // namespace boost_adaptbx
+
+BOOST_PYTHON_MODULE(boost_adaptbx_graph_metric_ext)
+{
+  boost_adaptbx::exporting::class_list< boost_adaptbx::graph_type::exports >::process(
+    boost_adaptbx::metric_exporter()
+    );
+}
---
+++ boost_adaptbx/graph/tests/tst_graph_metric.py
@@ -0,0 +1,151 @@
+from __future__ import division
+
+from boost_adaptbx import graph
+from boost_adaptbx.graph import metric
+
+import unittest
+
+
+class TestBrandesBetweennessCentrality(unittest.TestCase):
+
+  def graph_build(self, g):
+
+    vds = [ g.add_vertex() for i in range( 6 ) ]
+    return (
+      vds,
+      (
+        g.add_edge( vertex1 = vds[0], vertex2 = vds[1], weight = 1 )[0],
+        g.add_edge( vertex1 = vds[0], vertex2 = vds[2], weight = 1 )[0],
+        g.add_edge( vertex1 = vds[1], vertex2 = vds[2], weight = 1 )[0],
+        g.add_edge( vertex1 = vds[2], vertex2 = vds[3], weight = 1 )[0],
+        g.add_edge( vertex1 = vds[4], vertex2 = vds[5], weight = 1 )[0],
+        g.add_edge( vertex1 = vds[4], vertex2 = vds[3], weight = 1 )[0],
+        g.add_edge( vertex1 = vds[3], vertex2 = vds[5], weight = 1 )[0],
+        )
+      )
+
+
+  def undirected_results(self):
+
+    return ( [ 0, 0, 6, 6, 0, 0 ], [ 1, 4, 4, 9, 1, 4, 4 ] )
+
+
+  def directed_results(self):
+
+    return ( [ 0, 0, 4, 3, 0, 0 ], [ 1, 3, 3, 6, 1, 1, 4 ] )
+
+
+  def build_and_test(self, g, results):
+
+    ( vds, eds ) = self.graph_build( g )
+    ( exp_vcs, exp_ecs ) = results
+    ( vcmap, ecmap ) = metric.brandes_betweenness_centrality( graph = g )
+
+    self.assertEqual( len( vcmap ), len( exp_vcs ) )
+    self.assertTrue( all( vd in vcmap for vd in vds ) )
+    self.assertEqual( exp_vcs, [ int( vcmap[ vd ] ) for vd in vds ] )
+
+    self.assertEqual( len( ecmap ), len( exp_ecs ) )
+    self.assertTrue( all( ed in ecmap for ed in eds ) )
+    self.assertEqual( exp_ecs, [ int( ecmap[ ed ] ) for ed in eds ] )
+
+
+  def test_adjacency_list_undirected_vector_set(self):
+
+    try:
+      g = graph.adjacency_list(
+        graph_type = "undirected",
+        vertex_type = "vector",
+        edge_type = "set",
+        )
+
+    except NotImplementedError:
+      pass
+
+    else:
+      self.build_and_test( g, self.undirected_results() )
+
+
+  def test_adjacency_list_undirected_list_set(self):
+
+    try:
+      g = graph.adjacency_list(
+        graph_type = "undirected",
+        vertex_type = "list",
+        edge_type = "set",
+        )
+
+    except NotImplementedError:
+      pass
+
+    else:
+      self.build_and_test( g, self.undirected_results() )
+
+
+  def test_adjacency_list_undirected_vector_vector(self):
+
+    try:
+      g = graph.adjacency_list(
+        graph_type = "undirected",
+        vertex_type = "vector",
+        edge_type = "vector",
+        )
+
+    except NotImplementedError:
+      pass
+
+    else:
+      self.build_and_test( g, self.undirected_results() )
+
+
+  def test_adjacency_list_undirected_list_vector(self):
+
+    try:
+      g = graph.adjacency_list(
+        graph_type = "undirected",
+        vertex_type = "list",
+        edge_type = "vector",
+        )
+
+    except NotImplementedError:
+      pass
+
+    else:
+      self.build_and_test( g, self.undirected_results() )
+
+
+  def test_adjacency_list_directed_vector_vector(self):
+
+    try:
+      g = graph.adjacency_list(
+        graph_type = "directed",
+        vertex_type = "vector",
+        edge_type = "vector",
+        )
+
+    except NotImplementedError:
+      pass
+
+    else:
+      self.build_and_test( g, self.directed_results() )
+
+
+suite_brandes_betweenness_centrality = unittest.TestLoader().loadTestsFromTestCase(
+  TestBrandesBetweennessCentrality
+  )
+
+
+alltests = unittest.TestSuite(
+  [
+    suite_brandes_betweenness_centrality,
+    ]
+  )
+
+
+def load_tests(loader, tests, pattern):
+
+  return alltests
+
+
+if __name__ == "__main__":
+  unittest.TextTestRunner( verbosity = 2 ).run( alltests )
