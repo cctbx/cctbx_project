912ce65bb9871be59f86c261f62f3d358a7d66b6
graeme-winter <graeme.winter@gmail.com>
2016-11-29 09:48:05 +0000

    Now recurses the tree

--- dxtbx/command_line/show_matching_formats.py
+++ dxtbx/command_line/show_matching_formats.py
@@ -1,4 +1,16 @@
 from __future__ import division
+
+def recurse(fmt, arg):
+  for _fmt in fmt._children:
+    if _fmt.understand(arg):
+      print _fmt.__name__, _fmt.understand(arg)
+      recurse(_fmt, arg)
+    else:
+      print fmt.__name__, fmt.understand(arg)
+
+
+
+
 def show_matching_formats():
   import sys
   from dxtbx.format.Registry import Registry
@@ -6,7 +18,11 @@ def show_matching_formats():
   for arg in sys.argv[1:]:
     print '=== %s ===' % arg
     for fmt in Registry.get():
-      print fmt.__name__, fmt.understand(arg)
+      if fmt.understand(arg):
+        print fmt.__name__, fmt.understand(arg)
+        recurse(fmt, arg)
+      else:
+        print fmt.__name__, fmt.understand(arg)
 
 if __name__ == '__main__':
   show_matching_formats()
