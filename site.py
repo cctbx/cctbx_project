c1a3f074ef3667f7cf10b584d0d9755f9af629be
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-01-24 15:12:15 +0000

    Skip cleanup routine in site.py

--- site.py
+++ site.py
@@ -1,5 +1,10 @@
 #
-# 2017-01-18
+#   Copyright (C) 2017 Diamond Light Source, Markus Gerstel
+#
+#   This code is distributed under the BSD license, a copy of which is
+#   attached at the end of this file.
+#
+# 2017-01-24
 #
 # This is a proof-of-concept test.
 #
@@ -68,5 +73,33 @@ if _libtbx['true_site']:
     sys.modules['site'] = imp.load_module('site', *_libtbx['true_site'])
     __file__ = sys.modules['site'].__file__
 
-# Clean up
-del(_libtbx)
+# The end.
+
+# Copyright (c) 2017 Diamond Light Source, Lawrence Berkeley National Laboratory
+# and the Science and Technology Facilities Council, All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# Neither the name of the Diamond Light Source, Lawrence Berkeley National
+# Laboratory or the Science and Technology Facilities Council, nor the names of
+# its contributors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
