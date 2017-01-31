98e5f84fefc12cac65f1fd7a4572a333bba3f583
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-01-31 22:03:13 +0000

    Add command line help/help string as per NKS' request

--- libtbx/command_line/git_avoid_merges.py
+++ libtbx/command_line/git_avoid_merges.py
@@ -3,6 +3,34 @@ import libtbx.easy_run
 import libtbx.load_env
 import os
 import re
+import sys
+
+what_is_this = '''
+
+The command libtbx.git_avoid_merges identifies all module directories that are
+git repositories (ie. it checks all paths listed by libtbx.show_dist_paths),
+and changes the git settings of those repositories to 'rebase' by default
+instead of creating merge commits. The global git settings are also changed
+so any new git repositories are also set to 'rebase' by default.
+Any existing settings are left as they are.
+
+What does 'rebase' vs 'merge' mean?
+A 'git pull' (without rebase setting or '--rebase' option) will silently
+introduce a new commit, a so-called merge commit, if you and someone else have
+committed anything since the last time you pulled. Invariably this will create
+a lot of merge commits, going quite far back in history as well. Merge commits
+will generally have a useless commit message, clutter the project history and
+make it difficult to see what was going on. With our repositories the merge
+commits will also cause pointless notification mail spam.
+
+A 'rebase' will take your commits off the current tree and tack them back on to
+the most recent commit. This results in a linear history.
+
+Now there may be situations where you want to override this default policy.
+You can do that by specifying --no-rebase on the command line, eg.:
+   git pull --no-rebase
+
+'''
 
 def find_all_git_modules():
   for path in (abs(p) for p in libtbx.env.repository_paths):
@@ -69,4 +97,7 @@ def run():
   print "\nAll done."
 
 if __name__ == "__main__":
-  run()
+  if len(sys.argv) != 1:
+    print what_is_this
+  else:
+    run()
