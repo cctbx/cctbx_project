8187b3b445d68e48775c9e4c1de607f7930ba0c2
idyoung <idyoung@lbl.gov>
2017-02-14 14:46:45 -0800

    XFEL UI: necessary for the printed timings.

---
+++ xfel/ui/components/timeit.py
@@ -0,0 +1,18 @@
+from __future__ import division
+
+import time, math
+
+def now():
+  return "%02d:%02d:%02d" % (time.localtime().tm_hour, time.localtime().tm_min, time.localtime().tm_sec)
+
+def duration(t1, t2):
+  diff = t2 - t1
+  seconds = int(math.floor(diff))
+  frac = diff - seconds
+  hh = seconds // 3600
+  mm = seconds // 60
+  if hh > 0:
+    mm = mm % 60
+  ss = seconds % 60
+  return "%02dh %02dm %fs" % (hh, mm, ss + frac)
+
