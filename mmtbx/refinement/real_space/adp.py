e626d9a430ba08f461992c12000e8de80b771edb
Pavel <pafonine@lbl.gov>
2017-04-13 06:30:30 -0700

    stop gap for poor ss based selections

--- mmtbx/refinement/real_space/adp.py
+++ mmtbx/refinement/real_space/adp.py
@@ -6,6 +6,59 @@ from libtbx import adopt_init_args
 from cctbx import adptbx
 from libtbx import easy_mp
 
+from libtbx.test_utils import approx_equal
+import mmtbx.secondary_structure
+from libtbx.utils import null_out
+from itertools import groupby
+from operator import itemgetter
+import iotbx.phil
+
+def get_chain_selections(pdb_hierarchy):
+  pdb_hierarchy.atoms().reset_i_seq()
+  params = mmtbx.secondary_structure.sec_str_master_phil.extract()
+  params.secondary_structure.protein.search_method="from_ca"
+  ssm = mmtbx.secondary_structure.manager(
+    pdb_hierarchy                = pdb_hierarchy,
+    sec_str_from_pdb_file        = None,
+    params                       = params.secondary_structure,
+    log                          = null_out())
+  n_atoms = pdb_hierarchy.atoms().size()
+  helix_sel = [s.iselection() for s in ssm.helix_selections()]
+  beta_sel  = [s.iselection() for s in ssm.beta_selections()]
+  bp_sel    = [s.iselection() for s in ssm.base_pair_selections()]
+  ss_all = helix_sel + beta_sel + bp_sel
+  #
+  chain_selections = []
+  for sel in ss_all:
+    ph = pdb_hierarchy.select(sel)
+    isels = ph.atoms().extract_i_seq()
+    chain_selections.append(isels)
+  #
+  ss_all_as_one_array = ss_all[0]
+  if(len(ss_all)>1):
+    for s in ss_all[1:]:
+      ss_all_as_one_array.extend(s)
+  ss_all_as_one_array_bool = flex.bool(n_atoms, ss_all_as_one_array)
+  #
+  iseqs = []
+  for atom in pdb_hierarchy.atoms():
+    if(not ss_all_as_one_array_bool[atom.i_seq]):
+      iseqs.append(atom.i_seq)
+  for k, g in groupby(enumerate(iseqs), lambda (i, x): i-x):
+    isels = list(map(itemgetter(1), g))
+    chain_selections.append(flex.size_t(isels))
+  # DEBUG
+  C1 = flex.size_t()
+  for s in chain_selections:
+    C1.extend(s)
+  s = flex.sort_permutation(C1)
+  C1 = C1.select(s)
+  C2 = flex.size_t(xrange(n_atoms))
+  asert approx_equal(C1, C2)
+  assert C1.size()==C2.size()
+  #
+  return chain_selections
+
 class real_space_group_adp_refinery_via_reciprocal_space(object):
   def __init__(self,
                target_map,
@@ -29,9 +82,11 @@ class real_space_group_adp_refinery_via_reciprocal_space(object):
     #    value     = flex.mean(b_isos.select(sel)),
     #    selection = sel)
     self.pdb_hierarchy.adopt_xray_structure(self.xray_structure)
-    self.chain_selections = []
-    for chain in self.pdb_hierarchy.chains():
-      self.chain_selections.append(chain.atoms().extract_i_seq())
+    #self.chain_selections = []
+    #for chain in self.pdb_hierarchy.chains():
+    #  self.chain_selections.append(chain.atoms().extract_i_seq())
+    self.chain_selections = get_chain_selections(
+      pdb_hierarchy = self.pdb_hierarchy)
 
   def refine_box_with_selected(self, selection=None):
     if(selection is None): selections = self.xray_structure.all_selection()
@@ -45,15 +100,20 @@ class real_space_group_adp_refinery_via_reciprocal_space(object):
       mask_atoms_atom_radius = self.atom_radius)
     ph_box.adopt_xray_structure(box.xray_structure_box)
     group_adp_sel = []
+    
+    #group_adp_sel.append(ph_box.atoms().extract_i_seq())
+    
     for rg in ph_box.residue_groups():
       group_adp_sel.append(rg.atoms().extract_i_seq())
+      
     f_obs_box = abs(box.box_map_coefficients(d_min = self.target_map.d_min))
     fmodel = mmtbx.f_model.manager(
       f_obs          = f_obs_box,
       xray_structure = box.xray_structure_box)
 
     #XXX
-    fmodel.update_all_scales(update_f_part1=False, apply_back_trace=True)
+    fmodel.update_all_scales(update_f_part1=False, apply_back_trace=True,
+      remove_outliers=False)
     #XXX
 
     fmodel.update(target_name="ls_wunit_k1")
