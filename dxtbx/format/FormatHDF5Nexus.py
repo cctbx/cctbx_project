61e72b5ff6b721bf720697ada9d594f4e79000a1
Aaron Brewster <asbrewster@lbl.gov>
2017-04-17 15:14:36 -0700

    Turn off FormatHDF5Nexus.  This was implemented to an older example and has been superceeded by FormatNexus

--- dxtbx/format/FormatHDF5Nexus.py
+++ dxtbx/format/FormatHDF5Nexus.py
@@ -6,6 +6,7 @@ class FormatHDF5Nexus(FormatHDF5):
 
   @staticmethod
   def understand(image_file):
+    return False
     try:
       tag = FormatHDF5.open_file(image_file, 'rb').read(8)
     except IOError, e:
