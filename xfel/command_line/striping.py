01680adf664d092bbb9a0d398c8814b61634b03d
idyoung <idyoung@lbl.gov>
2017-05-04 11:16:41 -0700

    Add refinement to striping. Refactor phil parsing.
    dials.refine automatically follows dials.combine_experiments, either for the per-chunk combined experiments or serially for each cluster produced from a chunk (although this is not parameterized correctly by default yet). Phil parsing begins with master default options and is sequentially overridden by striping defaults, phil file defaults and any command line keyword arguments, in that order. Diff phils are written for reproducibility.

--- xfel/command_line/striping.py
+++ xfel/command_line/striping.py
@@ -11,11 +11,21 @@ from libtbx.phil import parse
 from libtbx.utils import Sorry
 from libtbx import easy_run
 from xfel.util.dials_file_matcher import match_dials_files
-from xfel.util.mp import mp_phil_str, get_submit_command_chooser
+from xfel.util.mp import mp_phil_str as multiprocessing_str
+from xfel.util.mp import get_submit_command_chooser
+# from dials.command_line.combine_experiments import phil_scope as combine_phil_scope
+# from dials.command_line.refine import phil_scope as refine_phil_scope
 
 import os, math
 
+multiprocessing_override_str = '''
+mp {
+  use_mpi = False
+}
+'''
+
 striping_str = '''
+striping {
   results_dir = None
     .type = path
     .help = "LCLS results directory containint runs starting with r."
@@ -29,22 +39,105 @@ striping_str = '''
   chunk_size = 1000
     .type = float
     .help = "Maximum number of images per chunk or stripe."
-  combine_experiments {
-    phil = None
-      .type = path
-      .help = "Path to a phil file to be used with dials.combine_experiments."
-    interactive = False
-      .type = bool
-      .help = "Overrides any multiprocessing parameters to allow interactive"
-      .help = "run. Clustering dendrograms can only be displayed in this mode."
-    keep_integrated = False
-      .type = bool
-      .help = "Combine refined_experiments.json and integrated.pickle files."
-      .help = "If False, ignore integrated.pickle files in favor of"
-      .help = "indexed.pickle files in preparation for reintegrating."
-  }
+}
 '''
-phil_scope = parse(striping_str + mp_phil_str, process_includes=True)
+
+combining_str = '''
+combine_experiments {
+  interactive = False
+    .type = bool
+    .help = "Overrides any multiprocessing parameters to allow interactive"
+    .help = "run. Clustering dendrograms can only be displayed in this mode."
+  keep_integrated = False
+    .type = bool
+    .help = "Combine refined_experiments.json and integrated.pickle files."
+    .help = "If False, ignore integrated.pickle files in favor of"
+    .help = "indexed.pickle files in preparation for reintegrating."
+  include scope dials.command_line.combine_experiments.phil_scope
+}
+'''
+
+combining_override_str = '''
+combine_experiments {
+  output {
+    experiments_filename = %s_combined_experiments.json
+    reflections_filename = %s_combined_reflections.pickle
+    delete_shoeboxes = True
+  }
+}
+'''
+
+# future feature: filter experiments by rmsd after combining/clustering
+filtering_str = '''
+filtering {
+  enable = False
+}
+'''
+
+refinement_str = '''
+refinement {
+  include scope dials.command_line.refine.phil_scope
+  input {
+    experiments = None
+    reflections = None
+  }
+}
+'''
+
+refinement_override_str = '''
+refinement {
+  output {
+    experiments = %s_refined_experiments_CLUSTER.json
+    reflections = %s_refined_reflections_CLUSTER.pickle
+    include_unused_reflections = False
+    log = %s_refine_CLUSTER.log
+    debug_log = %s_refine_CLUSTER.debug.log
+  }
+  refinement {
+    parameterisation {
+      auto_reduction {
+        action = remove
+      }
+      compose_model_per = image
+      beam {
+        fix = all
+      }
+    }
+    refinery {
+      engine = SparseLevMar
+    }
+    reflections {
+      outlier {
+        algorithm = sauter_poon
+        minimum_number_of_reflections = 3
+        separate_experiments = False
+        separate_panels = True
+      }
+    }
+  }
+  input {
+    experiments = %s_combined_experiments_CLUSTER.json
+    reflections = %s_combined_reflections_CLUSTER.pickle
+  }
+}
+'''
+
+# future feature: reintegrate after joint refinement
+postprocessing_str = '''
+postprocessing {
+  enable = False
+}
+'''
+
+master_defaults_str = multiprocessing_str + striping_str + combining_str + filtering_str + \
+                        refinement_str + postprocessing_str
+
+# initialize a master scope from the multiprocessing phil string
+master_defaults_scope = parse(master_defaults_str, process_includes=True)
+# update master scope with customized and local phil scopes
+master_scope = master_defaults_scope.fetch(parse(refinement_override_str, process_includes=True))
+master_scope = master_scope.fetch(parse(combining_override_str, process_includes=True))
+master_scope = master_scope.fetch(parse(multiprocessing_override_str, process_includes=True))
 
 def allocate_chunks_per_rungroup(results_dir, trial_no, stripe=False, max_size=1000, integrated=False):
   refl_ending = "_integrated.pickle" if integrated else "_indexed.pickle"
@@ -113,84 +206,161 @@ def allocate_chunks_per_rungroup(results_dir, trial_no, stripe=False, max_size=1
         (rg, len(rg_chunks[rg][0][0]), len(rg_chunks[rg]))
   return rg_chunks
 
+def parse_retaining_scope(args, master_scope=master_scope):
+  if "-c" in args:
+    master_scope.show(attributes_level=2)
+    return
+  file_phil = []
+  cmdl_phil = []
+  for arg in args:
+    if os.path.isfile(arg):
+      try:
+        file_phil.append(parse(file_name=arg))
+      except Exception, e:
+        raise Sorry("Unrecognized file: %s" % arg)
+    else:
+      try:
+        cmdl_phil.append(parse(arg))
+      except Exception, e:
+        raise Sorry("Unrecognized argument: %s" % arg)
+
+  run_scope = master_scope.fetch(sources=file_phil)
+  run_scope = run_scope.fetch(sources=cmdl_phil)
+  return run_scope
+
+def script_to_expand_over_clusters(clustered_json_name,
+                                   phil_template_name, command, location):
+  """
+  Write a bash script to find results of a clustering step and produce customized
+  phils and commands to run with each of them. For example, run the command
+  dials.refine ...cluster8.json ...cluster8.pickle ...cluster8.phil followed by
+  dials.refine ...cluster9.json ...cluster9.pickle ...cluster9.phil.
+  clustered_json_name, clustered_refl_name and phil_template_name must each
+  contain an asterisk, and substitution in phil_template itself will occur at
+  each instance of CLUSTER.
+  """
+  import pdb; pdb.set_trace()
+  clj_part_first, clj_part_last = clustered_json_name.split("CLUSTER")
+  ph_part_first, ph_part_last = phil_template_name.split("CLUSTER")
+  # ph_part_first = "refine_" + ph_part_first.split("combine_")[1]
+  clustered_template_name = "*".join([clj_part_first, clj_part_last])
+
+  bash_str = '''
+#! /bin/sh
+
+for file in `ls {clname}`
+  do export cluster=`echo $file | sed "s:{cljfirst}::; s:{cljlast}::"`
+  export philname="{phfirst}${cluster}{phlast}"
+  export outname=`echo $philname | sed "s:.phil:.out:"`
+  sed "s:CLUSTER:${cluster}:g" {phtempl} > $philname
+  {command} $philname > $outname
+done
+'''.format(clname=clustered_template_name, phtempl=phil_template_name,
+           cljfirst=clj_part_first, cljlast=clj_part_last,
+           phfirst=ph_part_first, phlast=ph_part_last,
+           command=command, cluster="{cluster}")
+
+  bash_name = "generator".join([ph_part_first, ph_part_last]).split(".phil")[0] + ".sh"
+  with open(os.path.join(location, bash_name), "wb") as script:
+    script.write(bash_str)
+  return bash_name
 
 class Script(object):
 
   def __init__(self):
     '''Initialise the script.'''
-    from dials.util.options import OptionParser
-    import libtbx.load_env
 
     # The script usage
-    usage  = "usage: %s [options] [param.phil] " \
-             "*refined_experiments.json [*integrated.pickle]" \
-             "[*indexed.pickle]" \
-             % libtbx.env.dispatcher_name
-
-    # Create the parser
-    self.parser = OptionParser(
-      usage=usage,
-      phil=phil_scope)
+    self.master_defaults_scope = master_defaults_scope
+    self.run_scope = parse_retaining_scope(sys.argv[1:])
+    self.params = self.run_scope.extract()
 
   def run(self):
     '''Execute the script.'''
 
-    # Parse the command line
-    params, options = self.parser.parse_args(show_diff_phil=False)
-
-    # Set up template phil file for dials.combine_experiments
-    import shutil
-    template = "template_combine_experiments_phil"
-    if params.combine_experiments.phil is not None:
-      shutil.copyfile(params.combine_experiments.phil, template)
-    else:
-      open(template, "wb").close()
-
-    rg_chunks = allocate_chunks_per_rungroup(params.results_dir,
-      params.trial, stripe=params.stripe, max_size=params.chunk_size,
-      integrated=params.combine_experiments.keep_integrated)
-    dirname = "combine_experiments_t%03d" % params.trial
+    rg_chunks = allocate_chunks_per_rungroup(self.params.striping.results_dir,
+                                             self.params.striping.trial,
+                                             stripe=self.params.striping.stripe,
+                                             max_size=self.params.striping.chunk_size,
+                                             integrated=self.params.combine_experiments.keep_integrated)
+    dirname = "combine_experiments_t%03d" % self.params.striping.trial
     if not os.path.isdir(dirname):
       os.mkdir(dirname)
     cwd = os.getcwd()
-    tag = "stripe" if params.stripe else "chunk"
+    tag = "stripe" if self.params.striping.stripe else "chunk"
     for rg, ch_list in rg_chunks.iteritems():
       for idx in xrange(len(ch_list)):
         chunk = ch_list[idx]
-        filename = "t%03d_%s_%s%03d" % (params.trial, rg, tag, idx)
+
+        # get the diff scope
+        diff_scope = self.master_defaults_scope.fetch_diff(self.run_scope)
+
+        # set up the file containing input expts and refls
+        filename = "t%03d_%s_%s%03d" % (self.params.striping.trial, rg, tag, idx)
         chunk_path = os.path.join(cwd, dirname, filename)
         if os.path.isfile(chunk_path):
           os.remove(chunk_path)
         with open(chunk_path, "wb") as outfile:
           for i in (0, 1): # expts then refls
             outfile.write("\n".join(chunk[i]) + "\n")
-        new_phil_filename = filename + ".phil"
-        new_phil_path = os.path.join(cwd, dirname, new_phil_filename)
-        if os.path.isfile(new_phil_path):
-          os.remove(new_phil_path)
-        shutil.copyfile(template, new_phil_path)
-        with open(new_phil_path, "ab") as phil_outfile:
-          phil_outfile.write("input {\n")
-          for expt_path in chunk[0]:
-            phil_outfile.write("  experiments = %s\n" % expt_path)
-          for refl_path in chunk[1]:
-            phil_outfile.write("  reflections = %s\n" % refl_path)
-          phil_outfile.write("}\n")
-          phil_outfile.write("output {\n")
-          phil_outfile.write("  experiments_filename = %s_combined_experiments.json\n" % filename)
-          phil_outfile.write("  reflections_filename = %s_combined_reflections.pickle\n" % filename)
-          if params.combine_experiments.keep_integrated:
-            phil_outfile.write("  delete_shoeboxes = True\n")
-          phil_outfile.write("}\n")
+
+        # set up the params for dials.combine_experiments
+        combine_diff_str = diff_scope.get("combine_experiments").as_str() % (filename, filename)
+        combine_diff_lines = combine_diff_str.split("\n")[:-2]
+        combine_diff_lines.pop(0)
+        combine_diff_lines.append("  input {")
+        for expt_path in chunk[0]:
+          combine_diff_lines.append("    experiments = %s" % expt_path)
+        for refl_path in chunk[1]:
+          combine_diff_lines.append("    reflections = %s" % refl_path)
+        combine_diff_lines.append("  }")
+        combine_diff_str = "\n".join(combine_diff_lines)
+        combine_phil_filename = filename + "_combine.phil"
+        combine_phil_path = os.path.join(cwd, dirname, combine_phil_filename)
+        if os.path.isfile(combine_phil_path):
+          os.remove(combine_phil_path)
+        with open(combine_phil_path, "wb") as phil_outfile:
+          phil_outfile.write(combine_diff_str + "\n")
+
+        # set up the params for dials.refine (to be customized per cluster at execution time)
+        refine_diff_str = diff_scope.get("refinement").as_str() % \
+          (filename, filename, filename, filename, filename, filename)
+        refine_diff_parts = refine_diff_str.split("\n")[:-2]
+        refine_diff_parts.pop(0)
+        refine_diff_str = "\n".join(refine_diff_parts)
+        if self.params.combine_experiments.clustering.use:
+          refine_phil_filename = filename + "_refine_CLUSTER.phil"
+          refine_phil_path = os.path.join(cwd, dirname, refine_phil_filename)
+          if os.path.isfile(refine_phil_path):
+            os.remove(refine_phil_path)
+          with open(refine_phil_path, "wb") as phil_outfile:
+            phil_outfile.write(refine_diff_str + "\n")
+          script = script_to_expand_over_clusters(
+            self.params.refinement.input.experiments[0] % filename,
+            refine_phil_filename,
+            "dials.refine",
+            dirname)
+          refine_command = ". %s" % os.path.join(cwd, dirname, script)
+        else:
+          refine_diff_str = refine_diff_str.replace('_CLUSTER', '')
+          refine_phil_filename = filename + "_refine.phil"
+          refine_phil_path = os.path.join(cwd, dirname, refine_phil_filename)
+          if os.path.isfile(refine_phil_path):
+            os.remove(refine_phil_path)
+          with open(refine_phil_path, "wb") as phil_outfile:
+            phil_outfile.write(refine_diff_str + "\n")
+          refine_command = "dials.refine %s" % refine_phil_filename
+
         # submit queued job from appropriate directory
         os.chdir(dirname)
-        params.mp.use_mpi = False
         submit_path = os.path.join(cwd, dirname, "combine_%s.sh" % filename)
-        command = "dials.combine_experiments %s" % new_phil_filename
-        if params.combine_experiments.interactive:
+        command = "dials.combine_experiments %s && %s" % \
+          (combine_phil_filename, refine_command)
+        import pdb; pdb.set_trace()
+        if self.params.combine_experiments.interactive:
           easy_run.fully_buffered(command).raise_if_errors().show_stdout()
         else:
-          submit_command = get_submit_command_chooser(command, submit_path, dirname, params.mp,
+          submit_command = get_submit_command_chooser(command, submit_path, dirname, self.params.mp,
             log_name=(submit_path.split(".sh")[0] + ".out"))
           print "executing command: %s" % submit_command
           try:
@@ -200,10 +370,15 @@ class Script(object):
               raise e
         os.chdir(cwd)
         # go back to working directory
-    os.remove(template)
 
 if __name__ == "__main__":
   from dials.util import halraiser
+  import sys
+  if "-c" in sys.argv[1:]:
+    master_scope.show(attributes_level=2)
+    with open("striping_defaults.phil", "wb") as defaults:
+      defaults.write(master_scope.as_str())
+    exit()
   try:
     script = Script()
     script.run()
