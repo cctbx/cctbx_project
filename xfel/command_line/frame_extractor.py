30f919f99f274508c3a53f8e04b043e4279fc2c2
idyoung <idyoung@lbl.gov>
2017-07-03 03:43:15 -0700

    Support extracting frames from files of multiple experiments.
    New function iterates over the refl/json input files and also iterates
    over any experiments within a single file to dump individual frames as
    integration pickles. Dumping one frame per file remains a derived class.
    
    Also changes to the phil params.

--- xfel/command_line/frame_extractor.py
+++ xfel/command_line/frame_extractor.py
@@ -2,7 +2,7 @@ from __future__ import division
 # -*- mode: python; coding: utf-8; indent-tabs-mode: nil; python-indent: 2 -*-
 #
 # LIBTBX_SET_DISPATCHER_NAME cxi.frame_extractor
-# LIBTBX_SET_DISPATCHER_NAME xfel.frame_extractor
+# LIBTBX_SET_DISPATCHER_NAME cctbx.xfel.frame_extractor
 #
 # $Id: frame_extractor.py idyoung $
 
@@ -13,6 +13,25 @@ from cctbx.crystal_orientation import crystal_orientation
 import iotbx.phil
 import cctbx, os, glob
 from libtbx import easy_pickle
+
+phil_scope = iotbx.phil.parse("""
+  input {
+    experiments = None
+      .type = path
+      .help = path to an experiments.json file
+    reflections = None
+      .type = path
+      .help = path to a reflection table (integrated.pickle) file
+  }
+  output {
+    filename = None
+      .type = str
+      .help = if set, name of final pickle file
+    dirname = None
+      .type = path
+      .help = if set, path to directory to save the new pickle file
+    }
+    """)
 
 class ConstructFrame(object):
   def get_template_pickle(self):
@@ -186,39 +205,49 @@ class ConstructFrame(object):
     return self.frame
 
 class ConstructFrameFromFiles(ConstructFrame):
-  def __init__(self, pickle_name, json_name):
+  def __init__(self, refl_name, json_name, outname=None):
     # load the integration.pickle file (reflection table) into memory and
     # load the experiments.json file (json) into memory, piecewise.
     # check_format=False because we don't wont to load any imagesets in the
     # experiement list
-    importer = Importer([pickle_name, json_name], read_experiments=True, read_reflections=True, check_format=False)
+    importer = Importer([refl_name, json_name], read_experiments=True, read_reflections=True, check_format=False)
     if importer.unhandled:
       print "unable to process:", importer.unhandled
+    reflections_l = flatten_reflections(importer.reflections)
+    experiments_l = flatten_experiments(importer.experiments)
+    assert len(experiments_l) == 1, "cannot construct a single frame from multiple experiments"
+    self.frame = ConstructFrame.__init__(self, reflections_l[0], experiments_l[0]).make_frame()
 
-    ConstructFrame.__init__(self, flatten_reflections(importer.reflections)[0],
-                                  flatten_experiments(importer.experiments)[0])
+def construct_frames_from_files(refl_name, json_name, outname=None, outdir=None):
+  importer = Importer([refl_name, json_name], read_experiments=True, read_reflections=True, check_format=False)
+  if importer.unhandled:
+    print "unable to process:", importer.unhandled
+  reflections_l = flatten_reflections(importer.reflections)[0]
+  experiments_l = flatten_experiments(importer.experiments)
+  frames = []
+  if outdir is None:
+    outdir = '.'
+  if outname is None:
+    outname = 'int-%d' + refl_name.split('.pickle')[0] + '_extracted.pickle'
+  elif '%' not in outname:
+    outname = outname.split(".pickle")[0] + ("_%d.pickle")
+  for i in xrange(len(experiments_l)):
+    refl = reflections_l.select(reflections_l['id'] == i)
+    if len(refl) == 0: continue
+    expt = experiments_l[i]
+    frame = ConstructFrame(refl, expt).make_frame()
+    name = outname % i
+    easy_pickle.dump(os.path.join(outdir, name), frame)
 
 if __name__ == "__main__":
-  master_phil_scope = iotbx.phil.parse("""
-    pickle_name = None
-      .type = path
-      .help = path to a reflection table (integrated.pickle) file
-    json_name = None
-      .type = path
-      .help = path to an experiments.json file
-    output_dir = None
-      .type = path
-      .help = if set, path to directory to save the new pickle file
-      """)
-  parser = OptionParser(phil=master_phil_scope)
+  parser = OptionParser(phil=phil_scope)
   params, options = parser.parse_args(show_diff_phil=True)
-  for pickle_name, json_name in zip(glob.glob(params.pickle_name), glob.glob(params.json_name)):
-    frame = ConstructFrameFromFiles(pickle_name, json_name).make_frame()
-    if not params.output_dir is None:
-      assert os.path.isdir(params.output_dir)
-      basename = os.path.basename(pickle_name)
+  if params.output.dirname is not None:
+    assert os.path.isdir(params.output.dirname)
+  for refl_name, json_name in zip(glob.glob(params.input.reflections), glob.glob(params.input.experiments)):
+    if params.output.filename is None:
+      basename = os.path.basename(refl_name)
       name = os.path.splitext(basename)[0] + "_extracted.pickle"
-      dest_path = os.path.join(params.output_dir, name)
-      assert not os.path.isfile(dest_path)
-      print dest_path
-      easy_pickle.dump(dest_path, frame)
+    else:
+      name = params.output.filename
+    construct_frames_from_files(refl_name, json_name, outname=name, outdir=params.output.dirname)
