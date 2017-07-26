0dfd74d966184aa6f433c2a68a47c9893ed37aa2
Aaron Brewster <asbrewster@lbl.gov>
2017-07-25 17:47:08 -0700

    Pick up program defaults from dials.stills_process.

--- xfel/command_line/xtc_process.py
+++ xfel/command_line/xtc_process.py
@@ -300,7 +300,7 @@ xtc_phil_str = '''
   }
 '''
 
-from dials.command_line.stills_process import dials_phil_str
+from dials.command_line.stills_process import dials_phil_str, program_defaults_phil_str
 
 extra_dials_phil_str = '''
   verbosity = 1
@@ -345,7 +345,7 @@ extra_dials_phil_str = '''
 
 from xfel.ui import db_phil_str
 
-phil_scope = parse(xtc_phil_str + dials_phil_str + extra_dials_phil_str + db_phil_str, process_includes=True)
+phil_scope = parse(xtc_phil_str + dials_phil_str + extra_dials_phil_str + db_phil_str, process_includes=True).fetch(parse(program_defaults_phil_str))
 
 from xfel.command_line.xfel_process import Script as DialsProcessScript
 class InMemScript(DialsProcessScript):
