1da7b53182066fa89f1e54c1d22db14da508f741
idyoung <idyoung@lbl.gov>
2017-08-07 15:30:59 -0700

    bugfix

--- xfel/command_line/xtc_dump.py
+++ xfel/command_line/xtc_dump.py
@@ -59,7 +59,7 @@ phil_scope = parse('''
     }
     cbf {
       detz_offset = None
-        .type = int
+        .type = float
         .help = Distance from back of detector rail to sample interaction region (CXI) \
                 or actual detector distance (XPP/MFX)
       override_energy = None
