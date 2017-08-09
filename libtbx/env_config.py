4c8ca0cd1aaa8c072e4eab7176fcf0fa58c39672
Billy K. Poon <bkpoon@lbl.gov>
2017-08-09 15:44:27 -0700

    Installer: wrap SSL_CERT_FILE in double quotes so that relative path is preserved in macOS point-and-click installer

--- libtbx/env_config.py
+++ libtbx/env_config.py
@@ -1022,7 +1022,7 @@ Wait for the command to finish, then try again.""" % vars())
     essentials.append(("PATH", [self.bin_path]))
 
     if (cert_file is not None):
-      print >> f, 'SSL_CERT_FILE=%s' % cert_file.sh_value()
+      print >> f, 'SSL_CERT_FILE="%s"' % cert_file.sh_value()
       print >> f, 'export SSL_CERT_FILE'
 
     pangorc = abs(self.build_path / '..' / 'base' / 'etc' / 'pango' / 'pangorc')
