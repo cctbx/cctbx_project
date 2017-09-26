087d7c3af24d27670920f7225f04e6ecd15477e9
Oleg Sobolev <osobolev@lbl.gov>
2017-09-26 13:10:23 -0700

    refactoring of pdb_atom_selection to use mmtbx.model

--- mmtbx/command_line/pdb_atom_selection.py
+++ mmtbx/command_line/pdb_atom_selection.py
@@ -1,14 +1,13 @@
 from __future__ import division
 # LIBTBX_SET_DISPATCHER_NAME phenix.pdb_atom_selection
 
-from mmtbx.monomer_library import pdb_interpretation
-from mmtbx.monomer_library import server
 import argparse
 from cctbx.array_family import flex
 from libtbx.utils import plural_s
 from libtbx.str_utils import show_string
 import libtbx.load_env
-from iotbx.pdb import write_whole_pdb_file
+import mmtbx.model
+import iotbx.pdb
 import sys
 
 def run(args, command_name=libtbx.env.dispatcher_name):
@@ -37,22 +36,15 @@ def run(args, command_name=libtbx.env.dispatcher_name):
         " atoms plus a surrounding buffer layer",
       default=None)
   co = parser.parse_args(args)
-  mon_lib_srv = server.server()
-  ener_lib = server.ener_lib()
-  processed_pdb_file = pdb_interpretation.process(
-    mon_lib_srv=mon_lib_srv,
-    ener_lib=ener_lib,
-    file_name=co.file_name[0],
-    log=sys.stdout)
-  print
-  acp = processed_pdb_file.all_chain_proxies
-
-  selection_cache = acp.pdb_hierarchy.atom_selection_cache()
-  atoms = acp.pdb_atoms
+  pdb_inp = iotbx.pdb.input(file_name=co.file_name[0])
+  model = mmtbx.model.manager(
+      model_input=pdb_inp,
+      process_input=True)
+  atoms = model.get_atoms()
   all_bsel = flex.bool(atoms.size(), False)
   for selection_string in co.inselections:
     print selection_string
-    isel = acp.iselection(string=selection_string, cache=selection_cache)
+    isel = model.iselection(selstr=selection_string)
     all_bsel.set_selected(isel, True)
     if (not co.write_pdb_file):
       print "  %d atom%s selected" % plural_s(isel.size())
@@ -61,19 +53,17 @@ def run(args, command_name=libtbx.env.dispatcher_name):
   print
   if (co.write_pdb_file):
     print "Writing file:", show_string(co.write_pdb_file)
-    sel_hierarchy = acp.pdb_hierarchy.select(all_bsel)
-    if (co.cryst1_replacement_buffer_layer is None):
-      crystal_symmetry = acp.special_position_settings
-    else:
+    selected_model = model.select(all_bsel)
+    if (co.cryst1_replacement_buffer_layer is not None):
       import cctbx.crystal
       crystal_symmetry = cctbx.crystal.non_crystallographic_symmetry(
-        sites_cart=sel_hierarchy.atoms().extract_xyz(),
+        sites_cart=selected_model.get_atoms().extract_xyz(),
         buffer_layer=co.cryst1_replacement_buffer_layer)
-    write_whole_pdb_file(
-        file_name=co.write_pdb_file,
-        processed_pdb_file=processed_pdb_file,
-        pdb_hierarchy=sel_hierarchy,
-        crystal_symmetry=crystal_symmetry)
+      selected_model.set_crystal_symmetry(crystal_symmetry)
+    pdb_str = selected_model.model_as_pdb()
+    f = open(co.write_pdb_file, 'w')
+    f.write(pdb_str)
+    f.close()
     print
 
 if (__name__ == "__main__"):
