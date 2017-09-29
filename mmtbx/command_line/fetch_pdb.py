aa31ece0ac0f7ec72f61c74a079ab423d9587a73
rjgildea <richard.gildea@diamond.ac.uk>
2017-09-29 09:56:01 +0100

    add iotbx.fetch_pdb synonym

--- mmtbx/command_line/fetch_pdb.py
+++ mmtbx/command_line/fetch_pdb.py
@@ -6,6 +6,7 @@ reflection data.
 from __future__ import division
 
 # LIBTBX_SET_DISPATCHER_NAME phenix.fetch_pdb
+# LIBTBX_SET_DISPATCHER_NAME iotbx.fetch_pdb
 
 # XXX most of this code is unused when run from the command line, but the
 # PHENIX GUI includes a simple frontend that uses the phil interface.
