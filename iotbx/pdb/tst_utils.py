ff6052596217df3955be83bb0e1a1db43e2d170b
Vincent Chen <vbchen@gmail.com>
2017-10-04 15:25:29 -0400

    bug fix to all_chain_ids to fix missing chains with two of the same character

--- iotbx/pdb/tst_utils.py
+++ iotbx/pdb/tst_utils.py
@@ -3,7 +3,8 @@ import iotbx.pdb.utils
 
 def exercise_all_chain_ids():
   ids = iotbx.pdb.utils.all_chain_ids()
-  assert len(ids)==3844
+  assert len(ids)==3906
+  assert len(set(ids))==3906
 
 def run():
   exercise_all_chain_ids()
--- iotbx/pdb/utils.py
+++ iotbx/pdb/utils.py
@@ -3,13 +3,22 @@ import string
 from itertools import product
 
 def all_chain_ids():
-  chars = string.ascii_letters+string.digits
+  """
+  Returns all possible 2 character chain IDs for PDB format files.
+  In general, returns single character chains first.
+  Also tries to use all combinations of uppercase/numbers before going to lower case.
+  """
+  chars = string.ascii_uppercase+string.digits
+  lowerchars = string.ascii_lowercase
   def permutations(iterable, r=None): # XXX This may go to libtbx or scitbx
     pool = tuple(iterable)
     n = len(pool)
     r = n if r is None else r
     for indices in product(range(n), repeat=r):
-      if(len(set(indices)) == r):
+      if(len(indices) == r):
         yield tuple(pool[i] for i in indices)
-  result = permutations(iterable = chars, r = 2)
-  return [c for c in chars]+["".join(p) for p in result]
+  both_char_upper = permutations(iterable = chars, r = 2)
+  second_char_lower = product(chars, lowerchars)
+  first_char_lower = product(lowerchars, chars)
+  both_char_lower = permutations(iterable = lowerchars, r = 2)
+  return [" "+c for c in chars]+[" "+c for c in lowerchars]+["".join(p) for p in both_char_upper]+["".join(p) for p in second_char_lower]+["".join(p) for p in first_char_lower]+["".join(p) for p in both_char_lower]
