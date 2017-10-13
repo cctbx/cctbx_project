0e11b2e7638dfab29888718ccd4ec06eb066e198
Oleg Sobolev <osobolev@lbl.gov>
2017-10-12 17:20:57 -0700

    bugfix

--- mmtbx/refinement/utils.py
+++ mmtbx/refinement/utils.py
@@ -15,8 +15,8 @@ def two_group_selections_per_residue(
   #   pdb_hierarchy = all_chain_proxies.pdb_hierarchy
   #   sc = all_chain_proxies.sel_sidechain().iselection()
   #   bb = all_chain_proxies.sel_backbone().iselection()
-  sc = model.sel_sidechain()
-  bb = model.sel_backbone()
+  sc = model.sel_sidechain().iselection()
+  bb = model.sel_backbone().iselection()
   ogpr = [rg.atoms().extract_i_seq() for rg in model.get_hierarchy().residue_groups()]
   result = []
   for g in ogpr:
