91b8dad9d98923aa4d83c3f41713400d90e84f1a
Robert Oeffner <rdo20@cam.ac.uk>
2017-10-16 16:08:08 +0100

    fix for boost::optional semantics

--- scitbx/array_family/tst_vec3.cpp
+++ scitbx/array_family/tst_vec3.cpp
@@ -124,8 +124,8 @@ int main(int /*argc*/, char* /*argv*/[])
     check_true(__LINE__, std::fabs(
       (*(vec3<double>(1.,.5,-1.8).angle_rad(vec3<double>(-.3,.75,.5))))
       - 1.99306755584) < 1.e-6);
-    check_false(__LINE__,
-      vec3<double>(0,0,0).angle_rad(vec3<double>(0,0,0)));
+    check_true(__LINE__,
+      std::fabs(*(vec3<double>(0.,0.,0.).angle_rad(vec3<double>(0.,0.,0.))) - 0.0) < 1e-6);
     check_true(__LINE__, std::fabs(
       (  vec3<double>(1.,.5,-1.8).reflect(vec3<double>(1.,0.,1.))
        - vec3<double>(2.6,.5,-.2)).length()) < 1.e-6);
