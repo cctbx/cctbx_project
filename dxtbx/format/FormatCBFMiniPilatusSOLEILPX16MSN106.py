beaadad23205d08230397faf0edaa5066da17451
rjgildea <richard.gildea@diamond.ac.uk>
2017-10-19 13:25:16 +0100

    dxtbx: support for Smargon goniometer on Soleil PROXIMA-1

--- dxtbx/format/FormatCBFMiniPilatusSOLEILPX16MSN106.py
+++ dxtbx/format/FormatCBFMiniPilatusSOLEILPX16MSN106.py
@@ -79,24 +79,44 @@ class FormatCBFMiniPilatusSOLEILPX16MSN106(FormatCBFMiniPilatus):
   def _goniometer(self):
     '''Construct a goniometer from the records in the mini CBF header.'''
 
-    #import math
+    if ('Alpha' in self._cif_header_dictionary and
+        'Kappa' in self._cif_header_dictionary):
+      # Kappa
+      alpha = float(self._cif_header_dictionary['Alpha'].split()[0])
+      omega = float(self._cif_header_dictionary['Chi'].split()[0])
+      kappa = float(self._cif_header_dictionary['Kappa'].split()[0])
+      phi = float(self._cif_header_dictionary['Phi'].split()[0])
 
-    alpha = float(self._cif_header_dictionary['Alpha'].split()[0])
-    omega = float(self._cif_header_dictionary['Chi'].split()[0])
-    kappa = float(self._cif_header_dictionary['Kappa'].split()[0])
-    phi = float(self._cif_header_dictionary['Phi'].split()[0])
+      axis = self._cif_header_dictionary['Oscillation_axis']
 
-    axis = self._cif_header_dictionary['Oscillation_axis']
+      scanaxis  = {'OMEGA':'Omega', 'PHI':'Phi'}
 
-    scanaxis  = {'OMEGA':'Omega', 'PHI':'Phi'}
+      assert axis in scanaxis
 
-    assert axis in scanaxis
+      # this is the direction the arm points in at datum
+      direction = '+z'
 
-    # this is the direction the arm points in at datum
-    direction = '+z'
+      return self._goniometer_factory.make_kappa_goniometer(
+        alpha, omega, kappa, phi, direction, scanaxis[axis])
 
-    return self._goniometer_factory.make_kappa_goniometer(
-      alpha, omega, kappa, phi, direction, scanaxis[axis])
+    else:
+      # Smargon
+      from scitbx.array_family import flex
+
+      phi = float(self._cif_header_dictionary['Phi'].split()[0])
+      chi = float(self._cif_header_dictionary['Chi'].split()[0])
+      omega = float(self._cif_header_dictionary['Omega'].split()[0])
+
+      names = flex.std_string(("PHI", "CHI", "OMEGA"))
+      axes = flex.vec3_double(((1,0,0), (0,0,-1), (1,0,0)))
+      angles = flex.double((phi, chi, omega))
+
+      axis = self._cif_header_dictionary['Oscillation_axis'].upper()
+      assert axis in names, axis
+      scan_axis = flex.first_index(names, axis)
+
+      return self._goniometer_factory.make_multi_axis_goniometer(
+        axes, angles, names, scan_axis)
 
 if __name__ == '__main__':
 
