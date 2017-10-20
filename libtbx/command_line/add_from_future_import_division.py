15fff623495be5912c527617f22195c3ce211fa4
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-10-20 09:09:48 +0100

    Another Python3 migration help
    Add --absolute_import and --print_function parameters (and '-3' as shortcut for both)
    to libtbx.add_from_future_import_division.

--- libtbx/command_line/add_from_future_import_division.py
+++ libtbx/command_line/add_from_future_import_division.py
@@ -1,44 +1,69 @@
 from __future__ import division
-import libtbx.load_env
-import os, sys
 import ast
+import os
+import sys
+import libtbx.load_env
 
-def fix(module_path):
+def fix(module_path, imports=None):
+  if not imports:
+    imports = ['division']
+  want = set(imports)
+
   if os.path.getsize(module_path) == 0: # Do not add __future__ imports to empty files
     return
-  module_lines = file(module_path).readlines()
+
+  with file(module_path) as fh:
+    module_lines = fh.readlines()
   module_node = ast.parse(''.join(module_lines), filename=module_path)
   # the attribute lineno is the index of the line *following* that node
   if ast.get_docstring(module_node):
     insertion_lineno = module_node.body[0].lineno
-    index_to_check = 1
   else:
     insertion_lineno = 0
-    index_to_check = 0
-  if index_to_check < len(module_node.body):
-    node_to_check = module_node.body[index_to_check]
-    if isinstance(node_to_check, ast.ImportFrom) and node_to_check.module == '__future__':
-      for name in node_to_check.names:
-        if name.name == 'division':
-          return
-  module_lines.insert(insertion_lineno, "from __future__ import division\n")
-  fixed_source = ''.join(module_lines)
-  file(module_path, mode='w').writelines(module_lines)
+
+  for node in ast.iter_child_nodes(module_node):
+    if isinstance(node, ast.ImportFrom) and node.module == '__future__':
+      insertion_lineno = node.lineno
+      module = node.module.split('.')
+      for n in node.names:
+        if n.name in want:
+          want.remove(n.name)
+
+  if not want:
+    return
+
+  missing_future = ", ".join(sorted(want))
+  print("Adding {stmt} to {file}".format(stmt=missing_future, file=module_path))
+  module_lines.insert(insertion_lineno, "from __future__ import %s\n" % missing_future)
+  with file(module_path, mode='w') as fh:
+    fh.writelines(module_lines)
 
 def run(locations):
+  check_for_imports = ['division']
+  if '--absolute_import' in locations:
+    check_for_imports.append('absolute_import')
+    locations.remove('--absolute_import')
+  if '--print_function' in locations:
+    check_for_imports.append('print_function')
+    locations.remove('--print_function')
+  if '-3' in locations:
+    check_for_imports.append('absolute_import')
+    check_for_imports.append('print_function')
+    locations.remove('-3')
+
   if not locations:
     locations = [ os.path.dirname(libtbx.env.dist_path('libtbx')) ]
-  modules = []
   for l in locations:
     if os.path.isfile(l) and l.endswith('.py'):
-      fix(l)
+      print(l)
+      fix(l, imports=check_for_imports)
     else:
       for dirpath, dirnames, filenames in os.walk(l):
         if dirpath.endswith('jinja2'): continue
+        print(dirpath)
         for f in filenames:
-          if f.endswith('.py'): fix(os.path.join(dirpath, f))
-
-
+          if f.endswith('.py'):
+            fix(os.path.join(dirpath, f), imports=check_for_imports)
 
 if __name__ == '__main__':
   run(sys.argv[1:])
