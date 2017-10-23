aea8f6fe97ed76287a5a1cf1f49b844b17ecde94
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-10-23 11:46:09 +0100

    automatically discover module name. add code to run libtbx tests from pytest

--- libtbx/test_utils/pytest.py
+++ libtbx/test_utils/pytest.py
@@ -1,13 +1,16 @@
 from __future__ import division, absolute_import, print_function
 import atexit
-import libtbx.load_env
 import os
+import sys
 import itertools
+
+import libtbx.load_env
+from libtbx.test_utils.parallel import make_commands, run_command
 
 _first_pytest_collection = True
 _pytest_unique_counter = itertools.count(1)
 
-def discover(module, pytestargs=None):
+def discover(module=None, pytestargs=None):
   '''
   pytest compatibility layer
 
@@ -17,11 +20,11 @@ def discover(module, pytestargs=None):
 
   To use this function, add
     from libtbx.test_utils.pytest import discover
-    tst_list = tst_list + discover( $module )
-  to your run_tests.py, substituting '$module' with the name of your module
-  directory.
+    tst_list = tst_list + discover()
+  to your run_tests.py.
   You can pass further arguments to the pytest discovery process as a list in
-  the pytestargs parameter.
+  the pytestargs parameter, and you can specify the cctbx module name if the
+  automatic discovery does not work for any reason.
 
   The function tests for the presence of pytest and mock, and generates a
   helpful warning if either is missing. You can skip the discovery of tests
@@ -55,6 +58,19 @@ def discover(module, pytestargs=None):
     pytest_warning()
     atexit.register(pytest_warning)
     return []
+
+  if not module:
+    # Try to determine the name of the calling module, and thus the name of the
+    # cctbx module. Use exception trick to pick up the current frame.
+    try:
+      raise Exception()
+    except Exception:
+      frame = sys.exc_info()[2].tb_frame.f_back
+    caller = frame.f_globals['__name__']
+    if not caller.endswith('.run_tests'):
+      raise RuntimeError('Only use discover() from within run_tests.py ' \
+                       + 'or specify the module name manually.')
+    module = caller[:-10]
 
   class L(list):
     """Subclass list so that it can accept additional attributes."""
@@ -99,3 +115,93 @@ def discover(module, pytestargs=None):
       pass
 
   return test_list
+
+def libtbx_collector():
+  '''
+  libtbx compatibility layer:
+
+  return a function that enables pytest to collect and run all libtbx legacy tests of a module
+
+  To use this you need to add
+    import libtbx.test_utils.pytest
+    pytest_collect_file = libtbx.test_utils.pytest.libtbx_collector()
+  to your conftest.py in the module root directory.
+  '''
+  # Determine the name of the calling module, and thus the internal module name
+  # of the run_tests file. Use exception trick to pick up the current frame.
+  try:
+    raise Exception()
+  except Exception:
+    frame = sys.exc_info()[2].tb_frame.f_back
+  caller = frame.f_globals['__name__']
+  if not caller.endswith('.conftest'):
+    raise RuntimeError('Only use libtbx_collector() from within conftest.py')
+  caller_run_tests_module = caller[:-9] + '.run_tests'
+
+  import pytest
+
+  class LibtbxTestException(Exception):
+    '''Custom exception for error reporting.'''
+    def __init__(self, stdout, stderr):
+      self.stdout = stdout
+      self.stderr = stderr
+
+  class LibtbxTest(pytest.Item):
+    def __init__(self, name, parent, test_command, test_parameters):
+      super(LibtbxTest, self).__init__(name, parent)
+      self.test_cmd = make_commands([test_command])[0]
+      self.test_parms = test_parameters
+      self.full_cmd = ' '.join([self.test_cmd] + self.test_parms)
+
+    def runtest(self):
+      rc = run_command(self.full_cmd)
+      if rc is None:
+        # run_command only returns None if CTRL+C pressed
+        raise KeyboardInterrupt()
+      self.add_report_section('call', 'stdout', '\n'.join(rc.stdout_lines))
+      self.add_report_section('call', 'stderr', '\n'.join(rc.stderr_lines))
+      if rc.stderr_lines or rc.return_code != 0:
+        raise LibtbxTestException(rc.stdout_lines, rc.stderr_lines)
+
+    def repr_failure(self, excinfo):
+      '''called when self.runtest() raises an exception.'''
+      if isinstance(excinfo.value, LibtbxTestException):
+        return "\n".join(excinfo.value.stderr)
+
+    def reportinfo(self):
+      return self.fspath, 0, self.full_cmd
+
+  def pytest_collect_file(path, parent):
+    if 'LIBTBX_SKIP_PYTEST' in os.environ:
+      '''The pytest discovery process is ran from within libtbx, so do not
+         attempt to find libtbx legacy tests.'''
+      return
+
+    class LibtbxRunTestsFile(pytest.File):
+      def collect(self):
+        try:
+          os.environ['LIBTBX_SKIP_PYTEST'] = '1'
+          import importlib
+          run_tests = importlib.import_module(caller_run_tests_module)
+        finally:
+          del os.environ['LIBTBX_SKIP_PYTEST']
+
+        for test in run_tests.tst_list:
+          if isinstance(test, basestring):
+            testfile = test
+            testparams = []
+            testname = 'main'
+          else:
+            testfile = test[0]
+            testparams = [str(s) for s in test[1:]]
+            testname = "_".join(str(p) for p in testparams)
+
+          full_command = testfile.replace("$D", str(self.session.fspath))
+          shortpath = testfile.replace("$D/", "")
+          pytest_file_object = pytest.File(shortpath, self.session)
+          yield LibtbxTest(testname, pytest_file_object, full_command, testparams)
+
+    if path.basename == 'run_tests.py':
+      return LibtbxRunTestsFile(path, parent)
+
+  return pytest_collect_file
