0907136e556e6efbc4c082d225bc0e7ccc857458
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-11-30 11:08:21 +0000

    Placate buildbot and libtbx clutter false alarm. Original commit incidentally related to #91

--- libtbx/auto_build/bootstrap.py
+++ libtbx/auto_build/bootstrap.py
@@ -487,10 +487,10 @@ class Toolbox(object):
 
 if git rev-parse --verify HEAD >/dev/null 2>&1
 then
-	against=HEAD
+  against=HEAD
 else
-	# Initial commit: diff against an empty tree object
-	against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+  # Initial commit: diff against an empty tree object
+  against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
 fi
 
 # If there are whitespace errors, print the offending file names and abort the commit.
