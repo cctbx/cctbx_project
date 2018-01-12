c30c607ca41090377e28801a37994983f548f1a4
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-01-12 11:51:54 +0000

    drop compatibility code for collections for ancient Python versions

--- libtbx/containers.py
+++ libtbx/containers.py
@@ -1,87 +1,8 @@
-from __future__ import division
-import sys
+from __future__ import absolute_import, division
 
-if (sys.version_info[:2] == (2,3)):
-  MutableSet = None
-  OrderedDict = None
-else:
-  import collections
-  MutableSet = getattr(collections, "MutableSet", None)
-  OrderedDict = getattr(collections, "OrderedDict", None)
-
-if (MutableSet is None):
-  class MutableSet(set):
-
-    def add(self, value):
-        """Add an element."""
-        raise NotImplementedError
-
-    def discard(self, value):
-        """Remove an element.  Do not raise an exception if absent."""
-        raise NotImplementedError
-
-    def remove(self, value):
-        """Remove an element. If not a member, raise a KeyError."""
-        if value not in self:
-            raise KeyError(value)
-        self.discard(value)
-
-    def pop(self):
-        """Return the popped value.  Raise KeyError if empty."""
-        it = iter(self)
-        try:
-            value = next(it)
-        except StopIteration:
-            raise KeyError
-        self.discard(value)
-        return value
-
-    def clear(self):
-        """This is slow (creates N new iterators!) but effective."""
-        try:
-            while True:
-                self.pop()
-        except KeyError:
-            pass
-
-    def __ior__(self, it):
-        for value in it:
-            self.add(value)
-        return self
-
-    def __iand__(self, it):
-        for value in (self - it):
-            self.discard(value)
-        return self
-
-    def __ixor__(self, it):
-        if not isinstance(it, Set):
-            it = self._from_iterable(it)
-        for value in it:
-            if value in self:
-                self.discard(value)
-            else:
-                self.add(value)
-        return self
-
-    def __isub__(self, it):
-        for value in it:
-            self.discard(value)
-        return self
-
-    @classmethod
-    def _from_iterable(cls, it):
-      '''Construct an instance of the class from any iterable input.
-
-      Must override this method if the class constructor signature
-      does not accept an iterable for an input.
-      '''
-      return cls(it)
-
-    def __sub__(self, other):
-      return self._from_iterable(value for value in self
-                                 if value not in other)
-
+import collections
+MutableSet = collections.MutableSet
+OrderedDict = collections.OrderedDict
 
 class OrderedSet(MutableSet):
   """
@@ -174,131 +95,6 @@ class OrderedSet(MutableSet):
       result.add(deepcopy(elt, memo))
     return result
 
-if OrderedDict is None:
-  from UserDict import DictMixin
-  class OrderedDict(dict, DictMixin):
-    """
-    An equivalent recipe for the OrderedDict class introduced in Python 2.7
-    from ActiveState:
-    http://code.activestate.com/recipes/576693/
-    With minor modifications for Python 2.3 compatibility.
-    """
-
-    def __init__(self, *args, **kwds):
-      if len(args) > 1:
-        raise TypeError('expected at most 1 arguments, got %d' % len(args))
-      try:
-        self.__end
-      except AttributeError:
-        self.clear()
-      self.update(*args, **kwds)
-
-    def clear(self):
-      self.__end = end = []
-      end += [None, end, end]         # sentinel node for doubly linked list
-      self.__map = {}                 # key --> [key, prev, next]
-      dict.clear(self)
-
-    def __setitem__(self, key, value):
-      if key not in self:
-        end = self.__end
-        curr = end[1]
-        curr[2] = end[1] = self.__map[key] = [key, curr, end]
-      dict.__setitem__(self, key, value)
-
-    def __delitem__(self, key):
-      dict.__delitem__(self, key)
-      key, prev, next = self.__map.pop(key)
-      prev[2] = next
-      next[1] = prev
-
-    def __iter__(self):
-      end = self.__end
-      curr = end[2]
-      while curr is not end:
-        yield curr[0]
-        curr = curr[2]
-
-    def __reversed__(self):
-      end = self.__end
-      curr = end[1]
-      while curr is not end:
-        yield curr[0]
-        curr = curr[1]
-
-    def popitem(self, last=True):
-      if not self:
-        raise KeyError('dictionary is empty')
-      if last:
-        key = reversed(self).next()
-      else:
-        key = iter(self).next()
-      value = self.pop(key)
-      return key, value
-
-    def __reduce__(self):
-      items = [[k, self[k]] for k in self]
-      tmp = self.__map, self.__end
-      del self.__map, self.__end
-      inst_dict = vars(self).copy()
-      self.__map, self.__end = tmp
-      if inst_dict:
-        return (self.__class__, (items,), inst_dict)
-      return self.__class__, (items,)
-
-    def keys(self):
-      return list(self)
-
-    setdefault = DictMixin.setdefault
-    #update = DictMixin.update
-    pop = DictMixin.pop
-    values = DictMixin.values
-    items = DictMixin.items
-    iterkeys = DictMixin.iterkeys
-    itervalues = DictMixin.itervalues
-    iteritems = DictMixin.iteritems
-
-    # this method copied from Python26 DictMixin source
-    # needed for compatibility with Python 2.3
-    def update(self, other=None, **kwargs):
-      # Make progressively weaker assumptions about "other"
-      if other is None:
-        pass
-      elif hasattr(other, 'iteritems'):  # iteritems saves memory and lookups
-        for k, v in other.iteritems():
-          self[k] = v
-      elif hasattr(other, 'keys'):
-        for k in other.keys():
-          self[k] = other[k]
-      else:
-        for k, v in other:
-          self[k] = v
-      if kwargs:
-        self.update(kwargs)
-
-    def __repr__(self):
-      if not self:
-        return '%s()' % (self.__class__.__name__,)
-      return '%s(%r)' % (self.__class__.__name__, self.items())
-
-    def copy(self):
-        return self.__class__(self)
-
-    def fromkeys(cls, iterable, value=None):
-      d = cls()
-      for key in iterable:
-        d[key] = value
-      return d
-    fromkeys = classmethod(fromkeys)
-
-    def __eq__(self, other):
-      if isinstance(other, OrderedDict):
-        return len(self)==len(other) and self.items() == other.items()
-      return dict.__eq__(self, other)
-
-    def __ne__(self, other):
-      return not self == other
-
 class deque_template(object):
 
   __slots__ = ('_list_proxy', '_set_proxy')
@@ -337,35 +133,15 @@ class stack(deque_template):
 class hashed_stack(stack):
   set_proxy_type = set
 
+class queue(deque_template):
+  list_proxy_type = collections.deque
+  set_proxy_type  = None
 
-if (sys.version_info[:2] > (2,3)):
-  import collections
-
-  class queue(deque_template):
-    list_proxy_type = collections.deque
-    set_proxy_type  = None
-
-    def pull(self):
-      result = self._list_proxy.popleft()
-      if self._set_proxy is not self._list_proxy:
-        self._set_proxy.discard(result)
-      return result
-
-else:
-
-  class queue(deque_template):
-    list_proxy_type = list
-    set_proxy_type  = None
-
-    def pull(self):
-      result = self._list_proxy.pop(0)
-      if self._set_proxy is not self._list_proxy:
-        try:
-          self._set_proxy.remove(result)
-        except ValueError:
-          pass
-      return result
-
+  def pull(self):
+    result = self._list_proxy.popleft()
+    if self._set_proxy is not self._list_proxy:
+      self._set_proxy.discard(result)
+    return result
 
 class hashed_queue(queue):
   set_proxy_type = set
