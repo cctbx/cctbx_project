7c8553ec059a3e0a655cb6ee5c3d3fda383f12ee
Nicholas Sauter <nksauter@lbl.gov>
2018-01-13 11:30:23 -0800

    Please don't rain on my parade - removing verbose warning with prejudice.

--- dxtbx/model/crystal.py
+++ dxtbx/model/crystal.py
@@ -30,8 +30,9 @@ class CrystalFactory(object):
       if d['ML_half_mosaicity_deg'] is None or d['ML_domain_size_ang'] is None:
         assert d['ML_half_mosaicity_deg'] is None and d['ML_domain_size_ang'] is None
       else:
-        if 'mosaicity' in d and d['mosaicity'] > 0:
-          print "Warning, two kinds of mosaicity found. Using Sauter2014 model"
+        # comment verbose warning out, but keep here for documentation purposes:
+        # if 'mosaicity' in d and d['mosaicity'] > 0:
+          # print "Warning, two kinds of mosaicity found. Using Sauter2014 model"
         from dxtbx.model import MosaicCrystalSauter2014
         return MosaicCrystalSauter2014.from_dict(d)
     if 'mosaicity' in d:
