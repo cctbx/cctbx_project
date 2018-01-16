913210c701f45b363ed602df3808b259886ae8c6
Oleg Sobolev <osobolev@lbl.gov>
2018-01-16 13:28:18 -0800

    Unmark ok function, mark couple others with explanation.

--- iotbx/pdb/hierarchy.py
+++ iotbx/pdb/hierarchy.py
@@ -666,7 +666,6 @@ class _(boost.python.injector, ext.root, __hash_eq_mixin):
           raise RuntimeError("Mismatch: \n %s \n %s \n"%(sc.label,a.id_str()))
       set_attr(sc=sc, a=a)
 
-  #MARKED_FOR_DELETION_OLEG
   def apply_rotation_translation(self, rot_matrices, trans_vectors):
     """
     LIMITATION: ANISOU records in resulting hierarchy will be invalid!!!
@@ -687,7 +686,6 @@ class _(boost.python.injector, ext.root, __hash_eq_mixin):
     result = iotbx.pdb.hierarchy.join_roots(roots=roots)
     result.reset_i_seq_if_necessary()
     return result
-  # END_MARKED_FOR_DELETION_OLEG
 
   def remove_residue_groups_with_atoms_on_special_positions_selective(self,
         crystal_symmetry):
@@ -2623,6 +2621,9 @@ class show_summary(input):
         level_id=level_id,
         level_id_exception=level_id_exception)
 
+# MARKED_FOR_DELETION_OLEG
+# Reason: functionality is moved to mmtbx.model and uses better all_chain_ids
+# function from iotbx.pdb.utils
 def suffixes_for_chain_ids(suffixes=Auto):
   if (suffixes is Auto):
     suffixes="123456789" \
@@ -2639,7 +2640,6 @@ def append_chain_id_suffixes(roots, suffixes=Auto):
         assert len(chain.id) == 1, len(chain.id)
         chain.id += suffix
 
-# MARKED_FOR_DELETION_OLEG
 def join_roots(roots, chain_id_suffixes=Auto):
   """
   Combine two root objects.
