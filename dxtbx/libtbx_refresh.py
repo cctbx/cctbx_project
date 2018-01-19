4d27cbaa98962256b9d5679cb7ca4d44ab455f37
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-01-19 20:14:01 +0000

    better than clean clutter

--- dxtbx/libtbx_refresh.py
+++ dxtbx/libtbx_refresh.py
@@ -1,4 +1,4 @@
-from __future__ import division
+from __future__ import absolute_import, division, print_function
 import libtbx.pkg_utils
 libtbx.pkg_utils.require('mock', '>=2.0')
 libtbx.pkg_utils.require('pytest', '>=3.1')
