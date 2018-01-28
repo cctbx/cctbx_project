0afab790bb8fd2a6a8e92161dc72036c93d9da17
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-01-28 19:01:35 +0000

    remove spurious libtbx dependencies

--- fable/command_line/show_calls.py
+++ fable/command_line/show_calls.py
@@ -1,24 +1,22 @@
-from __future__ import division
+from __future__ import absolute_import, division
+import optparse
+
 def run(args):
-  if (len(args) == 0): args = ["--help"]
-  from libtbx.option_parser import option_parser
+  if not args: args = ["--help"]
   import libtbx.load_env
-  command_line = (option_parser(
-    usage="%s [options] fortran_file ..." % libtbx.env.dispatcher_name)
-    .option(None, "--top_procedure",
-      action="append",
-      type="str")
-    .option(None, "--write_graphviz_dot",
-      action="store",
-      type="str")
-  ).process(args=args)
-  co = command_line.options
+  parser = optparse.OptionParser(usage="%s [options] fortran_file ..." % libtbx.env.dispatcher_name)
+  parser.add_option("-?", action="help", help=optparse.SUPPRESS_HELP)
+  parser.add_option("--top_procedure", action="append", type="str")
+  parser.add_option("--top-procedure", action="append", type="str", help=optparse.SUPPRESS_HELP)
+  parser.add_option("--write_graphviz_dot", action="store", type="str")
+  parser.add_option("--write-graphviz-dot", action="store", type="str", help=optparse.SUPPRESS_HELP)
+  option, files = parser.parse_args(args)
   from fable.read import process
-  all_fprocs = process(file_names=command_line.args)
+  all_fprocs = process(file_names=files)
   topological_fprocs = all_fprocs.build_bottom_up_fproc_list_following_calls(
-    top_procedures=co.top_procedure)
+    top_procedures=option.top_procedure)
   dep_cycles = topological_fprocs.dependency_cycles
-  if (len(dep_cycles) != 0):
+  if len(dep_cycles) != 0:
     print "Dependency cycles:", len(dep_cycles)
     for cycle in dep_cycles:
       print " ", " ".join(cycle)
@@ -38,20 +36,20 @@ def run(args):
         fproc.name.value, []))
     for identifier in sorted(fproc.fdecl_by_identifier.keys()):
       fdecl = fproc.fdecl_by_identifier[identifier]
-      if (fdecl.is_fproc_name()): continue
-      if (not fdecl.is_user_defined_callable()):
+      if fdecl.is_fproc_name(): continue
+      if not fdecl.is_user_defined_callable():
         continue
       called_name = fdecl.id_tok.value
       passed = fproc.externals_passed_by_arg_identifier.get(called_name)
-      if (passed is None):
+      if passed is None:
         digraph_lhs_rhs.append((lhs, called_name))
       else:
         called_name += "->" + ",".join(sorted(passed))
         for indirectly_called_name in passed:
           digraph_lhs_rhs.append((lhs, indirectly_called_name))
-      if (fdecl.is_function()):
+      if fdecl.is_function():
         sz = ""
-        if (fdecl.size_tokens is not None):
+        if fdecl.size_tokens:
           if (len(fdecl.size_tokens) == 1
                 and fdecl.size_tokens[0].is_integer()):
             sz = "*%s" % fdecl.size_tokens[0].value
@@ -60,18 +58,17 @@ def run(args):
         s = "%s (%s%s)" % (called_name, fdecl.data_type.value, sz)
       else:
         s = called_name
-      if (called_name in fwds):
+      if called_name in fwds:
         s += " (dependency cycle)"
       print "  %s" % s
   print
-  if (co.write_graphviz_dot is not None):
-    f = open(co.write_graphviz_dot, "w")
-    print >> f, "digraph G {"
-    for lhs_rhs in digraph_lhs_rhs:
-      print >> f, "  %s -> %s;" % lhs_rhs
-    print >> f, "}"
-    del f
+  if option.write_graphviz_dot:
+    with open(option.write_graphviz_dot, "w") as f:
+      print >> f, "digraph G {"
+      for lhs_rhs in digraph_lhs_rhs:
+        print >> f, "  %s -> %s;" % lhs_rhs
+      print >> f, "}"
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
   import sys
   run(args=sys.argv[1:])
