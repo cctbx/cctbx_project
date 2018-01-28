36915ec5d1eb8444fcc5c387af25d7854e04d490
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-01-28 18:39:56 +0000

    Actually fail the tests on errors, don't just swallow them and pretend everything is fine

--- fable/test/tst_command_line.py
+++ fable/test/tst_command_line.py
@@ -1,14 +1,12 @@
-from __future__ import division
+from __future__ import absolute_import, division
 def run(args):
   assert len(args) == 0
   from libtbx import easy_run
   import libtbx.load_env
   import os
-  op = os.path
   t_dir = libtbx.env.under_dist(
-    module_name="fable", path="test/valid", test=op.isdir)
+    module_name="fable", path="test/valid", test=os.path.isdir)
   assert t_dir.find('"') < 0
-  n_errors = 0
   for command,expected_output_fragment in [
         ('fable.split "%ssubroutine_1.f"', 'program_prog.f'),
         ('fable.read --each "%swrite_star.f"', 'Success: 1'),
@@ -43,34 +41,18 @@ def run(args):
     run_buffers = easy_run.fully_buffered(
       command=command,
       join_stdout_stderr=join_stdout_stderr)
-    class SpecificError(RuntimeError): pass
-    try:
-      if (not join_stdout_stderr):
-        run_buffers.raise_if_errors(Error=SpecificError)
-    except SpecificError, e:
-      n_errors += 1
-      print "ERROR:"
-      print str(e)
-      print
-    else:
-      stdout_buffer = "\n".join(run_buffers.stdout_lines)
-      if (expected_output_fragment is None):
-        if (len(stdout_buffer) != 0):
-          n_errors += 1
-          print stdout_buffer
-          print "ERROR: unexpected output above."
-          print
-      elif (stdout_buffer.find(expected_output_fragment) < 0):
-        n_errors += 1
+    if (not join_stdout_stderr):
+      run_buffers.raise_if_errors()
+    stdout_buffer = "\n".join(run_buffers.stdout_lines)
+    if (expected_output_fragment is None):
+      if (len(stdout_buffer) != 0):
         print stdout_buffer
-        print "ERROR: not found in output above:"
-        print [expected_output_fragment]
-        print
-  if (n_errors != 0):
-    print "Number of errors:", n_errors
-    print "Done."
-  else:
-    print "OK"
+        raise RuntimeError("ERROR: unexpected output above.")
+    elif (stdout_buffer.find(expected_output_fragment) < 0):
+      print stdout_buffer
+      print "ERROR: not found in output above:"
+      print [expected_output_fragment]
+      raise RuntimeError("ERROR: not found in output above:")
 
 if (__name__ == "__main__"):
   import sys
