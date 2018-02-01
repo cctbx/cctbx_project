2b755a5d213c6dd78bb8582a94b63dcfa5a4a16c
Aaron Brewster <asbrewster@lbl.gov>
2018-02-01 12:54:45 -0800

    Move plot_group to a callable function

--- dxtbx/command_line/plot_detector_models.py
+++ dxtbx/command_line/plot_detector_models.py
@@ -48,6 +48,52 @@ class Arrow3D(FancyArrowPatch):
     self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
     FancyArrowPatch.draw(self, renderer)
 
+def plot_group(g, color, ax, orthographic = False, show_origin_vectors = True, panel_numbers = True):
+  # recursively plot a detector group
+  p = g.parent()
+  if show_origin_vectors:
+    if p is None:
+      #parent origin
+      pori = (0,0,0)
+    else:
+      #parent origin
+      pori = p.get_origin()
+    ori = g.get_origin()
+    if not orthographic:
+      a = Arrow3D([pori[0], ori[0]], [pori[1], ori[1]],
+                  [pori[2], ori[2]], mutation_scale=20,
+                  lw=1, arrowstyle="-|>", color='gray')
+      ax.add_artist(a)
+  if g.is_group():
+    for c in g:
+      # plot all the children
+      plot_group(c, color, ax, orthographic, show_origin_vectors, panel_numbers)
+  else:
+    # plot the panel boundaries
+    size = g.get_image_size()
+    p0 = col(g.get_pixel_lab_coord((0,0)))
+    p1 = col(g.get_pixel_lab_coord((size[0]-1,0)))
+    p2 = col(g.get_pixel_lab_coord((size[0]-1,size[1]-1)))
+    p3 = col(g.get_pixel_lab_coord((0,size[1]-1)))
+    v1 = p1-p0
+    v2 = p3-p0
+    vcen = ((v2/2) + (v1/2)) + p0
+    z = zip(p0,p1,p2,p3,p0)
+
+    if orthographic:
+      ax.plot(z[0], z[1], color=color)
+
+      if panel_numbers:
+        # Annotate with panel numbers
+        ax.text(vcen[0], vcen[1], '%d'%g.index())
+    else:
+      ax.plot(z[0], z[1], z[2], color=color)
+
+      if panel_numbers:
+        # Annotate with panel numbers
+        ax.text(vcen[0], vcen[1], vcen[2], '%d'%g.index())
+
+
 def run(args):
   user_phil=[]
   files = []
@@ -62,51 +108,6 @@ def run(args):
   params = phil_scope.fetch(sources = user_phil).extract()
   if not params.orthographic:
     from mpl_toolkits.mplot3d import proj3d
-
-  def plot_group(g, color, orthographic = False):
-    # recursively plot a detector group
-    p = g.parent()
-    if params.show_origin_vectors:
-      if p is None:
-        #parent origin
-        pori = (0,0,0)
-      else:
-        #parent origin
-        pori = p.get_origin()
-      ori = g.get_origin()
-      if not orthographic:
-        a = Arrow3D([pori[0], ori[0]], [pori[1], ori[1]],
-                    [pori[2], ori[2]], mutation_scale=20,
-                    lw=1, arrowstyle="-|>", color='gray')
-        ax.add_artist(a)
-    if g.is_group():
-      for c in g:
-        # plot all the children
-        plot_group(c, color, orthographic)
-    else:
-      # plot the panel boundaries
-      size = g.get_image_size()
-      p0 = col(g.get_pixel_lab_coord((0,0)))
-      p1 = col(g.get_pixel_lab_coord((size[0]-1,0)))
-      p2 = col(g.get_pixel_lab_coord((size[0]-1,size[1]-1)))
-      p3 = col(g.get_pixel_lab_coord((0,size[1]-1)))
-      v1 = p1-p0
-      v2 = p3-p0
-      vcen = ((v2/2) + (v1/2)) + p0
-      z = zip(p0,p1,p2,p3,p0)
-
-      if orthographic:
-        ax.plot(z[0], z[1], color=color)
-
-        if params.panel_numbers:
-          # Annotate with panel numbers
-          ax.text(vcen[0], vcen[1], '%d'%g.index())
-      else:
-        ax.plot(z[0], z[1], z[2], color=color)
-
-        if params.panel_numbers:
-          # Annotate with panel numbers
-          ax.text(vcen[0], vcen[1], vcen[2], '%d'%g.index())
 
   fig = plt.figure()
   colormap = plt.cm.gist_ncar
@@ -129,7 +130,9 @@ def run(args):
         ax = fig.gca()
       else:
         ax = fig.gca(projection='3d')
-      plot_group(detector.hierarchy(), color, orthographic = params.orthographic)
+      plot_group(detector.hierarchy(), color, ax, orthographic = params.orthographic,
+                                                  show_origin_vectors = params.show_origin_vectors,
+                                                  panel_numbers = params.panel_numbers)
 
   if params.orthographic:
     plt.axes().set_aspect('equal', 'datalim')
