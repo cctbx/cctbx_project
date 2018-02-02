9592f7929b49d621f40e187e4465b4be8e9cb580
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-02-02 14:16:52 +0000

    fixing missed test

--- libtbx/tst_object_oriented_patterns.py
+++ libtbx/tst_object_oriented_patterns.py
@@ -1,4 +1,4 @@
-from __future__ import division
+from __future__ import absolute_import, division, print_function
 from libtbx import object_oriented_patterns as oop
 from libtbx.test_utils import approx_equal, Exception_expected
 import libtbx
@@ -172,8 +172,6 @@ def run():
   exercise_injector()
   exercise_memoize()
   exercise_null()
-  exercise_easy_property()
-  print 'OK'
 
 if __name__ == '__main__':
   run()
