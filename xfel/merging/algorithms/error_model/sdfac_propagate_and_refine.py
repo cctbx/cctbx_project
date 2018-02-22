929f0103695fff63fd9fcdc1d07ba00289f8abdf
Aaron Brewster <asbrewster@lbl.gov>
2018-02-22 15:46:01 -0800

    LevMar minimizer for sdfac propagate and refine.
    (Not hooked up to cxi.merge yet)

--- xfel/merging/algorithms/error_model/sdfac_propagate_and_refine.py
+++ xfel/merging/algorithms/error_model/sdfac_propagate_and_refine.py
@@ -170,3 +170,27 @@ class sdfac_propagate_and_refine(sdfac_refine_refltable_lbfgs):
 
       all_sigmas_normalized = all_sigmas_normalized.select(all_sigmas_normalized != 0)
       self.normal_probability_plot(all_sigmas_normalized, (-0.5, 0.5), plot = True)
+
+class sdfac_propagate_and_refine_levmar(sdfac_propagate_and_refine):
+  def run_minimzer(self, values, sels, **kwargs):
+    from xfel.merging.algorithms.error_model.sdfac_refine_levmar import sdfac_helper
+    from scitbx.lstbx import normal_eqns_solving
+
+    self.refinery = sdfac_propagate_refinery(self.scaler, self.scaler.miller_set.indices(), sels, self.log)
+    self.helper = sdfac_helper(current_x = values.reference,
+                               parameterization = self.parameterization, refinery = self.refinery,
+                               out = self.log )
+    self.iterations = normal_eqns_solving.levenberg_marquardt_iterations(
+      non_linear_ls = self.helper,
+      track_all=True,
+      gradient_threshold=1e-08,
+      step_threshold=1e-08,
+      tau=1e-08,
+      n_max_iterations=200)
+    return self
+
+  def get_refined_params(self):
+    return self.parameterization(self.helper.x)
+
+  def apply_sd_error_params(self, data, values):
+    self.refinery.apply_sd_error_params(data, values)
