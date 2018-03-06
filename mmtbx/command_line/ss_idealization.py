59385a20b17db496d9186b7ab54b3e878d3595db
Oleg Sobolev <osobolev@lbl.gov>
2018-03-06 13:56:49 -0800

    Removing use of obsoleted write_whole_pdb_file.

--- mmtbx/command_line/ss_idealization.py
+++ mmtbx/command_line/ss_idealization.py
@@ -2,6 +2,7 @@ from __future__ import division
 # LIBTBX_SET_DISPATCHER_NAME mmtbx.ss_idealization
 
 from mmtbx.secondary_structure import build as ssb
+import mmtbx.model
 import iotbx.pdb
 import os, sys
 
@@ -10,29 +11,19 @@ import os, sys
 def run(args):
   pdb_inp = iotbx.pdb.input(source_info=None,
     file_name=args[0])
-  ss_ann = pdb_inp.extract_secondary_structure()
-  pdb_h = pdb_inp.construct_hierarchy()
+  model = mmtbx.model.manager(
+      model_input=pdb_inp)
   params = ssb.master_phil.extract()
   params.ss_idealization.file_name_before_regularization="before_reg.pdb"
   params.ss_idealization.enabled=True
   rm = ssb.substitute_ss(
-    real_h=pdb_h,
-    xray_structure=pdb_inp.xray_structure_simple(),
-    use_plane_peptide_bond_restr=True,
-    log=sys.stdout,
-    ss_annotation=ss_ann,
-    params=params)
+    model = model,
+    params = params,
+    log=sys.stdout)
   out_fname = "%s_ss_ideal.pdb" % os.path.basename(args[0])
-  iotbx.pdb.write_whole_pdb_file(
-    file_name=out_fname,
-    output_file=None,
-    processed_pdb_file=None,
-    pdb_hierarchy=pdb_h,
-    crystal_symmetry=None,
-    ss_annotation=ss_ann,
-    append_end=True,
-    atoms_reset_serial_first_value=None,
-    link_records=None)
+  txt = model.model_as_pdb()
+  with open(out_fname, 'w') as f:
+    f.write(txt)
   print "File saved: %s" % out_fname
   print "All done."
 
