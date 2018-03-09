df0255c1bef5ba038a8f9656979f590aed1d3d43
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-03-09 12:49:15 +0000

    Make temp_chdir() thread safe.
    Part 1 of #150, which should fix xia2/xia2#214

--- dxtbx/serialize/filename.py
+++ dxtbx/serialize/filename.py
@@ -1,27 +1,21 @@
-#!/usr/bin/env python
-#
-# dxtbx.serialize.imageset.py
-#
-#  Copyright (C) 2013 Diamond Light Source
-#
-#  Author: James Parkhurst
-#
-#  This code is distributed under the BSD license, a copy of which is
-#  included in the root directory of this package.
-from __future__ import absolute_import, division
+from __future__ import absolute_import, division, print_function
 from contextlib import contextmanager
+import os
+import threading
+
+temp_chdir_lock = threading.RLock()
 
 @contextmanager
 def temp_chdir(path):
   ''' A context manager to temporarily change the current directory, perform a
   task and then change back to the previous working directory. '''
-  from os import getcwd, chdir
-  cwd = getcwd()
-  try:
-    chdir(path)
-    yield
-  finally:
-    chdir(cwd)
+  with temp_chdir_lock:
+    cwd = os.getcwd()
+    try:
+      os.chdir(path)
+      yield
+    finally:
+      os.chdir(cwd)
 
 def load_path(path):
   ''' Load a filename from a JSON file.
@@ -34,7 +28,6 @@ def load_path(path):
     path The path to the file.
 
   '''
-  from os.path import abspath, expanduser, expandvars
   if path is None or path == "":
     return ""
-  return abspath(expanduser(expandvars(path)))
+  return os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
