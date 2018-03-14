bf00d1e4375ebb43f920846dfa25ddfa4ae813c9
Asmit Bhowmick <asmit3@gmail.com>
2018-03-14 10:42:14 -0700

    importing h5py before importing anything in dxtbx to get around ASan issue

--- dxtbx/__init__.py
+++ dxtbx/__init__.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
 import logging
+try:
+  import h5py # h5py needs to be imported first before anything in dxtbx to work around ASan issues with conda builds
+except ImportError:
+  pass
 
 from scitbx.array_family import flex # import dependency
 try:
