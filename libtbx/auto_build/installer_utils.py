c75f56ce4318a426a5e854ce2d1c540422386abc
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-03-14 20:17:20 +0000

    Make file python3 compatible and clean up a bit

--- libtbx/auto_build/installer_utils.py
+++ libtbx/auto_build/installer_utils.py
@@ -1,9 +1,8 @@
-from __future__ import absolute_import, division
+from __future__ import absolute_import, division, print_function, with_statement
 
 import os
 import os.path as op
 import platform
-import re
 import shutil
 import stat
 import subprocess
@@ -18,9 +17,8 @@ def check_python_version():
     sys.exit("Python version 2.5 or greater required to run this script")
 
 def call(args, log=sys.stdout, shell=True, cwd=None, verbose=False, env=None):
-  rc = None
   # shell=True requires string as args.
-  if shell and isinstance(args, list) :
+  if shell and isinstance(args, list):
     args = " ".join(args)
   if verbose:
     stdout = subprocess.PIPE
@@ -43,7 +41,7 @@ def call(args, log=sys.stdout, shell=True, cwd=None, verbose=False, env=None):
       # this will cause a deadlock if process is writing to stderr
       # stderr is redirected to stdout, so this cannot happen
       if line:
-        print ": " + line.strip()
+        print(": " + line.strip())
         log.write(line)
   #o, e = p.communicate()
   #log.write(o)
@@ -51,10 +49,10 @@ def call(args, log=sys.stdout, shell=True, cwd=None, verbose=False, env=None):
   p.wait()
   log.flush()
   rc = p.returncode
-  if (rc != 0) :
+  if rc != 0:
     raise RuntimeError("Call to '%s' failed with exit code %d" % (args, rc))
 
-def check_output (*popenargs, **kwargs) :
+def check_output(*popenargs, **kwargs):
   # Back-port of Python 2.7 subprocess.check_output.
   process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
   output, unused_err = process.communicate()
@@ -63,18 +61,18 @@ def check_output (*popenargs, **kwargs) :
     raise RuntimeError("Call to '%s' failed with exit code %d" % (popenargs, retcode))
   return output
 
-def untar (pkg_name, log=sys.stdout, verbose=False, change_ownership=False,
-    check_output_path=True) :
+def untar(pkg_name, log=sys.stdout, verbose=False, change_ownership=False,
+    check_output_path=True):
   assert os.path.isfile(pkg_name), pkg_name
   verbose_flag = owner_flag = ""
-  if (verbose) :
+  if verbose:
     verbose_flag = "v"
-  if (change_ownership) :
+  if change_ownership:
     owner_flag = "o"
   cmd = [ "tar", "x%s%sf" % (owner_flag, verbose_flag) ]
-  if (pkg_name.endswith("gz")) :
+  if pkg_name.endswith("gz"):
     cmd = ["tar", "zx%s%sf" % (owner_flag, verbose_flag) ]
-  elif (pkg_name.endswith("bz2")) :
+  elif pkg_name.endswith("bz2"):
     cmd = ["tar", "jx%s%sf" % (owner_flag, verbose_flag) ]
   args = cmd + [pkg_name]
   if os.name != 'nt':
@@ -91,32 +89,34 @@ def untar (pkg_name, log=sys.stdout, verbose=False, change_ownership=False,
     tar.extractall()
     tar.close()
 
-  dir_name = re.sub(".tar", "",
-               re.sub(".tgz", "",
-                 re.sub(".tar.gz", "",
-                   re.sub(".tar.bz2", "",
-                     os.path.basename(pkg_name)))))
-  if (check_output_path) :
-    if (not os.path.isdir(dir_name)) :
+  dir_name = os.path.basename(pkg_name) \
+                 .replace(".tar.bz2", "") \
+                 .replace(".tar.gz", "") \
+                 .replace(".tgz", "") \
+                 .replace(".tar", "")
+  if check_output_path:
+    if not os.path.isdir(dir_name):
       time.sleep(1)
-      if (not os.path.isdir(dir_name)) :
-        if (os.path.isdir(dir_name.capitalize())):
+      if not os.path.isdir(dir_name):
+        if os.path.isdir(dir_name.capitalize()):
           dir_name = dir_name.capitalize()
         else:
           raise RuntimeError("Expected directory '%s' not found!" % dir_name)
     return os.path.abspath(dir_name)
   return None
 
-def detect_osx_version () :
+def detect_osx_version():
   uname = os.uname()
   version = uname[2]
   major, minor, rev = version.split(".")
   return int(major)
 
-def copy_file (src_path, dest_path, executable=None) :
+def copy_file(src_path, dest_path, executable=None):
   assert os.path.isfile(src_path)
-  open(dest_path, "wb").write(open(src_path, "rb").read())
-  if os.access(src_path, os.X_OK) or executable :
+  with open(src_path, "rb") as fi:
+    with open(dest_path, "wb") as fo:
+      fo.write(fi.read())
+  if os.access(src_path, os.X_OK) or executable:
     mode = os.stat(dest_path).st_mode
     os.chmod(dest_path, mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
@@ -125,70 +125,67 @@ def copy_file (src_path, dest_path, executable=None) :
 # http://bugs.python.org/issue14662
 # this is not a general solution, but it is good enough for the installer
 # process (at least on Unix).
-def copy_tree (src_path, dest_path, verbose=False, log=sys.stdout) :
-  if (sys.version_info > (2,7,4)) :
-    shutil.copytree(src_path, dest_path, symlinks=True)
-  else :
-    assert os.path.isdir(src_path), src_path
-    assert not os.path.exists(dest_path), dest_path
-    if (verbose) :
-      print >> log, "creating %s" % dest_path
-    os.makedirs(dest_path)
-    for path_name in os.listdir(src_path) :
-      node_src_path = os.path.join(src_path, path_name)
-      node_dest_path = os.path.join(dest_path, path_name)
-      if os.path.islink(node_src_path) :
-        target_path = os.readlink(node_src_path)
-        os.symlink(target_path, node_dest_path)
-      elif os.path.isfile(node_src_path) :
-        if (verbose) :
-          print >> log, "  copy %s -> %s" % (node_src_path, node_dest_path)
-        copy_file(node_src_path, node_dest_path)
-      elif os.path.isdir(node_src_path) :
-        copy_tree(node_src_path, node_dest_path)
-      else :
-        if (verbose) :
-          print >> log, "  skipping %s" % node_src_path
+def copy_tree(src_path, dest_path, verbose=False, log=sys.stdout):
+  if sys.hexversion >= 0x2070400:
+    return shutil.copytree(src_path, dest_path, symlinks=True)
+  assert os.path.isdir(src_path), src_path
+  assert not os.path.exists(dest_path), dest_path
+  if verbose:
+    print("creating %s" % dest_path, file=log)
+  os.makedirs(dest_path)
+  for path_name in os.listdir(src_path):
+    node_src_path = os.path.join(src_path, path_name)
+    node_dest_path = os.path.join(dest_path, path_name)
+    if os.path.islink(node_src_path):
+      target_path = os.readlink(node_src_path)
+      os.symlink(target_path, node_dest_path)
+    elif os.path.isfile(node_src_path):
+      if verbose:
+        print("  copy %s -> %s" % (node_src_path, node_dest_path), file=log)
+      copy_file(node_src_path, node_dest_path)
+    elif os.path.isdir(node_src_path):
+      copy_tree(node_src_path, node_dest_path)
+    elif verbose:
+      print("  skipping %s" % node_src_path, file=log)
 
-def get_os_version () :
+def get_os_version():
   uname = os.uname()
   kernel_version = uname[2]
-  if (uname[0] == "Darwin") :
+  if uname[0] == "Darwin":
     # Use Python's platform module to determine OSX version
     release, _, _ = platform.mac_ver()
     # Convert X.Y.Z to X.Y - discard minor version
     release_major = ".".join(release.split(".")[:2])
     assert release, "Could not determine OSX version"
     return release_major
-
   return kernel_version
 
-def machine_type () :
+def machine_type():
   if sys.platform == "win32":
     mtype = "intel-windows"
     arch, os_type = platform.architecture()
-    if (arch == "64bit") :
+    if arch == "64bit":
       mtype += "-x86_64"
     return mtype
   uname = os.uname()
-  if (uname[0] == "Linux") :
+  if uname[0] == "Linux":
     mtype = "intel-linux-2.6"
-  elif (uname[0] == "Darwin") :
+  elif uname[0] == "Darwin":
     mtype = "mac-intel-osx"
-  else :
+  else:
     mtype = uname[0]
-  if (uname[-1] == "x86_64") :
+  if uname[-1] == "x86_64":
     mtype += "-x86_64"
-  elif (mtype == "mac-intel-osx") :
+  elif mtype == "mac-intel-osx":
     version_fields = uname[2].split(".")
     major_version = int(version_fields[0])
-    if (major_version >= 10) :
+    if major_version >= 10:
       mtype += "-x86_64"
-    if (major_version >= 13):
+    if major_version >= 13:
       mtype += "-10.9"
   return mtype
 
-def regenerate_relative_symlinks (dir_name, log=sys.stdout) :
+def regenerate_relative_symlinks(dir_name, log=sys.stdout):
   """
   Rewrite all symlinks in a directory with relative paths (allows later
   relocation).  This is mostly just for Mac OS where there are a lot of links
@@ -196,73 +193,73 @@ def regenerate_relative_symlinks (dir_name, log=sys.stdout) :
   """
   old_cwd = os.getcwd()
   os.chdir(dir_name)
-  for file_name in os.listdir(dir_name) :
+  for file_name in os.listdir(dir_name):
     # e.g. /usr/bin/cmd
     full_path = op.join(dir_name, file_name)
-    if op.islink(full_path) :
+    if op.islink(full_path):
       real_path = op.realpath(full_path) # e.g. /usr/cctbx/bin/cmd
       prefix = op.commonprefix([full_path, real_path]) # /usr
       rel_path_link = op.relpath(full_path, prefix) # bin/cmd
       rel_path_target = op.relpath(real_path, prefix) # cctbx/bin/cmd
       parent_dirs = [ ".." for d in op.split(rel_path_link) ][1:]
-      if (len(parent_dirs) > 0) :
+      if parent_dirs:
         parent_dir = op.join(*parent_dirs)
-      else :
+      else:
         parent_dir = "."
       new_path = op.join(parent_dir, rel_path_target) # ../../cctbx/bin/cmd
-      print >> log, "  creating symlink to %s" % new_path
+      print("  creating symlink to %s" % new_path, file=log)
       os.remove(file_name)
       os.symlink(new_path, file_name)
 
-def find_and_delete_files (dir_name, file_name=None, file_ext=None) :
+def find_and_delete_files(dir_name, file_name=None, file_ext=None):
   """
   Recursively walk through a directory and delete any files (or directories)
   with the specified file name or extension.  Effectively equivalent to
   running 'find . -name "PATTERN" | xargs rm -rf'.
   """
   deleted = []
-  for dirname, dirnames, filenames in os.walk(dir_name) :
-    for dn in dirnames :
-      if (dn == file_name) :
+  for dirname, dirnames, filenames in os.walk(dir_name):
+    for dn in dirnames:
+      if dn == file_name:
         full_path = op.join(dirname, dn)
         shutil.rmtree(full_path)
         deleted.append(full_path)
-    for fn in filenames :
+    for fn in filenames:
       full_path = op.join(dirname, fn)
-      if (fn == file_name) :
+      if fn == file_name:
         os.remove(full_path)
         deleted.append(full_path)
-      elif (file_ext is not None) and fn.endswith(file_ext) :
+      elif file_ext is not None and fn.endswith(file_ext):
         os.remove(full_path)
         deleted.append(full_path)
   return deleted
 
-def archive_dist (dir_name, create_tarfile=True, use_shutil=True) :
+def archive_dist(dir_name, create_tarfile=True, use_shutil=True):
   """
   Create a clean copy of a source repository, optionally bundling it as a
   gzipped tar file.
   """
   dir_name = op.abspath(dir_name)
-  assert op.isdir(dir_name) and (dir_name != os.getcwd())
+  assert op.isdir(dir_name) and dir_name != os.getcwd()
   module_name = op.basename(dir_name)
   local_path = op.join(os.getcwd(), module_name)
-  if (use_shutil) :
+  if use_shutil:
     shutil.copytree(dir_name, local_path)
-  else :
+  else:
     copy_tree(dir_name, local_path)
-  if op.exists(op.join(local_path, ".svn")) :
-    try :
+  if op.exists(op.join(local_path, ".svn")):
+    try:
       call("svnversion %s > %s/.svnversion" % (module_name, module_name),
         log=sys.stdout)
     except RuntimeError as e:
-      print e
+      print(e)
   find_and_delete_files(local_path, file_ext=".pyc")
   find_and_delete_files(local_path, file_ext=".pyo")
   find_and_delete_files(local_path, file_ext=".swp")
   find_and_delete_files(local_path, file_name=".svn")
   find_and_delete_files(local_path, file_name=".git")
   find_and_delete_files(local_path, file_name=".sconsign")
-  if (create_tarfile) :
+  if create_tarfile:
     #call("tar -czf %s.tar.gz %s" % (module_name, module_name), log=sys.stdout)
     tar = tarfile.open("%s.tar.gz" %module_name, "w:gz")
     tar.add(module_name)
@@ -274,12 +271,12 @@ def archive_dist (dir_name, create_tarfile=True, use_shutil=True) :
     return tar_file
   return op.join(os.getcwd(), module_name)
 
-def strip_libs (dir_name, log) :
-  for dirname, dirnames, filenames in os.walk(dir_name) :
-    for fn in filenames :
+def strip_libs(dir_name, log):
+  for dirname, dirnames, filenames in os.walk(dir_name):
+    for fn in filenames:
       full_path = op.join(dirname, fn)
-      if fn.endswith(".so") and op.isfile(full_path) :
-        try :
+      if fn.endswith(".so") and op.isfile(full_path):
+        try:
           call("strip %s" % full_path, log=log)
-        except RuntimeError :
+        except RuntimeError:
           pass
