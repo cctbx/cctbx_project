8f75fffccde678d740e8676ec566448fdae95d24
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-04-03 09:21:34 +0100

    python3 syntax compatibility

--- libtbx/queuing_system_utils/pbs_utils.py
+++ libtbx/queuing_system_utils/pbs_utils.py
@@ -1,7 +1,8 @@
 "Portable Batch System (PBS) utilities"
-from __future__ import division
+from __future__ import absolute_imports, division, print_function
 
-import sys, os
+import sys
+import os
 
 def eval_env(variable_name):
   return eval(os.environ.get(variable_name, "None"))
@@ -13,46 +14,46 @@ def raise_if_none(value, variable_name):
 
 class chunk_info(object):
 
-  def __init__(O):
-    O.pbs_array_size = eval_env("MY_PBS_ARRAY_SIZE")
-    O.pbs_arrayid_offset = eval_env("MY_PBS_ARRAYID_OFFSET")
-    O.pbs_arrayid = eval_env("PBS_ARRAYID")
-    O.mpi_num_procs = eval_env("OMPI_MCA_ns_nds_num_procs")
-    O.mpi_vpid = eval_env("OMPI_MCA_ns_nds_vpid")
+  def __init__(self):
+    self.pbs_array_size = eval_env("MY_PBS_ARRAY_SIZE")
+    self.pbs_arrayid_offset = eval_env("MY_PBS_ARRAYID_OFFSET")
+    self.pbs_arrayid = eval_env("PBS_ARRAYID")
+    self.mpi_num_procs = eval_env("OMPI_MCA_ns_nds_num_procs")
+    self.mpi_vpid = eval_env("OMPI_MCA_ns_nds_vpid")
 
-  def show(O, out=None, prefix="", even_if_none=False):
+  def show(self, out=None, prefix="", even_if_none=False):
     if (out is None): out = sys.stdout
-    if (O.pbs_array_size is not None or even_if_none):
-      print >> out, prefix+"MY_PBS_ARRAY_SIZE =", O.pbs_array_size
-    if (O.pbs_arrayid_offset is not None or even_if_none):
-      print >> out, prefix+"MY_PBS_ARRAYID_OFFSET =", O.pbs_arrayid_offset
-    if (O.pbs_arrayid is not None or even_if_none):
-      print >> out, prefix+"PBS_ARRAYID =", O.pbs_arrayid
-    if (O.mpi_num_procs is not None or even_if_none):
-      print >> out, prefix+"OMPI_MCA_ns_nds_num_procs =", O.mpi_num_procs
-    if (O.mpi_vpid is not None or even_if_none):
-      print >> out, prefix+"OMPI_MCA_ns_nds_vpid =", O.mpi_vpid
-    return O
+    if (self.pbs_array_size is not None or even_if_none):
+      print(prefix+"MY_PBS_ARRAY_SIZE =", self.pbs_array_size, file=out)
+    if (self.pbs_arrayid_offset is not None or even_if_none):
+      print(prefix+"MY_PBS_ARRAYID_OFFSET =", self.pbs_arrayid_offset, file=out)
+    if (self.pbs_arrayid is not None or even_if_none):
+      print(prefix+"PBS_ARRAYID =", self.pbs_arrayid, file=out)
+    if (self.mpi_num_procs is not None or even_if_none):
+      print(prefix+"OMPI_MCA_ns_nds_num_procs =", self.mpi_num_procs, file=out)
+    if (self.mpi_vpid is not None or even_if_none):
+      print(prefix+"OMPI_MCA_ns_nds_vpid =", self.mpi_vpid, file=out)
+    return self
 
-  def have_array(O):
-    return O.pbs_array_size is not None
+  def have_array(self):
+    return self.pbs_array_size is not None
 
-  def as_n_i_pair(O):
-    if (not O.have_array()): return 1, 0
-    raise_if_none(O.pbs_arrayid_offset, "MY_PBS_ARRAYID_OFFSET")
-    raise_if_none(O.pbs_arrayid, "PBS_ARRAYID")
-    if (O.mpi_num_procs is None and O.mpi_vpid is None):
-      n = O.pbs_array_size
-      i = O.pbs_arrayid-1 + O.pbs_arrayid_offset
+  def as_n_i_pair(self):
+    if (not self.have_array()): return 1, 0
+    raise_if_none(self.pbs_arrayid_offset, "MY_PBS_ARRAYID_OFFSET")
+    raise_if_none(self.pbs_arrayid, "PBS_ARRAYID")
+    if (self.mpi_num_procs is None and self.mpi_vpid is None):
+      n = self.pbs_array_size
+      i = self.pbs_arrayid-1 + self.pbs_arrayid_offset
     else:
-      raise_if_none(O.mpi_num_procs, "OMPI_MCA_ns_nds_num_procs")
-      raise_if_none(O.mpi_vpid, "OMPI_MCA_ns_nds_vpid")
-      n = O.pbs_array_size * O.mpi_num_procs
-      i = (O.pbs_arrayid-1 + O.pbs_arrayid_offset) * O.mpi_num_procs + O.mpi_vpid
+      raise_if_none(self.mpi_num_procs, "OMPI_MCA_ns_nds_num_procs")
+      raise_if_none(self.mpi_vpid, "OMPI_MCA_ns_nds_vpid")
+      n = self.pbs_array_size * self.mpi_num_procs
+      i = (self.pbs_arrayid-1 + self.pbs_arrayid_offset) * self.mpi_num_procs + self.mpi_vpid
     return n, i
 
 # XXX this is probably only going to work with newer versions, e.g. Torque
-def qstat_parse () :
+def qstat_parse():
   from libtbx.queuing_system_utils.sge_utils import qstat_items
   from libtbx import easy_run
   from xml.dom import minidom
@@ -91,5 +92,5 @@ def qstat_parse () :
 
 if (__name__ == "__main__"):
   n,i = chunk_info().show(prefix="*** ", even_if_none=True).as_n_i_pair()
-  print "n,i:", n,i
-  print "OK"
+  print("n,i:", n,i)
+  print("OK")
