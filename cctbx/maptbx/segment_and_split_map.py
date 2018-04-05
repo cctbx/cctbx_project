3cbf905ba5df6f33ac25450f824dcc5c1f041e2a
terwill <tterwilliger@newmexicoconsortium.org>
2018-04-05 12:50:39 -0700

    Catch case where there are no atoms

--- cctbx/maptbx/segment_and_split_map.py
+++ cctbx/maptbx/segment_and_split_map.py
@@ -8874,6 +8874,9 @@ def get_target_boxes(si=None,ncs_obj=None,map=None,
   if pdb_inp and pdb_inp.atoms().extract_xyz().size()>1:
     xyz_list=pdb_inp.atoms().extract_xyz()
     i_end=xyz_list.size()
+  else:
+    i_end=0
+  if i_end:
     n_centers=min(i_end,max(1,len(tracking_data.output_region_map_info_list)))
     n_steps=min(n_centers,xyz_list.size())
     i_step=int(0.5+min(i_end/2,i_end/n_steps)) # about n_centers but up to n_atoms
