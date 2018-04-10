34884982d81ac4e5d764ca57c4e2bf8941f0f8da
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-04-10 13:52:14 +0100

    drop libtbx env dependency

--- dxtbx/conftest.py
+++ dxtbx/conftest.py
@@ -5,17 +5,17 @@
 
 from __future__ import absolute_import, division, print_function
 
-import libtbx.load_env
 import pytest
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def dials_regression():
   '''Return the absolute path to the dials_regression module as a string.
      Skip the test if dials_regression is not installed.'''
   try:
-    return libtbx.env.dist_path('dials_regression')
-  except KeyError:
+    import dials_regression as dr
+  except ImportError:
     pytest.skip("dials_regression required for this test")
+  return os.path.dirname(dr.__file__)
 
 from libtbx.test_utils.pytest import libtbx_collector
 pytest_collect_file = libtbx_collector()
