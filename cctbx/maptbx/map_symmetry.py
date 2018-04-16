7d50769a583d9e6aa019dc2cbf81b8cf13c37bca
terwill <tterwilliger@newmexicoconsortium.org>
2018-04-15 20:47:09 -0600

    Add map_symmetry

---
+++ cctbx/maptbx/map_symmetry.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+import os, sys
+from libtbx import  adopt_init_args
+
+#  map_symmetry
+#  tool to identify and evaluate reconstruction symmetry in a map
+
+class map_symmetry:
+
+  def __init__(self,params=None,
+      map_data=None,
+      crystal_symmetry=None,
+      ncs_object=None,
+      log=sys.stdout):
+    adopt_init_args(self, locals())
+
+    self.ncs_cc=None
+    self.ncs_object=None
+
+    if self.params and self.params.control.verbose:
+      self.local_log=log
+    else:
+      self.local_log=null_out()
+       
+  def clean_up(self):
+    pass 
+
+  def run(self):
+    print ("Finding symmetry in map",file=self.log)
