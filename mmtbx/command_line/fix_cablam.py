c03b0028685bfb3d2f5c91998835518b187205dd
Oleg Sobolev <osobolev@lbl.gov>
2018-04-18 13:16:01 -0700

    Using freshly discovered function for command-line programs. Now this is official way of doing default stuff.

--- mmtbx/command_line/fix_cablam.py
+++ mmtbx/command_line/fix_cablam.py
@@ -1,43 +1,7 @@
-from __future__ import division, print_function
+from __future__ import division
 # LIBTBX_SET_DISPATCHER_NAME phenix.fix_cablam
 
-import sys
-
-from iotbx.cli_parser import CCTBXParser
-from libtbx.utils import multi_out, show_total_time
+from iotbx.cli_parser import run_program
 from mmtbx.programs import fix_cablam
 
-
-# =============================================================================
-def run(args):
-
-  # create parser
-  logger = multi_out() #logging.getLogger('main')
-  logger.register('stdout', sys.stdout)
-
-  parser = CCTBXParser(
-    program_class=fix_cablam.Program,
-    logger=logger)
-  namespace = parser.parse_args(sys.argv[1:])
-
-  # start program
-  print('Starting job', file=logger)
-  print('='*79, file=logger)
-  task = fix_cablam.Program(
-    parser.data_manager, parser.working_phil.extract(), logger=logger)
-
-  # validate inputs
-  task.validate()
-
-  # run program
-  task.run()
-
-  # stop timer
-  print('', file=logger)
-  print('='*79, file=logger)
-  print('Job complete', file=logger)
-  show_total_time(out=logger)
-
-# =============================================================================
-if __name__ == '__main__':
-  run(sys.argv[1:])
+run_program(fix_cablam.Program)
