cbc9e8210e94eea6813c2020c63e4cb4d15cab0d
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-04-28 21:11:39 +0100

    Remove dxtbx root imports
    I am fairly certain these are not required. dxtbx/dials/xia2 and
    regression tests ran without issues.

--- dxtbx/__init__.py
+++ dxtbx/__init__.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, division, print_function
 
 import logging
 
-from scitbx.array_family import flex # import dependency
 try:
   import boost.python
 except Exception:
@@ -12,8 +11,6 @@ else:
 
 if ext is not None:
   from dxtbx_ext import *
-
-import dxtbx.imageset # implicit import
 
 logging.getLogger('dxtbx').addHandler(logging.NullHandler())
 
