268c21c1bfc881aa14f25dd053c8f3114348d03f
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-05-11 07:56:11 +0100

    Move list of image files into a common location.
    Fix tests for Windows when run via libtbx test runner.

--- dxtbx/tests/test_image_readers.py
+++ dxtbx/tests/test_image_readers.py
@@ -2,87 +2,8 @@ from __future__ import absolute_import, division, print_function
 
 import os
 
+import dxtbx.tests.imagelist
 import pytest
-
-smv_images = [
-    "image_examples/ALS_501/als501_q4_1_001.img",
-    "image_examples/SPring8_BL26B1_SaturnA200/A200_000001.img",
-    "image_examples/SPring8_BL26B1_SaturnA200/A200_000002.img",
-    "image_examples/APS_19ID/q315_unbinned_a.0001.img",
-    "image_examples/ALS_821/q210_lyso_1_101.img",
-    "image_examples/MLFSOM_simulation/fake_00001.img",
-    "image_examples/ALS_831/q315r_lyso_001.img",
-    "image_examples/DESY_ID141/q210_2_001.img",
-    "image_examples/SSRL_bl91/q315_1_001.img",
-    "image_examples/APS_24IDC/q315_1_001.img",
-    "image_examples/ALS_1231/q315r_lyso_1_001.img",
-    "image_examples/SRS_142/q4_1_001.img",
-    "image_examples/ALS_422/lyso_041013a_1_001.img",
-    "image_examples/APS_17ID/q210_1_001.img",
-    "image_examples/saturn/lyso_00001.img",
-]
-
-tiff_images = [
-    "image_examples/SPring8_BL32XU_MX225HS/ds_000045.img",
-    "image_examples/SPring8_BL32XU_MX225HS/ds_000001.img",
-    "image_examples/SPring8_BL44XU_MX300HE/bl44xu_lys_000002.img",
-    "image_examples/SPring8_BL44XU_MX300HE/bl44xu_lys_000001.img",
-    "image_examples/SPring8_BL32XU/rayonix225hs_0001.img",
-    "image_examples/SPring8_BL32XU/rayonix225_0001.img",
-    "image_examples/SLS_X06SA/mar225_2_001.img",
-    "image_examples/CLS1_08ID1/mar225_2_E0_0001.img",
-    "image_examples/SPring8_BL38B1_MX225HE/bl38b1_001.img",
-    "image_examples/SPring8_BL38B1_MX225HE/bl38b1_090.img",
-    "image_examples/SRS_101/mar225_001.img",
-    "image_examples/SPring8_BL12B2_MX225HE/lys001_000091.img",
-    "image_examples/SPring8_BL12B2_MX225HE/lys001_000001.img",
-    "image_examples/SPring8_BL26B2_MX225/2sec_Al200um_000001.img",
-    "image_examples/SPring8_BL26B2_MX225/2sec_Al200um_000090.img",
-]
-
-cbf_images = [
-    "image_examples/ESRF_ID29/trypsin_1_0001.cbf",
-    "image_examples/xia2/merge2cbf_averaged_0001.cbf",
-    "image_examples/dials-190/whatev1_01_00002.cbf",
-    "image_examples/dials-190/whatev1_02_00001.cbf",
-    "image_examples/dials-190/whatev1_02_00002.cbf",
-    "image_examples/dials-190/whatev1_03_00001.cbf",
-    "image_examples/dials-190/whatev1_01_00001.cbf",
-    "image_examples/dials-190/whatev1_03_00002.cbf",
-    "image_examples/APS_24IDE_test/thaum-12_1_0005.cbf",
-    "image_examples/APS_24IDE_test/thaum-12_1_0004.cbf",
-    "image_examples/APS_24IDE_test/thaum-12_1_0006.cbf",
-    "image_examples/APS_24IDE_test/thaum-12_1_0008.cbf",
-    "image_examples/APS_24IDE_test/thaum-12_1_0009.cbf",
-    "image_examples/APS_24IDE_test/thaum-12_1_0010.cbf",
-    "image_examples/APS_24IDE_test/thaum-12_1_0007.cbf",
-    "image_examples/APS_24IDE_test/thaum-12_1_0002.cbf",
-    "image_examples/APS_24IDE_test/thaum-12_1_0001.cbf",
-    "image_examples/APS_24IDE_test/thaum-12_1_0003.cbf",
-    "image_examples/APS_24IDC/pilatus_1_0001.cbf",
-    "image_examples/SLS_Eiger_16M_as_CBF/insu_with_bs_labelit_0901.cbf",
-    "image_examples/SLS_Eiger_16M_as_CBF/insu_with_bs_labelit_0001.cbf",
-    "image_examples/SPring8_BL41XU_PILATUS3_6M/data1_000001.cbf",
-    "image_examples/SPring8_BL41XU_PILATUS3_6M/data1_000901.cbf",
-    "image_examples/DLS_I02/X4_wide_M1S4_1_0001.cbf",
-    "image_examples/DLS_I23/I23_P12M_alpha_0001.cbf",
-    "image_examples/DLS_I23/germ_13KeV_0001.cbf",
-    "image_examples/SLS_X06SA/pilatus6m_1_00001.cbf",
-    "image_examples/SPring8_ADSC_SN916/Xtal17-2phi_3_015.cbf",
-    "image_examples/DLS_I19/I19_P300k_00001.cbf",
-    "image_examples/ED_From_TIFF/170112330001.cbf",
-]
-
-cbf_multitile_images = [
-    "stills_test_data/hit-20111202210224984.cbf",
-    "stills_test_data/hit-s00-20140306002935980.cbf",
-    "stills_test_data/hit-s00-20140306002857363.cbf",
-]
-
-hdf5_images = [
-    "image_examples/putative_imgCIF_HDF5_mapping/minicbf.h5",
-]
-
 
 def get_smv_header(image_file):
   header_size = int(open(image_file, 'rb').read(45).split(
@@ -275,7 +196,7 @@ def read_multitile_cbf_image(cbf_image):
 
   return tuple(raw_data)
 
-@pytest.mark.parametrize('smv_image', smv_images)
+@pytest.mark.parametrize('smv_image', dxtbx.tests.imagelist.smv_images, ids=dxtbx.tests.imagelist.smv_image_ids)
 def test_smv(dials_regression, smv_image):
   from dxtbx.format.image import SMVReader
   from scitbx.array_family import flex
@@ -294,7 +215,7 @@ def test_smv(dials_regression, smv_image):
   diff = flex.abs(data1 - data2)
   assert flex.max(diff) < 1e-7
 
-@pytest.mark.parametrize('tiff_image', tiff_images)
+@pytest.mark.parametrize('tiff_image', dxtbx.tests.imagelist.tiff_images, ids=dxtbx.tests.imagelist.tiff_image_ids)
 def test_tiff(dials_regression, tiff_image):
   from scitbx.array_family import flex
   from dxtbx.format.image import TIFFReader
@@ -315,7 +236,7 @@ def test_tiff(dials_regression, tiff_image):
 
 
 #@pytest.mark.skip(reason="test unused")
-#@pytest.mark.parametrize('cbf_image', cbf_images)
+#@pytest.mark.parametrize('cbf_image', dxtbx.tests.imagelist.cbf_images, ids=dxtbx.tests.imagelist.cbf_image_ids)
 #def test_cbf_fast(dials_regression, cbf_image):
 #  from scitbx.array_family import flex
 #  from dxtbx.format.image import CBFFastReader
@@ -331,7 +252,11 @@ def test_tiff(dials_regression, tiff_image):
 #  assert flex.max(diff) < 1e-7
 
 
-@pytest.mark.parametrize('cbf_image', cbf_images + cbf_multitile_images)
+@pytest.mark.parametrize(
+    'cbf_image',
+    dxtbx.tests.imagelist.cbf_images + dxtbx.tests.imagelist.cbf_multitile_images,
+    ids=dxtbx.tests.imagelist.cbf_image_ids + dxtbx.tests.imagelist.cbf_multitile_image_ids,
+)
 def test_cbf(dials_regression, cbf_image):
   from scitbx.array_family import flex
   from dxtbx.format.image import CBFReader
@@ -357,7 +282,7 @@ def test_cbf(dials_regression, cbf_image):
     assert flex.max(diff) < 1e-7
 
 
-@pytest.mark.parametrize('hdf5_image', hdf5_images)
+@pytest.mark.parametrize('hdf5_image', dxtbx.tests.imagelist.hdf5_images, ids=dxtbx.tests.imagelist.hdf5_image_ids)
 def test_hdf5(dials_regression, hdf5_image):
   from dxtbx.format.image import HDF5Reader
   from dxtbx.format.nexus import dataset_as_flex_int
