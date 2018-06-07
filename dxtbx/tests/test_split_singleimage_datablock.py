daf39d27a45ab62b68703495b89c550bd8a3564a
Aaron Brewster <asbrewster@lbl.gov>
2018-06-07 11:14:42 -0700

    Add test for split datablocks. Test is marked as expected to fail.
    The test reads an hdf5 file as a datablock, splits it so the datablock contains one image, then serializes the datablock.  Deserializing the datablock with check_format=True works fine.  The single file indices array, which tracks which images are in the datablock, is correctly read from the json.  However, check_format=False does not correctly deserialize the array.  This affects all cases of check_format=False, noteably in dials.split_experiments and dials.combine_experiments.

---
+++ dxtbx/tests/test_split_singleimage_datablock.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import, division, print_function
+import pytest, os
+from dxtbx.datablock import DataBlockFactory, DataBlockDumper
+
+"""
+Test to show that deserializing a datablock that has single file indices while using check_format=False fails
+"""
+
+def get_indices(datablock):
+  imageset = datablock.extract_imagesets()[0]
+  return list(imageset.indices())
+
+@pytest.mark.xfail
+def test_split_single_image_datablock(dials_regression, tmpdir):
+  tmpdir.chdir()
+  sacla_file = os.path.join(dials_regression, "image_examples", "SACLA_MPCCD_Cheetah", "run266702-0-subset.h5")
+  db = DataBlockFactory.from_filenames([sacla_file])[0]
+  assert db.num_images() == 4
+  imageset = db.extract_imagesets()[0]
+  subset = imageset[2:3]
+  subblock = DataBlockFactory.from_imageset(subset)[0]
+  assert subblock.num_images() == 1
+  assert get_indices(subblock) == [2]
+
+  dumped_filename = "split_datablock.json"
+  dump = DataBlockDumper(subblock)
+  dump.as_json(dumped_filename)
+
+  db = DataBlockFactory.from_json_file(dumped_filename, check_format = True)[0]
+  assert db.num_images() == 1
+  assert get_indices(db) == [2]
+
+  db = DataBlockFactory.from_json_file(dumped_filename, check_format = False)[0]
+  assert db.num_images() == 1
+  assert get_indices(db) == [2] # fails. returns [0]
