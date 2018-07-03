421475f73c291abd07bbe1879ca66e6d076a7756
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-07-03 14:13:58 +0100

    dxtbx: Add pytest option '--regression'
    We run new, time-intensive regression tests only when the command line
    option '--regression' is specified.

--- dxtbx/conftest.py
+++ dxtbx/conftest.py
@@ -18,3 +18,17 @@ def dials_regression():
   except ImportError:
     pytest.skip("dials_regression required for this test")
   return os.path.dirname(dr.__file__)
+
+def pytest_addoption(parser):
+  '''Add '--regression' options to pytest.'''
+  parser.addoption("--regression", action="store_true", default=False,
+                   help="run (time-intensive) regression tests")
+
+def pytest_collection_modifyitems(config, items):
+  '''Tests marked as regression are only run with --regression.
+  '''
+  if not config.getoption("--regression"):
+    skip_regression = pytest.mark.skip(reason="Test only runs with --regression")
+    for item in items:
+      if "regression" in item.keywords:
+        item.add_marker(skip_regression)
