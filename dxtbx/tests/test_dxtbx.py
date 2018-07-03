edf34ccbe93ba6068a8ffced5d9d062bbeec61eb
Nicholas Devenish <ndevenish@gmail.com>
2018-07-03 12:04:29 +0100

    Merge image reading tests with dials_regression
    There was a test in test_dxtbx.py that read some subset of
    dials_regression/image_examples, and a test in dials_regression that
    read most of the images that it could find. This replaces the former
    with a pytest version of the latter. The original
    dials_regression/test.py test will remain for now. Also includes the
    dials_regression/tst_all_formats.py tests.
    
    Also, removed the (skipped) compression tests; these are probably better
    tested with explicit compressed image files and/or a unit test of the
    functionality - compressed image reading is not handled at the
    per-Format level.

--- dxtbx/tests/test_dxtbx.py
+++ dxtbx/tests/test_dxtbx.py
@@ -3,85 +3,9 @@ from __future__ import absolute_import, division, print_function
 import os
 import pytest
 
-def pytest_generate_tests(metafunc):
-  if 'dxtbx_test_image' not in metafunc.fixturenames:
-    return
-  try:
-    from dials_regression.image_examples.get_all_working_images import get_all_working_images
-  except ImportError:
-    metafunc.parametrize('dxtbx_test_image',
-        [pytest.param(None, marks=pytest.mark.skip('dials_regression is required for this group of tests'))],
-        ids=["noregression"])
-    return
-  imagelist = get_all_working_images()
-  metafunc.parametrize('dxtbx_test_image', imagelist, ids=[i[1] for i in imagelist])
+from dxtbx.sweep_filenames import template_regex
 
-def test_dxtbx(dials_regression, dxtbx_test_image):
-  #from boost.python import streambuf
-  #from dxtbx import read_uint16
-  from dxtbx.format.Registry import Registry
-
-  directory, image = dxtbx_test_image
-  file_path = os.path.join(dials_regression, 'image_examples', directory, image)
-
-  format = Registry.find(file_path)
-  i = format(file_path)
-  det = i.get_detector()
-  if det is not None:
-    size = det[0].get_image_size()
-  b = i.get_beam()
-  g = i.get_goniometer()
-  s = i.get_scan()
-  try:
-    d = i.get_raw_data()
-  except IOError:
-    pass
-
-@pytest.mark.skip("Test disabled in https://github.com/cctbx/cctbx_project/commit/e6ae0e03afe87500ad573b2fde42dca9334ea2e5")
-def test_dxtbx_compressed(dials_regression, tmpdir):
-  from dxtbx.format.Registry import Registry
-
-  from dials_regression.image_examples.get_all_working_images import \
-      get_all_working_images
-
-  # test that reading gz or bz2 compressed files works: it doesn't!
-
-  from libtbx import smart_open
-  import shutil
-
-  tmpdir.chdir()
-
-  for directory, image in get_all_working_images():
-    file_path = os.path.join(dials_regression, 'image_examples',
-                             directory, image)
-    for ext in ('.gz', '.bz2')[:]:
-      compressed_path = os.path.basename(file_path) + ext
-      with open(file_path, 'rb') as f_in, smart_open.for_writing(compressed_path) as f_out:
-        shutil.copyfileobj(f_in, f_out)
-      print(file_path, compressed_path)
-      format = Registry.find(compressed_path)
-      try:
-        i = format(compressed_path)
-      except Exception:
-        print('Error reading compressed file: %s' % compressed_path)
-        import traceback
-        traceback.print_exc()
-      else:
-        print('Successfully read compressed file: %s' % compressed_path)
-        det = i.get_detector()
-        if det is not None:
-          size = det[0].get_image_size()
-        b = i.get_beam()
-        g = i.get_goniometer()
-        s = i.get_scan()
-        try:
-          d = i.get_raw_data()
-        except IOError:
-          pass
-
-def test_sweep():
-  from dxtbx.sweep_filenames import template_regex
-
+def test_template_regex():
   questions_answers = {
       'foo_bar_001.img':'foo_bar_###.img',
       'foo_bar001.img':'foo_bar###.img',
