7f1a44e3bccd0635db09f42550ca15866e6107f2
Nicholas Sauter <nksauter@lbl.gov>
2018-07-06 21:19:37 -0700

    stub for new merging application.  run libtbx.python phil.py

---
+++ xfel/merging/application/validation/application.py
@@ -0,0 +1,10 @@
+from __future__ import division
+
+class application:
+  def __init__(self,param):
+
+    self.param = param
+    self.application_level_validation()
+
+  def application_level_validation(self):
+     pass
