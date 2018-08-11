c4ee66dc769a1b928fe7829c18c80fd015468989
Oleg Sobolev <osobolev@lbl.gov>
2018-08-10 17:30:00 -0700

    Removing obsolete, not tested and likely not working stuff.

--- mmtbx/refinement/geometry_minimization.py
+++ mmtbx/refinement/geometry_minimization.py
@@ -9,22 +9,6 @@ import mmtbx.utils
 from scitbx.array_family import flex
 from mmtbx import monomer_library
 
-master_params_str = """\
-  alternate_nonbonded_off_on=False
-    .type = bool
-  restrain_c_alpha_positions=False
-    .type = bool
-  max_iterations=500
-    .type = int
-  macro_cycles=1
-    .type = int
-  show_geometry_restraints=False
-    .type = bool
-"""
-
-def master_params():
-  return iotbx.phil.parse(master_params_str)
-
 class lbfgs(geometry_restraints.lbfgs.lbfgs):
 
   def __init__(self,
@@ -61,102 +45,6 @@ class lbfgs(geometry_restraints.lbfgs.lbfgs):
       if(self.rmsd_angles < self.rmsd_angles_termination_cutoff and
          self.rmsd_bonds < self.rmsd_bonds_termination_cutoff):
         return True
-
-def run(processed_pdb_file, params=master_params().extract(), log=sys.stdout):
-  co = params
-  geometry_restraints_flags = geometry_restraints.flags.flags(default=True)
-  all_chain_proxies = processed_pdb_file.all_chain_proxies
-  reference_manager = None
-  if (co.restrain_c_alpha_positions):
-    assert 0, "Broken! - rewrite this part first."
-    from mmtbx.geometry_restraints import reference
-    ca_selection=all_chain_proxies.pdb_hierarchy.get_peptide_c_alpha_selection()
-    ca_sites_cart = \
-      all_chain_proxies.sites_cart.deep_copy().select(ca_selection)
-    reference_manager = reference.manager()
-    reference_manager.add_coordinate_restraints(
-      sites_cart=ca_sites_cart,
-      selection=ca_selection)
-  geometry_restraints_manager = processed_pdb_file.\
-    geometry_restraints_manager(show_energies = False,
-                                reference_manager=\
-                                  reference_manager)
-  special_position_settings = all_chain_proxies.special_position_settings
-  sites_cart = all_chain_proxies.sites_cart_exact().deep_copy()
-  atom_labels = [atom.id_str() for atom in all_chain_proxies.pdb_atoms]
-  geometry_restraints_manager.site_symmetry_table \
-    .show_special_position_shifts(
-      special_position_settings=special_position_settings,
-      site_labels=atom_labels,
-      sites_cart_original=all_chain_proxies.sites_cart,
-      sites_cart_exact=sites_cart,
-      out=log,
-      prefix="  ")
-  if (co.show_geometry_restraints):
-    geometry_restraints_manager.show_sorted(
-      flags=geometry_restraints_flags,
-      sites_cart=sites_cart,
-      site_labels=atom_labels)
-  pair_proxies =  geometry_restraints_manager.pair_proxies(
-    sites_cart=all_chain_proxies.sites_cart,
-    flags=geometry_restraints_flags)
-  pair_proxies.bond_proxies.show_sorted(
-    by_value="residual",
-    sites_cart=sites_cart,
-    site_labels=atom_labels,
-    f=log,
-    max_items=10)
-  if (pair_proxies.nonbonded_proxies is not None):
-    pair_proxies.nonbonded_proxies.show_sorted(
-      by_value="delta",
-      sites_cart=sites_cart,
-      site_labels=atom_labels,
-      f=log,
-      max_items=10)
-  del pair_proxies
-  print >> log
-  log.flush()
-  if (co.alternate_nonbonded_off_on and co.macro_cycles % 2 != 0):
-    co.macro_cycles += 1
-    print >> log, "INFO: Number of macro cycles increased by one to ensure use of"
-    print >> log, "      nonbonded interactions in last macro cycle."
-    print >> log
-  for i_macro_cycle in xrange(co.macro_cycles):
-    if (co.alternate_nonbonded_off_on):
-      geometry_restraints_flags.nonbonded = bool(i_macro_cycle % 2)
-      print >> log, "Use nonbonded interactions this macro cycle:", \
-        geometry_restraints_flags.nonbonded
-    minimized = lbfgs(
-      sites_cart=sites_cart,
-      geometry_restraints_manager=geometry_restraints_manager,
-      geometry_restraints_flags=geometry_restraints_flags,
-      lbfgs_termination_params=scitbx.lbfgs.termination_parameters(
-        max_iterations=co.max_iterations),
-      lbfgs_exception_handling_params=
-        scitbx.lbfgs.exception_handling_parameters(
-          ignore_line_search_failed_step_at_lower_bound=True))
-    print >> log, "Energies at start of minimization:"
-    minimized.first_target_result.show(f=log)
-    print >> log
-    print >> log, "Number of minimization iterations:", minimized.minimizer.iter()
-    print >> log, "Root-mean-square coordinate difference: %.3f" % (
-      all_chain_proxies.sites_cart.rms_difference(sites_cart))
-    print >> log
-    print >> log, "Energies at end of minimization:"
-    minimized.final_target_result.show(f=log)
-    print >> log
-    geometry_restraints_manager.pair_proxies(
-      sites_cart=sites_cart,
-      flags=geometry_restraints_flags) \
-        .bond_proxies.show_sorted(
-          by_value="residual",
-          sites_cart=sites_cart,
-          site_labels=atom_labels,
-          f=log,
-          max_items=10)
-    print >> log
-  assert geometry_restraints_flags.nonbonded
-  return sites_cart
 
 def add_rotamer_restraints(
       pdb_hierarchy,
