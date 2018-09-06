1dae0a3ed15fc1da92c2af7803dc577145002966
pcxod <oleg_dolomanov@hotmail.com>
2018-09-06 18:29:10 +0100

    wrapping the wrapping to avoid duplication

--- smtbx/refinement/boost_python/least_squares.cpp
+++ smtbx/refinement/boost_python/least_squares.cpp
@@ -12,16 +12,12 @@ namespace smtbx { namespace refinement { namespace least_squares {
 
   template <typename FloatType,
     class OneMillerIndexLinearisation>
-  struct normal_equation_building
-  {
-    typedef build_normal_equations<FloatType> wt;
-
-    template <class NormalEquations,
+  struct wrapper {
+    template <class ObjectType,
+      class NormalEquations,
       template<typename> class WeightingSchemeType>
-    static void def_init(boost::python::class_<wt> &klass) {
+    static void def_init_(boost::python::class_<ObjectType> &klass) {
       using namespace boost::python;
-      // below the comment after each type is the argument name
-      // (c.f. smtbx::refinement::build_normal_equations)
       klass.def(
         init<
         NormalEquations &, // normal_equations
@@ -41,19 +37,18 @@ namespace smtbx { namespace refinement { namespace least_squares {
           arg("may_parallelise") = false)));
     }
 
-    static void wrap(char const *name) {
-      using namespace boost::python;
-
-      class_<wt> klass(name, no_init);
-
+    template <class ObjectType>
+    static void wrap_init(char const *name,
+      boost::python::class_<ObjectType> &klass)
+    {
       typedef
         lstbx::normal_equations::non_linear_ls_with_separable_scale_factor<
         FloatType,
         scitbx::matrix::sum_of_symmetric_rank_1_updates>
         NormalEquations_BLAS2;
-      def_init<NormalEquations_BLAS2, mainstream_shelx_weighting>(klass);
-      def_init<NormalEquations_BLAS2, unit_weighting            >(klass);
-      def_init<NormalEquations_BLAS2, sigma_weighting           >(klass);
+      def_init_<ObjectType, NormalEquations_BLAS2, mainstream_shelx_weighting>(klass);
+      def_init_<ObjectType, NormalEquations_BLAS2, unit_weighting            >(klass);
+      def_init_<ObjectType, NormalEquations_BLAS2, sigma_weighting           >(klass);
 
 #ifdef CCTBX_HAS_LAPACKE
       typedef
@@ -61,96 +56,53 @@ namespace smtbx { namespace refinement { namespace least_squares {
         FloatType,
         scitbx::matrix::rank_n_update>
         NormalEquations_BLAS3;
-      def_init<NormalEquations_BLAS3, mainstream_shelx_weighting>(klass);
-      def_init<NormalEquations_BLAS3, unit_weighting            >(klass);
-      def_init<NormalEquations_BLAS3, sigma_weighting           >(klass);
+      def_init_<ObjectType, NormalEquations_BLAS3, mainstream_shelx_weighting>(klass);
+      def_init_<ObjectType, NormalEquations_BLAS3, unit_weighting            >(klass);
+      def_init_<ObjectType, NormalEquations_BLAS3, sigma_weighting           >(klass);
 #endif
-
-      klass
-        .def("observables", &wt::observables)
-        .def("f_calc", &wt::f_calc)
-        .def("weights", &wt::weights)
-        ;
-    }
-  };
-
-  template <typename FloatType,
-    class OneMillerIndexLinearisation>
-  struct design_matrix_building {
-    typedef build_design_matrix<FloatType> wt;
-
-    template <class NormalEquations,
-      template<typename> class WeightingSchemeType>
-    static void def_init(boost::python::class_<wt> &klass) {
-      using namespace boost::python;
-      klass.def(
-        init<
-        NormalEquations &, // normal_equations
-        cctbx::xray::observations<FloatType> const &, // reflections
-        af::const_ref<std::complex<FloatType> > const &, // f_mask
-        WeightingSchemeType<FloatType> const &, // weighting_scheme
-        boost::optional<FloatType>, // scale_factor
-        OneMillerIndexLinearisation &, // f_calc_function
-        scitbx::sparse::matrix<FloatType> const &,
-        // jacobian_transpose_matching_grad_fc
-        cctbx::xray::extinction_correction<FloatType> const & // exti
-        >((arg("normal_equations"), arg("reflections"), arg("f_mask"),
-          arg("weighting_scheme"), arg("scale_factor"),
-          arg("f_calc_function"), arg("jacobian_transpose_matching_grad_fc"),
-          arg("extinction"), arg("objective_only") = false)));
     }
 
-    static void wrap(char const *name) {
-      using namespace boost::python;
-      class_<wt> klass(name, no_init);
+    struct normal_equation_building {
+      typedef build_normal_equations<FloatType> wt;
 
-      typedef
-        lstbx::normal_equations::non_linear_ls_with_separable_scale_factor<
-        FloatType,
-        scitbx::matrix::sum_of_symmetric_rank_1_updates>
-        NormalEquations_BLAS2;
-      def_init<NormalEquations_BLAS2, mainstream_shelx_weighting>(klass);
-      def_init<NormalEquations_BLAS2, unit_weighting            >(klass);
-      def_init<NormalEquations_BLAS2, sigma_weighting           >(klass);
+      static void wrap(char const *name) {
+        using namespace boost::python;
+        class_<wt> klass(name, no_init);
+        wrap_init<wt>(name, klass);
+        klass
+          .def("observables", &wt::observables)
+          .def("f_calc", &wt::f_calc)
+          .def("weights", &wt::weights)
+          ;
+      }
+    };
 
-#ifdef CCTBX_HAS_LAPACKE
-      typedef
-        lstbx::normal_equations::non_linear_ls_with_separable_scale_factor<
-        FloatType,
-        scitbx::matrix::rank_n_update>
-        NormalEquations_BLAS3;
-      def_init<NormalEquations_BLAS3, mainstream_shelx_weighting>(klass);
-      def_init<NormalEquations_BLAS3, unit_weighting            >(klass);
-      def_init<NormalEquations_BLAS3, sigma_weighting           >(klass);
-#endif
+    struct design_matrix_building {
+      typedef build_design_matrix<FloatType> wt;
 
-      klass
-        .def("observables", &wt::observables)
-        .def("f_calc", &wt::f_calc)
-        .def("weights", &wt::weights)
-        .def("design_matrix", &wt::design_matrix)
-        ;
-    }
+      static void wrap(char const *name) {
+        using namespace boost::python;
+        class_<wt> klass(name, no_init);
+        wrap_init<wt>(name, klass);
+        klass
+          .def("observables", &wt::observables)
+          .def("f_calc", &wt::f_calc)
+          .def("weights", &wt::weights)
+          .def("design_matrix", &wt::design_matrix)
+          ;
+      }
+    };
   };
 
   void wrap_least_squares() {
     using namespace boost::python;
-
-    normal_equation_building<
-      double,
+    typedef wrapper<double,
       structure_factors::direct::one_h::std_trigonometry<
         double,
-        structure_factors::direct::one_h::modulus_squared
-      >
-    >::wrap("build_normal_equations");
-
-    design_matrix_building<
-      double,
-      structure_factors::direct::one_h::std_trigonometry<
-        double,
-        structure_factors::direct::one_h::modulus_squared
-      >
-    >::wrap("build_design_matrix");
+        structure_factors::direct::one_h::modulus_squared>
+    > wrapper_t;
+    wrapper_t::normal_equation_building::wrap("build_normal_equations");
+    wrapper_t::design_matrix_building::wrap("build_design_matrix");
   }
 
 
