f4466b26a5ad959f9f0263b14f52b15227c6b753
Pascal Parois <pascal22p@parois.net>
2018-09-07 10:33:27 +0100

    fixed clutter

--- cctbx/adp_restraints/rigu.h
+++ cctbx/adp_restraints/rigu.h
@@ -15,11 +15,11 @@ using scitbx::mat3;
 using scitbx::sym_mat3;
 
   /** RIGU restraint
-   * Restrains U33, U13 and U23 of 2 adps expressed in a cartesian base along the bond of the 2 atoms.
-   * U33 is aligned along the bond
-   * See Thorn, A., Dittrich, B. & Sheldrick, G. M. (2012). Acta Cryst. A68, 448-451.
-   * and Parois, P., Arnold, J. & Cooper, R. (2018). J. Appl. Cryst. 51, 1059-1068.
-  */
+   *  Restrains U33, U13 and U23 of 2 adps expressed in a cartesian base
+   *  along the bond of the 2 atoms. U33 is aligned along the bond
+   *  See Thorn, A., et. al. (2012). Acta Cryst. A68, 448-451.
+   *  and Parois, P., et. al. (2018). J. Appl. Cryst. 51, 1059-1068.
+   */
 
   struct rigu_proxy
   {
@@ -67,35 +67,39 @@ using scitbx::sym_mat3;
       CCTBX_ASSERT(proxy.i_seqs[1] < params.sites_cart.size());
       init_delta(
         af::tiny<scitbx::vec3<double>, 2>(
-          params.sites_cart[proxy.i_seqs[0]], params.sites_cart[proxy.i_seqs[1]]),
+          params.sites_cart[proxy.i_seqs[0]],
+          params.sites_cart[proxy.i_seqs[1]]),
         af::tiny<scitbx::sym_mat3<double>, 2>(
-          params.u_cart[proxy.i_seqs[0]], params.u_cart[proxy.i_seqs[1]]));
+          params.u_cart[proxy.i_seqs[0]],
+          params.u_cart[proxy.i_seqs[1]]));
       calc_gradients();
     }
 
     //! weight * delta[i]**2.
     double
-    residual33() const { 
-      return weight * scitbx::fn::pow2(delta_33_); 
+    residual33() const {
+      return weight * scitbx::fn::pow2(delta_33_);
     }
     double
-    residual13() const { 
-      return weight * scitbx::fn::pow2(delta_13_); 
+    residual13() const {
+      return weight * scitbx::fn::pow2(delta_13_);
     }
     double
-    residual23() const { 
-      return weight * scitbx::fn::pow2(delta_23_); 
+    residual23() const {
+      return weight * scitbx::fn::pow2(delta_23_);
     }
     double
-    residual() const { 
-      return residual33() + residual13() + residual23(); 
+    residual() const {
+      return residual33() + residual13() + residual23();
     }
-    
-    //! Gradient of U_cart after linear transformation RM (basis aligned along bond) in respect to Uij_cart
+
+    /** Gradient of U_cart after linear transformation RM
+     *  (basis aligned along bond) in respect to Uij_cart
+     */
     scitbx::sym_mat3<double> grad_delta_n(int r) const {
       scitbx::sym_mat3<double> result;
 
-      //! partial derivatives against Uij are then just a row in dRUcart 
+      //! partial derivatives against Uij are then just a row in dRUcart
       for(int i = 0; i< 6; i++) {
         result[i] = dRUcart[r][i];
       }
@@ -184,9 +188,10 @@ using scitbx::sym_mat3;
       scitbx::sym_mat3<double> grad_u_cart;
       scitbx::sym_mat3<double> grad_u_star;
       std::size_t row_i;
-      int const indices[3] = {8, 6, 7}; //! derivatives to consider: U33, U13, U23
+      //! derivatives to consider: U33, U13, U23
+      int const indices[3] = {8, 6, 7}; // index in dRUcart
       double const deltas[3] = {delta_33_, delta_13_, delta_23_};
-      
+
       for (std::size_t k = 0; k < 3; k++) {
         grad_u_cart = grad_delta_n(indices[k]);
         scitbx::matrix::matrix_transposed_vector(
@@ -205,77 +210,85 @@ using scitbx::sym_mat3;
           linearised_eqns.deltas[row_i] = deltas[k];
         }
       }
-            
+
     }
 
-    double delta_33() { return delta_33_; } 
+    double delta_33() { return delta_33_; }
     double delta_13() { return delta_13_; }
     double delta_23() { return delta_23_; }
     double delta() { return delta_33_+delta_13_+delta_23_; }
 
     double weight;
-    
+
   protected:
     void init_delta(af::tiny<scitbx::vec3<double>, 2> const &sites,
       af::tiny<scitbx::sym_mat3<double>, 2> const &u_cart)
-    {      
-      
-      //! first step, calculating the rotation matrix to align U33 along the bond
+    {
+
+      /** Calculating the rotation matrix to align U33 along the bond
+       *  z-axis along the bond
+       */
       vec3<double> rot3 = sites[0] - sites[1];
-      
-      // Any perpendicular axis will do
+
+      //! Any perpendicular axis will do
       vec3<double> rot2;
       rot2[0] = rot3[2];
       rot2[1] = rot3[2];
       rot2[2] = -rot3[0]-rot3[1];
-    
+
       if(abs(rot2[0])+abs(rot2[1])+abs(rot2[1])<1e-4) {
         rot2[0] = -rot3[1]-rot3[2];
         rot2[1] = rot3[1];
         rot2[2] = rot3[1];
       }
-    
-      // Last axis to form a direct orthonormal basis
+
+      //! Last axis to form a direct orthonormal basis
       vec3<double> rot1 = rot2.cross(rot3);
 
       RM.set_row(0, rot1.normalize());
       RM.set_row(1, rot2.normalize());
       RM.set_row(2, rot3.normalize());
-    
-      //! Calculating Ucart in the new basis 
+
+      //! Calculating Ucart in the new basis
       mat3<double> tmp1(u_cart[0]);
       RUcart1 = (RM*tmp1)*RM.transpose();
       mat3<double> tmp2(u_cart[1]);
       RUcart2 = (RM*tmp2)*RM.transpose();
-    
+
       //! calculating the 3 deltas involved
       delta_33_ = RUcart1(2,2) - RUcart2(2,2);
       delta_13_ = RUcart1(0,2) - RUcart2(0,2);
       delta_23_ = RUcart1(1,2) - RUcart2(1,2);
-      
-      //! Update weight see Thorn, A., Dittrich, B. & Sheldrick, G. M. (2012). Acta Cryst. A68, 448-451
-      //! sqrt(p**2 + UeqA + UeqB) * d * p *sigma
+
+      /** Update weight see
+       *  Thorn, A., et. al. (2012). Acta Cryst. A68, 448-451
+       *  sqrt(p**2 + UeqA + UeqB) * d * p *sigma
+       */
       double d = rot3.length();
-      double const p = 0.5; 
+      double const p = 0.5;
       double UeqA = u_cart[0].trace()/3.0;
       double UeqB = u_cart[1].trace()/3.0;
       weight = weightfinal(weight, p, d, UeqA, UeqB);
 
     }
 
-    //! Calculate all the partial derivatives of the adp in the local cartesian coordinate system.
+    //! Calculate all the partial derivatives of the adp
+    //! in the local cartesian coordinate system.
     void calc_gradients() {
-      /* See Parois, P., Arnold, J. & Cooper, R. (2018). J. Appl. Cryst. 51, 1059-1068.
-       * for the detail on how it works
+      /** See Parois, et. al. (2018). J. Appl. Cryst. 51, 1059-1068.
+       *  for the detail on how it works
        *
-       * The U tensors are vectorized as 9 elements vectors column by column to simplify the calculations
-       * Operations like V = A U B translate then to vec(V) = (B^t @ A) U with @ the kronecker product
+       *  The U tensors are vectorized as 9 elements vectors
+       *  column by column to simplify the calculations.
+       *  Operations like V = A U B translate then to vec(V) =
+       *  (B^t @ A) U with @ the kronecker product
        */
-      
-      /** dUcart contains the 6 partial derivatives dU_cart/dUij_cart writen as vectors. 
+
+      /** dUcart contains the 6 partial derivatives dU_cart/dUij_cart
+       *  writen as vectors.
        *  The 6 columns vectors are then stacked column wise to form dUcart
        */
-      static const double dUcart[9][6] = {
+      static const int dUcart[9][6] = {
         //dU11 dU22 dU33 dU12 dU13 dU23
         { 1,   0,   0,   0,   0,   0}, // U11
         { 0,   0,   0,   1,   0,   0}, // U21
@@ -287,45 +300,50 @@ using scitbx::sym_mat3;
         { 0,   0,   0,   0,   0,   1}, // U32
         { 0,   0,   1,   0,   0,   0}, // U33
       };
-      
-      double kron[9][9];    
-      int i,j,k,l,startRow,startCol;
 
-      //! calulating the kronecker product    
-      for(i = 0; i < 3; i++){
-        for(j = 0; j < 3; j++){
-          startRow = i*3;
-          startCol = j*3;
-          for(k = 0; k < 3; k++){
-            for(l = 0; l < 3; l++){
+      double kron[9][9];
+
+      //! calulating the kronecker product
+      for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+          int startRow = i*3;
+          int startCol = j*3;
+          for(int k = 0; k < 3; k++){
+            for(int l = 0; l < 3; l++){
               kron[startRow+k][startCol+l] = RM(i,j)*RM(k,l);
             }
           }
         }
       }
-        
-      //! vec(dRUcart) = kron vec(dUcart). Derivatives of (RUcart = RM Ucart RM^t)
+
+      /** vec(dRUcart) = kron vec(dUcart).
+       *  Derivatives of (RUcart = RM Ucart RM^t)
+       */
       memset(&dRUcart[0][0], 0, 9*6*sizeof(dRUcart[0][0]));
-      for (i = 0; i < 9; i++) {
-        for (j = 0; j < 6; j++) {
-          for (k = 0; k < 9; k++) {
+      for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 6; j++) {
+          for (int k = 0; k < 9; k++) {
             dRUcart[i][j] += kron[i][k] * dUcart[k][j];
           }
         }
-      }       
+      }
     }
 
-    //! weights in RIGU depends on the distance between atoms, Ueq and a fixed parameter p
-    double weightfinal(double weight, double p, double d, double UeqA, double UeqB) {
+    /** weights in RIGU depends on the distance between atoms,
+     *  Ueqs and a fixed parameter p
+     */
+    double weightfinal(double weight, double p, double d,
+      double UeqA, double UeqB) const
+    {
       return p*p / ((p*p + UeqA + UeqB) * d*d) * weight;
     }
-    
+
     double delta_33_;
     double delta_13_;
     double delta_23_;
     mat3<double> RUcart1, RUcart2;
     mat3<double> RM;
-        
+
     double dRUcart[9][6];
   };
 
