f6b87e7a6573a08333faddef5fc5a7a1f4b3c1d4
Nicholas Sauter <nksauter@lbl.gov>
2018-09-16 09:55:12 -0700

    Fine tune the gaussian mosaicity test, add it to run_tests.

--- simtbx/nanoBragg/tst_gaussian_mosaicity.py
+++ simtbx/nanoBragg/tst_gaussian_mosaicity.py
@@ -4,6 +4,7 @@ from scitbx.matrix import sqr,col
 from simtbx.nanoBragg import nanoBragg
 import libtbx.load_env # possibly implicit
 import math
+from libtbx.test_utils import approx_equal
 
 class plotter:
   def __init__(self, tophat, normal):
@@ -48,11 +49,13 @@ class plotter2:  # compare the transformation of 001 with that of .57735,.57735,
 
     # columns plot the transformation of x, y, and z unit vectors
     rows = [tophat,normal]
+    differences = []
     for irow,dist in enumerate(rows):
       iaxes = axes[irow];
-      for icol, RLP in enumerate([(0,0,1), (0.57735, 0.57735, 0.57735)]):
+      cube_diag = math.sqrt(1./3) # 0.57735
+      for icol, RLP in enumerate([(0,0,1), (cube_diag, cube_diag, cube_diag)]):
         RLP = col(RLP)
-        print "%27s"%(str(RLP.elems)),"Vector length:%8.6f"%(RLP.length()),
+        print "(%7.5f %7.5f %7.5f)"%(RLP.elems),"Vector length:%8.6f"%(RLP.length()),
         axis = iaxes[icol]
         unit = RLP.normalize()
         seed = col((1,0,0))
@@ -67,6 +70,7 @@ class plotter2:  # compare the transformation of 001 with that of .57735,.57735,
           a2.append(newvec.dot(perm2)); a3.append(newvec.dot(perm3))
         rms = math.sqrt( flex.mean ( difference_vectors.dot(difference_vectors) ) )
         print "The rms difference is", rms
+        differences.append(rms)
         if plot:
           axis.plot (a2,a3,'r,')
           axis.set_aspect("equal")
@@ -74,8 +78,10 @@ class plotter2:  # compare the transformation of 001 with that of .57735,.57735,
           axis.set_xlim(-0.05,0.05)
           axis.set_ylim(-0.05,0.05)
 
-    if plot: plt.show()
+      assert approx_equal(differences[0],differences[1],eps=1e-04), \
+      "RMS mosaic distribution for axis vector and diagonal vector should be similar, as proposed by J Holton"
 
+    if plot: plt.show()
 
 class check_distributions:
   @staticmethod
@@ -88,11 +94,12 @@ class check_distributions:
     return angle_deg
 
 MOSAIC_SPREAD = 2.0 # top hat half width rotation in degrees
+SAMPLE_SIZE = 90000
 
 def run_sim2smv(fileout):
   SIM = nanoBragg(detpixels_slowfast=(1000,1000),pixel_size_mm=0.1,Ncells_abc=(5,5,5),verbose=0)
-  SIM.mosaic_domains = 10000
-  SIM.mosaic_spread_deg = MOSAIC_SPREAD
+  SIM.mosaic_domains = SAMPLE_SIZE
+  SIM.mosaic_spread_deg = MOSAIC_SPREAD # apparently this is half width
   SIM.distance_mm=100 # this triggers the generation of mosaic distribution
   UMAT_th = SIM.get_mosaic_blocks() # extract top-hat distributed U-mats
 
@@ -103,13 +110,15 @@ def run_sim2smv(fileout):
   assert max_angle > 0.99 * MOSAIC_SPREAD # insist that max angle is near the limit we gave it
   rms_angle = math.sqrt(flex.mean(th_angles*th_angles))
   print rms_angle
+  assert rms_angle < MOSAIC_SPREAD
 
-  import scitbx # computer an array of normally-distributed U-mats into the simulator
+  import scitbx # compute an array of normally-distributed U-mats into the simulator
   UMAT_nm = flex.mat3_double()
-  mersenne_twister = flex.mersenne_twister(seed=0)
+  mersenne_twister = flex.mersenne_twister(seed=0) # set seed, get reproducible results
+  scitbx.random.set_random_seed(4321) # set seed, get reproducibe results
   rand_norm = scitbx.random.normal_distribution(mean=0, sigma=rms_angle*math.pi/180.)
   g = scitbx.random.variate(rand_norm)
-  mosaic_rotation = g(10000)
+  mosaic_rotation = g(SAMPLE_SIZE)
   for m in mosaic_rotation:
     site = col(mersenne_twister.random_double_point_on_sphere())
     UMAT_nm.append( site.axis_and_angle_as_r3_rotation_matrix(m,deg=False) )
@@ -120,10 +129,20 @@ def run_sim2smv(fileout):
   # get them back
   new_UMAT_nm = SIM.get_mosaic_blocks()
 
+  #double check that simulator does not alter the UMATs
+  for iumat in range(0,SAMPLE_SIZE,100):
+
+
+
+
+    assert UMAT_nm[iumat]==new_UMAT_nm[iumat]
+
   #sanity check on the gaussian distribution
   nm_angles = check_distributions.get_angular_rotation(new_UMAT_nm)
   nm_rms_angle = math.sqrt(flex.mean(nm_angles*nm_angles))
   print nm_rms_angle
+  assert approx_equal(rms_angle,nm_rms_angle,eps=1e-03), \
+  "The top hat and gaussian models should have similar standard deviations"
 
   return UMAT_th, new_UMAT_nm
 
