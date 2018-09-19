93a4e92407ea7a0bc0c205d2c1e467366ad2dd4f
pcxod <oleg_dolomanov@hotmail.com>
2018-09-19 14:03:09 +0100

    trying to automatically initialise openblas on import of fast_linalg

--- fast_linalg/__init__.py
+++ fast_linalg/__init__.py
@@ -1,3 +1,19 @@
+import sys
 import boost.python
 ext = boost.python.import_ext('fast_linalg_ext')
 env = ext.env
+if not env.initialised:
+  if sys.platform[:3] == "win":
+    lib_path = "openblas.dll"
+  else:
+    lib_path = "libopenblas.so"
+  print("Trying OpenBlas at: %s" %lib_path)
+  try:
+    env.initialise(lib_path)
+    if env.initialised:
+      print("Successfully initialised OpenBlas:")
+      print(env.build_config)
+    else:
+      print("Located OpenBlas but could not initialise")
+  except:
+    print("Could not initialise OpenBlas")
