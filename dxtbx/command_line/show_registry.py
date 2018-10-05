7e167d323909c6cdce952c73e13c4bbace36cc01
Aaron Brewster <asbrewster@lbl.gov>
2018-10-05 09:16:41 -0700

    Rejigger dxtbx.show_registry to accept a file name as an optional argument. If provided, only show the format classes that understand that argument.

--- dxtbx/command_line/show_registry.py
+++ dxtbx/command_line/show_registry.py
@@ -1,15 +1,37 @@
 from __future__ import absolute_import, division, print_function
 from dxtbx.format.Registry import Registry
 Registry.setup()
+import sys
 
-print ("Showing hierarchy of classes in the dxtbx registry. The root classes are shown with depth of 1, and subclasses are shown indented and with a higher depth number.")
-print ()
-print ("Depth  Class name")
-print ("    0  Format")
-
-def print_class(classobj, depth = 1):
-    print ("% 5d"%depth, "  " * depth, classobj.__name__)
+def print_class(classobj, filename = None, depth = 1):
+    if filename is None:
+      print ("% 5d"%depth, "  " * depth, classobj.__name__)
+    else:
+      try:
+        ok = classobj.understand(filename)
+      except Exception:
+        ok = False
+      if ok:
+        print ("% 5d"%depth, "  " * depth, classobj.__name__)
+      else:
+        return
     for child in classobj._children:
-        print_class(child, depth + 1)
+        print_class(child, filename, depth + 1)
 
-for classobj in Registry._formats: print_class(classobj)
+def show_registry(filename = None):
+  if filename is None:
+    extrabit = ""
+  else:
+    extrabit = " that understand image %s"%filename
+  print ("Showing hierarchy of classes in the dxtbx registry%s. The root classes are shown with depth of 1, and subclasses are shown indented and with a higher depth number."%extrabit)
+  print ()
+  print ("Depth  Class name")
+  print ("    0  Format")
+
+  for classobj in Registry._formats: print_class(classobj, filename)
+
+if __name__ == "__main__":
+  if len(sys.argv) == 2:
+    show_registry(sys.argv[1])
+  else:
+    show_registry()
