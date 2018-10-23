bce5d1b34bec514f1d9a45dca5b5202c681873ea
dcliebschner <dcliebschner@lbl.gov>
2018-10-23 10:54:16 -0700

    Activate test.

--- mmtbx/run_tests.py
+++ mmtbx/run_tests.py
@@ -310,6 +310,8 @@ general_tests = [
   "$D/regression/tst_map_model_cc.py",
   "$D/regression/tst_model_map.py",
   #
+  "$D/regression/tst_validate_ligands.py"
+  #
   "$D/regression/tst_cis_trans_peptide_link.py",
   "$D/regression/tst_apply_cif_restraints.py",
   "$D/regression/tst_multi_residue_class.py",
