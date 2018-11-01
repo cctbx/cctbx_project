1fefa6a45aa52487e8d1f74644776525b6167242
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-11-01 09:57:51 +0000

    import code from libtbx instead

--- dxtbx/tests/test_python3_violations.py
+++ dxtbx/tests/test_python3_violations.py
@@ -1,11 +1,10 @@
 from __future__ import absolute_import, division, print_function
+
+import pytest
+
 def test_find_python3_violations():
-  import pytest
-  try:
-    import dials.test.python3_regression as py3test
-  except ImportError:
-    pytest.skip('DIALS required for this test')
   import dxtbx
+  import libtbx.test_utils.python3_regression as py3test
   result = py3test.find_new_python3_incompatible_code(dxtbx)
   if result is None:
     pytest.skip('No python3 interpreter available')
