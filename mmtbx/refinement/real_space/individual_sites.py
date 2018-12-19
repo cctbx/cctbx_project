3eddcfa166d91bf748f5a94bfdbc7a8c10e12827
Gydo van Zundert <gydo.vanzundert@schrodinger.com>
2018-12-19 14:17:43 -0500

    Use get_source method to identify geometry manager.

--- mmtbx/refinement/real_space/individual_sites.py
+++ mmtbx/refinement/real_space/individual_sites.py
@@ -350,7 +350,7 @@ class box_refinement_manager(object):
 
       # When using the Schrodinger force field, move the whole structure as the
       # selected atoms are environment aware.
-      if hasattr(geo_box, 'schrodinger') and geo_box.schrodinger.use_schrodinger:
+      if geobox.get_source() == 'SCHRODINGER':
         geo_box.shift_cart(box.shift_cart)
 
       rsr_simple_refiner = simple(
@@ -377,7 +377,7 @@ class box_refinement_manager(object):
         iselection, sites_cart_refined)
       self.xray_structure.set_sites_cart(sites_cart_moving)
       self.sites_cart = self.xray_structure.sites_cart()
-      if hasattr(geo_box, 'schrodinger') and geo_box.schrodinger.use_schrodinger:
+      if geobox.get_source() == 'SCHRODINGER':
         geo_box.shift_cart(shift_back)
     else: # NCS constraints are present
       # select on xrs, grm, ncs_groups
