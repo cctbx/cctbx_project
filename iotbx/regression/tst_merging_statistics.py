4489dd45cf291d1639b09772613c2af3b60ec7f6
rjgildea <richard.gildea@diamond.ac.uk>
2019-01-11 12:02:15 +0000

    fix test

--- iotbx/regression/tst_merging_statistics.py
+++ iotbx/regression/tst_merging_statistics.py
@@ -35,10 +35,10 @@ def exercise(debug=False):
   assert ("""  1.81   1.74  12528   2073    6.04  97.05    1449.2     5.2    0.252    0.275    0.110   0.967   0.281""" in out.getvalue()), out.getvalue()
   cif_block = result.as_cif_block()
   assert "_reflns_shell" in cif_block
-  assert approx_equal(float(cif_block["_reflns.pdbx_Rpim_I_all"]), result.overall.r_pim)
+  assert approx_equal(float(cif_block["_reflns.pdbx_Rpim_I_obs"]), result.overall.r_pim)
   assert approx_equal(float(cif_block["_reflns.pdbx_CC_half"]), result.overall.cc_one_half)
   assert approx_equal(
-    flex.int(cif_block["_reflns_shell.number_measured_all"]),
+    flex.int(cif_block["_reflns_shell.number_measured_obs"]),
     [15737, 15728, 15668, 15371, 14996, 14771, 13899, 13549, 13206, 12528])
   assert "_reflns_shell.pdbx_CC_half" in cif_block
   remark_200 = result.as_remark_200(wavelength=0.9792).splitlines()
@@ -94,7 +94,7 @@ def exercise(debug=False):
   assert os.path.exists("merging_stats.mmcif")
   cif = iotbx.cif.reader("merging_stats.mmcif").model()
   assert approx_equal(float(cif["test"]["_reflns.pdbx_CC_half"]), 0.997601585888)
-  assert list(cif["test"]["_reflns_shell.number_obs"]) == ['15737', '15728', '15668', '15371', '14996', '14771', '13899', '13549', '13206', '12528']
+  assert list(cif["test"]["_reflns_shell.number_measured_obs"]) == ['15737', '15728', '15668', '15371', '14996', '14771', '13899', '13549', '13206', '12528']
   # these should crash
   args2 = list(args[:-1]) + ["high_resolution=15", "low_resolution=2.5"]
   try :
