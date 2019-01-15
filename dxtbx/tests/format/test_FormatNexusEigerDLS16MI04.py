0a00d206009b4f16dcbf33b6accd401f5331d79b
rjgildea <richard.gildea@diamond.ac.uk>
2019-01-15 10:09:34 +0000

    add failing test for #280

--- dxtbx/tests/format/test_FormatNexusEigerDLS16MI04.py
+++ dxtbx/tests/format/test_FormatNexusEigerDLS16MI04.py
@@ -92,3 +92,32 @@ def test_screening():
   assert len(imagesets) == 3
   assert imageset.get_format_class() == FormatNexusEigerDLS16MI04
 
+@pytest.mark.xfail
+def test_units():
+  master_h5 = '/dls/i04/data/2019/cm23004-1/20190114/Eiger/grid/Se_Thaum/Se_Thaum_12/Se_Thaum_12_2_master.h5'
+  assert FormatNexusEigerDLS16MI04.understand(master_h5)
+
+  datablocks = DataBlockFactory.from_filenames([master_h5])
+  imageset = datablocks[0].extract_imagesets()[0]
+  assert imageset.get_format_class() == FormatNexusEigerDLS16MI04
+
+  detector = imageset.get_detector()
+  gonio = imageset.get_goniometer()
+  scan = imageset.get_scan()
+  beam = imageset.get_beam()
+
+  panel = detector[0]
+  assert panel.get_pixel_size() == (0.075,0.075)
+  assert panel.get_fast_axis() == (1,0,0)
+  assert panel.get_slow_axis() == (0,-1,0)
+  # XXX Need to check and update expected values here, however the ones
+  # dxtbx is currently generating from the file are very wrong
+  assert panel.get_origin() == pytest.approx(
+    (-167.44717577120824, 172.46833023184868, -350.0))
+  assert panel.get_distance() == 350
+
+  assert scan is None
+
+  assert beam.get_wavelength() == pytest.approx(0.979499)
+  assert beam.get_s0() == pytest.approx((0,0,-1), abs=5e-2)
+  # XXX should check beam centre in mm, px
