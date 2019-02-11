10f4a33e49941b3880134a7fccd9ad2a8dcb2d4d
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2019-02-11 08:58:06 -0800

    syntax error correction

--- iotbx/run_tests.py
+++ iotbx/run_tests.py
@@ -37,6 +37,8 @@ tst_list = (
   "$D/pdb/tst_atom_name_interpretation.py",
   "$D/pdb/tst_extract_rfactors_resolutions_sigma.py",
   "$D/pdb/tst_pdb.py",
+  "$D/pdb/modified_aa_names.py",
+  "$D/pdb/modified_rna_dna_names.py",
   "$D/regression/secondary_structure/tst_sheet.py",
   "$D/regression/secondary_structure/tst_annotation.py",
   "$D/pdb/secondary_structure.py",
