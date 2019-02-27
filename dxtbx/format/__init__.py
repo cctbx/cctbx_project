5be6962c08b140dd67ec7e85737341e97af78d6a
Nicholas Devenish <ndevenish@gmail.com>
2019-02-27 11:52:05 +0000

    Remove temporary support for old HDF5 plugin locations

--- dxtbx/format/__init__.py
+++ dxtbx/format/__init__.py
@@ -4,11 +4,6 @@ from __future__ import absolute_import, division
 def setup_hdf5_plugin_path():
   import os, libtbx.load_env
   plugin_path = libtbx.env.under_base('lib/plugins')
-  # FIXME Temporarily work with out-of-date base installations.
-  #       - Remove after Dec/2018
-  if not os.path.isdir(plugin_path):
-    plugin_path = libtbx.env.under_base('lib')
-  # END OF FIXME
   try:
     os.environ['HDF5_PLUGIN_PATH'] = plugin_path + ":" + os.environ['HDF5_PLUGIN_PATH']
   except Exception:
