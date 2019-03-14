cfc3439c5d5d03bf9b19991cc3d16abbd2421b1f
chrissciwilliams <christopher.sci.williams@gmail.com>
2019-03-14 19:21:55 -0400

    updated ramalyze to use program template

--- mmtbx/command_line/ramalyze.py
+++ mmtbx/command_line/ramalyze.py
@@ -1,136 +1,45 @@
+from __future__ import division, print_function
 # LIBTBX_SET_DISPATCHER_NAME phenix.ramalyze
 # LIBTBX_SET_DISPATCHER_NAME molprobity.ramalyze
 # LIBTBX_PRE_DISPATCHER_INCLUDE_SH export PHENIX_GUI_ENVIRONMENT=1
 
-from __future__ import division
-import mmtbx.validation.ramalyze
-import iotbx.phil
-from libtbx.utils import Usage, Sorry
-import os.path
-import os, sys
+import sys
 
-# Pavel's style:
-# plot=True show_labels=False markerfacecolor=yellow markeredgecolor=red
-def get_master_phil():
-  return iotbx.phil.parse(input_string="""
-    include scope mmtbx.validation.molprobity_cmdline_phil_str
-    plot = False
-      .type = bool
-      .help = Create graphics of plots (if Matplotlib is installed)
-    show_labels = True
-      .type = bool
-      .help = Show labels on outlier residues
-    point_style = 'bo'
-      .type = str
-      .help = choose style of points, use matplotlib format from e.g. here: \
-        https://matplotlib.org/api/_as_gen/matplotlib.axes.Axes.plot.html \
-        very small is ',', little bigger is '.'
-    markersize=3
-      .type=int
-    markerfacecolor = white
-      .type = str
-    markeredgecolor="black"
-      .type = str
-    show_filling = True
-      .type = bool
-    show_contours = True
-      .type = bool
-    dpi=100
-      .type=int
-    wxplot = False
-      .type = bool
-      .help = Display interactive plots (requires wxPython and Matplotlib)
-    model_list = None
-      .type = str
-      .help = Comma separated file list to accumulate onto one plot
-    output_prefix = None
-      .type = str
-      .help = prefix for outputted plots (if plot=True)
-""", process_includes=True)
-prog = os.getenv('LIBTBX_DISPATCHER_NAME')
-usage_string = """
-%(prog)s file.pdb [params.eff] [options ...]
+from iotbx.cli_parser import CCTBXParser
+from libtbx.utils import multi_out, show_total_time, null_out
+from mmtbx.programs import ramalyze
 
-Options:
+#=============================================================================
+def run(args):
 
-  model=input_file      input PDB file
-  outliers_only=False   only print outliers
-  verbose=False         verbose text output
-  plot=False            Create graphics of plots (if Matplotlib is installed)
+  # create parser
+  logger = null_out()
+  logger2 = multi_out()
+  logger2.register('stdout', sys.stdout)
 
-Example:
+  parser = CCTBXParser(
+    program_class=ramalyze.Program,
+    logger=logger2)
+  namespace = parser.parse_args(sys.argv[1:])
 
-  %(prog)s model=1ubq.pdb outliers_only=True
-""" % locals()
+  # start program
+  print('Starting job', file=logger)
+  print('='*79, file=logger)
+  task = ramalyze.Program(
+    parser.data_manager, parser.working_phil.extract(), logger=logger2)
 
-def run(args, out=sys.stdout, quiet=False):
-  cmdline = iotbx.phil.process_command_line_with_files(
-    args=args,
-    master_phil=get_master_phil(),
-    pdb_file_def="model",
-    usage_string=usage_string)
-  params = cmdline.work.extract()
-  if (params.model is None and params.model_list is None):
-    raise Usage(usage_string)
-  if params.model:
-    models = [params.model]
-  elif params.model_list:
-    if os.path.isfile(params.model_list):
-      with open(params.model_list, 'r') as f:
-        models = f.read().split('\n')
-        models = [m for m in models if m != ""]
-    else:
-      models = params.model_list.split(',')
-    params.verbose=False
-    params.model=models[0]
-  results = []
-  for model in models:
-    if not os.path.isfile(model) and params.model_list:
-      print "Cannot find '%s', skipping." % model
-      continue
-    pdb_in = cmdline.get_file(model, force_type="pdb")
-    hierarchy = pdb_in.file_object.hierarchy
-    hierarchy.atoms().reset_i_seq()
-    result = mmtbx.validation.ramalyze.ramalyze(
-      pdb_hierarchy=hierarchy,
-      show_errors=None,
-      outliers_only=params.outliers_only,
-      out=out,
-      quiet=quiet)
-    results.append(result)
-    if params.model_list:
-      print '\nmodel  : %s' % model
-      result.show_summary()
-  # combine
-  result = results[0]
-  for i in range(1,len(results)):
-    result += results[i]
-  if params.verbose:
-    result.show_old_output(out=out, verbose=True)
-  if params.plot :
-    plot_file_base = params.output_prefix
-    if plot_file_base is None:
-      plot_file_base = os.path.splitext(os.path.basename(params.model))[0]
-    result.write_plots(
-        plot_file_base=plot_file_base,
-        out=out,
-        show_labels=params.show_labels,
-        point_style=params.point_style,
-        markerfacecolor=params.markerfacecolor,
-        show_filling=params.show_filling,
-        show_contours=params.show_contours,
-        dpi=params.dpi,
-        markeredgecolor=params.markeredgecolor,
-        markersize=params.markersize)
-  if params.wxplot :
-    try :
-      import wxtbx.app
-    except ImportError, e :
-      raise Sorry("wxPython not available.")
-    else :
-      app = wxtbx.app.CCTBXApp(0)
-      result.display_wx_plots()
-      app.MainLoop()
+  # validate inputs
+  task.validate()
 
-if (__name__ == "__main__"):
+  # run program
+  task.run()
+
+  # stop timer
+  print('', file=logger)
+  print('='*79, file=logger)
+  print('Job complete', file=logger)
+  show_total_time(out=logger)
+
+# =============================================================================
+if __name__ == '__main__':
   run(sys.argv[1:])
---
+++ mmtbx/programs/ramalyze.py
@@ -0,0 +1,122 @@
+from __future__ import division, print_function
+
+import os
+from mmtbx.validation.ramalyze import ramalyze
+from libtbx.program_template import ProgramTemplate
+try:
+  from phenix.program_template import ProgramTemplate
+except ImportError:
+  pass
+from libtbx.utils import Sorry
+
+class Program(ProgramTemplate):
+  prog = os.getenv('LIBTBX_DISPATCHER_NAME')
+  description="""
+  %(prog)s file.pdb [params.eff] [options ...]
+
+Options:
+
+  model=input_file      input PDB file
+  outliers_only=False   only print outliers
+  verbose=False         verbose text output
+  plot=False            Create graphics of plots (if Matplotlib is installed)
+
+Example:
+
+  %(prog)s model=1ubq.pdb outliers_only=True
+""" % locals()
+
+  master_phil_str = """
+    plot = False
+      .type = bool
+      .help = Create graphics of plots (if Matplotlib is installed)
+    show_labels = True
+      .type = bool
+      .help = Show labels on outlier residues
+    point_style = 'bo'
+      .type = str
+      .help = choose style of points, use matplotlib format from e.g. here: \
+        https://matplotlib.org/api/_as_gen/matplotlib.axes.Axes.plot.html \
+        very small is ',', little bigger is '.'
+    markersize=3
+      .type=int
+    markerfacecolor = white
+      .type = str
+    markeredgecolor="black"
+      .type = str
+    show_filling = True
+      .type = bool
+    show_contours = True
+      .type = bool
+    dpi=100
+      .type=int
+    wxplot = False
+      .type = bool
+      .help = Display interactive plots (requires wxPython and Matplotlib)
+    outliers_only = False
+      .type = bool
+      .help = "Only display outliers"
+    verbose = True
+      .type = bool
+      .help = '''Verbose'''
+    output_prefix = None
+      .type = str
+      .help = prefix for outputted plots (if plot=True)
+"""
+  datatypes = ['model','phil']
+  known_article_ids = ['molprobity']
+
+  def validate(self):
+    self.data_manager.has_models(raise_sorry=True)
+
+  def run(self):
+    results = []
+    for model_name in self.data_manager.get_model_names():
+      # tst geometry restraints manager
+      model = self.data_manager.get_model(model_name)
+      grm = model.get_restraints_manager()
+      print(grm)
+      # end tst
+      hierarchy = self.data_manager.get_model(model_name).get_hierarchy()
+      hierarchy.atoms().reset_i_seq()
+      result = ramalyze(
+        pdb_hierarchy=hierarchy,
+        show_errors=None,
+        outliers_only=self.params.outliers_only,
+        out=self.logger,
+        quiet=False)
+      results.append(result)
+      if len(self.data_manager.get_model_names()) > 1:
+        self.params.verbose=False
+        print('\nmodel : %s' % model_name, file=self.logger)
+    # combine models
+    result = results[0]
+    for i in range(1,len(results)):
+      result += results[i]
+    if self.params.verbose:
+      result.show_old_output(out=self.logger, verbose=True)
+    if self.params.plot :
+      plot_file_base = self.params.output_prefix
+      if plot_file_base is None:
+        plot_file_base = os.path.splitext(os.path.basename(self.data_mananger.get_model_names()[0]))[0]
+      result.write_plots(
+          plot_file_base=plot_file_base,
+          out=self.logger,
+          show_labels=self.params.show_labels,
+          point_style=self.params.point_style,
+          markerfacecolor=self.params.markerfacecolor,
+          show_filling=self.params.show_filling,
+          show_contours=self.params.show_contours,
+          dpi=self.params.dpi,
+          markeredgecolor=self.params.markeredgecolor,
+          markersize=self.params.markersize)
+    if self.params.wxplot :
+      try :
+        import wxtbx.app
+      except ImportError, e :
+        raise Sorry("wxPython not available.")
+      else :
+        app = wxtbx.app.CCTBXApp(0)
+        result.display_wx_plots()
+        app.MainLoop()
+
