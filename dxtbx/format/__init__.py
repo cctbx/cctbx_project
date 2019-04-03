e96d385fcc63339c8dbaa896936bfd9877a68d00
Nicholas Devenish <ndevenish@gmail.com>
2019-04-03 14:41:11 +0100

    Use os.pathsep for HDF5 plugin path
    Makes it slightly more platform-compatible (possible: related to
    issues CCP4 has been having with plugins on windows?)

--- dxtbx/format/__init__.py
+++ dxtbx/format/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division
 
+import os
+
 # Pick up Dectris's LZ4 and bitshuffle compression plugins
 def setup_hdf5_plugin_path():
     import os, libtbx.load_env
@@ -7,7 +9,7 @@ def setup_hdf5_plugin_path():
     plugin_path = libtbx.env.under_base("lib/plugins")
     try:
         os.environ["HDF5_PLUGIN_PATH"] = (
-            plugin_path + ":" + os.environ["HDF5_PLUGIN_PATH"]
+            plugin_path + os.pathsep + os.environ["HDF5_PLUGIN_PATH"]
         )
     except Exception:
         os.environ["HDF5_PLUGIN_PATH"] = plugin_path
