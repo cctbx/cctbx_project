593a5aa636259d31d98194e9b1ffba955b6c2176
Nicholas Devenish <ndevenish@gmail.com>
2019-04-04 10:22:50 +0100

    Clean up format/init import/doc

--- dxtbx/format/__init__.py
+++ dxtbx/format/__init__.py
@@ -2,11 +2,15 @@ from __future__ import absolute_import, division
 
 import os
 
-# Pick up Dectris's LZ4 and bitshuffle compression plugins
-def setup_hdf5_plugin_path():
-    import os, libtbx.load_env
+import libtbx.load_env
 
-    plugin_path = libtbx.env.under_base(os.path.join("lib","plugins"))
+
+def setup_hdf5_plugin_path():
+    """Sets up the plugin path for HDF5.
+
+    Pick up Dectris's LZ4 and bitshuffle compression plugins
+    """
+    plugin_path = libtbx.env.under_base(os.path.join("lib", "plugins"))
     try:
         os.environ["HDF5_PLUGIN_PATH"] = (
             plugin_path + os.pathsep + os.environ["HDF5_PLUGIN_PATH"]
