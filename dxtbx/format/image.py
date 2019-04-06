a7090ae46fc60f81883c77836954ffeafdf9790d
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-04-06 02:06:15 +0100

    Move dxtbx to new repository
    The new dxtbx repository lives at https://github.com/cctbx/dxtbx
    
    To migrate an existing developer installation follow the instructions in
    https://github.com/cctbx/cctbx_project/blob/master/dxtbx/README.md
    
    For more information on the background see #310

--- dxtbx/format/image.py
+++
@@ -1,2 +0,0 @@
-from __future__ import division
-from dxtbx_format_image_ext import *
--- dxtbx/tests/model/test_scan.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-
-def test_scan_to_string_does_not_crash_on_empty_scan():
-    import dxtbx.model
-
-    print(dxtbx.model.Scan())
