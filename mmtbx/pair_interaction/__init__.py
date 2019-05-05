19a1b2104893ecdbe39a3f52d9998d5d8246569c
Billy K. Poon <bkpoon@lbl.gov>
2019-05-04 17:28:30 -0700

    mmtbx: fix extension name

--- mmtbx/pair_interaction/__init__.py
+++ mmtbx/pair_interaction/__init__.py
@@ -2,5 +2,5 @@ from __future__ import division
 import cctbx.array_family.flex # import dependency
 
 import boost.python
-ext = boost.python.import_ext("mmtbx_interaction_ext")
-from mmtbx_interaction_ext import *
+ext = boost.python.import_ext("mmtbx_pair_interaction_ext")
+from mmtbx_pair_interaction_ext import *
