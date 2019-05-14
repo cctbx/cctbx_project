6069c0e315a6211bb97bb952692b05f1b3f80c30
dcliebschner <dcliebschner@lbl.gov>
2019-05-14 14:31:06 -0700

    Use import flex

--- mmtbx/validation/validate_ligands.py
+++ mmtbx/validation/validate_ligands.py
@@ -6,7 +6,7 @@ import mmtbx.model
 import cctbx.geometry_restraints.nonbonded_overlaps as nbo
 from cctbx import adptbx
 from iotbx import phil
-#from cctbx.array_family import flex
+from cctbx.array_family import flex
 from libtbx import group_args
 from libtbx.str_utils import make_sub_header
 from libtbx.utils import null_out
