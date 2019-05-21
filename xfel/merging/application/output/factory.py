6bca89413c147415b5caf22135c9d9b386ea4981
RBolotovsky <RBolotovsky@lbl.gov>
2019-05-21 12:16:10 -0700

    Output more detailed intensity statistics, create new worker for output, use os.path.join instead of '/'.

---
+++ xfel/merging/application/output/factory.py
@@ -0,0 +1,9 @@
+from __future__ import division
+from xfel.merging.application.output.output import output
+from xfel.merging.application.worker import factory as factory_base
+
+class factory(factory_base):
+  """ Factory class for outputting merged multiple measurements of symmetry-reduced hkl's. """
+  @staticmethod
+  def from_parameters(params, additional_info=None):
+    return [output(params)]
---
+++ xfel/merging/application/statistics/intensity_histogram.py
@@ -0,0 +1,39 @@
+from __future__ import division
+from xfel.merging.application.worker import worker
+from dials.array_family import flex
+from six.moves import cStringIO as StringIO
+
+class intensity_histogram(worker):
+  '''Builds a histogram of reflection intensities'''
+
+  def __repr__(self):
+    return 'Intensity histogram'
+
+  def run(self, experiments, reflections):
+    comm = self.mpi_helper.comm
+    MPI = self.mpi_helper.MPI
+    if self.mpi_helper.rank == 0:
+      self.logger.log_step_time("INTENSITY_HISTOGRAM")
+      self.histogram(reflections['intensity'])
+      self.logger.log_step_time("INTENSITY_HISTOGRAM", True)
+
+    return experiments, reflections
+
+  def histogram(self, data):
+    from matplotlib import pyplot as plt
+    nslots = 100
+    histogram = flex.histogram(
+                               data=data,
+                               n_slots=nslots)
+    out = StringIO()
+    histogram.show(f=out, prefix="    ", format_cutoffs="%6.2f")
+    self.logger.main_log(out.getvalue() + '\n' + "Total: %d"%data.size() + '\n')
+
+    if False:
+      fig = plt.figure()
+      plt.bar(histogram.slot_centers(), histogram.slots(), align="center", width=histogram.slot_width())
+      plt.show()
+
+if __name__ == '__main__':
+  from xfel.merging.application.worker import exercise_worker
+  exercise_worker(intensity_histogram)
