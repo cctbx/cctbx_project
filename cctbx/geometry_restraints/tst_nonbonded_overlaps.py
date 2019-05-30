f5f370bf545c6db349e3488df87b9da21da425d7
Billy K. Poon <bkpoon@lbl.gov>
2019-05-29 22:59:31 -0700

    py3: fix new print and remove unused import

--- cctbx/geometry_restraints/tst_nonbonded_overlaps.py
+++ cctbx/geometry_restraints/tst_nonbonded_overlaps.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, division, print_function
-import mmtbx.monomer_library.pdb_interpretation as pdb_inter
 from cctbx.geometry_restraints.nonbonded_overlaps import compute
 import cctbx.geometry_restraints.nonbonded_overlaps as nbo
 import mmtbx.validation.clashscore as mvc
