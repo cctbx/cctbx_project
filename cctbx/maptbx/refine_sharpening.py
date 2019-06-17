cd9c87ad4580dc942fd95bc43eee82fb1a54a2cf
terwill <tterwilliger@newmexicoconsortium.org>
2019-06-17 14:58:45 -0700

    Print out average CC

--- cctbx/maptbx/refine_sharpening.py
+++ cctbx/maptbx/refine_sharpening.py
@@ -524,7 +524,7 @@ def calculate_fsc(si=None,
     print("\nFraction complete (%5.2f) is less than minimum (%5.2f)..." %(
       fraction_complete,min_fraction_complete) + "\nSkipping scaling", file=out)
     target_scale_factors=flex.double(target_scale_factors.size()*(1.0,))
-
+  print ("\nAverage CC: %.3f" %(cc_list.min_max_mean().mean),file=out)
   print("\nScale factors vs resolution:", file=out)
   print("Note 1: CC* estimated from sqrt(2*CC/(1+CC))", file=out)
   print("Note 2: CC estimated by fitting (smoothing) for values < %s" %(cc_cut), file=out)
