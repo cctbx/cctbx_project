5567a0061e3de80e1bccd39384047f15b45a6cca
Pavel <pafonine@lbl.gov>
2019-08-07 16:59:17 -0700

    On Mark's behalf.. Getting rid of Java dependency of qr code (work in progress)

--- mmtbx/pair_interaction/pair_interaction.py
+++ mmtbx/pair_interaction/pair_interaction.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, division, print_function
+from __future__ import division
 import os
 import time
 import math
@@ -10,9 +10,8 @@ import numpy as np
 from qrefine.super_cell import expand
 from libtbx import easy_pickle
 from libtbx.utils import Sorry
-import six
+
 import boost.python
-from six.moves import range
 ext = boost.python.import_ext("mmtbx_pair_interaction_ext")
 
 dat_path = libtbx.env.find_in_repositories("qrefine")
@@ -124,39 +123,41 @@ def run(ph, core=None):
     #
     interactions=get_interactions(sub_ph,atom_in_residue,silva_type='sedd',core=core)
     new_core=[]
-    print(("1. interactions got: ",len(interactions)))
+    print("1. interactions got: ",len(interactions))
     for pair in interactions:
       if(len(set(pair).intersection(set(core)))>0):
         new_core+=pair
     new_core=list(set(new_core)|set(core))
-    print(("new core:",new_core))
-    interaction_mols=new_core
+    print("new core:",new_core)
     #
     interactions=get_interactions(sub_ph, atom_in_residue, silva_type='dori', core=new_core)
-    print(("2. interactions got: ",len(interactions)))
-    interaction_mols=[]
+    print("2. interactions got: ",len(interactions))
+    interaction_mols=new_core
     for item in interactions:
-      interaction_mols=interaction_mols+list(item)
+      if(len(set(item).intersection(set(new_core)))>0):
+        interaction_mols=interaction_mols+list(item)
     interaction_atoms=[]
     for i in range(len(core_atoms)):
       core_atoms[i]=core_atoms[i].serial_as_int()
-    print(interaction_mols)
+    print("3. interaction mols:",set(interaction_mols))
     for mol_id in interaction_mols:
       ams=[a.serial_as_int() for a in atoms_group_dict[mol_id]]
       interaction_atoms+=ams
-    return(core_atoms, interaction_atoms, interaction_mols)
+
+    return(list(set(core_atoms)), list(set(interaction_atoms)), list(set(interaction_mols)))
   else:
     return get_interactions(ph, atom_in_residue)
 
 def get_interactions(ph, atom_in_residue, step_size=0.5*A2B, silva_type='dori',
       core=None):
+  if(silva_type=='sedd'):step_size=0.3*A2B
   t0=time.time()
   atoms = ph.atoms()
-  print(("num atoms:",len(atoms)))
+  print("num atoms:",len(atoms))
   elements = atoms.extract_element()
   eldict = dict(enumerate(set(elements)))
   tmp = {}
-  for k, v in six.iteritems( eldict):
+  for k, v in zip(eldict.keys(), eldict.values()):
     tmp[v.strip()] = k
   eldict = tmp
   element_flags = flex.int()
@@ -169,9 +170,10 @@ def get_interactions(ph, atom_in_residue, step_size=0.5*A2B, silva_type='dori',
   xyz = ph.atoms().extract_xyz()
   xyz_min=xyz.min()
   xyz_max=xyz.max()
+  print(xyz_min,xyz_max)
   xyz_step = [
     int(math.floor((xyz_max[i]-xyz_min[i])/step_size)+1) for i in range(3)]
-  print(("points to process:",xyz_step[0]*xyz_step[1]*xyz_step[2]))
+  print("points to process:",xyz_step[0]*xyz_step[1]*xyz_step[2])
   interacting_pairs = ext.points_and_pairs(
     ngrid           = xyz_step,
     step_size       = step_size,
@@ -179,13 +181,15 @@ def get_interactions(ph, atom_in_residue, step_size=0.5*A2B, silva_type='dori',
     xyz_min         = xyz_min,
     atom_in_residue = atom_in_residue,
     element_flags   = element_flags,
-    wfc_obj         = wave_functions)
+    wfc_obj         = wave_functions,
+    silva_type      = silva_type)
   tmp = []
   for it in interacting_pairs:
     pair = [int(it[0]),int(it[1])]
     tmp.append(tuple(pair))
   interacting_pairs = list(set(tmp))
-  print(("Time ",(time.time()-t0)))
+  print(interacting_pairs)
+  print("Time ",(time.time()-t0))
   return interacting_pairs
 
 if __name__=="__main__":
@@ -193,12 +197,14 @@ if __name__=="__main__":
   print("1. clustering")
   pdb_inp = iotbx.pdb.input(f)
   ph = pdb_inp.construct_hierarchy()
+  print(dir(ph))
+  print(len(list(ph.residue_groups())))
   run(ph)
   print("2. fragment for residues 1 and 2")
   pdb_inp = iotbx.pdb.input(f)
   ph = pdb_inp.construct_hierarchy()
-  run(ph,core=[1,2])
-
+  print(run(ph,core=[1,2]))
+  stop
   print("3. clustering within expanded ph")
   pdb_inp = iotbx.pdb.input(f)
   cs=pdb_inp.crystal_symmetry()
