48b83d953b2d3200ae6c0d4d2f6ab9bb1e73359b
Luc J. Bourhis <luc_j_bourhis@mac.com>
2019-08-14 01:35:44 +0200

    Port of injector metaclass to Python 3: fix Python 2 breakage
    using module six (learning curve!)

--- libtbx/object_oriented_patterns.py
+++ libtbx/object_oriented_patterns.py
@@ -6,69 +6,58 @@ from builtins import object
 import sys
 import weakref
 from types import MethodType
+import six
 
-injector_doc = """\
-Injection of new methods into an existing class
 
-* synopsis *
+class meta_injector(type):
 
-class some_descriptive_text(injector, some_existing_class,
-                                      another_existing_class, ...):
+  def __init__(cls, name, bases, namespace, **kwds):
+    for target_class in bases[1:]:
+      for name, attribute in namespace.items():
+        if name in ('__module__', '__doc__'):
+          continue
+        if sys.hexversion >= 0x03000000:
+          if name in ('__qualname__',):
+            continue
+        assert not hasattr(target_class, name), (
+          "class %s already has attribute '%s'"
+          % (target_class.__name__, name))
+        setattr(target_class, name, attribute)
+
+
+class injector(six.with_metaclass(meta_injector)):
+  """ Injection of new methods into an existing class
+
+  * synopsis *
+
+  class some_descriptive_text(injector, some_existing_class,
+                                        another_existing_class, ...):
+
+    def new_method(self, ...): ...
+
+    def another_new_method(self, ...): ...
+
+  obj = some_existing_class(...)
+  obj.new_method(...)
+  obj = another_existing_class(...)
+  obj.new_method(...)
+  # class 'some_descriptive_text' is an empty shell with no use by itself.
+
+  * motivation *
+
+  The traditional way to add methods to an existing class involves typing
+  three times the same information:
 
   def new_method(self, ...): ...
+  existing_class.new_method = new_method
 
-  def another_new_method(self, ...): ...
+  or to defer the naming to after the definition:
 
-obj = some_existing_class(...)
-obj.new_method(...)
-obj = another_existing_class(...)
-obj.new_method(...)
-# class 'some_descriptive_text' is an empty shell with no use by itself.
+  def foo(self, ...): ...
+  existing_class.new_method = foo
 
-* motivation *
-
-The traditional way to add methods to an existing class involves typing
-three times the same information:
-
-def new_method(self, ...): ...
-existing_class.new_method = new_method
-
-or to defer the naming to after the definition:
-
-def foo(self, ...): ...
-existing_class.new_method = foo
-
-A bit of metaclass trickery results in a cleaner syntax.
-"""
-
-## Python 2 compatibility ##
-if sys.hexversion < 0x03000000:
-  class injector:
-    __doc__ = injector_doc
-
-    class __metaclass__(type):
-      def __init__(cls, classname, bases, classdict):
-        for target_class in bases[1:]:
-          for name, attribute in classdict.items():
-            if name in ('__module__', '__doc__'): continue
-            assert not hasattr(target_class, name), (
-              "class %s already has attribute '%s'"
-              % (target_class.__name__, name))
-            setattr(target_class, name, attribute)
-else:
-  class meta_injector(type):
-    def __init__(cls, name, bases, namespace, **kwds):
-      for target_class in bases[1:]:
-        for name, attribute in namespace.items():
-          if name in ('__module__', '__doc__', '__qualname__'):
-            continue
-          assert not hasattr(target_class, name), (
-            "class {} already has attribute '{}'"
-            .format(target_class.__name__, name))
-          setattr(target_class, name, attribute)
-
-  class injector(metaclass=meta_injector):
-    __doc__ = injector_doc
+  A bit of metaclass trickery results in a cleaner syntax.
+  """
 
 
 class memoize(object):
