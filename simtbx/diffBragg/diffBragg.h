b42bda9270d68c6f6d121ba1add5d8bad9e0a77b
dermen <dermen@lbl.gov>
2019-08-23 12:25:50 -0700

    updates to use rotation matrix parameters

--- simtbx/diffBragg/diffBragg.h
+++ simtbx/diffBragg/diffBragg.h
@@ -10,6 +10,14 @@ class diffBragg: public nanoBragg{
   diffBragg(const dxtbx::model::Detector& detector, const dxtbx::model::Beam& beam,
             int verbose, int panel_id = 0):
             nanoBragg(detector, beam, verbose, panel_id)
-            {}
+            {
+            thetaX=0;
+            thetaY=0;
+            thetaZ=0;
+            }
+
+  void add_diffBragg_spots();
+  double thetaX, thetaY, thetaZ;
+  mat3 RX, RY, RZ, RXYZ;
 };
 }}
