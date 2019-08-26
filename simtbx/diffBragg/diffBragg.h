ba235badea9b49508b495069d801693867b856d6
dermen <dermen@lbl.gov>
2019-08-25 19:59:24 -0700

    addition of derivatives is in place for rotation managers.. Next step is to test

--- simtbx/diffBragg/diffBragg.h
+++ simtbx/diffBragg/diffBragg.h
@@ -1,5 +1,6 @@
 
 #include <simtbx/nanoBragg/nanoBragg.h>
+#include <vector>
 
 namespace simtbx {
 namespace nanoBragg {
@@ -11,12 +12,23 @@ class derivative_manager{
 
     double* floatimage;
     af::flex_double raw_pixels;
-    double value;
+    double value; // the value of the parameter
+    double dI; // the incremental derivative
 };
 
 class rot_manager: public derivative_manager{
   public:
     rot_manager();
+    double compute_increment(
+        int Na, int Nb, int Nc,
+        double hfrac, double kfrac, double lfrac,
+        double fudge,
+        mat3 U, mat3 A, mat3 B, mat3 C,
+        vec3 a, vec3 b, vec3 c,  vec3 q,
+        double Hrad, double Fcell, double Flatt,
+        double source_I, double capture_fraction, double omega_pixel);
+
+    mat3 XYZ;
 }; // end of rot_manager
 
 class diffBragg: public nanoBragg{
@@ -25,8 +37,18 @@ class diffBragg: public nanoBragg{
             int verbose, int panel_id);
 
   void initialize_managers();
+  void vectorize_umats();
   void add_diffBragg_spots();
+
   mat3 RX, RY, RZ, RXYZ;
+  mat3 dRX, dRY, dRZ;
+  vec3 a_vec, ap_vec;
+  vec3 b_vec, bp_vec;
+  vec3 c_vec, cp_vec;
+  vec3 q_vec; // scattering vector
+
+  std::vector<mat3> UMATS;
+  std::vector<mat3> UMATS_RXYZ;
 
   /* derivative managers */
   rot_manager rotX_man;
