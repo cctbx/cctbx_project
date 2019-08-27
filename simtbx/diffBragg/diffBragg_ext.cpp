f7e487f2c79b7bc68ac604f79b07a2f778166691
dermen <dermen@lbl.gov>
2019-08-26 17:21:05 -0700

    vectorized the derivative managers and made simple python interface

--- simtbx/diffBragg/diffBragg_ext.cpp
+++ simtbx/diffBragg/diffBragg_ext.cpp
@@ -7,28 +7,6 @@ namespace simtbx{
 namespace diffBragg{
 namespace boost_python { namespace {
 
-  /* choosing to expose these as simply thetaX as opposed to rotX.value */
-  static double get_thetaZ(simtbx::nanoBragg::diffBragg const& diffBragg) {
-      return diffBragg.rotZ_man.value*simtbx::nanoBragg::RTD;
-  }
-  static void   set_thetaZ(simtbx::nanoBragg::diffBragg& diffBragg, double const& value) {
-      diffBragg.rotZ_man.value = value/simtbx::nanoBragg::RTD;
-  }
-
-  static double get_thetaY(simtbx::nanoBragg::diffBragg const& diffBragg) {
-      return diffBragg.rotY_man.value*simtbx::nanoBragg::RTD;
-  }
-  static void   set_thetaY(simtbx::nanoBragg::diffBragg& diffBragg, double const& value) {
-      diffBragg.rotY_man.value = value/simtbx::nanoBragg::RTD;
-  }
-
-  static double get_thetaX(simtbx::nanoBragg::diffBragg const& diffBragg) {
-      return diffBragg.rotX_man.value*simtbx::nanoBragg::RTD;
-  }
-  static void   set_thetaX(simtbx::nanoBragg::diffBragg& diffBragg, double const& value) {
-      diffBragg.rotX_man.value = value/simtbx::nanoBragg::RTD;
-  }
-
   void diffBragg_init_module() {
     using namespace boost::python;
     typedef return_value_policy<return_by_value> rbv;
@@ -57,24 +35,15 @@ namespace boost_python { namespace {
       .def("initialize_managers",&simtbx::nanoBragg::diffBragg::initialize_managers,
        "sets up the managers")
 
-      .add_property("thetaZ",
-                 make_function(&get_thetaZ,rbv()),
-                 make_function(&set_thetaZ,dcp()),
-                 "Rotation perturbation about lab Z ")
+      .def("refine", &simtbx::nanoBragg::diffBragg::refine,
+      "sets the refinement flag")
 
-      .add_property("thetaY",
-                 make_function(&get_thetaY,rbv()),
-                 make_function(&set_thetaY,dcp()),
-                 "Rotation perturbation about lab Y ")
+      .def("get_value", &simtbx::nanoBragg::diffBragg::get_value, "get value of the refinement parameter")
 
-      .add_property("thetaX",
-                 make_function(&get_thetaX,rbv()),
-                 make_function(&set_thetaX,dcp()),
-                 "Rotation perturbation about lab X ")
+      .def("set_value", &simtbx::nanoBragg::diffBragg::set_value, "set value of the refinement parameter")
 
-    ; // end of class def
-    class_<simtbx::nanoBragg::derivative_manager>
-        ("derivative_manager", no_init);
+    ; // end of diffBragg extention
+
   } // end of diffBragg_init_module
 } // end of namespace
 } // end of boost python namespace
