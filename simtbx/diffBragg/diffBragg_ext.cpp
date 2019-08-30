430c31d83d7da0a923caaf586f7085b949ae15c7
dermen <dermen@lbl.gov>
2019-08-30 10:40:04 -0700

    intermediate commit towards refinement

--- simtbx/diffBragg/diffBragg_ext.cpp
+++ simtbx/diffBragg/diffBragg_ext.cpp
@@ -20,6 +20,7 @@ namespace boost_python { namespace {
        /* fix this up so its not out-of-range */
       diffBragg.init_beamcenter();
       diffBragg.init_raw_pixels_roi();
+      diffBragg.initialize_managers();
   }
    /* region of interest in pixels */
   static boost::python::tuple get_roi(simtbx::nanoBragg::diffBragg const& diffBragg) {
@@ -49,17 +50,17 @@ namespace boost_python { namespace {
 
       /* function for doing some differentiation */
       .def("add_diffBragg_spots",&simtbx::nanoBragg::diffBragg::add_diffBragg_spots,
-       "gives derivitive of average photon count w.r.t. parameter of choice")
+        "gives derivitive of average photon count w.r.t. parameter of choice")
 
       /* Run this before running add_diffBragg_spots */
       .def("vectorize_umats",&simtbx::nanoBragg::diffBragg::vectorize_umats,
-       "caches the UMATS")
+        "caches the UMATS")
 
       .def("initialize_managers",&simtbx::nanoBragg::diffBragg::initialize_managers,
-       "sets up the managers")
+        "sets up the managers")
 
       .def("refine", &simtbx::nanoBragg::diffBragg::refine,
-      "sets the refinement flag")
+        "sets the refinement flag")
 
       .def("get_value", &simtbx::nanoBragg::diffBragg::get_value, "get value of the refinement parameter")
 
@@ -67,16 +68,18 @@ namespace boost_python { namespace {
 
       .def("get_derivative_pixels", &simtbx::nanoBragg::diffBragg::get_derivative_pixels, "gets the manager raw image")
 
+      .def("zero_raw_pixel_rois", &simtbx::nanoBragg::diffBragg::zero_raw_pixel_rois,
+           "reallocate the raw image ROIs (they are usually tiny)")
+
       .add_property("region_of_interest",
              make_function(&get_roi,rbv()),
              make_function(&set_roi,dcp()),
-             "region of interst on detector: fast_min fast_max slow_min slow_max")
+             "region of interest on detector: fast_min fast_max slow_min slow_max")
 
       .add_property("raw_pixels_roi",
                      make_getter(&simtbx::nanoBragg::diffBragg::raw_pixels_roi,rbv()),
                      make_setter(&simtbx::nanoBragg::diffBragg::raw_pixels_roi,dcp()),
-                     "raw pixels from region of interest only")
-
+                    "raw pixels from region of interest only")
 
     ; // end of diffBragg extention
 
