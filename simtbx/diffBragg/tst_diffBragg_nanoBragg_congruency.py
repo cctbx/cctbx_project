2ee46456e1f019a2fcd50d37d1886ccc74635e1d
dermen <dermen@lbl.gov>
2019-08-30 12:13:48 -0700

    congruency between diffBragg and nanoBragg

---
+++ simtbx/diffBragg/tst_diffBragg_nanoBragg_congruency.py
@@ -0,0 +1,27 @@
+
+def main():
+    from sim_data_for_tests import SimData
+
+    S = SimData()
+    S.instantiate_diffBragg()
+
+    S.D.add_diffBragg_spots()
+    S._add_background()
+
+    diff_img = S.D.raw_pixels.as_numpy_array()
+
+    S.D.raw_pixels *= 0
+    S.D.add_nanoBragg_spots()
+    S._add_background()
+
+    nano_img = S.D.raw_pixels.as_numpy_array()
+
+    import numpy as np
+
+    assert np.allclose(diff_img, nano_img, atol=1e-9)
+
+
+if __name__ == "__main__":
+    main()
+    print ("OK!")
+
