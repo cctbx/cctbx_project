40820a6509971e9853c865d96cf22a9ae14e9027
dermen <dermen@lbl.gov>
2019-09-13 15:16:30 -0700

    LS49 refinement as a test case

---
+++ simtbx/diffBragg/nanoBragg_crystal.py
@@ -0,0 +1,123 @@
+"""
+organizer for setting the nanoBragg crystal properties
+"""
+
+from simtbx.nanoBragg import shapetype
+from scitbx.matrix import sqr
+
+
+class nanoBragg_crystal:
+
+    def __init__(self):
+
+        self.dxtbx_crystal = nanoBragg_crystal.dxtbx_crystal_from_ucell_and_symbol()
+        self.miller_array = nanoBragg_crystal.dummie_Fhkl()
+        self.xtal_shape = shapetype.Gauss
+        self.Ncells_abc = 10, 10, 10
+        self.mos_spread_deg = 0
+        self.n_mos_domains = 1
+        self.thick_mm = 0.1
+        self.missetting_matrix = sqr((1, 0, 0, 0, 1, 0, 0, 0, 1))
+
+    @property
+    def dxtbx_crystal(self):
+        return self._dxtbx_crystal
+
+    @dxtbx_crystal.setter
+    def dxtbx_crystal(self, val):
+        self._dxtbx_crystal = val
+
+    @property
+    def missetting_matrix(self):
+        return self._missetting_matrix
+
+    @missetting_matrix.setter
+    def missetting_matrix(self, val):
+        self._missetting_matrix = val
+
+    @property
+    def miller_array(self):
+        return self._miller_array
+
+    @miller_array.setter
+    def miller_array(self, val):
+        self._miller_array = val
+
+    @property
+    def Ncells_abc(self):
+        return self._Ncells_abc
+
+    @Ncells_abc.setter
+    def Ncells_abc(self, val):
+        self._Ncells_abc = val
+
+    @property
+    def mos_spread_deg(self):
+        return self._mos_spread_deg
+
+    @mos_spread_deg.setter
+    def mos_spread_deg(self, val):
+        self._mos_spread_deg = val
+
+    @property
+    def n_mos_domains(self):
+        return self._n_mos_domains
+
+    @n_mos_domains.setter
+    def n_mos_domains(self, val):
+        self._n_mos_domains = val
+
+    @property
+    def Amatrix_realspace(self):
+        B = sqr(self.dxtbx_crystal.get_B()).inverse().transpose()
+        A = (self.missetting_matrix * B).transpose()
+        return A
+
+    @property
+    def xtal_shape(self):
+        return self._xtal_shape
+
+    @xtal_shape.setter
+    def xtal_shape(self, val):
+        self._xtal_shape = val
+
+    @property
+    def thick_mm(self):
+        return self._thick_mm
+
+    @thick_mm.setter
+    def thick_mm(self, val):
+        self._thick_mm = val
+
+    @staticmethod
+    def dxtbx_crystal_from_ucell_and_symbol(ucell_tuple_Adeg=(79,79,79,90,90,90), symbol="P43212"):
+        """
+        :param ucell_tuple_Adeg:  unit cell tuple a,b,c al, be, ga in Angstom and degrees
+        :param symbol: lookup symbol for space group, e.g. 'P1'
+        :return:a default crystal in conventional orientation, a along x-axis
+        """
+        from cctbx import crystal
+        from dxtbx.model.crystal import CrystalFactory
+        symm = crystal.symmetry("%f,%f,%f,%f,%f,%f" % ucell_tuple_Adeg, symbol)
+
+        ucell = symm.unit_cell()
+        O = ucell.orthogonalization_matrix()
+        real_space_a = O[0], O[3], O[6]
+        real_space_b = O[1], O[4], O[7]
+        real_space_c = O[2], O[5], O[8]
+
+        hall_symbol = symm.space_group_info().type().hall_symbol()
+
+        return CrystalFactory.from_dict(
+            {'__id__': 'crystal',
+                       'real_space_a': real_space_a,
+                       'real_space_b': real_space_b,
+                       'real_space_c': real_space_c,
+                       'space_group_hall_symbol': hall_symbol})
+
+    @staticmethod
+    def dummie_Fhkl():
+        from simtbx.nanoBragg.tst_nanoBragg_basic import fcalc_from_pdb
+        return fcalc_from_pdb(resolution=2, algorithm="fft", wavelength=1)
+
+
