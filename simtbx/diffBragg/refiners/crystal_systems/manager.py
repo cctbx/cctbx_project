7f9e2152efb5f0a767ec3227b3de157a4cf73774
dermen <dermen@lbl.gov>
2019-09-25 14:34:31 -0700

    crystal system manager module added

---
+++ simtbx/diffBragg/refiners/crystal_systems/manager.py
@@ -0,0 +1,107 @@
+
+from abc import ABCMeta, abstractproperty
+import numpy as np
+from scitbx.matrix import sqr
+
+
+class CrystalSystemManager(object):
+
+    __metaclass__ = ABCMeta
+
+    @abstractproperty
+    def a(self):
+        """unit cell a parameter"""
+        pass
+
+    @abstractproperty
+    def b(self):
+        """unit cell b parameter"""
+        pass
+
+    @abstractproperty
+    def c(self):
+        """unit cell c parameter"""
+        pass
+
+    @abstractproperty
+    def al(self):
+        """unit cell alpha angle"""
+        pass
+
+    @abstractproperty
+    def be(self):
+        """unit cell beta angle"""
+        pass
+
+    @abstractproperty
+    def ga(self):
+        """unit cell gamma angle"""
+        pass
+
+    @abstractproperty
+    def variables(self):
+        """the unit cell variables"""
+        pass
+
+    @property
+    def cal(self):
+        """cosine of alpha"""
+        return np.cos(self.al)
+
+    @property
+    def cbe(self):
+        """cosine of beta"""
+        return np.cos(self.be)
+
+    @property
+    def cga(self):
+        """cosine of gamma"""
+        return np.cos(self.ga)
+
+    @property
+    def sal(self):
+        """sine of alpha"""
+        return np.sin(self.al)
+
+    @property
+    def sbe(self):
+        """sine of beta"""
+        return np.sin(self.be)
+
+    @property
+    def sga(self):
+        """sine of gamma"""
+        return np.sin(self.ga)
+
+    @property
+    def V(self):
+        """unit cell volume in cubic Angstrom"""
+        return self.a*self.b*self.c*np.sqrt(1-self.cal**2 - self.cbe**2-self.cga**2+2*self.cal*self.cbe*self.cga)
+
+    @property
+    def B_realspace(self):
+        """
+        real space B-matrix in upper triangular form
+        such that the columns are a_real, b_real, c_real
+        aligned according to the PDB convention
+        """
+        return sqr(
+            (self.a, self.b*self.cga, self.c*self.cbe,
+             0,      self.b*self.sga, self.c*(self.cal-self.cbe*self.cga)/self.sga,
+             0,      0,               self.V/(self.a*self.b*self.sga)))
+
+    @property
+    def B_recipspace(self):
+        """
+        reciprocal space B-matrix in lower-triangular form
+        such that it corresponds to the dxtbx Crystal get_B() method
+        """
+        return self.B_realspace.inverse().transpose()
+
+    @property
+    def _names(self):
+        return ["a_Ang", "b_Ang", "c_Ang", "alpha_rad", "beta_rad", "gamma_rad"]
+
+    @abstractproperty
+    def variable_names(self):
+        pass
