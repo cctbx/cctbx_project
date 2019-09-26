ea42c051245881e18a5f2ae15daec32b6ad62ab9
dermen <dermen@lbl.gov>
2019-09-25 17:43:37 -0700

    moved tests to test dir

--- simtbx/diffBragg/tests/tst_diffBragg_ucell_refine.py
+++ simtbx/diffBragg/tests/tst_diffBragg_ucell_refine.py
@@ -5,7 +5,6 @@ parser.add_argument("--plot", action='store_true')
 parser.add_argument("--refine", action='store_true')
 parser.add_argument("--crystalsystem", default='tetragonal',
                     choices=["monoclinic", "tetragonal"])
-parser.add_argument("--curvatures", action='store_true')
 args = parser.parse_args()
 is_tet = args.crystalsystem == "tetragonal"
 import numpy as np
@@ -13,6 +12,8 @@ from scipy.spatial.transform import Rotation
 from scipy.stats import pearsonr
 import pylab as plt
 
+from cctbx import sgtbx
+from rstbx.symmetry.constraints import parameter_reduction
 from scitbx.matrix import sqr
 from scitbx.matrix import rec
 from simtbx.diffBragg.nanoBragg_crystal import nanoBragg_crystal
@@ -21,7 +22,7 @@ from simtbx.diffBragg.sim_data import SimData
 from simtbx.diffBragg import utils
 from simtbx.diffBragg.refiners import crystal_systems
 from dxtbx.model.crystal import Crystal
-from simtbx.diffBragg.refiners import RefineUcell
+from IPython import embed
 
 #TODO parameters shifts should be percentages
 
@@ -32,10 +33,11 @@ if is_tet:
     ucell = (55, 55, 77, 90, 90, 90)
     symbol = "P43212"
 else:  # args.crystalsystem == "monoclinic"
-    ucell = (70, 60, 50, 90.0, 110, 90.0)
-    symbol = "C121"
+    ucell = (55, 65, 75, 90, 95, 90)
+    symbol = "P121"
 
 a_real, b_real, c_real = sqr(uctbx.unit_cell(ucell).orthogonalization_matrix()).transpose().as_list_of_lists()
+#a_real, b_real, c_real = np.reshape(uctbx.unit_cell(ucell).orthogonalization_matrix(), (3,3)).T   #transpose().as_list_of_lists()
 C = Crystal(a_real, b_real, c_real, symbol)
 
 # random raotation
@@ -45,15 +47,28 @@ rot_ang, rot_axis = Q.unit_quaternion_as_axis_and_angle()
 C.rotate_around_origin(rot_axis, rot_ang)
 a_rot, b_rot, c_rot = C.get_real_space_vectors()
 
-# STEP3:
-# create the unit cell parameter manager
+# make the parameter reduction manager
+point_group = sgtbx.space_group_info(symbol=symbol).group().build_derived_point_group()
+S = parameter_reduction.symmetrize_reduce_enlarge(point_group)
+S.set_orientation(np.hstack((a_real, b_real, c_real)), length_unit=1e-10)
+X = S.forward_independent_parameters()
+dX = S.forward_gradients()  # gradients in meters
+n_ucell_params = len(X)
+
+
+# STEP3: sanity check
+B = S.backward_orientation(independent=X)
+print("Number of parameters = %d" % n_ucell_params)
+print("Test congruency between parameter reduction module and dxtbx...")
+assert np.allclose(C.get_B(), B.reciprocal_matrix())
+#Bstar = B.reciprocal_matrix()
+print("OK!")
+
 if is_tet:
     UcellMan = crystal_systems.TetragonalManager(a=ucell[0], c=ucell[2])
 else:
     UcellMan = crystal_systems.MonoclinicManager(a=ucell[0], b=ucell[1],
                                        c=ucell[2], beta=ucell[4]*np.pi/180)
-
-n_ucell_params = len(UcellMan.variables)
 
 assert np.allclose(UcellMan.B_recipspace, C.get_B())
 
@@ -68,13 +83,13 @@ nbcryst.Ncells_abc = (7, 7, 7)
 # STEP5: make an instance of diffBRagg, use the simData wrapper
 SIM = SimData()
 # overwrite the default detector with a smaller pixels one
-SIM.detector = SimData.simple_detector(300, 0.1, (700, 700))
+SIM.detector = SimData.simple_detector(298, 0.1, (700, 700))
+# FIXME: determine why setting detdist > 298 causes oversample to diverge...
 SIM.crystal = nbcryst
-SIM.instantiate_diffBragg(oversample=0, verbose=0)
+SIM.instantiate_diffBragg(oversample=0)
 # D is an instance of diffBragg with reasonable parameters
 # and our dxtbx crystal created above
 D = SIM.D
-D.progress_meter = True
 
 # STEP6:
 # initialize the derivative managers for the unit cell parameters
@@ -83,8 +98,6 @@ for i_param in range(n_ucell_params):
 D.initialize_managers()
 for i in range(n_ucell_params):
     D.set_ucell_derivative_matrix(3+i, UcellMan.derivative_matrices[i])
-    if args.curvatures:
-        D.set_ucell_second_derivative_matrix(3 + i, UcellMan.second_derivative_matrices[i])
 D.initialize_managers()
 
 roi = ((0, 699), (0, 699))
@@ -94,45 +107,41 @@ D.region_of_interest = roi
 
 # STEP7:
 # compute the scattering and its derivative
-print("Adding diffBragg spots")
 D.add_diffBragg_spots()
-print("Done!")
 img = D.raw_pixels_roi.as_numpy_array()
 # reset all pixel values
 D.raw_pixels *= 0
 D.raw_pixels_roi *= 0
 
 derivs = []
-second_derivs = []
 for i_param in range(n_ucell_params):
-    analy_deriv = D.get_derivative_pixels(3+i_param).as_numpy_array()
+    analy_deriv = SIM.D.get_derivative_pixels(3+i_param).as_numpy_array()
     derivs.append(analy_deriv)
-    if args.curvatures:
-        second_derivs.append(D.get_second_derivative_pixels(3+i_param).as_numpy_array())
 
 # STEP8
 # iterate over the parameters and do a finite difference test for each one
 # parameter shifts:
-shifts = [1e-4 * (2**i) for i in range(1, 12, 2)]
+shifts = [1e-3 * (2**i) for i in range(1, 12, 2)]
 
 if not args.refine:
     for i_param in range(n_ucell_params):
+        #if i_param < 3:
+        #    continue
         analy_deriv = derivs[i_param]
         diffs = []
-        diffs2 = []
-        error2 = []
-        for i_shift, percent_shift in enumerate(shifts):
+        for i_shift, param_shift in enumerate(shifts):
 
+            #uc2 = C2.get_unit_cell().parameters()
+            #Tet.a = a2_real[0]
+            #Tet.c = c2_real[2]
             if is_tet:
                 var = [ucell[0], ucell[2]]
             else:
                 var = [ucell[0], ucell[1], ucell[2], ucell[4]*np.pi/180]
 
-            #name = UcellMan.variable_names[i_param]
-            #if "alpha" in name or "beta" in name or "gamma" in name:
-            #    param_shift = param_shift * 1e-4  # rescale the radian parameters
-
-            param_shift = var[i_param] * percent_shift
+            name = UcellMan.variable_names[i_param]
+            if "alpha" in name or "beta" in name or "gamma" in name:
+                param_shift = param_shift * 1e-4  # rescale the radian parameters
 
             var[i_param] += param_shift
             UcellMan.variables = var
@@ -140,47 +149,23 @@ if not args.refine:
             D.Bmatrix = UcellMan.B_recipspace
             D.add_diffBragg_spots()
 
-            img_forward = D.raw_pixels_roi.as_numpy_array()
+            img2 = D.raw_pixels_roi.as_numpy_array()
 
             # reset for next computation
             D.raw_pixels_roi *= 0
             D.raw_pixels *= 0
 
-            if args.curvatures:
-                # estimate the second derivative
-                var[i_param] = var[i_param] - 2*param_shift  # do the backwards finite deriv
-                UcellMan.variables = var
+            finite_deriv = (img2-img) / param_shift
 
-                D.Bmatrix = UcellMan.B_recipspace
-                D.add_diffBragg_spots()
-
-                img_backward = D.raw_pixels_roi.as_numpy_array()
-
-                # reset for next computation
-                D.raw_pixels_roi *= 0
-                D.raw_pixels *= 0
-
-            finite_deriv = (img_forward-img) / param_shift
-
-            if second_derivs:
-                finite_second_deriv = (img_forward - 2*img + img_backward) / param_shift / param_shift
-
+            y = slice(340, 360)
+            x = slice(240, 260)
             bragg = img > 0.5
 
             ave_error = np.abs(finite_deriv[bragg] - analy_deriv[bragg]).mean()
+            print "\tAverage error=%f; parameter shift h=%f" % (ave_error, abs(param_shift))
 
             r = pearsonr(analy_deriv[bragg].ravel(), finite_deriv[bragg].ravel())[0]
             diffs.append(r)
-
-            print ("\tAverage error=%f; parameter shift h=%f" % (ave_error, abs(param_shift)))
-            if args.curvatures:
-                ave_error2 = np.abs(finite_second_deriv[bragg] - second_derivs[i_param][bragg]).mean()
-                print("\tsecond derivative Average error=%f; parameter shift squared h^2=%f"
-                      % (ave_error2, abs(param_shift)**2))
-                r2 = pearsonr(second_derivs[i_param][bragg].ravel(), finite_second_deriv[bragg].ravel())[0]
-
-                error2.append(ave_error2)
-                diffs2.append(r2)
             if args.plot:
                 plt.subplot(121)
                 plt.imshow(finite_deriv)
@@ -192,34 +177,15 @@ if not args.refine:
                 plt.suptitle("Shift %d / %d"
                              % (i_shift+1, len(shifts)))
                 plt.pause(0.8)
-                if args.curvatures:
-                    plt.subplot(121)
-                    plt.imshow(finite_second_deriv)
-                    plt.title("finite second diff")
-                    plt.subplot(122)
-                    plt.imshow(second_derivs[i_param])
-                    plt.title("analytical")
-                    plt.draw()
-                    plt.suptitle("Shift %d / %d"
-                                 % (i_shift + 1, len(shifts)))
-                    plt.pause(0.8)
 
         if args.plot:
             plt.close()
             plt.plot(shifts, diffs, 'o')
             title = "Unit cell parameter %d / %d" % (i_param+1, n_ucell_params)
-            plt.title(title + "\nPearson corr between finite deriv and analytical")
+            plt.title(title + "\nPearson corr between finite diff and analytical")
             plt.xlabel("unit cell shifts")
             plt.ylabel("Pearson corr")
             plt.show()
-            if args.curvatures:
-                plt.close()
-                plt.plot(np.array(shifts)**2, diffs2, 'o')
-                title = "Unit cell parameter %d / %d" % (i_param + 1, n_ucell_params)
-                plt.title(title + "\nPearson corr between finite second deriv and analytical")
-                plt.xlabel("unit cell shifts")
-                plt.ylabel("Pearson corr")
-                plt.show()
 
         # verify a high correlation for the smallest parameter shift
         print("Check high pearson R between analytical and finite diff")
@@ -229,21 +195,25 @@ if not args.refine:
         print("Fit polynomial and check monotonic decrease")
         trend = np.polyval(np.polyfit(shifts, diffs, 2), shifts)
         assert np.all(np.diff(zip(trend[:-1], trend[1:]), axis=1) <= 0)
-        if args.curvatures:
-            assert (diffs2[0] > .99), "%f" % diffs2[0]
     print("OK!")
+
 
 if is_tet:
     ucell2 = (55.5, 55.5, 76.1, 90, 90, 90)
 else:  # args.crystalsystem == "monoclinic"
-    ucell2 = (70.5, 60.1, 50.1, 90.0, 110.1, 90.0)
+    ucell2 = (55.2, 66, 74, 90, 94.3, 90)
 
 a2_real, b2_real, c2_real = sqr(uctbx.unit_cell(ucell2).orthogonalization_matrix()).transpose().as_list_of_lists()
+#a_real, b_real, c_real = np.reshape(uctbx.unit_cell(ucell).orthogonalization_matrix(), (3,3)).T   #transpose().as_list_of_lists()
 C2 = Crystal(a2_real, b2_real, c2_real, symbol)
 
 print ("Ensure the U matrices are the same for the ground truth and perturbation")
+np.random.seed(1)
+axis = np.random.random(3)
+axis /= np.linalg.norm(axis)
 C2.rotate_around_origin(rot_axis, rot_ang)
 assert np.allclose(C.get_U(), C2.get_U())
+
 
 # Setup the simulation and create a realistic image
 # with background and noise
@@ -253,7 +223,7 @@ nbcryst.thick_mm = 0.5
 nbcryst.Ncells_abc = 12, 12, 12
 
 SIM = SimData()
-SIM.detector = SimData.simple_detector(150, 0.1, (1024, 1024))
+SIM.detector = SimData.simple_detector(150, 0.1, (512, 512))
 SIM.crystal = nbcryst
 SIM.instantiate_diffBragg(oversample=0)
 SIM.D.progress_meter = False
@@ -265,9 +235,6 @@ SIM.include_noise = True
 SIM.D.add_diffBragg_spots()
 spots = SIM.D.raw_pixels.as_numpy_array()
 SIM._add_background()
-bg_img = SIM.D.raw_pixels.as_numpy_array()
-
-SIM.D.readout_noise_adu = 0
 SIM._add_noise()
 # this is the ground truth image:
 img = SIM.D.raw_pixels.as_numpy_array()
@@ -285,7 +252,57 @@ SIM.D.raw_pixels *= 0
 # spot_rois, abc_init, img, SimData_instance,
 # locate the strong spots and fit background planes
 # <><><><><><><><><><><><><><><><><><><><><><><><><>
-spot_roi, tilt_abc = utils.process_simdata(spots, img, thresh=20, plot=args.plot)
+spot_data = utils.get_spot_data(spots, thresh=19)
+ss_spot, fs_spot = map(np.array, zip(*spot_data["maxIpos"]))  # slow/fast  scan coords of strong spots
+num_spots = len(ss_spot)
+if args.plot:
+    plt.imshow(img, vmax=200)
+    plt.plot(fs_spot, ss_spot, 'o', mfc='none', mec='r')
+    plt.title("Simulated image with strong spots marked")
+    plt.show()
+
+is_bg_pixel = np.ones(img.shape, bool)
+for bb_ss, bb_fs in spot_data["bboxes"]:
+    is_bg_pixel[bb_ss, bb_fs] = False
+
+# now fit tilting planes
+shoebox_sz = 20
+tilt_abc = np.zeros((num_spots, 3))
+spot_roi = np.zeros((num_spots, 4), int)
+if args.plot:
+    patches = []
+img_shape = SIM.detector[0].get_image_size()
+for i_spot, (x_com, y_com) in enumerate(zip(fs_spot, ss_spot)):
+    i1 = int(max(x_com - shoebox_sz / 2., 0))
+    i2 = int(min(x_com + shoebox_sz / 2., img_shape[0]-1))
+    j1 = int(max(y_com - shoebox_sz / 2., 0))
+    j2 = int(min(y_com + shoebox_sz / 2., img_shape[1]-1))
+
+    shoebox_img = img[j1:j2, i1:i2]
+    shoebox_mask = is_bg_pixel[j1:j2, i1:i2]
+
+    tilt, bgmask, coeff = utils.tilting_plane(
+        shoebox_img,
+        mask=shoebox_mask,  # mask specifies which spots are bg pixels...
+        zscore=2)
+
+    tilt_abc[i_spot] = coeff[1], coeff[2], coeff[0]  # store as fast-scan coeff, slow-scan coeff, offset coeff
+
+    spot_roi[i_spot] = i1, i2, j1, j2
+    if args.plot:
+        R = plt.Rectangle(xy=(x_com-shoebox_sz/2, y_com-shoebox_sz/2.),
+                      width=shoebox_sz,
+                      height=shoebox_sz,
+                      fc='none', ec='r')
+        patches.append(R)
+
+if args.plot:
+    patch_coll = plt.mpl.collections.PatchCollection(patches, match_original=True)
+    plt.imshow(img, vmin=0, vmax=200)
+    plt.gca().add_collection(patch_coll)
+    plt.show()
+
+from simtbx.diffBragg.refiners import RefineUcell
 
 if is_tet:
     UcellMan.variables = [ucell2[0], ucell2[2]]
@@ -294,9 +311,6 @@ else:
 
 nbcryst.dxtbx_crystal = C2
 SIM.crystal = nbcryst
-SIM.D.Omatrix = nbcryst.Omatrix
-SIM.D.Bmatrix = C2.get_B()
-SIM.D.Umatrix = C2.get_U()
 
 RUC = RefineUcell(
     spot_rois=spot_roi,
@@ -306,63 +320,7 @@ RUC = RefineUcell(
     plot_images=args.plot,
     ucell_manager=UcellMan)
 
-RUC.refine_background_planes = False
-RUC.refine_crystal_scale = False
-RUC.refine_gain_fac = False
-RUC.refine_Amatrix = True
 RUC.trad_conv = True
-RUC.trad_conv_eps = 1e-4
-RUC.max_calls = 150
-RUC.use_curvatures = args.curvatures
-#RUC._setup()
-#RUC._cache_roi_arrays()
+RUC.trad_conv_eps = 1e-5
 RUC.run()
 
-#from scitbx.array_family import flex
-#
-#
-#def func(x, p0):
-#    p0.x = flex.double(x)
-#    f, g = p0.compute_functional_and_gradients()
-#    return f
-#
-#
-#def fprime(x, p0):
-#    p0.x = flex.double(x)
-#    f, g = p0.compute_functional_and_gradients()
-#    return g.as_numpy_array()
-#
-#
-#from scipy.optimize import fmin_l_bfgs_b
-#
-#print("GO!")
-#out = fmin_l_bfgs_b(func=func, x0=np.array(RUC.x), fprime=fprime, args=[RUC])
-#
-#
-#RUC.run()
-
-if is_tet:
-    a_init, _, c_init, _, _, _ = ucell2
-    a, _, c, _, beta, _ = ucell
-    a_ref, c_ref = RUC.ucell_manager.variables
-
-    err_init = np.linalg.norm((abs(a_init-a)/a, abs(c_init-c)/c))*100
-    err_ref = np.linalg.norm((abs(a_ref-a)/a, abs(c_ref-c)/c))*100
-
-    print ("Percent error in unit cell before refinement: %2.7g %%" % err_init)
-    print (" ''                ''      after refinement: %2.7g %%" % err_ref)
-    assert err_ref < 1e-2*err_init
-else:
-    a_init, b_init, c_init, _, beta_init, _ = ucell2
-    a, b, c, _, beta, _ = ucell
-    a_ref, b_ref, c_ref, beta_ref = RUC.ucell_manager.variables
-    beta_ref = beta_ref * 180 / np.pi
-
-    err_init = np.linalg.norm((abs(a_init-a)/a, abs(b_init-b)/b, abs(c_init-c)/c, abs(beta_init-beta)/beta))*100
-    err_ref = np.linalg.norm((abs(a_ref-a)/a, abs(b_ref-b)/b, abs(c_ref-c)/c, abs(beta_ref-beta)/beta))*100
-
-    print ("\nPercent error in unit cell before refinement: %2.7g %%" % err_init)
-    print (" ''                ''      after refinement: %2.7g %%\n" % err_ref)
-    assert err_ref < 1e-2*err_init
-
-print("OK!")
