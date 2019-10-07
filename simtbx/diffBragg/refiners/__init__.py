efcf9572395daecaf34551199f4ceb9fd9ccc347
dermen <dermen@lbl.gov>
2019-10-07 12:24:48 -0700

    det dist refinement, and 3D plot

--- simtbx/diffBragg/refiners/__init__.py
+++ simtbx/diffBragg/refiners/__init__.py
@@ -3,3 +3,4 @@ from .pixel_refinement import PixelRefinement
 from .rotXYZ_refiner import RefineRot
 from .ucell_refiner import RefineUcell
 from .rotXYZ_ucell_refiner import RefineMissetAndUcell
+from .detdist_refiner import RefineDetdist
