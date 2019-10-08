c565bb1557414aa4e50cf00c3b7b350ece2bcfbc
dermen <dermen@lbl.gov>
2019-10-07 17:03:54 -0700

    updates towards afull refinement

--- simtbx/diffBragg/refiners/__init__.py
+++ simtbx/diffBragg/refiners/__init__.py
@@ -4,3 +4,5 @@ from .rotXYZ_refiner import RefineRot
 from .ucell_refiner import RefineUcell
 from .rotXYZ_ucell_refiner import RefineMissetAndUcell
 from .detdist_refiner import RefineDetdist
+from .ncells_refiner import RefineNcells
+from .all_refine import RefineAll
