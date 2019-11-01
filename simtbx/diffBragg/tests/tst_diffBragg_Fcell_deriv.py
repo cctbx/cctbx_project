77ac7b9a05da4bb7789e0d34efe337977a1225c9
dermen <dermen@lbl.gov>
2019-10-31 20:25:12 -0700

    second Fcell derivative also looks good

--- simtbx/diffBragg/tests/tst_diffBragg_Fcell_deriv.py
+++ simtbx/diffBragg/tests/tst_diffBragg_Fcell_deriv.py
@@ -12,7 +12,6 @@ import numpy as np
 from scipy.spatial.transform import Rotation
 from scipy.stats import linregress
 
-
 from scitbx.array_family import flex
 from scitbx.matrix import sqr
 from scitbx.matrix import rec
@@ -20,8 +19,6 @@ from simtbx.diffBragg.nanoBragg_crystal import nanoBragg_crystal
 from cctbx import uctbx
 from simtbx.diffBragg.sim_data import SimData
 from dxtbx.model.crystal import Crystal
-from IPython import embed
-
 
 ucell = (70, 60, 50, 90.0, 110, 90.0)
 symbol = "C121"
@@ -34,9 +31,7 @@ rotation = Rotation.random(num=1, random_state=101)[0]
 Q = rec(rotation.as_quat(), n=(4, 1))
 rot_ang, rot_axis = Q.unit_quaternion_as_axis_and_angle()
 C.rotate_around_origin(rot_axis, rot_ang)
-a_rot, b_rot, c_rot = C.get_real_space_vectors()
 
-# STEP4:
 # make a nanoBragg crystal to pass to diffBragg
 nbcryst = nanoBragg_crystal()
 nbcryst.dxtbx_crystal = C
@@ -44,7 +39,7 @@ nbcryst.n_mos_domains = 1
 nbcryst.thick_mm = 0.01
 nbcryst.Ncells_abc = (7, 7, 7)
 
-# STEP5: make an instance of diffBRagg, use the simData wrapper
+# make an instance of diffBRagg, use the simData wrapper
 SIM = SimData()
 # overwrite the default detector with a smaller pixels one
 SIM.detector = SimData.simple_detector(300, 0.1, (700, 700))
@@ -56,9 +51,8 @@ SIM.instantiate_diffBragg(oversample=0, verbose=0, default_F=Fcell)
 D = SIM.D
 D.progress_meter = True
 
-# STEP6:
-# initialize the derivative managers for the unit cell parameters
-fcell = 11
+# initialize the derivative manager for Fcell
+fcell = 11  # internal index of fcell manager within diffBragg
 D.refine(fcell)
 D.initialize_managers()
 
@@ -67,13 +61,13 @@ rX = slice(roi[0][0], roi[0][1], 1)
 rY = slice(roi[1][0], roi[1][1], 1)
 D.region_of_interest = roi
 
-# STEP7:
 # compute the scattering and its derivative
 print("Adding diffBragg spots")
 # Set all miller indices to have the same Fcell value
 indices, data = D.Fhkl_tuple
 data = flex.double(np.ones(len(indices))*Fcell)
 D.Fhkl_tuple = indices, data
+D.F000 = Fcell
 
 D.add_diffBragg_spots()
 
@@ -88,35 +82,28 @@ first_deriv = D.get_derivative_pixels(fcell).as_numpy_array()
 if args.curvatures:
     second_deriv = D.get_second_derivative_pixels(fcell).as_numpy_array()
 
-
-# STEP8
 # iterate over the parameters and do a finite difference test for each one
-# parameter shifts:
-diffs = []
-diffs2 = []
-error2 = []
+shifts = [1e-5*(i**2) for i in range(1, 20)]
 all_error = []
-shifts = [1e-5*(i**2) for i in range(1, 30)]
 for i_shift, percent_shift in enumerate(shifts):
 
-    # TODO compute param shift (different for each HKL)
     delta_F = Fcell * percent_shift*1e-2
 
-    #param_shift = var[i_param] * percent_shift
-
-    #var[i_param] += param_shift
-    # TODO step Fcell forward
     D.Fhkl_tuple = indices, data+delta_F
+    D.F000 = Fcell + delta_F
     D.add_diffBragg_spots()
-    img_forward = D.raw_pixels_roi.as_numpy_array()
 
+    img_forward = D.raw_pixels_roi.as_numpy_array()
     # reset for next computation
     D.raw_pixels_roi *= 0
     D.raw_pixels *= 0
 
+    finite_deriv = (img_forward-img) / delta_F
+
     if args.curvatures:
         # estimate the second derivative
-        #TODO step Fcell backward
+        D.Fhkl_tuple = indices, data - delta_F
+        D.F000 = Fcell - delta_F
         D.add_diffBragg_spots()
         img_backward = D.raw_pixels_roi.as_numpy_array()
 
@@ -124,12 +111,10 @@ for i_shift, percent_shift in enumerate(shifts):
         D.raw_pixels_roi *= 0
         D.raw_pixels *= 0
 
-    finite_deriv = (img_forward-img) / delta_F
-
-    bragg = img > 10
+    bragg = img > 10  # look at the strong pixels
     ave_error = np.abs(finite_deriv[bragg] - first_deriv[bragg]).mean()
     all_error.append(ave_error)
-    print ("error=%f, step=%f" % (ave_error, delta_F))
+    print ("error=%f, step=%f (%d/%d)" % (ave_error, delta_F, i_shift+1, len(shifts)))
 
     if args.curvatures:
         finite_second_deriv = (img_forward - 2*img + img_backward) / delta_F / delta_F
@@ -147,14 +132,16 @@ for i_shift, percent_shift in enumerate(shifts):
             plt.pause(0.8)
 
 if args.plot:
-    plt.clf()
+    plt.close()
     plt.plot(shifts, all_error, 'o')
     plt.show()
 
+# one expects errors to scale linearly with parameter shift for finite differences
 l = linregress(shifts, all_error)
 assert l.rvalue > .9999  # this is definitely a line!
 assert l.slope > 0
 assert l.pvalue < 1e-6
+print ("Error versus parameter shift fits a line with slope=%2.7g and Correleation Coef=%2.7g" % (l.slope, l.rvalue))
+#TODO: analytical test for finite second difference, how should error scale ?
 
 print("OK!")
-
