e34aff715681ce32e2a2274dfbe1f6ff2ffc087b
Billy K. Poon <bkpoon@lbl.gov>
2019-12-04 16:19:03 -0800

    scitbx: update test values tst_random.py
    - Boost 1.64 changes the exponential distribution to use the ziggurat algorithm
    - Update values drawn from gamma distribution due to change
    - Loosen tolerance for skew
    - Remove old normal distribution test values for Boost 1.56

--- scitbx/random/tests/tst_random.py
+++ scitbx/random/tests/tst_random.py
@@ -7,8 +7,6 @@ from scitbx.array_family import flex
 import itertools
 import math
 
-# Boost 1.56 changes normal distribution, but keep old tests for testing
-# older versions of Boost
 import libtbx.load_env
 from six.moves import range
 boost_version = libtbx.env.boost_version
@@ -27,22 +25,13 @@ def exercise_variate_generators():
   for i in range(10):
     scitbx.random.set_random_seed(0)
     g = variate(normal_distribution())
-    if (boost_version < 105600):
-      assert approx_equal(g(), -1.2780081289048213)
-      assert approx_equal(g(10),
-        (-0.40474189234755492, -0.41845505596083288,
-         -1.8825790263067721, -1.5779112018107659,
-         -1.1888174422378859, -1.8619619179878537,
-         -0.53946818661388318, -1.2400941724410812,
-         0.64511959841907285, -0.59934120033270688))
-    else:
-      assert approx_equal(g(), -0.917787219374)
-      assert approx_equal(g(10),
-        (1.21838707856, 1.732426915,
-         0.838038157555, -0.296895169923,
-         0.246451144946, -0.635474652255,
-         -0.0980626986425, 0.36458295417,
-         0.534073780268, -0.665073136294))
+    assert approx_equal(g(), -0.917787219374)
+    assert approx_equal(g(10),
+      (1.21838707856, 1.732426915,
+        0.838038157555, -0.296895169923,
+        0.246451144946, -0.635474652255,
+        -0.0980626986425, 0.36458295417,
+        0.534073780268, -0.665073136294))
 
   stat = basic_statistics(flex.double(itertools.islice(g, 1000000)))
   assert approx_equal(stat.mean,            0, eps=0.005)
@@ -59,13 +48,18 @@ def exercise_variate_generators():
     0.1,
     eps = 0.01)
 
+  # Boost 1.64 changes the exponential distribution to use Ziggurat algorithm
   scitbx.random.set_random_seed(0)
   g = variate(gamma_distribution())
-  assert approx_equal(g(), 0.79587450456577546)
-  assert approx_equal(g(2), (0.89856038848394115, 1.2559307580473893))
+  if (boost_version < 106400):
+    assert approx_equal(g(), 0.79587450456577546)
+    assert approx_equal(g(2), (0.89856038848394115, 1.2559307580473893))
+  else:
+    assert approx_equal(g(), 0.864758191783)
+    assert approx_equal(g(2), (1.36660841837, 2.26740986094))
   stat = basic_statistics(flex.double(itertools.islice(g, 1000000)))
   assert approx_equal(stat.mean,            1, eps=0.005)
-  assert approx_equal(stat.skew,            2, eps=0.005)
+  assert approx_equal(stat.skew,            2, eps=0.01)
   assert approx_equal(stat.biased_variance, 1, eps=0.005)
   scitbx.random.set_random_seed(0)
   g = variate(gamma_distribution(alpha=2, beta=3))
