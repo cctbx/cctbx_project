da9add250e550d005c4a090ab3b73ebb1fedf402
Nicholas Sauter <nksauter@lbl.gov>
2019-12-17 13:22:19 -0800

    Additional OpenMP-enabled unit cell distance-matrix options.

--- cctbx/uctbx/determine_unit_cell/ext.cpp
+++ cctbx/uctbx/determine_unit_cell/ext.cpp
@@ -92,12 +92,7 @@ namespace cctbx { namespace uctbx {
     return ncdist2017::CS6Dist_in_G6(&mm1[0],&mm2[0]);
   }
 
-  af::tiny<double,6> S6Dist_reduce_wrapper(af::tiny<double,6> mm1, std::string centering_symbol) {
-    // stub
-    return mm1;
-  }
-
-  scitbx::af::versa<double, scitbx::af::c_grid<2> > CS6Dist_in_G6_flatten(scitbx::af::shared<double> MM){
+  scitbx::af::versa<double, scitbx::af::c_grid<2> > NCDist2017_flatten(scitbx::af::shared<double> MM){
     int NN = MM.size()/6;
     af::versa<double, af::c_grid<2> > result( af::c_grid<2> (NN,NN));
     double* MM_ptr = MM.begin();
@@ -116,8 +111,46 @@ namespace cctbx { namespace uctbx {
       int i = (int)( (-quad_b + radical)/ (2.*quad_a) );
       int total_count_above_row_i=(NN*i)-((i*i-i)/2)-i;
       int j = idx - total_count_above_row_i + (i+1);
+      // for the NCDist metric, pass in pointers to the two metrical matrices
+      double metric = ncdist2017::NCDist(&(MM_ptr[i*6]),&(MM_ptr[j*6]));
+      result_ptr[i*NN + j] = metric;
+      result_ptr[j*NN + i] = metric;
+      }
+    }
+    return result;
+  }
+
+  scitbx::af::versa<double, scitbx::af::c_grid<2> > CS6Dist_in_G6_flatten(scitbx::af::shared<double> MM){
+    int NN = MM.size()/6;
+    scitbx::af::shared<double> MM_S6(MM.size());
+    af::versa<double, af::c_grid<2> > result( af::c_grid<2> (NN,NN));
+    double* MM_ptr = MM.begin();
+    double* MM_S6_ptr = MM_S6.begin();
+    double* result_ptr = result.begin();
+    // Convert NN primitive Niggli-reduced G6 cells to Selling-reduced S6 cells
+    # pragma omp parallel
+    {
+    # pragma omp for
+    for (int idx = 0; idx < NN; ++idx){
+         ncdist2017::G6toS6Reduce(&MM_ptr[idx*6],&MM_S6_ptr[idx*6]);
+      }
+    }
+    // figure out the number of non-diagonal elements in an NN x NN square matrix
+    int n_elements = (NN*NN-NN)/2;
+    # pragma omp parallel
+    {
+    # pragma omp for
+    for (int idx = 0; idx < n_elements; ++idx) {
+      // Given idx, can we deduce the i and j upper-triangular coordinates?
+      double quad_a = -0.5;
+      double quad_b = (NN-0.5);
+      double quad_c = -(double)(idx);
+      double radical = std::sqrt(quad_b*quad_b - 4.*quad_a*quad_c);
+      int i = (int)( (-quad_b + radical)/ (2.*quad_a) );
+      int total_count_above_row_i=(NN*i)-((i*i-i)/2)-i;
+      int j = idx - total_count_above_row_i + (i+1);
       //
-      double metric = ncdist2017::CS6Dist_in_G6(&(MM_ptr[i*6]),&(MM_ptr[j*6]));
+      double metric = ncdist2017::CS6Dist(&(MM_ptr[i*6]),&(MM_ptr[j*6]));
       result_ptr[i*NN + j] = metric;
       result_ptr[j*NN + i] = metric;
       }
@@ -132,7 +165,14 @@ BOOST_PYTHON_MODULE(determine_unit_cell_ext)
 {
   def ("NCDist",&cctbx::uctbx::NCDist_wrapper);
   def ("NCDist_matrix",&cctbx::uctbx::NCDist_matrix);
-  def ("NCDist_flatten",&cctbx::uctbx::NCDist_flatten);
+  def ("NCDist_flatten",&cctbx::uctbx::NCDist_flatten,
+      "Obtain an NxN flex versa <double> containing NCDist metrics from a list of N G6 vectors.\n"
+      "The resulting matrix is symmetric.\n"
+      "The G6 vectors [a.a, b.b, c.c, 2*b.c, 2*a.c, 2*a.b]\n"
+      "are to be passed in as a flex.double containing 6*N elements.\n"
+      "It is assumed the a,b,c vectors are primitive, Niggli reduced\n"
+      "Uses 2014 library with FAST=true, skips some of the tests for smaller differences\n"
+      );
 # if HAVE_NCDIST
   def ("NCDist2017",&cctbx::uctbx::NCDist2017_wrapper);
   def ("CS6Dist",&cctbx::uctbx::CS6Dist_wrapper);
@@ -140,12 +180,22 @@ BOOST_PYTHON_MODULE(determine_unit_cell_ext)
       "Use this in place of the NCDist wrapper when working with G6 arguments\n"
       "A G6 argument is [a.a, b.b, c.c, 2*b.c, 2*a.c, 2*a.b]\n"
       "It is assumed the arguments are primitive, Niggli reduced\n");
+  def ("NCDist2017_flatten",&cctbx::uctbx::NCDist2017_flatten,(arg("G6")),
+      "Obtain an NxN flex versa <double> containing NCDist metrics from a list of N G6 vectors.\n"
+      "The resulting matrix is symmetric.\n"
+      "The G6 vectors [a.a, b.b, c.c, 2*b.c, 2*a.c, 2*a.b]\n"
+      "are to be passed in as a flex.double containing 6*N elements.\n"
+      "It is assumed the a,b,c vectors are primitive, Niggli reduced\n"
+      "Uses live library with FAST=false, includes the tests for smaller differences\n"
+      );
   def ("CS6Dist_in_G6_flatten",&cctbx::uctbx::CS6Dist_in_G6_flatten,(arg("G6")),
       "Obtain an NxN flex versa <double> containing S6 metrics from a list of N G6 vectors.\n"
       "The resulting matrix is symmetric.\n"
       "The G6 vectors [a.a, b.b, c.c, 2*b.c, 2*a.c, 2*a.b]\n"
       "are to be passed in as a flex.double containing 6*N elements.\n"
-      "It is assumed the a,b,c vectors are primitive, Niggli reduced\n");
-  def ("S6_reduce",&cctbx::uctbx::S6Dist_reduce_wrapper);
+      "It is assumed the a,b,c vectors are primitive, Niggli reduced\n"
+      "Uses live library with FAST=false, includes the tests for smaller differences\n"
+      );
+
 # endif // HAVE_NCDIST
 }
