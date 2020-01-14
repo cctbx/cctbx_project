1b2a17b15cd8a256098d19b176f70905ab7e6380
Billy Poon <bkpoon@lbl.gov>
2020-01-13 16:43:57 -0800

    GUI: add code to connect to ChimeraX

--- qttbx/viewers/__init__.py
+++ qttbx/viewers/__init__.py
@@ -4,6 +4,10 @@ Define basic interfaces for communicating with various viewers
 
 from __future__ import absolute_import, division, print_function
 
+import os
+
+from libtbx.utils import Sorry
+
 # =============================================================================
 class ModelViewer(object):
   """
@@ -18,6 +22,8 @@ class ModelViewer(object):
     * NGLViewer - http://nglviewer.org
   """
 
+  viewer_name = None
+
   def __init__(self):
     self.command = None # path to viewer executable
     self.flags = None   # flags to pass to executable
@@ -25,6 +31,29 @@ class ModelViewer(object):
     self.port = None    # port for communicating with viewer
     self.process = None # viewer process
 
+    self._client = None
+    self._connected = False
+
+  def run_basic_checks(self):
+    """
+    Check that the minimum information is provided about the viewer.
+    """
+    if self.command is None:
+      raise Sorry('The command for {viewer_name} is not set.'
+                  .format(viewer_name=self.viewer_name))
+    if not os.path.isfile(self.command):
+      raise Sorry('The command for {viewer_name} is not available.'
+                  'Please check that {command} exists.'
+                  .format(viewer_name=self.viewer_name, command=self.command))
+    if self.port is None:
+      raise Sorry('The port has not been set.')
+    if not isinstance(self.port, int):
+      raise Sorry('The port should be an integer.')
+    if self.port > 65535:
+      raise Sorry('The port number should be 65535 or lower')
+
+  # ---------------------------------------------------------------------------
+  # Viewer functions
   def start_viewer(self):
     raise NotImplementedError
 
--- qttbx/viewers/chimerax.py
+++ qttbx/viewers/chimerax.py
@@ -10,43 +10,88 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import subprocess
+import sys
 import tempfile
 
-from libtbx.utils import Sorry
+from libtbx.utils import to_str, Sorry
 from qttbx.viewers import ModelViewer
 
 # =============================================================================
 class ChimeraXViewer(ModelViewer):
 
+  viewer_name = 'ChimeraX'
+
+  # ---------------------------------------------------------------------------
   def start_viewer(self):
-    # basic checks
-    if self.command is None:
-      raise Sorry('The command for ChimeraX is not set.')
-    if not os.path.isfile(self.command):
-      raise Sorry('The command for ChimeraX is not available.'
-                  'Please check that {command} exists.'
-                  .format(command=self.command))
-    if self.port is None:
-      raise Sorry('The port has not been set.')
-    if not isinstance(self.port, int):
-      raise Sorry('The port should be an integer.')
-    if self.port > 65535:
-      raise Sorry('The port number should be 65535 or lower')
+    self.run_basic_checks()
 
     # write script
-    self.script = tempfile.NamedTemporaryFile()
-    self.script.write('isolde remote rest start port {port}'.format(port=self.port))
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.cxc', delete=False) as self.script:
+      self.script.write('isolde remote rest start port {port}'.format(port=self.port))
 
     # start viewer
-    command = self.command + ' ' + self.script.name
-    print(command)
-    self.process = subprocess.Popen(args=command)
+    command = [self.command, self.script.name]
+    self.process = subprocess.Popen(args=command, stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE, shell=False)
 
-# =============================================================================
-if __name__ == '__main__':
-  viewer = ChimeraXViewer()
-  viewer.command = '/Users/bkpoon/work/ChimeraX-0.91.app/Contents/bin/ChimeraX'
-  viewer.port = 12546
+    # connect to viewer
+    #self._connect()
 
-  viewer.start_viewer()
+  # ---------------------------------------------------------------------------
+  def close_viewer(self):
+    if os.path.isfile(self.script.name):
+      os.remove(self.script.name)
+
+  # ---------------------------------------------------------------------------
+  def load_model(self, filename=None):
+    model_id = None
+    if os.path.isfile(filename):
+      model_id = self._client.load_model(filename)
+    else:
+      raise Sorry('Model file ({filename}) is not found.'.format(filename=filename))
+    return model_id
+
+  # ---------------------------------------------------------------------------
+  def _connect(self):
+    # find client.py in ISOLDE
+    client_path = None
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.py', delete=False) as python_script:
+      python_script.write("""\
+from chimerax import isolde
+print(isolde.__file__)
+""")
+    command = [self.command, '--nogui', '--exit', '--script', python_script.name]
+    python_process = subprocess.Popen(args=command, stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE, shell=False)
+    stdout, stderr = python_process.communicate()
+    python_process.wait()
+    if python_process.returncode != 0:
+      raise Sorry('The ISOLDE installation location could not be found.')
+    else:
+      if os.path.isfile(python_script.name):
+        try:
+          os.remove(python_script.name)
+        except IOError:
+          pass
+      stdout = to_str(stdout).split('\n')
+      line = None
+      for line in stdout:
+        if 'isolde' in line:
+          break
+      client_path = os.path.abspath(
+        os.path.join(line, '..', 'remote_control', 'rest_server', 'client.py'))
+    if client_path is not None and os.path.isfile(client_path):
+      # import from file
+      if sys.version_info.major > 2:
+        import importlib.util
+        spec = importlib.util.spec_from_file_location('client', client_path)
+        client = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(client)
+      else:
+        import imp
+        client = imp.load_source('client', client_path)
+      self._client = client.IsoldeRESTClient('localhost', self.port)
+      self._client.connect()
+      self._connected = True
+
 # =============================================================================
