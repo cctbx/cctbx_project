e48622da992a15b7378316c0d03c51043ed2cb9f
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2020-03-19 15:06:08 -0700

    removed comments

--- mmtbx/restraints.py
+++ mmtbx/restraints.py
@@ -97,8 +97,6 @@ class manager(object):
       geometry=geometry,
       cartesian_ncs_manager=cartesian_ncs_manager,
       normalization=self.normalization,
-      # use_amber=self.use_amber,
-      # amber_structs=self.amber_structs,
       )
 
   def energies_sites(self,
@@ -128,39 +126,9 @@ class manager(object):
     if (self.geometry is None):
       result.geometry = None
     else:
-      # if (self.use_amber and not force_restraints_model):
-      #   assert 0
-      #   geometry_energy = self.geometry.energies_sites(
-      #     sites_cart=sites_cart,
-      #     flags=geometry_flags,
-      #     external_energy_function=external_energy_function,
-      #     custom_nonbonded_function=custom_nonbonded_function,
-      #     compute_gradients=False,
-      #     gradients=None,
-      #     disable_asu_cache=disable_asu_cache,
-      #     normalization=False)
-      #   ##################################################################
-      #   #                                                                #
-      #   # AMBER CALL - Amber force field gradients and target            #
-      #   #                                                                #
-      #   ##################################################################
-      #   import amber_adaptbx
-      #   amber_geometry_manager = amber_adaptbx.geometry_manager(
-      #     sites_cart=sites_cart,
-      #     number_of_restraints=geometry_energy.number_of_restraints,
-      #     gradients_factory=flex.vec3_double,
-      #     amber_structs=self.amber_structs)
-      #   result.geometry = amber_geometry_manager.energies_sites(
-      #     crystal_symmetry = self.geometry.crystal_symmetry,
-      #     compute_gradients = compute_gradients,
-      #     # these are only available for use_amber=True
-      #     log=self.log,
-      #     print_amber_energies=self.print_amber_energies,
-      #     qmmask=self.qmmask)
-
       if (self.use_afitt and
-            len(sites_cart)==self.afitt_object.total_model_atoms
-            ):
+          len(sites_cart)==self.afitt_object.total_model_atoms
+          ):
         ##################################################################
         #                                                                #
         # AFITT CALL - OpenEye AFITT gradients and target                #
