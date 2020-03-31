813789a6c1c9ebdfcdf935ac62ac0e30b313d866
Pavel Afonine <pafonine@lbl.gov>
2020-03-31 16:58:15 -0700

    Extract out computational part so it can be used elsewhere

--- mmtbx/programs/ramalyze.py
+++ mmtbx/programs/ramalyze.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 import os
+import iotbx.phil
 from mmtbx.validation.ramalyze import ramalyze
 from libtbx.program_template import ProgramTemplate
 from six.moves import range
@@ -9,6 +10,88 @@ try:
 except ImportError:
   pass
 from libtbx.utils import Sorry
+
+master_phil_str = """
+plot = False
+  .type = bool
+  .help = Create graphics of plots (if Matplotlib is installed)
+show_labels = True
+  .type = bool
+  .help = Show labels on outlier residues
+point_style = 'bo'
+  .type = str
+  .help = choose style of points, use matplotlib format from e.g. here: \
+    https://matplotlib.org/api/_as_gen/matplotlib.axes.Axes.plot.html \
+    very small is ',', little bigger is '.'
+markersize=3
+  .type=float
+markerfacecolor = white
+  .type = str
+markeredgecolor="black"
+  .type = str
+show_filling = True
+  .type = bool
+show_contours = True
+  .type = bool
+dpi=100
+  .type=int
+wxplot = False
+  .type = bool
+  .help = Display interactive plots (requires wxPython and Matplotlib)
+outliers_only = False
+  .type = bool
+  .help = "Only display outliers"
+verbose = True
+  .type = bool
+  .help = '''Verbose'''
+output_prefix = None
+  .type = str
+  .help = prefix for outputted plots (if plot=True)
+"""
+
+def master_params():
+  return iotbx.phil.parse(master_phil_str)
+
+def compute(hierarchies, params, log, quiet=False, plot_file_base_default=None):
+  results = []
+  for hierarchy in hierarchies:
+    result = ramalyze(
+      pdb_hierarchy = hierarchy,
+      show_errors   = None,
+      outliers_only = params.outliers_only,
+      out           = log,
+      quiet         = quiet)
+    results.append(result)
+  # combine models
+  result = results[0]
+  for i in range(1,len(results)):
+    result += results[i]
+  if params.verbose:
+    result.show_old_output(out=log, verbose=True)
+  if params.plot:
+    plot_file_base = params.output_prefix
+    if plot_file_base is None:
+      plot_file_base = plot_file_base_default
+    result.write_plots(
+      plot_file_base  = plot_file_base,
+      out             = log,
+      show_labels     = params.show_labels,
+      point_style     = params.point_style,
+      markerfacecolor = params.markerfacecolor,
+      show_filling    = params.show_filling,
+      show_contours   = params.show_contours,
+      dpi             = params.dpi,
+      markeredgecolor = params.markeredgecolor,
+      markersize      = params.markersize)
+  if params.wxplot:
+    try:
+      import wxtbx.app
+    except ImportError as e:
+      raise Sorry("wxPython not available.")
+    else:
+      app = wxtbx.app.CCTBXApp(0)
+      result.display_wx_plots()
+      app.MainLoop()
 
 class Program(ProgramTemplate):
   prog = os.getenv('LIBTBX_DISPATCHER_NAME')
@@ -29,43 +112,9 @@ Example:
 
   # Pavel's style:
   # plot=True show_labels=False markerfacecolor=yellow markeredgecolor=red
-  master_phil_str = """
-    plot = False
-      .type = bool
-      .help = Create graphics of plots (if Matplotlib is installed)
-    show_labels = True
-      .type = bool
-      .help = Show labels on outlier residues
-    point_style = 'bo'
-      .type = str
-      .help = choose style of points, use matplotlib format from e.g. here: \
-        https://matplotlib.org/api/_as_gen/matplotlib.axes.Axes.plot.html \
-        very small is ',', little bigger is '.'
-    markersize=3
-      .type=int
-    markerfacecolor = white
-      .type = str
-    markeredgecolor="black"
-      .type = str
-    show_filling = True
-      .type = bool
-    show_contours = True
-      .type = bool
-    dpi=100
-      .type=int
-    wxplot = False
-      .type = bool
-      .help = Display interactive plots (requires wxPython and Matplotlib)
-    outliers_only = False
-      .type = bool
-      .help = "Only display outliers"
-    verbose = True
-      .type = bool
-      .help = '''Verbose'''
-    output_prefix = None
-      .type = str
-      .help = prefix for outputted plots (if plot=True)
-"""
+
+  master_phil_str = master_phil_str
+
   datatypes = ['model','phil']
   known_article_ids = ['molprobity']
 
@@ -73,48 +122,16 @@ Example:
     self.data_manager.has_models(raise_sorry=True)
 
   def run(self):
-    results = []
+    hierarchies = []
     for model_name in self.data_manager.get_model_names():
       hierarchy = self.data_manager.get_model(model_name).get_hierarchy()
       hierarchy.atoms().reset_i_seq()
-      result = ramalyze(
-        pdb_hierarchy=hierarchy,
-        show_errors=None,
-        outliers_only=self.params.outliers_only,
-        out=self.logger,
-        quiet=False)
-      results.append(result)
-      if len(self.data_manager.get_model_names()) > 1:
-        self.params.verbose=False
-        print('\nmodel : %s' % model_name, file=self.logger)
-    # combine models
-    result = results[0]
-    for i in range(1,len(results)):
-      result += results[i]
-    if self.params.verbose:
-      result.show_old_output(out=self.logger, verbose=True)
-    if self.params.plot :
-      plot_file_base = self.params.output_prefix
-      if plot_file_base is None:
-        plot_file_base = os.path.splitext(os.path.basename(self.data_manager.get_model_names()[0]))[0]
-      result.write_plots(
-          plot_file_base=plot_file_base,
-          out=self.logger,
-          show_labels=self.params.show_labels,
-          point_style=self.params.point_style,
-          markerfacecolor=self.params.markerfacecolor,
-          show_filling=self.params.show_filling,
-          show_contours=self.params.show_contours,
-          dpi=self.params.dpi,
-          markeredgecolor=self.params.markeredgecolor,
-          markersize=self.params.markersize)
-    if self.params.wxplot :
-      try :
-        import wxtbx.app
-      except ImportError as e :
-        raise Sorry("wxPython not available.")
-      else :
-        app = wxtbx.app.CCTBXApp(0)
-        result.display_wx_plots()
-        app.MainLoop()
-
+      hierarchies.append(hierarchy)
+    fb = os.path.splitext(os.path.basename(
+      self.data_manager.get_model_names()[0]))[0]
+    compute(
+      hierarchies            = hierarchies,
+      params                 = self.params,
+      log                    = self.logger,
+      quiet                  = False,
+      plot_file_base_default = fb)
