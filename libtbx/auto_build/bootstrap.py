ae02d1575642dc005839ba4738220c8a29b3bf40
Billy Poon <bkpoon@lbl.gov>
2020-04-01 15:24:40 -0700

    Bump Boost to 1.70

--- libtbx/auto_build/bootstrap.py
+++ libtbx/auto_build/bootstrap.py
@@ -775,7 +775,7 @@ class geostd_module(SourceModule):
 
 class boost_module(SourceModule):
   module = 'boost'
-  anonymous = ['git',
+  anonymous = ['git', '-b 1.70',
                'git@github.com:cctbx/boost.git',
                'https://github.com/cctbx/boost.git',
                'https://github.com/cctbx/boost/archive/master.zip']
