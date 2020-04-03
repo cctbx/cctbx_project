3397b5f2930d3c6215f22904133733050417a932
Derek Mendez <dermen@lbl.gov>
2020-04-02 17:37:07 -0700

    aslkdlaskd

--- simtbx/diffBragg/refiners/rotXYZ_refiner.py
+++ simtbx/diffBragg/refiners/rotXYZ_refiner.py
@@ -138,17 +138,10 @@ class RefineRot(PixelRefinement):
         self.scale_fac = self.x[-1]
 
     def _evaluate_log_averageI(self):
-        # fix log(x<=0)
         bad = self.model_Lambda <= 0
-        self.model_Lambda[bad] = 1e-10
-        self.log_Lambda = np.log(self.model_Lambda)
-        #try:
-        #    self.log_Lambda = np.log(self.model_Lambda)
-        #except FloatingPointError:
-        #    pass
-        #if any((self.model_Lambda <= 0).ravel()):
-        #    print("\n<><><><><><><><>\n\tWARNING: NEGATIVE INTENSITY IN MODEL!!!!!!!!!\n<><><><><><><><><>\n")
-        #    raise ValueError("model of Bragg spots cannot have negative intensities...")
+        mod = self.model_Lambda.copy()
+        mod[bad] = 1
+        self.log_Lambda = np.log(mod)
         #self.log_Lambda[bad] = 0
 
     def compute_functional_and_gradients(self):
