e0a328a96c8192fdf810931ab5f63d67412984ce
Nicholas Sauter <nksauter@lbl.gov>
2020-04-07 17:49:05 -0700

    Test code is in a separate script, tst_sublattice.py

--- rstbx/sublattice_support/change_basis.py
+++ rstbx/sublattice_support/change_basis.py
@@ -141,16 +141,3 @@ class sublattice_relation:
 
     assert len(list(cosets.keys()))==N-1
     return list(cosets.keys())
-
-def example_application():
-  S = sublattice_change_of_basis(6)
-  for item in S.yield_transformations_descending_modulus():
-    #item.show_summary()
-    print (item.sublattice_cosets())
-
-if __name__=="__main__":
-  user_max_modulus = 6
-  S = sublattice_change_of_basis(user_max_modulus)
-  S.show_summary()
-  example_application()
-  print ("OK")
