ef7cee5783682305b691e7a5f21544160e41964f
dwpaley <dwpaley@gmail.com>
2020-04-22 14:00:49 -0400

    output esd's of fp and fdp in smtbx.anom_refine

--- smtbx/regression/tst_commandline_anomrefine.py
+++ smtbx/regression/tst_commandline_anomrefine.py
@@ -8,20 +8,21 @@ insfile=fnames.thpp_ins
 hklfile=fnames.thpp_hkl
 outfile=fnames.thpp_out
 
-cl1 = "smtbx.anom_refine {} {} F -e13000 -t --outfile={} --overwrite".format(
+cl1 = "smtbx.anom_refine {} {} F -e13000 -t --outfile={} --overwrite -d0 -s0".format(
     ciffile, hklfile, outfile)
-cl2 = "smtbx.anom_refine {} {} F -e13000 -t --outfile={} --overwrite".format(
+cl2 = "smtbx.anom_refine {} {} F -e13000 -T --outfile={} --overwrite -d0 -s0".format(
     insfile, hklfile, outfile)
 
-out1 = "13000.0 0.152 -0.053 0.744 1.303"
-out2 = "13000.0 -0.032 0.001 0.006 0.008"
+out1 = ["13000.0  0.152 -0.053  0.743  1.303"]
+out2 = ["13000.0 -0.032  0.001  0.006  0.008",
+        "         0.016  0.017  4.121  4.466"]
 
 def run():
   for cl,out in zip((cl1,cl2), (out1,out2)):
     print(cl)
     assert call(cl)==0, "smtbx.anom_refine refinement failed"
-    with open(outfile) as f: result = f.readlines()[0].strip()
-    assert out == result, "{} | {}".format(result, out)
+    with open(outfile) as f: result = [l.rstrip() for l in f.readlines()]
+    assert out == result, "{}\n{}".format('\n'.join(result), '\n'.join(out))
   os.remove(outfile)
 
 
