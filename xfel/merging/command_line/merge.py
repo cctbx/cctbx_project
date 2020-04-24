0674b46b5cfdb59770df524316276bbc004b51a5
Aaron Brewster <asbrewster@lbl.gov>
2020-04-24 15:41:40 -0700

    Change step_list from multiple to strings.
    Makes command line specification of steps a bit easier.

--- xfel/merging/command_line/merge.py
+++ xfel/merging/command_line/merge.py
@@ -101,7 +101,7 @@ class Script(object):
     import importlib
 
     workers = []
-    steps = default_steps if self.params.dispatch.step_list == [] else self.params.dispatch.step_list
+    steps = self.params.dispatch.step_list if self.params.dispatch.step_list else default_steps
     for step in steps:
       step_factory_name = step
       step_additional_info = []
