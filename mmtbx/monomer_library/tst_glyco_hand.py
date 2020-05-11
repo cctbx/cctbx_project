a3b090bd64a38306e98d27885f1977c13769685a
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2020-05-11 12:13:34 -0700

    actual test script

---
+++ mmtbx/monomer_library/tst_glyco_hand.py
@@ -0,0 +1,19 @@
+from mmtbx.monomer_library.glyco_chiral_values import alpha_beta
+
+def main():
+  print 'codes',len(alpha_beta)
+  tests = {"NAG" : "beta",
+           'NDG' : 'alpha',
+           'MAN' : "alpha",
+           'BMA' : "beta",
+           "FUC" : "alpha",
+           "FUL" : "beta",
+           'BDP' : 'beta',
+           'GCU' : 'alpha',
+           }
+  for code in tests:
+    print "  %-3s %-8s %s" % (code, tests[code], alpha_beta[code])
+    assert tests[code]==alpha_beta[code]
+
+if __name__ == '__main__':
+  main()
