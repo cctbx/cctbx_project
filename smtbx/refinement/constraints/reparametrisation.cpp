19d01c9b96e0c7640e3abe21271d3d1d3cae9170
pcxod <oleg_dolomanov@hotmail.com>
2020-05-24 15:19:01 +0100

    - restoring some merging-by removed code...

--- smtbx/refinement/constraints/reparametrisation.cpp
+++ smtbx/refinement/constraints/reparametrisation.cpp
@@ -291,7 +291,11 @@ namespace smtbx { namespace refinement { namespace constraints {
     scatterer->fdp = value;
   }
 
-
+  void asu_fdp_parameter::validate() {
+    if (value < 0) {
+      value *= -1;
+    }
+  }
   // reparametrisation
 
   void reparametrisation
