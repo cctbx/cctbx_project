4500a3b76da5fecfbff65e86678c70fb107d0139
pcxod <oleg_dolomanov@hotmail.com>
2020-05-24 16:28:44 +0100

    - reverting to the original!

--- libtbx/path.py
+++ libtbx/path.py
@@ -42,6 +42,11 @@ def posix_relpath(
   return op.join(*rel_list)
 
 def nt_is_true_abs(path):
+  try: # splitunc is removed in Python 3.7,
+    prefix, _ = op.splitunc(path)
+    if (bool(prefix)): return True
+  except AttributeError:
+    pass
   prefix, _ = op.splitdrive(path)
   return bool(prefix)
 
@@ -60,8 +65,13 @@ def nt_relpath(
   start_abs = abs_norm(start)
   path_abs = abs_norm(path)
   def _abspath_split(abs):
-    is_unc = op.ismount(abs)
-    prefix, rest = op.splitdrive(abs)
+    try: # splitunc is removed in Python 3.7
+      prefix, rest = op.splitunc(abs)
+      is_unc = bool(prefix)
+    except AttributeError:
+      is_unc = False
+    if not is_unc:
+      prefix, rest = op.splitdrive(abs)
     rest_list = []
     for _ in rest.split("\\"):
       if _: rest_list.append(_)
