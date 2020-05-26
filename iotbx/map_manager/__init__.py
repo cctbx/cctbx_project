fcbc06b9cd3723652c5a5c2642581389aa20ac5e
Billy Poon <bkpoon@lbl.gov>
2020-05-26 16:45:18 -0700

    clean clutter

--- iotbx/map_manager/__init__.py
+++ iotbx/map_manager/__init__.py
@@ -505,7 +505,6 @@ class map_manager(map_reader,write_ccp4_map):
      )
 
   def is_similar(self,other=None):
-    from libtbx.test_utils import approx_equal
     # Check to make sure origin, gridding and symmetry are similar
     if self.origin_shift_grid_units != other.origin_shift_grid_units:
       return False
