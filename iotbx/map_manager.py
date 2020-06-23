74010d1d28fcc8ff0399a397904d5614d59a85df
terwill <tterwilliger@newmexicoconsortium.org>
2020-06-23 10:09:07 -0700

    Restore setstate and six.moves version of StringIO

--- iotbx/map_manager.py
+++ iotbx/map_manager.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function
 from libtbx.utils import to_str
 from libtbx import group_args
 import sys
+import io
 from cctbx import miller
 from iotbx.mrcfile import map_reader, write_ccp4_map
 from scitbx.array_family import flex
@@ -181,7 +182,7 @@ class map_manager(map_reader, write_ccp4_map):
      unit_cell_grid = None,
      unit_cell_crystal_symmetry = None,
      origin_shift_grid_units = None, # OPTIONAL first point in map in full cell
-     log = sys.stdout,
+     log = None,
      ):
 
     '''
@@ -278,6 +279,19 @@ class map_manager(map_reader, write_ccp4_map):
     # make sure labels are strings
     if self.labels is not None:
       self.labels = [to_str(label, codec = 'utf8') for label in self.labels]
+
+  # prevent pickling error in Python 3 with self.log = sys.stdout
+  # unpickling is limited to restoring sys.stdout
+  def __getstate__(self):
+    pickle_dict = self.__dict__.copy()
+    if isinstance(self.log, io.TextIOWrapper):
+      pickle_dict['log'] = None
+    return pickle_dict
+
+  def __setstate__(self, pickle_dict):
+    self.__dict__ = pickle_dict
+    if self.log is None:
+      self.log = sys.stdout
 
   def set_log(self, log = sys.stdout):
     '''
@@ -586,7 +600,7 @@ class map_manager(map_reader, write_ccp4_map):
     if map_data.origin()  ==  (0, 0, 0):  # Usual
       self._print("Writing map with origin at %s and size of %s to %s" %(
         str(origin_shift_grid_units), str(map_data.all()), file_name))
-      from cStringIO import StringIO
+      from six.moves import StringIO
       f=StringIO()
       write_ccp4_map(
         file_name   = file_name,
