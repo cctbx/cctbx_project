83499c1c0e8d7b2c30eb1524a35e7f9eca7ef7ba
Johannes Blaschke <jpblaschke@lbl.gov>
2020-06-28 00:06:21 -0700

    add more nvtx tags

--- simtbx/nanoBragg/nanoBragg_nvtx.cu
+++ simtbx/nanoBragg/nanoBragg_nvtx.cu
@@ -7,6 +7,7 @@
 namespace simtbx {
 namespace nanoBragg {
 
+
 void nanoBragg::add_nanoBragg_spots_nvtx(){
 
     nvtxRangePushA("add_nanoBragg_spots");
@@ -16,6 +17,7 @@ void nanoBragg::add_nanoBragg_spots_nvtx(){
 }
 
 
+
 void nanoBragg::add_nanoBragg_spots_cuda_nvtx(){
 
     nvtxRangePushA("add_nanoBragg_spots_cuda");
@@ -23,6 +25,29 @@ void nanoBragg::add_nanoBragg_spots_cuda_nvtx(){
     nvtxRangePop();
 
 }
+
+
+
+void nanoBragg::add_noise_nvtx(){
+
+    nvtxRangePushA("add_noise");
+    add_noise();
+    nvtxRangePop();
+
+}
+
+
+
+void nanoBragg::to_smv_format_nvtx(std::string const & fileout, double
+                                   intfile_scale, int debug_x, int debug_y){
+
+    nvtxRangePushA("to_smv_format");
+    to_smv_format(fileout, intfile_scale, debug_x, debug_y);
+    nvtxRangePop();
+
+}
+
+
 
 } // namespace nanoBragg
 } // namespace simtbx
