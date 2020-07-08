f66badda803ac893989baa08f351a95b62f02b54
Graeme Winter <graeme.winter@gmail.com>
2020-07-08 05:31:24 +0100

    Clean up test
    Set a random seed to make test more stable (though should be independent of
    this), fix some typos and clean up / make consistent variable names

--- cbflib_adaptbx/exercise_compression.py
+++ cbflib_adaptbx/exercise_compression.py
@@ -8,30 +8,32 @@ def create_random_data_with_gaussian_distribution(mu=0.0,sigma=1.0):
   slow=2048
   fast=2048
   total_sz = slow*fast
+  flex.set_random_seed(2048)
   random_data = flex.random_int_gaussian_distribution(total_sz,mu,sigma)
   random_data.reshape(flex.grid(slow,fast))
   return random_data
 
 def basic_tests(verbose=True):
-  initial_intdata = create_random_data_with_gaussian_distribution(0.0,100.0)
+  initial_data = create_random_data_with_gaussian_distribution(0.0,100.0)
 
   #special deltas to test the compression algorithm
   addresses = [3,6,9,12,15,18]
   deltas = [-127,128,-32767,32768,-2147483647,2147483647]
   for x in range(6):
-    initial_intdata[addresses[x]-1]=0
-    initial_intdata[addresses[x]]=deltas[x]
+    initial_data[addresses[x]-1]=0
+    initial_data[addresses[x]]=deltas[x]
 
   if verbose: P=Profiler("compress")
-  array_shape = initial_intdata.focus()
+  array_shape = initial_data.focus()
   if verbose: print(array_shape)
-  compressed = compress(initial_intdata)
-  if verbose: print(len(compressed))
+  compressed_data = compress(initial_data)
+  if verbose: print(len(compressed_data))
   if verbose: P=Profiler("uncompress")
-  decompressed_dat = uncompress(packed=compressed, fast=array_shape[1], slow=array_shape[0])
+  decompressed_data = uncompress(packed=compressed_data, fast=array_shape[1],
+                                 slow=array_shape[0])
 
   if verbose: del P
-  assert assert_equal(initial_intdata, decompressed_dat)
+  assert assert_equal(initial_data, decompressed_data)
 
 if __name__=="__main__":
   basic_tests(False)
