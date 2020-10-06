ef5096739c53afe7aabe7e7ec509b3dacb47dca6
Pavel Afonine <pafonine@lbl.gov>
2020-10-06 09:33:36 -0700

    Removing model_input and processed_pdb_file from model

--- mmtbx/regression/model/tst_model_tors_ref_restraints.py
+++ mmtbx/regression/model/tst_model_tors_ref_restraints.py
@@ -12,11 +12,11 @@ def exercise_adopting_ref_tors_restraints_h():
   params.pdb_interpretation.flip_symmetric_amino_acids=False
   inp_1 = iotbx.pdb.input(lines=tst_model_cart_ref_restraints.pdb_str_h, source_info=None)
   h_model = mmtbx.model.manager(model_input = inp_1,
-                                pdb_interpretation_params=params)
+                                pdb_interpretation_params=params, build_grm=True)
 
   inp_2 = iotbx.pdb.input(lines=tst_model_cart_ref_restraints.pdb_str, source_info=None)
   model = mmtbx.model.manager(model_input = inp_2,
-                              pdb_interpretation_params=params)
+                              pdb_interpretation_params=params, build_grm=True)
 
   params = mmtbx.model.manager.get_default_pdb_interpretation_params()
   params.pdb_interpretation.flip_symmetric_amino_acids=False
