4b3a1d361fc4bd7da26a26c40cc09a4dac4fa99e
Nicholas K Sauter <nksauter@lbl.gov>
2020-11-19 13:54:48 -0800

    argchk housekeeping
    Implement the GAUSS_ARGCHK shapetype on CPU.
    Test it on both CPU and GPU.

--- simtbx/nanoBragg/nanotypes.h
+++ simtbx/nanoBragg/nanotypes.h
@@ -11,7 +11,7 @@
 typedef enum { SAMPLE, BEAM } pivot;
 typedef enum { UNKNOWN, SQUARE, ROUND, GAUSS, GAUSS_ARGCHK, TOPHAT, FIBER } shapetype;
 // GAUSS_ARGCHK provides a lightweight backdoor for efficient implementation of
-// the GAUSS shapetype in CUDA. Developers cautioned accordingly.
+// the GAUSS shapetype in CUDA. Precaluates the argument of the exponential.
 typedef enum { CUSTOM, ADXV, MOSFLM, XDS, DIALS, DENZO } convention;
 
 #endif /* NANOTYPES_H_ */
