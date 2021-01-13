97c229f53f02ba3fcdbee96be36c06e36937d091
Nicholas Sauter <nksauter@lbl.gov>
2021-01-13 13:02:22 -0800

    Second draft of coset voting

--- xfel/merging/application/modify/df_cosym.py
+++ xfel/merging/application/modify/df_cosym.py
@@ -1,58 +1,9 @@
 from __future__ import absolute_import, division, print_function
 from pandas import DataFrame as df
 import pandas as pd
-import pickle
 from scitbx.array_family import flex
-import numpy as np
 from collections import OrderedDict
 from itertools import permutations
-
-def iop():
-  with open("../merge_002/myfile","rb") as F:
-    refdata = pickle.load(F)
-  symops = list(refdata.keys())[1:]
-
-  refdata["max"] = refdata[symops].max(axis=1)
-  refdata["min"] = refdata[symops].min(axis=1)
-  refdata['diff'] = refdata["max"] - refdata["min"]
-  refdata["reindex_op"] = refdata[symops].idxmax(axis=1)
-
-  with open("cosym_myfile","rb") as F:
-    cosdata = pickle.load(F)
-
-  #inner join
-  merged_inner = pd.merge(left=refdata, right=cosdata, left_on='experiment', right_on='experiment')
-
-
-  same = []
-  diff_same = flex.double()
-  diff_diff = flex.double()
-  for idx in range(len(merged_inner["reindex_op_x"])):
-    X,Y = merged_inner["reindex_op_x"][idx],merged_inner["reindex_op_y"][idx]
-    #if (X == "h,k,l" and
-    #    Y == "h,k,l") or (
-    #    X == "h,-h-k,-l" and
-    #    Y == "-h,-l,-k"):
-    #  same.append(False)
-    #else: same.append(True)
-    if (X == Y):
-      same.append(True)
-      diff_same.append(merged_inner["diff"][idx])
-    else:
-      same.append(False)
-      diff_diff.append(merged_inner["diff"][idx])
-  merged_inner['same'] = same
-
-
-  pd.set_option('display.max_rows', 300)
-  # What's the size of the output data?
-  print(merged_inner.shape)
-  print(merged_inner)
-  A = len(merged_inner["same"])
-  B = len(merged_inner[merged_inner["same"]==True])
-  print("%d of %d comparisons match, %0.2f%%"%(B,A,100.*B/A))
-  print("with same answers, diff was ", flex.mean(diff_same))
-  print("with opposite answers, diff was ", flex.mean(diff_diff))
 
 class reconcile_cosym_reports:
   def __init__(self, reports):
@@ -64,36 +15,7 @@ class reconcile_cosym_reports:
     self.reference_partitions = reports[0][1].partitions
     self.result_dataframe = None
     self.n_cosets = len(self.reference_partitions)
-  def merge_all(self):
-    pd.set_option('display.max_rows', 300)
-    pd.set_option('display.max_columns', 8)
-    pd.set_option("display.width", None)
-    for item in self.reports[1:]:
-      item_dataframe = item[0]
 
-      reindexed_dataframes = [] # for each report, index it over all possible cosets
-      anchor_item_inner = pd.merge(left=self.anchor_dataframe, right=item_dataframe, left_on='experiment', right_on='experiment')
-      agreement_scores = np.array([0]*self.n_cosets,dtype=int)
-      coset_x = np.array(anchor_item_inner["coset_x"],dtype=int)
-      for icos in range(self.n_cosets): # loop over each coset, score agreement with the anchor
-        coset_y = (np.array(anchor_item_inner["coset_y"],dtype=int) + icos) % self.n_cosets
-        anchor_item_inner["agree"] = (coset_x==coset_y)
-        n_agree = anchor_item_inner["agree"].value_counts()[True]
-        agreement_scores[icos]=n_agree
-        #print("Agree:",n_agree)
-        #print(anchor_item_inner)
-      consensus_icoset = np.argmax(agreement_scores)
-      print("The consensus is ",consensus_icoset)
-      #reset the agreement column to consensus coset
-      if True:
-        coset_y = (np.array(anchor_item_inner["coset_y"],dtype=int) + consensus_icoset) % self.n_cosets
-        anchor_item_inner["agree"] = (coset_x==coset_y)
-      item_dataframe["coset"] = (np.array(item_dataframe["coset"],dtype=int) + consensus_icoset) % self.n_cosets
-      item_dataframe["keep"] = np.array([True]*len(item_dataframe["coset"]),dtype=bool) # fill a dummy value, keep all the new elements
-      # update the item_dataframe keep column with agree flags from anchor_item_inner.  Only keep experiments that agree.
-      print(item_dataframe)
-      print(anchor_item_inner)
-      return
   def simple_merge(self, voting_method="consensus"):
     print("simple merge")
     pd.set_option('display.max_rows', 300)
