eb3b79cbf2aab5c1558c8f8db81fd8f812cb64c9
Vincent Chen <vbchen@gmail.com>
2021-01-26 19:39:42 -0500

    adding assert messages to tst_remediator

--- iotbx/pdb/remediation/tst_remediator.py
+++ iotbx/pdb/remediation/tst_remediator.py
@@ -13,7 +13,8 @@ def test_pdb_to_v3(old_pdb_file, v3_pdb_file):
   v3_pdb_model = remediator.get_model_from_file(v3_pdb_file)
   #write_model_to_file(old_pdb_model_v3.model_as_pdb(), "old_file_remed.pdb")
   #write_model_to_file(v3_pdb_model.model_as_pdb(), "v3_file.pdb")
-  assert old_pdb_model_v3.get_hierarchy().as_pdb_string() == v3_pdb_model.get_hierarchy().as_pdb_string()
+  assert old_pdb_model_v3.get_hierarchy().as_pdb_string() == v3_pdb_model.get_hierarchy().as_pdb_string(), \
+    "remediation test of "+old_pdb_file+" to v3 failed"
   print("OK")
   #print(old_pdb_model_v3.get_hierarchy().is_similar_hierarchy(v3_pdb_model.get_hierarchy()))
   #print(old_pdb_model.get_hierarchy().is_similar_hierarchy(v3_pdb_model.get_hierarchy()))
@@ -23,18 +24,19 @@ def test_pdb_to_v23(pdb_file):
 
 def test_pdb_remediate_cycle(pdb_file):
   t0 = time.time()
+  print("testing remediation cycle of "+pdb_file)
   remediatorObj = remediator.Remediator()
   pdb_model = remediator.get_model_from_file(pdb_file)
   pdb_model = remediatorObj.remediate_model_object(pdb_model, True)
   #write_model_to_file(pdb_model.model_as_pdb(), "input_model.pdb")
   print("............checking if remediated "+pdb_file+" is v3")
-  assert remediatorObj.is_model_v3(pdb_model)
+  assert remediatorObj.is_model_v3(pdb_model), "remediation of "+pdb_file+ " during remediation cycle test failed"
   pdb_model = remediatorObj.remediate_model_object(pdb_model, False)
   #write_model_to_file(pdb_model.model_as_pdb(), "old_model.pdb")
   pdb_model = remediatorObj.remediate_model_object(pdb_model, True)
   #write_model_to_file(pdb_model.model_as_pdb(), "reold_model.pdb")
   print("............checking remediated-to-v2.3 and remediated "+pdb_file+" is v3")
-  assert remediatorObj.is_model_v3(pdb_model)
+  assert remediatorObj.is_model_v3(pdb_model), "cycling of "+pdb_file+" from v3 to v2.3 back to v3 failed"
   print("OK. "+pdb_file+" time: %8.3f"%(time.time()-t0))
 
 def write_model_to_file(model, file_name):
