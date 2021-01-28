36ead88670425f620561ca9977808c7b1404cbf4
dcliebschner <dcliebschner@lbl.gov>
2021-01-28 11:28:22 -0800

    box_around_molecule: better automatic filename and format handling

--- iotbx/programs/box_around_molecule.py
+++ iotbx/programs/box_around_molecule.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division, print_function
+import os
 from libtbx.program_template import ProgramTemplate
 #from libtbx.utils import date_and_time
 import libtbx.load_env
@@ -8,9 +9,10 @@ master_phil_str = '''
 buffer_layer = 5.0
   .type = float
   .help = buffer around atoms, in Angstrom
-output
-{
-  format = *pdb cif
+output {
+  suffix = _box
+    .type = str
+  format = *Auto pdb cif
     .type = choice(multi=False)
     .help = output model file type
 }
@@ -27,20 +29,16 @@ Inputs:
   optional: buffer layer in Angstrom (default is 5.0 A)
 
 Usage examples:
-  1) Print PDB format string of model (input is mmCIF)
+  1) Save model as PDB (input is mmCIF)
      iotbx.pdb.box_around_molecule.py test.cif format=pdb
 
-  2) Print mmCIF format string of model (input is PDB format)
+  2) Save model as mmCIF (input is PDB)
   pdb.box_around_molecule.py test.pdb format=cif
-
-  3) Print and save model in PDB format (input is PDB format)
-    pdb.box_around_molecule.py test.pdb format=pdb prefix=toto
 
   The program template accepts PDB and mmCIF input formats.
   The paramter output.format allows choosing the output format.
-  If output filename or prefix is given, a file is saved; otherwise, the result
-  is only printed.
 '''
+
   datatypes = ['model', 'phil']
   master_phil_str = master_phil_str
 
@@ -55,21 +53,39 @@ Usage examples:
 
   def run(self):
     #
+
     self.model = self.data_manager.get_model()
     self.model = shift_and_box_model(model = self.model,
-                                     box_cushion = self.params.buffer_layer
-                                     )
-    # PDB format
-    if (self.params.output.format == 'pdb'):
-      # If output file should contain REMARK with buffer_layer
-      #model_str = 'REMARK %s --buffer-layer=%.6g %s\n' % (
-      #            libtbx.env.dispatcher_name, self.params.buffer_layer, fn) + \
-      #       'REMARK %s\n' % date_and_time() + \
-      #       self.model.model_as_pdb()
-      print(self.model.model_as_pdb(), file=self.logger)
-    # mmCIF format
-    if (self.params.output.format == 'cif'):
-      print(self.model.model_as_mmcif(), file=self.logger)
+                                     box_cushion = self.params.buffer_layer)
+#    # PDB format
+#    if (self.params.output.format == 'pdb'):
+#      # If output file should contain REMARK with buffer_layer
+#      #model_str = 'REMARK %s --buffer-layer=%.6g %s\n' % (
+#      #            libtbx.env.dispatcher_name, self.params.buffer_layer, fn) + \
+#      #       'REMARK %s\n' % date_and_time() + \
+#      #       self.model.model_as_pdb()
+#      print(self.model.model_as_pdb(), file=self.logger)
 
+    # Get output filename if not provided
+    fn = self.data_manager.get_default_model_name()
+    basename = os.path.splitext(os.path.basename(fn))[0]
+    if self.params.output.prefix is None:
+      self.params.output.prefix = basename
+      self.data_manager.set_default_output_filename(self.get_default_output_filename())
+
+    # data manager has some hickups with formatting, so get it here
+    # (culprit is function in model, see below)
+    if self.params.output.format == 'Auto':
+      if (fn.endswith('.pdb')):
+      #if self.model.input_model_format_pdb(): #broken
+        self.params.output.format = 'pdb'
+      elif (fn.endswith('.cif')):
+      #elif self.model.input_model_format_cif(): #broken
+        self.params.output.format = 'cif'
+    extension = '.' + self.params.output.format
+
+    # save output
     self.data_manager.write_model_file(self.model,
                                        format = self.params.output.format)
+    output_fn = self.data_manager.get_default_output_model_filename(extension=extension)
+    print('Created file', output_fn, file=self.logger)
