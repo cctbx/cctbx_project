ef411b2fdeec954dea53254cb9d28dfb63ee7254
Billy K. Poon <bkpoon@lbl.gov>
2021-01-30 16:32:22 -0800

    libtbx: Add information fields to ProgramTemplate
    - Add keywords for searching
    - Add maintainers for contacts
    - Some cleanup

--- libtbx/program_template.py
+++ libtbx/program_template.py
@@ -45,6 +45,12 @@ class ProgramTemplate(object):
 Program Description
 '''
 
+  # list of keywords for categorizing the program (optional)
+  keywords = None
+
+  # list of maintainer(s) (GitHub names) for the program (optional)
+  maintainers = None
+
   # datatypes for program
   # see iotbx/data_manager/<datatype>.py for list of supported datatypes
   # default datatypes are set in iotbx/data_manager/__init__.py (default_datatypes)
@@ -120,11 +126,24 @@ output {
   phil_converters = list()
 
   # ---------------------------------------------------------------------------
+  # Convenience features
+  def _print(self, text):
+    '''
+    Print function that just replaces print(text, file=self.logger)
+    '''
+    print(text, file=self.logger)
+
+  def header(self, text):
+    self._print("-"*79)
+    self._print(text)
+    self._print("*"*len(text))
+
+  # ---------------------------------------------------------------------------
   # Function for showing default citation for template
   @staticmethod
   def show_template_citation(text_width=80, logger=None,
                              citation_format='default'):
-    assert(logger is not None)
+    assert logger is not None
 
     print('\nGeneral citation for CCTBX:', file=logger)
     print('-'*text_width, file=logger)
@@ -159,12 +178,12 @@ output {
     self.params = params
     self.logger = logger
 
-    if (self.logger is None):
+    if self.logger is None:
       self.logger = multi_out()
 
     # master_phil should be provided by CCTBXParser or GUI because of
     # potential PHIL extensions
-    if (self.master_phil is None):
+    if self.master_phil is None:
       self.master_phil = libtbx.phil.parse(
         self.master_phil_str, process_includes=True)
 
@@ -180,11 +199,6 @@ output {
 
     # optional initialization
     self.custom_init()
-
-  def header(self, text):
-    print("-"*79, file=self.logger)
-    print(text, file=self.logger)
-    print("*"*len(text), file=self.logger)
 
   # ---------------------------------------------------------------------------
   def custom_init(self):
