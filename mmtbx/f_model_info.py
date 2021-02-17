366e026ef5c6c3df7444a18421a94b77a7460558
Pavel <pafonine@lbl.gov>
2021-02-16 16:28:10 -0800

    add clarification

--- mmtbx/f_model_info.py
+++ mmtbx/f_model_info.py
@@ -7,5 +7,5 @@ versions of the Phenix GUI where the class for storing results in a pickle,
 phenix.runtime.result_summary, has an import statement for this file.
 Pickling an instance of result_summary stored the location of this file in the
 cctbx hierarchy. The import statement in the result_summary class has been
-moved outside of the class definition to prevent future issues.
+moved outside of the class definition to prevent future issues. Moved to f_model.py
 '''
