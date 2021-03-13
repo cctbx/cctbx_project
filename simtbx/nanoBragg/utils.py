01e2593ee1c213528b07c912685845298083f27a
Daniel Paley <dwpaley@gmail.com>
2021-03-12 17:09:44 -0800

    py2 compatibility

--- simtbx/nanoBragg/utils.py
+++ simtbx/nanoBragg/utils.py
@@ -333,7 +333,8 @@ def downsample_spectrum(energies, fluences, total_flux=1e12, nbins=100, method=0
     ydata = np.hstack((fluences[:tail], fluences[-tail:]))
     pfit = np.polyfit(xdata, ydata, deg=1)
     baseline = np.polyval(pfit, energies)
-    denz = signal.filtfilt(*signal.butter(filt_order, filt_freq,'low'), fluences-baseline)
+    butter_b, butter_a = signal.butter(filt_order, filt_freq, 'low')
+    denz = signal.filtfilt(butter_b, butter_a, fluences-baseline)
     enbin = np.arange(energies.min(), energies.max(), delta_en)
     fluences, _, _ = binned_statistic(energies, denz, bins=enbin)
     energies = (enbin[1:] + enbin[:-1])*0.5
