3f7ae6be048c6da47ffc9ee5f5722b2dd72afc77
Pavel <pafonine@lbl.gov>
2021-03-29 10:24:04 -0700

    Individual isotropic ADP refinement against the map via Fourier space, primed with group ADP refinement.

--- mmtbx/refinement/real_space/adp.py
+++ mmtbx/refinement/real_space/adp.py
@@ -7,6 +7,11 @@ from libtbx import easy_mp
 from mmtbx import bulk_solvent
 from libtbx.test_utils import approx_equal
 from six.moves import range
+from cctbx import crystal
+
+from mmtbx.refinement import adp_refinement
+from cctbx import adp_restraints
+from libtbx import group_args
 
 import boost_adaptbx.boost.python as bp
 cctbx_maptbx_ext = bp.import_ext("cctbx_maptbx_ext")
@@ -41,13 +46,96 @@ def map_and_model_to_fmodel(map_data, xray_structure, atom_radius, d_min,
     remove_outliers=False)
   return fmodel
 
+def get_plain_pair_sym_table(crystal_symmetry, sites_frac, plain_pairs_radius=5):
+  asu_mappings = crystal.symmetry.asu_mappings(crystal_symmetry,
+    buffer_thickness = plain_pairs_radius)
+  special_position_settings = crystal.special_position_settings(
+    crystal_symmetry = crystal_symmetry)
+  sites_cart = crystal_symmetry.unit_cell().orthogonalize(sites_frac)
+  site_symmetry_table = special_position_settings.site_symmetry_table(
+    sites_cart = sites_cart)
+  asu_mappings.process_sites_frac(
+    original_sites      = sites_frac,
+    site_symmetry_table = site_symmetry_table)
+  pair_asu_table = crystal.pair_asu_table(asu_mappings=asu_mappings)
+  pair_asu_table.add_all_pairs(distance_cutoff = plain_pairs_radius)
+  return pair_asu_table.extract_pair_sym_table()
+
+class tg(object):
+  def __init__(self, fmodel, x):
+    self.fmodel = fmodel
+    self.plain_pair_sym_table = get_plain_pair_sym_table(
+      crystal_symmetry = self.fmodel.xray_structure.crystal_symmetry(),
+      sites_frac       = self.fmodel.xray_structure.sites_frac())
+    self.adp_iso_params = \
+      adp_refinement.adp_restraints_master_params.extract().iso
+    self.fmodel.xray_structure.scatterers().flags_set_grads(state=False)
+    self.fmodel.xray_structure.scatterers().flags_set_grad_u_iso(
+      iselection = self.fmodel.xray_structure.all_selection().iselection())
+    # required fields
+    self.x = x
+    self.t = None
+    self.g = None
+    self.d = None
+    self.use_curvatures=False
+    #
+    self.weight = self._weight()
+    self.tgo = self._compute(x = self.x)
+    self.update_target_and_grads(x=x)
+
+  def _weight(self):
+    num = self._restraints().gradients.norm()
+    den = self._data().gradient_xray.norm()
+    if(den==0): return 1
+    return num/den
+
+  def _restraints(self):
+    return adp_restraints.energies_iso(
+      plain_pair_sym_table = self.plain_pair_sym_table,
+      xray_structure       = self.fmodel.xray_structure,
+      parameters           = self.adp_iso_params,
+      compute_gradients    = True,
+      use_u_local_only     = self.adp_iso_params.use_u_local_only,
+      use_hd               = False)
+
+  def _data(self):
+    fmodels = mmtbx.fmodels(fmodel_xray = self.fmodel)
+    return fmodels.target_and_gradients(compute_gradients=True)
+
+  def _compute(self, x):
+    self.fmodel.xray_structure.set_b_iso(values = x)
+    self.fmodel.update_xray_structure(update_f_calc = True)
+    R = self._restraints()
+    D = self._data()
+    self.tgo = group_args(
+      target   = D.target()*self.weight + R.target,
+      gradient = D.gradient_xray*self.weight + R.gradients)
+    return self.tgo
+
+  def update(self, x):
+    self.update_target_and_grads(x = x)
+
+  def update_target_and_grads(self, x):
+    self.x = x
+    self.tgo = self._compute(x=self.x)
+    self.t = self.tgo.target
+    self.g = self.tgo.gradient
+
+  def target(self): return self.t
+
+  def gradients(self): return self.g
+
+  def gradient(self): return self.gradients()
+
 class ncs_aware_refinement(object):
-  def __init__(self, map_model_manager, d_min, atom_radius, nproc=1, log = None):
-    self.mmm   = map_model_manager
-    self.nproc = nproc
-    self.d_min = d_min
+  def __init__(self, map_model_manager, d_min, atom_radius, nproc=1,
+               log = None, individual = True):
+    self.mmm         = map_model_manager
+    self.nproc       = nproc
+    self.d_min       = d_min
     self.atom_radius = atom_radius
     self.log         = log
+    self.individual  = individual
     #
     if(self.nproc>1): self.log = None
     #
@@ -94,16 +182,6 @@ class ncs_aware_refinement(object):
       xray_structure = model.get_xray_structure(),
       atom_radius    = self.atom_radius,
       d_min          = self.d_min)
-    #
-    #from mmtbx.refinement import adp_refinement
-    #adp_iso_params = adp_refinement.adp_restraints_master_params.extract().iso
-    #energies_adp_iso = self.restraints_manager.energies_adp_iso(
-    #    xray_structure    = fmodel.xray_structure,
-    #    parameters        = adp_iso_params,
-    #    use_u_local_only  = adp_iso_params.use_u_local_only,
-    #    use_hd            = False,
-    #    compute_gradients = True)
-    #
     # selections for group ADP
     ph_box = model.get_hierarchy()
     ph_box.atoms().reset_i_seq()
@@ -111,14 +189,33 @@ class ncs_aware_refinement(object):
     for rg in ph_box.residue_groups():
       group_adp_sel.append(rg.atoms().extract_i_seq())
     #
+    number_of_macro_cycles = 3
+    if(self.individual): number_of_macro_cycles = 1
     group_b_manager = mmtbx.refinement.group.manager(
       fmodel                   = fmodel,
       selections               = group_adp_sel,
       convergence_test         = False,
       max_number_of_iterations = 50,
-      number_of_macro_cycles   = 3,
+      number_of_macro_cycles   = number_of_macro_cycles,
       run_finite_differences_test = False,
       use_restraints           = True,
       refine_adp               = True,
       log                      = self.log)
+    #
+    if(self.individual):
+      from mmtbx.ncs import tncs
+      print("r_work (start): %6.4f"%fmodel.r_work(), file=self.log)
+      for it in [1,2]:
+        x = fmodel.xray_structure.extract_u_iso_or_u_equiv()*adptbx.u_as_b(1.)
+        lower = flex.double(x.size(), 0)
+        upper = flex.double(x.size(), flex.max(x)*2)
+        calculator = tg(fmodel = fmodel, x = x)
+        m = tncs.minimizer(
+          potential      = calculator,
+          use_bounds     = 2,
+          lower_bound    = lower,
+          upper_bound    = upper,
+          initial_values = x).run()
+        print("r_work: %6.4f"%fmodel.r_work(), file=self.log)
+    #
     return fmodel.xray_structure.extract_u_iso_or_u_equiv()*adptbx.u_as_b(1.)
