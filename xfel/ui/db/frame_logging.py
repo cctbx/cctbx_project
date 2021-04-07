712ba70cbd548edc742fe2f9b531d85f297a30f8
dwpaley <dwpaley@sdf-login01.slac.stanford.edu>
2021-04-06 18:24:59 -0700

    WIP: batch logging db queries
    Co-authored-by: Aaron Brewster <asbrewster@lbl.gov>
    Co-authored-by: Daniel Paley <dwpaley@lbl.gov>

--- xfel/ui/db/frame_logging.py
+++ xfel/ui/db/frame_logging.py
@@ -10,16 +10,39 @@ class DialsProcessorWithLogging(Processor):
     super(DialsProcessorWithLogging, self).__init__(params, composite_tag, rank)
     self.tt_low = None
     self.tt_high = None
-    self.db_app = dxtbx_xfel_db_application(params, cache_connection=True)
+    if params.db.logging_batch_size:
+      self.queries = []
+    else:
+      self.db_app = dxtbx_xfel_db_application(params, cache_connection=True)
+
+  def finalize(self):
+    super(DialsProcessorWithLogging, self).finalize()
+    if self.params.db.logging_batch_size: self.log_batched_frames()
+    
+  def log_batched_frames(self):
+    db_app = dxtbx_xfel_db_application(self.params, cache_connection=True)
+    for q in self.queries:
+      experiments, reflections, run, n_strong, timestamp, two_theta_low, two_theta_high, db_event = q
+      log_frame(experiments, reflections, self.params, run, n_strong, timestamp = timestamp,
+                           two_theta_low = two_theta_low, two_theta_high = two_theta_high,
+                           db_event = db_event, app = db_app)
+    self.queries = []
 
   def log_frame(self, experiments, reflections, run, n_strong, timestamp = None,
                 two_theta_low = None, two_theta_high = None, db_event = None):
     # update an existing db_event if db_event is not None
     if self.params.experiment_tag is None:
       return
-    db_event = log_frame(experiments, reflections, self.params, run, n_strong, timestamp = timestamp,
-                         two_theta_low = two_theta_low, two_theta_high = two_theta_high,
-                         db_event = db_event, app = self.db_app)
+    if self.params.db.logging_batch_size:
+      self.queries.append((experiments, reflections, run, n_strong, timestamp,
+                           two_theta_low, two_theta_high,
+                           db_event))
+      if len(self.queries) >= self.params.db.logging_batch_size:
+        self.log_batched_frames()
+    else:
+      db_event = log_frame(experiments, reflections, self.params, run, n_strong, timestamp = timestamp,
+                           two_theta_low = two_theta_low, two_theta_high = two_theta_high,
+                           db_event = db_event, app = self.db_app)
     return db_event
 
   def get_run_and_timestamp(self, obj):
@@ -66,19 +89,28 @@ class DialsProcessorWithLogging(Processor):
 
   def find_spots(self, experiments):
     observed = super(DialsProcessorWithLogging, self).find_spots(experiments)
-    run, timestamp = self.get_run_and_timestamp(experiments)
-    self.db_event = self.log_frame(None, None, run, len(observed), timestamp = timestamp,
-                                   two_theta_low = self.tt_low, two_theta_high = self.tt_high)
+    if (
+        not self.params.dispatch.index or
+        len(observed) < self.params.dispatch.hit_finder.minimum_number_of_reflections
+    ):
+      run, timestamp = self.get_run_and_timestamp(experiments)
+      self.log_frame(None, None, run, len(observed), timestamp = timestamp,
+                     two_theta_low = self.tt_low, two_theta_high = self.tt_high)
     return observed
 
   def index(self, experiments, reflections):
-    experiments, indexed = super(DialsProcessorWithLogging, self).index(experiments, reflections)
-
-    if not(self.params.dispatch.integrate):
+    try:
+      experiments, indexed = super(DialsProcessorWithLogging, self).index(experiments, reflections)
+    except Exception as e:
       run, timestamp = self.get_run_and_timestamp(experiments)
-      self.log_frame(experiments, indexed, run, len(indexed), timestamp = timestamp,
-                     two_theta_low = self.tt_low, two_theta_high = self.tt_high,
-                     db_event = self.db_event)
+      self.log_frame(None, None, run, len(reflections), timestamp = timestamp,
+                     two_theta_low = self.tt_low, two_theta_high = self.tt_high)
+      raise e
+    else:
+      if not self.params.dispatch.integrate:
+        run, timestamp = self.get_run_and_timestamp(experiments)
+        self.log_frame(experiments, indexed, run, len(indexed), timestamp = timestamp,
+                       two_theta_low = self.tt_low, two_theta_high = self.tt_high)
     return experiments, indexed
 
   def integrate(self, experiments, indexed):
@@ -87,11 +119,12 @@ class DialsProcessorWithLogging(Processor):
     try:
       integrated = super(DialsProcessorWithLogging, self).integrate(experiments, indexed)
       self.log_frame(experiments, integrated, run, len(integrated), timestamp = timestamp,
-                     two_theta_low = self.tt_low, two_theta_high = self.tt_high,
-                     db_event = self.db_event)
+                     two_theta_low = self.tt_low, two_theta_high = self.tt_high)
       return integrated
     except Exception as e:
       self.log_frame(experiments, indexed, run, len(indexed), timestamp = timestamp,
-                     two_theta_low = self.tt_low, two_theta_high = self.tt_high,
-                     db_event = self.db_event)
+                     two_theta_low = self.tt_low, two_theta_high = self.tt_high)
       raise e
+    else:
+      self.log_frame(experiments, indexed, run, len(indexed), timestamp = timestamp,
+                     two_theta_low = self.tt_low, two_theta_high = self.tt_high)
