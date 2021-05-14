f52f186a631b6467ff5c2c68a24ec8a8e98f61c4
Russ Taylor <russ@reliasolve.com>
2021-05-14 08:34:55 -0400

    Adding Apache2 license file and clarifying license in source files

--- mmtbx/probe/boost_python/probe_bpl.cpp
+++ mmtbx/probe/boost_python/probe_bpl.cpp
@@ -1,3 +1,16 @@
+// Copyright(c) 2021, Richardson Lab at Duke
+// Licensed under the Apache 2 license
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissionsand
+// limitations under the License.
+
 // Enable functions with up to 20 parameters to be called.  Default of 15 is insufficient
 #define BOOST_PYTHON_MAX_ARITY 20
 #include <boost/python.hpp>
