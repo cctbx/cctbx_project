b36a16a4e77a83a760cf76b8858e6be651bb657e
Aaron Brewster <asbrewster@lbl.gov>
2021-05-25 10:51:44 -0700

    Where possible, we can look up help strings from the phil parameters

--- xfel/ui/components/tooltips.py
+++ xfel/ui/components/tooltips.py
@@ -1,7 +1,12 @@
 from __future__ import absolute_import, division, print_function
 
+from xfel.ui import master_phil_scope
+
+def get_help(path):
+  return master_phil_scope.get(path).objects[0].help
+
 tooltips = {
-  'db_cred_ctr': 'Identifier for these processing results. Similar to a project name.',
+  'db_cred_ctr': get_help('experiment_tag'),
   'db_cred_btn_big': 'Set up database credentials the GUI will use to connect with',
 }
 
