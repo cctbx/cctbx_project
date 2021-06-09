86631933c867e90abc8092b0f69a2fb388f4b59c
Felix Wittwer <fwittwer@lbl.gov>
2021-06-09 13:49:43 -0700

    Convert structure_factors to KOKKOS

--- simtbx/kokkos/kokkos_ext.cpp
+++ simtbx/kokkos/kokkos_ext.cpp
@@ -4,6 +4,7 @@
 #include <boost/python/args.hpp>
 
 #include "simtbx/kokkos/kokkos_instance.h"
+#include "simtbx/kokkos/structure_factors.h"
 
 namespace simtbx { namespace Kokkos {
 
@@ -20,9 +21,24 @@ namespace simtbx { namespace Kokkos {
     }
   };
 
+  struct structure_factor_wrapper {
+    static void
+    wrap() {
+      using namespace boost::python;
+      class_<simtbx::Kokkos::kokkos_energy_channels>("kokkos_energy_channels",init<>() )
+        .def("get_nchannels", &simtbx::Kokkos::kokkos_energy_channels::get_nchannels)
+        .def("structure_factors_to_KOKKOS_direct_cuda",
+             &simtbx::Kokkos::kokkos_energy_channels::structure_factors_to_KOKKOS_direct_cuda,
+             (arg_("dummy_int"), arg_("indices"), arg_("amplitudes"))
+            )
+        ;
+    }
+  };
+
   } // namespace Kokkos
 
   BOOST_PYTHON_MODULE(simtbx_kokkos_ext) {
     simtbx::Kokkos::kokkos_instance_wrapper::wrap();
+    simtbx::Kokkos::structure_factor_wrapper::wrap();
   }
 } // namespace simtbx
