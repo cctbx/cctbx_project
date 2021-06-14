62a8c6945f514a32fad1247375d132818b779ede
Aaron Brewster <asbrewster@lbl.gov>
2021-06-14 15:51:31 -0700

    Linux only for lunus

--- xfel/merging/application/lunus/lunus.py
+++ xfel/merging/application/lunus/lunus.py
@@ -6,81 +6,7 @@ from dials.array_family import flex
 from lunus.command_line.process import get_experiment_params, get_experiment_xvectors
 import lunus as lunus_processor
 from scitbx import matrix
-import os
 import numpy as np
-
-def mpi_enabled():
-  return ('MPI_LOCALRANKID' in os.environ.keys() or 'OMPI_COMM_WORLD_RANK' in os.environ.keys())
-#  return False
-
-mpi_comm = None
-def mpi_init():
-  global mpi_comm
-  global MPI
-  from mpi4py import MPI as MPI
-  mpi_comm = MPI.COMM_WORLD
-
-def get_mpi_rank():
-  return mpi_comm.Get_rank() if mpi_enabled() else 0
-
-
-def get_mpi_size():
-  return mpi_comm.Get_size() if mpi_enabled() else 1
-
-def mpi_send(d,dest):
-  if mpi_enabled():
-    mpi_comm.Send(d,dest=dest)
-
-def mpi_recv(d,source):
-  if mpi_enabled():
-    mpi_comm.Recv(d,source=source)
-
-def mpi_bcast(d,root=0):
-  if mpi_enabled():
-    db = mpi_comm.bcast(d,root=root)
-  else:
-    db = d
-
-  return db
-
-def mpi_barrier():
-  if mpi_enabled():
-    mpi_comm.Barrier()
-
-def mpi_sum(d, dt, root=0):
-  if mpi_enabled():
-    mpi_comm.Reduce(d,dt,op=MPI.SUM,root=root)
-  else:
-    dt = d
-
-def mpi_reduce_p(p, root=0):
-  if mpi_enabled():
-#    if get_mpi_rank() == 0:
-#      print("LUNUS.PROCESS: Convertinf flex arrays to numpy arrays")
-#      sys.stdout.flush()
-
-    l = p.get_lattice().as_numpy_array()
-    c = p.get_counts().as_numpy_array()
-
-    if get_mpi_rank() == root:
-      lt = np.zeros_like(l)
-      ct = np.zeros_like(c)
-    else:
-      lt = None
-      ct = None
-
-
-    mpi_comm.Reduce(l,lt,op=MPI.SUM,root=root)
-    mpi_comm.Reduce(c,ct,op=MPI.SUM,root=root)
-
-    if get_mpi_rank() == root:
-      #logger.info("LUNUS.PROCESS: Converting numpy arrays to flex arrays")
-      p.set_lattice(flex.double(lt))
-      p.set_counts(flex.int(ct))
-
-  return p
-
-
 
 class lunus(worker):
   """
@@ -93,10 +19,10 @@ class lunus(worker):
 
     self.current_path = None
     self.current_imageset = None
-    mpi_init()
+#    mpi_init()
 
   def __repr__(self):
-    return 'Compute diffuse scatter using Lunus'
+    return 'Process diffuse scattering using Lunus, yielding a 3D dataset'
 
   def filter_by_n_laticces(self, experiments, reflections, n = 1):
     # filter out experiments with more than one lattice
@@ -147,12 +73,11 @@ class lunus(worker):
 
     if self.mpi_helper.rank == 0:
       self.reference_experiments = experiments[0:1]
+      self.logger.log("LUNUS: Using reference experiment %s with image %s %d" % (self.reference_experiments[0].identifier,self.reference_experiments[0].imageset.paths()[0],self.reference_experiments[0].imageset.indices()[0]))
     else:
       self.reference_experiments = None
 
     self.reference_experiments = self.mpi_helper.comm.bcast(self.reference_experiments, root=0)
-
-    self.logger.log("Received reference %s %s %d" % (self.reference_experiments[0].identifier,self.reference_experiments[0].imageset.paths()[0],self.reference_experiments[0].imageset.indices()[0]))
 
     reference_experiment_params = get_experiment_params(self.reference_experiments[0:1])
     self.processor = lunus_processor.Process(len(reference_experiment_params))
@@ -192,7 +117,7 @@ class lunus(worker):
     idx = experiments[0].imageset.indices()[0]
     experiments[0].imageset = self.current_imageset[idx:idx+1]
 
-#    self.logger.log("LUNUS_INTEGRATE: Ready with %s %d" % (experiments[0].imageset.paths()[0],experiments[0].imageset.indices()[0]))
+    self.logger.log("LUNUS_INTEGRATE: Processing image %s %d" % (experiments[0].imageset.paths()[0],experiments[0].imageset.indices()[0]))
 
     data = experiments[0].imageset[0]
     if not isinstance(data, tuple):
@@ -205,7 +130,7 @@ class lunus(worker):
       deck_and_extras = self.deck+experiment_params[pidx]
       p.LunusSetparamsim(pidx,deck_and_extras)
 
-    self.logger.log("LUNUS: Processing image")
+#    self.logger.log("LUNUS: Processing image")
 
     if is_reference:
       x = get_experiment_xvectors(experiments)
@@ -225,9 +150,7 @@ class lunus(worker):
 
       p.set_amatrix(At_flex)
 
-      self.logger.log("LUNUS: Entering lprocimlt()")
       p.LunusProcimlt(0)
-      self.logger.log("LUNUS: Done with lprocimlt()")
     else:
       crystal = experiments[0].crystal
       A_matrix = matrix.sqr(crystal.get_A()).inverse()
@@ -238,17 +161,37 @@ class lunus(worker):
       p.LunusProcimlt(1)
 
   def finalize(self):
-    mpi_barrier() # Need to synchronize at this point so that all the server/client ranks finish
+    self.mpi_helper.comm.Barrier() # Need to synchronize at this point so that all the server/client ranks finish
 
-    self.logger.log("STARTING FINALIZE, rank %d, size %d"%(get_mpi_rank(), get_mpi_size()))
+    self.logger.log("LUNUS: STARTING FINALIZE, rank %d, size %d"%(self.mpi_helper.rank, self.mpi_helper.size))
 
     p = self.processor
-    p = mpi_reduce_p(p)
+    p = self.mpi_reduce_p(p)
 
-    if get_mpi_rank() == 0:
+    if self.mpi_helper.rank == 0:
       p.divide_by_counts()
       p.write_as_hkl('results.hkl')
       p.write_as_vtk('results.vtk')
+
+  def mpi_reduce_p(self,p, root=0):
+    l = p.get_lattice().as_numpy_array()
+    c = p.get_counts().as_numpy_array()
+
+    if self.mpi_helper.rank == root:
+      lt = np.zeros_like(l)
+      ct = np.zeros_like(c)
+    else:
+      lt = None
+      ct = None
+
+    self.mpi_helper.comm.Reduce(l,lt,op=self.mpi_helper.MPI.SUM,root=root)
+    self.mpi_helper.comm.Reduce(c,ct,op=self.mpi_helper.MPI.SUM,root=root)
+
+    if self.mpi_helper.rank == root:
+      p.set_lattice(flex.double(lt))
+      p.set_counts(flex.int(ct))
+
+    return p
 
 if __name__ == '__main__':
   from xfel.merging.application.worker import exercise_worker
