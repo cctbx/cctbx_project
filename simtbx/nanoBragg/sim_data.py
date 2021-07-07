93f659c588424fe70353710182030f4f66ba4660
Nicholas K Sauter <nksauter@lbl.gov>
2021-07-06 17:32:49 -0700

    NBcrystal: don't make a lysozyme crystal by default
    New constructor argument (use_default_crystal=False) is much more efficient.
    
    Co-authored-by: Daniel Paley <dwpaley@lbl.gov>

--- simtbx/nanoBragg/sim_data.py
+++ simtbx/nanoBragg/sim_data.py
@@ -37,10 +37,10 @@ def determine_spot_scale(beam_size_mm, crystal_thick_mm, mosaic_vol_mm3):
 
 class SimData:
 
-  def __init__(self):
+  def __init__(self, use_default_crystal=False):
     self.detector = SimData.simple_detector(180, 0.1, (512, 512))
     self.seed = 1
-    self.crystal = NBcrystal()
+    self.crystal = NBcrystal(use_default_crystal=use_default_crystal)
     self.add_air = False
     self.add_water = True
     self.water_path_mm = 0.005
