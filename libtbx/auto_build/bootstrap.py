34b260d1c443ad52a22326a3da4affcede3bab5c
Billy K. Poon <bkpoon@lbl.gov>
2021-07-08 00:50:35 -0700

    bootstrap: add phaser_voyager to PhenixBuilder
    [skip ci]

--- libtbx/auto_build/bootstrap.py
+++ libtbx/auto_build/bootstrap.py
@@ -2273,6 +2273,7 @@ class PhenixBuilder(CCIBuilder):
     'xia2',
     'phaser',
     'phaser_regression',
+    'phaser_voyager',
     'iota',
   ]
   HOT_EXTRA = ['msgpack']
@@ -2287,6 +2288,7 @@ class PhenixBuilder(CCIBuilder):
     'reel',
     'phaser',
     'phaser_regression',
+    'phaser_voyager',
     'labelit',
     'elbow',
     'amber_adaptbx',
