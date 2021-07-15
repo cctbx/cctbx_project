bd68fe1d026cfb78af935733232804a11a3932a6
Russ Taylor <russ@reliasolve.com>
2021-07-15 15:55:31 -0400

    Adding init file so that probe .py files will be treated as modules within the mmtbx package

---
+++ mmtbx/probe/__init__.py
@@ -0,0 +1,3 @@
+# This file is present so that the .py files in this directory will be considered
+# as modules in the mmtbx.probe package.  Its presence lets the system know to
+# treat them as such.
