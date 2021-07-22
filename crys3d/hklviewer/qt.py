689970e968db9bed3d9ef73f72f915cfadad730e
Robert Oeffner <rdo20@cam.ac.uk>
2021-07-23 00:47:55 +0100

    remove duplicate module import

--- crys3d/hklviewer/qt.py
+++ crys3d/hklviewer/qt.py
@@ -1,42 +1,31 @@
-
+from __future__ import absolute_import, division
 import sys
 
 if 'PyQt5' in sys.modules:
   # PyQt5
-  from PyQt5.QtWebEngineWidgets import QWebEngineView
-  from PyQt5.QtCore import QCoreApplication, QMetaObject, QModelIndex, QRect, QSize, Qt
-  from PyQt5.QtWidgets import QAbstractItemView, QAction, QApplication, QCheckBox, QComboBox, \
-    QDoubleSpinBox, QFrame, QGridLayout, QGroupBox, QLabel, QMenu, QMenuBar, \
-    QPlainTextEdit, QPushButton, QRadioButton, QSlider, QSplitter, QSizePolicy, QSpinBox, \
-    QStatusBar, QTableWidget, QTabWidget, QTextEdit, QWidget
-  from PyQt5.QtGui import QIcon, QCursor, QKeySequence
-  from PyQt5 import QtCore, QtWidgets
-  from PyQt5.QtCore import QAbstractTableModel, Qt, QEvent, QItemSelectionModel, QUrl, QSize, QSettings, QTimer
-  from PyQt5.QtWidgets import (  QAction, QCheckBox, QComboBox, QDialog,
-        QFileDialog, QGridLayout, QGroupBox, QHeaderView, QHBoxLayout, QLabel, QLineEdit,
-        QMainWindow, QMenu, QProgressBar, QPushButton, QRadioButton, QScrollArea, QScrollBar, QSizePolicy,
-        QSlider, QDoubleSpinBox, QSpinBox, QStyleFactory, QTableView, QTableWidget,
+  from PyQt5 import ( QtCore, QtWidgets )
+  from PyQt5.QtCore import  QAbstractTableModel, QCoreApplication, QMetaObject, QModelIndex, \
+        QRect, Qt, QEvent, QItemSelectionModel, QUrl, QSize, QSettings, QTimer  # special import
+  from PyQt5.QtWidgets import ( QAction, QAbstractItemView, QApplication, QCheckBox, QComboBox, 
+        QDialog, QDoubleSpinBox, QFileDialog, QFrame, QGridLayout, QGroupBox, QHeaderView, 
+        QHBoxLayout, QLabel, QLineEdit, QMainWindow, QMenu, QMenuBar, QPlainTextEdit,
+        QProgressBar, QPushButton, QRadioButton, QScrollArea, QScrollBar, QSizePolicy,
+        QSlider, QSplitter, QSpinBox, QStatusBar, QStyleFactory, QTableView, QTableWidget,
         QTableWidgetItem, QTabWidget, QTextEdit, QTextBrowser, QWidget, QVBoxLayout )
-  from PyQt5.QtGui import QColor, QFont, QCursor, QDesktopServices
+  from PyQt5.QtGui import ( QColor, QFont, QCursor, QDesktopServices, QIcon, QKeySequence )   # implicit import
   from PyQt5.QtWebEngineWidgets import ( QWebEngineView, QWebEngineProfile, QWebEnginePage )
-
 
 else:
   # PySide2
-  from PySide2.QtWebEngineWidgets import QWebEngineView
-  from PySide2.QtCore import QAbstractTableModel, QCoreApplication, QMetaObject, QModelIndex, QUrl, QRect, QSize, Qt
-  from PySide2.QtWidgets import QAbstractItemView, QAction, QApplication, QCheckBox, QComboBox, \
-    QDoubleSpinBox, QFrame, QGridLayout, QGroupBox, QLabel, QMenu, QMenuBar, \
-    QPlainTextEdit, QPushButton, QRadioButton, QSlider, QSplitter, QSizePolicy, QSpinBox, \
-    QStatusBar, QTableWidget, QTabWidget, QTextEdit, QWidget
-  from PySide2.QtGui import QIcon, QCursor, QKeySequence
-  from PySide2 import QtCore, QtWidgets
-  from PySide2.QtCore import Qt, QEvent, QItemSelectionModel, QSize, QSettings, QTimer
-  from PySide2.QtWidgets import (  QAction, QCheckBox, QComboBox, QDialog,
-        QFileDialog, QGridLayout, QGroupBox, QHeaderView, QHBoxLayout, QLabel, QLineEdit,
-        QMainWindow, QMenu, QProgressBar, QPushButton, QRadioButton, QScrollArea, QScrollBar, QSizePolicy,
-        QSlider, QDoubleSpinBox, QSpinBox, QStyleFactory, QTableView, QTableWidget,
+  from PySide2 import ( QtCore, QtWidgets )
+  from PySide2.QtCore import Qt, QEvent, QAbstractTableModel, QCoreApplication, QMetaObject, \
+        QModelIndex, QUrl, QRect, QItemSelectionModel, QSize, QSettings, QTimer   # special import
+  from PySide2.QtWidgets import ( QAction, QAbstractItemView, QApplication, QCheckBox, QComboBox, 
+        QDialog, QDoubleSpinBox, QFileDialog, QFrame, QGridLayout, QGroupBox, QHeaderView, 
+        QHBoxLayout, QLabel, QLineEdit, QMainWindow, QMenu, QMenuBar, QPlainTextEdit,
+        QProgressBar, QPushButton, QRadioButton, QScrollArea, QScrollBar, QSizePolicy,
+        QSlider, QSplitter, QSpinBox, QStatusBar, QStyleFactory, QTableView, QTableWidget,
         QTableWidgetItem, QTabWidget, QTextEdit, QTextBrowser, QWidget, QVBoxLayout )
-  from PySide2.QtGui import QColor, QFont, QCursor, QDesktopServices
+  from PySide2.QtGui import ( QColor, QFont, QCursor, QDesktopServices, QIcon, QKeySequence )  # implicit import
   from PySide2.QtWebEngineWidgets import ( QWebEngineView, QWebEngineProfile, QWebEnginePage )
 
