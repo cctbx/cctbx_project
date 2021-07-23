5e44a7e9b9d6aa30ee4b7e1c8b4e73c23c64d549
Robert Oeffner <rdo20@cam.ac.uk>
2021-07-23 21:14:22 +0100

    de-clutter

--- crys3d/hklviewer/qt.py
+++ crys3d/hklviewer/qt.py
@@ -6,8 +6,8 @@ if 'PyQt5' in sys.modules:
   from PyQt5 import ( QtCore, QtWidgets )
   from PyQt5.QtCore import  QAbstractTableModel, QCoreApplication, QMetaObject, QModelIndex, \
         QRect, Qt, QEvent, QItemSelectionModel, QUrl, QSize, QSettings, QTimer  # special import
-  from PyQt5.QtWidgets import ( QAction, QAbstractItemView, QApplication, QCheckBox, QComboBox, 
-        QDialog, QDoubleSpinBox, QFileDialog, QFrame, QGridLayout, QGroupBox, QHeaderView, 
+  from PyQt5.QtWidgets import ( QAction, QAbstractItemView, QApplication, QCheckBox, QComboBox,
+        QDialog, QDoubleSpinBox, QFileDialog, QFrame, QGridLayout, QGroupBox, QHeaderView,
         QHBoxLayout, QLabel, QLineEdit, QMainWindow, QMenu, QMenuBar, QPlainTextEdit,
         QProgressBar, QPushButton, QRadioButton, QScrollArea, QScrollBar, QSizePolicy,
         QSlider, QSplitter, QSpinBox, QStatusBar, QStyleFactory, QTableView, QTableWidget,
@@ -20,12 +20,11 @@ else:
   from PySide2 import ( QtCore, QtWidgets )
   from PySide2.QtCore import Qt, QEvent, QAbstractTableModel, QCoreApplication, QMetaObject, \
         QModelIndex, QUrl, QRect, QItemSelectionModel, QSize, QSettings, QTimer   # special import
-  from PySide2.QtWidgets import ( QAction, QAbstractItemView, QApplication, QCheckBox, QComboBox, 
-        QDialog, QDoubleSpinBox, QFileDialog, QFrame, QGridLayout, QGroupBox, QHeaderView, 
+  from PySide2.QtWidgets import ( QAction, QAbstractItemView, QApplication, QCheckBox, QComboBox,
+        QDialog, QDoubleSpinBox, QFileDialog, QFrame, QGridLayout, QGroupBox, QHeaderView,
         QHBoxLayout, QLabel, QLineEdit, QMainWindow, QMenu, QMenuBar, QPlainTextEdit,
         QProgressBar, QPushButton, QRadioButton, QScrollArea, QScrollBar, QSizePolicy,
         QSlider, QSplitter, QSpinBox, QStatusBar, QStyleFactory, QTableView, QTableWidget,
         QTableWidgetItem, QTabWidget, QTextEdit, QTextBrowser, QWidget, QVBoxLayout )
   from PySide2.QtGui import ( QColor, QFont, QCursor, QDesktopServices, QIcon, QKeySequence )  # implicit import
   from PySide2.QtWebEngineWidgets import ( QWebEngineView, QWebEngineProfile, QWebEnginePage )
-
