3e0c52d0b461e4ab0402603c7ea86f3fbae84ec5
Billy K. Poon <bkpoon@lbl.gov>
2021-07-25 21:16:18 -0700

    Add __future__ import

--- libtbx/command_line/check_libcpp.py
+++ libtbx/command_line/check_libcpp.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
 import sys
 from   libtbx import easy_run
 from   os     import listdir
