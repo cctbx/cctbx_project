1e0216fc7c6d520e55c6f39470ee22ea48a9f1a6
Robert Oeffner <rdo20@cam.ac.uk>
2021-07-26 15:30:31 +0100

    revert broken shim module

--- crys3d/hklviewer/qt.py
+++ crys3d/hklviewer/qt.py
@@ -2,18 +2,29 @@ from __future__ import absolute_import, division
 import sys
 
 if 'PyQt5' in sys.modules:
-  qt_mod = PyQT5
-else:
-  qt_mod = PySide2
+  # PyQt5
+  from PyQt5 import ( QtCore, QtWidgets )
+  from PyQt5.QtCore import  QAbstractTableModel, QCoreApplication, QMetaObject, QModelIndex, \
+        QRect, Qt, QEvent, QItemSelectionModel, QUrl, QSize, QSettings, QTimer  # special import
+  from PyQt5.QtWidgets import ( QAction, QAbstractItemView, QApplication, QCheckBox, QComboBox,
+        QDialog, QDoubleSpinBox, QFileDialog, QFrame, QGridLayout, QGroupBox, QHeaderView,
+        QHBoxLayout, QLabel, QLineEdit, QMainWindow, QMenu, QMenuBar, QPlainTextEdit,
+        QProgressBar, QPushButton, QRadioButton, QScrollArea, QScrollBar, QSizePolicy,
+        QSlider, QSplitter, QSpinBox, QStatusBar, QStyleFactory, QTableView, QTableWidget,
+        QTableWidgetItem, QTabWidget, QTextEdit, QTextBrowser, QWidget, QVBoxLayout )
+  from PyQt5.QtGui import ( QColor, QFont, QCursor, QDesktopServices, QIcon, QKeySequence )   # implicit import
+  from PyQt5.QtWebEngineWidgets import ( QWebEngineView, QWebEngineProfile, QWebEnginePage )
 
-from qt_mod import ( QtCore, QtWidgets )
-from qt_mod.QtCore import  QAbstractTableModel, QCoreApplication, QMetaObject, QModelIndex,  # special import
-      QRect, Qt, QEvent, QItemSelectionModel, QUrl, QSize, QSettings, QTimer
-from qt_mod.QtWidgets import ( QAction, QAbstractItemView, QApplication, QCheckBox, QComboBox,  # special import
-      QDialog, QDoubleSpinBox, QFileDialog, QFrame, QGridLayout, QGroupBox, QHeaderView,
-      QHBoxLayout, QLabel, QLineEdit, QMainWindow, QMenu, QMenuBar, QPlainTextEdit,
-      QProgressBar, QPushButton, QRadioButton, QScrollArea, QScrollBar, QSizePolicy,
-      QSlider, QSplitter, QSpinBox, QStatusBar, QStyleFactory, QTableView, QTableWidget,
-      QTableWidgetItem, QTabWidget, QTextEdit, QTextBrowser, QWidget, QVBoxLayout )
-from qt_mod.QtGui import ( QColor, QFont, QCursor, QDesktopServices, QIcon, QKeySequence )   # implicit import
-from qt_mod.QtWebEngineWidgets import ( QWebEngineView, QWebEngineProfile, QWebEnginePage )  # special import
+else:
+  # PySide2
+  from PySide2 import ( QtCore, QtWidgets )
+  from PySide2.QtCore import Qt, QEvent, QAbstractTableModel, QCoreApplication, QMetaObject, \
+        QModelIndex, QUrl, QRect, QItemSelectionModel, QSize, QSettings, QTimer   # special import
+  from PySide2.QtWidgets import ( QAction, QAbstractItemView, QApplication, QCheckBox, QComboBox,
+        QDialog, QDoubleSpinBox, QFileDialog, QFrame, QGridLayout, QGroupBox, QHeaderView,
+        QHBoxLayout, QLabel, QLineEdit, QMainWindow, QMenu, QMenuBar, QPlainTextEdit,
+        QProgressBar, QPushButton, QRadioButton, QScrollArea, QScrollBar, QSizePolicy,
+        QSlider, QSplitter, QSpinBox, QStatusBar, QStyleFactory, QTableView, QTableWidget,
+        QTableWidgetItem, QTabWidget, QTextEdit, QTextBrowser, QWidget, QVBoxLayout )
+  from PySide2.QtGui import ( QColor, QFont, QCursor, QDesktopServices, QIcon, QKeySequence )  # implicit import
+  from PySide2.QtWebEngineWidgets import ( QWebEngineView, QWebEngineProfile, QWebEnginePage )
