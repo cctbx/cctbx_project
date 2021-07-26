84a12abbb8a2232d06e25511ae38c60dbd78230d
Johannes Blaschke <jpblaschke@lbl.gov>
2021-07-25 17:03:10 -0700

    add mandatory import

--- libtbx/command_line/check_libcpp.py
+++ libtbx/command_line/check_libcpp.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from   __future__        import division
 
 import sys
 import libtbx.env_config
-from   libtbx             import easy_run
-from   os                 import listdir
+from   libtbx            import easy_run
+from   os                import listdir
 
 
 
