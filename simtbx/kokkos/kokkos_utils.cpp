b2bda85060007925fe45c84ae1fb3a9eb582797f
Felix Wittwer <fwittwer@lbl.gov>
2021-07-30 16:00:52 -0700

    Make Makefile fit for OpenMP, deactivate debug msg

--- simtbx/kokkos/kokkos_utils.cpp
+++ simtbx/kokkos/kokkos_utils.cpp
@@ -4,18 +4,18 @@ namespace simtbx { namespace Kokkos {
   void
   transfer_double2kokkos(vector_cudareal_t &dst, const double *src, const size_t length) {
     if (true) {
-      printf("== Transfer %s from %p\n", dst.label().c_str(), (void*) dst.data());
-      printf(" - size src|dst: %d|%d\n", length, dst.span() );
+      // printf("== Transfer %s from %p\n", dst.label().c_str(), (void*) dst.data());
+      // printf(" - size src|dst: %d|%d\n", length, dst.span() );
     }
     if (dst.span() < length) {
       resize(dst, length);
-      printf(" - size changed, new size: %d\n", dst.span() );
+      // printf(" - size changed, new size: %d\n", dst.span() );
     }
 
     transfer_X2kokkos(dst, src, length);
     
     if (length < 20) {
-      print_view(dst);
+      // print_view(dst);
     }
   }
 
