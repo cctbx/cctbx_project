4c66e6e578ccd2857f6899ebf33c7df7d7eccc9e
Russ Taylor <russ@reliasolve.com>
2021-08-10 20:13:52 -0400

    Importing division from the future so that it will work as expected in Python 2.7

--- mmtbx/suitename/suitename.py
+++ mmtbx/suitename/suitename.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import print_function
+from __future__ import print_function, division
 
 """
 Suitename is a program to aid model-building and perform validation of RNA 
