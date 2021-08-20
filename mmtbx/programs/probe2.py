16185a9b71c590a92754a84b88ae858ec55fd5ab
Russ Taylor <russ@reliasolve.com>
2021-08-20 14:19:54 -0400

    Continuing to implement probe2.py

--- mmtbx/programs/probe2.py
+++ mmtbx/programs/probe2.py
@@ -22,9 +22,9 @@ use_neutron_distances = False
   .type = bool
   .help = Use neutron distances (-nuclear in probe)
 
-approach = *self both once out dump
+approach = *self both once out count
   .type = choice
-  .help = self (src -> src) both (src <=> targ) once (src -> targ) surface (VdW surface) dump (count atoms)
+  .help = self (src -> src) both (src <=> targ) once (src -> targ) surface (VdW surface) count (count atoms)
 
 quiet = False
   .type = bool
@@ -154,7 +154,7 @@ Inputs:
   PDB or mmCIF file containing atomic model
   Ligand CIF file, if needed
 Output:
-  Text file describing the score and other information, depending on the parameters.
+  Kinemage file describing the score and other information, depending on the parameters.
 '''
   datatypes = ['model', 'restraint', 'phil']
   master_phil_str = master_phil_str
@@ -194,73 +194,82 @@ Output:
     # Get the list of all atoms in the model
     atoms = self.model.get_atoms()
 
-    # Get the bonding information we'll need to exclude our bonded neighbors.
-    try:
-      p = mmtbx.model.manager.get_default_pdb_interpretation_params()
-      self.model.set_pdb_interpretation_params(params = p)
-      self.model.process_input_model(make_restraints=True) # make restraints
-      geometry = self.model.get_restraints_manager().geometry
-      sites_cart = self.model.get_sites_cart() # cartesian coordinates
-      bond_proxies_simple, asu = \
-          geometry.get_all_bond_proxies(sites_cart = sites_cart)
-    except Exception as e:
-      raise Sorry("Could not get bonding information for input file: " + str(e))
-    bondedNeighbors = Helpers.getBondedNeighborLists(atoms, bond_proxies_simple)
-
-    # Construct a SpatialQuery and fill in the atoms.  Ensure that we can make a
-    # query within 1000 Angstroms of the origin.
-    sq = probeExt.SpatialQuery(atoms)
-
-    make_sub_header('Fill in chemical information', out=self.logger)
-    ret = Helpers.getExtraAtomInfo(self.model)
-    extra = ret.extraAtomInfo
-    if len(ret.warnings) > 0:
-      print('Warnings returned by getExtraAtomInfo():\n'+ret.warnings, file=self.logger)
-
-    make_sub_header('Compute Probe Score', out=self.logger)
-    # Construct a DotScorer object.
-    ds = probeExt.DotScorer(extra)
-
-    # Construct a dot-sphere cache
-    cache = probeExt.DotSphereCache(self.params.probe.density)
-
+    # Get the source selection (and target selection if there is one)
     # @todo
-    # Find the radius of each atom in the structure and construct dot spheres for
-    # them. Find the atoms that are bonded to them and add them to an excluded list.
-    # Then compute the score for each of them and report the summed score over the
-    # whole molecule the way that Reduce will.
-    total = 0
-    badBumpTotal = 0
-    for a in atoms:
-      rad = extra.getMappingFor(a).vdwRadius
-      if rad <= 0:
-        alt = a.parent().altloc
-        resName = a.parent().resname.strip().upper()
-        resID = str(a.parent().parent().resseq_as_int())
-        chainID = a.parent().parent().parent().id
-        myFullName = "chain "+str(chainID)+" "+resName+" "+resID+" "+a.name+" "+alt
-        raise Sorry("Invalid radius for atom look-up: "+myFullName+"; rad = "+str(rad))
-      sphere = cache.get_sphere(rad)
+    source_atoms = atoms
 
-      # Excluded atoms that are bonded to me or to one of my neightbors.
-      # It has the side effect of excluding myself if I have any neighbors.
-      # Construct as a set to avoid duplicates.
-      exclude = set()
-      for n in bondedNeighbors[a]:
-        exclude.add(n)
-        for n2 in bondedNeighbors[n]:
-          exclude.add(n2)
-      exclude = list(exclude)
+    if self.params.approach == 'count':
+      ret = 'atoms selected: '+str(len(source_atoms))
 
-      dots = sphere.dots()
-      res = ds.score_dots(a, 1.0, sq, rad*3, self.params.probe.radius, exclude, sphere.dots(), sphere.density(), False)
-      total += res.totalScore()
-      if res.hasBadBump:
-        badBumpTotal += 1
-    ret = 'Summed probe score for molecule = {:.2f} with {} bad bumps'.format(total, badBumpTotal)
+    else:
+
+      # Get the bonding information we'll need to exclude our bonded neighbors.
+      try:
+        p = mmtbx.model.manager.get_default_pdb_interpretation_params()
+        self.model.set_pdb_interpretation_params(params = p)
+        self.model.process_input_model(make_restraints=True) # make restraints
+        geometry = self.model.get_restraints_manager().geometry
+        sites_cart = self.model.get_sites_cart() # cartesian coordinates
+        bond_proxies_simple, asu = \
+            geometry.get_all_bond_proxies(sites_cart = sites_cart)
+      except Exception as e:
+        raise Sorry("Could not get bonding information for input file: " + str(e))
+      bondedNeighbors = Helpers.getBondedNeighborLists(atoms, bond_proxies_simple)
+
+      # Construct a SpatialQuery and fill in the atoms.  Ensure that we can make a
+      # query within 1000 Angstroms of the origin.
+      sq = probeExt.SpatialQuery(atoms)
+
+      make_sub_header('Fill in chemical information', out=self.logger)
+      ret = Helpers.getExtraAtomInfo(self.model)
+      extra = ret.extraAtomInfo
+      if len(ret.warnings) > 0:
+        print('Warnings returned by getExtraAtomInfo():\n'+ret.warnings, file=self.logger)
+
+      make_sub_header('Compute Probe Score', out=self.logger)
+      # Construct a DotScorer object.
+      ds = probeExt.DotScorer(extra)
+
+      # Construct a dot-sphere cache
+      cache = probeExt.DotSphereCache(self.params.probe.density)
+
+      # @todo
+      # Find the radius of each atom in the structure and construct dot spheres for
+      # them. Find the atoms that are bonded to them and add them to an excluded list.
+      # Then compute the score for each of them and report the summed score over the
+      # whole molecule the way that Reduce will.
+      total = 0
+      badBumpTotal = 0
+      for a in atoms:
+        rad = extra.getMappingFor(a).vdwRadius
+        if rad <= 0:
+          alt = a.parent().altloc
+          resName = a.parent().resname.strip().upper()
+          resID = str(a.parent().parent().resseq_as_int())
+          chainID = a.parent().parent().parent().id
+          myFullName = "chain "+str(chainID)+" "+resName+" "+resID+" "+a.name+" "+alt
+          raise Sorry("Invalid radius for atom look-up: "+myFullName+"; rad = "+str(rad))
+        sphere = cache.get_sphere(rad)
+
+        # Excluded atoms that are bonded to me or to one of my neightbors.
+        # It has the side effect of excluding myself if I have any neighbors.
+        # Construct as a set to avoid duplicates.
+        exclude = set()
+        for n in bondedNeighbors[a]:
+          exclude.add(n)
+          for n2 in bondedNeighbors[n]:
+            exclude.add(n2)
+        exclude = list(exclude)
+
+        dots = sphere.dots()
+        res = ds.score_dots(a, 1.0, sq, rad*3, self.params.probe.radius, exclude, sphere.dots(), sphere.density(), False)
+        total += res.totalScore()
+        if res.hasBadBump:
+          badBumpTotal += 1
+      ret = 'Summed probe score for molecule = {:.2f} with {} bad bumps'.format(total, badBumpTotal)
 
     base = self.params.output.file_name_prefix
-    of = open("%s.txt"%base,"w")
+    of = open("%s.kin"%base,"w")
     of.write(ret)
     of.close()
 
