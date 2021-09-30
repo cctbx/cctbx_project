01dc4716ac46d4123a21924f1edd886f01ea14f6
dermen <dermen@lbl.gov>
2021-09-30 16:23:57 -0700

    includes liquid like motions model in GPU code

--- simtbx/diffBragg/src/diffBragg_gpu_kernel.cu
+++ simtbx/diffBragg/src/diffBragg_gpu_kernel.cu
@@ -60,6 +60,7 @@ void gpu_sum_over_steps(
 
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     int thread_stride = blockDim.x * gridDim.x;
+    __shared__ bool s_use_diffuse;
     __shared__ bool s_use_nominal_hkl;
     __shared__ bool s_refine_fp_fdp;
     __shared__ bool s_complex_miller;
@@ -115,6 +116,7 @@ void gpu_sum_over_steps(
         for(int i=0; i<6; i++){
             s_refine_Bmat[i] = refine_Bmat[i];
         }
+        s_use_diffuse = use_diffuse;
         s_num_atoms = num_atoms;
         s_refine_fcell = refine_fcell;
         s_refine_eta = refine_eta;
@@ -353,16 +355,46 @@ void gpu_sum_over_steps(
                     I0 = (s_NABC_det_sq)*exp(-2*exparg);
 
             // are we doing diffuse scattering
-            CUDAREAL I_latt_diffuse = 0;
-            if (use_diffuse){
-                CUDAREAL exparg = 4*M_PI*M_PI*H0.dot(anisoU*H0);
-                VEC3 anisoG_q = anisoG*delta_H;
-                I_latt_diffuse = 4.*M_PI*(anisoG*UBO).determinant() /
-                        (1.+ anisoG_q.dot(anisoG_q)* 4*M_PI*M_PI);
-                if (exparg  < .5) // only valid up to a point
-                    I_latt_diffuse *= (exparg);
-                I0 += I_latt_diffuse;
+            //CUDAREAL I_latt_diffuse = 0;
+            if (s_use_diffuse){
+                MAT3 Ainv = UBO.inverse();
+                CUDAREAL anisoG_determ = anisoG.determinant();
+                for (int hh=0; hh <1; hh++){
+                    for (int kk=0; kk <1; kk++){
+                        for (int ll=0; ll <1; ll++){
+                            VEC3 H0_offset(_h0+hh, _k0+kk, _l0+ll);
+                            VEC3 Q0 = Ainv*H0_offset;
+                            CUDAREAL exparg = 4*M_PI*M_PI*Q0.dot(anisoU*Q0);
+                            //double dwf = exp(-exparg);
+                            VEC3 delta_H_offset = H_vec - H0_offset;
+                            VEC3 delta_Q = Ainv*delta_H_offset;
+                            VEC3 anisoG_q = anisoG*delta_Q;
+
+                            CUDAREAL this_I_latt_diffuse = 8.*M_PI*anisoG_determ /
+                                    pow( (1.+ anisoG_q.dot(anisoG_q)* 4*M_PI*M_PI),2);
+                            if (exparg  < .5) // only valid up to a point
+                                this_I_latt_diffuse *= (exparg);
+
+                            I0 += this_I_latt_diffuse;
+                        }
+                    }
+                }
             }
+            //CUDAREAL I_latt_diffuse = 0;
+            //if (use_diffuse){
+            //    MAT3 Ainv = UBO.inverse();
+            //    VEC3 Q0 = Ainv*H0;
+            //    CUDAREAL exparg_diffuse = 4*M_PI*M_PI*Q0.dot(anisoU*Q0);
+            //    //CUDAREAL dwf = exp(-exparg_diffuse);
+
+            //    VEC3 delta_Q = Ainv*delta_H;
+            //    VEC3 anisoG_q = anisoG*delta_Q;
+            //    I_latt_diffuse = 4.*M_PI*anisoG.determinant() /
+            //            (1.+ anisoG_q.dot(anisoG_q)* 4*M_PI*M_PI);
+            //    if (exparg_diffuse  < .5) // only valid up to a point
+            //        I_latt_diffuse *= (exparg_diffuse);
+            //    I0 += I_latt_diffuse;
+            //}
 
             CUDAREAL _F_cell = s_default_F;
             CUDAREAL _F_cell2 = 0;
@@ -751,6 +783,7 @@ void gpu_sum_over_steps(
                    printf("hkl= %f %f %f  hkl0= %d %d %d\n", _h,_k,_l,_h0,_k0,_l0);
                    printf(" F_cell=%g  F_cell2=%g I_latt=%g   I = %g\n", _F_cell,_F_cell2,I0,_I);
                    printf("I/steps %15.10g\n", _I/s_Nsteps);
+                   //printf("Ilatt diffuse %15.10g\n", I_latt_diffuse);
                    printf("omega   %15.10g\n", _omega_pixel);
                    printf("default_F= %f\n", s_default_F);
                    printf("Incident[0]=%g, Incident[1]=%g, Incident[2]=%g\n", _incident[0], _incident[1], _incident[2]);
