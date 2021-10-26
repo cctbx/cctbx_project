a134ab8e4c98765c8e9b76110c59acfad0dda87f
Russ Taylor <russ@reliasolve.com>
2021-10-26 15:52:22 -0400

    Initial Reduce2 implementation that will optimize and write to either a PDB or mmCIF file. Also outputs a text file describing the operations that it performed.

--- mmtbx/programs/reduce2.py
+++ mmtbx/programs/reduce2.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, division, print_function
 import sys
+import os
 import math
+import time
 from datetime import datetime
 from libtbx.program_template import ProgramTemplate
 from libtbx import group_args, phil
@@ -11,6 +13,10 @@ import mmtbx_probe_ext as probeExt
 from mmtbx.probe import Helpers
 from iotbx import pdb
 from iotbx.pdb import common_residue_names_get_class
+# @todo See if we can remove the shift and box once reduce_hydrogen is complete
+from cctbx.maptbx.box import shift_and_box_model
+from mmtbx.hydrogens import reduce_hydrogen
+from mmtbx.reduce import Optimizers
 
 version = "0.1.0"
 
@@ -22,10 +28,14 @@ use_neutron_distances = False
 output
   .style = menu_item auto_align
 {
-  file_name = None
+  model_file_base_name = None
     .type = str
-    .short_caption = Output file name
-    .help = Output file name
+    .short_caption = Model output file name base
+    .help = Model output file name
+  description_file_name = None
+    .type = str
+    .short_caption = Description output file name
+    .help = Description output file name
 }
 ''' + Helpers.probe_phil_parameters
 
@@ -52,7 +62,11 @@ Inputs:
   PDB or mmCIF file containing atomic model
   Ligand CIF file, if needed
 Output:
-  PDB or mmCIF file with added hydrogens.
+  PDB or mmCIF file with added hydrogens.  If output.suffix is set to "pdb" then a PDB file
+  will be written, otherwise an mmCIF (.cif) file will be written.  If output.model_file_base_name
+  is specified, that will be the base file name that is written; otherwise, the file will be
+  written into the current working directory with the same base name as the original file and
+  with _reduced added to it; 1xs0.pdb would be written to ./1xso_reduced.cif by default.
 
 NOTES:
   Equivalent PHIL arguments for original Reduce command-line options:
@@ -71,8 +85,10 @@ NOTES:
 
   def validate(self):
     self.data_manager.has_models(raise_sorry=True)
-    if self.params.output.file_name is None:
-      raise Sorry("Must specify output.file_name")
+    #if self.params.output.model_file_name is None:
+    #  raise Sorry("Must specify output.model_file_name")
+    if self.params.output.description_file_name is None:
+      raise Sorry("Must specify output.description_file_name")
 
 # ------------------------------------------------------------------------------
 
@@ -89,12 +105,74 @@ NOTES:
 
     make_sub_header('Interpret Model', out=self.logger)
 
-    make_sub_header('Writing descriptive text', out=self.logger)
+    # Get our model.
+    self.model = self.data_manager.get_model()
 
-    # Write the output to the specified file.
-    of = open(self.params.output.file_name,"w")
+    # Fix up bogus unit cell when it occurs by checking crystal symmetry.
+    cs = self.model.crystal_symmetry()
+    if (cs is None) or (cs.unit_cell() is None):
+      self.model = shift_and_box_model(model = self.model)
+
+    # Add Hydrogens to the model
+    make_sub_header('Adding Hydrogens', out=self.logger)
+    startAdd = time.clock()
+    reduce_add_h_obj = reduce_hydrogen.place_hydrogens(model = self.model)
+    reduce_add_h_obj.run()
+    model = reduce_add_h_obj.get_model()
+    doneAdd = time.clock()
+
+    # Interpret the model after shifting and adding Hydrogens to it so that
+    # all of the needed fields are filled in when we use them below.
+    # @todo Remove this once place_hydrogens() does all the interpretation we need.
+    make_sub_header('Interpret Model', out=self.logger)
+    startInt = time.clock()
+    p = mmtbx.model.manager.get_default_pdb_interpretation_params()
+    p.pdb_interpretation.allow_polymer_cross_special_position=True
+    p.pdb_interpretation.clash_guard.nonbonded_distance_threshold=None
+    p.pdb_interpretation.proceed_with_excessive_length_bonds=True
+    model.process(make_restraints=True, pdb_interpretation_params=p) # make restraints
+    doneInt = time.clock()
+
+    make_sub_header('Optimizing', out=self.logger)
+    # @todo Let the caller specify the model index and altID rather than doing only the default (first).
+    startOpt = time.clock()
+    opt = Optimizers.FastOptimizer(model, probeRadius=0.25, altID="")
+    doneOpt = time.clock()
+    outString += opt.getInfo()
+    outString += 'Time to Add Hydrogen = '+str(doneAdd-startAdd)+'\n'
+    outString += 'Time to Interpret = '+str(doneInt-startInt)+'\n'
+    outString += 'Time to Optimize = '+str(doneOpt-startOpt)+'\n'
+
+    make_sub_header('Writing output', out=self.logger)
+
+    # Write the description output to the specified file.
+    of = open(self.params.output.description_file_name,"w")
     of.write(outString)
     of.close()
+
+    # Determine whether to write a PDB or CIF file and write the appropriate text output.
+    # Then determine the output file name and write it there.
+    if str(self.params.output.suffix).lower() == "pdb":
+      txt = self.model.model_as_pdb()
+      suffix = ".pdb"
+    else:
+      txt = self.model.model_as_mmcif()
+      suffix = ".cif"
+    if self.params.output.model_file_base_name is not None:
+      base = self.params.output.model_file_base_name
+    else:
+      file_name = self.data_manager.get_model_names()[0]
+      base = os.path.splitext(os.path.basename(file_name))[0] + "_reduced"
+    fullname = base+suffix
+    with open(fullname, 'w') as f:
+      f.write(txt)
+
+    print('Wrote',fullname,'and',self.params.output.description_file_name, file = self.logger)
+
+# ------------------------------------------------------------------------------
+
+  def get_results(self):
+    return group_args(model = self.model)
 
 # ------------------------------------------------------------------------------
 
@@ -106,9 +184,3 @@ NOTES:
 
     #=====================================================================================
     # @todo Unit tests for other methods
-
-# ------------------------------------------------------------------------------
-
-  #def get_results(self):
-  #  return group_args(model = self.model)
-
