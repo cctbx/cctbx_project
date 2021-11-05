bcad938dbde4268a7c362fcea50e8f994eb98a1a
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2021-11-05 09:38:56 -0700

    updated Mg ionic radii cause larger repulsions

--- mmtbx/regression/model/tst_model.py
+++ mmtbx/regression/model/tst_model.py
@@ -1083,7 +1083,7 @@ def exercise_convert_atom():
   # not push the
   for atom in mol.get_hierarchy().atoms():
     xyz_max = max([ abs(n) for n in atom.xyz])
-    assert (xyz_max < 2.5)
+    assert (xyz_max < 2.6), 'max %s is larger than 2.6' % xyz_max
   mol = mol.select(flex.size_t([1,2,3,4,5,6]))
   assert mol.have_anomalous_scatterer_groups()
   # assert mol.update_anomalous_groups(out=null_out()) # not needed because select() processed groups correctly
