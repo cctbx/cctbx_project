0213f21670968eabd6401cb599fb6c94d2a844a0
Billy K. Poon <bkpoon@lbl.gov>
2021-11-04 22:48:57 -0700

    crys3d: set expected test failure

--- crys3d/run_tests.py
+++ crys3d/run_tests.py
@@ -1,10 +1,16 @@
 from __future__ import absolute_import, division, print_function
+import sys
 from libtbx import test_utils
 import libtbx.load_env
 
 tst_list = [
 "$D/regression/tst_hklinfo.py",
 ]
+
+# expected failure for Python 2
+if sys.version_info < (3, 0):
+  tst_list_expected_failures = tst_list
+  tst_list = []
 
 def run():
   build_dir = libtbx.env.under_build("crys3d")
