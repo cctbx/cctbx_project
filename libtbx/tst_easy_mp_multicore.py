94b594fea40ae9cf1d5f176b441cd2470463e5f7
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2021-11-23 12:47:34 -0800

    easy_mp.multi_core test

---
+++ libtbx/tst_easy_mp_multicore.py
@@ -0,0 +1,24 @@
+import os, sys
+
+def factorial(n):
+  f = 1
+  while n>1:
+    f*=n
+    n-=1
+  return f
+
+def main(nproc=2):
+  from libtbx import easy_mp
+  argss = []
+  for i in range(7):
+    argss.append([i+1])
+  for args, res, err_str in easy_mp.multi_core_run(factorial,
+                                                   tuple(argss),
+                                                   nproc,
+                                                   ):
+    if res:
+      print (args, res)
+    assert not err_str, 'Error: %s' % err_str
+
+if __name__ == '__main__':
+  main()
