06e7cb7a48f0afcb9eaf3946c1969db15afdeeb1
pcxod <oleg_dolomanov@hotmail.com>
2021-11-26 07:29:31 +0000

    - adding syr to fast_linalg - rank-1 matrix update - also 'generalising' syrk to float

--- fast_linalg/lapacke.h
+++ fast_linalg/lapacke.h
@@ -17,7 +17,9 @@
 
 namespace fast_linalg {
   const int LAPACK_ROW_MAJOR = 101,
-    LAPACK_COL_MAJOR = 102;
+    LAPACK_COL_MAJOR = 102,
+    CBLAS_UPPER = 121,
+    CBLAS_LOWER = 122;
 }
 
 #if defined(USE_FAST_LINALG)
@@ -63,34 +65,44 @@ extern "C" {
   fast_linalg_api lapack_int lapack_spftri(int matrix_order, char transr,
     char uplo, lapack_int n, float* a);
 
+  fast_linalg_api void cblas_ssyr(int Order, int Uplo, int N, float Alpha,
+    const float *X, int incX, float* A, int lda);
+  fast_linalg_api void cblas_dsyr(int Order, int Uplo, int N, double Alpha,
+    const double *X, int incX, double* A, int lda);
+  fast_linalg_api void cblas_ssyr(int Order, int Uplo, int N, float alpha,
+    const float *X, int incX, float *A, int lda);
+  fast_linalg_api void cblas_dsyr(int Order, int Uplo, int N, double alpha,
+    const double *X, int incX, double *A, int lda);
+  fast_linalg_api void cblas_ssyrk(int Order, int Uplo, int Trans, int N, int K,
+    float alpha, float *A, int lda, float beta, float *C, int ldc);
   fast_linalg_api void cblas_dsyrk(int Order, int Uplo, int Trans, int N, int K,
-    double alpha, double *A, int lda, double beta, double *C, int ldc);
+    double alpha, double* A, int lda, double beta, double* C, int ldc);
 };
 
 namespace fast_linalg {
-  bool is_initialised() {
+  inline bool is_initialised() {
     return is_fast_linalg_initialised();
   }
 
   /// pass lib name like 'openblas.so' or full library path
-  void initialise(const std::string &lib_name) {
+  inline void initialise(const std::string &lib_name) {
     initialise_fast_linalg(lib_name);
   }
 
   /// cleans up the library
-  void finalise() {
+  inline void finalise() {
     finalise_fast_linalg();
   }
 
   /// @name Cholesky factorisation of A in-place (RFP)
   //@{
-  lapack_int pftrf(int matrix_order, char transr, char uplo,
+  inline lapack_int pftrf(int matrix_order, char transr, char uplo,
     lapack_int n, double* a)
   {
     return lapack_pftrf(matrix_order, transr, uplo, n, a);
   }
 
-  lapack_int pftrf(int matrix_order, char transr, char uplo,
+  inline lapack_int pftrf(int matrix_order, char transr, char uplo,
     lapack_int n, float* a)
   {
     return lapack_spftrf(matrix_order, transr, uplo, n, a);
@@ -99,7 +111,7 @@ namespace fast_linalg {
 
   /// @name Rank-k update (RFP)
   //@{
-  lapack_int sfrk(int matrix_order, char transr,
+  inline lapack_int sfrk(int matrix_order, char transr,
     char uplo, char trans, lapack_int n, lapack_int k, double alpha,
     const double* a, lapack_int lda, double beta,
     double* c)
@@ -108,7 +120,7 @@ namespace fast_linalg {
       uplo, trans, n, k, alpha, a, lda, beta, c);
   }
 
-  lapack_int sfrk(int matrix_order, char transr,
+  inline lapack_int sfrk(int matrix_order, char transr,
     char uplo, char trans, lapack_int n, lapack_int k, float alpha,
     const float* a, lapack_int lda, float beta,
     float* c)
@@ -120,13 +132,13 @@ namespace fast_linalg {
 
   /// @name Copies arf in RFP format to ap in standard packed format
   //@{
-  lapack_int tfttp(int matrix_order, char transr,
+  inline lapack_int tfttp(int matrix_order, char transr,
     char uplo, lapack_int n, const double* arf, double* ap)
   {
     return lapack_tfttp(matrix_order, transr, uplo, n, arf, ap);
   }
 
-  lapack_int tfttp(int matrix_order, char transr,
+  inline lapack_int tfttp(int matrix_order, char transr,
     char uplo, lapack_int n, const float* arf, float* ap)
   {
     return lapack_stfttp(matrix_order, transr, uplo, n, arf, ap);
@@ -135,13 +147,13 @@ namespace fast_linalg {
 
   /// @name Copies ap in standard packed format to arf in RFP format
   //@{
-  lapack_int tpttf(int matrix_order, char transr, char uplo,
+  inline lapack_int tpttf(int matrix_order, char transr, char uplo,
     lapack_int n, const double* ap, double* arf)
   {
     return lapack_tpttf(matrix_order, transr, uplo, n, ap, arf);
   }
 
-  lapack_int tpttf(int matrix_order, char transr, char uplo,
+  inline lapack_int tpttf(int matrix_order, char transr, char uplo,
     lapack_int n, const float* ap, float* arf)
   {
     return lapack_stpttf(matrix_order, transr, uplo, n, ap, arf);
@@ -150,23 +162,50 @@ namespace fast_linalg {
 
   /// @name Inverse of Cholesky decomposed matrix (RFP)
   //@{
-  lapack_int pftri(int matrix_order, char transr, char uplo,
+  inline lapack_int pftri(int matrix_order, char transr, char uplo,
     lapack_int n, double* a)
   {
     return lapack_pftri(matrix_order, transr, uplo, n, a);
   }
 
-  lapack_int pftri(int matrix_order, char transr, char uplo,
+  inline lapack_int pftri(int matrix_order, char transr, char uplo,
     lapack_int n, float* a)
   {
     return lapack_spftri(matrix_order, transr, uplo, n, a);
   }
   //@}
 
+  /// @name Performs a symmetric rank-1 update
+  //@{
+  inline void syr(int Order, int Uplo, int N, float Alpha,
+    const float *X, int incX, float* A, int lda)
+  {
+    cblas_ssyr(Order, Uplo, N, Alpha, X, incX, A, lda);
+  }
+  //@}
+
+  /// @name Performs a symmetric rank-1 update
+  //@{
+  inline void syr(int Order, int Uplo, int N, double Alpha,
+    const double *X, int incX, double* A, int lda)
+  {
+    cblas_dsyr(Order, Uplo, N, Alpha, X, incX, A, lda);
+  }
+  //@}
+
   /// @name Performs a symmetric rank-k update
   //@{
-  void dsyrk(int Order, int Uplo, int Trans, int N, int K,
-    double alpha, double *A, int lda, double beta, double *C, int ldc)
+  inline void syrk(int Order, int Uplo, int Trans, int N, int K,
+    float alpha, float *A, int lda, float beta, float *C, int ldc)
+  {
+    cblas_ssyrk(Order, Uplo, Trans, N, K, alpha, A, lda, beta, C, ldc);
+  }
+
+  //@}
+  /// @name Performs a symmetric rank-k update
+  //@{
+  inline void syrk(int Order, int Uplo, int Trans, int N, int K,
+    double alpha, double* A, int lda, double beta, double* C, int ldc)
   {
     cblas_dsyrk(Order, Uplo, Trans, N, K, alpha, A, lda, beta, C, ldc);
   }
@@ -175,15 +214,15 @@ namespace fast_linalg {
 #else
 namespace fast_linalg {
 
-  bool is_initialised() {
+  inline bool is_initialised() {
     return false;
   }
 
-  void initialise(const std::string &lib_name) {
+  inline void initialise(const std::string &lib_name) {
     SCITBX_NOT_IMPLEMENTED();
   }
 
-  void finalise() {
+  inline void finalise() {
     SCITBX_NOT_IMPLEMENTED();
   }
 
@@ -229,8 +268,17 @@ namespace fast_linalg {
     return 0;
   }
 
-  void dsyrk(int, int, int, int, int, double, double *, int,
-    double , double *, int)
+  template <typename FloatType>
+  void syr(int, int, int, FloatType, const FloatType *, int,
+    FloatType *, int)
+  {
+    SCITBX_NOT_IMPLEMENTED();
+  }
+
+
+  template <typename FloatType>
+  void syrk(int, int, int, int, int, FloatType, FloatType *, int,
+    FloatType, FloatType *, int)
   {
     SCITBX_NOT_IMPLEMENTED();
   }
