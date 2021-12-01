f27f474c0495be584d163070257da288cbdb2af6
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2021-12-01 15:44:44 -0800

    updated modified for Glyco

--- iotbx/pdb/modified_rna_dna_names.py
+++ iotbx/pdb/modified_rna_dna_names.py
@@ -7,7 +7,7 @@ procedure:
   phenix.python elbow/elbow/scripts/process_amino_acid_parentage_from_chemical_componts.py
 
 This file is intended to be generated monthly.
-The date of file generation: Wed Dec  1 11:45:47 2021
+The date of file generation: Wed Dec  1 15:06:09 2021
 """
 from __future__ import absolute_import, division, print_function
 
