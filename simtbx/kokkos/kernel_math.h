a8a8e6534e08ee312bc19d49f2ee212883a1abba
William Hart <whart222@gmail.com>
2021-12-09 19:30:04 +0000

    Further updates to get things building

--- simtbx/kokkos/kernel_math.h
+++ simtbx/kokkos/kernel_math.h
@@ -215,7 +215,8 @@ KOKKOS_FUNCTION CUDAREAL sincg(CUDAREAL x, CUDAREAL N) {
 
 KOKKOS_FUNCTION CUDAREAL sincgrad(CUDAREAL x, CUDAREAL N) {
         if (x != 0.0) {
-                /*return sinpi(x * N) / sinpi(x);*/
+                /* WEH - sinpi not defined
+                return sinpi(x * N) / sinpi(x); */
                 return sin(M_PI*x * N) / sin(M_PI*x);
         }
         return N;
