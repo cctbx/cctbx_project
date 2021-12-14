0f45063a04739649c4059a41d13f2d6d91510e7f
Russ Taylor <russ@reliasolve.com>
2021-12-14 16:14:56 -0500

    Starting to implement comparison script between Probe and Probe2 as part of CCTBX so that we can continue to do comparisons between the two as the CCTBX code changes.

---
+++ mmtbx/probe/CompareOriginalAndNewProbeAtomInfo.py
@@ -0,0 +1,58 @@
+##################################################################################
+#                Copyright 2021  Richardson Lab at Duke University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function, nested_scopes, generators, division
+from __future__ import absolute_import
+
+import subprocess
+import os
+import argparse
+from mmtbx.probe import Helpers
+
+##################################################################################
+# If we're run on the command line, test our classes and functions.
+if __name__ == '__main__':
+
+  #==============================================================
+  # Parse command-line arguments.  The 0th argument is the name
+  # of the script. There can be the name of a PDB file to read.
+  parser = argparse.ArgumentParser(description='Test mmtbx.reduce.Optimizers.')
+  parser.add_argument("--distanceThreshold", type=float, help="Same atom must be >= this distance between files to report", default=0.02)
+  parser.add_argument('inputFile', type=str)
+  args = parser.parse_args()
+
+  #==============================================================
+  # Check out and build the original probe program in a subdirectory.
+  print('Cloning original Probe repository')
+  subprocess.run(["git","clone","https://github.com/rlabduke/probe"])
+
+  print('Building original Probe')
+  cwd = os.getcwd();
+  os.chdir("./probe")
+  subprocess.run(["make"])
+  os.chdir(cwd)
+
+  # @todo
+
+  #==============================================================
+  # @todo
+
+  #==============================================================
+  # Compare the two dump files.  Store the results in a comparison file and say
+  # to look there.
+  # @todo
+  compareFileName = "./"+args.inputFile+".compare"
+
+  print('Look in',compareFileName,'for comparisons')
--- mmtbx/probe/Helpers.py
+++ mmtbx/probe/Helpers.py
@@ -420,6 +420,113 @@ def writeAtomInfoToString(atoms, extraAtomInfo):
     )
   return ret
 
+def compareAtomInfoFiles(fileName1, fileName2, distanceThreshold):
+  """
+    Write information about atom differences in two files into a string that describes
+    their differences.  This is used to perform a difference on files that contain the
+    output of writeAtomInfoToString() run on different versions of Probe or Reduce.
+    The lines in the files are sorted so that reordering of the atoms between files will
+    not cause this function to report differences.
+    :param fileName1: The first file.
+    :param fileName2: The second file.
+    :param distanceThreshold: If the same atom in each file is further apart than this
+    between files, report it as a difference; otherwise, not.
+    :return: String suitable for writing to a file to describe the differences.
+  """
+
+  ###########################
+  # Helper utility functions usable only inside this function
+  def atomID(s):
+    # Return the ID of the atom, which includes its chain, residue name,
+    # residue number, atom name, and alternate separated by spaces
+    w = s.split()
+    return w[0]+" "+w[1]+" "+w[2]+" "+w[3]+" "+w[4]
+
+  def position(s):
+    # Return a tuple that has the location of the atom.
+    w = s.split()
+    return ( float(w[5]), float(w[6]), float(w[7]) )
+
+  def radius(s):
+    # Return the radius for the atom.
+    w = s.split()
+    return ( w[8] )
+
+  def flag(s,f):
+    # Return a flag for the atom, indexed by 0-2.
+    w = s.split()
+    return ( w[9+f] )
+
+  def distance(a, b):
+    # Return the distance between 3-space tuples a and b
+    return math.sqrt( (a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2 )
+
+  ###########################
+  # Initialize our return value to empty.
+  ret = ""
+
+  # Read the the data from each file.
+  # Sort the lines from the file so that we'll get chain, residue, atom name sorting.
+  with open(fileName1) as f:
+    m1 = f.readlines()
+  m1.sort(key=lambda x:atomID(x))
+
+  with open(fileName2) as f:
+    m2 = f.readlines()
+  m2.sort(key=lambda x:atomID(x))
+
+  # Compare the two and report on any differences.
+  # Both lists are sorted, so we'll know which has a missing element compared
+  # to the other.  We walk from the beginning to the end of each list.
+  m1ID = 0
+  m2ID = 0
+  while m1ID < len(m1) and m2ID < len(m2):
+
+    # Atom names are unique within residues, so we can use that to
+    # match atoms in one file from those in the other and print out
+    # missing atoms and differences in positions.
+    if atomID(m1[m1ID]) < atomID(m2[m2ID]):
+      ret += 'Only in first: '+atomID(m1[m1ID])+'\n'
+      m1ID += 1
+      continue
+
+    if atomID(m1[m1ID]) > atomID(m2[m2ID]):
+      ret += 'Only in second: '+atomID(m2[m2ID])+'\n'
+      m2ID += 1
+      continue
+
+    # Check the radius to see if they differ
+    oldRadius = radius(m1[m1ID])
+    newRadius = radius(m2[m2ID])
+    if oldRadius != newRadius:
+      ret += atomID(m1[m1ID])+' radii differ: old is {}, new is {}'.format(oldRadius,newRadius)+'\n'
+
+    # Check the flag fields to see if any of them differ
+    for f in range(3):
+      oldFlag = flag(m1[m1ID], f)
+      newFlag = flag(m2[m2ID], f)
+      if oldFlag != newFlag:
+        ret += atomID(m1[m1ID])+' flags differ: old is {}, new is {}'.format(oldFlag,newFlag)+'\n'
+
+    # Check the distance between the atoms.
+    dist = distance(position(m1[m1ID]),position(m2[m2ID]))
+    if dist > thresh:
+      ret += atomID(m1[m1ID])+' Distance between runs: {:.2f}'.format(dist)+'\n'
+
+    # Go on to next line
+    m1ID += 1
+    m2ID += 1
+
+  # Finish up either list that is not done
+  while m1ID < len(m1):
+    ret += 'Only in first: '+atomID(m1[m1ID])+'\n'
+    m1ID += 1
+  while m2ID < len(m2):
+    ret += 'Only in second: '+atomID(m2[m2ID])+'\n'
+    m2ID += 1
+
+  return ret
+
 def getPhantomHydrogensFor(atom, spatialQuery, extraAtomInfo, minOccupancy, acceptorOnly = False,
       placedHydrogenRadius = 1.05):
   """
