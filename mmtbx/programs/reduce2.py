adbf07e903a8e04f9ea88ff264aacd3cf9ff59f5
Russ Taylor <russ@reliasolve.com>
2022-01-17 17:40:45 -0500

    Adding neutron-distances option to interpretation to match Phil parameter.

--- mmtbx/programs/reduce2.py
+++ mmtbx/programs/reduce2.py
@@ -179,6 +179,7 @@ NOTES:
       p = mmtbx.model.manager.get_default_pdb_interpretation_params()
       p.pdb_interpretation.allow_polymer_cross_special_position=True
       p.pdb_interpretation.clash_guard.nonbonded_distance_threshold=None
+      p.pdb_interpretation.use_neutron_distances = self.params.use_neutron_distances
       p.pdb_interpretation.proceed_with_excessive_length_bonds=True
       #p.pdb_interpretation.sort_atoms=True
       self.model.process(make_restraints=True, pdb_interpretation_params=p) # make restraints
