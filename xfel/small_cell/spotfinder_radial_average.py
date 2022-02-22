3379ff0acbcfb3f013efbdccb78dc6095cf0848b
dperl-sol <72250187+dperl-sol@users.noreply.github.com>
2022-02-23 04:14:01 +1100

    interactive peak picking in powder_from_spots (#730)
    cctbx.xfel.powder_from_spots: add interactive peak picking

--- xfel/small_cell/spotfinder_radial_average.py
+++ xfel/small_cell/spotfinder_radial_average.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tick
 
-
 class Spotfinder_radial_average:
 
   def __init__(self, params):
@@ -23,6 +22,22 @@ class Spotfinder_radial_average:
         )
     if i_bin < 0 or i_bin >= n_bins: return
     self.panelsums[i_panel][i_bin] += value
+
+  def _nearest_peak(self, x, xvalues, yvalues):
+    i = np.searchsorted(xvalues, x, side="left")
+    #Exclude (before) first and (after) last points
+    if i < 1 or i >= len(xvalues):
+      print ("Not a valid peak.")
+      return None
+    #Exclude troughs
+    if yvalues[i-1] >= yvalues[i] and yvalues[i+1] >= yvalues[i]:
+      print ("Not a valid peak.")
+      return None
+    #find the highest nearby yvalue
+    direction = 1 if yvalues[i+1] > yvalues[i] else -1
+    while yvalues[i+direction] > yvalues[i]:
+      i += direction
+    return 1/xvalues[i]
 
   def calculate(self):
     params = self.params
@@ -112,5 +127,40 @@ class Spotfinder_radial_average:
       with open(params.output.xy_file, 'w') as f:
         for x,y in zip(xvalues, yvalues):
           f.write("{:.6f}\t{}\n".format(1/x, y))
-    plt.show()
+
+    if params.output.peak_file:
+      backend_list = ["TkAgg","QtAgg"]
+      assert (plt.get_backend() in backend_list), """Matplotlib backend not compatible with interactive peak picking.
+You can set the MPLBACKEND environment varibale to change this.
+Currently supported options: %s""" %backend_list
+      #If a peak list output file is specified, do interactive peak picking:
+      with open(params.output.peak_file, 'w') as f:
+        vertical_line = ax.axvline(color='r', lw=0.8, ls='--', x=xvalues[1])
+        vertical_line.set_visible(False)
+        def onmove(event):
+          if fig.canvas.toolbar.mode: return
+          x = event.xdata
+          vertical_line.set_xdata(x)
+          if plt.getp(vertical_line, 'visible'):
+            ax.figure.canvas.draw()
+        def onclick(event):
+          if fig.canvas.toolbar.mode: return
+          vertical_line.set_visible(True)
+        def onrelease(event):
+          if fig.canvas.toolbar.mode: return
+          vertical_line.set_visible(False)
+          ax.figure.canvas.draw()
+          peak = self._nearest_peak(event.xdata,xvalues,yvalues)
+          if peak is not None:
+            print('Selected x=%f, nearest local maximum=%f, writing to %s.' % (1/event.xdata, peak, params.output.peak_file))
+            f.write(str(peak)+"\n")
+
+        mmv = fig.canvas.mpl_connect('motion_notify_event', onmove)
+        cid = fig.canvas.mpl_connect('button_press_event', onclick)
+        ciu = fig.canvas.mpl_connect('button_release_event', onrelease)
+
+        plt.show()
+
+    else:
+      plt.show()
 
