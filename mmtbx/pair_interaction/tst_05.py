948ce1b338f7b69bd404dbfa8f54dcc3e3ea9ec1
Pavel Afonine <pafonine@lbl.gov>
2022-02-22 16:41:21 -0800

    Skip test if required folder is missing

--- mmtbx/pair_interaction/tst_05.py
+++ mmtbx/pair_interaction/tst_05.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, division, print_function
 from mmtbx.pair_interaction import pair_interaction
 import os
+import libtbx.load_env
 
 def run():
+  folder = libtbx.env.find_in_repositories("qrefine/plugin/yoink/dat")
+  if folder is None: return # skip test
   assert not os.path.isfile("p__lda.wfc")
   o = pair_interaction.load_wfc("p")
   assert os.path.isfile("p__lda.wfc")
