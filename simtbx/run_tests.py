d04f8e1358dbeab7ee16f1d17b4c22a29416e296
Billy K. Poon <bkpoon@lbl.gov>
2022-03-30 06:48:11 -0700

    Only run kokkos tests on linux

--- simtbx/run_tests.py
+++ simtbx/run_tests.py
@@ -87,7 +87,7 @@ if OPT.enable_cuda:
           tst_list_parallel.append(par_tst)
 
       tst_list_parallel += db_tst_list_onlyCuda
-elif OPT.enable_kokkos:
+elif OPT.enable_kokkos and sys.platform.startswith('linux'):
    tst_list_parallel = [
      ["$D/nanoBragg/tst_gauss_argchk.py","CPU"], # tests CPU argchk optimization
      "$D/kokkos/tst_kokkos_lib.py",              # GPU in kokkos
