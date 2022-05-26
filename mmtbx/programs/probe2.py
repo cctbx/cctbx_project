a1c1f94c3c5f76718e09728be791b8e4beded9d6
Russ Taylor <russ@reliasolve.com>
2022-05-26 10:18:29 -0400

    Modifying the way Probe2 handles looking for contacts when keeping unselected atoms so that collisions with them are found and then they are ignored afterwards.

--- mmtbx/programs/probe2.py
+++ mmtbx/programs/probe2.py
@@ -675,13 +675,15 @@ Note:
 
 # ------------------------------------------------------------------------------
 
-  def _generate_interaction_dots(self, sourceAtoms, targetQuery, phantomsQuery, bondedNeighborLists):
+  def _generate_interaction_dots(self, sourceAtoms, targetSet, spatialQuery, phantomsQuery, bondedNeighborLists):
     '''
       Find all interaction dots for the specified atom.
       This does not include locations where the probe is inside a bonded neighbor.
-      :param sourceAtoms: Atoms that can be the source of an interaction.
-      :param targetQuery: Spatial-query structure that can be used to look up nearby target atoms.
-      (can be the same list as sourceAtoms for some approaches).
+      :param sourceAtoms: Sorted list of atoms that can be the source of an interaction.
+      :param targetSet: Set of atoms that are targets; others will block dots but not interact
+      (can be the same as sourceAtoms for some approaches).
+      :param spatialQuery: Spatial-query structure that can be used to look up nearby atoms
+      (all atoms, whether or not they are targets).
       :param phantomsQuery: Spatial-query structure that can be used to look up nearby Phantom
       Hydrogens (whether or not they are in the source or target atoms).
       :param bondedNeighborLists: List of bonded neighbors for atoms in sourceAtoms.
@@ -710,7 +712,7 @@ Note:
         continue
 
       # Find atoms that are close enough that they might touch.
-      nearby = targetQuery.neighbors(src.xyz, 0.00001, maxRadius)
+      nearby = spatialQuery.neighbors(src.xyz, 0.00001, maxRadius)
 
       # Find our characteristics
       srcMainChain = self._inMainChain[src]
@@ -804,6 +806,10 @@ Note:
 
           # Handle any dots that should not be ignored.
           if overlapType != probeExt.OverlapType.Ignore:
+
+            # If the cause of the dot is not in the target set, we ignore the dot.
+            if not res.cause in targetSet:
+              continue
 
             # If the overlap type is not a Hydrogen bond, then check the occupancy of atoms where
             # at least one of the pair is on the "" or " " alternate conformation to make sure the
@@ -2183,9 +2189,8 @@ Note:
         make_sub_header('Find self-intersection dots', out=self.logger)
 
         # Generate dots for the source atom set against itself.
-        self._generate_interaction_dots(self._source_atoms_sorted,
-          Helpers.createSpatialQuery(self._source_atoms_sorted, self.params.probe),
-          self._phantomHydrogensSpatialQuery, bondedNeighborLists)
+        self._generate_interaction_dots(self._source_atoms_sorted, source_atoms,
+          self._spatialQuery, self._phantomHydrogensSpatialQuery, bondedNeighborLists)
 
         # Generate our report
         outString += self._report_single_interaction(groupLabel, "self", "1->1", "SelfIntersect",
@@ -2195,9 +2200,8 @@ Note:
         make_sub_header('Find single-direction intersection dots', out=self.logger)
 
         # Generate dots for the source atom set against the target atom set.
-        self._generate_interaction_dots(self._source_atoms_sorted,
-          Helpers.createSpatialQuery(self._target_atoms_sorted, self.params.probe),
-          self._phantomHydrogensSpatialQuery, bondedNeighborLists)
+        self._generate_interaction_dots(self._source_atoms_sorted, target_atoms,
+          self._spatialQuery, self._phantomHydrogensSpatialQuery, bondedNeighborLists)
 
         # Generate our report
         outString += self._report_single_interaction(groupLabel, "once", "1->2", "IntersectOnce",
@@ -2226,9 +2230,8 @@ Note:
         # =================== First direction ========================
 
         # Generate dots for the source atom set against the target atom set.
-        self._generate_interaction_dots(self._source_atoms_sorted,
-          Helpers.createSpatialQuery(self._target_atoms_sorted, self.params.probe),
-          self._phantomHydrogensSpatialQuery, bondedNeighborLists)
+        self._generate_interaction_dots(self._source_atoms_sorted, target_atoms,
+          self._spatialQuery, self._phantomHydrogensSpatialQuery, bondedNeighborLists)
 
         # Count the dots if we've been asked to do so.
         if self.params.output.count_dots:
@@ -2262,9 +2265,8 @@ Note:
         self._clear_results();
 
         # Generate dots for the target atom set against the source atom set.
-        self._generate_interaction_dots(self._target_atoms_sorted,
-          Helpers.createSpatialQuery(self._source_atoms_sorted, self.params.probe),
-          self._phantomHydrogensSpatialQuery, bondedNeighborLists)
+        self._generate_interaction_dots(self._target_atoms_sorted, source_atoms,
+          self._spatialQuery, self._phantomHydrogensSpatialQuery, bondedNeighborLists)
 
         # Count the dots if we've been asked to do so.
         if self.params.output.count_dots:
