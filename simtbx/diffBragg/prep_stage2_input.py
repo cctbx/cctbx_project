1eb935f6a5ffffab8dbbc89b5ea2307a1e09a3e6
Felix Wittwer <fwittwer@lbl.gov>
2022-06-22 10:21:17 -0700

    Streamline prep_dataframe code

--- simtbx/diffBragg/prep_stage2_input.py
+++ simtbx/diffBragg/prep_stage2_input.py
@@ -10,21 +10,21 @@ import logging
 
 LOGGER = logging.getLogger("diffBragg.main")
 
-def get_equal_partition(samples, partitions):
+def get_equal_partition(weights, partitions):
     """
     Use Longest-processing-time-first to schedule the set of samples into partitions.
 
-    :param samples: list of weights
+    :param weights: list of weights
     :param partitions: number of partitions
     :return: list of indices for each partition
     """
     distribution = [[] for _ in range(partitions)]
     load = np.zeros(partitions)
-    descending = np.array(samples).argsort()[::-1]
+    descending = np.array(weights).argsort()[::-1]
     for idx in descending:
         lightest = load.argmin()
         distribution[lightest].append(idx)
-        load[lightest] += samples[idx]
+        load[lightest] += weights[idx]
     return distribution
 
 def prep_dataframe(df):
@@ -46,22 +46,18 @@ def prep_dataframe(df):
     work_distribution = None
     if COMM.rank==0:
         print("Found %d shots" % nshots)
-        high_shot = max([i_shot for i_shot,n in refls_per_shot])
-        assert nshots == (high_shot+1)
-
-        weights = [0] * nshots
-        for i_shot,n in refls_per_shot:
-            assert weights[i_shot] == 0
-            weights[i_shot] = n
+        refls_per_shot = sorted(refls_per_shot)
+        indices, weights = zip(*refls_per_shot)
+        assert list(indices) == list(range(nshots)) # new sanity test
 
         work_distribution = get_equal_partition(weights, COMM.size)
     work_distribution = COMM.bcast(work_distribution, root=0)
-    assert work_distribution is not None, f"ERROR! Rank {COMM.rank} has no work_distribution!"
+    assert work_distribution is not None, "ERROR! Rank %d has no work_distribution!" % COMM.rank
     return work_distribution
 
 
 if __name__ == "__main__":
     import sys
     df = pandas.read_pickle(sys.argv[1])
-    new_df = prep_dataframe(df, time_to_try=float(sys.argv[2]))
-    new_df.to_pickle(sys.argv[3])
+    new_df = prep_dataframe(df)
+    new_df.to_pickle(sys.argv[2])
