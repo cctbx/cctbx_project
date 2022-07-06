8fafb14da3abdd7b2ce21338b921f69ed6a2bb59
Oleg Sobolev <osobolev@lbl.gov>
2022-07-06 09:45:52 -0700

    Should be more compatible with various platforms

--- cctbx/regression/tst_wavelength_units.py
+++ cctbx/regression/tst_wavelength_units.py
@@ -1,16 +1,16 @@
 from __future__ import absolute_import, division, print_function
 from libtbx import easy_run
-from libtbx.test_utils import assert_lines_in_file
+from libtbx.test_utils import assert_lines_in_text
 
-def exercise_1(prefix='tst_wavelength_units'):
-  easy_run.call("cctbx.wavelength_units &> %s" % prefix)
-  assert_lines_in_file('%s' % prefix, "Usage: cctbx.wavelength_units 1A|1keV [...]")
+def exercise_1():
+  fb = easy_run.fully_buffered("cctbx.wavelength_units")
+  assert_lines_in_text('\n'.join(fb.stderr_lines), "Usage: cctbx.wavelength_units 1A|1keV [...]")
 
-  easy_run.call("cctbx.wavelength_units 2.3a &> %s" % prefix)
-  assert_lines_in_file('%s' % prefix, "2.30000 A = 5.39062 keV")
+  fb = easy_run.fully_buffered("cctbx.wavelength_units 2.3a")
+  assert_lines_in_text('\n'.join(fb.stdout_lines), "2.30000 A = 5.39062 keV")
 
-  easy_run.call("cctbx.wavelength_units 9.8kev &> %s" % prefix)
-  assert_lines_in_file('%s' % prefix, "1.26514 A = 9.80000 keV")
+  fb = easy_run.fully_buffered("cctbx.wavelength_units 9.8kev")
+  assert_lines_in_text('\n'.join(fb.stdout_lines), "1.26514 A = 9.80000 keV")
 
 if (__name__ == "__main__"):
   exercise_1()
