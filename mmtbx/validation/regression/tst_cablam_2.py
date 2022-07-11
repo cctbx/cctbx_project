564cbd8f43db42a6edb30ddf428e3e0e17fee7aa
Oleg Sobolev <osobolev@lbl.gov>
2022-07-11 15:17:08 -0700

    Buffering outputs because cablam mirrors output to stderr which is discouraged in tests.

--- mmtbx/validation/regression/tst_cablam_2.py
+++ mmtbx/validation/regression/tst_cablam_2.py
@@ -5,7 +5,8 @@ import libtbx.load_env
 
 def exercise_01():
   easy_run.call("phenix.fetch_pdb 5o61 -c")
-  assert not easy_run.call("phenix.cablam 5o61.cif"), "For Chris to fix"
+  fb = easy_run.fully_buffered("phenix.cablam 5o61.cif")
+  assert fb.return_code == 0, fb.return_code
 
 if (__name__ == "__main__"):
   t0 = time()
