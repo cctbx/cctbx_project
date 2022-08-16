f581b546270ecc773e4021fa17358c5e3ec88b4d
cschlick <cjschlicksup@lbl.gov>
2022-08-16 15:34:27 -0700

    Initial commit, rdkit utils

---
+++ mmtbx/ligands/rdkit_utils.py
@@ -0,0 +1,155 @@
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import rdDistGeom
+from rdkit.Chem import rdFMCS
+
+"""
+Utility functions to work with rdkit
+
+Functions:
+  convert_model_to_rdkit: Convert cctbx model to rdkit mol
+  convert_elbow_to_rdkit: Convert elbow molecule to rdkit mol
+  mol_to_3d: Generate 3D conformer of an rdkit mol
+  mol_to_2d: Generate 2D conformer of an rdkit mol
+  mol_from_smiles: Generate rdkit mol from smiles string
+  match_mol_indices: Match atom indices of different mols
+"""
+
+def convert_model_to_rdkit(cctbx_model):
+  """
+  Convert a cctbx model molecule object to an
+  rdkit molecule object
+
+  TODO: Bond type is always unspecified
+  """
+  assert cctbx_model.restraints_manager is not None, "Restraints manager must be set"
+
+  mol = Chem.Mol()
+  rwmol = Chem.RWMol(mol)
+  conformer = Chem.Conformer(cctbx_model.get_number_of_atoms())
+
+  for i,atom in enumerate(cctbx_model.get_atoms()):
+      element = atom.element.strip().upper()
+      if element =="D":
+        element = "H"
+      else:
+        element = element
+      atomic_number = Chem.GetPeriodicTable().GetAtomicNumber(element)
+      rdatom = Chem.Atom(atomic_number)
+      rdatom.SetFormalCharge(atom.charge_as_int())
+      rdatom_idx = rwmol.AddAtom(rdatom)
+
+      conformer.SetAtomPosition(rdatom_idx,atom.xyz)
+
+
+  rm = cctbx_model.restraints_manager
+  grm = rm.geometry
+  bonds_simple, bonds_asu = grm.get_all_bond_proxies()
+  bond_proxies = bonds_simple.get_proxies_with_origin_id()
+  for bond_proxy in bond_proxies:
+    begin, end = bond_proxy.i_seqs
+    order = Chem.rdchem.BondType.UNSPECIFIED
+    rwmol.AddBond(int(begin),int(end),order)
+
+  rwmol.AddConformer(conformer)
+  mol = rwmol.GetMol()
+  return mol
+
+def convert_elbow_to_rdkit(elbow_mol):
+  """
+  Convert elbow molecule object to an
+  rdkit molecule object
+
+  TODO: Charge
+  """
+
+  # elbow bond order to rdkit bond orders
+  bond_order_elbowkey = {
+    1.5:Chem.rdchem.BondType.AROMATIC,
+    1: Chem.rdchem.BondType.SINGLE,
+    2: Chem.rdchem.BondType.DOUBLE,
+    3: Chem.rdchem.BondType.TRIPLE,
+  }
+  bond_order_rdkitkey = {value:key for key,value in bond_order_elbowkey.items()}
+
+
+  atoms = list(elbow_mol)
+
+  mol = Chem.Mol()
+  rwmol = Chem.RWMol(mol)
+  conformer = Chem.Conformer(len(atoms))
+
+  for i,atom in enumerate(atoms):
+    xyz = atom.xyz
+    atomic_number = atom.number
+    rdatom = rwmol.AddAtom(Chem.Atom(int(atomic_number)))
+    conformer.SetAtomPosition(rdatom,xyz)
+
+  for i,bond in enumerate(elbow_mol.bonds):
+    bond_atoms = list(bond)
+    start,end = atoms.index(bond_atoms[0]), atoms.index(bond_atoms[1])
+    order = bond_order_elbowkey[bond.order]
+    rwmol.AddBond(int(start),int(end),order)
+
+  rwmol.AddConformer(conformer)
+  mol = rwmol.GetMol()
+  return mol
+
+
+
+def mol_to_3d(mol):
+  """
+  Convert and rdkit mol to 3D coordinates
+  """
+  assert len(mol.GetConformers())==0, "mol already has conformer"
+  param = rdDistGeom.ETKDGv3()
+  conf_id = rdDistGeom.EmbedMolecule(mol,clearConfs=True)
+  return mol
+
+def mol_to_2d(mol):
+  """
+  Convert and rdkit mol to 2D coordinates
+  """
+  mol = Chem.Mol(mol) # copy to preserve original coords
+  ret = Chem.rdDepictor.Compute2DCoords(mol)
+  return mol
+
+def mol_from_smiles(smiles,embed3d=False,addHs=True,removeHs=False):
+  """
+  Convert a smiles string to rdkit mol
+  """
+  ps = Chem.SmilesParserParams()
+  ps.removeHs=removeHs
+  rdmol = Chem.MolFromSmiles(smiles,ps)
+
+  if addHs:
+    rdmol = Chem.AddHs(rdmol)
+
+  if embed3d:
+    rdmol = mol_to_3d(rdmol)
+
+
+  if removeHs:
+    rdmol = Chem.RemoveHs(rdmol)
+
+  Chem.SetHybridization(rdmol)
+  rdmol.UpdatePropertyCache()
+  return rdmol
+
+def match_mol_indices(mol_list):
+  """
+  Match atom indices of molecules.
+
+  Args:
+      mol_list (list): a list of rdkit mols
+
+  Returns:
+      match_list: (list): a list of tuples
+                          Each entry is a match beween in mols
+                          Each value is the atom index for each mol
+  """
+  mol_list = [Chem.Mol(mol) for mol in mol_list]
+  mcs_SMARTS = rdFMCS.FindMCS(mol_list)
+  smarts_mol = Chem.MolFromSmarts(mcs_SMARTS.smartsString)
+  match_list = [x.GetSubstructMatch(smarts_mol) for x in mol_list]
+  return list(zip(*match_list))
