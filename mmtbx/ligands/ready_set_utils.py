fd2bf597d1b699f0d26defebb7e5a368fb2e6e7e
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2022-10-12 10:18:05 -0700

    removed print

--- mmtbx/ligands/ready_set_utils.py
+++ mmtbx/ligands/ready_set_utils.py
@@ -384,7 +384,6 @@ def _hierarchy_into_slots(hierarchy,
                           verbose=False,
                           ):
   def _is_linked(residue_group1, residue_group2, bpt):
-    print(residue_group1, residue_group2)
     if residue_group2 is None: return False
     if residue_group2 is False: return False
     atom_group1 = residue_group1.only_atom_group()
