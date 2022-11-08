9e484d6dc86559f408e5db33a02aaa13c6464bed
Aaron Brewster <asbrewster@lbl.gov>
2022-11-07 16:00:13 -0800

    XFEL cosets: use logger instead of print

--- xfel/util/sublattice_helper.py
+++ xfel/util/sublattice_helper.py
@@ -4,7 +4,7 @@ from rstbx.sublattice_support.change_basis import sublattice_change_of_basis
 # for debug: from cctbx.crystal_orientation import crystal_orientation
 from cctbx.miller import set as miller_set
 from cctbx.crystal import symmetry
-import copy
+import copy, logging
 from dials.array_family import flex
 from dials.algorithms.indexing.stills_indexer import calc_2D_rmsd_and_displacements
 
@@ -21,15 +21,17 @@ assert SLT[4].matS().elems == (2,0,1,0,1,0,0,0,1) # B-face centering
 assert SLT[5].matS().elems == (1,0,0,0,2,1,0,0,1) # A-face centering
 assert SLT[6].matS().elems == (2,1,1,0,1,0,0,0,1) # Body centering
 
+logger = logging.getLogger(__name__)
+
 def integrate_coset(self, experiments, indexed):
         TRANS = self.params.integration.coset.transformation
 
         # here get a deepcopy that we are not afraid to modify:
         experiments_local = copy.deepcopy(experiments)
 
-        print("*" * 80)
-        print("Coset Reflections for modeling or validating the background")
-        print("*" * 80)
+        logger.info("*" * 80)
+        logger.info("Coset Reflections for modeling or validating the background")
+        logger.info("*" * 80)
         from dials.algorithms.profile_model.factory import ProfileModelFactory
         from dials.algorithms.integration.integrator import create_integrator
 
@@ -60,8 +62,8 @@ def integrate_coset(self, experiments, indexed):
           # debug OriAstarprime = crystal_orientation(Astarprime,True)
           # debug OriAstarprime.show(legend="new ")
 
-        print("Predicting coset reflections")
-        print("")
+        logger.info("Predicting coset reflections")
+        logger.info("")
         predicted = flex.reflection_table.from_predictions_multi(
             experiments_local,
             dmin=self.params.prediction.d_min,
@@ -69,7 +71,7 @@ def integrate_coset(self, experiments, indexed):
             margin=self.params.prediction.margin,
             force_static=self.params.prediction.force_static,
         )
-        print("sublattice total predictions %d"%len(predicted))
+        logger.info("sublattice total predictions %d"%len(predicted))
 
         # filter the sublattice, keep only the coset indices
         miller = predicted["miller_index"]
@@ -86,9 +88,9 @@ def integrate_coset(self, experiments, indexed):
             if icom.denominator() > 1: on_coset=True; break
           coset_select_algorithm_2.append(on_coset)
         predicted = predicted.select(coset_select_algorithm_2)
-        print("of which %d are in coset %d"%(len(predicted), TRANS))
+        logger.info("of which %d are in coset %d"%(len(predicted), TRANS))
 
-        print("")
+        logger.info("")
         integrator = create_integrator(self.params, experiments_local, predicted)
 
         # Integrate the reflections
@@ -158,5 +160,5 @@ def integrate_coset(self, experiments, indexed):
                     )
                 )
 
-        print(log_str)
-        print("")
+        logger.info(log_str)
+        logger.info("")
