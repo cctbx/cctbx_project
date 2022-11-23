79de879ce4d2622e5cc59e6506447a32104cd0a1
Oleg Sobolev <osobolev@lbl.gov>
2022-11-23 13:26:21 -0800

    Function for group rounding with tests.

--- iotbx/run_tests.py
+++ iotbx/run_tests.py
@@ -139,6 +139,7 @@ tst_list_base = [
   "$D/regression/tst_extract_xtal_data.py",
   "$D/regression/tst_cli_parser.py",
   "$D/regression/tst_mtz_as_cif.py",
+  "$D/regression/tst_group_rounding.py",
   ]
 
 # failing tests on Windows, Python 2.7
