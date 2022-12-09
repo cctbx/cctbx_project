cad0585b5e8e98d159295f46d7eb9b497e439d67
Pavel <pafonine@lbl.gov>
2022-12-08 18:10:46 -0800

    Converted to use program template and DataManager, moved to phenix/programs

--- mmtbx/command_line/twin_map_utils.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import, division, print_function
-# LIBTBX_SET_DISPATCHER_NAME phenix.twin_map_utils
-
-from mmtbx.twinning import twin_map_utils
-import sys
-
-if (__name__ == "__main__"):
-  twin_map_utils.run(args=sys.argv[1:])
