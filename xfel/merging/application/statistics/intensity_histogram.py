e86734a2e0b5dbba7b39ee5f6d8e5c3d3bbab9f2
Daniel Paley <dwpaley@lbl.gov>
2022-12-19 10:02:42 -0800

    Basic working intensity histogram
    Put `statistics_intensity_histogram` in step_list after group. Run with
    `statistics.histogram.mode=individual`.

--- xfel/merging/application/statistics/intensity_histogram.py
+++ xfel/merging/application/statistics/intensity_histogram.py
@@ -6,6 +6,8 @@ from xfel.merging.application.reflection_table_utils import \
     reflection_table_utils as rt_util
 from cctbx.crystal import symmetry
 from cctbx import miller
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class intensity_histogram(worker):
@@ -63,22 +65,47 @@ class intensity_histogram(worker):
           d_min=self.params.merging.d_min,
           d_max=self.params.merging.d_max
       ).set_observation_type_xray_intensity()
-      import IPython;IPython.embed()
 
 
       d_vals = [x[1] for x in all_obs.d_spacings()] # dspacings are ((h,k,l),d)
       n_unique = len(d_vals)
       d_percentiles = [0, 5, 10, 40, 80]
+      to_plot = [[] for _ in d_percentiles]
       d_slice_starts = [int(x*n_unique/100) for x in d_percentiles]
-      d_slice_size = n_unique/100
+      d_slice_size = int(n_unique/100)
       d_slices = [slice(x, x+d_slice_size) for x in d_slice_starts]
 
-      d_i_miller = zip(d_vals, all_obs.data(), all_obs.indices())
+      # This has to be a list because we're going to slice it... vvv
+      d_i_miller = list(zip(d_vals, all_obs.data(), all_obs.indices()))
+      d_i_miller = sorted(d_i_miller, reverse=True)
       outer_slices = [d_i_miller[s] for s in d_slices]
 
-      iobs_percentiles = [0,5,10,40,80]
-      print(list(all_obs[0:2].indices()))
-      print(list(all_obs[0:2].data()))
+      for i_outer, outer in enumerate(outer_slices):
+        # now sort the d-slices by decreasing intensity
+        outer_sorted = sorted(outer, key=lambda x:x[1], reverse=True)
+        n_inner = len(outer_sorted)
+        iobs_percentiles = [0,5,10,40,80]
+        indices_inner = [int(x*n_inner/100) for x in iobs_percentiles]
+        for i_inner, i in enumerate(indices_inner):
+          item = outer_sorted[i]
+          to_plot[i_outer].append(item)
+          #self.single_histogram(reflections, m_i, ax[i_outer][i_inner])
+
+    if self.mpi_helper.rank != 0: to_plot = None
+    to_plot = self.mpi_helper.comm.bcast(to_plot, root=0)
+    if self.mpi_helper.rank == 0:
+      fig, axes = plt.subplots(5,5)
+      fig.set_size_inches((12,8))
+    else:
+      fig, axes = None, None
+    for i1, l in enumerate(to_plot):
+      for i2, m_i in enumerate(l):
+        self.single_histogram(reflections, m_i, axes, i1, i2)
+    if self.mpi_helper.rank == 0:
+      plt.savefig('plot.png')
+
+
+
 
 #    if self.mpi_helper.rank==0:
 #      import IPython;IPython.embed()
@@ -86,7 +113,8 @@ class intensity_histogram(worker):
 #      import time;time.sleep(10000)
     return experiments, reflections
 
-  def single_histogram(self, reflections, miller, ax):
+  def single_histogram(self, reflections, item, axes, i1, i2):
+    d, iobs, miller = item
     sel = reflections['miller_index_asymmetric'] == miller
     matching_refl = reflections.select(sel)
     matching_intensities = matching_refl['intensity.sum.value']
@@ -98,7 +126,29 @@ class intensity_histogram(worker):
       final_intensities = flex.double()
       for x in all_matching_intensities:
         final_intensities.extend(x)
-      ax.hist(final_intensities.as_numpy_array())
+      i_arr = final_intensities.as_numpy_array()
+
+      q1 = np.percentile(i_arr, 25)
+      q3 = np.percentile(i_arr, 75)
+      med = np.percentile(i_arr, 50)
+      iqr = q3-q1
+      xmin = min(0, q1) - 2*iqr
+      xmin = max(xmin, min(i_arr))
+      xmax = iobs + 2*iqr
+      xmin = np.percentile(i_arr, 2.5) - iqr/5
+      xmax = np.percentile(i_arr, 97.5) + iqr/5
+
+
+      ax = axes[i1][i2]
+      hist = ax.hist(i_arr, bins=np.linspace(xmin, xmax, 50))
+      ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+      title = "%.2f, %.0f, %d" %(d, iobs, len(i_arr))
+      ax.set_title(title)
+
+      ymax = max(hist[0])
+      ax.plot((0,0), (0,ymax), 'k-')
+      ax.plot((iobs,iobs), (0,ymax), 'r-')
+
 
 
     
