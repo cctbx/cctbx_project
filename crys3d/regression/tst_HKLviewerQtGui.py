1a03fc2d109d2e0a077f2126d24b32d142475ffd
Robert Oeffner <rdo20@cam.ac.uk>
2022-12-20 19:59:44 +0000

    run all OS browser tests sequentially and all QtGUI tests sequentially

--- crys3d/regression/tst_HKLviewerQtGui.py
+++ crys3d/regression/tst_HKLviewerQtGui.py
@@ -5,6 +5,8 @@ from crys3d.regression import tests_HKLviewer
 def run():
   tests_HKLviewer.exerciseQtGUI(tests_HKLviewer.philstr1, tests_HKLviewer.reflections2match1,
                                "QtGuiSliceK-9")
+  tests_HKLviewer.exerciseQtGUI(tests_HKLviewer.philstr3, tests_HKLviewer.reflections2match3,
+                               "QtGuiBinFSigF")
   print("OK")
 
 
