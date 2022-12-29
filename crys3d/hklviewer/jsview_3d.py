70f1ca6dc0f2a713d2577ef09ab25feca9c67bb7
Robert Oeffner <robert@oeffner.net>
2022-12-29 21:08:46 +0100

    sleep(3)

--- crys3d/hklviewer/jsview_3d.py
+++ crys3d/hklviewer/jsview_3d.py
@@ -543,7 +543,7 @@ class HKLview_3d:
         self.mprint("set_volatile_params released clipplane_msg_sem", verbose="threadingmsg")
 
         if len(self.WBmessenger.clientmsgqueue):
-          time.sleep(1)
+          time.sleep(3)
 
       if self.params.viewer.fixorientation == "vector":
         self.orient_vector_to_screen(self.currentrotvec)
