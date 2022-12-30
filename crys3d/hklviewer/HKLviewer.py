d6f230286dcdb23e4b50574fd7d52159c1bd490b
Robert Oeffner <robert@oeffner.net>
2022-12-30 10:04:40 +0100

    don't overwrite os.environ["QTWEBENGINE_CHROMIUM_FLAGS"]

--- crys3d/hklviewer/HKLviewer.py
+++ crys3d/hklviewer/HKLviewer.py
@@ -2730,10 +2730,10 @@ clip_plane {
       self.settings = QSettings("CCTBX", "HKLviewer" )
     if self.QWebEngineViewFlags is None: # avoid doing this test over and over again on the same PC
       flgs = os.environ.get("QTWEBENGINE_CHROMIUM_FLAGS", "")
-      self.QWebEngineViewFlags = flgs + " --disable-web-security" \
-        + " --enable-webgl-software-rendering --disable-gpu-compositing" \
-        + " --disable_chromium_framebuffer_multisample --use-gl=swiftshader" \
-        + " --swiftshader --swiftshader-webgl --ignore-gpu-blacklist"
+      self.QWebEngineViewFlags = flgs + " --disable-web-security" #\
+      #  + " --enable-webgl-software-rendering --disable-gpu-compositing" \
+      #  + " --disable_chromium_framebuffer_multisample --use-gl=swiftshader" \
+      #  + " --swiftshader --swiftshader-webgl --ignore-gpu-blacklist"
 
       os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = self.QWebEngineViewFlags
       # QTWEBENGINE_CHROMIUM_FLAGS environment is now set for TestWebGL()
@@ -2840,7 +2840,7 @@ def run(isembedded=False, chimeraxsession=None):
           if not kwargs.get('hklin', False):
             kwargs['hklin'] = arg
 
-    os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = " "
+    os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = os.environ.get("QTWEBENGINE_CHROMIUM_FLAGS", "")
     argstr = " ".join(sys.argv[1:])
 
     if "remove_settings" in argstr:
