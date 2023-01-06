2d263db6bca40fe8fd3740e46bb4f6dbf4218a44
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2023-01-06 13:34:46 -0800

    generalised from ARG to all torsions in planes

--- mmtbx/programs/arginine_geometry.py
+++ mmtbx/programs/arginine_geometry.py
@@ -4,6 +4,7 @@ from libtbx.program_template import ProgramTemplate
 from cctbx import geometry_restraints
 
 from cctbx.array_family import flex
+from libtbx.utils import null_out
 
 limits = {
   ('CD', 'NE', 'CZ', 'NH1')  : 10,
@@ -18,29 +19,37 @@ limits = {
   ('NE', 'CZ', 'NHn', 'HHn?'): 1,
   }
 
+def get_torsion_deviations(v):
+  if v<-90: v+=360
+  if v>90: v-=180
+  return v
+
 def get_atoms_key(atoms):
   if atoms[3].find('HH')>-1: return ('NE', 'CZ', 'NHn', 'HHn?')
   if atoms[0]=='CD': return ('CD', 'NE', 'CZ', 'NH?')
   return atoms
 
+def format_atoms(t):
+  outl = ''
+  for atom in t:
+    outl += '%4s -' % atom
+  return outl[:-1]
+
+def format_values(v):
+  outl = ''
+  for attr, value in zip(['mean', 'min', 'max'], v):
+    outl += ' %s: %4.1f' % (attr, value)
+  return outl
+
+def format_annotation(values):
+  limit = limits.get(atoms, 1e9)
+  ann = ''
+  if abs(values[1])>limit: ann+='<'
+  if values[2]>limit: ann+='>'
+  return ann
+
 class arginine_deviations(dict):
   def __repr__(self):
-    def format_atoms(t):
-      outl = ''
-      for atom in t:
-        outl += '%4s -' % atom
-      return outl[:-1]
-    def format_values(v):
-      outl = ''
-      for attr, value in zip(['mean', 'min', 'max'], v):
-        outl += ' %s: %4.1f' % (attr, value)
-      return outl
-    def format_annotation(values):
-      limit = limits.get(atoms, 1e9)
-      ann = ''
-      if abs(values[1])>limit: ann+='<'
-      if values[2]>limit: ann+='>'
-      return ann
     outl = 'ARG devs\n'
     outl += '  Number of ARG : %s\n' % len(self)
     current = getattr(self, 'atoms',{})
@@ -54,19 +63,40 @@ class arginine_deviations(dict):
     return outl
 
   def process(self):
-    def positive(v):
-      if v<-90: v+=360
-      if v>90: v-=180
-      return v
     self.data = {}
     for residue, chis in self.items():
       for atoms, chi in chis.items():
         key = get_atoms_key(atoms)
         self.data.setdefault(key, flex.float())
-        self.data[key].append(positive(chi))
+        self.data[key].append(get_torsion_deviations(chi))
     self.atoms = {}
     for atoms, chis in self.data.items():
       self.atoms[atoms]=[flex.mean(chis), flex.min(chis), flex.max(chis)]
+
+class plane_torsion_deviations(arginine_deviations):
+  def __init__(self, print_torsion_limit=1., print_torsion_number=10):
+    self.print_torsion_limit=print_torsion_limit
+    self.print_torsion_number=print_torsion_number
+
+  def __repr__(self):
+    tmp = dict(sorted(self.items(), reverse=True, key=lambda item: abs(item[1][0])))
+    outl = 'Plane Torsion Deviations\n'
+    outl += '  Number : %s\n' % len(self)
+    i=0
+    for key, item in tmp.items():
+      if abs(item[0])<self.print_torsion_limit: continue
+      i+=1
+      outl += '    %s : %5.1f %s\n' % (key, item[0], '*' if item[1] else '')
+      if i>= self.print_torsion_number: break
+    return outl
+
+  def is_possible_reduce(self):
+    rc=0
+    for key, item in self.items():
+      if not item[1]: continue
+      if abs(item[0])>1.:
+        rc+=1
+    return rc
 
 class Program(ProgramTemplate):
 
@@ -85,6 +115,10 @@ Usage examples:
       .type = atom_selection
       .help = what to select
       .multiple = True
+    print_torsion_limit = 1.
+      .type = float
+    print_torsion_number = 10
+      .type = int
   }
 """
 
@@ -96,7 +130,6 @@ Usage examples:
   def arginine_simple(self):
     model = self.data_manager.get_model()
     hierarchy = model.get_hierarchy()
-    self.results = arginine_deviations()
     for residue_group in hierarchy.residue_groups():
       if len(residue_group.atom_groups())>1: continue
       atom_group = residue_group.atom_groups()[0]
@@ -116,12 +149,58 @@ Usage examples:
     self.results.process()
 
   def torsions_in_planes(self):
-    assert 0
+    model = self.data_manager.get_model()
+    model.set_log(null_out())
+    model.process(make_restraints=True)
+    grm = model.get_restraints_manager()
+    atoms = model.get_hierarchy().atoms()
+    plane_i_seqs = []
+    remove = []
+    for i, plane in enumerate(grm.geometry.planarity_proxies):
+      plane_i_seqs.append(set(plane.i_seqs))
+      torsion_xyzs = []
+      torsion_quotes = []
+      for i_seq in plane.i_seqs:
+        torsion_xyzs.append(atoms[i_seq].xyz)
+        torsion_quotes.append(atoms[i_seq].quote()[8:])
+        if len(torsion_xyzs)==4:
+          v = geometry_restraints.dihedral(sites=torsion_xyzs, angle_ideal=0, weight=1).angle_model
+          v=get_torsion_deviations(v)
+          if abs(v)>1: break
+          del torsion_xyzs[0]
+          del torsion_quotes[0]
+      else:
+        remove.append(i)
+    if remove:
+      remove.reverse()
+      for r in remove:
+        del plane_i_seqs[r]
+    data = {}
+    for torsion in grm.geometry.dihedral_proxies:
+      for plane in plane_i_seqs:
+        if len(plane.intersection(set(torsion.i_seqs)))==4:
+          torsion_xyzs = []
+          torsion_strs = ''
+          h_atom = False
+          for i_seq in torsion.i_seqs:
+            atom = atoms[i_seq]
+            if atom.element_is_hydrogen(): h_atom=True
+            torsion_xyzs.append(atom.xyz)
+            torsion_strs += '%s - ' % (atom.id_str()[4:])
+          v = geometry_restraints.dihedral(sites=torsion_xyzs, angle_ideal=0, weight=1).angle_model
+          v = get_torsion_deviations(v)
+          self.results[torsion_strs[:-2]]=[v, h_atom]
 
   # ---------------------------------------------------------------------------
   def run(self, log=None):
-    self.arginine_simple()
+    # self.results = arginine_deviations()
+    # self.arginine_simple()
+    self.results = plane_torsion_deviations(self.params.arginine.print_torsion_limit,
+                                            self.params.arginine.print_torsion_number)
+    self.torsions_in_planes()
     print('\n%s' % self.results, file=log)
+    reduce_H_atoms = self.results.is_possible_reduce()
+    print('  Has reduce-like H atoms : %s\n' % reduce_H_atoms if reduce_H_atoms else '')
 
   # ---------------------------------------------------------------------------
   def get_results(self):
