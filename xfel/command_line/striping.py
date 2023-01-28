ff79a9d2bbc3e1a3a4e8b7261b9ecb89b9b57a19
Daniel Tchon <dantch@onet.eu>
2023-01-27 18:10:16 -0800

    Remove unused abc import

--- xfel/command_line/striping.py
+++ xfel/command_line/striping.py
@@ -1,7 +1,4 @@
 from __future__ import absolute_import, division, print_function
-
-import abc
-
 from six.moves import range
 # -*- Mode: Python; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 8 -*-
 #
