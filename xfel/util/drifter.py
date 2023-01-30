144c9c29ef915165728c7fdac2f4fbc4a3b6d004
Daniel Tchon <dantch@onet.eu>
2023-01-30 13:49:58 -0800

    Prevent JSONDecodeError when reading expts

--- xfel/util/drifter.py
+++ xfel/util/drifter.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, print_function, division
+from json.decoder import JSONDecodeError
 import glob
 import os
 import sys
@@ -324,7 +325,7 @@ class DriftScraper(object):
             if self.parameters.uncertainties:
               o_deltas = self.extract_origin_deltas(refined_expts, refined_refls)
               scrap_dict.update(o_deltas)
-          except (KeyError, IndexError) as e:
+          except (KeyError, IndexError, JSONDecodeError) as e:
             print(e)
           else:
             self.table.add(**scrap_dict)
