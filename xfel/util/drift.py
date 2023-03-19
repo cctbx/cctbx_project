33256d61ccc9fcb9b5626b5c2510f5c8d82c1bd6
Daniel Tchon <dantch@onet.eu>
2023-03-18 22:06:44 -0700

    Remove debug statements

--- xfel/util/drift.py
+++ xfel/util/drift.py
@@ -719,8 +719,6 @@ class DriftArtist(object):
   def _plot_drift_distribution(self, axes, values_key):
     x_flat = self.table_flat['original_index']
     y_flat = self.table_flat[values_key]
-    print('x:', x_flat)
-    print('y:', y_flat)
     b = (len(self.x), 100)
     r = [[-0.5, len(self.x) - 0.5], [min(y_flat), max(y_flat)]]
     axes.hist2d(x_flat, y_flat, bins=b, range=r, cmap=plt.cm.magma_r, cmin=0.5)
@@ -800,7 +798,6 @@ def run(params_):
   ds.scrap()
   dt.sort(by='run')
   print(dt)
-  print(dt.flat)
   da.publish()
 
 
