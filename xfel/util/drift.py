b48e3aad4d07f69d6ee4638b40ebd5f295b84da0
Daniel Tchon <dantch@onet.eu>
2023-03-20 12:00:00 -0700

    Add IndexLocator to secondary axis on drift plot 2

--- xfel/util/drift.py
+++ xfel/util/drift.py
@@ -701,7 +701,7 @@ class DriftArtist(object):
       ax_top = self.axx.secondary_xaxis('top')
       ax_top.tick_params(rotation=90)
       ax_top.set_xticks(self.axx.get_xticks())
-      ax_top.set_major_locator(IndexLocator(base=1, offset=0))
+      ax_top.xaxis.set_major_locator(IndexLocator(base=1, offset=0))
       ax_top.set_xticklabels(self.table['expts'])
     axes.set_xticklabels(self.table[self.order_by[0]])
     flattened_y = self.table_flat[values_key]
