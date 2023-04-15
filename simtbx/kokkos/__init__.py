d76f6f88e381a35c85cc1f0d456507da9eb8a8d0
Felix Wittwer <40206209+Trzs@users.noreply.github.com>
2023-04-14 21:24:27 -0700

    Fix #868
    Importing simtbx.nanobragg in simtbx.kokkos to fix the 'undefined symbol' error

--- simtbx/kokkos/__init__.py
+++ simtbx/kokkos/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 import boost_adaptbx.boost.python as bp
 import cctbx.uctbx # possibly implicit
+from simtbx import nanoBragg
 ext = bp.import_ext("simtbx_kokkos_ext")
 from simtbx_kokkos_ext import *
 
