3eddc155e6247ae8132cdf84e4910f4e7f5ea2ad
Iris Young <irisdyoung@gmail.com>
2023-05-12 10:27:45 -0700

    Bugfix: don't attempt os.path.join on something that might return None

--- xfel/ui/components/xfel_gui_init.py
+++ xfel/ui/components/xfel_gui_init.py
@@ -41,9 +41,9 @@ import getpass
 user = getpass.getuser()
 
 import libtbx.load_env
-license = os.path.join(libtbx.env.find_in_repositories('cctbx_project'), 'LICENSE.txt')
-if license is None or not os.path.exists(license):
-  license = libtbx.env.under_root(os.path.join('cctbx', 'LICENSE.txt')) # conda installations
+license = libtbx.env.under_root(os.path.join("modules","cctbx_project","LICENSE.txt"))
+if not os.path.exists(license):
+  license = libtbx.env.under_root(os.path.join("cctbx", "LICENSE.txt")) # conda installations
 
 description = 'The cctbx.xfel UI is developed for use during data collection ' \
               'and initial processing of serial crystallographic data from' \
