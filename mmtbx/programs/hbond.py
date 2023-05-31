d631cba8a458d444e2a109771c97b4ac6723d69f
Oleg Sobolev <osobolev@lbl.gov>
2023-05-31 12:38:54 -0700

    hbond: enable several input models, output separate pymol/restraint/pdf, one skew-kurtosis plot.

--- mmtbx/programs/hbond.py
+++ mmtbx/programs/hbond.py
@@ -29,37 +29,45 @@ Usage example:
   def run(self):
     print('Using model: %s' % self.data_manager.get_default_model_name(),
       file=self.logger)
-    model = self.data_manager.get_model()
-    model.set_log(log = null_out())
-    model.process(make_restraints=True)
-    self.results = mmtbx.nci.hbond.find(model = model)
-    if self.params.hbond.show_hbonds:
-      self.results.show(log = self.logger)
-    print("-"*79, file=self.logger)
-    self.results.show_summary(log = self.logger)
-    prefix=self.params.output.prefix
-    if not prefix:
-      prefix='%s_hbond' % os.path.basename(
-          self.data_manager.get_default_model_name()).split('.')[0]
-    if self.params.hbond.output_pymol_file:
-      self.results.as_pymol(prefix=prefix)
-    if self.params.hbond.output_restraint_file:
-      self.results.as_restraints(file_name='%s.eff' % prefix)
-    #
-    if self.params.hbond.output_stats_pdf:
-      mmtbx.nci.hbond.stats(model = model, prefix="%s_stats" % prefix)
+    inp_models = []
+    for model_name in self.data_manager.get_model_names():
+      inp_models.append((model_name, self.data_manager.get_model(model_name)))
+
+    theta1_data = []
+    Rha_data = []
+    for m_name, model in inp_models:
+      # model = self.data_manager.get_model()
+      model.set_log(log = null_out())
+      model.process(make_restraints=True)
+      self.results = mmtbx.nci.hbond.find(model = model)
+      if self.params.hbond.show_hbonds:
+        self.results.show(log = self.logger)
+      print("-"*79, file=self.logger)
+      self.results.show_summary(log = self.logger)
+      prefix=self.params.output.prefix
+      if not prefix:
+        prefix='%s_hbond' % os.path.basename(m_name).split('.')[0]
+      if self.params.hbond.output_pymol_file:
+        self.results.as_pymol(prefix=prefix)
+      if self.params.hbond.output_restraint_file:
+        self.results.as_restraints(file_name='%s.eff' % prefix)
+      #
+      if self.params.hbond.output_stats_pdf:
+        mmtbx.nci.hbond.stats(model = model, prefix="%s_stats" % prefix)
+      theta1_data.append(self.results.get_counts().theta_1)
+      Rha_data.append(self.results.get_counts().d_HA)
     if self.params.hbond.output_skew_kurtosis_plot and self.results.get_counts():
-      theta1_data = self.results.get_counts().theta_1
-      Rha_data = self.results.get_counts().d_HA
       # To use other than 'all' type, nci.hbond.find needs to be called with selected model again,
       # like in stats().
       fn = '%s_skew_kurtosis' % prefix
       if self.params.hbond.plot_colorblind_friendly:
         fn += "_cbf"
+      theta1_c = [(x.skew, x.kurtosis) for x in theta1_data]
+      Rha_c = [(x.skew, x.kurtosis) for x in Rha_data]
       mmtbx.nci.skew_kurt_plot.make_figure(
           file_name=fn,
-          theta1_coords=[theta1_data.skew, theta1_data.kurtosis],
-          Rha_coords=[Rha_data.skew, Rha_data.kurtosis],
+          theta1_coords=theta1_c,
+          Rha_coords=Rha_c,
           type='all',
           colorblind_friendly=self.params.hbond.plot_colorblind_friendly)
 
