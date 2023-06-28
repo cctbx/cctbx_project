5e5271ef95b070ef2d4e5695afa6a9ee73f858b6
Daniel Tchon <dantch@onet.eu>
2023-06-27 17:16:23 -0700

    If `experiment_identifiers` empty, assume they're empty strings

--- xfel/merging/application/input/file_loader.py
+++ xfel/merging/application/input/file_loader.py
@@ -176,7 +176,7 @@ class simple_file_loader(worker):
 
           if refls_sel.count(True) == 0: continue
 
-          refls_identifier = eid_copy[experiment_id]
+          refls_identifier = eid_copy.get(experiment_id, '')
           if identifiers_void(experiment.identifier, refls_identifier) \
                   or self.params.input.override_identifiers:
             new_identifier = create_experiment_identifier(
