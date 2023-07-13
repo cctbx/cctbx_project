634fe1b729bc18182c9d94c85e73322a158f7772
Billy K. Poon <bkpoon@lbl.gov>
2023-07-13 16:08:33 -0700

    iotbx: move test to expected failure list

--- iotbx/run_tests.py
+++ iotbx/run_tests.py
@@ -146,7 +146,6 @@ tst_list_base = [
   "$D/regression/tst_hierarchy_occupancies_rounding.py",
   "$D/regression/tst_hierarchy_long_chain_ids_1.py",
   "$D/regression/tst_hierarchy_long_resname_1.py",
-  "$D/regression/tst_hierarchy_copy_select.py",
   ]
 
 # failing tests on Windows, Python 2.7
@@ -154,7 +153,9 @@ tst_list_windows_fail = [
   "$D/detectors/tst_debug_write.py",
 ]
 
-tst_list_fail = ["$D/regression/ncs/tst_ncs_reordered_chains.py",
+tst_list_fail = [
+  "$D/regression/ncs/tst_ncs_reordered_chains.py",
+  "$D/regression/tst_hierarchy_copy_select.py",
   ]
 if sys.platform == 'win32':
   tst_list_fail += tst_list_windows_fail
