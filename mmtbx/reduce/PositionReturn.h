e2b6880a9680fc12c0f8585f884f120d2f74958f
Russ Taylor <russ@reliasolve.com>
2023-08-08 16:02:23 -0400

    Making const references in the constructor to make life easier for conversions.

--- mmtbx/reduce/PositionReturn.h
+++ mmtbx/reduce/PositionReturn.h
@@ -29,12 +29,29 @@ namespace molprobity {
       PositionReturn() {};
 
       /// @brief Constructor with all arguments
+      ///
+      /// The arguments must all be const references so that the automagic wrapping
+      /// works correctly with them.
+      /// @param p_atoms A list of all of the atoms to be adjusted.
+      /// @param p_positions The positions element has the new location of each atom in each set
+      ///     of positions.
+      /// @param p_extraInfos The extraInfos element has the new ExtraAtomInfo for each atom in
+      ///     each set of positions that tells new information for a subset of the atoms.
+      ///     If there are no changes, this list is empty. It can be smaller than the
+      ///     p_atoms list, only effecting the first n atoms.
+      /// @param p_deleteMes The deleteMes element has a list of booleans for each atom in each
+      ///     set of positions that tells whether the atom should be deleted in this set.
+      ///     If there are no deletions, this list is empty. It can be smaller than the
+      ///     p_atoms list, only effecting the first n atoms.
+      /// @param p_preferenceEnergies The preferenceEnergies element has an energy for
+      ///     each set of positions.
+      /// @return A PositionReturn object with the given values.
       PositionReturn(
-        scitbx::af::shared<iotbx::pdb::hierarchy::atom>  p_atoms
-        , scitbx::af::shared< scitbx::af::shared<molprobity::probe::Point> >  p_positions
-        , scitbx::af::shared< scitbx::af::shared<molprobity::probe::ExtraAtomInfo> > p_extraInfos
-        , scitbx::af::shared< scitbx::af::shared<bool> > p_deleteMes
-        , scitbx::af::shared<double>  p_preferenceEnergies
+        scitbx::af::shared<iotbx::pdb::hierarchy::atom>  const &p_atoms
+        , scitbx::af::shared< scitbx::af::shared<molprobity::probe::Point> >  const &p_positions
+        , scitbx::af::shared< scitbx::af::shared<molprobity::probe::ExtraAtomInfo> > const &p_extraInfos
+        , scitbx::af::shared< scitbx::af::shared<bool> > const &p_deleteMes
+        , scitbx::af::shared<double>  const &p_preferenceEnergies
       ) : atoms(p_atoms)
         , positions(p_positions)
         , extraInfos(p_extraInfos)
