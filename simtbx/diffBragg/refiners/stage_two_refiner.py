5c76e91272f2da5c937661480faa6757de8ceffc
Daniel Tchon <dantch@onet.eu>
2023-08-19 02:17:56 -0700

    Again, don't iterate over Miller indexes, but rather use Counters

--- simtbx/diffBragg/refiners/stage_two_refiner.py
+++ simtbx/diffBragg/refiners/stage_two_refiner.py
@@ -253,12 +253,29 @@ class StageTwoRefiner(BaseRefiner):
 
         LOGGER.info("--Setting up per shot parameters - before loop")
         LOGGER.info(memory_report())
-        self.hkl_totals = []
+        # self.hkl_totals = []
+        # if self.refine_Fcell:
+        #     for i_shot in self.shot_ids:
+        #         for i_h, h in enumerate(self.Modelers[i_shot].Hi_asu):
+        #             self.hkl_totals.append(self.idx_from_asu[h])
+        #     # FIXME this calculates sums of indices' indexes and I am fairly
+        #     # FIXME sure it is supposed to count miller indices (again) later
+        #     # FIXME This is very important because it influences refinement
+        #     self.hkl_totals = self._MPI_reduce_broadcast(self.hkl_totals)
+
+        LOGGER.info("compute HKL multiplicity")
+        Hi_asu_counter = Counter()
         if self.refine_Fcell:
             for i_shot in self.shot_ids:
-                for i_h, h in enumerate(self.Modelers[i_shot].Hi_asu):
-                    self.hkl_totals.append(self.idx_from_asu[h])
-            self.hkl_totals = self._MPI_reduce_broadcast(self.hkl_totals)
+                Hi_asu_counter += Counter(self.Modelers[i_shot].Hi_asu)
+        hkl_frequency = Counter({self.idx_from_asu[h]: c
+                                 for h, c in Hi_asu_counter.items()})
+        hkl_frequencies = COMM.gather(hkl_frequency, root=0)
+        hkl_total_frequency = Counter()
+        if COMM.rank == 0:
+            for hkl_rank_frequency in hkl_frequencies:
+                hkl_total_frequency += hkl_rank_frequency
+        self.hkl_frequency = COMM.bcast(hkl_total_frequency, root=0)
 
         LOGGER.info("--Setting up per shot parameters - before setups")
         LOGGER.info(memory_report())
@@ -392,11 +409,13 @@ class StageTwoRefiner(BaseRefiner):
 
     def _setup_fcell_params(self):
         if self.refine_Fcell:
+            # TODO Isn't this a bunch of unrelevant logging points?
             LOGGER.info("----loading fcell data")
             # this is the number of observations of hkl (accessed like a dictionary via global_fcell_index)
             LOGGER.info("---- -- counting hkl totes")
-            LOGGER.info("compute HKL multiplicity")
-            self.hkl_frequency = Counter(self.hkl_totals)
+            # TODO: The next two lines are now done elsewhere
+            # LOGGER.info("compute HKL multiplicity")
+            # self.hkl_frequency = Counter(self.hkl_totals)
             LOGGER.info("save HKL multiplicity")
             np.save(os.path.join(self.output_dir, "f_asu_multi"), self.hkl_frequency)
             LOGGER.info("Done ")
