94d28f0b348850225089b65afd8937a203c51cdc
Russ Taylor <russ@reliasolve.com>
2023-08-24 16:48:41 -0400

    Adding imports needed to satisfy find_clutter.

--- mmtbx/probe/tst_mmtbx_probe_ext.py
+++ mmtbx/probe/tst_mmtbx_probe_ext.py
@@ -15,6 +15,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function, nested_scopes, generators, division
+from __future__ import absolute_import
+
 # These other imports are needed within the Scons build environment.
 import boost_adaptbx.boost.python as bp
 bp.import_ext("mmtbx_probe_ext")
