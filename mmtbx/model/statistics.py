1af4f032a5f967f7af377c81fb9f8659f4c3bc05
Pavel <pafonine@lbl.gov>
2023-11-17 13:24:58 +1100

    Add max dev from planes (proteins only)

--- mmtbx/model/statistics.py
+++ mmtbx/model/statistics.py
@@ -123,7 +123,15 @@ class geometry(object):
     if(self.from_restraints is not None):
       mi,ma,me = self.from_restraints.planarity_deviations()
       n = self.from_restraints.get_filtered_n_planarity_proxies()
-    return group_args(min = mi, max = ma, mean = me, n = n)
+    pp = protein_planes(hierarchy = self.pdb_hierarchy)
+    asc = self.pdb_hierarchy.atom_selection_cache()
+    sel = asc.selection("element H or element D")
+    pp_noH = None
+    if sel.count(True) > 0:
+      pp_noH = protein_planes(hierarchy = self.pdb_hierarchy.select(~sel))
+    return group_args(min = mi, max = ma, mean = me, n = n,
+      protein_planes_max_dev = pp,
+      protein_planes_max_dev_noH = pp_noH)
 
   def parallelity(self):
     mi,ma,me,n = 0,0,0,0
@@ -370,6 +378,17 @@ class geometry(object):
       result += """
 %s"""%prefix
       result += res.rama_z.as_string(prefix=prefix)
+    #
+    result += "\n\n%sMax deviation from planes:"%prefix
+    result += "\n%s   Type MaxDev LineInFile"%prefix
+    for pp in p.protein_planes_max_dev:
+      result += "\n  %s %3s %6.3f %s"%(prefix, pp.resname[:3], pp.max_dev, pp.id)
+    if p.protein_planes_max_dev_noH is not None:
+      result += "\n\n%sMax deviation from planes (no H):"%prefix
+      result += "\n%s   Type MaxDev LineInFile"%prefix
+      for pp in p.protein_planes_max_dev_noH:
+        result += "\n  %s %3s %6.3f %s"%(prefix, pp.resname[:3], pp.max_dev, pp.id)
+    #
     if( uppercase ):
       result = result.upper()
     print(result, file=log)
@@ -647,3 +666,53 @@ class info(object):
     cif_block = self.model.tls_groups_as_cif_block(cif_block=cif_block)
     # What about anomalous_scatterer_groups here?
     return cif_block
+
+def protein_planes(hierarchy):
+  from cctbx import geometry_restraints
+  # XXX This needs to be generalized !!! XXX
+  planes = {
+  "ARG":     [' NE ', ' CZ ', ' NH1', ' NH2', ' HE ', 'HH11', 'HH12', 'HH21', 'HH22'],
+  "TYR":     ["CB","CG", "CD1","CD2","CE1","CE2","CZ", "OH", "HD1","HD2","HE1","HE2"],
+  "PHE":     ["CB","CG", "CD1","CD2","CE1","CE2","CZ", "OH", "HD1","HD2","HE1","HE2"],
+  "peptide": ["C","O","N","H"],
+  "TRP":     ["CB","CG","CD1","CD2","NE1","CE2","CE3","CZ2","CZ3","CH2","HD1","HE1","HE3","HZ2","HZ3","HH2"],
+  "HIS":     [ "CG","ND1","CE1","NE2","CD2","HCD2","HNE2","HND1"]
+  }
+  asc = hierarchy.atom_selection_cache()
+  result = []
+  for resname in planes.keys():
+    MAX = -1
+    ID = None
+    RESNAME = None
+    names = planes[resname]
+    ss = " or ".join(["name %s"%it for it in names])
+    if resname != "peptide":
+      ss = "resname %s and (%s)"%(resname,ss)
+    sel = asc.selection(string=ss)
+    h = hierarchy.select(sel)
+    for residue_group in h.residue_groups():
+      if len(residue_group.atom_groups())>1: continue
+      atom_group = residue_group.atom_groups()[0]
+      if resname != "peptide":
+        assert atom_group.resname==resname
+      atoms = atom_group.atoms()
+      size = atoms.size()
+      if resname == "peptide" and size<3: continue
+      sites_cart = atoms.extract_xyz()
+      pproxy = geometry_restraints.planarity_proxy(
+        i_seqs  = list(range(size)),
+        weights = flex.double([1]*size))
+      planarity = geometry_restraints.planarity(
+        sites_cart = sites_cart,
+        proxy      = pproxy)
+      max_delta = flex.max(flex.abs(planarity.deltas()))
+      if max_delta > MAX:
+        MAX = max_delta
+        ID = atom_group.id_str()
+        RESNAME = resname
+    r = group_args(
+      resname = RESNAME,
+      max_dev = MAX,
+      id      = ID)
+    result.append(r)
+  return result
