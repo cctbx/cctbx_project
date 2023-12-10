81b43c665993a939bc468839ec6774d4e18ead1c
Billy K. Poon <bkpoon@lbl.gov>
2023-12-10 13:51:33 -0800

    boost: switch deprecated boost/timer.hpp to boost/timer/timer.hpp
    - Change header from boost/timer.hpp to boost/timer/timer.hpp
    - Change boost::timer to boost::timer::auto_cpu_timer
    - Handle switch to nanoseconds
    - Build Boost timer library if needed

--- cctbx/math/time_trigonometry.cpp
+++ cctbx/math/time_trigonometry.cpp
@@ -1,23 +1,23 @@
 #include <cctbx/math/cos_sin_table.h>
 #include <iostream>
-#include <boost/timer.hpp>
+#include <boost/timer/timer.hpp>
 
 int main() {
   unsigned const n = 1024, p = 1024;
   cctbx::math::cos_sin_table<double> table(n);
   cctbx::math::cos_sin_exact<double> exact;
   std::complex<double> sum = 0;
-  boost::timer t;
+  boost::timer::auto_cpu_timer t;
   for (unsigned i=0; i<p*n; ++i) {
     sum += table(i);
   }
-  std::cout << t.elapsed() << "\t" << sum << std::endl;
+  std::cout << boost::timer::format(t.elapsed()) << "\t" << sum << std::endl;
   sum = 0;
-  t.restart();
+  t.start();
   for (unsigned i=0; i<p*n; ++i) {
     sum += exact(i);
   }
-  std::cout << t.elapsed() << "\t" << sum << std::endl;
+  std::cout << boost::timer::format(t.elapsed()) << "\t" << sum << std::endl;
   std::cout << "OK\n" << std::endl;
   return 0;
 }
