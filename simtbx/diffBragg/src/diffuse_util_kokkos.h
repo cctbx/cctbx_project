0c116451699e5afb715cbdf277b8068c67818b48
Mike Wall <mewall@lanl.gov>
2023-12-13 23:23:48 -0700

    Correct scale factor for diffuse

--- simtbx/diffBragg/src/diffuse_util_kokkos.h
+++ simtbx/diffBragg/src/diffuse_util_kokkos.h
@@ -573,7 +573,7 @@ void calc_diffuse_at_hkl(KOKKOS_VEC3 H_vec, KOKKOS_VEC3 H0, KOKKOS_VEC3 dHH, int
                      else
                         _this_diffuse_scale = 1.0;
 
-                     _this_diffuse_scale *= _this_diffuse_scale/(CUDAREAL)num_laue_mats/(CUDAREAL)num_stencil_points;
+                     _this_diffuse_scale *= _this_diffuse_scale/(CUDAREAL)num_laue_mats;
 
                      const KOKKOS_VEC3 H0_offset(H0[0]+hh, H0[1]+kk, H0[2]+ll);
                      const KOKKOS_VEC3 delta_H_offset = H_vec - H0_offset;
