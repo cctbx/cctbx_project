ff02d7e94e2585f7729e2ab9c6e760fb260d1a5c
cschlick <cjschlicksup@lbl.gov>
2023-12-17 17:22:56 -0800

    Rename program to start_viewer

--- qttbx/command_line/start_viewer_demo.py
+++
@@ -1,9 +0,0 @@
-# LIBTBX_SET_DISPATCHER_NAME phenix.start_viewer_demo
-# LIBTBX_SET_DISPATCHER_NAME qttbx.start_viewer_demo
-from __future__ import absolute_import, division, print_function
-
-from iotbx.cli_parser import run_program
-from qttbx.programs import start_viewer_demo
-
-if __name__ == '__main__':
-  run_program(program_class=start_viewer_demo.Program)
