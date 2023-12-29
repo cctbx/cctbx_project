a62891eaf9c556e067f3d5cd57532da28b79f6b5
terwill <tterwilliger@newmexicoconsortium.org>
2023-12-29 10:23:46 -0700

    Put back path

--- mmtbx/regression/tst_model_vs_sequence.py
+++ mmtbx/regression/tst_model_vs_sequence.py
@@ -12,7 +12,7 @@ def run():
     relative_path="mmtbx/regression/pdbs/p9.pdb", test=os.path.isfile)
   seq_filename = "tst_model_vs_sequence_seq.fa"
   assert not easy_run.call("iotbx.pdb_as_fasta %s output_file=%s" % (pdb_fname, seq_filename))
-  cmd = "phenix.model_vs_sequence %s %s" % (pdb_fname, seq_filename)
+  cmd = "mmtbx.model_vs_sequence %s %s" % (pdb_fname, seq_filename)
   print(cmd)
   assert not easy_run.call(cmd)
 
