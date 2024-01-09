281d0bae2847f4fedeee25a55832bce383e89afb
Oleg Sobolev <osobolev@lbl.gov>
2024-01-09 09:57:53 -0800

    Take expected failures from outside (phenix_regression).

--- libtbx/command_line/run_tests_parallel.py
+++ libtbx/command_line/run_tests_parallel.py
@@ -44,7 +44,8 @@ def run(args,
    python_keyword_text="",
    max_tests=None,
    start_test=None,
-   tests_to_skip=None):
+   tests_to_skip=None,
+   expected_failures_from_phenix_regression=[]):
 
   if (len(args) == 0):
     raise Usage("""libtbx.run_tests_parallel [module=NAME] [directory=path]""")
@@ -110,6 +111,12 @@ def run(args,
     expected_failure_list.extend(fail_tests)
     expected_unstable_list.extend(unstable_tests)
     parallel_list.extend(parallel_tests)
+
+  # add expected failures from phenix regression
+  for ef in expected_failures_from_phenix_regression:
+    for t in all_tests:
+      if t.find(ef) > -1:
+        expected_failure_list.append(t)
 
   # remove any specified tests:
   if tests_to_skip:
