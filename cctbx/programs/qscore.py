66c399a734d3792a185297a267503c6434aab3e4
cschlick <cjschlicksup@lbl.gov>
2024-01-24 11:04:48 -0800

    Updates to qscore code

--- cctbx/programs/qscore.py
+++ cctbx/programs/qscore.py
@@ -1,10 +1,9 @@
 from __future__ import absolute_import, division, print_function
-from phenix.program_template import ProgramTemplate
+from libtbx.program_template import ProgramTemplate
 from libtbx import group_args
-from cctbx.maptbx.qscore import qscore_np
+from cctbx.maptbx import qscore
 
 import numpy as np
-
 
 # =============================================================================
 
@@ -17,77 +16,30 @@ class Program(ProgramTemplate):
     datatypes = ['phil', 'model', 'real_map']
 
     master_phil_str = """
-  nproc = 8
-      .type = int
-      .help = Number of processors to use
-      .short_caption = Number of processors to use
-      .expert_level = 1
-  n_probes = 32
-      .type = int
-      .help = Number of radial probes to use
-      .short_caption = Number of radial probes to use
-      .expert_level = 1
-  selection = None
-    .type = str
-    .help = Only test atoms within this selection
-    .short_caption = Only test atoms within this selection
-    .expert_level = 1
-
-  shell_radius_start = 0.1
-    .type = float
-    .help = Start testing density at this radius from atom
-    .short_caption = Start testing density at this radius from atom
-    .expert_level = 1
-
-  shell_radius_stop = 2
-    .type = float
-    .help = Stop testing density at this radius from atom
-    .short_caption = Stop testing density at this radius from atom
-    .expert_level = 1
-
-  shell_radius_num = 20
-    .type = int
-    .help = The number of radial shells
-    .short_caption = The number of radial shells (includes start/stop, so minimum 2)
-    .expert_level = 1
-
-  probe_allocation_method = precalculate
-    .type = str
-    .help = The method used to allocate radial probes
-    .short_caption = Either 'progressive' or 'precalculate'. Progressive is the original method \
-                     where probes are proposed and rejected iteratively. \
-                     Precalculate is a method where probes are pre-allocated and \
-                     rejected once. Parallelization is done by radial shell. \
-                     Precalculate is much faster but will yield slightly different results.
-
-
-  """
+    include scope cctbx.maptbx.qscore.master_phil_str
+    """
 
     def validate(self):
         pass
 
     def run(self):
         print("Running")
-        shells = np.linspace(self.params.shell_radius_start,
-                             self.params.shell_radius_stop,
-                             num=self.params.shell_radius_num,
-                             endpoint=True)
         mmm = self.data_manager.get_map_model_manager()
-        version = 2 if self.params.probe_allocation_method == "precalculate" else 1  # 'progressive'
-
-        # TODO: move param unpacking to the library function
-        result = qscore_np(
+        if self.params.qscore.selection != None:
+            selection = np.where(mmm.model().selection(self.params.qscore.selection).as_numpy_array())[0]
+            if len(selection)==0:
+                print("Finished... nothing selected")
+                self.result = group_args()
+                return
+            self.params.qscore.selection = selection
+        qscore_per_atom= qscore.run_qscore(
             mmm,
-            selection=self.params.selection,
-            n_probes=self.params.n_probes,
-            shells=shells,
-            nproc=self.params.nproc,
-            version=version,
+            self.params.qscore,
             log=self.logger)
-        print("Result:")
-        for val in result:
-            print(str(val)+",")
-        self.result = group_args(qscore=result)
+
+        print("Finished")
+        self.result = group_args(qscore_per_atom=qscore_per_atom)
+
 
     def get_results(self):
         return self.result
