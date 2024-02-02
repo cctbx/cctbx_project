8cc32f5730273b52d6b2482d4a8cc5c7ff5cee5b
Daniel Tchon <dantch@onet.eu>
2024-02-02 15:09:15 -0800

    Let `params_from_phil` handle any arbitrary scope provided 2

--- xfel/command_line/preferential_orientation.py
+++ xfel/command_line/preferential_orientation.py
@@ -2,12 +2,12 @@
 
 from __future__ import division
 import sys
-from xfel.util.preferential_orientation import params_from_phil, run, message
+from xfel.util.preferential_orientation import params_from_phil, phil_scope, run, message
 
 
 if __name__ == '__main__':
   if '--help' in sys.argv[1:] or '-h' in sys.argv[1:]:
     print(message)
     exit()
-  params = params_from_phil(sys.argv[1:])
+  params = params_from_phil(phil_scope, sys.argv[1:])
   run(params)
