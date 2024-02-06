3575d66725d30ad5b494d9b454cdd4ccff1d5b95
cschlick <cjschlicksup@lbl.gov>
2024-02-06 09:06:59 -0800

    Major changes to qscore. 1. Implemented kdtree in pure python with flex arrays to speed up nn probe queries, 2. Aggregation by residue which matches the method in mapq, 3. Removed all tests that required running mapq on command line, moved to a personal test file, 4. Check actual qscore values in tests rather than summed values, 5. Misc cleanup, indentation.

--- cctbx/programs/qscore.py
+++ cctbx/programs/qscore.py
@@ -1,8 +1,23 @@
 from __future__ import absolute_import, division, print_function
+<<<<<<< Updated upstream
 from phenix.program_template import ProgramTemplate
 from libtbx import group_args
 from cctbx.maptbx.qscore import qscore_np
 
+=======
+
+import json
+from pathlib import Path
+
+from libtbx.program_template import ProgramTemplate
+from libtbx import group_args
+from cctbx.maptbx.qscore import (
+    calc_qscore,
+    calc_qscore_flex,
+    cctbx_atoms_to_df,
+    write_bild_spheres
+)
+>>>>>>> Stashed changes
 import numpy as np
 
 
@@ -10,12 +25,13 @@ import numpy as np
 
 class Program(ProgramTemplate):
 
-    description = """
+  description = """
   Perform a Qscore analysis for map-model fit
   """
 
-    datatypes = ['phil', 'model', 'real_map']
+  datatypes = ['phil', 'model', 'real_map']
 
+<<<<<<< Updated upstream
     master_phil_str = """
   nproc = 8
       .type = int
@@ -61,11 +77,18 @@ class Program(ProgramTemplate):
                      Precalculate is much faster but will yield slightly different results.
 
 
+=======
+  master_phil_str = """
+  include scope cctbx.maptbx.qscore.master_phil_str
+>>>>>>> Stashed changes
   """
 
-    def validate(self):
-        pass
+  def validate(self):
+    assert self.params.qscore.backend in [
+      "numpy","flex"
+      ], "Provide one of 'numpy', 'flex'"
 
+<<<<<<< Updated upstream
     def run(self):
         print("Running")
         shells = np.linspace(self.params.shell_radius_start,
@@ -91,3 +114,84 @@ class Program(ProgramTemplate):
 
     def get_results(self):
         return self.result
+=======
+    assert self.params.qscore.probe_allocation_method in [
+      "progressive", "precalculate"
+    ], "Provide one of 'progressive' or 'precalculate'"
+
+  def run(self):
+    print("Running")
+
+    # get initial data
+    mmm = self.data_manager.get_map_model_manager()
+
+    # calculate shells
+    if len(self.params.qscore.shells) ==0 :
+        start = self.params.qscore.shell_radius_start
+        stop = self.params.qscore.shell_radius_stop
+        num = self.params.qscore.shell_radius_num
+        shells = list(np.linspace(
+        start,
+        stop,
+        num,
+        endpoint=True))
+        for shell in shells:
+          self.params.qscore.shells.append(shell)
+
+
+    # ignore hydrogens
+    model = mmm.model()
+    model = model.select(model.selection("not element H"))
+
+    # make mmm
+    mmm.set_model(model,overwrite=True)
+
+
+    # run qscore
+    backend = self.params.qscore.backend
+    calc_func = calc_qscore if backend == "numpy" else calc_qscore_flex
+    qscore_result= calc_func(
+        mmm,
+        self.params.qscore,
+        log=self.logger)
+
+
+    self.result = group_args(**qscore_result)
+
+    # # save as dataframe
+    # df = cctbx_atoms_to_df(model.get_atoms())
+    # df["Q-score"] = self.result.qscore_per_atom
+    # self.result.qscore_dataframe =
+
+
+
+    self.write_results()
+
+  def get_results(self):
+    return self.result
+
+  def get_results_as_JSON(self):
+    results_dict = {
+      "flat_results" : self.result.qscore_dataframe.to_dict(orient="records")
+    }
+    return json.dumps(results_dict,indent=2)
+
+
+  def write_results(self):
+    with open("qscore_results.json","w") as fh:
+      fh.write(self.get_results_as_JSON())
+
+    # write bild files
+    if self.params.qscore.debug:
+      print("Writing probe debug files...Using a small selection is recommended",
+            file=self.logger)
+      debug_path = Path("qscore_debug")
+      debug_path.mkdir(exist_ok=True)
+      for i,shell in enumerate(self.params.qscore.shells):
+        shell = str(round(shell,2))
+        probe_xyz = self.result.probe_xyz[i]
+        n_shells, n_atoms,n_probes,_ = self.result.probe_xyz.shape
+        probe_xyz_flat = probe_xyz.reshape((n_atoms*n_probes,3))
+        out_file = Path(debug_path,f"probes_shell_{shell}.bild")
+        write_bild_spheres(probe_xyz_flat,str(out_file),r=0.2)
+>>>>>>> Stashed changes
