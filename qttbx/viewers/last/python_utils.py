94606054225f897bb8aff4a7a7807cb512b438ab
cschlick <cjschlicksup@lbl.gov>
2024-02-16 10:04:42 -0800

    Place flatten dict function in python utils

--- qttbx/viewers/last/python_utils.py
+++ qttbx/viewers/last/python_utils.py
@@ -18,3 +18,15 @@ class DotDict(dict):
 
   def __repr__(self):
     return f"<DotDict {super().__repr__()}>"
+
+
+def flatten_dict(d, parent_key='', sep='.'):
+  items = []
+  for k, v in d.items():
+    new_key = f"{parent_key}{sep}{k}" if parent_key else k
+    if isinstance(v, dict):
+      items.extend(flatten_dict(v, new_key, sep=sep).items())
+    else:
+      items.append((new_key, v))
+  return dict(items)
+
