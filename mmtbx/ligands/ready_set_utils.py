be67fedf6c50c093eb644b0469aeabe4d06364c1
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2024-03-14 11:58:00 -0700

    small change

--- mmtbx/ligands/ready_set_utils.py
+++ mmtbx/ligands/ready_set_utils.py
@@ -63,8 +63,8 @@ def add_n_terminal_hydrogens_to_atom_group(ag,
                                            n_ca_c=None,
                                            verbose=False,
                                           ):
-  if bonds is None and verbose:
-    print('\n\t%s\n' % 'add_n_terminal_hydrogens_to_atom_group has not being given bonds')
+  if verbose and bonds is None:
+    print('\n\t%s\n' % 'add_n_terminal_hydrogens_to_atom_group has not been given bonds')
   rc=[]
   if n_ca_c is not None:
     n, ca, c = n_ca_c
