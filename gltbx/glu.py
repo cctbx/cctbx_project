dfe16a9fda0d3205a4b74ec4223769bcea4885bc
Billy K. Poon <bkpoon@lbl.gov>
2024-03-26 13:09:20 -0700

    gltbx: switch OpenGL imports to pyopengl

--- gltbx/glu.py
+++ gltbx/glu.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import, division, print_function
-import boost_adaptbx.boost.python as bp
-ext = bp.import_ext("gltbx_glu_ext")
-from gltbx_glu_ext import *
+# import boost_adaptbx.boost.python as bp
+# ext = bp.import_ext("gltbx_glu_ext")
+# from gltbx_glu_ext import *
+import OpenGL  # implicit import
+from OpenGL.GLU import *
