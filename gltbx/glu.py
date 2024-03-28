b9d25de331cd3d33bc8a814c37ba1c202ec3ef98
Billy K. Poon <bkpoon@lbl.gov>
2024-03-28 10:19:37 -0700

    gltbx: preserve previous behavior if pyopengl is not available

--- gltbx/glu.py
+++ gltbx/glu.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, division, print_function
-# import boost_adaptbx.boost.python as bp
-# ext = bp.import_ext("gltbx_glu_ext")
-# from gltbx_glu_ext import *
-import OpenGL  # implicit import
-from OpenGL.GLU import *
+try:
+  import OpenGL  # implicit import
+  from OpenGL.GLU import *
+except ImportError:
+  import boost_adaptbx.boost.python as bp
+  ext = bp.import_ext("gltbx_glu_ext")
+  from gltbx_glu_ext import *
