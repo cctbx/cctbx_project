4381b35adee251493c5f7d1611b7af0fd6d42f25
dcliebschner <dcliebschner@lbl.gov>
2024-04-16 10:46:19 -0700

    rename dispatcher

--- mmtbx/command_line/validate_ligands.py
+++ mmtbx/command_line/validate_ligands.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, division, print_function
-
+# LIBTBX_SET_DISPATCHER_NAME mmtbx.development.validate_ligands
 from iotbx.cli_parser import run_program
 from mmtbx.programs import validate_ligands
 
