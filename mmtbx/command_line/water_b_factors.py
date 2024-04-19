107afe207fb720eda50db4e3969488af5c009aa6
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2024-04-19 15:29:14 -0700

    move to development

--- mmtbx/command_line/water_b_factors.py
+++ mmtbx/command_line/water_b_factors.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division, print_function
+# LIBTBX_SET_DISPATCHER_NAME phenix.development.water_b_factors
 
 from iotbx.cli_parser import run_program
 from mmtbx.programs.water_b_factors import Program
