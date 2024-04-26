669ff59d9b9791c5f337caeef7b996e9dc36494a
Oleg Sobolev <osobolev@lbl.gov>
2024-04-26 12:24:56 -0700

    Test for sheet with long ids.

--- iotbx/regression/secondary_structure/tst_annotation_long.py
+++ iotbx/regression/secondary_structure/tst_annotation_long.py
@@ -33,6 +33,64 @@ _struct_conf_type.criteria    ?
 _struct_conf_type.reference   ?
 """
 
+cif_one_sheet = """\
+#
+_struct_sheet.id               A
+_struct_sheet.type             ?
+_struct_sheet.number_strands   2
+_struct_sheet.details          ?
+#
+_struct_sheet_order.sheet_id     A
+_struct_sheet_order.range_id_1   1
+_struct_sheet_order.range_id_2   2
+_struct_sheet_order.offset       ?
+_struct_sheet_order.sense        anti-parallel
+#
+loop_
+_struct_sheet_range.sheet_id
+_struct_sheet_range.id
+_struct_sheet_range.beg_label_comp_id
+_struct_sheet_range.beg_label_asym_id
+_struct_sheet_range.beg_label_seq_id
+_struct_sheet_range.pdbx_beg_PDB_ins_code
+_struct_sheet_range.end_label_comp_id
+_struct_sheet_range.end_label_asym_id
+_struct_sheet_range.end_label_seq_id
+_struct_sheet_range.pdbx_end_PDB_ins_code
+_struct_sheet_range.symmetry
+_struct_sheet_range.beg_auth_comp_id
+_struct_sheet_range.beg_auth_asym_id
+_struct_sheet_range.beg_auth_seq_id
+_struct_sheet_range.end_auth_comp_id
+_struct_sheet_range.end_auth_asym_id
+_struct_sheet_range.end_auth_seq_id
+A 1 longSER longA 4  ? longALA longA 7  ? ? alongSER alongA 4  alongALA alongA 7
+A 2 longMET longA 22 ? longGLU longA 25 ? ? alongMET alongA 22 alongGLU alongA 25
+#
+_pdbx_struct_sheet_hbond.sheet_id                A
+_pdbx_struct_sheet_hbond.range_id_1              1
+_pdbx_struct_sheet_hbond.range_id_2              2
+_pdbx_struct_sheet_hbond.range_1_label_atom_id   N
+_pdbx_struct_sheet_hbond.range_1_label_comp_id   longSER
+_pdbx_struct_sheet_hbond.range_1_label_asym_id   longA
+_pdbx_struct_sheet_hbond.range_1_label_seq_id    4
+_pdbx_struct_sheet_hbond.range_1_PDB_ins_code    ?
+_pdbx_struct_sheet_hbond.range_1_auth_atom_id    N
+_pdbx_struct_sheet_hbond.range_1_auth_comp_id    alongSER
+_pdbx_struct_sheet_hbond.range_1_auth_asym_id    alongA
+_pdbx_struct_sheet_hbond.range_1_auth_seq_id     4
+_pdbx_struct_sheet_hbond.range_2_label_atom_id   O
+_pdbx_struct_sheet_hbond.range_2_label_comp_id   longGLU
+_pdbx_struct_sheet_hbond.range_2_label_asym_id   longA
+_pdbx_struct_sheet_hbond.range_2_label_seq_id    25
+_pdbx_struct_sheet_hbond.range_2_PDB_ins_code    ?
+_pdbx_struct_sheet_hbond.range_2_auth_atom_id    O
+_pdbx_struct_sheet_hbond.range_2_auth_comp_id    alongGLU
+_pdbx_struct_sheet_hbond.range_2_auth_asym_id    alongA
+_pdbx_struct_sheet_hbond.range_2_auth_seq_id     25
+#
+"""
+
 def tst_helix_interface():
   cif_str = "data_1UCS" + cif_one_helix
   cif_model = iotbx.cif.reader(input_string=cif_str).model()
@@ -94,7 +152,97 @@ loop_
 
 """)
 
+def tst_sheet_interface():
+  cif_str = "data_1UCS" + cif_one_sheet
+  cif_model = iotbx.cif.reader(input_string=cif_str).model()
+  cif_block = list(cif_model.values())[0]
+  ann = annotation.from_cif_block(cif_block)
+
+  sh = ann.sheets[0]
+  st = sh.strands[1]
+  reg = sh.registrations[1]
+
+  #
+  # !!! Note, here we got info from *auth* fields.
+  #
+
+  assert reg.cur_atom == ' O  '
+  assert reg.cur_resname == 'alongGLU', reg.cur_resname
+  assert reg.cur_chain_id == 'alongA'
+  assert reg.cur_resseq == '  25'
+  assert reg.cur_icode == ' '
+  assert reg.prev_atom == ' N  '
+  assert reg.prev_resname == 'alongSER'
+  assert reg.prev_chain_id == 'alongA'
+  assert reg.prev_resseq == '   4'
+  assert reg.prev_icode  == ' '
+
+  assert st.sheet_id == 'A', st.sheet_id
+  assert st.strand_id == 2, st.strand_id
+  assert st.start_resname == 'alongMET',st.start_resname
+  assert st.start_chain_id == 'alongA'
+  assert st.start_resseq == '  22', st.start_resseq
+  assert st.start_icode == ' '
+  assert st.end_resname == 'alongGLU'
+  assert st.end_chain_id == 'alongA'
+  assert st.end_resseq == '  25'
+  assert st.end_icode == ' '
+  assert st.sense == -1
+
+  assert not sh.fits_in_pdb_format()
+  assert not ann.fits_in_pdb_format()
+  # print (sh.as_pdb_or_mmcif_str())
+  assert not show_diff(sh.as_mmcif_str(),
+"""data_phenix
+loop_
+  _struct_sheet.id
+  _struct_sheet.type
+  _struct_sheet.number_strands
+  _struct_sheet.details
+  A  ?  2  ?
+
+loop_
+  _struct_sheet_order.sheet_id
+  _struct_sheet_order.range_id_1
+  _struct_sheet_order.range_id_2
+  _struct_sheet_order.offset
+  _struct_sheet_order.sense
+  A  1  2  ?  anti-parallel
+
+loop_
+  _struct_sheet_range.sheet_id
+  _struct_sheet_range.id
+  _struct_sheet_range.beg_label_comp_id
+  _struct_sheet_range.beg_label_asym_id
+  _struct_sheet_range.beg_label_seq_id
+  _struct_sheet_range.pdbx_beg_PDB_ins_code
+  _struct_sheet_range.end_label_comp_id
+  _struct_sheet_range.end_label_asym_id
+  _struct_sheet_range.end_label_seq_id
+  _struct_sheet_range.pdbx_end_PDB_ins_code
+  A  1  alongSER  alongA   4  ?  alongALA  alongA   7  ?
+  A  2  alongMET  alongA  22  ?  alongGLU  alongA  25  ?
+
+loop_
+  _pdbx_struct_sheet_hbond.sheet_id
+  _pdbx_struct_sheet_hbond.range_id_1
+  _pdbx_struct_sheet_hbond.range_id_2
+  _pdbx_struct_sheet_hbond.range_1_label_atom_id
+  _pdbx_struct_sheet_hbond.range_1_label_comp_id
+  _pdbx_struct_sheet_hbond.range_1_label_asym_id
+  _pdbx_struct_sheet_hbond.range_1_label_seq_id
+  _pdbx_struct_sheet_hbond.range_1_PDB_ins_code
+  _pdbx_struct_sheet_hbond.range_2_label_atom_id
+  _pdbx_struct_sheet_hbond.range_2_label_comp_id
+  _pdbx_struct_sheet_hbond.range_2_label_asym_id
+  _pdbx_struct_sheet_hbond.range_2_label_seq_id
+  _pdbx_struct_sheet_hbond.range_2_PDB_ins_code
+  A  1  2  N  alongSER  alongA  4  ?  O  alongGLU  alongA  25  ?
+
+""")
+
 if (__name__ == "__main__"):
   t0 = time.time()
   tst_helix_interface()
+  tst_sheet_interface()
   print("OK time =%8.3f"%(time.time() - t0))
