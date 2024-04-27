a396fc6e76d40e8964ad738a92c9c8cba76c702b
Pavel <pafonine@lbl.gov>
2024-04-26 18:21:31 -0700

    make plots more transparent

--- mmtbx/nci/skew_kurt_plot.py
+++ mmtbx/nci/skew_kurt_plot.py
@@ -171,6 +171,7 @@ def make_figure(
       aspect=aspect,
       # interpolation='bicubic',
       # norm=norm,
+      alpha=0.5
       )
   plt.contour( blue_filling, contours[type],
       origin="lower",
