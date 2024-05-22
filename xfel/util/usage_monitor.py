0e2cc54880372add39d4edd7f94175c1da0726ae
Daniel Tchon <dantch@onet.eu>
2024-05-22 16:21:55 -0700

    Add logging details, period, null loggers

--- xfel/util/usage_monitor.py
+++ xfel/util/usage_monitor.py
@@ -1,5 +1,6 @@
 from contextlib import ContextDecorator
 from enum import Enum
+from functools import cached_property
 import logging
 import platform
 import psutil
@@ -10,29 +11,28 @@ from typing import List
 
 from libtbx.mpi4py import MPI
 comm = MPI.COMM_WORLD
-log_path = f'usage_{comm.rank}.log'
-logging.basicConfig(filename=log_path, encoding='utf-8', level=logging.ERROR)
-log = logging.getLogger("cctbx.usage")
-log.setLevel(logging.DEBUG)
+node = platform.node()
+node_ranks = [r for n, r in comm.allgather((node, comm.rank)) if n == node]
 
 
 class UsageMonitor(ContextDecorator):
   """This class can be used both as context manager (with) and @decorator"""
 
   class Detail(Enum):
-    off = 'off'
-    single = 'single'
-    node = 'node'
     rank = 'rank'
+    node = 'node'
+    single = 'single'
+    none = 'none'
 
-  def __init__(self, detail: str = 'node', every: float = 1.0) -> None:
+  def __init__(self, detail: str = 'node', period: float = 1.0) -> None:
     self.detail = self.Detail(detail)
-    self.every = every
-    self._gpu_count = None
+    self.period: float = period
+    self.log: logging.Logger = self.configure_logger()
+    self._daemon = None
 
   def __enter__(self) -> None:
     if comm.rank == 0:
-      self.start_logging_deamon()
+      self.start_logging_daemon()
 
   def __exit__(self, exc_type, exc_val, exc_tb):
     pass
@@ -47,6 +47,10 @@ class UsageMonitor(ContextDecorator):
     return psutil.cpu_percent(interval=None)
 
   @property
+  def daemon(self) -> threading.Thread:
+    return self._daemon
+
+  @property
   def gpus_memory(self) -> List[float]:
     smi = subprocess.run(['nvidia-smi', '--query-gpu=utilization.memory',
                           '--format=csv,noheader,nounits'],
@@ -59,32 +63,54 @@ class UsageMonitor(ContextDecorator):
                           '--format=csv,noheader,nounits'], stdout=subprocess.PIPE)
     return [float(u) for u in smi.stdout.decode('utf-8').strip().split()]
 
-  @property
-  def gpu_count(self):
-    if not self._gpu_count:
-      self._gpu_count = len(self.gpus_usage)
-    return self._gpu_count
+  @cached_property
+  def gpu_count(self) -> int:
+    return len(self.gpus_usage)
 
-  @property
-  def node(self) -> str:
-    return platform.node()
+  @cached_property
+  def is_logging(self) -> bool:
+    return {
+      self.Detail.rank: True,
+      self.Detail.node: comm.rank == min(node_ranks),
+      self.Detail.single: comm.rank == 0,
+      self.Detail.none: False
+    }[self.detail]
 
-  def log_usage(self) -> None:
+  @cached_property
+  def log_path(self) -> str:
+    return {
+      self.Detail.rank: f'usage_{comm.rank}.log',
+      self.Detail.node: f'usage_{node}.log',
+      self.Detail.single: 'usage.log',
+      self.Detail.none: ValueError
+    }[self.detail]
+
+  def configure_logger(self) -> logging.Logger:
+    if self.is_logging:
+      logging.basicConfig(filename=self.log_path, encoding='utf-8',
+                          level=logging.ERROR)
+      log = logging.getLogger("cctbx.usage")
+      log.setLevel(logging.DEBUG)
+    else:
+      log = logging.getLogger("cctbx.usage")
+      log.setLevel(logging.CRITICAL + 1)
+      log.addHandler(logging.NullHandler())
+    return log
+
+  def log_current_usage(self) -> None:
     cpu_usage = self.cpu_usage
     cpu_memory = self.cpu_memory
     gpu_usage = self.gpus_usage
     gpu_memory = self.gpus_memory
-    log.info(msg=f'{cpu_usage=}, {cpu_memory=}, {gpu_usage=}, {gpu_memory=}.')
+    self.log.info(msg=f'{cpu_usage=}, {cpu_memory=}, {gpu_usage=}, {gpu_memory=}.')
 
-  def logging_deamon(self) -> None:
+  def log_usage_every_period(self) -> None:
     """Warning: call as threading daemon only, otherwise will never stop"""
     while True:
-      threading.Thread(target=self.log_usage, args=(self, ))
-      time.sleep(self.every)
+      threading.Thread(target=self.log_current_usage, args=(self,)).start()
+      time.sleep(self.period)
 
-  def start_logging_deamon(self) -> threading.Thread:
-    return threading.Thread(target=self.logging_deamon,
-                            args=(self,), daemon=True)
-
-
-
+  def start_logging_daemon(self) -> None:
+    self._daemon = threading.Thread(target=self.log_usage_every_period,
+                                    args=(self,), daemon=True)
+    self.daemon.start()
