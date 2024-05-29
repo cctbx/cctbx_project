3af4e4b683cb4717b7301bb0dd7be305da1a619d
Daniel Tchon <dantch@onet.eu>
2024-05-29 14:30:46 -0700

    Move logging to `UsageLogManager`, allow plotting from logs

--- xfel/util/usage_monitor.py
+++ xfel/util/usage_monitor.py
@@ -1,18 +1,23 @@
 from collections import UserDict
 from contextlib import ContextDecorator
-from dataclasses import dataclass
+from dataclasses import astuple, dataclass
 from datetime import datetime, timedelta
 from enum import Enum
 from functools import cached_property
+from glob import glob
 import itertools
 import logging
+from pathlib import Path
 import platform
 import psutil
+import re
+import os
 from statistics import mean
 import subprocess
 import threading
 import time
-from typing import List, Tuple
+from typing import List, Tuple, Union
+PathLike = Union[str, bytes, os.PathLike]
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,6 +28,37 @@ comm = MPI.COMM_WORLD
 process = psutil.Process()
 
 
+
+
+class UsageLogManager:
+  date_fmt = '%Y-%m-%d %H:%M:%S,uuu'
+  fmt = '%(asctime)s - %(message)s'
+  formatter = logging.Formatter(fmt=fmt, datefmt=date_fmt)
+  line_regex = re.compile(
+    r'(\d{2,4}-\d\d-\d\d \d\d:\d\d:\d\d,\d+) - '
+    r'UsageStats\(cpu_usage=(-?\d+\.?\d*), ?cpu_memory=(-?\d+\.?\d*), ?'
+    r'gpu_usage=(-?\d+\.?\d*), ?gpu_memory=(-?\d+\.?\d*)')
+
+  def __init__(self, logger_name: str) -> None:
+    self.log = logging.getLogger(logger_name)
+
+  def get_file_logger(self, file_name: str) -> logging.Logger:
+    self.log.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(filename=file_name)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(self.formatter)
+    self.log.addHandler(file_handler)
+    return self.log
+
+  def get_null_logger(self) -> logging.Logger:
+    self.log.setLevel(logging.CRITICAL + 1)
+    self.log.addHandler(logging.NullHandler())
+    return self.log
+
+
+usage_log_manager = UsageLogManager('cctbx.usage')
+
+
 @dataclass
 class UsageStats:
   cpu_usage: float = 0.0
@@ -30,16 +66,34 @@ class UsageStats:
   gpu_usage: float = 0.0
   gpu_memory: float = 0.0
 
+  @property
+  def vector(self) -> np.ndarray:
+    return np.array(list(astuple(self)), dtype=float)
+
   def __add__(self, other: 'UsageStats') -> 'UsageStats':
-    return UsageStats(
-      cpu_usage=(self.cpu_usage + other.cpu_usage),
-      cpu_memory=(self.cpu_memory + other.cpu_memory),
-      gpu_usage=(self.gpu_usage + other.gpu_usage),
-      gpu_memory=(self.gpu_memory + other.gpu_memory),
-    )
+    return self.__class__(*(self.vector + other.vector))  # noqa
+
+  def __mul__(self, other: float) -> 'UsageStats':
+    return self.__class__(*(self.vector * other))  # noqa
+
+  def __truediv__(self, other: float) -> 'UsageStats':
+    return self.__class__(*(self.vector / other))  # noqa
 
 
 class UsageStatsHistory(UserDict):
+  """A convenience class to store and manipulate"""
+
+  @classmethod
+  def from_file(cls, path: PathLike) -> 'UsageStatsHistory':
+    new = cls()
+    with open(path, 'r') as file:
+      for line in file.readlines():
+        if match := UsageLogManager.line_regex.match(line):
+          t = datetime.strptime(match.group(1), UsageLogManager.date_fmt)
+          us = UsageStats(*match.group(2, 3, 4, 5))
+          new[t] = us
+    return new
+
   def get_deltas_in_minutes(self) -> np.ndarray:
     times = np.array(list(self.keys()), dtype='datetime64')
     return (times - times[0]) / np.timedelta64(1, 'm')
@@ -109,6 +163,14 @@ class RankInfo:
 
 rank_info = RankInfo()
 
+#from xfel.util.usage_monitor import UsageMonitor#\
+#
+#@UsageMonitor(detail='rank', period=1.0)
+#function_to_be_timed()
+#
+#with UsageMonitor(detail='rank', period=1.0):
+#  function_to_be_timed()
+
 
 class UsageMonitor(ContextDecorator):
   """This class can be used both as context manager (with) and @decorator"""
@@ -122,7 +184,7 @@ class UsageMonitor(ContextDecorator):
   def __init__(self, detail: str = 'node', period: float = 5.0) -> None:
     self.detail = self.Detail(detail)
     self.period: float = period  # <5 de-prioritizes sub-procs & they stop...
-    self.log: logging.Logger = self.configure_logger()
+    self.log: logging.Logger = self.get_logger()
     self.usage_stats_history = UsageStatsHistory()
     self._daemon = None
 
@@ -163,20 +225,9 @@ class UsageMonitor(ContextDecorator):
       self.Detail.none: None
     }[self.detail]
 
-  def configure_logger(self) -> logging.Logger:
-    if self.is_logging:
-      log = logging.getLogger("cctbx.usage")
-      log.setLevel(logging.DEBUG)
-      file_handler = logging.FileHandler(self.log_path)
-      file_handler.setLevel(logging.DEBUG)
-      formatter = logging.Formatter('%(asctime)s - %(message)s')
-      file_handler.setFormatter(formatter)
-      log.addHandler(file_handler)
-    else:
-      log = logging.getLogger("cctbx.usage")
-      log.setLevel(logging.CRITICAL + 1)
-      log.addHandler(logging.NullHandler())
-    return log
+  def get_logger(self) -> logging.Logger:
+    return usage_log_manager.get_file_logger(self.log_path) \
+      if self.is_logging else usage_log_manager.get_null_logger()
 
   def log_current_usage(self) -> None:
     usage_stats = self.usage_stats
@@ -202,7 +253,7 @@ class UsageMonitor(ContextDecorator):
     usage_stats_histories = comm.gather(self.usage_stats_history, root=0)
     if rank_info.rank == 0:
       usage_stats_histories = [u for u in usage_stats_histories if u is not None]
-      UsageArtist().plot(usage_stats_histories)
+      UsageArtist().plot(usage_stats_histories, 'usage.png')
 
 
 class UsageArtist:
@@ -216,7 +267,10 @@ class UsageArtist:
     self.ax_gu = self.fig.add_subplot(gs[2, 0], sharex=self.ax_cu)
     self.ax_gm = self.fig.add_subplot(gs[3, 0], sharex=self.ax_cu)
 
-  def plot(self, usage_stats_histories: List[UsageStatsHistory]) -> None:
+  def plot(self,
+           usage_stats_histories: List[UsageStatsHistory],
+           save_path: PathLike = None,
+           ) -> None:
     axes = self.ax_cu, self.ax_cm, self.ax_gu, self.ax_gm
     stats = ['cpu_usage', 'cpu_memory', 'gpu_usage', 'gpu_memory']
     labels = ['CPU usage', 'CPU memory', 'GPU usage', 'GPU memory']
@@ -228,4 +282,13 @@ class UsageArtist:
     for ax, label in zip(axes, labels):
       ax.set_ylabel(label)
     self.fig.set_size_inches(12, 10)
-    self.fig.savefig('usage.png')
+    if save_path:
+      self.fig.savefig('usage.png')
+    else:
+      plt.show()
+
+
+def plot_usage_from_logs(log_glob: PathLike, save_path: PathLike) -> None:
+  """Manually plot if UsageMonitor terminates unexpectedly i.e. due to OOM"""
+  histories = [UsageStatsHistory.from_file(Path(p)) for p in glob(log_glob)]
+  UsageArtist().plot(usage_stats_histories=histories, save_path=save_path)
