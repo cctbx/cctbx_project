757691c07ce01ffb1c3ea70fa773a50a172be54c
Daniel Tchon <dantch@onet.eu>
2024-06-03 17:42:07 -0700

    Assert all `ResourceStats` contain only `PerCentFloat`s, not `str`

--- libtbx/resource_monitor.py
+++ libtbx/resource_monitor.py
@@ -66,7 +66,7 @@ class ResourceLogManager:
 class PerCentFloat(float):
   """Convenience wrapper that clamps float between 0 and 100"""
   def __new__(cls, value) -> 'PerCentFloat':
-    return float.__new__(float, max(min(value, 100.0), 0.0))
+    return float.__new__(float, max(min(float(value), 100.0), 0.0))
 
 
 @dataclass
@@ -76,6 +76,10 @@ class ResourceStats:
   cpu_memory: PerCentFloat = 0.0
   gpu_usage: PerCentFloat = 0.0
   gpu_memory: PerCentFloat = 0.0
+
+  def __post_init__(self):
+    for attr in ['cpu_usage', 'cpu_memory', 'gpu_usage', 'gpu_memory']:
+      setattr(self, attr, PerCentFloat(getattr(self, attr)))
 
   @property
   def vector(self) -> np.ndarray:
