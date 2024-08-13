38960db2449101d57087f695b943b77785ed7576
dcliebschner <dcliebschner@lbl.gov>
2024-08-13 12:52:37 -0700

    Decluttering Phenix: remove phenix.fest

--- mmtbx/command_line/fest.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import, division, print_function
-# LIBTBX_SET_DISPATCHER_NAME phenix.fest
-
-from mmtbx.scaling import fest
-import sys
-
-if (__name__ == "__main__"):
-  fest.run(args=sys.argv[1:])
