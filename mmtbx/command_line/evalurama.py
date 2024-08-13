50c7b7210bdb59ef1fe751b9146390e599449e3a
dcliebschner <dcliebschner@lbl.gov>
2024-08-13 11:35:05 -0700

    Decluttering Phenix: archive phenix.evalurama

--- mmtbx/command_line/evalurama.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import, division, print_function
-# LIBTBX_SET_DISPATCHER_NAME phenix.evalurama
-
-from mmtbx.programs import evalurama
-from iotbx.cli_parser import run_program
-
-run_program(evalurama.Program)
