9c0c0f22af7af96d25e6098a1f94575130326441
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2024-08-30 12:02:21 -0700

    update from CC

--- iotbx/pdb/modified_rna_dna_names.py
+++ iotbx/pdb/modified_rna_dna_names.py
@@ -7,7 +7,7 @@ procedure:
   phenix.python elbow/elbow/scripts/process_amino_acid_parentage_from_chemical_componts.py
 
 This file is intended to be generated monthly.
-The date of file generation: Tue Jul 30 14:45:49 2024
+The date of file generation: Fri Aug 30 11:46:00 2024
 """
 from __future__ import absolute_import, division, print_function
 
