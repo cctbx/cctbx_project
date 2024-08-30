4fa059925d0a09a918113f3e32d5d6eceea3e63b
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2024-08-30 12:02:40 -0700

    mostly updates to test

--- libtbx/tst_mahalanobis.py
+++ libtbx/tst_mahalanobis.py
@@ -23,6 +23,154 @@ diamonds_short = '''"carat","cut","color","clarity","depth","table","price","x",
 0.3,"Good","J","SI1",63.8,56,351,4.23,4.26,2.71
 '''
 
+zz=[(0.23, 61.5, 326),
+    (0.23, 56.9, 329),
+    (0.23, 56.9, 349),
+   ]
+
+def mahalanobis_using_pandas():
+  import numpy as np
+  import pandas as pd
+  import scipy as stats
+  from scipy.stats import chi2
+
+  # calculateMahalanobis Function to calculate
+  # the Mahalanobis distance
+  def calculateMahalanobis(y=None, data=None, cov=None):
+
+      y_mu = y - np.mean(data)
+      print(data)
+      print(y)
+      print(y_mu)
+      if not cov:
+        cov = np.cov(data.values.T)
+        print(cov)
+      inv_covmat = np.linalg.inv(cov)
+      left = np.dot(y_mu, inv_covmat)
+      mahal = np.dot(left, y_mu.T)
+      return mahal.diagonal()
+
+  # data
+  data = { 'Price': [100000, 800000, 650000, 700000,
+                     860000, 730000, 400000, 870000,
+                     780000, 400000],
+           'Distance': [16000, 60000, 300000, 10000,
+                        252000, 350000, 260000, 510000,
+                        2000, 5000],
+           'Emission': [300, 400, 1230, 300, 400, 104,
+                        632, 221, 142, 267],
+           'Performance': [60, 88, 90, 87, 83, 81, 72,
+                           91, 90, 93],
+           'Mileage': [76, 89, 89, 57, 79, 84, 78, 99,
+                       97, 99]
+             }
+
+  # Creating dataset
+  df = pd.DataFrame(data,columns=['Price', 'Distance',
+                                  'Emission','Performance',
+                                  'Mileage'])
+
+  # Creating a new column in the dataframe that holds
+  # the Mahalanobis distance for each row
+  data = { 'Price': [100000],
+           'Distance': [60000],
+           'Emission': [104,
+                        ],
+           'Performance': [ 87],
+           'Mileage': [99]
+             }
+  y=pd.DataFrame(data,columns=['Price', 'Distance',
+                                  'Emission','Performance',
+                                  'Mileage'])
+  rc = calculateMahalanobis(y=y, data=df[[
+    'Price', 'Distance', 'Emission','Performance', 'Mileage']])
+  print(rc)
+
+  # calculate p-value for each mahalanobis distance
+  rc = 1 - chi2.cdf(rc, 4)
+  print(rc)
+
+  # display first five rows of dataframe
+  print(df)
+  assert 0
+
+def another_pandas():
+  import numpy as np
+
+  def mahalanobis(x, y, cov=None):
+      x_mean = np.mean(x,0)
+      Covariance = np.cov(np.transpose(y))
+      inv_covmat = np.linalg.inv(Covariance)
+      x_minus_mn = x - x_mean
+      D_square = np.dot(np.dot(x_minus_mn, inv_covmat), np.transpose(x_minus_mn))
+      return D_square.diagonal()
+
+  import pandas as pd
+
+  # filepath = 'https://raw.githubusercontent.com/selva86/datasets/master/diamonds.csv'
+  filepath='diamonds_short.csv'
+  f=open(filepath, 'w')
+  f.write(diamonds_short)
+  del f
+  df = pd.read_csv(filepath).iloc[:, [0,4,6]]
+  df.head()
+
+  #"carat","cut","color","clarity","depth","table","price","x","y","z"
+  data = { 'carat': [],
+           'depth': [],
+           'price': []
+        }
+  for i in range(len(zz)):
+    print(i,zz[i])
+    data['carat'].append(zz[i][0])
+    data['depth'].append(zz[i][1])
+    data['price'].append(zz[i][2])
+  print(data)
+
+  X=pd.DataFrame(data,columns=['carat', 'depth', 'price'])
+  print(X)
+
+  X = np.asarray(X[['carat', 'depth', 'price']].values)
+  Y = np.asarray(df[['carat', 'depth', 'price']].values)
+  print(X)
+  print(Y)
+
+  rc=mahalanobis(X, Y)
+  print('another_pandas',rc)
+
+def mahalanobis_using_numpy(x, data=None, cov=None, verbose=False):
+  import numpy as np
+  def mahalanobis(x, data, cov=None):
+    x_mean = np.mean(x, 0)
+    if verbose: print('x_mean',x_mean)
+    if cov:
+      Covariance=np.asarray(cov)
+    else:
+      Covariance = np.cov(np.transpose(data))
+    if verbose:
+      print('Covariance')
+      print(Covariance)
+    inv_covmat = np.linalg.inv(Covariance)
+    if verbose:
+      print('inv_covmat')
+      print(inv_covmat)
+    x_minus_mn = x - x_mean
+    if verbose:
+      print('x_minus_mn')
+      print(x_minus_mn)
+    D_square = np.dot(np.dot(x_minus_mn, inv_covmat), np.transpose(x_minus_mn))
+    if verbose:
+      print('D_square')
+      print(D_square)
+      print(D_square.diagonal())
+    return D_square.diagonal()
+
+  if verbose:
+    print(x)
+    print(data)
+  rc = mahalanobis(x, data=data, cov=cov)
+  return rc
+
 def mahalanobis_using_numpy_and_scipy(x=None, data=None, mu=None, cov=None):
   """
   Compute the Mahalanobis Distance between each row of x and the data
@@ -32,16 +180,30 @@ def mahalanobis_using_numpy_and_scipy(x=None, data=None, mu=None, cov=None):
     cov  : covariance matrix (p x p) of the distribution. If None, will be
            computed from data.
   """
-  # error prone
-  assert 0
   import numpy as np
   import scipy as sp
+
+  x=np.array(x)
+  print(x)
+  data=np.array(data)
+  print(data)
+  x_mu = x - np.mean(data)
+  if not cov:
+      cov = np.cov(data.values.T)
+  inv_covmat = np.linalg.inv(cov)
+  left = np.dot(y_mu, inv_covmat)
+  mahal = np.dot(left, y_mu.T)
+  return mahal.diagonal()
+
   if mu:
     x_minus_mu = x - mu
   else:
     print(x)
     print(np.mean(data))
     x_minus_mu = x - np.mean(data)
+    print('x_minus_mu',x_minus_mu)
+    assert 0
+  # x_minus_mu=np.array(x)
   print('x_minus_mu',x_minus_mu)
   if not cov: cov = np.cov(data)
   print(cov)
@@ -59,13 +221,26 @@ def main():
   spamreader = csv.reader(f, delimiter=',', quotechar='|')
   for i, row in enumerate(spamreader):
     if not i: continue
-    # print(', '.join(row))
+    print(i,', '.join(row))
     data.append([float(row[0]), float(row[4]), float(row[6])])
 
-  zz=[(0.23, 61.5, 326),
-      (0.23, 56.9, 329),
-      (0.23, 56.9, 349),
-     ]
+  use_numpy=0
+  use_pandas=0
+  if use_pandas:
+    another_pandas()
+
+  if use_numpy:
+    # another_pandas()
+    # mahalanobis_using_pandas()
+    #np.asarray
+    rc1 = mahalanobis_using_numpy(zz, data, verbose=1)
+    # rc = mahalanobis_using_numpy_and_scipy(zz, data)
+    print('rc1',rc1)
+    rc2 = mahalanobis_using_numpy(zz, cov=[
+      [1.55789474e-03, 3.08947368e-02, 1.42105263e-01],
+      [3.08947368e-02, 3.50038781e+00, 5.25207756e+00],
+      [1.42105263e-01, 5.25207756e+00, 5.39556787e+01]], verbose=1)
+    print('rc2',rc2)
 
   rc=math_utils.mahalanobis(zz, data)
   print(rc)
@@ -81,6 +256,9 @@ def main():
   cov = math_utils.covariance_using_sklearn(data, verbose=True)
   rc=math_utils.mahalanobis(zz, cov=cov)
   print(rc)
+  if use_numpy:
+    print(rc1)
+    print(rc2)
   rc=math_utils.mahalanobis_p_values(zz, cov=cov, verbose=True)
   print(rc)
   rc=math_utils.mahalanobis_p_values_outlier_indices(zz, cov=cov)
