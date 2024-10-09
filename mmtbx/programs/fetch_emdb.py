20d939a6a63d3cf3bbfe48f41337db805ef03359
Oleg Sobolev <osobolev@lbl.gov>
2024-10-09 11:41:24 -0700

    Move useful function into a central location. Create a test.

--- mmtbx/programs/fetch_emdb.py
+++ mmtbx/programs/fetch_emdb.py
@@ -1,15 +1,9 @@
 from __future__ import absolute_import, division, print_function
-try:
-  from phenix.program_template import ProgramTemplate
-except ImportError:
-  from libtbx.program_template import ProgramTemplate
-import json
-import urllib.parse
-import libtbx.utils
+from libtbx.program_template import ProgramTemplate
 from libtbx.utils import Sorry
+from mmtbx.wwpdb import rcsb_web_services
 
 pdb_url_base = "https://search.rcsb.org/rcsbsearch/v2/query?json="
-graphql_base = "https://data.rcsb.org/graphql?query="
 emdb_base = "https://ftp.ebi.ac.uk/pub/databases/emdb/structures/"
 
 master_phil_str = '''
@@ -43,45 +37,18 @@ phenix.fetch_emdb code=1aba
 
   # ---------------------------------------------------------------------------
 
-  def api_query(self):
-    graphql_query = '''
-query
-{
-  entry(entry_id:"%s") {
-    exptl {
-      method
-    }
-    rcsb_entry_container_identifiers {
-      emdb_ids
-    }
-  }
-}
-'''
-    url = graphql_base + urllib.parse.quote(graphql_query % self.params.pdb_code)
-    data = libtbx.utils.urlopen(url).read()
-    d = json.loads(data)
-    entry_data = d['data']['entry']
-    exptl = entry_data['exptl'][0]
-    if exptl['method'] != 'ELECTRON MICROSCOPY':
-      raise Sorry('This entry is not an EM structure.')
-    emdb_ids = entry_data['rcsb_entry_container_identifiers']['emdb_ids']
-    if len(emdb_ids)==0:
-      raise Sorry('No associated EMDB IDs found for this entry.')
-    print('Found the following map codes associated to PDB ID: %s'
-      % self.params.pdb_code, file=self.logger)
-    for emdb_id in emdb_ids:
-      print(emdb_id, file=self.logger)
-    self.emdb_ids = emdb_ids
-
-  # ---------------------------------------------------------------------------
-
   def download_pdb_file(self):
     pdb_url = 'https://files.rcsb.org/download/%s.pdb' % self.params.pdb_code
     pdb_fn = '%s.pdb' % self.params.pdb_code
     urllib.request.urlretrieve(pdb_url, pdb_fn)
 
-  def download_maps(self):
-    for emdb_id in self.emdb_ids:
+  def download_maps(self, emdb_ids):
+    if emdb_ids is None:
+      raise Sorry('No associated EMDB IDs found for this entry.')
+    print('Found the following map codes associated to PDB ID: %s'
+      % self.params.pdb_code, file=self.logger)
+    for emdb_id in emdb_ids:
+      print("Downloading %s" % emdb_id, file=self.logger)
       emdb_id_numeral = emdb_id.split('-')[1]
       emdb_url = emdb_base + 'EMD-%s/map/emd_%s.map.gz' % (
         emdb_id_numeral, emdb_id_numeral)
@@ -91,7 +58,6 @@ query
   # ---------------------------------------------------------------------------
 
   def run(self):
-    self.emdb_ids = []
-    self.api_query()
+    emdb_ids = rcsb_web_services.get_emdb_id_for_pdb_id(self.params.pdb_code)
     self.download_pdb_file()
-    self.download_maps()
+    self.download_maps(emdb_ids)
