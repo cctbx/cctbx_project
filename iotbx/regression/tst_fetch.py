dc532a7c2101cba77a3330c39c886a0b3f92e4e0
Oleg Sobolev <osobolev@lbl.gov>
2024-10-10 08:55:29 -0700

    Refactoring fetch_pdb for readability, adding map functionality, without breaking anything yet.

--- iotbx/regression/tst_fetch.py
+++ iotbx/regression/tst_fetch.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 from libtbx.utils import format_cpu_times
 import requests
-from iotbx.pdb.fetch import fetch
+from iotbx.pdb.fetch import fetch, get_link
 from libtbx.test_utils import assert_lines_in_text
 
 def exercise_1():
@@ -14,7 +14,19 @@ ATOM      2  CA  GLY A   1      -9.052   4.207   4.651  1.00 16.57           C
 ATOM      3  C   GLY A   1      -8.015   3.140   4.419  1.00 16.16           C
 """)
 
+def exercise_get_link():
+  r = []
+  for ft in ['model_pdb', 'model_cif', 'sequence', 'sf', 'map']:
+    r.append(get_link('rcsb', ft, '1ab2', emdb_number="1111"))
+  # print (r)
+  assert r == ['https://files.rcsb.org/pub/pdb/data/structures/divided/pdb/ab/pdb1ab2.ent.gz',
+               'https://files.rcsb.org/pub/pdb/data/structures/divided/mmCIF/ab/1ab2.cif.gz',
+               'https://www.rcsb.org/fasta/entry/1ab2',
+               'https://files.rcsb.org/download/1ab2-sf.cif.gz',
+               'https://files.rcsb.org/pub/emdb/structures/EMD-1111/map/emd_1111.map.gz'], r
+
 if (__name__ == "__main__"):
+  exercise_get_link()
   exception_occured = False
   try:
     r = requests.get('https://search.rcsb.org/')
