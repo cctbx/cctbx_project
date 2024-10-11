0d87aa0869dbc0e68004ea1e0f9edab425e93d27
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2024-10-11 15:04:13 -0700

    automatic lookup of parent and child

--- mmtbx/conformation_dependent_library/cdl_utils.py
+++ mmtbx/conformation_dependent_library/cdl_utils.py
@@ -43,22 +43,37 @@ def round_to_int(d, n=10, wrap=True):
 def round_to_ten(d):
   return round_to_int(d, 10)
 
-children = {'PRO' : ['HYP']} # prePRO
-parents = { # should be done automatically
-  'HYP' : 'PRO',
-  }
+def get_children():
+  from iotbx.pdb.modified_aa_names import lookup as parent_child
+  rc={}
+  for c, p in parent_child.items():
+    rc.setdefault(p,[])
+    rc[p].append(c)
+  return rc
+
+children = get_children()
+
+def is_child_of_PRO(resname):
+  return resname in children['P']
+
+def get_parent(resname):
+  from iotbx.pdb.modified_aa_names import lookup as parent_child
+  rc=parent_child.get(resname, '???')
+  from iotbx.pdb.amino_acid_codes import three_letter_given_one_letter
+  rc = three_letter_given_one_letter.get(rc, '???')
+  return rc
 
 def get_res_type_group(resname1, resname2):
   resname1=resname1.strip()
   resname2=resname2.strip()
   # check for prePro
-  if resname2=="PRO" or resname2 in children['PRO']:
+  if resname2=="PRO" or is_child_of_PRO(resname2):
     lookup = before_pro_groups
   else:
     lookup = not_before_pro_groups
   # find four other sub-classes
   for key in lookup:
-    if resname1 in lookup[key] or parents.get(resname1, '???') in lookup[key]:
+    if resname1 in lookup[key] or get_parent(resname1) in lookup[key]:
       return key
   return None
 
