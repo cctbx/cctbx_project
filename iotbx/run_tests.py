ada2e0f520dd6bcb5adcfbcf2f875c0a004e19eb
Oleg Sobolev <osobolev@lbl.gov>
2024-11-26 15:33:40 -0800

    Make mmCIF unit_cell consistent with PDB in case where it is 1,1,1,90,90,90

--- iotbx/run_tests.py
+++ iotbx/run_tests.py
@@ -10,6 +10,7 @@ tst_list_base = [
   "$D/regression/tst_simple_parser.py",
   "$D/regression/tst_phil.py",
   "$D/regression/tst_pdb_cif_inputs.py",
+  "$D/regression/tst_pdb_cif_cells.py",
   "$D/regression/tst_data_manager.py",
   "$D/regression/tst_map_manager_wrapping.py",
   "$D/regression/tst_map_manager.py",
