4b24ef08605492670e994cc2b00921a70eda6aba
Oleg Sobolev <osobolev@lbl.gov>
2025-01-14 13:11:58 -0800

    Moving find_reference to Phenix

--- mmtbx/command_line/find_reference.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-# LIBTBX_SET_DISPATCHER_NAME mmtbx.find_reference
-# LIBTBX_SET_DISPATCHER_NAME phenix.find_reference
-
-from mmtbx.programs import find_reference
-from iotbx.cli_parser import run_program
-
-result = run_program(find_reference.Program)
