72af29b1cb111f6c010c637a36edd30f99561820
terwill <tterwilliger@newmexicoconsortium.org>
2025-04-05 15:15:24 -0600

    Move mmtbx/command_line/development.aev.py to mmtbx/command_line/development_aev.py keeping original dispatcher name and add dummy test mmtbx/hydrogens/tst_add_hydrogen.py to prevent failure of automatic imports in pdoc3

---
+++ mmtbx/hydrogens/tst_add_hydrogen.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, division, print_function
+import time
+
+"""Dummy file just to have  from mmtbx.hydrogens.tst_add_hydrogen import compare_models not fail"""
+def compare_models():
+  pass
