13d8f1e0aef2c54929b43adc01498acd02e50d47
Aaron Brewster <asbrewster@lbl.gov>
2025-04-18 16:01:23 -0700

    Job submission updates
    - Rename output logs from .out to .log
    - Reduce chunk size for ensemble refinement to 64
    - Add string replacement to the command for Phenix jobs, not just the phil params
    - Check evaluate_anom for Phenix jobs as a not multiprocessed job
    - Extra debugging for new dataset versions

--- xfel/ui/db/job.py
+++ xfel/ui/db/job.py
@@ -69,7 +69,7 @@ class Job(db_proxy):
 
   def get_log_path(self):
     run_path = get_run_path(self.app.params.output_folder, self.trial, self.rungroup, self.run)
-    return os.path.join(run_path, "stdout", "log.out")
+    return os.path.join(run_path, "stdout", "out.log")
 
   def submit(self, previous_job = None):
     raise NotImplementedError("Override me!")
@@ -653,7 +653,7 @@ class EnsembleRefinementJob(Job):
     striping.rungroup={}
     striping.run={}
     {}
-    striping.chunk_size=256
+    striping.chunk_size=64
     striping.stripe=False
     striping.dry_run=True
     striping.output_folder={}
@@ -853,7 +853,7 @@ class MergingJob(Job):
       params = copy.deepcopy(params)
       params.nnodes = params.nnodes_merge
 
-    return do_submit(command, submit_path, output_path, params, log_name="log.out", err_name="err.out", job_name=identifier_string)
+    return do_submit(command, submit_path, output_path, params, log_name="out.log", err_name="err.log", job_name=identifier_string)
 
 class PhenixJob(Job):
   def get_global_path(self):
@@ -888,12 +888,15 @@ class PhenixJob(Job):
     target_phil_path = os.path.join(output_path, identifier_string + "_params.phil")
     input_folder, _, _, input_mtz, _ = previous_job.get_output_files()
 
-    command = self.task.parameters.split('\n')[0]
-    phil_params = '\n'.join(self.task.parameters.split('\n')[1:])
-    phil_params = phil_params.replace('<PREVIOUS_TASK_MTZ>', os.path.join(input_folder, input_mtz))
-    phil_params = phil_params.replace('<PREVIOUS_TASK_FOLDER>', input_folder)
-    phil_params = phil_params.replace('<DATASET_NAME>', self.dataset.name)
-    phil_params = phil_params.replace('<DATASET_VERSION>', str(self.dataset_version.version))
+    def replace_keywords(input_str):
+      input_str = input_str.replace('<PREVIOUS_TASK_MTZ>', os.path.join(input_folder, input_mtz))
+      input_str = input_str.replace('<PREVIOUS_TASK_FOLDER>', input_folder)
+      input_str = input_str.replace('<DATASET_NAME>', self.dataset.name)
+      input_str = input_str.replace('<DATASET_VERSION>', str(self.dataset_version.version))
+      return input_str
+
+    command = replace_keywords(self.task.parameters.split('\n')[0])
+    phil_params = replace_keywords('\n'.join(self.task.parameters.split('\n')[1:]))
 
     with open(target_phil_path, 'w') as f:
       f.write(phil_params)
@@ -906,7 +909,7 @@ class PhenixJob(Job):
       params.nnodes = params.nnodes_merge
     params.use_mpi = False
     params.shifter.staging = None
-    if 'upload' in command:
+    if 'upload' in command or 'evaluate_anom' in command:
       params.nnodes = 1
       params.nproc_per_node = 1
       #params.queue = 'shared'
@@ -920,7 +923,7 @@ class PhenixJob(Job):
        params.shifter.srun_script_template = os.path.join( \
          libtbx.env.find_in_repositories("xfel/ui/db/cfgs"), "phenix_srun.sh")
 
-    return do_submit(command, submit_path, output_path, params, log_name="log.out", err_name="err.out", job_name=identifier_string)
+    return do_submit(command, submit_path, output_path, params, log_name="out.log", err_name="err.log", job_name=identifier_string)
 
 # Support classes and functions for job submission
 
@@ -1162,6 +1165,7 @@ def submit_all_jobs(app):
 
       if next_version is not None:
         latest_version = app.create_dataset_version(dataset_id = dataset.id, version=next_version)
+        print("Created a new dataset version", next_version, "with jobs", [j.id for j in global_tasks[key]])
         for job in global_tasks[key]:
           latest_version.add_job(job)
 
