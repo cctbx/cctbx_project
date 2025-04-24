afe1e80d3c70bb1e0fef920aa4c9765b4f6c5777
Viljar Femoen <viljar.femoen@hotmail.no>
2025-04-24 10:44:22 +0200

    Implicitly import the different from_scatterers and gradient implementations

--- cctbx/xray/structure_factors/__init__.py
+++ cctbx/xray/structure_factors/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 from cctbx.xray.structure_factors.misc import *
+from cctbx.xray.structure_factors.algorithm import *
 from cctbx.xray.structure_factors.gradient_flags import *
 from cctbx.xray.structure_factors.from_scatterers import *
 from cctbx.xray.structure_factors.gradients import *
