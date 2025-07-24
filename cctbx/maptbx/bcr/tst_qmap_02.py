2c064fa895e4c4a165e3285c5d7ee366d6cb8b7f
Pavel <pafonine@lbl.gov>
2025-07-24 16:50:19 -0700

    clean test

--- cctbx/maptbx/bcr/tst_qmap_02.py
+++ cctbx/maptbx/bcr/tst_qmap_02.py
@@ -3,23 +3,13 @@ from scitbx.array_family import flex
 import iotbx.map_manager
 import iotbx.pdb
 from cctbx.maptbx.bcr import qmap
-
 from cctbx import maptbx
 
 import boost_adaptbx.boost.python as bp
 ext = bp.import_ext("cctbx_maptbx_bcr_bcr_ext")
 
-#pdb_str = """
-#CRYST1   20.000   22.000   24.000  90.00  90.00  90.00 P 1
-#ATOM      1  N   HIS A 109       9.500  14.000   9.000  1.10 60.10           N
-#ATOM      2  C   HIS A 109      20.000   0.000   0.000  0.90 20.00           C
-#ATOM      3  O   HIS A 109       0.000   0.000   0.000  0.50 40.00           O
-#TER
-#END
-#"""
-
 pdb_str = """
-CRYST1   20.000   20.000   20.000  90.00  90.00  90.00 P 1
+CRYST1   20.000   25.000   30.000  90.00  90.00  90.00 P 1
 ATOM      1  N   HIS A 109      10.000  14.000   9.000  1.10 30.10           N
 ATOM      2  C   HIS A 109      13.000  10.000  10.000  0.90 20.00           C
 ATOM      3  O   HIS A 109      12.000  12.000  11.000  0.50 10.00           O
@@ -27,11 +17,10 @@ TER
 END
 """
 
-def run(d_min=2, prefix="12"):
+def run(d_min=2):
   pdb_inp = iotbx.pdb.input(source_info=None, lines = pdb_str)
   cs = pdb_inp.crystal_symmetry()
   hierarchy = pdb_inp.construct_hierarchy()
-  pdb_inp.write_pdb_file(file_name="m%s.pdb"%prefix)
   crystal_gridding = maptbx.crystal_gridding(
     unit_cell        = cs.unit_cell(),
     space_group_info = cs.space_group_info(),
@@ -42,7 +31,7 @@ def run(d_min=2, prefix="12"):
   n_real = crystal_gridding.n_real()
   xrs = pdb_inp.xray_structure_simple()
   xrs.scattering_type_registry(
-        table = "it1992",
+        table = "wk1995",
         d_min = 2,
         types_without_a_scattering_contribution=["?"])
 
@@ -51,20 +40,24 @@ def run(d_min=2, prefix="12"):
   m1 = fft_map.real_map_unpadded()
   OmegaMap = qmap.compute(hierarchy=hierarchy, unit_cell=cs.unit_cell(),
                      n_real=n_real, resolution=d_min, resolutions=None,
-                     debug=True)
-  print(flex.linear_correlation(x=m1.as_1d(), y=OmegaMap.as_1d()).coefficient())
+                     debug=False)
+  assert flex.linear_correlation(
+    x=m1.as_1d(), y=OmegaMap.as_1d()).coefficient() > 0.99
   mm1 = iotbx.map_manager.map_manager(
     map_data                   = m1,
     unit_cell_grid             = m1.all(),
     unit_cell_crystal_symmetry = cs,
     wrapping                   = True)
-  mm1.write_map("m_fft_%s.ccp4"%prefix)
+  #mm1.write_map("m_fft.ccp4")
   mm2 = iotbx.map_manager.map_manager(
     map_data                   = OmegaMap,
     unit_cell_grid             = OmegaMap.all(),
     unit_cell_crystal_symmetry = cs,
     wrapping                   = True)
-  mm2.write_map("m_bcr_%s.ccp4"%prefix)
+  #mm2.write_map("m_bcr.ccp4")
+  assert flex.linear_correlation(
+    x=mm1.map_data().as_1d(),
+    y=mm2.map_data().as_1d()).coefficient() > 0.99
 
 if (__name__ == "__main__"):
   run()
