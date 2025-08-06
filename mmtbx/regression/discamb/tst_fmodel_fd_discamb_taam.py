c70152f45602b585598ec57b9149b18db245e834
dcliebschner <dcliebschner@lbl.gov>
2025-08-06 11:29:16 -0700

    Discamb: update TAAM fd test

--- mmtbx/regression/discamb/tst_fmodel_fd_discamb_taam.py
+++ mmtbx/regression/discamb/tst_fmodel_fd_discamb_taam.py
@@ -6,13 +6,11 @@ from scitbx.array_family import flex
 from libtbx.test_utils import approx_equal
 from cctbx import adptbx
 
-try:                from pydiscamb import DiscambWrapper
+try:
+  from pydiscamb import DiscambWrapper, FCalcMethod
 except ImportError: DiscambWrapper = None
 
-# set this to True to print values
 debug = False
-
-# ------------------------------------------------------------------------------
 
 pdb_str = """
 CRYST1   13.000   16.000   19.000  97.00 110.00  95.00 P 1
@@ -24,59 +22,48 @@ HETATM    2  H2  HOH A3116       4.907   5.843   5.087  1.00  3.03           H
 
 # ------------------------------------------------------------------------------
 
-def get_discamb_grads(xrs, d_target_d_fcalc):
+def show(g1,g2,g3):
+  print('wrapper from fmodel ', [round(_,8) for _ in g1])
+  print('new wrapper from xrs', [round(_,8) for _ in g2])
+  print('finite diff fmodel  ', [round(_,8) for _ in g3])
+
+# ------------------------------------------------------------------------------
+
+def get_wrapper_grads(xrs, d_target_d_fcalc):
   '''
-  Compute analytical gradients using DiSCaMB.
-
-  Parameters
-  ----------
-  xrs : cctbx.xray.structure
-      The input crystal structure.
-  d_target_d_fcalc : flex.complex_double
-      Gradient of the target function with respect to F_calc.
-
-  Returns
-  -------
-  list of float
-      Analytical gradients with respect to model parameters.
-
-  Notes
-  -----
-  Uses pyDiscamb to compute parameter gradients.
-  If debug mode is enabled, prints the gradients.
-'''
-  w = DiscambWrapper(xrs)
+  '''
+  w = DiscambWrapper(xrs,
+                     method = FCalcMethod.TAAM,
+                     frozen_lcs = True)
   g = w.d_target_d_params(d_target_d_fcalc)
-  if debug:
-    print("gtaam:", [round(_,8) for _ in g])
   return g
 
 # ------------------------------------------------------------------------------
 
-def get_fd(fmodel_, eps=1.e-5):
+def get_fd(fmodel_, eps=1.e-5, use_discamb=False):
   '''
-  Compute numerical gradients using finite differences.
+  Compute numerical gradients via finite differences.
 
   Parameters
   ----------
   fmodel_ : mmtbx.f_model.manager
-      Fmodel object containing structure factors and model object.
+      fmodel object containing structure factors and model object.
   eps : float, optional
       Step size for finite difference calculation (default is 1e-5).
 
   Returns
   -------
   flex.double
-      Numerical gradients with respect to all enabled parameters
-      (sites, u_iso, u_aniso, occupancy).
+      Finite difference approximations of the gradients.
 
   Notes
   -----
-  Iterates over all scatterers in the structure and perturbs each enabled
-  parameter to compute numerical derivatives of the target function.
+  For each enabled parameter in the structure, this function perturbs the value
+  positively and negatively by eps, recomputes the target function, and uses
+  central difference to approximate the gradient.
   '''
   fmodel = fmodel_.deep_copy()
-  print('start is_taam', fmodel.is_taam())
+  #
   target_functor = fmodel.target_functor()
   structure = fmodel.xray_structure
   unit_cell = structure.unit_cell()
@@ -94,7 +81,6 @@ def get_fd(fmodel_, eps=1.e-5):
           site_cart[ix] += signed_eps
           sc.site = unit_cell.fractionalize(site_cart)
           fmodel.update_xray_structure(update_f_calc=True)
-          print('update sites is_taam', fmodel.is_taam())
           ts.append(target_functor().target_work())
         gs.append((ts[0]-ts[1])/(2*eps))
       sc.site = site_orig
@@ -135,132 +121,140 @@ def get_fd(fmodel_, eps=1.e-5):
     print("fd:", [round(_,8) for _ in gs])
   return gs
 
-# ------------------------------------------------------------------------------
-
-def run(table):
-  '''
-  Compare analytical and numerical gradients for different scattering tables.
-
-  Parameters
-  ----------
-  table : str
-      Scattering table to use, one of: 'electron', 'wk1995', 'it1992'.
-
-  Returns
-  -------
-  None
-
-  Raises
-  ------
-  AssertionError
-      If the analytical and numerical gradients do not match within the
-      expected tolerance.
-
-  Notes
-  -----
-  This function:
-  - Builds a model from a water molecule PDB string.
-  - Computes gradients using both DiSCaMB (analytical) and finite differences
-    (numerical).
-  - Compares gradients for selected atomic parameters:
-    - Site (Cartesian coordinates)
-    - Isotropic and anisotropic displacement parameters (U_iso, U_aniso)
-    - Occupancy
-  '''
+def run(table, fd_delta = 1.e-5):
+  """
+  Check packed gradients
+  """
   print('-------------- %s --------------' % table)
   pdb_inp = iotbx.pdb.input(source_info=None, lines=pdb_str)
   xrs = pdb_inp.xray_structure_simple()
   xrs.scattering_type_registry(table = table)
   xrs_iso = xrs.deep_copy_scatterers()
   xrs_iso.convert_to_isotropic()
-  f_obs = abs(xrs_iso.structure_factors(d_min=0.5).f_calc())
+  f_obs = abs(xrs_iso.structure_factors(d_min=1.0).f_calc())
   #
   params = mmtbx.f_model.sf_and_grads_accuracy_master_params.extract()
-  params.algorithm = "direct"
-  params.taam = True
+  params.algorithm = "taam"
+  params.extra.discamb.taam.freeze_local_coordinate_system = True
+  params.exp_table_one_over_step_size=0
   fmodel = mmtbx.f_model.manager(
     f_obs                        = f_obs,
     xray_structure               = xrs,
+    target_name                  = "ls_wunit_kunit",
     sf_and_grads_accuracy_params = params)
   scatterers = xrs.scatterers()
   tf = fmodel.target_functor()(compute_gradients = True)
   d_target_d_fcalc = tf.d_target_d_f_calc_work()
   #
+  scatterers.flags_set_grads(state=False)
+  #
   # sites cart only for atom 1
   #
-  print("sites_cart_1")
-  scatterers.flags_set_grads(state=False)
-  scatterers.flags_set_grad_site(iselection = flex.size_t([1]))
-  g2 = get_discamb_grads(xrs, d_target_d_fcalc)
-  g3 = get_fd(fmodel, eps=1.e-5)
-  assert approx_equal(g2,g3, 1.e-3)
-  if debug: print()
+#  print("sites_cart_1")
+#  scatterers.flags_set_grad_site(iselection = flex.size_t([1]))
+#  #
+#  g1 = tf.gradients_wrt_atomic_parameters().packed()
+#  g2 = get_wrapper_grads(xrs, d_target_d_fcalc)
+#  g3 = get_fd(fmodel, eps=fd_delta)
+#  #assert approx_equal(g1,g2, 1.e-6)
+#  #assert approx_equal(g1,g3, 1.e-6)
+#  show(g1,g2,g3)
   #
   # Now add iso B for H #2
   #
-#  print("sites_cart_1 + uiso_2")
-#  scatterers.flags_set_grad_u_iso(iselection = flex.size_t([2]))
-#  g2 = get_discamb_grads(xrs, d_target_d_fcalc)
-#  g3 = get_fd(fmodel, eps=1.e-5)
-#  assert approx_equal(g2,g3, 1.e-5)
-#  if debug: print()
-#  #
-#  # Now add occupancy of H #0
-#  #
-#  print("sites_cart_1 + uiso_2 + occ_0")
-#  scatterers.flags_set_grad_occupancy(iselection = flex.size_t([0]))
-#  g2 = get_discamb_grads(xrs, d_target_d_fcalc)
-#  g3 = get_fd(fmodel, eps=1.e-5)
-#  assert approx_equal(g2,g3, 1.e-3)
-#  if debug: print()
-#  #
-#  # Now add aniso ADP of O #1
-#  #
-#  print("sites_cart_1 + uiso_2 + occ_0 + uaniso_1")
-#  scatterers.flags_set_grad_u_aniso(iselection = flex.size_t([1]))
-#  g2 = get_discamb_grads(xrs, d_target_d_fcalc)
-#  g3 = get_fd(fmodel, eps=1.e-5)
-#  assert approx_equal(g2,g3, 1.e-3)
-#  if debug: print()
-#  #
-#  # Now add sites cart of H #2
-#  #
-#  print("sites_cart_1 + uiso_2 + occ_0 + uaniso_1 + sites_cart_2")
-#  scatterers.flags_set_grad_site(iselection = flex.size_t([2]))
-#  g2 = get_discamb_grads(xrs, d_target_d_fcalc)
-#  g3 = get_fd(fmodel, eps=1.e-5)
-#  assert approx_equal(g2,g3, 1.e-5)
-#  if debug: print()
-#  #
-#  # Cancel all
-#  #
-#  scatterers.flags_set_grads(state=False)
-#  #
-#  # Now all parameters
-#  #
+  print("sites_cart_1 + uiso_2")
+  scatterers.flags_set_grad_u_iso(iselection = flex.size_t([2]))
+  g1 = tf.gradients_wrt_atomic_parameters().packed()
+  g2 = get_wrapper_grads(xrs, d_target_d_fcalc)
+  g3 = get_fd(fmodel, eps=fd_delta)
+  show(g1,g2,g3)
+  assert approx_equal(g1,g2, 1.e-5)
+  assert approx_equal(g1,g3, 1.e-4)
+  #
+  # Now add occupancy of H #0
+  #
+  print("sites_cart_1 + uiso_2 + occ_0")
+  scatterers.flags_set_grad_occupancy(iselection = flex.size_t([0]))
+  g1 = tf.gradients_wrt_atomic_parameters().packed()
+  g2 = get_wrapper_grads(xrs, d_target_d_fcalc)
+  g3 = get_fd(fmodel, eps=fd_delta)
+  show(g1,g2,g3)
+  assert approx_equal(g1,g2, 1.e-5)
+  assert approx_equal(g1,g3, 1.e-4)
+
+  #
+  # Now add aniso ADP of O #1
+  #
+  print("sites_cart_1 + uiso_2 + occ_0 + uaniso_1")
+  scatterers.flags_set_grad_u_aniso(iselection = flex.size_t([1]))
+  g1 = tf.gradients_wrt_atomic_parameters().packed()
+  g2 = get_wrapper_grads(xrs, d_target_d_fcalc)
+  g3 = get_fd(fmodel, eps=fd_delta)
+  show(g1,g2,g3)
+  assert approx_equal(g1,g2, 1.e-5)
+  assert approx_equal(g1,g3, 1.e-3)
+
+  #
+  # Now add sites cart of H #2
+  #
+  print("sites_cart_1 + uiso_2 + occ_0 + uaniso_1 + sites_cart_2")
+  scatterers.flags_set_grad_site(iselection = flex.size_t([2]))
+  g1 = tf.gradients_wrt_atomic_parameters().packed()
+  g2 = get_wrapper_grads(xrs, d_target_d_fcalc)
+  g3 = get_fd(fmodel, eps=fd_delta)
+  show(g1,g2,g3)
+  #assert approx_equal(g1,g2, 1.e-6)
+  #assert approx_equal(g1,g3, 1.e-6)
+
+  #
+  # Cancel all
+  #
+  scatterers.flags_set_grads(state=False)
+  #
+  # Now all parameters
+  #
 #  all_selection = flex.size_t([0,1,2])
 #  scatterers.flags_set_grad_site(iselection = all_selection)
 #  scatterers.flags_set_grad_occupancy(iselection = all_selection)
 #  scatterers.flags_set_grad_u_aniso(iselection = flex.size_t([1]))
 #  scatterers.flags_set_grad_u_iso(iselection = flex.size_t([0,2]))
-#
-#  g2 = get_discamb_grads(xrs, d_target_d_fcalc)
-#  g3 = get_fd(fmodel, eps=1.e-5)
-#  assert approx_equal(g2,g3, 1.e-5)
+#  g1 = tf.gradients_wrt_atomic_parameters().packed()
+#  g2 = get_wrapper_grads(xrs, d_target_d_fcalc)
+#  g3 = get_fd(fmodel, eps=fd_delta)
+#  #assert approx_equal(g1,g2, 1.e-6)
+#  #assert approx_equal(g1,g3, 1.e-6)
 #  if debug: print()
+#  show(g1,g2,g3)
 
 # ------------------------------------------------------------------------------
 
 if(__name__ == "__main__"):
   '''
-  Test to compare gradients computed by DiSCaMB and finite differences.
+  Compare gradients from DiSCaMB and finite differences.
 
-  This script runs the run() function for each supported scattering table.
-  If DiscambWrapper is not available, it skips the tests.
+  Parameters
+  ----------
+  table : str
+      Scattering factor table to use. Must be one of: 'electron', 'wk1995', 'it1992'.
 
   Returns
   -------
   None
+
+  Raises
+  -------
+  AssertionError
+      If gradients from the methods differ beyond the allowed tolerance.
+
+  Notes
+  -----
+  For a test water molecule (with anisotropic O and isotropic H), the function:
+  - Enables different parameter gradients (site, u_iso, u_aniso, occupancy).
+  - Computes gradients using:
+      1. DiSCaMB analytical method via fmodel object,
+      2. DiSCaMB analytical method via a new wrapper instance,
+      3. Finite difference method based on fmodel.
+  - Compares all three methods for consistency.
   '''
   t0 = time.time()
   if DiscambWrapper is None:
