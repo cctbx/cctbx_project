433e02623964c830702a654a35ad506d1004def7
Pavel Afonine <pafonine@lbl.gov>
2025-08-10 17:24:17 -0700

    remove output

--- cctbx/maptbx/bcr/compute_tables.py
+++ cctbx/maptbx/bcr/compute_tables.py
@@ -41,13 +41,8 @@ def run_one(args):
     epsc=0.001,
     )
   r, err = rfactor(b.image_values, b.bcr_approx_values)
-  for r, i, a in zip(b.radii, b.image_values, b.bcr_approx_values):
-    print(r, i, a)
-  STOP()
-
-  print(r, err)
-  for R, B, C in zip(b.R, b.B, b.C):
-    print("%20.16f %20.16f %20.16f"%(R, B, C) )
+  #for R, B, C in zip(b.R, b.B, b.C):
+  #  print("%20.16f %20.16f %20.16f"%(R, B, C) )
   return group_args(
    d_min = d_min,
    dist  = dist,
