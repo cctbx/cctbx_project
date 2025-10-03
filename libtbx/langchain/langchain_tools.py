74d8a9f2814e6ccbf5d77cab3b0531c80020b374
terwill <tterwilliger@newmexicoconsortium.org>
2025-10-03 16:12:06 -0600

    Clarify

--- libtbx/langchain/langchain_tools.py
+++ libtbx/langchain/langchain_tools.py
@@ -544,7 +544,7 @@ async def analyze_log_summary(log_info, llm, embeddings,
         error = error_message)
 
   except Exception as e:
-      error_message = "Reranking failed - try again in a few seconds..."+str(e)
+      error_message = "Reranking failed - try again in a couple minutes..."+str(e)
       print(error_message)
       return group_args(group_args_type = 'answer',
         analysis = None,
