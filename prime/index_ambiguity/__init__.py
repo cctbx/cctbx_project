e79c59350d26314715883be7fb38d8ee71d7e519
Daniel Paley <dwpaley@lbl.gov>
2025-10-17 11:27:31 -0400

    complete prime migration to cctbx-xfel

--- prime/index_ambiguity/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import, division, print_function
-import boost_adaptbx.boost.python as bp
-ext = bp.import_ext("prime_index_ambiguity_ext")
-from prime_index_ambiguity_ext import *
