4fba27fbfaa418ec8c4a4b924aabc8d53bd8d931
dcliebschner <dcliebschner@lbl.gov>
2025-10-23 14:25:52 -0400

    Refactor real_space_correlation to use program template

--- mmtbx/command_line/real_space_correlation.py
+++ mmtbx/command_line/real_space_correlation.py
@@ -2,11 +2,17 @@
 from __future__ import absolute_import, division, print_function
 # LIBTBX_SET_DISPATCHER_NAME phenix.real_space_correlation
 
-from mmtbx import real_space_correlation
-import sys
+#from mmtbx import real_space_correlation
+#import sys
+#
+#if(__name__ == "__main__"):
+#  real_space_correlation.cmd_run(
+#    args         = sys.argv[1:],
+#    command_name = "phenix.real_space_correlation")
 
-if(__name__ == "__main__"):
-  real_space_correlation.cmd_run(
-    args         = sys.argv[1:],
-    command_name = "phenix.real_space_correlation")
+from iotbx.cli_parser import run_program
+from mmtbx.programs import real_space_correlation
+
+if __name__ == '__main__':
+  run_program(program_class=real_space_correlation.Program)
 
