96ebe3f390f69801887975ca6917bc81bd6e34d2
dcliebschner <dcliebschner@lbl.gov>
2026-03-06 16:37:36 -0800

    Ligand validation: add resolution attribute

--- mmtbx/validation/validate_ligands.py
+++ mmtbx/validation/validate_ligands.py
@@ -360,6 +360,7 @@ class ligand_result(object):
     }
 
     self._set_internals()
+    self.d_min = self.fmodel.f_obs().d_min()
     self._fragment()
 
     for attr, func in self._result_attrs.items():
