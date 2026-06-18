4241fb4791a0a829bc2f227fb57131b93ebaf4cb
Pavel Afonine <pafonine@lbl.gov>
2026-06-17 17:48:59 -0700

    Difference map implementation for cryoEM as implemented in Servalcat (CCP4/CCPEM)

---
+++ cctbx/maptbx/servalcat_difference_map.py
@@ -0,0 +1,83 @@
+from __future__ import absolute_import, division, print_function
+from scitbx.array_family import flex
+from cctbx import maptbx
+
+"""
+Implementation of difference map calculation following the exact description in:
+
+Yamashita, K., Palmer, C. M., Burnley, T. & Murshudov, G. N. (2021).
+Acta Crystallographica Section D, 77, 1282–1291.
+
+Hugely inefficient in favor of maximal transparency code <> paper. Though still
+rather fast.
+
+Actual implementation seems to use mask internally. They might be doing some
+sharpening too. Both hypotheses are inspired by looking at the log file from
+running Servalcat.
+
+reflections_per_bin needs to be automated.
+"""
+
+def formula_3(fo1, fo2):
+  x = fo1 - fo2
+  diff = x - flex.mean(x)
+  return flex.sum_sq(diff).real / diff.size() / 4
+
+def formula_8(fo, fc):
+  num = flex.sum( fo * flex.conj(fc) )
+  den = flex.sum_sq(fc).real  # = sum |Fc|^2
+  assert abs(den)>1.e-9
+  return (num.real) / den   # should be real; use real() for safety
+
+def formula_9(fo, fc, D, sig_n_sq):
+  diff = fo - D * fc          # complex residual Fo - D Fc
+  # Natively sum the squared moduli of the complex differences
+  var_U_T = (flex.sum_sq(diff).real / diff.size()) - sig_n_sq
+  return max(0.0, var_U_T)
+
+def formula_4(fo1, fo2):
+  return maptbx.cc_complex_complex(f_1 = fo1, f_2 = fo2)
+
+def formula_5(FSChalf):
+  return 2 * FSChalf / (FSChalf + 1)
+
+def formula_13(sig_UT_sq, sig_n_sq):
+  return sig_UT_sq / (sig_UT_sq + sig_n_sq)
+
+def formula_17(FSCfull, fo, fc, w, D):
+  num = w * (fo - D * fc)
+  den = ( FSCfull * flex.sum_sq(fo).real / fo.size() )**0.5
+  return num/den
+
+def one_bin(fo, fo1, fo2, fc):
+  sig_n_sq  = formula_3(fo1 = fo1, fo2 = fo2)
+  D         = formula_8(fo = fo, fc = fc)
+  sig_UT_sq = formula_9(fo = fo, fc = fc, D = D, sig_n_sq = sig_n_sq)
+  FSChalf   = formula_4(fo1 = fo1, fo2 = fo2)
+  FSCfull   = formula_5(FSChalf = FSChalf)
+  w         = formula_13(sig_UT_sq = sig_UT_sq, sig_n_sq = sig_n_sq)
+  Fdiff     = formula_17(FSCfull = FSCfull, fo = fo, fc = fc, w = w, D = D)
+  return Fdiff
+
+def compute(fo, fo1, fo2, fc, reflections_per_bin = 5000):
+  fo1d = fo1.data()
+  fo2d = fo2.data()
+
+  fod = fo.data() # use ACTUAL data, not from 1/2 maps!
+  #fod  = (fo1d + fo2d) / 2.
+
+  fcd  = fc.data()
+  result = flex.complex_double(fod.size(), 0)
+  fo.setup_binner(reflections_per_bin = reflections_per_bin)
+  ds = fo.d_spacings().data()
+  for i_bin in fo.binner().range_used():
+    sel = fo.binner().selection(i_bin)
+    di = ds.select(sel)
+    #print("%6.4f %6.4f"%(flex.min(di), flex.max(di)))
+    Fdiff = one_bin(
+      fo  = fod.select(sel),
+      fo1 = fo1d.select(sel),
+      fo2 = fo2d.select(sel),
+      fc  = fcd.select(sel))
+    result.set_selected(sel, Fdiff)
+  return fo.array(data = result)
